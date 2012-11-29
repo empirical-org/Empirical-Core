cfe2a49591d376ebe4d202e606eb90bee6cee41a
Devin Wadsworth <devinwadsworth@gmail.com>
2012-11-29 12:11:39 -0800

    youtube and slideshow partials + second page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,17 @@
 class PagesController < ApplicationController
   def home
+    @video_id = '6H07A5RYBns'
+  end
+
+  def the_peculiar_institution
+    @video_id = 'sIcGaamTFxk'
+
+    @slides = Dir.entries('app/assets/images/slides/the_peculiar_institution')
+    @slides.delete_if do |image|
+      !image.include?('.jpg') &&
+      !image.include?('.png') &&
+      !image.include?('.gif')
+    end
+    @slides.map! { |slide| slide = "slides/the_peculiar_institution/#{slide}" }
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ PGSite::Application.routes.draw do
   resource :session
 
   root to: 'pages#home'
+  get 'the_peculiar_institution' => 'pages#the_peculiar_institution'
 end
