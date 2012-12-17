7a9b662a5b5e133c0d04b857f5d253960bc7b9e4
Quinn Shanahan <q.shanahan@gmail.com>
2012-12-17 08:07:20 -0600

    setup interface

---
+++ app/controllers/courses_controller.rb
@@ -0,0 +1,5 @@
+class CoursesController < ApplicationController
+  def index
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,5 @@ PGSite::Application.routes.draw do
   get 'democracy_in_america' => 'pages#democracy_in_america'
   get 'aggregation' => 'pages#aggregation'
 
-  resources :comments, :courses
+  resources :comments, :courses, :lectures, :chapters
 end
