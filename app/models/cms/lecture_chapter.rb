10505003712748519adf1d19fd13c6fe051c5dbb
Quinn Shanahan <q.shanahan@gmail.com>
2013-01-16 17:36:37 -0500

    add chapter groups

--- app/models/cms/lecture_chapter.rb
+++ app/models/cms/lecture_chapter.rb
@@ -1,9 +1,9 @@
 class CMS::LectureChapter < ActiveRecord::Base
   self.table_name = 'lecture_chapters'
-  belongs_to :lecture, class_name: 'CMS::Lecture'
+  belongs_to :chapter_group, class_name: 'CMS::ChapterGroup'
   has_many :lecture_chapter_images, class_name: 'CMS::LectureChapterImage'
   has_many :questions
-  attr_accessible :annotatable_text, :chart_embed_code, :citation_text, :discussion_text, :globe_embed_code, :lecture_id, :position, :quiz_embed_code, :reading_text, :slideshow_embed_code, :subtitle, :title, :youtube_embed_code
+  attr_accessible :annotatable_text, :chapter_group_id, :chart_embed_code, :citation_text, :discussion_text, :globe_embed_code, :position, :quiz_embed_code, :reading_text, :slideshow_embed_code, :subtitle, :title, :youtube_embed_code
   include CMS::Orderable
   orderable(:position, order_scope: "lecture")
 
