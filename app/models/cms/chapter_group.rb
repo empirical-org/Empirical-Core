a8489a6fd7ba609813c5fbee3a4519eb5f33ea16
Quinn Shanahan <q.shanahan@gmail.com>
2013-01-16 20:40:20 -0500

    add chapter groups

--- app/models/cms/chapter_group.rb
+++ app/models/cms/chapter_group.rb
@@ -2,7 +2,9 @@ class CMS::ChapterGroup < ActiveRecord::Base
   self.table_name = 'chapter_groups'
   belongs_to :lecture, class_name: 'CMS::Lecture'
   has_many :lecture_chapters, class_name: 'CMS::LectureChapter'
-  attr_accessible :lecture_id, :title
+  attr_accessible :lecture_id, :position, :title
+  include CMS::Orderable
+  orderable(:position, order_scope: "lecture")
 
   def self.name
     'ChapterGroup'
