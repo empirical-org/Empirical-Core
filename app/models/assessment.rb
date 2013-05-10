b66e97e41bc2050e517674ed029867a9ee5fc277
mzemel <admin@kpsu.org>
2013-05-10 14:54:59 -0700

    Commit for heroku2

--- app/models/assessment.rb
+++ app/models/assessment.rb
@@ -1,4 +1,12 @@
 class Assessment < ActiveRecord::Base
-	attr_accessible :title, :body, :chapter_id
-	belongs_to :chapter
+  attr_accessible :title, :body, :chapter_id
+  belongs_to :chapter
+
+  def chunks
+    @chunks ||= ::GrammarChunker.chunk(parsed)
+  end
+
+  def parsed
+    @parsed ||= ::GrammarParser.new.parse(body)[:questions]
+  end
 end
