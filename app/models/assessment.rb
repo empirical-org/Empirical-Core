ba2ee29857d57c7f44aed52c5f67c365f603ac88
Quinn Shanahan <quinn@stacksocial.com>
2013-07-27 22:20:22 -0700

    basic newline support

--- app/models/assessment.rb
+++ app/models/assessment.rb
@@ -7,12 +7,19 @@ class Assessment < ActiveRecord::Base
   end
 
   def parsed
-    @parsed ||= ::GrammarParser.new.parse(body)[:questions]
+    @parsed ||= ::GrammarParser.new.parse(markdown(body.to_s))[:questions]
   end
 
   def as_json *args
     super.merge({
       rule_position: chapter.rule_position
     })
+  end
+
+  private
+
+  def markdown text
+    # fake markdown
+    text.gsub("\n", "<br>").html_safe
   end
 end
