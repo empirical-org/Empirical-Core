0dca4dd6f2c8d54360a1ef1549b162c7ad589501
Quinn Shanahan <quinn@stacksocial.com>
2013-07-28 06:51:57 -0700

    Update to rails 4

--- config/application.rb
+++ config/application.rb
@@ -17,6 +17,12 @@ module EmpiricalGrammar
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
+    # Custom directories with classes and modules you want to be autoloadable.
+    config.autoload_paths += %W(
+      #{config.root}/app/controllers/concerns
+      #{config.root}/lib
+    )
+
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
