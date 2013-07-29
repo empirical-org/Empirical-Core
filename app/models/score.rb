8f6c1e4e72a8c2b117bb331e7c91f1e1891a44d2
Quinn Shanahan <quinn@stacksocial.com>
2013-07-28 23:11:06 -0700

    show final scores

--- app/models/score.rb
+++ app/models/score.rb
@@ -4,6 +4,7 @@ class Score < ActiveRecord::Base
   serialize :practice_lesson_input, Hash
   serialize :review_lesson_input, Hash
   serialize :missed_rules, Array
+  serialize :score_values, Hash
 
   def missed_rules
     Rule.where(id: super)
@@ -11,5 +12,10 @@ class Score < ActiveRecord::Base
 
   def give_time
   	self.completion_date = Time.now
+  end
+
+  def finalize!
+    self.score_values = ScoreFinalizer.new(self).results
+    save!
   end
 end
---
+++ app/models/score_finalizer.rb
@@ -0,0 +1,32 @@
+class ScoreFinalizer
+  delegate :practice_lesson_input, :review_lesson_input, :assignment,
+           :missed_rules, :score_values, to: :@score
+
+  def initialize score
+    @score = score
+  end
+
+  def results
+    {
+      practice_percentage: grade_lessons(:practice),
+      story_percentage:    1 - missed_rules.length.to_f / story_rule_ids.length,
+      review_percentage:   grade_lessons(:review)
+    }
+  end
+
+  private
+
+  def grade_lessons(step)
+    lesson_input = send(:"#{step}_lesson_input")
+
+    missed_lessons = lesson_input.reject do |lesson_id, input|
+      Lesson.find(lesson_id).answers.reject{ |a| a.strip == input.strip }.empty?
+    end
+
+    (lesson_input.keys - missed_lessons.keys).length.to_f / lesson_input.length
+  end
+
+  def story_rule_ids
+    assignment.chapter.assessment.chunks.select{ |c| c.is_a?(GrammarQuestion) }.map(&:grammar).uniq
+  end
+end
