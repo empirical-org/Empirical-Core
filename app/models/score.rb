4c137e8e5681b97d42e97abbfe050683382197c3
Quinn Shanahan <quinn@stacksocial.com>
2013-07-28 19:00:32 -0700

    simple stepper

--- app/models/score.rb
+++ app/models/score.rb
@@ -1,8 +1,13 @@
 class Score < ActiveRecord::Base
   belongs_to :assignment
   belongs_to :user
-  serialize :lesson_input, Hash
+  serialize :practice_lesson_input, Hash
+  serialize :review_lesson_input, Hash
   serialize :missed_rules, Array
+
+  def missed_rules
+    Rule.where(id: super)
+  end
 
   def give_time
   	self.completion_date = Time.now
