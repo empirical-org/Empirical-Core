26a4d0b516df3a6dcf1a6f15d4ed9f3a341e9f5e
Quinn Shanahan <quinn@stacksocial.com>
2013-08-02 00:01:45 -0700

    more fixes for viewing chapters

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -2,4 +2,14 @@ class Assignment < ActiveRecord::Base
   belongs_to :user
   belongs_to :chapter
   has_many :scores, dependent: :destroy
+
+  class << self
+    def temporary chapter
+      assignment = new
+      assignment.temporary = true
+      assignment.chapter = chapter
+      assignment.save!
+      assignment
+    end
+  end
 end
