e8970baef7dfb1c495ebcb8997b988af6e84f4b4
Quinn Shanahan <quinn@stacksocial.com>
2013-08-01 22:52:44 -0700

    do not uglify

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,7 +24,7 @@ EmpiricalGrammar::Application.configure do
   config.serve_static_assets = true
 
   # Compress JavaScripts and CSS.
-  config.assets.js_compressor = :uglifier
+  # config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
