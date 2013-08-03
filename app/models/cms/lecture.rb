2dbf3078bcc20290ab04160564ef70fcbdaea641
Quinn Shanahan <quinn@stacksocial.com>
2013-08-03 11:10:23 -0700

    cleanup

--- app/models/cms/lecture.rb
+++
@@ -1,9 +0,0 @@
-class CMS::Lecture < ActiveRecord::Base
-  self.table_name = 'lectures'
-  belongs_to :course, class_name: 'CMS::Course'
-  has_many :chapter_groups, class_name: 'CMS::ChapterGroup'
-
-  def self.name
-    'Lecture'
-  end
-end
--- app/models/cms/lecture_chapter_image.rb
+++
@@ -1,9 +0,0 @@
-class CMS::LectureChapterImage < ActiveRecord::Base
-  self.table_name = 'lecture_chapter_images'
-  belongs_to :lecture_chapter, class_name: 'CMS::LectureChapter'
-  mount_uploader :image_file, CMS::Uploader
-
-  def self.name
-    'LectureChapterImage'
-  end
-end
