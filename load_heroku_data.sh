fef7f6ba03453796b553b52ccbae0361e208b391
Quinn Shanahan <quinn@stacksocial.com>
2013-08-03 07:46:24 -0700

    cleanup

--- load_heroku_data.sh
+++ load_heroku_data.sh
@@ -1,4 +1,4 @@
 heroku pgbackups:capture --expire
 curl -o latest.dump `heroku pgbackups:url`
 pg_restore --verbose --clean --no-acl --no-owner -h localhost -d emp_gr_development latest.dump
-rm latest.dump
+# rm latest.dump
