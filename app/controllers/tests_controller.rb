62b47d75426b08412a0eeedb3c0a70df7894e152
Quinn Shanahan <quinn@stacksocial.com>
2013-08-03 11:26:28 -0700

    better links for teachers

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,15 +1,8 @@
 class TestsController < BaseChapterController
 	before_filter :signed_in!
 
-  def index
-    redirect_to chapter_test_practice_index_path(@chapter, step: "practice")
-  end
-
   def show
-    @chapter = @assignment.chapter
-    @assessment = @chapter.assessment
-    @user = current_user
-    render :index
+    redirect_to chapter_test_practice_index_path(@chapter, step: "practice")
   end
 
   def story
