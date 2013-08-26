9c787253f0fe2d37cd9e37c6248f414fb36e939d
Quinn Shanahan <quinn@stacksocial.com>
2013-08-25 22:30:54 -0700

    db import script

--- load_heroku_data.sh
+++ load_heroku_data.sh
@@ -1,4 +1,4 @@
-heroku pgbackups:capture --expire
-curl -o latest.dump `heroku pgbackups:url`
+heroku pgbackups:capture --expire --app empirical-grammar
+curl -o latest.dump `heroku pgbackups:url --app empirical-grammar`
 pg_restore --verbose --clean --no-acl --no-owner -h localhost -d emp_gr_development latest.dump
 # rm latest.dump
