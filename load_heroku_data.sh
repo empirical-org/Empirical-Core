b4653a04ff75a45393cb40d60b1040d874503e29
Quinn Shanahan <quinn@stacksocial.com>
2013-09-15 17:43:27 -0700

    localhost is too unpredictable

--- load_heroku_data.sh
+++ load_heroku_data.sh
@@ -1,4 +1,4 @@
 heroku pgbackups:capture --expire --app empirical-grammar
 curl -o latest.dump `heroku pgbackups:url --app empirical-grammar`
-pg_restore --verbose --clean --no-acl --no-owner -h localhost -d emp_gr_development latest.dump
+pg_restore --verbose --clean --no-acl --no-owner -h 127.0.0.1 -d emp_gr_development latest.dump
 # rm latest.dump
