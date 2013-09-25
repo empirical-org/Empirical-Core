6c6ce54948f27837c6a61fd2374139244e48fce8
Quinn Shanahan <quinn@stacksocial.com>
2013-09-25 16:15:40 -0700

    Hide the secret token

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-EmpiricalGrammar::Application.config.secret_key_base = '13e4205dc0802fbaf518ea185f73323c7fed319078c757a441e8a2b96cdb864c8c94d6a98e1fe434b440d97fbc5df8d30bb41b8284d9d2d47abbd50962b7b4cf'
+EmpiricalGrammar::Application.config.secret_key_base = ENV['APP_SECRET']
