a44e0e2dcf52234af8774d85269899c16641cef3
Quinn Shanahan <quinn@stacksocial.com>
2013-09-26 07:15:18 -0700

    Fixes to get tests to pass with Travis

--- test/models/peg_test.rb
+++ test/models/peg_test.rb
@@ -5,8 +5,14 @@ describe GrammarParser do
     @parser = GrammarParser.new
   end
 
-  it 'parses plain text' do
-    @parser.parse('This is just some regular text')
+  it 'fails without question' do
+    assert_raises(Parslet::ParseFailed) do
+      @parser.parse('This is just some regular text')
+    end
+  end
+
+  it 'succeeds with question' do
+    @parser.parse('This is just {+some|-som} regular text')
   end
 
   it 'fails when empty' do
