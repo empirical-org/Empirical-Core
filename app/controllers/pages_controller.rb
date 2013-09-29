aa15b51979affcd522279f2933b731ab2a38b0b5
Quinn Shanahan <quinn@stacksocial.com>
2013-09-29 12:54:49 -0700

    Develop page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,16 +7,8 @@ class PagesController < ApplicationController
     @assessment = @chapter.assessment
   end
 
-  def the_peculiar_institution
-    @video_id = 'sIcGaamTFxk'
-
-    @slides = Dir.entries('app/assets/images/slides/the_peculiar_institution')
-    @slides.delete_if do |image|
-      !image.include?('.jpg') &&
-      !image.include?('.png') &&
-      !image.include?('.gif')
-    end
-    @slides.map! { |slide| slide = "slides/the_peculiar_institution/#{slide}" }
+  def develop
+    @body_class = 'white-page'
   end
 
   def democracy_in_america
