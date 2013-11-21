4e99936c07995f7655075a85d8b948c404fe60c5
Michael Zemel <michael.zemel@gmail.com>
2013-11-20 16:10:08 -0800

    Removed reference to non-existent role param in @user creation for #new

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -2,7 +2,7 @@ class AccountsController < ApplicationController
   before_filter :signed_in!, only: [:edit, :update]
 
   def new
-    @user = User.new(role: params[:as])
+    @user = User.new
   end
 
   def create
