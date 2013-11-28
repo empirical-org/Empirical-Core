c3d4312271d6329198859766624950c57f03a2ef
Quinn Shanahan <q.shanahan@gmail.com>
2013-11-28 12:26:50 -0600

    Fixes for Travis deploy

--- config/application.rb
+++ config/application.rb
@@ -47,5 +47,7 @@ module EmpiricalGrammar
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     config.hamlcoffee.escapeHtml = false
+
+    config.assets.initialize_on_precompile = false
   end
 end
