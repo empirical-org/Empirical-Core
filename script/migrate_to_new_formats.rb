f481ceb69ba7e17e1a94db0e5388079d0c58a533
Quinn Shanahan <q.shanahan@gmail.com>
2014-01-28 01:57:30 -0600

    syntax error

--- script/migrate_to_new_formats.rb
+++ script/migrate_to_new_formats.rb
@@ -20,7 +20,7 @@ Chapter.find_by_id(55).try(:destroy)
 story_class = ActivityClassification.create!(
   name: 'Story',
   key: 'story'
-  module_url: 'http://quill.lvh.me:3002/stories/module'
+  module_url: 'http://quill.lvh.me:3002/stories/module',
   form_url:   'http://quill.lvh.me:3002/stories/form'
 )
 
