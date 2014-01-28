3932f7d4ce4b17c56dd82078728a8ff828f0ded7
Quinn Shanahan <q.shanahan@gmail.com>
2014-01-28 01:58:11 -0600

    update url

--- script/migrate_to_new_formats.rb
+++ script/migrate_to_new_formats.rb
@@ -20,8 +20,8 @@ Chapter.find_by_id(55).try(:destroy)
 story_class = ActivityClassification.create!(
   name: 'Story',
   key: 'story'
-  module_url: 'http://quill.lvh.me:3002/stories/module',
-  form_url:   'http://quill.lvh.me:3002/stories/form'
+  module_url: 'http://quill-questions-module.herokuapp.com/stories/module',
+  form_url:   'http://quill-questions-module.herokuapp.com/stories/form'
 )
 
 practice_class = ActivityClassification.create!(
