7638d79892d6b9497786169ba3a80ad928750d1f
Quinn Shanahan <q.shanahan@gmail.com>
2014-02-04 16:53:41 -0600

    Hopefully this will make it recognize the pkey on sections

---
+++ db/migrate/20140204201027_rename_sections_sequence.rb
@@ -0,0 +1,10 @@
+class RenameSectionsSequence < ActiveRecord::Migration
+  def up
+    execute 'ALTER SEQUENCE chapter_levels_id_seq RENAME TO sections_id_seq'
+    execute 'ALTER TABLE ONLY sections ADD CONSTRAINT sections_pkey PRIMARY KEY (id)'
+  end
+
+  def down
+    execute 'ALTER SEQUENCE sections_id_seq RENAME TO chapter_levels_id_seq'
+  end
+end
