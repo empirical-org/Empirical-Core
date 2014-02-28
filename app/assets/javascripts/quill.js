ffdc31dc7c2dc5efc73830c51649e9f456c27a82
Quinn Shanahan <q.shanahan@gmail.com>
2014-02-27 22:47:34 -0600

    updates to core

--- app/assets/javascripts/quill.js
+++ app/assets/javascripts/quill.js
@@ -1,3 +1,65 @@
+function Counter(interval, interval_action, idle, idle_action, callbacks) {
+
+  this.interval = interval;
+  this.interval_action = interval_action;
+
+  this.idle = idle;
+  this.idle_action = idle_action;
+
+  this.callbacks = (callbacks === undefined ? {} : callbacks);
+
+  this.interval_timer = null;
+  this.idle_timer = null;
+  this.counter = idle;
+}
+
+Counter.prototype =  {
+  play: function() {
+    this.interval_timer =
+      window.setInterval(this.interval_action, this.interval);
+    this.timeout();
+
+    if(this.callbacks['play'] !== undefined) this.callbacks['play'];
+  },
+  stop: function() {
+    window.clearInterval(this.interval_timer);
+    window.clearTimeout(this.idle_timer);
+    this.counter = this.idle;
+
+    if(this.callbacks['stop'] !== undefined) this.callbacks['stop'];
+  },
+  pause: function() {
+    window.clearInterval(this.interval_timer);
+    window.clearTimeout(this.idle_timer);
+
+    if(this.callbacks['pause'] !== undefined) this.callbacks['pause'];
+  },
+  replay: function() {
+    this.stop();
+    this.start();
+
+    if(this.callbacks['replay'] !== undefined) this.callbacks['replay'];
+  },
+  timeout: function() {
+    if(this.counter <= 0) {
+      this.pause();
+      this.idle_action();
+    } else {
+      var that = this;
+      this.idle_timer = window.setTimeout(function() {
+        that.counter -= 1000;
+        that.timeout();
+      }, 1000);
+    }
+  },
+  in_timeout: function() {
+    return this.counter <= 0;
+  },
+  refresh: function() {
+    this.counter = this.idle;
+  }
+}
+
 // constructor. Takes the host of the target as an argument.
 window.Quill = function Quill (host) {
   // assign host.
@@ -141,4 +203,153 @@ jQuery.extend(Quill.prototype, {
       this.events[data.messageType](data.payload);
     }.bind(this));
   }
+});
+
+jQuery.extend(Quill.prototype, {
+
+  //
+  //  idleCheck
+  //
+  //  This function handles client idle checking.
+  //
+  //
+  idleCheck: function(options) {
+
+    var settings = jQuery.extend({
+      autoping: true,
+      refreshevents: 'mousemove keydown click',
+      interval: 1000,  // secs
+      idle: 3000,  // secs
+      to: 'quill-api.org',
+      profiles: {
+        'START': 'start',
+        'PAUSE': 'pause',
+        'PING': 'ping',
+        'DONE': 'done',
+        'STOP': 'stop'
+      }
+    }, options);
+
+    //
+    //  Actvities
+    //
+    //  This object holds all methods for user logging.
+    //
+    function Activities(settings) {
+
+      this.settings = settings;
+      this.sleepCntr = null;
+    }
+
+    Activities.prototype = {
+
+      init: function() {
+
+        // send initial message
+        this.ping(this.settings.profiles['START']);
+
+        // polling mode
+        if(this.settings.autoping) this.autoping('on');
+
+      },
+
+      //
+      //  Activities.ping
+      //
+      //  Sends a user activity update message to a listening server.
+      //
+      ping: function(type) {
+
+        // send activity to listener
+        jQuery.ajax(this.settings.to, {
+          type: 'post',
+          data: {
+            'ping': 'true',
+            'activity_log_item': {
+              'created_at': new Date(),
+              'activity_type': type,
+              // TODO figure out how the data is constructed
+              'activity_data': this.__eval({})
+            }
+          },
+          success: function(resp) {
+            // success
+          },
+          error: function(jqXhr, err) {
+            // error
+            console.log(err);
+          },
+        });
+
+        // DOM trigger for ping action
+        jQuery(document).trigger(type+'.activities');
+      },
+
+      //
+      //  Activities.autoping
+      //
+      //  Enables automated user activity logging.
+      //
+      autoping: function(mode) {
+
+        $this = this;
+
+        if(
+          mode === "on" ||
+          mode === true
+        ) {
+          this.sleepCntr = new Counter(
+            settings.interval,
+            function() { $this.ping($this.settings.profiles['PING']); },
+            settings.idle,
+            function() { $this.ping($this.settings.profiles['PAUSE']); },
+            { stop: function() { $this.ping($this.settings.profiles['STOP']); } }
+          );
+          this.sleepCntr.play();
+
+          var sleepCntr = this.sleepCntr;
+          jQuery(document).on(settings.refreshevents, function(e) {
+            if( sleepCntr.in_timeout()) {
+              sleepCntr.stop();
+              sleepCntr.play();
+            } else sleepCntr.refresh();
+          });
+        } else if(
+          mode === "off" ||
+          mode === false
+        ) {
+          this.sleepCntr.stop();
+          this.sleepCntr = null;
+
+          jQuery(document).off(settings.refreshevents);
+        } else if(
+          mode === "toggle" ||
+          mode === undefined
+        ) {
+          this.sleepCntr === null ? this.autoping('on') : this.autoping('off');
+        }
+      },
+
+      //
+      //  Activities.__eval
+      //
+      //  Maps an object of functions to an object of return values. Non
+      //  function values are mapped to themselves.
+      //
+      __eval: function(hash) {
+
+        evaled_hash = {};
+        jQuery.each(hash, function(i,e) {
+          evaled_hash[i] = typeof e === "function" ? e() : e;
+        });
+        return evaled_hash
+
+      }
+    } // end Activities
+
+    // instantiate Activities object
+    logger = new Activities(settings);
+    logger.init();
+
+  } // end idleCheck()
 });
