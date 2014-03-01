d6b5be2411cdc250b212e49fd2c14ba1fcbc3916
Chris Jeon <chris0374@gmail.com>
2014-03-01 17:50:23 -0500

    filled in some user model specs

--- db/schema.rb
+++ db/schema.rb
@@ -16,15 +16,15 @@ ActiveRecord::Schema.define(version: 20131110031852) do
   create_table "assessments", force: true do |t|
     t.text     "body"
     t.integer  "chapter_id"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.text     "instructions"
   end
 
   create_table "categories", force: true do |t|
     t.text     "title"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "chapter_levels", force: true do |t|
@@ -36,8 +36,8 @@ ActiveRecord::Schema.define(version: 20131110031852) do
 
   create_table "chapters", force: true do |t|
     t.string   "title"
-    t.datetime "created_at",           null: false
-    t.datetime "updated_at",           null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.integer  "workbook_id"
     t.text     "article_header"
     t.text     "rule_position"
@@ -52,8 +52,8 @@ ActiveRecord::Schema.define(version: 20131110031852) do
     t.string   "classcode"
     t.integer  "chapter_id"
     t.datetime "due_date"
-    t.datetime "created_at",                   null: false
-    t.datetime "updated_at",                   null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.boolean  "temporary",    default: false, null: false
     t.integer  "classroom_id"
   end
@@ -66,40 +66,27 @@ ActiveRecord::Schema.define(version: 20131110031852) do
     t.datetime "updated_at"
   end
 
-  create_table "comments", force: true do |t|
-    t.string   "title"
-    t.text     "body"
-    t.integer  "user_id"
-    t.datetime "created_at",         null: false
-    t.datetime "updated_at",         null: false
-    t.string   "ancestry"
-    t.string   "reply_type"
-    t.integer  "lecture_chapter_id"
-  end
-
-  add_index "comments", ["ancestry"], name: "index_comments_on_ancestry", using: :btree
-
   create_table "file_uploads", force: true do |t|
     t.string   "name"
     t.string   "file"
     t.text     "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "grammar_rules", force: true do |t|
     t.string   "identifier"
     t.text     "description"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.text     "practice_lesson"
     t.integer  "author_id"
   end
 
   create_table "grammar_tests", force: true do |t|
     t.text     "text"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "homepage_news_slides", force: true do |t|
@@ -116,8 +103,8 @@ ActiveRecord::Schema.define(version: 20131110031852) do
     t.string   "name"
     t.string   "description"
     t.text     "content"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "rule_examples", force: true do |t|
@@ -141,8 +128,8 @@ ActiveRecord::Schema.define(version: 20131110031852) do
 
   create_table "rule_questions", force: true do |t|
     t.text     "body"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.integer  "rule_id"
     t.text     "prompt"
     t.text     "instructions"
@@ -151,22 +138,12 @@ ActiveRecord::Schema.define(version: 20131110031852) do
 
   create_table "rules", force: true do |t|
     t.text     "title"
-    t.datetime "created_at",                 null: false
-    t.datetime "updated_at",                 null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.integer  "category_id"
     t.integer  "workbook_id",    default: 1
     t.text     "description"
     t.string   "classification"
-  end
-
-  create_table "rules_misseds", force: true do |t|
-    t.integer  "rule_id"
-    t.integer  "user_id"
-    t.integer  "assessment_id"
-    t.datetime "time_take"
-    t.boolean  "missed"
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
   end
 
   create_table "scores", force: true do |t|
@@ -175,8 +152,8 @@ ActiveRecord::Schema.define(version: 20131110031852) do
     t.datetime "completion_date"
     t.integer  "items_missed"
     t.integer  "lessons_completed"
-    t.datetime "created_at",                                 null: false
-    t.datetime "updated_at",                                 null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.text     "practice_step_input"
     t.text     "review_step_input"
     t.text     "missed_rules"
@@ -191,8 +168,8 @@ ActiveRecord::Schema.define(version: 20131110031852) do
     t.string   "email"
     t.string   "password_digest"
     t.string   "role",            default: "user"
-    t.datetime "created_at",                       null: false
-    t.datetime "updated_at",                       null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.string   "classcode"
     t.boolean  "active",          default: false
     t.string   "username"
@@ -201,8 +178,8 @@ ActiveRecord::Schema.define(version: 20131110031852) do
 
   create_table "workbooks", force: true do |t|
     t.string   "title"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,16 +2,64 @@ require 'spec_helper'
 
 describe User do
   it 'Can auth by user or email' do
-    User.create(username: 'test',          password: '123456', password_confirmation: '123456')
-    User.create(email: 'test@example.com', password: '654321', password_confirmation: '654321')
+    User.create(username: 'test',          
+                password: '123456', 
+                password_confirmation: '123456')
+    User.create(email: 'test@example.com', 
+                password: '654321', 
+                password_confirmation: '654321')
 
     expect(User.authenticate(email: 'test',             password: '123456')).to be_true
     expect(User.authenticate(email: 'test@example.com', password: '654321')).to be_true
   end
 
-  it 'requires email if a teacher'
-  it 'requires email if no username present'
-  it 'is ok with a username if not a teacher and presented with a username'
+  describe 'requires email if user is teacher' do
+    before do
+      @teacher = User.create(username: 'teacher_test', 
+                             password: 'password', 
+                             password_confirmation: 'password',
+                             role: 'teacher')
+    end
+    it 'teacher should not be valid' do
+      @teacher.should_not be_valid
+    end
+  end
+
+  describe 'requires email if no username is present' do
+    describe 'teacher' do
+      before do
+        @teacher = User.create(password: 'password', 
+                               password_confirmation: 'password',
+                               role: 'teacher')
+      end
+      it 'teacher should not be valid' do
+        @teacher.should_not be_valid
+      end
+    end
+
+    describe 'student' do
+      before do
+        @student = User.create(password: 'password', 
+                               password_confirmation: 'password',
+                               role: 'student')
+      end
+      it 'student should not be valid' do
+        @student.should_not be_valid
+      end
+    end
+  end
+
+  describe 'is valid if not a teacher and presented with a username' do
+    before do
+      @student = User.create(username: 'student_test',
+                             password: 'password', 
+                             password_confirmation: 'password',
+                             role: 'student')
+    end
+    it 'student should not be valid' do
+      @student.should be_valid
+    end
+  end
   it 'checks for a password confirmation only when a password is present on update'
   it 'only requires a password on create'
   it 'doesn\'t care about all the validation stuff when the user is temporary'
