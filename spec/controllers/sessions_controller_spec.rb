f2316a8cd027188d88826b318dff9af400115095
Chris Jeon <chris0374@gmail.com>
2014-03-02 18:59:36 -0500

    filled in controller tests for sessions_controller

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -2,26 +2,47 @@ require 'spec_helper'
 
 describe SessionsController do
   before do
-    User.create(email: 'student@quill.org',
-                username: 'student1',
-                password: '12345',
-                password_confirmation: '12345',
-                role: 'student')
+    @user = User.create(email: 'student@quill.org',
+                        username: 'student1',
+                        password: '12345',
+                        password_confirmation: '12345',
+                        role: 'student')
   end
 
-  describe 'create with valid attributes' do
-    before do
-      post :create, user: {email: 'Student@quill.org', password: '12345'}
-    end
+  describe 'POST create' do
+    describe 'with valid attributes' do
+      before do
+        post :create, user: {email: 'Student@quill.org', password: '12345'}
+      end
 
-    it { should redirect_to profile_path }
+      it { should redirect_to profile_path }
+    end
+    describe 'with invalid attributes' do
+      before do
+        post :create, user: {email: 'Student@quill.org', password: 'wrong'}
+      end
+
+      it { should_not redirect_to profile_path }
+      it { should render_template :new }
+    end
   end
 
-  describe 'create with invalid attributes' do
+  describe 'DELETE destroy' do
     before do
-      post :create, user: {email: 'Student@quill.org', password: 'wrong'}
+      sign_in @user
+      delete :destroy
     end
 
-    it { should_not redirect_to profile_path }
+    it { should redirect_to root_path }
+    it { should set_the_flash.to 'Logged Out' }
+  end
+
+  describe 'GET new' do
+    before { get :new }
+
+    it { should respond_with :success }
+    it 'should assign @user as a new user' do
+      assigns(:user).should be_a_new User
+    end
   end
 end
