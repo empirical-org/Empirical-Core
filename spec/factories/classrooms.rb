02783427b67f779872be055edd9e193bed0ba35c
Chris Jeon <chris0374@gmail.com>
2014-03-02 19:12:21 -0500

    fixed sequence syntax error

--- spec/factories/classrooms.rb
+++ spec/factories/classrooms.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :classroom do
     teacher
-    sequence :name { |n| "Classroom #{n}" }
-    sequence :code { |n| "code-#{n}" }
+    sequence(:name) { |n| "Classroom #{n}" }
+    sequence(:code) { |n| "code-#{n}" }
   end
 end
