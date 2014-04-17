a3c7940454224014d26b71ee6799eada3173ce9c
Quinn Shanahan <q.shanahan@gmail.com>
2014-04-17 17:48:47 -0500

    max 10

--- script/parallel_migration.rb
+++ script/parallel_migration.rb
@@ -1,13 +1,16 @@
 system %q|/usr/local/heroku/bin/heroku run "rails r \"load('./script/begin_migration.rb')\"" -a empirical-grammar-staging|
 
-Chapter.all.map do |chapter|
-  puts 'loading '
-  sleep 2
-  thread = Thread.new do
-    sleep 5
-    puts 'starting now'
-    system %Q|/usr/local/heroku/bin/heroku run "rails r \\"load('./script/migrate_to_new_formats.rb')\\" #{chapter.id}" -a empirical-grammar-staging|
-  end
-  sleep 30
-  thread
-end.each(&:join)
+Chapter.all.each_slice(10) do |chapters|
+  chapters.map do |chapter|
+    puts 'loading '
+    sleep 2
+    thread = Thread.new do
+      sleep 5
+      puts 'starting now'
+      puts   %Q|/usr/local/heroku/bin/heroku run "rails r \\"load('./script/migrate_to_new_formats.rb')\\" #{chapter.id}" -a empirical-grammar-staging|
+      system %Q|/usr/local/heroku/bin/heroku run "rails r \\"load('./script/migrate_to_new_formats.rb')\\" #{chapter.id}" -a empirical-grammar-staging|
+    end
+    sleep 30
+    thread
+  end.each(&:join)
+end
