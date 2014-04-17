cab7be4f2caa0276ac646473d5c6cb15a00f9642
Quinn Shanahan <q.shanahan@gmail.com>
2014-04-17 14:32:42 -0500

    fixes for migration

--- script/parallel_migration.rb
+++ script/parallel_migration.rb
@@ -3,10 +3,11 @@ system %q|/usr/local/heroku/bin/heroku run "rails r \"load('./script/begin_migra
 Chapter.all.map do |chapter|
   puts 'loading '
   sleep 2
-  Thread.new do
+  thread = Thread.new do
     sleep 5
     puts 'starting now'
     system %Q|/usr/local/heroku/bin/heroku run "rails r \\"load('./script/migrate_to_new_formats.rb')\\" #{chapter.id}" -a empirical-grammar-staging|
   end
   sleep 30
+  thread
 end.each(&:join)
