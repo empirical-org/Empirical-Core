a279fabecaa0dd63e9139ac5a3e66108fd7b4979
Quinn Shanahan <q.shanahan@gmail.com>
2014-04-22 09:21:34 -0500

    require admin

--- app/controllers/cms/users_controller.rb
+++ app/controllers/cms/users_controller.rb
@@ -1,6 +1,6 @@
 class CMS::UsersController < ApplicationController
   before_filter :signed_in!
-  before_filter :admin!, only: [:sign_in, :create]
+  before_filter :admin!
 
   def index
     @users = if params[:q].present?
