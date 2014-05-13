3488e045be365ff93660b8a807dd887a5eab4bd2
Quinn Shanahan <q.shanahan@gmail.com>
2014-05-13 13:39:21 -0400

    migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140422211405) do
+ActiveRecord::Schema.define(version: 20140423225449) do
 
   create_table "activities", force: true do |t|
     t.string   "name"
