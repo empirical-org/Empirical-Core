0bdf5efca7ec736f187a0df061d894f48dc7efbd
Quinn Shanahan <q.shanahan@gmail.com>
2014-05-13 15:47:36 -0400

    Update aws credentials

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,11 +1,15 @@
+aws_credentials = AWS::Core::CredentialProviders::ENVProvider.new('AWS')
+
 CarrierWave.configure do |config|
   config.cache_dir = "#{Rails.root}/tmp/"
   config.storage = :fog
   config.permissions = 0666
+
   config.fog_credentials = {
     provider:              'AWS',
-    aws_access_key_id:     AWS.config.access_key_id,
-    aws_secret_access_key: AWS.config.secret_access_key,
+    aws_access_key_id:     aws_credentials.access_key_id,
+    aws_secret_access_key: aws_credentials.secret_access_key,
   }
+
   config.fog_directory = 'empirical-grammar-production'
 end
