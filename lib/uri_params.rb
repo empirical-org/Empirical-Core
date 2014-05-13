8586d4ded5f8759ab4befc165b7a311098af6b0d
Quinn Shanahan <q.shanahan@gmail.com>
2014-05-13 13:39:36 -0400

    fix bug with url parsing

--- lib/uri_params.rb
+++ lib/uri_params.rb
@@ -1,7 +1,7 @@
 module UriParams
   def add_param(url, param_name, param_value)
     uri = URI(url)
-    params = URI.decode_www_form(uri.query || []) << [param_name, param_value]
+    params = URI.decode_www_form(uri.query.to_s) << [param_name, param_value]
     uri.query = URI.encode_www_form(params)
     uri.to_s
   end
