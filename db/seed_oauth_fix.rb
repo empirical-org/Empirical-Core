065e0fddb42fc2d07bebcf721e1204aa514d5163
Quinn Shanahan <q.shanahan@gmail.com>
2014-05-22 12:49:10 -0400

    simple seed script to replace imported DB with DB that can be used locally with oauth / doorkeeper

---
+++ db/seed_oauth_fix.rb
@@ -0,0 +1,15 @@
+def subit rec, col
+  rec.update_column col, rec.send(col).sub('quill-questions-module.herokuapp.com', 'localhost:3002')
+end
+
+
+Doorkeeper::Application.all.each do |app|
+  subit(app, :redirect_uri)
+end
+
+ActivityClassification.all.each do |type|
+  subit(type, :form_url)
+  subit(type, :module_url)
+end
+
+Doorkeeper::Application.find_by_uid!('quill-lessons').update_column :secret, 'quill-lessons-non-secret'
