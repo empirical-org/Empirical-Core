7022e36f8a879c4929f72ed0bde1f016134031e2
Quinn Shanahan <q.shanahan@gmail.com>
2014-05-27 18:25:19 -0400

    Fix for test

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,15 +1,17 @@
-aws_credentials = AWS::Core::CredentialProviders::ENVProvider.new('AWS')
+unless Rails.env.test?
+  aws_credentials = AWS::Core::CredentialProviders::ENVProvider.new('AWS')
 
-CarrierWave.configure do |config|
-  config.cache_dir = "#{Rails.root}/tmp/"
-  config.storage = :fog
-  config.permissions = 0666
+  CarrierWave.configure do |config|
+    config.cache_dir = "#{Rails.root}/tmp/"
+    config.storage = :fog
+    config.permissions = 0666
 
-  config.fog_credentials = {
-    provider:              'AWS',
-    aws_access_key_id:     aws_credentials.access_key_id,
-    aws_secret_access_key: aws_credentials.secret_access_key,
-  }
+    config.fog_credentials = {
+      provider:              'AWS',
+      aws_access_key_id:     aws_credentials.access_key_id,
+      aws_secret_access_key: aws_credentials.secret_access_key,
+    }
 
-  config.fog_directory = 'empirical-grammar-production'
+    config.fog_directory = 'empirical-grammar-production'
+  end
 end
