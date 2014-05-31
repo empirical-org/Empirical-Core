1c833b1201b8a40fef53713e1894be2b8739070f
Jahde <jahde.hiywan@gmail.com>
2014-05-31 15:18:10 -0400

    changes made to app

--- db/migrate/20140522033151_add_queue_classic.rb
+++ db/migrate/20140522033151_add_queue_classic.rb
@@ -1,5 +1,7 @@
 class AddQueueClassic < ActiveRecord::Migration
   def self.up
+    ENV['DATABASE_URL'] ||= "postgres://super:4Rh0izHYfmfWx9cV@#{ENV['PG_PORT_5432_TCP_ADDR']}:#{ENV['PG_PORT_5432_TCP_PORT']}/compass_development"
+
     QC::Setup.create
   end
 
