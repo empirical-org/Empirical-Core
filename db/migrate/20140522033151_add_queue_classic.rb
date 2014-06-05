ce26fdf821dab88c3f4015316c386cab70224915
Quinn Shanahan <q.shanahan@gmail.com>
2014-06-05 17:45:52 -0400

    cleanup

--- db/migrate/20140522033151_add_queue_classic.rb
+++ db/migrate/20140522033151_add_queue_classic.rb
@@ -1,7 +1,5 @@
 class AddQueueClassic < ActiveRecord::Migration
   def self.up
-    ENV['DATABASE_URL'] ||= "postgres://super:4Rh0izHYfmfWx9cV@#{ENV['PG_PORT_5432_TCP_ADDR']}:#{ENV['PG_PORT_5432_TCP_PORT']}/compass_development"
-
     QC::Setup.create
   end
 
