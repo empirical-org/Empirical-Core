e451ce884dfeea27b8b61bc805f14a19d427fe8a
James Cox <james@imaj.es>
2014-07-28 18:43:10 -0400

    Convert specs to RSpec 3.0.3 syntax with Transpec
    This conversion is done by Transpec 2.3.6 with the following command:
        transpec
    
    * 8 conversions
        from: be_true
          to: be_truthy
    
    * 8 conversions
        from: it { should ... }
          to: it { is_expected.to ... }
    
    * 5 conversions
        from: obj.should
          to: expect(obj).to
    
    * 3 conversions
        from: describe 'some controller' { }
          to: describe 'some controller', :type => :controller { }
    
    * 3 conversions
        from: describe 'some request' { }
          to: describe 'some request', :type => :request { }
    
    * 3 conversions
        from: expect(obj).to have(n).error_on(...)
          to: expect(obj.error_on(...).size).to eq(n)
    
    * 3 conversions
        from: obj.should_not
          to: expect(obj).not_to
    
    * 2 conversions
        from: == expected
          to: eq(expected)
    
    * 2 conversions
        from: be_false
          to: be_falsey
    
    * 2 conversions
        from: describe 'some model' { }
          to: describe 'some model', :type => :model { }
    
    * 2 conversions
        from: its(:attr) { }
          to: describe '#attr' do subject { super().attr }; it { } end
    
    * 1 conversion
        from: expect(obj).to have(n).errors_on(...)
          to: expect(obj.errors_on(...).size).to eq(n)
    
    * 1 conversion
        from: it { should_not ... }
          to: it { is_expected.not_to ... }
    
    For more details: https://github.com/yujinakayama/transpec#supported-conversions

--- spec/models/ability_spec.rb
+++ spec/models/ability_spec.rb
@@ -1,6 +1,6 @@
 require "cancan/matchers"
 
-describe "User" do
+describe "User", :type => :model do
   describe "abilities" do
     subject(:ability) { Ability.new(user) }
     let(:user) { nil }
@@ -8,21 +8,21 @@ describe "User" do
     context "when an admin" do
       let(:user) { build(:admin) }
 
-      it { should be_able_to(:manage, ActivitySession.new) }
-      it { should be_able_to(:manage, User.new) }
+      it { is_expected.to be_able_to(:manage, ActivitySession.new) }
+      it { is_expected.to be_able_to(:manage, User.new) }
     end
 
     context "when a teacher" do
       let(:user) { build(:teacher) }
 
-      it { should be_able_to(:create, Classroom.new) }
+      it { is_expected.to be_able_to(:create, Classroom.new) }
     end
 
     context "when a user" do
       let(:user) { build(:user) }
 
-      it { should be_able_to(:read, ActivitySession.new(user_id: user.id)) }
-      it { should be_able_to(:create, ActivitySession.new) }
+      it { is_expected.to be_able_to(:read, ActivitySession.new(user_id: user.id)) }
+      it { is_expected.to be_able_to(:create, ActivitySession.new) }
     end
   end
 end
