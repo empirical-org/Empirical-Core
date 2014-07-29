474c0314501e2d1cf4bff953a77c530fd0d7cab9
James Cox <james@imaj.es>
2014-07-28 20:50:10 -0400

    make the tests pass, to enable a clean platform for further tests

--- spec/factories/base.rb
+++ spec/factories/base.rb
@@ -1,32 +1,4 @@
 FactoryGirl.define do
-  sequence(:name) { |i| "Name #{i}" }
-  sequence(:username) { |i| "Username#{i}" }
-  sequence(:key) { |i| "key-#{i}" }
-  sequence(:description) { |i| "Description #{i}" }
-  sequence(:email) { |n| "user#{n}@example.com" }
-
-  factory :user do
-    first_name 'Test'
-    last_name  'User'
-    role 'user'
-    password '123456'
-    password_confirmation '123456'
-    email
-
-    factory :admin do
-      role 'admin'
-    end
-
-    factory :teacher do
-      role 'teacher'
-    end
-
-    factory :student do
-      role 'student'
-      username
-      classroom
-    end
-  end
 
   factory :classroom do
     sequence(:name) { |i| "classroom #{i}" }
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,79 +1,102 @@
 require 'spec_helper'
 
 describe User, :type => :model do
-  it 'Can auth by user or email' do
-    User.create(username: 'test',          password: '123456', password_confirmation: '123456')
-    User.create(email: 'test@example.com', password: '654321', password_confirmation: '654321')
 
-    expect(User.authenticate(email: 'test',             password: '123456')).to be_truthy
-    expect(User.authenticate(email: 'test@example.com', password: '654321')).to be_truthy
-  end
+  let(:user) { FactoryGirl.build(:user) }
 
-  describe 'requires email if user is teacher' do
-    before do
-      @teacher = User.create(username: 'teacher_test',
-                             password: 'password',
-                             password_confirmation: 'password',
-                             role: 'teacher')
-    end
-    it 'teacher should not be valid' do
-      expect(@teacher).not_to be_valid
+  context "user authentication" do
+    it 'Can auth by user or email' do
+      User.create(username: 'test',          password: '123456', password_confirmation: '123456')
+      User.create(email: 'test@example.com', password: '654321', password_confirmation: '654321')
+
+      expect(User.authenticate(email: 'test',             password: '123456')).to be_truthy
+      expect(User.authenticate(email: 'test@example.com', password: '654321')).to be_truthy
     end
   end
 
-  describe 'requires email if no username is present' do
-    describe 'teacher' do
+  context "for all users" do
+    let(:user) { FactoryGirl.build(:user) }
+
+    describe "validations for email" do
       before do
-        @teacher = User.create(password: 'password',
-                               password_confirmation: 'password',
-                               role: 'teacher')
+        user.email = nil
+        user.username = nil
       end
-      it 'teacher should not be valid' do
-        expect(@teacher).not_to be_valid
-      end
-    end
 
-    describe 'student' do
-      before do
-        @student = User.create(password: 'password',
-                               password_confirmation: 'password',
-                               role: 'student')
+      it 'should be invalid without email or username' do
+        expect(user).not_to be_valid
       end
-      it 'student should not be valid' do
-        expect(@student).not_to be_valid
+
+      it 'requires email for no username' do
+        expect(user.errors[:email]).not_to be_nil
       end
     end
+
   end
 
-  describe 'is valid if not a teacher and presented with a username' do
-    before do
-      @student = User.create(username: 'student_test',
-                             password: 'password',
-                             password_confirmation: 'password',
-                             role: 'student')
+
+  context "behaving as a teacher" do
+
+    describe 'requires email' do
+
+      let!(:teacher) { FactoryGirl.build(:teacher, email: nil) }
+
+      it 'requires email if a teacher' do
+        expect(teacher).not_to be_valid
+        expect(teacher.errors[:email]).not_to be_nil
+      end
+
     end
-    it 'student should not be valid' do
-      expect(@student).to be_valid
+
+  end
+
+
+  context "behaving as a student" do
+
+    describe 'when no username is present' do
+
+      let!(:student) { FactoryGirl.build(:student, username: nil) }
+
+      it 'should be valid' do
+        expect(student).to be_valid
+      end
+
+      context 'when email also missing' do
+
+        before do
+          student.email = nil
+        end
+
+        it 'should be invalid' do
+          expect(student).not_to be_valid
+        end
+
+        it 'should have errors on the email attr' do
+          expect(student.errors[:email]).not_to be_nil
+        end
+
+      end
+
     end
+
   end
 
-  it 'can set a first and last name' do
-    expect(User.new(first_name: 'John').name).to eq('John')
-    expect(User.new(last_name: 'Doe').name).to eq('Doe')
-    expect(User.new(first_name: 'John', last_name: 'Doe').name).to eq('John Doe')
-  end
+  context "#name attribute" do
 
-  it 'requires email if a teacher' do
-    expect(User.new(role: 'teacher').error_on(:email).size).to eq(1)
-  end
+    it "has a first_name only" do
+      user.last_name = nil
+      expect(user.name).to eq('Test')
+    end
 
-  it 'requires email if no username present' do
-    expect(User.new.error_on(:email).size).to eq(1)
-  end
+    it "has a last name only" do
+      user.first_name = nil
+      expect(user.name).to eq('User')
+    end
 
-  it 'is ok with a username if not a teacher and presented with a username' do
-    expect(User.new(username: 'john').errors_on(:email).size).to eq(0)
-    expect(User.new(username: 'john', role: 'teacher').error_on(:email).size).to eq(1)
+    it "has a full name" do
+      expect(user.name).to eq('Test User')
+    end
+
   end
 
   context('#generate_student') do
@@ -100,24 +123,14 @@ describe User, :type => :model do
     end
   end
 
-  it 'checks for a password confirmation only when a password is present on update' do
-    user = User.create(username: 'test', password: '123456', password_confirmation: '123456')
+  context "#password attribute" do
+    let(:user) { FactoryGirl.build(:user, password: nil, password_confirmation: nil) }
 
-    expect(user.update(first_name: 'John', password: '123456', password_confirmation: '123456')).to be_truthy
-    expect(User.find(user.id).update(password_confirmation: '')).to be_truthy
-    expect(User.find(user.id).update(password: '', password_confirmation: '')).to be_truthy
-
-    expect(User.find(user.id).update(password: '123', password_confirmation: '')).to be_falsey
-    expect(User.find(user.id).update(password: '', password_confirmation: '123')).to be_falsey
+    it 'requires a password on create' do
+      expect(user).to_not be_valid
+    end
   end
 
-  it 'only requires a password on create' do
-    expect(User.create(username: 'test')).to_not be_valid
-
-    other_user = User.create(email: 'test@example.com', password: '654321', password_confirmation: '654321')
-    expect(other_user.update(password: nil)).to be_truthy
-  end
-
-  it 'doesn\'t care about all the validation stuff when the user is temporary'
+  it 'does not care about all the validation stuff when the user is temporary'
   it 'disallows regular assignment of roles that are restricted'
 end
