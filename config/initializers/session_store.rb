f83cbfec01b827112de2d3602e74f802f8f828b4
Josh Symonds <veraticus@gmail.com>
2014-08-13 10:02:11 -0500

    Continued efforts to get around CSRF issues

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-EmpiricalGrammar::Application.config.session_store :cookie_store, key: '_empirical-grammar_session'
+EmpiricalGrammar::Application.config.session_store :cookie_store, key: '_empirical-grammar_session', domain: ENV['DOMAIN'] || 'localhost'
