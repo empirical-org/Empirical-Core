9f144eb66b781ce27430ffc9c5348658083570e7
James Cox <james@imaj.es>
2014-08-12 22:50:01 -0400

    switch to serialized; this got lost in merge

---
+++ app/serializers/activity_session_serializer.rb
@@ -0,0 +1,3 @@
+class ActivitySessionSerializer < ActiveModel::Serializer
+  attributes :uid, :percentage, :time_spent, :state, :completed_at, :data, :temporary, :activity_uid, :anonymous, :access_token
+end
