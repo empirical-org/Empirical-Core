b870451e2801efba75afd10f5d9ea1610ac8b504
James Cox <james@imaj.es>
2014-08-14 14:54:42 -0400

    Fix factories

--- spec/factories/activity_classification.rb
+++ spec/factories/activity_classification.rb
@@ -1,11 +1,14 @@
 FactoryGirl.define do
 
-
-
-
   factory :activity_classification, aliases: [:classification] do
+
     sequence(:name) { |i| "activity cls #{i}" }
-    key
+    sequence(:key)  { |i| "activity_cls_#{i}" }
+
+    module_url { "http://#{Forgery(:internet).domain_name}/activity_cls/module" }
+      form_url { "http://#{Forgery(:internet).domain_name}/activity_cls/form" }
+
+
   end
 
 end
