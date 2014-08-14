a34757636cf4a4d07d2094a99c5e3e1ad6e5c102
James Cox <james@imaj.es>
2014-08-14 14:54:42 -0400

    Fix factories

--- spec/factories/section.rb
+++ spec/factories/section.rb
@@ -2,7 +2,9 @@ FactoryGirl.define do
 
   factory :section do
     sequence(:name) { |i| "section #{i}" }
-    position 1
+    sequence(:position) { |i| i }
+
+    workbook { Workbook.first || FactoryGirl.create(:workbook) }
   end
 
 end
