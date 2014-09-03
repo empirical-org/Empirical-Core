f37e2f73b63a888eb8371a3e805a4be01753f317
Christian <christian@softwareallies.com>
2014-09-03 14:18:48 -0500

    added one test to acitivty session spec

--- spec/models/activity_session_spec.rb
+++ spec/models/activity_session_spec.rb
@@ -11,5 +11,28 @@ describe ActivitySession, :type => :model do
 
   end
 
+  let(:activity_session) {FactoryGirl.build(:activity_session)}
+
+  describe "#activity" do 
+
+  	context "when there is one direct association from activity" do
+
+	  	let(:activity){ FactoryGirl.create(:activity) }
+		let(:activity_session){ FactoryGirl.build(:activity_session,activity_id: activity.id) }  	
+	  	
+		it "must return the direct associated activity" do 
+			expect(activity_session.activity).to eq activity
+		end	
+
+	end
+
+	context "when there is not any direct association to activity but there is one classroom" do 
+
+		it "must return the classroom activity" do 
+		end
+
+	end
+
+  end
 
 end
