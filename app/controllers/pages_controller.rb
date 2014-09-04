589397dce4c22220bed3bd8b8f86b8eddbeef706
lishiyo <c.ziye.li@gmail.com>
2014-09-04 20:51:46 +0000

    lessons.html.slim - use sort_by to sort activities alphabetically

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -47,7 +47,7 @@ class PagesController < ApplicationController
     @body_class = 'auxiliary white-page formatted-text'
     @section = if params[:section_id].present? then Section.find(params[:section_id]) else Section.first end
 		#@topics = @section.topics.map{ |topic| [topic, topic.activities.production] }.select{ |group| group.second.any? }
-		@topics = @section.topics.map{|topic| [topic, topic.activities] }
+		@topics = @section.topics.map{|topic| [topic, topic.activities] }.select{ |group| group.second.any? }
   end
 
   private
