c09f43d628037bcca01bbc7c04bc7afef550a234
James Cox <james@imaj.es>
2014-09-15 19:12:42 -0400

    remove queue classic from its memory entirely

--- db/migrate/20140522033151_add_queue_classic.rb
+++
@@ -1,9 +0,0 @@
-class AddQueueClassic < ActiveRecord::Migration
-  def self.up
-    QC::Setup.create(ActiveRecord::Base.connection.raw_connection)
-  end
-
-  def self.down
-    QC::Setup.drop
-  end
-end
