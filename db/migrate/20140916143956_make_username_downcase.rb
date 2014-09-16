e98048c538f8c78ce7b5c34cb73cb23362fe3d70
James Cox <james@imaj.es>
2014-09-16 11:09:46 -0400

    fix migration

--- db/migrate/20140916143956_make_username_downcase.rb
+++ db/migrate/20140916143956_make_username_downcase.rb
@@ -1,7 +1,11 @@
 class MakeUsernameDowncase < ActiveRecord::Migration
   def change
 
-    User.all.each { |u| u.update_columns(email: "LOWER(email)", username: "LOWER(username)") }
+    User.all.each { |u| 
+      em = u.try(:email).downcase rescue nil
+      un = u.try(:username).downcase rescue nil
+
+      u.update_columns(email: em, username: un) }
 
   end
 end
