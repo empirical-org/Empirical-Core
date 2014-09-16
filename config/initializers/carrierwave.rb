9305ccbd678f1e5205149892afff948365587754
James Cox <james@imaj.es>
2014-09-16 13:38:31 -0400

    normalize to fog_directory

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -9,6 +9,6 @@ CarrierWave.configure do |config|
     aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
   }
 
-  config.fog_directory = ENV['S3_BUCKET']
+  config.fog_directory = ENV['FOG_DIRECTORY']
   config.asset_host = ENV.fetch('ASSET_HOST', "http://s3.amazonaws.com/#{ENV['S3_BUCKET']}")
 end
