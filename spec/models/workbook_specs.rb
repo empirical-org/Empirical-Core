dfdaf5ee838ef8b2a7b6e3439bd5ff1dbf4d7b68
Christian <christian@softwareallies.com>
2014-09-17 16:38:54 -0500

    correcting workbook description

--- spec/models/workbook_specs.rb
+++ spec/models/workbook_specs.rb
@@ -10,12 +10,14 @@ describe Workbook, :type => :model do
 			expect(workbook.valid?).to be_truthy
 		end
 
-		context "when it validates" do 
+		context "when it runs validations" do 
+
 			it "title must be required" do
 				workbook.title=nil
 				workbook.valid?			
 				expect(workbook.errors[:title]).to include "can't be blank"
 			end
+			
 		end
 		
 	end
