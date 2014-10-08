54896517621805303991a821eb2df76ee8c4a780
James Cox <james@imaj.es>
2014-10-08 15:12:49 -0400

    move painful stuff from migrations as heroku can't cope

--- db/migrate/20141007210647_fix_bad_activity_id_on_session.rb
+++ db/migrate/20141007210647_fix_bad_activity_id_on_session.rb
@@ -1,11 +1,7 @@
 class FixBadActivityIdOnSession < ActiveRecord::Migration
   def change
 
-   broken_sessions = ActivitySession.unscoped.joins(:classroom_activity).includes(:classroom_activity).where("activity_sessions.activity_id is null AND classroom_activity_id is NOT NULL")
-
-   broken_sessions.each do |as|
-     as.update_columns(activity_id: as.classroom_activity.activity_id)
-   end
+    # noop, because this now lives in a worker.
 
   end
 end
--- db/migrate/20141008152913_changes_to_activity_session.rb
+++ db/migrate/20141008152913_changes_to_activity_session.rb
@@ -7,39 +7,5 @@ class ChangesToActivitySession < ActiveRecord::Migration
     add_index :activity_sessions, :started_at
     add_index :activity_sessions, :completed_at
 
-    puts "completed index/column creation"
-    # fix the missing data....
-    ActivitySession.unscoped.where(temporary: nil).update_all(temporary: 'f')
-
-    puts "completed temporary set"
-    # now fix timestamps and such
-    ActivitySession.unscoped.started_or_better.each do |as|
-      updates = {}
-
-      if as.created_at.nil?
-
-        if !as.updated_at.nil?
-          updates[:created_at] = as.updated_at
-        elsif !as.completed_at.nil?
-          updates[:created_at] = as.completed_at - 10.minutes
-        else
-          updates[:created_at] = 1.year.ago
-        end
-
-        updates[:updated_at] = updates[:created_at] if as.updated_at.nil?
-        updates[:started_at] = updates[:created_at] if as.started_at.nil?
-      else
-        updates[:updated_at] = as.created_at if as.updated_at.nil?
-        updates[:started_at] = as.created_at if as.started_at.nil?
-      end
-
-      if as.time_spent.nil? && !as.completed_at.nil?
-        updates[:time_spent] = as.completed_at.to_f - updates[:started_at].to_f
-      end
-
-      as.update_columns(updates)
-    end
-
-    puts "completed timestamp jostling"
   end
 end
