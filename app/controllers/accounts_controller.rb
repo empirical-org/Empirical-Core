403ebedaf544430f84e77ee57936f48e0d675d68
Chris Jeon <chris0374@gmail.com>
2014-10-17 22:41:15 -0400

    ISSUE-414 Adds first_name and last_name to teacher sign up and allows first_name last_name to user_params

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -45,6 +45,8 @@ class AccountsController < ApplicationController
 protected
 
   def user_params
-    params.require(:user).permit(:classcode, :email, :name, :username, :password, :password_confirmation, :newsletter, :terms_of_service, :school_ids)
+    params.require(:user).permit(:classcode, :email, :first_name, :last_name,
+      :name, :username, :password, :password_confirmation, :newsletter,
+      :terms_of_service, :school_ids)
   end
 end
