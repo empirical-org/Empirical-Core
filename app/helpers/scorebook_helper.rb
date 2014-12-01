c85938e2e8251eeadb50380bbe04708b09d926ba
Marcello Sachs <marcello.sachs@gmail.com>
2014-12-01 16:19:00 -0500

    integrating new student profile; ;

---
+++ app/helpers/scorebook_helper.rb
@@ -0,0 +1,39 @@
+module ScorebookHelper
+
+  def percentage_color(score)
+    return 'gray' unless score
+    score = score.to_f / 100.0 if score > 1
+    case score
+    when 0.75..1.0
+      'green'
+    when 0.5..0.75
+      'orange'
+    when 0.0..0.5
+      'red'
+    else
+      'gray'
+    end
+  end
+
+  def icon_for_classification(classification)
+    case classification.key
+    when 'story'
+      'flag'
+    when 'practice_question_set'
+      'puzzle'
+    else
+      ''
+    end
+  end
+
+  def tooltip_html(activity_or_session)
+    activity, session = if activity_or_session.respond_to?(:activity)
+      [activity_or_session.activity, activity_or_session]
+    else
+      [activity_or_session, nil]
+    end
+
+    %Q(data-toggle="tooltip" data-placement="left" title="<h1>#{activity.name}</h1><p>#{activity.classification.name}</p><p>#{activity.section.name}: #{activity.topic.name}</p>#{session ? "<p>Scored #{session.score}</p>" : ''}").html_safe
+  end
+
+end
