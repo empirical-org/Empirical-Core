467b6935f7493da05330576c8790dd19e4d08ed0
Kristopher Kelly <kristopherbkelly@gmail.com>
2015-01-26 14:33:40 -0500

    Remove workbook validation from Section model

--- spec/factories/section.rb
+++ spec/factories/section.rb
@@ -3,8 +3,6 @@ FactoryGirl.define do
   factory :section do
     sequence(:name) { |i| "section #{i}" }
     sequence(:position) { |i| i }
-
-    workbook { Workbook.first || FactoryGirl.create(:workbook) }
   end
 
 end
