945fe97269decf20949132db0bd2cc153d8667af
Kristopher Kelly <kristopherbkelly@gmail.com>
2015-01-27 18:26:15 -0500

    Add initializer for segment.io configuration

--- app/models/segment_analytics.rb
+++ app/models/segment_analytics.rb
@@ -4,6 +4,14 @@ class SegmentAnalytics
   # elsewhere.
   class_attribute :backend
 
+  def initialize
+    # Do not clobber the backend object if we already set a fake one under test
+    self.backend ||= Segment::Analytics.new({
+      write_key: SegmentIo.configuration.write_key,
+      on_error: Proc.new { |status, msg| print msg }
+    })
+  end
+
   # All events should be defined as instance methods like so:
   #
   # def track_user_sign_in
@@ -16,32 +24,38 @@ class SegmentAnalytics
   #  )
   # end
 
-  # def track_test(user_id)
-  #   identify(identify_params(user_id))
-  #   track({user: user_id})
-  # end
+  def track_student_creation(student_user)
+    identify(student_user)
+    track({
+      user_id: student_user.id,
+      event: 'Student Account Creation',
+      properties: {
+        student: student_user
+      }
+    })
+  end
 
   private
 
-  def identify(options)
-    backend.identify(options)
+  def identify(user)
+    backend.identify(identify_params(user))
   end
 
-  def identify_params(user_id)
+  def identify_params(user)
     {
-      user_id: user_id,
-      traits: {}
+      user_id: user.id,
+      traits: user_traits(user)
     }
   end
 
-  # def user_traits
-  #   {
-  #     email: user.email,
-  #     first_name: user.first_name,
-  #     last_name: user.last_name,
-  #     city_state: user.city_state,
-  #   }.reject { |key, value| value.blank? }
-  # end
+  def user_traits(user)
+    {
+      # email: user.email,
+      # first_name: user.first_name,
+      # last_name: user.last_name,
+      # city_state: user.city_state,
+    }.reject { |key, value| value.blank? }
+  end
 
   def track(options)
     backend.track(options)
--- spec/models/segment_analytics_spec.rb
+++ spec/models/segment_analytics_spec.rb
@@ -1,7 +1,49 @@
 require 'spec_helper'
 
-describe SegmentAnalytics do
-  context 'tracking user signup' do
-    it 'sends the right parameters to Segment.io'
+describe SegmentAnalytics, :type => :model do
+
+  let(:analytics) { SegmentAnalytics.new }
+
+  context 'tracking student account creation' do
+    context 'when the user registered through the home page' do
+      it 'identifies the new user and sends an event' do
+        student = FactoryGirl.create(:student)
+        analytics.track_student_creation(student)
+        identify_calls = analytics.backend.identify_calls
+        track_calls = analytics.backend.track_calls
+        expect(identify_calls.size).to eq(1)
+        expect(track_calls.size).to eq(1)
+        expect(track_calls[0][:user_id]).to eq(student.id)
+        expect(track_calls[0][:properties][:student][:id]).to eq(student.id)
+      end
+    end
+
+    context 'when the user was created by the teacher' do
+      it 'identifies the new user and sends an event with info about the teacher'
+    end
+  end
+
+  context 'tracking teacher account creation' do
+    it 'identifies the new user and send an event'
+  end
+
+  context 'teacher signin' do
+    it 'identifies the teacher who signed in and sends an event'
+  end
+
+  context 'student signin' do
+    it 'identifies the student who signed in and sends an event'
+  end
+
+  context 'tracking classroom creation' do
+    it 'sends an event with info about the new classroom'
+  end
+
+  context 'tracking activity assignment' do
+    it 'sends an event with info about the teacher, student, and activity'
+  end
+
+  context 'tracking activity completion' do
+    it 'sends an event with info about the activity and student'
   end
 end
