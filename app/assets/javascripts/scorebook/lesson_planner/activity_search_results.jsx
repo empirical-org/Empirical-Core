0290644225186bd6cda65ba523ef2284c94a499f
Marcello Sachs <marcello.sachs@gmail.com>
2015-02-03 19:59:55 -0500

    miscellaneous progress

--- app/assets/javascripts/scorebook/lesson_planner/activity_search_results.jsx
+++ app/assets/javascripts/scorebook/lesson_planner/activity_search_results.jsx
@@ -4,15 +4,15 @@ EC.ActivitySearchResults = React.createClass({
 
 	render: function () {
 		console.log(this.props)
-		rows = _.map(this.props.activitySearchResults, function (ele) {
+		rows = _.map(this.props.currentPageSearchResults, function (ele) {
 			var selected = _.include(this.props.selectedActivities, ele)
 			
 			return <EC.ActivitySearchResult data={ele} selected={selected} toggleActivitySelection={this.props.toggleActivitySelection} />
 		}, this);
 		return (
-				<tbody>
-					{rows}
-				</tbody>
+			<tbody>
+				{rows}
+			</tbody>
 		);
 	}
 
--- app/assets/javascripts/scorebook/lesson_planner/selected_activities.jsx
+++ app/assets/javascripts/scorebook/lesson_planner/selected_activities.jsx
@@ -2,10 +2,19 @@ EC.SelectedActivities = React.createClass({
 
 
 	render: function () {
-
+		var rows, buttonClassName;
+		
 		rows = _.map(this.props.selectedActivities, function (ele){
 			return <EC.SelectedActivity toggleActivitySelection={this.props.toggleActivitySelection} data={ele} />
 		}, this);
+
+		if (this.props.selectedActivities.length > 0) {
+			buttonClassName = "button-green pull-right";
+		} else {
+			buttonClassName = "hidden";
+		}
+		
+
 		return (
 			<section className="teaching-cart">
 				<h3 className="section-header unit_name">{this.props.unitName}</h3>
@@ -15,7 +24,7 @@ EC.SelectedActivities = React.createClass({
 					</tbody>
 				</table>
 				<div className="fake-border"></div>
-				<button className="button-green pull-right" id='continue'>Continue</button>
+				<button onClick={this.props.clickContinue} className={buttonClassName} id='continue'>Continue</button>
 			</section>
 		);
 	}
