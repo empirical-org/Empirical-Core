5d77b945130c2d51113f41be153fa057b6b57386
Marcello Sachs <marcello.sachs@gmail.com>
2015-02-04 17:51:22 -0500

    fixing bugs

--- app/assets/javascripts/scorebook/lesson_planner/activity_search_sorts.jsx
+++ app/assets/javascripts/scorebook/lesson_planner/activity_search_sorts.jsx
@@ -19,16 +19,3 @@ EC.ActivitySearchSorts = React.createClass({
 		);
 	}
 });
-/*
-<thead>
-	<tr>
-		<th>
-			<%= image_tag 'scorebook/icon-check.png' %>
-		</th>
-		<th id='activity_classification' class='sorter'>App<i class="fa fa-caret-up"></i></th>
-		<th id='activity' class='sorter'>Activity<i class="fa fa-caret-up"></i></th>
-		<th id='section' class='sorter'>Standard Level<i class="fa fa-caret-up"></i></th>
-		<th id='topic' class='sorter active'>Concept<i class="fa fa-caret-up"></i></th>
-	</tr>
-</thead>
-*/
