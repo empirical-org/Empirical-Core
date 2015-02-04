71bbeeef996a59c0c8a48faea3ce89884f2d4762
Kristopher Kelly <kristopherbkelly@gmail.com>
2015-02-04 18:30:01 -0500

    Finishing lesson planner almost works

--- app/assets/javascripts/scorebook/lesson_planner/activity_due_date.jsx
+++ app/assets/javascripts/scorebook/lesson_planner/activity_due_date.jsx
@@ -1,12 +1,19 @@
 EC.ActivityDueDate = React.createClass({
-  componentDidMount: function() {
+  // componentDidMount: function() {
     // Set up the datepicker on the dueDate input
-    $(this.refs.dueDate.getDOMNode()).datepicker({
-      selectOtherMonths: true,
-      dayNamesMin: ['S', 'M', 'T', 'W', 'T', 'F', 'S'],
-      minDate: -20,
-      maxDate: "+1M +10D"       
-    });
+
+    // FIXME: Datepicker not working (conflict with bootstrap? see noConflict())
+    // $(this.refs.dueDate.getDOMNode()).datepicker({
+    //   selectOtherMonths: true,
+    //   dayNamesMin: ['S', 'M', 'T', 'W', 'T', 'F', 'S'],
+    //   minDate: -20,
+    //   maxDate: "+1M +10D"       
+    // });
+  // },
+
+  handleChange: function(e) {
+    var val = $(e.target).val(); // due date
+    this.props.assignActivityDueDate(this.props.activity, val);
   },
 
   removeActivity: function () {
@@ -19,7 +26,7 @@ EC.ActivityDueDate = React.createClass({
         <td className={this.props.activity.image_class}></td>
         <td>{this.props.activity.name}</td>
         <td>
-          <input type="text" ref="dueDate" className="datepicker-input" placeholder="mm/dd/yyyy" />
+          <input type="text" ref="dueDate" className="datepicker-input" placeholder="mm/dd/yyyy" onChange={this.handleChange} />
         </td>
         <td className="icon-x-gray" onClick={this.props.removeActivity}></td>
       </tr>
