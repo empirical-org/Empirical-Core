f161eb724eec118ea2b66a21e8a63b1f3f3ab799
Marcello Sachs <marcello.sachs@gmail.com>
2015-02-06 13:55:03 -0500

    create new field is_retry on activity_sessions, filter them out of incomplete_activity_sessions (to handle disrupted retries)

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150204201738) do
+ActiveRecord::Schema.define(version: 20150206184619) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -63,6 +63,7 @@ ActiveRecord::Schema.define(version: 20150204201738) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.datetime "started_at"
+    t.boolean  "is_retry",              default: false
   end
 
   add_index "activity_sessions", ["activity_id"], name: "index_activity_sessions_on_activity_id", using: :btree
