a30f7af4e61e6568ed004d0cbb1bede038e5c4be
Marcello Sachs <marcello.sachs@gmail.com>
2015-02-09 09:57:39 -0500

    transfer bug fixes for activity planner from homepage branch

--- app/serializers/classification_serializer.rb
+++ app/serializers/classification_serializer.rb
@@ -1,12 +1,22 @@
 class ClassificationSerializer < ActiveModel::Serializer
-  attributes :uid, :id, :name, :key, :form_url, :module_url, :created_at, :updated_at, :image_class
+  attributes :uid, :id, :name, :key, :form_url, :module_url, :created_at, :updated_at, :image_class, :alias
+
+
+  def alias
+  	case object.id
+  	when 1
+  		'Quill Proofreader'
+  	when 2
+  		'Quill Grammar'
+  	end 
+  end
 
   def image_class
   	case object.id
   	when 1
-  		'icon-puzzle-gray'
-  	when 2
   		'icon-flag-gray'
+  	when 2
+  		'icon-puzzle-gray'
   	end
   end
 
