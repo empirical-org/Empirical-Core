5858f400ec9f037aa8d264d504b59f48f42193ce
Marcello Sachs <marcello.sachs@gmail.com>
2015-02-11 23:48:40 -0500

    add develop page (sub-page of about section)

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,5 +1,16 @@
 module PagesHelper
 
+	def pages_tab_class tabname
+		arr = ["mission", "develop"]
+
+		if tabname == 'about'
+			arr.include?(action_name) ? "active" : ""
+		else
+			arr.include?(action_name) ? "" : "active"
+		end
+
+	end
+
 	def about_subtab_class tabname
 		if action_name == tabname
 			"active"
