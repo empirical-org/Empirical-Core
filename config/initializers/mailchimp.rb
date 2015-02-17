97add8429eb35f40d394779449280551c97cc580
Bob Mazanec <bob@bobmazanec.com>
2015-02-16 23:30:16 -0600

    Move User#subscribe_to_newsletter details
    into MailchimpConnection#subscribe_to_newsletter
    
    making MailchimpConnection something of a facade so that callers
    (e.g., User) don't need to know how to navigate through the Mailchimp API

--- config/initializers/mailchimp.rb
+++ config/initializers/mailchimp.rb
@@ -1,6 +1,14 @@
 module MailchimpConnection
-  def connection
-    Mailchimp::API.new(ENV['MAILCHIMP_API_KEY'])
+  def subscribe_to_newsletter(email)
+    connection = Mailchimp::API.new(ENV['MAILCHIMP_API_KEY'])
+    connection.lists.subscribe('eadf6d8153',     # id
+                               { email: email }, # email
+                               nil,              # merge_vars
+                               'html',           # email_type
+                               false,            # double_optin
+                               false,            # update_existing
+                               true,             # replace_interests
+                               false)            # send_welcome
   end
 
   extend self
