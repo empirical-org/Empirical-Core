6c5d02553063f99f5c2388da18376f5749d8a63e
Kristopher Kelly <kristopherbkelly@gmail.com>
2015-02-18 13:22:32 -0500

    Move 3rd party JS libs to vendor/

--- app/assets/javascripts/scorebook/scorebook.js
+++ app/assets/javascripts/scorebook/scorebook.js
@@ -1,22 +1,22 @@
-//= require scorebook/jquery.selectbox-0.2.js
+//= require jquery.selectbox-0.2.js
 //= require scorebook/bootstrap
 
 
-//= require scorebook/lodash.min.js
+//= require lodash.min.js
 //= require underscore.string
 
-//= require scorebook/react
+//= require react
 
-//= require scorebook/selecter.js
+//= require selecter.js
 //= require scorebook/custom
 
 //= require scorebook/lol.js
-//= require scorebook/slick.js
-//= require scorebook/tabslet.js
+//= require slick.js
+//= require tabslet.js
 
 //= require scorebook/custom-slick.js
 
-//= require scorebook/jquery-ui-custom.min.js
+//= require jquery-ui-custom.min.js
 
 
 //= require scorebook/empirical_core.js
