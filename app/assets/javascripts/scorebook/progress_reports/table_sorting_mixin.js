e41f791915de5095d046afaefa50cc8f7a3a07c0
Kristopher Kelly <kristopherbkelly@gmail.com>
2015-02-20 12:16:06 -0500

    Extract sort definition so it can vary on different tables

--- app/assets/javascripts/scorebook/progress_reports/table_sorting_mixin.js
+++ app/assets/javascripts/scorebook/progress_reports/table_sorting_mixin.js
@@ -1,39 +1,48 @@
 EC.TableSortingMixin = {
   getInitialState: function() {
     return {
-      currentSort: {
-        field: 'activity_classification_name',
-        direction: 'asc'
-      }
+      currentSort: {},
+      sortConfig: {}
     };
+  },
+
+  // Sign = 1 or -1 depending on whether the sort is normal or reverse order
+  numericSort: function(sign, fieldName, a, b) {
+    return (a[fieldName] - b[fieldName]) * sign;
+  },
+
+  // Sign = 1 or -1 depending on whether the sort is normal or reverse order
+  naturalSort: function(sign, fieldName, a, b) {
+    return s.naturalCmp(a[fieldName], b[fieldName]) * sign;
+  },
+
+  // config = {field: string, sortFunc: function, ...}
+  // currentSort = {field: string, direction: string ('asc' or 'desc')}
+  defineSorting: function(config, currentSort) {
+    this.setState({
+      sortConfig: config,
+      currentSort: currentSort
+    });
   },
 
   applySorting: function(results) {
     var sortDirection = this.state.currentSort.direction,
         sortByFieldName = this.state.currentSort.field;
+    if (!sortDirection && !sortByFieldName) {
+      return results;
+    }
     // Flip the sign of the return value to sort in reverse
     var sign = (sortDirection === 'asc' ? 1 : -1);
 
-    var sortFunc;
-    switch(sortByFieldName) {
-      case 'completed_at':
-      case 'percentage':
-      case 'time_spent':
-        // Integer sort
-        sortFunc = function(a, b) {
-          return (a - b) * sign;
-        };
-      case 'activity_classification_name':
-      case 'activity_name':
-      case 'standard':
-      default:
-        // Alphanumeric sort
-        sortFunc = function(a, b) {
-          return s.naturalCmp(a[sortByFieldName], b[sortByFieldName]) * sign;
-        };
+    var sortFunc = this.state.sortConfig[sortByFieldName];
+
+    if (!sortFunc) {
+      throw "Sort function not defined for '" + sortByFieldName + "' field";
     }
 
-    results.sort(sortFunc);
+    // Partially apply arguments so that the new func has signature -> (a, b)
+    var appliedSort = _.partial(sortFunc, sign, sortByFieldName);
+    results.sort(appliedSort);
     return results;
   },
 
