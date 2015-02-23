527fa5eb1dcec6fb1343609fbc870ee2ad45d1b1
Kristopher Kelly <kristopherbkelly@gmail.com>
2015-02-23 13:30:38 -0500

    Implement sections progress report endpoint

--- app/models/section.rb
+++ app/models/section.rb
@@ -8,4 +8,20 @@ class Section < ActiveRecord::Base
   has_many :topics, dependent: :destroy
 
   validates :name, presence: true
+
+  def self.for_progress_report(teacher)
+    query = select(<<-SELECT
+      sections.name,
+      COUNT(DISTINCT(topics.id)) as topic_count,
+      SUM(CASE WHEN activity_sessions.percentage > 0.75 THEN 1 ELSE 0 END) as proficient_count,
+      SUM(CASE WHEN activity_sessions.percentage <= 0.75 THEN 1 ELSE 0 END) as not_proficient_count,
+      SUM(activity_sessions.time_spent) as total_time_spent
+    SELECT
+    ).joins(:topics => {:activities => {:classroom_activities => [:activity_sessions, :classroom => :teacher]}})
+      .group("sections.id")
+      .where("users.id = ?", teacher.id) # Filter based on teacher ID
+
+    results = ActiveRecord::Base.connection.select_all(query)
+    results.to_hash
+  end
 end
---
+++ spec/controllers/teachers/progress_reports/sections_controller_spec.rb
@@ -0,0 +1,60 @@
+require 'rails_helper'
+
+describe Teachers::ProgressReports::SectionsController, :type => :controller do
+  render_views
+
+  let(:teacher) { FactoryGirl.create(:teacher) }
+  let(:student) { FactoryGirl.create(:student) }
+  let(:teacher_classroom) { FactoryGirl.create(:classroom, teacher: teacher, students: [student]) }
+  let(:section) { FactoryGirl.create(:section) }
+
+  before do
+    ActivitySession.destroy_all
+    3.times do |i| 
+      topic = FactoryGirl.create(:topic, section: section)
+      activity = FactoryGirl.create(:activity, topic: topic)
+      classroom_activity = FactoryGirl.create(:classroom_activity, classroom: teacher_classroom, activity: activity)
+      3.times do |j|
+        activity_session = FactoryGirl.create(:activity_session, 
+                                              classroom_activity: classroom_activity, 
+                                              user: student,
+                                              activity: activity, 
+                                              state: 'finished',
+                                              percentage: i / 3.0)
+      end
+    end
+  end
+
+  describe 'GET #index' do
+    before do
+      session[:user_id] = teacher.id # sign in, is there a better way to do this in test?
+    end
+
+    it 'displays the html' do
+      get :index, {}
+      expect(response.status).to eq(200)
+    end
+  end
+
+  context 'XHR GET #index' do
+
+    it 'requires a logged-in teacher' do
+      get :index
+      expect(response.status).to eq(401)
+    end
+
+    context 'when logged in' do
+      before do
+        session[:user_id] = teacher.id
+      end
+
+      it 'fetches aggregated section data' do
+        xhr :get, :index
+        expect(response.status).to eq(200)
+        json = JSON.parse(response.body)
+        expect(json['sections'].size).to eq(1)
+        expect(json['sections'][0]['topic_count']).to eq('3')
+      end
+    end
+  end
+end
