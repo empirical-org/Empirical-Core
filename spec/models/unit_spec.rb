fb355585e4b70e7a802a96fe20dbf49a3a0fb04c
Marcello Sachs <marcello.sachs@gmail.com>
2015-02-23 18:14:48 -0500

    spec examples related to unit and classroom_activity

---
+++ spec/models/unit_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+
+describe Unit, type: :model do 
+	let!(:classroom) {FactoryGirl.create(:classroom)}
+	let!(:student) {FactoryGirl.create(:student, classroom: classroom)}
+	let!(:classroom_activity) {FactoryGirl.create(:classroom_activity, classroom: classroom)}
+	let!(:unit) {FactoryGirl.create :unit, classroom_activities: [classroom_activity]}
+
+
+	describe '#destroy' do 
+
+		it 'destroys associated classroom_activities' do 
+			unit.destroy
+			expect(ClassroomActivity.where(id: classroom_activity.id)).to be_empty
+
+		end
+	end
+
+end
