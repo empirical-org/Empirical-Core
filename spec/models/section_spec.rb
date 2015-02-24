44b4a9dbcc25282787b429157659c09a551cdbb6
Kristopher Kelly <kristopherbkelly@gmail.com>
2015-02-24 09:30:46 -0500

    DRY up test setup for progress reports

--- spec/models/section_spec.rb
+++ spec/models/section_spec.rb
@@ -21,38 +21,12 @@ describe Section, type: :model do
 	end
 
 	context "retrieving sections for the progress report" do
+		include ProgressReportHelper
+
 	  let!(:teacher) { FactoryGirl.create(:teacher) }
 
 	  before do
-	    ActivitySession.destroy_all
-	    @sections = []
-	    @units = []
-	    @classrooms = []
-	    @students = []
-	    3.times do |i|
-	    	student = FactoryGirl.create(:student)
-	    	@students << student
-	    	classroom = FactoryGirl.create(:classroom, teacher: teacher, students: [student])
-	    	@classrooms << classroom
-	    	section = FactoryGirl.create(:section)
-	    	@sections << section
-	    	unit = FactoryGirl.create(:unit)
-	    	@units << unit
-	      topic = FactoryGirl.create(:topic, section: section)
-	      activity = FactoryGirl.create(:activity, topic: topic)
-	      classroom_activity = FactoryGirl.create(:classroom_activity,
-	                                              classroom: classroom,
-	                                              activity: activity,
-	                                              unit: unit)
-	      3.times do |j|
-	        activity_session = FactoryGirl.create(:activity_session,
-	                                              classroom_activity: classroom_activity,
-	                                              user: student,
-	                                              activity: activity,
-	                                              state: 'finished',
-	                                              percentage: i / 3.0)
-	      end
-	    end
+	  	setup_sections_progress_report
 	  end
 
 	  it "retrieves aggregated section data" do
