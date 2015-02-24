8c00ad26c31b66b0912788c3d1094b47183d40a8
Kristopher Kelly <kristopherbkelly@gmail.com>
2015-02-24 10:22:06 -0500

    Add dummy controller and spec for topics progress report

--- spec/support/helpers/progress_report_helper.rb
+++ spec/support/helpers/progress_report_helper.rb
@@ -30,4 +30,37 @@ module ProgressReportHelper
       end
     end
   end
+
+  def setup_topics_progress_report
+    ActivitySession.destroy_all
+    @section = FactoryGirl.create(:section)
+    @units = []
+    @classrooms = []
+    @students = []
+    @topics = []
+    3.times do |i|
+      student = FactoryGirl.create(:student)
+      @students << student
+      classroom = FactoryGirl.create(:classroom, teacher: teacher, students: [student])
+      @classrooms << classroom
+      unit = FactoryGirl.create(:unit)
+      @units << unit
+      topic = FactoryGirl.create(:topic, section: @section)
+      @topics << topic
+      activity = FactoryGirl.create(:activity, topic: topic)
+      classroom_activity = FactoryGirl.create(:classroom_activity,
+                                              classroom: classroom,
+                                              activity: activity,
+                                              unit: unit)
+      3.times do |j|
+        activity_session = FactoryGirl.create(:activity_session,
+                                              classroom_activity: classroom_activity,
+                                              user: student,
+                                              activity: activity,
+                                              state: 'finished',
+                                              percentage: i / 3.0)
+      end
+    end
+
+  end
 end
