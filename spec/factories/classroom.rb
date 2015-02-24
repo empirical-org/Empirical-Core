d076c31e32354f50497aed9fbc0a5c3ebd2e3d3a
Marcello Sachs <marcello.sachs@gmail.com>
2015-02-24 12:36:24 -0500

    undo the changes to factories I made in yesterdays commit d5ddb2c6b2e963f55c44f3b5469e7bfd57c63ed6 because they were causing bugs in other tests

--- spec/factories/classroom.rb
+++ spec/factories/classroom.rb
@@ -3,6 +3,7 @@ FactoryGirl.define do
   factory :classroom do
     sequence(:name) { |i| "classroom #{i}" }
     teacher
+    after(:create) {|c| c.units.create_next }
     grade '8'
 
 
--- spec/factories/classroom_activity.rb
+++ spec/factories/classroom_activity.rb
@@ -2,7 +2,6 @@ FactoryGirl.define do
 
   factory :classroom_activity do
 	  	
-	  classroom {Classroom.first || FactoryGirl.create(:classroom)}
 
 	  factory :classroom_activity_with_activity do 
 	  	activity { Activity.first || FactoryGirl.create(:activity) }
