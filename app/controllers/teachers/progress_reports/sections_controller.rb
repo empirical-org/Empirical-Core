a8c60b30f26e06382c4bd0674dd32e5db8fad2e6
Kristopher Kelly <kristopherbkelly@gmail.com>
2015-02-24 09:37:38 -0500

    Add more filtering for units

--- app/controllers/teachers/progress_reports/sections_controller.rb
+++ app/controllers/teachers/progress_reports/sections_controller.rb
@@ -7,7 +7,7 @@ class Teachers::ProgressReports::SectionsController < ApplicationController
       section_data = Section.for_progress_report(current_user, params)
       section_ids = section_data.map {|s| s['id']}
       classrooms = Classroom.for_progress_report(section_ids, current_user, params)
-      units = Unit.for_sections(section_ids, current_user)
+      units = Unit.for_progress_report(section_ids, current_user, params)
       students = User.for_progress_report(section_ids, current_user, params)
       render json: {
         sections: section_data,
--- app/models/unit.rb
+++ app/models/unit.rb
@@ -4,9 +4,19 @@ class Unit < ActiveRecord::Base
   has_many :activities, through: :classroom_activities
   has_many :topics, through: :activities
 
-  def self.for_sections(section_ids, teacher)
-    joins(:classroom_activities => [:classroom, {:activity => {:topic => :section}}])
+  def self.for_progress_report(section_ids, teacher, filters)
+    query = joins(:classroom_activities => [:activity_sessions, :classroom, {:activity => {:topic => :section}}])
       .where('sections.id IN (?)', section_ids)
       .where('classrooms.teacher_id = ?', teacher.id).uniq
+
+    if filters[:classroom_id].present?
+      query = query.where("classrooms.id = ?", filters[:classroom_id])
+    end
+
+    if filters[:student_id].present?
+      query = query.where("activity_sessions.user_id = ?", filters[:student_id])
+    end
+
+    query
   end
 end
