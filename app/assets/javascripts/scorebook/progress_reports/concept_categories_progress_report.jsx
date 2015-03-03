e00db8a09cd5ed8144b4fecd344e5b1ee6fae880
Kristopher Kelly <kristopherbkelly@gmail.com>
2015-03-03 12:29:59 -0500

    Refactor concept categories progress report to use new component

--- app/assets/javascripts/scorebook/progress_reports/concept_categories_progress_report.jsx
+++ app/assets/javascripts/scorebook/progress_reports/concept_categories_progress_report.jsx
@@ -3,103 +3,62 @@ EC.ConceptCategoriesProgressReport = React.createClass({
     sourceUrl: React.PropTypes.string.isRequired
   },
 
-  mixins: [EC.TableSortingMixin, EC.TableFilterMixin], // There's no pagination, so sorting can be done client-side.
-
-  getInitialState: function() {
-    return {
-      classroomFilters: [],
-      studentFilters: [],
-      unitFilters: [],
-      results: [],
-      section: {}
-    }
+  columnDefinitions: function() {
+    // Derive column names from this.state.section
+    return [
+      {
+        name: 'Concept Category',
+        field: 'concept_category_name',
+        sortByField: 'concept_category_name',
+        customCell: function(row) {
+          console.log('TODO Link names to sub-report');
+            // <a href={row['section_link']}>{row['section_name']}</a>
+          return (
+            <p>{row['concept_category_name']}</p>
+          );
+        }
+      },
+      {
+        name: 'Total Results',
+        field: 'total_result_count',
+        sortByField: 'total_result_count'
+      },
+      {
+        name: 'Correct Results',
+        field: 'correct_result_count',
+        sortByField: 'correct_result_count'
+      },
+      {
+        name: 'Incorrect Results',
+        field: 'incorrect_result_count',
+        sortByField: 'incorrect_result_count'
+      }
+    ];
   },
 
-  getDefaultProps: function() {
+  sortDefinitions: function() {
     return {
-      tableColumns: [
-        {
-          name: 'Concept Category',
-          field: 'concept_category_name',
-          sortByField: 'concept_category_name'
-          // customCell: function(row) {
-          //   return (
-          //     <a href={row['section_link']}>{row['section_name']}</a>
-          //   );
-          // }
-        },
-        {
-          name: 'Total Results',
-          field: 'total_result_count',
-          sortByField: 'total_result_count'
-        },
-        {
-          name: 'Correct Results',
-          field: 'correct_result_count',
-          sortByField: 'correct_result_count'
-        },
-        {
-          name: 'Incorrect Results',
-          field: 'incorrect_result_count',
-          sortByField: 'incorrect_result_count'
-        }
-      ]
+      config: {
+        concept_category_name: 'natural',
+        total_result_count: 'numeric',
+        correct_result_count: 'numeric',
+        incorrect_result_count: 'numeric'
+      },
+      default: {
+        field: 'concept_category_name',
+        direction: 'asc'
+      }
     };
   },
 
-  componentDidMount: function() {
-    this.defineSorting({
-      concept_category_name: this.naturalSort,
-      total_result_count: this.numericSort,
-      correct_result_count: this.numericSort,
-      incorrect_result_count: this.numericSort
-    }, {
-        field: 'concept_category_name',
-        direction: 'asc'
-    });
-
-    this.fetchData();
-  },
-
-  fetchData: function() {
-    $.get(this.props.sourceUrl, this.state.currentFilters, function onSuccess(data) {
-        this.setState({
-          results: data.concept_categories,
-          classroomFilters: this.getFilterOptions(data.classrooms, 'name', 'id', 'All Classrooms'),
-          studentFilters: this.getFilterOptions(data.students, 'name', 'id', 'All Students'),
-          unitFilters: this.getFilterOptions(data.units, 'name', 'id', 'All Units')
-        });
-    }.bind(this));
-  },
-
-  selectClassroom: function(classroomId) {
-    this.filterByField('classroom_id', classroomId, this.fetchData);
-  },
-
-  // Filter sessions based on the student ID
-  selectStudent: function(studentId) {
-    this.filterByField('student_id', studentId, this.fetchData);
-  },
-
-  // Filter sessions based on the unit ID
-  selectUnit: function(unitId) {
-    this.filterByField('unit_id', unitId, this.fetchData);
-  },
-
   render: function() {
-    // var visibleResults = this.applySorting(this.state.results);
-    var visibleResults = this.state.results;
-
     return (
-      <div className="container">
-        <EC.ProgressReportFilters classroomFilters={this.state.classroomFilters}
-                                  studentFilters={this.state.studentFilters}
-                                  unitFilters={this.state.unitFilters}
-                                  selectClassroom={this.selectClassroom}
-                                  selectStudent={this.selectStudent}
-                                  selectUnit={this.selectUnit} />
-        <EC.SortableTable rows={visibleResults} columns={this.props.tableColumns} sortHandler={this.sortResults} />
-      </div>
+      <EC.ProgressReport columnDefinitions={this.columnDefinitions}
+                         pagination={false}
+                         clientSideFiltering={false}
+                         sourceUrl={this.props.sourceUrl}
+                         sortDefinitions={this.sortDefinitions}
+                         jsonResultsKey={'concept_categories'} />
     );
   }
 });
