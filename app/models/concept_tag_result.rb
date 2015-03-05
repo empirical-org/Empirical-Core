931c09ad90da6382da841593b6f8c404496024cb
Kristopher Kelly <kristopherbkelly@gmail.com>
2015-03-05 12:30:38 -0500

    Display concept tag results for Grammar Concepts

--- app/models/concept_tag_result.rb
+++ app/models/concept_tag_result.rb
@@ -16,6 +16,21 @@ class ConceptTagResult < ActiveRecord::Base
     .average("cast(metadata->>'wpm' as int)")
   end
 
+  def self.grammar_counts
+    select("concept_tags.name, #{correct_result_count_sql}, #{incorrect_result_count_sql}")
+    .joins(:concept_tag)
+    .group("concept_tags.name")
+    .order("concept_tags.name asc")
+  end
+
+  def self.correct_result_count_sql
+    "SUM(CASE WHEN cast(concept_tag_results.metadata->>'correct' as int) = 1 THEN 1 ELSE 0 END) as correct_result_count"
+  end
+
+  def self.incorrect_result_count_sql
+    "SUM(CASE WHEN cast(concept_tag_results.metadata->>'correct' as int) = 1 THEN 0 ELSE 1 END) as incorrect_result_count"
+  end
+
   private
 
   def extract_tag_from_metadata
--- spec/helpers/concept_tag_helper_spec.rb
+++ spec/helpers/concept_tag_helper_spec.rb
@@ -1,17 +1,76 @@
 require "rails_helper"
 
 describe ConceptTagHelper, type: :helper do
-  describe "#typing_speed_stats" do
-    let(:activity_session) { FactoryGirl.create(:activity_session) }
-    let(:concept_class) { FactoryGirl.create(:concept_class, name: "Typing Speed")}
-    let(:concept_tag) { FactoryGirl.create(:concept_tag, name: "Typing Speed", concept_class: concept_class)}
-    let(:concept_category) { FactoryGirl.create(:concept_category, name: "Typing Speed")}
+  let!(:activity_session) { FactoryGirl.create(:activity_session) }
+  let(:typing_speed_class) { FactoryGirl.create(:concept_class, name: "Typing Speed") }
+  let(:typing_speed_category) { FactoryGirl.create(:concept_category, name: "Typing Speed") }
+  let(:typing_speed_tag) { FactoryGirl.create(:concept_tag, name: "Typing Speed", concept_class: typing_speed_class) }
+  let(:grammar_concepts_class) { FactoryGirl.create(:concept_class, name: "Grammar Concepts")}
+  let(:grammar_category) { FactoryGirl.create(:concept_category, name: "Something grammar")}
+  let(:prepositions_tag) { FactoryGirl.create(:concept_tag, name: "Prepositions", concept_class: grammar_concepts_class)}
 
+  describe "#all_concept_class_stats" do
+    before do
+      activity_session.concept_tag_results.create!(
+        concept_tag: typing_speed_tag,
+        concept_category: typing_speed_category,
+        metadata: {
+          wpm: 5
+        }
+      )
+      activity_session.concept_tag_results.create!(
+        concept_tag: prepositions_tag,
+        concept_category: grammar_category,
+        metadata: {
+          correct: 1
+        }
+      )
+    end
+
+    it "displays all concept class stats for an activity session" do
+      html = helper.all_concept_class_stats(activity_session)
+      typing_speed_header = "Typing Speed"
+      grammar_header = "Grammar Concepts"
+      expect(html).to include(typing_speed_header)
+      expect(html).to include(grammar_header)
+      expect(html).to include(prepositions_tag.name)
+    end
+  end
+
+  describe "#grammar_concepts_stats" do
+    before do
+      activity_session.concept_tag_results.create!(
+        concept_tag: prepositions_tag,
+        concept_category: grammar_category,
+        metadata: {
+          correct: 1
+        }
+      )
+
+      activity_session.concept_tag_results.create!(
+        concept_tag: prepositions_tag,
+        concept_category: grammar_category,
+        metadata: {
+          correct: 0
+        }
+      )
+    end
+
+    it "displays a breakdown of the grammar concepts and correct/incorrect" do
+      html = helper.grammar_concepts_stats(activity_session.concept_tag_results)
+      # These expectations are kinda weird because it's a pain to match the html structure
+      expect(html).to include(prepositions_tag.name)
+      expect(html).to include("1")
+      expect(html).to include("0")
+    end
+  end
+
+  describe "#typing_speed_stats" do
     before do
       10.times do |i|
         activity_session.concept_tag_results.create!(
-          concept_tag: concept_tag,
-          concept_category: concept_category,
+          concept_tag: typing_speed_tag,
+          concept_category: typing_speed_category,
           metadata: {
             wpm: i
           }
@@ -26,9 +85,5 @@ describe ConceptTagHelper, type: :helper do
       expect(html).to include(average_wpm.to_s)
       # expect(html).to have_tag('div', with: 'Average words per minute')
     end
-  end
-
-  describe "#grammar_concept_stats" do
-
   end
 end
