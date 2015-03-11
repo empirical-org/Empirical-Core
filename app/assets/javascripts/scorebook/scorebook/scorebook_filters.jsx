2de4787265dee22454cc017e241c6905b2f1bcac
Marcello Sachs <marcello.sachs@gmail.com>
2015-03-11 16:00:59 -0400

    wrap specific col classes around filters

--- app/assets/javascripts/scorebook/scorebook/scorebook_filters.jsx
+++ app/assets/javascripts/scorebook/scorebook/scorebook_filters.jsx
@@ -6,14 +6,18 @@ EC.ScorebookFilters = React.createClass({
 	render: function () {
 		return (
 			<div className="row activity-page-dropdown-wrapper">
-				<EC.DropdownFilter 
-					options={this.props.classroomFilters}
-					selectOption={this.props.selectClassroom}
-					defaultOption={this.props.defaultClassroom} />
-				<EC.DropdownFilter
-					options={this.props.unitFilters}
-					selectOption={this.props.selectUnit}
-					defaultOption={this.props.defaultUnit}/>
+				<div className="col-xs-12 col-sm-4 col-md-4 col-lg-4 col-xl-4 no-pl">
+					<EC.DropdownFilter 
+						options={this.props.classroomFilters}
+						selectOption={this.props.selectClassroom}
+						defaultOption={this.props.defaultClassroom} />
+				</div>
+				<div className="col-xs-12 col-sm-4 col-md-4 col-lg-4 col-xl-4 no-pl">
+					<EC.DropdownFilter
+						options={this.props.unitFilters}
+						selectOption={this.props.selectUnit}
+						defaultOption={this.props.defaultUnit}/>
+				</div>
 			</div>
 		);
 	}
