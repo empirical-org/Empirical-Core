d0be92654e21b6c66b0086d7f27a14d61c8a3dad
Bob Mazanec <bob@bobmazanec.com>
2015-03-11 16:25:12 -0600

    Make ScorebookPage 'path' into an instance method
    initialize the class with a Classroom with which to generate the #path

--- spec/support/pages/teachers/scorebook_page.rb
+++ spec/support/pages/teachers/scorebook_page.rb
@@ -3,8 +3,14 @@ require_relative './teachers'
 
 module Teachers
   class ScorebookPage < Page
-    def self.path(classroom)
-      "#{Teachers.classroom_path(classroom)}/scorebook"
+    attr_accessor :classroom
+
+    def initialize(classroom)
+      @classroom = classroom
+    end
+
+    def path
+      "#{Teachers.classroom_path(self.classroom)}/scorebook"
     end
   end
 end
