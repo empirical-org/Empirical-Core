85d1f17e3273ea1c71c689449b75d115c8e78171
Kristopher Kelly <kristopherbkelly@gmail.com>
2015-03-16 19:07:30 -0400

    Add another feature spec for activity progress report

--- app/assets/javascripts/scorebook/progress_reports/progress_report_filters.jsx
+++ app/assets/javascripts/scorebook/progress_reports/progress_report_filters.jsx
@@ -12,13 +12,13 @@ EC.ProgressReportFilters = React.createClass({
   render: function() {
     return (
       <div className="row activity-page-dropdown-wrapper">
-        <div className="col-xs-12 col-sm-4 col-md-4 col-lg-4 col-xl-4">
+        <div className="col-xs-12 col-sm-4 col-md-4 col-lg-4 col-xl-4 classroom-filter">
           <EC.DropdownFilter defaultOption={'All Classrooms'} options={this.props.classroomFilters} selectOption={this.props.selectClassroom} />
         </div>
-        <div className="col-xs-12 col-sm-4 col-md-4 col-lg-4 col-xl-4">
+        <div className="col-xs-12 col-sm-4 col-md-4 col-lg-4 col-xl-4 unit-filter">
           <EC.DropdownFilter defaultOption={'All Units'} options={this.props.unitFilters} selectOption={this.props.selectUnit} />
         </div>
-        <div className="col-xs-12 col-sm-4 col-md-4 col-lg-4 col-xl-4">
+        <div className="col-xs-12 col-sm-4 col-md-4 col-lg-4 col-xl-4 student-filter">
           <EC.DropdownFilter defaultOption={'All Students'} options={this.props.studentFilters} selectOption={this.props.selectStudent} />
         </div>
       </div>
