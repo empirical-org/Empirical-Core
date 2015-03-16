4507bd9d3ba5f8bab67b17965268143caa4aa122
Kristopher Kelly <kristopherbkelly@gmail.com>
2015-03-16 18:32:48 -0400

    Add another spec for activity progress report

--- spec/features/teacher/progress_reports/activity_progress_report_spec.rb
+++ spec/features/teacher/progress_reports/activity_progress_report_spec.rb
@@ -6,6 +6,22 @@ feature 'Activity Listing Progress Report', js: true do
   let(:mr_kotter) { FactoryGirl.create :mr_kotter }
   let(:report_page) { Teachers::ActivityProgressReportPage.new }
 
+  # TODO: Copied from activity_sessions_controller_spec. Would make
+  # more sense as a shared_context
+  let(:activity) { FactoryGirl.create(:activity) }
+  let(:classroom) { FactoryGirl.create(:classroom, teacher: mr_kotter) }
+  let!(:student) { FactoryGirl.create(:arnold_horshack, classroom: classroom) }
+  let(:classroom_activity) { FactoryGirl.create(:classroom_activity,
+    classroom: classroom, unit: classroom.units.first, activity: activity) }
+  let!(:activity_session) do
+    student.activity_sessions.create!(
+      state: 'finished',
+      time_spent: 120,
+      percentage: 0.75,
+      classroom_activity: classroom_activity
+    )
+  end
+
   context 'for a logged-in teacher' do
     before do
       sign_in_user mr_kotter
@@ -22,6 +38,20 @@ feature 'Activity Listing Progress Report', js: true do
           'Standard',
           'Score',
           'Student'
+        ]
+      )
+    end
+
+    it 'displays activity session data in the table' do
+      expect(report_page.table_rows.first).to eq(
+        [
+          activity.classification.name,
+          activity.name,
+          activity_session.completed_at.to_formatted_s(:quill_default),
+          '2 minutes',
+          'topic', # Derived from topic #
+          '75%',
+          student.name
         ]
       )
     end
--- spec/support/pages/teachers/progress_reports/progress_report_page.rb
+++ spec/support/pages/teachers/progress_reports/progress_report_page.rb
@@ -12,6 +12,14 @@ module Teachers
       table.all('th').map(&:text)
     end
 
+    def table_rows
+      rows = table.all('tr').map do |tr|
+        tr.all('td').map(&:text)
+      end
+      rows.shift # Ignore the header row
+      rows
+    end
+
     def table
       find('table')
     end
