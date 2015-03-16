5685e760ebe6ce620800e3c079d1d7fd273972d4
Ryan Novas <RyanNovas@users.noreply.github.com>
2015-03-16 14:31:18 -0400

    Update pages_helper.rb

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -24,40 +24,59 @@ module PagesHelper
 	      {
 		      img: 'thumb-peter.png',
 		      name: 'Peter Gault',
-		      title: 'Designer, Founder',
+		      title: 'Executive DIrector, Founder',
 		      desc: "Peter designs the user's experience and coordinates the Empirical team. Peter wrote his senior thesis at Bates College on the role of emotion in moral judgements."
 		  },
 		  {
 
 		      img: 'thumb-ryan.jpg',
 		      name: "Ryan Novas",
-		      title: "Operations Director",
+		      title: "Operations Director, Cofounder",
 		      desc: "Ryan works with teachers to create new features content for Quill. He also helps integrate Quill into classrooms and oversees daily operations at Empirical."
 	  	  },
 	  	  {
 		      img: 'thumb-tom.png',
 		      name: "Tom Calabrese",
-		      title: "Creative Director",
+		      title: "Creative Director, Cofounder",
 		      desc: "Thomas is a designer who likes to think about strategy, people, and science fiction. He studied at the Fashion Institute of Technology with a Bachelor’s degree in Communication Design, as well as the School of Visual Arts with a Masters in Branding."
 	      },
 	      {
 		      img: 'thumb-marcello.jpg',
 		      name: 'Marcello Sachs',
-		      title: 'Lead Developer',
+		      title: 'Technical Director, Cofounder',
 		      desc: "Marcello has a background in pure math from Brown University, and currently enjoys reading about ideas in analytic philosophy."
 		  },
 	  	  {
-		      img: 'thumb-josh.png',
-		      name: "Josh Symonds",
-		      title: "Developer",
-		      desc: "Josh performs devops and server wrangling on cloud-scale infrastructures, deploys amazing web applications with Ruby on Rails, and creates awesome iOS apps with Objective-C and RubyMotion."
+		      img: 'thumb_collin.png',
+		      name: "Collin Cowdery",
+		      title: "Development Director, Cofounder",
+		      desc: "Collin studied Japanese at Oberlin College. He raises funds for Quill via grants and sales. He also assists teachers and helps develop new learning mechanics for our apps."
 	      },
 	      {
-		      img: 'thumb-james.png',
-		      name: 'James Cox',
-		      title: "Developer",
-		      desc: "James is responsible for developing the Empirical API. He has experience in software engineering, engineering management, system architecture, SOA strategy, product validation, and MVPs."
+		      img: 'thumb_kris.jpg',
+		      name: 'Kristopher Kelly',
+		      title: "Senior Developer",
+		      desc: "Kristopher Kelly has been writing code professionally for the past 8 years. He previously was the lead developer of Omeka, an open source citation tool that is popular with digital historians."
 		  },
+   		{
+		      img: 'thumb-will.jpg',
+		      name: 'Will Laurance',
+		      title: 'Lead Apps Developer',
+		      desc: "Will is passionate about HTTP, web and mobile applications, distributed systems, databases, network security, and computer science as a whole. Open Source is changing the world one repository at a time, and he tries to open source as much as much as possible on GitHub."
+   		},
+   	      {
+
+		      img: 'thumb-alex.png',
+		      name: "Alex Redmon",
+		      title: "Open Source Developer",
+		      desc: "Alex is an art collector, I.T. professional, and entrepreneur living in Brooklyn who enjoys reading, writing, photography, and music."
+	  	  },
+	  	  {
+		      img: 'thumb-matt-coleman.png',
+		      name: "Matt Coleman",
+		      title: "Developer",
+		      desc: "Matt is a developer with a BS in Computer Engineering. He likes Fedora and lives in Boston."
+	      },
 		  {
 
 		      img: 'thumb-maggie.png',
@@ -71,31 +90,18 @@ module PagesHelper
 		      title: "Editor",
 		      desc: "Bill has thirty years' experience writing and editing his way through engineering failure analysis, gaming, life sciences, corporate training, hardware & software procedural documentation, political survey design, and education."
 	      },
-	      {
-		      img: 'thumb-will.jpg',
-		      name: 'Will Laurance',
-		      title: 'Developer',
-		      desc: "Will is passionate about HTTP, web and mobile applications, distributed systems, databases, network security, and computer science as a whole. Open Source is changing the world one repository at a time, and he tries to open source as much as possible on GitHub."
-		  },
 		  # {
 
 		  #     img: 'thumb-matt.png',
 		  #     name: "Matt Cox",
 		  #     title: "Community Manager",
 		  #     desc: "Matt engages community members and partners. He attends college in New York City and is studying communications."
-	  	#   },
-	      {
-
-		      img: 'thumb-alex.png',
-		      name: "Alex Redmon",
-		      title: "Developer",
-		      desc: "Alex is an art collector, I.T. professional, and entrepreneur living in Brooklyn who enjoys reading, writing, photography, and music."
-	  	  },
-	  	  {
-		      img: 'thumb-matt-coleman.png',
-		      name: "Matt Coleman",
-		      title: "Developer",
-		      desc: "Matt is a developer with a BS in Computer Engineering. He likes Fedora and lives in Boston."
+	  	#   }
+	  	{
+		      img: 'thumb_ben.png',
+		      name: "Ben Sussman",
+		      title: "Board Member",
+		      desc: "Ben is the Director of Engineering at Zoc Doc and advises Quill on all things technical."
 	      },
 	      {
 	      	img: "thumb-tim.png",
