70e8b75f69b52f618377a622623a4a195a79f74a
Marcello Sachs <marcello.sachs@gmail.com>
2015-03-16 13:14:24 -0400

    more building out of react scorebook, remove section.name from activity tooltip partial so that students dont see the grade level of their activities in their profiles (keep it though in tooltip shown in teacher-facing scorebook)

--- app/assets/javascripts/scorebook/scorebook/activity_icon_with_tooltip.jsx
+++ app/assets/javascripts/scorebook/scorebook/activity_icon_with_tooltip.jsx
@@ -2,12 +2,12 @@ EC.ActivityIconWithTooltip = React.createClass({
 
   percentage_color: function (percentage) {
     var y;
-    x = this.props.data.percentage
+    var x = this.props.data.percentage
     
 
     if (x < 0.5) {
       y = 'red'
-    } else if (x < 0.75) {
+    } else if (x <= 0.75) {
       y = 'orange'
     } else if (x <= 1.0) {
       y = 'green'
@@ -16,19 +16,49 @@ EC.ActivityIconWithTooltip = React.createClass({
     }
     return y
   },
+  componentDidMount: function () {
+    $(this.refs.activateTooltip.getDOMNode()).tooltip()
+  },  
+  icon_for_classification: function () {
+      var y; 
+      var x = this.props.data.activity.classification.id
+      if (x == 1) {
+        y = 'flag'
+      } else {
+        y = 'puzzle'
+      }
+      return y
+  },
+  displayPercentage: function () {
+    if (this.props.data.percentage == null) {
+      return ""
+    } else {
+      return "<p>" + (100*this.props.data.percentage) + "%" + "</p>"
+    }
+  },
+  tooltipTitle: function () {
+    return (
+          "<h1>" + this.props.data.activity.name +  "</h1>"
+        + "<p>" + this.props.data.activity.classification.alias + "</p>"
+        + "<p>" + this.props.data.activity.topic.section.name + "</p>"
+        + "<p>" + this.props.data.activity.topic.name + "</p>"
+        + "<p>" + this.props.data.activity.description + "</p>"
+        + "<p>" + this.props.data.activity.topic.topic_category.name + "</p>"
+        + this.displayPercentage()
+        + "<p>" + "...all concept class stats..." + "</p>"
+      )
 
-  icon_for_classification: function (classification) {
-    return 'puzzle'
   },
 
   render: function () {
     return (
       <div 
+        ref='activateTooltip'
         className={"activate-tooltip icon-wrapper icon-" + this.percentage_color() + " icon-" + this.icon_for_classification() }
         data-toggle='tooltip'
         data-html={true}
-        data-placement='left'
-        title={(this.props.data.percentage)}
+        data-placement='top'
+        title= {this.tooltipTitle()}
       >
       </div>
 
