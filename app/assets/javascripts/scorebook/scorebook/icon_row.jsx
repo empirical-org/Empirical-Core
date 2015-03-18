8878af39de01f62c9c3fc22e217c00a77bdb5d88
Marcello Sachs <marcello.sachs@gmail.com>
2015-03-18 12:52:31 -0400

    add keys to react array children; make #scores_for_scorebook order by percentage after ordering for sorting_name (to fix bug where scores corresponding to students on the boundary of scroll fetches were getting messed up)

---
+++ app/assets/javascripts/scorebook/scorebook/icon_row.jsx
@@ -0,0 +1,18 @@
+EC.IconRow = React.createClass({
+	render: function () {
+		
+		var row = _.map(this.props.data, function (ele) {
+			return (
+				<EC.ActivityIconWithTooltip key={ele.id} data={ele} />
+			);
+		});
+		
+
+		return (
+			<div className='icon-row'>
+				{row}
+			</div>
+
+		);
+	}
+})
