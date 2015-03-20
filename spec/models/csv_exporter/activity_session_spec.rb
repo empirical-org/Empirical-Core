439cf2a47c7f62b3879a81040e6e24cca299650d
Kristopher Kelly <kristopherbkelly@gmail.com>
2015-03-20 16:54:59 -0400

    Add more specs for activity session exporter

--- spec/models/csv_exporter/activity_session_spec.rb
+++ spec/models/csv_exporter/activity_session_spec.rb
@@ -4,6 +4,27 @@ describe CsvExporter::ActivitySession do
   include_context 'Activity Progress Report'
   let(:csv_exporter) { CsvExporter::ActivitySession.new }
 
+  describe '#header_row' do
+    it 'returns an array of headers' do
+      expect(csv_exporter.header_row).to eq(['app', 'activity', 'date', 'time_spent', 'standard', 'score', 'student'])
+    end
+  end
+
+  describe '#data_row' do
+    it 'returns an array of row data' do
+      expected_row = [
+        horshack_session.activity.classification.name,
+        horshack_session.activity.name,
+        horshack_session.completed_at.to_formatted_s(:quill_default),
+        horshack_session.time_spent,
+        horshack_session.activity.topic.name_prefix,
+        horshack_session.percentage,
+        horshack_session.user.name
+      ]
+      expect(csv_exporter.data_row(horshack_session)).to eq(expected_row)
+    end
+  end
+
   describe '#model_data' do
     subject { csv_exporter.model_data(mr_kotter, filters) }
 
