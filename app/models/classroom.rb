b9ed5dfe29aab71f0f3c849c893a86479e657007
Marcello Sachs <marcello.sachs@gmail.com>
2015-03-24 13:42:10 -0400

    fix issue 640

--- app/models/classroom.rb
+++ app/models/classroom.rb
@@ -3,8 +3,7 @@ class Classroom < ActiveRecord::Base
 
   validates_uniqueness_of :code
   validates_uniqueness_of :name, scope: :teacher_id, message: "A classroom called %{value} already exists. Please rename this classroom to a different name."
-
-  validates :grade, presence: true, inclusion: { in: Classroom::GRADES, message: "%{value} is not a valid grade" }
+  validates :grade, presence: true
   validates_presence_of :name
 
 
