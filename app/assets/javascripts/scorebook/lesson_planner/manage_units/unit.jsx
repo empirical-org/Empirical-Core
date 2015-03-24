59674ed160901cc04a471e6585c6e5cd1cfa97a0
Marcello Sachs <marcello.sachs@gmail.com>
2015-03-24 15:31:46 -0400

    add detail about who was assigned unit to units manager

--- app/assets/javascripts/scorebook/lesson_planner/manage_units/unit.jsx
+++ app/assets/javascripts/scorebook/lesson_planner/manage_units/unit.jsx
@@ -1,5 +1,5 @@
+"use strict";
 EC.Unit = React.createClass({
-
 	deleteUnit: function () {
 		var x = confirm("Are you sure you want to delete this Unit? It will delete all assignments given to students associated with this unit, even if those assignments have already been completed.");
 		if (x) {
@@ -7,18 +7,43 @@ EC.Unit = React.createClass({
 		}
 	},
 
-	render: function () {
-		var studentNoun, classroomActivities;
+	assignedToText: function () {
+		var studentNoun, classroomNoun, classroomsString;
 		if (this.props.data.num_students_assigned === 1) {
 			studentNoun = " Student";
 		} else {
 			studentNoun = " Students";
 		}
+		if (this.props.data.classrooms.length > 1) {
+			classroomNoun = " classes";
+		} else {
+			classroomNoun = " class";
+		}
 
-		classroomActivities = _.map(this.props.data.classroom_activities, function (ca) {
-			return (<EC.ClassroomActivity 
-							updateDueDate={this.props.updateDueDate} 
-							deleteClassroomActivity={this.props.deleteClassroomActivity} 
+		classroomsString = " ("
+		for (var i=0; i<this.props.data.classrooms.length; i++) {
+			var add;
+			if (i < this.props.data.classrooms.length -1) {
+				add = this.props.data.classrooms[i].name + ", ";
+			} else {
+				add = this.props.data.classrooms[i].name + ")";
+			}
+			classroomsString = classroomsString + add;
+		}
+
+		var txt = "Assigned to "
+		+ this.props.data.num_students_assigned
+		+ studentNoun
+		+ " in " + this.props.data.classrooms.length + classroomNoun
+		+ classroomsString
+		return txt;
+	},
+
+	render: function () {
+		var classroomActivities = _.map(this.props.data.classroom_activities, function (ca) {
+			return (<EC.ClassroomActivity
+							updateDueDate={this.props.updateDueDate}
+							deleteClassroomActivity={this.props.deleteClassroomActivity}
 							data={ca} />);
 		}, this);
 
@@ -29,7 +54,7 @@ EC.Unit = React.createClass({
 					<div className="col-md-2 vcenter pull-right delete-unit" onClick={this.deleteUnit}>Delete Unit</div>
 				</div>
 				<div className="unit-label">
-					{"Assigned to "  + this.props.data.num_students_assigned + studentNoun}
+					{this.assignedToText()}
 				</div>
 				<div className="table">
 					{classroomActivities}
@@ -37,5 +62,4 @@ EC.Unit = React.createClass({
 			</section>
 		);
 	}
-
 });
--- spec/controllers/teachers/units_controller_spec.rb
+++ spec/controllers/teachers/units_controller_spec.rb
@@ -1,24 +1,36 @@
 require 'spec_helper'
 
-describe Teachers::UnitsController, type: :controller do 
-  describe 'creating a unit' do
-    let(:teacher) { FactoryGirl.create(:teacher) }
-    let(:classroom) { FactoryGirl.create(:classroom, teacher: teacher) }
+describe Teachers::UnitsController, type: :controller do
+  let!(:teacher) { FactoryGirl.create(:teacher) }
+  let!(:classroom) { FactoryGirl.create(:classroom, teacher: teacher) }
 
-    before do
+  before do
       session[:user_id] = teacher.id # sign in, is there a better way to do this in test?
-    end
+  end
 
+  describe '#create' do
     it 'kicks off a background job' do
       expect {
-        post :create, classroom_id: classroom.id, 
+        post :create, classroom_id: classroom.id,
                       unit: {
-                        name: 'A Cool Learning Experience', 
+                        name: 'A Cool Learning Experience',
                         classrooms: [],
                         activities: []
                       }
         expect(response.status).to eq(200)
       }.to change(AssignActivityWorker.jobs, :size).by(1)
+    end
+  end
+
+  describe '#index' do
+    let!(:activity) {FactoryGirl.create(:activity)}
+    let!(:unit) {FactoryGirl.create(:unit)}
+    let!(:classroom_activity) {FactoryGirl.create(:classroom_activity, due_date: Time.now, unit: unit, classroom: classroom, activity: activity)}
+
+    it 'includes classrooms' do
+      post :index
+      x = JSON.parse(response.body)
+      expect(x['units'][0]['classrooms']).to_not be_empty
     end
   end
 end
