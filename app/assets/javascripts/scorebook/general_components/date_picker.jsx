e1cfcfa3e3133e4a361962f5b3d296b0740c9c4e
Marcello Sachs <marcello.sachs@gmail.com>
2015-03-24 12:49:00 -0400

    enable deselect of date filters

--- app/assets/javascripts/scorebook/general_components/date_picker.jsx
+++ app/assets/javascripts/scorebook/general_components/date_picker.jsx
@@ -1,6 +1,12 @@
 "use strict";
 EC.DatePicker = React.createClass({
 
+  getInitialState: function () {
+    return {
+      isDateSelected: false
+    };
+  },
+
   componentDidMount: function () {
     $(this.refs.date.getDOMNode()).datepicker({
       selectOtherMonths: true,
@@ -13,21 +19,42 @@ EC.DatePicker = React.createClass({
   },
 
   handleChange: function () {
-    var x1 = '#railsFormatDate' + this.props.key;
-    var dom = $(x1);
-    var val = dom.val();
-    this.props.handleChange(val);
+    this.setState({isDateSelected: true}, function () {
+      var x1 = '#railsFormatDate' + this.props.key;
+      var dom = $(x1);
+      var val = dom.val();
+      this.props.handleChange(val);
+    });
+  },
+
+  deselect: function () {
+    this.setState({isDateSelected: false}, function () {
+      $(this.refs.date.getDOMNode()).datepicker('setDate')
+      this.props.handleChange(null)
+    })
   },
 
   render: function () {
+    var unselect, additionalClassName;
+    if (this.state.isDateSelected) {
+      unselect = <span className='icon-x-gray' onClick={this.deselect}></span>
+      additionalClassName = "date-is-selected";
+    } else {
+      unselect = null;
+      additionalClassName = "";
+    }
+
     return (
       <span className="datepicker-container">
-        <input
-          type="text"
-          ref="date"
-          className="datepicker-input"
-          placeholder={this.props.placeHolder}
-          onChange={this.handleChange} />
+        <span className="datepicker-and-x">
+          <input
+            type="text"
+            ref="date"
+            className={"datepicker-input " + additionalClassName}
+            placeholder={this.props.placeHolder}
+            onChange={this.handleChange} />
+          {unselect}
+        </span>
         <input
           type="text"
           ref="railsFormatDate"
