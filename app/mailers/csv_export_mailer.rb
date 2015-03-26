8153592eaed363c2ab9c95d5ab3f29d851f2d9bd
Kristopher Kelly <kristopherbkelly@gmail.com>
2015-03-26 14:01:25 -0400

    Refs #625, change URL in email to a signed S3 URL

--- app/mailers/csv_export_mailer.rb
+++ app/mailers/csv_export_mailer.rb
@@ -2,7 +2,7 @@ class CsvExportMailer < ActionMailer::Base
   default from: 'hello@quill.org'
 
   def csv_download(csv_export)
-    @download_url = csv_export.csv_file.url
+    @download_url = csv_export.csv_file.file.authenticated_url
     mail to: csv_export.teacher.email, subject: 'Quill.org Teacher CSV Export'
   end
 end
