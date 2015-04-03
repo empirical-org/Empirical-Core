8d3d4474baea1a0294ff6fb9e393f2d153d6021b
Kristopher Kelly <kristopherbkelly@gmail.com>
2015-04-03 10:54:04 -0400

    Implement server-side pagination for activity sessions report

--- app/assets/javascripts/scorebook/progress_reports/progress_report.jsx
+++ app/assets/javascripts/scorebook/progress_reports/progress_report.jsx
@@ -1,9 +1,7 @@
 //= require ./table_filter_mixin.js
 //= require ./table_sorting_mixin.js
-//= require ./table_pagination_mixin.js
-
 EC.ProgressReport = React.createClass({
-  mixins: [EC.TableFilterMixin, EC.TablePaginationMixin, EC.TableSortingMixin],
+  mixins: [EC.TableFilterMixin, EC.TableSortingMixin],
 
   propTypes: {
     columnDefinitions: React.PropTypes.func.isRequired,
@@ -16,8 +14,17 @@ EC.ProgressReport = React.createClass({
     exportCsv: React.PropTypes.string
   },
 
+  getDefaultProps: function() {
+    return {
+      maxPageNumber: 4
+    };
+  },
+
   getInitialState: function() {
     return {
+      currentPage: 1,
+      numPages: 1,
+
       loading: false,
 
       results: [],
@@ -47,11 +54,18 @@ EC.ProgressReport = React.createClass({
 
   // Get results after pagination has been applied.
   getVisibleResults: function(filteredResults) {
-    if (this.props.pagination) {
-      return this.applyPagination(filteredResults, this.state.currentPage);
-    } else {
-      return filteredResults;
-    }
+    return filteredResults;
+  },
+
+  goToPage: function(page) {
+    var newState = {
+      currentPage: page
+    };
+    this.setState(newState, this.fetchData);
+  },
+
+  resetPagination: function(next) {
+    this.setState({currentPage: 1}, next);
   },
 
   // Filter sessions based on the classroom ID.
@@ -82,8 +96,13 @@ EC.ProgressReport = React.createClass({
 
   fetchData: function() {
     this.setState({loading: true});
-    $.get(this.props.sourceUrl, this.state.currentFilters, function onSuccess(data) {
+    var requestParams = _.extend(this.state.currentFilters, {});
+    if (this.props.pagination) {
+      requestParams = _.extend(requestParams, {page: this.state.currentPage});
+    }
+    $.get(this.props.sourceUrl, requestParams, function onSuccess(data) {
       this.setState({
+        numPages: data.page_count,
         loading: false,
         results: data[this.props.jsonResultsKey],
         teacher: data.teacher,
@@ -103,11 +122,10 @@ EC.ProgressReport = React.createClass({
     var pagination, csvExport, mainSection;
     var filteredResults = this.getFilteredResults();
     if (this.props.pagination) {
-      var numberOfPages = this.calculateNumberOfPages(filteredResults);
       pagination = <EC.Pagination maxPageNumber={this.props.maxPageNumber}
                                   selectPageNumber={this.goToPage}
                                   currentPage={this.state.currentPage}
-                                  numberOfPages={numberOfPages}  />;
+                                  numberOfPages={this.state.numPages}  />;
     }
     var visibleResults = this.getVisibleResults(filteredResults);
 
--- spec/controllers/teachers/progress_reports/activity_sessions_controller_spec.rb
+++ spec/controllers/teachers/progress_reports/activity_sessions_controller_spec.rb
@@ -4,6 +4,7 @@ describe Teachers::ProgressReports::ActivitySessionsController, :type => :contro
   let(:teacher) { FactoryGirl.create(:teacher) }
   include_context 'Topic Progress Report'
   it_behaves_like 'Progress Report' do
+    let(:default_filters) { {page: 1} }
     let(:result_key) { 'activity_sessions' }
     let(:expected_result_count) { visible_activity_sessions.size }
   end
@@ -17,27 +18,32 @@ describe Teachers::ProgressReports::ActivitySessionsController, :type => :contro
       end
 
       it 'includes the serialized data for activity sessions' do
-        xhr :get, :index
+        xhr :get, :index, page: 1
         expect(json['activity_sessions'][0]['activity_classification_name']).to_not be_nil
       end
 
+      it 'includes the number of pages of results' do
+        xhr :get, :index, {page: 1}
+        expect(json['page_count']).to eq(1)
+      end
+
       it 'can filter by classroom' do
-        xhr :get, :index, {classroom_id: empty_classroom.id}
+        xhr :get, :index, {classroom_id: empty_classroom.id, page: 1}
         expect(json['activity_sessions'].size).to eq(0)
       end
 
       it 'can filter by unit' do
-        xhr :get, :index, {unit_id: empty_unit.id}
+        xhr :get, :index, {unit_id: empty_unit.id, page: 1}
         expect(json['activity_sessions'].size).to eq(0)
       end
 
       it 'can filter by student' do
-        xhr :get, :index, {student_id: zojirushi.id}
+        xhr :get, :index, {student_id: zojirushi.id, page: 1}
         expect(json['activity_sessions'].size).to eq(1)
       end
 
       it 'fetches classroom, unit, and student data for the filter options' do
-        xhr :get, :index
+        xhr :get, :index, page: 1
         expect(json['classrooms'].size).to eq(1)
         expect(json['units'].size).to eq(1)
         expect(json['students'].size).to eq(visible_students.size)
