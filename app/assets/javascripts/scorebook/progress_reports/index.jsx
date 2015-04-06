20ead326cbd1bf7befd8c159e0f4923179c4d316
Kristopher Kelly <kristopherbkelly@gmail.com>
2015-04-06 15:32:47 -0400

    Remove some unused progress reports JS

--- app/assets/javascripts/scorebook/progress_reports/index.jsx
+++ app/assets/javascripts/scorebook/progress_reports/index.jsx
@@ -1,12 +1,9 @@
 $(function() {
   var progressReportMapping = {
     '.progress-reports-activities': EC.ActivitiesProgressReport,
-    '.progress-reports-sections': EC.SectionsProgressReport,
-    '.progress-reports-topics': EC.TopicsProgressReport,
     '.progress-reports-concept-categories': EC.ConceptCategoriesProgressReport,
     '.progress-reports-concept-tags': EC.ConceptTagsProgressReport,
     '.progress-reports-concept-students': EC.ConceptTagsStudentsProgressReport,
-    '.progress-reports-topic-students': EC.TopicsStudentsProgressReport,
     '.progress-reports-standards-classrooms': EC.StandardsAllClassroomsProgressReport,
     '.progress-reports-standards-classroom-students': EC.StandardsClassroomStudentsProgressReport,
     '.progress-reports-standards-topics': EC.StandardsTopicsProgressReport,
