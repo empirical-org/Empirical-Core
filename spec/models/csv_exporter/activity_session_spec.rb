0e25c4267330983aa529b5521759c3013809f64f
Kristopher Kelly <kristopherbkelly@gmail.com>
2015-04-07 12:10:56 -0400

    Add CSV export for Standards: All Classrooms page

--- spec/models/csv_exporter/activity_session_spec.rb
+++ spec/models/csv_exporter/activity_session_spec.rb
@@ -2,25 +2,18 @@ require 'rails_helper'
 
 describe CsvExporter::ActivitySession do
   include_context 'Activity Progress Report'
-  let(:csv_exporter) { CsvExporter::ActivitySession.new }
 
-  describe '#header_row' do
-    it 'returns an array of headers' do
-      expect(csv_exporter.header_row).to eq(['Student', 'Activity', 'Score', 'Time Spent', 'Standard Level', 'Standard', 'App', 'Date'])
-    end
-  end
+  it_behaves_like 'CSV Exporter' do
+    let(:expected_header_row) {
+      ['Student', 'Activity', 'Score', 'Time Spent', 'Standard Level', 'Standard', 'App', 'Date']
+    }
 
-  describe '#data_row' do
-    it 'returns an array of row data' do
-      # Student -
-      # Activity -
-      # Score -
-      # Time Spent -
-      # Standard Level (Section) -
-      # Standard (Topic) -
-      # App -
-      # Date
-      expected_row = [
+    let(:model_instance) {
+      horshack_session
+    }
+
+    let(:expected_data_row) {
+      [
         horshack_session.user.name,
         horshack_session.activity.name,
         horshack_session.percentage_as_decimal,
@@ -32,27 +25,14 @@ describe CsvExporter::ActivitySession do
         horshack_session.completed_at.to_formatted_s(:quill_default)
 
       ]
-      expect(csv_exporter.data_row(horshack_session)).to eq(expected_row)
-    end
-  end
+    }
 
-  describe '#model_data' do
-    subject { csv_exporter.model_data(mr_kotter, filters) }
+    let(:model_data_subject) {
+      csv_exporter.model_data(mr_kotter, { 'classroom_id' => sweathogs.id })
+    }
 
-    context 'filtering by classroom ID' do
-      let(:filters) { { 'classroom_id' => sweathogs.id } }
-
-      it 'retrieves filtered activity sessions' do
-        expect(subject.size).to eq(sweathogs_sessions.size)
-      end
-    end
-
-    context 'unfiltered' do
-      let(:filters) { {} }
-
-      it 'retrieves activity sessions without any filters' do
-        expect(subject.to_a).to eq(all_sessions)
-      end
-    end
+    let(:expected_model_data_size) {
+      sweathogs_sessions.size
+    }
   end
 end
