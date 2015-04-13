ddd35d6bcbb749b4c91164bcac2baef7977660f2
Josh Symonds <veraticus@gmail.com>
2015-04-13 11:15:12 -0500

    When a teacher signs in, import all their students

--- app/models/district.rb
+++ app/models/district.rb
@@ -2,14 +2,14 @@ class District < ActiveRecord::Base
   has_and_belongs_to_many :users
 
   def self.setup_from_clever(auth_hash)
-    district = District.where(clever_id: auth_hash[:info][:id]).first_or_initialize
+    district = ::District.where(clever_id: auth_hash[:info][:id]).first_or_initialize
     district.import_from_clever! if district.new_record?
     district.update_attributes(name: auth_hash[:info][:name], token: auth_hash[:credentials][:token])
     district
   end
 
   def self.create_from_clever(id)
-    district = District.where(clever_id: id).first_or_initialize
+    district = ::District.where(clever_id: id).first_or_initialize
     district.import_from_clever! if district.new_record?
     district.update_attributes(name: district.clever_district_name)
     district
