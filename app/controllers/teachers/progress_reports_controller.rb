d75d31e6acc30ff217044d1779cf5678e9b28e88
Marcello Sachs <marcello.sachs@gmail.com>
2015-04-15 17:43:17 -0400

    change username of demo teacher

--- app/controllers/teachers/progress_reports_controller.rb
+++ app/controllers/teachers/progress_reports_controller.rb
@@ -4,7 +4,7 @@ class Teachers::ProgressReportsController < ApplicationController
   before_action :set_vary_header, if: -> { request.xhr? }
 
   def demo
-    @user = User.find_by_username 'chavezdemo279'
+    @user = User.find_by_username 'demo'
     sign_in @user
     redirect_to teachers_progress_reports_standards_classrooms_path
   end
