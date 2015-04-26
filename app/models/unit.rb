3e71d14bf23c2ddfa5d91d7b7525768445aba29b
Scott <scottalankatz@gmail.com>
2015-04-26 16:08:05 -0400

    Added Validation To Unit Model
    validates the presence of name

--- app/models/unit.rb
+++ app/models/unit.rb
@@ -5,6 +5,8 @@ class Unit < ActiveRecord::Base
   has_many :activities, through: :classroom_activities
   has_many :topics, through: :activities
 
+  validates_presence_of :name
+
   def self.for_standards_progress_report(teacher, filters)
     with(best_activity_sessions: ActivitySession.for_standards_report(teacher, filters))
       .select("units.id as id, units.name as name")
