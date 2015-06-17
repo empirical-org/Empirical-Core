f6082cc7a643eb31839a274fe11b1df25d004d33
Kristopher Kelly <kristopherbkelly@gmail.com>
2015-06-16 18:48:01 -0700

    Refactor login form to use 'scorebook' layout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,6 @@
 class SessionsController < ApplicationController
   before_filter :signed_in!, only: [:destroy]
-  layout 'old'
+  layout 'scorebook'
 
   def create
     params[:user][:email].downcase! unless params[:user][:email].nil?
