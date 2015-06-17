d5706094ca51bafb2922101bb4501ddd6b41ea09
Kristopher Kelly <kristopherbkelly@gmail.com>
2015-06-16 18:32:43 -0700

    Refactor activities view to use 'scorebook' layout

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,5 +1,5 @@
 class ActivitiesController < ApplicationController
-  layout 'old'
+  layout 'scorebook'
   before_action :authorize!, only: [:show]
   before_action :get_activity, only: [:update, :retry, :show]
 
