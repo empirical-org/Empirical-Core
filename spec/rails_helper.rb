1f87d3f955a003f1b7646facad0570eb42b55b86
Marcello Sachs <marcello.sachs@gmail.com>
2015-06-18 17:26:24 -0400

    remove line that makes vcr record new cassettes

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -18,7 +18,6 @@ VCR.configure do |c|
   c.cassette_library_dir = 'spec/cassettes'
   c.hook_into :webmock
   c.configure_rspec_metadata!
-  c.default_cassette_options = {:record => :new_episodes}
 end
 
 
