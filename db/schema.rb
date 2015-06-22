6112746f60055c09530256d43ee03b33b000e941
Kristopher Kelly <kristopherbkelly@gmail.com>
2015-06-22 11:27:42 -0700

    Remove unused HomepageNewsSlides model/controller/views

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150622172421) do
+ActiveRecord::Schema.define(version: 20150622181401) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -192,16 +192,6 @@ ActiveRecord::Schema.define(version: 20150622172421) do
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
-  end
-
-  create_table "homepage_news_slides", force: true do |t|
-    t.string   "link"
-    t.integer  "position"
-    t.string   "image"
-    t.text     "text"
-    t.integer  "author_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
   end
 
   create_table "oauth_access_grants", force: true do |t|
