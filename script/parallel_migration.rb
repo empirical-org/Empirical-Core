a81df3b9836e40a46df239094e2006eea6b95c9b
Kristopher Kelly <kristopherbkelly@gmail.com>
2015-06-22 12:26:55 -0700

    Remove old unused scripts from scripts/

--- script/parallel_migration.rb
+++
@@ -1,9 +0,0 @@
-system %q|/usr/local/heroku/bin/heroku run "rails r 'load(%(./script/begin_migration.rb))'" -a empirical-grammar-staging|
-
-chapters = Chapter.order('id asc')
-
-chapters.map do |chapter|
-  puts 'starting now'
-  puts   %Q|/usr/local/heroku/bin/heroku run "rails r 'load(%(./script/migrate_to_new_formats.rb))' #{chapter.id}" -a empirical-grammar-staging|
-  res =    `/usr/local/heroku/bin/heroku run "rails r 'load(%(./script/migrate_to_new_formats.rb))' #{chapter.id}" -a empirical-grammar-staging`
-end
