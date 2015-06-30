da249253e8f7245beef2e9a4860c574d91d3a875
Kristopher Kelly <kristopherbkelly@gmail.com>
2015-06-30 11:07:06 -0700

    Fix firebase token generation and add specs

--- app/controllers/api/v1/firebase_tokens_controller.rb
+++ app/controllers/api/v1/firebase_tokens_controller.rb
@@ -1,9 +1,7 @@
 class Api::V1::FirebaseTokensController < ApiController
 
   def create
-    byebug
     app = FirebaseApp.find_by_name(params[:app])
-    current_user = User.find(session[:user_id])
     render json: {
       token: app.token_for(current_user)
     }
