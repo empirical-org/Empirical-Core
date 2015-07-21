c9c9bd550af2d967b080b26b0ef1c8eb746406d2
Marcello Sachs <marcello.sachs@gmail.com>
2015-07-21 11:53:07 -0400

    organize lesson_planner react code into subfolders; also fix #789 by giving react elements key prop

--- app/assets/javascripts/lesson_planner/create_unit/stage1/activity_search/activity_search_results.jsx
+++ app/assets/javascripts/lesson_planner/create_unit/stage1/activity_search/activity_search_results.jsx
@@ -1,12 +1,12 @@
 EC.ActivitySearchResults = React.createClass({
-	
+
 
 
 	render: function () {
 		rows = _.map(this.props.currentPageSearchResults, function (ele) {
 			var selected = _.include(this.props.selectedActivities, ele)
-			
-			return <EC.ActivitySearchResult data={ele} selected={selected} toggleActivitySelection={this.props.toggleActivitySelection} />
+
+			return <EC.ActivitySearchResult key={ele.id} data={ele} selected={selected} toggleActivitySelection={this.props.toggleActivitySelection} />
 		}, this);
 		return (
 			<tbody>
--- app/assets/javascripts/lesson_planner/manage_units/units.jsx
+++ app/assets/javascripts/lesson_planner/manage_units/units.jsx
@@ -9,10 +9,11 @@ EC.Units = React.createClass({
 
 	render: function () {
 		var units = _.map(this.props.data, function (data) {
-			return (<EC.Unit 
-							updateDueDate={this.props.updateDueDate} 
-							deleteUnit={this.props.deleteUnit} 
-							deleteClassroomActivity={this.props.deleteClassroomActivity} 
+			return (<EC.Unit
+							key={data.unit.id}
+							updateDueDate={this.props.updateDueDate}
+							deleteUnit={this.props.deleteUnit}
+							deleteClassroomActivity={this.props.deleteClassroomActivity}
 							data={data} />);
 		}, this);
 
