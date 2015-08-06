7353ae3178cb2b66ceb8ddb974331b657291c9d0
Kristopher Kelly <kristopherbkelly@gmail.com>
2015-08-06 16:20:00 -0700

    Refs #821 add method to retrieve leaf nodes from the tree

--- app/models/concept.rb
+++ app/models/concept.rb
@@ -2,4 +2,12 @@ class Concept < ActiveRecord::Base
   include Uid
   belongs_to :parent, class_name: 'Concept', foreign_key: :parent_id
   validates :name, presence: true
+
+  # Find all the concepts that are not a parent of any other concept
+  def self.leaf_nodes
+    concepts = Concept.arel_table
+    distinct_parent_ids = concepts.project('DISTINCT(parent_id)')
+                                  .where(concepts[:parent_id].not_eq(nil))
+    where.not(concepts[:id].in(distinct_parent_ids))
+  end
 end
