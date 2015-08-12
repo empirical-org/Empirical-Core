12ae57c00142eb09287b0cbe9f71640af50c3b35
Kristopher Kelly <kristopherbkelly@gmail.com>
2015-08-12 14:57:05 -0700

    Refs #838, add percentage column to concepts view

--- app/assets/javascripts/progress_reports/concepts_concepts_progress_report.jsx
+++ app/assets/javascripts/progress_reports/concepts_concepts_progress_report.jsx
@@ -32,6 +32,14 @@ EC.ConceptsConceptsProgressReport = React.createClass({
         name: 'Incorrect',
         field: 'incorrect_result_count',
         sortByField: 'incorrect_result_count'
+      },
+      {
+        name: 'Percentage',
+        field: 'percentage',
+        sortByField: 'percentage',
+        customCell: function(row) {
+          return row['percentage'] + '%';
+        }
       }
     ];
   },
