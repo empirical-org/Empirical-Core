5fcec804775b029fc5d22fe4d51ad92eb24d1584
Marcello Sachs <marcello.sachs@gmail.com>
2015-08-17 15:17:44 -0400

    code cleanup

--- app/models/concerns/api_data.rb
+++ app/models/concerns/api_data.rb
@@ -2,14 +2,8 @@ module ApiData
   extend ActiveSupport::Concern
 
   def api_data
-
-    #x = Section
-    x = self
-    data = self.all.map do |s|
+    self.all.map do |s|
       (Api::SimpleSerializer.new(s)).as_json(root: false)
     end
-
-
-    data
   end
 end
