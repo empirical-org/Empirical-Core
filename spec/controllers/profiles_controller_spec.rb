b1c67a96a6f7f82a9e5413fd5865e22d0afda241
Kristopher Kelly <kristopherbkelly@gmail.com>
2015-08-24 10:47:56 -0400

    Fix exploding student profile and add spec

---
+++ spec/controllers/profiles_controller_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+describe ProfilesController, :type => :controller do
+  render_views
+
+  describe 'as a student' do
+    let(:student) { FactoryGirl.create(:student) }
+    let(:activity) { FactoryGirl.create(:activity) }
+    let(:classroom_activity) { FactoryGirl.create(:classroom_activity, activity: activity) }
+    let!(:activity_session) { FactoryGirl.create(:activity_session_incompleted,
+                                                classroom_activity: classroom_activity,
+                                                activity: activity,
+                                                state: 'unstarted',
+                                                user: student) }
+
+    before do
+      session[:user_id] = student.id
+    end
+
+    subject do
+      get :show
+    end
+
+    it 'loads the student profile' do
+      subject
+      expect(response.status).to eq(200)
+    end
+  end
+end
