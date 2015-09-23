186b5de5cf9cff62faebd31689138741430e4b1a
Donald McKendrick <ddmckendrick@gmail.com>
2015-09-22 22:51:32 -0400

    change migrations so that they now complete when run from a new install

--- db/migrate/20141014162137_add_app_name_to_classification.rb
+++ db/migrate/20141014162137_add_app_name_to_classification.rb
@@ -3,8 +3,5 @@ class AddAppNameToClassification < ActiveRecord::Migration
 
     add_column :activity_classifications, :app_name, :string
 
-    ActivityClassification.find(1).try(:update_column,:app_name, 'grammar')
-    ActivityClassification.find(2).try(:update_column, :app_name, 'grammar')
-
   end
 end
