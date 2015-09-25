a6f8f3e17ca02ade0efe325a7c2f22db36bbfa1b
Marcello Sachs <marcello.sachs@gmail.com>
2015-09-25 19:58:47 -0400

    stop sending buggy welcome email

--- app/workers/account_creation_worker.rb
+++ app/workers/account_creation_worker.rb
@@ -6,7 +6,8 @@ class AccountCreationWorker
     @user = User.find(id)
 
     # send email, subscriptions
-    @user.send_welcome_email
+    # FIXME : had to comment out below because its sending out like 7 emails per new account
+    #@user.send_welcome_email
 
     # tell keen
     KeenWrapper.publish(:accounts, {event: 'creation', role: @user.role, classcode: @user.classcode})
