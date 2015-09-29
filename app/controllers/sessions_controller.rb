463ee9122194f42cdbfa63c8eb576fc25a37b50c
Marcello Sachs <marcello.sachs@gmail.com>
2015-09-29 15:43:17 -0400

    debug email

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -79,6 +79,7 @@ class SessionsController < ApplicationController
       redirect_to new_account_path
     else
       sign_in user
+      user.send_welcome_email
       AccountCreationWorker.perform_async(user.id)
       user.subscribe_to_newsletter
       if user.role == 'teacher'
