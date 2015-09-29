0c75d61a6bb36fb4bd631cdfe4188edb721cbd58
Marcello Sachs <marcello.sachs@gmail.com>
2015-09-29 16:42:59 -0400

    debug worker

--- app/services/account_creation_callbacks.rb
+++ app/services/account_creation_callbacks.rb
@@ -8,7 +8,7 @@ class AccountCreationCallbacks
   def trigger
     # dont combine these into one worker, because
     # each worker continuously retries if it fails (so you dont want to repeatedly do the one which doesnt fail)
-    WelcomeEmailWorker.perform_async(@user)
+    WelcomeEmailWorker.perform_async(@user.id)
     AccountCreationWorker.perform_async(@user.id)
   end
 
