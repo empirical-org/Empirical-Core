2ac5a6e06af4e95497dd3097f28fd12f8d449b68
Marcello Sachs <marcello.sachs@gmail.com>
2015-10-05 14:59:58 -0400

    fixes #929

--- db/oauth.sql
+++ db/oauth.sql
@@ -2,8 +2,8 @@
 -- Data for Name: activity_classifications; Type: TABLE DATA; Schema: public; Owner: -
 --
 DELETE FROM activity_classifications;
-INSERT INTO activity_classifications VALUES (1, 'passage', 'passage', 'http://localhost:3002/stories/form', 'IACa8Egt7CvVYgtHPtEn2w', 'http://localhost:3002/stories/module', '2014-04-19 00:05:03.052133', '2014-04-19 00:05:03.052133', 'grammar');
-INSERT INTO activity_classifications VALUES (2, 'sentence', 'sentence', 'http://localhost:3002/practice_questions/form', 's2u3tVuguhfUjOQxDP-7RA', 'http://localhost:3002/practice_questions/module', '2014-04-19 00:05:03.11343', '2014-04-19 00:05:03.11343', 'grammar');
+INSERT INTO activity_classifications VALUES (1, 'passage', 'passage', 'http://localhost:3002/play/pf', 'IACa8Egt7CvVYgtHPtEn2w', 'http://localhost:3002/play/pf', '2014-04-19 00:05:03.052133', '2014-04-19 00:05:03.052133', 'grammar');
+INSERT INTO activity_classifications VALUES (2, 'sentence', 'sentence', 'http://localhost:3002/play/sw', 's2u3tVuguhfUjOQxDP-7RA', 'http://localhost:3002/play/sw', '2014-04-19 00:05:03.11343', '2014-04-19 00:05:03.11343', 'grammar');
 
 
 --
