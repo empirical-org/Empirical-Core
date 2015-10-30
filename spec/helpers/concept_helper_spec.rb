2bd60742703917eee36076b6197d7253ada832cc
Marcello Sachs <marcello.sachs@gmail.com>
2015-10-30 12:10:07 -0400

    remove concept results from student profile until we can make a less expensive query

--- spec/helpers/concept_helper_spec.rb
+++ spec/helpers/concept_helper_spec.rb
@@ -21,17 +21,22 @@ describe ConceptHelper, type: :helper do
       )
     end
 
-    it "displays all concept stats for an activity session" do
+    it 'displays nothing because weve turned it off since the query is killing response time (need to rewrite query)' do
       html = helper.all_concept_stats(activity_session)
-      expect(html).to include(punctuation_concept.name)
-      expect(html).to include(prepositions_concept.name)
+      expect(html).to eq('')
     end
 
-    it "displays a breakdown of the grammar concepts and correct/incorrect" do
-      html = helper.all_concept_stats(activity_session)
-      expect(html).to include(prepositions_concept.name)
-      expect(html).to include("1")
-      expect(html).to include("0")
-    end
+    # it "displays all concept stats for an activity session" do
+    #   html = helper.all_concept_stats(activity_session)
+    #   expect(html).to include(punctuation_concept.name)
+    #   expect(html).to include(prepositions_concept.name)
+    # end
+
+    # it "displays a breakdown of the grammar concepts and correct/incorrect" do
+    #   html = helper.all_concept_stats(activity_session)
+    #   expect(html).to include(prepositions_concept.name)
+    #   expect(html).to include("1")
+    #   expect(html).to include("0")
+    # end
   end
 end
