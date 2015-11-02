fa98864a561d0fe5fd55c635acdbde6bfc7862ba
RyanNovas <ryan@quill.org>
2015-11-02 17:35:29 -0500

    all archived classes hidden except in invite students

--- app/models/classroom.rb
+++ app/models/classroom.rb
@@ -5,6 +5,7 @@ class Classroom < ActiveRecord::Base
   validates_uniqueness_of :name, scope: :teacher_id
   validates :grade, presence: true
   validates_presence_of :name
+  scope :visible, -> {where(archived: false)}
 
 
   has_many :units
