9cd23d01c7c3adfca1d1cdd7597ea0ae99f6b543
Marcello Sachs <marcello.sachs@gmail.com>
2015-11-04 18:42:49 -0500

    remove ConceptSerializer in ScorebookQuery and put into json manually to cut down time significantly; refactor queries/; add performance tests

---
+++ spec/queries/scorebook/activity_sessions_query_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+describe 'ActivitySessionsQuery' do
+  let!(:teacher) { FactoryGirl.create(:user, role: 'teacher') }
+  let!(:classroom) { FactoryGirl.create(:classroom, teacher: teacher) }
+  let!(:student) { FactoryGirl.create(:user, role: 'student', classcode: classroom.code) }
+  let!(:activity) { FactoryGirl.create(:activity) }
+  let!(:activity_session) { FactoryGirl.create(:activity_session, user: student, activity: activity) }
+
+  let!(:activity_sessions_query) { ActivitySessionsQuery.new }
+
+  def subject
+    activity_sessions_query.query(teacher, classroom.id)
+  end
+
+  # FIXME: break this method out into a separate class so were not testing a private method
+  it 'works' do
+    sessions = subject
+    expect(sessions).to_not be_empty
+  end
+
+end
