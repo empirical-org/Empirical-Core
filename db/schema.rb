c57544442899520cefb1455e5c4526f8275962af
RyanNovas <ryan@quill.org>
2015-11-09 18:37:18 -0500

    deletes hidden and introduces visible column

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151109223356) do
+ActiveRecord::Schema.define(version: 20151109231813) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -121,7 +121,7 @@ ActiveRecord::Schema.define(version: 20151109223356) do
     t.datetime "updated_at"
     t.string   "clever_id"
     t.string   "grade"
-    t.boolean  "hidden",     default: false, null: false
+    t.boolean  "visible",    default: true, null: false
   end
 
   add_index "classrooms", ["code"], name: "index_classrooms_on_code", using: :btree
