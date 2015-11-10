29e2da5f11d5ff41d7a225a45f6788432688c7d6
Marcello Sachs <marcello.sachs@gmail.com>
2015-11-10 13:11:26 -0500

    perf tests for profile query

---
+++ spec/performance/queries/profile/query_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+require 'benchmark'
+
+describe 'Profile::Query' do
+
+  include_context "big profile"
+
+  let(:profile_query) { Profile::Query.new }
+
+  def subject
+    profile_query.query(student)
+  end
+
+  it 'takes less than 1 sec' do
+    time = Benchmark.realtime{ subject }
+    expect(time).to be < 1
+  end
+
+
+end
