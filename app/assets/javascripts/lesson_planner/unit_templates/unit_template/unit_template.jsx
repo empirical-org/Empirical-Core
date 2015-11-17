413d84309805c637d77bc136b215b8e6ac8e9a52
Donald McKendrick <ddmckendrick@gmail.com>
2015-11-17 18:05:48 -0500

    Add most of the styling for the title area

--- app/assets/javascripts/lesson_planner/unit_templates/unit_template/unit_template.jsx
+++ app/assets/javascripts/lesson_planner/unit_templates/unit_template/unit_template.jsx
@@ -15,82 +15,67 @@ EC.UnitTemplate = React.createClass({
   render: function () {
     return (
       <div>
-        <div className='row big-title middle-school'>
-          <div className='col-xs-12'>
-            <a className='unit-template-category-label img-rounded'>UNIVERSITY</a>
-            <h1>{this.props.data.model.name}</h1>
+        <div className='big-title middle-school'>
+
+            <p>
+              <a className='unit-template-category-label img-rounded'>UNIVERSITY</a>
+            </p>
+            <h1><strong>{this.props.data.model.name}</strong></h1>
             <div className="author-details">
-              <img src={this.props.data.model.author.avatar_url}></img>
-              <p>by Silvia Furth</p>
+              <div className="author-picture">
+                <img src="http://www.fillmurray.com/300/300"></img>
+              </div>
+              <p>by Bill Murray</p>
+            </div>
+
+        </div>
+
+        <div className='row'>
+          <div className='col-xs-6'>
+            <dl>
+              <dt><strong>Problem</strong></dt>
+              <dd>Students often forget to add commas in addresses and to add apostrophes in contractions</dd>
+
+              <dt><strong>Summary</strong></dt>
+              <dd>Students often forget to add commas in addresses and to add apostrophes in contractions</dd>
+
+              <dt><strong>About the Author</strong></dt>
+              <dd>Students often forget to add commas in addresses and to add apostrophes in contractions</dd>
+
+              <dt><strong>Teacher Review</strong></dt>
+              <dd>Students often forget to add commas in addresses and to add apostrophes in contractions</dd>
+            </dl>
+
+          </div>
+
+          <div className='col-xs-6'>
+            <div className='row'>
+              <div className='col-xs-12'>
+                <button className='button-green' onClick={this.assign}>assign</button>
+                <p>Estimated Time: 45 mins</p>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-xs-12">
+                <p><strong>Standards</strong></p>
+                <ul>
+                  <li>3.1g Form and use Comparative and Superlative Adjectives.</li>
+                  <li>3.2b Use Commas in Addresses</li>
+                  <li>3.2c Form and use Possessives</li>
+                </ul>
+                <p><strong>Concepts</strong></p>
+                <ul>
+                  <li>Commas in Numbers</li>
+                  <li>Capitalization</li>
+                  <li>Determiners</li>
+                  <li>Prepositions</li>
+                  <li>Commas & Proper Nouns</li>
+                </ul>
+              </div>
             </div>
           </div>
         </div>
 
-        <div>
-          <div className='row'>
-            <div className='col-xs-6'>
-              <div className='row'>
-                <div className='col-xs-12'>
-                  <strong>Problem</strong>
-                  <p>Students often forget to add commas in addresses and to add apostrophes in contractions
-                  </p>
-                </div>
-              </div>
-
-              <div className='row'>
-                <div className='col-xs-12'>
-                  <strong>Summary</strong>
-                  <p>Students often forget to add commas in addresses and to add apostrophes in contractions
-                  </p>
-                </div>
-              </div>
-
-              <div className='row'>
-                <div className='col-xs-12'>
-                  <strong>About the Author</strong>
-                  <p>Students often forget to add commas in addresses and to add apostrophes in contractions
-                  </p>
-                </div>
-              </div>
-
-              <div className='row'>
-                <div className='col-xs-12'>
-                  <strong>Teacher Review</strong>
-                  <p>Students often forget to add commas in addresses and to add apostrophes in contractions
-                  </p>
-                </div>
-              </div>
-
-            </div>
-
-            <div className='col-xs-6'>
-              <div className='row'>
-                <div className='col-xs-12'>
-                  <button className='button-green' onClick={this.assign}>assign</button>
-                  <p>Estimated Time: 45 mins</p>
-                </div>
-              </div>
-              <div className="row">
-                <div className="col-xs-12">
-                  <p><strong>Standards</strong></p>
-                  <ul>
-                    <li>3.1g Form and use Comparative and Superlative Adjectives.</li>
-                    <li>3.2b Use Commas in Addresses</li>
-                    <li>3.2c Form and use Possessives</li>
-                  </ul>
-                  <p><strong>Concepts</strong></p>
-                  <ul>
-                    <li>Commas in Numbers</li>
-                    <li>Capitalization</li>
-                    <li>Determiners</li>
-                    <li>Prepositions</li>
-                    <li>Commas & Proper Nouns</li>
-                  </ul>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
 
         <EC.RelatedUnitTemplates models={this.props.data.relatedModels}
                                  eventHandlers={this.props.eventHandlers} />
