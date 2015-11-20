e43a64afc1449276b407ae7798c897bd489360ad
Donald McKendrick <ddmckendrick@gmail.com>
2015-11-20 15:02:15 -0500

    Refactor Unit template description into component

---
+++ app/assets/javascripts/lesson_planner/unit_templates/unit_template_profile/unit_template_profile_description.jsx
@@ -0,0 +1,19 @@
+EC.UnitTemplateProfileDescription = React.createClass({
+  propTypes: {
+    data: React.PropTypes.object.isRequired,
+    eventHandlers: React.PropTypes.object.isRequired
+  },
+
+  render: function () {
+    return (
+      <dl>
+        <dt><strong>Problem</strong></dt>
+        <dd>{this.props.data.model["problem"]}</dd>
+        <dt><strong>Summary</strong></dt>
+        <dd>{this.props.data.model["summary"]}</dd>
+        <dt><strong>Teacher Review</strong></dt>
+        <dd>{this.props.data.model["teacher_review"]}</dd>
+      </dl>
+    )
+  }
+});
