0b619b23bfd242d1e2ea1cede632551de784e7be
Donald McKendrick <ddmckendrick@gmail.com>
2015-11-25 14:29:46 -0500

    render standards via a react template.

--- app/assets/javascripts/lesson_planner/unit_templates_manager/unit_template_profile/unit_template_profile.jsx
+++ app/assets/javascripts/lesson_planner/unit_templates_manager/unit_template_profile/unit_template_profile.jsx
@@ -33,21 +33,7 @@ EC.UnitTemplateProfile = React.createClass({
                 <div className="col-xs-12">
                   <div className='row'>
                     <div className='col-xs-12'>
-                      <div className='standards-and-concepts light-gray-bordered-box'>
-                        <dl>
-                          <dt><strong>Standards</strong></dt>
-                          <dd>3.1g Form and use Comparative and Superlative Adjectives.</dd>
-                          <dd>3.2b Use Commas in Addresses</dd>
-                          <dd>3.2c Form and use Possessives</dd>
-
-                          <dt className='concepts'><strong>Concepts</strong></dt>
-                          <dd className='concept'>Commas in Numbers</dd>
-                          <dd className='concept'>Capitalization</dd>
-                          <dd className='concept'>Determiners</dd>
-                          <dd className='concept'>Prepositions</dd>
-                          <dd className='concept'>Commas & Proper Nouns</dd>
-                        </dl>
-                      </div>
+                      <EC.UnitTemplateProfileStandards data={this.props.data} />
                     </div>
                   </div>
                   <div className='row'>
