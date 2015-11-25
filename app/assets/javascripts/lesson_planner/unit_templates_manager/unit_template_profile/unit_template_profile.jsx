c6fa538c2951c1ac005c51002b5029eb6daa8289
Donald McKendrick <ddmckendrick@gmail.com>
2015-11-25 17:12:21 -0500

    Refactor social share buttons into a component.

--- app/assets/javascripts/lesson_planner/unit_templates_manager/unit_template_profile/unit_template_profile.jsx
+++ app/assets/javascripts/lesson_planner/unit_templates_manager/unit_template_profile/unit_template_profile.jsx
@@ -38,12 +38,7 @@ EC.UnitTemplateProfile = React.createClass({
                   </div>
                   <div className='row'>
                     <div className='col-xs-12'>
-                      <div className='share light-gray-bordered-box'>
-                        <strong>Share this Activity Pack</strong>
-                        <a href={"https://www.linkedin.com/shareArticle?mini=true&url=" + window.location + "&title=Checkout this " + this.props.data.model.name +  " Activity pack by @Quill_org&source=Quill.org"} title="Share on LinkedIn" target="_blank" className="btn btn-default btn-social btn-linkedin"><i className="fa fa-linkedin"></i></a>
-                        <a href={"http://twitter.com/home?status=" + window.location + " checkout this " + this.props.data.model.name +  " Activity pack by @Quill_org"} title="Share on Twitter" target="_blank" className="btn btn-default btn-social btn-twitter"><i className="fa fa-twitter"></i></a>
-                        <a href={"https://www.facebook.com/share.php?u=" + window.location} title="Share this Activity pack on Facebook" target="_blank" className="btn btn-default btn-social btn-facebook"><i className="fa fa-facebook"></i></a>
-                      </div>
+                      <EC.UnitTemplateProfileShareButtons data={this.props.data} />
                     </div>
                   </div>
                 </div>
