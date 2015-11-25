409de87cea2e5da4e30a2cf435e6580f993bc0e3
Donald McKendrick <ddmckendrick@gmail.com>
2015-11-25 14:56:40 -0500

    Show teacher bio in description

--- app/assets/javascripts/lesson_planner/unit_templates_manager/unit_template_profile/unit_template_profile_description.jsx
+++ app/assets/javascripts/lesson_planner/unit_templates_manager/unit_template_profile/unit_template_profile_description.jsx
@@ -10,6 +10,8 @@ EC.UnitTemplateProfileDescription = React.createClass({
         <dd>{this.props.data.model["problem"]}</dd>
         <dt><strong>Summary</strong></dt>
         <dd>{this.props.data.model["summary"]}</dd>
+        <dt><strong>About the Author</strong></dt>
+        <dd>{this.props.data.model["author"]["description"]}</dd>
         <dt><strong>Teacher Review</strong></dt>
         <dd>{this.props.data.model["teacher_review"]}</dd>
       </dl>
