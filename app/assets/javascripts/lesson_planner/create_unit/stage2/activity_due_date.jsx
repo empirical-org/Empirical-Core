08c23574d5acd25c14abf3bec871a90fc2a6b280
Marcello Sachs <marcello.sachs@gmail.com>
2015-11-25 14:50:45 -0500

    fix dueDate bug in unitCreator

--- app/assets/javascripts/lesson_planner/create_unit/stage2/activity_due_date.jsx
+++ app/assets/javascripts/lesson_planner/create_unit/stage2/activity_due_date.jsx
@@ -1,7 +1,10 @@
 EC.ActivityDueDate = React.createClass({
   componentDidMount: function() {
     // Set up the datepicker on the dueDate input
+    this.initializeDatePicker();
+  },
 
+  initializeDatePicker: function () {
     $(this.refs.dueDate.getDOMNode()).datepicker({
       selectOtherMonths: true,
       dayNamesMin: ['S', 'M', 'T', 'W', 'T', 'F', 'S'],
@@ -11,7 +14,6 @@ EC.ActivityDueDate = React.createClass({
       altFormat: 'yy-mm-dd',
       onSelect: this.handleChange
     });
-
   },
 
   tooltipTrigger: function (e) {
@@ -33,6 +35,7 @@ EC.ActivityDueDate = React.createClass({
 
   removeActivity: function () {
     this.props.toggleActivitySelection(this.props.activity, false);
+    this.initializeDatePicker();
   },
 
   render: function() {
