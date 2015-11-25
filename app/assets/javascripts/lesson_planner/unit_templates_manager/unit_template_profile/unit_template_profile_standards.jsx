dee63f0dd790379afac9b61cb728e45db803cb1e
Donald McKendrick <ddmckendrick@gmail.com>
2015-11-25 14:57:15 -0500

    render standards and concepts via map.

--- app/assets/javascripts/lesson_planner/unit_templates_manager/unit_template_profile/unit_template_profile_standards.jsx
+++ app/assets/javascripts/lesson_planner/unit_templates_manager/unit_template_profile/unit_template_profile_standards.jsx
@@ -5,33 +5,31 @@ EC.UnitTemplateProfileStandards = React.createClass({
 
   getStandards: function () {
     return _.map(this.props.data.model.activities, function (act) {
-      return act.topic.name
+      return act.topic
     })
   },
 
-  // renderStandards: function (standards) {
-  //   return (
-  //     { _.map(standards, function(standard){
-  //       return <dd>{standard}</dd>
-  //     }) }
-  //   )
-  // },
+  renderStandards: function (standards) {
+    return _.map(standards, function(standard){
+      return <dd>{standard.name}</dd>
+    })
+  },
+
+  renderConcepts: function (standards) {
+    return _.map(standards, function(standard){
+      return <dd className='concept'>{standard.topic_category.name}</dd>
+    })
+  },
 
   render: function () {
     return (
       <div className='standards-and-concepts light-gray-bordered-box'>
         <dl>
           <dt><strong>Standards</strong></dt>
-          { this.getStandards().map(function(standard){
-            return <dd>{standard}</dd>
-          }) }
+          { this.renderStandards(this.getStandards()) }
 
           <dt className='concepts'><strong>Concepts</strong></dt>
-          <dd className='concept'>Commas in Numbers</dd>
-          <dd className='concept'>Capitalization</dd>
-          <dd className='concept'>Determiners</dd>
-          <dd className='concept'>Prepositions</dd>
-          <dd className='concept'>Commas & Proper Nouns</dd>
+          { this.renderConcepts(this.getStandards()) }
         </dl>
       </div>
     )
