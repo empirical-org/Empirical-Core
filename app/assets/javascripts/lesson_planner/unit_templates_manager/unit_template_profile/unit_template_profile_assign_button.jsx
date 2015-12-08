65182fe1b0e70720bbcb2ee580a3f8ecbbc5cb4c
Donald McKendrick <ddmckendrick@gmail.com>
2015-12-08 15:23:31 -0500

    show the right button for the activity pack call to action.

--- app/assets/javascripts/lesson_planner/unit_templates_manager/unit_template_profile/unit_template_profile_assign_button.jsx
+++ app/assets/javascripts/lesson_planner/unit_templates_manager/unit_template_profile/unit_template_profile_assign_button.jsx
@@ -4,10 +4,18 @@ EC.UnitTemplateProfileAssignButton = React.createClass({
     actions: React.PropTypes.object.isRequired
   },
 
+  stateSpecificComponent: function () {
+    if (this.props.data.authenticated) {
+      return <button className='button-green full-width' onClick={this.props.actions.assign}>Assign to Your Class</button>
+    } else {
+      return <button className='button-green full-width' onClick={this.props.actions.assign}>Sign in</button>
+    }
+  },
+
   render: function () {
     return (
       <div>
-        <button className='button-green full-width' onClick={this.props.actions.assign}>Assign to Your Class</button>
+        {this.stateSpecificComponent()}
         <p className="time"><i className='fa fa-clock-o'></i>Estimated Time: {this.props.data.model.time} mins</p>
       </div>
     )
