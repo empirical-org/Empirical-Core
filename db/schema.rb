061a6c7e1ba24315672f8d096d8fc7d1987802b7
RyanNovas <ryan@quill.org>
2015-12-14 15:59:16 -0500

    fixes welcome email and presskit link

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151214185801) do
+ActiveRecord::Schema.define(version: 20151214204602) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -306,7 +306,6 @@ ActiveRecord::Schema.define(version: 20151214185801) do
     t.integer  "position"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "workbook_id"
     t.string   "uid"
   end
 
