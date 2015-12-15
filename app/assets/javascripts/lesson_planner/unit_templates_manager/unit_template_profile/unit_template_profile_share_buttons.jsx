b318bb2e820254c89b5040f7b4d2ff83b46a613a
Marcello Sachs <marcello.sachs@gmail.com>
2015-12-15 13:34:52 -0500

    fixes #1072

--- app/assets/javascripts/lesson_planner/unit_templates_manager/unit_template_profile/unit_template_profile_share_buttons.jsx
+++ app/assets/javascripts/lesson_planner/unit_templates_manager/unit_template_profile/unit_template_profile_share_buttons.jsx
@@ -5,12 +5,41 @@ EC.UnitTemplateProfileShareButtons = React.createClass({
   },
 
   render: function () {
+    var url = "http://www.quill.org/activities/packs/" + this.props.data.model.id;
+
+    var stuff = [
+      {
+        icon: 'fa-envelope',
+        className: 'btn-linkedin',
+        href: "mailto:?subject=Checkout this " + this.props.data.model.name +  " Activity pack by Quill.org&source=Quill.org&body=" + url,
+        title: 'Share by Email',
+      },
+      {
+        icon: 'fa-twitter',
+        className: 'btn-twitter',
+        href: "http://twitter.com/home?status=" + url + " checkout this " + this.props.data.model.name +  " Activity pack by @Quill_org",
+        title: 'Share on Twitter',
+      },
+      {
+        icon: 'fa-facebook',
+        className: 'btn-facebook',
+        href: "https://www.facebook.com/share.php?u=" + url,
+        title: "Share this Activity pack on Facebook",
+      }
+    ]
+
+    var links = _.map(stuff, function (hash) {
+      return (
+        <a href={hash.href} title={hash.title} className={"btn btn-default btn-social " + hash.className}  target="_blank">
+          <i className={"fa " + hash.icon}></i>
+        </a>
+      );
+    });
+
     return (
       <div className='share light-gray-bordered-box'>
         <strong>Share this Activity Pack</strong>
-        <a href={"https://www.linkedin.com/shareArticle?mini=true&url=" + window.location + "&title=Checkout this " + this.props.data.model.name +  " Activity pack by @Quill_org&source=Quill.org"} title="Share on LinkedIn" target="_blank" className="btn btn-default btn-social btn-linkedin"><i className="fa fa-linkedin"></i></a>
-        <a href={"http://twitter.com/home?status=" + window.location + " checkout this " + this.props.data.model.name +  " Activity pack by @Quill_org"} title="Share on Twitter" target="_blank" className="btn btn-default btn-social btn-twitter"><i className="fa fa-twitter"></i></a>
-        <a href={"https://www.facebook.com/share.php?u=" + window.location} title="Share this Activity pack on Facebook" target="_blank" className="btn btn-default btn-social btn-facebook"><i className="fa fa-facebook"></i></a>
+        {links}
       </div>
     )
   }
