d57b5d858d7927b62b1ad23cb9afaae17b4c7bf9
RyanNovas <ryan@quill.org>
2015-12-28 18:20:11 -0500

    renders assign_success page based off of students presence

--- app/assets/javascripts/lesson_planner/unit_template_assigned.jsx
+++ app/assets/javascripts/lesson_planner/unit_template_assigned.jsx
@@ -1,22 +1,47 @@
 'use strict';
+
 EC.UnitTemplatesAssigned = React.createClass({
   propTypes: {
-    // data: React.PropTypes.object.isRequired,
+    data: React.PropTypes.object.isRequired,
     actions: React.PropTypes.object.isRequired
   },
 
-
+  teacherSpecificComponents: function() {
+    var message;
+    if (this.props.actions.studentsPresent === true) {
+      message = (
+        <span>
+          <p>Your activity-pack has been assigned.</p>
+          <div className="assignSuccess-button-container">
+            <p>Your next step is to add students.</p>
+            <a href = '/teachers/classrooms/lesson_planner'>
+              <button onClick className="button-green add-students">
+                View Assigned Activity Packs
+              </button>
+            </a>
+          </div>
+       </span>);
+    } else {
+      message = (
+        <span>
+          <p>
+            You've assigned an activity-pack to:
+              <b>{this.props.actions.getLastClassroomName()}</b>
+          </p>
+          <div className = "assignSuccess-button-container">
+            <p>Your next step is to add students.</p>
+            <a href = {this.props.actions.getInviteStudentsUrl()} >
+              <button onClick className="button-green add-students">
+                Add Students
+              </button>
+            </a>
+          </div>
+        </span>);
+    };
+    return message;
+  },
 
   render: function () {
-    return (
-      <div className='successBox'>
-        <div className='messageBox'><h2>Success</h2></div>
-        <p>You've assigned an activity-pack to: <b>{this.props.actions.getLastClassroomName()}</b></p>
-        <p>Your next step is to add students.</p>
-        <div  className= "add-students-button-container">
-            <a href={this.props.actions.getInviteStudentsUrl()}><button onClick className="button-green add-students">Add Students</button></a>
-        </div>
-      </div>
-    );
+    return this.teacherSpecificComponents();
   }
 });
