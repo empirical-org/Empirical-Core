e9a46d838f5db1745597d366e0bf05f0b48b88bd
Marcello Sachs <marcello.sachs@gmail.com>
2016-01-04 14:01:13 -0500

    query for time_spent, associated specs

--- app/queries/teachers_data.rb
+++ app/queries/teachers_data.rb
@@ -1,9 +1,13 @@
 module TeachersData
+  # AVERAGE_TIME_SPENT was gathered from below query on 1/4/2016
+  # https://dataclips.heroku.com/tympuxntqzshpmngbnbivaectbqm-average-time-per-activity_session?autosave=true
+  AVERAGE_TIME_SPENT = 441 # (seconds)  ie "interval '7 minutes 21 seconds'"
+
+
 
   # num_students
   # num_questions_completd
   # num_time_spent
-
 
   def self.run(teacher_ids)
     teachers = User.arel_table
@@ -13,8 +17,8 @@ module TeachersData
     concept_results = ConceptResult.arel_table
 
     x = teachers.project(teachers[:id]).where(teachers[:id].in(teacher_ids))
-            .join(classrooms, Arel::Nodes::OuterJoin).on(teachers[:id].eq(classrooms[:teacher_id]))
-            .join(students, Arel::Nodes::OuterJoin).on(classrooms[:code].eq(students[:classcode]))
+            .join(classrooms).on(teachers[:id].eq(classrooms[:teacher_id]))
+            .join(students).on(classrooms[:code].eq(students[:classcode]))
             .join(activity_sessions, Arel::Nodes::OuterJoin).on(students[:id].eq(activity_sessions[:user_id]))
             .join(concept_results, Arel::Nodes::OuterJoin).on(activity_sessions[:id].eq(concept_results[:activity_session_id]))
             .group(teachers[:id])
@@ -22,15 +26,45 @@ module TeachersData
                      students[:id].count(true).as('number_of_students'),
                      concept_results[:id].count(true).as('number_of_questions_completed'))
 
-    ts = User.find_by_sql(x)
+
+   y = teachers.project(teachers[:id].as('teacher_id'), activity_sessions[:id].as('activity_session_id')).where(teachers[:id].in(teacher_ids))
+               .join(classrooms).on(teachers[:id].eq(classrooms[:teacher_id]))
+               .join(students).on(classrooms[:code].eq(students[:classcode]))
+               .join(activity_sessions).on(students[:id].eq(activity_sessions[:user_id]))
+               .group(teachers[:id], activity_sessions[:id])
+               .as('acss_ids')
+
+   z = activity_sessions.join(y).on(activity_sessions[:id].eq(y[:activity_session_id]))
+                        .group(y[:teacher_id])
+                        .project(y[:teacher_id],
+                                 self.time_spent.as('time_spent'))
+                        .as('time_spent_query')
+
+   z2 = x.join(z).on(teachers[:id].eq(z[:teacher_id]))
+         .project(z[:time_spent].maximum.as('time_spent'))
+
+
+    ts = User.find_by_sql(z2)
   end
 
+  private
+
+  def self.time_spent
+    string = "
+      SUM (
+        CASE
+        WHEN (activity_sessions.started_at IS NULL)
+          OR (activity_sessions.completed_at IS NULL)
+          OR (activity_sessions.completed_at - activity_sessions.started_at < interval '1 minute')
+          OR (activity_sessions.completed_at - activity_sessions.started_at > interval '30 minutes')
+        THEN #{AVERAGE_TIME_SPENT}
+        ELSE
+          EXTRACT (
+            'epoch' FROM (activity_sessions.completed_at - activity_sessions.started_at)
+          )
+        END
+      )
+    "
+    Arel::Nodes::SqlLiteral.new(string)
+  end
 end
-
-=begin
-concept_results_completed
-  join students to activity_sessions
-
-
-
-=end
