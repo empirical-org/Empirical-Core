2df238a599915ccde64cd9986242204f4a507917
Marcello Sachs <marcello.sachs@gmail.com>
2016-01-04 14:25:20 -0500

    show time_spent on the front-end of admin dashboard

--- app/serializers/admin/teacher_serializer.rb
+++ app/serializers/admin/teacher_serializer.rb
@@ -4,7 +4,8 @@ class Admin::TeacherSerializer < ActiveModel::Serializer
             :classroom_manager_path,
             :progress_reports_path,
             :number_of_students,
-            :number_of_questions_completed
+            :number_of_questions_completed,
+            :time_spent
 
   def classroom_manager_path
     admin_sign_in_classroom_manager_user_path(object)
@@ -12,6 +13,16 @@ class Admin::TeacherSerializer < ActiveModel::Serializer
 
   def progress_reports_path
     admin_sign_in_progress_reports_user_path(object)
+  end
+
+  def time_spent
+    x = object.time_spent
+    mm, ss = x.divmod(60)
+    ss2 = ss.floor
+    hh, mm2 = mm.divmod(60)
+    dd, hh2 = hh.divmod(24)
+
+    "#{dd} days, #{hh2} hours, #{mm2} minutes, #{ss2} seconds"
   end
 
 end
