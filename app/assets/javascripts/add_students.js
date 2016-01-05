7867b11c723c552c3c352dc7f4a37120d4e98539
RyanNovas <ryan@quill.org>
2016-01-05 16:35:01 -0500

    fixes bugs with add student table, temporary hack fix for double assign until csv upload or react version exists

---
+++ app/assets/javascripts/add_students.js
@@ -0,0 +1,16 @@
+//Hacky fix until this page is done in React --
+// without this, the Add Student Button can easily be double triggered.
+var buttonBlocker = function(blockedButton) {
+  $(blockedButton).click(function() {
+    var that = this;
+    //if we set disabled right away, it doesn't have time to submit the form.
+    setTimeout(function() {
+      $(that).attr("disabled", true);
+    }, 75);
+    setTimeout(function() {
+      $('that').attr("disabled", false);
+    }, 500);
+  });
+};
+
+buttonBlocker('.ajax-button');
