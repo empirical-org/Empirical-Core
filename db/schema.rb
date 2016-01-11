ef4038c794ad368ddc90d8c29d27051d66d39b3e
Marcello Sachs <marcello.sachs@gmail.com>
2016-01-11 16:26:52 -0500

    regenerate schema to include admin-branch and develop-branch migrations

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 20160111193235) do
-=======
-ActiveRecord::Schema.define(version: 20151222172610) do
->>>>>>> admin
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
