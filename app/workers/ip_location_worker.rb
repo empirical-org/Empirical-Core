866f57bd86ac83e2ecfeed57ef950b464779a44d
RyanNovas <ryan@quill.org>
2016-01-27 15:24:43 -0500

    blocks office zipcode from ziplocation

--- app/workers/ip_location_worker.rb
+++ app/workers/ip_location_worker.rb
@@ -2,12 +2,11 @@ class IpLocationWorker
   include Sidekiq::Worker
 
   def perform(id, ip_address)
-    puts "\n\n\n\n\n\n\n\n\n\n\n\n\n\n#{ip_address}\n\n\n\n\n\n\n\n\n\n\n\n\n\n"
     @user = User.find(id)
     location = Pointpin.locate(ip_address)
-    puts "\n\n\n\n\n\n\n\n\n\n\n\n\n\n#{location}\n\n\n\n\n\n\n\n\n\n\n\n\n\n"
+    if location["post_code"] == ""
     new_ip_location = IpLocation.new
-    IpLocation.create(country: location["country_name"], state: location["region_name"], city: location["city_name"], zip: location["post_code"], user_id: @user.id)
+    IpLocation.create(country: location["country_name"], state: location["region_name"], city: location["city_name"], zip: location["post_code"].to_i, user_id: @user.id)
   end
 
 end
