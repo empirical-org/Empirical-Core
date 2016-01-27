cb6786390f5e6499dee25eec32aa4db9f706bbde
RyanNovas <ryan@quill.org>
2016-01-27 18:20:41 -0500

    removes extraneous methods

--- spec/workers/ip_location_worker_spec.rb
+++ spec/workers/ip_location_worker_spec.rb
@@ -10,14 +10,12 @@ describe IpLocationWorker, type: :worker do
   # it 'uses Pointpin API to get zip, country, state, and city of user' do
   #   ip_address = '74.125.224.72' #google's address
   #   worker.perform(teacher.id, ip_address)
-  #   binding.pry
   #   expect(teacher.ip_location).to eq('a')
   # end
 
   # it 'does not return a location for ip addresses where the zip is 10005' do
   #   ip_address = '74.125.224.72'
   #   worker.perform(teacher.id, ip_address)
-  #   binding.pry
   #   expect(teacher.ip_location).to eq('a')
   # end
 end
