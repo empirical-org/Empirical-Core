1e7010056a1726506597b7b5f304ff53feba5236
RyanNovas <ryan@quill.org>
2016-01-27 18:17:51 -0500

    adds rake task, hides pointpin key, fixes bug with zipcodes

--- app/workers/ip_location_worker.rb
+++ app/workers/ip_location_worker.rb
@@ -4,9 +4,9 @@ class IpLocationWorker
   def perform(id, ip_address)
     @user = User.find(id)
     location = Pointpin.locate(ip_address)
-    unless location["post_code"] == "10005" #blocks our zip from creating accounts so they don't disrupt database accuracy
+    unless location["postcode"] == ("10005" || "11237" || "11385") #locations where we've had offices and therefore triggered a ton of false ip addresses
       new_ip_location = IpLocation.new
-      IpLocation.create(country: location["country_name"], state: location["region_name"], city: location["city_name"], zip: location["post_code"].to_i, user_id: @user.id)
+      IpLocation.create(country: location["country_name"], state: location["region_name"], city: location["city_name"], zip: location["postcode"].to_i, user_id: @user.id)
     end
   end
 
---
+++ lib/tasks/ip_location.rake
@@ -0,0 +1,15 @@
+namespace :ip_location do
+  desc 'give every teacher with an ip address but no school a location'
+  task :location_of_schoolless_teachers => :environment do
+    # Selects from teachers that have no school, but do have an ip address
+    target = User.where(role: 'teacher').select{|t| (t.school == nil) && (!!t.ip_address) && !t.ip_location}[0..1]
+    generate_location(target)
+  end
+
+  def generate_location(target)
+    target.each do |t|
+      IpLocationWorker.perform_async(t.id, t.ip_address)
+    end
+  end
+
+end
