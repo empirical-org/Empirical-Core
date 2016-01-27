9bfbcfd04fc369139f682455232424a81797ae39
RyanNovas <ryan@quill.org>
2016-01-27 15:27:47 -0500

    fixes unfinished line of code

--- app/workers/ip_location_worker.rb
+++ app/workers/ip_location_worker.rb
@@ -4,9 +4,10 @@ class IpLocationWorker
   def perform(id, ip_address)
     @user = User.find(id)
     location = Pointpin.locate(ip_address)
-    if location["post_code"] == ""
-    new_ip_location = IpLocation.new
-    IpLocation.create(country: location["country_name"], state: location["region_name"], city: location["city_name"], zip: location["post_code"].to_i, user_id: @user.id)
+    unless location["post_code"] == "10005" #blocks our zip from creating accounts so they don't disrupt database accuracy
+      new_ip_location = IpLocation.new
+      IpLocation.create(country: location["country_name"], state: location["region_name"], city: location["city_name"], zip: location["post_code"].to_i, user_id: @user.id)
+    end
   end
 
 end
