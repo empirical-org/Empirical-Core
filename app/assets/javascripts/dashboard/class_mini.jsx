702b18fcaab4caef2420c9c0ec3566cd55e84e82
RyanNovas <ryan@quill.org>
2016-01-28 19:07:19 -0500

    grid structure for classroom minis

--- app/assets/javascripts/dashboard/class_mini.jsx
+++ app/assets/javascripts/dashboard/class_mini.jsx
@@ -1,13 +1,27 @@
 EC.ClassMini = React.createClass({
 
+  // offset: function() {
+  //   if (this.props.rowNum !== 1) {
+  //     return  "col-md-offset-1";
+  //     }
+  // },
 
+  // render: function() {
+  //   console.log('class mini rendered');
+  //   return (
+  //     <div className={"classroom_mini col-md-2 row_num_" + this.props.rowNum}>
+  //       {this.props.classObj}
+  //     </div>
+  //   );
+  // }
   render: function() {
     console.log('class mini rendered');
     return (
-      <div className='col-md-3'>
-        {this.props.classObj}
+      <div className={"classroom_mini_container col-md-4 row_num_" + this.props.rowNum}>
+        <div className ={"classroom_mini_content"}>
+          {this.props.classObj}
+        </div>
       </div>
     );
   }
-
 });
