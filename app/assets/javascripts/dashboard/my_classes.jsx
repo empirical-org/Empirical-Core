cb330573551dbc16310525747f62af3cad82fa8a
RyanNovas <ryan@quill.org>
2016-01-28 19:07:19 -0500

    grid structure for classroom minis

--- app/assets/javascripts/dashboard/my_classes.jsx
+++ app/assets/javascripts/dashboard/my_classes.jsx
@@ -1,9 +1,14 @@
 EC.MyClasses = React.createClass({
 
   createMinis: function() {
-    var classes = ['1st Period','2nd Period','3rd Period'];
+    var classes = ['1st Period','2nd Period','3rd Period', '5th Period','6th Period','7th Period'];
+    var i = 0;
     var minis = _.map(classes, function(classObj) {
-      return <EC.ClassMini classObj={classObj}/>
+      if (i === 3) {
+        i = 0;
+      }
+      i++;
+      return <EC.ClassMini classObj={classObj} rowNum={i}/>;
     });
     return minis;
   },
@@ -11,7 +16,7 @@ EC.MyClasses = React.createClass({
 
   render: function() {
     return (
-      <div>
+      <div className='classes_container'>
     <h3 className='dashboard-header'>My Classes</h3>
       <div className='row'>
         {this.createMinis()}
