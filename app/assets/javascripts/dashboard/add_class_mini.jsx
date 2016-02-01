4c0cb7cbd7c54ae57d6180af6557961cf130fe00
RyanNovas <ryan@quill.org>
2016-02-01 12:18:26 -0500

    adds additional responsiveness

--- app/assets/javascripts/dashboard/add_class_mini.jsx
+++ app/assets/javascripts/dashboard/add_class_mini.jsx
@@ -2,7 +2,7 @@ EC.AddClassMini = React.createClass({
 
   render: function() {
     return (
-      <div className={"classroom_mini_container col-md-4 row_num_" + this.props.rowNum}>
+      <div className={"classroom_mini_container col-md-4 col-sm-5 row_num_" + this.props.rowNum}>
         <div className ={"classroom_mini_content text-center"}>
           <a className='add_class_link' href="/teachers/classrooms/new">
             <img className='plus_icon' src='/add_class.png'></img>
