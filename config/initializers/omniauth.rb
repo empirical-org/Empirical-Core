0bfc7df8b53d43404626cacc63834d0a7be926bf
Marcello Sachs <marcello.sachs@gmail.com>
2016-02-02 11:15:01 -0500

    add skip_jwt:true to google omniauth config

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -4,7 +4,9 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :clever, Clever::CLIENT_ID, Clever::CLIENT_SECRET, options
   provider :google_oauth2, ENV["GOOGLE_CLIENT_ID"], ENV["GOOGLE_CLIENT_SECRET"],
     {
-      scope: ['email', 'profile', 'classroom.courses'], access_type: 'offline'
+      skip_jwt: true,
+      scope: ['email', 'profile', 'classroom.courses'],
+      access_type: 'offline'
     }
 end
 
