5f745fc8f0b2cf055f0f7a7d51c2807cc34e9a75
Marcello Sachs <marcello.sachs@gmail.com>
2016-02-02 18:07:31 -0500

    wip

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -10,9 +10,7 @@ EmpiricalGrammar::Application.configure do
   config.eager_load = true
 
   # Show full error reports and disable caching.
-  #config.consider_all_requests_local       = true
-  # changed this while debugging 500 page, change back to 'true' if you find this still here
-  config.consider_all_requests_local       = false
+  config.consider_all_requests_local       = true
 
   config.action_dispatch.show_detailed_exceptions = false
   config.action_controller.perform_caching = false
