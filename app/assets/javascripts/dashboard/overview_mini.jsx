9cb76eeba9b0293ccd9dba0271a45fbc98ac92d2
RyanNovas <ryan@quill.org>
2016-02-02 19:22:35 -0500

    minimal button styling

--- app/assets/javascripts/dashboard/overview_mini.jsx
+++ app/assets/javascripts/dashboard/overview_mini.jsx
@@ -4,32 +4,58 @@ EC.OverviewMini = React.createClass({
     results = this.props.overviewObj.results;
     var leftColumn = Object.keys(results);
     var dataRows = _.map(leftColumn, function(left) {
-      return (<tr key={left}>
-                <td>{left}</td>
-                <td>{results[left]}</td>
-              </tr>
-            );
+      return (
+        <tr key={left}>
+          <td className="left-column">{left}</td>
+          <td className="right-column">{results[left]}%</td>
+        </tr>
+      );
     });
     return (
       <div>
         {this.header()}
         <table>
-           <tbody>
-          {dataRows}
+          <tbody>
+            {dataRows}
           </tbody>
         </table>
-
-    </div>
+        {this.miniSpecificButton()}
+      </div>
     );
   },
 
+  miniSpecificButton: function() {
+    var header = this.props.overviewObj.header.toLowerCase();
+    // this just searches for mention of student, concept, etc, in the header
+    // so we can use the queries with minimal meta-data
+    var buttonLink = function() {
+      if (header.indexOf('student') > -1)
+        return (
+          <a href="progress_reports/concepts/students">
+            <button className='button-white'>View All Student Results</button>
+          </a>);
+      else if (header.indexOf('concept') > -1) {
+        return (
+        <a href="progress_reports/standards/classrooms">
+          <button className='button-white'>View All Concept Results</button>
+        </a>);
+      }
+    };
+    return buttonLink();
+  },
+
   header: function() {
-    return <h3>{this.props.overviewObj.header}</h3>;
+    return (
+      <div className='header'>
+        <h4>{this.props.overviewObj.header}</h4>
+        <p>All Time</p>
+      </div>
+    );
   },
 
   render: function() {
     return (
-      <div className={"mini_container col-md-4 col-sm-5 text-center"}>
+      <div className={"mini_container results-overview-mini-container col-md-4 col-sm-5 text-center"}>
         <div className ={"mini_content "}>
           {this.overviewMiniBuilder()}
         </div>
