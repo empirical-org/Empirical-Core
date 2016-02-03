b34c240994e5be232453997663cfc2b6a23f2863
Donald McKendrick <ddmckendrick@gmail.com>
2016-02-03 12:02:39 -0500

    track IP location in google callback

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -89,7 +89,8 @@ class SessionsController < ApplicationController
       redirect_to new_account_path
     else
       sign_in user
-      AccountCreationCallbacks.new(user).trigger
+      ip = request.remote_ip
+      AccountCreationCallbacks.new(user, ip).trigger
       user.subscribe_to_newsletter
       if user.role == 'teacher'
         @teacherFromGoogleSignUp = true
