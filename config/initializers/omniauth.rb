b5b0ff57c7f0ee0ff292bed3e0b9dd8714647030
Marcello Sachs <marcello.sachs@gmail.com>
2016-02-08 12:58:33 -0500

    wip

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -5,7 +5,7 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, ENV["GOOGLE_CLIENT_ID"], ENV["GOOGLE_CLIENT_SECRET"],
     {
       skip_jwt: true,
-      scope: ['email', 'profile', 'classroom.courses'],
+      scope: ['email', 'profile', 'classroom.courses', 'classroom.rosters'],
       access_type: 'offline'
     }
 end
