d8da69f343e3988f262e23e9c506ea3864cb4986
RyanNovas <ryan@quill.org>
2016-02-16 13:07:40 -0500

    adds a premium subscription after user purchases

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -3,4 +3,8 @@ class Subscription < ActiveRecord::Base
   validates :expiration, presence: true
   validates :account_limit, presence: true
 
+  def self.create_premium current_user
+    self.create(user_id: current_user.id, expiration: (Date.today + 365), account_limit: 1000, account_type: 'paid')
+  end
+
 end
