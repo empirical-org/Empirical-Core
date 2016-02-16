5de2cd484f7641e1c91f7b0e4412d230a5c64ab9
RyanNovas <ryan@quill.org>
2016-02-16 15:17:27 -0500

    adds link to new_signup_banner

--- app/assets/javascripts/scorebook/premium_banners/free_trial_countdown.jsx
+++ app/assets/javascripts/scorebook/premium_banners/free_trial_countdown.jsx
@@ -1,30 +1,31 @@
 EC.FreeTrialStatus = React.createClass({
 
-  stateSpecificComponents:function() {
+  stateSpecificComponents: function() {
     if (this.props.status == 'trial') {
-      return(
-        <div>
-            <h4>You have {this.props.data} days left in your trial.</h4>
-              <span>Getting value out of Premium? Check out our pricing plans.</span>
-      </div>
-    );} else if (this.props.status == 'locked') {
-      return(<div>
-        <h4>Your Premium Trial Has Expired</h4>
-          <span>To continue using Premium check out our pricing plans.</span>
-      </div>);
+      return (
+          <h4>You have
+            {this.props.data}
+            days left in your trial.</h4>
+      );
+    } else if (this.props.status == 'locked') {
+      return (
+          <h4>Your Premium Trial Has Expired</h4>
+      );
     }
   },
-
 
   render: function() {
     return (
       <div className='row'>
         <div className='col-md-9 col-xs-12 pull-left'>
           {this.stateSpecificComponents()}
+          <span>Getting value out of Premium? <a href='/premium'>Check out our pricing plans.</a></span>
         </div>
         <div className='col-md-3 col-xs-12 pull-right'>
           <div className='premium-button-box text-center'>
-            <a href='/premium'><button type='button' className='btn-orange'>Upgrade to Premium Now</button></a>
+            <a href='/premium'>
+              <button type='button' className='btn-orange'>Upgrade to Premium Now</button>
+            </a>
           </div>
         </div>
       </div>
