db035b89b12f273e1e1cd9e01b1c23322472e792
RyanNovas <ryan@quill.org>
2016-02-24 18:08:30 -0500

    ready to go -- just needs the link

---
+++ app/assets/javascripts/dashboard/google_classroom_mini.jsx
@@ -0,0 +1,23 @@
+EC.GoogleClassroomMini = React.createClass({
+
+  miniBuilder: function() {
+    return (
+      <div className='resources-container google-classroom-announcement'>
+        <h4>Google Classroom Announcement</h4>
+          <img src='/images/google_classroom_icon.png'></img>
+          <p>Your students can now use Clever or Google Classroom to automatically join your classroom.</p>
+          <span><a href='placeholder'>Read the Announcement ></a></span>
+      </div>
+    );
+  },
+
+  render: function() {
+    return (
+      <div className={"mini_container col-md-4 col-sm-5 text-center"}>
+        <div className="mini_content">
+          {this.miniBuilder()}
+        </div>
+      </div>
+    );
+  }
+});
