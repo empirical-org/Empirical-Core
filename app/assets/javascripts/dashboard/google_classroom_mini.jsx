70227722d27b3e965726302d0adce56013065d1c
RyanNovas <ryan@quill.org>
2016-02-25 12:07:06 -0500

    comments out google classroom notification pending blog post and QA testing

--- app/assets/javascripts/dashboard/google_classroom_mini.jsx
+++ app/assets/javascripts/dashboard/google_classroom_mini.jsx
@@ -6,18 +6,19 @@ EC.GoogleClassroomMini = React.createClass({
         <h4>Google Classroom Announcement</h4>
           <img src='/images/google_classroom_icon.png'></img>
           <p>Your students can now use Clever or Google Classroom to automatically join your classroom.</p>
-          <span><a href='placeholder'>Read the Announcement ></a></span>
       </div>
     );
+    // <span><a href='placeholder'>Read the Announcement ></a></span> #### FOR WHEN THE ANNOUNCMENT IS UP
   },
 
   render: function() {
-    return (
-      <div className={"mini_container col-md-4 col-sm-5 text-center"}>
-        <div className="mini_content">
-          {this.miniBuilder()}
-        </div>
-      </div>
-    );
+    return (<span/>)
+    // return (
+    //   // <div className={"mini_container col-md-4 col-sm-5 text-center"}>
+    //   //   <div className="mini_content">
+    //   //     {this.miniBuilder()}
+    //   //   </div>
+    //   // </div>
+    // );
   }
 });
