ccdc0cee81bea5a757e8baea7732de29ae648924
Donald McKendrick <ddmckendrick@gmail.com>
2016-02-26 16:53:00 -0500

    Wait until images have had a chance to load/error before measuring

--- app/assets/javascripts/activity_page.jsx
+++ app/assets/javascripts/activity_page.jsx
@@ -3,9 +3,11 @@ jQuery(function ($) {
     return;
   }
 
-  if ($('.firewall-test-image').length && $('.firewall-test-image')[0].naturalWidth === 0) {
-    React.render(React.createElement(EC.FirewallWarning), $('.container-fluid')[0]);
-  }
+  $(window).on("load", function(){
+    if ($('.firewall-test-image').length && $('.firewall-test-image')[0].naturalWidth === 0) {
+      React.render(React.createElement(EC.FirewallWarning), $('.container-fluid')[0]);
+    }
+  })
 
   var moduleUrl = $('iframe').prop('src');
   window.quill = new Quill(moduleUrl);
