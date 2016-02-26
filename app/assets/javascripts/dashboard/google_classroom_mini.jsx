1796f519d2e8859e8cb747bbdcf4af199ee2efc1
RyanNovas <ryan@quill.org>
2016-02-26 11:44:02 -0500

    uncomments google classroom and adds correct link

--- app/assets/javascripts/dashboard/google_classroom_mini.jsx
+++ app/assets/javascripts/dashboard/google_classroom_mini.jsx
@@ -6,19 +6,18 @@ EC.GoogleClassroomMini = React.createClass({
         <h4>Google Classroom Announcement</h4>
           <img src='/images/google_classroom_icon.png'></img>
           <p>Your students can now use Clever or Google Classroom to automatically join your classroom.</p>
+          <span><a href='https://medium.com/@quill/we-re-now-supporting-google-classroom-and-clever-55f2c12272f7#.xfrtabflg'>Read the Announcement ></a></span>
       </div>
     );
-    // <span><a href='placeholder'>Read the Announcement ></a></span> #### FOR WHEN THE ANNOUNCMENT IS UP
   },
 
   render: function() {
-    return (<span/>)
-    // return (
-    //   // <div className={"mini_container col-md-4 col-sm-5 text-center"}>
-    //   //   <div className="mini_content">
-    //   //     {this.miniBuilder()}
-    //   //   </div>
-    //   // </div>
-    // );
+    return (
+      <div className={"mini_container col-md-4 col-sm-5 text-center"}>
+        <div className="mini_content">
+          {this.miniBuilder()}
+        </div>
+      </div>
+    );
   }
 });
