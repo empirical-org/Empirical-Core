38fb1324e5418465d172d0d285c1e6c3df31e7f1
RyanNovas <ryan@quill.org>
2016-03-07 17:15:23 -0500

    reverts bad commit regarding students classrooms model

--- app/models/classroom.rb
+++ app/models/classroom.rb
@@ -14,9 +14,8 @@ class Classroom < ActiveRecord::Base
   has_many :activities, through: :classroom_activities
   has_many :activity_sessions, through: :classroom_activities
   has_many :sections, through: :activities
-  has_many :students_classrooms, foreign_key: 'classroom_id', dependent: :destroy
-  has_many :students, through: :students_classrooms, source: :students, inverse_of: :classrooms
 
+  has_many :students, -> { where role: 'student' }, foreign_key: 'classcode', class_name: 'User', primary_key: 'code'
   belongs_to :teacher, class_name: 'User'
 
   before_validation :generate_code, if: Proc.new {|c| c.code.blank?}
