2b071fe0059cfbfbaf72dd07300afa78a5f671bf
RyanNovas <ryan@quill.org>
2016-03-24 18:45:02 -0400

    deprecates delete student button, adds remove student from class button

---
+++ app/controllers/students_classrooms_controller.rb
@@ -0,0 +1,12 @@
+class StudentsClassroomsController < ApplicationController
+
+
+    def hide
+      row = StudentsClassrooms.where(student_id: params[:student_id], classroom_id: params[:classroom_id]).first
+      row.update(visible: false)
+      redirect_to teachers_classrooms_path
+    end
+
+
+
+end
