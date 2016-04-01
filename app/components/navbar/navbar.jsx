8165ee936b6aeae0643650c2bb2cf5686a8db908
Donald McKendrick <ddmckendrick@gmail.com>
2016-04-01 14:24:46 -0400

    Tidy up the nav bar for now

--- app/components/navbar/navbar.jsx
+++ app/components/navbar/navbar.jsx
@@ -9,28 +9,28 @@ export default React.createClass({
           <div className="header-left">
             <Link to={'/'} className="header-item">Quill Connect</Link>
             <Link to={'/play'} className="header-tab" activeClassName="is-active">Play</Link>
-            <Link to={'/results'} className="header-tab" activeClassName="is-active">Results</Link>
           </div>
           <span className="header-toggle">
             <span />
             <span />
             <span />
           </span>
-          <div className="header-right header-menu">
-            <span className="header-item">
-              <Link to={'/admin'} className="header-item" activeClassName="is-active">Admin</Link>
-            </span>
-            <span className="header-item">
-              <a href="#">
-                Help
-              </a>
-            </span>
-            <span className="header-item">
-              <a className="button" href="#">Button</a>
-            </span>
-          </div>
         </div>
       </header>
     )
   }
 })
+
+const rightNav = (<div className="header-right header-menu">
+  <span className="header-item">
+    <Link to={'/admin'} className="header-item" activeClassName="is-active">Admin</Link>
+  </span>
+  <span className="header-item">
+    <a href="#">
+      Help
+    </a>
+  </span>
+  <span className="header-item">
+    <a className="button" href="#">Button</a>
+  </span>
+</div>)
