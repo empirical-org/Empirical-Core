2f9ba6b34d7482bd29d26ef7848b9e571ea7378a
RyanNovas <ryan@quill.org>
2016-04-12 14:26:56 -0400

    adds tests

--- spec/features/profile_spec.rb
+++ spec/features/profile_spec.rb
@@ -1,21 +1,29 @@
 require 'rails_helper'
+include AsyncHelper
 
 feature 'Profile', js: true do
   include_context "profile"
 
 
+
   before :each do
+
     vcr_ignores_localhost
     sign_in_user student
     visit('/')
   end
 
-  it 'includes teacher name' do
-    expect(page).to have_content(teacher.name)
+  it 'includes all teachers names' do
+    expect(page).to have_content(student.classrooms[0].teacher.name)
+    expect(page).to have_content(student.classrooms[1].teacher.name)
   end
 
-  it 'includes unit name' do
-    expect(page).to have_content(unit1.name)
+  it 'includes unit name of unit from last classroom added by default' do
+    eventually { expect(page).to have_content(unit1.name) }
+  end
+
+  it 'does not include unit name of unit from first classroom added by default' do
+    eventually { expect(page).to have_content(unit1.name) }
   end
 
   it 'includes activity name for unstarted activity_session' do
@@ -25,5 +33,7 @@ feature 'Profile', js: true do
   it 'includes activity name for finished activity_session' do
     expect(page).to have_content(as1.activity.name)
   end
+
+  it 'does not include activity name from '
 
 end
--- spec/support/shared/profile/profile.rb
+++ spec/support/shared/profile/profile.rb
@@ -1,7 +1,9 @@
 shared_context 'profile' do
-  let(:teacher) { FactoryGirl.create(:user, role: 'teacher', name: 'Teacher teacher') }
-  let(:classroom) { FactoryGirl.create(:classroom, teacher: teacher) }
-  let(:student) { FactoryGirl.create(:user, role: 'student', classrooms: [classroom]) }
+  let(:teacher1) { FactoryGirl.create(:user, role: 'teacher', name: 'Teacher 1') }
+  let(:teacher2) { FactoryGirl.create(:user, role: 'teacher', name: 'Teacher 2') }
+  let!(:classroom1) { FactoryGirl.create(:classroom, teacher: teacher1, name: 'class1') }
+  let!(:classroom2) { FactoryGirl.create(:classroom, teacher: teacher2, name: 'class2') }
+  let!(:student) { FactoryGirl.create(:arnold_horshack, classrooms: [classroom1, classroom2]) }
 
   let(:game1) { FactoryGirl.create(:activity_classification, id: 1) }
   let(:game2) { FactoryGirl.create(:activity_classification, id: 2) }
@@ -12,23 +14,23 @@ shared_context 'profile' do
   let!(:activity_1b) { FactoryGirl.create(:activity, classification: game2) }
   let(:unit1) { FactoryGirl.create(:unit) }
   let!(:classroom_activity) { FactoryGirl.create(:classroom_activity,
-                                                  classroom: classroom,
+                                                  classroom: classroom2,
                                                   activity: activity,
                                                   unit: unit1) }
 
   let!(:classroom_activity_1a) { FactoryGirl.create(:classroom_activity,
-                                                    classroom: classroom,
+                                                    classroom: classroom2,
                                                     activity: activity_1a,
                                                     unit: unit1)}
 
 
   let!(:classroom_activity_1aa) { FactoryGirl.create(:classroom_activity,
-                                                    classroom: classroom,
+                                                    classroom: classroom2,
                                                     activity: activity_1aa,
                                                     unit: unit1)}
 
   let!(:classroom_activity_1b) { FactoryGirl.create(:classroom_activity,
-                                                    classroom: classroom,
+                                                    classroom: classroom2,
                                                     activity: activity_1b,
                                                     unit: unit1)}
 
@@ -39,21 +41,21 @@ shared_context 'profile' do
 
   let!(:unit2) { FactoryGirl.create(:unit) }
   let!(:classroom_activity2) { FactoryGirl.create(:classroom_activity,
-                                                  classroom: classroom,
+                                                  classroom: classroom2,
                                                   activity: activity2,
                                                   unit: unit2,
                                                   assigned_student_ids: [],
                                                   due_date: Date.today + 3) }
 
   let!(:classroom_activity_2a) { FactoryGirl.create(:classroom_activity,
-                                                    classroom: classroom,
+                                                    classroom: classroom2,
                                                     activity: activity_2a,
                                                     unit: unit2,
                                                     assigned_student_ids: [],
                                                     due_date: Date.today + 100)}
 
   let!(:classroom_activity_2aa) { FactoryGirl.create(:classroom_activity,
-                                                    classroom: classroom,
+                                                    classroom: classroom2,
                                                     activity: activity_2aa,
                                                     unit: unit2,
                                                     assigned_student_ids: [],
@@ -61,7 +63,7 @@ shared_context 'profile' do
 
 
   let!(:classroom_activity_2b) { FactoryGirl.create(:classroom_activity,
-                                                    classroom: classroom,
+                                                    classroom: classroom2,
                                                     activity: activity_2b,
                                                     unit: unit2,
                                                     assigned_student_ids: [],
@@ -74,7 +76,7 @@ shared_context 'profile' do
 
   # let!(:unit3) { FactoryGirl.create(:unit) }
   # let!(:classroom_activity3) { FactoryGirl.create(:classroom_activity,
-  #                                                 classroom: classroom,
+  #                                                 classroom: classroom2,
   #                                                 activity: activity3,
   #                                                 unit: unit3,
   #                                                 due_date: Date.today + 3) }
