850682b9a04aa0c81c9992c26c1e479e05cf8196
Donald McKendrick <ddmckendrick@gmail.com>
2016-04-17 22:06:24 -0400

    include link component on admin page so that the play question button doesn’t cause a render error.

--- app/components/questions/question.jsx
+++ app/components/questions/question.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import {Link} from 'react-router'
 import questionActions from '../../actions/questions'
 import _ from 'underscore'
 import {hashToCollection} from '../../libs/hashToCollection'
