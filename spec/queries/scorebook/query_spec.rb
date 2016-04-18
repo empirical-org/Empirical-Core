5d17c5f5e412ea101bea9e703ceefe075710de7c
RyanNovas <ryan@quill.org>
2016-04-18 12:25:10 -0400

    adds classroom activity to query specs so they pass again

--- spec/queries/scorebook/query_spec.rb
+++ spec/queries/scorebook/query_spec.rb
@@ -5,11 +5,13 @@ describe 'ScorebookQuery' do
   let!(:classroom) { FactoryGirl.create(:classroom, teacher: teacher) }
   let!(:student) { FactoryGirl.create(:user, role: 'student', classrooms: [classroom]) }
   let!(:activity) { FactoryGirl.create(:activity) }
+  let!(:classroom_activity) {FactoryGirl.create(:classroom_activity, classroom_id: classroom.id)}
   let!(:activity_session) { FactoryGirl.create(:activity_session,
                                                 user: student,
                                                 activity: activity,
                                                 is_final_score: true,
                                                 state: 'finished',
+                                                classroom_activity_id: classroom_activity.id,
                                                 completed_at: Time.now
                                                 ) }
 
