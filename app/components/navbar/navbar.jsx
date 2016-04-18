d538eb9d51f2b44871ddd4c06d7cb35a04595a19
Donald McKendrick <ddmckendrick@gmail.com>
2016-04-17 20:36:07 -0400

    update navbar links

--- app/components/navbar/navbar.jsx
+++ app/components/navbar/navbar.jsx
@@ -7,12 +7,15 @@ export default React.createClass({
       <header className="header" style={{height: '65px'}}>
         <div className="container">
           <div className="header-left">
-            <Link to={'/'} className="header-item">
-              <img src="http://45.55.217.62/wp-content/uploads/2016/04/quill_connect_logo2.png" alt="" style={{height: "35px"}}/>
-            </Link>
+            <a href="http://www.connect.quill.org" className="header-item">
+              <img src="http://45.55.217.62/wp-content/uploads/2016/04/quill_connect_logo2.png"
+                alt=""
+                style={{height: "35px"}}/>
+            </a>
           </div>
           <div className="header-right">
-            <Link to={'/play'} className="header-tab" activeClassName="is-active">Questions</Link>
+            <a href="http://www.connect.quill.org/dwqa-questions/" className="header-tab" activeClassName="is-active">Questions</a>
+            <Link to={'/play'} className="header-tab" activeClassName="is-active">Demo</Link>
             <Link to={'/results'} className="header-tab" activeClassName="is-active">Results</Link>
           </div>
           <span className="header-toggle">
