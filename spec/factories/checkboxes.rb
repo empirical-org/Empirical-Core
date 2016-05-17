abc433b7fb060cbbcf466d4f95a3ca733e5b9ef9
RyanNovas <ryan@quill.org>
2016-05-17 13:49:56 -0400

    adds tests and makes find_or_create_checkbox more resilient

--- spec/factories/checkboxes.rb
+++ spec/factories/checkboxes.rb
@@ -1,7 +1,7 @@
-FactoryGirl.define do
-  factory :checkbox do
-    user_id 1
-    objective_id 1
-    created_at "2016-05-16"
-  end
-end
+# FactoryGirl.define do
+#   factory :checkbox do
+#     user_id 1
+#     objective_id 1
+#     created_at "2016-05-16"
+#   end
+# end
--- spec/factories/objectives.rb
+++ spec/factories/objectives.rb
@@ -1,7 +1,5 @@
 FactoryGirl.define do
   factory :objective do
     name "MyString"
-    amount 1
-    metadata "MyString"
   end
 end
