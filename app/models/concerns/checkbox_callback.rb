22a83844ecbed235d7366cbcec9620cb8f881659
RyanNovas <ryan@quill.org>
2016-05-17 13:49:56 -0400

    adds tests and makes find_or_create_checkbox more resilient

--- app/models/concerns/checkbox_callback.rb
+++ app/models/concerns/checkbox_callback.rb
@@ -2,8 +2,9 @@ module CheckboxCallback
   extend ActiveSupport::Concern
 
   def find_or_create_checkbox(name, user)
-      objective_id = Objective.find_by_name(name).id
-      Checkbox.find_or_create_by(user_id: user.id, objective_id: objective_id)
+    if (Objective.find_by_name(name) && user.id)
+      Checkbox.find_or_create_by(user_id: user.id, objective_id: Objective.find_by_name(name).id)
+    end
   end
 
 end
--- app/workers/join_classroom_worker.rb
+++ app/workers/join_classroom_worker.rb
@@ -1,9 +1,9 @@
 class JoinClassroomWorker
   include Sidekiq::Worker
+  include CheckboxCallback
 
   def perform(id)
     @user = User.find(id)
-
     # tell segment.io
     analytics = JoinClassroomAnalytics.new
     analytics.track(@user)
