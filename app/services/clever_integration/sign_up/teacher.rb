2e05af2513074b01756ed30eb920a187538ca5d3
Donald McKendrick <ddmckendrick@gmail.com>
2016-05-18 16:39:09 -0400

    Don’t pass requesters as an argument as we don’t need it.

--- app/services/clever_integration/sign_up/teacher.rb
+++ app/services/clever_integration/sign_up/teacher.rb
@@ -16,7 +16,7 @@ module CleverIntegration::SignUp::Teacher
         puts "Imported School"
         classrooms = self.import_classrooms(teacher, district.token, requesters)
         puts "Imported Classrooms"
-        students = self.import_students(classrooms, district.token, requesters)
+        students = self.import_students(classrooms, district.token)
         puts "Imported Students"
         result = {type: 'user_success', data: teacher}
       else
@@ -49,8 +49,8 @@ module CleverIntegration::SignUp::Teacher
     CleverIntegration::Importers::Classrooms.run(teacher, district_token, requesters[:teacher_requester])
   end
 
-  def self.import_students(classrooms, district_token, requesters)
+  def self.import_students(classrooms, district_token)
     classroom_ids = classrooms.collect {|c| c.id}
-    CleverStudentImporterWorker.perform_async(classroom_ids, district_token, requesters)
+    CleverStudentImporterWorker.perform_async(classroom_ids, district_token)
   end
 end
