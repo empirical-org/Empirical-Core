e07e3a2deb8190c80b05d2456444e209916ac9c1
Donald McKendrick <ddmckendrick@gmail.com>
2016-05-18 16:40:23 -0400

    Remove requesters argument from the perform function in CleverStudentImportWorker

--- app/workers/clever_student_importer_worker.rb
+++ app/workers/clever_student_importer_worker.rb
@@ -1,7 +1,7 @@
 class CleverStudentImporterWorker
   include Sidekiq::Worker
 
-  def perform(classroom_ids, district_token, requesters)
+  def perform(classroom_ids, district_token)
     puts "Running Clever student import in the background"
     classrooms = Classroom.unscoped.where(id: classroom_ids)
     CleverIntegration::Importers::Students.run(classrooms, district_token, CleverIntegration::Requesters.section)
