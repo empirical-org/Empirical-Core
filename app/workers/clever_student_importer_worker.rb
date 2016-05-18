c264fffcac0686c53aebeeeb5ca178bcbf8dd190
Donald McKendrick <ddmckendrick@gmail.com>
2016-05-18 14:33:00 -0400

    log more of the arguments out.

--- app/workers/clever_student_importer_worker.rb
+++ app/workers/clever_student_importer_worker.rb
@@ -3,6 +3,9 @@ class CleverStudentImporterWorker
 
   def perform(classrooms, district_token, requesters)
     puts "Running Clever student import in the background"
+    puts "Classrooms: #{classrooms}"
+    puts "district_token #{district_token}"
+    puts "requesters #{requesters}"
     CleverIntegration::Importers::Students.run(classrooms, district_token, requesters[:section_requester])
   end
 end
