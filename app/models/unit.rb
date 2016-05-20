d6c08b0c16fe1bc81eaf82a209a576a9c5355f1f
RyanNovas <ryan@quill.org>
2016-05-20 15:05:16 -0400

    moves checkbox for units into classroom activity model

--- app/models/unit.rb
+++ app/models/unit.rb
@@ -1,28 +1,16 @@
 class Unit < ActiveRecord::Base
 
-  include CheckboxCallback
-
   belongs_to :classroom
   has_many :classroom_activities, dependent: :destroy
   has_many :activities, through: :classroom_activities
   has_many :topics, through: :activities
   default_scope { where(visible: true)}
-  after_save :teacher_checkbox
 
 
 
-  def teacher_checkbox
 
-    if (self.classroom && self.classroom.teacher)
-      if UnitTemplate.find_by_name(self.name)
-        checkbox_name = 'Assign Featured Activity Pack'
-      else
-        checkbox_name = 'Build Your Own Activity Pack'
-      end
 
-      find_or_create_checkbox(checkbox_name, self.classroom.teacher)
-    end
-  end
+
 
 
 end
--- app/workers/checkbox_analytics_worker.rb
+++ app/workers/checkbox_analytics_worker.rb
@@ -4,9 +4,9 @@ class CheckboxAnalyticsWorker
   def perform(user_id, name)
     user = User.find(user_id)
     analytics = SegmentAnalytics.new
-    
     if report_to_segment?(name)
-      analytics.track_event_from_string(name.upcase.gsub(' ', '_'), user_id)
+      constanty_name = name.upcase.gsub(' ', '_')
+      analytics.track_event_from_string(constanty_name, user_id)
     end
   end
 
