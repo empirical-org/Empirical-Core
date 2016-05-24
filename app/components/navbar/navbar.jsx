5c6658127b0d299f04a933c9f88177136bcf59e4
Donald McKendrick <ddmckendrick@gmail.com>
2016-05-24 11:31:47 -0400

    update nav bar for new bulma lib

--- app/components/navbar/navbar.jsx
+++ app/components/navbar/navbar.jsx
@@ -33,15 +33,15 @@ const Navbar = React.createClass({
   renderLinks: function () {
     if (this.inLesson()) {
       return (
-        <div className="header-right header-menu" style={this.navStyles()}>
+        <div className="nav-right nav-menu" style={this.navStyles()}>
         </div>
       )
     } else {
       return (
-        <div className="header-right header-menu" style={this.navStyles()}>
-          <a href="http://www.connect.quill.org/dwqa-questions/" className="header-tab" activeClassName="is-active">FAQ</a>
-          <Link to={'/play'} className="header-tab" activeClassName="is-active" onClick={this.reset}>Demo</Link>
-          <Link to={'/results'} className="header-tab" activeClassName="is-active" onClick={this.reset}>Results</Link>
+        <div className="nav-right nav-menu" style={this.navStyles()}>
+          <a href="http://www.connect.quill.org/dwqa-questions/" className="nav-item" activeClassName="is-active">FAQ</a>
+          <Link to={'/play'} className="nav-item" activeClassName="is-active" onClick={this.reset}>Demo</Link>
+          <Link to={'/results'} className="nav-item" activeClassName="is-active" onClick={this.reset}>Results</Link>
         </div>
       )
     }
@@ -49,17 +49,17 @@ const Navbar = React.createClass({
 
   render: function () {
     return (
-      <header className="header" style={{height: '65px'}}>
+      <header className="nav" style={{height: '65px'}}>
         <div className="container">
-          <div className="header-left">
-            <a href="http://www.connect.quill.org" className="header-item">
+          <div className="nav-left">
+            <a href="http://www.connect.quill.org" className="nav-item">
               <img src="http://45.55.217.62/wp-content/uploads/2016/04/quill_connect_logo2.png"
                 alt=""
                 style={{height: "35px"}}/>
             </a>
           </div>
           {this.renderLinks()}
-          <span className="header-toggle" onClick={this.toggle}>
+          <span className="nav-toggle" onClick={this.toggle}>
             <span />
             <span />
             <span />
@@ -70,16 +70,16 @@ const Navbar = React.createClass({
   }
 })
 
-const rightNav = (<div className="header-right header-menu">
-  <span className="header-item">
-    <Link to={'/admin'} className="header-item" activeClassName="is-active">Admin</Link>
+const rightNav = (<div className="nav-right nav-menu">
+  <span className="nav-item">
+    <Link to={'/admin'} className="nav-item" activeClassName="is-active">Admin</Link>
   </span>
-  <span className="header-item">
+  <span className="nav-item">
     <a href="#">
       Help
     </a>
   </span>
-  <span className="header-item">
+  <span className="nav-item">
     <a className="button" href="#">Button</a>
   </span>
 </div>)
