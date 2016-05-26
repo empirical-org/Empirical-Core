c24199f7cee3262000ef4fdd6cdbf07654ae0b31
RyanNovas <ryan@quill.org>
2016-05-26 13:24:03 -0400

    completes styling of checklist and graph

--- app/assets/javascripts/teacher_guide/getting_started_mini.jsx
+++ app/assets/javascripts/teacher_guide/getting_started_mini.jsx
@@ -4,8 +4,40 @@ EC.GettingStartedMini = React.createClass({
     checkboxData: React.PropTypes.object.isRequired
   },
 
+  percentageCompleted: function(){
+    var boxes = this.props.checkboxData;
+    var ratio = boxes.filter(obj => obj.completed === true).length / boxes.length;
+    var percentage = Math.floor(ratio * 100);
+    return percentage;
+  },
+
+  graphSection: function(){
+    return (
+      <div id='graph-section'>
+        <h3>Getting Started</h3>
+        <EC.PercentageGraph percentage={this.percentageCompleted()}/>
+        <a className='green-link' href='/teachers/teacher_guide'>View All Tasks ></a>
+      </div>);
+  },
+
+  checklistSection: function() {
+    return <EC.CheckboxSection checkboxes={this.props.checkboxData} dashboard={true}/>
+  },
+
   render: function() {
-    return <EC.PercentageGraph percentage={50}/>
+    return (
+      <div className='mini-content'>
+      <div className='row'>
+        <div className='col-sm-4'>
+          {this.graphSection()}
+      </div>
+      <div className='col-sm-8'>
+          {this.checklistSection()}
+      </div>
+    </div>
+    </div>
+  );
+
   }
 
 
--- app/assets/javascripts/teacher_guide/percentage_graph.jsx
+++ app/assets/javascripts/teacher_guide/percentage_graph.jsx
@@ -5,14 +5,13 @@ EC.PercentageGraph = React.createClass({
   },
 
   render: function() {
-    alert('working')
     return (
       <div className='circle-graph'>
-      <div class="c100 p25">
-      <span>25%</span>
-      <div class="slice">
-        <div class="bar"></div>
-        <div class="fill"></div>
+      <div className={"c100 p" + this.props.percentage}>
+      <span>{this.props.percentage + '%'}</span>
+      <div className="slice">
+        <div className="bar"></div>
+        <div className="fill"></div>
       </div>
     </div>
   </div>)
