4c583399509056dbe8c2e2b8dd94ec34df9952a1
RyanNovas <ryan@quill.org>
2016-06-02 17:56:59 -0400

    makes circle graph go to full checkbox list on click

--- app/assets/javascripts/teacher_guide/percentage_graph.jsx
+++ app/assets/javascripts/teacher_guide/percentage_graph.jsx
@@ -3,12 +3,15 @@ EC.PercentageGraph = React.createClass({
   propTypes: {
     percentage: React.PropTypes.number.isRequired
   },
+  goToTeacherGuide: function(){
+    window.location="/teachers/teacher_guide";
+  },
 
   render: function() {
     return (
       <div className='circle-graph'>
       <div className={"c100 p" + this.props.percentage}>
-      <span>{this.props.percentage + '%'}</span>
+      <span onClick={this.goToTeacherGuide}>{this.props.percentage + '%'}</span>
       <div className="slice">
         <div className="bar"></div>
         <div className="fill"></div>
