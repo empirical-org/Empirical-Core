867b953deac21c9f0853792ba0b62953187c634a
RyanNovas <ryan@quill.org>
2016-06-08 12:07:59 -0400

    migrates dashboard

--- client/app/bundles/HelloWorld/startup/clientRegistration.jsx
+++ client/app/bundles/HelloWorld/startup/clientRegistration.jsx
@@ -1,5 +1,6 @@
 import ReactOnRails from 'react-on-rails';
 import HelloWorldApp from './HelloWorldAppClient';
+import DashboardApp from './DashboardAppClient';
 import TeacherGuideApp from './TeacherGuideClient.jsx';
 // This is how react_on_rails can see the HelloWorldApp in the browser.
-ReactOnRails.register({ TeacherGuideApp });
+ReactOnRails.register({ TeacherGuideApp, HelloWorldApp, DashboardApp  });
