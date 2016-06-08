7ec0ad9d509bb2eaa949e413a736a4c0ed4d1b67
RyanNovas <ryan@quill.org>
2016-06-08 11:22:02 -0400

    moves teacher guide to client

--- client/app/bundles/HelloWorld/startup/clientRegistration.jsx
+++ client/app/bundles/HelloWorld/startup/clientRegistration.jsx
@@ -1,5 +1,5 @@
 import ReactOnRails from 'react-on-rails';
 import HelloWorldApp from './HelloWorldAppClient';
-
+import TeacherGuideApp from './TeacherGuideClient.jsx';
 // This is how react_on_rails can see the HelloWorldApp in the browser.
-ReactOnRails.register({ HelloWorldApp });
+ReactOnRails.register({ TeacherGuideApp });
