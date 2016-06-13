ac720fb1fc9f33ce1cc43e04f16f723546106d71
Donald McKendrick <ddmckendrick@gmail.com>
2016-06-13 11:03:14 -0400

    Do not check for ID on user in the callback, as if it is nil, it will error instead of not saving.

--- app/models/concerns/checkbox_callback.rb
+++ app/models/concerns/checkbox_callback.rb
@@ -2,9 +2,9 @@ module CheckboxCallback
   extend ActiveSupport::Concern
 
   def find_or_create_checkbox(name, user, flag=nil)
-    if (Objective.find_by_name(name) && user.id)
+    if (Objective.find_by_name(name) && user)
       Checkbox.find_or_create_by(user_id: user.id, objective_id: Objective.find_by_name(name).id)
-      CheckboxAnalyticsWorker.perform_async(user.id, name) unless flag 
+      CheckboxAnalyticsWorker.perform_async(user.id, name) unless flag
     end
   end
 
