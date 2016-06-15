ba44b78c9b124724ba49aa710ae68c20fa5e4db1
RyanNovas <ryan@quill.org>
2016-06-15 14:37:18 -0400

    changes asset pipeline for hotreloading

--- client/webpack.server.rails.build.config.js
+++ client/webpack.server.rails.build.config.js
@@ -12,7 +12,7 @@ module.exports = {
   context: __dirname,
   entry: [
     'babel-polyfill',
-    './app/bundles/comments/startup/serverRegistration',
+    // './app/bundles/comments/startup/serverRegistration',
   ],
   output: {
     filename: 'server-bundle.js',
