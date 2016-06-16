e140f70782b33051786ce7558e2db84a5b037193
RyanNovas <ryan@quill.org>
2016-06-16 16:42:12 -0400

    fixes setter

--- client/app/bundles/HelloWorld/components/modules/updater.jsx
+++ client/app/bundles/HelloWorld/components/modules/updater.jsx
@@ -1,16 +1,13 @@
 'use strict'
 
-  import _ from 'underscore'
-  import setter from './setter'
+ import _ from 'underscore'
+ import setter from './setter'
 
-  export default
-
-  function (component) {
-  var _setter = new setter();
-  this.updater = function (path) {
-    return function (value) {
-      var x = _setter.setOrExtend(component.state, path, value)
-      component.setState(x)
-    }
-  }
-}
+ export default function (component) {
+ this.updater = function (path) {
+   return function (value) {
+     var x = setter(component.state, path, value)
+     component.setState(x)
+   }
+ }
+ }
