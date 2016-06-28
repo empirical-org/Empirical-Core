0206708150f0404aae64888efd1bae1992b57d76
RyanNovas <ryan@quill.org>
2016-06-28 11:34:10 -0400

    fixes broken activity filters

--- client/app/bundles/HelloWorld/components/lesson_planner/create_unit/activity_search/activity_search_filter.jsx
+++ client/app/bundles/HelloWorld/components/lesson_planner/create_unit/activity_search/activity_search_filter.jsx
@@ -1,7 +1,7 @@
 'use strict'
 
  import React from 'react'
-  import _ from 'underscore'
+  import _ from 'lodash'
 	import FilterOption from '../filter_option'
 
  export default  React.createClass({
