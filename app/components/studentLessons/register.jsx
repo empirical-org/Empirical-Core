0da9bff3635aad91df298405cc9f7aad54c39716
Donald McKendrick <ddmckendrick@gmail.com>
2016-07-06 17:52:32 -0400

    if the lesson has a lessonURl, then we show an iframe of it after they enter their name.
    Updated the lesson form to allow staff to add a url.

--- app/components/studentLessons/register.jsx
+++ app/components/studentLessons/register.jsx
@@ -1,17 +1,44 @@
 import React from 'react'
 
 export default React.createClass({
+  getInitialState: function () {
+    return {
+      showIntro: false,
+      name: ''
+    }
+  },
+
+  handleNameChange: function (e) {
+    this.setState({name: e.target.value});
+  },
+
   getLessonName: function () {
     return this.props.lesson.name
   },
 
   startActivity: function () {
-    this.props.startActivity(this.refs.name.value)
+    if (this.props.lesson.introURL) {
+      this.setState({showIntro: true})
+    } else {
+      this.props.startActivity(this.state.name)
+    }
   },
 
-  render: function () {
-    return (
-      <section className="section is-fullheight minus-nav">
+  leaveIntro: function () {
+    this.props.startActivity(this.state.name)
+  },
+
+  renderIntro: function () {
+    if (this.state.showIntro) {
+      return (
+        <div className="container">
+          <button className="button is-primary intro-next-button" onClick={this.leaveIntro}>Start Lesson</button>
+
+          <iframe className="intro-slides"  src={this.props.lesson.introURL}/>
+        </div>
+      )
+    } else {
+      return (
         <div className="container">
           <h2 className="title is-3">
             Welcome to Quill Connect!
@@ -26,11 +53,19 @@ export default React.createClass({
             You are joining the <span style={{fontWeight: '700'}}>{this.getLessonName()}</span> class.
           </h4>
           <p className="control">
-            <input className="input" type="text" ref="name" placeholder="Enter your name"></input>
+            <input className="input" type="text" onChange={this.handleNameChange} placeholder="Enter your name"></input>
           </p>
           <button className="button is-primary" onClick={this.startActivity}>Start</button>
           <br/>
         </div>
+      )
+    }
+  },
+
+  render: function () {
+    return (
+      <section className="section is-fullheight minus-nav">
+        {this.renderIntro()}
       </section>
     )
   }
