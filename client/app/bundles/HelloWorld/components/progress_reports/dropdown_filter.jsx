02d8e04238db222284ab33eaaacde2fd8542ee74
RyanNovas <ryan@quill.org>
2016-07-08 12:10:48 -0400

    more debuggers for staging

--- client/app/bundles/HelloWorld/components/progress_reports/dropdown_filter.jsx
+++ client/app/bundles/HelloWorld/components/progress_reports/dropdown_filter.jsx
@@ -35,6 +35,7 @@ export default React.createClass({
     }
   },
   render: function() {
+    console.log(this.props)
     return (
         <div className="button-select">
           <button type="button" className={this.getButtonClassName() + " select-mixin button-select button-select-wrapper"} data-toggle="dropdown">
