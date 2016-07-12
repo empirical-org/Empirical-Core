246d691c590c2eb897bc09b3b95afba4d4ea5410
Donald McKendrick <ddmckendrick@gmail.com>
2016-07-12 15:27:27 -0400

    staging env for firebase.

--- app/libs/firebase.js
+++ app/libs/firebase.js
@@ -1,11 +1,22 @@
-import firebase from 'firebase'
+const prod = process.env.NODE_ENV === "production";
 
-var config = {
+import firebase from 'firebase'
+var config = {}
+if (prod) {
+  config = {
     apiKey: "AIzaSyC6mYwIeRVh3B1d0fuvXH6oKekFtSCg-IE",
     authDomain: "quillconnect.firebaseapp.com",
     databaseURL: "https://quillconnect.firebaseio.com",
     storageBucket: "quillconnect.appspot.com",
   };
+} else {
+  config = {
+    apiKey: "AIzaSyAJNeVssNkcjNonREdRZ7gTyEDqmAfz7Go",
+    authDomain: "quillconnectstaging.firebaseapp.com",
+    databaseURL: "https://quillconnectstaging.firebaseio.com",
+    storageBucket: "quillconnectstaging.appspot.com",
+  };
+}
 
 firebase.initializeApp(config);
 
