59268ac93e9a611353040f528ee50dedfc7dd833
RyanNovas <ryan@quill.org>
2016-07-20 13:41:01 -0400

    renders modal without correct fields

--- app/components/questions/focusPointForm.jsx
+++ app/components/questions/focusPointForm.jsx
@@ -1,28 +1,61 @@
 import React from 'react'
-import { connect } from 'react-redux'
+import {connect} from 'react-redux'
 import {Link} from 'react-router'
 // import questionActions from '../../actions/questions'
 import _ from 'underscore'
+import Modal from '../modal/modal.jsx'
 // import {hashToCollection} from '../../libs/hashToCollection'
 import C from '../../constants'
 
 export default React.createClass({
     propTypes: {
-      getFocusPoint: React.PropTypes.func.isRequired
+        getFocusPoint: React.PropTypes.func.isRequired
+    },
+
+    getInitialState: function() {
+        return ({modalDisplay: false});
+    },
+
+    addOrEditFocusPoint: function() {
+        let prefix = (!!this.props.getFocusPoint()? 'Edit': 'Add New');
+        return prefix + ' Focus Point'
+    },
+
+    renderFocusPointForm: function() {
+        this.setState({
+            modalDisplay: !this.state.modalDisplay
+        });
+    },
+
+    modal: function() {
+        if (this.state.modalDisplay) {
+            return (
+                <Modal>
+                    <div className="box">
+                        <h4 className="title">{this.addOrEditFocusPoint()}</h4>
+                        <p className="control">
+                            <label className="label">Name</label>
+                            <input className="input" type="text" placeholder="Text input" ref="newConceptName"/>
+                        </p>
+                        <p className="control">
+                            {/*<button className={"button is-primary " + stateSpecificClass} onClick={this.submitNewConcept}>Submit</button>*/}
+                            <button className={"button is-primary "}>Submit</button>
+                        </p>
+                    </div>
+                </Modal>
+              );
+        }
     },
 
     render: function() {
         let fp = this.props.getFocusPoint();
-        if (fp) {
-          return (
-            <div className="box">
-              <h4 className="control title is-4">Focus Point: '{fp.text}'</h4>
-              <h6 className="control sub-title is-6">Feedback: {fp.feedback}</h6>
+        let classy = fp? 'is-info' : 'is-primary';
+        return (
+            <div>
+                <button type='button' onClick={this.renderFocusPointForm} className={'button ' + classy}>{this.addOrEditFocusPoint()}</button>
+                {this.modal()}
             </div>
-          );
-    } else {
-      return (<h1>NO FOCUS POINT</h1>);
+        );
     }
-  }
 
 });
