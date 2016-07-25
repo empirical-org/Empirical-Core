6002a50a14d53c7177dd8b20c32e6d1265e1976f
RyanNovas <ryan@quill.org>
2016-07-25 17:16:12 -0400

    content for first page of diagnostic assignment workflow

---
+++ client/app/bundles/HelloWorld/components/diagnostic/diagnostic_questionnaire/IntroPage.jsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import $ from 'jquery'
+import DropdownButton from 'react-bootstrap/lib/DropdownButton';
+import MenuItem from 'react-bootstrap/lib/MenuItem';
+
+
+export default React.createClass({
+
+  getInitialState: function(){
+    return ({selectedGrade: '1st'})
+  },
+
+  handleSelect: function(grade){
+    this.setState({selectedGrade: grade})
+  },
+
+  grades: function(){
+    let grades = []
+    let formattedGrade;
+    for(let grade = 1; grade <= 12; grade++){
+      if (grade === 1) {
+        formattedGrade = grade + 'st'
+      } else if (grade === 2) {
+        formattedGrade = grade + 'nd'
+      } else if (grade === 3) {
+        formattedGrade = grade + 'rd'
+      } else {
+        formattedGrade = grade + 'th'
+      }
+      grades.push(<MenuItem key={formattedGrade} eventKey={formattedGrade}>{formattedGrade}</MenuItem>)
+    }
+    return grades
+  },
+
+
+    render: function() {
+        return (
+            <div id='intro-page'>
+                <div>
+                    <h2>Would you like to see the<span>
+                      <DropdownButton bsStyle='default' title={this.state.selectedGrade || 'st'} id='select-grade' onSelect={this.handleSelect}>
+                        {this.grades()}
+                      </DropdownButton>
+                        </span>grade reading level diagnostic?</h2>
+                    <span>The reading level may be higher or lower than the grade level.</span>
+                </div>
+                <button className='button-green'>Preview the diagnostic</button>
+                <br/>
+                <button className='button-green'>Continue to Assign</button>
+            </div>
+        )
+    }
+});
--- client/app/bundles/HelloWorld/components/diagnostic/diagnostic_questionnaire/StatusBar.jsx
+++ client/app/bundles/HelloWorld/components/diagnostic/diagnostic_questionnaire/StatusBar.jsx
@@ -3,22 +3,37 @@ import React from 'react'
 
 export default React.createClass({
 
-  render: function() {
-    return (
-      <div  >
-        <div>
-          <img src="images/diagnostic_icon.svg" alt="diagnostic_icon"/>
-          <span>Entry Diagnostic></span>
-        </div>
-        <div id='status-bar'>
-            <div className='check-point'><span>1</span></div>
-            <div className='connector-line'></div>
-            <div className='check-point'><span>2</span></div>
-              <div className='connector-line'></div>
-            <div className='check-point'><span>3</span></div>
-        </div>
-      </div>
-    );
-  }
+    render: function() {
+        return (
+            <div id='status-bar-wrapper'>
+                <div>
+                    <img src="images/diagnostic_icon.svg" alt="diagnostic_icon"/>
+                    <span>Entry Diagnostic</span>
+                </div>
+                <div id='status-bar'>
+                    <div className='check-point'>
+                        <span>1</span>
+                        <div className='check-point-type'>
+                            <span>Introduction</span>
+                        </div>
+                    </div>
+                    <div className='connector-line'></div>
+                    <div className='check-point'>
+                        <span>2</span>
+                        <div className='check-point-type'>
+                          <span>Preview</span>
+                        </div>
+                    </div>
+                    <div className='connector-line'></div>
+                    <div className='check-point'>
+                        <span>3</span>
+                        <div className='check-point-type'>
+                            <span>Assign</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+          );
+        }
 
-});
+      });
