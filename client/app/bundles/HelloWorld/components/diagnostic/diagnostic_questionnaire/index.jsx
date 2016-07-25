130565c970ec22ae7941a03b224e7c5fd1e5e611
RyanNovas <ryan@quill.org>
2016-07-25 17:16:12 -0400

    content for first page of diagnostic assignment workflow

--- client/app/bundles/HelloWorld/components/diagnostic/diagnostic_questionnaire/index.jsx
+++ client/app/bundles/HelloWorld/components/diagnostic/diagnostic_questionnaire/index.jsx
@@ -2,15 +2,31 @@
 
 import React from 'react'
 import StatusBar from './StatusBar.jsx'
+import IntroPage from './IntroPage'
 
 export default React.createClass({
 
-    render: function() {
-        return (
-            <div id='diagnostic-planner'>
-                <StatusBar/>
-            </div>
-        );
+  getInitialState: function() {
+    return {stage: 1}
+  },
+
+  content: function() {
+    var content;
+    switch (this.state.stage) {
+      case 1:
+        content = <IntroPage/>
+        break;
     }
+    return content
+  },
+
+  render: function() {
+      return (
+          <div id='diagnostic-planner'>
+              <StatusBar/>
+              {this.content()}
+          </div>
+      );
+  }
 
 });
