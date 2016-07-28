2d39164ee75f77d2d2ddd304c63a60dd28d2ec44
Donald McKendrick <ddmckendrick@gmail.comredis-server>
2016-07-28 17:11:42 -0400

    Add tests and code for underlining student errors.

---
+++ app/libs/markupUserResponses.js
@@ -0,0 +1,43 @@
+import {diffWords} from 'diff';
+import _ from 'lodash';
+
+export function getChangeObjects (targetString, userString) {
+  return diffWords(targetString, userString)
+};
+
+export function getChangeObjectsWithoutRemoved (targetString, userString) {
+  const changeObjects = getChangeObjects(targetString, userString);
+  return _.reject(changeObjects, (changeObject) => {
+    return changeObject.removed
+  })
+}
+
+export function getChangeObjectsWithoutAdded (targetString, userString) {
+  const changeObjects = getChangeObjects(targetString, userString);
+  return _.reject(changeObjects, (changeObject) => {
+    return changeObject.added
+  })
+}
+
+export function getErroneousWordLength (changeObjects) {
+  const addedWord = _.filter(changeObjects, {added: true})[0]
+  return addedWord.value.length || 0
+}
+
+export function getErroneousWordOffset (changeObjects) {
+  const precedingObjects = _.takeWhile(changeObjects, (changeObject) => {
+    return !changeObject.added
+  })
+  return _.reduce(precedingObjects, (sum, changeObject) => {
+    return sum + changeObject.value.length
+  }, 0)
+}
+
+export function getInlineStyleRangeObject (targetString, userString) {
+  const changeObjects = getChangeObjectsWithoutRemoved(targetString, userString)
+  return {
+    length: getErroneousWordLength(changeObjects),
+    offset: getErroneousWordOffset(changeObjects),
+    style: "UNDERLINED"
+  }
+}
---
+++ test/libs/underlineResponse.spec.js
@@ -0,0 +1,77 @@
+import expect from 'expect';
+import {diffWords} from 'diff'
+import { getChangeObjects, getChangeObjectsWithoutRemoved, getChangeObjectsWithoutAdded, getErroneousWordLength, getErroneousWordOffset, getInlineStyleRangeObject } from '../../app/libs/markupUserResponses.js'
+
+
+describe("finding the position of the substring", () => {
+  const target = "I like NYC.";
+  const user = "I liked NYC.";
+
+  it("returns change objects", () => {
+    const expected = [
+      { count: 2, value: 'I ' },
+      { count: 1, added: undefined, removed: true, value: 'like' },
+      { count: 1, added: true, removed: undefined, value: 'liked' },
+      { count: 3, value: ' NYC.' }
+    ]
+    expect(diffWords(target, user)).toEqual(expected);
+  });
+
+  it("gets change objects from the library", () => {
+    const expected = [
+      { count: 2, value: 'I ' },
+      { count: 1, added: undefined, removed: true, value: 'like' },
+      { count: 1, added: true, removed: undefined, value: 'liked' },
+      { count: 3, value: ' NYC.' }
+    ]
+    expect(getChangeObjects(target, user)).toEqual(expected);
+  })
+
+  it("gets change objects without removed objects", () => {
+    const expected = [
+      { count: 2, value: 'I ' },
+      { count: 1, added: true, removed: undefined, value: 'liked' },
+      { count: 3, value: ' NYC.' }
+    ]
+    expect(getChangeObjectsWithoutRemoved(target, user)).toEqual(expected);
+  })
+
+  it("gets change objects without added objects", () => {
+    const expected = [
+      { count: 2, value: 'I ' },
+      { count: 1, added: undefined, removed: true, value: 'like' },
+      { count: 3, value: ' NYC.' }
+    ]
+    expect(getChangeObjectsWithoutAdded(target, user)).toEqual(expected);
+  })
+
+  it("gets length of first erroneous word", () => {
+    const input = [
+      { count: 2, value: 'I ' },
+      { count: 1, added: true, removed: undefined, value: 'liked' },
+      { count: 3, value: ' NYC.' }
+    ]
+    const expected = 5;
+    expect(getErroneousWordLength(input)).toEqual(expected)
+  })
+
+  it("gets offset of first erroneous word", () => {
+    const input = [
+      { count: 2, value: 'I ' },
+      { count: 1, added: true, removed: undefined, value: 'liked' },
+      { count: 3, value: ' NYC.' }
+    ]
+    const expected = 2;
+    expect(getErroneousWordOffset(input)).toEqual(expected)
+  })
+
+
+  it("returns a inline style range object", () => {
+    const expected = {
+      length: 5,
+      offset: 2,
+      style: "UNDERLINED"
+    }
+    expect(getInlineStyleRangeObject(target, user)).toEqual(expected)
+  })
+});
