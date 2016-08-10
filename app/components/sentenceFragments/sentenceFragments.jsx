91aa1f5c5fb1ec0bd56573ec87f7ac05de3f4b7c
Donald McKendrick <ddmckendrick@gmail.comredis-server>
2016-08-10 17:37:56 -0400

    show the list of sentence fragments and a link to create a new one.

--- app/components/sentenceFragments/sentenceFragments.jsx
+++ app/components/sentenceFragments/sentenceFragments.jsx
@@ -1,17 +1,30 @@
 import React, {Component} from 'react'
+import {connect} from 'react-redux'
+import { Link } from 'react-router'
 
-class SentenceFragments extends Component {
-  constructor(props) {
-    super(props);
-  }
+import SentenceFragmentsList from './sentenceFragmentsList.jsx'
+import {hashToCollection} from '../../libs/hashToCollection'
+
+const SentenceFragments = React.createClass({
 
   render() {
+    const sentenceFragments = hashToCollection(this.props.sentenceFragments.data)
     return (
-      <div>
-      <div>index</div>
-      </div>
+      <section className="section">
+        <div className="container">
+          <Link to={'admin/sentence-fragments/new'}>Create a New Sentence Fragment</Link>
+          <SentenceFragmentsList sentenceFragments={sentenceFragments || []}/>
+        </div>
+      </section>
     )
+  }
+})
+
+function select(state) {
+  return {
+    sentenceFragments: state.sentenceFragments,
+    routing: state.routing
   }
 }
 
-export default SentenceFragments
+export default connect(select)(SentenceFragments)
