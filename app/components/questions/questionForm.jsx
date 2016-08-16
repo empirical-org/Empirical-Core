92472c31ee088def492055226bc16a9dee98beee
raghavmehrotra <raghavmehrotra@raghavs-mbp.home>
2016-08-16 19:02:03 -0400

    Question form has a concept dropdown with fuzzy matching

--- app/components/questions/questionForm.jsx
+++ app/components/questions/questionForm.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import TextEditor from './textEditor.jsx';
 import {hashToCollection} from '../../libs/hashToCollection'
 import _ from 'lodash'
+import ConceptSelector from 'react-select-search'
+// import ConceptSelector from 'react-selectize'.SimpleSelect
+var SimpleSelect = require("react-selectize").SimpleSelect
 
 export default React.createClass({
   getInitialState: function () {
@@ -13,8 +16,13 @@ export default React.createClass({
   },
 
   submit: function () {
-    const conceptID = _.find(this.props.concepts.data, {name: this.state.concept}).key
-    console.log("The new ID is: ", conceptID)
+    var conceptID;
+    var temp = hashToCollection(this.props.concepts.data)
+
+    for(var index in temp) {
+      if(temp[index].name===this.state.concept) conceptID = temp[index].key
+    }
+
     this.props.submit({
       prompt: this.state.prompt,
       prefilledText: this.refs.prefilledText.value,
@@ -42,53 +50,69 @@ export default React.createClass({
     })
   },
 
+  handleSelectorChange: function(e) {
+    this.setState({concept: e.name})
+  },
+
   handleConceptChange: function() {
     this.setState({concept: this.refs.concept.value})
   },
 
   conceptsToOptions: function() {
-    return hashToCollection(this.props.concepts.data).map((concept) => {
+    return _.map(this.props.concepts.data, (concept)=>{
       return (
-        <option>{concept.name}</option>
+        {name: concept.name, value: concept.name}
       )
     })
   },
 
   render: function () {
-    console.log("inside questionForm.jsx, props: ", this.props)
-    return (
-      <div className="box">
-        <h6 className="control subtitle">Create a new question</h6>
-        <label className="label">Prompt</label>
-        <TextEditor text={this.props.question.prompt || ""} handleTextChange={this.handlePromptChange}/>
-        <label className="label">Cues (separated by commas, no spaces eg "however,therefore,hence")</label>
-        <p className="control">
-          <input className="input" type="text" ref="cues" defaultValue={this.props.question.cues}></input>
-        </p>
-        <label className="label">Prefilled Text (place 5 underscores where you want the user to fill in _____)</label>
-        <p className="control">
-          <input className="input" type="text" ref="prefilledText" defaultValue={this.props.question.prefilledText}></input>
-        </p>
-        <label className="label">Item level</label>
-        <p className="control">
-          <span className="select">
-            <select onChange={this.handleLevelChange} ref="itemLevel" value={this.state.itemLevel}>
-              <option>Select Item Level</option>
-              {this.itemLevelToOptions()}
-            </select>
-          </span>
-        </p>
-        <label className="label">Concept</label>
-        <p className="control">
-          <span className="select">
-            <select onChange={this.handleConceptChange} ref="concept" value={this.state.concept}>
-              <option>Select Concept</option>
-              {this.conceptsToOptions()}
-            </select>
-          </span>
-        </p>
-        <button className="button is-primary" onClick={this.submit}>Update Question</button>
-      </div>
-    )
+    if(this.props.concepts.hasreceiveddata) {
+      console.log("State: ", this.state)
+      return (
+        <div className="box">
+          <h6 className="control subtitle">Create a new question</h6>
+          <label className="label">Prompt</label>
+          <TextEditor text={this.props.question.prompt || ""} handleTextChange={this.handlePromptChange}/>
+          <label className="label">Cues (separated by commas, no spaces eg "however,therefore,hence")</label>
+          <p className="control">
+            <input className="input" type="text" ref="cues" defaultValue={this.props.question.cues}></input>
+          </p>
+          <label className="label">Prefilled Text (place 5 underscores where you want the user to fill in _____)</label>
+          <p className="control">
+            <input className="input" type="text" ref="prefilledText" defaultValue={this.props.question.prefilledText}></input>
+          </p>
+          <label className="label">Item level</label>
+          <p className="control">
+            <span className="select">
+              <select onChange={this.handleLevelChange} ref="itemLevel" value={this.state.itemLevel}>
+                <option>Select Item Level</option>
+                {this.itemLevelToOptions()}
+              </select>
+            </span>
+          </p>
+          <label className="label">Concept</label>
+          <div>
+            <ConceptSelector options={this.conceptsToOptions()} placeholder={this.state.concept} onChange={this.handleSelectorChange}/>
+          </div>
+          <br/>
+          <button className="button is-primary" onClick={this.submit}>Update Question</button>
+        </div>
+      )
+    } else {
+      return (<div>Loading...</div>)
+    }
   }
 })
+// <ConceptSelector options={[{label: "1", value: "1"}, {label: "2", value: "2"}]} placeholder="Select a concept" />
+// <p className="control">
+//   <span className="select">
+//     <select onChange={this.handleConceptChange} ref="concept" value={this.state.concept}>
+//       <option>Select Concept</option>
+//       {this.conceptsToOptions()}
+//     </select>
+//   </span>
+// </p>
+
+// <SimpleSelect theme="material" options={options} placeholder="Select a concept" filterOptions={(e)=>{this.filterOptions(options, e)}} className="selector" onValueChange={this.handleSelectorChange}>
+// </SimpleSelect>
