9c69b7648c8b3ff612f0adc9f0ab7d892163d07f
RyanNovas <ryan@quill.org>
2016-08-18 12:24:28 -0400

    adds class dropdown to navbar

--- client/app/bundles/HelloWorld/containers/DiagnosticReports.jsx
+++ client/app/bundles/HelloWorld/containers/DiagnosticReports.jsx
@@ -1,22 +1,20 @@
 import React from 'react'
-import { Router, Route, Link, hashHistory } from 'react-router'
+import {Router, Route, Link, hashHistory} from 'react-router'
 import App from '../components/progress_reports/diagnostic_reports/index.jsx'
 import Reports from '../components/progress_reports/diagnostic_reports/reports.jsx'
 import createHashHistory from 'history/lib/createHashHistory'
-const hashhistory = createHashHistory({ queryKey: false })
+const hashhistory = createHashHistory({queryKey: false})
 
 export default React.createClass({
 
-
-
-  render: function () {
-      return (
-        <Router history={hashHistory}>
-          <Route path="/" component={App}>
-            <Route path=':classrom_id/:report' component={Reports}/>
-            <Route path='**' component={Reports}/>
-          </Route>
-        </Router>
-      );
-  }
+	render: function() {
+		return (
+			<Router history={hashHistory}>
+				<Route path="/" component={App}>
+					<Route path=':classrom_id/:report' component={Reports}/>
+					<Route path='**' component={Reports}/>
+				</Route>
+			</Router>
+		);
+	}
 });
