2c01ae6d884f6132d7dffa7a241ade84c91f7dc7
Donald McKendrick <ddmckendrick@gmail.comredis-server>
2016-08-22 18:38:56 -0400

    Add a route to get student results by classroom.

--- app/controllers/teachers/progress_reports/diagnostic_reports_controller.rb
+++ app/controllers/teachers/progress_reports/diagnostic_reports_controller.rb
@@ -21,6 +21,48 @@ class Teachers::ProgressReports::DiagnosticReportsController < Teachers::Progres
                   }
   end
 
+  def students_by_classroom
+    render json: results_for_classroom(params[:classroom_id])
+  end
+
   private
+
+  def results_for_classroom classroom_id
+    classroom = Classroom.find(classroom_id)
+    diagnostic = Activity.find(413)
+    scores = {
+      id: classroom.id,
+      name: classroom.name
+    }
+    scores[:students] = classroom.students.map { |student|
+      loaded = student.activity_sessions.includes(concept_results: :concept).find_by(activity_id: diagnostic.id, is_final_score: true)
+      session = {
+        id: loaded.id,
+        concept_results: loaded.concept_results.group_by{|cr| cr[:metadata]["questionNumber"]}.map { |key, cr|
+          {
+            directions: cr.first[:metadata]["directions"],
+            prompt: cr.first[:metadata]["prompt"],
+            answer: cr.first[:metadata]["answer"],
+            score: cr.inject(0) {|sum, crs| sum + crs[:metadata]["correct"]} / cr.length * 100,
+            concepts: cr.map { |crs|
+              {
+                id: crs.concept_id,
+                name: crs.concept.name,
+                correct: crs[:metadata]["correct"] == 1
+              }
+            },
+            question_number: cr.first[:metadata]["questionNumber"]
+          }
+        }
+      }
+
+      {
+        id: student.id,
+        name: student.name,
+        session: session
+      }
+    }
+    scores
+  end
 
 end
