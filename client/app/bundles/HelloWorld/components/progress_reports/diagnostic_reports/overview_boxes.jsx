7d987e1f8a87f79bdb1905c62f9db80f0e6b98db
RyanNovas <ryan@quill.org>
2016-08-24 12:49:51 -0400

    adds class report with student breakdown
    adds class report with student breakdown

---
+++ client/app/bundles/HelloWorld/components/progress_reports/diagnostic_reports/overview_boxes.jsx
@@ -0,0 +1,62 @@
+import React from 'react'
+import ScoreColor from '../../modules/score_color.js'
+import Pluralize from 'Pluralize'
+
+export default React.createClass({
+
+	propTypes: {
+		data: React.PropTypes.array.isRequired
+	},
+
+	componentDidMount: function() {
+		this.countBoxType();
+	},
+
+	countBoxType: function() {
+		let count = {};
+		let scoreColor;
+		this.props.data.forEach(student => {
+			scoreColor = ScoreColor(student.session.score);
+			count[scoreColor] = count[scoreColor] || 0;
+			count[scoreColor] += 1;
+		})
+		return count;
+	},
+
+	boxCreator: function(group, count) {
+		let range,
+			proficiency
+		if (group === 'red') {
+			range = '0 - 59%';
+			proficiency = 'Not Yet Proficient'
+		} else if (group === 'yellow') {
+			range = '60 - 79%';
+			proficiency = 'Nearly Proficient'
+		} else {
+			range = '80 - 100%';
+			proficiency = 'Proficient'
+		}
+		return (
+			<div className={'student-groupings ' + group} key={group}>
+				<h3>{(count || 0) + ' ' + Pluralize('Student', count)}</h3>
+				<span>{range + ' | ' + proficiency}</span>
+			</div>
+		)
+	},
+
+	groupingBoxes: function() {
+		// need to list the keys in an array instead of just using a for in
+		// loop as we want them in this particular order
+		let groupCounts = this.countBoxType();
+		let groups = ['red', 'yellow', 'green'];
+		return groups.map(group => {
+			group += '-score-color'
+			return this.boxCreator(group, groupCounts[group])
+		})
+	},
+
+	render: function() {
+		return <div id='student-groupings-wrapper'>{this.groupingBoxes()}</div>
+	}
+
+});
