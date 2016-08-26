a8ce5396c88605d4864d84dd68b1c647afa8c7d0
RyanNovas <ryan@quill.org>
2016-08-26 15:05:54 -0400

    router refactoring

--- client/app/bundles/HelloWorld/containers/DiagnosticReports.jsx
+++ client/app/bundles/HelloWorld/containers/DiagnosticReports.jsx
@@ -3,6 +3,9 @@ import {Router, Route, Link, hashHistory} from 'react-router'
 import Index from '../components/progress_reports/diagnostic_reports/index.jsx'
 import Reports from '../components/progress_reports/diagnostic_reports/reports.jsx'
 import createHashHistory from 'history/lib/createHashHistory'
+import StudentReport from '../components/progress_reports/diagnostic_reports/student_report.jsx'
+import ClassReport from '../components/progress_reports/diagnostic_reports/class_report.jsx'
+import QuestionReport from '../components/progress_reports/diagnostic_reports/generic_report.jsx'
 const hashhistory = createHashHistory({queryKey: false})
 
 export default React.createClass({
@@ -10,10 +13,11 @@ export default React.createClass({
 	render: function() {
 		return (
 			<Router history={hashhistory}>
-				<Route path="/" component={Index}>
-					<Route path=':classroomId/:report' component={Reports}/>
-					{/*TODO: build a default route -- the ** below breaks the params*/}
-					{/*<Route path='**' component={Reports}/>*/}
+				<Route path="/" component={Index}> 
+					<Route path=':classroomId/student_report/' component={StudentReport}/>
+					<Route path=':classroomId/student_report/:studentId' component={StudentReport}/>
+					<Route path=':classroomId/class_report/' component={ClassReport}/>
+					<Route path=':classroomId/question_report/' component={QuestionReport}/>
 				</Route>
 			</Router>
 		);
