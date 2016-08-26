0a53225e5101fa8409445d2fb409b662e298f6cc
RyanNovas <ryan@quill.org>
2016-08-26 11:49:55 -0400

    student questions renders current student from url

--- client/app/bundles/HelloWorld/components/progress_reports/diagnostic_reports/student_report.jsx
+++ client/app/bundles/HelloWorld/components/progress_reports/diagnostic_reports/student_report.jsx
@@ -18,41 +18,39 @@ export default React.createClass({
     this.getStudentData()
   },
 
+	selectedStudent: function(students){
+		let query = this.props.params.query
+		if (query && query.studentId) {
+			return students.find(s => s.id === parseInt(query.studentId))
+		} else {
+			return students[0]
+		}
+	},
+
   getStudentData: function(){
     const that = this;
     $.get('/teachers/progress_reports/students_by_classroom/' + that.props.classroom, (data) => {
-      that.setState({questions: data.students[0].session.concept_results, loading: false})
+      // that.setState({questions: data.students[0].session.concept_results, loading: false})
+      that.setState({students: data.students, selectedStudent: this.selectedStudent(data.students), loading: false})
     })
-    //
   },
-
-  mockData: function(){
-    return [{directions: 'Combine the two sentences below into one sentence using one of the words in parentheses. (although, and, so)',
-      prompt: 'The sky turned grey. The rain started. The game was canceled.',
-      score: 100,
-      answer: 'The sky turned grey, then the rain started and the game was canceled.',
-      concepts: [{id: 12232, correct: true, name: 'Punctuate Complex Sentence (beginning)'},
-                {id: 1222232, correct: false, name: 'Identify Fragments and Sentences'}]
-    }]
-  },
-
 
   studentBoxes: function(){
-    // return this.state.questions.map((question, index) => <StudentReportBox key={index} boxNumber={index+1} questionData={question}/>)
-    return this.mockData().map((question, index) => <StudentReportBox key={index} boxNumber={index+1} questionData={question}/>)
+		let concept_results = this.state.selectedStudent.session.concept_results
+    return concept_results.map((question, index) => <StudentReportBox key={index} boxNumber={index+1} questionData={question}/>)
   },
 
 	render: function() {
 		let content;
-		// if (this.state.loading) {
-		// 	content = <LoadingSpinner/>
-		// } else {
+		if (this.state.loading) {
+			content = <LoadingSpinner/>
+		} else {
 			content = (
 				<div id='individual-student-activity-view'>
           {this.studentBoxes()}
 				</div>
 			)
-		// }
+		}
 		return content
 	}
 });
