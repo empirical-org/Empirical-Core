5b885c010128c95bbcc44c7a38bdfdf52a94f7e0
RyanNovas <ryan@quill.org>
2016-08-29 17:33:07 -0400

    refactors add student feature

--- client/app/bundles/HelloWorld/containers/AddStudent.jsx
+++ client/app/bundles/HelloWorld/containers/AddStudent.jsx
@@ -4,6 +4,8 @@ import ClassroomDropdown from '../components/general_components/dropdown_selecto
 import $ from 'jquery'
 import ClassroomsStudentsTable from '../components/general_components/classrooms_students_table.jsx'
 import LoadingSpinner from '../components/general_components/loading_indicator.jsx'
+import StudentCreatesAccountSection from '../components/invite_users/add_students/StudentCreatesAccountSection.jsx'
+import TeacherCreatesAccountSection from '../components/invite_users/add_students/TeacherCreatesAccountSection.jsx'
 
 export default React.createClass({
 
@@ -62,10 +64,10 @@ export default React.createClass({
     const firstName = this.state.firstName;
     const lastName = this.state.lastName;
     if (firstName && lastName ) {
-        this.setState({disabled: true})
+        this.setState({disabled: true, loading: true})
         let that = this
         $.post(`/teachers/classrooms/${this.state.selectedClassroom.id}/students`, {user: {first_name: firstName, last_name: lastName}})
-        .success(this.setState({firstName: '', lastName: '', disabled: false}))
+        .success(this.setState({firstName: '', lastName: '', disabled: false, loading: false}))
     } else {
       this.setState({errors: 'Student requires a first and last name'})
     }
@@ -74,42 +76,15 @@ export default React.createClass({
 	render: function() {
 		return (
 			<div className="container invite-students">
-				<div className="row">
-					<div className="col-xs-12 col-sm-6 col-md-6 col-lg-5 col-xl-5 button-select-wrapper select-white-wrapper">
-						<div className="button-select">
-							<ClassroomDropdown classrooms={this.props.classrooms} callback={this.updateClassroom}/>
-						</div>
-					</div>
-				</div>
-				<h1 className="section-header">
-					Have Students Create Their Accounts
-				</h1>
-				<ol>
-					<li>
-						Have students sign up at quill.org/account/new
-					</li>
-					<li>
-						Once students sign up, in the "Join My Class" field,
-						<br/>they enter the class code
-						<strong>prize-bait</strong>
-					</li>
-				</ol>
-				<span className="class-code">Class Code</span><input className="inactive" disabled="" type="text" value={this.state.selectedClassroom.code}/>
-				<h1 className="section-header">
-					Or You Create Student Accounts
-				</h1>
-				<div>
-					The students’ usernames are their names combined with your<br/>class code. For example, John Smith is 'John.Smith@prize-bait'.<br/>
-					<div className="mt-15">
-						Students passwords are set to their last names by default.<br/>For example, ‘Smith’ (first letter is capitalized).
-					</div>
-          <div className="add-student-fields">
-            <input  placeholder='First Name' type="text" value={this.state.firstName} onChange={(e)=> this.nameChange(e, 'firstName')}/>
-            <input  placeholder='Last Name' type="text" value={this.state.lastName} onChange={(e)=> this.nameChange(e, 'lastName')}/>
-            <button disabled={this.state.disabled} className="button-green ajax-button" onClick={this.submitStudent}>Add Student</button>
-            <span className="errors">{this.state.errors}</span>
-          </div>
-				</div>
+				<ClassroomDropdown classrooms={this.props.classrooms} callback={this.updateClassroom}/>
+        <StudentCreatesAccountSection classCode={this.state.selectedClassroom.code} />
+        <TeacherCreatesAccountSection key="teacher-create-account"
+                              firstName={this.state.firstName}
+                              lastName={this.state.lastName}
+                              nameChange={this.nameChange}
+                              disabled={this.state.disabled}
+                              submitStudent= {this.submitStudent}
+                              errors= {this.state.errors}/>
 				{this.stateSpecificComponent()}
 			</div>
 		);
