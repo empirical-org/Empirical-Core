8bee17dfd3cba7aa020cff06e89ea9a15a0f5c29
RyanNovas <ryan@quill.org>
2016-09-09 14:40:55 -0400

    styling for landing page

--- client/app/bundles/HelloWorld/components/progress_reports/landing_page.jsx
+++ client/app/bundles/HelloWorld/components/progress_reports/landing_page.jsx
@@ -13,44 +13,48 @@ export default React.createClass({
 			}, {
 				title: 'Activity Analysis',
 				href: '/teachers/progress_reports/diagnostic_reports#/activity_packs',
-				img: '/images/visual_overview.svg',
+				img: '/images/activity_analysis.svg',
 				bodyText: 'See how students responded to each question and get a clear analysis of the skills they demonstrated.'
 			}, {
 				title: 'Diagnostic',
 				href: '/teachers/classrooms/scorebook',
-				img: '/images/visual_overview.svg',
+				img: '/images/diagnostic.svg',
 				bodyText: 'View the results of the diagnostic, and get a personalized learning plan with recommended activities.'
 			}, {
 				title: 'List Overview',
+				premium: true,
 				href: '/teachers/classrooms/scorebook',
-				img: '/images/visual_overview.svg',
+				img: '/images/list_overview.svg',
 				bodyText: 'Get the big picture of how your students are performing with the list view. Easily download the reports as a CSV.'
 			}, {
 				title: 'Concepts',
+				premium: true,
 				href: '/teachers/progress_reports/concepts/students',
-				img: '/images/visual_overview.svg',
+				img: '/images/concepts.svg',
 				bodyText: 'View an overall summary of how each of your students is performing on all of the  grammar concepts.'
 			}, {
 				title: 'Common Core Standards',
+				premium: true,
 				href: '/teachers/progress_reports/standards/classrooms',
-				img: '/images/visual_overview.svg',
+				img: '/images/common_core.svg',
 				bodyText: 'Following the Common Core? Check this view to see how your students are performing on specific standards.'
 			}
 		]
 	},
 
 	miniBuilder: function(mini) {
+		const premium = mini.premium ?  <h4 className='premium'>Premium<i className="fa fa-star" aria-hidden="true"></i></h4> : null
 		return (
 			<GenericMini key={mini.title}>
 				<a href={mini.href}>
 					<h3>{mini.title}</h3>
+					{premium}
 					<div className="img-wrapper">
 						<img src={mini.img}/>
 					</div>
 					<p>{mini.bodyText}</p>
 				</a>
 			</GenericMini>
-
 		)
 	},
 
