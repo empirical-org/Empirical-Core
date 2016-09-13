c22c7e4d75c5c59d1004bd5dab616f1798fd464f
RyanNovas <ryan@quill.org>
2016-09-13 12:30:34 -0400

    highlights diagnostic report nav buttons based off of current url

--- client/app/bundles/HelloWorld/components/progress_reports/diagnostic_reports/nav_button_group.jsx
+++ client/app/bundles/HelloWorld/components/progress_reports/diagnostic_reports/nav_button_group.jsx
@@ -17,8 +17,26 @@ export default React.createClass({
 	},
 
 	buttons: function() {
-		const contents = ['Students', 'Questions', 'Recommendations'];
-		return contents.map((buttonName)=>	<button key={buttonName} type="button" onClick={this.buttonBuilder(buttonName)} className="btn btn-secondary">{buttonName}</button>)
+		const contents = [
+			{name: 'Students', words: ['student_report', 'students']},
+			{name: 'Questions', words:['questions']},
+			{name: 'Recommendations', words: ['recommendations']}
+		];
+		let hash = window.location.hash
+		return contents.map((navButton)=>	{
+			let activeState;
+			let words = navButton.words;
+			let name = navButton.name
+			// if the url has button in it, we mark it as active
+			for (var i = 0; i < words.length; i++) {
+				if (hash.includes(words[i])) {
+					activeState = 'active-true'
+					break;
+				}
+			}
+			return <button key={name} type="button" onClick={this.buttonBuilder(name)} className={`btn btn-secondary ${activeState}`}>{name}</button>
+		})
+		// return buttons
 	},
 
 	render: function() {
