1b8c98afcccd3eceacf9ff3ed84b9be70a2c4c0d
Hannahmonk <hannah@quill.org>
2016-10-18 14:41:05 -0400

    Update constants.js

--- app/constants.js
+++ app/constants.js
@@ -90,13 +90,13 @@ export default {
 	  punctuationError: "There may be an error. How could you update the punctuation?",
 		punctuationAndCaseError: "There may be an error. How could you update the punctuation and capitalization?",
 	  typingError: "Try again. There may be a spelling mistake.",
-	  caseError: "Try again. There may be a capitalization error.",
-	  minLengthError: "Try again. Do you have all of the information from the prompt?",
-	  maxLengthError: "Try again. How could this sentence be shorter and more concise?",
-		modifiedWordError: "Try again. You may have mixed up a word?",
-		additionalWordError: "Try again. You may have added an unnecessary a word?",
-		missingWordError: "Try again. You may have forgotten a word?",
-		whitespaceError: "Try again. You may have forgotten a space between two words."
+	  caseError: "Proofread your work. There may be a capitalization error.",
+	  minLengthError: "Revise your work. Do you have all of the information from the prompt?",
+	  maxLengthError: "Revise your work. How could this sentence be shorter and more concise?",
+		modifiedWordError: "Revise your work. You may have mixed up a word.",
+		additionalWordError: "Revise your work. You may have added an extra word.",
+		missingWordError: "Revise your work. You may have left out an important word.",
+		whitespaceError: "There may be an error. You may have forgotten a space between two words."
 	},
 
 	ERROR_TYPES: ['typingError', 'caseError', 'punctuationError', 'punctuationAndCaseError', 'minLengthError', 'maxLengthError', "modifiedWordError", "additionalWordError", "missingWordError", "whitespaceError"],
