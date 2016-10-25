bf52d5fececa8dec58042029c822b8645a167a2f
Donald McKendrick <ddmckendrick@gmail.comredis-server>
2016-10-25 18:49:10 -0400

    Switch to a fade in for new feedback.

--- app/components/renderForQuestions/feedback.jsx
+++ app/components/renderForQuestions/feedback.jsx
@@ -10,24 +10,25 @@ export default React.createClass({
   renderFeedback: function () {
     const data = this.props
     const latestAttempt = getLatestAttempt(data.question.attempts)
+    const key = data.question.attempts.length;
     if (latestAttempt) {
       if (data.override) {
         return (
-          <div className="feedback-row student-feedback-inner-container" key={data.sentence}>
+          <div className="feedback-row student-feedback-inner-container" key={key}>
             <img className="multiple" src={multiple}/>
             <p>{data.sentence}</p>
           </div>
         )
       } else if (latestAttempt.found && latestAttempt.response.feedback !== undefined) {
         return (
-          <div className="feedback-row student-feedback-inner-container" key={latestAttempt.response.feedback}>
+          <div className="feedback-row student-feedback-inner-container" key={key}>
             <img className="revise" src={revise}/>
             {data.renderFeedbackStatements(latestAttempt)}
           </div>
         )
       } else {
         return (
-          <div className="feedback-row student-feedback-inner-container" key={data.sentence}>
+          <div className="feedback-row student-feedback-inner-container" key={key}>
             <img className="revise" src={revise}/>
             <p>{data.sentence}</p>
           </div>
@@ -36,7 +37,7 @@ export default React.createClass({
     } else {
       if(!!data.question.instructions) {
         return (
-          <div className="feedback-row student-feedback-inner-container" key={data.question.instructions}>
+          <div className="feedback-row student-feedback-inner-container" key={key}>
             <img className="info" src={icon}/>
             <p>{data.question.instructions}</p>
           </div>
@@ -44,7 +45,7 @@ export default React.createClass({
       }
       else if(data.getQuestion && data.getQuestion().instructions!=="") {
         return (
-          <div className="feedback-row student-feedback-inner-container" key={data.getQuestion().instructions}>
+          <div className="feedback-row student-feedback-inner-container" key={key}>
             <img className="info" src={icon}/>
             <p>{data.getQuestion().instructions}</p>
           </div>
@@ -53,14 +54,14 @@ export default React.createClass({
       else if (data.getQuestion && data.getQuestion().cues && data.getQuestion().cues.length > 0 && data.getQuestion().cues[0] !== "") {
         const cues = data.getQuestion().cues.join(', ')
         return (
-          <div className="feedback-row student-feedback-inner-container" key={cues}>
+          <div className="feedback-row student-feedback-inner-container" key={key}>
             <img className="info" src={icon}/>
             <p>Combine the sentences using {data.listCuesAsString(data.getQuestion().cues)}</p>
           </div>
         )
       } else {
         return (
-          <div className="feedback-row student-feedback-inner-container" key="default">
+          <div className="feedback-row student-feedback-inner-container" key={key}>
             <img className="info" src={icon}/>
             <p>Combine the sentences into one sentence.</p>
           </div>
@@ -75,8 +76,7 @@ export default React.createClass({
       <div className="student-feedback-container">
       <ReactCSSTransitionGroup
         transitionName="feedback-carousel"
-        transitionEnterTimeout={350}
-        transitionLeaveTimeout={350}
+        transitionEnterTimeout={500}
         >
         {this.renderFeedback()}
       </ReactCSSTransitionGroup>
