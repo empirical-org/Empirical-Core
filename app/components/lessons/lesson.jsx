e2bfb9e30f36c2f4cce26f7b4d61f8f5b4d7f2b9
RyanNovas <ryan@quill.org>
2016-10-28 15:14:47 -0400

    instead of converting an obj to an array, then finding keys, we just use keys....

--- app/components/lessons/lesson.jsx
+++ app/components/lessons/lesson.jsx
@@ -12,25 +12,29 @@ import EditLessonForm from './lessonForm.jsx'
 const Lesson = React.createClass({
   questionsForLesson: function () {
     var questionsCollection = hashToCollection(this.props.questions.data);
-    var sentenceFragmentsCollection = hashToCollection(this.props.sentenceFragments.data);
+    const questionsObj = this.props.questions;
+    const sentenceFragmentsObj = this.props.sentenceFragments;
     const {data} = this.props.lessons, {lessonID} = this.props.params;
     if(data[lessonID].questions) {
       return data[lessonID].questions.map((question) => {
-        // TODO: this is rediculous -- ultimately we need to refactor to include the question type on the object at the point of creation
-        const collection = question.questionType === 'sentenceFragments' ? sentenceFragmentsCollection : questionsCollection
-        let qFromDB = _.find(collection, {key: question.key})
+        // TODO: this is rediculous -- ultimately we need to refactor to include the question type on the object at the point of creation,
+        // ensure that both have keys, and create a displayable text field....
+        const collection = question.questionType === 'sentenceFragments' ? sentenceFragmentsObj.data : questionsObj.data
+        let qFromDB = Object.assign({}, collection[question.key]);
         qFromDB.questionType = question.questionType;
+        qFromDB.key = question.key
         return qFromDB;
       })
     }
   },
+
 
   renderQuestionsForLesson: function () {
     var questionsForLesson = this.questionsForLesson()
     if(questionsForLesson) {
       var listItems = questionsForLesson.map((question) => {
         const nameKey = question.questionType === 'questions' ? 'prompt' : 'questionText'
-        return (<li key={question.key}><Link to={'/results/questions/' + question.key}>{question[nameKey].replace(/(<([^>]+)>)/ig, "").replace(/&nbsp;/ig, "")}</Link></li>)
+        return (<li key={question.key}><Link to={`/results/${question.questionType}/` + question.key}>{question[nameKey].replace(/(<([^>]+)>)/ig, "").replace(/&nbsp;/ig, "")}</Link></li>)
       })
       return (
         <ul>{listItems}</ul>
