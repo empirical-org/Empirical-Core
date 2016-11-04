a58547a3a7505cbaaba9c2159a6a80d2bfb473d9
RyanNovas <ryan@quill.org>
2016-11-04 16:31:30 -0700

    adds styling

--- client/app/bundles/HelloWorld/containers/AccountSettings.jsx
+++ client/app/bundles/HelloWorld/containers/AccountSettings.jsx
@@ -4,7 +4,7 @@ export default React.createClass({
 
   render: function () {
     return (
-      <div className="container">
+      <div className="container user-select-role">
         <UserAccessibleSelectRole {...this.props}/>
       </div>
     );
