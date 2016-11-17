a1ffdaa78c07080fe9cebd478c2bdb51f0155855
Donald McKendrick <ddmckendrick@gmail.comredis-server>
2016-11-17 17:32:52 -0500

    Rename filter actions

--- app/components/questions/responseComponent.jsx
+++ app/components/questions/responseComponent.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import actions from '../../actions/responses'
+import actions from '../../actions/filters'
 import _ from 'underscore'
 import {hashToCollection} from '../../libs/hashToCollection'
 import ResponseList from './responseList.jsx'
