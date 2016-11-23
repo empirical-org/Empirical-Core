e40fc478e24462d7a55362dabedbce8b67690710
Jared Silver <jared@jaredsilver.name>
2016-11-23 15:50:37 -0500

    Added test to ensure teachers cannot access student login information that does not belong to them

--- spec/controllers/teachers/classrooms_controller_spec.rb
+++ spec/controllers/teachers/classrooms_controller_spec.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
+require 'rails_helper'
 
-describe Teachers::ClassroomsController, type: :controller do 
+describe Teachers::ClassroomsController, type: :controller do
   describe 'creating a classroom' do
     render_views
     let(:teacher) { FactoryGirl.create(:teacher) }
@@ -21,4 +22,24 @@ describe Teachers::ClassroomsController, type: :controller do
       expect(response.status).to eq(200)
     end
   end
+
+  describe 'creating a login pdf' do
+
+    let(:teacher) { FactoryGirl.create(:teacher) }
+    let(:different_teacher) { FactoryGirl.create(:teacher) }
+    let(:different_classroom) { FactoryGirl.create(:classroom, teacher: different_teacher) }
+
+    before do
+      session[:user_id] = teacher.id
+    end
+
+    it 'does not allow teacher unauthorized access to other PDFs' do
+      get :generate_login_pdf, id: different_classroom.id
+      # expected result is a redirect away from the download page
+      # because the teacher in question should not be able to access
+      # student login information of a class that is not theirs
+      expect(response.status).to eq(303)
+    end
+  end
+
 end
