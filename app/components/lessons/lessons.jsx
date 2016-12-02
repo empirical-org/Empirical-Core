1aff72b0a2e2c021714089faa6b15f3ce00c1e0e
Donald McKendrick <ddmckendrick@gmail.comredis-server>
2016-12-02 11:51:11 -0500

    Better display of bad data

--- app/components/lessons/lessons.jsx
+++ app/components/lessons/lessons.jsx
@@ -1,56 +1,52 @@
-import React from 'react'
-import { connect } from 'react-redux'
-import actions from '../../actions/lessons'
-import _ from 'underscore'
-import { Link } from 'react-router'
-import Modal from '../modal/modal.jsx'
-import {hashToCollection} from '../../libs/hashToCollection'
-import EditLessonForm from './lessonForm.jsx'
+import React from 'react';
+import { connect } from 'react-redux';
+import actions from '../../actions/lessons';
+import _ from 'underscore';
+import { Link } from 'react-router';
+import Modal from '../modal/modal.jsx';
+import { hashToCollection } from '../../libs/hashToCollection';
+import EditLessonForm from './lessonForm.jsx';
 
-const Lessons =  React.createClass({
+const Lessons = React.createClass({
 
-  getInitialState: function() {
-    return {lessonFlags: "All Flags"}
+  getInitialState() {
+    return { lessonFlags: 'All Flags', };
   },
 
-  createNew: function () {
-    this.props.dispatch(actions.toggleNewLessonModal())
+  createNew() {
+    this.props.dispatch(actions.toggleNewLessonModal());
   },
 
-  submitNewLesson: function (data) {
-    this.props.dispatch(actions.submitNewLesson(data))
+  submitNewLesson(data) {
+    this.props.dispatch(actions.submitNewLesson(data));
     // this.props.dispatch(actions.toggleNewLessonModal())
   },
 
-  renderLessons: function () {
-    const {data} = this.props.lessons;
+  renderLessons() {
+    const { data, } = this.props.lessons;
     let keys = _.keys(data);
-    if(this.state.lessonFlags !== "All Flags") {
-      keys = _.filter(keys, (key) => {
-        return data[key].flag === this.state.lessonFlags
-      })
+    if (this.state.lessonFlags !== 'All Flags') {
+      keys = _.filter(keys, key => data[key].flag === this.state.lessonFlags);
     }
-    return keys.map((key) => {
-      return (<li key={key}><Link to={'/admin/lessons/' + key} activeClassName="is-active">{data[key].name}</Link></li>)
-    })
+    return keys.map(key => (<li key={key}><Link to={`/admin/lessons/${key}`} activeClassName="is-active">{data[key].name || 'No name'}</Link></li>));
   },
 
-  renderModal: function () {
-    var stateSpecificClass = this.props.lessons.submittingnew ? 'is-loading' : '';
+  renderModal() {
+    const stateSpecificClass = this.props.lessons.submittingnew ? 'is-loading' : '';
     if (this.props.lessons.newLessonModalOpen) {
-        return (
-          <Modal close={this.createNew}>
-            <EditLessonForm submit={this.submitNewLesson} stateSpecificClass={stateSpecificClass}/>
-          </Modal>
-        )
-      }
+      return (
+        <Modal close={this.createNew}>
+          <EditLessonForm submit={this.submitNewLesson} stateSpecificClass={stateSpecificClass} />
+        </Modal>
+      );
+    }
   },
 
-  handleSelect: function(e) {
-    this.setState({lessonFlags: e.target.value})
+  handleSelect(e) {
+    this.setState({ lessonFlags: e.target.value, });
   },
 
-  render: function() {
+  render() {
     return (
       <section className="section">
         <div className="container">
@@ -80,18 +76,17 @@ const Lessons =  React.createClass({
         </div>
       </section>
 
-    )
-  }
+    );
+  },
 
-})
-
+});
 
 function select(state) {
   return {
     lessons: state.lessons,
     routing: state.routing,
-    questions: state.questions
-  }
+    questions: state.questions,
+  };
 }
 
-export default connect(select)(Lessons)
+export default connect(select)(Lessons);
