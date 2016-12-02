58d614cd2cf14dc12572cf45b4e34ef1d1117ca5
Donald McKendrick <ddmckendrick@gmail.comredis-server>
2016-12-02 16:12:32 -0500

    Check for inane errors in diagnostic logic.

--- app/libs/diagnosticQuestion.js
+++ app/libs/diagnosticQuestion.js
@@ -1,70 +1,62 @@
 import _ from 'underscore';
-import fuzzy from 'fuzzyset.js'
+import fuzzy from 'fuzzyset.js';
 import constants from '../constants';
-import {diffWords} from 'diff';
+import { diffWords } from 'diff';
 import {
   checkChangeObjectMatch
-} from './algorithms/changeObjects'
+} from './algorithms/changeObjects';
 const jsDiff = require('diff');
 
 const ERROR_TYPES = {
   NO_ERROR: 'NO_ERROR',
-  MISSING_WORD: "MISSING_WORD",
-  ADDITIONAL_WORD: "ADDITIONAL_WORD",
-  INCORRECT_WORD: "INCORRECT_WORD"
-}
+  MISSING_WORD: 'MISSING_WORD',
+  ADDITIONAL_WORD: 'ADDITIONAL_WORD',
+  INCORRECT_WORD: 'INCORRECT_WORD',
+};
 
-String.prototype.normalize = function() {
+String.prototype.normalize = function () {
   return this.replace(/[\u201C\u201D]/g, '\u0022').replace(/[\u00B4\u0060\u2018\u2019]/g, '\u0027').replace('‚', ',');
-}
+};
 
 export default class Question {
   constructor(data) {
     this.prompt = data.prompt;
     this.sentences = data.sentences;
     this.responses = data.responses;
-    this.questionUID = data.questionUID
+    this.questionUID = data.questionUID;
     this.focusPoints = data.focusPoints || [];
   }
 
   getOptimalResponses() {
-    return _.where(this.responses, {optimal: true})
+    return _.where(this.responses, { optimal: true, });
   }
 
   getSubOptimalResponses(responses) {
-    return _.filter(this.responses, function (resp){
-      return resp.parentID === undefined && resp.feedback !== undefined && resp.optimal !== true
-    })
+    return _.filter(this.responses, resp => resp.parentID === undefined && resp.feedback !== undefined && resp.optimal !== true);
   }
 
   getTopOptimalResponse() {
-    return _.sortBy(this.getOptimalResponses(), (r) => {
-      return r.count
-    }).reverse()[0]
+    return _.sortBy(this.getOptimalResponses(), r => r.count).reverse()[0];
   }
 
   getWeakResponses() {
-    return _.filter(this.responses, function (resp) {
-      return resp.weak === true
-    })
+    return _.filter(this.responses, resp => resp.weak === true);
   }
 
   getCommonUnmatchedResponses() {
-    return _.filter(this.responses, function (resp) {
-      return resp.feedback === undefined && resp.count > 2
-    })
+    return _.filter(this.responses, resp => resp.feedback === undefined && resp.count > 2);
   }
 
   getSumOfWeakAndCommonUnmatchedResponses() {
-    return this.getWeakResponses().length + this.getCommonUnmatchedResponses().length
+    return this.getWeakResponses().length + this.getCommonUnmatchedResponses().length;
   }
 
   getPercentageWeakResponses() {
-    return (this.getSumOfWeakAndCommonUnmatchedResponses() / this.responses.length * 100).toPrecision(4)
+    return (this.getSumOfWeakAndCommonUnmatchedResponses() / this.responses.length * 100).toPrecision(4);
   }
 
   checkMatch(response) {
-    console.log("Checking Diagnostic Match")
+    console.log('Checking Diagnostic Match');
     // remove leading and trailing whitespace
     response = response.trim();
     // make sure all words are single spaced
@@ -75,208 +67,187 @@ export default class Question {
       response: {
         text: response,
         questionUID: this.questionUID,
-        count: 1
-      }
-    }
-    let res = returnValue.response;
-    var exactMatch = this.checkExactMatch(response)
+        count: 1,
+      },
+    };
+    const res = returnValue.response;
+    const exactMatch = this.checkExactMatch(response);
     if (exactMatch !== undefined) {
-      returnValue.response = exactMatch.key
-      return returnValue
+      returnValue.response = exactMatch;
+      return returnValue;
     }
-    var lowerCaseMatch = this.checkCaseInsensitiveMatch(response)
+    const lowerCaseMatch = this.checkCaseInsensitiveMatch(response);
     if (lowerCaseMatch !== undefined) {
       res.feedback = constants.FEEDBACK_STRINGS.caseError;
-      res.author = "Capitalization Hint"
-      res.parentID = lowerCaseMatch.key
-      this.copyParentResponses(res, lowerCaseMatch)
-      return returnValue
+      res.author = 'Capitalization Hint';
+      res.parentID = lowerCaseMatch.key;
+      this.copyParentResponses(res, lowerCaseMatch);
+      return returnValue;
     }
-    var punctuationMatch = this.checkPunctuationInsensitiveMatch(response)
+    const punctuationMatch = this.checkPunctuationInsensitiveMatch(response);
     if (punctuationMatch !== undefined) {
       res.feedback = constants.FEEDBACK_STRINGS.punctuationError;
-      res.author = "Punctuation Hint"
-      res.parentID = punctuationMatch.key
-      this.copyParentResponses(res, punctuationMatch)
-      return returnValue
+      res.author = 'Punctuation Hint';
+      res.parentID = punctuationMatch.key;
+      this.copyParentResponses(res, punctuationMatch);
+      return returnValue;
     }
-    var punctuationAndCaseMatch = this.checkPunctuationAndCaseInsensitiveMatch(response)
+    const punctuationAndCaseMatch = this.checkPunctuationAndCaseInsensitiveMatch(response);
     if (punctuationAndCaseMatch !== undefined) {
       res.feedback = constants.FEEDBACK_STRINGS.punctuationAndCaseError;
-      res.author = "Punctuation and Case Hint"
-      res.parentID = punctuationAndCaseMatch.key
-      this.copyParentResponses(res, punctuationAndCaseMatch)
-      return returnValue
+      res.author = 'Punctuation and Case Hint';
+      res.parentID = punctuationAndCaseMatch.key;
+      this.copyParentResponses(res, punctuationAndCaseMatch);
+      return returnValue;
     }
-    var changeObjectMatch = this.checkChangeObjectRigidMatch(response)
+    var changeObjectMatch = this.checkChangeObjectRigidMatch(response);
     if (changeObjectMatch !== undefined) {
       switch (changeObjectMatch.errorType) {
         case ERROR_TYPES.INCORRECT_WORD:
           res.feedback = constants.FEEDBACK_STRINGS.modifiedWordError;
-          res.author = "Modified Word Hint"
-          res.parentID = changeObjectMatch.response.key
-          this.copyParentResponses(res, changeObjectMatch.response)
-          return returnValue
+          res.author = 'Modified Word Hint';
+          res.parentID = changeObjectMatch.response.key;
+          this.copyParentResponses(res, changeObjectMatch.response);
+          return returnValue;
         case ERROR_TYPES.ADDITIONAL_WORD:
           res.feedback = constants.FEEDBACK_STRINGS.additionalWordError;
-          res.author = "Additional Word Hint"
-          res.parentID = changeObjectMatch.response.key
-          this.copyParentResponses(res, changeObjectMatch.response)
-          return returnValue
+          res.author = 'Additional Word Hint';
+          res.parentID = changeObjectMatch.response.key;
+          this.copyParentResponses(res, changeObjectMatch.response);
+          return returnValue;
         case ERROR_TYPES.MISSING_WORD:
           res.feedback = constants.FEEDBACK_STRINGS.missingWordError;
-          res.author = "Missing Word Hint"
-          res.parentID = changeObjectMatch.response.key
-          this.copyParentResponses(res, changeObjectMatch.response)
-          return returnValue
+          res.author = 'Missing Word Hint';
+          res.parentID = changeObjectMatch.response.key;
+          this.copyParentResponses(res, changeObjectMatch.response);
+          return returnValue;
         default:
-          return
+          return;
       }
     }
-    var changeObjectMatch = this.checkChangeObjectSubMatch(response)
+    var changeObjectMatch = this.checkChangeObjectSubMatch(response);
     if (changeObjectMatch !== undefined) {
       switch (changeObjectMatch.errorType) {
         case ERROR_TYPES.INCORRECT_WORD:
           res.feedback = constants.FEEDBACK_STRINGS.modifiedWordError;
-          res.author = "Modified Word Hint"
-          res.parentID = changeObjectMatch.response.key
-          this.copyParentResponses(res, changeObjectMatch.response)
-          return returnValue
+          res.author = 'Modified Word Hint';
+          res.parentID = changeObjectMatch.response.key;
+          this.copyParentResponses(res, changeObjectMatch.response);
+          return returnValue;
         case ERROR_TYPES.ADDITIONAL_WORD:
           res.feedback = constants.FEEDBACK_STRINGS.additionalWordError;
-          res.author = "Additional Word Hint"
-          res.parentID = changeObjectMatch.response.key
-          this.copyParentResponses(res, changeObjectMatch.response)
-          return returnValue
+          res.author = 'Additional Word Hint';
+          res.parentID = changeObjectMatch.response.key;
+          this.copyParentResponses(res, changeObjectMatch.response);
+          return returnValue;
         case ERROR_TYPES.MISSING_WORD:
           res.feedback = constants.FEEDBACK_STRINGS.missingWordError;
-          res.author = "Missing Word Hint"
-          res.parentID = changeObjectMatch.response.key
-          this.copyParentResponses(res, changeObjectMatch.response)
-          return returnValue
+          res.author = 'Missing Word Hint';
+          res.parentID = changeObjectMatch.response.key;
+          this.copyParentResponses(res, changeObjectMatch.response);
+          return returnValue;
         default:
-          return
+          return;
       }
     }
-    var whitespaceMatch = this.checkWhiteSpaceMatch(response)
+    const whitespaceMatch = this.checkWhiteSpaceMatch(response);
     if (whitespaceMatch !== undefined) {
       res.feedback = constants.FEEDBACK_STRINGS.whitespaceError;
-      res.author = "Whitespace Hint"
-      res.parentID = whitespaceMatch.key
-      this.copyParentResponses(res, whitespaceMatch)
-      return returnValue
+      res.author = 'Whitespace Hint';
+      res.parentID = whitespaceMatch.key;
+      this.copyParentResponses(res, whitespaceMatch);
+      return returnValue;
     }
-    returnValue.found = false
-    return returnValue
+    returnValue.found = false;
+    return returnValue;
   }
 
   nonChildResponses(responses) {
-    return _.filter(this.responses, function (resp){
-      return resp.parentID === undefined && resp.feedback !== undefined
-    })
+    return _.filter(this.responses, resp => resp.parentID === undefined && resp.feedback !== undefined);
   }
 
   checkExactMatch(response) {
-    return _.find(this.responses, (resp) => {
-      return resp.text.normalize() === response.normalize();
-    });
+    return _.find(this.responses, resp => resp.text.normalize() === response.normalize());
   }
 
   checkCaseInsensitiveMatch(response) {
-    return _.find(this.getOptimalResponses(), (resp) => {
-      return resp.text.normalize().toLowerCase() === response.normalize().toLowerCase();
-    });
+    return _.find(this.getOptimalResponses(), resp => resp.text.normalize().toLowerCase() === response.normalize().toLowerCase());
   }
 
   checkPunctuationInsensitiveMatch(response) {
-    return _.find(this.getOptimalResponses(), (resp) => {
-      return removePunctuation(resp.text.normalize()) === removePunctuation(response.normalize())
-    });
+    return _.find(this.getOptimalResponses(), resp => removePunctuation(resp.text.normalize()) === removePunctuation(response.normalize()));
   }
 
   checkPunctuationAndCaseInsensitiveMatch(response) {
     return _.find(this.getOptimalResponses(), (resp) => {
-      const supplied = removeCaseSpacePunc(response)
-      const target = removeCaseSpacePunc(resp.text)
-      return supplied === target
+      const supplied = removeCaseSpacePunc(response);
+      const target = removeCaseSpacePunc(resp.text);
+      return supplied === target;
     });
   }
 
   checkWhiteSpaceMatch(response) {
-    return _.find(this.getOptimalResponses(), (resp) => {
-      return removeSpaces(response.normalize()) === removeSpaces(resp.text.normalize())
-    })
+    return _.find(this.getOptimalResponses(), resp => removeSpaces(response.normalize()) === removeSpaces(resp.text.normalize()));
   }
 
   checkChangeObjectRigidMatch(response) {
-    const fn = (string) => {
-      return string.normalize()
-    }
-    return checkChangeObjectMatch(response, this.getOptimalResponses(), fn)
+    const fn = string => string.normalize();
+    return checkChangeObjectMatch(response, this.getOptimalResponses(), fn);
   }
 
   checkChangeObjectSubMatch(response) {
-    const fn = (string) => {
-      return string.normalize()
-    }
-    return checkChangeObjectMatch(response, this.getSubOptimalResponses(), fn)
+    const fn = string => string.normalize();
+    return checkChangeObjectMatch(response, this.getSubOptimalResponses(), fn);
   }
 
   checkFuzzyMatch(response) {
-    const set = fuzzy(_.map(_.pluck(this.responses, "text"), (resp) => {
-      return removeCaseSpacePunc(resp)
-    }));
+    const set = fuzzy(_.map(_.pluck(this.responses, 'text'), resp => removeCaseSpacePunc(resp)));
     const matches = set.get(removeCaseSpacePunc(response), []);
-    var foundResponse = undefined;
-    var text = undefined;
+    let foundResponse;
+    let text;
     if (matches.length > 0) {
-      var threshold = (matches[0][1].length - 1) / matches[0][1].length
+      const threshold = (matches[0][1].length - 1) / matches[0][1].length;
       text = (matches[0][0] >= threshold) ? matches[0][1] : null;
     }
     if (text) {
-      foundResponse = _.findWhere(this.responses, (resp) => {
-        return removeCaseSpacePunc(resp.text) === text })
+      foundResponse = _.findWhere(this.responses, resp => removeCaseSpacePunc(resp.text) === text);
     }
-    return foundResponse
+    return foundResponse;
   }
 
   copyParentResponses(newResponse, parentResponse) {
     if (parentResponse.conceptResults) {
-      newResponse.conceptResults = Object.assign({}, {}, parentResponse.conceptResults)
+      newResponse.conceptResults = Object.assign({}, {}, parentResponse.conceptResults);
     }
   }
 
 }
 
-const removePunctuation = (string) => {
-  return string.replace(/\./g,"")
-}
+const removePunctuation = string => string.replace(/\./g, '');
 
-const removeSpaces = (string) => {
-  return string.replace(/\s+/g, '');
-}
+const removeSpaces = string => string.replace(/\s+/g, '');
 
 const removeCaseSpacePunc = (string) => {
   let transform = string.normalize();
-  transform = removePunctuation(transform)
-  transform = removeSpaces(transform)
-  return transform.toLowerCase()
-}
+  transform = removePunctuation(transform);
+  transform = removeSpaces(transform);
+  return transform.toLowerCase();
+};
 
 // Check number of chars added.
 
 const getErrorType = (targetString, userString) => {
-  const changeObjects = getChangeObjects(targetString, userString)
-  const hasIncorrect = checkForIncorrect(changeObjects)
-  const hasAdditions = checkForAdditions(changeObjects)
-  const hasDeletions = checkForDeletions(changeObjects)
+  const changeObjects = getChangeObjects(targetString, userString);
+  const hasIncorrect = checkForIncorrect(changeObjects);
+  const hasAdditions = checkForAdditions(changeObjects);
+  const hasDeletions = checkForDeletions(changeObjects);
   if (hasIncorrect) {
-    return ERROR_TYPES.INCORRECT_WORD
+    return ERROR_TYPES.INCORRECT_WORD;
   } else if (hasAdditions) {
-    return ERROR_TYPES.ADDITIONAL_WORD
+    return ERROR_TYPES.ADDITIONAL_WORD;
   } else if (hasDeletions) {
-    return ERROR_TYPES.MISSING_WORD
+    return ERROR_TYPES.MISSING_WORD;
   } else {
-    return
+
   }
-}
+};
