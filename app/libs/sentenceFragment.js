30857a2d62c6fde877f7114d248ee06da20df548
RyanNovas <ryan@quill.org>
2016-12-06 12:22:38 -0500

    updates constants, finds exact match across all fragments, not just graded

--- app/libs/sentenceFragment.js
+++ app/libs/sentenceFragment.js
@@ -1,6 +1,7 @@
 import _ from 'underscore';
 import * as qpos from './partsOfSpeechTagging';
 import validEndingPunctuation from '../libs/validEndingPunctuation.js';
+import constants from '../constants';
 
 String.prototype.normalize = function () {
   return this.replace(/[\u201C\u201D]/g, '\u0022').replace(/[\u00B4\u0060\u2018\u2019]/g, '\u0027').replace('‚', ',');
@@ -91,7 +92,7 @@ export default class POSMatcher {
   }
 
   checkExactMatch(userSubmission) {
-    return _.find(this.getGradedResponses(), response => response.text === userSubmission);
+    return _.find(this.responses, response => response.text === userSubmission);
   }
 
   checkLengthMatch(userSubmission) {
