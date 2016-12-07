531a4febc7bd467c6d6be034e2be4167274843bf
Donald McKendrick <ddmckendrick@gmail.comredis-server>
2016-12-07 15:18:01 -0500

    Lint the concept results file.

--- app/libs/conceptResults/diagnostic.js
+++ app/libs/conceptResults/diagnostic.js
@@ -1,35 +1,31 @@
 import {
   getAllSentenceFragmentConceptResults
-} from './sentenceFragment'
+} from './sentenceFragment';
 import {
   getConceptResultsForSentenceCombining
-} from './sentenceCombining'
+} from './sentenceCombining';
 
-export function getConceptResultsForQuestion (question) {
-  if (question.type === "SF") {
-    return getAllSentenceFragmentConceptResults(question.data)
-  } else if (question.type === "SC") {
-    return getConceptResultsForSentenceCombining(question.data)
+export function getConceptResultsForQuestion(question) {
+  if (question.type === 'SF') {
+    return getAllSentenceFragmentConceptResults(question.data);
+  } else if (question.type === 'SC') {
+    return getConceptResultsForSentenceCombining(question.data);
   }
 }
 
-export function getNestedConceptResultsForAllQuestions (questions) {
-  return questions.map((question) => {
-    return getConceptResultsForQuestion(question)
-  })
+export function getNestedConceptResultsForAllQuestions(questions) {
+  return questions.map(question => getConceptResultsForQuestion(question));
 }
 
-export function embedQuestionNumbers (nestedConceptResultArray) {
-  return nestedConceptResultArray.map((conceptResultArray, index)=> {
-    return conceptResultArray.map((conceptResult) => {
-      conceptResult.metadata.questionNumber = index + 1
-      return conceptResult
-    })
-  })
+export function embedQuestionNumbers(nestedConceptResultArray) {
+  return nestedConceptResultArray.map((conceptResultArray, index) => conceptResultArray.map((conceptResult) => {
+    conceptResult.metadata.questionNumber = index + 1;
+    return conceptResult;
+  }));
 }
 
-export function getConceptResultsForAllQuestions (questions) {
-  const nested = getNestedConceptResultsForAllQuestions(questions)
-  const withKeys = embedQuestionNumbers(nested)
-  return [].concat.apply([], withKeys)
+export function getConceptResultsForAllQuestions(questions) {
+  const nested = getNestedConceptResultsForAllQuestions(questions);
+  const withKeys = embedQuestionNumbers(nested);
+  return [].concat.apply([], withKeys);
 }
