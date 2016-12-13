655e51bc61d42ed8cbefb4ad9174bc5c2de6d624
RyanNovas <ryan@quill.org>
2016-12-13 12:02:21 -0500

    custom message in tooltip for diagnostic

--- client/app/bundles/HelloWorld/components/general_components/tooltip/total_score.jsx
+++ client/app/bundles/HelloWorld/components/general_components/tooltip/total_score.jsx
@@ -2,13 +2,21 @@ import React from 'react'
 
 export default React.createClass({
   propTypes: {
-    percentage: React.PropTypes.string.isRequired
+    percentage: React.PropTypes.string,
+    diagnostic: React.PropTypes.bool
   },
 
+
   render: function () {
+    let message;
+    if (this.props.diagnostic) {
+      message = <span>100% Complete</span>
+    } else {
+      message = <span>Total Score: <span className='percentage'>{this.props.percentage}</span></span>
+    }
     return (
       <div className='total-score'>
-        Total Score: <span className='percentage'>{this.props.percentage}</span>
+        {message}
       </div>
     );
   }
