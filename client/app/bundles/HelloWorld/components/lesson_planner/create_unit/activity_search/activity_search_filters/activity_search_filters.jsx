b870473f1db2906f480cf7577ab7c2d425edda0f
RyanNovas <ryan@quill.org>
2016-12-14 11:26:23 -0500

    removed outdated bootstrap columns

--- client/app/bundles/HelloWorld/components/lesson_planner/create_unit/activity_search/activity_search_filters/activity_search_filters.jsx
+++ client/app/bundles/HelloWorld/components/lesson_planner/create_unit/activity_search/activity_search_filters/activity_search_filters.jsx
@@ -32,7 +32,7 @@
 
 		return (
 			<div className="row activity-page-dropdown-wrapper">
-				<div className="col-xs-12 col-sm-12 col-md-9 col-lg-9 col-xl-9">
+				<div className="">
 					{filters}
           {/*{clearButton}*/}
 				</div>
