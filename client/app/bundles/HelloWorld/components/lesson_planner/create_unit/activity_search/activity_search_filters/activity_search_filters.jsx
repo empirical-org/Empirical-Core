f81b5136f57e93602db59222033251ef4849b353
RyanNovas <ryan@quill.org>
2016-12-14 17:40:39 -0500

    activity search works, still needs additional styling

--- client/app/bundles/HelloWorld/components/lesson_planner/create_unit/activity_search/activity_search_filters/activity_search_filters.jsx
+++ client/app/bundles/HelloWorld/components/lesson_planner/create_unit/activity_search/activity_search_filters/activity_search_filters.jsx
@@ -1,42 +1,37 @@
 'use strict'
 
- import React from 'react'
- import ActivitySearchFilter from './activity_search_filter'
- import _ from 'underscore'
+import React from 'react'
+import ActivitySearchFilter from './activity_search_filter'
+import _ from 'underscore'
+import SearchActivitiesInput from '../search_activities_input'
 
- export default  React.createClass({
+export default React.createClass({
+
+    render: function() {
+        let dropDowns = []
+        let appFilter;
+        this.props.data.forEach((filter, index) => {
+          if (index < 2) {
+            dropDowns.push(<ActivitySearchFilter key={filter.alias} selectFilterOption={this.props.selectFilterOption} data={filter}/>)
+          } else {
+            appFilter = <ActivitySearchFilter key={filter.alias} selectFilterOption={this.props.selectFilterOption} data={filter}/>
+          }
+        });
+        const clearAll = <button key='clear-all' type='button' className="clear-filter-btn btn-danger btn" onClick={this.props.clearFilters}>Clear Filters</button>
+        const dropDownFilters = <div className='activity-filter-drop-downs' key='activity-filter-drop-downs'>
+          {[dropDowns, clearAll]}
+        </div>
 
 
 
-	render: function () {
-
-    let clearButton;
-    if (_.any(this.props.data, (el)=>el.selected !== null)) {
-        clearButton = <div className='clear-filters' onClick={this.props.clearFilters}><span><img src="/images/x.svg"/>Clear All Filters</span></div>;
+        let dropDownFiltersAndSearch = [<SearchActivitiesInput key='activity-search' updateSearchQuery={this.updateSearchQuery} />].concat(dropDownFilters);
+        return (
+            <div className="row activity-page-dropdown-wrapper">
+                <div className='drop-down-filters-and-search'>
+                  {dropDownFiltersAndSearch}
+                </div>
+                {appFilter}
+            </div>
+        );
     }
-
-
-    var filters = this.props.data.map((filter, index)=> {
-      if (index === 0) {
-      	return (<ActivitySearchFilter key={filter.alias} selectFilterOption = {this.props.selectFilterOption} data={filter}>
-          {clearButton}
-        </ActivitySearchFilter>);
-      } else {
-        return <ActivitySearchFilter key={filter.alias} selectFilterOption = {this.props.selectFilterOption} data={filter}/>;
-      }
-    });
-
-    filters.reverse();
-
-
-
-		return (
-			<div className="row activity-page-dropdown-wrapper">
-				<div className="">
-					{filters}
-          {/*{clearButton}*/}
-				</div>
-			</div>
-		);
-	}
 });
