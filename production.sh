edd872f734abf3220c187f3c6f64e3cd374e9f25
Donald McKendrick <ddmckendrick@gmail.comredis-server>
2016-12-15 18:43:51 -0500

    Update scripts

--- production.sh
+++ production.sh
@@ -1,3 +1,3 @@
-NODE_ENV=production EMPIRICAL_BASE_URL=https://www.quill.org webpack --optimize-minimize
+rm -r dist/; NODE_ENV=production EMPIRICAL_BASE_URL=https://www.quill.org webpack --optimize-minimize
 firebase deploy --project production
 ./rollbar.sh
