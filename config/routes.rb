744585487d46bbd2ddbac3603a2a4f5bc0c48343
RyanNovas <ryan@quill.org>
2016-12-27 16:42:48 -0500

    mobile version of diagnostic features

--- config/routes.rb
+++ config/routes.rb
@@ -254,7 +254,7 @@ EmpiricalGrammar::Application.routes.draw do
   end
 
   # tooltip is just for prototyping tooltip, if its still there you can remove it.
-  %w(tooltip beta board press blog_posts supporters partnerships middle_school story learning develop mission faq tos privacy activities new impact stats team premium teacher_resources media_kit play media news home_new diagnostic_landing_page).each do |page|
+  %w(tooltip beta board press blog_posts supporters partnerships middle_school story learning develop mission faq tos privacy activities new impact stats team premium teacher_resources media_kit play media news home_new diagnostic_tool).each do |page|
     get page => "pages##{page}", as: "#{page}"
   end
 
