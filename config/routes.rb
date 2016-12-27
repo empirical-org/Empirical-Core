c6cdf624d46bbc94638416fc0335b0ef48d58658
RyanNovas <ryan@quill.org>
2016-12-27 14:31:56 -0500

    barebones landing page template, some styling

--- config/routes.rb
+++ config/routes.rb
@@ -254,9 +254,12 @@ EmpiricalGrammar::Application.routes.draw do
   end
 
   # tooltip is just for prototyping tooltip, if its still there you can remove it.
-  %w(tooltip beta board press blog_posts supporters partnerships middle_school story learning develop mission faq tos privacy activities new impact stats team premium teacher_resources media_kit play media news home_new).each do |page|
+  %w(tooltip beta board press blog_posts supporters partnerships middle_school story learning develop mission faq tos privacy activities new impact stats team premium teacher_resources media_kit play media news home_new diagnostic_landing_page).each do |page|
     get page => "pages##{page}", as: "#{page}"
   end
+
+
+
   get 'activities/section/:section_id' => 'pages#activities', as: "activities_section"
   get 'activities/packs' => 'teachers/unit_templates#index'
   get 'activities/packs/:id' => 'teachers/unit_templates#show'
