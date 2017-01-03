6dae54f79fbd261ffba86534d11470c086170d63
Donald McKendrick <donald@seo-1002.seo-nyc.local>
2017-01-03 16:12:26 -0500

    fix eslint dependencies

--- app/components/diagnostics/finishedDiagnostic.jsx
+++ app/components/diagnostics/finishedDiagnostic.jsx
@@ -32,15 +32,7 @@ export default React.createClass({
           You'll be redirected automatically once they are saved.
         </p>
         <Spinner/>
-        {/*<button className="button student-begin" onClick={this.props.begin}>
-          Begin <img className="begin-arrow" src={beginArrow}/>
-        </button>*/}
       </div>
-      // {/*<div className="container">
-      //   Finished Diagnostic
-      //   {this.renderSavedIndicator()}
-      //   <button onClick={this.props.saveToLMS}>Save again.</button>
-      // </div>*/}
     )
   },
 
