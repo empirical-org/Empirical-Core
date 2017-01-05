3f88e8032ee830a56b26f168bea398ea3c61e545
RyanNovas <ryan@quill.org>
2017-01-05 18:02:45 -0500

    removes deprecated id param from diagnostic_reports#assign_selected_activities path

--- config/routes.rb
+++ config/routes.rb
@@ -110,7 +110,7 @@ EmpiricalGrammar::Application.routes.draw do
       get 'classrooms_with_students/u/:unit_id/a/:activity_id/c/:classroom_id' => 'diagnostic_reports#classrooms_with_students'
       get 'students_by_classroom/u/:unit_id/a/:activity_id/c/:classroom_id' => 'diagnostic_reports#students_by_classroom'
       get 'recommendations_for_classroom/:classroom_id' => 'diagnostic_reports#recommendations_for_classroom'
-      post 'assign_selected_packs/:classroom_id' => 'diagnostic_reports#assign_selected_packs'
+      post 'assign_selected_packs' => 'diagnostic_reports#assign_selected_packs'
 
       namespace :concepts do
         resources :students, only: [:index] do
