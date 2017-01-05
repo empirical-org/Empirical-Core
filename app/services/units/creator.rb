31834fe9fa9d7e6e8294be8ffbb8a3f6858c98d8
RyanNovas <ryan@quill.org>
2017-01-05 14:51:56 -0500

    refactors unit updater and creator

--- app/services/units/creator.rb
+++ app/services/units/creator.rb
@@ -13,27 +13,29 @@ module Units::Creator
     self.create_helper(teacher, unit_template.name, activities_data, classrooms_data)
   end
 
-  def self.assign_unit_template_to_one_class(teacher_id, unit_template_id, classroom_id, student_ids=[])
+  def self.assign_unit_template_to_one_class(teacher_id, unit_template_id, classroom)
     teacher = User.find(teacher_id)
     unit_template = UnitTemplate.find(unit_template_id)
     activities_data = unit_template.activities.map{ |a| {id: a.id, due_date: nil} }
-    classrooms_data = [{id: classroom_id, student_ids: student_ids}]
-    self.create_helper(teacher, unit_template.name, activities_data, classrooms_data)
+    self.create_helper(teacher, unit_template.name, activities_data, classroom)
   end
 
   private
 
-  def self.create_helper(teacher, name, activities_data, classrooms_data)
+  def self.create_helper(teacher, name, activities_data, classrooms)
     unit = Unit.create(name: name, user: teacher)
     # makes a permutation of each classroom with each activity to
     # create all necessary activity sessions
-    product = activities_data.product(classrooms_data)
-    product.each do |pair|
-      activity_data, classroom_data = pair
-      unit.classroom_activities.create!(activity_id: activity_data[:id],
-                                        due_date: activity_data[:due_date],
-                                        classroom_id: classroom_data[:id],
-                                        assigned_student_ids: classroom_data[:student_ids])
+    classrooms.each do |classroom|
+      product = activities_data.product(classroom)
+      product.each do |pair|
+        #binding.pry
+        activity_data, classroom = pair
+        unit.classroom_activities.create!(activity_id: activity_data[:id],
+                                          due_date: activity_data[:due_date],
+                                          classroom_id: classroom[:id],
+                                          assigned_student_ids: classroom[:student_ids])
+      end
     end
     # activity_sessions in the state of 'unstarted' are automatically created in an after_create callback in the classroom_activity model
     AssignActivityWorker.perform_async(teacher.id)
