c72009477c9192193e297f607f9f9f4379d8fcf5
RyanNovas <ryan@quill.org>
2017-01-05 15:17:56 -0500

    update creator to match updater, all tests passing

--- spec/services/units/creator_spec.rb
+++ spec/services/units/creator_spec.rb
@@ -11,7 +11,7 @@ describe Units::Creator do
   describe 'unit_creator' do
 
     it 'creates a unit with teacher and kicks off an assign activity worker' do
-      Units::Creator.run(teacher, 'Something Really Cool', [{id: 1}], [{id: classroom.id}])
+      Units::Creator.run(teacher, 'Something Really Cool', [{id: 1}], [{id: classroom.id, student_ids: []}])
       expect(Unit.last.user).to eq(teacher)
       expect(AssignActivityWorker.jobs.size).to eq(1)
     end
