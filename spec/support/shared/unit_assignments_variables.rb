739e86716434269639d5e2a625f3ebc00085176f
RyanNovas <ryan@quill.org>
2017-01-11 17:28:24 -0500

    tests for fast assign worker and unit template controller

--- spec/support/shared/unit_assignments_variables.rb
+++ spec/support/shared/unit_assignments_variables.rb
@@ -1,12 +1,12 @@
 shared_context 'Unit Assignments Variables' do
 
-  let(:teacher) { FactoryGirl.create(:teacher) }
-  let(:classroom) { FactoryGirl.create(:classroom, teacher: teacher) }
-  let(:student) {FactoryGirl.create(:student)}
-  let(:student1) {FactoryGirl.create(:student)}
-  let(:student2) {FactoryGirl.create(:student)}
-  let(:activity) {FactoryGirl.create(:activity)}
-  let(:activity1) {FactoryGirl.create(:activity)}
+  let!(:teacher) { FactoryGirl.create(:teacher) }
+  let!(:classroom) { FactoryGirl.create(:classroom, teacher: teacher) }
+  let!(:student) {FactoryGirl.create(:student)}
+  let!(:student1) {FactoryGirl.create(:student)}
+  let!(:student2) {FactoryGirl.create(:student)}
+  let!(:activity) {FactoryGirl.create(:activity)}
+  let!(:activity1) {FactoryGirl.create(:activity)}
   let(:activity2) {FactoryGirl.create(:activity)}
   let(:activity3) {FactoryGirl.create(:activity)}
   let(:activity4) {FactoryGirl.create(:activity)}
@@ -16,5 +16,14 @@ shared_context 'Unit Assignments Variables' do
   let!(:unit_template4) { FactoryGirl.create(:unit_template, activities: [activity4]) }
   let(:classroom_activity) { FactoryGirl.create(:classroom_activity, activity_id: activity.id, classroom_id: classroom.id, unit_id: unit.id, assigned_student_ids: [student.id])}
   let(:activity_session) {FactoryGirl.create(:activity_session, classroom_activity_id: classroom_activity.id, activity_id: activity.id, user_id: student.id, state: 'finished')}
+
+  def unit_templates_have_a_corresponding_unit?(unit_template_ids)
+    UnitTemplate.where(id: unit_template_ids).pluck(:name) == Unit.all.map(&:name).flatten
+  end
+
+  def units_have_a_corresponding_classroom_activities?(unit_template_ids)
+    UnitTemplate.where(id: unit_template_ids).pluck(:name) == ClassroomActivity.all.map(&:unit).map(&:name).flatten
+  end
+
 
 end
