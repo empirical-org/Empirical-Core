a971dea92ff40f9886be14480476703ad9f0eafd
Emilia Friedberg <emiliafriedberg@gmail.com>
2017-01-12 15:28:18 -0500

    add appropriate images to visual overview and diagnostic

--- client/app/bundles/HelloWorld/components/scorebook/app_legend.jsx
+++ client/app/bundles/HelloWorld/components/scorebook/app_legend.jsx
@@ -5,24 +5,37 @@ export default React.createClass({
   render: function () {
     return (
       <div className="icons-wrapper icon-legend">
-        <div className="row no-pl">
+        <div className="row">
           <div className="col-xs-12 col-sm-12 col-md-12 col-lg-10 col-xl-10 no-pl">
-            <div className="col-xs-6 col-sm-3 col-xl-3">
-              <div className="icon-wrapper icon-green icon-flag"></div>
+          <div className="col-xs-6 col-sm-3 col-xl-3">
+            <div className="icon-wrapper icon-green icon-diagnostic"/>
+            <div className="icons-description-wrapper">
+              <p className="title">Quill Diagnostic</p>
+          </div>
+          </div>
+          <div className="col-xs-6 col-sm-3 col-xl-3 no-pl">
+            <div className="icon-wrapper icon-green icon-connect"/>
+            <div className="icons-description-wrapper">
+              <p className="title">Quill Connect</p>
+          </div>
+          </div>
+            <div className="col-xs-6 col-sm-3 col-xl-3 no-pl">
+              <div className="icon-wrapper icon-green icon-flag"/>
               <div className="icons-description-wrapper">
                 <p className="title">Quill Proofreader</p>
               </div>
             </div>
             <div className="col-xs-6 col-sm-3 col-xl-3 no-pl">
-              <div className="icon-wrapper icon-green icon-puzzle"></div>
+              <div className="icon-wrapper icon-green icon-puzzle"/>
               <div className="icons-description-wrapper">
                 <p className="title">Quill Grammar</p>
               </div>
             </div>
-          </div>
+
 
         </div>
       </div>
+    </div>
     );
   }
 });
