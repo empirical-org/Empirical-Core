bacc48bf743165293fb4d6007f9fac1820089698
Donald McKendrick <ddmckendrick@gmail.com>
2017-01-14 21:08:48 -0500

    fix bugs with sessions

--- app/actions/sessions.js
+++ app/actions/sessions.js
@@ -1,5 +1,6 @@
 var C = require("../constants").default
 import rootRef from '../libs/firebase';
+import _ from 'lodash';
 const sessionsRef = rootRef.child('savedSessions');
 
 export default {
@@ -7,10 +8,12 @@ export default {
     sessionsRef.child(sessionID).once("value", (snapshot) => {
       if (snapshot.exists()) {
         const session = snapshot.val();
-        if (session.currentQuestion.question) {
-          session.currentQuestion.question.attempts = [];
-        } else {
-          session.currentQuestion.data.attempts = [];
+        if (session.currentQuestion) {
+          if (session.currentQuestion.question) {
+            session.currentQuestion.question.attempts = [];
+          } else {
+            session.currentQuestion.data.attempts = [];
+          }
         }
         cb(session);
       }
@@ -18,11 +21,25 @@ export default {
   },
 
   update: function(sessionID, session){
-    sessionsRef.child(sessionID).set(session)
+    // delete_null_properties(session, true)
+    // console.log(session);
+    console.log(_.pickBy(session))
+    sessionsRef.child(sessionID).set(_.pickBy(session))
   },
 
   delete: function(sessionID){
-    sessionsRef.child(sessionID).remove(session)
+    sessionsRef.child(sessionID).remove()
   },
 
+}
+
+function delete_null_properties(test, recurse) {
+    for (var i in test) {
+        if (test[i] === null) {
+            delete test[i];
+        } else if (recurse && typeof test[i] === 'object') {
+            delete_null_properties(test[i], recurse);
+        }
+    }
+    return
 }
