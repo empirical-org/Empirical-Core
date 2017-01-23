d6b093dd0ab4e9987c164854c6740d7c2b30c7e5
Emilia Friedberg <emiliafriedberg@gmail.com>
2017-01-23 17:59:31 -0500

    rewrite lastactiveclassroom to be more reusable, update test

--- spec/controllers/concerns/last_active_classroom_spec.rb
+++ spec/controllers/concerns/last_active_classroom_spec.rb
@@ -5,11 +5,11 @@ include LastActiveClassroom
 describe LastActiveClassroom do
   let! (:teacher) {FactoryGirl.create(:teacher, username: 'dsdada', email: 'dsada@gmail.com')}
 
-  describe "#last_active_classroom" do
-    it "returns the classroom id of the class with the most recently completed activity session" do
+  describe "#last_active_classrooms" do
+    it "returns an array of classrooms in the order of the most recently completed activity session" do
       FactoryGirl.create_list(:classroom_with_classroom_activities, 10, teacher_id: teacher.id)
       last_completed_activity_session = ActivitySession.all.sort_by { |as| as.completed_at }.last
-      expect(last_completed_activity_session.classroom_activity.classroom.id).to eq LastActiveClassroom::last_active_classroom(teacher.id)
+      expect(last_completed_activity_session.classroom_activity.classroom.id).to eq LastActiveClassroom::last_active_classrooms(teacher.id, 1).first
     end
   end
 
