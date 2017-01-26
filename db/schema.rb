0f1ab49f3c18382fe9cbe7f146630276eafde36b
Donald McKendrick <ddmckendrick@gmail.com>
2017-01-26 18:50:47 -0500

    Add a migration that adds trigrams

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170105213544) do
+ActiveRecord::Schema.define(version: 20170126211938) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
   enable_extension "hstore"
   enable_extension "pg_stat_statements"
+  enable_extension "pg_trgm"
 
   create_table "activities", force: :cascade do |t|
     t.string   "name",                       limit: 255
