33fd3665a9926b2e4b710099d1f3c18ffe1f71f1
RyanNovas <ryan@quill.org>
2017-01-27 14:28:22 -0500

    classrooms with student container gets student data and responds to checks in assign units

--- client/app/bundles/HelloWorld/components/lesson_planner/create_unit/stage2/ClassroomsWithStudents.jsx
+++ client/app/bundles/HelloWorld/components/lesson_planner/create_unit/stage2/ClassroomsWithStudents.jsx
@@ -3,30 +3,29 @@ import Classroom from './classroom.jsx'
 
 export default class extends React.Component {
 
+
+
 	render() {
-		// let classroomList;
-		// if (this.props.classrooms) {
-		// 	let that = this;
-		// 	classroomList = this.props.classrooms.map((el)=> {
-		// 		return <Classroom    key = {el.classroom.id}
-		// 												 classroom={el.classroom}
-		// 												 students={el.students}
-		// 												 allSelected={el.allSelected}
-		// 												 toggleClassroomSelection={that.props.toggleClassroomSelection}
-		// 												 toggleStudentSelection={that.props.toggleStudentSelection} />;
-		//
-		// 	})
-		// } else {
-		// 	// TODO: make this a message that they don't have aany classrooms
-		// 	classroomList = []
-		// }
-		//
+		let classroomList;
+		if (this.props.classrooms) {
+			let that = this;
+			classroomList = this.props.classrooms.map((el)=> {
+				return <Classroom    key = {el.id}
+														 classroom={el}
+														 students={el.students}
+														 allSelected={el.allSelected}
+														 toggleClassroomSelection={that.props.toggleClassroomSelection}
+														 toggleStudentSelection={that.props.toggleStudentSelection} />;
 
+			})
+		} else {
+			// TODO: make this a message that they don't have any classrooms
+			classroomList = []
+		}
 
-    // return (
-		// 	{classroomList}
-    // );
-		return (<div></div>)
+    return (
+			<div>{classroomList || 'I am the div that is undefined'}</div>
+    );
 	}
 
 }
