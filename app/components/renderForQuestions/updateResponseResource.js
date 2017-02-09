6e1f9020bc97663b658ef7d8369aec855f3c2513
Donald McKendrick <ddmckendrick@gmail.com>
2017-02-09 11:16:51 -0500

    Updating response resource checks for a response with the same text string.

--- app/components/renderForQuestions/updateResponseResource.js
+++ app/components/renderForQuestions/updateResponseResource.js
@@ -1,31 +1,44 @@
-import {hashToCollection} from '../../libs/hashToCollection'
-import _ from 'underscore'
-import generateFeedbackString from './generateFeedbackString.js'
+import { hashToCollection } from '../../libs/hashToCollection';
+import _ from 'underscore';
+import generateFeedbackString from './generateFeedbackString.js';
 import {
   incrementResponseCount,
-  submitNewResponse
-} from "../../actions/responses.js"
-
+  submitNewResponse,
+  findResponseByText
+} from '../../actions/responses.js';
 
 const getLatestAttempt = function (attempts = []) {
   const lastIndex = attempts.length - 1;
-  return attempts[lastIndex]
-}
+  return attempts[lastIndex];
+};
 
-export default function updateResponseResource (returnValue, questionID, attempts, dispatch, playQuestion) {
-  var previousAttempt;
+export default function updateResponseResource(returnValue, questionID, attempts, dispatch, playQuestion) {
+  let previousAttempt;
 
-  const preAtt = getLatestAttempt(attempts)
-  if (preAtt) {previousAttempt = getLatestAttempt(attempts).response}
-  const prid = previousAttempt ? previousAttempt.key : undefined
+  const preAtt = getLatestAttempt(attempts);
+  if (preAtt) { previousAttempt = getLatestAttempt(attempts).response; }
+  const prid = previousAttempt ? previousAttempt.key : undefined;
 
   if (returnValue.found && returnValue.response.key) {
     dispatch(
       incrementResponseCount(questionID, returnValue.response.key, prid)
-    )
+    );
   } else {
-    dispatch(
-      submitNewResponse(returnValue.response, prid)
-    )
+    incrementOrCreateResponse(questionID, returnValue, prid, dispatch);
   }
+}
+
+export function incrementOrCreateResponse(questionID, returnValue, prid, dispatch) {
+  const callback = (response) => {
+    if (response) {
+      dispatch(
+        incrementResponseCount(questionID, response.key, prid)
+      );
+    } else {
+      dispatch(
+        submitNewResponse(returnValue.response, prid)
+      );
+    }
+  };
+  findResponseByText(returnValue.response.text, questionID, callback);
 }
