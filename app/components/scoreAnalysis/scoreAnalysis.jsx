585dea9d2816886c8ad267382d1d3e7b9efd1049
Donald McKendrick <ddmckendrick@gmail.com>
2017-02-14 16:23:27 -0500

    Add action and reducer for score analysis

--- app/components/scoreAnalysis/scoreAnalysis.jsx
+++ app/components/scoreAnalysis/scoreAnalysis.jsx
@@ -1,37 +1,48 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import scoreActions from '../../actions/scoreAnalysis.js';
+import LoadingSpinner from '../shared/spinner.jsx';
 
 class ScoreAnalysis extends Component {
   constructor(props) {
     super();
   }
 
+  componentWillMount() {
+    this.props.dispatch(scoreActions.loadScoreData());
+  }
+
   render() {
-    return (
-      <div>
-        <h1>Hello World.</h1>
-        <table>
-          <thead>
-            <tr>
-              <th>Name</th>
-              <th>Responses</th>
-              <th>Attempts</th>
-              <th>Unmatched</th>
-              <th>Common Unmatched</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>Name</td>
-              <td>Responses</td>
-              <td>Attempts</td>
-              <td>Unmatched</td>
-              <td>Common Unmatched</td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-    );
+    const { questions, scoreAnalysis, } = this.props;
+    if (questions.hasreceiveddata && scoreAnalysis.hasreceiveddata) {
+      return (
+        <div>
+          <h1>Hello World.</h1>
+          <table>
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Responses</th>
+                <th>Attempts</th>
+                <th>Unmatched</th>
+                <th>Common Unmatched</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>Name</td>
+                <td>Responses</td>
+                <td>Attempts</td>
+                <td>Unmatched</td>
+                <td>Common Unmatched</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      );
+    } else {
+      return (<LoadingSpinner />);
+    }
   }
 
 }
@@ -39,6 +50,7 @@ class ScoreAnalysis extends Component {
 function select(state) {
   return {
     questions: state.questions,
+    scoreAnalysis: state.scoreAnalysis,
     routing: state.routing,
   };
 }
