7e921020de712db53f056e661a57f14c803d749c
RyanNovas <ryan@quill.org>
2017-02-22 17:56:42 -0500

    analytics track activity search

--- app/workers/activity_search_analytics_worker.rb
+++ app/workers/activity_search_analytics_worker.rb
@@ -3,6 +3,6 @@ class ActivitySearchAnalyticsWorker
 
   def perform(user_id, search_query)
     analytics = SegmentAnalytics.new
-    analytics.track_activity_seach(user_id, search_query)
+    analytics.track_activity_search(user_id, search_query)
   end
 end
