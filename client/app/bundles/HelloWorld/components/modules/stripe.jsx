8538de8a19585e876d467c3f9979814d358e9ad4
RyanNovas <ryan@quill.org>
2017-02-27 17:54:18 -0500

    update premium page to include buy now link for school premium

--- client/app/bundles/HelloWorld/components/modules/stripe.jsx
+++ client/app/bundles/HelloWorld/components/modules/stripe.jsx
@@ -1,6 +1,6 @@
 import $ from 'jquery'
-export default function () {
-    var amount = 8000;
+export default function (priceInCents, description) {
+    var amount = priceInCents;
     var handler = StripeCheckout.configure({
     key: stripePubKey,
     image: 'https://d1yxac6hjodhgc.cloudfront.net/wp-content/uploads/2015/11/Quill-Icon.svg',
@@ -9,6 +9,7 @@ export default function () {
           $.post('charges.json',
           {authenticity_token: $('meta[name=csrf-token]').attr('content'), source: token, card: token.card, amount: amount})
           .done(function( data ) {
+            if (amount === 40000) {alert('Premium has been activated for your account. You will receive follow-up communication within a business day.')}
             window.location.assign(data.route);
           });
     }
@@ -18,7 +19,7 @@ export default function () {
     // // Open Checkout with further options
     handler.open({
       name: 'Quill Premium',
-      description: '$80 Teacher Premium',
+      description: description,
       amount: amount,
     });
 
