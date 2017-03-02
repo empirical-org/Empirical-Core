b2d9b64fb7b0ad50b10e9bece4795b1d0049b394
RyanNovas <ryan@quill.org>
2017-03-02 14:29:48 -0500

    styling and copy
    styling wip
    
    
    styling wip

---
+++ client/app/bundles/HelloWorld/components/premium/premium_minis/school_pricing_mini.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import Stripe from '../../modules/stripe.jsx'
+export default React.createClass({
+
+  charge: function() {
+      new Stripe(40000, '$400 School Premium');
+  },
+
+  render: function(){
+    return(
+  <div className='pricing-mini'>
+    <div className='promo-tab'>Get 50% off for the first year!</div>
+    <div className='placeholder-div'>
+      <div className='promo-tab'>Get 50% off for the first year!</div>
+    </div>
+    <header className='pricing-mini-header purple'>
+      <div className='img-holder'>
+          <img src="/images/school_premium_icon.png" alt="teacher_premium_icon"/>
+      </div>
+      <h4>School Premium</h4>
+    </header>
+    <section className='pricing-info'>
+      <div className='premium-rates'>
+        <img className='red-line' src="/images/red-line-premium.svg" alt="red-line"/>
+        <h3>
+          <span className='four-fifty'>
+            $450
+          </span>
+          <span>
+            $900
+          </span>
+        </h3>
+        <h4>per year</h4>
+      </div>
+      <ul>
+        <li>Everything in Teacher Premium</li>
+        <li>Dedicated representative to ensure a successful onboarding experience</li>
+        <li>Professional Development Sessions</li>
+        <li>Administrator dashboard for school-<br/>
+          wide reports
+        </li>
+      </ul>
+    </section>
+    <button type='button' id='purchase-btn' data-toggle="modal" onClick={this.charge} className='btn btn-default mini-btn purple'>Get Quote</button>
+    <a href='https://quillpremium.wufoo.com/forms/quill-premium-quote'><button type='button' className='btn btn-default mini-btn empty-purple'>Schedule Demo</button></a>
+  </div>
+);
+  }
+});
