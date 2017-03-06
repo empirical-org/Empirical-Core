1505c2d8da32a61b8288944bd0527a48cbfdd1ce
RyanNovas <ryan@quill.org>
2017-03-06 13:51:58 -0500

    merge develop

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,11 +60,7 @@ EmpiricalGrammar::Application.configure do
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-  config.assets.precompile += %w(epiceditor/themes/base/epiceditor.css
-                                 epiceditor/themes/preview/github.css
-                                 epiceditor/themes/editor/epic-dark.css
-                                 codemirror.css
-                                 porthole.js
+  config.assets.precompile += %w(   
                                  cms.js
                                  cms.css
                                  scorebook/*
