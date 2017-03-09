00f2917265e67b067079f5cee59cb373501bd4f8
Jared Silver <jared@jaredsilver.name>
2017-03-09 15:04:16 -0500

    Add tests for EducatorType component

---
+++ client/app/bundles/HelloWorld/components/accounts/new/__tests__/educator_type.test.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import EducatorType from '../educator_type';
+
+import UsK12View from '../../school/us_k12_view'
+import NotUsK12View from '../../school/not_us_k12_view'
+
+describe('EducatorType component', () => {
+
+  it('should render if stage is 1', () => {
+      const wrapper = shallow(
+        <EducatorType />
+      );
+      expect(wrapper).toMatchSnapshot();
+  });
+
+  it('should render UsK12View if stage state is 2', () => {
+      const wrapper = shallow(
+        <EducatorType />
+      );
+      wrapper.setState({ stage: 2 });
+      expect(wrapper.find(UsK12View).length).toBe(1);
+  });
+
+  it('should render NotUsK12View if stage state is 3', () => {
+      const wrapper = shallow(
+        <EducatorType />
+      );
+      wrapper.setState({ stage: 3 });
+      expect(wrapper.find(NotUsK12View).length).toBe(1);
+  });
+
+});
