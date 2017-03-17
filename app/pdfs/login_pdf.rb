59150f61c98bb102a7eabc660547170da987c525
Jared Silver <jared@jaredsilver.name>
2017-03-17 14:37:55 -0400

    Implements Amr's new login PDF design for student list

--- app/pdfs/login_pdf.rb
+++ app/pdfs/login_pdf.rb
@@ -3,31 +3,96 @@ class LoginPdf
 
   def initialize(classroom)
     @classroom = classroom
-    list_students
+    render_login_pdf
   end
 
-  def list_students
-    count = 0
-    @classroom.students.each do |student|
-      # We have to make sure that we don't have a page break in the middle
-      # of a student's login information. At the default font size, we can fit
-      # 10 students on one page. After that, we add two line breaks to move to
-      # the following page as we can fit 52 lines of text, and each student
-      # takes up 5 lines.
-      if count == 10
-        count = 0
-        text "\n\n"
-      else
-        count += 1
+  def render_cover_page
+    text "This is where Amr's awesome cover page will go."
+    start_new_page(:margin => [22, 24, 22, 24])
+  end
 
-        if student.clever_id.present?
-          text "<b>#{student.name} (has clever)</b>\nemail: #{student.email}\npassword: Login with Clever with the above email address\n\n\n", inline_format: true
-        elsif student.signed_up_with_google?
-          text "<b>#{student.name} (has google)</b>\nemail: #{student.email}\npassword: Login with Google with the above email address\n\n\n", inline_format: true
-        else
-          text "<b>#{student.name}</b>\nusername: #{student.username}\ndefault password: #{student.last_name}\n\n\n", inline_format: true
+  def render_section_for_one_student(student)
+    font("Helvetica", size: 18, style: :bold) do
+      text_box(
+        student.name,
+        at: [0, cursor],
+        height: 18,
+        width: 564,
+        overflow: :shrink_to_fit
+      )
+    end
+    move_down 28
+    float do
+      fill_color 'EEEEEE'
+      fill_rectangle [252, cursor], 312, 80
+      bounding_box([260, cursor - 8], width: 296, height: 64) do
+        move_down 2
+        fill_color '000000'
+        render_text "Username:", 10
+        move_down 2
+        font("Helvetica", style: :bold) do
+          text_box(
+            render_username_or_email_for_student(student),
+            at: [0, cursor],
+            height: 12,
+            width: 312,
+            overflow: :shrink_to_fit
+          )
+        end
+        move_down 20
+        render_text "Password: (First letter is <b>Capitalized</b>)", 10
+        move_down 2
+        font("Helvetica", style: :bold) do
+          text_box(
+            student.last_name.capitalize,
+            at: [0, cursor],
+            height: 12,
+            width: 312,
+            overflow: :shrink_to_fit
+          )
         end
       end
+    end
+    render_text "1- Visit <b>quill.org</b>"
+    move_down 8
+    render_text "2- Click <b>Login</b> (at the top of the page)"
+    move_down 8
+    render_text "3- Enter your <b>username</b> and <b>password</b>"
+    move_down 8
+    render_text "4- Click the <b>Login</b> button"
+  end
+
+  def render_username_or_email_for_student(student)
+    if student.clever_id.present? || student.signed_up_with_google?
+      student.email
+    else
+      student.username
+    end
+  end
+
+  def render_login_pdf
+    font("Helvetica")
+    render_cover_page
+    student_count = 0
+    @classroom.students.each do |student|
+      render_section_for_one_student(student)
+      student_count += 1
+      if student_count % 5 > 0 && student_count != @classroom.students.length
+        move_down 20
+        stroke_color 'CCCCCC'
+        dash(12)
+        stroke_horizontal_line 0, 564
+        move_down 24
+      elsif student_count % 5 == 0
+        start_new_page
+      end
+    end
+  end
+
+  def render_text(content, size = nil)
+    size ||= 12
+    font_size size do
+      text content, inline_format: true
     end
   end
 
