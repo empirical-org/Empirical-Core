7816b6f98d45e53cc4e5314c61d693fe8bd8214f
Emilia Friedberg <emiliafriedberg@gmail.com>
2017-03-17 13:46:40 -0400

    create and position premium flyer div

---
+++ client/app/bundles/HelloWorld/components/premium/premium_flyer.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default class extends React.Component{
+  constructor(){
+    super()
+  }
+
+  render(){
+    return (
+      <div className="premium-flyer">
+        {/* <img src="/images/homepage/homepage_pattern_blue_premium.png"/> */}
+          <div className="content">
+            <h1>Get Our Premium Flyer</h1>
+            <p>To learn more about our Premium product, its features and benefits, you can download and print this one-page overview and share it with your school.</p>
+          </div>
+      </div>
+    )}
+}
