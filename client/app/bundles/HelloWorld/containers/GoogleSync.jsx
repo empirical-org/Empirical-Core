98a1e3b2f5bed672c0ffb52070a0cda69ca604c8
RyanNovas <ryan@quill.org>
2017-03-20 09:38:01 -0400

    json of Google Classrooms renders on front end, test coverage

--- client/app/bundles/HelloWorld/containers/GoogleSync.jsx
+++ client/app/bundles/HelloWorld/containers/GoogleSync.jsx
@@ -1,25 +1,41 @@
 import React from 'react'
 import $ from 'jquery'
 import LoadingIndicator from '../components/shared/loading_indicator.jsx'
+import GoogleClassroomList from '../components/google_classroom/google_classroom_sync/GoogleClassroomsList.jsx'
 
 
 export default class extends React.Component{
   constructor(){
     super()
-    this.getGoogleClassrooms()
   }
 
   state = {loading: true}
 
+  componentDidMount(){
+    this.getGoogleClassrooms()
+  }
+
   getGoogleClassrooms(){
+    const that = this;
     $.get('/teachers/classrooms/retrieve_google_classrooms', (data) => {
-            
-);
+      that.setState({classrooms: data.classrooms})
+    })
+    .fail((data)=>{
+      that.setState({error: data.error})
+    })
+    .always(()=>{
+      that.setState({loading: false})
+    })
+    ;
   }
 
   loadingIndicatorOrContent(){
     if (this.state.loading) {
       return <LoadingIndicator/>
+    } else if (this.state.errors) {
+      // error page
+    } else {
+      return <GoogleClassroomList classrooms={this.state.classrooms}/>
     }
   }
 
