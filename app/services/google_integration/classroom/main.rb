cc3c4556a18b306f5f24741b0bd5ad00dd3d74d0
RyanNovas <ryan@quill.org>
2017-03-21 10:37:34 -0400

    validate parsed courses by user and archival status

--- app/services/google_integration/classroom/main.rb
+++ app/services/google_integration/classroom/main.rb
@@ -3,22 +3,22 @@ module GoogleIntegration::Classroom::Main
   def self.pull_and_save_data(user, access_token)
     client = GoogleIntegration::Client.create(access_token)
     if user.role == 'teacher'
-      GoogleIntegration::Classroom::Teacher.run(user, self.parse_courses(client), access_token)
+      GoogleIntegration::Classroom::Teacher.run(user, self.parse_courses(user, client), access_token)
     elsif user.role == 'student'
-      GoogleIntegration::Classroom::Student.run(user, self.parse_courses(client))
+      GoogleIntegration::Classroom::Student.run(user, self.parse_courses(user, client))
     end
   end
 
   def self.pull_data(user, access_token)
     client = GoogleIntegration::Client.create(access_token)
-    self.parse_courses(client)
+    self.parse_courses(user, client)
   end
 
   private
 
-  def self.parse_courses(client)
+  def self.parse_courses(user, client)
     course_response = GoogleIntegration::Classroom::Requesters::Courses.run(client)
-    GoogleIntegration::Classroom::Parsers::Courses.run(course_response)
+    GoogleIntegration::Classroom::Parsers::Courses.run(user, course_response)
   end
 
 end
