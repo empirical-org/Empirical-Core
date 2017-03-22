30bff1f4a6ee2885a62f232931ccd8ef71168ee0
Jared Silver <jared@jaredsilver.name>
2017-03-22 13:28:54 -0400

    Add tests for ListFilterOption

---
+++ client/app/bundles/HelloWorld/components/shared/list_filter_options/__tests__/list_filter_options.test.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import ListFilterOptions from '../list_filter_options';
+
+describe('ListFilterOptions component', () => {
+
+  describe('with a user who is not logged in', () => {
+    //TODO: write these tests
+  });
+
+  describe('with a user who is logged in', () => {
+    //TODO: write these tests
+  });
+
+});
