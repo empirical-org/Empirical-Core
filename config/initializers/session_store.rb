7316a1560573efd599886279da7e8215f17a0c6c
RyanNovas <ryan@quill.org>
2017-03-28 13:32:53 -0400

    staging cookie fix

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -3,6 +3,8 @@
 
 if Rails.env.production?
   EmpiricalGrammar::Application.config.session_store :cookie_store, key: '_quill_session', domain: '.quill.org'
+elsif Rails.env.staging?
+  EmpiricalGrammar::Application.config.session_store :cookie_store, key: '_quill_staging_session', domain: 'staging.quill.org'
 else
   EmpiricalGrammar::Application.config.session_store :cookie_store, key: '_quill_session'
 end
