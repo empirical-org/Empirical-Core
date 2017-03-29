51fa834f404bced986e0df66718c5d811bad732a
RyanNovas <ryan@quill.org>
2017-03-29 14:06:20 -0400

    view for if there are no google classrooms

--- client/app/bundles/HelloWorld/containers/__tests__/GoogleSync.test.js
+++ client/app/bundles/HelloWorld/containers/__tests__/GoogleSync.test.js
@@ -10,45 +10,54 @@ describe('GoogleSync container', () => {
 
   describe('the loading indicator', () => {
     it('should render a loading indicator by default', () => {
-        const wrapper = shallow(
-          <GoogleSync />
-        );
+        const wrapper = shallow(<GoogleSync />);
         expect(wrapper.find(LoadingIndicator).length).toBe(1);
     });
 
     it('should render a loading indicator if the state is loading', () => {
-        const wrapper = shallow(
-          <GoogleSync />
-        );
+        const wrapper = shallow(<GoogleSync />);
         wrapper.setState({ loading: true });
         expect(wrapper.find(LoadingIndicator).length).toBe(1);
     });
 
     it('should not render a loading indicator if the state is not loading', () => {
-        const wrapper = shallow(
-          <GoogleSync />
-        );
+        const wrapper = shallow(<GoogleSync />);
         wrapper.setState({ loading: false});
         expect(wrapper.find(LoadingIndicator).length).toBe(0);
     });
   })
 
   describe('the GoogleClassroomsList', ()=>{
-    it('should not render when the state is loading', () => {
-        const wrapper = shallow(
-          <GoogleSync />
-        );
-        wrapper.setState({ loading: true });
-        expect(wrapper.find(GoogleClassroomsList).length).toBe(0);
-    });
 
-    it('should render when the state is not loading', () => {
-        const wrapper = shallow(
-          <GoogleSync />
-        );
-        wrapper.setState({ loading: false });
-        expect(wrapper.find(GoogleClassroomsList).length).toBe(1);
-    });
+    describe('should not render when', () => {
+
+      it('should not render when the state is loading', () => {
+          const wrapper = shallow(<GoogleSync />);
+          wrapper.setState({ loading: true,  classrooms: [1] });
+          expect(wrapper.find(GoogleClassroomsList).length).toBe(0);
+      });
+
+      it('when there are no classrooms', () => {
+        const wrapper = shallow(<GoogleSync />);
+        wrapper.setState({loading: false, classrooms: []});
+        expect(wrapper.find(GoogleClassroomsList).length).toBe(0);
+      })
+
+    })
+
+    it('it should render when the state is not loading and there are classrooms', () => {
+      const wrapper = shallow(<GoogleSync />);
+      wrapper.setState({ loading: false, classrooms: [1]  });
+      expect(wrapper.find(GoogleClassroomsList).length).toBe(1);
+    })
+  })
+
+  describe('the no classrooms notice', ()=> {
+    it('renders if loading is false and there no classrooms', ()=>{
+      const wrapper = shallow(<GoogleSync />);
+      wrapper.setState({loading: false, classrooms: []});
+      expect(wrapper.find(GoogleClassroomsList).length).toBe(0);
+    })
   })
 
 });
