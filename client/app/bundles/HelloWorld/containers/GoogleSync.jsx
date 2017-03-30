5615f5bc18b672d759de738a59206e9f60ae4e26
Emilia Friedberg <emiliafriedberg@gmail.com>
2017-03-30 15:53:27 -0400

    use more specific language for warning modal

--- client/app/bundles/HelloWorld/containers/GoogleSync.jsx
+++ client/app/bundles/HelloWorld/containers/GoogleSync.jsx
@@ -11,7 +11,7 @@ import Modal from 'react-bootstrap/lib/Modal'
 export default class extends React.Component{
   constructor(){
     super()
-    this.hideModal = this.hideModal.bind(this);
+    this.hideWarningModal = this.hideWarningModal.bind(this);
     this.hideSuccessModal = this.hideSuccessModal.bind(this);
     this.syncClassrooms = this.syncClassrooms.bind(this);
     this.syncClassroomsAjax = this.syncClassroomsAjax.bind(this);
@@ -74,14 +74,14 @@ export default class extends React.Component{
 
   modalWarning = () => {
     if (this.state.classData.archivedCount > 0) {
-      this.setState({showModal: true})
+      this.setState({showWarningModal: true})
     } else {
       this.syncClassroomsAjax();
     }
   }
 
-  hideModal() {
-    this.setState({showModal: false})
+  hideWarningModal() {
+    this.setState({showWarningModal: false})
   }
 
   hideSuccessModal() {
@@ -95,7 +95,7 @@ export default class extends React.Component{
       url: '/teachers/classrooms/import_google_students',
       statusCode: {
         200: function() {
-          that.setState({showModal: false})
+          that.setState({showWarningModal: false})
           if (that.state.classData.selectedClassrooms.length > 0) {
             that.setState({showSuccessModal: true})
           } else {
@@ -112,7 +112,7 @@ export default class extends React.Component{
         <h2>Choose Which Google Classrooms To Sync</h2>
         <p>Select all of the classes that you would like to sync with Google Classroom. Previously connected classes will import new students.</p>
         <GoogleClassroomList classrooms={this.state.classrooms} syncClassrooms={this.syncClassrooms}/>
-        <ArchiveClassesWarning show={this.state.showModal} data={this.state.classData} syncClassroomsAjax={this.syncClassroomsAjax} hideModal={this.hideModal} />
+        <ArchiveClassesWarning show={this.state.showWarningModal} data={this.state.classData} syncClassroomsAjax={this.syncClassroomsAjax} hideModal={this.hideWarningModal} />
         <SyncSuccessModal show={this.state.showSuccessModal} data={this.state.classData} syncClassroomsAjax={this.syncClassroomsAjax} hideModal={this.hideSuccessModal} />
       </div>)
   }
