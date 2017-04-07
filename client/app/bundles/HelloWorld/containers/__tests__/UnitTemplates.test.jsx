d4f4cc5c51536e84641017e3def8316adb3469ab
Jared Silver <jared@jaredsilver.name>
2017-04-07 18:02:35 -0400

    Add UnitTemplates component jest tests

--- client/app/bundles/HelloWorld/containers/__tests__/UnitTemplates.test.jsx
+++ client/app/bundles/HelloWorld/containers/__tests__/UnitTemplates.test.jsx
@@ -3,10 +3,13 @@ import { shallow } from 'enzyme';
 
 import UnitTemplates from '../UnitTemplates.jsx';
 
+import Cms from '../Cms.jsx'
+
 describe('UnitTemplates container', () => {
 
-  it('should ', () => {
-
+  it('should render a Cms component', () => {
+    const wrapper = shallow(<UnitTemplates />);
+    expect(wrapper.find(<Cms />).exists()).toBe(true);
   });
 
 });
