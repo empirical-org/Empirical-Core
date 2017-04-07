e9afcc8a594219c2e7561615fa51165a5b25c728
Jared Silver <jared@jaredsilver.name>
2017-04-07 11:40:46 -0400

    Adds AccountSettings container test

--- client/app/bundles/HelloWorld/containers/__tests__/AccountSettings.test.jsx
+++ client/app/bundles/HelloWorld/containers/__tests__/AccountSettings.test.jsx
@@ -3,10 +3,12 @@ import { shallow } from 'enzyme';
 
 import AccountSettings from '../AccountSettings.jsx';
 
+import UserAccessibleSelectRole from '../../components/accounts/edit/user_accessible_select_role.jsx'
+
 describe('AccountSettings container', () => {
 
-  it('should ', () => {
-
+  it('should render a UserAccessibleSelectRole component', () => {
+    expect(shallow(<AccountSettings />).find(UserAccessibleSelectRole).exists()).toBe(true);
   });
 
 });
