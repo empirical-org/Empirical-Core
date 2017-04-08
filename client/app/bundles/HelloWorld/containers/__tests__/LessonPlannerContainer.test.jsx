f01c33edd3dfcb13a7e204b9b6be7d5964d1b239
Jared Silver <jared@jaredsilver.name>
2017-04-08 01:43:05 -0400

    Add Jest test for LessonPlannerContainer

--- client/app/bundles/HelloWorld/containers/__tests__/LessonPlannerContainer.test.jsx
+++ client/app/bundles/HelloWorld/containers/__tests__/LessonPlannerContainer.test.jsx
@@ -3,10 +3,28 @@ import { shallow } from 'enzyme';
 
 import LessonPlannerContainer from '../LessonPlannerContainer.jsx';
 
+import UnitTabs from '../../components/lesson_planner/unit_tabs.jsx'
+
 describe('LessonPlannerContainer container', () => {
 
-  it('should ', () => {
+  it('should render unitTabs component for teachers', () => {
+    const wrapperTeachers = shallow(
+      <LessonPlannerContainer location={{pathname: 'includes-teachers'}} />
+    );
+    const wrapperNotTeachers = shallow(
+      <LessonPlannerContainer location={{pathname: 'does-not-include'}} />
+    );
+    expect(wrapperTeachers.find(UnitTabs).exists()).toBe(true);
+    expect(wrapperNotTeachers.find(UnitTabs).exists()).toBe(false);
+  });
 
+  it('should render children', () => {
+    const wrapper = shallow(
+      <LessonPlannerContainer location={{pathname: ''}}>
+        <h1>I am a child.</h1>
+      </LessonPlannerContainer>
+    );
+    expect(wrapper.find('h1').text()).toBe('I am a child.');
   });
 
 });
