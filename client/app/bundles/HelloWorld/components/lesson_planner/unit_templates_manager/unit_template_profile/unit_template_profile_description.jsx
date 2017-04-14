449cdbd4a8f6336a6038f72353f6085a46e3a4cf
Emilia Friedberg <emiliafriedberg@gmail.com>
2017-04-14 14:19:29 -0400

    test unit_template_profile and unit_template_profile_description

--- client/app/bundles/HelloWorld/components/lesson_planner/unit_templates_manager/unit_template_profile/unit_template_profile_description.jsx
+++ client/app/bundles/HelloWorld/components/lesson_planner/unit_templates_manager/unit_template_profile/unit_template_profile_description.jsx
@@ -8,25 +8,25 @@
 
   renderProblem: function () {
     if (this.props.data.problem) {
-      return [(<dt key='problem_title'><strong>Problem</strong></dt>),(<dd key='problem_text'>{this.props.data.problem}</dd>)]
+      return [(<dt id="problem_title" key='problem_title'><strong>Problem</strong></dt>),(<dd id="problem_text" key='problem_text'>{this.props.data.problem}</dd>)]
     }
   },
 
   renderSummary: function () {
     if (this.props.data.summary) {
-      return [(<dt key='summary_title' ><strong>Summary</strong></dt>),(<dd key='summary_text'>{this.props.data.summary}</dd>)]
+      return [(<dt id="summary_title" key='summary_title' ><strong>Summary</strong></dt>),(<dd id='summary_text' key='summary_text'>{this.props.data.summary}</dd>)]
     }
   },
 
   renderTeacherReview: function () {
     if (this.props.data.teacher_review) {
-      return [(<dt key='review_title'><strong>Teacher Review</strong></dt>),(<dd key='review_text'>{this.props.data.teacher_review}</dd>)]
+      return [(<dt id='review_title' key='review_title'><strong>Teacher Review</strong></dt>),(<dd id='review_text' key='review_text'>{this.props.data.teacher_review}</dd>)]
     }
   },
 
   renderAuthor: function () {
     if (this.props.data.author.description) {
-      return [(<dt key='author_title'><strong>About the Author</strong></dt>),(<dd key='author_text'>{this.props.data.author.description}</dd>)]
+      return [(<dt id='author_title' key='author_title'><strong>About the Author</strong></dt>),(<dd id='author_text' key='author_text'>{this.props.data.author.description}</dd>)]
     }
   },
 
