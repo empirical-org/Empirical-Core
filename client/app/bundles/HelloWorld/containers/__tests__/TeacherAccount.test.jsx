7a5e0541a016af289662f68e08c67963c8f73a32
Jared Silver <jared@jaredsilver.name>
2017-04-14 15:35:16 -0400

    Add some TeacherAccount container tests, refactor some of TeacherAccount container

--- client/app/bundles/HelloWorld/containers/__tests__/TeacherAccount.test.jsx
+++ client/app/bundles/HelloWorld/containers/__tests__/TeacherAccount.test.jsx
@@ -1,12 +1,54 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 
 import TeacherAccount from '../TeacherAccount.jsx';
+import SelectRole from '../../components/accounts/edit/select_role';
+import UserSelectRole from '../../components/accounts/edit/user_accessible_select_role.jsx';
+import SelectSubscription from '../../components/accounts/subscriptions/select_subscription';
+import StaticDisplaySubscription from '../../components/accounts/subscriptions/static_display_subscription';
+import SelectSchool from '../../components/accounts/school/select_school';
+import LoadingSpinner from '../../components/shared/loading_indicator.jsx'
 
 describe('TeacherAccount container', () => {
+  // Sadly, we have to mount here instead of using shallow
+  // because the TeacherAccount container uses quite a few
+  // refs for input fields. In order to test these, we need
+  // to use mount because shallow does not play with refs.
+  const wrapper = mount(
+    <TeacherAccount
+      userType='teacher'
+      teacherId={7}
+    />
+  );
+  const wrapperStaff = mount(
+    <TeacherAccount
+      userType='staff'
+    />
+  );
+  wrapper.setState({loading: false});
+  wrapperStaff.setState({loading: false});
 
   it('should have the expected initial state', () => {
-
+    expect(shallow(<TeacherAccount userType='arbitrary' teacherId={4} />).state()).toEqual({
+      id: 4,
+			name: '',
+			username: '',
+			email: '',
+			isSaving: false,
+			selectedSchool: null,
+			originalSelectedSchool: null,
+			schoolOptions: [],
+			schoolOptionsDoNotApply: false,
+			role: 'teacher',
+			password: null,
+			loading: true,
+			errors: {},
+			subscription: {
+				id: null,
+				expiration: '2016-01-01',
+				account_limit: null
+			}
+    });
   });
 
   describe.skip('componentDidMount', () => {
@@ -21,13 +63,16 @@ describe('TeacherAccount container', () => {
   });
 
   it('should render LoadingSpinner component if loading', () => {
-
+    wrapper.setState({loading: true});
+    expect(wrapper.find(LoadingSpinner).exists()).toBe(true);
+    wrapper.setState({loading: false});
+    expect(wrapper.find(LoadingSpinner).exists()).toBe(false);
   });
 
   describe('if the user is staff', () => {
     describe('SelectRole component', () => {
       it('should render', () => {
-
+        expect(wrapperStaff.find(SelectRole).exists()).toBe(true);
       });
 
       it('should have role prop based on state', () => {
@@ -40,12 +85,12 @@ describe('TeacherAccount container', () => {
 
       it('should have prop errors based on state', () => {
 
-      });
+      })
     });
 
     describe('SelectSubscription component', () => {
       it('should render', () => {
-
+        expect(wrapperStaff.find(SelectSubscription).exists()).toBe(true);
       });
 
       it('should have prop subscription based on state', () => {
@@ -65,7 +110,7 @@ describe('TeacherAccount container', () => {
   describe('if the user is not staff', () => {
     describe('UserSelectRole component', () => {
       it('should render', () => {
-
+        expect(wrapper.find(UserSelectRole).exists()).toBe(true);
       });
 
       it('should have role prop equal to state or teacher', () => {
@@ -79,7 +124,7 @@ describe('TeacherAccount container', () => {
 
     describe('StaticDisplaySubscription component', () => {
       it('should render', () => {
-
+        expect(wrapper.find(StaticDisplaySubscription).exists()).toBe(true);
       });
 
       it('should have subscription prop based on state', () => {
@@ -119,7 +164,7 @@ describe('TeacherAccount container', () => {
   describe('if the user does not use google', () => {
     describe('input with ref email', () => {
       it('should render', () => {
-
+        expect(wrapper.ref('email').exists()).toBe(true);
       });
 
       it('have value based on state', () => {
@@ -131,17 +176,19 @@ describe('TeacherAccount container', () => {
       });
     });
     it('should render email errors based on state', () => {
-
+      wrapper.setState({errors: {email: 'I am error'}});
+      expect(wrapper.text()).toMatch('I am error');
     });
   });
 
   it('should render user\'s name', () => {
-    // George Costanza's Account
+    wrapper.setState({name: 'George Costanza'});
+    expect(wrapper.text()).toMatch('George Costanza\'s Account');
   });
 
   describe('input with ref name', () => {
     it('should render', () => {
-
+      expect(wrapper.ref('name').exists()).toBe(true);
     });
 
     it('should have value equal to state.name', () => {
@@ -153,13 +200,14 @@ describe('TeacherAccount container', () => {
     });
   });
 
-  it('should render name errors', () => {
-
+  it('should render name errors based on state', () => {
+    wrapper.setState({errors: {name: 'I am error'}});
+    expect(wrapper.text()).toMatch('I am error');
   });
 
   describe('input with ref username', () => {
     it('should render', () => {
-
+      expect(wrapper.ref('username').exists()).toBe(true);
     });
 
     it('should have value equal to state.username', () => {
@@ -171,13 +219,14 @@ describe('TeacherAccount container', () => {
     });
   });
 
-  it('should render username errors', () => {
-
+  it('should render username errors based on state', () => {
+    wrapper.setState({errors: {username: 'I am error'}});
+    expect(wrapper.text()).toMatch('I am error');
   });
 
   describe('input of type password with ref password', () => {
     it('should render', () => {
-
+      expect(wrapper.ref('password').exists()).toBe(true);
     });
 
     it('should have an onChange event that changes state.password', () => {
@@ -185,13 +234,14 @@ describe('TeacherAccount container', () => {
     });
   });
 
-  it('should render password errors', () => {
-
+  it('should render password errors based on state', () => {
+    wrapper.setState({errors: {password: 'I am error'}});
+    expect(wrapper.text()).toMatch('I am error');
   });
 
   describe('SelectSchool component', () => {
     it('should render', () => {
-
+      expect(wrapper.find(SelectSchool).exists()).toBe(true);
     });
 
     it('should have errors prop depending on state', () => {
@@ -217,21 +267,28 @@ describe('TeacherAccount container', () => {
 
   describe('input with ref schoolOptionsDoNotApply', () => {
     it('should render', () => {
-
+      expect(wrapper.ref('schoolOptionsDoNotApply').exists()).toBe(true);
     });
 
     it('should have onChange prop that updates state', () => {
-
+      expect(wrapper.state().schoolOptionsDoNotApply).toBe(false);
+      wrapper.ref('schoolOptionsDoNotApply').props().onChange();
+      expect(wrapper.state().schoolOptionsDoNotApply).toBe(true);
+      wrapper.ref('schoolOptionsDoNotApply').props().onChange();
+      expect(wrapper.state().schoolOptionsDoNotApply).toBe(false);
     });
 
     it('should have checked prop that returns checked depending on state', () => {
-
+      wrapper.setState({schoolOptionsDoNotApply: true});
+      expect(wrapper.ref('schoolOptionsDoNotApply').props().checked).toBe(true)
+      wrapper.setState({schoolOptionsDoNotApply: false});
+      expect(wrapper.ref('schoolOptionsDoNotApply').props().checked).toBe(false)
     });
   });
 
   describe('save button', () => {
     it('should render', () => {
-
+      expect(wrapper.find('button').filterWhere(e => e.text() == 'Save Changes').exists()).toBe(true);
     });
 
     describe.skip('onClick prop', () => {
@@ -239,16 +296,19 @@ describe('TeacherAccount container', () => {
     });
 
     it('should have class button-grey if state.isSaving or button-green if not', () => {
-
+      wrapper.setState({isSaving: true});
+      expect(wrapper.find('button').filterWhere(e => e.text() == 'Save Changes').props().className).toBe('button-grey');
+      wrapper.setState({isSaving: false});
+      expect(wrapper.find('button').filterWhere(e => e.text() == 'Save Changes').props().className).toBe('button-green');
     });
   });
 
   describe('delete button', () => {
     it('should render', () => {
-
+      expect(wrapper.find('.delete-account').exists()).toBe(true);
     });
 
-    describe('onClick prop', () => {
+    describe.skip('onClick prop', () => {
       //TODO: write these tests once we implement requestjs
     });
   });
