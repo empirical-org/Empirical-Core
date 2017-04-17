eece42f8fe538501105ae4023f587a6a08c5b37b
Emilia Friedberg <emiliafriedberg@gmail.com>
2017-04-17 14:34:01 -0400

    add scrolltotop component and include it where necessary

--- client/app/bundles/HelloWorld/components/lesson_planner/unit_templates_manager/unit_template_profile/unit_template_profile.jsx
+++ client/app/bundles/HelloWorld/components/lesson_planner/unit_templates_manager/unit_template_profile/unit_template_profile.jsx
@@ -3,6 +3,7 @@
  import React from 'react'
  import { Link } from 'react-router'
  import LoadingIndicator from '../../../shared/loading_indicator'
+ import ScrollToTop from '../../../shared/scroll_to_top'
  import ListFilterOptions from '../../../shared/list_filter_options/list_filter_options'
  import UnitTemplateProfileHeader from './unit_template_profile_header'
  import UnitTemplateProfileDescription from './unit_template_profile_description'
@@ -81,6 +82,7 @@
     } else {
       return (
         <div className='unit-template-profile'>
+          <ScrollToTop/>
           {this.showListFilterOptions()}
           <UnitTemplateProfileHeader data={this.state.data} />
           <div className="container white">
