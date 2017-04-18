8400cbadac2b6fab39e7a5e6aefcfa1cf65a0057
Jared Silver <jared@jaredsilver.name>
2017-04-18 17:23:59 -0400

    Add TODOs to LessonPlanner.test.jsx

--- client/app/bundles/HelloWorld/containers/__tests__/LessonPlanner.test.jsx
+++ client/app/bundles/HelloWorld/containers/__tests__/LessonPlanner.test.jsx
@@ -3,10 +3,28 @@ import { shallow } from 'enzyme';
 
 import LessonPlanner from '../LessonPlanner.jsx';
 
-describe('LessonPlanner container', () => {
+describe.skip('LessonPlanner container', () => {
 
-  it('should ', () => {
+  //TODO: write these tests
+  //TODO: also, write tests for lifecycle events
+  //TODO: also, write tests for each of the functions we're passing down
 
+  describe('on create unit tab', () => {
+    it('should render CreateUnit component with relevant data, actions, and analytics props', () => {
+
+    });
+  });
+
+  describe('on assign new activity tab', () => {
+    it('should render AssignANewActivity component with toggleTab function and flag', () => {
+
+    });
+  });
+
+  describe('on manage units tab', () => {
+    it('should render ManageUnits component with toggleTab and editUnit actions', () => {
+
+    });
   });
 
 });
