25941043287c5c82d9f68ba13c190f2d0a9cfcd7
Donald McKendrick <ddmckendrick@gmail.com>
2017-04-18 23:18:06 -0400

    Fire model feedback if available

--- app/components/studentLessons/question.jsx
+++ app/components/studentLessons/question.jsx
@@ -271,6 +271,11 @@ const playLessonQuestion = React.createClass({
             return <ConceptExplanation {...data} />;
           }
         }
+      } else if (this.getQuestion() && this.getQuestion().modelConceptUID) {
+        const dataF = this.props.conceptsFeedback.data[this.getQuestion().modelConceptUID];
+        if (dataF) {
+          return <ConceptExplanation {...dataF} />;
+        }
       } else if (this.getQuestion().conceptID) {
         const data = this.props.conceptsFeedback.data[this.getQuestion().conceptID];
         if (data) {
@@ -378,9 +383,8 @@ const playLessonQuestion = React.createClass({
           {component}
         </div>
       );
-    } else {
-      return (<p>Loading...</p>);
     }
+    return (<p>Loading...</p>);
   },
 });
 
