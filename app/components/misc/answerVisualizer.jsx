22e53f8e9a2489451575349b812ad2138ff39687
Jared Silver <jared@jaredsilver.name>
2017-04-20 14:30:10 -0400

    Show diffs between incorrect response and nearest correct response on visualize page

--- app/components/misc/answerVisualizer.jsx
+++ app/components/misc/answerVisualizer.jsx
@@ -3,24 +3,73 @@ import { connect } from 'react-redux';
 import _ from 'underscore';
 import fuzzy from 'fuzzyset.js';
 import DiffedResponse from './diffedResponse.jsx';
+import { listenToResponsesWithCallback } from '../../actions/responses';
+import respWithStatus from '../../libs/responseTools.js';
 
 export class AnswerVisualizer extends Component {
+  constructor() {
+    super();
+    this.state = {
+      responses: {},
+      loadedResponses: false
+    };
+  }
+
+  componentWillMount() {
+    listenToResponsesWithCallback(
+      this.props.params.questionID,
+      (data) => {
+        this.setState({
+          responses: respWithStatus(data),
+          loadedResponses: true,
+        });
+      }
+    );
+  }
+
+  getHumanCorrectResponses() {
+    return _.reject(_.values(_.mapObject(this.state.responses, (r) => {
+      return r.statusCode == 0 ? r.text : null;
+    })), (r) => { return !r });
+  }
+
+  getHumanIncorrectResponses() {
+    return _.reject(_.values(_.mapObject(this.state.responses, (r) => {
+      return r.statusCode == 1 ? r.text : null;
+    })), (r) => { return !r });
+  }
 
   getClosestCorrectResponse(response) {
-    // fuzzy(_.pluck(this.props.responses.data[this.props.params.questionID], 'text')).get(response);
+    return fuzzy(this.getHumanCorrectResponses()).get(response)[0][1];
+  }
+
+  renderDiffsBetweenIncorrectResponsesAndNearestCorrectResponse() {
+    return this.getHumanIncorrectResponses().map((response) => {
+      return(
+        <DiffedResponse
+          firstResponse={this.getClosestCorrectResponse(response)}
+          newResponse={response}
+        />
+      );
+    });
   }
 
   render() {
-    return (
-      <div>
-        <h1 className="title is-3">Answer Diff Visualizer</h1>
-        <DiffedResponse
-          firstResponse="Jared liked the dog. The dog was cute."
-          newResponse="Jared liked the dog because it was cute."
-        />
-        {this.props.children}
-      </div>
-    )
+    if(this.state.loadedResponses) {
+      return (
+        <div>
+          <div className="card is-fullwidth has-bottom-margin">
+            <div className="card-content">
+              <h1 className="title is-3">Diffs Between Incorrect Responses and Nearest Correct Response</h1>
+              {this.renderDiffsBetweenIncorrectResponsesAndNearestCorrectResponse()}
+            </div>
+          </div>
+          {this.props.children}
+        </div>
+      )
+    } else {
+      return <h1>Loading...</h1>;
+    }
   }
 
 }
@@ -28,7 +77,6 @@ export class AnswerVisualizer extends Component {
 function select(props) {
   return {
     questions: props.questions,
-    responses: props.responses,
   };
 }
 
