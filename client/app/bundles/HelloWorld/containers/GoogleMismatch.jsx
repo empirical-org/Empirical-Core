962ff2e82c16157be6b94e063c11656c30ad7b6d
RyanNovas <ryan@quill.org>
2017-04-24 16:45:29 -0400

    route and view for if user tries to synch google with incongruous email

---
+++ client/app/bundles/HelloWorld/containers/GoogleMismatch.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+export default class extends React.Component{
+  constructor(){
+    super()
+  }
+
+  render(){
+    alert('hi')
+    return(
+      <div className='google-sync-container'>
+      </div>)
+  }
+}
