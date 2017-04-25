acc6c233dbbc9dcd7f779f7ef9476b9910aff165
Donald McKendrick <ddmckendrick@gmail.com>
2017-04-25 16:55:16 -0400

    style language page

--- app/components/eslDiagnostic/languagePage.jsx
+++ app/components/eslDiagnostic/languagePage.jsx
@@ -7,18 +7,23 @@ export default class LanguagePage extends React.Component {
 
   render() {
     return (
-      <div>
-        <p>Hello there! You are about to start a 22 question Placement Activity. First, let’s set up your language preference</p>
-        <p>All the directions are in:</p>
-        <p><span>English</span>(Default)</p>
-        <p>and I want <span>additional</span> directions in:</p>
-        <div>Español</div>
-        <div>中文</div>
-        <div>Français</div>
-        <div>Tiếng Việt</div>
-        <div>العربية</div>
-        <div>हिंदी</div>
-        <div>No translations needed - Show only English</div>
+      <div className="language-page">
+        <div className="introductory-text">
+          <p>Hello there! You are about to start a 22 question placement activity. First, let’s set up your language preference.</p>
+          <p>All the directions are in:</p>
+          <div className="default-language"><img src="https://s3.amazonaws.com/empirical-core-prod/assets/flags/U.S._Outlying_Islands.png" /><span>English</span> (Default)</div>
+          <p>and I want <span>additional</span> directions in:</p>
+        </div>
+        <div className="language-button-container">
+          <div className="language-button" onClick={() => this.props.selectLanguage('spanish')}><img className="language-button-img" src="https://s3.amazonaws.com/empirical-core-prod/assets/flags/Spain.png" />Español</div>
+          <div className="language-button" onClick={() => this.props.selectLanguage('chinese')}><img className="language-button-img" src="https://s3.amazonaws.com/empirical-core-prod/assets/flags/China.png" />中文</div>
+          <div className="language-button" onClick={() => this.props.selectLanguage('french')}><img className="language-button-img" src="https://s3.amazonaws.com/empirical-core-prod/assets/flags/France.png" />Français</div>
+          <div className="language-button" onClick={() => this.props.selectLanguage('vietnamese')}><img className="language-button-img" src="https://s3.amazonaws.com/empirical-core-prod/assets/flags/Vietnam.png" />Tiếng Việt</div>
+          <div className="language-button" onClick={() => this.props.selectLanguage('arabic')}><img className="language-button-img" src="https://s3.amazonaws.com/empirical-core-prod/assets/flags/Egypt.png" />العربية</div>
+          <div className="language-button" onClick={() => this.props.selectLanguage('hindi')}><img className="language-button-img" src="https://s3.amazonaws.com/empirical-core-prod/assets/flags/India.png" />हिंदी</div>
+        </div>
+        <div className="divider" />
+        <div className="no-language-button" onClick={() => this.props.selectLanguage('english')}><img className="language-button-img" src="https://s3.amazonaws.com/empirical-core-prod/assets/flags/U.S._Outlying_Islands.png" /> No translations needed - Show only English</div>
       </div>
     );
   }
