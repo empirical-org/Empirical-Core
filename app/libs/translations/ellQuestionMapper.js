6a6ff3738615255a29c79147d5503e971d83114f
Donald McKendrick <ddmckendrick@gmail.com>
2017-04-25 13:56:40 -0400

    ADD INSTRUCTION MAP FOR ELL DIAGNOSTIC

---
+++ app/libs/translations/ellQuestionMapper.js
@@ -0,0 +1,21 @@
+const questionToCopyMap = {
+  '-Ke_LpBo1GAcq4gXHORz': 'sentence fragment complete vs incomplete completion prompt',
+  '-Ke_Lw0PG3zy5QodpFdo': 'sentence fragment complete vs incomplete completion prompt',
+  '-KOqMWRFOrYgv-KjRYdy-esp': 'sentence fragment complete vs incomplete completion prompt',
+  '-Ke_6gyGVXuHiealUgD3': 'fill-in-the-blank-scaffolded-instructions',
+  '-Ke_70Ul5Ka4AKbbtAOa': 'fill-in-the-blank-scaffolded-instructions',
+  '-Ke_7AgPuCE56lUNwFJN': 'fill-in-the-blank-scaffolded-instructions',
+  '-KdCbiGL0xjiiPQ-vYLz': 'tense-question-instructions',
+  '-KdCcbo8BK-kJqgPObbc': 'tense-question-instructions',
+  '-KdCcwDIhHsDCZcJ9YPW': 'tense-question-instructions',
+  '-KdCdCkL84VCZfaLDJue': 'tense-question-instructions',
+  '-KdCebEKd5NcM8VNB5Fz': 'tense-question-instructions',
+  '-KdChlhBafaHb8zZvE7t': 'combine-sentences-instructions',
+  '-KeKkcDr7CfAx81hSWWp': 'combine-sentences-instructions',
+  '-KP-M1Crf2pvqO4QH6zI-esp': 'combine-sentences-instructions',
+  '-KP-MEpdOxjU7OyzL6ss-esp': 'combine-sentences-with-joining-words-instructions',
+  '-KPt3I_hR_Xlv5Cr1mvB-esp': 'combine-sentences-with-joining-words-instructions',
+  '-KP-Mv5jsZKhraQH2DOt-esp': 'combine-sentences-instructions',
+};
+
+export default questionToCopyMap;
--- app/libs/translations/example.js
+++ app/libs/translations/example.js
@@ -1,19 +1,37 @@
-const exampleTranslation = {
-  'diagnostic intro text': '',
+const buttonText = {
+  'submit button': '',
+  'resume button text': '',
+  'begin button text': '',
+  'sentence fragment complete button': '',
+  'sentence fragment incomplete button': '',
+};
+
+const titleCards = {
   'fill in multiple blanks title card': '',
   'fill in single blanks title card': '',
   'sentence fragments title card': '',
   'tense title card': '',
   'sentence combining title card': '',
+  'diagnostic intro text': '',
   'completion page': '',
-  'sentence fragment complete button': '',
-  'sentence fragment incomplete button': '',
-  'sentence fragment complete vs incomplete choice prompt': '',
-  'sentence fragment complete vs incomplete completion prompt': '',
+};
+
+const instructions = {
+  'sentence fragment complete vs incomplete choice instructions': '',
+  'sentence fragment complete vs incomplete completion instructions': '',
+  'fill-in-the-blank-scaffolded-instructions': '',
+  'tense-question-instructions': '',
+};
+
+const scaffolds = {
   'joining word cues': '',
-  'submit button': '',
-  'resume button text': '',
-  'begin button text': '',
+};
+
+const exampleTranslation = {
+  ...instructions,
+  ...titleCards,
+  ...buttonText,
+  ...scaffolds,
 };
 
 export default exampleTranslation;
