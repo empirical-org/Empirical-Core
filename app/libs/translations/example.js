7806795f8b80dafde840edc44085e3533d4f8b37
Donald McKendrick <ddmckendrick@gmail.com>
2017-04-25 12:09:57 -0400

    Scaffold translation file.

---
+++ app/libs/translations/example.js
@@ -0,0 +1,19 @@
+const exampleTranslation = {
+  'diagnostic intro text': '',
+  'fill in multiple blanks title card': '',
+  'fill in single blanks title card': '',
+  'sentence fragments title card': '',
+  'tense title card': '',
+  'sentence combining title card': '',
+  'completion page': '',
+  'sentence fragment complete button': '',
+  'sentence fragment incomplete button': '',
+  'sentence fragment complete vs incomplete choice prompt': '',
+  'sentence fragment complete vs incomplete completion prompt': '',
+  'joining word cues': '',
+  'submit button': '',
+  'resume button text': '',
+  'begin button text': '',
+};
+
+export default exampleTranslation;
