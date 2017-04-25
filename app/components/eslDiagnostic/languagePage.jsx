4e756cf7c64ad27d40456dbc6148b1736da62128
Donald McKendrick <ddmckendrick@gmail.com>
2017-04-25 15:46:13 -0400

    add copy to language page

--- app/components/eslDiagnostic/languagePage.jsx
+++ app/components/eslDiagnostic/languagePage.jsx
@@ -6,6 +6,20 @@ export default class LanguagePage extends React.Component {
   }
 
   render() {
-    return (<div>I am a language component!</div>);
+    return (
+      <div>
+        <p>Hello there! You are about to start a 22 question Placement Activity. First, let’s set up your language preference</p>
+        <p>All the directions are in:</p>
+        <p><span>English</span>(Default)</p>
+        <p>and I want <span>additional</span> directions in:</p>
+        <div>Español</div>
+        <div>中文</div>
+        <div>Français</div>
+        <div>Tiếng Việt</div>
+        <div>العربية</div>
+        <div>हिंदी</div>
+        <div>No translations needed - Show only English</div>
+      </div>
+    );
   }
 }
