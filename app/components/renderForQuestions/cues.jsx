24c02f304c6e6e1ad2b1ca2d7d4618be5798dae6
Donald McKendrick <ddmckendrick@gmail.com>
2017-04-27 15:40:50 -0400

    Fix bug with languages and cue

--- app/components/renderForQuestions/cues.jsx
+++ app/components/renderForQuestions/cues.jsx
@@ -7,13 +7,13 @@ export default React.createClass({
   getJoiningWordsText() {
     if (this.props.getQuestion().cues && this.props.getQuestion().cues.length === 1) {
       let text = translations.english['joining word cues single'];
-      if (this.props.language !== 'english') {
+      if (this.props.language && this.props.language !== 'english') {
         text += ` / ${translations[this.props.language]['joining word cues single']}`;
       }
       return text;
     } else {
       let text = translations.english['joining word cues multiple'];
-      if (this.props.language !== 'english') {
+      if (this.props.language && this.props.language !== 'english') {
         text += ` / ${translations[this.props.language]['joining word cues multiple']}`;
       }
       return text;
@@ -25,7 +25,7 @@ export default React.createClass({
     if (this.props.customText) {
       text = this.props.customText;
     } else {
-      text = this.getJoiningWordsText()
+      text = this.getJoiningWordsText();
     }
     return (
       <div className="cue-explanation" key="explanation">
