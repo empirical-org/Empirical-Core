7f3e79e18ce593e90ac630a9c81515353b414c61
RyanNovas <ryan@quill.org>
2017-04-27 14:57:00 -0400

    front and back end depend on proficiency cut off file for grades

--- app/helpers/scorebook_helper.rb
+++ app/helpers/scorebook_helper.rb
@@ -3,15 +3,17 @@ module ScorebookHelper
 
 
   def percentage_color(score)
+    proficiency_cutoff = ProficiencyEvaluator.proficiency_cutoff
+    near_proficiency_cutoff = ProficiencyEvaluator.near_proficiency_cutoff
     return 'gray' unless score
     score = score.to_f / 100.0 if score > 1
     score = score.round(2)
     case score
-    when 0.8..1.0
+    when proficiency_cutoff..1.0
       'green'
-    when 0.6..0.79
+    when near_proficiency_cutoff...proficiency_cutoff
       'orange'
-    when 0.0..0.59
+    when 0.0...near_proficiency_cutoff
       'red'
     else
       'gray'
