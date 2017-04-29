8a9bbe6d9149f2fe02e46d65a05390023c228e65
Donald McKendrick <ddmckendrick@gmail.com>
2017-04-29 12:27:16 -0400

    Update base url for connect

--- production.sh
+++ production.sh
@@ -1,4 +1,4 @@
-rm -r dist/; NODE_ENV=production EMPIRICAL_BASE_URL=https://quill.org webpack --optimize-minimize
+rm -r dist/; NODE_ENV=production EMPIRICAL_BASE_URL=https://www.quill.org webpack --optimize-minimize
 firebase deploy --project production
 aws s3 sync ./dist/ s3://aws-website-quillconnect-6sy4b
 ./rollbar.sh
