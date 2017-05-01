e5a2f1b0188c9c3abc066d32955ca581960be37f
Hannahmonk <hannah@quill.org>
2017-05-01 14:00:03 -0400

    Update spanish.js

--- app/libs/translations/spanish.js
+++ app/libs/translations/spanish.js
@@ -8,8 +8,8 @@ const buttonText = {
 };
 
 const titleCards = {
-  'fill in multiple blanks title card': '<h3>Completa el Espacio en Blanco: A, An, The</h3><p>En esta sección, cada oración tendrá dos o tres espacios en blanco.</p><p>Volverás a escribir la oración, rellenando el espacio en blanco con una de las palabras de una lista o eligiendo que no se necesite ninguna palabra en el espacio en blanco.</p>',
-  'fill in single blanks title card': '<h3>Completa el Espacio en Blanco</h3><p>¡Ya estás en la mitad del camino! En esta sección, elige la palabra más adecuada para completar la oración. Vuelve a escribir la oración, completando el espacio en blanco con una de las palabras de la lista.</p>',
+  'fill in multiple blanks title card': '<h3>Completa el Espacio en Blanco: A, An, The</h3><p>En esta sección, cada oración tendrá dos o tres espacios en blanco.</p><p>Usted llenará el espacio en blanco con una de las palabras de una lista, o dejará el espacio en blanco si no se necesita ninguna palabra.</p>',
+  'fill in single blanks title card': '<h3>Completa el Espacio en Blanco</h3><p>¡Ya estás en la mitad del camino! En esta sección, elige la palabra más adecuada para completar la oración. Posteriormente, llenarás el espacio en blanco con una de las palabras en la lista.</p>',
   'sentence fragments title card': '<h3>Oraciones Completas</h3><p>En esta sección, agregarás un grupo de palabras para formar una oración. Trata de agregar el menor número posible de palabras.</p><p>¡Comencemos!</p>',
   'tense title card': '<h3>Tiempo del Verbo: Pasado, Presente y Futuro</h3><p>En esta sección, corrige las oraciones conjugando el verbo en el tiempo que sea adecuado para que coincida con el resto de la oración.</p><p>¡Lo estas haciendo excelente!</p>',
   'sentence combining title card': '<h3>Combinación de Oraciones</h3><p>En esta sección, combina las oraciones para formar una sola oración.</p><p>En algunas ocasiones, podrás elegir de las conjunciones que se te presenten para combinar las oraciones. En otras ocasiones, combinarás las oraciones de la manera que tu consideres que sea mejor.</p><p>Agrega o cambia palabras únicamente si es necesario y trata de mantener el mismo significado de las oraciones.</p><p>¡Tu puedes hacerlo!</p>',
@@ -21,9 +21,9 @@ const instructions = {
   'sentence-fragment-complete-vs-incomplete-button-choice-instructions': 'Esta oración esta complete o incompleta?',
   'sentence-fragment-complete-vs-incomplete-choice-instructions': 'Si es una oración completa, aprieta el botón que dice “enviar”. Si es una oración incompleta, complete la oración ahora.',
   'sentence-fragment-complete-vs-incomplete-completion-instructions': 'Añadir al grupo de palabras para hacer una oración completa. Añada el menor número posible de palabras.',
-  'fill-in-the-blank-multiple-instructions': 'Completa el espacio en blanco con una de estas palabras: a, an, the. Si no se necesita ninguna palabra, quite el espacio en blanco.',
+  'fill-in-the-blank-multiple-instructions': 'Completa el espacio en blanco con una de las palabras de arriba. Si no es necesario insertar una palabra, deja el espacio en blanco.',
   'tense-question-instructions': 'Vuelve a escribir la oración. Conjuga el verbo resaltado en negro al tiempo correcto para que coincida con el resto de la oración.',
-  'fill-in-the-blank-single-instructions': 'Vuelve a escribir la oración y completa el espacio en blanco con una de estas palabras: on, in, at, to',
+  'fill-in-the-blank-single-instructions': 'Completa el espacio en blanco con una las palabras de arriba.',
   'combine-sentences-instructions': 'Combina las oraciones a una sola oración.',
   'combine-sentences-with-joining-words-instructions': 'Combina las oraciones, usando una de las palabras sumarias.',
 };
