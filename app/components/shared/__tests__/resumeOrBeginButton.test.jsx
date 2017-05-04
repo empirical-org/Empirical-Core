c1def9a5343b4decb7d48397b0aed2cd582f8a16
Emilia Friedberg <emiliafriedberg@gmail.com>
2017-05-04 14:41:03 -0400

    componentize and test resumeOrBeginButton

---
+++ app/components/shared/__tests__/resumeOrBeginButton.test.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import ResumeOrBeginButton from '../resumeOrBeginButton.jsx';
+
+describe('ResumeOrBeginButton component', () => {
+  const clickMock = jest.fn()
+  const text = 'I am the text that got passed'
+  const wrapper = shallow(<ResumeOrBeginButton text={text} onClickFn={clickMock}/> )
+
+  it('renders a button element', () => {
+    expect(wrapper.find('button')).toHaveLength(1)
+  })
+
+  it('calls the click function it is passed when clicked', () => {
+    wrapper.simulate('click')
+    expect(clickMock.mock.calls).toHaveLength(1)
+  })
+
+  it('renders the text it is passed', () => {
+    expect(wrapper.text()).toEqual(text)
+  })
+
+})
