3d4cf8a68787f32c62309269dcce77c6edff3081
Emilia Friedberg <emiliafriedberg@gmail.com>
2017-05-04 13:59:05 -0400

    es6ify diagnosticprogressbar component

--- app/components/shared/diagnosticProgressBar.jsx
+++ app/components/shared/diagnosticProgressBar.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 
-export default function DiagnosticProgressReport(props) {
-  return (<progress className="progress diagnostic-progress" value={props.percent} max="100">15%</progress>)
-}
+const DiagnosticProgressBar = props => (
+  <progress className="progress diagnostic-progress" value={props.percent} max="100">15%</progress>)
+
+export default DiagnosticProgressBar
