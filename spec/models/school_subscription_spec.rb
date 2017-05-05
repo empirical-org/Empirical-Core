ae1008cc429e60305a4ebf797de931d89bdbcb9e
RyanNovas <ryan@quill.org>
2017-05-05 16:46:04 -0400

    school sub update or create spec

--- spec/models/school_subscription_spec.rb
+++ spec/models/school_subscription_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 describe SchoolSubscription, type: :model do
+let!(:school_sub) {FactoryGirl.create(:school_subscription)}
 
   context "validates" do
 
@@ -23,6 +24,19 @@ describe SchoolSubscription, type: :model do
       end
     end
 
+  end
+
+  context "SchoolSubscription.update_or_create" do
+    it "updates existing SchoolSubscriptions to the new subscription_id" do
+      SchoolSubscription.update_or_create(school_sub.id, 11)
+      expect(school_sub.reload.subscription_id).to eq(11)
+    end
+
+    it "creates new UserSubscriptions with the passed subscription_id and user_id" do
+      SchoolSubscription.update_or_create(99, 11)
+      new_school_sub = SchoolSubscription.last
+      expect([ new_school_sub.school_id,  new_school_sub.subscription_id]).to eq([99,11])
+    end
   end
 
 end
