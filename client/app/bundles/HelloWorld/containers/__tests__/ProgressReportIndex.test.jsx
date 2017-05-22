e61d3b0d9a191c6ec85ffeb6c8ccc854859b0df2
Emilia Friedberg <emiliafriedberg@gmail.com>
2017-05-22 09:47:52 -0400

    add fastassign loading state and test

--- client/app/bundles/HelloWorld/containers/__tests__/ProgressReportIndex.test.jsx
+++ client/app/bundles/HelloWorld/containers/__tests__/ProgressReportIndex.test.jsx
@@ -3,7 +3,7 @@ import { shallow } from 'enzyme';
 
 import ProgressReportIndex from '../ProgressReportIndex.jsx';
 
-describe.skip('ProgressReportIndex container', () => {
+it.skip('ProgressReportIndex container', () => {
 
   //TODO: write tests for this component once we switch from ajax to request
 });
