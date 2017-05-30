3bca3d6115a4967805d47761a176775cce465887
RyanNovas <ryan@quill.org>
2017-05-30 17:15:27 -0400

    cors works on develop

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,6 +37,13 @@ Rails.application.configure do
   # Raise an error on page load if there are pending migrations.
   config.active_record.migration_error = :page_load
 
+  config.middleware.insert_before 0, Rack::Cors do
+    allow do
+      origins '*'
+      resource '*', :headers => :any, :methods => [:get, :post, :options]
+    end
+  end
+
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
