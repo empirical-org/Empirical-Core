8abbb5df63a3b017b3ab08bcbeb1c0e459c6723c
RyanNovas <ryan@quill.org>
2017-05-31 15:58:20 -0400

    remove increment_counts route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   resources :responses
   get 'questions/:question_uid/responses' => 'responses#responses_for_question'
-  put 'responses/:id/count' => 'responses#increment_counts'
   post 'responses/create_or_increment'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
