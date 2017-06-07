3f55cae10008cd38bd76ff87b559767f54961fce
Emilia Friedberg <emiliafriedberg@gmail.com>
2017-06-07 16:54:33 -0400

    stop clearing responses when unmounting response component

--- app/components/questions/responseComponent.jsx
+++ app/components/questions/responseComponent.jsx
@@ -72,10 +72,6 @@ const Responses = React.createClass({
     };
   },
 
-  componentWillUnmount() {
-    this.clearResponsesFromMassEditArray();
-  },
-
   componentDidMount() {
     this.searchResponses();
   },
