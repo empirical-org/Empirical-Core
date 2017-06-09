8b3def40fcceb2b9a853f34b8ad26f4a0a9d1be9
Emilia Friedberg <emiliafriedberg@gmail.com>
2017-06-09 11:19:54 -0400

    Mass edit tab - activeness and number of responses selected

--- app/reducers/massEdit.js
+++ app/reducers/massEdit.js
@@ -2,6 +2,7 @@ import C from '../constants';
 
 const initialState = {
   selectedResponses: [],
+  numSelectedResponses: 0
 }
 
 export default function(currentState, action) {
@@ -11,15 +12,18 @@ export default function(currentState, action) {
     case C.ADD_RESPONSE_TO_MASS_EDIT_ARRAY:
       if(newState.selectedResponses.indexOf(responseKey) < 0) {
         newState.selectedResponses.push(responseKey);
+        newState.numSelectedResponses = newState.selectedResponses.length
       }
       return newState;
     case C.REMOVE_RESPONSE_FROM_MASS_EDIT_ARRAY:
       if(newState.selectedResponses.indexOf(responseKey) > -1) {
         newState.selectedResponses.splice(newState.selectedResponses.indexOf(responseKey), 1);
+        newState.numSelectedResponses = newState.selectedResponses.length
       }
       return newState;
     case C.CLEAR_RESPONSES_FROM_MASS_EDIT_ARRAY:
       newState.selectedResponses = [];
+      newState.numSelectedResponses = 0
       return newState;
     default:
       return newState;
