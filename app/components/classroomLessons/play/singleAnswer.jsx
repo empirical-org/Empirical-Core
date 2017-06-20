5ab4e497fbe9a415ffb949f1c545733f2525f6a9
Donald McKendrick <ddmckendrick@gmail.com>
2017-06-20 17:57:37 -0400

    Scaffold single answer.

--- app/components/classroomLessons/play/singleAnswer.jsx
+++ app/components/classroomLessons/play/singleAnswer.jsx
@@ -1,4 +1,8 @@
 import React, { Component } from 'react';
+import Cues from '../../renderForQuestions/cues.jsx';
+import RenderSentenceFragments from '../../renderForQuestions/sentenceFragments.jsx';
+import icon from '../../../img/question_icon.svg';
+import TextEditor from '../../renderForQuestions/renderTextEditor.jsx';
 
 class SingleAnswer extends Component {
   constructor(props) {
@@ -6,9 +10,25 @@ class SingleAnswer extends Component {
   }
 
   render() {
+    console.log(this.props.data);
     return (
       <div>
-        Single Answer
+        <RenderSentenceFragments prompt={this.props.data.play.prompt} />
+        <Cues
+          getQuestion={() => ({
+            cues: this.props.data.play.cues,
+          })
+        }
+        />
+        <div className="feedback-row">
+          <img src={icon} />
+          <p>{this.props.data.play.instructions}</p>
+        </div>
+        <TextEditor
+          className={'textarea is-question is-disabled'} defaultValue={''}
+          disabled={false} checkAnswer={() => {}}
+          hasError={undefined} handleChange={() => {}}
+        />
       </div>
     );
   }
