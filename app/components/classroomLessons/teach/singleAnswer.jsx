eca41790ba58e1057260c897af744a267886c067
Donald McKendrick <ddmckendrick@gmail.com>
2017-06-21 10:16:07 -0400

    View student responses in teacher single answer.

--- app/components/classroomLessons/teach/singleAnswer.jsx
+++ app/components/classroomLessons/teach/singleAnswer.jsx
@@ -5,11 +5,48 @@ class SingleAnswer extends Component {
     super(props);
   }
 
-
   renderNextSlideButton() {
     return (
       <button onClick={this.props.goToNextSlide} >Next Slide</button>
     );
+  }
+
+  renderReview(item) {
+    const { submissions, current_slide, students, } = this.props.data;
+    const submissionComponents = Object.keys(submissions[current_slide]).map(key => (
+      <li
+        style={{
+          marginTop: 10,
+          borderBottom: '1px solid magenta',
+        }}
+      >
+        {submissions[current_slide][key]} - {students[key]}
+      </li>
+      ));
+    return (
+      <ul
+        style={{
+          margin: 10,
+          padding: 10,
+          border: '1px solid magenta',
+        }}
+      >
+        {submissionComponents}
+      </ul>
+    );
+  }
+
+  renderScript(script) {
+    return script.map((item) => {
+      if (item.type === 'T-REVIEW') {
+        return <li>{this.renderReview(item)}</li>;
+      }
+      return (
+        <li>
+          {item.text}
+        </li>
+      );
+    });
   }
 
   render() {
@@ -18,6 +55,9 @@ class SingleAnswer extends Component {
         <h1>
           Single Answer Page
         </h1>
+        <ul>
+          {this.renderScript(this.props.data.questions[this.props.data.current_slide].data.teach.script)}
+        </ul>
         {this.renderNextSlideButton()}
       </div>
     );
