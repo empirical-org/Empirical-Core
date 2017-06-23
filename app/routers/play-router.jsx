672870a99d535a67879bf4ccebfd8c02a22b0ef5
Donald McKendrick <ddmckendrick@gmail.com>
2017-06-23 15:47:33 -0400

    Fix router

--- app/routers/play-router.jsx
+++ app/routers/play-router.jsx
@@ -10,22 +10,13 @@ import StudentDiagnostic from '../components/diagnostics/studentDiagnostic.jsx';
 import ESLDiagnostic from '../components/eslDiagnostic/studentDiagnostic.jsx';
 import PlaySentenceFragment from '../components/sentenceFragments/playSentenceFragment.jsx';
 import Turk from '../components/turk/sentenceFragmentsQuiz.jsx';
+import { getParameterByName } from '../libs/getParameterByName';
 
 const Passthrough = React.createClass({
   render() {
     return this.props.children;
   },
 });
-
-function getParameterByName(name, url) {
-  if (!url) url = window.location.href;
-  name = name.replace(/[\[\]]/g, '\\$&');
-  let regex = new RegExp(`[?&]${name}(=([^&#]*)|&|#|$)`),
-    results = regex.exec(url);
-  if (!results) return null;
-  if (!results[2]) return '';
-  return decodeURIComponent(results[2].replace(/\+/g, ' '));
-}
 
 const PlayRoutes = (
   <Route path="/play" component={StudentRoot}>
