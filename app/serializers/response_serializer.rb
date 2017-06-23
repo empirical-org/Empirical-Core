bcf0cafc44476bc252fcf31f3cdfbfbc532c7004
Donald McKendrick <ddmckendrick@gmail.com>
2017-06-23 11:40:45 -0400

    Fix params and serializer

--- app/serializers/response_serializer.rb
+++ app/serializers/response_serializer.rb
@@ -1,3 +1,3 @@
 class ResponseSerializer < ActiveModel::Serializer
-  attributes :id, :text, :feedback, :count, :child_count, :first_attempt_count, :concept_results, :parent_id
+  attributes :id, :text, :feedback, :count, :child_count, :first_attempt_count, :concept_results, :question_uid, :parent_id
 end
