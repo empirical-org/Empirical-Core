7086b4c425782998c730056d366215f2fa94d7fa
Donald McKendrick <ddmckendrick@gmail.com>
2017-06-26 15:19:10 -0400

    Add missing comma to admin routes index

--- app/routers/Admin/index.js
+++ app/routers/Admin/index.js
@@ -8,7 +8,7 @@ export default {
       System.import(/* webpackChunkName: "admin-lessons" */ './routes/Lessons/index.js'),
       System.import(/* webpackChunkName: "admin-diagsnostic-questions" */ './routes/DiagnosticQuestions/index.js'),
       System.import(/* webpackChunkName: "admin-questions" */ './routes/Questions/index.js'),
-      System.import(/* webpackChunkName: "admin-fill-in-the-blanks" */ './routes/FillInTheBlanks/index.js')
+      System.import(/* webpackChunkName: "admin-fill-in-the-blanks" */ './routes/FillInTheBlanks/index.js'),
       System.import(/* webpackChunkName: "admin-sentence-fragments" */ './routes/SentenceFragments/index.js'),
       System.import(/* webpackChunkName: "admin-item-levels" */ './routes/ItemLevels/index.js')
     ])
