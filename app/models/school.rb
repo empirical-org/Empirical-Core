9a1525da2a8a56a05fe22643724a5fff92d8b373
RyanNovas <ryan@quill.org>
2017-06-29 11:19:07 -0400

    school and user models now have has_one/has_many relationship

--- app/models/school.rb
+++ app/models/school.rb
@@ -1,5 +1,6 @@
 class School < ActiveRecord::Base
-  has_and_belongs_to_many :users
+  has_many :schools_users
+  has_many :users, through: :schools_users
 
   validate :lower_grade_within_bounds, :upper_grade_within_bounds,
            :lower_grade_greater_than_upper_grade
