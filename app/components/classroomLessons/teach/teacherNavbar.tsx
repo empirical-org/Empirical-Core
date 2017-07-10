bc948427265598e4595d6932ad1a11560a59ec7a
Emilia Friedberg <emiliafriedberg@gmail.com>
2017-07-10 14:23:16 -0400

    implement click functionality for exit and help dropdown display

--- app/components/classroomLessons/teach/teacherNavbar.tsx
+++ app/components/classroomLessons/teach/teacherNavbar.tsx
@@ -9,12 +9,15 @@ class TeacherNavbar extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      tooltip: ''
+      tooltip: '',
+      showHelpDropdown: false
     }
 
     this.showTooltip = this.showTooltip.bind(this)
     this.hideTooltip = this.hideTooltip.bind(this)
     this.renderTooltip = this.renderTooltip.bind(this)
+    this.toggleHelpDropdown = this.toggleHelpDropdown.bind(this)
+    this.hideHelpDropdown = this.hideHelpDropdown.bind(this)
   }
 
   presentStudentCount() {
@@ -31,6 +34,14 @@ class TeacherNavbar extends Component {
 
   hideTooltip(icon:string) {
     this.setState({tooltip: ''})
+  }
+
+  toggleHelpDropdown() {
+    this.setState({showHelpDropdown: !this.state.showHelpDropdown})
+  }
+
+  hideHelpDropdown() {
+    this.setState({showHelpDropdown: false})
   }
 
   renderTooltip(icon:string) {
@@ -51,16 +62,55 @@ class TeacherNavbar extends Component {
         }
         break
       case 'help':
-        if (this.state.tooltip === 'help') {
+        if (this.state.tooltip === 'help' && !this.state.showHelpDropdown) {
           return <Tooltip text="Help" className={icon}/>
         }
+        break
       default:
         break
     }
 
   }
 
+  renderHelpDropdown() {
+    if (this.state.showHelpDropdown) {
+      return <div className='help-dropdown'>
+        <i className="fa fa-caret-up"/>
+        <p>Tutorial</p>
+        <hr/>
+        <p>How It Works</p>
+        <hr/>
+        <p>Teacher FAQ</p>
+      </div>
+    }
+  }
+
+  exitLesson() {
+    if (window.confirm('Are you sure you want to exit the lesson?')) {
+      document.location.href = process.env.EMPIRICAL_BASE_URL;
+    }
+  }
+
   render() {
+    let projectorClass, watchTeacherClass, exitClass
+    let helpClass = this.state.showHelpDropdown ? 'hover' : ''
+    switch (this.state.tooltip) {
+      case 'projector':
+        projectorClass = "hover"
+        break
+      case 'watchTeacher':
+        watchTeacherClass = "hover"
+        break
+      case 'exit':
+        exitClass = "hover"
+        break
+      case 'help':
+        helpClass = "hover"
+        break
+      default:
+        break
+    }
+
     return (
       <div className="lessons-teacher-navbar">
         <p className="lesson-title"><span>Lesson 1:</span> Conjunctions of Time</p>
@@ -70,29 +120,34 @@ class TeacherNavbar extends Component {
             onMouseEnter={() => this.showTooltip('projector')}
             onMouseLeave={this.hideTooltip}
           >
-            <img src={projectorIcon} />
+            <img src={projectorIcon} className={projectorClass}/>
             {this.renderTooltip('projector')}
           </div>
           <div
             onMouseEnter={() => this.showTooltip('watchTeacher')}
             onMouseLeave={this.hideTooltip}
           >
-            <img src={watchTeacherIcon}/>
+            <img src={watchTeacherIcon} className={watchTeacherClass}/>
             {this.renderTooltip('watchTeacher')}
           </div>
           <div
             onMouseEnter={() => this.showTooltip('exit')}
             onMouseLeave={this.hideTooltip}
+            onClick={this.exitLesson}
           >
-            <img src={exitIcon}/>
+            <img src={exitIcon} className={exitClass}/>
             {this.renderTooltip('exit')}
           </div>
           <div
             onMouseEnter={() => this.showTooltip('help')}
             onMouseLeave={this.hideTooltip}
+            onClick={this.toggleHelpDropdown}
+            onBlur={this.hideHelpDropdown}
+            tabIndex="0"
           >
-            <img className="help-icon" src={helpIcon}/>
+            <img className={`help-icon ${helpClass}`} src={helpIcon}/>
             {this.renderTooltip('help')}
+            {this.renderHelpDropdown()}
           </div>
         </span>
       </div>
