b04761c2e69222e1b967b61220371ca688b7c495
Donald McKendrick <ddmckendrick@gmail.com>
2017-07-12 11:15:34 -0400

    Types for next question button

--- app/components/classroomLessons/teach/nextSlideButton.tsx
+++ app/components/classroomLessons/teach/nextSlideButton.tsx
@@ -2,8 +2,14 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { getParameterByName } from 'libs/getParameterByName';
 import { goToNextSlide } from '../../../actions/classroomSessions';
+import {
+  ClassroomLessonSession,
+} from '../interfaces'
+import {
+  ClassroomLesson
+} from 'interfaces/classroomLessons'
 
-class NextSlideButton extends React.Component {
+class NextSlideButton extends Component<any, any> {
   constructor(props) {
     super(props);
     this.goToNextSlide = this.goToNextSlide.bind(this);
@@ -11,8 +17,10 @@ class NextSlideButton extends React.Component {
 
   goToNextSlide() {
     const ca_id: string|null = getParameterByName('classroom_activity_id');
+    const sessionData: ClassroomLessonSession = this.props.classroomSessions.data;
+    const lessonData: ClassroomLesson = this.props.classroomLesson.data;
     if (ca_id) {
-      const updateInStore = goToNextSlide(ca_id, this.props.classroomSessions.data, this.props.classroomLesson.data);
+      const updateInStore = goToNextSlide(ca_id, sessionData, lessonData);
       if (updateInStore) {
         this.props.dispatch(updateInStore);
       }
