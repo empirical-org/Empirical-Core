d16502bcbb236fca8b68a44e40874c7995d028a2
Emilia Friedberg <emiliafriedberg@gmail.com>
2017-07-13 12:31:16 -0400

    Added fill in blank question to student container, stripped script tags

--- app/components/classroomLessons/play/container.tsx
+++ app/components/classroomLessons/play/container.tsx
@@ -5,6 +5,7 @@ import CLStudentLobby from './lobby';
 import CLWatchTeacher from './watchTeacher'
 import CLStudentStatic from './static';
 import CLStudentSingleAnswer from './singleAnswer';
+import CLStudentFillInTheBlank from './fillInTheBlank'
 import { saveStudentSubmission } from '../../../actions/classroomSessions';
 import { getClassLessonFromFirebase } from '../../../actions/classroomLesson';
 import { getParameterByName } from 'libs/getParameterByName';
@@ -15,7 +16,10 @@ import {
 } from '../interfaces';
 import {
   ClassroomLesson
-} from '../../../interfaces/classroomLessons'
+} from '../../../interfaces/classroomLessons';
+import {
+  scriptTagStrip
+} from '../shared/scriptTagStrip';
 
 class PlayLessonClassroomContainer extends React.Component<any, any> {
   constructor(props) {
@@ -40,7 +44,8 @@ class PlayLessonClassroomContainer extends React.Component<any, any> {
     const classroom_activity_id: string|null = getParameterByName('classroom_activity_id');
     const student: string|null = getParameterByName('student');
     const current_slide: string = this.props.classroomSessions.data.current_slide;
-    const submission = {data, timestamp}
+    const safeData = scriptTagStrip(data)
+    const submission = {data: safeData, timestamp}
     if (classroom_activity_id && student) {
       saveStudentSubmission(
         classroom_activity_id,
@@ -54,6 +59,9 @@ class PlayLessonClassroomContainer extends React.Component<any, any> {
 
   renderCurrentSlide(data: ClassroomLessonSession, lessonData: ClassroomLesson) {
     const current = lessonData.questions[data.current_slide];
+    const mode: string|null = data.modes && data.modes[data.current_slide] ? data.modes[data.current_slide] : null;
+    const submissions: QuestionSubmissionsList | null = data.submissions && data.submissions[data.current_slide] ? data.submissions[data.current_slide] : null;
+    const selected_submissions = data.selected_submissions && data.selected_submissions[data.current_slide] ? data.selected_submissions[data.current_slide] : null;
     console.log(current.type);
     switch (current.type) {
       case 'CL-LB':
@@ -65,12 +73,14 @@ class PlayLessonClassroomContainer extends React.Component<any, any> {
           <CLStudentStatic data={current.data} />
         );
       case 'CL-SA':
-        const mode: string|null = data.modes && data.modes[data.current_slide] ? data.modes[data.current_slide] : null;
-        const submissions: QuestionSubmissionsList | null = data.submissions && data.submissions[data.current_slide] ? data.submissions[data.current_slide] : null;
-        const selected_submissions = data.selected_submissions && data.selected_submissions[data.current_slide] ? data.selected_submissions[data.current_slide] : null;
         const props = { mode, submissions, selected_submissions, };
         return (
           <CLStudentSingleAnswer data={current.data} handleStudentSubmission={this.handleStudentSubmission} {...props} />
+        );
+      case 'CL-FB':
+      const props = { mode, submissions, selected_submissions, };
+        return (
+          <CLStudentFillInTheBlank data={current.data} handleStudentSubmission={this.handleStudentSubmission} {...props} />
         );
       default:
 
