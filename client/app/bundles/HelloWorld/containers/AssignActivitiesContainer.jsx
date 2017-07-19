f252361553e510f080d7305703dcf798e79161d4
Emilia Friedberg <emiliafriedberg@gmail.com>
2017-07-19 17:48:21 -0400

    add tabs for assign_activities section

--- client/app/bundles/HelloWorld/containers/AssignActivitiesContainer.jsx
+++ client/app/bundles/HelloWorld/containers/AssignActivitiesContainer.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import AssignActivitiesTabs from '../components/lesson_planner/assign_activities_tabs'
 
 export default React.createClass({
 
   render: function() {
     return (
       <div>
+        <AssignActivitiesTabs/>
         {this.props.children}
       </div>)
    }
