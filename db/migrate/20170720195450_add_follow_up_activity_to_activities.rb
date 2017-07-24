0a6deab38d6a5bf56b96bf75d9f8a47df01ce00e
RyanNovas <ryan@quill.org>
2017-07-24 17:16:49 -0400

    fixes broken migration, adds method to update all classrooms

--- db/migrate/20170720195450_add_follow_up_activity_to_activities.rb
+++ db/migrate/20170720195450_add_follow_up_activity_to_activities.rb
@@ -1,5 +1,5 @@
 class AddFollowUpActivityToActivities < ActiveRecord::Migration
   def change
-    add_reference :activities, :follow_up_activity_id, foreign_key: true
+    add_reference :activities, :follow_up_activity, foreign_key: false
   end
 end
