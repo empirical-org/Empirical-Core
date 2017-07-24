86b9570bf2b3bdeb122d4fa820bdcbe340d115aa
Emilia Friedberg <emiliafriedberg@gmail.com>
2017-07-24 18:01:46 -0400

    wip

--- client/app/bundles/HelloWorld/components/dashboard/lessons_list.jsx
+++ client/app/bundles/HelloWorld/components/dashboard/lessons_list.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import request from 'request';
 
+import PreviewOrLaunchModal from '../shared/preview_or_launch_modal'
+
 export default class extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       lessons: null,
+      showModal: false
     };
+
+    this.openModal = this.openModal.bind(this)
   }
 
   componentDidMount() {
@@ -21,20 +26,31 @@ export default class extends React.Component {
     (e, r, lessons) => that.setState({ lessons: JSON.parse(lessons).data, }));
   }
 
+  openModal() {
+    this.setState({showModal: true})
+  }
+
   renderAssignedLessons() {
     return this.state.lessons.map(l =>
       <div key={l.classroom_activity_id}>
+        {this.renderModal(l.classroom_activity_id, l.activity_id)}
         <div className="flex-row space-between vertically-centered lesson-item">
           <div className="flex-row vertically-centered">
             <div className="image-container flex-row space-around vertically-centered">
               <img alt="quill-logo" src="/images/lesson_icon_green.svg" />
             </div>
-            <span className="">{l.activity_name}</span>
+            <span onClick={this.openModal} className="">{l.activity_name}</span>
           </div>
           <a href={`/activity_sessions/anonymous?activity_id=${l.activity_id}`} className="q-button bg-quillgreen text-white">Launch Lesson</a>
         </div>
       </div>
       );
+  }
+
+  renderModal(classroomActivityUID, lessonUID) {
+    if (this.state.showModal) {
+      return <PreviewOrLaunchModal lessonUID classroomActivityUID />
+    }
   }
 
   render() {
