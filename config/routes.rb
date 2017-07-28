a18e235ae117fd263214ff177662a123232b77ce
RyanNovas <ryan@quill.org>
2017-07-28 15:43:24 -0400

    changes lesson recommendation route to go off of activity id and

--- config/routes.rb
+++ config/routes.rb
@@ -133,7 +133,7 @@ EmpiricalGrammar::Application.routes.draw do
       get 'classrooms_with_students/u/:unit_id/a/:activity_id/c/:classroom_id' => 'diagnostic_reports#classrooms_with_students'
       get 'students_by_classroom/u/:unit_id/a/:activity_id/c/:classroom_id' => 'diagnostic_reports#students_by_classroom'
       get 'recommendations_for_classroom/:classroom_id/activity/:activity_id' => 'diagnostic_reports#recommendations_for_classroom'
-      get 'lesson_recommendations_for_classroom/:classroom_activit_id' => 'diagnostic_reports#lesson_recommendations_for_classroom'
+      get 'lesson_recommendations_for_classroom/u/:unit_id/c/:classroom_id/a/:activity_id' => 'diagnostic_reports#lesson_recommendations_for_classroom'
       get 'previously_assigned_recommendations/:classroom_id/activity/:activity_id' => 'diagnostic_reports#previously_assigned_recommendations'
       post 'assign_selected_packs' => 'diagnostic_reports#assign_selected_packs'
 
