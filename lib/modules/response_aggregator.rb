d45865b274f4ba11d5d3f35aaa65d0f1616cdc0a
Donald McKendrick <ddmckendrick@gmail.com>
2017-07-28 11:49:58 -0400

    Add support for health functionality

--- lib/modules/response_aggregator.rb
+++ lib/modules/response_aggregator.rb
@@ -1,6 +1,6 @@
 module ResponseAggregator
 
-  def self.health_of_question(question_uid)
+  def health_of_question(question_uid)
     total_number_of_responses = Response.where(question_uid: question_uid).count
     total_number_of_attempts = Response.where(question_uid: question_uid).sum('count')
     common_matched_attempts = Response.where(question_uid: question_uid).where('count > 4').sum('count')
@@ -13,7 +13,7 @@ module ResponseAggregator
     }
   end
 
-  def self.optimality_counts_of_question(question_uid)
+  def optimality_counts_of_question(question_uid)
     human_optimal = Response.where(question_uid: question_uid, optimal: true, parent_id: nil).count
     human_suboptimal = Response.where(question_uid: question_uid, optimal: false, parent_id: nil).count
     algo_optimal = Response.where(question_uid: question_uid, optimal: true).where.not(parent_id: nil).count
@@ -21,11 +21,11 @@ module ResponseAggregator
     unmatched = Response.where(question_uid: question_uid, optimal: nil, parent_id: nil).count
 
     return {
-      human_optimal: human_optimal,
-      human_suboptimal: human_suboptimal,
-      algo_optimal: algo_optimal,
-      algo_suboptimal: algo_suboptimal,
-      unmatched: unmatched
+      "Human Optimal": human_optimal,
+      "Human Sub-Optimal": human_suboptimal,
+      "Algorithm Optimal": algo_optimal,
+      "Algorithm Sub-Optimal": algo_suboptimal,
+      "Unmatched": unmatched
     }
   end
 
