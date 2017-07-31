abefb78f5d3b00fe6a57fed5588bf3e6a9ef0c19
RyanNovas <ryan@quill.org>
2017-07-31 17:38:46 -0400

    style fix

--- client/app/bundles/HelloWorld/components/progress_reports/diagnostic_reports/lesson_recommendation_row.jsx
+++ client/app/bundles/HelloWorld/components/progress_reports/diagnostic_reports/lesson_recommendation_row.jsx
@@ -21,7 +21,7 @@ export default class LessonsRecommendationRow extends React.Component {
     const rec = this.props.recommendation;
     const activities = rec.activities.map((act, index) => (
       <div key={rec.url} className="activity-row flex-row vertically-centered">
-        <span>{`Lesson ${index + 1}: `}</span>
+        <span>{`Lesson ${index + 1}: ` + ' '}</span>
         <span>{` ${rec.name}`}</span>
         <a href={rec.url}>Preview Lesson</a>
       </div>));
@@ -36,11 +36,12 @@ export default class LessonsRecommendationRow extends React.Component {
     const expandState = this.state.expanded ? 'expanded' : 'collapsed';
     const rec = this.props.recommendation;
     let expandedSection;
+    const recommendationState = rec.percentage_needing_instruction > 50 ? 'recommended' : false;
     if (expandState === 'expanded') {
       expandedSection = this.renderExpandedSection();
     }
     return (
-      <div className={`${expandState} lesson-recommendation-row`}>
+      <div className={`${expandState} ${recommendationState} lesson-recommendation-row`}>
         <div className="top-row flex-row vertically-centered space-between ">
           <div className="left-side flex-row vertically-centered">
             <h3 className={'lesson-name'}>
@@ -52,7 +53,7 @@ export default class LessonsRecommendationRow extends React.Component {
             <span>
               {`${rec.percentage_needing_instruction}% of students need instruction`}
             </span>
-            <a onClick={this.assignLessonPack} className="assign q-button bg-quillgreen text-white">Assign Lesson Pack</a>
+            <a onClick={this.assignLessonPack} className="assign q-button bg-quillgreen text-white">Assign Pack</a>
           </div>
         </div>
         {expandedSection}
