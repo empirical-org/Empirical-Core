461930723fb0ee4a79aab16661b0ca4a6ddaf939
Emilia Friedberg <emiliafriedberg@gmail.com>
2017-07-31 17:11:21 -0400

    style first tutorial slide

---
+++ client/app/bundles/HelloWorld/components/tutorials/LessonsSlides.jsx
@@ -0,0 +1,69 @@
+import React from 'react'
+
+const SlideOne =() => {
+  return <div className='slide lessons-slide-one'>
+    <div className="content">
+      <div className="image">
+        <img className="illustration" src="http://localhost:45537/images/tutorials/lessons/slide_1.svg" />
+        <img className="caption" src="http://localhost:45537/images/tutorials/lessons/slide_1_text.svg" />
+      </div>
+      <div className="text">
+        <h1>Class-Wide Interactive Activities</h1>
+        <p><span>Quill Lessons</span> provides a series of interactive slides that enable you to introduce new writing concepts to your students. Each slide contains a step-by-step guide to help you present each concept.</p>
+      </div>
+    </div>
+  </div>
+}
+
+const SlideTwo =() => {
+  return <div className='slide lessons-slide-two'>
+    <img className="illustration" src="http://localhost:45537/images/tutorials/lessons/slide_2.svg" />
+    <img src="http://localhost:45537/images/tutorials/lessons/slide_2_text.svg" />
+    <div className="text">
+      <h1>Lead The Lesson</h1>
+      <p>You control the pacing - all of the students see the slide you project, and you can select another slide on the left panel.</p>
+    </div>
+  </div>
+}
+
+const SlideThree =() => {
+  return <div className='slide lessons-slide-three'>
+    <img className="illustration" src="http://localhost:45537/images/tutorials/lessons/slide_3.svg" />
+    <img src="http://localhost:45537/images/tutorials/lessons/slide_3_text.svg" />
+    <div className="text">
+      <h1>Discuss Student Answers</h1>
+      <p> See your student answers in real-time, and facilitate a conversation by selecting both strong and weak answers to project to the class. The students can then discuss as pairs and as a class why certain sentences are stronger than others.</p>
+    </div>
+  </div>
+}
+
+const SlideFour =() => {
+  return <div className='slide lessons-slide-four'>
+    <img className="illustration" src="http://localhost:45537/images/tutorials/lessons/slide_4.svg" />
+    <img src="http://localhost:45537/images/tutorials/lessons/slide_4_text.svg" />
+    <div className="text">
+      <h1>Identify Students For Small Group</h1>
+      <p>When the students respond, you can click on the “flag” button to select students who would benefit from follow up small group instruction. At the end of the lesson, you can pull aside the flagged students for small group instruction.</p>
+    </div>
+  </div>
+}
+
+const SlideFive =() => {
+  return <div className='slide lessons-slide-five'>
+    <div className="text">
+      <h1>Class-Wide Interactive Activities</h1>
+      <p><span>Quill Lessons</span> provides a series of interactive slides that enable you to introduce new writing concepts to your students. Each slide contains a step-by-step guide to help you present each concept.</p>
+    </div>
+  </div>
+}
+
+const SlideSix =() => {
+  return <div className='slide lessons-slide-six'>
+    <div className="text">
+      <h1>Class-Wide Interactive Activities</h1>
+      <p><span>Quill Lessons</span> provides a series of interactive slides that enable you to introduce new writing concepts to your students. Each slide contains a step-by-step guide to help you present each concept.</p>
+    </div>
+  </div>
+}
+
+export default [SlideOne, SlideTwo, SlideThree, SlideFour, SlideFive, SlideSix]
