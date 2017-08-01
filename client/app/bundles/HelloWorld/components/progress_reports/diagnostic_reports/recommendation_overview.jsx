b65c1a8b5a0582627a176b919d9f060193f92d2d
RyanNovas <ryan@quill.org>
2017-08-01 15:47:48 -0400

    adds svg links from cdn

--- client/app/bundles/HelloWorld/components/progress_reports/diagnostic_reports/recommendation_overview.jsx
+++ client/app/bundles/HelloWorld/components/progress_reports/diagnostic_reports/recommendation_overview.jsx
@@ -22,8 +22,13 @@ export default class RecommendationsOverview extends React.Component {
         <h3>Quill now provides both <strong>Group Lessons</strong> and <strong>Independent Practice</strong></h3>
         <p>In the new Quill Lessons tool, you can teach writing skills to your class through a set of teacher-led slides.</p>
         <div className="flex-row space-between button-group">
-          <a onClick={this.scrollToLessons} className="q-button text-quillblue bg-white">View Group Lessons</a>
-          <a onClick={this.scrollToIndependent} className="q-button text-quillblue bg-white">View Independent Practice</a>
+          <a onClick={this.scrollToLessons} className="q-button text-quillblue bg-white">
+            <img src="https://assets.quill.org/images/icons/group-lesson-icon-blue.svg" alt="group-lesson-logo" />
+            View Group Lessons
+          </a>
+          <a onClick={this.scrollToIndependent} className="q-button text-quillblue bg-white">
+            <img src="https://assets.quill.org/images/icons/independent-lesson-black.svg" alt="independent-lesson-logo" />
+            View Independent Practice</a>
         </div>
       </div>);
   }
