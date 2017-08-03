418448b0384955bce2e79906ac502a22e13e869a
Emilia Friedberg <emiliafriedberg@gmail.com>
2017-08-03 10:47:44 -0400

    render basic structure of QuestionAndAnswer section

---
+++ client/app/bundles/Home/startup/clientRegistration.jsx
@@ -0,0 +1,4 @@
+import ReactOnRails from 'react-on-rails';
+import LessonsQuestionsAndAnswersApp from './LessonsQuestionsAndAnswersAppClient.jsx'
+
+ReactOnRails.register({ LessonsQuestionsAndAnswersApp });
