0307e00ea153ff021e6dcc6ef9ae87c0a6f442cb
RyanNovas <ryan@quill.org>
2017-08-03 09:45:27 -0400

    rescue commit 4

---
+++ client/app/bundles/HelloWorld/components/lesson_planner/create_unit/stage2/name_the_unit.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import $ from 'jquery';
+
+export default React.createClass({
+
+  updateName(e) {
+    const unitName = $(this.refs.name).val();
+    this.props.updateUnitName(unitName);
+  },
+
+  render() {
+    return (
+      <section className="section-content-wrapper name-the-unit">
+        <h3 className="section-header">Name Your Activity Pack:</h3>
+        <input className={this.props.nameError} id="unit_name" ref="name" onChange={this.updateName} value={this.props.unitName} type="text" placeholder="e.g. Learning How to Use Nouns" />
+      </section>
+    );
+  },
+
+});
