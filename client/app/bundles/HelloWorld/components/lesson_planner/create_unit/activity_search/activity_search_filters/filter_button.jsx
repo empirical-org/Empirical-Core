daab130865fb6c5a60049a7c7b04785225869e25
Ryan Novas <RyanNovas@users.noreply.github.com>
2017-08-04 11:31:21 -0400

    Revert "Feat/new custom activities view"

--- client/app/bundles/HelloWorld/components/lesson_planner/create_unit/activity_search/activity_search_filters/filter_button.jsx
+++ client/app/bundles/HelloWorld/components/lesson_planner/create_unit/activity_search/activity_search_filters/filter_button.jsx
@@ -1,18 +1,21 @@
-import React from 'react';
+'use strict'
 
-export default React.createClass({
+import React from 'react'
+
+export default  React.createClass({
 
   propTypes: {
     handleFilterButtonClick: React.PropTypes.func.isRequired,
     data: React.PropTypes.object.isRequired,
-    active: React.PropTypes.bool,
+    active: React.PropTypes.bool
   },
 
-  handleClick() {
-    this.props.handleFilterButtonClick(this.props.data.id);
+
+  handleClick: function() {
+    this.props.handleFilterButtonClick(this.props.data.id)
   },
 
-  iconType() {
+  iconType: function() {
     let type = this.props.data.key;
     if (type === 'passage') {
       type = 'flag';
@@ -22,32 +25,31 @@ export default React.createClass({
     return type;
   },
 
-  description() {
-    switch (this.props.data.key) {
-      case 'connect':
+  description: function() {
+    switch(this.props.data.key) {
+    case 'connect':
         return 'Combine Sentences';
-      case 'sentence':
-        return 'Practice Mechanics';
-      case 'passage':
-        return 'Fix Errors in Passages';
-      case 'diagnostic':
+    case 'sentence':
+        return 'Practice Basic Grammar';
+    case 'passage':
+        return 'Find & Fix Errors in Passages';
+    case 'diagnostic':
         return 'Identify Learning Gaps';
-      case 'lessons':
-        return 'Shared Group Lessons';
-    }
+      }
   },
 
-  render() {
-    const active = this.props.active ? 'active' : null;
+  render: function() {
+    let active = this.props.active ? 'active' : null;
     return (
-      <button className={active} onClick={() => this.handleClick()}>
-        <div className={`icon-${this.iconType()}-gray`} />
+      <button className={active} onClick={()=> this.handleClick()}>
+        <div className={`icon-${this.iconType()}-gray`}></div>
         <div>
           <h4>{this.props.data.alias}</h4>
           <p>{this.description()}</p>
         </div>
       </button>
     );
-  },
+  }
+
 
 });
