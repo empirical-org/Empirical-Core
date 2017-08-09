a04c212e5cdc6779e31776eeb853c44a9760d871
RyanNovas <ryan@quill.org>
2017-08-09 16:35:49 -0400

    fixes places where schools were not uploading right, adds unique index

--- app/services/clever_integration/importers/school.rb
+++ app/services/clever_integration/importers/school.rb
@@ -4,7 +4,8 @@ module CleverIntegration::Importers::School
     clever_teacher = teacher_requester.call(teacher.clever_id, district_token)
     parsed_response = CleverIntegration::Parsers::School.run(clever_teacher.school)
     school = CleverIntegration::Creators::School.run(parsed_response)
-    teacher.update(school: school)
+    su = SchoolsUsers.find_or_initialize_by(user_id: teacher.id)
+    su.update(school_id: school.id)
     school
   end
 
