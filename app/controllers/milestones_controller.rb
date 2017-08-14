8136f97383e4f90c8b3e63af2e059681d29470c5
Emilia Friedberg <emiliafriedberg@gmail.com>
2017-08-14 11:59:37 -0400

    fix route to get activity for classroom lesson

--- app/controllers/milestones_controller.rb
+++ app/controllers/milestones_controller.rb
@@ -7,7 +7,7 @@ class MilestonesController < ApplicationController
 
   def complete_view_lesson_tutorial
     milestone = Milestone.find_by(name: 'View Lessons Tutorial')
-    if !::UserMilestone.find_by(user: current_user, milestone: milestone)
+    if milestone.users.find_by(id: current_user.id)
       current_user.milestones.push(milestone)
     end
     render json: {}
