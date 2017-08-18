48facc73747d462e3a599a3cc9c216c331eb8e80
Donald McKendrick <ddmckendrick@gmail.com>
2017-08-18 16:01:08 -0400

    Show the slides

--- app/components/classroomLessons/admin/show.tsx
+++ app/components/classroomLessons/admin/show.tsx
@@ -6,11 +6,26 @@ class ShowClassroomLesson extends Component {
     super(props);
   }
 
+  classroomLesson() {
+    return this.props.classroomLessons.data[this.props.routeParams.classroomLessonID]
+  }
+
+  renderSlides() {
+    if (this.props.classroomLessons.hasreceiveddata) {
+      const questions = this.classroomLesson().questions
+      const slides = Object.keys(questions).map(key => {
+        return <li key={key}>Type: {questions[key].type}, Title: {questions[key].data.teach.title}</li>
+      }
+      return <ul>{slides}</ul>
+    }
+  }
+
   render() {
-    const title = this.props.classroomLessons.hasreceiveddata ? this.props.classroomLessons.data[this.props.routeParams.classroomLessonID].title : 'Loading'
+    const title = this.props.classroomLessons.hasreceiveddata ? this.classroomLesson().title : 'Loading'
     return (
       <div>
         <h1>{title}</h1>
+        {this.renderSlides()}
       </div>
     )
   }
