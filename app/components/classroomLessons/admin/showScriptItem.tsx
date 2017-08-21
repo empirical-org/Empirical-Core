4f2f42a88c7bddfd1b295ae6399f80db7f7e537f
Donald McKendrick <ddmckendrick@gmail.com>
2017-08-21 11:04:12 -0400

    changing input field on scriptItem updates preview

--- app/components/classroomLessons/admin/showScriptItem.tsx
+++ app/components/classroomLessons/admin/showScriptItem.tsx
@@ -1,5 +1,6 @@
 import React, {Component} from 'react'
 import { connect } from 'react-redux';
+import _ from 'lodash';
 import {
   getClassroomLessonScriptItem
 } from './helpers';
@@ -10,6 +11,14 @@ import ScriptComponent from '../shared/scriptComponent'
 class showScriptItem extends Component<any, any> {
   constructor(props){
     super(props);
+
+    this.state = {}
+  }
+
+  componentDidUpdate() {
+    if (this.props.classroomLessons.hasreceiveddata && !this.state.scriptItem) {
+      this.setState({scriptItem: this.getCurrentScriptItem()})
+    }
   }
 
   getCurrentScriptItem(): IntF.ScriptItem {
@@ -17,9 +26,36 @@ class showScriptItem extends Component<any, any> {
     return getClassroomLessonScriptItem(this.props.classroomLessons.data, classroomLessonID, slideID, scriptItemID)
   }
 
+  updateValue(e, value) {
+    const newScriptItem = _.merge({}, this.state.scriptItem)
+    newScriptItem.data[value] = e.target.value
+    this.setState({scriptItem: newScriptItem})
+  }
+
+  // updateBody(e) {
+  //   const newScriptItem = _.merge({}, this.state.scriptItem)
+  //   newScriptItem.data.body = e.target.value
+  //   this.setState({scriptItem: newScriptItem})
+  // }
+  //
+  renderForm() {
+    switch (this.state.scriptItem.type) {
+      case 'STEP-HTML':
+        return (<div>
+          <textarea onChange={(e) => this.updateValue(e, 'heading')} value={this.state.scriptItem.data.heading}></textarea>
+          <textarea onChange={this.updateBody} value={this.state.scriptItem.data.body}></textarea>
+        </div>)
+    }
+  }
+
   render() {
-    if (this.props.classroomLessons.hasreceiveddata) {
-      return <ScriptComponent script={[this.getCurrentScriptItem()]}
+    if (this.state.scriptItem) {
+      return (
+        <div>
+          <ScriptComponent script={[this.state.scriptItem]} />
+          {this.renderForm()}
+        </div>
+      )
       // return (
       //   <div>
       //     {this.getCurrentScriptItem().data.heading}
