8a1e2092b1c76c15b5ac259702dea3824cb8f1b0
Donald McKendrick <ddmckendrick@gmail.com>
2017-08-21 18:37:20 -0400

    Prevent firing Classrooms associator before student is saved.

--- app/services/google_integration/classroom/creators/students.rb
+++ app/services/google_integration/classroom/creators/students.rb
@@ -54,12 +54,14 @@ module GoogleIntegration::Classroom::Creators::Students
                        password: data[:last_name],
                        username: username,
                        signed_up_with_google: true)
-        StudentJoinedClassroomWorker.perform_async(classroom.teacher_id, student.id)
       end
       if student.errors.any?
         puts "Error: Could not save google classroom student."
         puts data[:email]
+        puts username
         puts student.errors.full_messages
+        puts 'classroom of errored students'
+        puts classroom.attributes
       else
         data[:classrooms].each do |id|
           classroom = Classroom.find(id)
