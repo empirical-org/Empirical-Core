e67a36156d49053e514fe168ff216dcfd4a01f17
Donald McKendrick <ddmckendrick@gmail.com>
2017-08-21 10:36:48 -0400

    render script component on scriptitem page

--- app/components/classroomLessons/admin/showScriptItem.tsx
+++ app/components/classroomLessons/admin/showScriptItem.tsx
@@ -5,6 +5,8 @@ import {
 } from './helpers';
 import * as IntF from '../interfaces';
 
+import ScriptComponent from '../shared/scriptComponent'
+
 class showScriptItem extends Component<any, any> {
   constructor(props){
     super(props);
@@ -17,11 +19,12 @@ class showScriptItem extends Component<any, any> {
 
   render() {
     if (this.props.classroomLessons.hasreceiveddata) {
-      return (
-        <div>
-          {this.getCurrentScriptItem().data.heading}
-        </div>
-      )
+      return <ScriptComponent script={[this.getCurrentScriptItem()]}
+      // return (
+      //   <div>
+      //     {this.getCurrentScriptItem().data.heading}
+      //   </div>
+      // )
     } else {
       return (
         <p>Loading...</p>
