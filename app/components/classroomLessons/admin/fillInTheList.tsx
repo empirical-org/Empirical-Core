cb859bab3ade79af021c4f98b029903d225f08ff
RyanNovas <ryan@quill.org>
2017-08-22 15:02:06 -0400

    adds admin capabilities to all other slide types

--- app/components/classroomLessons/admin/fillInTheList.tsx
+++ app/components/classroomLessons/admin/fillInTheList.tsx
@@ -1,14 +1,133 @@
 import React, {Component} from 'react'
+import * as IntF from '../interfaces';
+import _ from 'lodash'
+import MultipleTextEditor from '../shared/multipleTextEditor'
 
-class AdminFillInTheList extends Component<any, any> {
+interface SingleAnswerProps {
+  question: IntF.QuestionData,
+
+}
+
+class AdminFillInTheList extends Component<SingleAnswerProps, any>{
   constructor(props){
     super(props);
+
+    this.state = {
+      question: this.props.question
+    }
+
+    this.handleTitleChange = this.handleTitleChange.bind(this)
+    this.handlePromptChange = this.handlePromptChange.bind(this)
+    this.handleInstructionsChange = this.handleInstructionsChange.bind(this)
+    this.handleCuesChange = this.handleCuesChange.bind(this)
+    this.save = this.save.bind(this)
+  }
+
+  handleTitleChange(e) {
+    const newVals = Object.assign(
+      {},
+      this.state.question
+    );
+    _.set(newVals, 'teach.title', e.target.value)
+    this.setState({question: newVals})
+  }
+
+  handlePromptChange(e) {
+    const newVals = Object.assign(
+      {},
+      this.state.question
+    );
+    _.set(newVals, 'play.prompt', e)
+    this.setState({question: newVals})
+  }
+
+  handleInstructionsChange(e) {
+    const newVals = Object.assign(
+      {},
+      this.state.question
+    );
+    _.set(newVals, 'play.instructions', e.target.value)
+    this.setState({question: newVals})
+  }
+
+  handleBlankLableChange(e) {
+    const newVals = Object.assign(
+      {},
+      this.state.question
+    );
+    _.set(newVals, 'play.blankLabel', e.target.value)
+    this.setState({question: newVals})
+  }
+
+  handleCuesChange(e) {
+    const newVals = Object.assign(
+      {},
+      this.state.question
+    );
+    const formattedCues = Object.assign({}, e.target.value.split(','));
+    _.set(newVals, 'play.cues', formattedCues)
+    this.setState({question: newVals})
+  }
+
+  handleNBlanks(e) {
+    const newVals = Object.assign(
+      {},
+      this.state.question
+    );
+    const nBlanks = Object.assign({}, Number(e.target.value));
+    _.set(newVals, 'play.nBlanks', nBlanks)
+    this.setState({question: newVals})
+  }
+
+
+
+  save() {
+    this.props.save(this.state.question)
   }
 
   render() {
     return (
-      <div>
-        Fill In The List
+      <div style={{marginTop: 30, marginBottom: 30}}>
+        <div className="field">
+          <label className="label">Title</label>
+          <div className="control">
+            <input value={this.state.question.teach.title} onChange={this.handleTitleChange} className="input" type="text" placeholder="Text input"/>
+          </div>
+        </div>
+        <div className="field">
+          <label className="label">Prompt</label>
+          <div className="control">
+            <MultipleTextEditor
+              text={this.state.question.play.prompt}
+              handleTextChange={(e) => this.handlePromptChange(e)}
+            />
+          </div>
+        </div>
+        <div className="field">
+          <label className="label">Instructions (Optional)</label>
+          <div className="control">
+            <input value={this.state.question.play.instructions} onChange={this.handleInstructionsChange} className="input" type="text" placeholder="Text input"/>
+          </div>
+        </div>
+        <div className="field">
+          <label className="label">Cues comma seperated (Optional)</label>
+          <div className="control">
+            <input value={Object.values(this.state.question.play.cues || {}).join(',')} onChange={this.handleCuesChange} className="input" type="text" placeholder="Text input"/>
+          </div>
+        </div>
+        <div className="field">
+          <label className="label">Blank Label</label>
+          <div className="control">
+            <input value={this.state.question.play.blankLabel} onChange={this.handleBlankLableChange} className="input" type="text" placeholder="Text input"/>
+          </div>
+        </div>
+        <div className="field">
+          <label className="label">Number of Blanks</label>
+          <div className="control">
+            <input value={this.state.question.play.nBlanks} onChange={this.handleNBlanks} className="input" type="text" placeholder="Text input"/>
+          </div>
+        </div>
+        <button onClick={this.save}>Save Changes</button>
       </div>
     )
   }
