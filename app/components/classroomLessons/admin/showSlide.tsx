645db7023240a0bcce64e6a1ab5e2c82a4cbf4f9
Emilia Friedberg <emiliafriedberg@gmail.com>
2017-08-22 16:33:54 -0400

    admins cannot delete lobby or exit slides from lessons

--- app/components/classroomLessons/admin/showSlide.tsx
+++ app/components/classroomLessons/admin/showSlide.tsx
@@ -72,6 +72,7 @@ class ShowClassroomLessonSlide extends Component<any, any> {
   render() {
     if (this.props.classroomLessons.hasreceiveddata) {
       const Component = getComponent(this.currentSlide().type)
+      const deleteButton = this.currentSlide().type === 'CL-LB' || this.currentSlide().type === 'CL-EX' ? <span /> : <button onClick={this.deleteSlide}>Delete Slide</button>
       return (
         <div>
           <h4 className="title is-4">
@@ -80,7 +81,7 @@ class ShowClassroomLessonSlide extends Component<any, any> {
           <h5 className="title is-5">
             {this.currentSlide().data.teach.title}
           </h5>
-          <button onClick={this.deleteSlide}>Delete Slide</button>
+          {deleteButton}
           <p>{getComponentDisplayName(this.currentSlide().type)}</p>
           <Component question={this.currentSlide().data} save={this.save}/>
           <Script
