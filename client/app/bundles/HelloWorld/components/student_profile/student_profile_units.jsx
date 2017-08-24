c6276cb35c7f6260b33095484557aa53a3aa5e1e
RyanNovas <ryan@quill.org>
2017-08-24 16:08:56 -0400

    updated query and student profile logic

--- client/app/bundles/HelloWorld/components/student_profile/student_profile_units.jsx
+++ client/app/bundles/HelloWorld/components/student_profile/student_profile_units.jsx
@@ -1,24 +1,40 @@
-'use strict';
-import React from 'react'
-import _ from 'underscore'
-import StudentProfileUnit from './student_profile_unit.jsx'
-
+import React from 'react';
+import _ from 'underscore';
+import StudentProfileUnit from './student_profile_unit.jsx';
 
 export default React.createClass({
 
-  propTypes: {
-    data: React.PropTypes.object.isRequired
+  groupUnits() {
+    const groupedUnits = _.groupBy(this.props.data, 'unit_id');
+    const unitsWithGroupedActivities = {};
+    for (const unit in groupedUnits) {
+      const partitionedActivities = _.partition(groupedUnits[unit], activity => (activity.max_percentage != null));
+      unitsWithGroupedActivities[unit] = {};
+      if (partitionedActivities[0].length) {
+        unitsWithGroupedActivities[unit].complete = _.sortBy(partitionedActivities[0], 'classroom_activity_created_at');
+      }
+      if (partitionedActivities[1].length) {
+        unitsWithGroupedActivities[unit].incomplete = _.sortBy(partitionedActivities[1], 'act_sesh_updated_at');
+      }
+    }
+    const unitsGroupedByCompletion = _.partition(unitsWithGroupedActivities, unit => (!!unit.incomplete));
+    const finalUnitGrouping = {};
+    finalUnitGrouping.incomplete = unitsGroupedByCompletion[0].sort((a, b) => a.incomplete[0].unit_created_at - b.incomplete[0].unit_created_at);
+    finalUnitGrouping.complete = unitsGroupedByCompletion[1].sort((a, b) => a.complete[0].unit_created_at - b.complete[0].unit_created_at);
+    console.log(finalUnitGrouping);
+    return finalUnitGrouping;
   },
 
-  render: function () {
-    var units = _.reduce(this.props.data, function (acc, value, key) {
-      var x = <StudentProfileUnit key={key} data={_.extend(value, {unitName: key})} />
-      return _.chain(acc).push(x).value();
-    }, []);
+  render() {
+    console.log('props', this.groupUnits());
+    // var units = _.reduce(this.props.data, function (acc, value, key) {
+    //   var x = <StudentProfileUnit key={key} data={_.extend(value, {unitName: key})} />
+    //   return _.chain(acc).push(x).value();
+    // }, []);
     return (
-      <div className='container'>
-        {this.props.loading ? null : units}
+      <div className="container">
+        {this.props.loading ? null : 'HIEHEHEHEH'}
       </div>
     );
-  }
+  },
 });
--- client/app/bundles/HelloWorld/containers/StudentProfile.jsx
+++ client/app/bundles/HelloWorld/containers/StudentProfile.jsx
@@ -1,61 +1,49 @@
-import React from 'react'
-import $ from 'jquery'
-import _ from 'underscore'
-import StudentClassroomNavbar from '../components/student_profile/student_classroom_navbar.jsx'
-import NextActivity from '../components/student_profile/next_activity.jsx'
-import StudentProfileUnits from '../components/student_profile/student_profile_units.jsx'
-import StudentProfileHeader from '../components/student_profile/student_profile_header'
-import Setter from '../components/modules/setter.jsx'
+import React from 'react';
+import $ from 'jquery';
+import _ from 'underscore';
+import StudentClassroomNavbar from '../components/student_profile/student_classroom_navbar.jsx';
+import NextActivity from '../components/student_profile/next_activity.jsx';
+import StudentProfileUnits from '../components/student_profile/student_profile_units.jsx';
+import StudentProfileHeader from '../components/student_profile/student_profile_header';
+import Setter from '../components/modules/setter.jsx';
 
 export default React.createClass({
-  getInitialState: function () {
+  getInitialState() {
     this.modules = {
       setter: new Setter(),
       // scrollify: new EC.modules.scrollify()
     };
     return {
-      next_activity_session: {activity: {}},
-      student: {classroom: {teacher: {}}},
-      grouped_scores: {},
-      is_last_page: false,
-      currentPage: 0,
-      firstBatchLoaded: false,
-      loading: false
+      loading: true,
     };
   },
 
-  componentDidMount: function () {
+  componentDidMount() {
     // this.modules.scrollify.scrollify('#page-content-wrapper', this)
-    this.setState({loading: true})
+    this.setState({ loading: true, });
     this.fetchData();
   },
 
-  fetchData: function (currentClassroom) {
-    this.setState({currentClassroom: currentClassroom});
-    this.setState({loading: true})
-    $.ajax({url: '/student_profile_data', data: {current_classroom_id: currentClassroom}, format: 'json', success: this.loadProfile})
+  fetchData(currentClassroom) {
+    this.setState({ currentClassroom, });
+    this.setState({ loading: true, });
+    $.ajax({ url: '/student_profile_data', data: { current_classroom_id: currentClassroom, }, format: 'json', success: this.loadProfile, });
   },
 
-  loadProfile: function (data) {
-    // commented out lines are no longer necessary as we don't do infinite scroll any longer
-    // they will need to be refactored if we turn it back on, as they are not compatible with multiple classrooms
-    // //  need to deep merge the grouped_scores
-    // var mergeArrays, merged;
-    // mergeArrays = true;
-    // merged = this.modules.setter.setOrExtend(this.state, null, data, mergeArrays)
-    this.setState(_.extend(data, {ajaxReturned: true, loading: false, firstBatchLoaded: true}))
+  loadProfile(data) {
+    this.setState({ loading: false, scores: data.scores, student: data.student, });
   },
 
-  render: function () {
-    if (this.state.firstBatchLoaded) {
+  render() {
+    if (!this.state.loading) {
       return (
-          <div id="student-profile">
-            <StudentClassroomNavbar data={this.state.student} fetchData={this.fetchData} loading={this.state.loading} />
-            <StudentProfileHeader studentName={this.state.student.name} classroomName={this.state.student.classroom.name} teacherName={this.state.student.classroom.teacher.name}/>
-            <NextActivity data={this.state.next_activity_session} loading={this.state.loading} hasActivities={!(_.isEmpty(this.state.grouped_scores))}/>
-            <StudentProfileUnits data={this.state.grouped_scores} loading={this.state.loading}/>
-          </div>
-      )
-    } else return <span></span>
-  }
+        <div id="student-profile">
+          <StudentClassroomNavbar data={this.state.student} fetchData={this.fetchData} loading={this.state.loading} />
+          <StudentProfileHeader studentName={this.state.student.name} classroomName={this.state.student.classroom.name} teacherName={this.state.student.classroom.teacher.name} />
+          <NextActivity data={this.state.next_activity_session} loading={this.state.loading} hasActivities={!(_.isEmpty(this.state.grouped_scores))} />
+          <StudentProfileUnits data={this.state.scores} loading={this.state.loading} />
+        </div>
+      );
+    } return <span />;
+  },
 });
