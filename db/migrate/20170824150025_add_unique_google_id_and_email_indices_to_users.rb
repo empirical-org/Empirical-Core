b7a8c08f16707769e6551d9aa161d92e9c0d3d74
Donald McKendrick <ddmckendrick@gmail.com>
2017-08-25 12:38:34 -0400

    Fix migrations

--- db/migrate/20170824150025_add_unique_google_id_and_email_indices_to_users.rb
+++ db/migrate/20170824150025_add_unique_google_id_and_email_indices_to_users.rb
@@ -1,6 +1,6 @@
 class AddUniqueGoogleIdAndEmailIndicesToUsers < ActiveRecord::Migration
   def change
-    add_index :users, :email, unique: true, name: 'unique_index_users_on_email', where: "id > 1625425 AND email IS NOT null"
-    add_index :users, :google_id, unique: true, name: 'unique_index_users_on_google_id', where: "id > 1625425 and google_id IS NOT null"
+    add_index :users, :email, unique: true, name: 'unique_index_users_on_email', where: "id > 1641954 AND email IS NOT null AND email <>  '' "
+    add_index :users, :google_id, unique: true, name: 'unique_index_users_on_google_id', where: "id > 1641954 and google_id IS NOT null AND google_id <> '' "
   end
 end
--- db/migrate/20170824171451_add_unique_user_name_index_to_users.rb
+++ db/migrate/20170824171451_add_unique_user_name_index_to_users.rb
@@ -1,5 +1,5 @@
 class AddUniqueUserNameIndexToUsers < ActiveRecord::Migration
   def change
-    add_index :users, :username, unique: true, name: 'unique_index_users_on_username', where: "id > 1625425 and username IS NOT null"
+    add_index :users, :username, unique: true, name: 'unique_index_users_on_username', where: "id > 1641954 and username IS NOT null AND username <>  '' "
   end
 end
