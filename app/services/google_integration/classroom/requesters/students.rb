9b4a3a34265bf0bc01e965b467885cbd96c91d73
RyanNovas <ryan@quill.org>
2017-08-30 16:27:02 -0400

    converts google classroom query to functional method

--- app/services/google_integration/classroom/requesters/students.rb
+++ app/services/google_integration/classroom/requesters/students.rb
@@ -1,32 +1,30 @@
 module GoogleIntegration::Classroom::Requesters::Students
 
   def self.generate(client)
-    @@client = client
     lambda do |course_id|
-      @@students = []
-      @@course_id = course_id
+      students = []
       service = client.discovered_api('classroom', 'v1')
-      @@api_method = service.courses.students.list
-      self.make_google_classroom_api_call
+      api_method = service.courses.students.list
+      self.make_google_classroom_api_call(course_id, api_method, client, students)
     end
   end
 
-  def self.make_google_classroom_api_call(page_token=nil)
+  def self.make_google_classroom_api_call(course_id, api_method, client, students, page_token=nil)
     # google classroom inconsistenly caps the students returned at 30
     # to get around this, we request 30 students, and then recursively
     # keep calling on the classroom with the updated page token until
     # there is not another page token
     # in each iteration, we concat the latest student response value
     # to an array of previous ones from within the course
-    parameters = {'courseId' => @@course_id, 'pageSize'=> 30}
+    parameters = {'courseId' => course_id, 'pageSize'=> 30}
     page_token ? parameters['pageToken'] = page_token : nil
-    response = @@client.execute(api_method: @@api_method,
+    response = client.execute(api_method: api_method,
                               parameters: parameters)
-    @@students ||= []
-    @@students.concat(JSON.parse(response.body)['students'] || [])
+    students ||= []
+    students.concat(JSON.parse(response.body)['students'] || [])
     if response.next_page_token
-      return self.make_google_classroom_api_call(response.next_page_token)
+      return self.make_google_classroom_api_call(course_id, api_method, client, students, response.next_page_token)
     end
-    @@students
+    students
   end
 end
