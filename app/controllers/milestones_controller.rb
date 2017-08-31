f3a97c5e03c53e55bbfd8b2a2efd34340da9711c
Donald McKendrick <ddmckendrick@gmail.com>
2017-08-31 15:16:11 -0400

    Make sure current user exists.

--- app/controllers/milestones_controller.rb
+++ app/controllers/milestones_controller.rb
@@ -1,9 +1,11 @@
 class MilestonesController < ApplicationController
 
   def complete_view_lesson_tutorial
-    milestone = Milestone.find_by(name: 'View Lessons Tutorial')
-    if !milestone.users.find_by(id: current_user.id)
-      current_user.milestones.push(milestone)
+    if current_user
+      milestone = Milestone.find_by(name: 'View Lessons Tutorial')
+      if !milestone.users.find_by(id: current_user.id)
+        current_user.milestones.push(milestone)
+      end
     end
     render json: {}
   end
