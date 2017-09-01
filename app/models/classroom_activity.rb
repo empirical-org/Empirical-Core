b107c976e752bb1fdac84c3e651731a2f4dbaa77
Donald McKendrick <ddmckendrick@gmail.com>
2017-08-31 23:09:23 -0400

    Save a trip to the database for an id we already have.

--- app/models/classroom_activity.rb
+++ app/models/classroom_activity.rb
@@ -26,16 +26,16 @@ class ClassroomActivity < ActiveRecord::Base
 
   def assign_follow_up_lesson(locked=true)
     extant_ca = ClassroomActivity.find_by(classroom_id: self.classroom_id,
-                                          activity_id: self.activity.follow_up_activity.id,
+                                          activity_id: self.activity.follow_up_activity_id,
                                           unit_id: self.unit_id)
-    if !self.activity.follow_up_activity
-      return
+    if !self.activity.follow_up_activity_id
+      return false
     elsif extant_ca
       extant_ca.update(locked: false)
       return extant_ca
     end
     follow_up = ClassroomActivity.create(classroom_id: self.classroom_id,
-                             activity_id: self.activity.follow_up_activity.id,
+                             activity_id: self.activity.follow_up_activity_id,
                              unit_id: self.unit_id,
                              visible: true,
                              locked: locked,
