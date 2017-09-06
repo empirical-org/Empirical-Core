71fe4e4a9d41b492ac0bdb2ae9b3e2fe48b636f3
Emilia Friedberg <emiliafriedberg@gmail.com>
2017-09-06 14:00:06 -0400

    hide input fields and submit buttons for projector mode

--- app/components/classroomLessons/play/listBlanks.tsx
+++ app/components/classroomLessons/play/listBlanks.tsx
@@ -174,11 +174,13 @@ class ListBlanks extends React.Component<ListBlankProps, ListBlankState> {
         this.textEditListComponents(i)
       )
     }
-    return (
-      <div className="list-blanks">
+    if (!this.props.projector) {
+      return (
+        <div className="list-blanks">
         {textEditorArr}
-      </div>
-    )
+        </div>
+      )
+    }
   }
 
   answerValues(){
@@ -224,6 +226,7 @@ class ListBlanks extends React.Component<ListBlankProps, ListBlankState> {
       let instructionsRow = this.props.data.play.instructions ? (<div style={{marginTop: -20}} className="feedback-row">
         <p dangerouslySetInnerHTML={{__html: this.props.data.play.instructions}}></p>
       </div>) : null;
+      let submitButton = !this.props.projector ? <SubmitButton key={`${this.state.isSubmittable}`} disabled={this.state.submitted || !this.state.isSubmittable} onClick={this.handleStudentSubmission}/> : null;
       return (
         <div>
         <h1 className="prompt">
@@ -235,7 +238,7 @@ class ListBlanks extends React.Component<ListBlankProps, ListBlankState> {
           <div className='feedback-and-button-container'>
             {errorArea}
             {feedbackRow}
-            <SubmitButton key={`${this.state.isSubmittable}`} disabled={this.state.submitted || !this.state.isSubmittable} onClick={this.handleStudentSubmission}/>
+            {submitButton}
           </div>
         </div>
         </div>
