003819718d18e451b6e5c2b289ee42fde7ca698d
Jared Silver <jared@jaredsilver.name>
2017-09-06 17:02:29 -0400

    Fix student profile unit tests by renaming to complete and incomplete

--- client/app/bundles/HelloWorld/components/student_profile/__tests__/student_profile_unit.test.jsx
+++ client/app/bundles/HelloWorld/components/student_profile/__tests__/student_profile_unit.test.jsx
@@ -16,31 +16,31 @@ describe('StudentProfileUnit component', () => {
     expect(wrapper.find('h3.section-header').text()).toBe('Unit');
   });
 
-  it('should render "Completed Activities" and "Assigned Activities" <StudentProfileActivities /> components if not_finished and finished are defined', () => {
+  it('should render "Completed Activities" and "Assigned Activities" <StudentProfileActivities /> components if incomplete and complete are defined', () => {
     const wrapper = shallow(
       <StudentProfileUnit
         data={{
           unitName: 'Unit',
-          not_finished: [{}, {}, {}],
-          finished: [{}, {}]
+          incomplete: [{}, {}, {}],
+          complete: [{}, {}]
         }}
       />
     );
     expect(wrapper.find(StudentProfileActivities).length).toBe(2);
     expect(wrapper.find(StudentProfileActivities).at(0).props().header).toBe('Assigned Activities');
-    expect(wrapper.find(StudentProfileActivities).at(0).props().finished).toBe(false);
+    expect(wrapper.find(StudentProfileActivities).at(0).props().complete).toBe(false);
     expect(wrapper.find(StudentProfileActivities).at(0).props().data).toEqual([{}, {}, {}]);
     expect(wrapper.find(StudentProfileActivities).at(1).props().header).toBe('Completed Activities');
-    expect(wrapper.find(StudentProfileActivities).at(1).props().finished).toBe(true);
+    expect(wrapper.find(StudentProfileActivities).at(1).props().complete).toBe(true);
     expect(wrapper.find(StudentProfileActivities).at(1).props().data).toEqual([{}, {}]);
   });
 
-  it('should render only "Completed Activities" if not_finished is not defined', () => {
+  it('should render only "Completed Activities" if incomplete is not defined', () => {
     const wrapper = shallow(
       <StudentProfileUnit
         data={{
           unitName: 'Unit',
-          finished: [{}, {}]
+          complete: [{}, {}]
         }}
       />
     );
@@ -48,12 +48,12 @@ describe('StudentProfileUnit component', () => {
     expect(wrapper.find(StudentProfileActivities).at(0).props().header).toBe('Completed Activities');
   });
 
-  it('should render only "Assigned Activities" if finished is not defined', () => {
+  it('should render only "Assigned Activities" if complete is not defined', () => {
     const wrapper = shallow(
       <StudentProfileUnit
         data={{
           unitName: 'Unit',
-          not_finished: [{}, {}, {}],
+          incomplete: [{}, {}, {}],
         }}
       />
     );
