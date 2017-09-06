34c3eee2cbcfa33b5c4715d54fce98df175df619
Jared Silver <jared@jaredsilver.name>
2017-09-06 16:49:20 -0400

    Remove legacy premium promo tests, delete some cruft code

--- client/app/bundles/HelloWorld/components/dashboard/__tests__/premium_promo.test.jsx
+++ client/app/bundles/HelloWorld/components/dashboard/__tests__/premium_promo.test.jsx
@@ -5,17 +5,7 @@ import MockDate from 'mockdate';
 import PremiumPromo from '../premium_promo';
 
 describe('PremiumPromo component', () => {
-
   it('should render', () => {
-    MockDate.set('4/1/17');
     expect(shallow(<PremiumPromo />)).toMatchSnapshot();
   });
-
-  it('should render the correct number of free months', () => {
-    MockDate.set('4/1/17');
-    expect(shallow(<PremiumPromo />).text()).toMatch('Premium Bonus: 2 Months Free');
-    MockDate.set('5/1/17');
-    expect(shallow(<PremiumPromo />).text()).toMatch('Premium Bonus: 1 Months Free');
-  });
-
 });
