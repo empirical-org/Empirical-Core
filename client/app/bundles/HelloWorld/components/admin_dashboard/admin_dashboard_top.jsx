28d940f39b96e3cbf5b7d29fcad639fd5c0411ce
Donald McKendrick <ddmckendrick@gmail.com>
2017-09-06 14:25:01 -0400

    Style Admin header

--- client/app/bundles/HelloWorld/components/admin_dashboard/admin_dashboard_top.jsx
+++ client/app/bundles/HelloWorld/components/admin_dashboard/admin_dashboard_top.jsx
@@ -6,10 +6,10 @@ export default React.createClass({
     return (
       <div className="admin-dashboard-top">
         <div className="representative">
-          <h4 className="representative-header">Your Personal Quill Premium Representative</h4>
-          <div className="flex-row vertically-centered space-between rep-info" style={{ maxWidth: '400px', }}>
+          <div className="flex-row rep-info">
             <img src="https://assets.quill.org/images/headshots/thumb-becca.jpg" alt="becca thumb" />
             <div>
+              <h4 className="representative-header">Your Personal Representative</h4>
               <h3>Becca Garrison</h3>
               <p>{"As your Quill representative, I'm here to help!"}</p>
               <div className="representative-contact">
