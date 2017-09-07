803afb9ddae998badcd93dcd2e70bf38bacaf8ac
Jared Silver <jared@jaredsilver.name>
2017-09-07 16:23:32 -0400

    Restrict left menu alignment to a specific class

--- client/app/bundles/HelloWorld/components/lesson_planner/create_unit/create_unit.jsx
+++ client/app/bundles/HelloWorld/components/lesson_planner/create_unit/create_unit.jsx
@@ -330,7 +330,7 @@ export default React.createClass({
     }
     return (
       <span>
-        <div className="container" id="activity-planner">
+        <div className="container" id="activity-planner" className="menu-aligned-left">
           {stageSpecificComponents}
         </div>
       </span>
