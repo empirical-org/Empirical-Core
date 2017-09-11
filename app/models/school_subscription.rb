6f75e90f75b661009aaa5d495d43fb8a28f81204
RyanNovas <ryan@quill.org>
2017-09-11 10:37:35 -0400

    spec and callback fix

--- app/models/school_subscription.rb
+++ app/models/school_subscription.rb
@@ -2,11 +2,13 @@ class SchoolSubscription < ActiveRecord::Base
   validates :school_id, :subscription_id, presence: true
   belongs_to :school
   belongs_to :subscription
-  after_commit :update_schools_users
+  after_save :update_schools_users
 
   def self.update_or_create(school_id, subscription_id)
     school_sub = self.find_or_initialize_by(school_id: school_id)
     school_sub.update(subscription_id: subscription_id)
+    # after commit callback is regularly failingn
+    school_sub.update_schools_users
     school_sub.save!
   end
 
