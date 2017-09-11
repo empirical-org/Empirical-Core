70c3c00b3e36b76e05ef6c6e7976f6cffc0ea78f
Emilia Friedberg <emiliafriedberg@gmail.com>
2017-09-11 17:41:45 -0400

    add routes and interface for ordering and creating activityCategoires

---
+++ client/app/bundles/HelloWorld/containers/ActivityCategories.jsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import SortableList from '../components/shared/sortableList'
+import request from 'request'
+import getAuthToken from '../components/modules/get_auth_token'
+
+export default class ActivityCategories extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      activity_categories: props.activity_categories
+    }
+
+    this.updateActivityCategoryOrder = this.updateActivityCategoryOrder.bind(this)
+    this.saveActivityCategories = this.saveActivityCategories.bind(this)
+  }
+
+  updateActivityCategoryOrder(sortInfo) {
+    const originalOrderedActivityCategories = this.state.activity_categories
+    const newOrder = sortInfo.data.items.map(item => item.key);
+    const newOrderedActivityCategories = newOrder.map((key, i) => {
+      const newActivityCategory = originalOrderedActivityCategories[key]
+      newActivityCategory.order_number = i
+      return newActivityCategory
+    })
+    this.setState({activity_categories: newOrderedActivityCategories})
+  }
+
+  saveActivityCategories() {
+    console.log(getAuthToken())
+    const that = this
+    request.put(`${process.env.DEFAULT_URL}/cms/activity_categories/update_order_numbers`, {
+      json: {
+        activity_categories: this.state.activity_categories,
+        authenticity_token: getAuthToken()
+      }}, (e, r, response) => {
+      this.setState({activity_categories: response.activity_categories})
+      // that.setState({activity_categories: })
+    })
+  }
+
+  renderActivityCategory(name, key) {
+    return <span key={key}>{name}</span>
+  }
+
+  render() {
+    // return <div>I am not the problem</div>
+    const activityCategoryItems = this.state.activity_categories.map((ac, i) => this.renderActivityCategory(ac.name, i))
+    return <div className="activity-categories">
+      <SortableList data={activityCategoryItems} sortCallback={this.updateActivityCategoryOrder} />
+      <button onClick={this.saveActivityCategories}>Save Activity Categories</button>
+    </div>
+  }
+}
