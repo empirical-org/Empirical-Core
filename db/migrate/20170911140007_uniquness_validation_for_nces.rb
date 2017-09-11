0a5a394c2df05bef00f19dd6e742437ce92eb749
Jared Silver <jared@jaredsilver.name>
2017-09-11 13:48:55 -0400

    Ignore empty strings in unique validation for nces_id on schools

--- db/migrate/20170911140007_uniquness_validation_for_nces.rb
+++ db/migrate/20170911140007_uniquness_validation_for_nces.rb
@@ -1,5 +1,5 @@
 class UniqunessValidationForNces < ActiveRecord::Migration
   def change
-    add_index :schools, :nces_id, unique: true, name: 'unique_index_nces_id_on_schools'
+    add_index :schools, :nces_id, unique: true, name: 'unique_index_schools_on_nces_id', where: "nces_id != ''"
   end
 end
