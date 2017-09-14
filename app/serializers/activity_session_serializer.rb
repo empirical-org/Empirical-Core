1bd832cd45d67fab52b9a5339240801fc80e3bb0
Jared Silver <jared@jaredsilver.name>
2017-09-14 13:54:12 -0400

    Update ActivitySessionSerializer spec

--- app/serializers/activity_session_serializer.rb
+++ app/serializers/activity_session_serializer.rb
@@ -1,6 +1,4 @@
 class ActivitySessionSerializer < ActiveModel::Serializer
   attributes :uid, :percentage, :state, :completed_at, :data, :temporary,
               :activity_uid, :anonymous
-
-  # has_many :concept_results
 end
