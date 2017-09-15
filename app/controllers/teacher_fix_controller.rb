ce6dcc905e1b8b506c26f2ea2456fcaa8e3d21a0
Emilia Friedberg <emiliafriedberg@gmail.com>
2017-09-15 10:14:43 -0400

    test methods in teacher fixes concern

--- app/controllers/teacher_fix_controller.rb
+++ app/controllers/teacher_fix_controller.rb
@@ -1,4 +1,5 @@
 class TeacherFixController < ApplicationController
+  include TeacherFixes
   before_filter :staff!
 
   def index
@@ -42,13 +43,13 @@ class TeacherFixController < ApplicationController
   end
 
   def merge_student_accounts
-    @account1 = User.find_by_username_or_email(params['account_1_identifier'])
-    @account2 = User.find_by_username_or_email(params['account_2_identifier'])
-    if @account1 && @account2
-      if @account1.role === 'student' && @account2.role === 'student'
-        if same_classroom?
-          if @account2.classrooms.length == 1
-            merge_activity_sessions
+    account1 = User.find_by_username_or_email(params['account_1_identifier'])
+    account2 = User.find_by_username_or_email(params['account_2_identifier'])
+    if account1 && account2
+      if account1.role === 'student' && account2.role === 'student'
+        if TeacherFixes::same_classroom?(account1.id, account2.id)
+          if account2.classrooms.length == 1
+            TeacherFixes::merge_activity_sessions
             render json: {}, status: 200
           else
             render json: {error: "#{params['account_2_identifier']} is in more than one classroom."}
@@ -57,47 +58,14 @@ class TeacherFixController < ApplicationController
           render json: {error: "These students are not in the same classroom."}
         end
       else
-        nonstudent_account_identifier = @account1.role === 'student' ? params['account_2_identifier'] : params['account_1_identifier']
+        nonstudent_account_identifier = account1.role === 'student' ? params['account_2_identifier'] : params['account_1_identifier']
         render json: {error: "#{nonstudent_account_identifier} is not a student."}
       end
     else
-      missing_account_identifier = @account1 ? params['account_2_identifier'] : params['account_1_identifier']
+      missing_account_identifier = account1 ? params['account_2_identifier'] : params['account_1_identifier']
       render json: {error: "We do not have an account for #{missing_account_identifier}"}
     end
   end
 
-  private
-
-  def merge_activity_sessions
-    a1_grouped_activity_sessions = @account1.activity_sessions.group_by { |as| as.classroom_activity_id }
-    a2_grouped_activity_sessions = @account2.activity_sessions.group_by { |as| as.classroom_activity_id }
-    a2_grouped_activity_sessions.each do |ca_id, activity_sessions|
-      activity_sessions.each {|as| as.update_columns(user_id: @account1.id) }
-      if a1_grouped_activity_sessions[ca_id]
-        hide_extra_activity_sessions(ca_id, @account1.id)
-      else
-        ClassroomActivity.find(ca_id).assigned_student_ids.push(@account1.id)
-      end
-    end
-  end
-
-  def hide_extra_activity_sessions(ca_id, user_id)
-    ActivitySession.joins("JOIN users ON activity_sessions.user_id = users.id")
-    .joins("JOIN classroom_activities ON activity_sessions.classroom_activity_id = classroom_activities.id")
-    .where("users.id = ?", user_id)
-    .where("classroom_activities.id = ?", ca_id)
-    .where("activity_sessions.visible = true")
-    .order("activity_sessions.is_final_score DESC, activity_sessions.percentage ASC, activity_sessions.started_at")
-    .offset(1)
-    .update_all(visible: false)
-  end
-
-  def same_classroom?
-    ActiveRecord::Base.connection.execute("SELECT A.student_id, B.student_id, A.classroom_id
-      FROM students_classrooms A, students_classrooms B
-      WHERE A.student_id = 1649437
-      AND B.student_id = 1649436
-      AND A.classroom_id = B.classroom_id").to_a.any?
-  end
 
 end
