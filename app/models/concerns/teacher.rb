dc6947d2afcd0ca5245293f7ecd92ace4ab23fc0
Jared Silver <jared@jaredsilver.name>
2017-09-19 09:40:46 -0400

    Better prevent teachers from turning themselves into stff

--- app/models/concerns/teacher.rb
+++ app/models/concerns/teacher.rb
@@ -98,6 +98,7 @@ module Teacher
 
   def update_teacher params
     return if !self.teacher?
+    params[:role] = 'teacher' if !(['teacher', 'student'].include? params[:role])
     params.permit(:id,
                   :name,
                   :role,
