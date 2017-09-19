6bdfbd4a4ac7e813b18726e8e31924b8d78da7c4
Emilia Friedberg <emiliafriedberg@gmail.com>
2017-09-19 15:59:22 -0400

    responsive design

--- app/models/schools_users.rb
+++ app/models/schools_users.rb
@@ -1,4 +1,10 @@
 class SchoolsUsers < ActiveRecord::Base
   belongs_to :school
   belongs_to :user
+
+  before_save :update_subscriptions
+
+  def update_subscriptions
+    self.user.updated_school(self.school_id)
+  end
 end
