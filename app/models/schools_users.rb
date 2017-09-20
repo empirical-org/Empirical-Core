bb56a652ff9d70eac20d7555361d4a53c0d63478
Donald McKendrick <ddmckendrick@gmail.com>
2017-09-20 14:32:40 -0400

    Add docs.

--- app/models/schools_users.rb
+++ app/models/schools_users.rb
@@ -2,6 +2,7 @@ class SchoolsUsers < ActiveRecord::Base
   belongs_to :school
   belongs_to :user
 
+  # When a teacher sets their school, we make sure they they have the appropriate subscription type.
   before_save :update_subscriptions
 
   def update_subscriptions
