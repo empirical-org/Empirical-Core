4cafb9f9c3f9ef977c8c8ce82f1eb9aea1390f37
Emilia Friedberg <emiliafriedberg@gmail.com>
2017-09-20 17:40:43 -0400

    fix failing rspec test

--- spec/serializers/activity_serializer_spec.rb
+++ spec/serializers/activity_serializer_spec.rb
@@ -15,7 +15,9 @@ describe ActivitySerializer, type: :serializer do
          name
          topic
          uid
-         updated_at)
+         updated_at
+         activity_category
+         )
     end
   end
 end
