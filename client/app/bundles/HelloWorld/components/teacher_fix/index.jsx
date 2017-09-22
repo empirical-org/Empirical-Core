bf4f2819dc3475b378d7ab071bc356ab19703645
Emilia Friedberg <emiliafriedberg@gmail.com>
2017-09-22 10:15:02 -0400

    add google_unsync route, page, and method to teacher fixes

--- client/app/bundles/HelloWorld/components/teacher_fix/index.jsx
+++ client/app/bundles/HelloWorld/components/teacher_fix/index.jsx
@@ -12,6 +12,7 @@ export default class TeacherFixIndex extends React.Component {
           <p><a href="/teacher_fix/recover_classroom_activities">Restore Classroom Activities</a></p>
           <p><a href="/teacher_fix/merge_student_accounts">Merge Student Accounts</a></p>
           <p><a href="/teacher_fix/move_student">Move Student from One Class to Another</a></p>
+          <p><a href="/teacher_fix/google_unsync">Unsync User with Google Classroom</a></p>
       </div>
     }
   }
