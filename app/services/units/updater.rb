3aa918af9d91461831e61fd86a7b06ec01985cf7
RyanNovas <ryan@quill.org>
2017-09-25 18:33:49 -0400

    test suite passes

--- app/services/units/updater.rb
+++ app/services/units/updater.rb
@@ -11,7 +11,7 @@ module Units::Updater
   end
 
   def self.assign_unit_template_to_one_class(unit_id, classrooms_data)
-    activities_data = ClassroomActivity.where(unit_id: unit_id).select('activity_id AS id, NULL as due_date').symbolize_keys
+    activities_data = ClassroomActivity.where(unit_id: unit_id).select('activity_id AS id, NULL as due_date')
     self.update_helper(unit_id, activities_data, classrooms_data)
   end
 
@@ -23,14 +23,14 @@ module Units::Updater
 
   private
 
-  def self.matching_or_new_classroom_activity(activity_data, classroom_id, extant_classroom_activities, new_cas, hidden_cas_ids, classroom)
+  def self.matching_or_new_classroom_activity(activity_data, classroom_id, extant_classroom_activities, new_cas, hidden_cas_ids, classroom, unit_id)
     visible = classroom[:student_ids] != false
     matching_activity = extant_classroom_activities.find{|ca| (ca.activity_id == activity_data[:id] && ca.classroom_id == classroom_id)}
     if matching_activity
       if !visible
         # then there are no assigned students and we should hide the cas
         hidden_cas_ids.push(matching_activity.id)
-      elsif (matching_activity.due_date != activity_data[:due_date]) || (matching_activity.assigned_student_ids != classroom[:student_ids])
+      elsif (matching_activity[:due_date] != activity_data[:due_date]) || (matching_activity.assigned_student_ids != classroom[:student_ids])
         # then something changed and we should update
         matching_activity.update(due_date: activity_data[:due_date], assigned_student_ids: classroom[:student_ids])
       end
@@ -38,6 +38,7 @@ module Units::Updater
       # making an array of hashes to create in one bulk option
       new_cas.push({activity_id: activity_data[:id],
          classroom_id: classroom_id,
+         unit_id: unit_id,
          due_date: activity_data[:due_date],
          assigned_student_ids: classroom[:student_ids]})
     end
@@ -52,11 +53,16 @@ module Units::Updater
         product = activities_data.product([classroom[:id].to_i])
         product.each do |pair|
           activity_data, classroom_id = pair
-          self.matching_or_new_classroom_activity(activity_data.symbolize_keys, classroom_id, extant_classroom_activities, new_cas, hidden_cas_ids, classroom)
+          self.matching_or_new_classroom_activity(activity_data, classroom_id, extant_classroom_activities, new_cas, hidden_cas_ids, classroom, unit_id)
         end
     end
-    new_cas.any? ? ClassroomActivity.bulk_insert(values: new_cas) : nil
-    hidden_cas_ids.any? ? ClassroomActivity.where(id: hidden_cas_ids).update_all(visible: false) : nil
+    # TODO: this is messing everything up by not generating new activity sessions since it skips the callback
+    # however, it is far more efficient
+    # new_cas.any? ? ClassroomActivity.bulk_insert(values: new_cas) : nil
+    new_cas.each{|ca| ClassroomActivity.create(ca)}
+    # TODO: same as above -- efficient, but we need the callbacks
+    # hidden_cas_ids.any? ? ClassroomActivity.where(id: hidden_cas_ids).update_all(visible: false) : nil
+    hidden_cas_ids.each{|ca_id| ClassroomActivity.find(ca_id).update(visible: false)}
     if (hidden_cas_ids.any?) && (new_cas.none?)
       # then there is a chance that there are no existing classroom activities
       unit.hide_if_no_visible_classroom_activities
