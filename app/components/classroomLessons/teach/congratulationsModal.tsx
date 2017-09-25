e47d238c2699fbb0d73d111e798796e7fabbf80e
Emilia Friedberg <emiliafriedberg@gmail.com>
2017-09-25 14:31:55 -0400

    update congratulations modal illustration

--- app/components/classroomLessons/teach/congratulationsModal.tsx
+++ app/components/classroomLessons/teach/congratulationsModal.tsx
@@ -4,7 +4,7 @@ const CongratulationsModal: React.SFC<{closeModal?: any}> = (props) => {
  return <div className="congratulations-modal-container">
      <div className="congratulations-modal">
         <img onClick={props.closeModal} className="exit" src="http://assets.quill.org/images/icons/CloseIcon.svg"/>
-        <img className="illustration" src="http://assets.quill.org/images/illustrations/congratulations_modal.svg" />
+        <img className="illustration" src="http://assets.quill.org/images/illustrations/congratulations_illustration.svg" />
         <h1 className="congratulations">Congratulations!</h1>
         <h1>You've completed a Quill Lessons Activity.</h1>
         <p>This lesson will be marked as complete for your students.</p>
