55acc1024f5fe7cd817be123ace920b76954f028
Emilia Friedberg <emiliafriedberg@gmail.com>
2017-09-27 10:42:30 -0400

    sqlize and speed up activity search

--- app/queries/activity_search.rb
+++ app/queries/activity_search.rb
@@ -2,12 +2,25 @@ class ActivitySearch
   # filters = hash of model_name/model_id pairs
   # sort = hash with 'field' and 'asc_or_desc' (?) as keys
   def self.search(search_text, filters, sort, flag)
-    # TODO sanitize search_text
-    query = ActiveRecord::Base.connection.execute("SELECT *,
+    filter_string = ''
+    filters.each do |model_name, model_id|
+      filter_string.concat("AND #{model_name}.id = #{model_id}")
+    end
+
+    sanitized_search_text = search_text.length > 0 ? ActiveRecord::Base.sanitize(search_text) : ''
+
+    ActiveRecord::Base.connection.execute("SELECT
+        activities.name AS activity_name,
+    		activities.description AS activity_description,
+    		activities.flags AS activity_flag,
+    		activities.id AS activity_id,
+    		activities.uid AS activity_uid,
         sections.id AS section_id,
         activity_categories.id AS activity_category_id,
         activity_categories.name AS activity_category_name,
         sections.name AS section_name,
+        topics.name AS topic_name,
+        topics.id AS topic_id,
         activity_classifications.id AS classification_id
       FROM activities
       LEFT JOIN activity_classifications ON activities.activity_classification_id = activity_classifications.id
@@ -15,27 +28,11 @@ class ActivitySearch
       LEFT JOIN sections ON topics.section_id = sections.id
       LEFT JOIN activity_category_activities ON activities.id = activity_category_activities.activity_id
       LEFT JOIN activity_categories ON activity_category_activities.activity_category_id = activity_categories.id
-      WHERE (activities.name ILIKE '%#{search_text}%' OR activity_categories.name ILIKE '%#{search_text}%' OR activities.description ILIKE '%#{search_text}%')
+      WHERE (activities.name ILIKE '%#{sanitized_search_text}%' OR activity_categories.name ILIKE '%#{sanitized_search_text}%' OR activities.description ILIKE '%#{sanitized_search_text}%')
       AND activity_categories.id IS NOT NULL
       AND sections.id IS NOT NULL
+      #{filter_string}
       ORDER BY #{search_sort_sql(sort)}").to_a
-
-    # query = Activity.user_scope(flag)
-    #         .joins("LEFT JOIN activity_classifications ON activities.activity_classification_id = activity_classifications.id")
-    #         .joins("LEFT JOIN topics ON activities.topic_id = topics.id")
-    #         .joins("LEFT JOIN sections ON topics.section_id = sections.id")
-    #         .joins("LEFT JOIN activity_category_activities ON activities.id = activity_category_activities.activity_id")
-    #         .joins("LEFT JOIN activity_categories ON activity_category_activities.activity_category_id = activity_categories.id")
-    #         .where("(activities.name ILIKE ?) OR (activity_categories.name ILIKE ?) OR (activities.description ILIKE ?)", "%#{search_text}%", "%#{search_text}%", "%#{search_text}%")
-    #         .where("activity_categories.id IS NOT NULL AND sections.id IS NOT NULL")
-    #         .order(search_sort_sql(sort))
-
-    # Sorry for the meta-programming.
-    filters.each do |model_name, model_id| # :activity_classifications, 123
-      query = query.where("#{model_name}.id = ?", model_id)
-    end
-
-    query
   end
 
   private
