69269f66eee527104d5a8a58fd293e40f58ba14a
Jared Silver <jared@jaredsilver.name>
2017-09-29 18:24:12 -0400

    Search modifications and initial part of query

--- app/controllers/cms/schools_controller.rb
+++ app/controllers/cms/schools_controller.rb
@@ -7,11 +7,13 @@ class Cms::SchoolsController < ApplicationController
   # This allows staff members to view and search through schools.
   def index
     @school_search_query = {}
+    @school_search_query_results = []
   end
 
   def search
     @school_search_query = school_query_params
     @school_search_query_results = school_query(school_query_params)
+    @school_search_query_results = @school_search_query_results ? @school_search_query_results : []
     render :index
   end
 
@@ -35,6 +37,7 @@ class Cms::SchoolsController < ApplicationController
 
   private
   def search_attributes
+    # These are the text input fields, but they are not all of the fields in the form.
     @search_attributes = ['school_name', 'school_city', 'school_state', 'school_zip', 'district_name']
     @school_premium_types = Subscription.where(id: SchoolSubscription.all.pluck(:subscription_id)).pluck(:account_type).uniq
   end
@@ -45,6 +48,52 @@ class Cms::SchoolsController < ApplicationController
   end
 
   def school_query(params)
-    
+    # This should return an array of hashes with the following order.
+    # [
+    #   {
+    #     school_name: 'school name',
+    #     district_name: 'district name',
+    #     school_city: 'school city',
+    #     school_state: 'school state',
+    #     school_zip: 'school zip',
+    #     frl: '% FRL',
+    #     number_teachers: '# teachers',
+    #     premium_status: 'premium status',
+    #     number_admins: '# admins',
+    #     id: #,
+    #   }
+    # ]
+
+    # TODO:
+    #   - Account for mail_city versus city
+    #   - Account for mail_state versus state
+    #   - Add default values where appropriate
+
+    # Welp, this obviously doesn't work yet. But it's getting there!
+
+    ActiveRecord::Base.connection.execute("
+      SELECT
+        schools.name AS school_name,
+        schools.leanm AS district_name,
+        schools.city AS school_city,
+        schools.state AS school_state,
+        schools.zipcode AS school_zip,
+        schools.free_lunches AS frl,
+        COUNT(schools_users.*) AS number_teachers,
+        subscriptions.account_type AS premium_status,
+        COUNT(schools_admins.*) AS number_admins,
+        schools.id AS id
+      FROM schools
+      LEFT JOIN schools_users ON schools_users.school_id = schools.id
+      LEFT JOIN schools_admins ON schools_admins.school_id = schools.id
+      LEFT JOIN school_subscriptions ON school_subscriptions.school_id = schools.id
+      LEFT JOIN subscriptions ON subscriptions.id = school_subscriptions.subscription_id
+      #{where_query_string}
+      GROUP BY schools.name, schools.leanm, schools.city, schools.state, schools.zipcode, schools.free_lunches, subscriptions.account_type, schools.id
+    ").to_a
+  end
+
+  def where_query_string
+    'WHERE schools.id=103341'
   end
 end
