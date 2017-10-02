e794dcb3a58072e67642a8c738204f41630edb43
Jared Silver <jared@jaredsilver.name>
2017-10-02 10:21:31 -0400

    Search query works for all fields but premium status

--- app/controllers/cms/schools_controller.rb
+++ app/controllers/cms/schools_controller.rb
@@ -2,7 +2,7 @@ class Cms::SchoolsController < ApplicationController
   before_filter :signed_in!
   before_filter :staff!
 
-  before_action :search_attributes, only: [:index, :search]
+  before_action :text_search_inputs, only: [:index, :search]
 
   # This allows staff members to view and search through schools.
   def index
@@ -36,19 +36,25 @@ class Cms::SchoolsController < ApplicationController
   # TODO: subscriptions manager?
 
   private
-  def search_attributes
+  def text_search_inputs
     # These are the text input fields, but they are not all of the fields in the form.
-    @search_attributes = ['school_name', 'school_city', 'school_state', 'school_zip', 'district_name']
+    @text_search_inputs = ['school_name', 'school_city', 'school_state', 'school_zip', 'district_name']
     @school_premium_types = Subscription.where(id: SchoolSubscription.all.pluck(:subscription_id)).pluck(:account_type).uniq
+  end
+
+  def all_search_inputs
+    @text_search_inputs.map(&:to_sym) + [:search_schools_with_zero_teachers, :premium_status => []]
   end
 
   def school_query_params
     default_params = [:utf8, :authenticity_token, :commit]
-    params.permit(@search_attributes.map(&:to_sym) + default_params + [:search_schools_with_zero_teachers, :premium_status => []])
+    params.permit(default_params + all_search_inputs)
   end
 
   def school_query(params)
     # This should return an array of hashes with the following order.
+    # (Order matters because of the order in which these are being
+    # displayed in the table on the front end.)
     # [
     #   {
     #     school_name: 'school name',
@@ -67,9 +73,8 @@ class Cms::SchoolsController < ApplicationController
     # TODO:
     #   - Account for mail_city versus city
     #   - Account for mail_state versus state
+    #   - Account for mail_zipcode versus zipcode
     #   - Add default values where appropriate
-
-    # Welp, this obviously doesn't work yet. But it's getting there!
 
     ActiveRecord::Base.connection.execute("
       SELECT
@@ -88,12 +93,56 @@ class Cms::SchoolsController < ApplicationController
       LEFT JOIN schools_admins ON schools_admins.school_id = schools.id
       LEFT JOIN school_subscriptions ON school_subscriptions.school_id = schools.id
       LEFT JOIN subscriptions ON subscriptions.id = school_subscriptions.subscription_id
-      #{where_query_string}
+      #{where_query_string_builder}
       GROUP BY schools.name, schools.leanm, schools.city, schools.state, schools.zipcode, schools.free_lunches, subscriptions.account_type, schools.id
+      #{having_string}
+      LIMIT 10
     ").to_a
   end
 
-  def where_query_string
-    'WHERE schools.id=103341'
+  def having_string
+    'HAVING COUNT(schools_users.*) != 0' unless school_query_params[:search_schools_with_zero_teachers]
+  end
+
+  def where_query_string_builder
+    conditions = []
+    all_search_inputs.map(&:to_s).each do |param|
+      param_value = school_query_params[param]
+      if param_value && !param_value.empty?
+        conditions << where_query_string_clause_for(param, param_value)
+      end
+    end
+    "WHERE #{conditions.reject(&:nil?).join(' AND ')}" unless conditions.empty?
+  end
+
+  def where_query_string_clause_for(param, param_value)
+    # Potential params by which to search:
+    # School name: schools.name
+    # School city: schools.city or schools.mail_city
+    # School state: schools.state or schools.mail_state
+    # School zip: schools.zipcode or schools.mail_zipcode
+    # District name: schools.leanm
+    # Premium status: subscriptions.account_type
+    param_value = ActiveRecord::Base.sanitize(param_value)
+    case param
+    when 'school_name'
+      "schools.name ILIKE #{format_sanitized_string_for_ilike(param_value)}"
+    when 'school_city'
+      "schools.city ILIKE #{format_sanitized_string_for_ilike(param_value)}"
+    when 'school_state'
+      "UPPER(schools.state) = UPPER(#{param_value})"
+    when 'school_zip'
+      "schools.zipcode = #{param_value}"
+    when 'district_name'
+      "schools.leanm ILIKE #{format_sanitized_string_for_ilike(param_value)}"
+    when 'premium_status'
+      "subscriptions.account_type = #{premium_status}" # TODO this won't work yet
+    else
+      nil
+    end
+  end
+
+  def format_sanitized_string_for_ilike(string)
+    "'%#{string[1..string.length-2]}%'"
   end
 end
