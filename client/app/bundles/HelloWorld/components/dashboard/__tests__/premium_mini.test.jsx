3b86613a731a65a03b3c0c3434fe2b8c9da42bdb
Donald McKendrick <ddmckendrick@gmail.com>
2017-10-02 17:37:31 -0400

    Fix a test.

--- client/app/bundles/HelloWorld/components/dashboard/__tests__/premium_mini.test.jsx
+++ client/app/bundles/HelloWorld/components/dashboard/__tests__/premium_mini.test.jsx
@@ -4,16 +4,13 @@ import $ from 'jquery';
 
 import PremiumMini from '../premium_mini';
 
-jest.mock('jquery', () => {
-  return {
-    post: jest.fn().mockReturnValue({
-      success: jest.fn()
-    })
-  };
-});
+jest.mock('jquery', () => ({
+  post: jest.fn().mockReturnValue({
+    success: jest.fn(),
+  }),
+}));
 
 describe('PremiumMini component', () => {
-
   it('should render', () => {
     expect(shallow(<PremiumMini />)).toMatchSnapshot();
   });
@@ -23,7 +20,6 @@ describe('PremiumMini component', () => {
     wrapper.find('button').simulate('click');
     expect($.post.mock.calls[0][0]).toBe('/subscriptions');
     expect($.post.mock.calls[0][1].account_limit).toBe(1000);
-    expect($.post.mock.calls[0][1].account_type).toBe('trial');
+    expect($.post.mock.calls[0][1].account_type).toBe('Teacher Trial');
   });
-
 });
