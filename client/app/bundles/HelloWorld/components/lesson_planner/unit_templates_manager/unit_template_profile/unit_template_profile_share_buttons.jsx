1cf8ebb7a83b47ee09d8ae9dd8380b814711485c
RyanNovas <ryan@quill.org>
2017-10-03 14:14:49 -0400

    removes extraneous line

--- client/app/bundles/HelloWorld/components/lesson_planner/unit_templates_manager/unit_template_profile/unit_template_profile_share_buttons.jsx
+++ client/app/bundles/HelloWorld/components/lesson_planner/unit_templates_manager/unit_template_profile/unit_template_profile_share_buttons.jsx
@@ -1,69 +1,64 @@
-'use strict'
+import React from 'react';
+import _ from 'underscore';
 
- import React from 'react'
- import _ from 'underscore'
-
- export default  React.createClass({
+export default React.createClass({
 
   propTypes: {
-    data: React.PropTypes.object.isRequired
+    data: React.PropTypes.object.isRequired,
   },
 
-  urlForPublicPage: function () {
-    return window.location.origin + '/activities/packs/' + this.props.data.id;
+  urlForPublicPage() {
+    return `${window.location.origin}/activities/packs/${this.props.data.id}`;
   },
 
-  render: function () {
-    var url = this.urlForPublicPage();
+  render() {
+    const url = this.urlForPublicPage();
 
-    var stuff = [
+    const stuff = [
       {
         icon: 'fa-twitter',
         className: 'btn-twitter',
-        href: `http://twitter.com/home?status=I’m using the ${this.props.data.name} Activity Pack from Quill.org to teach writing & grammar. ${url}`
         href: `http://twitter.com/home?status=I’m using the ${this.props.data.name} Activity Pack from Quill.org to teach writing & grammar. ${url}`,
         title: 'Share on Twitter',
-        action: 'Tweet'
+        action: 'Tweet',
       },
       {
         icon: 'fa-facebook',
         className: 'btn-facebook',
-        href: 'https://www.facebook.com/share.php?u=' + url,
+        href: `https://www.facebook.com/share.php?u=${url}`,
         title: 'Share this Activity pack on Facebook',
-        action: 'Post'
+        action: 'Post',
       },
       {
         icon: 'fa-google-plus',
         className: 'btn-google-plus',
-        href: 'https://plus.google.com/share?url=' + url,
+        href: `https://plus.google.com/share?url=${url}`,
         title: 'Share with Google Plus',
-        action: 'Post'
+        action: 'Post',
       },
       {
         icon: 'fa-pinterest',
         className: 'btn-linkedin',
-        href: 'https://pinterest.com/pin/create/button/?url=www.quill.org/activities/packs/4&media=https%3A//s-media-cache-ak0.pinimg.com/736x/92/46/50/9246509b2ad54a3c2ad97e21976b9176.jpg&description=I%E2%80%99m%20using%20the%20' + this.props.data.name + '%20Activity%20Pack,%20from%20Quill.org,%20to%20teach%20English%20grammar.%20' + url,
+        href: `https://pinterest.com/pin/create/button/?url=www.quill.org/activities/packs/4&media=https%3A//s-media-cache-ak0.pinimg.com/736x/92/46/50/9246509b2ad54a3c2ad97e21976b9176.jpg&description=I%E2%80%99m%20using%20the%20${this.props.data.name}%20Activity%20Pack,%20from%20Quill.org,%20to%20teach%20English%20grammar.%20${url}`,
         title: 'Share on Pinterest',
-        action: 'Pin'
+        action: 'Pin',
       }
     ];
 
-    var links = _.map(stuff, function (hash) {
-      return (
-        <a href={hash.href} key={hash.title} title={hash.title} className={'btn btn-default btn-social ' + hash.className}  target="_blank">
-          <i className={'fa ' + hash.icon}></i><span className='social-action'>{hash.action}</span>
-        </a>
-      );
-    });
+    const links = _.map(stuff, hash => (
+      <a href={hash.href} key={hash.title} title={hash.title} className={`btn btn-default btn-social ${hash.className}`} target="_blank">
+        <i className={`fa ${hash.icon}`} /><span className="social-action">{hash.action}</span>
+      </a>
+      ));
 
     return (
-      <div className='share light-gray-bordered-box'>
-          <strong>Share this Activity Pack</strong>
-          <div className='container' style={{width: 'auto'}}>
-            {links}
-          </div>
+      <div className="share light-gray-bordered-box">
+        <strong>Share this Activity Pack</strong>
+        <div className="container" style={{ width: 'auto', }}>
+          {links}
+        </div>
       </div>
     );
-  }
+  },
 
 });
