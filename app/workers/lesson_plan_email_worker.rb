60fc4e7b95c100d6cb8045ebaad43e98a0cddb58
Emilia Friedberg <emiliafriedberg@gmail.com>
2017-10-03 18:01:29 -0400

    send one email instead of several for each unit with a lesson

--- app/workers/lesson_plan_email_worker.rb
+++ app/workers/lesson_plan_email_worker.rb
@@ -1,9 +1,10 @@
 class LessonPlanEmailWorker
   include Sidekiq::Worker
 
-  def perform(user_id, lesson_id)
+  def perform(user_id, lesson_ids, unit_id)
     @user = User.find(user_id)
-    @lesson = Activity.find(lesson_id)
-    @user.send_lesson_plan_email(@lesson)
+    @lessons = Activity.where(id: lesson_ids)
+    @unit = Unit.find(unit_id)
+    @user.send_lesson_plan_email(@lessons, @unit)
   end
 end
