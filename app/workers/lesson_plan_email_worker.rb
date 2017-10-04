2634b2302a46a53f1f7e63b6506bc6c02d89d7e1
Emilia Friedberg <emiliafriedberg@gmail.com>
2017-10-04 10:04:48 -0400

    organize lessons in email by order number

--- app/workers/lesson_plan_email_worker.rb
+++ app/workers/lesson_plan_email_worker.rb
@@ -4,6 +4,9 @@ class LessonPlanEmailWorker
   def perform(user_id, lesson_ids, unit_id)
     @user = User.find(user_id)
     @lessons = Activity.where(id: lesson_ids)
+      .joins("JOIN activity_category_activities AS aca ON aca.activity_id = activities.id")
+      .joins("JOIN activity_categories AS ac ON ac.id = aca.activity_category_id")
+      .order("ac.order_number, aca.order_number")
     @unit = Unit.find(unit_id)
     @user.send_lesson_plan_email(@lessons, @unit)
   end
