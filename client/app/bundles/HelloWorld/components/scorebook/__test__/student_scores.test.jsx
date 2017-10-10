8bdba1ced3f411eee5928792533d33e60435f8c3
Jared Silver <jared@jaredsilver.name>
2017-10-10 17:56:04 -0400

    Add and adjust tests related to linking to list overview from visual overview

--- client/app/bundles/HelloWorld/components/scorebook/__test__/student_scores.test.jsx
+++ client/app/bundles/HelloWorld/components/scorebook/__test__/student_scores.test.jsx
@@ -6,6 +6,7 @@ import ActivityIconWithTooltip from '../../general_components/activity_icon_with
 
 const data = {
   userId: 666,
+  classroomId: 42,
   scores: [
     { activity_classification_id: '4', caId: '12345', name: 'Sentence Structure Diagnostic', percentage: '1', updated: '2016-09-30 00:05:50.361093', userId: '666', },
     { activity_classification_id: '4', caId: '12345', name: 'Sentence Structure Diagnostic', percentage: '1', updated: '2016-09-30 00:05:50.361093', userId: '666', }
@@ -13,14 +14,18 @@ const data = {
 };
 
 describe('StudentScores component', () => {
-  it('should as many Activity Icons as data.props.results, rounded up', () => {
-    const wrapper = shallow(
-      <StudentScores
-        data={data}
-        premium_state={'trial'}
-      />);
+  const wrapper = shallow(
+    <StudentScores
+      data={data}
+      premium_state={'trial'}
+    />);
 
+  it('should as many Activity Icons as data.props.results, rounded up', () => {
     const numberOfResults = wrapper.instance().props.data.scores.length;
     expect(wrapper.find(ActivityIconWithTooltip).length).toEqual(numberOfResults);
+  });
+
+  it('should have a link to the list overview page', () => {
+    expect(wrapper.find('.student-header a').prop('href')).toMatch('/teachers/progress_reports/activity_sessions?student_id=666&classroom_id=42');
   });
 });
