f372769aee168a88ab1692b74d70d9165a805acf
RyanNovas <ryan@quill.org>
2017-10-11 11:36:03 -0400

    restored string interpolation in query

--- app/queries/scorebook/query.rb
+++ app/queries/scorebook/query.rb
@@ -13,20 +13,25 @@ class Scorebook::Query
         MAX(acts.updated_at) AS updated_at,
         MAX(acts.percentage) AS percentage,
         SUM(CASE WHEN acts.percentage IS NOT NULL THEN 1 ELSE 0 END) AS completed_attempts,
-        SUM(CASE WHEN acts.state = 'started' THEN 1 ELSE 0 END) AS started
+        SUM(CASE WHEN acts.state = 'started' THEN 1 ELSE 0 END) AS started,
+        SUM(CASE WHEN acts.is_final_score = true THEN acts.id ELSE 0 END) AS id
      FROM classrooms AS classroom
      LEFT JOIN students_classrooms AS sc on sc.classroom_id = classroom.id
      RIGHT JOIN users AS students ON students.id = sc.student_id
      INNER JOIN classroom_activities AS ca ON ca.classroom_id = classroom.id
+     #{self.units(unit_id)&.first}
      LEFT JOIN activity_sessions AS acts ON (
            acts.classroom_activity_id = ca.id
            AND acts.user_id = students.id
            AND acts.visible = true
            )
      INNER JOIN activities AS activity ON activity.id = ca.activity_id
-     WHERE classroom.id = 43698
+     WHERE classroom.id = #{classroom_id}
      AND ca.visible = true
      AND sc.visible = true
+     #{self.units(unit_id)&.last}
+     #{self.begin_date(begin_date)}
+     #{self.end_date(end_date)}
      GROUP BY
       students.id,
        students.name, ca.id, activity.activity_classification_id, activity.name
@@ -35,46 +40,8 @@ class Scorebook::Query
        MIN(acts.completed_at),
        CASE WHEN SUM(CASE WHEN acts.state = 'started' THEN 1 ELSE 0 END) > 0 THEN true ELSE false END DESC,
        ca.created_at ASC
-     OFFSET (0)
-     FETCH NEXT 200 ROWS ONLY"
-#   ).to_a
-# end
-      # ActiveRecord::Base.connection.execute(
-      # "SELECT acts.user_id,
-      #    ca.id AS ca_id,
-      #    students.name AS name,
-      #    activity.activity_classification_id,
-      #    activity.name AS activity_name,
-      #    MAX(acts.updated_at) AS updated_at,
-      #    MAX(acts.percentage) AS percentage,
-      #    SUM(CASE WHEN acts.percentage IS NOT NULL THEN 1 ELSE 0 END) AS completed_attempts,
-      #    SUM(CASE WHEN acts.state = 'started' THEN 1 ELSE 0 END) AS started,
-      #    SUM(CASE WHEN acts.is_final_score = true THEN acts.id ELSE 0 END) AS id
-      # FROM classrooms AS classroom
-      # LEFT JOIN students_classrooms AS sc on sc.classroom_id = classroom.id
-      # RIGHT JOIN users AS students ON students.id = sc.student_id
-      # INNER JOIN classroom_activities AS ca ON ca.classroom_id = classroom.id
-      # #{self.units(unit_id)&.first}
-      # INNER JOIN activity_sessions AS acts ON (
-      #       acts.classroom_activity_id = ca.id
-      #       AND acts.user_id = students.id
-      #       )
-      # INNER JOIN activities AS activity ON activity.id = ca.activity_id
-      # WHERE classroom.id = #{classroom_id}
-      # AND acts.visible = true
-      # AND ca.visible = true
-      # AND sc.visible = true
-      # #{self.units(unit_id)&.last}
-      # #{self.begin_date(begin_date)}
-      # #{self.end_date(end_date)}
-      # GROUP BY acts.user_id, students.name, ca.id, activity.activity_classification_id, activity.name
-      # ORDER BY split_part( students.name, ' ' , 2),
-      #   CASE WHEN SUM(CASE WHEN acts.percentage IS NOT NULL THEN 1 ELSE 0 END) > 0 THEN true ELSE false END DESC,
-      #   MIN(acts.completed_at),
-      #   CASE WHEN SUM(CASE WHEN acts.state = 'started' THEN 1 ELSE 0 END) > 0 THEN true ELSE false END DESC,
-      #   ca.created_at ASC
-      # OFFSET (#{(current_page.to_i - 1) * SCORES_PER_PAGE})
-      # FETCH NEXT #{SCORES_PER_PAGE} ROWS ONLY"
+       OFFSET (#{(current_page.to_i - 1) * SCORES_PER_PAGE})
+       FETCH NEXT #{SCORES_PER_PAGE} ROWS ONLY"
       ).to_a
   end
 
