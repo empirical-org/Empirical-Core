e1086e798cc0d830457824d8fba905405c27d268
Emilia Friedberg <emiliafriedberg@gmail.com>
2017-10-13 16:07:14 -0400

    better lessons caching mechanism

--- app/controllers/teachers/classroom_activities_controller.rb
+++ app/controllers/teachers/classroom_activities_controller.rb
@@ -16,8 +16,8 @@ class Teachers::ClassroomActivitiesController < ApplicationController
   def hide
     cas = ClassroomActivity.where(activity_id: @classroom_activity.activity_id, unit_id: @classroom_activity.unit_id)
     activity_sessions = ActivitySession.where(classroom_activity_id: cas.ids)
-    # cannot use update_all here bc we need the callbacks to run
     cas.update_all(visible: false)
+    SetTeacherLessonCache.perform_async(current_user.id)
     activity_sessions.update_all(visible: false)
     @classroom_activity.unit.hide_if_no_visible_classroom_activities
     render json: {}
@@ -80,7 +80,12 @@ private
   end
 
   def lessons_cache
-    JSON.parse($redis.get("user_id:#{current_user.id}_lessons_array") || '[]')
+    lessons_cache = $redis.get("user_id:#{current_user.id}_lessons_array")
+    if lessons_cache
+      JSON.parse(lessons_cache)
+    else
+      current_user.set_and_return_lessons_cache_data
+    end
   end
 
   def classroom_activity_params
