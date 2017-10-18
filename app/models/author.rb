6391fe8119b46af94ca4c93b826590fece8a8099
Jared Silver <jared@jaredsilver.name>
2017-10-18 09:14:28 -0400

    Update default avatar URL to image on CDN

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,7 +1,7 @@
 class Author < ActiveRecord::Base
   has_many :unit_templates
 
-  DEFAULT_AVATAR_URL = 'http://jaredsilver.name/headshot.jpg'
+  DEFAULT_AVATAR_URL = 'https://assets.quill.org/images/authors/placeholder.png'
 
   def avatar_url
     self.avatar.blank? ? DEFAULT_AVATAR_URL : self.avatar
