02f39ba64c8c69f50d6725ff09049dca0aaea22c
Emilia Friedberg <emiliafriedberg@gmail.com>
2017-10-27 11:43:54 -0400

    add quill-string-normalizer package; use instead of local function

--- app/libs/quillNormalizer.js
+++ app/libs/quillNormalizer.js
@@ -1,4 +1,5 @@
-export default function () {
-  // in order: double quotation marks, single quotation marks and apostrophes, commas
-  return this.replace(/[\u201C\u201D\u02DD]/g, '\u0022').replace(/[\u00B4\u0060\u2018\u2019\u0301\u02BB\u02C8]/g, '\u0027').replace(/[\u02CC\u201A\uFF0C]/g, '\u002C');
-};
+import {stringNormalize} from 'quill-string-normalizer'
+
+export default function() {
+  return stringNormalize(this)
+}
