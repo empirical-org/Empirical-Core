5c55f9837f1fe3366249ed7d339bdfa3752c27a4
RyanNovas <ryan@quill.org>
2017-10-30 18:33:43 -0400

    fixed sort

--- client/app/bundles/HelloWorld/components/lesson_planner/create_unit/activity_search/activity_search_sort/activity_search_sort_config.jsx
+++ client/app/bundles/HelloWorld/components/lesson_planner/create_unit/activity_search/activity_search_sort/activity_search_sort_config.jsx
@@ -1,27 +1,26 @@
-'use strict'
- export default  [
-    {
-      field: 'activity_classification',
-      alias: 'App',
-      selected: false,
-      asc_or_desc: 'asc'
-    },
-    {
-      field: 'activity',
-      alias: 'Activity',
-      selected: false,
-      asc_or_desc: 'asc'
-    },
-    {
-      field: 'section',
-      alias: 'Standard Level',
-      selected: false,
-      asc_or_desc: 'asc'
-    },
-    {
-      field: 'activity_category',
-      alias: 'Category',
-      selected: false,
-      asc_or_desc: 'asc'
-    }
+export default [
+  {
+    field: 'activity_classification',
+    alias: 'App',
+    selected: false,
+    asc_or_desc: 'asc',
+  },
+  {
+    field: 'name',
+    alias: 'Activity',
+    selected: false,
+    asc_or_desc: 'asc',
+  },
+  {
+    field: 'section',
+    alias: 'Standard Level',
+    selected: false,
+    asc_or_desc: 'asc',
+  },
+  {
+    field: 'activity_category',
+    alias: 'Category',
+    selected: false,
+    asc_or_desc: 'asc',
+  }
 ];
