51fa727a59a515cbc8f0d8085d3e57056f3a445f
Jared Silver <jared@jaredsilver.name>
2017-11-02 11:25:08 -0400

    Refactor some terrible nomenclature

--- spec/support/activity_progress_report.rb
+++ spec/support/activity_progress_report.rb
@@ -2,32 +2,31 @@ shared_context 'Activity Progress Report' do
   let(:teacher) { create :teacher }
 
   let(:activity) { create(:activity) }
-  let(:sweathogs) { create(:sweathogs, teacher: teacher) }
-  # Absolutely no way that this could get confusing.
-  let(:sweatdogs) { create(:classroom, name: "Sweatdogs", teacher: teacher)}
-  let!(:horshack) { create(:student, classrooms: [sweathogs]) }
-  let!(:barbarino) { create(:student, classrooms: [sweatdogs]) }
-  let(:sweathogs_classroom_activity) { create(:classroom_activity,
-    classroom: sweathogs, unit: sweathogs.units.first, activity: activity) }
-  let(:sweatdogs_classroom_activity) { create(:classroom_activity,
-    classroom: sweatdogs, unit: sweatdogs.units.first, activity: activity) }
+  let(:classroom_one) { create(:classroom, name: 'Classroom One', teacher: teacher) }
+  let(:classroom_two) { create(:classroom, name: 'Classroom Two', teacher: teacher)}
+  let!(:student_in_classroom_one) { create(:student, classrooms: [classroom_one]) }
+  let!(:student_in_classroom_two) { create(:student, classrooms: [classroom_two]) }
+  let(:classroom_one_activity) { create(:classroom_activity,
+    classroom: classroom_one, unit: classroom_one.units.first, activity: activity) }
+  let(:classroom_two_activity) { create(:classroom_activity,
+    classroom: classroom_two, unit: classroom_two.units.first, activity: activity) }
 
-  let(:horshack_session) do
-    horshack.activity_sessions.create!(
+  let(:student_one_session) do
+    student_in_classroom_one.activity_sessions.create!(
       state: 'finished',
       percentage: 0.777778,
-      classroom_activity: sweathogs_classroom_activity
+      classroom_activity: classroom_one_activity
     )
   end
-  let(:barbarino_session) do
-    barbarino.activity_sessions.create!(
+  let(:student_two_session) do
+    student_in_classroom_two.activity_sessions.create!(
       state: 'finished',
       percentage: 0.75,
-      classroom_activity: sweatdogs_classroom_activity
+      classroom_activity: classroom_two_activity
     )
   end
 
-  let(:sweathogs_sessions) { [horshack_session]}
-  let(:sweatdogs_sessions) { [barbarino_session] }
-  let!(:all_sessions) { sweathogs_sessions + sweatdogs_sessions}
+  let(:classroom_one_sessions) { [student_one_session]}
+  let(:classroom_two_sessions) { [student_two_session] }
+  let!(:all_sessions) { classroom_one_sessions + classroom_two_sessions}
 end
