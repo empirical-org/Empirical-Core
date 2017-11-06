272821debe81b21f4394e819f17185d5dc0690b8
Jared Silver <jared@jaredsilver.name>
2017-11-06 17:57:19 -0500

    Correct formatting

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,17 @@
 ActiveRecord::Base.connection.execute(File.read('./db/seeds.sql'))
 
 [:admin, :teacher, :student, :staff].each do |role|
- User.create(
-   name: "#{role.capitalize} #{role.capitalize}son",
-   email: "#{role}@quill.org",
-   password: role,
-   password_confirmation: role,
-   created_at: Time.now,
-   updated_at: Time.now,
-   classcode: nil,
-   active: true,
-   username: role,
-   token: nil,
-   role: role.to_s
- ) unless User.find_by_role(role)
+  User.create(
+    name: "#{role.capitalize} #{role.capitalize}son",
+    email: "#{role}@quill.org",
+    password: role,
+    password_confirmation: role,
+    created_at: Time.now,
+    updated_at: Time.now,
+    classcode: nil,
+    active: true,
+    username: role,
+    token: nil,
+    role: role.to_s
+  ) unless User.find_by_role(role)
 end
