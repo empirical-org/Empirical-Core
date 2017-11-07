3eb0e6055ab2f42f705c7cd2c200cad0cee68a9c
Emilia Friedberg <emiliafriedberg@gmail.com>
2017-11-07 10:51:38 -0500

    call name on user, not schoolsadmins model

--- app/workers/premium_school_subscription_email_worker.rb
+++ app/workers/premium_school_subscription_email_worker.rb
@@ -4,8 +4,8 @@ class PremiumSchoolSubscriptionEmailWorker
   def perform(user_id)
     @user = User.find(user_id)
     school = @user.school
-    admin = school.schools_admins.first
+    admin = school.schools_admins.first.try(:user)
     @user.send_premium_school_subscription_email(school, admin)
   end
-  
+
 end
