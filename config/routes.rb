980f049eea4a772669205f8bc9dac0057e746812
RyanNovas <ryan@quill.org>
2017-11-08 09:52:46 -0500

    ideas trello renders some of the page

--- config/routes.rb
+++ config/routes.rb
@@ -337,7 +337,7 @@ EmpiricalGrammar::Application.routes.draw do
     end
   end
 
-  other_pages = %w(beta board press partners develop mission faq tos privacy activities impact stats team premium teacher_resources media_kit play news home_new map firewall_info)
+  other_pages = %w(beta ideas board press partners develop mission faq tos privacy activities impact stats team premium teacher_resources media_kit play news home_new map firewall_info)
   all_pages = other_pages
   all_pages.each do |page|
     get page => "pages##{page}", as: "#{page}"
