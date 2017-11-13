51867aa12dab209dd03e214c0a5cf05c26911ebe
RyanNovas <ryan@quill.org>
2017-11-13 13:53:25 -0500

    progress report spec wip

--- spec/serializers/progress_reports/standards/classroom_serializer_spec.rb
+++ spec/serializers/progress_reports/standards/classroom_serializer_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
 describe ProgressReports::Standards::ClassroomSerializer, type: :serializer do
-  let(:teacher) { create(:teacher) }
-  let!(:classroom) { create(:classroom_with_one_student, teacher: teacher) }
+  let!(:classroom) { create(:classroom_with_one_student) }
+  let(:teacher) { classroom.teacher }
   let(:activity) { create(:activity) }
   let(:classroom_activity) { create(:classroom_activity, classroom: classroom, activity: activity) }
   let(:classroom_for_report) { ProgressReports::Standards::Classroom.new(teacher).results({}).first }
