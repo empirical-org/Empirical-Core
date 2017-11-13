97ab3f6e4906800a6ca12d2e4bac89010c8016ac
RyanNovas <ryan@quill.org>
2017-11-13 14:50:06 -0500

    Fix teacher data spec and TeachersData query

--- spec/factories/classroom.rb
+++ spec/factories/classroom.rb
@@ -10,10 +10,10 @@ FactoryBot.define do
     # trait :from_clever do
     #   clever_id { (1..24).map { (('a'..'f').to_a + (1..9).to_a).sample }.join } # mock a clever id
     # end
-    #
-    # factory :classroom_with_a_couple_students do
-    #   students { create_pair(:student) }
-    # end
+    
+    factory :classroom_with_a_couple_students do
+      students { create_pair(:student) }
+    end
 
     factory :classroom_with_one_student do
       students { create_list(:student, 1) }
