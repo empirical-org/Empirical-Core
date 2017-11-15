35d6a18af413d1c162765ec5f2faffb58a2eba10
Donald McKendrick <ddmckendrick@gmail.com>
2017-11-15 16:17:14 -0500

    Get the unique words in the corpus.

--- src/processSentences.spec.ts
+++ src/processSentences.spec.ts
@@ -1,5 +1,5 @@
 import { assert } from 'chai';
-import {processSentences} from  './processSentences'
+import {processSentences, uniqueWordsFromSentences} from  './processSentences'
 
 describe('The processSentences function', () => {
   it('Should take an array of sentences and return \n seperated words in a list', () => {
@@ -22,6 +22,17 @@ describe('The processSentences function', () => {
     ]
     const expected = 'Even\nthough\nteams\nmay\ndisagree\nthey\nsupport\neach\nother\nTeams\nsupport\neach\nother\neven\nthough\nthey\nmay\ndisagree\nEven\nthough\nteams\ndisagree\nthey\nsupport\neach\nother';
     assert.equal(processSentences(sentences), expected);
+  })
+
+  it('Should be able to return the unique words from the sentences', () => {
+    const sentences = [
+      'Even though teams may disagree, they support each other.',
+      'Teams may disagree even though they support each other.',
+      'Teams support each other even though they may disagree.',
+      'Even though teams disagree, they support each other.'
+    ]
+    const expected = ['Even', 'though', 'teams', 'may', 'disagree', 'they', 'support', 'each', 'other', 'Teams', 'even'];
+    assert.deepEqual(uniqueWordsFromSentences(sentences), expected);
   })
 
   
--- src/processSentences.ts
+++ src/processSentences.ts
@@ -6,3 +6,15 @@ export function processSentences(sentences: string[]): string {
     .filter(sentence => sentence !== '') // remove empty strings
     .join('\n'); // join the remaining strings
 }
+
+export function uniqueWordsFromSentences(sentences: string[]): string[] {
+  const nestedWords = sentences
+    .map(sentence => processSentence(sentence).split('\n')) // process the individual sentences
+    .filter(sentence => sentence !== [] || sentence !== ['']); // remove empty arrays
+
+  const flattenedWords: string[] = [].concat.apply([], nestedWords); // flatten the arrays
+
+  return flattenedWords.filter(function(item, pos){
+    return flattenedWords.indexOf(item) === pos; // remove the word if its position is not the first appearance in the array.
+  });
+}
