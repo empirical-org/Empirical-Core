2d87217e62c044669d08cae599552134e1e65082
RyanNovas <ryan@quill.org>
2017-11-20 14:29:42 -0500

    Passing test suite!

--- app/services/clever_integration/sign_up/clever_district.rb
+++ app/services/clever_integration/sign_up/clever_district.rb
@@ -2,7 +2,7 @@ module CleverIntegration::SignUp::CleverDistrict
 
   def self.run(auth_hash)
     parsed_data = CleverIntegration::Parsers::DistrictFromAuth.run(auth_hash)
-    district = CleverIntegration::Creators::District.run(parsed_data)
+    district = CleverIntegration::Creators::CleverDistrict.run(parsed_data)
     {type: 'district_success', data: district}
   end
 end
