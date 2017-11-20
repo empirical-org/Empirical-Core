cca82db42e2bf31d86380fc5a6df6065d8e7a915
RyanNovas <ryan@quill.org>
2017-11-20 17:00:56 -0500

    wip 29 failing tests

--- app/controllers/teachers/units_controller.rb
+++ app/controllers/teachers/units_controller.rb
@@ -13,9 +13,9 @@ class Teachers::UnitsController < ApplicationController
     end
     units_with_same_name = units_with_same_name_by_current_user(params[:unit][:name], current_user.id)
     if units_with_same_name.any?
-      Units::Updater.run(units_with_same_name.first.id, params[:unit][:activities], params[:unit][:classrooms])
+      Units::Updater.run(units_with_same_name.first.id, params[:unit][:activities], params[:unit][:classrooms], current_user.id)
     else
-      Units::Creator.run(current_user, params[:unit][:name], params[:unit][:activities], params[:unit][:classrooms], params[:unit][:unit_template_id])
+      Units::Creator.run(current_user, params[:unit][:name], params[:unit][:activities], params[:unit][:classrooms], params[:unit][:unit_template_id], current_user.id)
     end
     render json: {id: Unit.where(user: current_user).last.id}
   end
--- app/services/units/creator.rb
+++ app/services/units/creator.rb
@@ -1,11 +1,11 @@
 module Units::Creator
 
 
-  def self.run(teacher, name, activities_data, classrooms_data, unit_template_id=nil)
-    self.create_helper(teacher, name, activities_data, classrooms_data, unit_template_id)
+  def self.run(teacher, name, activities_data, classrooms_data, unit_template_id=nil, current_user_id=nil)
+    self.create_helper(teacher, name, activities_data, classrooms_data, unit_template_id, current_user_id)
   end
 
-  def self.fast_assign_unit_template(teacher_id, unit_template_id)
+  def self.fast_assign_unit_template(teacher_id, unit_template_id, current_user_id=nil))
     # unit fix: pass whole teacher object
     teacher = User.find(teacher_id)
     # this call is unnecessary as we can do sql without it
@@ -13,10 +13,10 @@ module Units::Creator
     activities_data = unit_template.activities.map{ |a| {id: a.id, due_date: nil} }
     # unit fix: may be able to better optimize this one, but possibly not
     classrooms_data = teacher.classrooms_i_teach.map{ |c| {id: c.id, student_ids: [], assign_on_join: true} }
-    self.create_helper(teacher, unit_template.name, activities_data, classrooms_data, unit_template_id)
+    self.create_helper(teacher, unit_template.name, activities_data, classrooms_data, unit_template_id, current_user_id)
   end
 
-  def self.assign_unit_template_to_one_class(teacher_id, unit_template_id, classroom)
+  def self.assign_unit_template_to_one_class(teacher_id, unit_template_id, classroom, current_user_id=nil))
     classroom_array = [classroom]
     # converted to array so we can map in helper function as we would otherwise
     # unit fix: pass whole teacher object
@@ -24,12 +24,12 @@ module Units::Creator
     # this call is unnecessary as we can do sql without it
     unit_template = UnitTemplate.find(unit_template_id)
     activities_data = unit_template.activities.map{ |a| {id: a.id, due_date: nil} }
-    self.create_helper(teacher, unit_template.name, activities_data, classroom_array, unit_template_id)
+    self.create_helper(teacher, unit_template.name, activities_data, classroom_array, unit_template_id, current_user_id)
   end
 
   private
 
-  def self.create_helper(teacher, name, activities_data, classrooms, unit_template_id=nil)
+  def self.create_helper(teacher, name, activities_data, classrooms, unit_template_id=nil, current_user_id)
     unit = Unit.create!(name: name, user: teacher, unit_template_id: unit_template_id)
     # makes a permutation of each classroom with each activity to
     # create all necessary activity sessions
@@ -48,7 +48,7 @@ module Units::Creator
     unit.email_lesson_plan
     unit.hide_if_no_visible_classroom_activities
     # activity_sessions in the state of 'unstarted' are automatically created in an after_create callback in the classroom_activity model
-    AssignActivityWorker.perform_async(teacher.id)
+    AssignActivityWorker.perform_async(current_user_id)
   end
 
 
