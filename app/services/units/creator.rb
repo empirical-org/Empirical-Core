a8818bfe866eb64ff14cc1c273ad531eb357c769
RyanNovas <ryan@quill.org>
2017-11-21 10:01:56 -0600

    checkbox and milestone fixes

--- app/services/units/creator.rb
+++ app/services/units/creator.rb
@@ -48,7 +48,7 @@ module Units::Creator
     unit.email_lesson_plan
     unit.hide_if_no_visible_classroom_activities
     # activity_sessions in the state of 'unstarted' are automatically created in an after_create callback in the classroom_activity model
-    AssignActivityWorker.perform_async(current_user_id)
+    AssignActivityWorker.perform_async(current_user_id || teacher.id)
   end
 
 
