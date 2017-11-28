645525f944dfab0a3ba4e0e9cd72f8c9267752e1
RyanNovas <ryan@quill.org>
2017-11-28 14:45:58 -0500

    rename coteacher invitations to pending invitations

--- spec/controllers/coteacher_invitation_controller_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe CoteacherInvitationController, type: :controller do
-
-end
--- spec/factories/coteacher_invitations.rb
+++
@@ -1,5 +0,0 @@
-FactoryBot.define do
-  factory :coteacher_invitation do
-    
-  end
-end
