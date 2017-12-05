eba2f60a49f3853eb0a129973d8a6bd40761ae10
Jared Silver <jared@jaredsilver.name>
2017-12-05 12:12:18 -0500

    Destroy parent invitation if there are no children

--- app/models/coteacher_classroom_invitation.rb
+++ app/models/coteacher_classroom_invitation.rb
@@ -2,13 +2,12 @@ class CoteacherClassroomInvitation < ActiveRecord::Base
   belongs_to :invitation
   belongs_to :classroom
 
-  after_commit :update_parent_invitation, on: :destroy
+  after_destroy :update_parent_invitation
 
   private
   def update_parent_invitation
-    invitations = CoteacherClassroomInvitation.where(invitation_id: previous_changes[:invitation_id])
-    unless invitations.any?
-      Invitation.find(parent_invitation_id).update(archived: true)
+    unless CoteacherClassroomInvitation.exists?(invitation_id: self.invitation_id)
+      Invitation.find(self.invitation_id).update(archived: true)
     end
   end
 end
