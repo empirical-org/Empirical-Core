e35a01564b2367baff7bed6b5b08b88260ddc44f
Jared Silver <jared@jaredsilver.name>
2017-12-07 17:41:20 -0500

    Plug some holes leading to nil values in the AssignActivityWorker

--- app/workers/assign_recommendations_worker.rb
+++ app/workers/assign_recommendations_worker.rb
@@ -8,7 +8,7 @@ class AssignRecommendationsWorker
     unit = Unit.find_by(unit_template_id: unit_template_id, user_id: teacher.id) || Unit.find_by(name: UnitTemplate.find(unit_template_id).name, user_id: teacher.id)
     classroom_data = {id: classroom_id, student_ids: student_id_array, assign_on_join: assign_on_join}
     if unit
-        Units::Updater.assign_unit_template_to_one_class(unit.id, classroom_data, unit_template_id)
+        Units::Updater.assign_unit_template_to_one_class(unit.id, classroom_data, unit_template_id, teacher.id)
     else
         #  TODO: use a find or create for the unit var above.
         #  This way, we can just pass the units creator a unit argument.
