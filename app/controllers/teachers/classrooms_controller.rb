0fc31e7e10b7f6dac58ada2340cf071f8d63c7c0
Jared Silver <jared@jaredsilver.name>
2017-12-12 11:09:26 -0500

    Redirect to invite students page upon classroom creation if classroom has no students and teacher has assigned units

--- app/controllers/teachers/classrooms_controller.rb
+++ app/controllers/teachers/classrooms_controller.rb
@@ -32,7 +32,9 @@ class Teachers::ClassroomsController < ApplicationController
   def create
     @classroom = Classroom.create(classroom_params.merge(teacher: current_user))
     if @classroom.valid?
-      render json: {classroom: @classroom, toInviteStudents: @classroom.students.empty?}
+      # For onboarding purposes, we don't want to prompt a teacher to invite students before they've assigned any units.
+      should_redirect_to_invite_students = @classroom.students.empty? && current_user.units.any?
+      render json: {classroom: @classroom, toInviteStudents: should_redirect_to_invite_students}
     else
        render json: {errors: @classroom.errors.full_messages }, status: 422
     end
