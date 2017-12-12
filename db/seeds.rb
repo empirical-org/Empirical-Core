959fd4ae2ac9d9c3098efb5fba9119f0bfa69178
Jared Silver <jared@jaredsilver.name>
2017-12-12 14:55:44 -0500

    Seedfile sql paths

--- db/seeds.rb
+++ db/seeds.rb
@@ -25,35 +25,38 @@ create(:complete_diagnostic_milestone)
 create(:publish_customized_lesson_milestone)
 create(:complete_customized_lesson_milestone)
 
+# Path to SQL seeds files
+dir_path = File.dirname(__FILE__) + '/seeds/'
+
 # Import concepts
-ActiveRecord::Base.connection.execute(File.read('seeds/concepts.sql'))
+ActiveRecord::Base.connection.execute(File.read(dir_path + 'concepts.sql'))
 
 # Import activities
-ActiveRecord::Base.connection.execute(File.read('seeds/activities.sql'))
+ActiveRecord::Base.connection.execute(File.read(dir_path + 'activities.sql'))
 
 # Import categories
-ActiveRecord::Base.connection.execute(File.read('seeds/categories.sql'))
+ActiveRecord::Base.connection.execute(File.read(dir_path + 'categories.sql'))
 
 # Import activity categories
-ActiveRecord::Base.connection.execute(File.read('seeds/activity_categories.sql'))
+ActiveRecord::Base.connection.execute(File.read(dir_path + 'activity_categories.sql'))
 
 # Import activity category activities
-ActiveRecord::Base.connection.execute(File.read('seeds/activity_category_activities.sql'))
+ActiveRecord::Base.connection.execute(File.read(dir_path + 'activity_category_activities.sql'))
 
 # Import unit templates
-ActiveRecord::Base.connection.execute(File.read('seeds/unit_templates.sql'))
+ActiveRecord::Base.connection.execute(File.read(dir_path + 'unit_templates.sql'))
 
 # Import unit template categories
-ActiveRecord::Base.connection.execute(File.read('seeds/unit_template_categories.sql'))
+ActiveRecord::Base.connection.execute(File.read(dir_path + 'unit_template_categories.sql'))
 
 # Import activities unit templates
-ActiveRecord::Base.connection.execute(File.read('seeds/activities_unit_templates.sql'))
+ActiveRecord::Base.connection.execute(File.read(dir_path + 'activities_unit_templates.sql'))
 
 # Import topics
-ActiveRecord::Base.connection.execute(File.read('seeds/topics.sql'))
+ActiveRecord::Base.connection.execute(File.read(dir_path + 'topics.sql'))
 
 # Import activities topic categories
-ActiveRecord::Base.connection.execute(File.read('seeds/topic_categories.sql'))
+ActiveRecord::Base.connection.execute(File.read(dir_path + 'topic_categories.sql'))
 
 # Generate sections
 create(:grade_1_section)
