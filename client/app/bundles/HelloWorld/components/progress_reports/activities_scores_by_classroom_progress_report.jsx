76e361f5508ceb8fb02e5ce2dfa6e8b551c39962
RyanNovas <ryan@quill.org>
2017-12-14 10:40:41 -0500

    student_overview loads csv

--- client/app/bundles/HelloWorld/components/progress_reports/activities_scores_by_classroom_progress_report.jsx
+++ client/app/bundles/HelloWorld/components/progress_reports/activities_scores_by_classroom_progress_report.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import request from 'request'
+import {CSVDownload, CSVLink} from 'react-csv'
 
 export default class extends React.Component {
 
@@ -17,9 +18,18 @@ export default class extends React.Component {
       url: `${process.env.DEFAULT_URL}/api/v1/progress_reports/activities_scores_by_classroom_data`,
     },
     (e, r, body) => {
-      const data = JSON.parse(body)
-      that.setState({loading: false, errors: body.errors, classroomsData: body.classrooms_data});
+      const data = JSON.parse(body).data
+      const csvData = this.formatDataForCSV(data)
+      that.setState({loading: false, errors: body.errors, classroomsData: data, csvData});
     });
+  }
+
+  formatDataForCSV(data){
+    const csvData = [['Classroom Name', 'Student Name', 'Average Score', 'Activity Count']]
+    data.forEach((row)=>{
+      csvData.push([row['classroom_name'], row['name'], row['average_score']*100, row['activity_count']])
+    })
+    return csvData
   }
 
 	render() {
@@ -34,6 +44,7 @@ export default class extends React.Component {
     <div>
       {errors}
       'HOLA!'
+      <CSVLink data={this.state.csvData} target="_blank">Link Report</CSVLink>
     </div>
   	)}
 
--- client/app/bundles/HelloWorld/containers/ProgressReportIndex.jsx
+++ client/app/bundles/HelloWorld/containers/ProgressReportIndex.jsx
@@ -9,6 +9,7 @@ import ConceptsConceptsProgressReport from '../components/progress_reports/conce
 import PremiumBannerBuilder from '../components/scorebook/premium_banners/premium_banner_builder'
 import LandingPageContainer from './LandingPageContainer.jsx'
 import ActivitiesScoresByClassroomProgressReport from '../components/progress_reports/activities_scores_by_classroom_progress_report.jsx'
+import StudentOverview from '../components/progress_reports/student_overview.jsx'
 import $ from 'jquery'
 import _ from 'underscore'
 import React from 'react'
@@ -36,6 +37,7 @@ var renderRightComponentHack = function() {
     '.progress-reports-concepts-concepts': ConceptsConceptsProgressReport,
     '.progress-reports-landing-page-container': LandingPageContainer,
     '.progress-reports-activities-scores-by-classroom': ActivitiesScoresByClassroomProgressReport,
+    '.progress-reports-student-overview': StudentOverview,
   };
 
   _.each(progressReportMapping, function(component, rootNodeSelector) {
