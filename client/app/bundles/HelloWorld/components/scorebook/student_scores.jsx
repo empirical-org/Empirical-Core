6fc838cede550e7aace5c828c302f55bcdd9d528
Jared Silver <jared@jaredsilver.name>
2017-12-18 11:34:58 -0500

    Add link to premium report, add average score to scorebook

--- client/app/bundles/HelloWorld/components/scorebook/student_scores.jsx
+++ client/app/bundles/HelloWorld/components/scorebook/student_scores.jsx
@@ -1,4 +1,3 @@
-import _ from 'underscore';
 import React from 'react';
 import ActivityIconWithTooltip from '../general_components/activity_icon_with_tooltip';
 
@@ -12,12 +11,32 @@ export default React.createClass({
     return this.props.data.scores.map((score, index) => <ActivityIconWithTooltip key={`${this.props.data.name} ${index} ${score.caId}`} data={score} premium_state={this.props.premium_state} context={'scorebook'} />);
   },
 
+  calculateAverageScore() {
+    // We want to ignore classroom lessons and diagnostic activities since these values will always be 100%.
+    const nonRelevantActivityClassificationIds = [4, 6];
+    let totalScore = 0;
+    let relevantScores = 0;
+    this.props.data.scores.forEach(score => {
+      if(!nonRelevantActivityClassificationIds.includes(score.activity_classification_id) && score.percentage) {
+        relevantScores++;
+        totalScore += parseFloat(score.percentage);
+      }
+    });
+    const averageScore = totalScore / relevantScores;
+    if(averageScore) {
+      return `${Math.round(averageScore * 100)}% Avg. Score`;
+    } else {
+      return '';
+    }
+  },
+
   render() {
     return (
       <section className="overview-section">
         <header className="student-header">
           <h3 className="student-name">{this.props.data.name}</h3>
-          <a target="_blank" href={`/teachers/progress_reports/activity_sessions?student_id=${this.props.data.userId}&classroom_id=${this.props.data.classroomId}`}><i className="fa fa-list" /> View List</a>
+          <p className="average-score">{this.calculateAverageScore()}</p>
+          <a className="activity-scores-link" target="_blank" href={`/teachers/progress_reports/activity_sessions?student_id=${this.props.data.userId}&classroom_id=${this.props.data.classroomId}`}>View Activity Scores <i className="fa fa-star" /></a>
         </header>
         <div className="flex-row vertically-centered">
           {this.handleScores()}
