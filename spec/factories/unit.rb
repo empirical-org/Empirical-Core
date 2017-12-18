6f55786362db6b5aa9be9814d3f3bc2e5717525e
Jared Silver <jared@jaredsilver.name>
2017-12-18 17:45:12 -0500

    Some minor changes to factories

--- spec/factories/unit.rb
+++ spec/factories/unit.rb
@@ -2,5 +2,9 @@ FactoryBot.define do
   factory :unit do
     sequence(:name) { |i| "Unit #{i}" }
     user_id         { create(:teacher).id }
+
+    trait :sentence_structure_diagnostic do
+      name { 'Sentence Structure Diagnostic' }
+    end
   end
 end
