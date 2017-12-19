6ed08616772b6a839cae8d8ffe16124a8e660d60
Jared Silver <jared@jaredsilver.name>
2017-12-19 14:51:58 -0500

    Some cleanup 🚽

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,7 +1,3 @@
-# require 'carrierwave/storage/abstract'
-# require 'carrierwave/storage/file'
-# require 'carrierwave/storage/fog'
-
 CarrierWave.configure do |config|
   config.cache_dir = "#{Rails.root}/tmp/"
   config.permissions = 0666
