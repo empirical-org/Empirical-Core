2fdddfb5ed09520f7aaa5750ca3e0d2f2844ff21
RyanNovas <ryan@quill.org>
2017-12-20 12:20:52 -0500

    improved sql queries
    improved sql queries

--- app/queries/progress_reports/student_overview.rb
+++ app/queries/progress_reports/student_overview.rb
@@ -7,6 +7,7 @@ class ProgressReports::StudentOverview
        JOIN units ON units.id = classroom_activities.unit_id
        JOIN activities ON activities.id = classroom_activities.activity_id
        WHERE classroom_activities.classroom_id = #{ActiveRecord::Base.sanitize(classroom_id)} AND #{ActiveRecord::Base.sanitize(student_id)} = ANY (classroom_activities.assigned_student_ids::int[])
+       AND classroom_activities.visible = true AND units.visible = true AND activity_sessions.visible = true AND activity_sessions.is_final_score = true
        GROUP BY units.name, activity_sessions.completed_at, activities.name, activity_sessions.percentage, classroom_activities.completed, activity_sessions.id, activities.activity_classification_id, activities.id, units.id, classroom_activities.id
        ORDER BY activity_sessions.completed_at").to_a
   end
