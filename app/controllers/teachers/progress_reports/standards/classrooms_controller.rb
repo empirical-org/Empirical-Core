c5fa21e34ed7223f847f870ecd214e7bbae82b49
Emilia Friedberg <emiliafriedberg@gmail.com>
2017-12-21 17:46:15 -0500

    get students on standards page

--- app/controllers/teachers/progress_reports/standards/classrooms_controller.rb
+++ app/controllers/teachers/progress_reports/standards/classrooms_controller.rb
@@ -13,7 +13,7 @@ class Teachers::ProgressReports::Standards::ClassroomsController < Teachers::Pro
           data: data,
           teacher: UserWithEmailSerializer.new(current_user).as_json(root: false),
           classrooms: current_user.classrooms_i_teach,
-          students: student_names_and_ids
+          students: student_names_and_ids(classroom_id)
         }
       end
     end
@@ -22,12 +22,16 @@ end
 
 private
 
-def student_names_and_ids
-  ActiveRecord::Base.connection.execute("SELECT students.name, students.id FROM users AS teacher
-  JOIN classrooms_teachers AS ct ON ct.user_id = teacher.id
-  JOIN classrooms ON classrooms.id = ct.classroom_id AND classrooms.visible = TRUE
-  JOIN students_classrooms AS sc ON sc.classroom_id = ct.classroom_id
-  JOIN users AS students ON students.id = sc.student_id
-  WHERE teacher.id = #{current_user.id}
-  ORDER BY substring(students.name, '([^[:space:]]+)(?:,|$)')").to_a
+def student_names_and_ids(classroom_id)
+  if classroom_id
+    classroom_conditional = "AND classrooms.id = #{classroom_id}"
+  end
+    ActiveRecord::Base.connection.execute("SELECT students.name, students.id FROM users AS teacher
+    JOIN classrooms_teachers AS ct ON ct.user_id = teacher.id
+    JOIN classrooms ON classrooms.id = ct.classroom_id AND classrooms.visible = TRUE
+    JOIN students_classrooms AS sc ON sc.classroom_id = ct.classroom_id
+    JOIN users AS students ON students.id = sc.student_id
+    WHERE teacher.id = #{current_user.id}
+    #{classroom_conditional}
+    ORDER BY substring(students.name, '([^[:space:]]+)(?:,|$)')").to_a
 end
