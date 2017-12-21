b0f3b6b2429e73d34a11006450bf0a4c653f4cf9
Emilia Friedberg <emiliafriedberg@gmail.com>
2017-12-21 14:27:17 -0500

    fix merge conflict

--- app/controllers/teachers/progress_reports/standards/classrooms_controller.rb
+++ app/controllers/teachers/progress_reports/standards/classrooms_controller.rb
@@ -1,17 +1,14 @@
 class Teachers::ProgressReports::Standards::ClassroomsController < Teachers::ProgressReportsController
+
   def index
-    respond_to do |format|
-      format.html do
-        AccessProgressReportWorker.perform_async(current_user.id)
-      end
 
       format.json do
         render json: {
-          ::ProgressReports::Standards::AllClassroomsTopic.new(current_user)
-          classrooms: classroom_json,
+          data: ::ProgressReports::Standards::AllClassroomsTopic.new(current_user)
           teacher: UserWithEmailSerializer.new(current_user).as_json(root: false)
         }
       end
     end
   end
+
 end
