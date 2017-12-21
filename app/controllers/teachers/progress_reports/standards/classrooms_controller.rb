3e8a3f3210912c9ff5c8b58f31ca04fbbd5cb05a
Emilia Friedberg <emiliafriedberg@gmail.com>
2017-12-21 16:57:19 -0500

    standards page renders; filterable by classroom

--- app/controllers/teachers/progress_reports/standards/classrooms_controller.rb
+++ app/controllers/teachers/progress_reports/standards/classrooms_controller.rb
@@ -1,14 +1,33 @@
 class Teachers::ProgressReports::Standards::ClassroomsController < Teachers::ProgressReportsController
-
   def index
+    respond_to do |format|
+      format.html do
+        AccessProgressReportWorker.perform_async(current_user.id)
+      end
 
       format.json do
+        classroom_id = params[:classroom_id]
+        student_id = nil
+        data = ::ProgressReports::Standards::AllClassroomsTopic.new(current_user).results(classroom_id, student_id)
         render json: {
-          data: ::ProgressReports::Standards::AllClassroomsTopic.new(current_user)
-          teacher: UserWithEmailSerializer.new(current_user).as_json(root: false)
+          data: data,
+          teacher: UserWithEmailSerializer.new(current_user).as_json(root: false),
+          classrooms: current_user.classrooms_i_teach,
+          students: student_names_and_ids
         }
       end
     end
   end
+end
 
+private
+
+def student_names_and_ids
+  ActiveRecord::Base.connection.execute("SELECT students.name, students.id FROM users AS teacher
+  JOIN classrooms_teachers AS ct ON ct.user_id = teacher.id
+  JOIN classrooms ON classrooms.id = ct.classroom_id AND classrooms.visible = TRUE
+  JOIN students_classrooms AS sc ON sc.classroom_id = ct.classroom_id
+  JOIN users AS students ON students.id = sc.student_id
+  WHERE teacher.id = #{current_user.id}
+  ORDER BY substring(students.name, '([^[:space:]]+)(?:,|$)')").to_a
 end
--- app/queries/progress_reports/standards/all_classrooms_topic.rb
+++ app/queries/progress_reports/standards/all_classrooms_topic.rb
@@ -4,18 +4,18 @@ class ProgressReports::Standards::AllClassroomsTopic
     @proficiency_cutoff = ProficiencyEvaluator.proficiency_cutoff
   end
 
-  def results(filters)
-    ActiveRecord.Base::connection.execute("WITH final_activity_sessions AS
+  def results(classroom_id, student_id)
+    ActiveRecord::Base.connection.execute("WITH final_activity_sessions AS
      (SELECT activity_sessions.*, activities.topic_id FROM activity_sessions
           JOIN classroom_activities ON activity_sessions.classroom_activity_id = classroom_activities.id
           JOIN activities ON classroom_activities.activity_id = activities.id
           JOIN topics ON topics.id = activities.topic_id
-          JOIN classrooms ON classroom_activities.classroom_id = classrooms.id
-          JOIN classrooms_teachers ON classrooms.id = classrooms_teachers.classroom_id AND classrooms_teachers.user_id = 591975
+          #{classroom_joins(classroom_id)}
           WHERE activity_sessions.is_final_score
+          #{student_condition(student_id)}
+          #{classroom_condition(classroom_id)}
           AND activity_sessions.visible
           AND classroom_activities.visible)
-
       SELECT topics.id,
         topics.name,
         sections.name as section_name,
@@ -31,6 +31,24 @@ class ProgressReports::Standards::AllClassroomsTopic
           HAVING AVG(percentage) >= 0.8
           ) as avg_score_for_topic_by_user ON avg_score_for_topic_by_user.topic_id = topics.id
         GROUP BY topics.id, sections.name
-        ORDER BY topics.name asc")
+        ORDER BY topics.name asc").to_a
+  end
+
+  def classroom_joins(classroom_id)
+    if !classroom_id
+      "JOIN classrooms ON classroom_activities.classroom_id = classrooms.id JOIN classrooms_teachers ON classrooms.id = classrooms_teachers.classroom_id AND classrooms_teachers.user_id = #{@teacher.id}"
+    end
+  end
+
+  def classroom_condition(classroom_id)
+    if classroom_id
+      "AND classroom_activities.classroom_id = #{classroom_id}"
+    end
+  end
+
+  def student_condition(student_id)
+    if student_id
+      "AND activity_sessions.user_id = #{student_id}"
+    end
   end
 end
