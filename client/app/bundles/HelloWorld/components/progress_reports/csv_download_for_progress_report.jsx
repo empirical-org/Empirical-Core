06c8206045d1621dbb4fcb503ed8a1e27b9db849
RyanNovas <ryan@quill.org>
2018-01-09 17:44:17 -0500

    better printing

--- client/app/bundles/HelloWorld/components/progress_reports/csv_download_for_progress_report.jsx
+++ client/app/bundles/HelloWorld/components/progress_reports/csv_download_for_progress_report.jsx
@@ -3,11 +3,12 @@ import {CSVDownload, CSVLink} from 'react-csv'
 import userIsPremium from '../modules/user_is_premium'
 
 export default class extends React.Component {
-  //
+
   constructor(props) {
     super(props)
     this.state = {
-      userIsPremium: userIsPremium()
+      userIsPremium: userIsPremium(),
+      showDropdown: false
     }
   }
 
@@ -15,16 +16,32 @@ export default class extends React.Component {
     alert('Downloadable reports are a Premium feature. You can visit Quill.org/premium to upgrade now!')
   }
 
+  toggleDropdown(){
+    this.setState({showDropdown: !this.state.showDropdown})
+  }
+
   render() {
-    if (this.state.userIsPremium && this.props.data) {
+    // if (this.state.userIsPremium && this.props.data) {
+      let dropdown
+      if (this.state.dropdown) {
+        dropdown = <div>Show Me</div>
+      }
       return (
-        <CSVLink data={this.props.data} target="_blank">
-          <button style={{display: 'block'}} className={this.props.className || 'btn button-green'}>{this.props.buttonCoppy || "Download Report"}</button>
-        </CSVLink>
+        <div className='downloadButtonWrapper'>
+          <button onClick={this.toggleDropdown}>
+            Dropdown
+          </button>
+          {dropdown}
+        </div>
       )
-    } else {
-      return <button style={{display: 'block'}} onClick={this.handleClick} className={this.props.className || 'btn button-green'}>{this.props.buttonCoppy || "Download Report"}</button>
-    }
+      // return (
+      //   <CSVLink data={this.props.data} target="_blank">
+      //     <button style={{display: 'block'}} className={this.props.className || 'btn button-green'}>{this.props.buttonCoppy || "Download Report"}</button>
+      //   </CSVLink>
+      // )
+    // } else {
+    //   return <button style={{display: 'block'}} onClick={this.handleClick} className={this.props.className || 'btn button-green'}>{this.props.buttonCoppy || "Download Report"}</button>
+    // }
   }
 
 }
