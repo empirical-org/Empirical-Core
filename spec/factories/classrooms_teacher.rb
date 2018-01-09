636c2e4dbd5f2308f327187f5f6b49925b778a8d
Jared Silver <jared@jaredsilver.name>
2018-01-09 10:33:06 -0500

    Remove unnecessary id sequencing

--- spec/factories/classrooms_teacher.rb
+++ spec/factories/classrooms_teacher.rb
@@ -1,6 +1,5 @@
 FactoryBot.define do
   factory :classrooms_teacher do
-    sequence(:id) { |n| n } # not sure why this would be necessary; something in our configuration is likely broken
     user          { create(:teacher) }
     classroom     { create(:classroom) }
     role          ClassroomsTeacher::ROLE_TYPES[:owner]
