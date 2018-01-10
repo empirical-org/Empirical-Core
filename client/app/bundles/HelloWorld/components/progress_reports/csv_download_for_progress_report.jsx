d6491546566e2646283768b193c17bd4c7c52cd0
RyanNovas <ryan@quill.org>
2018-01-10 14:43:47 -0500

    download dropdown
    resolves margin issue on dropdown

--- client/app/bundles/HelloWorld/components/progress_reports/csv_download_for_progress_report.jsx
+++ client/app/bundles/HelloWorld/components/progress_reports/csv_download_for_progress_report.jsx
@@ -10,10 +10,11 @@ export default class extends React.Component {
       userIsPremium: userIsPremium(),
       showDropdown: false
     }
+    this.toggleDropdown = this.toggleDropdown.bind(this)
   }
 
   handleClick(){
-    alert('Downloadable reports are a Premium feature. You can visit Quill.org/premium to upgrade now!')
+    alert('Downloadable reports are a Premium feature. Visit Quill.org/premium to upgrade now!')
   }
 
   toggleDropdown(){
@@ -22,23 +23,35 @@ export default class extends React.Component {
 
   render() {
     // if (this.state.userIsPremium && this.props.data) {
-      let dropdown
-      if (this.state.dropdown) {
-        dropdown = <div>Show Me</div>
+      let dropdown, style, c
+      if (this.state.showDropdown) {
+        style = {
+          // border: "solid 1px #00c2a2",
+          boxShadow: "0 0 0 1px #00c2a2",
+          backgroundColor:'#fff',
+          color: '#00c2a2'
+        }
+        dropdown = (<div className='dropdown-content'>
+                      Download report as:
+                      <span className='pull-right'>
+                      <div><i className="fa  fa-caret-up gray-caret" aria-hidden="true"></i></div>
+                      <div><i className="fa fa-caret-up " aria-hidden="true"></i></div>
+                    </span>
+
+                      <div className='button-wrapper'>
+                        <button onClick={window.print}><img src="https://assets.quill.org/images/icons/pdf-icon.svg" alt="print"/>Print</button>
+                            <CSVLink data={this.props.data} target="_blank">
+                              <button><img src="https://assets.quill.org/images/icons/csv-icon.svg" alt="csv"/>CSV</button>
+                            </CSVLink>
+                      </div>
+                    </div>)
       }
       return (
-        <div className='downloadButtonWrapper'>
-          <button onClick={this.toggleDropdown}>
-            Dropdown
-          </button>
+        <div className='download-button-wrapper'>
+          <button onClick={this.toggleDropdown} style={style} className={this.props.className || 'btn button-green'}>{this.props.buttonCoppy || "Download Report"}</button>
           {dropdown}
         </div>
       )
-      // return (
-      //   <CSVLink data={this.props.data} target="_blank">
-      //     <button style={{display: 'block'}} className={this.props.className || 'btn button-green'}>{this.props.buttonCoppy || "Download Report"}</button>
-      //   </CSVLink>
-      // )
     // } else {
     //   return <button style={{display: 'block'}} onClick={this.handleClick} className={this.props.className || 'btn button-green'}>{this.props.buttonCoppy || "Download Report"}</button>
     // }
