4f1278c115cd3fd1f0afe2c02e3f71f7d711fc23
Emilia Friedberg <emiliafriedberg@gmail.com>
2018-01-10 17:45:24 -0500

    wip

---
+++ app/actions/users.ts
@@ -0,0 +1,21 @@
+declare function require(name:string);
+import  C from '../constants';
+import rootRef, { firebase } from '../libs/firebase';
+const users = rootRef.child('users');
+import request from 'request'
+
+export function firebaseAuth() {
+  return (dispatch) => {
+    request.post({
+      url: `${process.env.EMPIRICAL_BASE_URL}/api/v1/firebase_tokens/create_for_connect`,
+      json: { app: process.env.FIREBASE_APP_NAME}, },
+      (error, httpStatus, body) => {
+        if (body && body.token) {
+          debugger;
+          firebase.auth().signInWithCustomToken(body.token).catch(function(error) {
+            console.log(error.message)
+          })
+        }
+      });
+  };
+}
