fd5373411713a3a9f63e8494f028c1f7120cd84d
Emilia Friedberg <emiliafriedberg@gmail.com>
2018-01-12 13:28:02 -0500

    successfully disallow non-admin users from writing to questions

--- app/actions/users.ts
+++ app/actions/users.ts
@@ -3,18 +3,30 @@ import  C from '../constants';
 import rootRef, { firebase } from '../libs/firebase';
 const users = rootRef.child('users');
 import request from 'request'
+import jwt_decode from 'jwt-decode'
 
 export function firebaseAuth() {
   return (dispatch) => {
-    request.post({
-      url: `${process.env.EMPIRICAL_BASE_URL}/api/v1/firebase_tokens/create_for_connect`,
-      json: { app: process.env.FIREBASE_APP_NAME}, },
-      (error, httpStatus, body) => {
-        if (body && body.token) {
-          firebase.auth().signInWithCustomToken(body.token).catch(function(error) {
+      const data = new FormData();
+      data.append( "json", JSON.stringify( { app: process.env.FIREBASE_APP_NAME} ) );
+      fetch(`${process.env.EMPIRICAL_BASE_URL}/api/v1/firebase_tokens/create_for_connect`, {
+        method: "POST",
+        mode: "cors",
+        credentials: 'include',
+        body: data
+      }).then(response => {
+        if (!response.ok) {
+          throw Error(response.statusText);
+        }
+        return response.json();
+      }).then((response) => {
+          firebase.auth().signInWithCustomToken(response.token).then((firebaseResponse) => {
+            firebase.auth().currentUser.getToken().then((token) => {
+              console.log(jwt_decode(token));
+            });
+          }).catch((error) => {
             console.log(error.message)
           })
-        }
-      });
-  };
-}
+        })
+    }
+  }
