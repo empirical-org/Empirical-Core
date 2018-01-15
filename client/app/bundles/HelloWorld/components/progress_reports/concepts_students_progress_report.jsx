1678e645583f0470c2dcb450d02c0094b35e5599
RyanNovas <ryan@quill.org>
2018-01-15 17:58:05 -0500

    keys can be omitted from csv reports and all space cased

--- client/app/bundles/HelloWorld/components/progress_reports/concepts_students_progress_report.jsx
+++ client/app/bundles/HelloWorld/components/progress_reports/concepts_students_progress_report.jsx
@@ -136,6 +136,10 @@ export default class extends React.Component {
     }
   }
 
+  keysToOmit(){
+    return ['concepts_href']
+  }
+
   render() {
     if (this.state.loading || !this.state.reportData) {
       return <LoadingSpinner/>
@@ -148,7 +152,7 @@ export default class extends React.Component {
             <p>Each time a student correctly demonstrates a concept or creates an error, Quill generates a concept result. This report provides an aggregate picture of student progress on each concept.</p>
           </div>
           <div className='csv-and-how-we-grade'>
-            <CSVDownloadForProgressReport data={this.state.filteredReportData}/>
+            <CSVDownloadForProgressReport data={this.state.filteredReportData} keysToOmit={this.keysToOmit()}/>
             <a className='how-we-grade' href="https://support.quill.org/activities-implementation/how-does-grading-work">How We Grade<i className="fa fa-long-arrow-right"></i></a>
           </div>
           <div className='dropdown-container'>
