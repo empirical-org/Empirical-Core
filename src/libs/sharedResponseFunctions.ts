d88b76afbe28a6ec1d7359c1f302abfb18ea4bf7
Emilia Friedberg <emiliafriedberg@gmail.com>
2018-01-17 12:22:17 -0500

    typescriptify all functions in sharedResponseFunctions

--- src/libs/sharedResponseFunctions.ts
+++ src/libs/sharedResponseFunctions.ts
@@ -5,15 +5,15 @@ export function getOptimalResponses(responses: Array<Response>): Array<Response>
   return _.where(responses, { optimal: true, });
 }
 
-export function getSubOptimalResponses(responses) {
+export function getSubOptimalResponses(responses:Array<Response>) {
   return _.filter(responses, resp => resp.parentID === undefined && resp.feedback !== undefined && resp.optimal !== true);
 }
 
-export function getTopOptimalResponse(responses) {
-  return _.sortBy(getOptimalResponses(responses), r => r.count).reverse(responses)[0];
+export function getTopOptimalResponse(responses: Array<Response>) {
+  return _.sortBy(getOptimalResponses(responses), r => r.count).reverse()[0]
 }
 
-function getWeakResponses(responses) {
+function getWeakResponses(responses:Array<Response>) {
   return _.filter(responses, resp => resp.weak === true);
 }
 
@@ -21,15 +21,15 @@ function getCommonUnmatchedResponses(responses: Array<Response>): Array<Response
   return _.filter(responses, resp => resp.feedback === undefined && resp.count > 2);
 }
 
-function getSumOfWeakAndCommonUnmatchedResponses(responses) {
+function getSumOfWeakAndCommonUnmatchedResponses(responses:Array<Response>) {
   return getWeakResponses(responses).length + getCommonUnmatchedResponses(responses).length;
 }
 
-export function getPercentageWeakResponses(responses) {
+export function getPercentageWeakResponses(responses:Array<Response>) {
   return (getSumOfWeakAndCommonUnmatchedResponses(responses) / responses.length * 100).toPrecision(4);
 }
 
-export function getGradedResponses(responses) {
+export function getGradedResponses(responses:Array<Response>) {
     // Returns sorted collection optimal first followed by suboptimal
   const gradedResponses = _.reject(responses, response =>
       (response.optimal === undefined) || (response.parentID)
