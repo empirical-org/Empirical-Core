0221c7105e0e85851240346e1be588fd8267d563
Emilia Friedberg <emiliafriedberg@gmail.com>
2018-01-17 12:08:21 -0500

    typescriptify and test minLengthMatch

---
+++ src/libs/matchers/min_length_match.ts
@@ -0,0 +1,17 @@
+import * as _ from 'underscore'
+import {stringNormalize} from 'quill-string-normalizer'
+import {getOptimalResponses} from '../sharedResponseFunctions'
+import {Response} from '../../interfaces'
+
+export function minLengthMatch(responseString:string, responses:Array<Response>) {
+  const optimalResponses = getOptimalResponses(responses);
+  if (optimalResponses.length < 2) {
+    return undefined;
+  }
+  const lengthsOfResponses = optimalResponses.map(resp => stringNormalize(resp.text).split(' ').length);
+  const minLength = _.min(lengthsOfResponses) - 1;
+  if (responseString.split(' ').length < minLength) {
+    return _.sortBy(optimalResponses, resp => stringNormalize(resp.text).length)[0];
+  }
+  return undefined;
+}
