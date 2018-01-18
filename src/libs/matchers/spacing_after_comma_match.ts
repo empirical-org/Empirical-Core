eb5b2303b51a7ecef225c6796660d9b584ac73c3
Emilia Friedberg <emiliafriedberg@gmail.com>
2018-01-18 17:49:58 -0500

    spacing after comma match

--- src/libs/matchers/spacing_after_comma_match.ts
+++ src/libs/matchers/spacing_after_comma_match.ts
@@ -1,15 +1,34 @@
 import * as _ from 'underscore'
-import {Response, FeedbackObject} from '../../interfaces'
+import {Response, PartialResponse} from '../../interfaces'
+import {conceptResultTemplate} from '../helpers/concept_result_template'
+import {getTopOptimalResponse} from '../sharedResponseFunctions'
 
-export function spacingAfterCommaMatch(response):FeedbackObject|undefined {
+export function spacingAfterCommaMatch(response):Boolean {
   for (let i = 0; i < response.length; i++) {
     if (response[i] === ',' && (i + 1 < response.length)) {
       if (response[i + 1] !== ' ') {
-        return {
-          feedback: '<p>Revise your work. Always put a space after a <em>comma</em>.</p>',
-        };
-      }
+        return true
+      };
     }
   }
-  return undefined;
+  return false;
+}
+
+export function spacingAfterCommaChecker(responseString: string, responses:Array<Response>):PartialResponse|undefined {
+  const match = spacingAfterCommaMatch(responseString);
+  if (match) {
+    return spacingAfterCommaResponseBuilder(responses)
+  }
+}
+
+export function spacingAfterCommaResponseBuilder(responses:Array<Response>): PartialResponse {
+  const res = {
+    feedback: '<p>Revise your work. Always put a space after a <em>comma</em>.</p>',
+    author: 'Punctuation Hint',
+    parent_id: getTopOptimalResponse(responses) ? getTopOptimalResponse(responses).id : undefined,
+    concept_results: [
+      conceptResultTemplate('mdFUuuNR7N352bbMw4Mj9Q')
+    ]
+  }
+  return res
 }
