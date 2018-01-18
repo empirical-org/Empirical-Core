4ba321caa8b7cbb9bf2ef61f7bf21d766788f3ee
Emilia Friedberg <emiliafriedberg@gmail.com>
2018-01-18 16:35:51 -0500

    punctuation and case insensitivity tolerance

--- src/libs/matchers/punctuation_and_case_insensitive_match.spec.ts
+++ src/libs/matchers/punctuation_and_case_insensitive_match.spec.ts
@@ -1,23 +1,52 @@
 import { assert } from 'chai';
-import {punctuationAndCaseInsensitiveMatch} from './punctuation_and_case_insensitive_match'
-import {Response} from '../../interfaces'
+import {punctuationAndCaseInsensitiveMatch, punctuationAndCaseInsensitiveChecker} from './punctuation_and_case_insensitive_match'
+import {Response, PartialResponse} from '../../interfaces'
+import constants from '../../constants'
+import {conceptResultTemplate} from '../helpers/concept_result_template'
 
-describe('The punctuationInsensitiveMatch function', () => {
+const savedResponses: Array<Response> = [
+  {
+    id: 1,
+    text: "My dog took a nap.",
+    feedback: "Good job, that's a sentence!",
+    optimal: true,
+    count: 1,
+    question_uid: 'question 1'
+  }
+]
+
+describe('The punctuationAndCaseInsensitiveMatch function', () => {
 
     it('Should take a response string and find the corresponding saved response if the string matches exactly when punctuation is removed and both are downcased', () => {
         const responseString = "my dog took a nap";
-        const savedResponses: Array<Response> = [
-          {
-            id: 1,
-            text: "My dog took a nap.",
-            feedback: "Good job, that's a sentence!",
-            optimal: true,
-            count: 1,
-            question_uid: 'question 1'
-          }
-        ]
         const matchedResponse: Response = punctuationAndCaseInsensitiveMatch(responseString, savedResponses);
         assert.equal(matchedResponse.id, savedResponses[0].id);
     });
 
 });
+
+describe('The punctuationAndCaseInsensitiveChecker', () => {
+
+  it('Should return a partialResponse object if the lowercased response string matches a lowercased partial response', () => {
+    const responseString = "my dog took a nap.";
+    const partialResponse: PartialResponse =  {
+        feedback: constants.FEEDBACK_STRINGS.punctuationAndCaseError,
+        author: 'Punctuation and Case Hint',
+        parent_id: punctuationAndCaseInsensitiveMatch(responseString, savedResponses).key,
+        concept_results: [
+          conceptResultTemplate('66upe3S5uvqxuHoHOt4PcQ'),
+          conceptResultTemplate('mdFUuuNR7N352bbMw4Mj9Q')
+        ]
+      }
+    assert.equal(punctuationAndCaseInsensitiveChecker(responseString, savedResponses).feedback, partialResponse.feedback);
+    assert.equal(punctuationAndCaseInsensitiveChecker(responseString, savedResponses).author, partialResponse.author);
+    assert.equal(punctuationAndCaseInsensitiveChecker(responseString, savedResponses).parent_id, partialResponse.parent_id);
+    assert.equal(punctuationAndCaseInsensitiveChecker(responseString, savedResponses).concept_results.length, partialResponse.concept_results.length);
+  });
+
+  it('Should return undefined if the lowercased response string does not match a lowercased partial response', () => {
+    const responseString = "my cat took a nap.";
+    assert.equal(punctuationAndCaseInsensitiveChecker(responseString, savedResponses), undefined);
+  });
+
+})
--- src/libs/matchers/punctuation_and_case_insensitive_match.ts
+++ src/libs/matchers/punctuation_and_case_insensitive_match.ts
@@ -1,7 +1,9 @@
 import * as _ from 'underscore'
 import {stringNormalize} from 'quill-string-normalizer'
 import {getOptimalResponses} from '../sharedResponseFunctions'
-import {Response} from '../../interfaces'
+import {Response, PartialResponse} from '../../interfaces'
+import constants from '../../constants'
+import {conceptResultTemplate} from '../helpers/concept_result_template'
 
 export function punctuationAndCaseInsensitiveMatch(responseString:string, responses:Array<Response>):Response|undefined {
   return _.find(getOptimalResponses(responses), (resp) => {
@@ -13,4 +15,25 @@ export function punctuationAndCaseInsensitiveMatch(responseString:string, respon
 
 export function removePunctuation(string) {
   return string.replace(/[^A-Za-z0-9\s]/g, '');
+}
+
+export function punctuationAndCaseInsensitiveChecker(responseString: string, responses:Array<Response>):PartialResponse|undefined {
+  const match = punctuationAndCaseInsensitiveMatch(responseString, responses);
+  if (match) {
+    const parentID = match.key
+    return punctuationAndCaseInsensitiveResponseBuilder(responses, parentID)
+  }
+}
+
+export function punctuationAndCaseInsensitiveResponseBuilder(responses:Array<Response>, parentID:string|number): PartialResponse {
+  const res = {
+    feedback: constants.FEEDBACK_STRINGS.punctuationAndCaseError,
+    author: 'Punctuation and Case Hint',
+    parent_id: parentID,
+    concept_results: [
+      conceptResultTemplate('66upe3S5uvqxuHoHOt4PcQ'),
+      conceptResultTemplate('mdFUuuNR7N352bbMw4Mj9Q')
+    ]
+  }
+  return res
 }
