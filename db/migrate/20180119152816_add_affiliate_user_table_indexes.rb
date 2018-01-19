76b41f875fe63df4c435b460568009248356ef82
Jared Silver <jared@jaredsilver.name>
2018-01-19 12:12:37 -0500

    Add referrals users table, rename affiliate user table to referrer users

--- db/migrate/20180119152816_add_affiliate_user_table_indexes.rb
+++
@@ -1,6 +0,0 @@
-class AddAffiliateUserTableIndexes < ActiveRecord::Migration
-  def change
-    add_index :affiliate_user, :user_id, unique: true
-    add_index :affiliate_user, :affiliate_code, unique: true
-  end
-end
