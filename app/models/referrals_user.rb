2480261ae30171a387da0603de1fa2ed6056ab62
Jared Silver <jared@jaredsilver.name>
2018-01-23 15:22:20 -0500

    Move query to class method

--- app/models/referrals_user.rb
+++ app/models/referrals_user.rb
@@ -34,6 +34,16 @@ class ReferralsUser < ActiveRecord::Base
     end
   end
 
+  def self.ids_due_for_activation
+    ActiveRecord::Base.connection.execute("
+      SELECT DISTINCT referrals_users.id FROM referrals_users
+      JOIN classrooms_teachers ON referrals_users.referred_user_id = classrooms_teachers.user_id
+      JOIN classroom_activities ON classrooms_teachers.classroom_id = classroom_activities.classroom_id
+      JOIN activity_sessions ON activity_sessions.classroom_activity_id = classroom_activities.id
+      WHERE referrals_users.activated = FALSE
+    ").to_a.map(&:values).flatten
+  end
+
   private
   def trigger_invited_event
     # Unlike other analytics events, we want to track this event with respect
