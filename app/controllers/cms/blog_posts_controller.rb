111154a8810c6bf086ddc521152d5592c3af970c
RyanNovas <ryan@quill.org>
2018-01-23 16:19:35 -0500

    new and edit blog post wip

--- app/controllers/cms/blog_posts_controller.rb
+++ app/controllers/cms/blog_posts_controller.rb
@@ -5,17 +5,14 @@ class Cms::BlogPostsController < ApplicationController
     @blog_posts_name_and_id = BlogPost.all.select('title', 'id')
   end
 
+  def new
+  end
+
+  def edit
+  end
+
   def create
-    # attributes = unit_template_params
-    #
-    # attributes.delete(:authenticity_token)
-    #
-    # @unit_template = UnitTemplate.new(unit_template_params)
-    # if @unit_template.save!
-    #   render json: @unit_template
-    # else
-    #   render json: {errors: @unit_template.errors}, status: 422
-    # end
+
   end
 
   def update
