99764cfe5edd84ad76f3f9e1711d3443f71ba9e6
Jared Silver <jared@jaredsilver.name>
2018-01-23 15:43:37 -0500

    Test ids_due_for_activation method used in rake task

--- spec/models/referrals_user_spec.rb
+++ spec/models/referrals_user_spec.rb
@@ -87,4 +87,24 @@ RSpec.describe ReferralsUser, type: :model do
       expect(ActionMailer::Base.deliveries.last.to).to eq([referrer.email])
     end
   end
+
+  describe 'self.ids_due_for_activation' do
+    it 'should return ids where the referred teacher is not activated and has at least one activity session' do
+      activated_referrals_user = create(:referrals_user)
+      activated_referral = activated_referrals_user.referral
+      activated_referrals_user.update(activated: true)
+
+      referrals_user = create(:referrals_user)
+      referral = referrals_user.referral
+
+      expect(ReferralsUser.ids_due_for_activation).to eq([])
+
+      [activated_referral, referral].each do |teacher|
+        classroom_id = create(:classrooms_teacher, user_id: teacher.id).classroom_id
+        create(:classroom_activity_with_activity_sessions, classroom_id: classroom_id)
+      end
+
+      expect(ReferralsUser.ids_due_for_activation).to eq([referrals_user.id.to_s])
+    end
+  end
 end
