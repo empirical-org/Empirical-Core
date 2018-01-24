f90d8411cd7e3ef16c376bba6ebb87a7ef44e4d6
RyanNovas <ryan@quill.org>
2018-01-24 10:24:36 -0500

    edit blog post wip

--- client/app/bundles/HelloWorld/components/cms/blog_posts/create_or_edit_blog_post.jsx
+++ client/app/bundles/HelloWorld/components/cms/blog_posts/create_or_edit_blog_post.jsx
@@ -7,14 +7,23 @@ import _ from 'underscore'
 export default class extends React.Component {
   constructor(props) {
     super(props);
+    const p = this.props.postToEdit
+    // set state to empty values or those of the postToEdit
     this.state = {
-      title: null,
-      subtitle: null,
-      body: null,
-      author_id: null,
-      topic: null,
-      selectedAuthorId: null,
+      title: p
+        ? p.title
+        : '',
+      subtitle: p
+        ? p.subtitle
+        : '',
+      body: p
+        ? p.body
+        : '',
+      author_id: p ? p.author_id : null,
+      topic: p ? p.topic : '',
+      selectedAuthorId: p ? p.author_id : null,
     };
+
     this.handleTitleChange = this.handleTitleChange.bind(this)
     this.handleSubtitleChange = this.handleSubtitleChange.bind(this)
     this.handleBodyChange = this.handleBodyChange.bind(this)
@@ -35,24 +44,28 @@ export default class extends React.Component {
 
   handleSubmit(e) {
     e.preventDefault();
-    const action = this.props.action === 'new'
-      ? 'post'
-      : 'puts'
+    let action,
+      url
+    if (this.props.action === 'new') {
+      action = 'post'
+      url = '/cms/blog_posts'
+    } else {
+      action = 'puts'
+      url = `/cms/blog_posts${this.props.postToEdit.id}`
+    }
     request[action]({
       url: `${process.env.DEFAULT_URL}/cms/blog_posts`,
-    form: {
-      blog_post: _.omit(this.state, 'selectedAuthorId'),
-      authenticity_token: ReactOnRails.authenticityToken()
-    },
-    function(err, httpResponse, body) {
-      debugger;
-      if (httpResponse.status === 200) {
-        alert('success!')
-      } else {
-        alert('failure')
+      form: {
+        blog_post: _.omit(this.state, 'selectedAuthorId'),
+        authenticity_token: ReactOnRails.authenticityToken()
       }
-    }
-  })
+    }, (error, httpStatus, body) => {
+      if (httpStatus.statusCode === 200) {
+        alert('saved')
+      } else {
+        alert('there was a problem saving')
+      }
+    })
   }
 
   render() {
