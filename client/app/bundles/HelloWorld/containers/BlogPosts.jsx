cc6325ea8b0004b935bc577fb830ed59b932c3bc
RyanNovas <ryan@quill.org>
2018-01-24 11:40:31 -0500

    additional blog post fields in admin view

--- client/app/bundles/HelloWorld/containers/BlogPosts.jsx
+++ client/app/bundles/HelloWorld/containers/BlogPosts.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import ReactTable from 'react-table'
 import CreateOrEditBlogPost from '../components/cms/blog_posts/create_or_edit_blog_post.jsx'
+import request from 'request'
+import moment from 'moment'
 
 export default React.createClass({
 
@@ -10,9 +12,24 @@ export default React.createClass({
         Header: 'Title',
         accessor: 'title'
       }, {
-        Header: 'Edit',
+        Header: 'Created',
+        accessor: 'created_at',
+        Cell: props => <span>{moment(props.value).format('MM-DD-YY')}</span>
+      }, {
+        Header: 'Updated',
+        accessor: 'updated_at',
+        Cell: props => <span>{moment(props.value).format('MM-DD-YY')}</span>
+      }, {
+        Header: 'Topic',
+        accessor: 'topic'
+      }, {
+        Header: '',
         accessor: 'id',
-        Cell: props => <a href={`/cms/blog_posts/${props.value}/edit`}>Edit</a>
+        Cell: props => <a className= 'button' href={`/cms/blog_posts/${props.value}/edit`}>Edit</a>
+      }, {
+        Header: '',
+        accessor: 'id',
+        Cell: props => <a className='button' href={`/cms/blog_posts/${props.value}/delete`}>Delete</a>
       }
     ])
   },
