0f00737e05fb1254cf028fc946b2bcb5eac1188f
Emilia Friedberg <emiliafriedberg@gmail.com>
2018-01-30 17:59:01 -0500

    wip

--- config/rollup.js
+++ config/rollup.js
@@ -5,6 +5,7 @@ var uglify = require('uglify-js');
 var commonjs = require('rollup-plugin-commonjs');
 var nodeResolve = require('rollup-plugin-node-resolve');
 var typescript = require('rollup-plugin-typescript2');
+var istanbul = require('rollup-plugin-istanbul');
 var version = process.env.VERSION || require('../package.json').version;
 var banner =
     '/*!\n' +
@@ -18,11 +19,11 @@ rollup.rollup({
     plugins: [
       commonjs({
         namedExports: {
-          'node_modules/underscore/underscore.js': ['sortBy', 'reject', 'isEqual', 'where', 'find', 'filter', 'any', 'map', 'intersection', 'contains', 'zip', 'min', 'max']
-        },
-        ignore: ['diff']
+          'node_modules/underscore/underscore.js': ['sortBy', 'reject', 'isEqual', 'where', 'find', 'filter', 'any', 'map', 'intersection', 'contains', 'zip', 'min', 'max'],
+          'node_modules/diff/lib/util/params.js': ['_params']        }
       }),
       typescript(),
+      istanbul(),
       nodeResolve()
     ]
 })
