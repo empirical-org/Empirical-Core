809f7927b6b8d3c01c43b1b5021bd9fb467dabb5
Jared Silver <jared@jaredsilver.name>
2018-01-31 17:16:44 -0500

    Continue redesigning navigation, begin merging subnavigation, minor style updates

--- app/helpers/navigation_helper.rb
+++ app/helpers/navigation_helper.rb
@@ -1,29 +1,35 @@
 module NavigationHelper
-  def home_page?
-
+  def home_page_should_be_active?
+    (current_page?(controller: 'teachers/classroom_manager', action: 'dashboard') ||
+    ['invite_students', 'dashboard', 'my_account', 'teacher_guide', 'google_sync'].include?(action_name)) &&
+    action_name != 'invite_students'
   end
 
-  def classes_page?
-
+  def classes_page_should_be_active?
+    controller.class == Teachers::ClassroomsController || controller_name == 'students' || action_name == 'invite_students'
   end
 
-  def assign_activity_page?
-
+  def assign_activity_page_should_be_active?
+    current_page?(controller: 'teachers/classroom_manager', action: 'assign_activities')
   end
 
-  def my_activities_page?
-
+  def my_activities_page_should_be_active?
+    current_page?(controller: 'teachers/classroom_manager', action: 'lesson_planner')
   end
 
-  def student_reports_page?
-
+  def student_reports_page_should_be_active?
+    controller.class == Teachers::ProgressReportsController ||
+    controller.class.parent == Teachers::ProgressReports ||
+    controller.class.parent == Teachers::ProgressReports::Standards ||
+    controller.class.parent == Teachers::ProgressReports::Concepts ||
+    action_name == 'scorebook'
   end
 
-  def teacher_resources_page?
-
+  def teacher_resources_page_should_be_active?
+    controller.class == BlogPostsController
   end
 
-  def premium_page?
+  def premium_page_should_be_active?
 
   end
 
