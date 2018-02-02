f4a674a99eccdd6fc58e9d390b2df94bddfcdb39
Emilia Friedberg <emiliafriedberg@gmail.com>
2018-02-02 16:51:54 -0500

    get diagnostic questions in

--- app/components/renderForQuestions/checkAnswer.ts
+++ app/components/renderForQuestions/checkAnswer.ts
@@ -1,17 +1,18 @@
 import { hashToCollection } from '../../libs/hashToCollection';
-import _ from 'underscore'
+import * as _ from 'underscore'
 import Question from '../../libs/question';
 import DiagnosticQuestion from '../../libs/diagnosticQuestion';
-import {checkSentenceCombining} from 'quill-marking-logic'
+import {checkSentenceCombining, checkDiagnosticQuestion} from 'quill-marking-logic'
 
-export default function checkAnswer(question, response, responses) {
+export default function checkAnswer(question, response, responses, mode='default') {
   const fields = {
     responses: responses ? hashToCollection(responses) : [],
     questionUID: question.key,
     focusPoints: question.focusPoints ? hashToCollection(question.focusPoints): [],
     incorrectSequences: question.incorrectSequences ? hashToCollection(_.compact(question.incorrectSequences)) : [],
   };
-  const newResponse = checkSentenceCombining(fields.questionUID, response, fields.responses, fields.focusPoints, fields.incorrectSequences);
-  console.log('newResponse', newResponse)
+  const newResponse = mode === 'default'
+    ? checkSentenceCombining(fields.questionUID, response, fields.responses, fields.focusPoints, fields.incorrectSequences)
+    : checkDiagnosticQuestion(fields.questionUID, response, fields.responses)
   return {response: newResponse};
 }
