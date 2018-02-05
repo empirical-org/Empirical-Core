c6287e5b175c9e0f33afcdcd8f68e32c98c753e1
RyanNovas <ryan@quill.org>
2018-02-05 15:15:22 -0500

    shows all info from historic subscriptions

--- client/app/bundles/HelloWorld/containers/Subscriptions.jsx
+++ client/app/bundles/HelloWorld/containers/Subscriptions.jsx
@@ -1,35 +1,53 @@
 import React from 'react';
-import ReactTable from 'react-table';
+import moment from 'moment';
+import pluralize from 'pluralize';
 
 export default class extends React.Component {
 
-  render() {
-    const columnDefinitions = [
-      {
-        Header: 'Purchase Date',
-        width: 78,
-        accessor: 'created_at',
-        Cell: props => props.value,
-      },
-      {
-        Header: 'Subscription',
-        accessor: 'account_type',
-        Cell: props => props.value,
-      },
-      {
-        Header: 'Start / End Date',
-        accessor: 'start_date',
-        Cell: row => (
-          <span>
-            {`${row.original.start_date}-${row.original.expiration}`}
-          </span>),
+  subscriptionHistoryRows() {
+    const rows = [];
+    this.props.subscriptions.forEach((sub) => {
+      const startD = moment(sub.start_date);
+      const endD = moment(sub.expiration);
+      const duration = endD.diff(startD, 'months') + 1;
+      rows.push(<tr key={sub.id}>
+        <td>{moment(sub.created_at).format('MMMM Do, YYYY')}</td>
+        <td>{sub.account_type}</td>
+        <td>{sub.account_type}</td>
+        <td>{`${duration} ${pluralize('month', duration)}`}</td>
+        <td>{`${startD.format('MM/DD/YY')}-${endD.format('MM/DD/YY')}`}</td>
+      </tr>);
+      if (sub.credited) {
+        rows.push(<tr>
+          <td colSpan="5">credited</td>
+        </tr>);
       }
-    ];
+    });
+    return rows;
+  }
+
+  subscriptionHistory() {
+    return (
+      <table>
+        <tbody>
+          <tr>
+            <th>Purchase Date</th>
+            <th>Subscription</th>
+            <th>Payment</th>
+            <th>Length</th>
+            <th>Start / End Date</th>
+          </tr>
+          {this.subscriptionHistoryRows()}
+        </tbody>
+      </table>
+    );
+  }
+
+  render() {
     return (
       <div>
-        <ReactTable
-          data={this.props.subscriptions} columns={columnDefinitions} showPagination={false} showPaginationTop={false} showPaginationBottom={false} showPageSizeOptions={false} className="progress-report has-green-arrow"
-        />
-      </div>);
+        {this.subscriptionHistory()}
+      </div>
+    );
   }
 }
