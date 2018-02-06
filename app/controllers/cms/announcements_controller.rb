6ebc365a67636d20961ba04fc2d894faf91a2660
Jared Silver <jared@jaredsilver.name>
2018-02-06 17:42:20 -0500

    Adds CMS announcements controller

---
+++ app/controllers/cms/announcements_controller.rb
@@ -0,0 +1,31 @@
+class Cms::AnnouncementsController < ApplicationController
+  before_filter :signed_in!
+  before_filter :staff!
+
+  def index
+    @announcements = Announcement.all
+  end
+
+  def create
+    Announcement.create(announcement_params)
+    redirect_to cms_announcements_path
+  end
+
+  def new
+    @announcement = Announcement.new
+  end
+
+  def edit
+    @announcement = Announcement.find(params[:id])
+  end
+
+  def update
+    Announcement.find(params[:id]).update(announcement_params)
+    redirect_to cms_announcements_path
+  end
+
+  private
+  def announcement_params
+    params.require(:announcement).permit(:announcement_type, :html, :start, :end)
+  end
+end
