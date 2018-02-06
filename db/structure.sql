6342baffe2accfdf92de47fcf8c2177eac7136f2
RyanNovas <ryan@quill.org>
2018-02-06 13:42:59 -0500

    changes start_date and de_activated_date from datetime to date

--- db/structure.sql
+++ db/structure.sql
@@ -1548,11 +1548,11 @@ CREATE TABLE subscriptions (
     updated_at timestamp without time zone,
     account_type character varying,
     contact_email character varying,
-    start_date timestamp without time zone DEFAULT '2018-01-30 00:00:00'::timestamp without time zone,
+    start_date date DEFAULT '2018-01-30 00:00:00'::timestamp without time zone,
     subscription_type_id integer,
     contact_user_id integer,
     recurring boolean DEFAULT false,
-    de_activated_date timestamp without time zone
+    de_activated_date date
 );
 
 
@@ -3997,4 +3997,6 @@ INSERT INTO schema_migrations (version) VALUES ('20180201221940');
 INSERT INTO schema_migrations (version) VALUES ('20180205170220');
 
 INSERT INTO schema_migrations (version) VALUES ('20180206154253');
+
+INSERT INTO schema_migrations (version) VALUES ('20180206171118');
 
