579be7fbed645875b37f41d353267a46f7426a5c
Jared Silver <jared@jaredsilver.name>
2018-02-07 14:19:28 -0500

    Change mind about redirect functionality, do not let people edit slugs

--- app/models/blog_post.rb
+++ app/models/blog_post.rb
@@ -10,13 +10,9 @@ class BlogPost < ActiveRecord::Base
     title = self.title
     slug = title.gsub(/[^a-zA-Z\d\s]/, '').gsub(' ', '-').downcase
 
-    # TODO: revisit the following logic once the redirects table is
-    # implemented to account for the fact that a blog post's slug
-    # could have been changed but also exist in the redirects table.
-
     # This looks for slugs that look like #{current-slug}-2 so we
     # can change our slug for this post to increment the end digit.
-    existing_posts_with_incremented_slug = ActiveRecord::Base.connection.execute("SELECT slug FROM blog_posts WHERE slug ~* '#{slug}-\\d$';").to_a.map{ |h| h['slug'] }  
+    existing_posts_with_incremented_slug = ActiveRecord::Base.connection.execute("SELECT slug FROM blog_posts WHERE slug ~* '#{slug}-\\d$';").to_a.map{ |h| h['slug'] }
     if existing_posts_with_incremented_slug.any?
       incremented_values = existing_posts_with_incremented_slug.map do |incremented_slug|
         incremented_slug.gsub("#{slug}-", '').to_i
