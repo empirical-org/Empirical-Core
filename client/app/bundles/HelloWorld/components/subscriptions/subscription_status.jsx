36bd29814861146eaca19833eae6775fcb2e9ce3
RyanNovas <ryan@quill.org>
2018-02-07 12:01:20 -0500

    subscription status element with skeleton

--- client/app/bundles/HelloWorld/components/subscriptions/subscription_status.jsx
+++ client/app/bundles/HelloWorld/components/subscriptions/subscription_status.jsx
@@ -3,12 +3,33 @@ import moment from 'moment';
 
 export default class extends React.Component {
 
+  subscriptionType() {
+    const accountType = this.props.subscriptionStatus.account_type;
+    if (this.props.schoolSubscriptionTypes.includes(accountType)) {
+      return 'School';
+    } else if (this.props.trialSubscriptionTypes.includes(accountType)) {
+      return 'Trial';
+    }
+    return 'Teacher';
+  }
+
+  status() {
+    if (!this.props.subscriptionStatus) {
+      return "You don't have a subscription";
+    }
+    const subScriptionType = this.subscriptionType();
+    if (this.props.subscriptionStatus.expired) {
+      return `Your ${subScriptionType} Premium subscription has expired`;
+    }
+    return `You have a ${subScriptionType} Premium subscription`;
+  }
+
   render() {
     return (
       <section>
         <div className="flex-row space-between">
-          <h2>You have a School Premium subscription</h2>
-          <span>Valid Until: Novemeber 31st, 2019 (EST)</span>
+          <h2>{this.status()}</h2>
+          <span>Valid Until: {moment(this.props.subscriptionStatus.expiration).format('MMMM Do, YYYY')} (EST)</span>
         </div>
         <p>
           With Quill School Premium, you will have access to all of Quill’s
