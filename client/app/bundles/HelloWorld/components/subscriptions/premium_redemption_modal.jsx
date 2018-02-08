d2ced1cae2375ff90b3a9ad938ce1cc71b3a392a
RyanNovas <ryan@quill.org>
2018-02-08 15:50:05 -0500

    skeleton of premium modal

--- client/app/bundles/HelloWorld/components/subscriptions/premium_redemption_modal.jsx
+++ client/app/bundles/HelloWorld/components/subscriptions/premium_redemption_modal.jsx
@@ -7,9 +7,6 @@ export default React.createClass({
       <Modal {...this.props} show={this.props.show} onHide={this.props.hideModal} dialogClassName="" restoreFocus>
         <Modal.Body>
           <img className="pull-right react-bootstrap-close" onClick={this.props.hideModal} src={`${process.env.CDN_URL}/images/shared/close_x.svg`} alt="close-modal" />
-          <h1 className="q-h2">Which credit card would you like to use to buy Teacher Premium?</h1>
-          <button>Credit Card ending with {this.props.lastFour}</button>
-          <button>Use a Different Card</button>
         </Modal.Body>
       </Modal>
     );
