1012e758d076ca3604ea953bab27470bfe07368f
Emilia Friedberg <emiliafriedberg@gmail.com>
2018-02-08 15:01:24 -0500

    wip

--- karma.conf.js
+++ karma.conf.js
@@ -18,6 +18,7 @@ module.exports = function (config) {
 
         // list of files / patterns to load in the browser
         files: [
+            './node_modules/babel-polyfill/dist/polyfill.js',
             './node_modules/phantomjs-polyfill-object-assign/object-assign-polyfill.js',
             './test/index.js',
             // './test/module.spec.ts',
--- src/libs/matchers/machine_learning_sentence_match.ts
+++ src/libs/matchers/machine_learning_sentence_match.ts
@@ -1,31 +1,37 @@
-import * as _ from 'underscore'
-import requestPromise from 'request-promise'
 import constants from '../../constants';
-import {stringNormalize} from 'quill-string-normalizer'
 import {getTopOptimalResponse} from '../sharedResponseFunctions'
 import {Response, PartialResponse, ConceptResult, WordCountChange} from '../../interfaces'
 import {feedbackStrings} from '../constants/feedback_strings'
 import {conceptResultTemplate} from '../helpers/concept_result_template'
 
-export function machineLearningSentenceMatch(response: string, link: string):Boolean {
+export function machineLearningSentenceMatch(response: string, link: string):Promise<boolean|void> {
   const options = {
     method: 'POST',
-    uri: `${link}/fragments/is_sentence`,
-    form: {
-      text: response
-    },
+    body: JSON.stringify({text: response}),
+    // mode: 'cors',
+    // credentials: 'include',
+    headers: { "Content-Type": "application/json" }
   };
-  return requestPromise(options).then((parsedBody) => {
-    return JSON.parse(parsedBody).text > 0.5
-  })
+  const url = `${link}/fragments/is_sentence`
+  const matched = fetch(url, options)
+      .then(response => response.json())
+      .catch(err => err)
+      .then(parsedResponse => parsedResponse.text > 0.5)
+      .catch(err => err)
+  console.log('matched')
+  return matched
 }
 
-export function machineLearningSentenceChecker(responseString: string, responses:Array<Response>, link:string):PartialResponse|undefined {
-  const match = machineLearningSentenceMatch(responseString, link);
-  return machineLearningSentenceResponseBuilder(responses, match)
+export async function machineLearningSentenceChecker(responseString: string, responses:Array<Response>, link:string):Promise<PartialResponse|undefined> {
+  console.log('what is up')
+  const match:Boolean|void = await machineLearningSentenceMatch(responseString, link);
+  console.log('match', match)
+  if (match) {
+    return machineLearningSentenceResponseBuilder(responses, match)
+  }
 }
 
-export function machineLearningSentenceResponseBuilder(responses, matched:Boolean): PartialResponse {
+export function machineLearningSentenceResponseBuilder(responses: Array<Response>, matched:Boolean): PartialResponse {
   const res:PartialResponse = {
     author: 'Parts of Speech',
     parent_id: getTopOptimalResponse(responses).key,
