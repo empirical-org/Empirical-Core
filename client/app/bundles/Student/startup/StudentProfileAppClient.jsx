e3a154a751a8d462efad31c6368b4bd130a726d3
tronroy <tbconroy@gmail.com>
2018-02-12 17:35:46 -0500

    Use redux to manage student profile state

--- client/app/bundles/Student/startup/StudentProfileAppClient.jsx
+++ client/app/bundles/Student/startup/StudentProfileAppClient.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import StudentProfile from '../../HelloWorld/containers/StudentProfile.jsx';
+import { Provider } from 'react-redux'
+import studentProfile from '../../../reducers/student_profile.js'
+import { createStore } from 'redux';
 
-export default (props) => (
-  <StudentProfile {...props} />
+export default () => (
+  <Provider store={createStore(studentProfile)}>
+    <StudentProfile />
+  </Provider>
 );
