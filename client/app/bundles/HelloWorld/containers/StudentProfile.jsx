4cb7c12fd05297effc0fe292b2854bffbf7bc4d0
tronroy <tbconroy@gmail.com>
2018-02-12 17:35:46 -0500

    Use redux to manage student profile state

--- client/app/bundles/HelloWorld/containers/StudentProfile.jsx
+++ client/app/bundles/HelloWorld/containers/StudentProfile.jsx
@@ -5,40 +5,22 @@ import StudentClassroomNavbar from '../components/student_profile/student_classr
 import NextActivity from '../components/student_profile/next_activity.jsx';
 import StudentProfileUnits from '../components/student_profile/student_profile_units.jsx';
 import StudentProfileHeader from '../components/student_profile/student_profile_header';
-import Setter from '../components/modules/setter.jsx';
 import Pusher from 'pusher-js';
+import { connect } from 'react-redux'
+import { fetchStudentProfile } from '../../../actions/student_profile'
 
-
-export default React.createClass({
-  getInitialState() {
-    this.modules = {
-      setter: new Setter(),
-      // scrollify: new EC.modules.scrollify()
-    };
-    return {
-      loading: true,
-    };
-  },
-
+const StudentProfile = React.createClass({
   componentDidMount() {
     this.fetchData();
   },
 
-  fetchData(currentClassroom) {
-    this.setState({ currentClassroom, loading: true, });
-    $.ajax({ url: '/student_profile_data', data: { current_classroom_id: currentClassroom, }, format: 'json', success: this.loadProfile, });
-  },
-
-  loadProfile(data) {
-    this.setState({ nextActivitySession: data.next_activity_session,
-      loading: false,
-      scores: data.scores,
-      student: data.student, },
-      () => this.initializePusher());
+  componentDidUpdate() {
+    this.initializePusher();
   },
 
   initializePusher() {
-    const classroomId = this.state.student.classroom.id;
+    let classroomId = this.props.student.classroom.id;
+
     if (process.env.NODE_ENV === 'development') {
       Pusher.logToConsole = true;
     }
@@ -50,16 +32,25 @@ export default React.createClass({
     });
   },
 
+  fetchData(classroomId) {
+    fetchStudentProfile(this.props.dispatch, classroomId);
+  },
+
   render() {
-    if (!this.state.loading) {
+    if (!this.props.loading) {
       return (
         <div id="student-profile">
-          <StudentClassroomNavbar data={this.state.student} fetchData={this.fetchData} loading={this.state.loading} />
-          <StudentProfileHeader studentName={this.state.student.name} classroomName={this.state.student.classroom.name} teacherName={this.state.student.classroom.teacher.name} />
-          <NextActivity data={this.state.nextActivitySession} loading={this.state.loading} hasActivities={this.state.scores.length > 0} />
-          <StudentProfileUnits data={this.state.scores} loading={this.state.loading} />
+          <StudentClassroomNavbar data={this.props.student} fetchData={this.fetchData} loading={this.props.loading} />
+          <StudentProfileHeader studentName={this.props.student.name} classroomName={this.props.student.classroom.name} teacherName={this.props.student.classroom.teacher.name} />
+          <NextActivity data={this.props.nextActivitySession} loading={this.props.loading} hasActivities={this.props.scores.length > 0} />
+          <StudentProfileUnits data={this.props.scores} loading={this.props.loading} />
         </div>
       );
     } return <span />;
   },
 });
+
+const mapStateToProps = (state) => { return state };
+
+
+export default connect(mapStateToProps)(StudentProfile);
