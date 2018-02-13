656523e87090e2422079320d3f0f260e6b4d385f
RyanNovas <ryan@quill.org>
2018-02-13 16:21:36 -0500

    styling wip

--- client/app/bundles/HelloWorld/components/subscriptions/select_credit_card_modal.jsx
+++ client/app/bundles/HelloWorld/components/subscriptions/select_credit_card_modal.jsx
@@ -4,10 +4,14 @@ import Modal from 'react-bootstrap/lib/Modal';
 export default React.createClass({
   render() {
     return (
-      <Modal {...this.props} show={this.props.show} onHide={this.props.hideModal} dialogClassName="" restoreFocus>
+      <Modal {...this.props} show={this.props.show} onHide={this.props.hideModal} dialogClassName="select-credit-card-modal" restoreFocus>
         <Modal.Body>
           <img className="pull-right react-bootstrap-close" onClick={this.props.hideModal} src={`${process.env.CDN_URL}/images/shared/close_x.svg`} alt="close-modal" />
-          <h1 className="q-h2">Which credit card would you like to use to buy Teacher Premium?</h1>
+          <div className="pricing-info text-center">
+            <h1>Quill Teacher Premium</h1>
+            <span>$80 for one-year subscription</span>
+          </div>
+          <h2 className="q-h2">Which credit card would you like to pay with?</h2>
           <button>Credit Card ending with {this.props.lastFour}</button>
           <button>Use a Different Card</button>
         </Modal.Body>
