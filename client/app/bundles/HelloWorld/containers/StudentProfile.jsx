32127598493e3507d3c290390d5c88933c5f053b
tronroy <tbconroy@gmail.com>
2018-02-13 17:36:40 -0500

    Consolidate studen profile reducers and actions

--- client/app/bundles/HelloWorld/containers/StudentProfile.jsx
+++ client/app/bundles/HelloWorld/containers/StudentProfile.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
-import $ from 'jquery';
-import _ from 'underscore';
-import StudentsClassroomsHeader from '../components/student_profile/students_classrooms/students_classrooms_header.jsx'
+import StudentsClassroomsHeader from '../components/student_profile/students_classrooms/students_classrooms_header.jsx';
 import NextActivity from '../components/student_profile/next_activity.jsx';
 import StudentProfileUnits from '../components/student_profile/student_profile_units.jsx';
 import StudentProfileHeader from '../components/student_profile/student_profile_header';
 import Pusher from 'pusher-js';
-import { connect } from 'react-redux'
-import { fetchStudentProfile } from '../../../actions/student_profile'
+import { connect } from 'react-redux';
+import { fetchStudentProfile } from '../../../actions/student_profile';
 
 const StudentProfile = React.createClass({
   componentDidMount() {
@@ -19,7 +17,7 @@ const StudentProfile = React.createClass({
   },
 
   initializePusher() {
-    let classroomId = this.props.studentProfile.student.classroom.id;
+    const classroomId = this.props.student.classroom.id;
 
     if (process.env.NODE_ENV === 'development') {
       Pusher.logToConsole = true;
@@ -33,13 +31,13 @@ const StudentProfile = React.createClass({
   },
 
   render() {
-    if (!this.props.studentProfile.loading) {
+    if (!this.props.loading) {
       return (
         <div id="student-profile">
-          <StudentsClassroomsHeader currentClassroomId={this.props.studentProfile.student.classroom.id} fetchData={this.props.fetchStudentProfile} loading={this.props.studentProfile.loading}/>
-          <StudentProfileHeader studentName={this.props.studentProfile.student.name} classroomName={this.props.studentProfile.student.classroom.name} teacherName={this.props.studentProfile.student.classroom.teacher.name} />
-          <NextActivity data={this.props.studentProfile.nextActivitySession} loading={this.props.studentProfile.loading} hasActivities={this.props.studentProfile.scores.length > 0} />
-          <StudentProfileUnits data={this.props.studentProfile.scores} loading={this.props.studentProfile.loading} />
+          <StudentsClassroomsHeader />
+          <StudentProfileHeader studentName={this.props.student.name} classroomName={this.props.student.classroom.name} teacherName={this.props.student.classroom.teacher.name} />
+          <NextActivity data={this.props.nextActivitySession} loading={this.props.loading} hasActivities={this.props.scores.length > 0} />
+          <StudentProfileUnits data={this.props.scores} loading={this.props.loading} />
         </div>
       );
     } return <span />;
@@ -47,10 +45,8 @@ const StudentProfile = React.createClass({
 });
 
 const mapStateToProps = (state) => { return state };
-const mapDispatchToProps = (dispatch) => {
-  return {
-    fetchStudentProfile: (classroomId) => dispatch(fetchStudentProfile(classroomId))
-  };
-};
+const mapDispatchToProps = dispatch => ({
+  fetchStudentProfile: classroomId => dispatch(fetchStudentProfile(classroomId)),
+});
 
 export default connect(mapStateToProps, mapDispatchToProps)(StudentProfile);
--- client/app/bundles/Student/startup/StudentProfileAppClient.jsx
+++ client/app/bundles/Student/startup/StudentProfileAppClient.jsx
@@ -2,12 +2,10 @@ import React from 'react'
 import StudentProfile from '../../HelloWorld/containers/StudentProfile.jsx'
 import { Provider } from 'react-redux'
 import studentProfile from '../../../reducers/student_profile'
-import studentsClassroomsHeader from '../../../reducers/students_classrooms_header'
 import { createStore, combineReducers, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 
-const rootReducer = combineReducers({ studentProfile, studentsClassroomsHeader });
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const store = createStore(studentProfile, applyMiddleware(thunk));
 
 export default () => (
   <Provider store={store}>
