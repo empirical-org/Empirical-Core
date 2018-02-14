ada2db701dbeeac62a7d699e447e475902b5595d
tronroy <tbconroy@gmail.com>
2018-02-14 15:51:46 -0500

    Refactor continued

--- client/app/bundles/HelloWorld/components/student_profile/students_classrooms/students_classrooms_header.jsx
+++ client/app/bundles/HelloWorld/components/student_profile/students_classrooms/students_classrooms_header.jsx
@@ -5,28 +5,9 @@ import StudentsClassroom from './students_classroom';
 import StudentsClassroomsTabs from './students_classrooms_tabs.jsx';
 import StudentsClassroomsDropdown from './students_classrooms_dropdown.jsx'
 
-import { toggleDropdown, hideDropdown, fetchStudentsClassrooms, fetchStudentProfile, handleClassroomClick, updateNumberOfClassroomTabs} from '../../../../../actions/student_profile';
+import { toggleDropdown, hideDropdown, fetchStudentProfile } from '../../../../../actions/student_profile';
 
 const StudentsClassroomsHeader = React.createClass({
-
-  componentDidMount() {
-    window.addEventListener('resize', () => {
-      this.props.updateNumberOfClassroomTabs(window.innerWidth);
-    });
-    this.props.updateNumberOfClassroomTabs(window.innerWidth);
-    this.props.fetchStudentsClassrooms();
-  },
-
-  componentWillUnmount() {
-    window.removeEventListener('resize');
-  },
-
-  handleClassroomClick(classroomId) {
-    if (!this.props.loading) {
-      this.props.handleClassroomClick(classroomId);
-      this.props.fetchStudentProfile(classroomId);
-    }
-  },
 
   render() {
     return (
@@ -37,7 +18,7 @@ const StudentsClassroomsHeader = React.createClass({
               classrooms={this.props.classrooms}
               numberOfClassroomTabs={this.props.numberOfClassroomTabs}
               selectedClassroomId={this.props.selectedClassroomId || this.props.student.classroom.id}
-              handleClick={this.handleClassroomClick}
+              handleClick={this.props.handleClassroomTabClick}
               hideDropdownBoxes={this.props.hideDropdown}
               toggleDropdown={this.props.toggleDropdown}
               showDropdown={this.props.showDropdown}
@@ -54,9 +35,6 @@ const mapDispatchToProps = (dispatch) => {
   return {
     toggleDropdown: () => dispatch(toggleDropdown()),
     hideDropdown: () => dispatch(hideDropdown()),
-    fetchStudentsClassrooms: () => dispatch(fetchStudentsClassrooms()),
-    handleClassroomClick: (classroomId) => dispatch(handleClassroomClick(classroomId)),
-    updateNumberOfClassroomTabs: (screenWidth) => dispatch(updateNumberOfClassroomTabs(screenWidth)),
     fetchStudentProfile: (classroomId) => dispatch(fetchStudentProfile(classroomId))
   }
 }
