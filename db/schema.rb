4e67270dc6150c21bdc7bcf70be4818bdcdee8b1
Donald McKendrick <ddmckendrick@gmail.com>
2018-02-14 02:17:10 -0500

    Add a migration to add spelling error to responses

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170601140325) do
+ActiveRecord::Schema.define(version: 20180214051916) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 20170601140325) do
     t.json "concept_results"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "spelling_error", default: false
     t.index ["author"], name: "index_responses_on_author"
     t.index ["optimal"], name: "index_responses_on_optimal"
     t.index ["parent_uid"], name: "index_responses_on_parent_uid"
