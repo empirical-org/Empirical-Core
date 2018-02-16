c2f9aee25b5e238bfb1315a03f924480ac99131e
RyanNovas <ryan@quill.org>
2018-02-16 14:36:10 -0500

    recurring can be toggled on/off

--- client/app/bundles/HelloWorld/components/subscriptions/change_plan.jsx
+++ client/app/bundles/HelloWorld/components/subscriptions/change_plan.jsx
@@ -10,10 +10,14 @@ import LoadingIndicator from '../shared/loading_indicator.jsx';
 export default class extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      recurring: this.props.recurring,
+    };
     this.handleChange = this.handleChange.bind(this);
   }
 
   handleChange(val) {
+    this.setState({ recurring: val, });
     this.props.updateRecurring(val);
   }
 
@@ -21,10 +25,10 @@ export default class extends React.Component {
     return (
       <div className="change-plan">
         <RadioGroup onChange={this.handleChange} vertical className="radio-group">
-          <ReversedRadioButton padding={'0'} pointColor={' #00c2a2'} rootColor={'#666'} iconSize={20} value={Boolean(true)}>
+          <ReversedRadioButton checked={this.state.recurring} padding={'0'} pointColor={' #00c2a2'} rootColor={'#666'} iconSize={20} value={Boolean(true)}>
             Teacher Premium - $80 Annaul Subscription
           </ReversedRadioButton>
-          <ReversedRadioButton padding={'0'} pointColor={' #00c2a2'} rootColor={'#666'} iconSize={20} value={Boolean(false)}>
+          <ReversedRadioButton checked={!this.state.recurring} padding={'0'} pointColor={' #00c2a2'} rootColor={'#666'} iconSize={20} value={Boolean(false)}>
             Quill Basic - Free
           </ReversedRadioButton>
         </RadioGroup>
