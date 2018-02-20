5c3f1a549536c2c7f65158567b0e6c19b886a116
Jared Silver <jared@jaredsilver.name>
2018-02-20 16:44:13 -0500

    Display premium upgrade modal to nonpremium users viewing premium-only blog content

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -8,6 +8,7 @@ class BlogPostsController < ApplicationController
     find_by_hash = { slug: params[:slug] }
     find_by_hash[:draft] = false unless current_user&.role == 'staff'
     @blog_post = BlogPost.find_by!(find_by_hash)
+    @display_paywall = true unless @blog_post.can_be_accessed_by(current_user)
     @blog_post.increment_read_count
     @author = @blog_post.author
     @most_recent_posts = BlogPost.where("draft = false AND id != #{@blog_post.id}").order('updated_at DESC').limit(3)
