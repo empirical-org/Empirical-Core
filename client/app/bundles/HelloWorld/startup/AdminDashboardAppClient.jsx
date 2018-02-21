a76eda010e583d3e33cd93381c9d184c90f4ba6c
tronroy <tbconroy@gmail.com>
2018-02-21 11:50:19 -0500

    Remove unused admin view

--- client/app/bundles/HelloWorld/startup/AdminDashboardAppClient.jsx
+++ client/app/bundles/HelloWorld/startup/AdminDashboardAppClient.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import AdminDashboardRouter from '../containers/AdminDashboardRouter';
-import SubnavTabs from '../components/admin_dashboard/subnav_tabs';
 
 const AdminDashboardApp = (props) => {
   return(<AdminDashboardRouter {...props} />);
