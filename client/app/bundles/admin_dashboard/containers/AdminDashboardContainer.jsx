1a325f979e23bd3a479ba8fa5cb03d87e2ddeb0f
tronroy <tbconroy@gmail.com>
2018-02-21 14:59:24 -0500

    Correct paths in moved react components

--- client/app/bundles/admin_dashboard/containers/AdminDashboardContainer.jsx
+++ client/app/bundles/admin_dashboard/containers/AdminDashboardContainer.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import SubnavTabs from '../components/admin_dashboard/subnav_tabs';
+import SubnavTabs from '../components/subnav_tabs';
 
 const AdminDashboardContainer = (props) => {
   return (
     <div className="tab-content">
       <div className="tab-pane active" id="class-manager">
         <SubnavTabs />
-        <div id="admin-dashboard" data-id={props.id}>
+        <div id="admin-dashboard">
           {props.children}
         </div>
       </div>
--- client/app/bundles/admin_dashboard/containers/AdminDashboardRouter.jsx
+++ client/app/bundles/admin_dashboard/containers/AdminDashboardRouter.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 import { Router, Route, IndexRoute, browserHistory } from 'react-router'
 import AdminDashboard from './AdminDashboard';
 import AdminDashboardContainer from './AdminDashboardContainer'
-import DistrictActivityScoresProgressReport from '../components/progress_reports/district_activity_scores_progress_report'
+import DistrictActivityScoresProgressReport from '../components/district_activity_scores'
 
 const AdminDashboardRouter = (props) => {
   return (
     <Router Router history={browserHistory}>
       <Route path="teachers/admin_dashboard" component={AdminDashboardContainer} >
-        <IndexRoute component={AdminDashboard} />
+        <IndexRoute component={AdminDashboard} adminId={props.id} />
         <Route path="district_activity_scores" component={DistrictActivityScoresProgressReport} />
       </Route>
     </Router>
