5895578de91fad998f10fc3cc76fee88da5c8478
RyanNovas <ryan@quill.org>
2018-02-22 15:56:09 -0500

    change subscription status to case statement

--- client/app/bundles/HelloWorld/components/subscriptions/subscription_status.jsx
+++ client/app/bundles/HelloWorld/components/subscriptions/subscription_status.jsx
@@ -18,46 +18,6 @@ export default class extends React.Component {
     return false;
   }
 
-  getContent() {
-    const content = {};
-    let image,
-      expiration,
-      remainingDays;
-    if (this.state.subscriptionType !== 'Basic') {
-      expiration = moment(this.props.subscriptionStatus.expiration);
-      remainingDays = expiration.diff(moment(), 'days');
-    }
-    let subscriptionType = this.state.subscriptionType;
-    if (this.state.subscriptionType === 'Basic') {
-      image = 'basic_icon.png';
-      content.boxColor = '#00c2a2';
-      content.buttonOrDate = <a href="/premium" className="q-button cta-button bg-orange text-white">Learn More About Quill Premium</a>;
-      subscriptionType = 'Quill Basic';
-    } else if (this.props.subscriptionStatus.expired) {
-      content.boxColor = '#ff4542';
-      content.buttonOrDate = <button onClick={this.props.showPaymentModal} className="renew-subscription q-button bg-orange text-white cta-button">Renew Subscription</button>;
-      content.status = <h2><i className="fa fa-exclamation-triangle" />{`Your ${subscriptionType} Premium subscription has expired`}</h2>;
-    } else if (this.state.subscriptionType === 'Teacher') {
-      image = 'teacher_premium_icon.png';
-      content.boxColor = '#348fdf';
-    } else if (this.state.subscriptionType === 'Trial') {
-      image = 'teacher_premium_icon.png';
-    } else if (this.state.subscriptionType === 'School') {
-      content.boxColor = '#9c2bde';
-      image = 'school_premium_icon.png';
-      if (this.state.userIsContact) {
-        content.buttonOrDate = <a href="/premium" className="q-button bg-orange text-white cta-button">Renew School Premium</a>;
-      } else {
-        content.buttonOrDate = <button>Contact {this.props.subscriptionStatus.contact_name} to Renew</button>;
-      }
-    }
-    content.buttonOrDate = content.buttonOrDate || (<span className="expiration-date">
-      <span>Valid Until:</span> <span>{`${expiration.format('MMMM Do, YYYY')}`}</span><span className="time-left-in-days"> | {`${remainingDays} ${pluralize('days', remainingDays)}`}</span>
-    </span>);
-    content.status = content.status || <h2>{`You have a ${subscriptionType} Premium subscription`}<img src={`https://assets.quill.org/images/shared/${image}`} alt={`${subscriptionType}`} /></h2>;
-    return content;
-  }
-
   subscriptionType() {
     if (!this.props.subscriptionStatus) {
       return 'Basic';
@@ -69,6 +29,48 @@ export default class extends React.Component {
       return 'Trial';
     }
     return 'Teacher';
+  }
+
+  getContent() {
+    const content = {};
+    let image,
+      expiration,
+      remainingDays,
+      basicSubType;
+    let subscriptionType = this.state.subscriptionType;
+    if (this.state.subscriptionType !== 'Basic') {
+      expiration = moment(this.props.subscriptionStatus.expiration);
+      remainingDays = expiration.diff(moment(), 'days');
+    }
+    switch (this.state.subscriptionType) {
+      case 'Basic':
+        image = 'basic_icon.png';
+        content.boxColor = '#00c2a2';
+        content.buttonOrDate = <a href="/premium" className="q-button cta-button bg-orange text-white">Learn More About Quill Premium</a>;
+        subscriptionType = 'Quill Basic';
+        break;
+      case 'Teacher':
+        image = 'teacher_premium_icon.png';
+        content.boxColor = '#348fdf';
+        break;
+      case 'Trial':
+        image = 'teacher_premium_icon.png';
+        break;
+      case 'School':
+        content.boxColor = '#9c2bde';
+        image = 'school_premium_icon.png';
+        if (this.state.userIsContact) {
+          content.buttonOrDate = <a href="/premium" className="q-button bg-orange text-white cta-button">Renew School Premium</a>;
+        } else {
+          content.buttonOrDate = <button>Contact {this.props.subscriptionStatus.contact_name} to Renew</button>;
+        }
+        break;
+    }
+    content.buttonOrDate = content.buttonOrDate || (<span className="expiration-date">
+      <span>Valid Until:</span> <span>{`${expiration.format('MMMM Do, YYYY')}`}</span><span className="time-left-in-days"> | {`${remainingDays} ${pluralize('days', remainingDays)}`}</span>
+      </span>);
+    content.status = content.status || <h2>{`You have a ${subscriptionType} Premium subscription`}<img src={`https://assets.quill.org/images/shared/${image}`} alt={`${subscriptionType}`} /></h2>;
+    return content;
   }
 
   render() {
