e13c61be493236740d6e8b3b9ff83716abf8b338
RyanNovas <ryan@quill.org>
2018-02-22 18:03:04 -0500

    refactor subscription status

--- client/app/bundles/HelloWorld/components/subscriptions/subscription_status.jsx
+++ client/app/bundles/HelloWorld/components/subscriptions/subscription_status.jsx
@@ -2,6 +2,22 @@ import React from 'react';
 import moment from 'moment';
 import pluralize from 'pluralize';
 
+const quillBasicCopy = (
+  <span>
+    Quill Basic provides access to all of Quill's content. To access Quill Premium, you can purchase an individual teacher subscription or a school subscription. Teachers can earn free credits for Teacher Premium by sharing Quill and creating content.
+    <a className="green-link" href="">How to earn Quill Credits.</a>
+  </span>);
+
+const schoolPremiumCopy = (
+  <p>
+    With Quill School Premium, you will have access to all of Quill’s
+    free reports as well as additional advanced reporting. You will also
+    be able to view and print reports of your students’ progress. Our
+    advanced reports support concept, Common Core, and overall progress
+    analysis. <a className="green-link">Here’s more information</a> about your Teacher Premium features.
+  </p>
+);
+
 export default class extends React.Component {
 
   constructor(props) {
@@ -31,12 +47,40 @@ export default class extends React.Component {
     return 'Teacher';
   }
 
+  handleExpired(content, remainingDays) {
+    let statusOnClickEvent,
+      buttonCopy;
+    if (remainingDays < 1) {
+      content.boxColor = '#ff4542';
+      content.status = <h2><i className="fa fa-exclamation-triangle" />{`Your ${this.state.subscriptionType} Premium subscription has expired`}</h2>;
+      if (this.state.subscriptionType === 'School') {
+        buttonCopy = 'Renew School Subscription';
+        content.pCopy = (
+          <p>
+            <strong>Your School Premium subscription has expired and you are back to Quill Basic.</strong>
+            {quillBasicCopy}
+          </p>);
+        if (this.state.userIsContact) {
+          statusOnClickEvent = this.props.showPaymentModal;
+        } else {
+          statusOnClickEvent = () => alert('i will need to be something for when a school user is not the contact');
+        }
+      } else {
+        statusOnClickEvent = this.props.showPaymentModal;
+        content.pCopy = quillBasicCopy;
+        buttonCopy = 'Renew Subscription';
+      }
+      content.buttonOrDate = <button onClick={statusOnClickEvent} className="renew-subscription q-button bg-orange text-white cta-button">{buttonCopy}</button>;
+    }
+  }
+
+  checkIfExpired() {}
+
   getContent() {
     const content = {};
     let image,
       expiration,
-      remainingDays,
-      basicSubType;
+      remainingDays;
     let subscriptionType = this.state.subscriptionType;
     if (this.state.subscriptionType !== 'Basic') {
       expiration = moment(this.props.subscriptionStatus.expiration);
@@ -45,18 +89,24 @@ export default class extends React.Component {
     switch (this.state.subscriptionType) {
       case 'Basic':
         image = 'basic_icon.png';
+        content.pCopy = quillBasicCopy;
         content.boxColor = '#00c2a2';
         content.buttonOrDate = <a href="/premium" className="q-button cta-button bg-orange text-white">Learn More About Quill Premium</a>;
         subscriptionType = 'Quill Basic';
         break;
       case 'Teacher':
         image = 'teacher_premium_icon.png';
-        content.boxColor = '#348fdf';
+        if (remainingDays < 1) {
+          content.boxColor = '#ff4542';
+        } else {
+          content.boxColor = '#348fdf';
+        }
         break;
       case 'Trial':
         image = 'teacher_premium_icon.png';
         break;
       case 'School':
+        content.pCopy = schoolPremiumCopy;
         content.boxColor = '#9c2bde';
         image = 'school_premium_icon.png';
         if (this.state.userIsContact) {
@@ -66,6 +116,7 @@ export default class extends React.Component {
         }
         break;
     }
+    this.handleExpired(content, remainingDays);
     content.buttonOrDate = content.buttonOrDate || (<span className="expiration-date">
       <span>Valid Until:</span> <span>{`${expiration.format('MMMM Do, YYYY')}`}</span><span className="time-left-in-days"> | {`${remainingDays} ${pluralize('days', remainingDays)}`}</span>
       </span>);
@@ -84,13 +135,7 @@ export default class extends React.Component {
           </div>
           {content.buttonOrDate}
         </div>
-        <p>
-          With Quill School Premium, you will have access to all of Quill’s
-          free reports as well as additional advanced reporting. You will also
-          be able to view and print reports of your students’ progress. Our
-          advanced reports support concept, Common Core, and overall progress
-          analysis. <a className="green-link">Here’s more information</a> about your Teacher Premium features.
-        </p>
+        {content.pCopy}
       </section>
     );
   }
