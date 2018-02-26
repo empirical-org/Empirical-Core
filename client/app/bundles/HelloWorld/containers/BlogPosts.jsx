5d5db34a76f2f96d60efd80f3d8b363aed84fe34
Emilia Friedberg <emiliafriedberg@gmail.com>
2018-02-26 16:42:09 -0500

    support setting order by dragging and dropping blog posts

--- client/app/bundles/HelloWorld/containers/BlogPosts.jsx
+++ client/app/bundles/HelloWorld/containers/BlogPosts.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactTable from 'react-table';
 import CreateOrEditBlogPost from '../components/cms/blog_posts/create_or_edit_blog_post.jsx';
+import BlogPostTable from '../components/cms/blog_posts/blog_post_table.jsx';
 import BlogPostIndex from '../components/blog_posts/blog_post_index.jsx';
 import BlogPost from '../components/blog_posts/blog_post.jsx';
 import request from 'request';
@@ -13,56 +14,6 @@ export default class BlogPosts extends React.Component {
     this.renderBlogPostsByTopic = this.renderBlogPostsByTopic.bind(this)
   }
 
-  columns() {
-    return ([
-      {
-        Header: '',
-        accessor: 'draft',
-        width: 75,
-        Cell: props => <span>{props.value ? 'DRAFT' : ''}</span>,
-      },
-      {
-        Header: 'Title',
-        accessor: 'title',
-      }, {
-        Header: 'Topic',
-        accessor: 'topic',
-      }, {
-        Header: 'Created',
-        accessor: 'created_at',
-        width: 115,
-        Cell: props => <span>{moment(props.value).format('MM-DD-YY')}</span>,
-      }, {
-        Header: 'Updated',
-        accessor: 'updated_at',
-        width: 115,
-        Cell: props => <span>{moment(props.value).format('MM-DD-YY')}</span>,
-      }, {
-        Header: 'Rating',
-        accessor: 'rating',
-        width: 75
-      }, {
-        Header: '',
-        accessor: 'id',
-        sortable: false,
-        width: 75,
-        Cell: props => <a className="button" href={`/cms/blog_posts/${props.value}/edit`}>Edit</a>,
-      }, {
-        Header: '',
-        accessor: 'slug',
-        sortable: false,
-        width: 75,
-        Cell: props => <a className="button" href={`/teacher_resources/${props.value}`}>Preview</a>,
-      }, {
-        Header: '',
-        accessor: 'id',
-        sortable: false,
-        width: 75,
-        Cell: props => <a className="button" onClick={this.confirmDelete} href={`/cms/blog_posts/${props.value}/delete`}>Delete</a>,
-      }
-    ]);
-  }
-
   confirmDelete(e) {
     if(window.prompt('To delete this post, please type DELETE.') !== 'DELETE') {
       e.preventDefault();
@@ -73,18 +24,10 @@ export default class BlogPosts extends React.Component {
     const tables = this.props.topics.map(t => {
       const filteredBlogPosts = this.props.blogPosts.filter(bp => bp.topic === t)
       if (filteredBlogPosts.length > 0) {
-        return <div>
-          <h1>{t}</h1>
-          <ReactTable
-            data={filteredBlogPosts}
-            columns={this.columns()}
-            showPagination={false}
-            showPaginationTop={false}
-            showPaginationBottom={false}
-            showPageSizeOptions={false}
-            defaultPageSize={filteredBlogPosts ? filteredBlogPosts.length : 0}
-          />
-        </div>
+        return <BlogPostTable
+          topic={t}
+          blogPosts={filteredBlogPosts}
+        />
       }
     }
     )
