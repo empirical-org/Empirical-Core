972d81ad242e1d422aa21b5782011d07bce72ee4
RyanNovas <ryan@quill.org>
2018-02-27 14:09:46 -0500

    credit card wip

--- client/app/bundles/HelloWorld/containers/Subscriptions.jsx
+++ client/app/bundles/HelloWorld/containers/Subscriptions.jsx
@@ -176,9 +176,8 @@ export default class extends React.Component {
   render() {
     const userHasValidSub = this.state.subscriptionStatus && !this.state.subscriptionStatus.expired;
     const subId = `${_.get(this.state.subscriptionStatus, 'subscriptionStatus.id')}-subscription-status-id`;
-    // don't show any last four unless they have an authority level with their purchase
-    // const lastFour = this.props.authorityLevel ? this.props.lastFour : null;
-    const lastFour = this.state.authorityLevel ? this.props.lastFour : null;
+    // don't show any last four unless they have an authority level with their purchase, or they don't have a sub
+    const lastFour = (this.state.authorityLevel || !this.state.subscriptionStatus) ? this.props.lastFour : null;
     return (
       <div>
         <SubscriptionStatus
