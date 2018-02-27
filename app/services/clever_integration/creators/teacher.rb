e9c462e263a584631b67da7da79a0c4350642c3a
Donald McKendrick <ddmckendrick@gmail.com>
2018-02-27 18:50:51 -0500

    Add logs for clever

--- app/services/clever_integration/creators/teacher.rb
+++ app/services/clever_integration/creators/teacher.rb
@@ -1,6 +1,8 @@
 module CleverIntegration::Creators::Teacher
 
   def self.run(hash)
+    puts "## 3214: Clever Details"
+    puts hash
     teacher = User.find_or_initialize_by(email: hash[:email])
     teacher.update(
       clever_id: hash[:clever_id],
