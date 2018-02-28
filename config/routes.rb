4439e957cf3e06280bb0a076ae9e9e14b08ffb89
Max Buck <maxwellhigginsbuck@gmail.com>
2018-02-28 15:54:25 -0500

    add new routes to config/routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -91,6 +91,7 @@ EmpiricalGrammar::Application.routes.draw do
   put 'make_teacher' => 'students#make_teacher'
   get 'teachers/admin_dashboard' => 'teachers#admin_dashboard'
   get 'teachers/admin_dashboard/district_activity_scores' => 'teachers#admin_dashboard'
+  get 'teachers/admin_dashboard/district_concept_results' => 'teachers#admin_dashboard'
   put 'teachers/update_current_user' => 'teachers#update_current_user'
   get 'teachers/:id/schools/:school_id' => 'teachers#add_school'
   get 'teachers/get_completed_diagnostic_unit_info' => 'teachers#get_completed_diagnostic_unit_info'
@@ -296,6 +297,7 @@ EmpiricalGrammar::Application.routes.draw do
       post 'published_edition' => 'activities#published_edition'
       get 'progress_reports/activities_scores_by_classroom_data' => 'progress_reports#activities_scores_by_classroom_data'
       get 'progress_reports/district_activity_scores' => 'progress_reports#district_activity_scores'
+      get 'progress_reports/district_concept_reports' => 'progress_reports#district_concept_reports'
       get 'progress_reports/student_overview_data/:student_id/:classroom_id' => 'progress_reports#student_overview_data'
     end
 
