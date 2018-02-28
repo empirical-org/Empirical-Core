540c05f7535797164071c7249b3dcb2c24133d89
Max Buck <maxwellhigginsbuck@gmail.com>
2018-02-28 15:10:23 -0500

    added district conceept reports progress report to admin dashboard router

--- client/app/bundles/admin_dashboard/containers/AdminDashboardRouter.jsx
+++ client/app/bundles/admin_dashboard/containers/AdminDashboardRouter.jsx
@@ -3,6 +3,7 @@ import { Router, Route, IndexRoute, browserHistory } from 'react-router'
 import AdminDashboard from 'bundles/admin_dashboard/containers/AdminDashboard';
 import AdminDashboardContainer from 'bundles/admin_dashboard/containers/AdminDashboardContainer'
 import DistrictActivityScoresProgressReport from 'bundles/admin_dashboard/containers/DistrictActivityScores'
+import DistrictConceptReportsProgressReport from 'bundles/admin_dashboard/containers/DistrictConceptReports'
 
 const AdminDashboardRouter = (props) => {
   return (
@@ -10,6 +11,7 @@ const AdminDashboardRouter = (props) => {
       <Route path="teachers/admin_dashboard" component={AdminDashboardContainer} >
         <IndexRoute component={AdminDashboard} adminId={props.id} />
         <Route path="district_activity_scores" component={DistrictActivityScoresProgressReport} />
+        <Route path="district_concept_reports" component={DistrictConceptReportsProgressReport} />
       </Route>
     </Router>
   );
