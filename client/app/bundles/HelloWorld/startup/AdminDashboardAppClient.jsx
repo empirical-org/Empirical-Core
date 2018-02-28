e42173e636ef4b1d3c0796ec8717464e07fdcc36
Max Buck <maxwellhigginsbuck@gmail.com>
2018-02-28 12:06:12 -0500

    import and use new reducer in admin dashboard app client; combined reducers; might neeed to update namespaces in tom's app

--- client/app/bundles/HelloWorld/startup/AdminDashboardAppClient.jsx
+++ client/app/bundles/HelloWorld/startup/AdminDashboardAppClient.jsx
@@ -6,7 +6,9 @@ import districtActivityScores from 'reducers/district_activity_scores';
 import districtConceptReports from 'reducers/district_concept_reports';
 import { Provider } from 'react-redux';
 
-const store = createStore(districtActivityScores, applyMiddleware(thunk));
+const reducer = combineReducers({districtActivityScores, districtConceptReports})
+//const store = createStore(districtActivityScores, applyMiddleware(thunk));
+const store = createStore(reducer, applyMiddleware(thunk));
 
 const AdminDashboardApp = (props) => {
   return(
