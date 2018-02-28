d5acea61b036145f450680de50c8c4fca5ca2681
Max Buck <maxwellhigginsbuck@gmail.com>
2018-02-28 14:31:47 -0500

    added route for concept reports in admin dashboard router

--- client/app/bundles/HelloWorld/containers/AdminDashboardRouter.jsx
+++ client/app/bundles/HelloWorld/containers/AdminDashboardRouter.jsx
@@ -3,6 +3,7 @@ import { Router, Route, IndexRoute, browserHistory } from 'react-router'
 import AdminDashboard from './AdminDashboard';
 import AdminDashboardContainer from './AdminDashboardContainer'
 import DistrictActivityScoresProgressReport from '../components/progress_reports/district_activity_scores_progress_report'
+import DistrictConceptReportsProgressReport from '../components/progress_reports/district_concept_reports_progress_report'
 
 const AdminDashboardRouter = (props) => {
   return (
@@ -10,6 +11,7 @@ const AdminDashboardRouter = (props) => {
       <Route path="teachers/admin_dashboard" component={AdminDashboardContainer} >
         <IndexRoute component={AdminDashboard} />
         <Route path="district_activity_scores" component={DistrictActivityScoresProgressReport} />
+        <Route path="district_concept_reports" component={DistrictConceptReportsProgressReport} />
       </Route>
     </Router>
   );
