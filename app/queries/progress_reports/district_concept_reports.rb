b2a5f35d6ec413f1868516f4599890d8199c16c2
Max Buck <maxwellhigginsbuck@gmail.com>
2018-02-28 11:10:57 -0500

    correct query added-- working, but slow (~110s local) could be optimized?

--- app/queries/progress_reports/district_concept_reports.rb
+++ app/queries/progress_reports/district_concept_reports.rb
@@ -11,41 +11,44 @@ class ProgressReports::DistrictConceptReports
 
   private
 
-  def classroom_ids_for_admin
-    Classroom.joins(teachers: { school: :admins })
-      .where('schools_admins.user_id = ?', admin_id)
-      .pluck(:id)
-      .join(', ')
-  end
-
-  ## new query
   def query
     <<~SQL
+    WITH results AS (
     SELECT
-      schools.name,
-      teachers.name,
-      classrooms.name,
-      students.name,
-      students.id
-      SUM( CAST(concept_results.metadata->>'correct' as INT)) as correct,
-      SUM( COUNT(*) - correct) as incorrect,
-      (correct / SUM(COUNT(*)) * 100 as percentage
-    FROM schools_admins
-    JOIN schools ON schools.id = schools_admins.school_id
-    JOIN schools_users on schools_users.school_id = schools.id
-    JOIN users AS teachers on teachers.id = schools_users.user_id
-    JOIN classrooms_teachers ON classrooms_teachers.user_id = teachers.id AND classrooms_teachers.role = 'owner'
-    JOIN classrooms ON classrooms.id = classrooms_teachers.classroom_id
-    JOIN classroom_activities ON classroom_activities.classroom_id = classrooms.id
-    JOIN activity_sessions ON activity_sessions.classroom_activity_id = classroom_activities.id
-    JOIN users AS students ON students.id = activity_sessions.user_id
-    JOIN concept_results ON concept_results.activity_session_id = activity_sessions.id
-
-    WHERE schools_admins.user_id = 209523
-    GROUP BY students.id, schools.name, teachers.name, classrooms.name
+          schools.name AS school_name,
+          teachers.name AS teacher_name,
+          classrooms.name AS classroom_name,
+          students.name AS student_name,
+          students.id AS student_id,
+          SUM( CAST(concept_results.metadata->>'correct' as INT)) as correct,
+          COUNT(concept_results) as concept_results_count
+        FROM schools_admins
+        JOIN schools ON schools.id = schools_admins.school_id
+        JOIN schools_users on schools_users.school_id = schools.id
+        JOIN users AS teachers on teachers.id = schools_users.user_id
+        JOIN classrooms_teachers ON classrooms_teachers.user_id = teachers.id AND           classrooms_teachers.role = 'owner'
+        JOIN classrooms ON classrooms.id = classrooms_teachers.classroom_id
+        JOIN classroom_activities ON classroom_activities.classroom_id =
+          classrooms.id
+        JOIN activity_sessions ON activity_sessions.classroom_activity_id =
+          classroom_activities.id
+        JOIN users AS students ON students.id = activity_sessions.user_id
+        JOIN concept_results ON concept_results.activity_session_id =
+          activity_sessions.id
+        WHERE schools_admins.user_id = #{admin_id}
+        GROUP BY students.id, teachers.name, classrooms.name, schools.name
+    )
+    SELECT
+      school_name,
+      teacher_name,
+      classroom_name,
+      student_name,
+      correct,
+      (concept_results_count - correct) as incorrect,
+      FLOOR(( correct/concept_results_count::float ) * 100 ) as percentage
+    FROM results;
     SQL
   end
-  ### end new query
 
 end
 
