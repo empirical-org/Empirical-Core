a1148a11e39256002e67e725257e9b55c375d1bd
Donald McKendrick <ddmckendrick@gmail.com>
2018-03-01 02:53:28 -0500

    Revert "Chore/implement redux on student profile page"

--- client/app/bundles/Student/startup/StudentProfileAppClient.jsx
+++ client/app/bundles/Student/startup/StudentProfileAppClient.jsx
@@ -1,14 +1,6 @@
-import React from 'react'
-import StudentProfile from '../../HelloWorld/containers/StudentProfile.jsx'
-import { Provider } from 'react-redux'
-import studentProfile from '../../../reducers/student_profile'
-import { createStore, combineReducers, applyMiddleware } from 'redux'
-import thunk from 'redux-thunk'
+import React from 'react';
+import StudentProfile from '../../HelloWorld/containers/StudentProfile.jsx';
 
-const store = createStore(studentProfile, applyMiddleware(thunk));
-
-export default () => (
-  <Provider store={store}>
-    <StudentProfile />
-  </Provider>
+export default (props) => (
+  <StudentProfile {...props} />
 );
