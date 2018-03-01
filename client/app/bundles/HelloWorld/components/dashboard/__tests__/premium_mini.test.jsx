b670d03d67f0e8dbe3e4c6fdc2ccad728c62db71
Donald McKendrick <ddmckendrick@gmail.com>
2018-03-01 00:49:13 -0500

    Fix tests

--- client/app/bundles/HelloWorld/components/dashboard/__tests__/premium_mini.test.jsx
+++ client/app/bundles/HelloWorld/components/dashboard/__tests__/premium_mini.test.jsx
@@ -19,7 +19,7 @@ describe('PremiumMini component', () => {
     const wrapper = shallow(<PremiumMini userIsEligibleForTrial />);
     wrapper.find('button').simulate('click');
     expect($.post.mock.calls[0][0]).toBe('/subscriptions');
-    expect($.post.mock.calls[0][1].account_limit).toBe(1000);
-    expect($.post.mock.calls[0][1].account_type).toBe('Teacher Trial');
+    expect($.post.mock.calls[0][1].subscription.account_limit).toBe(1000);
+    expect($.post.mock.calls[0][1].subscription.account_type).toBe('Teacher Trial');
   });
 });
