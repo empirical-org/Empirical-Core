cacbfe89eb2a32817ca85ac2426ba585e6b2cee4
Max Buck <maxwellhigginsbuck@gmail.com>
2018-03-01 11:11:42 -0500

    add new combined reducer to AdminDashboardAppClient

--- client/app/bundles/HelloWorld/startup/AdminDashboardAppClient.jsx
+++ client/app/bundles/HelloWorld/startup/AdminDashboardAppClient.jsx
@@ -2,13 +2,16 @@ import React from 'react';
 import AdminDashboardRouter from 'bundles/admin_dashboard/containers/AdminDashboardRouter';
 import { combineReducers, createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
-import districtActivityScores from 'reducers/district_activity_scores';
-import districtConceptReports from 'reducers/district_concept_reports';
+import adminDashboardReducer from 'reducers/admin_dashboard';
+import districtActivityScoresReducer from 'reducers/district_activity_scores';
+import districtConceptReportsReducer from 'reducers/district_concept_reports';
 import { Provider } from 'react-redux';
 
 // TODO: start here tm: combined reducer is causing problems -- probably a
 // namespace issue
-const reducer = combineReducers({districtActivityScores, districtConceptReports})
+const reducer = combineReducers({ adminDashboardReducer,
+  districtActivityScoresReducer, districtConceptReportsReducer})
+
 const store = createStore(reducer, applyMiddleware(thunk));
 
 //const store = createStore(districtActivityScores, applyMiddleware(thunk));
