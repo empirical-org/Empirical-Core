297d90a880a78bfacf9f658568495ca1cda3e091
Jared Silver <jared@jaredsilver.name>
2018-03-01 10:19:43 -0500

    Revert "Navbar says 'Premium' if the user is upgraded"
    This reverts commit e6dd891c3f69b5617e7314f66cd685e0dbe6d421.

--- app/helpers/navigation_helper.rb
+++ app/helpers/navigation_helper.rb
@@ -41,11 +41,9 @@ module NavigationHelper
   def premium_tab_copy
     case current_user.premium_state
     when 'trial'
-      "Premium <i class='fa fa-star'></i> #{current_user.trial_days_remaining} Days Left"
-    when 'paid'
-      "Premium <i class='fa fa-star'></i>"
+      "Premium  <i class='fa fa-star'></i> #{current_user.trial_days_remaining} Days Left"
     when 'locked'
-      "Premium <i class='fa fa-star'></i> Trial Expired"
+      "Premium  <i class='fa fa-star'></i> Trial Expired"
     else
       "Try Premium <i class='fa fa-star'></i>"
     end
