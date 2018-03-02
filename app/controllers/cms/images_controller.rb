c15c14d5873a3133c771fd764f4a60d4acc12dbd
Emilia Friedberg <emiliafriedberg@gmail.com>
2018-03-02 12:19:09 -0500

    wip

--- app/controllers/cms/images_controller.rb
+++ app/controllers/cms/images_controller.rb
@@ -1,16 +1,11 @@
 class Cms::ImagesController < ApplicationController
-  # mount_uploader :image, ImageUploader
 
   def save_image
     i = Image.new
-    file = params[:file].gsub("\u0000", '')
+    file = params[:file]
     i.file.store!(file)
+    i.file = file
     i.save!
     render json: {url: i.file.url}
-    # stored_image = image.store!(params[:file])
-    # binding.pry
-    # render json: {}
-    # params[:id] = params[:activity_id]
-    # @activity = subject
   end
 end
