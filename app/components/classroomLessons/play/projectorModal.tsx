aea33db066ed6228a7c043199a360d5fbc04f037
Emilia Friedberg <emiliafriedberg@gmail.com>
2018-03-05 12:23:20 -0500

    make projector modal prettier

--- app/components/classroomLessons/play/projectorModal.tsx
+++ app/components/classroomLessons/play/projectorModal.tsx
@@ -4,13 +4,33 @@ const ProjectorModal: React.SFC<{closeModal: any}> = (props) => {
  return <div className="projector-modal-container">
     <div className="projector-modal-background" />
      <div className="projector-modal">
-        <img onClick={props.closeModal} className="exit" src="http://assets.quill.org/images/icons/CloseIcon.svg"/>
-        <img className="illustration" src="http://assets.quill.org/images/illustrations/projector_modal.svg" />
-        <h1><span>Next:</span> Project This Window</h1>
-        <p>In unmirrored mode, drag this window across your extended screen until it appears on your projector. Keep your teacher view on your computer's screen only.</p>
-        <p>For more information on how to unmirror your projector, click on the link below.</p>
-        <a target="_blank" href="http://support.quill.org/activities-implementation/quill-lessons/how-do-i-project-quill-lessons">Learn How To Unmirror Projectors</a>
-        <button onClick={props.closeModal}>Got it!</button>
+       <div className="top-section">
+          <img onClick={props.closeModal} className="exit" src="http://assets.quill.org/images/icons/CloseIcon.svg"/>
+          <img className="illustration" src="http://assets.quill.org/images/illustrations/projector_modal.svg" />
+          <h1><span>Next:</span> Project this window</h1>
+          <p>Project this screen to your students by adjusting your display settings to unmirrored mode so that you can project this window while looking at a different window on your computer.</p>
+          <p className="follow-instructions">Follow the instructions below to adjust your display settings, and then drag this window to the left or right of your screen to project.</p>
+          <button onClick={props.closeModal}>Got it!</button>
+        </div>
+        <div className="bottom-section">
+          <p className="bottom-section-header">How to adjust your display settings?</p>
+
+          <div className="list">
+            <p className="list-header">For Windows:</p>
+            <p className="list-item">1. Go to Control Panel or right-click on your desktop.</p>
+            <p className="list-item">2. Choose Display Settings.</p>
+            <p className="list-item">3. Select Extend Desktop from Multiple Display Dropdown.</p>
+          </div>
+
+          <div className="list">
+            <p className="list-header">For Macs:</p>
+            <p className="list-item">1. Go to System Preferences.</p>
+            <p className="list-item">2. Go to Displays.</p>
+            <p className="list-item">3. Select the Arrangement tab.</p>
+            <p className="list-item">4. Uncheck Mirror Displays.</p>
+          </div>
+
+        </div>
      </div>
    </div>
 }
