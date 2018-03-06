6b93d02a3667e085f493b05fc052129c84aac386
tronroy <tbconroy@gmail.com>
2018-03-06 12:50:16 -0500

    Typescript works with static webpack config

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,4 +1,7 @@
 Rails.application.config.assets.version = "1.0"
 Rails.application.config.assets.paths << Rails.root.join('public', 'webpack')
 
-Rails.application.config.assets.precompile += %w( cookie_warning.js )
+Rails.application.config.assets.precompile += [
+  'cookie_warning.js',
+  'jquery-1.8.2',
+]
