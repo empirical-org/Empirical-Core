1b92f80e4f23fbdf6ce5f7d3f22e2970b4012484
Max Buck <maxwellhigginsbuck@gmail.com>
2018-03-07 16:28:37 -0500

    wrote heroic sql query in app/queries/progress_reports/district_standards_reports.rb

--- app/queries/progress_reports/district_standards_reports.rb
+++ app/queries/progress_reports/district_standards_reports.rb
@@ -1,4 +1,4 @@
-class ProgressReports::DistrictConceptReports
+class ProgressReports::DistrictStandardsReports
   attr_reader :admin_id
 
   def initialize(admin_id)
@@ -13,41 +13,39 @@ class ProgressReports::DistrictConceptReports
 
   def query
     <<~SQL
-    WITH results AS (
-    SELECT
-          schools.name AS school_name,
-          teachers.name AS teacher_name,
-          classrooms.name AS classroom_name,
-          students.name AS student_name,
-          students.id AS student_id,
-          SUM( CAST(concept_results.metadata->>'correct' as INT)) as correct,
-          COUNT(concept_results) as concept_results_count
-        FROM schools_admins
-        JOIN schools ON schools.id = schools_admins.school_id
-        JOIN schools_users on schools_users.school_id = schools.id
-        JOIN users AS teachers on teachers.id = schools_users.user_id
-        JOIN classrooms_teachers ON classrooms_teachers.user_id = teachers.id AND           classrooms_teachers.role = 'owner'
-        JOIN classrooms ON classrooms.id = classrooms_teachers.classroom_id
-        JOIN classroom_activities ON classroom_activities.classroom_id =
-          classrooms.id
-        JOIN activity_sessions ON activity_sessions.classroom_activity_id =
-          classroom_activities.id
-        JOIN users AS students ON students.id = activity_sessions.user_id
-        JOIN concept_results ON concept_results.activity_session_id =
-          activity_sessions.id
-        WHERE schools_admins.user_id = #{admin_id}
-        GROUP BY students.id, teachers.name, classrooms.name, schools.name
-    )
-    SELECT
-      school_name,
-      teacher_name,
-      classroom_name,
-      student_name,
-      correct,
-      (concept_results_count - correct) as incorrect,
-      FLOOR(( correct/concept_results_count::float ) * 100 ) as percentage
-    FROM results;
-    SQL
+    WITH final_activity_sessions AS (
+      SELECT activity_sessions.*, activities.topic_id FROM activity_sessions
+        JOIN classroom_activities ON activity_sessions.classroom_activity_id = classroom_activities.id
+        JOIN activities ON classroom_activities.activity_id = activities.id
+        JOIN topics ON topics.id = activities.topic_id
+        JOIN classrooms_teachers on classrooms_teachers.classroom_id = classroom_activities.classroom_id
+        WHERE activity_sessions.is_final_score
+        AND classrooms_teachers.user_id in (SELECT schools_users.user_id FROM users as researcher
+          JOIN schools_admins ON schools_admins.user_id = researcher.id
+          JOIN schools ON schools.id = schools_admins.school_id
+          JOIN schools_users ON schools_users.school_id = schools.id
+          WHERE researcher.id = #{admin_id}
+        )
+        AND activity_sessions.visible
+            AND classroom_activities.visible
+    ) SELECT
+        topics.id,
+        topics.name,
+        sections.name as section_name,
+        COUNT(DISTINCT(final_activity_sessions.activity_id)) as total_activity_count,
+        COUNT(DISTINCT(final_activity_sessions.user_id)) as total_student_count,
+        COUNT(DISTINCT(avg_score_for_topic_by_user.user_id)) as proficient_count
+      FROM topics
+        JOIN sections ON sections.id = topics.section_id
+        JOIN final_activity_sessions ON final_activity_sessions.topic_id = topics.id
+        LEFT JOIN (SELECT topic_id, user_id, AVG(percentage) as avg_score
+          FROM final_activity_sessions
+          GROUP BY final_activity_sessions.topic_id, final_activity_sessions.user_id
+          HAVING AVG(percentage) >= 0.8
+        ) AS avg_score_for_topic_by_user ON avg_score_for_topic_by_user.topic_id = topics.id
+      GROUP BY topics.id, sections.name
+      ORDER BY topics.name ASC;
+   SQL
   end
 
 end
