38145e9766f4e622e4585b6a061b89bfdfa5cbc2
Donald McKendrick <ddmckendrick@gmail.com>
2018-03-08 13:51:50 -0500

    Revert "Launch Initial School Level Reporting."

--- spec/models/concerns/teacher_spec.rb
+++ spec/models/concerns/teacher_spec.rb
@@ -195,6 +195,7 @@ describe User, type: :model do
       let!(:coteacher_classroom_invitation_2) {create(:coteacher_classroom_invitation, invitation_id: pending_coteacher_invitation_2.id)}
 
       context '#handle_negative_classrooms_from_update_coteachers' do
+
         it "deletes only the passed classrooms from invitations, leaving other ones unaffected" do
           expect(CoteacherClassroomInvitation.exists?(id: coteacher_classroom_invitation.id)).to be
           expect(CoteacherClassroomInvitation.exists?(id: coteacher_classroom_invitation_2.id)).to be
@@ -216,14 +217,15 @@ describe User, type: :model do
 
         it "adds new invitations to classrooms the teacher has not been invited to" do
           new_classroom = create(:classroom)
-
-          expect { teacher.handle_positive_classrooms_from_update_coteachers([new_classroom.id], new_classroom.owner.id) }
-            .to change(CoteacherClassroomInvitation, :count).by(1)
+          old_invitation_count = CoteacherClassroomInvitation.all.count
+          teacher.handle_positive_classrooms_from_update_coteachers([new_classroom.id], new_classroom.owner.id)
+          expect(CoteacherClassroomInvitation.all.count - old_invitation_count).to eq(1)
         end
 
         it "does not create additional invitations to classrooms the teacher has been invited to" do
-          expect { teacher.handle_positive_classrooms_from_update_coteachers([co_taught_classroom.id], student.id) }
-            .not_to change(CoteacherClassroomInvitation, :count)
+          old_invitation_count = CoteacherClassroomInvitation.all.count
+          teacher.handle_positive_classrooms_from_update_coteachers([co_taught_classroom.id], student.id)
+          expect(CoteacherClassroomInvitation.all.count - old_invitation_count).to eq(0)
         end
       end
 
