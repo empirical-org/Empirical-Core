41b191ce9aa7e7c16e4df80dd2c0b235164b2244
RyanNovas <ryan@quill.org>
2018-03-08 13:19:53 -0500

    hides credit card, adds warnings to subscription cms

--- app/controllers/cms/cms_controller.rb
+++ app/controllers/cms/cms_controller.rb
@@ -2,8 +2,10 @@ class Cms::CmsController < ApplicationController
   before_filter :staff!
 
   def get_subscription_data
-    @user_premium_types = Subscription::ALL_OFFICIAL_TYPES
-    @subscription_payment_methods = Subscription::PAYMENT_METHODS
+    @premium_types = @school  ? Subscription::OFFICIAL_SCHOOL_TYPES : Subscription::OFFICIAL_TEACHER_TYPES
+    @subscription_payment_methods = Subscription::PAYMENT_METHODS.dup
+    # we do not want credit card here as it should only occur automatically
+    @subscription_payment_methods.delete('Credit Card')
     @promo_expiration_date = Subscription.promotional_dates[:expiration]
     # get the user's colleagues at the same school if user subscription, or the school if we are editing a school subscription
     @school ||= @user.school
