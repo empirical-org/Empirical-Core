68aebd406cdc4e3ff8535fd729d9cabb4166d1c9
Max Buck <maxwellhigginsbuck@gmail.com>
2018-03-08 12:13:43 -0500

    removed tests for filters in client/app/bundles/admin_dashboard/containers/__tests__/DistrictStandardsReports.test.jsx and wrote tests for All, and CSV downloads

--- client/app/bundles/admin_dashboard/containers/__tests__/DistrictStandardsReports.test.jsx
+++ client/app/bundles/admin_dashboard/containers/__tests__/DistrictStandardsReports.test.jsx
@@ -8,10 +8,11 @@ describe('DistrictStandardsReports', () => {
 
   it('renders report for all students', () => {
     const classroom = {
-      classroom_name: 'Professor Trelawney - Divination',
-      school_name: 'Hogwarts',
-      student_name: 'Parvati Patil',
-      teacher_name: 'Sybill Trelawney',
+      section_name: '1st Grade CCSS',
+      name: '1.1b Use Common, Proper, and Posessive Nouns',
+      total_student_count: 60,
+      total_activity_count: 2,
+      proficient_count: 29,
     };
     const state = {
       district_standards_reports: {
@@ -31,13 +32,11 @@ describe('DistrictStandardsReports', () => {
 
   it('formats csv data', () => {
     const classroom = {
-      classroom_name: 'Mrs. Krabapple - 4th Grade',
-      school_name: 'Springfield Elementary',
-      student_name: 'Bart Simpson',
-      teacher_name: 'Mrs. Krabapple',
-      correct: '12',
-      incorrect: '20',
-      percentage: '37',
+      section_name: "Mrs. Bonker's 2nd Grade",
+      name: '1.1b How to tell a cactus from a cow',
+      total_student_count: 60,
+      total_activity_count: 2,
+      proficient_count: 29,
     };
     const state = {
       district_standards_reports: {
@@ -54,125 +53,20 @@ describe('DistrictStandardsReports', () => {
 
     expect(wrapper.prop('csvData')).toEqual([
       [
-        'Student',
-        'Teacher',
-        'Classroom',
-        'School',
-        'Correct',
-        'Incorrect',
-        'Success Rate'
+        'Standard Level',
+        'Standard Name',
+        'Students',
+        'Proficient',
+        'Activities'
       ],
       [
-        'Bart Simpson',
-        'Mrs. Krabapple',
-        'Mrs. Krabapple - 4th Grade',
-        'Springfield Elementary',
-        '12',
-        '20',
-        '37',
+        "Mrs. Bonker's 2nd Grade",
+        '1.1b How to tell a cactus from a cow',
+        60,
+        2,
+        29,
       ]
     ]);
   });
 
-
-  it('filters the report', () => {
-    const classrooms = [
-      {
-        classroom_name: 'Poetry 101',
-        school_name: 'Bel-Air Academy',
-        student_name: 'Carlton Banks',
-        teacher_name: 'Ned Fellows',
-      }, {
-        classroom_name: "Mr. Garrison's 4th Grade",
-        school_name: 'South Park Elementary',
-        student_name: 'Eric Cartman',
-        teacher_name: 'Mr. Garrison',
-      }, {
-        classroom_name: "Mrs. Crabtree's Morning Bus",
-        school_name: 'South Park Elementary',
-        student_name: 'Stan Marsh',
-        teacher_name: 'Mrs. Crabtree',
-      }, {
-        classroom_name: "Mrs. Crabtree's Afternoon Bus",
-        school_name: 'South Park Elementary',
-        student_name: 'Butters',
-        teacher_name: 'Mrs. Crabtree',
-      }
-    ];
-    const state = {
-      district_standards_reports: {
-        loading: false,
-        errors: false,
-        selectedClassroom: "Mrs. Crabtree's Afternoon Bus",
-        selectedSchool: 'South Park Elementary',
-        selectedTeacher: 'Mrs. Crabtree',
-        standardsReportsData: classrooms,
-      },
-    };
-    const store = createMockStore(state);
-    const wrapper = shallow(<DistrictStandardsReports store={store} />);
-
-    expect(wrapper.prop('filteredStandardsReportsData')).toEqual([{
-      classroom_name: "Mrs. Crabtree's Afternoon Bus",
-      school_name: 'South Park Elementary',
-      student_name: 'Butters',
-      teacher_name: 'Mrs. Crabtree',
-    }]);
-  });
-
-  it('populates the dropdown options', () => {
-    const classrooms = [
-      {
-        classroom_name: 'Poetry 101',
-        school_name: 'Bel-Air Academy',
-        student_name: 'Carlton Banks',
-        teacher_name: 'Ned Fellows',
-      }, {
-        classroom_name: "Mr. Garrison's 4th Grade",
-        school_name: 'South Park Elementary',
-        student_name: 'Eric Cartman',
-        teacher_name: 'Mr. Garrison',
-      }, {
-        classroom_name: "Mrs. Crabtree's Morning Bus",
-        school_name: 'South Park Elementary',
-        student_name: 'Stan Marsh',
-        teacher_name: 'Mrs. Crabtree',
-      }, {
-        classroom_name: "Mrs. Crabtree's Afternoon Bus",
-        school_name: 'South Park Elementary',
-        student_name: 'Butters',
-        teacher_name: 'Mrs. Crabtree',
-      }
-    ];
-    const state = {
-      district_standards_reports:{
-        loading: false,
-        errors: false,
-        selectedClassroom: "Mrs. Crabtree's Afternoon Bus",
-        selectedSchool: 'South Park Elementary',
-        selectedTeacher: 'Mrs. Crabtree',
-        standardsReportsData: classrooms,
-      },
-    };
-    const store = createMockStore(state);
-    const wrapper = shallow(<DistrictStandardsReports store={store} />);
-
-    expect(wrapper.prop('schoolNames')).toEqual([
-      'All Schools',
-      'Bel-Air Academy',
-      'South Park Elementary',
-    ]);
-
-    expect(wrapper.prop('teacherNames')).toEqual([
-      'All Teachers',
-      'Mr. Garrison',
-      'Mrs. Crabtree',
-    ]);
-
-    expect(wrapper.prop('classroomNames')).toEqual([
-      'All Classrooms',
-      "Mrs. Crabtree's Morning Bus",
-      "Mrs. Crabtree's Afternoon Bus",
-    ]);
-  });
 });
