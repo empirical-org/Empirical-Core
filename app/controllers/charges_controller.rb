363bd2e8060e8e5fc1c3ddd452f30bd255e49041
RyanNovas <ryan@quill.org>
2018-03-12 17:18:05 -0400

    merge conflict

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -40,37 +40,4 @@ class ChargesController < ApplicationController
     customer.save
   end
 
-<<<<<<< HEAD
-  def handle_subscription
-    attributes[:purchaser_id] = current_user.id
-    attributes[:payment_method] = 'Credit Card'
-    attributes[:payment_amount] = @charge.amount
-    attributes[:recurring] = true
-    current_user.update(stripe_customer_id: @charge.customer)
-    if @charge.amount == Subscription::SCHOOL_RENEWAL_PRICE
-      attributes[:account_type] = "School Paid"
-      if current_user.school && ['home school', 'us higher ed', 'international', 'other', 'not listed'].exclude?(current_user.school.name)
-        # if the user has a school, and it is not one of the aforementioned defaults, create or update the premium subscription for it
-        Subscription.create_with_school_or_user_join(current_user.school.id, 'school', attributes)
-      else
-        @message = 'You do not seem to be registered with a school. Your account has been upgraded, and we will reach out to you shortly to upgrade the rest of your school to premium.'
-        attributes[:account_type] = 'Purchase Missing School'
-        PremiumMissingSchoolEmailWorker.perform_async(current_user.id)
-        Subscription.create_with_school_or_user_join(current_user.id, 'user', attributes)
-      end
-    else
-      attributes[:account_type] = "Teacher Paid"
-      Subscription.create_with_school_or_user_join(current_user.id, 'user', attributes)
-    end
-  end
-
-  def premium_redirect
-    if current_user
-      subscriptions_path
-    else
-      new_session_path
-    end
-  end
-=======
->>>>>>> fix/subscription-post-launch
 end
