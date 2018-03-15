73caf95fa399800910e8c54e165683aab802124f
Emilia Friedberg <emiliafriedberg@gmail.com>
2018-03-15 17:25:33 -0400

    finish sign up tests

--- cypress/integration/sign_up.js
+++ cypress/integration/sign_up.js
@@ -2,10 +2,8 @@ const faker = require('faker');
 
 describe('Sign Up page', function() {
 
-  it('loads', function() {
-    // cy.exec('rake db:drop', {failOnNonZeroExit: false})
-    // cy.exec('rake db:create', {failOnNonZeroExit: false})
-    // cy.exec('rake db:migrate', {failOnNonZeroExit: false})
+  before(function() {
+    cy.exec('RAILS_ENV=cypress rake add_cypress_test_data:add_school', {failOnNonZeroExit: false})
     cy.visit('/account/new')
   })
 
@@ -14,9 +12,8 @@ describe('Sign Up page', function() {
     cy.contains('Student')
   })
 
-  describe('clicking on Educator', function() {
-    it ('takes you to a sign up form', function() {
-      // cy.exec('rake db:reset db:seed', {failOnNonZeroExit: false})
+  describe('I am an educator with an original email and a listed school', function() {
+    it ('clicking educator takes me to a sign up form', function() {
       cy.contains('Educator').click()
 
       const firstName = faker.name.firstName()
@@ -24,11 +21,8 @@ describe('Sign Up page', function() {
       const email = faker.internet.email()
 
       cy.get('#first_name')
-      // .type(`${firstName}`)
-      // .should('have.value', `${firstName}`)
       .type(firstName)
       .should('have.value', firstName)
-
 
       cy.get('#last_name')
       .type(lastName)
@@ -45,21 +39,99 @@ describe('Sign Up page', function() {
       cy.get('.sign-up-button').click()
 
     })
-    it('asks if you are a faculty member at a U.S K-12 School', function() {
-      cy.contains('Yes')
-      cy.contains('No')
 
-      describe('clicking yes', function() {
-        it ('has an input field for a zip code', function() {
-          cy.contains('Yes').click()
-          cy.get('#zip').type('11221')
-        })
-      })
+    it('asks if I am a faculty member at a U.S K-12 School', function() {
+      cy.contains('Yes').click()
+    })
 
-      describe('clicking no', function() {
-        cy.contains('No').click()
-      })
+    it ('has an input field for a zip code', function() {
+      cy.get('#zip').type('11221')
+    })
 
+    it ('populates options for the schools in that zipcode', function() {
+      cy.get('select').select('1')
+    })
+
+    it ('has a Confirm School button', function() {
+      cy.contains('Confirm School').click()
+    })
+
+  })
+
+  describe('I am a student with a unique username', function() {
+    it ('clicking student takes me to a sign up form', function() {
+      cy.visit('/account/new')
+      cy.contains('Student').click()
+
+      const firstName = faker.name.firstName()
+      const lastName = faker.name.lastName()
+      const email = faker.internet.email()
+      const username = faker.internet.userName()
+
+      cy.get('#first_name')
+      .type(firstName)
+      .should('have.value', firstName)
+
+      cy.get('#last_name')
+      .type(lastName)
+      .should('have.value', lastName)
+
+     cy.get('#username')
+        .type(username)
+        .should('have.value', username)
+
+      cy.get('#email')
+      .type(email)
+      .should('have.value', email)
+
+      cy.get('#password')
+      .type('password')
+      .should('have.value', 'password')
+
+      cy.get('.sign-up-button').click()
+    })
+
+    it('brings me to a different page', function() {
+      cy.url().should('not.include', 'account')
+    })
+
+  })
+
+  describe('I am a student with a non-unique username', function() {
+    it ('clicking student takes me to a sign up form', function() {
+      cy.visit('/account/new')
+      cy.contains('Student').click()
+
+      const firstName = faker.name.firstName()
+      const lastName = faker.name.lastName()
+      const email = faker.internet.email()
+      const username = faker.internet.userName()
+
+      cy.get('#first_name')
+      .type(firstName)
+      .should('have.value', firstName)
+
+      cy.get('#last_name')
+      .type(lastName)
+      .should('have.value', lastName)
+
+     cy.get('#username')
+        .type('student')
+        .should('have.value', 'student')
+
+      cy.get('#email')
+      .type(email)
+      .should('have.value', email)
+
+      cy.get('#password')
+      .type('password')
+      .should('have.value', 'password')
+
+      cy.get('.sign-up-button').click()
+    })
+
+    it('shows me an error after I submit a non-unique username', function() {
+      cy.contains('Username has already been taken.')
     })
 
   })
--- lib/tasks/add_cypress_test_data.rake
+++ lib/tasks/add_cypress_test_data.rake
@@ -1,15 +1,23 @@
 require 'factory_bot_rails'
 include FactoryBot::Syntax::Methods
 
-namespace :add_test_users do
-  task :create => :environment do
+namespace :add_cypress_test_data do
+  task :add_users => :environment do
+    add_users
+  end
 
+  task :add_school => :environment do
+    add_school
+  end
+
+  def add_users
     User.find_or_create_by(role: 'staff', username: 'staff').update(password: 'password')
-
     User.find_or_create_by(role: 'teacher', username: 'teacher').update(password: 'password')
-
     User.find_or_create_by(role: 'student', username: 'student').update(password: 'password')
+  end
 
+  def add_school
+    School.find_or_create_by(name: 'Cool Bushwick School', zipcode: 11221)
   end
 
 end
