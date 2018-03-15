ec6c0a5d68b386a97bdc74b09f476b694b75f443
tronroy <tbconroy@gmail.com>
2018-03-15 15:50:37 -0400

    Fix jsx syntax

--- client/app/bundles/admin_dashboard/containers/AdminDashboardRouter.jsx
+++ client/app/bundles/admin_dashboard/containers/AdminDashboardRouter.jsx
@@ -12,8 +12,7 @@ const AdminDashboardRouter = (props) => {
     <Router Router history={browserHistory}>
       <Route path="teachers/admin_dashboard" component={AdminDashboardContainer} >
         <IndexRoute component={AdminDashboard} adminId={props.id} />
-        <Route path="district_activity_scores" component={DistrictActivityScoresProgressReport} >
-        </Route>
+        <Route path="district_activity_scores" component={DistrictActivityScoresProgressReport} />
         <Route path="district_activity_scores/student_overview" component={ActivityScoresStudentOverview} />
         <Route path="district_concept_reports" component={DistrictConceptReportsProgressReport} />
         <Route path="district_standards_reports" component={DistrictStandardsReportsProgressReport} />
