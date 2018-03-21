3c937ff5cbb55b9430e25d6b1cabe4b174766e5d
RyanNovas <ryan@quill.org>
2018-03-21 17:59:38 -0400

    premium page and credit card changes

---
+++ cypress/integration/premium_page.js
@@ -0,0 +1,58 @@
+describe('Premium Page', () => {
+
+  describe('when I have never had a subscription', ()=>{
+    before(() =>{
+      // cy.exec('RAILS_ENV=cypress rake find_or_create_cypress_test_data:find_or_create_teacher', {failOnNonZeroExit: false})
+      // cy.login('teacher', 'password')
+      beforeEach(() => {
+        Cypress.Cookies.preserveOnce("_quill_session")
+      })
+      cy.visit('/premium')
+    })
+    
+    it('loads', ()=>{
+      cy.get('#premium-pricing-guide')
+    })
+    
+    describe('teacher premium', ()=>{
+      describe('buy now button', ()=>{
+        it.skip('gives the user premium when clicked', ()=>{
+          cy.get('#purchase-btn')
+          // .click()
+        })
+      })
+      
+      it('has a Free Trial button', ()=>{
+        cy.get('.empty-blue')
+      })
+    })
+    
+    describe('school premium', ()=>{
+      it('has a purchase button that opens a modal when clicked',()=>{
+        cy.get('.btn-purple')
+          .click().get('.school-premium-modal') 
+      })
+    })
+    
+    
+    it('has an option to start premium', ()=>{
+      
+    })
+    
+    it('has an option to start a trial', ()=>{
+      
+    })
+    
+    it('has an option to start a trial', ()=>{
+      
+    })
+  
+    
+  })
+  
+  
+
+
+
+
+})
