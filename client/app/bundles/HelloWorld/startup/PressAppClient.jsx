616d42a955200b428d9419a5f90d6297289d9cdd
Emilia Friedberg <emiliafriedberg@gmail.com>
2018-03-21 17:46:09 -0400

    style press and announcement pages

--- client/app/bundles/HelloWorld/startup/PressAppClient.jsx
+++ client/app/bundles/HelloWorld/startup/PressAppClient.jsx
@@ -5,8 +5,19 @@ export default (props) => {
   const blogPosts = props.blogPosts ? props.blogPosts.map(article =>
     <PreviewCard content={article.preview_card_content} link={`/teacher_resources/${article.slug}`} />
   ) : null
-    return <div id="preview-card-container">
-      {blogPosts}
+    return <div className="announcements-page">
+      <div className="header-section">
+        <div className="text">
+          <h1>In the Press</h1>
+          <p>Read articles that feature Quill</p>
+        </div>
+        <form className='width-422' action={`${process.env.DEFAULT_URL}/teacher_resources/search`}>
+        <input type='text' placeholder='Search for posts' name='query'/>
+      </form>
+      </div>
+      <div id="preview-card-container">
+        {blogPosts}
+      </div>
     </div>
 
 }
