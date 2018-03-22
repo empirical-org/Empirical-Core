50cd56e278d50276b56463b9eb3cb390b12b2119
RyanNovas <ryan@quill.org>
2018-03-22 16:14:30 -0400

    all logged in premium states

--- cypress/integration/premium_page.js
+++ cypress/integration/premium_page.js
@@ -1,8 +1,75 @@
 describe.only('Premium Page', () => {
 
-  describe('suscriptions', ()=>{
-    before(() =>{
-      cy.exec('RAILS_ENV=cypress rake find_or_create_cypress_test_data:find_or_create_teacher_with_stripe_id', {failOnNonZeroExit: false})
+  const teacherActivePremiumCopy = 'You have an active subscription and cannot buy premium now. If your subscription is a school subscription, you may buy Premium when it expires. If your subscription is a teacher one, please turn on recurring payments and we will renew it automatically when your subscription ends.'
+  const schoolActivePremiumCopy = 'You have an active subscription and cannot buy premium now. You may buy a new Premium subscription when your current subscription expires.'
+
+  const itHasYouCanBookMeLink = () => {
+    it('has a link to you can book me', () => {
+      cy.get('.pricing-mini > a').should('have.attr', 'href').should('match', /youcanbook/)
+    })
+  }
+  const itLoads = () => {
+    it('has a link to you can book me', () => {
+      cy.get('.pricing-mini > a').should('have.attr', 'href').should('match', /youcanbook/)
+    })
+  }
+  const itDoesNotHaveAFreeTrial = () => {
+    it('does not have a Free Trial', () => {
+      cy.get('.empty-blue').should('not.exist')
+    })
+  }
+  const itHasAPurchaseButtonThatOpensAModalWhenClicked = ()=>{
+    it('has a purchase button that opens a modal when clicked', () => {
+      cy.get('.btn.purple')
+        .click().get('.school-premium-modal').get('.pull-right').click()
+    })
+  }
+
+
+  const activePremiumSubscriptionBehavior = () => {
+
+    describe('it has all the active premium behavior', () => {
+
+        itLoads();
+
+        describe('school premium', () => {
+
+          itHasYouCanBookMeLink()
+
+          it('has a purchase button that triggers an alert mentioning active premium when clicked', () => {
+            const stub = cy.stub()
+            cy.on('window:alert', stub)
+            cy.get('#purchase-btn')
+              .click().then(() => {
+                expect(stub.getCall(0)).to.be.calledWith(teacherActivePremiumCopy)
+              })
+          })
+
+        })
+
+        describe('teacher premium', () => {
+
+          itDoesNotHaveAFreeTrial()
+
+          it('has a purchase button that triggers an alert mentioning active premium when clicked', () => {
+            const stub = cy.stub()
+            cy.on('window:alert', stub)
+            cy.get('.btn.purple')
+              .click().then(() => {
+                expect(stub.getCall(0)).to.be.calledWith(schoolActivePremiumCopy)
+              })
+          })
+        })
+      }
+    )
+  }
+
+
+  describe('suscriptions', () => {
+    before(() => {
+      cy.exec('RAILS_ENV=cypress rake find_or_create_cypress_test_data:find_or_create_teacher_with_stripe_id', {
+        failOnNonZeroExit: false
+      })
       cy.login('teacher', 'password')
       beforeEach(() => {
         Cypress.Cookies.preserveOnce('_quill_session')
@@ -10,34 +77,22 @@ describe.only('Premium Page', () => {
       cy.visit('/premium')
     })
 
-    describe('when I have never had one', ()=>{
-      it('loads', ()=>{
-        cy.get('#premium-pricing-guide')
+    describe('when I have never had one', () => {
+      itLoads();
+
+      describe('school premium', () => {
+        itHasYouCanBookMeLink();
+        itHasAPurchaseButtonThatOpensAModalWhenClicked()
       })
 
-
-
-      describe('school premium', ()=>{
-
-        it('has a link to you can book me', ()=>{
-          cy.get('.pricing-mini > a').should('have.attr', 'href').should('match', /youcanbook/)
-        })
-
-        it('has a purchase button that opens a modal when clicked',()=>{
-          cy.get('.btn.purple')
-          .click().get('.school-premium-modal')
-        })
-
-      })
-
-      describe('teacher premium', ()=>{
-        it('has a Buy Now button', ()=>{
+      describe('teacher premium', () => {
+        it('has a Buy Now button', () => {
           cy.get('#purchase-btn').click()
         })
 
-        it('has a Free Trial button that activate a trial when clicked and redirects to the scorebook', ()=>{
+        it('has a Free Trial button that activate a trial when clicked and redirects to the scorebook', () => {
           cy.get('.empty-blue').click()
-          cy.location().should((loc)=>{
+          cy.location().should((loc) => {
             expect(loc.pathname).to.eq('/teachers/classrooms/scorebook')
           })
           cy.get('.premium-tab > a > .hide-on-mobile').contains('30 Days Left')
@@ -48,60 +103,77 @@ describe.only('Premium Page', () => {
 
 
 
-    describe('when I have a trial the subscription page', ()=>{
+    describe('when I have a trial', () => {
       // keep this below the free trial activation so that we can build our way through the various states
-      before(() =>{
+      before(() => {
         beforeEach(() => {
           Cypress.Cookies.preserveOnce('_quill_session')
         })
         cy.visit('/premium')
       })
 
-      it('loads', ()=>{
-        cy.get('#premium-pricing-guide')
-      })
+      itLoads()
 
+      describe('school premium', () => {
 
-      describe('school premium', ()=>{
+        itHasYouCanBookMeLink();
 
-        it('has a link to you can book me', ()=>{
-          cy.get('.pricing-mini > a').should('have.attr', 'href').should('match', /youcanbook/)
-        })
-
-        it('has a purchase button that opens a modal when clicked',()=>{
-          cy.get('.btn.purple')
-          .click().get('.school-premium-modal').get('.pull-right').click()
-        })
+        itHasAPurchaseButtonThatOpensAModalWhenClicked();
 
       })
 
-      describe('teacher premium', ()=>{
-        it('does not have a Free Trial', ()=>{
-          cy.get('.empty-blue').should('not.exist')
+      describe('teacher premium', () => {
+        before(() => {
+          beforeEach(() => {
+            Cypress.Cookies.preserveOnce('_quill_session')
+          })
+          cy.visit('/premium')
         })
 
-        it('has a Buy Now button that opens up a modal', ()=>{
+        itDoesNotHaveAFreeTrial()
+
+        it('has a Buy Now button that opens up the select credit card modal', () => {
           cy.get('#purchase-btn').click()
           cy.get('.select-credit-card-modal')
-          cy.get('.pull-right').click()
+          // the remainder is just to give the user premium so we can quickly move to the next state
+          cy.get('.extant-card').click()
+          cy.get('.button').click()
+          cy.get('.premium-confirmation')
+          cy.reload()
         })
       })
 
 
     })
+    describe('when I have a teacher premium', () => {
+      // keep this below the trial so that we can build our way through the various states
+
+      before(() => {
+        beforeEach(() => {
+          Cypress.Cookies.preserveOnce('_quill_session')
+        })
+        cy.visit('/premium')
+      })
+
+      activePremiumSubscriptionBehavior()
+
+    })
+
+    describe('when I have school premium', () => {
+
+      before(() => {
+        cy.exec('RAILS_ENV=cypress rake find_or_create_cypress_test_data:find_or_create_teacher_with_school_premium', {
+          failOnNonZeroExit: false
+        })
+        cy.login('teacher', 'password')
+        cy.visit('/premium')
+      })
+
+      activePremiumSubscriptionBehavior()
+
+    })
 
 
-    // it('has an option to start premium', ()=>{
-    //
-    // })
-    //
-    // it('has an option to start a trial', ()=>{
-    //
-    // })
-    //
-    // it('has an option to start a trial', ()=>{
-    //
-    // })
 
 
   })
