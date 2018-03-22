942f22a7f2a4f0f79ea07e27a414c3a703f49e88
RyanNovas <ryan@quill.org>
2018-03-22 13:40:42 -0400

    better credit card test

--- cypress/integration/credit_card_modal.js
+++ cypress/integration/credit_card_modal.js
@@ -1,46 +1,49 @@
-describe('Credit Card Modal', () => {
+describe.only('Credit Card Modal', () => {
 
-  
+
   describe('when I open the modal without a credit card associated with my account', ()=>{
     before(() =>{
-      cy.exec('RAILS_ENV=cypress rake find_or_create_cypress_test_data:find_or_create_teacher_with_expired_teacher_premium', {failOnNonZeroExit: false})
+      cy.exec('RAILS_ENV=cypress rake find_or_create_cypress_test_data:find_or_create_teacher', {failOnNonZeroExit: false})
       cy.login('teacher', 'password')
-      // beforeEach(() => {
-      //   Cypress.Cookies.preserveOnce("_quill_session")
-      // })
-      cy.visit('/subscriptions')
-      cy.get(':nth-child(1) > .renew-subscription').click()
+      cy.visit('/premium')
+      cy.get('#purchase-btn').click()
     })
-    
+
     it('loads', ()=>{
       cy.get('.select-credit-card-modal')
     })
-    
+
     describe('the option to enter a credit card', ()=> {
       it('opens the stripe modal when clicked', ()=>{
-        cy.get('.enter-credit-card').click()
+        cy.get('#purchase-btn').click()
       })
     })
-    
+
   })
-  
+
   describe.only('when I open the modal with a credit card associated with my account', ()=>{
     before(()=>{
-      cy.exec('RAILS_ENV=cypress rake find_or_create_cypress_test_data:find_or_create_teacher_with_subscription_they_purchased_and_stripe_id', {failOnNonZeroExit: false})
+      cy.exec('RAILS_ENV=cypress rake find_or_create_cypress_test_data:find_or_create_teacher_with_stripe_id', {failOnNonZeroExit: false})
       cy.login('teacher', 'password')
-      // beforeEach(() => {
-      //   Cypress.Cookies.preserveOnce("_quill_session")
-      // })
-      cy.visit('/subscriptions')
-      cy.get(':nth-child(1) > .renew-subscription').click()
+      beforeEach(() => {
+        Cypress.Cookies.preserveOnce('_quill_session')
+      })
+      cy.visit('/premium')
+      cy.get('#purchase-btn').click()
     })
-    
-    it('shows my associated credit card', ()=>{
-      cy.get('.enter-credit-card')
+
+    it('and click my associated credit card it gives me the option to buy now', ()=>{
+      cy.get('.extant-card').click()
+      cy.get('.button').contains('Buy Now')
+    })
+
+    it('gives me a premium confirmation if I click buy now', ()=>{
+      cy.get('.button').click()
+      cy.get('.premium-confirmation')
     })
   })
-  
-  
+
+
 
 
 
