034e983e3f87b96d107e928dc27d273b4c570692
RyanNovas <ryan@quill.org>
2018-03-22 11:11:57 -0400

    premium page tests wip

--- cypress/integration/premium_page.js
+++ cypress/integration/premium_page.js
@@ -2,18 +2,18 @@ describe('Premium Page', () => {
 
   describe('when I have never had a subscription', ()=>{
     before(() =>{
-      // cy.exec('RAILS_ENV=cypress rake find_or_create_cypress_test_data:find_or_create_teacher', {failOnNonZeroExit: false})
-      // cy.login('teacher', 'password')
-      beforeEach(() => {
-        Cypress.Cookies.preserveOnce("_quill_session")
-      })
+      cy.exec('RAILS_ENV=cypress rake find_or_create_cypress_test_data:find_or_create_teacher', {failOnNonZeroExit: false})
+      cy.login('teacher', 'password')
+      // beforeEach(() => {
+      //   Cypress.Cookies.preserveOnce("_quill_session")
+      // })
       cy.visit('/premium')
     })
-    
+
     it('loads', ()=>{
       cy.get('#premium-pricing-guide')
     })
-    
+
     describe('teacher premium', ()=>{
       describe('buy now button', ()=>{
         it.skip('gives the user premium when clicked', ()=>{
@@ -21,36 +21,36 @@ describe('Premium Page', () => {
           // .click()
         })
       })
-      
+
       it('has a Free Trial button', ()=>{
         cy.get('.empty-blue')
       })
     })
-    
+
     describe('school premium', ()=>{
       it('has a purchase button that opens a modal when clicked',()=>{
-        cy.get('.btn-purple')
-          .click().get('.school-premium-modal') 
+        cy.get('.btn.purple')
+        .click().get('.school-premium-modal')
       })
     })
-    
-    
+
+
     it('has an option to start premium', ()=>{
-      
+
     })
-    
+
     it('has an option to start a trial', ()=>{
-      
+
     })
-    
+
     it('has an option to start a trial', ()=>{
-      
+
     })
-  
-    
+
+
   })
-  
-  
+
+
 
 
 
