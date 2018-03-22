d24fa2afe9bbdf222fd9b98b0072de90ca75ba78
Emilia Friedberg <emiliafriedberg@gmail.com>
2018-03-22 11:55:07 -0400

    pull premium out of scorebook tabs

--- app/helpers/navigation_helper.rb
+++ app/helpers/navigation_helper.rb
@@ -30,27 +30,6 @@ module NavigationHelper
     action_name == 'admin_dashboard'
   end
 
-  def premium_page_should_be_active?
-
-  end
-
-  def premium_page_should_display?
-    !current_user.is_premium? || current_user.premium_state == 'trial'
-  end
-
-  def premium_tab_copy
-    case current_user.premium_state
-    when 'trial'
-      "Premium  <i class='fa fa-star'></i> #{current_user.trial_days_remaining} Days Left"
-    when 'locked'
-      "Premium  <i class='fa fa-star'></i> Trial Expired"
-    when nil
-      "Try Premium <i class='fa fa-star'></i>"
-    when 'none'
-      "Try Premium <i class='fa fa-star'></i>"
-    end
-  end
-
   # NOTE: subnavs for other pages are handled on the front end with React.
   def should_render_subnav?
     home_page_should_be_active? || classes_page_should_be_active? || student_reports_page_should_be_active? || teacher_resources_page_should_be_active?
