c47f7e371710e6f19f1140ca170f25b193ac3e18
Emilia Friedberg <emiliafriedberg@gmail.com>
2018-03-26 15:56:24 -0400

    rebuild ela standards page

--- client/app/bundles/HelloWorld/startup/PressAppClient.jsx
+++ client/app/bundles/HelloWorld/startup/PressAppClient.jsx
@@ -3,17 +3,17 @@ import PreviewCard from '../components/shared/preview_card.jsx';
 import HeaderSection from '../components/blog_posts/header_section'
 
 export default (props) => {
-  const content = props.blogPosts.length > 0 ? props.blogPosts.map(article =>
+  const articles = props.blogPosts.length > 0 ? props.blogPosts.map(article =>
     <PreviewCard content={article.preview_card_content} link={`/teacher_resources/${article.slug}`} />
-  ) : <div style={{fontSize: '30px', display: 'flex', justifyContent: 'center', height: '60vh', alignItems: 'center', flexDirection: 'column', fontWeight: 'bold'}}>
+  ) : null
+  const content = articles ? <div id="preview-card-container">{articles}</div>
+  : <div style={{fontSize: '30px', display: 'flex', justifyContent: 'center', height: '60vh', alignItems: 'center', flexDirection: 'column', fontWeight: 'bold'}}>
         Coming Soon!
         <img style={{marginTop: '20px'}} src="https://assets.quill.org/images/illustrations/empty-state-premium-reports.svg"/>
       </div>
     return <div className="press-page">
       <HeaderSection title="In the Press" subtitle="Read articles that feature Quill"/>
-      <div id="preview-card-container">
-        {content}
-      </div>
+      {content}
     </div>
 
 }
