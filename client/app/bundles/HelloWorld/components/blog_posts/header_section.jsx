0c7bf20814e4f387614f83586a0d116bd756c569
Emilia Friedberg <emiliafriedberg@gmail.com>
2018-03-28 15:57:42 -0400

    finish internal footer

--- client/app/bundles/HelloWorld/components/blog_posts/header_section.jsx
+++ client/app/bundles/HelloWorld/components/blog_posts/header_section.jsx
@@ -7,7 +7,7 @@ export default (props) =>
     <h1>{props.title}</h1>
     <p>{props.subtitle}</p>
   </div>
-  <form className='width-422' action={`${process.env.DEFAULT_URL}/teacher_resources/search`}>
+  <form action={`${process.env.DEFAULT_URL}/teacher_resources/search`}>
     <input type='text' placeholder='Search for posts' name='query' defaultValue={props.query ? props.query : null}/>
     <i className="fa fa-icon fa-search"/>
   </form>
