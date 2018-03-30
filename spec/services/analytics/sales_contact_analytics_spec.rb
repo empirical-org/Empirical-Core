d966f96e2e4bd2fef2d638c9e2d74ad58d255d52
tronroy <tbconroy@gmail.com>
2018-03-30 11:54:04 -0400

    Fix failing specs

--- spec/services/analytics/sales_contact_analytics_spec.rb
+++ spec/services/analytics/sales_contact_analytics_spec.rb
@@ -2,16 +2,17 @@ require 'rails_helper'
 
 describe 'SalesContactAnalytics' do
   it 'sends data to the client' do
-    client = double('segment_client')
+    client = double('client')
     user   = create(:user, role: 'teacher')
     school = create(:school)
     school.users << user
 
-    expect(client).to receive(:track).with(
-      user_id: user.id,
-      event: 'basic_subscription',
-      properties: {
-        account_uid: school.id,
+    allow(client).to receive(:event) { double('instance', 'success?' => true) }
+    expect(client).to receive(:event).with(
+      contact_uid: user.id.to_s,
+      event_uid: 'basic_subscription',
+      params: {
+        account_uid: school.id.to_s,
         display_name: 'Basic Subscription',
       }
     )
@@ -20,11 +21,12 @@ describe 'SalesContactAnalytics' do
   end
 
   it 'only sends data for valid stages' do
-    client = double('segment_client')
+    client = double('client')
     user   = create(:user, role: 'teacher')
     school = create(:school)
     school.users << user
 
+    allow(client).to receive(:event) { double('instance', 'success?' => true) }
     expect(client).not_to receive(:track)
 
     result = SalesContactAnalytics.new(user.id, 'dont_fake_the_funk', client)
