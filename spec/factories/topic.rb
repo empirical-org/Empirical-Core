68e16901e8929e4fe524b0fd1b20c3c7408c9d40
Emilia Friedberg <emiliafriedberg@gmail.com>
2018-04-05 11:00:41 -0400

    make it harder for factories to create duplicate names

--- spec/factories/topic.rb
+++ spec/factories/topic.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :topic do
-    sequence(:name) { Faker::Lorem.words(3).join(' ') }
+    sequence(:name) { |n| "#{Faker::Lorem.words(3).join(' ')} #{n}" }
     section         { create(:section) }
     topic_category  { create(:topic_category) }
   end
