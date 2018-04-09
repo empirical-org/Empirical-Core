1927d22674e486d472ebe8d11315f852f5763c0d
Donald McKendrick <ddmckendrick@gmail.com>
2018-04-09 17:09:45 -0400

    Add support for Spacy to module

--- dist/src/libs/matchers/machine_learning_sentence_match.d.ts
+++ dist/src/libs/matchers/machine_learning_sentence_match.d.ts
@@ -1,4 +1,4 @@
 import { Response, PartialResponse } from '../../interfaces';
-export declare function machineLearningSentenceMatch(response: string, link: string): Promise<boolean>;
+export declare function machineLearningSentenceMatch(response: string, link: string): Promise<any>;
 export declare function machineLearningSentenceChecker(responseString: string, responses: Array<Response>, link: string, matcherFunction?: Function): Promise<PartialResponse | undefined>;
-export declare function machineLearningSentenceResponseBuilder(responses: Array<Response>, matched: Boolean): PartialResponse;
+export declare function machineLearningSentenceResponseBuilder(responses: Array<Response>, match: any): PartialResponse;
