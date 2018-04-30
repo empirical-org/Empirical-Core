5d38f5d7d0512e5e37a2bcbe2a5c7637d936a512
Donald McKendrick <ddmckendrick@gmail.com>
2018-04-29 21:03:42 -0400

    Update student assignments api endpoint for mobile

--- app/queries/profile/mobile/activity_sessions_by_unit.rb
+++ app/queries/profile/mobile/activity_sessions_by_unit.rb
@@ -1,14 +1,6 @@
 class Profile::Mobile::ActivitySessionsByUnit
   def query(student, classroom_id)
-    all_act_seshes = student.activity_sessions
-           .joins(:classroom_activity)
-           .where("classroom_activities.classroom_id = ?", classroom_id)
-           .where("((state = 'finished') and (is_final_score = true)) or ((state != 'finished') and (is_retry = false))")
-           .includes(classroom_activity: [:unit], activity: [:classification])
-           .references(classroom_activity: [:unit])
-           .order("units.created_at DESC")
-           .order("classroom_activities.due_date")
-           .order("classroom_activities.created_at")
+    all_act_seshes = student_profile_data_sql(classroom_id, student.id)
     grouped_sessions = group_by_unit(all_act_seshes)
 
     prepare_json(grouped_sessions)
@@ -17,17 +9,50 @@ class Profile::Mobile::ActivitySessionsByUnit
 
   private
 
+  def student_profile_data_sql(classroom_id=nil, student_id=nil)
+    ActiveRecord::Base.connection.execute(
+      "SELECT unit.name AS unit_name,
+       activity.name,
+       activity.description,
+       activity.repeatable,
+       activity.activity_classification_id,
+       unit.id AS unit_id,
+       unit.created_at AS unit_created_at,
+       ca.id AS ca_id,
+       ca.completed AS marked_complete,
+       ca.activity_id,
+       MAX(acts.updated_at) AS act_sesh_updated_at,
+       ca.due_date,
+       ca.created_at AS classroom_activity_created_at,
+       ca.locked,
+       ca.pinned,
+       MAX(acts.percentage) AS max_percentage,
+       SUM(CASE WHEN acts.state = 'started' THEN 1 ELSE 0 END) AS resume_link
+    FROM classroom_activities AS ca
+    LEFT JOIN activity_sessions AS acts ON ca.id = acts.classroom_activity_id AND acts.visible = true AND acts.user_id = #{student_id}
+    JOIN units AS unit ON unit.id = ca.unit_id
+    JOIN activities AS activity ON activity.id = ca.activity_id
+    WHERE #{student_id} = ANY (ca.assigned_student_ids::int[])
+    AND ca.classroom_id = #{classroom_id}
+    AND ca.visible = true
+    AND unit.visible = true
+    GROUP BY ca.id, activity.name, activity.description, acts.activity_id,
+            unit.name, unit.id, unit.created_at, unit_name, activity.repeatable,
+            activity.activity_classification_id, activity.repeatable
+    ORDER BY pinned DESC, locked ASC, max_percentage DESC, unit.created_at ASC, ca.created_at ASC").to_a
+  end
+
   def prepare_json(grouped_sessions)
     # update grouped sessions so it only has an array of
     # the pertinent details for each act sesh
     grouped_sessions.each do |unit_name, activity_sessions|
       updated_act_seshes = activity_sessions.map do |activity_session|
         {
-          uid: activity_session.uid,
-          name: activity_session.activity.name,
-          percentage: activity_session.percentage,
-          classroom_activity_id: activity_session.classroom_activity_id,
-          activity_classification_id: activity_session.activity.activity_classification_id
+          uid: activity_session["uid"],
+          name: activity_session["name"],
+          percentage: activity_session["max_percentage"],
+          classroom_activity_id: activity_session["ca_id"],
+          activity_classification_id: activity_session["activity_classification_id"]
         }
       end
       grouped_sessions[unit_name] = updated_act_seshes
@@ -35,8 +60,7 @@ class Profile::Mobile::ActivitySessionsByUnit
   end
 
   def group_by_unit(all)
-    all.select{|s| s.classroom_activity.unit.present?}
-       .group_by{|s| s.classroom_activity.unit.name}
+    all.select{|s| s["unit_name"].present?}.group_by{|s| s["unit_name"]}
   end
 
 
