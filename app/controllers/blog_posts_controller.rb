43de1b895516b75618687c242848df66df9d1458
Emilia Friedberg <emiliafriedberg@gmail.com>
2018-05-02 11:25:21 -0400

    add subnav of blog post categories to show page for posts

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -10,6 +10,7 @@ class BlogPostsController < ApplicationController
     find_by_hash = { slug: params[:slug] }
     find_by_hash[:draft] = false unless current_user&.role == 'staff'
     @blog_post = BlogPost.find_by!(find_by_hash)
+    @topic = @blog_post.topic
     @display_paywall = true unless @blog_post.can_be_accessed_by(current_user)
     @blog_post.increment_read_count
     @author = @blog_post.author
