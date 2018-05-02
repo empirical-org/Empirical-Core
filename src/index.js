a6adfd185285d1a354790ae9b4a59f101aedbd86
Tom Conroy <tom@>
2018-05-02 12:21:22 -0400

    Status code should be string when handling request

--- src/index.js
+++ src/index.js
@@ -78,11 +78,11 @@ function handler (req, res) {
   fs.readFile('../' + __dirname + '/index.html',
   function (err, data) {
     if (err) {
-      res.writeHead(500);
-      return res.end(err)
+      res.writeHead('500');
+      return res.end('Error loading index.html')
     }
 
-    res.writeHead(200);
+    res.writeHead('200');
     res.end(data);
   });
 }
