723a9bb3a6201f32a844495fc4011da182ac0344
Tom Conroy <tom@>
2018-05-03 17:50:55 -0400

    Configure elastic beanstalk scripts to start ssh tunnel to db

--- src/rethinkdbConfig.js
+++ src/rethinkdbConfig.js
@@ -9,21 +9,12 @@ const rethinkdbConfig = (() => {
     db:   'quill_lessons'
   }
 
-  if (process.env.RETHINKDB_AUTH_KEY) {
-    config['authKey'] = process.env.RETHINKDB_AUTH_KEY
-  }
-
   if (process.env.RETHINKDB_USER) {
     config['user'] = process.env.RETHINKDB_USER
   }
 
   if (process.env.RETHINKDB_PASSWORD) {
     config['password'] = process.env.RETHINKDB_PASSWORD
-  }
-
-  if (process.env.RETHINKDB_SSL_CERT === 'true') {
-    const cert = fs.readFileSync(path.resolve(__dirname, '../cert'))
-    config['ssl'] = { ca: cert }
   }
 
   return config
