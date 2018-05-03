c49d171af5ead7b264bd8534555166e04c6df297
RyanNovas <ryan@quill.org>
2018-05-03 10:21:52 -0400

    update teacher time zone

--- app/models/concerns/teacher.rb
+++ app/models/concerns/teacher.rb
@@ -265,6 +265,7 @@ module Teacher
       if self.update_attributes(username: params[:username] || self.username,
                                         email: params[:email] || self.email,
                                         name: params[:name] || self.name,
+                                        time_zone: params[:time_zone] || self.time_zone,
                                         password: params[:password] || self.password,
                                         role: params[:role] || self.role)
         are_there_non_school_related_errors = false
