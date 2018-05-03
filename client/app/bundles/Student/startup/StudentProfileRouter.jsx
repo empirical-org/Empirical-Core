4752608d3a1297b317dbcf87f3e31c92b815652d
Emilia Friedberg <emiliafriedberg@gmail.com>
2018-05-03 11:13:54 -0400

    add classroom is now a react router route

--- client/app/bundles/Student/startup/StudentProfileRouter.jsx
+++ client/app/bundles/Student/startup/StudentProfileRouter.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import { Router, Route, browserHistory } from 'react-router'
 import StudentProfile from './StudentProfileAppClient'
-// import JoinClass from './JoinClassAppClient'
+import JoinClass from './JoinClassAppClient'
 // import AccountSettings from './AccountSettingsAppClient'
 
 const StudentProfileRouter = props =>
   <Router Router history={browserHistory}>
     <Route path="profile" component={StudentProfile} />
     <Route path="classrooms/:classroomId" component={StudentProfile} />
-    {/* <Route path="students_classrooms/add_classroom" component={JoinClass} />
-    <Route path="account_settings" component={AccountSettings} /> */}
+    <Route path="add_classroom" component={JoinClass} />
+    {/* <Route path="account_settings" component={AccountSettings} /> */}
   </Router>
 
 export default StudentProfileRouter
