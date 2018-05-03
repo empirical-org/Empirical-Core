490bddb9f7f55334fee4a909d8835b2d82cd1981
Tom Conroy <tom@>
2018-05-03 11:43:17 -0400

    Connect to rethinkdb using ssl

--- src/rethinkdbConfig.js
+++ src/rethinkdbConfig.js
@@ -1,8 +1,13 @@
 const rethinkdbConfig = (() => {
+  const cert = fs.readFileSync(path.resolve(__dirname, '../cert'))
   let config = {
     host: process.env.RETHINKDB_HOST || 'localhost',
     port: process.env.RETHINKDB_PORT || 28015,
     db:   'quill_lessons'
+  }
+
+  if (process.env.RETHINKDB_AUTH_KEY) {
+    config['authKey'] = process.env.RETHINKDB_AUTH_KEY
   }
 
   if (process.env.RETHINKDB_USER) {
@@ -11,6 +16,10 @@ const rethinkdbConfig = (() => {
 
   if (process.env.RETHINKDB_PASSWORD) {
     config['password'] = process.env.RETHINKDB_PASSWORD
+  }
+
+  if (cert) {
+    config['ssl'] = { ca: cert }
   }
 
   return config
