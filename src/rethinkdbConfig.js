56174104ae89612cdbc9ec4943af5b44b34eafb1
Tom Conroy <tom@>
2018-05-04 12:25:53 -0400

    Connect to rethink with proxy url instead of ssh tunnel

--- src/rethinkdbConfig.js
+++ src/rethinkdbConfig.js
@@ -1,20 +1,27 @@
 import fs from 'fs'
 import path from 'path'
 
-
 const rethinkdbConfig = (() => {
+  const pathToCert = path.resolve(__dirname + '/..') + '/public-key.cert'
+
   let config = {
     host: process.env.RETHINKDB_HOST || 'localhost',
     port: process.env.RETHINKDB_PORT || 28015,
-    db:   'quill_lessons'
+    db: 'quill_lessons'
   }
 
-  if (process.env.RETHINKDB_USER) {
-    config['user'] = process.env.RETHINKDB_USER
+  if (process.env.RETHINKDB_AUTH_KEY) {
+    config['authKey'] = process.env.RETHINKDB_AUTH_KEY
   }
 
-  if (process.env.RETHINKDB_PASSWORD) {
-    config['password'] = process.env.RETHINKDB_PASSWORD
+  if (process.env.RETHINKDB_USE_SSL === 'true') {
+    fs.readFile(pathToCert, (err, caCert) => {
+      if (err) {
+        console.log(err)
+      } else {
+        config['ssl'] = { ca: caCert }
+      }
+    })
   }
 
   return config
