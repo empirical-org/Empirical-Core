66d39d4a0d5dcf29c3dfe340bdbd11cbacee813a
RyanNovas <ryan@quill.org>
2018-05-08 16:40:27 -0400

    style changes

--- client/app/bundles/HelloWorld/containers/GoogleMismatch.jsx
+++ client/app/bundles/HelloWorld/containers/GoogleMismatch.jsx
@@ -12,12 +12,12 @@ export default class extends React.Component{
     const currentGoogleWarning = this.props.googleEmail ? <p>You are currently signed into Google as <span>{this.props.googleEmail}</span>.</p> : null
     return(
       <div className='google-mismatch-container'>
-        <h2 className="q-h2"><i className="fa fa-exclamation-triangle"/>Your Quill and Google Classroom emails do not match.</h2>
+        <h2 className="q-h2"><i className="fa fa-warning"/>Your Quill and Google Classroom emails do not match.</h2>
         {currentGoogleWarning}
-        <p>Your email account on Quill.org is <span>{this.props.email}</span>.</p>
+        <p>Your email account on Quill.org is <strong>{this.props.email}</strong>.</p>
         <p>Your Google Classroom email must be the same as your Quill email. If you want to use a different Google Classroom account, please <a href="https://classroom.google.com/">sign in</a> to that Google account first and then come back to Quill.</p>
         <UpdateEmail />
-        <p>Make sure to press the <span>update email button</span> before you press next.</p>
+        <p>Make sure to press the <strong>update email button</strong> before you press next.</p>
         <a className="q-button cta-button bg-quillgreen text-white sync-button" href='/auth/google_oauth2'>
           Next: Sync With Google Classrooms
         </a>
