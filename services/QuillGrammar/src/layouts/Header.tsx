4587395f416f269f525b09cc0584f8a21d1b17aa
Emilia Friedberg <emiliafriedberg@gmail.com>
2018-05-17 14:19:32 -0400

    reorganize directory

---
+++ services/QuillGrammar/src/layouts/Header.tsx
@@ -0,0 +1,19 @@
+import * as React from "react";
+import { Layout, Row } from "antd";
+import { Link } from "react-router-dom";
+import './Header.scss'
+
+export const Header: React.StatelessComponent<{}> = () => {
+    return (
+        <Layout.Header style={{
+          height: '60px',
+          width: "100%",
+          backgroundColor: "#00c2a2",
+          padding: "0 30px"}}>
+          <Row type="flex" align="middle" justify="space-between" style={{height: '100%'}}>
+            <img src="https://d2t498vi8pate3.cloudfront.net/assets/home-header-logo-8d37f4195730352f0055d39f7e88df602e2d67bdab1000ac5886c5a492400c9d.png" />
+            <a href="http://quill.org/profile">Save & Exit</a>
+          </Row>
+        </Layout.Header>
+    );
+};
--- services/QuillGrammar/src/layouts/PageLayout.tsx
+++ services/QuillGrammar/src/layouts/PageLayout.tsx
@@ -1,16 +1,13 @@
 import * as React from "react";
 import { Layout } from "antd";
-import Sidebar from "./Sidebar";
 import { Header } from "./Header";
 import { Redirect } from "react-router-dom";
 import {renderRoutes} from "react-router-config";
 import { routes } from "../routes";
-import "./PageLayout.less";
 
 const PageLayout: React.StatelessComponent<{}> = () => {
     return (
-        <Layout className="ant-layout-has-sider">
-            <Sidebar />
+        <Layout className="ant-layout">
             <Layout>
                 <Layout.Content>
                     <Header />
