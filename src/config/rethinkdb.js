8ce65492115eb3592e6c87fc86fa7182927cb285
Tom Conroy <tom@>
2018-05-21 16:10:02 -0400

    Revert rethinkdb config file commit

--- src/config/rethinkdb.js
+++ src/config/rethinkdb.js
@@ -1,7 +1,12 @@
 import dotenv from 'dotenv'
+import fs from 'fs'
+import path from 'path'
+
 dotenv.config()
 
 const rethinkdbConfig = (() => {
+  const pathToCert = path.resolve(__dirname + '/..') + '/public-key.crt'
+
   let config = {
     host: process.env.RETHINKDB_HOST,
     port: process.env.RETHINKDB_PORT,
@@ -12,8 +17,9 @@ const rethinkdbConfig = (() => {
     config['authKey'] = process.env.RETHINKDB_AUTH_KEY
   }
 
-  if (process.env.RETHINKDB_SSL_CERT) {
-    config['ssl'] = { ca: Buffer(process.env.RETHINKDB_SSL_CERT) }
+  if (process.env.RETHINKDB_USE_SSL === 'true') {
+    const caCert = fs.readFileSync(pathToCert)
+    config['ssl'] = { ca: caCert }
   }
 
   return config
