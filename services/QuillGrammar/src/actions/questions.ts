d9b8afe1f563fa5a5cdda470c114639183d34518
Emilia Friedberg <emiliafriedberg@gmail.com>
2018-05-22 12:18:56 -0400

    Revert "add tests for grammar checker"
    This reverts commit 4733c5fdc1315188e207a21f6fd74be2184e0357.

--- services/QuillGrammar/src/actions/questions.ts
+++ services/QuillGrammar/src/actions/questions.ts
@@ -2,7 +2,6 @@ import rootRef from '../firebase';
 import { ActionTypes } from './actionTypes'
 const questionsRef = rootRef.child('questions')
 import { Question } from '../interfaces/questions'
-import { checkGrammarQuestion } from 'quill-marking-logic'
 
 export const startListeningToQuestions = (conceptUIDs: Array[string]) => {
   return function(dispatch) {
@@ -27,31 +26,13 @@ export const startListeningToQuestions = (conceptUIDs: Array[string]) => {
   }
 }
 
-export const checkAnswer = (response:string, question:Question) => {
-  return function(dispatch) {
-    const questionUID: string = question.uid
-    const formattedAnswers: Array<any> = question.answers.map(a => {
-      return {
-        optimal: true,
-        count: 1,
-        text: a,
-        question_uid: questionUID,
-        feedback: "<b>Well done!</b> That's the correct answer."
-      }
-    })
-    const responseObj = checkGrammarQuestion(questionUID, response, formattedAnswers)
-    console.log(responseObj)
-    dispatch(submitResponse(responseObj))
-  }
-}
-
 export const goToNextQuestion = () => {
   return function(dispatch) {
     dispatch({ type: ActionTypes.GO_T0_NEXT_QUESTION })
   }
 }
 
-export const submitResponse = (response) => {
+export const submitResponse = (response: string) => {
   return function(dispatch) {
     dispatch({ type: ActionTypes.SUBMIT_RESPONSE, response })
   }
