9864bfaadeafddef274428073aa62fa8d5f08d47
RyanNovas <ryan@quill.org>
2018-05-23 16:20:17 -0400

    posts back link and description to announcements

--- app/services/google_integration/announcements.rb
+++ app/services/google_integration/announcements.rb
@@ -9,11 +9,21 @@ module GoogleIntegration::Announcements
     # if you change this call and are struggling with the body,
     # make sure you try out the API call with https://developers.google.com/classroom/reference/rest/v1/courses.announcements/create
     # to validate that you are using the right one
+    name = classroom_activity.activity.name
     api_call = client.execute(api_method: service.courses.announcements.create,
       parameters: {
         courseId: google_course_id,
       },
-      body: JSON.dump({:text => 'sample description'}),
+      body: JSON.dump({
+        text: name,
+        materials: [{
+          link: {
+            url: "#{ENV['DEFAULT_URL']}/teachers/classroom_activities/#{classroom_activity.id}/activity_from_classroom_activity",
+            title: name,
+            thumbnailUrl: "wtf"
+          }
+        }]
+      }),
       headers: {"Content-Type": 'application/json'}
     )
     api_call.body
