cda2b0ea97d28c72f030a7705dfdca5b847b46af
Emilia Friedberg <emiliafriedberg@gmail.com>
2018-05-24 11:00:01 -0400

    randomize and select correct number of questions for each concept

--- services/QuillGrammar/src/actions/questions.ts
+++ services/QuillGrammar/src/actions/questions.ts
@@ -3,22 +3,39 @@ import { ActionTypes } from './actionTypes'
 const questionsRef = rootRef.child('questions')
 import { Question } from '../interfaces/questions'
 import { checkGrammarQuestion, Response } from 'quill-marking-logic'
+import { shuffle } from '../helpers/shuffle'
+import _ from 'lodash'
 
-export const startListeningToQuestions = (conceptUIDs: Array[string]) => {
+// typescript this
+export const startListeningToQuestions = (concepts: any) => {
   return function(dispatch) {
 
+    const conceptUIDs = Object.keys(concepts)
     questionsRef.orderByChild('concept_uid').on('value', (snapshot) => {
       const questions = snapshot.val()
-      const questionsForConcepts:Array<Question> = []
+      const questionsForConcepts = {}
       Object.keys(questions).map(q => {
         if (conceptUIDs.includes(questions[q].concept_uid)) {
           const question = questions[q]
           question.uid = q
-          questionsForConcepts.push(question)
+          if (questionsForConcepts.hasOwnProperty(question.concept_uid)) {
+            // console.log(questionsForConcepts[question.concept_uid])
+            questionsForConcepts[question.concept_uid] = questionsForConcepts[question.concept_uid].concat(question)
+          } else {
+            questionsForConcepts[question.concept_uid] = [question]
+          }
         }
       })
-      if (questionsForConcepts.length > 0) {
-        dispatch({ type: ActionTypes.RECEIVE_QUESTION_DATA, data: questionsForConcepts, });
+
+      const arrayOfQuestions = []
+      Object.keys(questionsForConcepts).forEach(conceptUID => {
+        const shuffledQuestionArray = shuffle(questionsForConcepts[conceptUID])
+        const numberOfQuestions = concepts[conceptUID].quantity
+        arrayOfQuestions.push(shuffledQuestionArray.slice(0, numberOfQuestions))
+      })
+      const flattenedArrayOfQuestions = _.flatten(arrayOfQuestions)
+      if (flattenedArrayOfQuestions.length > 0) {
+        dispatch({ type: ActionTypes.RECEIVE_QUESTION_DATA, data: flattenedArrayOfQuestions, });
       } else {
         dispatch({ type: ActionTypes.NO_QUESTIONS_FOUND})
       }
