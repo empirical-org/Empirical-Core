d92194e3211c863d45f3a40eee759acaefd39086
tronroy <tbconroy@gmail.com>
2018-05-29 18:13:59 -0400

    Ensure edition questions are loaded on customize page

--- app/components/customize/edition.tsx
+++ app/components/customize/edition.tsx
@@ -12,10 +12,11 @@ import {
 } from '../classroomLessons/interfaces'
 
 import {
+  getEditionMetadataForUserIds,
+  getEditionQuestions,
+  publishEdition,
   setWorkingEditionMetadata,
   setWorkingEditionQuestions,
-  publishEdition,
-  getEditionQuestions
 } from '../../actions/customize'
 
 import {
@@ -52,6 +53,7 @@ class CustomizeEdition extends React.Component<any, any> {
     if (classroomActivityId) {
       setEditionId(classroomActivityId, this.props.params.editionID)
     }
+    this.props.dispatch(getEditionMetadataForUserIds([], this.props.params.lessonID))
     this.props.dispatch(getEditionQuestions(this.props.params.editionID))
   }
 
