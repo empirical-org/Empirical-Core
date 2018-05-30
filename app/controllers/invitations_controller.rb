66b1c06d4a1afbb954e7adc9e01ce01666531808
gaurav <gauravrakheja002@gmail.com>
2018-05-30 21:32:16 +0530

    added specs for and refactored invitations controller

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -1,19 +1,15 @@
 class InvitationsController < ApplicationController
   before_action :verify_current_user_owns_classrooms, only: :create_coteacher_invitation
+  before_action :set_classroom_ids_and_inviteee_email, only: :create_coteacher_invitation
+
 
   def create_coteacher_invitation
     begin
-      @classroom_ids = params[:classroom_ids]
-      invitee_email = params[:invitee_email]
-
-      raise StandardError.new("Please make sure you've entered a valid email and selected at least one classroom.") if @classroom_ids.empty? || invitee_email.empty?
-      raise StandardError.new("Please make sure you've entered a valid email.") unless invitee_email =~ /.+@.+\..+/i
-      @pending_invite = Invitation.find_or_create_by(inviter_id: current_user.id, invitee_email: invitee_email, invitation_type: Invitation::TYPES[:coteacher], archived: false)
+      validate_email_and_classroom_ids
+      @pending_invite = find_or_create_coteacher_invite_from_current_user
       assign_classrooms_to_invitee
-      if Rails.env.production? || invitee_email.match('quill.org')
-        InvitationEmailWorker.perform_async(@pending_invite.id)
-      end
-      return render json: {invite_id: @pending_invite.id}
+      invoke_email_worker
+      return render json: { invite_id: @pending_invite.id }
     rescue => e
       return render json: { error: e.message }, status: 422
     end
@@ -30,7 +26,7 @@ class InvitationsController < ApplicationController
 
   def destroy_pending_invitations_from_specific_inviter
     begin
-      Invitation.find_by(invitation_type: params[:invitation_type], inviter_id: params[:inviter_id], invitee_email: current_user.email, archived: false).destroy
+      Invitation.find_by(invitatiogn_type: params[:invitation_type], inviter_id: params[:inviter_id], invitee_email: current_user.email, archived: false).destroy
       return render json: {}
     rescue => e
       return render json: { error: e.message }, status: 422
@@ -38,6 +34,39 @@ class InvitationsController < ApplicationController
   end
 
   private
+
+  def invoke_email_worker
+    if Rails.env.production? || @invitee_email.match('quill.org')
+      InvitationEmailWorker.perform_async(@pending_invite.id)
+    end
+  end
+
+  def find_or_create_coteacher_invite_from_current_user
+    Invitation.find_or_create_by(inviter_id: current_user.id, invitee_email: @invitee_email, invitation_type: Invitation::TYPES[:coteacher], archived: false)
+  end
+
+  def validate_email_and_classroom_ids
+    validate_empty_classroom_ids_or_email
+    validate_email_format
+  end
+
+  def validate_empty_classroom_ids_or_email
+    if @classroom_ids.empty? || @invitee_email.empty?
+      raise StandardError.new("Please make sure you've entered a valid email and selected at least one classroom.")
+    end
+  end
+
+  def validate_email_format
+    unless @invitee_email =~ /.+@.+\..+/i
+      raise StandardError.new("Please make sure you've entered a valid email.")
+    end
+  end
+
+  def set_classroom_ids_and_inviteee_email
+    @classroom_ids = params[:classroom_ids]
+    @invitee_email = params[:invitee_email]
+  end
+
 
   def verify_current_user_owns_classrooms
     multiple_classroom_owner?(params[:classroom_ids])
