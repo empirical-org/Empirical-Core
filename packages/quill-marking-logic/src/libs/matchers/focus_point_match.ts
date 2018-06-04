dcd34ef7f6abc36b5efbcad3c66cb4288450db38
Emilia Friedberg <emiliafriedberg@gmail.com>
2018-06-04 12:47:30 -0400

    Changed focus points to do regex matching (wip)

--- packages/quill-marking-logic/src/libs/matchers/focus_point_match.ts
+++ packages/quill-marking-logic/src/libs/matchers/focus_point_match.ts
@@ -4,6 +4,15 @@ import {Response, FocusPoint, PartialResponse} from '../../interfaces'
 import {conceptResultTemplate} from '../helpers/concept_result_template'
 
 export function focusPointMatch(responseString:string, focusPoints:Array<FocusPoint>):FocusPoint {
+  return _.find(focusPoints, (fp) => {
+    const options = fp.text.split('|||');
+    // if it matches any focus point in a group (read|||reading|||write)
+    const anyMatches = _.any(options, opt => responseString.indexOf(opt) !== -1);
+    return !anyMatches;
+  });
+}
+
+export function focusPointMatchNEW(responseString:string, focusPoints:Array<FocusPoint>):FocusPoint {
   return _.find(focusPoints, (focusPoint) => {
     const options = focusPoint.text.split('|||');
     const anyMatches = _.any(options, opt => new RegExp(opt).test(responseString));
