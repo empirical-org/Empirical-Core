88b9071df9dfee3086ea76b2fb2113fc3512fe4e
tronroy <tbconroy@gmail.com>
2018-06-05 16:20:36 -0400

    Refactor nested if statements, create credentials service object

--- services/QuillLMS/app/controllers/auth/google_controller.rb
+++ services/QuillLMS/app/controllers/auth/google_controller.rb
@@ -3,19 +3,24 @@ class Auth::GoogleController < ApplicationController
   def google
     @access_token = request.env['omniauth.auth']['credentials']['token']
     name, email, google_id = GoogleIntegration::Profile.fetch_name_email_and_google_id(@access_token)
-    if redirect_request(request)
+    if redirect_request?(request)
       # If we are here it is simply to get a new access token. Ultimately, we should
       # set this up for refresh tokens at which point, this will no longer be necessary.
-       find_or_initialize_and_update_auth_credential
+       GoogleIntegration::AuthCredentials.new(request, current_user)
+        .create_or_update
        redirect_to URI(request.referer).path and return
-    elsif session[:google_redirect]
+    end
+
+    if session[:google_redirect]
       # todo: we should be using this instead of the redirect request above. Then, make an afterhook that will delete
       # the google_redirect
-      find_or_initialize_and_update_auth_credential
+      GoogleIntegration::AuthCredentials.new(request, current_user)
+        .create_or_update
       redirect_route = session[:google_redirect]
       session[:google_redirect] = nil
       redirect_to redirect_route and return
     end
+
     if (session[:role].present? && User.where(google_id: google_id).none?) || (current_user && !current_user.signed_up_with_google)
       # If the above is true, the user is either currently signing up and has session[:role] or
       # the user is extant and is about to register with google for the first time
@@ -33,15 +38,7 @@ class Auth::GoogleController < ApplicationController
 
   private
 
-  def set_refresh_token
-    @refresh_token ||= request.env['omniauth.auth']['credentials']['refresh_token']
-  end
-
-  def set_expires_at
-    @expires_at ||= DateTime.strptime(request.env['omniauth.auth']['credentials']['expires_at'].to_s, '%s')
-  end
-
-  def redirect_request(request)
+  def redirect_request?(request)
     request.referer &&
     URI(request.referer).path &&
     URI(request.referer).host != "accounts.google.com" &&
@@ -53,7 +50,8 @@ class Auth::GoogleController < ApplicationController
     if user.present?
       user.google_id ? nil : user.update(google_id: google_id)
       sign_in(user)
-      find_or_initialize_and_update_auth_credential
+      GoogleIntegration::AuthCredentials.new(request, current_user)
+        .create_or_update
       TestForEarnedCheckboxesWorker.perform_async(user.id)
       GoogleStudentImporterWorker.perform_async(current_user.id, access_token)
        redirect_to profile_path and return
@@ -67,7 +65,8 @@ class Auth::GoogleController < ApplicationController
     @user.attributes = {signed_up_with_google: true, name: name, role: role, google_id: google_id, refresh_token: @refresh_token}
     if @user.save
       sign_in(@user)
-      find_or_initialize_and_update_auth_credential
+      GoogleIntegration::AuthCredentials.new(request, current_user)
+        .create_or_update
       ip = request.remote_ip
       AccountCreationCallbacks.new(@user, ip).trigger
       @user.subscribe_to_newsletter
@@ -84,42 +83,39 @@ class Auth::GoogleController < ApplicationController
 
   def register_with_google(name, email, role, access_token, google_id)
     email = email.downcase
+
     if current_user && current_user.email.downcase != email
       session[:google_email] = email
-       redirect_to "/auth/google_email_mismatch/" and return
-    else
-      @user = User.find_or_initialize_by(email: email)
-      if @user.new_record?
-        if !new_google_user(name, email, role, access_token, google_id, @user)
-           redirect_to new_account_path and return
-        end
-        if @user.role == 'teacher'
-           render 'accounts/new' and return
-        else
-          GoogleIntegration::Classroom::Main.join_existing_google_classrooms(@user, access_token)
-        end
-      end
-      if @user.errors.any?
-         redirect_to new_account_path and return
-      else
-        @user.update(signed_up_with_google: true, google_id: google_id)
-        find_or_initialize_and_update_auth_credential
-        if request.referer && URI(request.referer) &&
-          (URI(request.referer).path == '/teachers/classrooms/dashboard' || URI(request.referer).path == '/teachers/classrooms/new')
-          # if they are hitting this route through the dashboard or new classrooms page, they should be brought to the google sync page
-           redirect_to '/teachers/classrooms/google_sync' and return
-        end
-         redirect_to profile_path and return
-      end
+      redirect_to "/auth/google_email_mismatch/" and return
     end
-  end
 
-  def find_or_initialize_and_update_auth_credential
-    @auth = AuthCredential.find_or_initialize_by(user_id: current_user.id, provider: 'google')
-    set_refresh_token
-    set_expires_at
-    creds = {refresh_token: @refresh_token, access_token: @access_token}.delete_if{ |k, v| v.nil? }
-    @auth.update_attributes(creds)
-  end
+    @user = User.find_or_initialize_by(email: email)
 
+    if @user.new_record? && !new_google_user(name, email, role, access_token, google_id, @user)
+      redirect_to new_account_path and return
+    end
+
+    if @user.new_record? && @user.role == 'teacher'
+      render 'accounts/new' and return
+    else
+      GoogleIntegration::Classroom::Main
+        .join_existing_google_classrooms(@user, access_token)
+    end
+
+    if @user.errors.any?
+      redirect_to new_account_path and return
+    end
+
+    @user.update(signed_up_with_google: true, google_id: google_id)
+
+    GoogleIntegration::AuthCredentials.new(request, current_user)
+      .create_or_update
+
+    if request.referer && URI(request.referer) &&
+      (URI(request.referer).path == '/teachers/classrooms/dashboard' || URI(request.referer).path == '/teachers/classrooms/new')
+      # if they are hitting this route through the dashboard or new classrooms page, they should be brought to the google sync page
+       redirect_to '/teachers/classrooms/google_sync' and return
+    end
+    redirect_to profile_path and return
+  end
 end
---
+++ services/QuillLMS/app/services/google_integration/auth_credentials.rb
@@ -0,0 +1,37 @@
+class GoogleIntegration::AuthCredentials
+
+  def initialize(request, user)
+    @request = request
+    @user = user
+  end
+
+  def create_or_update
+    params = {}
+    params[:refresh_token] = @refresh_token if @refresh_token.present?
+    params[:access_token]  = @access_token  if @access_token.present?
+
+    @credentials.update(params)
+  end
+
+  def credentials
+    @credentials ||= AuthCredential.find_or_initialize_by(
+      user_id: @user.id,
+      provider: 'google'
+    )
+  end
+
+  def access_token
+    @request.env['omniauth.auth']['credentials']['token']
+  end
+
+  def refresh_token
+    @request.env['omniauth.auth']['credentials']['refresh_token']
+  end
+
+  private
+
+  def token_expiration
+    date = request.env['omniauth.auth']['credentials']['expires_at'].to_s
+    DateTime.strptime(date, '%s')
+  end
+end
