1a499a90b904c4e2f892a56e2eb281d6f8b9ee47
Donald McKendrick <ddmckendrick@gmail.com>
2018-06-05 16:37:07 -0400

    Remove lessons code

--- services/QuillConnect/deploy_lessons.sh
+++
@@ -1,3 +0,0 @@
-rm -r dist/; QUILL_CMS=https://cms.quill.org NODE_ENV=production EMPIRICAL_BASE_URL=https://www.quill.org PUSHER_KEY=b2cbf247b2e2d930b21d FIREBASE_APP_NAME=quillconnect webpack --optimize-minimize
-aws s3 sync ./dist/ s3://aws-website-quill-lessons
-./rollbar.sh
