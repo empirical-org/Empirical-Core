b5bbb57ea5c9ccb2163209c86a1c24832e013cb2
Donald McKendrick <ddmckendrick@gmail.com>
2018-06-06 15:56:21 -0400

    Add Question model.

---
+++ services/QuillComprehension/app/models/question.rb
@@ -0,0 +1,3 @@
+class Question < ApplicationRecord
+  belongs_to :activity
+end
--- services/QuillComprehension/db/schema.rb
+++ services/QuillComprehension/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_06_044813) do
+ActiveRecord::Schema.define(version: 2018_06_06_194446) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,4 +23,14 @@ ActiveRecord::Schema.define(version: 2018_06_06_044813) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "questions", force: :cascade do |t|
+    t.text "prompt"
+    t.bigint "activity_id"
+    t.integer "order"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["activity_id"], name: "index_questions_on_activity_id"
+  end
+
+  add_foreign_key "questions", "activities"
 end
