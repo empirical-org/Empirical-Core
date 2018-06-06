cf1ddfb1d4ff788f39efc839bbd54e7947e848d8
Donald McKendrick <ddmckendrick@gmail.com>
2018-06-05 22:07:37 -0400

    TSX is working with hot-reloading

--- services/QuillComprehension/app/javascript/packs/hello_react.tsx
+++ services/QuillComprehension/app/javascript/packs/hello_react.tsx
@@ -2,21 +2,12 @@
 // like app/views/layouts/application.html.erb. All it does is render <div>Hello React</div> at the bottom
 // of the page.
 
-import React from 'react'
-import ReactDOM from 'react-dom'
-import PropTypes from 'prop-types'
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
 
 const Hello = props => (
-  <div>Hello {props.name}!</div>
+  <div>Hello {props.name}?</div>
 )
-
-Hello.defaultProps = {
-  name: 'David'
-}
-
-Hello.propTypes = {
-  name: PropTypes.string
-}
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
