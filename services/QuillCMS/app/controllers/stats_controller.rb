2920ece953a848f32570607f37b0e60893c5fcaa
Emilia Friedberg <emiliafriedberg@gmail.com>
2018-06-07 14:12:56 -0400

    score analysis now has links to the activities questions belong to

--- services/QuillCMS/app/controllers/stats_controller.rb
+++ services/QuillCMS/app/controllers/stats_controller.rb
@@ -15,10 +15,34 @@ class StatsController < ApplicationController
       WHERE question_uid IS NOT null
       GROUP BY question_uid
     """
+
     dashboard_data = ActiveRecord::Base.connection.execute(sql)
     dashboard_data_hash = {}
-    dashboard_data.each {|x| dashboard_data_hash[x["question_uid"]] = x}
-    HTTParty.put("#{ENV["FIREBASE_URL"]}/v2/datadash.json", body: dashboard_data_hash.to_json).body
+    firebase_url = ENV["FIREBASE_URL"] || 'https://quillconnectstaging.firebaseio.com'
+
+    activities = HTTParty.get("#{firebase_url}/v2/lessons.json")
+
+    dashboard_data.each do |x|
+      question_uid = x["question_uid"]
+      dashboard_data_hash[question_uid] = x
+    end
+
+    activities.each do |k, v|
+      activity_obj = {uid: k, name: v['name']}
+      if v['questions']
+        v['questions'].each do |q|
+          question_data = dashboard_data_hash[q['key']]
+          if question_data && question_data['activities']
+            dashboard_data_hash[q['key']]['activities'] = question_data['activities'].push(activity_obj)
+          elsif question_data
+            dashboard_data_hash[q['key']]['activities'] = [activity_obj]
+          end
+          puts dashboard_data_hash[q['key']]
+        end
+      end
+    end
+
+    HTTParty.put("#{firebase_url}/v2/datadash.json", body: dashboard_data_hash.to_json).body
     render json: :ok
   end
 
--- services/QuillConnect/app/actions/scoreAnalysis.js
+++ services/QuillConnect/app/actions/scoreAnalysis.js
@@ -19,20 +19,23 @@ export function loadScoreData() {
   };
 }
 
-export function checkTimeout() {
-  console.log('checking');
+export function updateData() {
+  request(`${process.env.QUILL_CMS}/stats/question_health_index`, (error, response, body) => {
+    if (error) {
+      console.log('error:', error); // Print the error if one occurred
+    } else {
+      console.log('ok!')
+    }
+  });
+}
 
+export function checkTimeout() {
   setTimeoutRef.on('value', (snapshot) => {
     console.log('time now: ', moment().format('x'));
     console.log('time at snapshot: ', snapshot.val());
     if (moment().format('x') - (snapshot.val() || 0) > 300000) {
       setTimeoutRef.set(moment().format('x'));
-      console.log('Out of date');
-      request(`${process.env.QUILL_CMS}/stats/question_health_index`, (error, response, body) => {
-        if (error) {
-          console.log('error:', error); // Print the error if one occurred
-        }
-      });
+      updateData()
     }
   });
 }
