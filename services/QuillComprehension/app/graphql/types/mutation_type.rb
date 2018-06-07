515ed37e18200b891a100b350ae21a5ca40749a1
Donald McKendrick <ddmckendrick@gmail.com>
2018-06-07 16:12:14 -0400

    Create mutation for addding and incrementing responses

--- services/QuillComprehension/app/graphql/types/mutation_type.rb
+++ services/QuillComprehension/app/graphql/types/mutation_type.rb
@@ -1,11 +1,5 @@
 Types::MutationType = GraphQL::ObjectType.define do
   name "Mutation"
 
-  # TODO: Remove me
-  field :testField, types.String do
-    description "An example field added by the generator"
-    resolve ->(obj, args, ctx) {
-      "Hello World!"
-    }
-  end
+  field :createNewResponse, function: Mutations::CreateNewResponse.new
 end
