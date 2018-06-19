ab5fb4f3e9a00a30b741dbf790b1dd83dae27317
Donald McKendrick <ddmckendrick@gmail.com>
2018-06-19 17:10:38 -0400

    WIP tags

---
+++ services/QuillComprehension/app/javascript/src/components/activities/vocabulary_words.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+export interface VocabularyWord {
+  id:number
+  text:string
+  description:string
+  example:string
+}
+
+export interface Props {
+  vocabWords: Array<VocabularyWord>
+} 
+
+export default class VocabularyWords extends React.Component<Props, any> {
+
+  render() {
+    return (
+      <div className="d-fl-r ai-c">
+        <h3 className="m-r-1 focus">Focus Words</h3>
+        <div className="tags">
+          {this.props.vocabWords.map(word => (<span className="tag large" key={word.id}>{word.text}</span>))}
+        </div>
+      </div>
+    )
+  }
+}
