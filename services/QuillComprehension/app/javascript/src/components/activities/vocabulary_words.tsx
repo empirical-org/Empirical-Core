11cbe1c58929b5619ee226e9e2ba8d9bbd88ee88
Donald McKendrick <ddmckendrick@gmail.com>
2018-06-20 11:30:11 -0400

    WIP google classroom announcements

--- services/QuillComprehension/app/javascript/src/components/activities/vocabulary_words.tsx
+++ services/QuillComprehension/app/javascript/src/components/activities/vocabulary_words.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import Modal from '../modals'
 export interface VocabularyWord {
   id:number
   text:string
@@ -16,8 +16,9 @@ export default class VocabularyWords extends React.Component<Props, any> {
     return (
       <div className="d-fl-r ai-c">
         <h3 className="m-r-1 focus">Focus Words</h3>
+        <Modal>Hi</Modal>
         <div className="tags">
-          {this.props.vocabWords.map(word => (<span className="tag large" key={word.id}>{word.text}</span>))}
+          {this.props.vocabWords.map(word => (<a className="tag large" key={word.id}>{word.text}</a>))}
         </div>
       </div>
     )
