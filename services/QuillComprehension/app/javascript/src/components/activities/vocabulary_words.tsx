a734f0456ce5d96ccdc20fc3811ea98ae6201ceb
Donald McKendrick <ddmckendrick@gmail.com>
2018-06-20 14:07:44 -0400

    WIP MODALS

--- services/QuillComprehension/app/javascript/src/components/activities/vocabulary_words.tsx
+++ services/QuillComprehension/app/javascript/src/components/activities/vocabulary_words.tsx
@@ -11,14 +11,40 @@ export interface Props {
   vocabWords: Array<VocabularyWord>
 } 
 
-export default class VocabularyWords extends React.Component<Props, any> {
+export interface State {
+  readonly activeVocabWord: VocabularyWord|null
+}
+
+export default class VocabularyWords extends React.Component<Props, State> {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeVocabWord: null
+    }
+  }
+
+  selectVocabWord(vocabWord:VocabularyWord):void {
+    this.setState({activeVocabWord: vocabWord})
+  }
+
+  clearVocabWord():void {
+    this.setState({activeVocabWord: null});
+  }
+
+  renderModal() {
+    if (this.state.activeVocabWord) {
+      return (<Modal><p>{this.state.activeVocabWord.text}</p></Modal>)
+    }
+  }
+  
   render() {
     return (
       <div className="d-fl-r ai-c">
         <h3 className="m-r-1 focus">Focus Words</h3>
-        <Modal>Hi</Modal>
+        {this.renderModal()}
         <div className="tags">
-          {this.props.vocabWords.map(word => (<a className="tag large" key={word.id}>{word.text}</a>))}
+          {this.props.vocabWords.map(word => (<a className="tag large" key={word.id} onClick={e => this.selectVocabWord(word)} >{word.text}</a>))}
         </div>
       </div>
     )
