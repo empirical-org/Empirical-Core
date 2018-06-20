7b7c8a9721fcc978f6711a9a2441c200d728ac12
Donald McKendrick <ddmckendrick@gmail.com>
2018-06-20 14:28:11 -0400

    Bare minimum styling of modals

--- services/QuillComprehension/app/javascript/src/components/modals/index.tsx
+++ services/QuillComprehension/app/javascript/src/components/modals/index.tsx
@@ -3,34 +3,14 @@ import ReactDOM from 'react-dom'
 
 
 class Modal extends React.Component<any> {
-  // static el = document.createElement('div');
-  // static modalRoot = document.getElementById('modal-root');
 
   constructor(props) {
     super(props);
-
-    console.log(props.children)
-  }
-
-  componentDidMount() {
-    // The portal element is inserted in the DOM tree after
-    // the Modal's children are mounted, meaning that children
-    // will be mounted on a detached DOM node. If a child
-    // component requires to be attached to the DOM tree
-    // immediately when mounted, for example to measure a
-    // DOM node, or uses 'autoFocus' in a descendant, add
-    // state to Modal and only render the children when Modal
-    // is inserted in the DOM tree.
-    document.getElementById('modal-root').appendChild(document.createElement('div'));
-  }
-
-  componentWillUnmount() {
-    document.getElementById('modal-root').removeChild(document.createElement('div'));
   }
 
   render() {
     return ReactDOM.createPortal(
-      this.props.children,
+      (<div className="modal">{this.props.children}</div>),
       document.getElementById('modal-root'),
     );
   }
