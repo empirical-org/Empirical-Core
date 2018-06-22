27f1433d559c8c20e3deb2e61234afc35f7285e9
Emilia Friedberg <emiliafriedberg@gmail.com>
2018-06-22 10:48:15 -0400

    update qcl to properly import richbuttons plugin

--- packages/quill-component-library/declarations.d.ts
+++ packages/quill-component-library/declarations.d.ts
@@ -3,3 +3,8 @@ declare module '*.scss' {
     const content: {[className: string]: string};
     export = content;
 }
+
+declare module 'draft-js-richbuttons-plugin' {
+  const createRichButtonsPlugin: Function;
+  export = createRichButtonsPlugin;
+}
