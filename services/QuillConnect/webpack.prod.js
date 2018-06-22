2970801a7a18a0ce0f7adbdbb13ae1dcedbe14be
Max Buck <maxwellhigginsbuck@gmail.com>
2018-06-22 14:42:54 -0400

    divide webpack config into def and prod versions; remove NODE_ENV from jenkins envars, I think setting it in the package.json might be better so that webpack is installed with npm, but is still only a dev dep so not bundled

---
+++ services/QuillConnect/webpack.prod.js
@@ -0,0 +1,27 @@
+const merge = require('webpack-merge');
+const common = require('./webpack.common.js');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = merge(common, {
+  mode: 'production',
+  plugins: [
+    new webpack.EnvironmentPlugin({
+      QUILL_CMS: 'https://cms.quill.org',
+      NODE_ENV: 'production',
+      EMPIRICAL_BASE_URL: 'https://www.quill.org',
+      PUSHER_KEY: 'insert-pusher-key',
+      FIREBASE_APP_NAME: 'quillconnect',
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: ['css-loader', 'sass-loader'],
+        }),
+      }
+    ],
+  },
+});
