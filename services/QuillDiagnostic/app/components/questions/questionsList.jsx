99987085336a01f4fbd6e359aae1e028b99f238a
Emilia Friedberg <emiliafriedberg@gmail.com>
2018-06-26 12:53:56 -0400

    update scoreAnalysis and questionHealth to only show diagnostic data

--- services/QuillDiagnostic/app/components/questions/questionsList.jsx
+++ services/QuillDiagnostic/app/components/questions/questionsList.jsx
@@ -63,17 +63,17 @@ const QuestionsList = React.createClass({
   },
 
   renderQuestionsWithoutValidKey: function () {
-    if(!this.props.displayNoConceptQuestions) {
-      return (<div></div>)
+    const concepts = hashToCollection(this.props.concepts.data['0']);
+    const questions = hashToCollection(this.props.questions);
+    const label = (<p className="menu-label">
+      No valid concept
+    </p>)
+    if (!this.props.displayNoConceptQuestions) {
+      return this.renderConceptWithQuestions(questions, label);
     } else {
-      const concepts = hashToCollection(this.props.concepts.data['0']);
-      const questions = hashToCollection(this.props.questions.data);
       const questionsToRender = _.reject(questions, (question) => {
         return !!_.find(concepts, {uid: question.conceptID})
       })
-      const label = (<p className="menu-label">
-        No valid concept
-      </p>)
       return this.renderConceptWithQuestions(questionsToRender, label);
     }
   },
