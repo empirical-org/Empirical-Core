caf74c5f2e2ddc1b766928c02d04aa2ab87f7b2e
tronroy <tbconroy@gmail.com>
2018-06-28 15:46:42 -0400

    Refactor criteria controller

--- services/QuillLMS/app/controllers/cms/criteria_controller.rb
+++ services/QuillLMS/app/controllers/cms/criteria_controller.rb
@@ -1,17 +1,13 @@
 class Cms::CriteriaController < Cms::CmsController
+  before_action :set_activity
+  before_action :set_recommendation
+  before_action :set_activity_classification
+
   def new
-    @activity_classification = ActivityClassification
-      .find(params[:activity_classification_id])
-    @activity = Activity.find(params[:activity_id])
-    @recommendation = Recommendation.find(params[:recommendation_id])
     @criterion = Criterion.new
   end
 
   def create
-    @activity_classification = ActivityClassification
-      .find(params[:activity_classification_id])
-    @activity = Activity.find(params[:activity_id])
-    @recommendation = Recommendation.find(params[:recommendation_id])
     @criterion = Criterion.new(recommendation: @recommendation)
 
     if @criterion.update(criteria_params)
@@ -28,10 +24,6 @@ class Cms::CriteriaController < Cms::CmsController
 
   def destroy
     @criterion = Criterion.find(params[:id])
-    @activity_classification = ActivityClassification
-      .find(params[:activity_classification_id])
-    @activity = Activity.find(params[:activity_id])
-    @recommendation = Recommendation.find(params[:recommendation_id])
 
     if @criterion.destroy
       flash[:notice] = 'Criterion deleted!'
@@ -47,6 +39,19 @@ class Cms::CriteriaController < Cms::CmsController
   end
 
   private
+
+  def set_activity_classification
+    @activity_classification = ActivityClassification
+      .find(params[:activity_classification_id])
+  end
+
+  def set_activity
+    @activity = Activity.find(params[:activity_id])
+  end
+
+  def set_recommendation
+    @recommendation = Recommendation.find(params[:recommendation_id])
+  end
 
   def criteria_params
     params.require(:criterion).permit(:concept_id, :category, :count)
