2b77cc27d297e34b65827222021355a8a22726c1
tronroy <tbconroy@gmail.com>
2018-06-28 15:05:45 -0400

    Display errors on form if criterion creation fails

--- services/QuillLMS/app/models/criterion.rb
+++ services/QuillLMS/app/models/criterion.rb
@@ -1,6 +1,11 @@
 class Criterion < ActiveRecord::Base
   belongs_to :recommendation
   belongs_to :concept
+  validates :recommendation, :concept, :category, :count, presence: true
+  validates :category, uniqueness: {
+    scope: :recommendation,
+    message: "of this type already added to recommendation"
+  }
 
   enum category: { correct_submissions: 0, incorrect_submissions: 1 }
 end
