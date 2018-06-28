04ee6edd04b57aef5c2861fe6dddd9080848472a
Emilia Friedberg <emiliafriedberg@gmail.com>
2018-06-28 16:04:39 -0400

    add MultipleChoice to qcl

---
+++ packages/quill-component-library/src/components/studentLessons/index.tsx
@@ -0,0 +1,3 @@
+export {
+  MultipleChoice
+} from './multipleChoice'
--- services/QuillConnect/app/components/studentLessons/question.tsx
+++ services/QuillConnect/app/components/studentLessons/question.tsx
@@ -25,7 +25,7 @@ import handleFocus from '../renderForQuestions/handleFocus.js';
 import submitQuestionResponse from '../renderForQuestions/submitResponse.js';
 import updateResponseResource from '../renderForQuestions/updateResponseResource.js';
 import submitPathway from '../renderForQuestions/submitPathway.js';
-import MultipleChoice from './multipleChoice.jsx';
+import { MultipleChoice } from 'quill-component-library/dist/componentLibrary';
 import AnswerForm from '../renderForQuestions/renderFormForAnswer.jsx';
 import { getOptimalResponses, getSubOptimalResponses, getTopOptimalResponse } from '../../libs/sharedResponseFunctions';
 import {
