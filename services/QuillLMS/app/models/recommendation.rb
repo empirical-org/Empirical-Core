4fd23c5972ba315ac2f7b0412186b68c0aaf2822
tronroy <tbconroy@gmail.com>
2018-06-29 11:23:17 -0400

    Remove uniqueness constraint on recommendation name

--- services/QuillLMS/app/models/recommendation.rb
+++ services/QuillLMS/app/models/recommendation.rb
@@ -3,7 +3,6 @@ class Recommendation < ActiveRecord::Base
   belongs_to :unit_template
   has_many :criteria, dependent: :destroy
   validates :name, length: { minimum: 2, maximum: 150 }
-  validates :name, uniqueness: true
   validates :name, :activity, :category, :unit_template, presence: true
 
   enum category: { independent_practice: 0, group_lesson: 1 }
