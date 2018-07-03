71f2546bd4de2b3a21ab96d35db67ad2d43d0019
Donald McKendrick <ddmckendrick@gmail.com>
2018-07-03 14:56:11 -0400

    Show latest stats on the response show page

--- services/QuillComprehension/spec/models/response_spec.rb
+++ services/QuillComprehension/spec/models/response_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe Response, type: :model do
   let(:response) { create(:response, question: question)}
   let(:correct_response_label) { create(:response_label)}
   let(:factual_response_label) { create(:response_label, name: "factual", description: "Is the answer factual?")}
-
+  let(:spelling_response_label) { create(:response_label, name: "spelling", description: "Is the answer spelled correctly?")}
 
 
   it 'has the correct relations' do
@@ -15,8 +15,83 @@ RSpec.describe Response, type: :model do
     expect(question.activity).to be(activity)
   end
 
-  it "can calculate its metrics" do
-    rlt = build(:response_label_tag, response_id: nil)
-    expect(rlt.valid?).to be(false)
+  it "can calculate one of it metrics" do 
+    rlt1 = create(:response_label_tag, response: response, response_label: correct_response_label)
+    expect(response.metric("correct")).to eq(1)
+    rlt2 = create(:response_label_tag, response: response, response_label: correct_response_label, score: -1)
+    expect(response.metric("correct")).to eq(0)
   end
+
+  it "can calculate one of it metrics 2" do 
+    rlt2 = create(:response_label_tag, response: response, response_label: correct_response_label, score: -1)
+    expect(response.metric("correct")).to eq(-1)
+  end
+
+  it "can calculate all of its metrics" do
+    rlt1 = create(:response_label_tag, response: response, response_label: correct_response_label)
+    expect(response.metrics).to eq({
+      "correct": 1,
+    })
+  end
+
+  it "can calculate all of its metrics with multiple tags" do
+    create(:response_label_tag, response: response, response_label: correct_response_label)
+    create(:response_label_tag, response: response, response_label: correct_response_label)
+    create(:response_label_tag, response: response, response_label: correct_response_label)
+    create(:response_label_tag, response: response, response_label: correct_response_label, score: -1)
+    create(:response_label_tag, response: response, response_label: factual_response_label)
+    create(:response_label_tag, response: response, response_label: factual_response_label, score: -1)
+    create(:response_label_tag, response: response, response_label: factual_response_label, score: -1)
+    create(:response_label_tag, response: response, response_label: factual_response_label, score: -1)
+    expect(response.metrics).to eq({
+      "correct": 2,
+      "factual": -2
+    })
+  end
+
+  it "can calculate all of its metrics with multiple tags" do
+    create(:response_label_tag, response: response, response_label: correct_response_label)
+    create(:response_label_tag, response: response, response_label: correct_response_label)
+    create(:response_label_tag, response: response, response_label: correct_response_label)
+    create(:response_label_tag, response: response, response_label: correct_response_label, score: -1)
+    create(:response_label_tag, response: response, response_label: factual_response_label)
+    create(:response_label_tag, response: response, response_label: factual_response_label, score: -1)
+    create(:response_label_tag, response: response, response_label: factual_response_label, score: -1)
+    create(:response_label_tag, response: response, response_label: factual_response_label, score: -1)
+    create(:response_label_tag, response: response, response_label: spelling_response_label, score: -1)
+    create(:response_label_tag, response: response, response_label: spelling_response_label)
+    create(:response_label_tag, response: response, response_label: spelling_response_label, score: -1)
+    expect(response.metrics).to eq({
+      "correct": 2,
+      "factual": -2,
+      "spelling": -1
+    })
+  end
+
+  it "can access all of its tags including metrics" do
+    create(:response_label_tag, response: response, response_label: correct_response_label)
+    create(:response_label_tag, response: response, response_label: correct_response_label)
+    create(:response_label_tag, response: response, response_label: correct_response_label)
+    create(:response_label_tag, response: response, response_label: factual_response_label)
+    create(:response_label_tag, response: response, response_label: factual_response_label, score: -1)
+    create(:response_label, name: "spelling", description: "Is the answer spelled correctly?")
+    expect(response.all_metrics).to eq({
+      "correct": 3,
+      "factual": 0,
+      "spelling": 0
+    })
+  end
+
+
+  it "can calculate its latest metrics with multiple tags" do
+    create(:response_label_tag, response: response, response_label: correct_response_label)
+    create(:response_label_tag, response: response, response_label: correct_response_label, score: -1)
+    create(:response_label_tag, response: response, response_label: factual_response_label, score: -1)
+    create(:response_label_tag, response: response, response_label: factual_response_label)
+    expect(response.latest_metrics).to eq({
+      "correct": -1,
+      "factual": 1
+    })
+  end
+
 end
