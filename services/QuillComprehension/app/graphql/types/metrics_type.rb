30197dfca36237ec52935957159debfdadfae57e
Donald McKendrick <ddmckendrick@gmail.com>
2018-07-03 15:12:25 -0400

    Add metrics to graphql

---
+++ services/QuillComprehension/app/graphql/types/metrics_type.rb
@@ -0,0 +1,4 @@
+class Types::MetricsType < Types::BaseObject
+  field :correct, Integer, null: true
+  field :factual, Integer, null: true
+end
--- services/QuillComprehension/app/graphql/types/response_type.rb
+++ services/QuillComprehension/app/graphql/types/response_type.rb
@@ -4,4 +4,10 @@ class Types::ResponseType < Types::BaseObject
   field :question_id, ID, null: false
   field :submissions, Integer, null: false
   field :text, String, null: false
+
+  field :metrics, Types::MetricsType, null: false
+
+  def metrics
+    object.all_metrics
+  end
 end
