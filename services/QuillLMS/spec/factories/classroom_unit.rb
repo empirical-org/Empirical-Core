a4087b8f7afcb44e8266fc9337002a4bb8579daa
Emilia Friedberg <emiliafriedberg@gmail.com>
2018-07-11 12:26:14 -0400

    get classroom activities out of all the factories

--- services/QuillLMS/spec/factories/classroom_unit.rb
+++ services/QuillLMS/spec/factories/classroom_unit.rb
@@ -10,5 +10,11 @@ FactoryBot.define do
       end
     end
 
+    factory :lesson_classroom_unit_with_activity_sessions do
+       after(:create) do |cu|
+         create_list(:activity_session, 5, classroom_unit: cu, activity: create(:lesson_activity, :with_follow_up))
+       end
+     end
+
   end
 end
