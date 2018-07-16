b17f395e7a14e858246b544a18d7b05e2026e6f3
Emilia Friedberg <emiliafriedberg@gmail.com>
2018-07-16 12:14:31 -0400

    fix activity authorizer and activity authorizer spec

--- services/QuillLMS/app/services/activity_authorizer.rb
+++ services/QuillLMS/app/services/activity_authorizer.rb
@@ -14,7 +14,7 @@ class ActivityAuthorizer
   end
 
   def authorize_teacher
-    @activity_session.classroom_unit.classroom.owner == @current_user
+    @activity_session&.classroom_unit&.classroom&.owner == @current_user
   end
 
 end
