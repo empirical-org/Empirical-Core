0ba481881b9cc70d6406c438cbefcc699d61e221
Donald McKendrick <ddmckendrick@gmail.com>
2018-07-17 15:44:30 -0400

    Remove with call as it was causing inconsistencies

--- services/QuillLMS/spec/controllers/teachers/classroom_manager_controller_spec.rb
+++ services/QuillLMS/spec/controllers/teachers/classroom_manager_controller_spec.rb
@@ -453,7 +453,6 @@ describe Teachers::ClassroomManagerController, type: :controller do
       create(:auth_credential, user: teacher)
 
       expect(GoogleStudentImporterWorker).to receive(:perform_async)
-        .with(teacher.id)
       get :import_google_students, selected_classrooms: [1,2], format: :json
     end
   end
