ddd6286e6053e3e1ac81d7bd9ee8263c900fe0ae
Emilia Friedberg <emiliafriedberg@gmail.com>
2018-07-18 12:21:54 -0400

    teacher fix controller test updates

--- services/QuillLMS/spec/factories/activity_session.rb
+++ services/QuillLMS/spec/factories/activity_session.rb
@@ -29,7 +29,7 @@ FactoryBot.define do
 
     after(:create) do |activity_session|
       classroom_unit = activity_session.classroom_unit
-      UnitActivity.find_or_create_by(activity: activity_session.activity, unit: classroom_unit.unit)
+      UnitActivity.find_or_create_by(activity: activity_session.activity, unit_id: classroom_unit.unit_id)
       StudentsClassrooms.find_or_create_by(student_id: activity_session.user_id, classroom_id: classroom_unit.classroom_id )
       create(:concept_result, activity_session: activity_session)
     end
