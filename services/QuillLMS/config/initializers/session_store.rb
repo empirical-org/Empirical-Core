e37fa3bc004fab4cac5c33923a9aaf4efc857cce
Donald McKendrick <ddmckendrick@gmail.com>
2018-07-23 10:38:45 -0400

    change cypress cookie name

--- services/QuillLMS/config/initializers/session_store.rb
+++ services/QuillLMS/config/initializers/session_store.rb
@@ -6,7 +6,7 @@ if Rails.env.production?
 elsif Rails.env.staging?
   EmpiricalGrammar::Application.config.session_store :cookie_store, key: '_quill_staging_session', domain: 'staging.quill.org'
 elsif Rails.env.cypress?
-  EmpiricalGrammar::Application.config.session_store :cookie_store, key: '_quill_cypress_session', domain: :all
+  EmpiricalGrammar::Application.config.session_store :cookie_store, key: '_quill_session', domain: :all
 else
   EmpiricalGrammar::Application.config.session_store :cookie_store, key: '_quill_development_session', domain: :all
 end
