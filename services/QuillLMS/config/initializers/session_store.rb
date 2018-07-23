c9d7f6d830a9bee044be95699ea1738c78c2d091
Donald McKendrick <ddmckendrick@gmail.com>
2018-07-23 10:04:52 -0400

    Update node version

--- services/QuillLMS/config/initializers/session_store.rb
+++ services/QuillLMS/config/initializers/session_store.rb
@@ -5,6 +5,8 @@ if Rails.env.production?
   EmpiricalGrammar::Application.config.session_store :cookie_store, key: '_quill_session', domain: '.quill.org'
 elsif Rails.env.staging?
   EmpiricalGrammar::Application.config.session_store :cookie_store, key: '_quill_staging_session', domain: 'staging.quill.org'
+elsif Rails.env.cypress?
+  EmpiricalGrammar::Application.config.session_store :cookie_store, key: '_quill_cypress_session', domain: :all
 else
   EmpiricalGrammar::Application.config.session_store :cookie_store, key: '_quill_development_session', domain: :all
 end
