dd4fbd219f24810960a4e60478b0f097035c53ba
tronroy <tbconroy@gmail.com>
2018-07-24 20:00:25 -0400

    Fix unit creator spec, formatting

--- services/QuillLMS/spec/support/shared/student.rb
+++ services/QuillLMS/spec/support/shared/student.rb
@@ -1,12 +1,8 @@
 shared_examples_for "student" do
-
-
   let(:classroom){ build(:classroom, code: '101') }
   let(:student){build(:student)}
 
-
   context 'if username is not present' do
-
     let!(:student){ build(:student, username: nil) }
 
     it 'should be valid' do
@@ -14,17 +10,13 @@ shared_examples_for "student" do
     end
 
     context 'when email and username is missing' do
-
       it 'should have an error' do
         student.email = nil
         student.valid?
         expect(student.errors[:username]).to include "can't be blank"
       end
-
     end
-
   end
-
 
   describe "#activity_sessions" do
     let!(:activity){ create(:activity) }
@@ -40,6 +32,4 @@ shared_examples_for "student" do
       expect(student.activity_sessions).not_to be_empty
     end
   end
-
-
 end
