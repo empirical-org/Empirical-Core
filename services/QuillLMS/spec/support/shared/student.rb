ab958ea8a594f7b9a24e99e3364c83b9f78c5a77
tronroy <tbconroy@gmail.com>
2018-07-25 16:10:07 -0400

    Remove spec related to defunct classroom activity

--- services/QuillLMS/spec/support/shared/student.rb
+++ services/QuillLMS/spec/support/shared/student.rb
@@ -17,19 +17,4 @@ shared_examples_for "student" do
       end
     end
   end
-
-  describe "#activity_sessions" do
-    let!(:activity){ create(:activity) }
-    let!(:student){ create(:student, :in_one_classroom) }
-    let!(:classroom_activity) { create(:classroom_activity,activity_id: activity.id, classroom_id: student.classrooms.first.id) }
-
-    it "must returns an empty array when none is assigned" do
-      expect(student.activity_sessions).to be_empty
-    end
-
-    it "must not be empty when an activity session has been completed and is visible" do
-      ActivitySession.create(classroom_activity_id: classroom_activity.id, activity_id: activity.id, completed_at: Time.now, user_id: student.id)
-      expect(student.activity_sessions).not_to be_empty
-    end
-  end
 end
