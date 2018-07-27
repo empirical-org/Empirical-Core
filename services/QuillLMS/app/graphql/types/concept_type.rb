0f0f02a7cd24bb75e48ef3e2831b9c6a3357b9fd
Donald McKendrick <ddmckendrick@gmail.com>
2018-07-27 11:05:22 -0400

    WIP

---
+++ services/QuillLMS/app/graphql/types/concept_type.rb
@@ -0,0 +1,20 @@
+class Types::ConceptType < Types::BaseObject
+  graphql_name 'Concept'
+
+  field :id, ID, null: false
+  field :uid, String, null: false
+  field :name, String, null: false
+  field :parent_id, ID, null: true
+  
+  field :parent, Types::ConceptType, null: true
+  field :children, [Types::ConceptType, null: true], null: true
+  
+  def parent
+    Concept.find(object.parent_id)
+  end
+
+  def children
+    Concept.where(parent_id: object.id)
+  end
+
+end
--- services/QuillLMS/app/graphql/types/query_type.rb
+++ services/QuillLMS/app/graphql/types/query_type.rb
@@ -1,4 +1,11 @@
 class Types::QueryType < Types::BaseObject
 
-  field :current_user, Types::UserType, null: true, resolve: -> (obj, args, ctx) { ctx[:current_user] }
+  field :current_user, Types::UserType, null: true do
+     resolve -> (obj, args, ctx) { ctx[:current_user] }
+  end
+
+  field :concepts, [Types::ConceptType], null: false do
+    argument :parent_id, String, null: true
+    resolve -> (obj, args, ctx) { args.any? ? Concept.where(args) : Concept.all}
+  end
 end
