9af506f069fbc4be1a28f652df4d7b55fe303a6c
tronroy <tbconroy@gmail.com>
2018-07-27 16:05:48 -0400

    Fix broken specs from service refactor

--- services/QuillLMS/spec/services/authorized_teacher_for_activity_spec.rb
+++ services/QuillLMS/spec/services/authorized_teacher_for_activity_spec.rb
@@ -4,13 +4,13 @@ describe AuthorizedTeacherForActivity do
 
   it 'returns true if the classroom owner is the same as given user' do
     user = create(:user, role: 'teacher')
-    classroom = create(:classroom)
+    classroom = create(:simple_classroom)
     classroom_unit = create(:classroom_unit, classroom: classroom)
     activity_session = create(:activity_session, classroom_unit: classroom_unit)
-    create(:classrooms_teachers, role: 'owner', user: user, classroom: classroom)
+    create(:classrooms_teacher, role: 'owner', user: user, classroom: classroom)
 
     result = AuthorizedTeacherForActivity.new(user, activity_session).call
 
-    expect(subject.authorize_teacher).to eq true
+    expect(result).to eq true
   end
 end
