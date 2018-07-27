a949c5090e2ffe986cbcecd02cf3ece7b1c825d2
tronroy <tbconroy@gmail.com>
2018-07-27 14:57:00 -0400

    Extract and refactor method in activity authorizer service

--- services/QuillLMS/app/services/activity_authorizer.rb
+++ services/QuillLMS/app/services/activity_authorizer.rb
@@ -5,16 +5,7 @@ class ActivityAuthorizer
     @activity_session = activity_session
   end
 
-  def authorize
-    return true if @current_user.try(:staff?)
-    if @activity_session.blank? || @activity_session.user != @current_user
-      return false
-    end
-    true
-  end
-
   def authorize_teacher
     @activity_session&.classroom_unit&.classroom&.owner == @current_user
   end
-
 end
