c0303764894c5c5c80d25f91a82288c9e7f52126
gaurav <gauravrakheja002@gmail.com>
2018-07-28 21:03:58 +0530

    added specs for join school worker

---
+++ services/QuillLMS/spec/workers/join_school_worker_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+describe JoinSchoolEmailWorker do
+  let(:subject) { described_class.new }
+
+  describe '#perform' do
+    let!(:user) { create(:user) }
+    let!(:school) { create(:school) }
+
+    it 'should send the join school email' do
+      expect_any_instance_of(User).to receive(:send_join_school_email).with(school)
+      subject.perform(user.id, school.id)
+    end
+  end
+end
