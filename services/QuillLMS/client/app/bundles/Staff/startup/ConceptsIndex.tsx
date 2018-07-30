e089a729e8bc8153825c5391008127004a123f03
Donald McKendrick <ddmckendrick@gmail.com>
2018-07-30 15:56:10 -0400

    Spike concepts index

---
+++ services/QuillLMS/client/app/bundles/Staff/startup/ConceptsIndex.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ConceptsIndex from '../containers/ConceptsIndex';
+
+export default (props) => (
+  <ConceptsIndex {...props} />
+);
+
---
+++ services/QuillLMS/client/app/modules/apollo.ts
@@ -0,0 +1,30 @@
+// import ApolloClient from "apollo-boost";
+
+// const client = new ApolloClient({});
+import { ApolloClient } from 'apollo-client';
+import { HttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { ApolloLink, concat } from 'apollo-link';
+
+
+const httpLink = new HttpLink({ uri: '/graphql' });
+
+const authMiddleware = new ApolloLink((operation, forward) => {
+  // add the authorization to the headers
+  operation.setContext({
+    headers: {
+      'x-csrf-token': document.getElementsByName('csrf-token')[0] ? document.getElementsByName('csrf-token')[0].content : 0
+    } 
+  });
+
+  return forward(operation);
+})
+
+const client = new ApolloClient({
+  link: concat(authMiddleware, httpLink),
+  cache: new InMemoryCache(),
+});
+
+
+
+export default client;
