98dd141f66d486b99c90f88fc514607183ac6b52
Donald McKendrick <ddmckendrick@gmail.com>
2018-07-29 21:55:52 -0400

    use ruby keyword aguments

--- services/QuillLMS/app/graphql/types/query_type.rb
+++ services/QuillLMS/app/graphql/types/query_type.rb
@@ -6,8 +6,8 @@ class Types::QueryType < Types::BaseObject
     argument :id, Int, "Restrict items to this status", required: false
   end
 
-  def concept(id) 
-    return Concept.find(id[:id])
+  def concept(id: nil) 
+    return Concept.find(id) if id
   end  
 
   field :concepts, [Types::ConceptType], null: false, resolve: -> (obj, args, ctx) { Concept.all }
