1a5b6bb95e808070e700413675b49ae70799dd37
tronroy <tbconroy@gmail.com>
2018-08-02 17:36:52 -0400

    Rename and refactor name splitter service

--- services/QuillLMS/app/services/fullname_validator.rb
+++ services/QuillLMS/app/services/fullname_validator.rb
@@ -2,7 +2,7 @@ class FullnameValidator < ActiveModel::Validator
 
   def validate(record)
     return if record.name.nil?
-    f,l = NameSplitter.new(record.name).split
+    f,l = SplitName.new(record.name).call
     if f.nil? or l.nil?
       record.errors[:name] << "must include first and last name"
     end
