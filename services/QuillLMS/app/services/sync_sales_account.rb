f3145d61b6b879d22e0e31583151e752995f96c7
tronroy <tbconroy@gmail.com>
2018-08-03 15:59:10 -0400

    Rename and refactor sales account syncer service

--- services/QuillLMS/app/services/sync_sales_account.rb
+++ services/QuillLMS/app/services/sync_sales_account.rb
@@ -1,17 +1,20 @@
-class SalesAccountSyncer
+class SyncSalesAccount
+
   def initialize(school_id, serializer = nil, client = nil)
     @school_id  = school_id
     @serializer = serializer || SerializeSalesAccount
     @client     = client || SalesmachineClient
   end
 
-  def sync
-    @client.batch([account_data])
+  def call
+    client.batch([account_data])
   end
 
   private
 
+  attr_reader :school_id, :serializer, :client
+
   def account_data
-    @serializer.new(@school_id).data
+    serializer.new(school_id).data
   end
 end
