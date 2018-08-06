1bd182c8f52e174d34cb347f90d3a697ecbcf1e8
Emilia Friedberg <emiliafriedberg@gmail.com>
2018-08-06 17:26:55 -0400

    successfully alert user when clone has or has not taken place

--- services/QuillDiagnostic/app/actions/connectFillInBlank.js
+++ services/QuillDiagnostic/app/actions/connectFillInBlank.js
@@ -15,27 +15,29 @@ export function startListeningToConnectFillInBlankQuestions() {
 }
 
 export function cloneConnectFillInBlankQuestion(uid: string) {
-  connectFillInBlankRef.child(uid).on('value', (snapshot) => {
-    const connectFillInBlankQuestion = snapshot ? snapshot.val() : null
-    if (connectFillInBlankQuestion) {
-      const diagnosticFillInBlankQuestion = diagnosticFillInBlankRef.push(connectFillInBlankQuestion)
-      request(
-        {
-          url: `${process.env.QUILL_CMS}/responses/clone_responses`,
-          method: 'POST',
-          json: { original_question_uid: uid, new_question_uid: diagnosticFillInBlankQuestion.key, }
-        },
-        (err, httpResponse, data) => {
-          // check again for number in state
-          // if equal to const set earlier, update the state
-          // otherwise, do nothing
-          if (err) {
-            console.log('uh oh', err)
-          } else {
-            console.log('yay', data)
+  return (dispatch) => {
+    connectFillInBlankRef.child(uid).on('value', (snapshot) => {
+      const connectFillInBlankQuestion = snapshot ? snapshot.val() : null
+      if (connectFillInBlankQuestion) {
+        const diagnosticFillInBlankQuestion = diagnosticFillInBlankRef.push(connectFillInBlankQuestion)
+        request(
+          {
+            url: `${process.env.QUILL_CMS}/responses/clone_responses`,
+            method: 'POST',
+            json: { original_question_uid: uid, new_question_uid: diagnosticFillInBlankQuestion.key, }
+          },
+          (err, httpResponse, data) => {
+            // check again for number in state
+            // if equal to const set earlier, update the state
+            // otherwise, do nothing
+            if (err) {
+              dispatch({ type: C.ERROR_CLONING_CONNECT_FILL_IN_BLANK_QUESTION })
+            } else {
+              dispatch({ type: C.SUCCESSFULLY_CLONED_CONNECT_FILL_IN_BLANK_QUESTION })
+            }
           }
-        }
-      );
-    }
-  });
+        );
+      }
+    });
+  }
 }
--- services/QuillDiagnostic/app/actions/connectSentenceCombining.js
+++ services/QuillDiagnostic/app/actions/connectSentenceCombining.js
@@ -15,27 +15,29 @@ export function startListeningToConnectQuestions() {
 }
 
 export function cloneConnectSentenceCombiningQuestion(uid: string) {
-  connectSentenceCombiningRef.child(uid).on('value', (snapshot) => {
-    const connectSentenceCombiningQuestion = snapshot ? snapshot.val() : null
-    if (connectSentenceCombiningQuestion) {
-      const diagnosticSentenceCombiningQuestion = diagnosticSentenceCombiningRef.push(connectSentenceCombiningQuestion)
-      request(
-        {
-          url: `${process.env.QUILL_CMS}/responses/clone_responses`,
-          method: 'POST',
-          json: { original_question_uid: uid, new_question_uid: diagnosticSentenceCombiningQuestion.key, }
-        },
-        (err, httpResponse, data) => {
-          // check again for number in state
-          // if equal to const set earlier, update the state
-          // otherwise, do nothing
-          if (err) {
-            console.log('uh oh', err)
-          } else {
-            console.log('yay', data)
+  return (dispatch) => {
+    connectSentenceCombiningRef.child(uid).on('value', (snapshot) => {
+      const connectSentenceCombiningQuestion = snapshot ? snapshot.val() : null
+      if (connectSentenceCombiningQuestion) {
+        const diagnosticSentenceCombiningQuestion = diagnosticSentenceCombiningRef.push(connectSentenceCombiningQuestion)
+        request(
+          {
+            url: `${process.env.QUILL_CMS}/responses/clone_responses`,
+            method: 'POST',
+            json: { original_question_uid: uid, new_question_uid: diagnosticSentenceCombiningQuestion.key, }
+          },
+          (err, httpResponse, data) => {
+            // check again for number in state
+            // if equal to const set earlier, update the state
+            // otherwise, do nothing
+            if (err) {
+              dispatch({ type: C.ERROR_CLONING_CONNECT_SENTENCE_COMBINING_QUESTION })
+            } else {
+              dispatch({ type: C.SUCCESSFULLY_CLONED_CONNECT_SENTENCE_COMBINING_QUESTION })
+            }
           }
-        }
-      );
-    }
-  });
+        );
+      }
+    });
+  }
 }
