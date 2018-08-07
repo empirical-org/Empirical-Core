c613a6a4b4a43f148b5cdace9960198dcdc5fa3c
Donald McKendrick <ddmckendrick@gmail.com>
2018-08-07 17:40:52 -0400

    Tidy file structure and registration

--- services/QuillLMS/client/app/bundles/Staff/startup/clientRegistration.js
+++ services/QuillLMS/client/app/bundles/Staff/startup/clientRegistration.js
@@ -1,6 +1,5 @@
 import ReactOnRails from 'react-on-rails';
 import ConceptsIndex from './ConceptsIndex';
-import ConceptsShow from './ConceptsShow';
 import 'antd/dist/antd.css';
 
-ReactOnRails.register({ ConceptsIndex, ConceptsShow, });
+ReactOnRails.register({ ConceptsIndex, });
