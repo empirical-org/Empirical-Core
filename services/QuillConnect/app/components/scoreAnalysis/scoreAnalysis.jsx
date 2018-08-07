28e36ad7d4565c8b9d888bd5dbe6322f4e005075
Emilia Friedberg <emiliafriedberg@gmail.com>
2018-08-07 10:38:56 -0400

    minor changes

--- services/QuillConnect/app/components/scoreAnalysis/scoreAnalysis.jsx
+++ services/QuillConnect/app/components/scoreAnalysis/scoreAnalysis.jsx
@@ -85,7 +85,7 @@ class ScoreAnalysis extends Component {
     return _.map(hashToCollection(questionData), (question) => {
       const scoreData = scoreAnalysis.data[question.key];
       if (scoreData) {
-        const percentageWeakResponses = Math.round(scoreData.common_unmatched_responses/scoreData.responses * 100)
+        const percentageWeakResponses = Math.round((scoreData.common_unmatched_responses/scoreData.responses) * 100)
         return {
           key: `${question.key}-${typeName}`,
           uid: question.key,
