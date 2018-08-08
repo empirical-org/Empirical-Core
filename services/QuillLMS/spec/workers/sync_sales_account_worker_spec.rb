73882dc9975708103d21c3d0ca4251665f6324fa
tronroy <tbconroy@gmail.com>
2018-08-08 14:51:28 -0400

    Change reference to renamed service class

--- services/QuillLMS/spec/workers/sync_sales_account_worker_spec.rb
+++ services/QuillLMS/spec/workers/sync_sales_account_worker_spec.rb
@@ -7,11 +7,11 @@ describe SyncSalesAccountWorker do
     let(:syncer) { double(:syncer, sync: true) }
 
     before do
-      allow(SalesAccountSyncer).to receive(:new) { syncer }
+      allow(SyncSalesAccount).to receive(:new) { syncer }
     end
 
     it 'should sync the account worker' do
-      expect(SalesAccountSyncer).to receive(:new).with(1)
+      expect(SyncSalesAccount).to receive(:new).with(1)
       expect(syncer).to receive(:sync)
       subject.perform(1)
     end
