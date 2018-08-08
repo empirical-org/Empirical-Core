9c003d194ea1a52fed991f72ed4c6a0118933138
tronroy <tbconroy@gmail.com>
2018-08-08 15:27:06 -0400

    Fix milestone worker spec

--- services/QuillLMS/app/workers/update_milestones_worker.rb
+++ services/QuillLMS/app/workers/update_milestones_worker.rb
@@ -5,6 +5,7 @@ class UpdateMilestonesWorker
     activity_session = ActivitySession.find_by_uid(activity_session_uid)
     return unless activity_session
     # more milestones can be added here as relevant, for now this just checks to see if a Completed Diagnostic milestone needs to be created
+
     if activity_session.state == 'finished' && activity_session.classroom_unit_id && activity_session.activity.activity_classification_id === 4
       teacher_milestones = activity_session.classroom_unit.unit.user.milestones
       if !teacher_milestones.find_by(name: 'Complete Diagnostic')
