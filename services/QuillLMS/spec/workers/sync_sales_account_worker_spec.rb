9516287d5e76de7016f484923fab9cb1bcdd28cc
tronroy <tbconroy@gmail.com>
2018-08-08 15:28:18 -0400

    Fix sync sales machine spec

--- services/QuillLMS/spec/workers/sync_sales_account_worker_spec.rb
+++ services/QuillLMS/spec/workers/sync_sales_account_worker_spec.rb
@@ -4,7 +4,7 @@ describe SyncSalesAccountWorker do
   let(:subject) { described_class.new }
 
   describe '#perform' do
-    let(:syncer) { double(:syncer, sync: true) }
+    let(:syncer) { double(:syncer, call: true) }
 
     before do
       allow(SyncSalesAccount).to receive(:new) { syncer }
@@ -12,7 +12,7 @@ describe SyncSalesAccountWorker do
 
     it 'should sync the account worker' do
       expect(SyncSalesAccount).to receive(:new).with(1)
-      expect(syncer).to receive(:sync)
+      expect(syncer).to receive(:call)
       subject.perform(1)
     end
   end
