aba6ed6d1090acb007c3527e4e9392a0221998da
Donald McKendrick <ddmckendrick@gmail.com>
2018-08-09 23:24:43 -0400

    Add mutation for adding concept

--- services/QuillLMS/app/graphql/types/mutation_type.rb
+++ services/QuillLMS/app/graphql/types/mutation_type.rb
@@ -1,8 +1,4 @@
 class Types::MutationType < Types::BaseObject
   # TODO: remove me
-  field :test_field, String, null: false,
-    description: "An example field added by the generator"
-  def test_field
-    "Hello World"
-  end
+  field :create_concept, mutation: Mutations::Concepts::Create
 end
