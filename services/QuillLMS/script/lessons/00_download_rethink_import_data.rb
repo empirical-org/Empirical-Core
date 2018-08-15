643fe896baa3ef327e6f9ecfa604078fae29b79c
Donald McKendrick <ddmckendrick@gmail.com>
2018-08-15 16:38:02 -0400

    fix those bugs

--- services/QuillLMS/script/lessons/00_download_rethink_import_data.rb
+++ services/QuillLMS/script/lessons/00_download_rethink_import_data.rb
@@ -2,7 +2,7 @@ require 'rubygems'
 require 'json'
 require 'HTTParty'
 
-lessons = HTTParty.get("#{ENV['FIREBASE_DATABASE_URL']}/v2/classroom_lessons.json")
+lessons = HTTParty.get("https://quillconnect.firebaseio.com/v2/classroom_lessons.json")
 lessons_with_ids = []
 lessons.each do |k, v|
   v['id'] = k
@@ -11,26 +11,32 @@ end
 file_path = Rails.root.join('tmp', 'lessons.json')
 File.write(file_path, lessons_with_ids.to_json)
 
-edition_metadata = HTTParty.get("#{ENV['FIREBASE_DATABASE_URL']}/v2/lesson_edition_metadata.json")
+edition_metadata = HTTParty.get("https://quillconnect.firebaseio.com/v2/lesson_edition_metadata.json")
 metadata_with_ids = []
 edition_metadata.each do |k, v|
   v['id'] = k
   metadata_with_ids.push(v)
 end
 file_path = Rails.root.join('tmp', 'metadata.json')
+File.write(file_path, metadata_with_ids.to_json)
 
-edition_questions = HTTParty.get("#{ENV['FIREBASE_DATABASE_URL']}/v2/lesson_edition_questions.json")
+edition_questions = HTTParty.get("https://quillconnect.firebaseio.com/v2/lesson_edition_questions.json")
 edition_questions_with_ids = []
 edition_questions.each do |k, v|
   v['id'] = k
   edition_questions_with_ids.push(v)
 end
 file_path = Rails.root.join('tmp', 'edition_questions.json')
+File.write(file_path, edition_questions_with_ids.to_json)
 
-reviews = HTTParty.get("#{ENV['FIREBASE_DATABASE_URL']}/v2/reviews.json")
+reviews = HTTParty.get("https://quillconnect.firebaseio.com/v2/reviews.json")
+# puts reviews.length
+# puts reviews.first
+# puts reviews
 reviews_with_ids = []
 reviews.each do |k, v|
   v['id'] = k
   reviews_with_ids.push(v)
 end
 file_path = Rails.root.join('tmp', 'reviews.json')
+File.write(file_path, reviews_with_ids.to_json)
