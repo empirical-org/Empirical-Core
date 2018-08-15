34237d78ac501c65ed975e3dd58031744973b4f8
Donald McKendrick <ddmckendrick@gmail.com>
2018-08-15 11:21:47 -0400

    Update specs

--- services/QuillLessons/test/libs/conceptResultsFromClassroomLesson.test.js
+++ services/QuillLessons/test/libs/conceptResultsFromClassroomLesson.test.js
@@ -82,7 +82,7 @@ describe('Getting concept results from a Classroom lesson session', () => {
     expect(Object.keys(result).length).toEqual(2);
     expect(Object.keys(result[4]).length).toEqual(3);
     expect(result[4][23663289]).toEqual({
-      concept_uid: 'X37oyfiNxSphA34npOb-Ig',
+      concept_id: 'X37oyfiNxSphA34npOb-Ig',
       question_type: 'lessons-slide',
       metadata: {
         correct: 1,
@@ -93,12 +93,12 @@ describe('Getting concept results from a Classroom lesson session', () => {
       },
     });
     expect(result[4][23663289].metadata).toEqual({
-        correct: 1,
-        directions: qdata[4].data.play.instructions,
-        prompt: qdata[4].data.play.prompt,
-        answer: subs[4][23663289].data,
-        attemptNumber: 1,
-      },
+      correct: 1,
+      directions: qdata[4].data.play.instructions,
+      prompt: qdata[4].data.play.prompt,
+      answer: subs[4][23663289].data,
+      attemptNumber: 1,
+    },
     );
   });
 
@@ -109,7 +109,7 @@ describe('Getting concept results from a Classroom lesson session', () => {
     const results = embedActivitySessionUIDInConceptResult(conceptResults);
     expect(results[4][23663289]).toEqual({
       activity_session_uid: '23663289',
-      concept_uid: 'X37oyfiNxSphA34npOb-Ig',
+      concept_id: 'X37oyfiNxSphA34npOb-Ig',
       question_type: 'lessons-slide',
       metadata: {
         correct: 1,
@@ -123,7 +123,7 @@ describe('Getting concept results from a Classroom lesson session', () => {
 
     expect(results[5][23663289]).toEqual({
       activity_session_uid: '23663289',
-      concept_uid: 'X37oyfiNxSphA34npOb-Ig',
+      concept_id: 'X37oyfiNxSphA34npOb-Ig',
       question_type: 'lessons-slide',
       metadata: {
         correct: 1,
@@ -140,9 +140,8 @@ describe('Getting concept results from a Classroom lesson session', () => {
     const qdata = Object.assign({}, questions);
     const subs = Object.assign({}, submissions);
     const results = generate(qdata, subs);
-    expect(results.length).toEqual(6)
-  })
+    expect(results.length).toEqual(6);
+  });
 });
 
-
-const fullReturnValue = [ { activity_session_uid: '23663289', concept_uid: 'X37oyfiNxSphA34npOb-Ig', metadata: { answer: 'The football star leaped toward the end zone, but he did not score a touchdown.', attemptNumber: 1, correct: 1, directions: 'Combine the sentences using one of the joining words.', prompt: '<p>The football star leaped toward the end zone.&nbsp;</p>\n<p>He did not score a touchdown.</p>', questionNumber: 1 }, question_type: 'lessons-slide' }, { activity_session_uid: '23663290', concept_uid: 'X37oyfiNxSphA34npOb-Ig', metadata: { answer: 'The footballstarleaped tourd hend zone,but he did not score a tuch down.', attemptNumber: 1, correct: 1, directions: 'Combine the sentences using one of the joining words.', prompt: '<p>The football star leaped toward the end zone.&nbsp;</p>\n<p>He did not score a touchdown.</p>', questionNumber: 1 }, question_type: 'lessons-slide' }, { activity_session_uid: '23663291', concept_uid: 'X37oyfiNxSphA34npOb-Ig', metadata: { answer: 'The football star leaped toward the end zone, but he did not score a touchdown.', attemptNumber: 1, correct: 1, directions: 'Combine the sentences using one of the joining words.', prompt: '<p>The football star leaped toward the end zone.&nbsp;</p>\n<p>He did not score a touchdown.</p>', questionNumber: 1 }, question_type: 'lessons-slide' }, { activity_session_uid: '23663289', concept_uid: 'X37oyfiNxSphA34npOb-Ig', metadata: { answer: 'The quarterback was fast, so the other players couldn\'t catch him.', attemptNumber: 1, correct: 1, directions: 'Combine the sentences using a joining word.', prompt: '<p>The quarterback was fast.&nbsp;</p>\n<p>The other players couldn’t catch him.&nbsp;</p>', questionNumber: 2 }, question_type: 'lessons-slide' }, { activity_session_uid: '23663290', concept_uid: 'X37oyfiNxSphA34npOb-Ig', metadata: { answer: 'The quarterback was fast,so the other players couldn \'t catch him.', attemptNumber: 1, correct: 1, directions: 'Combine the sentences using a joining word.', prompt: '<p>The quarterback was fast.&nbsp;</p>\n<p>The other players couldn’t catch him.&nbsp;</p>', questionNumber: 2 }, question_type: 'lessons-slide' }, { activity_session_uid: '23663291', concept_uid: 'X37oyfiNxSphA34npOb-Ig', metadata: { answer: 'The quarterback was fast, and the other players couldn\'t catch him.', attemptNumber: 1, correct: 1, directions: 'Combine the sentences using a joining word.', prompt: '<p>The quarterback was fast.&nbsp;</p>\n<p>The other players couldn’t catch him.&nbsp;</p>', questionNumber: 2 }, question_type: 'lessons-slide' } ]
+const fullReturnValue = [{ activity_session_uid: '23663289', concept_id: 'X37oyfiNxSphA34npOb-Ig', metadata: { answer: 'The football star leaped toward the end zone, but he did not score a touchdown.', attemptNumber: 1, correct: 1, directions: 'Combine the sentences using one of the joining words.', prompt: '<p>The football star leaped toward the end zone.&nbsp;</p>\n<p>He did not score a touchdown.</p>', questionNumber: 1, }, question_type: 'lessons-slide', }, { activity_session_uid: '23663290', concept_uid: 'X37oyfiNxSphA34npOb-Ig', metadata: { answer: 'The footballstarleaped tourd hend zone,but he did not score a tuch down.', attemptNumber: 1, correct: 1, directions: 'Combine the sentences using one of the joining words.', prompt: '<p>The football star leaped toward the end zone.&nbsp;</p>\n<p>He did not score a touchdown.</p>', questionNumber: 1, }, question_type: 'lessons-slide', }, { activity_session_uid: '23663291', concept_uid: 'X37oyfiNxSphA34npOb-Ig', metadata: { answer: 'The football star leaped toward the end zone, but he did not score a touchdown.', attemptNumber: 1, correct: 1, directions: 'Combine the sentences using one of the joining words.', prompt: '<p>The football star leaped toward the end zone.&nbsp;</p>\n<p>He did not score a touchdown.</p>', questionNumber: 1, }, question_type: 'lessons-slide', }, { activity_session_uid: '23663289', concept_uid: 'X37oyfiNxSphA34npOb-Ig', metadata: { answer: 'The quarterback was fast, so the other players couldn\'t catch him.', attemptNumber: 1, correct: 1, directions: 'Combine the sentences using a joining word.', prompt: '<p>The quarterback was fast.&nbsp;</p>\n<p>The other players couldn’t catch him.&nbsp;</p>', questionNumber: 2, }, question_type: 'lessons-slide', }, { activity_session_uid: '23663290', concept_uid: 'X37oyfiNxSphA34npOb-Ig', metadata: { answer: 'The quarterback was fast,so the other players couldn \'t catch him.', attemptNumber: 1, correct: 1, directions: 'Combine the sentences using a joining word.', prompt: '<p>The quarterback was fast.&nbsp;</p>\n<p>The other players couldn’t catch him.&nbsp;</p>', questionNumber: 2, }, question_type: 'lessons-slide', }, { activity_session_uid: '23663291', concept_uid: 'X37oyfiNxSphA34npOb-Ig', metadata: { answer: 'The quarterback was fast, and the other players couldn\'t catch him.', attemptNumber: 1, correct: 1, directions: 'Combine the sentences using a joining word.', prompt: '<p>The quarterback was fast.&nbsp;</p>\n<p>The other players couldn’t catch him.&nbsp;</p>', questionNumber: 2, }, question_type: 'lessons-slide', }];
