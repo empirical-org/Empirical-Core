9d93851b1b0077b542bc421dddcb4d5f9ce3c2bd
Emilia Friedberg <emiliafriedberg@gmail.com>
2018-08-15 18:02:13 -0400

    add remaining question action types

--- services/QuillGrammar/src/reducers/questionsReducer.ts
+++ services/QuillGrammar/src/reducers/questionsReducer.ts
@@ -14,8 +14,79 @@ export default (
     action: Action,
 ) => {
     switch (action.type) {
+      let newstate
         case ActionTypes.RECEIVE_QUESTIONS_DATA:
           return Object.assign({}, currentState, { data: action.data}, {hasreceiveddata: true})
+        case ActionTypes.AWAIT_NEW_QUESTION_RESPONSE:
+          return Object.assign({}, currentState, {
+            submittingnew: true,
+          });
+        case ActionTypes.RECEIVE_NEW_QUESTION_RESPONSE:
+          return Object.assign({}, currentState, {
+            submittingnew: false,
+            newConceptModalOpen: false,
+          });
+        case ActionTypes.START_QUESTION_EDIT:
+          newstate = _.cloneDeep(currentState);
+          newstate.states[action.qid] = ActionTypes.EDITING_QUESTION;
+          return newstate;
+        case ActionTypes.FINISH_QUESTION_EDIT:
+          newstate = _.cloneDeep(currentState);
+          delete newstate.states[action.qid];
+          return newstate;
+        case ActionTypes.SUBMIT_QUESTION_EDIT:
+          newstate = _.cloneDeep(currentState);
+          newstate.states[action.qid] = ActionTypes.SUBMITTING_QUESTION;
+          return newstate;
+        case ActionTypes.TOGGLE_NEW_QUESTION_MODAL:
+          return Object.assign({}, currentState, {
+            newConceptModalOpen: !currentState.newConceptModalOpen,
+          });
+        case ActionTypes.START_RESPONSE_EDIT:
+          newstate = _.cloneDeep(currentState);
+          newstate.states[action.qid] = `${ActionTypes.START_RESPONSE_EDIT}_${action.rid}`;
+          return newstate;
+        case ActionTypes.FINISH_RESPONSE_EDIT:
+          newstate = _.cloneDeep(currentState);
+          delete newstate.states[action.qid];
+          return newstate;
+        case ActionTypes.START_CHILD_RESPONSE_VIEW:
+          newstate = _.cloneDeep(currentState);
+          newstate.states[action.qid] = `${ActionTypes.START_CHILD_RESPONSE_VIEW}_${action.rid}`;
+          return newstate;
+        case ActionTypes.CANCEL_CHILD_RESPONSE_VIEW:
+          newstate = _.cloneDeep(currentState);
+          delete newstate.states[action.qid];
+          return newstate;
+        case ActionTypes.START_FROM_RESPONSE_VIEW:
+          newstate = _.cloneDeep(currentState);
+          newstate.states[action.qid] = `${ActionTypes.START_FROM_RESPONSE_VIEW}_${action.rid}`;
+          return newstate;
+        case ActionTypes.CANCEL_FROM_RESPONSE_VIEW:
+          newstate = _.cloneDeep(currentState);
+          delete newstate.states[action.qid];
+          return newstate;
+        case ActionTypes.START_TO_RESPONSE_VIEW:
+          newstate = _.cloneDeep(currentState);
+          newstate.states[action.qid] = `${ActionTypes.START_TO_RESPONSE_VIEW}_${action.rid}`;
+          return newstate;
+        case ActionTypes.CANCEL_TO_RESPONSE_VIEW:
+          newstate = _.cloneDeep(currentState);
+          delete newstate.states[action.qid];
+          return newstate;
+        case ActionTypes.SUBMIT_RESPONSE_EDIT:
+          newstate = _.cloneDeep(currentState);
+          newstate.states[action.qid] = ActionTypes.SUBMITTING_RESPONSE;
+          return newstate;
+        case ActionTypes.SHOULD_RELOAD_RESPONSES:
+          newstate = _.cloneDeep(currentState);
+          newstate.states[action.qid] = ActionTypes.SHOULD_RELOAD_RESPONSES;
+          return newstate;
+        case ActionTypes.CLEAR_QUESTION_STATE:
+          newstate = _.cloneDeep(currentState);
+          delete newstate.states[action.qid];
+          return newstate;
+
         case ActionTypes.NO_QUESTIONS_FOUND:
             return Object.assign({}, currentState, { error: 'No questions found.'})
         default:
