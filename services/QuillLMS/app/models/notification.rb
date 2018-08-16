41be77dc80f690a944e279a764b335eef96355fa
tronroy <tbconroy@gmail.com>
2018-08-16 19:57:11 -0400

    Update notification model

--- services/QuillLMS/app/models/notification.rb
+++ services/QuillLMS/app/models/notification.rb
@@ -1,3 +1,6 @@
 class Notification < ActiveRecord::Base
+  belongs_to :user
+
   validates :text, presence: true, length: { maximum: 500 }
+  validates :user, presence: true
 end
--- services/QuillLMS/spec/models/notification_spec.rb
+++ services/QuillLMS/spec/models/notification_spec.rb
@@ -3,4 +3,6 @@ require 'rails_helper'
 RSpec.describe Notification do
   it { should validate_length_of(:text).is_at_most(500) }
   it { should validate_presence_of(:text) }
+  it { should belong_to(:user) }
+  it { should validate_presence_of(:user) }
 end
