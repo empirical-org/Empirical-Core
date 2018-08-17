0f8557de8748fddaad8a6b0526785f9508ca445c
Donald McKendrick <ddmckendrick@gmail.com>
2018-08-16 20:22:13 -0400

    Add Edit Concept mutation

--- services/QuillLMS/app/graphql/types/mutation_type.rb
+++ services/QuillLMS/app/graphql/types/mutation_type.rb
@@ -1,4 +1,5 @@
 class Types::MutationType < Types::BaseObject
   # TODO: remove me
   field :create_concept, mutation: Mutations::Concepts::Create
+  field :edit_concept, mutation: Mutations::Concepts::Edit
 end
