d1864ba6c25ea9a8d05abfbea24dcdc257f2b890
Donald McKendrick <ddmckendrick@gmail.com>
2018-08-21 11:52:18 -0400

    Filter live and archived concepts

--- services/QuillLMS/client/app/bundles/Staff/containers/ConceptsIndex.tsx
+++ services/QuillLMS/client/app/bundles/Staff/containers/ConceptsIndex.tsx
@@ -2,6 +2,8 @@ import * as React from "react";
 import { Query } from "react-apollo";
 import gql from "graphql-tag";
 import ConceptsTable from "../components/ConceptsTable";
+import RadioGroup from "../../../../node_modules/antd/lib/radio/group";
+import RadioButton from "../../../../node_modules/antd/lib/radio/radioButton";
 
 const conceptsIndexQuery:string = `
   {
@@ -9,6 +11,7 @@ const conceptsIndexQuery:string = `
       id
       name
       createdAt
+      visible
       parent {
         id
         name
@@ -25,6 +28,7 @@ export interface Concept {
   name:string;
   createdAt?:number;
   parent?:Concept;
+  visible?:Boolean
 }
 interface QueryResult {
   concepts: Array<Concept>
@@ -35,22 +39,36 @@ interface QueryResult {
 class App extends React.Component {
   constructor(props){
     super(props)
+
+    this.state = {
+      visible: true
+    }
   }
+
+  sey
 
   render() {
     return  (
-      <Query
-        query={gql(conceptsIndexQuery)}
-      >
-        {({ loading, error, data }) => {
-          if (loading) return <p>Loading...</p>;
-          if (error) return <p>Error :(</p>;
+      <div>
+        <h3>Concepts</h3>
+        <RadioGroup onChange={(e) => this.setState({visible: e.target.value})} defaultValue={this.state.visible}>
+          <RadioButton value={true}>Live</RadioButton>
+          <RadioButton value={false}>Archived</RadioButton>
+        </RadioGroup>
+        <Query
+          query={gql(conceptsIndexQuery)}
+        >
+          {({ loading, error, data }) => {
+            if (loading) return <p>Loading...</p>;
+            if (error) return <p>Error :(</p>;
 
-          return (
-            <ConceptsTable concepts={data.concepts}/>
-          )
-        }}
-      </Query>
+            return (
+              <ConceptsTable concepts={data.concepts} visible={this.state.visible}/>
+            )
+          }}
+        </Query>
+      </div>
+      
     )
   }
   
