f42841fd1f4e593bab212583253382e6323f5122
tronroy <tbconroy@gmail.com>
2018-08-21 11:13:38 -0400

    Only create data needed to pass feature spec (testing 101)

--- services/QuillLMS/spec/cypress/tests/features/teacher_can_delete_activity_from_unit.js
+++ services/QuillLMS/spec/cypress/tests/features/teacher_can_delete_activity_from_unit.js
@@ -5,12 +5,47 @@ describe('Teacher clicks the "x" next to an activity in unit', () => {
 
   it('they see it removed', () => {
     cy.appFactories([
-      ['create', 'teacher', 'with_classrooms_students_and_activities', {
+      ['create', 'simple_user', {
+        id: 101,
+        role: 'teacher',
         password: 'password',
-        email: 'someone@gmail.com'
+        email: 'someone@gmail.com',
+      }],
+      ['create', 'simple_classroom', {
+        id: 101
+      }],
+      ['create', 'classrooms_teacher', {
+        classroom_id: 101,
+        user_id: 101,
+        role: 'owner'
+      }],
+      ['create', 'simple_activity', {
+        id: 101,
+        name: 'Cool Activity'
+      }],
+      ['create', 'simple_unit', {
+        name: 'Monkey With Typewriters',
+        id: 101,
+        user_id: 101
+      }],
+      ['create', 'unit_activity', {
+        unit_id: 101,
+        activity_id: 101,
+      }],
+      ['create', 'classroom_unit', {
+        classroom_id: 101,
+        unit_id: 101,
       }]
     ])
+
     cy.login('someone@gmail.com', 'password')
-    cy.visit('/teachers/classrooms/activity_planner')
+    cy.visit('/')
+
+    cy.contains('My Activities').click()
+    cy.contains('Cool Activity').should('be.visible')
+
+    cy.get('.delete-classroom-activity').click()
+
+    cy.contains('Cool Activity').should('not.be.visible')
   })
 })
--- services/QuillLMS/spec/cypress/tests/features/teacher_can_edit_unit_name.js
+++ services/QuillLMS/spec/cypress/tests/features/teacher_can_edit_unit_name.js
@@ -5,9 +5,33 @@ describe('Teacher changes the name of an existing unit', () => {
 
   it('they see the change persisted', () => {
     cy.appFactories([
-      ['create', 'teacher', 'with_classrooms_students_and_activities', {
+      ['create', 'simple_user', {
+        id: 101,
+        role: 'teacher',
         password: 'password',
         email: 'someone@gmail.com'
+      }],
+      ['create', 'simple_activity', {
+        id: 101,
+        name: 'Cool Activity',
+      }],
+      ['create', 'simple_unit', {
+        id: 101,
+        name: 'Cool Unit',
+        user_id: 101,
+      }],
+      ['create', 'unit_activity', {
+        unit_id: 101,
+        activity_id: 101,
+      }],
+      ['create', 'simple_classroom', { id: 101 }],
+      ['create', 'classrooms_teacher', {
+        classroom_id: 101,
+        user_id: 101,
+      }],
+      ['create', 'classroom_unit', {
+        classroom_id: 101,
+        unit_id: 101
       }]
     ])
     cy.login('someone@gmail.com', 'password')
