c63918b970641fd3800b7cc3ae5cb9f2beedd9ee
Donald McKendrick <ddmckendrick@gmail.com>
2018-08-23 19:01:06 -0400

    change error hanfling of auth

--- services/QuillLessonsServer/src/handlers/authorization.js
+++ services/QuillLessonsServer/src/handlers/authorization.js
@@ -2,42 +2,63 @@ function _isPreviewSession(data) {
   const previewIdRegExp = RegExp('^prvw\-.+$');
   return previewIdRegExp.test(data.classroomUnitId);
 }
-
-function _checkUserRole(permittedRoles, currentRole) {
-  const authorizedRole = permittedRoles.includes(currentRole)
-  const previewSession = _isPreviewSession(data)
-
-  if (previewSession || authorizedRole) {
-    return true
+ 
+function _isRoleAuthorized(permittedRoles, currentRole) {
+  return permittedRoles.includes(currentRole);
+}
+ 
+function _belongsToSession(data, token) {
+  const regexedClassroomUnitId = new RegExp('^' + token.data.classroom_unit_id)
+  return regexedClassroomUnitId.test(data.classroomSessionId);
+}
+ 
+function _reportError(errorText, data, token, client) {
+  console.error({ error: errorText, data, token });
+  client.emit(errorText, { data, token });
+}
+ 
+function _checkToken(data, token, callback) {
+  if (token.isValid) {
+    callback();
+  } else {
+    _reportError('invalidToken', data, token, client);
   }
-
-  let error  = Error.new('User role is not authorized for this action');
-  error.name = 'UnauthorizedUserRoleError';
-  throw error;
 }
-
-function _checkUserSession(data, token) {
-  const ClassroomUnitIdRegExp = new RegExp('^' + token.data.classroom_unit_id);
-  const sessionAuthorized     = ClassroomUnitIdRegExp.test(data.classroomSessionId);
-  const previewSession        = _isPreviewSession(data)
-
-  if (previewSession || sessionAuthorized) {
-    return true
-  }
-
-  let error  = Error.new('User classroom session is not authorized for this action');
-  error.name = 'UnauthorizedUserSessionError';
-  throw error;
+ 
+export function authorizeSession(data, token, client, callback) {
+  _checkToken(data, token, () => {
+    const belongsToSession = _belongsToSession(data, token)
+ 
+    if (belongsToSession || _isPreviewSession(data)) {
+      callback();
+    } else {
+      _reportError('unauthorizedSession', data, token, client);
+    }
+  });
 }
-
-export function authorizeSession(data, token, callback) {
-  _checkUserSession(data, token);
-
-  callback();
+ 
+export function authorizeTeacherSession(data, token, client, callback) {
+  _checkToken(data, token, () => {
+    const userIsTeacher    = _isRoleAuthorized(['staff', 'teacher'], token.data.role);
+    const belongsToSession = _belongsToSession(data, token);
+    const isValidSession   = userIsTeacher && belongsToSession;
+ 
+    if (isValidSession || _isPreviewSession(data)) {
+      callback();
+    } else {
+      _reportError('unauthorizedTeacherSession', data, token, client);
+    }
+  });
 }
-
-export function authorizeRole(permittedRoles, data, token, callback) {
-  _checkUserRole(permittedRoles, token.data.role);
-
-  callback();
+ 
+export function authorizeRole(permittedRoles, data, token, client, callback) {
+  _checkToken(token, () => {
+    const isRoleAuthorized = _isRoleAuthorized(permittedRoles, token.data.role);
+ 
+    if (isRoleAuthorized || _isPreviewSession(data)) {
+      callback();
+    } else {
+      _reportError('unauthorizedRole', data, token, client);
+    }
+  });
 }
