7aca0ad5a1fdec485553627f4dbb05258426fbb8
tronroy <tbconroy@gmail.com>
2018-08-27 17:15:12 -0400

    Style the teacher dashboard component

--- services/QuillLMS/client/app/bundles/Teacher/components/dashboard/notification_feed.jsx
+++ services/QuillLMS/client/app/bundles/Teacher/components/dashboard/notification_feed.jsx
@@ -4,11 +4,19 @@ import Notification from './notification';
 const NotificationFeed = ({ notifications }) => {
   if (notifications) {
     return(
-      <ul>
-        {notifications.map((notification) => <li>
-          <Notification {...notification} />
-        </li>)}
-      </ul>
+      <div className="mini_container results-overview-mini-container col-md-4 col-sm-5 text-center">
+        <div className="mini_content">
+          <div className="gray-underline">
+            <h4>Notification Feed</h4>
+          </div>
+          <ul style={{'list-style': 'none', padding: '5px'}}>
+            {notifications.map((notification) => <li>
+              <Notification {...notification} />
+              <hr style={{margin: '5px'}}></hr>
+            </li>)}
+          </ul>
+        </div>
+      </div>
     );
   }
 
