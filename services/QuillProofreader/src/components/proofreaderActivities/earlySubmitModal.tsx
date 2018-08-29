eda59efc909106c2cf1653f6da2c89705080a8a3
Emilia Friedberg <emiliafriedberg@gmail.com>
2018-08-29 17:00:11 -0400

    add early submit modal

---
+++ services/QuillProofreader/src/components/proofreaderActivities/earlySubmitModal.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react'
+
+// const ProjectorModal: React.SFC<{closeModal: ((event: MouseEvent) => void), requiredEditCount:number}> = (props) => {
+const ProjectorModal: React.SFC<{closeModal: any, requiredEditCount:number}> = (props) => {
+ return <div className="early-submit-modal-container">
+    <div className="early-submit-modal-background" />
+     <div className="early-submit-modal">
+       <div className="top-section">
+          <h1>Keep Trying!</h1>
+          <p>You must make at least {props.requiredEditCount} edits.</p>
+          <button onClick={props.closeModal}>Find Edits</button>
+        </div>
+        </div>
+     </div>
+}
+
+export default ProjectorModal
