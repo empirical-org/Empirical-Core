522c697dcf37c025bfb87b548cc7c1baf9ae5e67
Max Buck <maxwellhigginsbuck@gmail.com>
2018-08-31 10:36:46 -0400

    add queries/progress_reports/real_time.rb to add this progress report

---
+++ services/QuillLMS/app/queries/progress_reports/real_time.rb
@@ -0,0 +1,29 @@
+class ProgressReports::RealTime
+  def self.results(student_ids)
+    ids = student_ids.join(', ')
+    ActiveRecord::Base.connection.execute(query(ids)).to_a
+  end
+
+  private
+
+  def self.query(classroom_ids)
+    <<~SQL
+      SELECT classrooms.name AS classroom_name,
+        students.id AS student_id,students.last_active,
+        students.name,
+        AVG(activity_sessions.percentage)
+        FILTER(WHERE activities.activity_classification_id <> 6 AND activities.activity_classification_id <> 4) AS average_score,
+        COUNT(activity_sessions.id) AS activity_count,
+        classrooms.id AS classroom_id
+      FROM classroom_units
+      JOIN activity_sessions ON classroom_units.id = activity_sessions.classroom_unit_id
+      JOIN activities ON activity_sessions.activity_id = activities.id
+      JOIN classrooms ON classrooms.id = classroom_units.classroom_id
+      JOIN users AS students ON students.id = activity_sessions.user_id
+      WHERE classroom_units.classroom_id IN (#{classroom_ids})
+      AND activity_sessions.is_final_score = TRUE
+      AND classroom_units.visible = true
+      GROUP BY classrooms.name, students.id, students.name, classrooms.id, last_active
+    SQL
+  end
+end
