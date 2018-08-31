97d635d6ef258d978873caf0679b87a471decd3c
Emilia Friedberg <emiliafriedberg@gmail.com>
2018-08-31 15:38:29 -0400

    get review page displaying correctly

--- services/QuillProofreader/src/components/proofreaderActivities/reviewModal.tsx
+++ services/QuillProofreader/src/components/proofreaderActivities/reviewModal.tsx
@@ -1,12 +1,12 @@
 import * as React from 'react'
 
-const ReviewModal: React.SFC<{closeModal: any, numberOfErrors:number, numberOfCorrectEdits:number}> = (props) => {
+const ReviewModal: React.SFC<{closeModal: any, numberOfErrors:number, numberOfCorrectChanges:number}> = (props) => {
  return <div className="review-modal-container">
     <div className="review-modal-background" />
      <div className="review-modal">
        <div className="top-section">
          <h1>Good work!</h1>
-          <p>You found {props.numberOfCorrectEdits} of {props.numberOfErrors} errors.</p>
+          <p>You found {props.numberOfCorrectChanges} of {props.numberOfErrors} errors.</p>
           <button onClick={props.closeModal}>Review Your Work</button>
         </div>
         </div>
