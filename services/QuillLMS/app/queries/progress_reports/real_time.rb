594f7d98d8a0510461aa7bbab444b839dc596813
Max Buck <maxwellhigginsbuck@gmail.com>
2018-08-31 17:18:17 -0400

    fix for time on current question

--- services/QuillLMS/app/queries/progress_reports/real_time.rb
+++ services/QuillLMS/app/queries/progress_reports/real_time.rb
@@ -14,7 +14,7 @@ class ProgressReports::RealTime
       SELECT name, activity_name, current_question, timespent_activity_session, last_interaction, timespent_question(activity_sess_id::int, current_question), activity_sess_id
       FROM (
 
-      SELECT name, activity_name, asil.meta ->> 'info' as current_question, timespent_activity_session, last_interaction, activity_sess_id FROM
+      SELECT name, activity_name, asil.meta ->> 'current_question' as current_question, timespent_activity_session, last_interaction, activity_sess_id FROM
       (SELECT users.id as user_id, users.name as name, activities.id as activity_id, activities.name as activity_name, timespent_activity_session(activity_sessions.id), max(activity_session_interaction_logs.date) as last_interaction, max(activity_session_interaction_logs.id) as asil_id, activity_sessions.id as activity_sess_id
         FROM users
         join activity_sessions on activity_sessions.user_id = users.id
