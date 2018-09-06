adc93f258c1a18478d3abc18e3a140de40d1f225
Max Buck <maxwellhigginsbuck@gmail.com>
2018-09-06 11:08:04 -0400

    remove old fish bones

--- services/QuillConnect/app/libs/sendActivitySessionInteractionLog.js
+++ services/QuillConnect/app/libs/sendActivitySessionInteractionLog.js
@@ -1,21 +1,16 @@
 export function sendActivitySessionInteractionLog(activitySessionUID, data = {}) {
   if (activitySessionUID === 'null' || activitySessionUID == null) {
-    return null; // no need to procede
+    return null;
   }
   const url = `${process.env.EMPIRICAL_BASE_URL}/api/v1/activity_sessions/${activitySessionUID}/activity_session_interaction_logs`;
   fetch(url, {
-    method: 'POST', // or 'PUT'
+    method: 'POST',
     mode: 'cors',
     credentials: 'include',
-    body: JSON.stringify({ meta: data, }), // data can be `string` or {object}!
+    body: JSON.stringify({ meta: data, }),
     headers: {
       'Content-Type': 'application/json',
       'Accept': 'application/json',
     },
   });
-  /*
-   *.then(res => res.json())
-   *.then(response => console.log('Success:', response))
-   *.catch(error => console.error('Error:', error));
-   */
 }
