b8c92582e369a9aa00e39d0fd78259c05470b6f3
Max Buck <maxwellhigginsbuck@gmail.com>
2018-09-10 14:12:45 -0400

    remove unnecessary envar; replace old envar in pages and use current string key name so that flash messages work again

--- services/QuillLMS/app/controllers/pages_controller.rb
+++ services/QuillLMS/app/controllers/pages_controller.rb
@@ -18,8 +18,8 @@ class PagesController < ApplicationController
     end
     @title = 'Quill.org — Interactive Writing and Grammar'
     @description = 'Quill provides free writing and grammar activities for middle and high school students.'
-    if request.env[ReferrerUser::ENV_VARIABLE_NAME]
-      name = ReferrerUser.find_by(referral_code: request.env[ReferrerUser::ENV_VARIABLE_NAME])&.user&.name
+    if request.env['affiliate.tag']
+      name = ReferrerUser.find_by(referral_code: request.env['affiliate.tag'])&.user&.name
       flash.now[:info] = "<strong>#{name}</strong> invited you to help your students become better writers with Quill!" if name
     end
   end
