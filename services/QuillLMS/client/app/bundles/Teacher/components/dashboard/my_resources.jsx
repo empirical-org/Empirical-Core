7d1fc23ae057ec09d2eecfabb59a593795e7a82e
Donald McKendrick <ddmckendrick@gmail.com>
2018-09-21 14:36:14 -0400

    lint

--- services/QuillLMS/client/app/bundles/Teacher/components/dashboard/my_resources.jsx
+++ services/QuillLMS/client/app/bundles/Teacher/components/dashboard/my_resources.jsx
@@ -1,5 +1,3 @@
-
-
 import React from 'react';
 import TeacherResourcesMini from './teacher_resources_mini';
 import GoogleClassroomMini from './google_classroom_mini';
@@ -8,23 +6,23 @@ import TeacherBestPracticesMini from './teacher_best_practices_mini';
 export default React.createClass({
 
   createMinis() {
-     return (
-      <div>
-        <TeacherResourcesMini  />
-        <TeacherBestPracticesMini  />
-        <GoogleClassroomMini  />
+    return (
+       <div>
+        <TeacherResourcesMini />
+        <TeacherBestPracticesMini />
+        <GoogleClassroomMini />
       </div>);
-   },
+  },
 
   render() {
-     return (
-      <div className="dashboard-section-container">
+    return (
+       <div className="dashboard-section-container">
         <h3 className="dashboard-header">My Resources</h3>
         <div className="row">
           {this.createMinis()}
         </div>
       </div>
-    );
-   },
+     );
+  },
 
 });
