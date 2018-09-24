2703c04390867bb7d39b7a20b6b091490ae18184
Emilia Friedberg <emiliafriedberg@gmail.com>
2018-09-24 14:17:19 -0400

    get structure of proofreader cms together

--- services/QuillProofreader/src/actions/proofreaderActivities.ts
+++ services/QuillProofreader/src/actions/proofreaderActivities.ts
@@ -3,6 +3,20 @@ import { ActionTypes } from './actionTypes'
 const activitiesRef = rootRef.child('passageProofreadings')
 import { ProofreaderActivities } from '../interfaces/proofreaderActivities'
 
+export const startListeningToActivities = () => {
+  return (dispatch:Function) => {
+    activitiesRef.on('value', (snapshot:any) => {
+      const activities: Array<ProofreaderActivities> = snapshot.val()
+      if (activities) {
+        dispatch({ type: ActionTypes.RECEIVE_PROOFREADER_ACTIVITIES_DATA, data: activities, });
+      } else {
+        dispatch({ type: ActionTypes.NO_PROOFREADER_ACTIVITIES_FOUND })
+      }
+    });
+
+  }
+}
+
 export const startListeningToActivity = (activityUID: string) => {
   return (dispatch: Function) => {
     activitiesRef.child(activityUID).on('value', (snapshot: any) => {
--- services/QuillProofreader/src/reducers/proofreaderActivitiesReducer.ts
+++ services/QuillProofreader/src/reducers/proofreaderActivitiesReducer.ts
@@ -1,10 +1,11 @@
 import { Action } from "redux";
 import { ActionTypes } from "../actions/actionTypes";
-import { ProofreaderActivity } from '../interfaces/proofreaderActivities'
+import { ProofreaderActivity, ProofreaderActivities } from '../interfaces/proofreaderActivities'
 
 export interface ProofreaderActivityState {
   hasreceiveddata: boolean;
   currentActivity: ProofreaderActivity;
+  data: ProofreaderActivities;
   error?: string;
 }
 
@@ -13,12 +14,17 @@ type ProofreaderActivityAction = Action & { data: ProofreaderActivity }
 export default (
     currentState = {hasreceiveddata: false},
     action: ProofreaderActivityAction,
+    data: {}
 ) => {
     switch (action.type) {
         case ActionTypes.RECEIVE_PROOFREADER_ACTIVITY_DATA:
             return Object.assign({}, currentState, { currentActivity: action.data }, {hasreceiveddata: true});
         case ActionTypes.NO_PROOFREADER_ACTIVITY_FOUND:
             return Object.assign({}, currentState, { error: 'No activity found.'})
+        case ActionTypes.RECEIVE_PROOFREADER_ACTIVITIES_DATA:
+          return Object.assign({}, currentState, { data: action.data}, {hasreceiveddata: true})
+        case ActionTypes.NO_PROOFREADER_ACTIVITIES_FOUND:
+            return Object.assign({}, currentState, { error: 'No activities found.'})
         default:
             return currentState;
     }
