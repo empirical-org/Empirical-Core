0e8382c5d454928d2fa3e47f87d6a528b1d5b13d
Emilia Friedberg <emiliafriedberg@gmail.com>
2018-09-26 14:12:05 -0400

    going with different approach for proofreader passage creation

--- services/QuillProofreader/src/components/lessons/passageCreator.tsx
+++ services/QuillProofreader/src/components/lessons/passageCreator.tsx
@@ -2,83 +2,99 @@ import { Editor } from 'slate-react'
 import Plain from 'slate-plain-serializer'
 import * as React from 'react'
 
-class HoverForm extends React.Component {
-  constructor(props) {
-    super(props)
-
-    this.state = {
-      displayText: '',
-      correctText: '',
-      visible: false
-    }
-
-    this.changeDisplayText = this.changeDisplayText.bind(this)
-    this.changeCorrectText = this.changeCorrectText.bind(this)
-  }
-
-  componentWillReceiveProps(nextProps) {
-    if (nextProps.value.selection.isExpanded) {
-      this.setState({ visible: true })
-    }
-    console.log('nextProps.value.selection.isExpanded', nextProps.value.selection.isExpanded)
-  }
-
-  style = () => {
-    let style = {}
-    const { visible } = this.state
-    if (!visible) return { display: 'none' }
-
-    const { value } = this.props
-    const { fragment, selection } = value
-
-    if (selection.isBlurred || selection.isCollapsed || fragment.text === '') {
-      return { display: 'none' }
-    }
-
-    const native = window.getSelection()
-    const range = native.getRangeAt(0)
-    const rect = range.getBoundingClientRect()
-    style.opacity = 1
-    style.top = `${rect.top + window.pageYOffset - style.offsetHeight}px`
-
-    style.left = `${rect.left +
-      window.pageXOffset -
-      style.offsetWidth / 2 +
-      rect.width / 2}px`
-
-    return style
-  }
-
-  onClickSubmit() {
-    const { value, onChange } = this.props
-    const edit = `{+${this.state.correctText}-${this.state.displayText}|}`
-    const change = value.change().insertText(edit)
-    onChange(change)
-    this.setState({ visible: false })
-  }
-
-  changeDisplayText(e) {
-    this.setState({ displayText: e.target.value })
-  }
-
-  changeCorrectText(e) {
-    this.setState({ correctText: e.target.value })
-  }
-
-  render() {
-    const { className } = this.props
-    const root = window.document.getElementById('root')
-
-    return <div className={className} style={this.style()}>
-      <label>Display Text</label>
-      <input onChange={this.changeDisplayText} value={this.state.displayText}/>
-      <label>Correct Text</label>
-      <input onChange={this.changeCorrectText} value={this.state.correctText}/>
-      <button onClick={this.onClickSubmit}>Submit</button>
-    </div>
-  }
-
-}
+// class HoverForm extends React.Component {
+//   constructor(props) {
+//     super(props)
+//
+//     this.state = {
+//       displayText: '',
+//       correctText: '',
+//       visible: false
+//     }
+//
+//     this.changeDisplayText = this.changeDisplayText.bind(this)
+//     this.changeCorrectText = this.changeCorrectText.bind(this)
+//     this.onClickSubmit = this.onClickSubmit.bind(this)
+//   }
+//
+//   componentWillReceiveProps(nextProps) {
+//     if (nextProps.value.selection.isExpanded && !this.state.value) {
+//       this.setState({ visible: true, value: nextProps.value.selection} )
+//       this.updateStyle()
+//     }
+//     console.log('nextProps.value.selection', nextProps.value.selection)
+//   }
+//
+//   // componentDidMount = () => {
+//   //   this.updateStyle()
+//   // }
+//   //
+//   // componentDidUpdate = () => {
+//   //   this.updateStyle()
+//   // }
+//
+//   updateStyle = () => {
+//     const form = document.getElementById('hover-form')
+//     if (!form) return
+//
+//     let style =  {}
+//
+//     const { value } = this.props
+//     const { fragment, selection } = value
+//
+//     // if (selection.isBlurred || selection.isCollapsed || fragment.text === '') {
+//     //   return { display: 'none' }
+//     // }
+//
+//     const native = window.getSelection()
+//     const range = native.getRangeAt(0)
+//     const rect = range.getBoundingClientRect()
+//     style.opacity = 1
+//     style.top = `${rect.top + window.pageYOffset - form.offsetHeight}px`
+//
+//     style.left = `${rect.left +
+//       window.pageXOffset -
+//       form.offsetWidth / 2 +
+//       rect.width / 2}px`
+//
+//     form.style = style
+//   }
+//
+//   onClickSubmit() {
+//     const { onChange } = this.props
+//     const edit = `{+${this.state.correctText}-${this.state.displayText}|}`
+//     const change = this.props.value.change().insertText(edit)
+//     onChange(change)
+//     this.setState({ visible: false, value: null, displayText: '', correctText: '' })
+//   }
+//
+//   changeDisplayText(e) {
+//     this.setState({ displayText: e.target.value })
+//   }
+//
+//   changeCorrectText(e) {
+//     this.setState({ correctText: e.target.value })
+//   }
+//
+//   render() {
+//     const { className } = this.props
+//     const root = window.document.getElementById('root')
+//     const style = this.state.visible ? null : { display: 'none '}
+//
+//     return <div className={className} id="hover-form" style={style}>
+//       <span>
+//         <label>Display Text</label>
+//         <input onChange={this.changeDisplayText} value={this.state.displayText}/>
+//       </span>
+//       <span>
+//         <label>Correct Text</label>
+//         <input onChange={this.changeCorrectText} value={this.state.correctText}/>
+//       </span>
+//       <button onClick={this.onClickSubmit}>Submit</button>
+//     </div>
+//   }
+//
+// }
 
 class PassageCreator extends React.Component {
   constructor(props) {
@@ -97,14 +113,10 @@ class PassageCreator extends React.Component {
   render() {
     return (
       <div>
-        <HoverForm
-          value={this.state.value}
-          onChange={this.onChange}
-        />
         <Editor
           value={this.state.value}
           onChange={this.onChange}
-          style={{minHeight: '50px', border: '1px solid black'}}
+          style={{minHeight: '100px', border: '1px solid black', padding: '10px'}}
         />
       </div>
     )
