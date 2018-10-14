3ec4ef12d014a1bc51ca6ef209da6f9a6d97a382
Donald McKendrick <ddmckendrick@gmail.com>
2018-10-13 17:19:05 -0700

    Add GraphQL Schema for Activity Model

---
+++ services/QuillLMS/app/graphql/types/activity_type.rb
@@ -0,0 +1,12 @@
+class Types::ActivityType < Types::BaseObject
+  graphql_name 'Activity'
+
+  field :id, ID, null: false
+  field :uid, String, null: false
+  field :name, String, null: false
+  field :description, String, null: false
+  field :activity_classification_id, ID, null: false
+  field :flags, [String], null: false
+  field :repeatable, Boolean, null: false
+
+end
