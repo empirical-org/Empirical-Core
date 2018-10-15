bb240b82037543acf8c6d50f2fa48e9ce136ae67
Donald McKendrick <ddmckendrick@gmail.com>
2018-10-15 11:29:15 -0700

    Get activity scores for current user in graphql

---
+++ services/QuillLMS/app/graphql/types/activity_score_type.rb
@@ -0,0 +1,12 @@
+class Types::ActivityScoreType < Types::BaseObject
+  graphql_name 'ActivityScore'
+
+  field :activity_id, Int, null: false
+  field :percentage, Float, null: false
+  field :updated_at, Int, null: false
+  field :in_progress, Boolean, null: false
+
+  def in_progress
+    object.resume_link == 1
+  end
+end
