b3bb1117356b8e0b04c2a53f62cb15abe427d31a
Donald McKendrick <ddmckendrick@gmail.com>
2018-10-15 11:29:15 -0700

    Get activity scores for current user in graphql

--- services/QuillLMS/app/graphql/types/user_type.rb
+++ services/QuillLMS/app/graphql/types/user_type.rb
@@ -10,8 +10,24 @@ class Types::UserType < Types::BaseObject
 
   field :notifications, [Types::NotificationType], null: true
 
+  field :activity_scores, [Types::ActivityScoreType], null: true
+
   def notifications
     object.notifications.order("created_at DESC").limit(10)
+  end
+
+  def activity_scores 
+    scores = ActivitySession.find_by_sql("
+      SELECT 
+        activity_sessions.activity_id as activity_id,
+        MAX(activity_sessions.percentage) as percentage,
+        MAX(activity_sessions.updated_at) AS updated_at,
+        SUM(CASE WHEN activity_sessions.state = 'started' THEN 1 ELSE 0 END) AS resume_link
+      FROM activity_sessions
+      
+      WHERE activity_sessions.user_id = #{object.id}
+      GROUP BY activity_sessions.activity_id
+    ")
   end
 
 end
