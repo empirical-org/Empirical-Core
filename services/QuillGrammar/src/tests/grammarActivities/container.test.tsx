b397362fb5915615118a6cc077ace8efaa4544b9
Emilia Friedberg <emiliafriedberg@gmail.com>
2018-10-17 15:34:26 -0400

    fix grammar tests

--- services/QuillGrammar/src/tests/grammarActivities/container.test.tsx
+++ services/QuillGrammar/src/tests/grammarActivities/container.test.tsx
@@ -1,13 +1,17 @@
 import * as React from "react";
 import {shallow} from "enzyme";
 import { PlayGrammarContainer } from "../../components/grammarActivities/container";
-import { grammarActivities, session } from './data'
+import { grammarActivities, session, conceptsFeedback } from './data'
+
+process.env.EMPIRICAL_BASE_URL = 'https://staging.quill.org'
+process.env.QUILL_CMS = 'https://cms.quill.org'
 
 describe("<PlayGrammarContainer />", () => {
   const wrapper = shallow(<PlayGrammarContainer
     dispatch={() => {}}
     grammarActivities={grammarActivities}
     session={session}
+    conceptsFeedback={conceptsFeedback}
    />)
 
     it("should render", () => {
