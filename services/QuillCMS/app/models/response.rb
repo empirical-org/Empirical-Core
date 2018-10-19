83b7bd112b3fdf934e33e9b2909d83e361cdbf35
Emilia Friedberg <emiliafriedberg@gmail.com>
2018-10-19 11:05:09 -0400

    wip

--- services/QuillCMS/app/models/response.rb
+++ services/QuillCMS/app/models/response.rb
@@ -9,7 +9,7 @@ class Response < ApplicationRecord
   settings index: { number_of_shards: 1 } do
     mappings dynamic: 'false' do
       indexes :text, type: 'string'
-      indexes :sortable_text, type: 'keyword'
+      indexes :sortable_text, type: 'keyword', index: "not_analyzed"
       indexes :id, type: 'integer'
       indexes :uid, type: 'string'
       indexes :question_uid, type: 'string', index: "not_analyzed"
