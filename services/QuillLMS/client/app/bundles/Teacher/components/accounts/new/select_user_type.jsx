ec8379fd17fab08cee5286729f1cf3f3b06009eb
Emilia Friedberg <emiliafriedberg@gmail.com>
2018-10-23 12:01:18 -0400

    select teacher or student page

--- services/QuillLMS/client/app/bundles/Teacher/components/accounts/new/select_user_type.jsx
+++ services/QuillLMS/client/app/bundles/Teacher/components/accounts/new/select_user_type.jsx
@@ -1,9 +1,11 @@
-import {
-  Link,
-} from 'react-router-dom';
-import React, { Component } from 'react';
+import * as React from 'react'
+import request from 'request'
+import getAuthToken from '../../../components/modules/get_auth_token'
 
-class SelectUserType extends Component {
+const studentPencilImg = `${process.env.CDN_URL}/images/onboarding/student-pencil.svg`
+const teacherChalkboardImg = `${process.env.CDN_URL}/images/onboarding/teacher-chalkboard.svg`
+
+class SelectUserType extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -17,55 +19,79 @@ class SelectUserType extends Component {
   setRoleOnSessionError() {
     alert('We had trouble setting your role. Please let us know if the problem persists.');
   }
+
   setStudentRoleOnSession() {
     this.setRoleOnSession('student');
   }
+
   setTeacherRoleOnSession() {
     this.setRoleOnSession('teacher');
   }
+
   setRoleOnSession(role) {
-    var that = this;
-    $.ajax({
-      type: 'POST',
-      url: '/account/role',
-      data: {
-        role: role,
-        authenticity_token: $('meta[name=csrf-token]').attr('content')
-      },
-      success: function () {
+    request.post(`${process.env.DEFAULT_URL}/account/role`, {
+      json: {
+        role,
+        authenticity_token: getAuthToken(),
+      }, }, (e) => {
+        if (e) {
+          this.setRoleOnSessionError()
+        } else {
           window.location = `/sign-up/${role}`;
-      },
-      error: this.setRoleOnSessionError
-    });
+        }
+      }
+    )
   }
 
   render () {
     return (
-      <div className='container account-form' id='sign-up'>
-        <div className='row sign_up_select_role'>
-            <div className='row'>
-              <h3 className='col-xs-12'>
-                Sign up for Quill as:
-              </h3>
+      <div id='user-type'>
+        <h1>Welcome! Let's create your account. Are you a student or a teacher?</h1>
+        <div className="cards">
+          <div className="card" onClick={this.setStudentRoleOnSession}>
+            <img src={studentPencilImg} />
+            <div className="text">
+              <h3>Student</h3>
+              <p>Select this option to join your teacher’s class and complete assigned activities.</p>
             </div>
-            <div className='option-wrapper'>
-              <Link to="#">
-                <button onClick={this.setTeacherRoleOnSession} className='button-green'>
-                  Educator
-                </button>
-              </Link>
-              <Link to="#">
-                <button onClick={this.setStudentRoleOnSession} className='button-green'>
-                  Student
-                </button>
-              </Link>
+          </div>
+          <div className="card" onClick={this.setTeacherRoleOnSession}>
+            <img src={teacherChalkboardImg} />
+            <div className="text">
+              <h3>Teacher</h3>
+              <p>Select this option to create classes, assign activities, and view reports.</p>
             </div>
-            <div className='row'>
-              <div className='col-xs-12'>Already signed up? <a href='/session/new'>Return to Login</a></div>
-            </div>
+          </div>
         </div>
+        <p className="return-to-login">Already have an account? <a href="/session/new">Log in</a></p>
       </div>
     )
+    // return (
+    //   <div className='container account-form' id='sign-up'>
+    //     <div className='row sign_up_select_role'>
+    //         <div className='row'>
+    //           <h3 className='col-xs-12'>
+    //             Sign up for Quill as:
+    //           </h3>
+    //         </div>
+    //         <div className='option-wrapper'>
+    //           <Link to="#">
+    //             <button onClick={this.setTeacherRoleOnSession} className='button-green'>
+    //               Educator
+    //             </button>
+    //           </Link>
+    //           <Link to="#">
+    //             <button onClick={this.setStudentRoleOnSession} className='button-green'>
+    //               Student
+    //             </button>
+    //           </Link>
+    //         </div>
+    //         <div className='row'>
+    //           <div className='col-xs-12'>Already signed up? <a href='/session/new'>Return to Login</a></div>
+    //         </div>
+    //     </div>
+    //   </div>
+    // )
   }
 }
 export default SelectUserType
