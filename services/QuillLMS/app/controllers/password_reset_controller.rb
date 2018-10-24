55ca9e4459fae5f4543f4c71b27894f45741ef77
Emilia Friedberg <emiliafriedberg@gmail.com>
2018-10-24 14:31:46 -0400

    finish forgot password page

--- services/QuillLMS/app/controllers/password_reset_controller.rb
+++ services/QuillLMS/app/controllers/password_reset_controller.rb
@@ -8,13 +8,20 @@ class PasswordResetController < ApplicationController
     user = User.find_by_email(params[:user][:email])
 
     if user && params[:user][:email].present?
-      user.refresh_token!
-      UserMailer.password_reset_email(user).deliver_now!
-      redirect_to password_reset_index_path, notice: 'We sent you an email with instructions on how to reset your password.'
+      if user.google_id
+        return render json: { message: 'Oops! You have a Google account. Log in that way instead.', type: 'email' }, status: 401
+      elsif user.clever_id
+        return render json: { message: 'Oops! You have a Clever account. Log in that way instead.', type: 'email' }, status: 401
+      else
+        user.refresh_token!
+        UserMailer.password_reset_email(user).deliver_now!
+        flash[:notice] = 'We sent you an email with instructions on how to reset your password.'
+        flash.keep(:notice)
+        return render json: { redirect: '/password_reset'}
+      end
     else
       @user = User.new
-      flash.now[:error] = 'We can\'t find that email in our system.'
-      render :index
+      return render json: { message: 'An account with this email does not exist. Try again.', type: 'email' }, status: 401
     end
   end
 
---
+++ services/QuillLMS/client/app/bundles/Teacher/components/accounts/reset_password/forgot_password.jsx
@@ -0,0 +1,101 @@
+import React from 'react'
+import request from 'request'
+import Input from '../../shared/input'
+import getAuthToken from '../../modules/get_auth_token';
+
+const bulbSrc = `${process.env.CDN_URL}/images/onboarding/bulb.svg`
+
+export default class ForgotPassword extends React.Component {
+  constructor() {
+    super();
+    this.handleEmailChange = this.handleEmailChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.state = {
+      email: '',
+      errors: {}
+    };
+  }
+
+  handleEmailChange(e) {
+    this.setState({ email: e.target.value, });
+  }
+
+  submitClass() {
+    let buttonClass = "button contained primary medium"
+    if (!this.state.email.length) {
+      buttonClass += ' disabled'
+    }
+    return buttonClass
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    request({
+      url: `${process.env.DEFAULT_URL}/password_reset`,
+      method: 'POST',
+      json: {
+        user: {
+          email: this.state.email,
+          password: this.state.password,
+        },
+        authenticity_token: getAuthToken(),
+      },
+    },
+    (err, httpResponse, body) => {
+      if (httpResponse.statusCode === 200 && body.redirect) {
+        // console.log(body);
+        window.location = `${process.env.DEFAULT_URL}${body.redirect}`;
+      } else {
+        let state
+        if (body.type && body.message) {
+          const errors = {}
+          errors[body.type] = body.message
+          state = { lastUpdate: new Date(), errors, }
+        } else {
+          let message = 'An account with this email does not exist. Try again.';
+          if (httpResponse.statusCode === 429) {
+            message = 'Too many failed attempts. Please wait one minute and try again.';
+          }
+          state = { lastUpdate: new Date(), message: (body.message || message), }
+        }
+        this.setState(state)
+      }
+    });
+  }
+
+  render() {
+    return (
+      <div className="container account-form reset-password">
+        <h1>Reset Your Password</h1>
+        <p className="sub-header">We'll email you a link to reset your password. You can also ask your teacher to reset it for you.</p>
+        <div className="reset-password-form-container">
+          <form onSubmit={this.handleSubmit} acceptCharset="UTF-8" >
+            <input name="utf8" type="hidden" value="✓" />
+            <input value={this.state.authToken} type="hidden" name="authenticity_token" />
+            <Input
+              label="Email"
+              value={this.state.email}
+              handleChange={this.handleEmailChange}
+              type="text"
+              className="email"
+              error={this.state.errors.email}
+            />
+            <input type="submit" name="commit" value="Reset password" className={this.submitClass()} />
+          </form>
+        </div>
+
+        <div className="student-info-box">
+          <h3><span>Need your teacher to reset your password? Share these instructions:</span> <img src={bulbSrc}/></h3>
+          <ol>
+            <li>Sign in to your teacher account</li>
+            <li>Click on the "Classes" tab, and then click on  "Edit Students"</li>
+            <li>Find the student, and click on "Edit Account"</li>
+            <li>Click on "Reset password to last name"</li>
+          </ol>
+        </div>
+
+      </div>
+
+    )
+  }
+}
