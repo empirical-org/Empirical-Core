49743ab11767defd221fc8fa74a31cddf3c315af
Emilia Friedberg <emiliafriedberg@gmail.com>
2018-10-25 16:35:59 -0400

    wip

--- services/QuillLMS/client/app/bundles/Teacher/components/accounts/new/sign_up_teacher.jsx
+++ services/QuillLMS/client/app/bundles/Teacher/components/accounts/new/sign_up_teacher.jsx
@@ -1,145 +1,171 @@
-import {
-  Link,
-} from 'react-router-dom';
-import React, { Component } from 'react';
+import React from 'react';
+import request from 'request'
 import AuthSignUp from './auth_sign_up'
+import Input from '../../shared/input'
 import AnalyticsWrapper from '../../shared/analytics_wrapper'
+import AgreementsAndLinkToLogin from './agreements_and_link_to_login'
+import getAuthToken from '../../modules/get_auth_token';
 
-class SignUpTeacher extends Component {
+const smallWhiteCheckSrc = `${process.env.CDN_URL}/images/shared/check-small-white.svg`
+
+class SignUpTeacher extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      first_name: null,
-      last_name: null,
-      username: null,
+      firstName: '',
+      lastName: '',
       email: null,
-      password: null,
-      password_confirmation: null,
-      errors: {},
+      password: '',
       sendNewsletter: true,
+      errors: {},
       analytics: new AnalyticsWrapper()
     }
-    this.formFields = [
-      {
-          name: 'first_name',
-          label: 'First Name',
-          errorLabel: 'First name'
-      },
-      {
-          name: 'last_name',
-          label: 'Last Name',
-          errorLabel: 'Last name'
-      },
-      {
-          name: 'email',
-          label: 'Email',
-          errorLabel: 'Email'
-      },
-      {
-          name: 'password',
-          label: 'Password',
-          errorLabel: 'Password'
-      }
-    ];
-    this.inputs = this.inputs.bind(this);
+
     this.updateKeyValue = this.updateKeyValue.bind(this);
-    this.updateSendNewsletter = this.updateSendNewsletter.bind(this);
     this.update = this.update.bind(this);
-    this.signUp = this.signUp.bind(this);
-    this.signUpData = this.signUpData.bind(this);
-    this.signUpError = this.signUpError.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this)
+    this.submitClass = this.submitClass.bind(this)
+    this.toggleNewsletter = this.toggleNewsletter.bind(this)
   }
 
   updateKeyValue(key, value) {
-      const newState = Object.assign({}, this.state);
-      newState[key] = value;
-      this.setState(newState);
-  }
-
-  updateSendNewsletter(event) {
-    this.updateKeyValue('sendNewsletter', event.target.checked);
+    const newState = Object.assign({}, this.state);
+    newState[key] = value;
+    this.setState(newState);
   }
 
   update(e) {
     this.updateKeyValue(e.target.id, e.target.value)
   }
 
-  signUpData() {
-    const name = this.state.first_name + ' ' + this.state.last_name
-    const data = {
-      role: 'teacher',
-      name,
-      username: this.state.username,
-      email: this.state.email,
-      password: this.state.password,
-      send_newsletter: this.state.sendNewsletter
-    };
-    return {user: data};
+  submitClass() {
+    const { password, firstName, lastName, email } = this.state
+    let buttonClass = "button contained primary medium"
+    if (!password.length || !firstName.length || !lastName.length || !email.length) {
+      buttonClass += ' disabled'
+    }
+    return buttonClass
   }
 
-  uponSignup() {
-    window.location = '/sign-up/pick-school-type'
+  handleSubmit(e) {
+    const { firstName, lastName, email, password, sendNewsletter } = this.state
+    e.preventDefault();
+    request({
+      url: `${process.env.DEFAULT_URL}/account`,
+      method: 'POST',
+      json: {
+        user: {
+          name: `${firstName} ${lastName}`,
+          password,
+          email,
+          role: 'teacher',
+          send_newsletter: sendNewsletter,
+        },
+        authenticity_token: getAuthToken(),
+      },
+    },
+    (err, httpResponse, body) => {
+      if (httpResponse.statusCode === 200) {
+        // console.log(body);
+        window.location = '/sign-up/pick-school-type'
+      } else {
+        let state
+        if (body.errors) {
+          state = { lastUpdate: new Date(), errors: body.errors, }
+        } else {
+          let message = 'You have entered an incorrect email or password.';
+          if (httpResponse.statusCode === 429) {
+            message = 'Too many failed attempts. Please wait one minute and try again.';
+          }
+          state = { lastUpdate: new Date(), message: (body.message || message), }
+        }
+        this.setState(state)
+      }
+    });
   }
 
-  signUpError(xhr) {
-    var errors = $.parseJSON(xhr.responseText).errors;
-    this.setState({errors: errors});
+  toggleNewsletter() {
+    this.setState({ sendNewsletter: !this.state.sendNewsletter, })
   }
 
-  signUp() {
-    if (this.state.first_name && this.state.last_name) {
-      $.ajax({
-        type: 'POST',
-        url: '/account',
-        data: this.signUpData(),
-        success: this.uponSignup,
-        error: this.signUpError
-      });
+  renderNewsletterRow() {
+    let checkbox
+    if (this.state.sendNewsletter) {
+      checkbox = <div className="quill-checkbox selected" onClick={this.toggleNewsletter}><img src={smallWhiteCheckSrc} /></div>
     } else {
-      const errors = {}
-      if (!this.state.first_name) {
-        errors['first_name'] = "can't be blank"
-      }
-      if (!this.state.last_name) {
-        errors['last_name'] = "can't be blank"
-      }
-      if (!this.state.email) {
-        errors['email'] = "can't be blank"
-      }
-      this.setState({errors});
+      checkbox = <div className="quill-checkbox unselected" onClick={this.toggleNewsletter} />
     }
-  }
-
-  inputs() {
-    const that = this
-    return this.formFields.map(function(field) {
-      const type = field.name === 'password' ? 'password' : 'text'
-      const error = that.state.errors[field.name]
-        ? <div className="error">{field.errorLabel} {that.state.errors[field.name]}.</div>
-        : <span />
-      return <div className="text-input-row" key={field.name}>
-        <div className="form-label">{field.label}</div>
-        <input id={field.name} placeholder={field.label} type={type} onChange={that.update}/>
-        {error}
-      </div>
-    }
-    )
+    return <div className="newsletter-row">{checkbox} <p>Send me a monthly update on new&nbsp;content</p></div>
   }
 
   render () {
     return (
-      <div className="new-teacher-account">
-        <h3 className='sign-up-header'>Sign up for a Teacher Account</h3>
-        <p className='support-p text-center'>We now support Google Classroom!</p>
-        <AuthSignUp/>
-        <div className='need-a-border'/>
-        {this.inputs()}
-        <input type='checkbox' name='sendNewsletter' ref='sendNewsletter' onChange={this.updateSendNewsletter} checked={this.state.sendNewsletter}/>
-        <p>Send me monthly Quill updates</p>
-        <button className='sign-up-button button-green' onClick={this.signUp}>Sign Up</button>
-        <div className='text-align-center'>
-          By signing up, you agree to our <a href='/tos' target='_blank'>terms of service</a> and <a href='/privacy' target='_blank'>privacy policy</a>.
+      <div className="container account-form teacher-sign-up">
+        <h1>Create a teacher account</h1>
+        <p className="sub-header">Are you a student? <a href="/sign-up/student">Sign up here</a></p>
+        <div className="info-and-form-container">
+          <div className="info">
+            <h2>More than 5,000 schools use Quill's free online tools to help their students become strong&nbsp;writers.</h2>
+            <ul>
+              <li>Quill provides free access to 400 writing and grammar&nbsp;activities</li>
+              <li>Students receive immediate feedback on their&nbsp;work</li>
+              <li>Teachers identify student needs and measure growth with diagnostics and&nbsp;reports</li>
+            </ul>
+          </div>
+          <div className="account-container text-center">
+            <AuthSignUp />
+            <div className='break'><span/>or<span/></div>
+              <div className="teacher-signup-form">
+                <div>
+                  <form onSubmit={this.handleSubmit} acceptCharset="UTF-8" >
+                    <input name="utf8" type="hidden" value="✓" />
+                    <input value={this.state.authToken} type="hidden" name="authenticity_token" />
+                    <div className="name">
+                      <Input
+                        label="First name"
+                        value={this.state.firstName}
+                        handleChange={this.update}
+                        type="text"
+                        className="first-name"
+                        id="firstName"
+                        error={this.state.errors.first_name}
+                      />
+                      <Input
+                        label="Last name"
+                        value={this.state.lastName}
+                        handleChange={this.update}
+                        type="text"
+                        className="last-name"
+                        id="lastName"
+                        error={this.state.errors.last_name}
+                      />
+                    </div>
+                    <Input
+                      label="Email"
+                      value={this.state.email}
+                      handleChange={this.update}
+                      type="text"
+                      className="email"
+                      id="email"
+                      error={this.state.errors.email}
+                    />
+                    <Input
+                      label="Password"
+                      value={this.state.password}
+                      handleChange={this.update}
+                      type='password'
+                      className="password"
+                      error={this.state.errors.password}
+                      id="password"
+                    />
+                    {this.renderNewsletterRow()}
+                    <input type="submit" name="commit" value="Sign up" className={this.submitClass()} />
+                  </form>
+                </div>
+              </div>
+          </div>
         </div>
+        <AgreementsAndLinkToLogin />
       </div>
     )
   }
