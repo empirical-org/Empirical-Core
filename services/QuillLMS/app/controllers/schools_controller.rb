2f023ef8b8274bb180e66929ba072e53e39b936c
Max Buck <maxwellhigginsbuck@gmail.com>
2018-10-29 15:45:51 -0400

    progress; still amplifying cache hits

--- services/QuillLMS/app/controllers/schools_controller.rb
+++ services/QuillLMS/app/controllers/schools_controller.rb
@@ -1,24 +1,53 @@
 class SchoolsController < ApplicationController
   include CheckboxCallback
+  MIN_PREFIX_LENGHT_WHEN_LAT_LON_NOT_PRESENT = 4
 
   def index
     # TODO: return here
-    radius = params[:radius].presence || 5
-    if params[:text]
-      if params[:lat] and params[:lng]
-        zip_arr = ZipcodeInfo.isinradius([params[:lat].to_f, params[:lng].to_f], radius.to_i).map {|z| z.zipcode}
-        prefix = params[:text]
+    @radius = params[:radius].presence || 5
+    @limit = params[:limit].presence || 10
+    @lat, @lng, @prefix = params[:lat],params[:lng], params[:prefix]
+    @schools = []
+
+    unless @prefix.blank?
+      if @lat.present? and @lng.present?
+        school_ids = JSON.load($redis.get("LAT_LNG_RADIUS_TO_SCHOOL_#{@lat}_#{@lng}_#{@radius}"))
+      else
+        school_ids = JSON.load($redis.get("PREFIX_TO_SCHOOL_#{@prefix}"))
+      end
+
+      unless school_ids.blank? 
+        @schools = School.where(id: school_ids).where(
+          "lower(name) LIKE :prefix", prefix: "#{@prefix.downcase}%"
+        ).limit(@limit)
+        unless @schools.empty?
+          puts 'CACHE HIT'
+        end
+      end
+
+      if @lat.present? and @lng.present? and @schools.empty?
+        zip_arr = ZipcodeInfo.isinradius([@lat.to_f, @lng.to_f], @radius.to_i).map {|z| z.zipcode}
         @schools = School.where(zipcode: zip_arr 
          ).where(
-         "lower(name) LIKE :prefix", prefix: "#{prefix.downcase}%"
-        )
-      else
-        # TODO: lookup lat and lng by ip
-        return []
+         "lower(name) LIKE :prefix", prefix: "#{@prefix.downcase}%"
+         ).limit(@limit)
+         $redis.set("LAT_LNG_RADIUS_TO_SCHOOL_#{@lat}_#{@lng}_#{@radius}", @schools.map {|s| s.id}.to_json)
+         # short cache, highly specific
+        $redis.expire("LAT_LNG_RADIUS_TO_SCHOOL_#{@lat}_#{@lng}_#{@radius}", 60*5)
       end
-    else
-      render status: 400, json: {'error' => 'You must enter a zipcode.'}
+
+      if @schools.empty? and @prefix.length < MIN_PREFIX_LENGHT_WHEN_LAT_LON_NOT_PRESENT
+        @schools = []
+      elsif @schools.empty?
+        @schools = School.where(
+           "lower(name) LIKE :prefix", prefix: "#{@prefix.downcase}%"
+         ).limit(@limit)
+        $redis.set("PREFIX_TO_SCHOOL_#{@prefix}", @schools.map {|s| s.id}.to_json)
+        # longer cache, more general
+        $redis.expire("PREFIX_TO_SCHOOL_#{@prefix}", 60*60)
+      end
     end
+    
   end
 
   def new
--- services/QuillLMS/app/views/schools/index.json.jbuilder
+++ services/QuillLMS/app/views/schools/index.json.jbuilder
@@ -1,5 +1,26 @@
-json.array! @schools do |school|
-  json.id school.id
-  json.text school.name
-  json.zipcode school.zipcode
+json.ignore_nil!
+
+json.data do
+  json.array! @schools do |school|
+    json.id school.id
+    json.type 'schools'
+    json.attributes do
+      json.text school.name
+      json.zipcode school.zipcode
+      json.street school.street
+      json.city school.city
+      json.state school.state
+    end
+  end
+end
+
+json.ignore_nil! false
+
+json.meta do
+  json.lat @lat
+  json.lng @lng
+  json.prefix @prefix
+  json.radius @radius
+  json.limit @limit
+  json.request_time DateTime.now.to_s
 end
