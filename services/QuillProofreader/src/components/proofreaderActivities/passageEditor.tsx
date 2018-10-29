0549a2cfc662d82666e2d5f36ffa50df8b72f024
Emilia Friedberg <emiliafriedberg@gmail.com>
2018-10-29 11:58:15 -0400

    fix issue with unnecessary edits displaying edited text as correct

--- services/QuillProofreader/src/components/proofreaderActivities/passageEditor.tsx
+++ services/QuillProofreader/src/components/proofreaderActivities/passageEditor.tsx
@@ -96,7 +96,8 @@ const plugins = [
 interface PassageEditorState {
   text: any;
   originalTextArray: string[],
-  indicesOfUTags: {[key: number]: number}
+  indicesOfUTags: {[key: number]: number},
+  editsWithOriginalValue: Array<{key: string, originalText: string, currentText: string}>
 }
 
 interface PassageEditorProps {
@@ -116,11 +117,14 @@ class PassageEditor extends React.Component <PassageEditorProps, PassageEditorSt
     this.state = {
       text: html.deserialize(text),
       originalTextArray,
-      indicesOfUTags
+      indicesOfUTags,
+      editsWithOriginalValue: []
     }
 
     this.handleTextChange = this.handleTextChange.bind(this)
     this.onKeyUp = this.onKeyUp.bind(this)
+    this.updateEditsWithOriginalValue = this.updateEditsWithOriginalValue.bind(this)
+    this.removeEditFromEditsWithOriginalValue = this.removeEditFromEditsWithOriginalValue.bind(this)
   }
 
   trimWord(word: string) {
@@ -214,7 +218,27 @@ class PassageEditor extends React.Component <PassageEditorProps, PassageEditorSt
   }
 
   handleTextChange({value}: any) {
-    this.setState({text: value}, () => this.props.handleTextChange(html.serialize(this.state.text)))
+    this.setState({text: value}, () => this.props.handleTextChange(html.serialize(this.state.text), this.state.editsWithOriginalValue))
+  }
+
+  removeEditFromEditsWithOriginalValue(key: string) {
+    this.setState({ editsWithOriginalValue: this.state.editsWithOriginalValue.filter(edit => edit.key !== key)})
+  }
+
+  updateEditsWithOriginalValue(key: string, currentText: string, originalText: string) {
+    const { editsWithOriginalValue } = this.state
+    const newUnnecessaryEdits = editsWithOriginalValue.filter(edit => edit.key !== key).concat({ key, currentText, originalText }).sort((editA, editB) => {
+      const editAKey = Number(editA.key)
+      const editBKey = Number(editB.key)
+      if (editAKey < editBKey) {
+        return -1;
+      }
+      if (editAKey > editBKey) {
+        return 1;
+      }
+      return 0;
+    })
+    this.setState({ editsWithOriginalValue: newUnnecessaryEdits })
   }
 
   onKeyDown(event: any, change: any, editor: any) {
@@ -327,16 +351,19 @@ class PassageEditor extends React.Component <PassageEditorProps, PassageEditorSt
         node = node.addMark({type: 'underline', data: {id}})
       }
 
-      if (normalizedAndTrimmedNewText === stringNormalize(originalText).trim()) {
+      const normalizedAndTrimmedOriginalText = stringNormalize(originalText).trim()
+      if (normalizedAndTrimmedNewText === normalizedAndTrimmedOriginalText) {
         if (event.key !== ' ' || (originalSelection.focus.offset !== 0 && originalSelection.anchor.offset !== 0)) {
           node
           .removeMark('bold')
+          this.removeEditFromEditsWithOriginalValue(dataOriginalIndex)
           // .setAnchor(originalSelection.anchor)
           // .setFocus(originalSelection.focus)
         }
       } else {
         node
         .addMark('bold')
+        this.updateEditsWithOriginalValue(dataOriginalIndex, normalizedAndTrimmedNewText, normalizedAndTrimmedOriginalText)
         // .setAnchor(originalSelection.anchor)
         // .setFocus(originalSelection.focus)
       }
@@ -372,6 +399,7 @@ class PassageEditor extends React.Component <PassageEditorProps, PassageEditorSt
 
             if (normalizedAndTrimmedCurrentText !== normalizedAndTrimmedOriginalText) {
               node.addMark('bold')
+              this.updateEditsWithOriginalValue(dataOriginalIndex, normalizedAndTrimmedCurrentText, normalizedAndTrimmedOriginalText)
             }
             return node.setAnchor(originalSelection.anchor).setFocus(originalSelection.focus)
           }
@@ -398,8 +426,11 @@ class PassageEditor extends React.Component <PassageEditorProps, PassageEditorSt
             }
 
             node.addMark({type: 'underline', data: {id}})
-            if (stringNormalize(previousInline.text).trim() !== stringNormalize(originalPreviousInlineText).trim()) {
+            const normalizedAndTrimmedCurrentText = stringNormalize(previousInline.text).trim()
+            const normalizedAndTrimmedOriginalText = stringNormalize(originalPreviousInlineText).trim()
+            if (normalizedAndTrimmedCurrentText !== normalizedAndTrimmedOriginalText) {
               node.addMark('bold')
+              this.updateEditsWithOriginalValue(dataOriginalIndex, normalizedAndTrimmedCurrentText, normalizedAndTrimmedOriginalText)
             }
 
             // return node.setAnchor(originalSelection.anchor).setFocus(originalSelection.focus)
