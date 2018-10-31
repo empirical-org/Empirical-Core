68ad425f60fcb45af3db393af91da43ae69ffee0
Emilia Friedberg <emiliafriedberg@gmail.com>
2018-10-31 18:03:04 -0400

    fixing dashboard cards

--- services/QuillLMS/client/app/bundles/Teacher/components/dashboard/diagnostic_mini.jsx
+++ services/QuillLMS/client/app/bundles/Teacher/components/dashboard/diagnostic_mini.jsx
@@ -29,14 +29,17 @@ export default class DiagnosticMini extends React.Component {
   }
 
   assignRecommendationsMini() {
-    const { unitInfo, numberOfFinishedDiagnostics, } = this.state;
+    // const { unitInfo, numberOfFinishedDiagnostics, } = this.state;
+    const numberOfFinishedDiagnostics = 1
+    const unitInfo = {}
+
 
     return (<div className="mini_content diagnostic-mini assign-recommendations">
       <div className="gray-underline">
         <h3>{numberOfFinishedDiagnostics} Completed Diagnostic{Number(numberOfFinishedDiagnostics) === 1 ? '' : 's'}</h3>
       </div>
       <img src={`${process.env.CDN_URL}/images/shared/diagnostics_completed.svg`} />
-      <p>View our recommendations for each student and assign their personalized learning&nbsp;plans.</p>
+      <p>View our recommendations for each&nbsp;student.</p>
       <a href={`/teachers/progress_reports/diagnostic_reports#/u/${unitInfo.unit_id}/a/${unitInfo.activity_id}/c/${unitInfo.classroom_id}/recommendations`} className="bg-quillgreen text-white">View Recommended Activities</a>
     </div>);
   }
@@ -44,11 +47,11 @@ export default class DiagnosticMini extends React.Component {
   awaitingStudentsMini() {
     return (<div className="mini_content diagnostic-mini awaiting-students">
       <div className="gray-underline">
-        <h3>Awaiting Students' Completion</h3>
+        <h3>Diagnostic Assigned</h3>
       </div>
 
       <img alt="" src={`${process.env.CDN_URL}/images/shared/new_diagnostic.svg`} />
-      <p>The diagnostic is waiting for students on their dashboards. Have students log in to complete it so you can assign their&nbsp;recommended&nbsp;activities.</p>
+      <p>The diagnostic is waiting for students on their dashboards. Have students log in to complete it so you can assign their recommended&nbsp;activities.</p>
     </div>);
   }
 
@@ -66,20 +69,20 @@ export default class DiagnosticMini extends React.Component {
 
   renderDiagnosticMini() {
     let miniContent;
-    switch (this.state.status) {
-      case 'recently completed':
+    // switch (this.state.status) {
+    //   case 'recently completed':
         miniContent = this.assignRecommendationsMini();
-        break;
-      case 'assigned':
-        miniContent = this.awaitingStudentsMini();
-        break;
-      case 'unassigned':
-        miniContent = this.assignDiagnosticMini();
-        break;
-      case 'completed':
-      default:
-        return <span />;
-    }
+      //   break;
+      // case 'assigned':
+      //   miniContent = this.awaitingStudentsMini();
+      //   break;
+      // case 'unassigned':
+      //   miniContent = this.assignDiagnosticMini();
+      //   break;
+      // case 'completed':
+      // default:
+        // return <span />;
+    // }
     return (<div className="mini_container results-overview-mini-container col-md-4 col-sm-5 text-center">
       {miniContent}
     </div>);
