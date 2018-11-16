657be5f8060d2d1a1bab92ad4ce8f79d622f61cd
Emilia Friedberg <emiliafriedberg@gmail.com>
2018-11-16 11:51:32 -0500

    update student account form to hide role switching option

--- services/QuillLMS/spec/controllers/students_controller_spec.rb
+++ services/QuillLMS/spec/controllers/students_controller_spec.rb
@@ -53,10 +53,10 @@ describe StudentsController do
     end
   end
 
-  describe '#make_teacher' do
+  describe '#update_email' do
     context 'when params role student' do
       it 'should update the role and email' do
-        post :make_teacher, email: "test@email.com"
+        post :update_email, email: "test@email.com"
         expect(user.reload.email).to eq "test@email.com"
         expect(user.reload.role).to eq "teacher"
       end
@@ -64,7 +64,7 @@ describe StudentsController do
 
     context 'when params role not student' do
       it 'should set the role as student if given' do
-        post :make_teacher, role: "student", email: "test@email.com"
+        post :update_email, role: "student", email: "test@email.com"
         expect(user.reload.role).to eq "student"
       end
     end
