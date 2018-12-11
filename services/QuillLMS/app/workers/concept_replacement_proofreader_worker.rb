84be7e32963ed6b65dafdd2aa1d25216aed200d2
Emilia Friedberg <emiliafriedberg@gmail.com>
2018-12-11 17:13:55 -0500

    add concept replacement worker for proofreader

---
+++ services/QuillLMS/app/workers/concept_replacement_proofreader_worker.rb
@@ -0,0 +1,15 @@
+class ConceptReplacementProofreaderWorker
+  include Sidekiq::Worker
+
+  def perform(original_concept_uid, new_concept_uid)
+    activities = HTTParty.get("#{ENV['FIREBASE_DATABASE_URL']}/v3/passageProofreadings").parsed_response
+    activities.each do |key, act|
+      if act['passage'].include?(original_concept_uid)
+        new_passage = act['passage'].gsub(original_concept_uid, new_concept_uid)
+        new_passage_hash = { passage: new_passage }
+        HTTParty.put("#{ENV['FIREBASE_DATABASE_URL']}/v3/passageProofreadings/#{key}", body: new_passage_hash.to_json)
+      end
+    end
+  end
+
+end
