422de74b67716d69ee512109cd9c80de984c70bc
Emilia Friedberg <emiliafriedberg@gmail.com>
2018-12-14 12:27:15 -0500

    move tooltip over activity name instead of tool icon

--- services/QuillLMS/client/app/bundles/Teacher/components/lesson_planner/create_unit/activity_search/activity_search_results/activity_search_result.jsx
+++ services/QuillLMS/client/app/bundles/Teacher/components/lesson_planner/create_unit/activity_search/activity_search_results/activity_search_result.jsx
@@ -21,28 +21,28 @@ export default React.createClass({
     const selectedClass = this.props.selected ? 'selected' : '';
     const toolTip = this.props.data.activity_classification
 
-    ? (<div
-      ref="activateTooltip"
-      className={`icon-${this.props.data.activity_classification.id}-green-no-border`}
-      data-html="true"
-      data-toggle="tooltip"
-      data-placement="top"
-      title={`<h1>${this.props.data.name}</h1><p>Tool: ${this.props.data.activity_classification.alias}</p><p>${this.props.data.section.name}</p><p>${this.props.data.topic_name}</p><p>${this.props.data.description}</p>`}
-    />)
+    ? (<td className="activity_name">
+        <a
+          className="activity_link"
+          href={this.props.data.anonymous_path}
+          target="_new"
+          ref="activateTooltip"
+          data-html="true"
+          data-toggle="tooltip"
+          data-placement="top"
+          title={`<h1>${this.props.data.name}</h1><p>Tool: ${this.props.data.activity_classification.alias}</p><p>${this.props.data.section.name}</p><p>${this.props.data.topic_name}</p><p>${this.props.data.description}</p>`}
+        >
+          {this.props.data.name}
+        </a>
+    </td>)
     : <span />;
     return (
       <tr onMouseEnter={this.tooltipTrigger} onMouseLeave={this.tooltipTriggerStop} className={`tooltip-trigger ${selectedClass}`}>
         <td>{this.props.data.activity_category ? this.props.data.activity_category.name : ''}</td>
         <td>
-          {toolTip}
+          <div className={`icon-${this.props.data.activity_classification.id}-green-no-border`} />
         </td>
-        <td className="activity_name">
-          <a className="activity_link" href={this.props.data.anonymous_path} target="_new">
-            {this.props.data.name}
-          </a>
-        </td>
-
-
+        {toolTip}
         <td>
           <input type="checkbox" checked={this.props.selected} onChange={this.callToggleActivitySelection} id={`activity_${this.props.data.id}`} data-model-id={this.props.data.id} className="css-checkbox" />
           <label htmlFor={`activity_${this.props.data.id}`} id={`activity_${this.props.data.id}`} className="css-label" />
