e1b7d81ab60af7f5767f8006fcf73c8da13d25b4
Donald McKendrick <ddmckendrick@gmail.com>
2019-01-07 16:56:49 -0500

    Connect to session

--- services/QuillGraphQL/src/resolvers/classroom_lessons.resolvers.ts
+++ services/QuillGraphQL/src/resolvers/classroom_lessons.resolvers.ts
@@ -3,7 +3,8 @@ import rethinkClient from '../utils/rethinkdb';
 export default {
   Query: {
     classroomLesson: classroomLesson,
-    classroomLessons: classroomLessonsIndex
+    classroomLessons: classroomLessonsIndex,
+    classroomLessonSession: classroomLessonSession
   },
   ClassroomLesson: {
     editions: editions,
@@ -23,16 +24,18 @@ function classroomLesson(parent, {id}) {
 }
 
 function editions(parent, args, ctx) {
-  // console.log("being called", parent, args, ctx)
   return rethinkClient.db('quill_lessons').table('lesson_edition_metadata').filter({lesson_id:  parent.id}).run()
 }
 
 function edition(parent, {id}, ctx) {
-  // console.log("being called", parent, args, ctx)
   return rethinkClient.db('quill_lessons').table('lesson_edition_metadata').get(id).run()
 }
 
 async function editionQuestions(parent, args, ctx) {
   const questionsData = await rethinkClient.db('quill_lessons').table('lesson_edition_questions').get(parent.id).run();
   return questionsData.questions
+}
+
+function classroomLessonSession(parent, {id}, ctx) {
+  return rethinkClient.db('quill_lessons').table('classroom_lesson_sessions').get(id).run();
 }
