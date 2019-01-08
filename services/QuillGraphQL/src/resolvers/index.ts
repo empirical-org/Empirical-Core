e7dd8caf4fe06befc15a120772869ba053070490
Donald McKendrick <ddmckendrick@gmail.com>
2019-01-08 10:56:47 -0500

    Remove comments

--- services/QuillGraphQL/src/resolvers/index.ts
+++ services/QuillGraphQL/src/resolvers/index.ts
@@ -1,10 +1,6 @@
 import * as path from 'path'
 import { fileLoader } from 'merge-graphql-schemas'
 import GraphQLJSON from 'graphql-type-json';
-/* MANUAL APPROACH: Update this file manually with each resolver file */
-// import userResolvers from "./user.resolvers";
-// import welcomeResolvers from "./welcome.resolvers";
-// const resolversArray = [userResolvers, welcomeResolvers];
 
 /*  AUTOMATED APPROACH: Put your resolvers anywhere 
     with ".resolvers.[js/ts]" naming convention */
