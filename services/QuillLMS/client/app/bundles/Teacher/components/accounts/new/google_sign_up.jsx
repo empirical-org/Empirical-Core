5ee8f05516c924df3654b249215458f8540a7d97
Donald McKendrick <ddmckendrick@gmail.com>
2019-01-09 00:35:48 -0500

    Remove prompt for consent for every login attempt

--- services/QuillLMS/client/app/bundles/Teacher/components/accounts/new/google_sign_up.jsx
+++ services/QuillLMS/client/app/bundles/Teacher/components/accounts/new/google_sign_up.jsx
@@ -1,8 +1,8 @@
-import React from 'react'
+import React from 'react';
 
 export default () => (
-  <a className="google-sign-up" href='/auth/google_oauth2?prompt=consent'>
-    <img src='/images/google_icon.svg' alt="google icon"/>
+  <a className="google-sign-up" href="/auth/google_oauth2">
+    <img src="/images/google_icon.svg" alt="google icon"  />
     <span>Sign up with Google</span>
   </a>
-)
+);
