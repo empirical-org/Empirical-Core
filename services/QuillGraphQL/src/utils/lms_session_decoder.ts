5b2755bf8f58ad7c0d648bf220daba6a445af3f7
Donald McKendrick <ddmckendrick@gmail.com>
2019-01-22 14:18:03 -0500

    Decipher cookies correctly

--- services/QuillGraphQL/src/utils/lms_session_decoder.ts
+++ services/QuillGraphQL/src/utils/lms_session_decoder.ts
@@ -1,8 +1,6 @@
 const crypto = require('crypto')
 const secret = process.env.SESSION_SECRET;
-var salt = 'encrypted cookie';
-const derivedKey = crypto.pbkdf2Sync(secret, salt, 1000, 64, 'sha1');
-
+var salt = process.env.SESSION_SALT;
 export interface DecodedCookie {
   session_id?: string
   _csrf_token?: string
@@ -10,6 +8,12 @@ export interface DecodedCookie {
   name?: string
   role?: string
 }
+
+export function createDerivedKey(secret:string, salt:string):Buffer{
+  return crypto.pbkdf2Sync(secret, salt, 1000, 64, 'sha1');
+}
+
+export const derivedKey = createDerivedKey(secret, salt);
 
 export function parseCookiesString(cookies: string): any {
   const cookiesArray:string[] = cookies.split("; ")
@@ -21,35 +25,46 @@ export function parseCookiesString(cookies: string): any {
   return cookieHash
 }
 
+export function decodeLMSWebsocketSession(cookieName:string, cookies:string):DecodedCookie {
+  const cookie = parseCookiesString(cookies)[cookieName];
+  return cookie ? lmsCookieToJSON(cookie) : {};
+}
+
+export function lmsCookieToJSON(cookie: string): DecodedCookie {
+  var cookieSegments = cookie.split('--');
+  // if (cookieSegments.length != 2) {
+  //   return next(new Error('invalid cookie format.'));
+  // }
+
+  var sessionData = new Buffer(cookieSegments[0], 'base64');
+  // var signature = cookieSegments[1];
+
+  var sessionDataSegments = sessionData.toString('utf8').split('--');
+  // if (sessionDataSegments.length != 2) {
+  //   return next(new Error('invalid cookie format.'));
+  // }
+
+  var data = new Buffer(sessionDataSegments[0], 'base64');
+  var iv = new Buffer(sessionDataSegments[1], 'base64');
+
+  var decipher = crypto.createDecipheriv('aes-256-cbc', derivedKey.slice(0, 32), iv.slice(0, 16));
+  var decryptedData = decipher.update(data, 'binary', 'utf8') + decipher.final('utf8');
+  return JSON.parse(decryptedData)
+}
+
 function decodeLmsSession(req): DecodedCookie {
   if (req == undefined) { return {} }
   const cookie = req.cookies[process.env.SESSION_NAME];
 
   if (cookie) {
-    var cookieSegments = cookie.split('--');
-    // if (cookieSegments.length != 2) {
-    //   return next(new Error('invalid cookie format.'));
-    // }
-  
-    var sessionData = new Buffer(cookieSegments[0], 'base64');
-    // var signature = cookieSegments[1];
-  
-    var sessionDataSegments = sessionData.toString('utf8').split('--');
-    // if (sessionDataSegments.length != 2) {
-    //   return next(new Error('invalid cookie format.'));
-    // }
-  
-    var data = new Buffer(sessionDataSegments[0], 'base64');
-    var iv = new Buffer(sessionDataSegments[1], 'base64');
-  
-    var decipher = crypto.createDecipheriv('aes-256-cbc', derivedKey.slice(0, 32), iv.slice(0, 16));
-    var decryptedData = decipher.update(data, 'binary', 'utf8') + decipher.final('utf8');
-    return JSON.parse(decryptedData)
+    return lmsCookieToJSON(cookie)
   } else {
     return {}
   }
 
   
 }
+
+
 
 export default decodeLmsSession
