8dc0ac2d911e013a59875ec220d28171c769c4a7
Emilia Friedberg <emiliafriedberg@gmail.com>
2019-01-23 15:28:59 -0500

    use find_by instead of find in lesson plan email worker

--- services/QuillLMS/app/workers/lesson_plan_email_worker.rb
+++ services/QuillLMS/app/workers/lesson_plan_email_worker.rb
@@ -7,7 +7,7 @@ class LessonPlanEmailWorker
       .joins("JOIN activity_category_activities AS aca ON aca.activity_id = activities.id")
       .joins("JOIN activity_categories AS ac ON ac.id = aca.activity_category_id")
       .order("ac.order_number, aca.order_number")
-    @unit = Unit.find(unit_id)
+    @unit = Unit.find_by(id: unit_id)
     @user.send_lesson_plan_email(@lessons, @unit)
   end
 end
