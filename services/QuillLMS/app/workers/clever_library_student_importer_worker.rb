24e00cacf5c17054da59cff9fe8cae73ee2537e0
Emilia Friedberg <emiliafriedberg@gmail.com>
2019-01-24 10:57:17 -0500

    null check students in clever library student importer worker

--- services/QuillLMS/app/workers/clever_library_student_importer_worker.rb
+++ services/QuillLMS/app/workers/clever_library_student_importer_worker.rb
@@ -7,14 +7,16 @@ class CleverLibraryStudentImporterWorker
     Classroom.where(id: classroom_ids).each do |classroom|
       if classroom
         students_data = client.get_section_students(section_id: classroom.clever_id)
-        students = CleverIntegration::Creators::Students.run(students_data.map do |student_data|
-          {
-            clever_id: student_data["id"],
-            name: "#{student_data['name']['first']} #{student_data['name']['middle']} #{student_data['name']['last']}".squish,
-            username: student_data["id"]
-          }
-        end)
-        CleverIntegration::Associators::StudentsToClassroom.run(students, classroom)
+        if students_data && students_data.length
+          students = CleverIntegration::Creators::Students.run(students_data.map do |student_data|
+            {
+              clever_id: student_data["id"],
+              name: "#{student_data['name']['first']} #{student_data['name']['middle']} #{student_data['name']['last']}".squish,
+              username: student_data["id"]
+            }
+          end)
+          CleverIntegration::Associators::StudentsToClassroom.run(students, classroom)
+        end
       end
     end
   end
