e9f6678244a77c8166603bd3d0d152149ac50286
Emilia Friedberg <emiliafriedberg@gmail.com>
2019-01-25 11:21:18 -0500

    nullcheck user for set_teacher_lesson_cache worker

--- services/QuillLMS/app/workers/account_created_email_worker.rb
+++ services/QuillLMS/app/workers/account_created_email_worker.rb
@@ -2,7 +2,9 @@ class AccountCreatedEmailWorker
   include Sidekiq::Worker
 
   def perform(id, temp_password, admin_name)
-    @user = User.find(id)
-    @user.send_account_created_email(temp_password, admin_name)
+    if id
+      @user = User.find_by(id: id)
+      @user.send_account_created_email(temp_password, admin_name) if @user
+    end
   end
 end
