d97278b7bea229bab4d1a78b60b455889a212265
Emilia Friedberg <emiliafriedberg@gmail.com>
2019-01-25 11:21:49 -0500

    nullcheck user for account created email worker

--- services/QuillLMS/app/workers/account_created_email_worker.rb
+++ services/QuillLMS/app/workers/account_created_email_worker.rb
@@ -2,9 +2,7 @@ class AccountCreatedEmailWorker
   include Sidekiq::Worker
 
   def perform(id, temp_password, admin_name)
-    if id
-      @user = User.find_by(id: id)
-      @user.send_account_created_email(temp_password, admin_name) if @user
-    end
+    @user = User.find_by(id: id)
+    @user.send_account_created_email(temp_password, admin_name) if @user
   end
 end
