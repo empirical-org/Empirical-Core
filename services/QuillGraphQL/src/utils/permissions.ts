22c111357ce974e4b787565deae75aea68cb1b4f
Donald McKendrick <ddmckendrick@gmail.com>
2019-01-28 14:13:57 -0500

    Create user permission logic

--- services/QuillGraphQL/src/utils/permissions.ts
+++ services/QuillGraphQL/src/utils/permissions.ts
@@ -5,3 +5,9 @@ export function teacherHasPermission(session:any, user:any):boolean {
   if (user.role === 'teacher' && user.lesson_session.classroom_session_id === session.id) return true 
   return false
 }
+
+export function userHasPermission(session:any, user:any):boolean {
+  if (session.public) return true;
+  if (user.lesson_session && user.lesson_session.classroom_session_id === session.id) return true;
+  return false;
+}
