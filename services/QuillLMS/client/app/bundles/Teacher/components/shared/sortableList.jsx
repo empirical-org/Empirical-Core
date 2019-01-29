e9c353da7bb8ce014ea8d9ec3920393d797684a0
Thomas Robertson <thomas.robertson@quill.org>
2019-01-29 12:12:29 -0500

    Fix some white space ambiguity
    NO CODE CHANGES

--- services/QuillLMS/client/app/bundles/Teacher/components/shared/sortableList.jsx
+++ services/QuillLMS/client/app/bundles/Teacher/components/shared/sortableList.jsx
@@ -6,7 +6,7 @@ const SortableListItem = sortable(ListItem);
 
 export default class SortableList extends React.Component {
 
-	constructor(props) {
+  constructor(props) {
     super(props)
 
     this.state = {
@@ -19,23 +19,23 @@ export default class SortableList extends React.Component {
     this.updateState = this.updateState.bind(this)
   }
 
-	componentWillReceiveProps(nextProps) {
-		if (nextProps.data !== this.state.data.items) {
-			this.setState({data: {items: nextProps.data}})
-		}
-	}
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.data !== this.state.data.items) {
+      this.setState({data: {items: nextProps.data}})
+    }
+  }
 
-	updateState(obj) {
-		this.setState(obj, this.props.sortCallback(this.state));
-	}
+  updateState(obj) {
+    this.setState(obj, this.props.sortCallback(this.state));
+  }
 
-	render() {
-		const childProps = {
-			className: 'myClass1'
-		};
-		const listItems = this.state.data.items.map(function(item, i) {
-			return (
-				<SortableListItem
+  render() {
+    const childProps = {
+      className: 'myClass1'
+    };
+    const listItems = this.state.data.items.map(function(item, i) {
+      return (
+        <SortableListItem
           key={i}
           updateState={this.updateState}
           items={this.state.data.items}
@@ -45,12 +45,12 @@ export default class SortableList extends React.Component {
           childProps={childProps}
           >
             {item}
-        </SortableListItem>
-			);
-		}, this);
+          </SortableListItem>
+      );
+    }, this);
 
-		return (
-			<div className="list">{listItems}</div>
-		);
-	}
+    return (
+      <div className="list">{listItems}</div>
+    );
+  }
 }
