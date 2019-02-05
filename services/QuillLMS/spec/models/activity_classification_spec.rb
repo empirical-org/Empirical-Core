9440fe17f4a7965271ae52b257928a6de4746b57
Donald McKendrick <ddmckendrick@gmail.com>
2019-02-05 11:13:51 -0500

    Update activity classification spec now that we can have multiple versions of the apps

--- services/QuillLMS/spec/models/activity_classification_spec.rb
+++ services/QuillLMS/spec/models/activity_classification_spec.rb
@@ -3,17 +3,9 @@ require 'rails_helper'
 describe ActivityClassification, type: :model, redis: true do
   it { should have_many(:activities) }
   it { should have_many(:concept_results) }
-  it { should validate_presence_of(:key) }
 
   it_behaves_like "uid"
-  
-  describe 'unique key' do
-    #We are creating an activity classification 
-    #here because validate_uniqueness_of requires 
-    #an existing activity_classification to work properly.
-    subject { create(:activity_classification) }
-    it { should validate_uniqueness_of(:key) }
-  end
+
 
   let(:activity_classification) { build(:activity_classification) }
 
