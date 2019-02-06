866a2765fb23860af67ecd4e517d83d720525899
Emilia Friedberg <emiliafriedberg@gmail.com>
2019-02-06 11:05:42 -0500

    wip

--- services/QuillLMS/client/app/bundles/Staff/containers/ConceptBoxContainer.tsx
+++ services/QuillLMS/client/app/bundles/Staff/containers/ConceptBoxContainer.tsx
@@ -1,20 +1,9 @@
 import * as React from "react";
-import {Link} from "react-router";
 import { Query } from "react-apollo";
 import gql from "graphql-tag";
 
 import client from '../../../modules/apollo';
-import Input from '../../Teacher/components/shared/input'
-import {
-  Breadcrumb,
-  Divider,
-  Card,
-  List,
-  Row,
-  Col,
-} from "antd";
-import ConceptsShow from "../components/ConceptsShow";
-import ConceptBreadCrumb from '../components/ConceptBreadCrumb';
+import ConceptBox from "../components/ConceptBox";
 
 function levelTwoConceptsQuery(){
   return `
@@ -92,56 +81,30 @@ interface QueryResult {
   siblings: Array<Concept>;
 }
 
-class ConceptBox extends React.Component {
+class ConceptBoxContainer extends React.Component {
   constructor(props){
     super(props)
-
-    this.state = {
-      concept: props.concept,
-      originalConcept: props.concept
-    }
-  }
-
-  renderLevels() {
-    const { concept } = this.state
-    if (this.props.levelNumber === 2) {
-      return <div>
-        <Input
-          label='Level 2'
-          value={concept.name}
-          type='text'
-        />
-      </div>
-    } else if (this.props.levelNumber === 1) {
-      return <div>
-        <Input
-          label='Level 1'
-          value={concept.name}
-          type='text'
-        />
-      </div>
-    } else if (this.props.levelNumber === 0) {
-      return <div>
-        <Input
-          label='Level 0'
-          value={concept.name}
-          type='text'
-        />
-      </div>
-    }
   }
 
   render() {
     return  (
-      <div className="concept-box">
-        <p>Level {this.props.levelNumber}</p>
-        <h1>{this.state.concept.name}</h1>
-        <p>UID: {this.state.concept.uid}</p>
-        {this.renderLevels()}
-      </div>
+      <Query
+        query={gql(conceptQuery(this.props.conceptID))}
+      >
+        {({ loading, error, data }) => {
+          debugger;
+          if (loading) return <p>Loading...</p>;
+          if (error) return <p>Error :(</p>;
+          const concept:QueryResult = data.concept;
+          console.log('concept', concept)
+          return (
+            <ConceptBox concept={concept} levelNumber={this.props.levelNumber} />
+          )
+        }}
+      </Query>
     )
   }
 
 };
 
-export default ConceptBox
+export default ConceptBoxContainer
