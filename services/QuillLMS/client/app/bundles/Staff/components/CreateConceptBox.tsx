a73578e02213a114308d6e8659d53045881c6ccf
Emilia Friedberg <emiliafriedberg@gmail.com>
2019-02-13 11:21:59 -0500

    mostly finish add concepts page

--- services/QuillLMS/client/app/bundles/Staff/components/CreateConceptBox.tsx
+++ services/QuillLMS/client/app/bundles/Staff/components/CreateConceptBox.tsx
@@ -6,18 +6,22 @@ import _ from 'lodash'
 import Input from '../../Teacher/components/shared/input'
 import DropdownInput from '../../Teacher/components/shared/dropdown_input'
 
-function levelTwoConceptsQuery(){
+function levelTwoConceptsQuery() {
   return `
   {
     concepts(levelTwoOnly: true) {
       value: id
-      label: name
+      label: name,
+      children: {
+        value: id
+        label: id
+      }
     }
   }
 `
 }
 
-function levelOneConceptsQuery(){
+function levelOneConceptsQuery() {
   return `
   {
     concepts(levelOneOnly: true) {
@@ -54,24 +58,44 @@ export interface Concept {
 interface CreateConceptBoxProps {
   levelNumber: Number;
   finishEditingOrCreatingConcept: Function;
+  concepts: [Concept];
 }
 
 interface CreateConceptBoxState {
   concept: { parent: Concept|{}, name: string }
+  level1Concepts: [Concept],
+  level2Concepts: [Concept]
 }
 
 class CreateConceptBox extends React.Component<CreateConceptBoxProps, CreateConceptBoxState> {
   constructor(props){
     super(props)
 
+    const { level2Concepts, level1Concepts } = this.sortConcepts(props)
+
     this.state = {
       concept: { name: '', parent: {}},
+      level2Concepts,
+      level1Concepts
     }
 
     this.changeLevel1 = this.changeLevel1.bind(this)
     this.changeLevel2 = this.changeLevel2.bind(this)
     this.renameConcept = this.renameConcept.bind(this)
     this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  sortConcepts(props):{level2Concepts: [Concept], level1Concepts: [Concept]} {
+    let level1Concepts:[Concept] = []
+    let level2Concepts:[Concept] = []
+    props.concepts.forEach(c => {
+      if (c.parent && !c.parent.parent) {
+        level1Concepts.push(c)
+      } else if (!c.parent) {
+        level2Concepts.push(c)
+      }
+    })
+    return { level2Concepts, level1Concepts }
   }
 
   renameConcept(e) {
@@ -90,15 +114,17 @@ class CreateConceptBox extends React.Component<CreateConceptBoxProps, CreateConc
   }
 
   changeLevel1(level1Concept) {
+    const { concept, level1Concepts, } = this.state
+    const savedLevel1Concept = level1Concepts.find(c => c.id === level1Concept.value)
     const newParent = {
-      id: level1Concept.value,
-      name: level1Concept.label,
+      id: savedLevel1Concept.id,
+      name: savedLevel1Concept.name,
       parent: {
-        id: level1Concept.parent.value,
-        name: level1Concept.parent.label
+        id: savedLevel1Concept.parent.id,
+        name: savedLevel1Concept.parent.name
       }
     }
-    const newConcept = Object.assign({}, this.state.concept, { parent: newParent })
+    const newConcept = Object.assign({}, concept, { parent: newParent })
     this.setState({ concept: newConcept })
   }
 
@@ -107,7 +133,12 @@ class CreateConceptBox extends React.Component<CreateConceptBoxProps, CreateConc
       id: level2Concept.value,
       name: level2Concept.label,
     }
-    const newConcept = Object.assign({}, this.state.concept, { parent: newParent })
+    let newConcept
+    if (this.props.levelNumber === 1) {
+      newConcept = Object.assign({}, this.state.concept, { parent: newParent })
+    } else {
+      newConcept = Object.assign({}, this.state.concept, { parent: { parent: newParent, } })
+    }
     this.setState({ concept: newConcept })
   }
 
@@ -115,46 +146,41 @@ class CreateConceptBox extends React.Component<CreateConceptBoxProps, CreateConc
     document.getElementById('concept-name').focus()
   }
 
-  renderDropdownInput() {
-    const { concept } = this.state
-    if (this.props.levelNumber === 0) {
-      return <Query
-        query={gql(levelOneConceptsQuery())}
-      >
-        {({ loading, error, data }) => {
-          console.log('error', error)
-          if (loading) return <p>Loading...</p>;
-          if (error) return <p>Error :(</p>;
-          const possibleConcepts:CascaderOptionType[] = data.concepts;
-          const options = possibleConcepts.map(c => {return { label: c.label, value: c.value }})
-          const value = possibleConcepts.find(opt => opt.value === concept.parent.id)
-          return <DropdownInput
-            label="Level 1"
-            value={value}
-            options={possibleConcepts}
-            handleChange={this.changeLevel1}
-          />
-        }}
-      </Query>
+  renderDropdownInput(dropdownLevel) {
+    console.log('dropdownLevel', dropdownLevel)
+    const { concept, level1Concepts, level2Concepts } = this.state
+    const { levelNumber} = this.props
+    if (dropdownLevel === 1) {
+      let possibleConcepts = level1Concepts
+      if (concept.parent && concept.parent.parent && concept.parent.parent.id) {
+        possibleConcepts = level1Concepts.filter(c => c.parent.id === concept.parent.parent.id)
+      }
+      const options = possibleConcepts.map(c => {return { label: c.name, value: c.id }})
+      const value = options.find(opt => opt.value === concept.parent.id)
+      return <DropdownInput
+        label="Level 1"
+        value={value}
+        options={options}
+        handleChange={this.changeLevel1}
+      />
     } else {
-      return <Query
-        query={gql(levelTwoConceptsQuery())}
-      >
-        {({ loading, error, data }) => {
-          if (loading) return <p>Loading...</p>;
-          if (error) return <p>Error :(</p>;
-          const possibleConcepts:CascaderOptionType[] = data.concepts;
-          const options = possibleConcepts.map(c => {return { label: c.label, value: c.value }})
-          const value = possibleConcepts.find(opt => opt.value === concept.parent.id)
-          return <DropdownInput
-            label="Level 2"
-            value={value}
-            options={possibleConcepts}
-            handleChange={this.changeLevel2}
-          />
-        }}
-      </Query>
-
+      let possibleConcepts = level2Concepts
+      if (levelNumber === 0 && concept.parent.id) {
+        possibleConcepts = [concept.parent.parent]
+      }
+      const options = possibleConcepts.map(c => {return { label: c.name, value: c.id }})
+      let value
+      if (levelNumber === 0 && concept.parent.parent) {
+        value = options.find(opt => opt.value === concept.parent.parent.id)
+      } else {
+        value = options.find(opt => opt.value === concept.parent.id)
+      }
+      return <DropdownInput
+        label="Level 2"
+        value={value}
+        options={options}
+        handleChange={this.changeLevel2}
+      />
     }
   }
 
@@ -167,39 +193,31 @@ class CreateConceptBox extends React.Component<CreateConceptBoxProps, CreateConc
             label='Level 2'
             value={concept.name}
             type='text'
-            id='concept-name'
             handleChange={this.renameConcept}
           />
         </div>
       </div>
     } else if (this.props.levelNumber === 1) {
       return <div>
-        {this.renderDropdownInput()}
+        {this.renderDropdownInput(2)}
         <div className="concept-input-container">
           <Input
             label='Level 1'
             value={concept.name}
             type='text'
-            id='concept-name'
             handleChange={this.renameConcept}
           />
         </div>
       </div>
     } else if (this.props.levelNumber === 0) {
       return <div>
-        <Input
-          disabled={true}
-          label='Level 2'
-          value={concept.parent && concept.parent.parent ? concept.parent.parent.name : ''}
-          type='text'
-        />
-        {this.renderDropdownInput()}
+        {this.renderDropdownInput(2)}
+        {this.renderDropdownInput(1)}
         <div className="concept-input-container">
           <Input
             label='Level 0'
             value={concept.name}
             type='text'
-            id='concept-name'
             handleChange={this.renameConcept}
           />
         </div>
@@ -208,8 +226,15 @@ class CreateConceptBox extends React.Component<CreateConceptBoxProps, CreateConc
   }
 
   renderSaveButton() {
+    const { levelNumber } = this.props
     const { concept } = this.state
-    if (concept.name) {
+    if (levelNumber === 2 && concept.name) {
+      return <input
+        type="submit"
+        value={`Add New Level ${this.props.levelNumber}`}
+        className="button contained primary medium"
+      />
+    } else if ((levelNumber === 1 || levelNumber === 2) && concept.parent.id) {
       return <input
         type="submit"
         value={`Add New Level ${this.props.levelNumber}`}
