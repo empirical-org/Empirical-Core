36a5d69639209192b677d33a960da1004f8f9e58
Emilia Friedberg <emiliafriedberg@gmail.com>
2019-02-14 16:36:04 -0500

    fix csv formatting

--- services/QuillLMS/app/controllers/cms/concepts_controller.rb
+++ services/QuillLMS/app/controllers/cms/concepts_controller.rb
@@ -23,9 +23,10 @@ class Cms::ConceptsController < Cms::CmsController
     stored_concepts_in_use = $redis.get('CONCEPTS_IN_USE')
     if stored_concepts_in_use
       concepts_in_use = JSON.parse(stored_concepts_in_use)
+      csv_str = concepts_in_use.inject([]) { |csv, rows|  csv << CSV.generate_line(rows) }.join("")
       respond_to do |format|
         format.html
-        format.csv { render csv: concepts_in_use, filename: 'concepts_in_use'}
+        format.csv { render csv: csv_str, filename: 'concepts_in_use'}
       end
     end
   end
