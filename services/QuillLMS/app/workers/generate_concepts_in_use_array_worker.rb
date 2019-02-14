d55f550760f34b3ba87bb5eda0bb0b61b34e94d3
Emilia Friedberg <emiliafriedberg@gmail.com>
2019-02-14 16:54:34 -0500

    add current date to concepts in use csv

--- services/QuillLMS/app/workers/generate_concepts_in_use_array_worker.rb
+++ services/QuillLMS/app/workers/generate_concepts_in_use_array_worker.rb
@@ -22,7 +22,8 @@ class GenerateConceptsInUseArrayWorker
       activity_classifications.name AS classification_name,
       parent_concepts.name AS parent_name,
       grandparent_concepts.name AS grandparent_name,
-      recommendations.name AS recommendation_name
+      recommendations.name AS recommendation_name,
+      CURRENT_DATE AS last_retrieved
       FROM concepts
       LEFT JOIN concepts AS parent_concepts ON concepts.parent_id = parent_concepts.id
       LEFT JOIN concepts AS grandparent_concepts ON parent_concepts.parent_id = grandparent_concepts.id
@@ -50,7 +51,7 @@ class GenerateConceptsInUseArrayWorker
         when "Quill Connect"
           new_oc["grades_connect_activities"] << (c["activity_name"])
         when "Quill Diagnostic"
-          new_oc["grades_connect_activities"] << (c["activity_name"])
+          new_oc["grades_diagnostic_activities"] << (c["activity_name"])
         when "Quill Grammar"
           new_oc["grades_grammar_activities"] << (c["activity_name"])
         when "Quill Proofreader"
@@ -63,17 +64,19 @@ class GenerateConceptsInUseArrayWorker
         parent_name = c["parent_name"] ?  c["parent_name"] + ' | ' : ''
         new_oc = {}
         new_oc["grades_connect_activities"] = [],
+        new_oc["grades_diagnostic_activities"] = [],
         new_oc["grades_grammar_activities"] = [],
         new_oc["grades_proofreader_activities"] = []
         new_oc["diagnostic_recommendations"] = []
         new_oc["name"] = grandparent_name + parent_name + c["concept_name"]
         new_oc["uid"] = uid
+        new_oc["last_retrieved"] = c["last_retrieved"]
 
         case c["classification_name"]
         when "Quill Connect"
           new_oc["grades_connect_activities"] << (c["activity_name"])
         when "Quill Diagnostic"
-          new_oc["grades_connect_activities"] << (c["activity_name"])
+          new_oc["grades_diagnostic_activities"] << (c["activity_name"])
         when "Quill Grammar"
           new_oc["grades_grammar_activities"] << (c["activity_name"])
         when "Quill Proofreader"
@@ -89,10 +92,16 @@ class GenerateConceptsInUseArrayWorker
     end
 
     concepts_in_use = []
-    headers = %w(name uid grades_proofreader_activities grades_grammar_activities grades_connect_activities categorized_connect_questions categorized_diagnostic_questions part_of_diagnostic_recommendations)
+    headers = %w(name uid grades_proofreader_activities grades_grammar_activities grades_connect_activities grades_diagnostic_activities categorized_connect_questions categorized_diagnostic_questions part_of_diagnostic_recommendations last_retrieved)
     concepts_in_use << headers
     organized_concepts.each do |oc|
-      concepts_in_use << headers.map{ |attr| oc[attr] }
+      concepts_in_use << headers.map do |attr|
+        if oc[attr].is_a?(Array)
+          oc[attr].flatten.join(', ')
+        else
+          oc[attr]
+        end
+      end
     end
     concepts_in_use
   end
