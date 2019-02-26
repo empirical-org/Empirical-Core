f47ef9377a745234e0665351fe8ef071eef4f6d2
Thomas Robertson <thomas.robertson@quill.org>
2019-02-26 16:39:19 -0500

    Update routes and controllers for Amplify browse-all activities page

--- services/QuillLMS/app/controllers/integrations_controller.rb
+++ services/QuillLMS/app/controllers/integrations_controller.rb
@@ -14,6 +14,15 @@ class IntegrationsController < ApplicationController
     @topics = Topic.all.map{ |topic| [topic, topic.activities.production] }.select{ |group| group.second.any? }
   end
 
+  def amplify_all
+    @body_class = 'full-width-page white-page'
+    @js_file = 'public'
+    @active_tab = 'Explore All Activities'
+    @section = if params[:section_id].present? then Section.find(params[:section_id]) else Section.first end
+    @topics = @section.topics.map{ |topic| [topic, topic.activities.production] }.select{ |group| group.second.any? }
+    render :template => 'integrations/amplify'
+  end
+
   def store_partner_session
     # Assumes that each partner gets a unique action corresponding to their name
     @partner_name = action_name
