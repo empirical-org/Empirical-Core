4b7a609fb4cd33f6fa002f5e7ee35238ae76daf3
Thomas Robertson <thomas.robertson@quill.org>
2019-02-28 17:25:21 -0500

    Update RethinkDB connection to use self-signing certs
    This still needs to pull out of .env rather than being an fs.readFileSync call, but the connection otherwise seems to work.  If testing needs to be done locally, you can pull the cert from compose.io and put it into a file with the referenced name.

--- services/QuillGraphQL/src/utils/rethinkdb.ts
+++ services/QuillGraphQL/src/utils/rethinkdb.ts
@@ -1,10 +1,12 @@
+const fs = require('fs');
+
 export default require('rethinkdbdash')({
   port: process.env.RETHINKDB_PORT,
   host: process.env.RETHINKDB_HOST,
   user: process.env.RETHINKDB_USER,
   password: process.env.RETHINKDB_PASS,
   ssl: {
-    key: process.env.RETHINKDB_TLS_KEY,
-    cert: process.env.RETHINKDB_TLS_CERT
+    cert: fs.readFileSync('rethinkdb.crt', 'utf8'),
+    ca: fs.readFileSync('rethinkdb.crt', 'utf8')
   }
 });
