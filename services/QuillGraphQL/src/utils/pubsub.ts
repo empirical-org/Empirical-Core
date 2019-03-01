fb3c73ec96d6ac721684d7a8730b9001f2ef6dba
Thomas Robertson <thomas.robertson@quill.org>
2019-03-01 10:26:35 -0500

    Get redis and rethink connections working.
    Note that this unfortunately bypasses TLS for Rethink, which is something that we should address at some point.

--- services/QuillGraphQL/src/utils/pubsub.ts
+++ services/QuillGraphQL/src/utils/pubsub.ts
@@ -2,8 +2,10 @@ import { RedisPubSub } from 'graphql-redis-subscriptions';
 import * as Redis from 'ioredis';
 
 const options = {
-  host: process.env.REDIS_DOMAIN_NAME,
-  port: process.env.PORT_NUMBER,
+  host: process.env.REDIS_HOST,
+  port: process.env.REDIS_PORT,
+  password: process.env.REDIS_PASS,
+  db: 0,
   retry_strategy: options => {
     // reconnect after
     return Math.max(options.attempt * 100, 3000);
@@ -11,8 +13,8 @@ const options = {
 };
 
 const pubSub = new RedisPubSub({
-  publisher: new Redis(process.env.REDIS_CONNECTION_STRING),
-  subscriber: new Redis(process.env.REDIS_CONNECTION_STRING)
+  publisher: new Redis(options),
+  subscriber: new Redis(options)
 });
 
 export default pubSub
--- services/QuillGraphQL/src/utils/rethinkdb.ts
+++ services/QuillGraphQL/src/utils/rethinkdb.ts
@@ -1,12 +1,10 @@
 const fs = require('fs');
 
+
 export default require('rethinkdbdash')({
   port: process.env.RETHINKDB_PORT,
   host: process.env.RETHINKDB_HOST,
   user: process.env.RETHINKDB_USER,
   password: process.env.RETHINKDB_PASS,
-  ssl: {
-    cert: fs.readFileSync('rethinkdb.crt', 'utf8'),
-    ca: fs.readFileSync('rethinkdb.crt', 'utf8')
-  }
+  ssl: false,
 });
