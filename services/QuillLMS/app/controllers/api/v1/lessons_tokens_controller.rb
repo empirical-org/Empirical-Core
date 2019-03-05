10b9ee84094e3827457b2fed7d405733e2c28e32
Thomas Robertson <thomas.robertson@quill.org>
2019-03-05 14:21:08 -0500

    Set the 'Access-Control-Allow-Credentials' header
    It's required for AJAX POST requests to work for some reason.

--- services/QuillLMS/app/controllers/api/v1/lessons_tokens_controller.rb
+++ services/QuillLMS/app/controllers/api/v1/lessons_tokens_controller.rb
@@ -5,6 +5,9 @@ class Api::V1::LessonsTokensController < Api::ApiController
     token = CreateLessonsToken
       .new(current_user, params[:classroom_unit_id])
       .call
+    # This HEADER is required when POST requests are made by Javascript
+    # At least, that's what we think to be the case
+    headers['Access-Control-Allow-Credentials'] = 'true'
     render json: { token: token }
   end
 end
