855b233f7ca26d045e2ca6051b7b3208747b9012
Emilia Friedberg <emiliafriedberg@gmail.com>
2019-03-12 10:29:40 -0400

    use number of sentences rather than activities

--- services/QuillLMS/app/workers/SetNumberOfStudentsSentencesAndCitiesWorker.rb
+++ services/QuillLMS/app/workers/SetNumberOfStudentsSentencesAndCitiesWorker.rb
@@ -1,11 +1,11 @@
-class SetNumberOfStudentsActivitiesAndCitiesWorker
+class SetNumberOfStudentsSentencesAndCitiesWorker
   include Sidekiq::Worker
 
   def perform
-    set_number_of_students_activities_and_cities_worker
+    set_number_of_students_sentences_and_cities_worker
   end
 
-  def set_number_of_students_activities_and_cities_worker
+  def set_number_of_students_sentences_and_cities_worker
     activity_sessions_query = ActiveRecord::Base.connection.execute("
       SELECT
         COUNT(DISTINCT user_id) AS number_of_students,
@@ -21,13 +21,13 @@ class SetNumberOfStudentsActivitiesAndCitiesWorker
         JOIN schools_users ON schools.id = schools_users.id
         ) AS school_cities
       ")
-    number_of_activities = activity_sessions_query.to_a[0]['number_of_activities'].to_i.round(-6)
-    $redis.set("NUMBER_OF_ACTIVITIES", number_of_activities)
-    number_of_students = activity_sessions_query.to_a[0]['number_of_students'].to_i.round(-3)
+    number_of_sentences = activity_sessions_query.to_a[0]['number_of_activities'].to_i.floor(-5) * 10
+    $redis.set("NUMBER_OF_SENTENCES", number_of_sentences)
+    number_of_students = activity_sessions_query.to_a[0]['number_of_students'].to_i.floor(-5)
     $redis.set("NUMBER_OF_STUDENTS", number_of_students)
-    number_of_cities = cities_query.to_a[0]['count'].to_i.round(-3)
+    number_of_cities = cities_query.to_a[0]['count'].to_i.floor(-3)
     $redis.set("NUMBER_OF_CITIES", number_of_cities)
-    $redis.set("NUMBER_OF_STUDENTS_ACTIVITIES_AND_CITIES_LAST_SET", Time.now)
+    $redis.set("NUMBER_OF_STUDENTS_SENTENCES_AND_CITIES_LAST_SET", Time.now)
   end
 
 end
