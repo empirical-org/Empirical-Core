7bdb3400f4702a96ccc49bea1f47ad9657255874
Emilia Friedberg <emiliafriedberg@gmail.com>
2019-03-13 16:07:21 -0400

    students get sent back to the /join link after they log in/sign up

--- services/QuillLMS/app/controllers/students_controller.rb
+++ services/QuillLMS/app/controllers/students_controller.rb
@@ -1,7 +1,7 @@
 class StudentsController < ApplicationController
   include QuillAuthentication
 
-  before_filter :authorize!, except: [:student_demo]
+  before_filter :authorize!, except: [:student_demo, :join_classroom]
 
   def index
     @current_user = current_user
@@ -38,22 +38,28 @@ class StudentsController < ApplicationController
   end
 
   def join_classroom
-    classcode = params[:classcode].downcase
-    begin
-      classroom = Classroom.find_by(code: classcode)
-      Associators::StudentsToClassrooms.run(current_user, classroom)
-      JoinClassroomWorker.perform_async(current_user.id)
-    rescue NoMethodError => exception
-      if Classroom.unscoped.find_by(code: classcode).nil?
-        InvalidClasscodeWorker.perform_async(current_user.id, params[:classcode], classcode)
-        flash[:error] = 'Oops! That link had an invalid class code. Ask your teacher for help.'
+    if current_user
+      classcode = params[:classcode].downcase
+      begin
+        classroom = Classroom.find_by(code: classcode)
+        Associators::StudentsToClassrooms.run(current_user, classroom)
+        JoinClassroomWorker.perform_async(current_user.id)
+      rescue NoMethodError => exception
+        if Classroom.unscoped.find_by(code: classcode).nil?
+          InvalidClasscodeWorker.perform_async(current_user.id, params[:classcode], classcode)
+          flash[:error] = 'Oops! That link had an invalid class code. Ask your teacher for help.'
+        else
+          flash[:error] = 'Oops! The class with that class code is archived. Ask your teacher for help.'
+        end
+        flash.keep(:error)
+        redirect_to '/profile'
       else
-        flash[:error] = 'Oops! The class with that class code is archived. Ask your teacher for help.'
+        redirect_to "/classrooms/#{classroom.id}?joined=success"
       end
-      flash.keep(:error)
-      redirect_to '/profile'
     else
-      redirect_to "/classrooms/#{classroom.id}?joined=success"
+      session[:post_auth_redirect] = request.env['PATH_INFO']
+      session[:post_sign_up_redirect] = request.env['PATH_INFO']
+      redirect_to(new_session_path, status: :see_other)
     end
   end
 
