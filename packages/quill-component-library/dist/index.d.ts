270ef1dae69fb4b348af75f8b2e1d0264fa39f6f
Emilia Friedberg <emiliafriedberg@gmail.com>
2019-04-04 12:41:18 -0400

    build new version of qcl

--- packages/quill-component-library/dist/index.d.ts
+++ packages/quill-component-library/dist/index.d.ts
@@ -2,7 +2,7 @@ import './styles/styles.scss';
 export { QuestionList, LinkListItem, QuestionListByConcept } from './components/questionList/index';
 export { Instructions, WarningDialogue, Prompt } from './components/fillInBlank/index';
 export { ConceptExplanation, } from './components/feedback/conceptExplanation';
-export { ArchivedButton, Card, CarouselAnimation, DiagnosticProgressBar, DropdownInput, Error, FlagDropdown, Input, Passthrough, ResumeOrBeginButton, SmartSpinner, Spinner, TextEditor } from './components/shared/index';
+export { ArchivedButton, Card, CarouselAnimation, DiagnosticProgressBar, DropdownInput, Error, FlagDropdown, Input, Passthrough, ResumeOrBeginButton, SmartSpinner, Spinner, TextEditor, UploadOptimalResponses } from './components/shared/index';
 export { Modal } from './components/modal/modal';
 export { ListItem, SortableList } from './components/sortableList/index';
 export { AffectedResponse, PieChart, QuestionBar, ResponseSortFields, ResponseToggleFields } from './components/questions/index';
