8748ced729c7b7185e0b110f9cd4d5e27f1940a7
Emilia Friedberg <emiliafriedberg@gmail.com>
2019-04-12 13:11:58 -0400

    install new qcl where necessary

--- services/QuillLMS/client/app/bundles/Teacher/components/shared/snackbar.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-const Snackbar = props =>
-  (<div id="quill-snackbar" className={props.visible ? 'visible' : ''}>
-    {props.text}
-  </div>);
-
-export default Snackbar
