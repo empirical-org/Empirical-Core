43b17e8b744c1bd46906e2ca9a51bc06b4836187
Emilia Friedberg <emiliafriedberg@gmail.com>
2019-04-15 11:33:14 -0400

    add prompt=consent param to all google oauth links

--- services/QuillLMS/client/app/bundles/Teacher/components/accounts/new/google_sign_up.jsx
+++ services/QuillLMS/client/app/bundles/Teacher/components/accounts/new/google_sign_up.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 export default () => (
-  <a className="google-sign-up" href="/auth/google_oauth2">
+  <a className="google-sign-up" href="/auth/google_oauth2?prompt=consent">
     <img src="/images/google_icon.svg" alt="google icon"  />
     <span>Sign up with Google</span>
   </a>
