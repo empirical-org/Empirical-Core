3b94346482bd259e4bf6e4a6ced29880c88a607a
Emilia Friedberg <emiliafriedberg@gmail.com>
2019-04-15 14:20:49 -0400

    wip

--- services/QuillGrammar/src/actions/questionAndConceptMap.ts
+++ services/QuillGrammar/src/actions/questionAndConceptMap.ts
@@ -1,6 +1,8 @@
+import { hashToCollection } from 'quill-component-library/dist/componentLibrary'
+import * as _ from 'lodash'
+
 import rootRef from '../firebase';
 import { ActionTypes } from './actionTypes'
-import { hashToCollection } from 'quill-component-library/dist/componentLibrary'
 
 const	grammarQuestionsAndConceptsRef = rootRef.child('grammarQuestionsAndConceptsMap');
 const setTimeoutRef = rootRef.child('timeouts/grammarQuestionsAndConceptsMap');
@@ -12,7 +14,7 @@ interface QuestionRow {
   flag: string,
   link: string,
   concept_uid: string,
-  concept: { name: string, link: string, },
+  concept: { name: string, link: string, }|{},
   explicitlyAssignedActivities: Array<{title: string, flag: string, link: string}>,
   implicitlyAssignedActivities: Array<{title: string, flag: string, link: string}>,
   noActivities: boolean,
@@ -28,26 +30,26 @@ export function startListeningToQuestionAndConceptMapData() {
 
 export function updateData() {
   return function (dispatch: Function, getState: Function) {
-    const { concepts, questions, lessons, } = getState()
+    const { concepts, questions, grammarActivities, } = getState()
     const conceptsArray = concepts.data[0]
     const questionsArray = hashToCollection(questions.data)
-    const activitiesArray = hashToCollection(lessons.data)
+    const activitiesArray = hashToCollection(grammarActivities.data)
     const questionRows: Array<QuestionRow> = questionsArray.map((q) => {
-      const questionRow: QuestionRow = {}
+      const questionRow:QuestionRow = {}
       questionRow.concept_uid = q.concept_uid
       questionRow.prompt = q.prompt
       questionRow.flag = q.flag
       questionRow.link = `/#/admin/questions/${q.key}/responses`
       const concept = conceptsArray.find(c => c.uid === q.concept_uid)
-      questionRow.concept = { name: concept.displayName, link: `/#/admin/concepts/${concept.uid}`}
+      questionRow.concept = concept ? { name: concept.displayName, link: `/#/admin/concepts/${concept.uid}`} : {}
       const explicitlyAssignedActivities:Array<{title: string, flag: string, link: string}> = []
       const implicitlyAssignedActivities:Array<{title: string, flag: string, link: string}> = []
       activitiesArray.forEach(act => {
-        const actObj = {
-          title: act.title,
+        const actObj = _.pickBy({
+          title: act.title || 'No Name',
           flag: act.flag,
           link: `/#/admin/lessons/${act.key}/`
-        }
+        })
         if (act.questions && act.questions.find(aq => aq.key === q.key)) {
           explicitlyAssignedActivities.push(actObj)
         } else if (act.concepts && Object.keys(act.concepts).includes(q.concept_uid)) {
@@ -59,23 +61,40 @@ export function updateData() {
       questionRow.implicitlyAssignedActivities = implicitlyAssignedActivities
       return questionRow
     })
-    const conceptRows = _.groupBy(questionRows, 'concept_uid')
-    debugger;
+
+    const groupedConcepts = _.groupBy(questionRows, 'concept_uid')
+    const conceptRows = Object.keys(groupedConcepts).map(uid => {
+      const associatedQuestions = groupedConcepts[uid]
+      const conceptRow = {}
+      conceptRow.link = associatedQuestions[0].concept.link || ''
+      conceptRow.name = associatedQuestions[0].concept.name || 'Missing Concept'
+      const explicitlyAssignedActivities = []
+      const implicitlyAssignedActivities = []
+      associatedQuestions.forEach(q => {
+        // explicit and implicitly assigned activities are inverted for questions and concepts
+        // because if a concept is explicitly associated, its questions are implicitly associated, and vice versa
+        explicitlyAssignedActivities.concat(q.implicitlyAssignedActivities)
+        implicitlyAssignedActivities.concat(q.explicitlyAssignedActivities)
+      })
+      conceptRow.explicitlyAssignedActivities = _.uniq(explicitlyAssignedActivities)
+      conceptRow.implicitlyAssignedActivities = _.uniq(implicitlyAssignedActivities)
+      return conceptRow
+    })
     grammarQuestionsAndConceptsRef.child('questionRows').set(questionRows)
     grammarQuestionsAndConceptsRef.child('conceptRows').set(conceptRows)
   }
 }
 
 export function checkTimeout() {
-  setTimeoutRef.on('value', (snapshot: any) => {
-    console.log('time now: ', moment().format('x'));
-    console.log('time at snapshot: ', snapshot.val());
-    if (moment().format('x') - (snapshot.val() || 0) > 1) {
-    // if (moment().format('x') - (snapshot.val() || 0) > 300000) {
-      setTimeoutRef.set(moment().format('x'));
-      return function (dispatch: Function) {
-        dispatch(updateData)
+  return function (dispatch: Function) {
+    setTimeoutRef.once('value', (snapshot: any) => {
+      console.log('time now: ', moment().format('x'));
+      console.log('time at snapshot: ', snapshot.val());
+      if (moment().format('x') - (snapshot.val() || 0) > 3000) {
+      // if (moment().format('x') - (snapshot.val() || 0) > 300000) {
+        setTimeoutRef.set(moment().format('x'));
+        dispatch(updateData())
       }
-    }
-  });
+    })
+  };
 }
