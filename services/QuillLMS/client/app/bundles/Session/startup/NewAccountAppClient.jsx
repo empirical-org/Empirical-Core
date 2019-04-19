32c46bb2fb43b670e95190c6609f709c870ff623
Thomas Robertson <thomas.robertson@quill.org>
2019-04-19 15:59:25 -0400

    Move our metrics wrapper closer to the react component definition

--- services/QuillLMS/client/app/bundles/Session/startup/NewAccountAppClient.jsx
+++ services/QuillLMS/client/app/bundles/Session/startup/NewAccountAppClient.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import { withSegmentMetricsProps } from '../../../modules/metrics';
+
 import NewAccount from '../../Teacher/components/accounts/new/new_account_stage1.jsx';
 
-export default React.createClass({
+const NewAccountApp = React.createClass({
   render: function() {
     return (
       <NewAccount {...this.props} />
     )
   }
 });
+
+const NewAccountAppWithMetrics = withSegmentMetricsProps(NewAccountApp);
+
+export default NewAccountAppWithMetrics
--- services/QuillLMS/client/app/bundles/Session/startup/clientRegistration.js
+++ services/QuillLMS/client/app/bundles/Session/startup/clientRegistration.js
@@ -1,7 +1,4 @@
 import ReactOnRails from 'react-on-rails';
 import NewAccountApp from './NewAccountAppClient.jsx';
-import { withSegmentMetricsProps } from '../../../modules/metrics';
 
-const NewAccountAppWithMetrics = withSegmentMetricsProps(NewAccountApp);
-
-ReactOnRails.register({ NewAccountAppWithMetrics, });
+ReactOnRails.register({ NewAccountApp, });
