8fee3b66a3ad43021ffcac8453d212e2787e009d
Daniel Drabik <danieldrabik@Daniels-MacBook-Pro.local>
2019-05-08 18:27:59 -0400

    Add simple tests for user.account_days_age.

--- services/QuillLMS/spec/models/user_spec.rb
+++ services/QuillLMS/spec/models/user_spec.rb
@@ -1170,4 +1170,17 @@ describe User, type: :model do
       expect(ReferrerUser.count).to be(referrer_users)
     end
   end
+
+  describe '#account_days_age' do
+    it 'should be 0 for a new user' do
+      user = create(:user)
+      expect(user.account_days_age).to be(0)
+    end
+
+    it 'new user should have 0 days_age.' do
+      user = create(:user)
+      user.created_at = Time.zone.now - 5.days - 10.minutes
+      expect(user.account_days_age).to be(5)
+    end
+  end
 end
