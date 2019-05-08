478e836053c2c9ad7c201d62f4de90622adc2b1a
Emilia Friedberg <emiliafriedberg@gmail.com>
2019-05-08 11:48:40 -0400

    refactor button code

--- services/QuillLMS/client/app/bundles/Staff/components/styleGuide/buttons.tsx
+++ services/QuillLMS/client/app/bundles/Staff/components/styleGuide/buttons.tsx
@@ -1,139 +1,32 @@
 import * as React from "react";
 
+const renderElement =(classNames, buttonText) => (
+  <div className="element">
+    <code>{classNames}</code>
+    <button className={classNames}>{buttonText}</button>
+  </div>
+)
+
+const renderElementRow = (classNames) => (
+  <div className="element-row">
+    {renderElement(`quill-button fun ${classNames}`, 'Button')}
+    {renderElement(`quill-button small ${classNames}`, 'Button')}
+    {renderElement(`quill-button medium ${classNames}`, 'Button')}
+    {renderElement(`quill-button medium icon ${classNames}`, '♡ Button')}
+    {renderElement(`quill-button large ${classNames}`, 'Button')}
+    {renderElement(`quill-button large icon ${classNames}`, '♡ Button')}
+  </div>
+)
+
 const Buttons = () => {
   return (<div id="buttons">
     <h2>Buttons</h2>
     <div className="element-container">
-      <div className="element-row">
-        <div className="element">
-          <code>quill-button fun primary contained</code>
-          <button className="quill-button fun primary contained">Button</button>
-        </div>
-        <div className="element">
-          <code>quill-button small primary contained</code>
-          <button className="quill-button small primary contained">Button</button>
-        </div>
-        <div className="element">
-          <code>quill-button medium primary contained</code>
-          <button className="quill-button medium primary contained">Button</button>
-        </div>
-        <div className="element">
-          <code>quill-button medium icon primary contained</code>
-          <button className="quill-button medium icon primary contained">♡ Button</button>
-        </div>
-        <div className="element">
-          <code>quill-button large primary contained</code>
-          <button className="quill-button large primary contained">Button</button>
-        </div>
-        <div className="element">
-          <code>quill-button large icon primary contained</code>
-          <button className="quill-button large icon primary contained">♡ Button</button>
-        </div>
-      </div>
-      <div className="element-row">
-        <div className="element">
-          <code>quill-button fun primary outlined</code>
-          <button className="quill-button fun primary outlined">Button</button>
-        </div>
-        <div className="element">
-          <code>quill-button small primary outlined</code>
-          <button className="quill-button small primary outlined">Button</button>
-        </div>
-        <div className="element">
-          <code>quill-button medium primary outlined</code>
-          <button className="quill-button medium primary outlined">Button</button>
-        </div>
-        <div className="element">
-          <code>quill-button medium icon primary outlined</code>
-          <button className="quill-button medium icon primary outlined">♡ Button</button>
-        </div>
-        <div className="element">
-          <code>quill-button large primary outlined</code>
-          <button className="quill-button large primary outlined">Button</button>
-        </div>
-        <div className="element">
-          <code>quill-button large icon primary outlined</code>
-          <button className="quill-button large icon primary outlined">♡ Button</button>
-        </div>
-      </div>
-      <div className="element-row">
-        <div className="element">
-          <code>quill-button fun secondary outlined</code>
-          <button className="quill-button fun secondary outlined">Button</button>
-        </div>
-        <div className="element">
-          <code>quill-button small secondary outlined</code>
-          <button className="quill-button small secondary outlined">Button</button>
-        </div>
-        <div className="element">
-          <code>quill-button medium secondary outlined</code>
-          <button className="quill-button medium secondary outlined">Button</button>
-        </div>
-        <div className="element">
-          <code>quill-button medium icon secondary outlined</code>
-          <button className="quill-button medium icon secondary outlined">♡ Button</button>
-        </div>
-        <div className="element">
-          <code>quill-button large secondary outlined</code>
-          <button className="quill-button large secondary outlined">Button</button>
-        </div>
-        <div className="element">
-          <code>quill-button large icon secondary outlined</code>
-          <button className="quill-button large icon secondary outlined">♡ Button</button>
-        </div>
-      </div>
-      <div className="element-row">
-        <div className="element">
-          <code>quill-button fun disabled contained</code>
-          <button className="quill-button fun disabled contained">Button</button>
-        </div>
-        <div className="element">
-          <code>quill-button small disabled contained</code>
-          <button className="quill-button small disabled contained">Button</button>
-        </div>
-        <div className="element">
-          <code>quill-button medium disabled contained</code>
-          <button className="quill-button medium disabled contained">Button</button>
-        </div>
-        <div className="element">
-          <code>quill-button medium icon disabled contained</code>
-          <button className="quill-button medium icon disabled contained">♡ Button</button>
-        </div>
-        <div className="element">
-          <code>quill-button large disabled contained</code>
-          <button className="quill-button large disabled contained">Button</button>
-        </div>
-        <div className="element">
-          <code>quill-button large icon disabled contained</code>
-          <button className="quill-button large icon disabled contained">♡ Button</button>
-        </div>
-      </div>
-      <div className="element-row">
-        <div className="element">
-          <code>quill-button fun disabled outlined</code>
-          <button className="quill-button fun disabled outlined">Button</button>
-        </div>
-        <div className="element">
-          <code>quill-button small disabled outlined</code>
-          <button className="quill-button small disabled outlined">Button</button>
-        </div>
-        <div className="element">
-          <code>quill-button medium disabled outlined</code>
-          <button className="quill-button medium disabled outlined">Button</button>
-        </div>
-        <div className="element">
-          <code>quill-button medium icon disabled outlined</code>
-          <button className="quill-button medium icon disabled outlined">♡ Button</button>
-        </div>
-        <div className="element">
-          <code>quill-button large disabled outlined</code>
-          <button className="quill-button large disabled outlined">Button</button>
-        </div>
-        <div className="element">
-          <code>quill-button large icon disabled outlined</code>
-          <button className="quill-button large icon disabled outlined">♡ Button</button>
-        </div>
-      </div>
+      {renderElementRow('primary contained')}
+      {renderElementRow('primary outlined')}
+      {renderElementRow('secondary outlined')}
+      {renderElementRow('disabled contained')}
+      {renderElementRow('disabled outlined')}
     </div>
   </div>)
 }
