d3684820149017a5bc9fdef4315d60780d2ae9a7
Emilia Friedberg <emiliafriedberg@gmail.com>
2019-05-09 11:24:40 -0400

    rewrite menus file as a stateful component

--- services/QuillLMS/client/app/bundles/Staff/components/styleGuide/menus.tsx
+++ services/QuillLMS/client/app/bundles/Staff/components/styleGuide/menus.tsx
@@ -1,62 +1,81 @@
 import * as React from "react";
 import { DropdownInput } from 'quill-component-library/dist/componentLibrary'
 
-const Cards = () => {
+const options = [
+  {value: 1, label: 'One'},
+  {value: 2, label: 'Two'},
+  {value: 3, label: 'Three'}
+]
 
-  const options = [
-    {value: 1, label: 'One'},
-    {value: 2, label: 'Two'},
-    {value: 3, label: 'Three'}
-  ]
+class Cards extends React.Component {
 
-  return (<div id="cards">
-    <h2>Menus</h2>
-    <div className="element-container">
-      <h3>Dropdown Menu</h3>
-      <div className="element-row">
-        <div className="big-element">
-          <pre>
-            {`
-              const options = ${options}
+  constructor(props) {
+    super(props)
 
-              <DropdownInput
-                label="Concept"
-                options={options}
-                handleChange={() => {}}
-                isSearchable={false}
-            />`}
-          </pre>
-          <DropdownInput
-            label="Concept"
-            options={options}
-            handleChange={() => {}}
-            isSearchable={false}
-          />
-        </div>
-        <div className="big-element">
-          <pre>
-            {`
-              const options = ${options}
+    this.state = {
+      dropdownOne: null,
+      dropdownTwo: options[0]
+    }
 
-              <DropdownInput
-                label="Concept"
-                value={options[0]}
-                options={options}
-                handleChange={() => {}}
-                isSearchable={false}
-            />`}
-          </pre>
-          <DropdownInput
-            label="Concept"
-            value={options[0]}
-            options={options}
-            handleChange={() => {}}
-            isSearchable={false}
-          />
+    this.changeSavedValues = this.changeSavedValues.bind(this)
+  }
+
+  changeSavedValues = (key, value) => {
+    this.setState({[key]: value})
+  }
+
+  render() {
+    return (<div id="cards">
+      <h2>Menus</h2>
+      <div className="element-container">
+        <h3>Dropdown Menu</h3>
+        <div className="element-row">
+          <div className="big-element">
+            <pre>
+              {`
+                const options = ${JSON.stringify(options)}
+
+                <DropdownInput
+                  label="Concept"
+                  options={options}
+                  handleChange={(e) => {this.changeSavedValues('dropdownOne', e)}}
+                  isSearchable={false}
+                  value={this.state.dropdownOne}
+              />`}
+            </pre>
+            <DropdownInput
+              label="Concept"
+              options={options}
+              handleChange={(e) => {this.changeSavedValues('dropdownOne', e)}}
+              isSearchable={false}
+              value={this.state.dropdownOne}
+            />
+          </div>
+          <div className="big-element">
+            <pre>
+              {`
+                const options = ${JSON.stringify(options)}
+
+                <DropdownInput
+                  label="Concept"
+                  value={this.state.dropdownTwo}
+                  options={options}
+                  handleChange={(e) => {this.changeSavedValues('dropdownTwo', e)}}
+                  isSearchable={false}
+              />`}
+            </pre>
+            <DropdownInput
+              label="Concept"
+              value={this.state.dropdownTwo}
+              options={options}
+              handleChange={(e) => {this.changeSavedValues('dropdownTwo', e)}}
+              isSearchable={false}
+            />
+          </div>
         </div>
       </div>
-    </div>
-  </div>)
+    </div>)
+  }
 }
 
 export default Cards
