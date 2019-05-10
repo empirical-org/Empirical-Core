c6f689a63c28f9ff9c668862c474f0ca0cf0bed7
Thomas Robertson <thomas.robertson@quill.org>
2019-05-10 11:16:23 -0400

    Add required param to tracking event

--- services/QuillLMS/client/app/bundles/Teacher/components/accounts/reset_password/reset_password.jsx
+++ services/QuillLMS/client/app/bundles/Teacher/components/accounts/reset_password/reset_password.jsx
@@ -40,7 +40,7 @@ export default class ForgotPassword extends React.Component {
   handleSubmit(e) {
     const { timesSubmitted, password, passwordConfirmation, } = this.state
     e.preventDefault();
-    SegmentAnalytics.track(Events.SUBMIT_SAVE_NEW_PASSWORD);
+    SegmentAnalytics.track(Events.SUBMIT_SAVE_NEW_PASSWORD, {source: 'passwordResetPage'});
     request({
       url: window.location.href,
       method: 'PUT',
