b3178f9be49367a49234c728446cb75240f3e054
Thomas Robertson <thomas.robertson@quill.org>
2019-05-22 15:46:28 -0400

    Update database migration to be a bit tighter

--- services/QuillLMS/db/migrate/20190128203336_add_order_number_to_unit_activities.rb
+++ services/QuillLMS/db/migrate/20190128203336_add_order_number_to_unit_activities.rb
@@ -1,5 +1,5 @@
 class AddOrderNumberToUnitActivities < ActiveRecord::Migration
   def change
-    add_column :unit_activities, :order_number, :integer
+    add_column :unit_activities, :order_number, :integer, limit: 2, after: :due_date
   end
 end
