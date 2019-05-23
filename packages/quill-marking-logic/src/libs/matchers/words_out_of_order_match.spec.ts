54b3bbe6af51413bd32457dad9fb156450749b5c
Thomas Robertson <thomas.robertson@quill.org>
2019-05-23 19:56:47 -0400

    Update and streamline tests

--- packages/quill-marking-logic/src/libs/matchers/words_out_of_order_match.spec.ts
+++ packages/quill-marking-logic/src/libs/matchers/words_out_of_order_match.spec.ts
@@ -11,21 +11,31 @@ const savedResponses: Array<Response> = [
     feedback: "Good job, that's a sentence!",
     optimal: true,
     count: 1,
-    question_uid: "questionOne"
+    question_uid: 'questionOne'
+  },
+  {
+    id: 2,
+    text: "My cat took a nap.",
+    feedback: "The animal woofs so try again!",
+    optimal: false,
+    count: 1,
+    question_uid: 'questionOne'
   }
 ]
 
 describe('The wordsOutOfOrderMatch function', () => {
 
-  it('Determine if the provided response matches the optimal saved response when the words are rearranged', () => {
+  it('Should match the optimal saved response when the words are rearranged', () => {
       const responseString:string = "My dog a took nap.";
       const matchedResponse = wordsOutOfOrderMatch(responseString, savedResponses);
       assert.equal(matchedResponse.id, savedResponses[0].id);
   });
 
-  if('Should not match a response when words are duplicated"', () => {
-      assert false;
-  }
+  it('Should not match a response when words are duplicated"', () => {
+      const responseString:string = "My dog took a took nap.";
+      const matchedResponse = wordsOutOfOrderMatch(responseString, savedResponses);
+      assert.isUndefined(matchedResponse);
+  });
 
 });
 
@@ -33,6 +43,7 @@ describe('The wordsOutOfOrderChecker', () => {
 
   it('Should return a partialResponse object if the string matches when the words are re-ordered', () => {
     const responseString:string = "My dog a took nap.";
+    const matchedResponse = wordsOutOfOrderChecker(responseString, savedResponses);
     const partialResponse: PartialResponse =  {
         feedback: feedbackStrings.wordsOutOfOrderError,
         author: 'Words Out of Order Hint',
@@ -41,10 +52,10 @@ describe('The wordsOutOfOrderChecker', () => {
           conceptResultTemplate('5Yv4-kNHwwCO2p8HI90oqQ')
         ]
       }
-    assert.equal(wordsOutOfOrderChecker(responseString, savedResponses).feedback, partialResponse.feedback);
-    assert.equal(wordsOutOfOrderChecker(responseString, savedResponses).author, partialResponse.author);
-    assert.equal(wordsOutOfOrderChecker(responseString, savedResponses).parent_id, partialResponse.parent_id);
-    assert.equal(wordsOutOfOrderChecker(responseString, savedResponses).concept_results.length, partialResponse.concept_results.length);
+    assert.equal(matchedResponse.feedback, partialResponse.feedback);
+    assert.equal(matchedResponse.author, partialResponse.author);
+    assert.equal(matchedResponse.parent_id, partialResponse.parent_id);
+    assert.equal(matchedResponse.concept_results.length, partialResponse.concept_results.length);
   });
 
 })
