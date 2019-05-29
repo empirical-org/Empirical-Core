9bdaf1087c8162768f24281d3a400574340dbc95
Thomas Robertson <thomas.robertson@quill.org>
2019-05-29 11:15:15 -0400

    Move raw SQL-intensive query from a controller into the model

--- services/QuillLMS/app/controllers/profiles_controller.rb
+++ services/QuillLMS/app/controllers/profiles_controller.rb
@@ -100,44 +100,7 @@ protected
   def student_profile_data_sql(classroom_id=nil)
     @current_classroom = current_classroom(classroom_id)
     if @current_classroom && current_user
-      @act_sesh_records = ActiveRecord::Base.connection.execute(
-        "SELECT unit.name,
-         activity.name,
-         activity.description,
-         activity.repeatable,
-         activity.activity_classification_id,
-         unit.id AS unit_id,
-         ua.id AS ua_id,
-         unit.created_at AS unit_created_at,
-         unit.name AS unit_name,
-         cu.id AS ca_id,
-         COALESCE(cuas.completed, 'f') AS marked_complete,
-         ua.activity_id,
-         MAX(acts.updated_at) AS act_sesh_updated_at,
-         ua.order_number,
-         ua.due_date,
-         cu.created_at AS unit_activity_created_at,
-         COALESCE(cuas.locked, 'f') AS locked,
-         COALESCE(cuas.pinned, 'f') AS pinned,
-         MAX(acts.percentage) AS max_percentage,
-         SUM(CASE WHEN acts.state = 'started' THEN 1 ELSE 0 END) AS resume_link
-      FROM unit_activities AS ua
-      JOIN units AS unit ON unit.id = ua.unit_id
-      JOIN classroom_units AS cu ON unit.id = cu.unit_id
-      LEFT JOIN activity_sessions AS acts ON cu.id = acts.classroom_unit_id AND acts.activity_id = ua.activity_id AND acts.visible = true
-      AND acts.user_id = #{current_user.id}
-      JOIN activities AS activity ON activity.id = ua.activity_id
-      LEFT JOIN classroom_unit_activity_states AS cuas ON ua.id = cuas.unit_activity_id
-      AND cu.id = cuas.classroom_unit_id
-      WHERE #{current_user.id} = ANY (cu.assigned_student_ids::int[])
-      AND cu.classroom_id = #{@current_classroom.id}
-      AND cu.visible = true
-      AND unit.visible = true
-      AND ua.visible = true
-      AND 'archived' != ANY(activity.flags)
-      GROUP BY unit.id, unit.name, unit.created_at, cu.id, activity.name, activity.activity_classification_id, activity.id, activity.uid, ua.due_date, ua.created_at, unit_activity_id, cuas.completed, cuas.locked, cuas.pinned, ua.id
-
-      ORDER BY pinned DESC, locked ASC, unit.created_at ASC, max_percentage DESC, ua.order_number ASC, ua.due_date ASC, ua.id ASC").to_a
+      @act_sesh_records = UnitActivity.get_classroom_user_profile(@current_classroom.id, current_user.id)
     else
       @act_sesh_records = []
     end
