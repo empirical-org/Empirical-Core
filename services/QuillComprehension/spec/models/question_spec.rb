8f6a96672e5fea9e9e39febff231110240e2b9a7
Emilia Friedberg <emiliafriedberg@gmail.com>
2019-05-29 09:24:11 -0400

    add test for incrementing order

--- services/QuillComprehension/spec/models/question_spec.rb
+++ services/QuillComprehension/spec/models/question_spec.rb
@@ -8,4 +8,12 @@ RSpec.describe Question, :type => :model do
     expect(question.order).not_to be(nil)
   end
 
+  it "will set the order of a question to be the last one in the question set + 1" do
+    question_set = create(:question_set)
+    question1 = create(:question, order: nil, question_set: question_set)
+    question2 = create(:question, order: nil, question_set: question_set)
+    expect(question1.order).to be(0)
+    expect(question2.order).to be(1)
+  end
+
 end
