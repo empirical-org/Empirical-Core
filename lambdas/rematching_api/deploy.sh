6b90dfd8992fe5784b00f8ab733dcdf55788053c
Thomas Robertson <thomas.robertson@quill.org>
2019-06-03 15:01:01 -0400

    Add deploy scripts to the lambdas

---
+++ lambdas/rematching_api/deploy.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+zip -rq deploy_code.zip .
+aws lambda update-function-code --function-name rematchApi \
+                                --zip-file fileb://deploy_code.zip \
+                                --publish \
+                                --profile peter-aws
+rm deploy_code.zip
