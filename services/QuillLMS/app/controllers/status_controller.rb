3db0e7792d99fbbf350829a55183bccb268a5f19
Thomas Robertson <thomas.robertson@quill.org>
2019-06-05 18:08:32 -0400

    Replace Firebase check with a TODO since we don't know how it works

--- services/QuillLMS/app/controllers/status_controller.rb
+++ services/QuillLMS/app/controllers/status_controller.rb
@@ -20,11 +20,15 @@ class StatusController < ApplicationController
   end
 
   def firebase
-    classroom_unit_id = ClassroomUnit.first.id
-    host = ENV['FIREBASE_DATABASE_URL']
-    path = "/v2/classroom_lesson_sessions/#{classroom_unit_id}/students.json"
-    res = Net::HTTP.get_response(host, path)
-    raise res.body unless res.code == 200
+    # I'm not sure that this stub is even the right approach, but I do know
+    # that my lack of knowledge about Firebase authentication means that it
+    # doesn't work as written.
+    # TODO: Get an actual condition that only raises when Firebase is inaccessible.
+    #classroom_unit_id = ClassroomUnit.first.id
+    #host = ENV['FIREBASE_DATABASE_URL']
+    #path = "/v2/classroom_lesson_sessions/#{classroom_unit_id}/students.json"
+    #res = Net::HTTP.get_response(host, path)
+    #raise res.body unless res.code == 200
     render plain: 'OK'
   end
 
