614c1398a49ae6f9d26256e968de8db6423da3f9
Emilia Friedberg <emiliafriedberg@gmail.com>
2019-06-06 16:09:37 -0400

    fix jest config

--- services/QuillConnect/jest.config.js
+++ services/QuillConnect/jest.config.js
@@ -1,5 +1,14 @@
 module.exports = {
   transform: {
-    '^.+\\.(js|jsx)?$': 'babel-jest'
+    '^.+\\.(js|jsx)?$': 'babel-jest',
+    '^.+\\.tsx?$': 'ts-jest'
+  },
+  testPathIgnorePatterns: [
+    'data.ts$'
+  ],
+  globals: {
+    'ts-jest': {
+      diagnostics: false
+    }
   }
 };
