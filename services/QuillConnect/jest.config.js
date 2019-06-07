b1e95cb941e46110ff7416b6794a39950796950a
Emilia Friedberg <emiliafriedberg@gmail.com>
2019-06-07 15:07:14 -0500

    fix jest config (#5266)
    * fix jest config
    
    * Hard lock the versions of the Quill Component library
    
    This lets us avoid minor incompatibilities that may pop up when we aren't ready to deal with them.
    
    * Rewrite test to not fail because hash key order is nondeterministic
    
    The old version of this test compared JSON string literals, but that assumed that hashes with similar keys would find themselves converted to JSON with keys in the same order, and that's not always the case.
    
    * Update Connect jest with --runInBand
    
    This is supposed to make it behave better in containerized environments like Circle
    
    * Attempt tweaking our NPM caching to see if that speeds up tests
    
    * Temporarily disable all non-connect tests to troubleshoot CircleCI
    
    * Bump cache key to test new caching regime
    
    * Add maxWorkers for Circle tests
    
    * Remove --runInBand since it seems to be mutually exclusive with maxWorkers>1
    
    * Try maxWorkers=1
    
    * Try adding /node_modules/ to the test ignore list explicitly
    
    * Try specifying test directories
    
    * Turn on disabled test suites
    
    * Try limiting the search targets for Jest in LMS node tests

--- services/QuillConnect/jest.config.js
+++ services/QuillConnect/jest.config.js
@@ -1,5 +1,15 @@
 module.exports = {
   transform: {
-    '^.+\\.(js|jsx)?$': 'babel-jest'
+    '^.+\\.(js|jsx)?$': 'babel-jest',
+    '^.+\\.tsx?$': 'ts-jest'
+  },
+  testPathIgnorePatterns: [
+    '/node_modules/',
+    'data.ts$'
+  ],
+  globals: {
+    'ts-jest': {
+      diagnostics: false
+    }
   }
 };
