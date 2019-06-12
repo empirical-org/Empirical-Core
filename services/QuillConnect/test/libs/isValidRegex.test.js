4e5bb3ee26a86fc2d2d42af05c1dcdf7a0f82887
Emilia Friedberg <emiliafriedberg@gmail.com>
2019-06-12 16:18:50 -0400

    test isValidRegex function

---
+++ services/QuillConnect/test/libs/isValidRegex.test.js
@@ -0,0 +1,18 @@
+import expect from 'expect';
+import { isValidRegex } from '../../app/libs/isValidRegex.js'
+
+
+describe('#isValidRegex', () => {
+
+  it('returns true if the string passed in is a string with no regex', () => {
+    expect(isValidRegex('string')).toEqual(true)
+  });
+
+  it('returns true if the string passed in is a string with regex', () => {
+    expect(isValidRegex('string|str')).toEqual(true)
+  });
+
+  it('returns false if the string passed in contains invalid regex', () => {
+    expect(isValidRegex('(string)|(str')).toEqual(false)
+  })
+});
