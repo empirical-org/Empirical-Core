0034bc5eac47cb6588eeccfbd39f64816ffc7200
Emilia Friedberg <emiliafriedberg@gmail.com>
2019-06-19 11:11:58 -0400

    add case statement with catchall to clever integration

--- services/QuillLMS/app/services/clever_integration/sign_up/sub_main.rb
+++ services/QuillLMS/app/services/clever_integration/sign_up/sub_main.rb
@@ -3,9 +3,14 @@ module CleverIntegration::SignUp::SubMain
   # we have to send district_success, district_failure, user_success, user_failure back to controller
 
   def self.run(auth_hash, requesters)
-    result = send(auth_hash[:info][:user_type],
-                  auth_hash,
-                  requesters)
+    case auth_hash[:info][:user_type]
+    when 'district'
+      result = self.district(auth_hash, requesters)
+    when 'student'
+      result = self.district(auth_hash, requesters)
+    else
+      result = self.teacher(auth_hash, requesters)
+    end
     result
   end
 
@@ -25,4 +30,5 @@ module CleverIntegration::SignUp::SubMain
     result = CleverIntegration::SignUp::Student.run(auth_hash)
     result
   end
+
 end
