5d48595780524764ebde0643732976b0124e5181
Emilia Friedberg <emiliafriedberg@gmail.com>
2019-06-20 16:17:47 -0400

    wip - testing

--- services/QuillLMS/app/services/clever_integration/requesters.rb
+++ services/QuillLMS/app/services/clever_integration/requesters.rb
@@ -1,26 +1,26 @@
 module CleverIntegration::Requesters
 
   def self.teacher
-    self.helper('teacher')
+    self.helper('Teacher')
   end
 
   def self.section
-    self.helper('section')
+    self.helper('Section')
   end
 
   def self.district
-    self.helper('district')
+    self.helper('District')
   end
 
   def self.school_admin
-    self.helper('school_admin')
+    self.helper('SchoolAdmin')
   end
 
   private
 
   def self.helper(resource_kind)
     lambda do |clever_id, district_token|
-      klass = "Clever::#{resource_kind.capitalize}".constantize
+      klass = "Clever::#{resource_kind}".constantize
       klass.retrieve(clever_id, district_token)
     end
   end
