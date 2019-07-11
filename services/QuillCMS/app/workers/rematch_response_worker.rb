138df302eb41175d2a1f91de0750023d2f4b657d
Thomas Robertson <thomas.robertson@quill.org>
2019-07-11 17:08:47 -0400

    Move all data look-up to the individual response processor
    This is less efficient, but does prevent data from going stale in the queue

--- services/QuillCMS/app/workers/rematch_response_worker.rb
+++ services/QuillCMS/app/workers/rematch_response_worker.rb
@@ -6,17 +6,24 @@ MAX_RETRIES = 3
 class RematchResponseWorker
   include Sidekiq::Worker
 
-  def perform(response, question_type, question, reference_responses, retry_count=0)
+  def perform(response_id, question_type, question_uid, retry_count=0)
+    response = Response.find_by(id: response_id)
+    question = retrieve_question_from_firebase(question_uid, question_type)
+    reference_responses = get_human_graded_responses(question_uid).to_a
+    rematch_response(response, question_type, question, reference_responses, retry_count)
+  end
+
+  def rematch_response(response, question_type, question, reference_responses, retry_count=0)
     lambda_payload = construct_lambda_payload(response, question_type, question, reference_responses)
     updated_response = call_lambda_http_endpoint(lambda_payload)
     if updated_response["message"] == "Endpoint request timed out"
       if retry_count < MAX_RETRIES
-        RematchResponseWorker.perform_async(response, question_type, question, reference_responses, retry_count+1)
+        RematchResponseWorker.perform_async(response.id, question_type, question["key"], retry_count+1)
       else
         raise "Retry limit exceeded for Response: #{response.id}"
       end
     else
-      #response.update_attributes(updated_response)
+      response.update_attributes(updated_response)
     end
   end
 
@@ -34,6 +41,28 @@ class RematchResponseWorker
                           lambda_payload.to_json,
                           "Content-Type" => "application/json"
     JSON.parse(resp.body)
+  end
+
+  def get_human_graded_responses(question_uid)
+    Response.where(question_uid: question_uid)
+            .where.not(optimal: nil)
+            .where(parent_id: nil)
+  end
+
+  def retrieve_question_from_firebase(question_uid, question_type)
+    url = get_firebase_url(question_uid, question_type)
+    res = Net::HTTP.get URI(url)
+    question = JSON.parse(res)
+    question[:key] = question_uid
+    question.stringify_keys
+  end
+
+  def get_firebase_url(question_uid, question_type)
+    if question_type == 'grammar_questions'
+      return "#{ENV['FIREBASE_URL']}/v3/questions/#{question_uid}.json"
+    else
+      return "#{ENV['FIREBASE_URL']}/v2/#{question_type}/#{question_uid}.json"
+    end
   end
 
 end
--- services/QuillCMS/app/workers/rematch_responses_for_question_worker.rb
+++ services/QuillCMS/app/workers/rematch_responses_for_question_worker.rb
@@ -5,29 +5,15 @@ class RematchResponsesForQuestionWorker
   include Sidekiq::Worker
 
   def perform(question_uid, question_type)
-    # If we don't have any human graded responses to use as references
-    # then there's no point in algorithmically processing them because
-    # all of our current algorithms require human graded responses to
-    # use as references.
-    reference_responses = get_human_graded_responses(question_uid).to_a
-    if reference_responses.length
-      question = retrieve_question_from_firebase(question_uid, question_type)
-      responses_to_reprocess = get_ungraded_responses(question_uid) + get_machine_graded_responses(question_uid)
-      responses_to_reprocess.each do |response|
-        enqueue_individual_response(response, question_type, question, reference_responses)
-      end
-      return responses_to_reprocess.length
+    responses_to_reprocess = get_ungraded_responses(question_uid) + get_machine_graded_responses(question_uid)
+    responses_to_reprocess.each do |response|
+      enqueue_individual_response(response.id, question_type, question_uid)
     end
+    return responses_to_reprocess.length
   end
 
-  def enqueue_individual_response(response_id, question_type, question, reference_responses)
-    RematchResponseWorker.perform_async(response_id, question_type, question, reference_responses)
-  end
-
-  def get_human_graded_responses(question_uid)
-    Response.where(question_uid: question_uid)
-            .where.not(optimal: nil)
-            .where(parent_id: nil)
+  def enqueue_individual_response(response_id, question_type, question_uid)
+    RematchResponseWorker.perform_async(response_id, question_type, question_uid)
   end
 
   def get_ungraded_responses(question_uid)
@@ -40,22 +26,6 @@ class RematchResponsesForQuestionWorker
   def get_machine_graded_responses(question_uid)
     Response.where(question_uid: question_uid)
             .where("responses.parent_id IS NOT NULL OR responses.parent_uid IS NOT NULL")
-  end
-
-  def retrieve_question_from_firebase(question_uid, question_type)
-    url = get_firebase_url(question_uid, question_type)
-    res = Net::HTTP.get URI(url)
-    question = JSON.parse(res)
-    question[:key] = question_uid
-    question
-  end
-
-  def get_firebase_url(question_uid, question_type)
-    if question_type == 'grammar_questions'
-      return "#{ENV['FIREBASE_URL']}/v3/questions/#{question_uid}.json"
-    else
-      return "#{ENV['FIREBASE_URL']}/v2/#{question_type}/#{question_uid}.json"
-    end
   end
 
 end
