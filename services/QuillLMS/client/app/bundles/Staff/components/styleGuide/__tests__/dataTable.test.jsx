bcf258a97ec31ac0344d0a1595f96c8bbd4819b5
Emilia Friedberg <emiliafriedberg@gmail.com>
2019-07-23 14:55:11 -0400

    wip

--- services/QuillLMS/client/app/bundles/Staff/components/styleGuide/__tests__/dataTable.test.jsx
+++ services/QuillLMS/client/app/bundles/Staff/components/styleGuide/__tests__/dataTable.test.jsx
@@ -27,6 +27,33 @@ const headers1 = [
   }
 ]
 
+const headers2 = [
+  {
+    name: 'Name',
+    attribute: 'name',
+    width: '126px'
+  },
+  {
+    name: 'Username',
+    attribute: 'username',
+    width: '230px'
+  }
+]
+
+const headers3 = [
+  {
+    name: 'Name',
+    attribute: 'name',
+    width: '126px'
+  },
+  {
+    name: 'Activities',
+    attribute: 'activities',
+    width: '77px',
+    isSortable: true
+  }
+]
+
 const rows1 = [
   {
     name: 'Maya Angelou',
@@ -65,6 +92,63 @@ const rows1 = [
   }
 ]
 
+const rows2 = [
+  {
+    name: 'Maya Angelou',
+    username: 'maya.angelou@local-writing',
+    id: 1
+  },
+  {
+    name: 'Ambrose Bierce',
+    username: 'ambrose.bierce@local-writing',
+    id: 2
+  },
+  {
+    name: 'George Gordon Byron',
+    username: 'georgegordon.byron@local-writing',
+    id: 3
+  },
+  {
+    name: 'Elizabeth Carter',
+    username: 'elizabeth.carter@local-writing',
+    id: 4
+  },
+  {
+    name: 'Anton Chekhov',
+    username: 'anton.checkhov@local-writing',
+    id: 5
+  }
+]
+
+const rows3 = [
+  {
+    name: 'Maya Angelou',
+    activities: 15,
+    id: 1
+  },
+  {
+    name: 'Ambrose Bierce',
+    activities: 14,
+    id: 2
+  },
+  {
+    name: 'George Gordon Byron',
+    activities: 16,
+    id: 3
+  },
+  {
+    name: 'Elizabeth Carter',
+    activities: 8,
+    id: 4
+  },
+  {
+    name: 'Anton Chekhov',
+    activities: 7,
+    id: 5
+  }
+]
+
+
 describe('DataTable component', () => {
 
   describe('simple case', () => {
@@ -73,7 +157,48 @@ describe('DataTable component', () => {
     );
 
     it('should render a header row', () => {
-      expect(wrapper.find('.data-table-header').exists()).toBe(true);
+      expect(wrapper.find('.data-table-headers').exists()).toBe(true);
+    })
+
+    it('should render a header for each header in props', () => {
+      expect(wrapper.find('.data-table-header').length).toBe(headers1.length)
+    })
+
+    it('should render a row for each row in props', () => {
+      expect(wrapper.find('.data-table-row').length).toBe(rows1.length)
+    })
+  })
+
+  describe('with checkboxes', () => {
+    const wrapper = shallow(
+      <DataTable
+        rows={row2}
+        headers={headers2}
+        showCheckboxes={true}
+        checkRow={() => {}}
+        uncheckRow={() => {}}
+        uncheckAllRows={() => {}}
+        checkAllRows={() => {}}
+      />
+    )
+
+    it('should render a checkbox for each row, plus the header', () => {
+      expect(wrapper.find('.quill-checkbox').length).toBe(rows2.length + 1)
+    })
+  })
+
+  describe('with sorting', () => {
+    const wrapper = shallow(
+      <DataTable
+        rows={rows3}
+        headers={headers3}
+        defaultSortAttribute='activities'
+      />
+    )
+
+    it('should render the rows in ascending order of activities', () => {
+      const sortedRows = rows3.sort((a, b) => b['activities'] - a['activities'])
+      expect(wrapper.find('.data-table-row-section').first()).toBe(sortedRows[0].name)
     })
   })
 
--- services/QuillLMS/client/app/bundles/Staff/components/styleGuide/dataTables.tsx
+++ services/QuillLMS/client/app/bundles/Staff/components/styleGuide/dataTables.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react'
-// import { DataTable } from 'quill-component-library/dist/componentLibrary'
-import { DataTable } from './dataTable/dataTable'
+import { DataTable } from 'quill-component-library/dist/componentLibrary'
 
 const headers1 = [
   {
