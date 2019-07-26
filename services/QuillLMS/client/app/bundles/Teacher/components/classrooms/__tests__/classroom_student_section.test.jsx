4d15d32c77900730a00a72f5ba96e077fe19a460
Emilia Friedberg <emiliafriedberg@gmail.com>
2019-07-26 09:29:11 -0400

    add tests for new/changed components

--- services/QuillLMS/client/app/bundles/Teacher/components/classrooms/__tests__/classroom_student_section.test.jsx
+++ services/QuillLMS/client/app/bundles/Teacher/components/classrooms/__tests__/classroom_student_section.test.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import { shallow } from 'enzyme';
 
 import ClassroomStudentSection from '../classroom_student_section'
+import EditStudentAccountModal from '../edit_student_account_modal'
+import ResetStudentPasswordModal from '../reset_student_password_modal'
+
 import { DropdownInput, DataTable } from 'quill-component-library/dist/componentLibrary'
 
 import { classroomWithStudents, classroomWithoutStudents, userProps } from './test_data/test_data'
@@ -11,7 +14,12 @@ describe('ClassroomStudentSection component', () => {
   describe('classroom without students', () => {
 
     const wrapper = shallow(
-      <ClassroomStudentSection classroom={classroomWithoutStudents} user={userProps} />
+      <ClassroomStudentSection
+        classroom={classroomWithoutStudents}
+        user={userProps}
+        onSuccess={() => {}}
+        inviteStudents={() => {}}
+      />
     );
 
     it('should render', () => {
@@ -27,7 +35,12 @@ describe('ClassroomStudentSection component', () => {
   describe('with students', () => {
 
     const wrapper = shallow(
-      <ClassroomStudentSection classroom={classroomWithStudents} user={userProps} />
+      <ClassroomStudentSection
+        classroom={classroomWithStudents}
+        user={userProps}
+        onSuccess={() => {}}
+        inviteStudents={() => {}}
+      />
     );
 
     it('should render', () => {
@@ -40,6 +53,16 @@ describe('ClassroomStudentSection component', () => {
 
     it('should render a data table', () => {
       expect(wrapper.find(DataTable).exists()).toBe(true)
+    })
+
+    it('should render the EditStudentAccountModal if showEditStudentAccountModal is true', () => {
+      wrapper.instance().setState({ showEditStudentAccountModal: true, studentIdsForModal: [classroomWithStudents.students[0].id] })
+      expect(wrapper.find(EditStudentAccountModal).exists()).toBe(true)
+    })
+
+    it('should render the ResetStudentPasswordModal if showResetStudentPasswordModal is true', () => {
+      wrapper.instance().setState({ showResetStudentPasswordModal: true, studentIdsForModal: [classroomWithStudents.students[0].id] })
+      expect(wrapper.find(ResetStudentPasswordModal).exists()).toBe(true)
     })
 
   })
--- services/QuillLMS/client/app/bundles/Teacher/components/classrooms/__tests__/classroom_teacher_section.test.jsx
+++ services/QuillLMS/client/app/bundles/Teacher/components/classrooms/__tests__/classroom_teacher_section.test.jsx
@@ -1,47 +1,23 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 
-import ClassroomStudentSection from '../classroom_student_section'
-import { DropdownInput, DataTable } from 'quill-component-library/dist/componentLibrary'
+import ClassroomTeacherSection from '../classroom_teacher_section'
+import { DataTable } from 'quill-component-library/dist/componentLibrary'
 
-import { classroomWithStudents, classroomWithoutStudents, userProps } from './test_data/test_data'
+import { classroomWithoutStudents, userProps } from './test_data/test_data'
 
-describe('ClassroomStudentSection component', () => {
+describe('ClassroomTeacherSection component', () => {
 
-  describe('classroom without students', () => {
+  const wrapper = shallow(
+    <ClassroomTeacherSection classroom={classroomWithoutStudents} user={userProps} />
+  );
 
-    const wrapper = shallow(
-      <ClassroomStudentSection classroom={classroomWithoutStudents} user={userProps} />
-    );
+  it('should render', () => {
+    expect(wrapper).toMatchSnapshot();
+  });
 
-    it('should render', () => {
-      expect(wrapper).toMatchSnapshot();
-    });
-
-    it('should render a no-students section', () => {
-      expect(wrapper.find('.no-students').exists()).toBe(true)
-    })
-
-  })
-
-  describe('with students', () => {
-
-    const wrapper = shallow(
-      <ClassroomStudentSection classroom={classroomWithStudents} user={userProps} />
-    );
-
-    it('should render', () => {
-      expect(wrapper).toMatchSnapshot();
-    });
-
-    it('should render a students actions dropdown', () => {
-      expect(wrapper.find(DropdownInput).exists()).toBe(true)
-    })
-
-    it('should render a data table', () => {
-      expect(wrapper.find(DataTable).exists()).toBe(true)
-    })
-
+  it('should render a data table', () => {
+    expect(wrapper.find(DataTable).exists()).toBe(true)
   })
 
 });
