e848678810fc6eba7b848d51d2e5fb3ecec60fee
Emilia Friedberg <emiliafriedberg@gmail.com>
2019-08-06 13:37:55 -0400

    fix formatting

--- services/QuillLMS/app/controllers/teachers/classrooms_controller.rb
+++ services/QuillLMS/app/controllers/teachers/classrooms_controller.rb
@@ -38,7 +38,7 @@ class Teachers::ClassroomsController < ApplicationController
     if @classroom.valid?
       render json: {classroom: @classroom}
     else
-       render json: {errors: @classroom.errors }
+      render json: {errors: @classroom.errors}
     end
   end
 
