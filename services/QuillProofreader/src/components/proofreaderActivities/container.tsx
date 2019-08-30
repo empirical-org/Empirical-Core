0975e0c47f95b71163edc79cc60450fa2c2dda2c
Emilia Friedberg <emiliafriedberg@gmail.com>
2019-08-30 13:51:12 -0400

    change firebase save interval to 30 seconds

--- services/QuillProofreader/src/components/proofreaderActivities/container.tsx
+++ services/QuillProofreader/src/components/proofreaderActivities/container.tsx
@@ -52,7 +52,7 @@ interface PlayProofreaderContainerState {
   conceptResultsObjects?: ConceptResultObject[];
 }
 
-const FIREBASE_SAVE_INTERVAL = 15000 // 15 seconds
+const FIREBASE_SAVE_INTERVAL = 30000 // 30 seconds
 
 export class PlayProofreaderContainer extends React.Component<PlayProofreaderContainerProps, PlayProofreaderContainerState> {
     private interval: any
