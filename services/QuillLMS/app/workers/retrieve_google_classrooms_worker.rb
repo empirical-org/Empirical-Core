060684267bda7515dcafec5f06dd537a7b7fd0d4
dandrabik <dandrabik@users.noreply.github.com>
2019-09-06 18:11:45 -0400

    Fix a naming bug and add a test. (#5420)
    * Fix a naming bug and add a test.
    
    * remove unneeded stub.
    
    * Other test fix.

--- services/QuillLMS/app/workers/retrieve_google_classrooms_worker.rb
+++ services/QuillLMS/app/workers/retrieve_google_classrooms_worker.rb
@@ -10,8 +10,8 @@ class RetrieveGoogleClassroomsWorker
     google_response = GoogleIntegration::Classroom::Main.pull_data(user)
     data = google_response === 'UNAUTHENTICATED' ? {errors: google_response} : {classrooms: google_response}
     serialized_data = data.to_json
-    $redis.set("#{ClassroomManagerController::SERIALIZED_GOOGLE_CLASSROOMS_FOR_}#{user_id}", serialized_data)
-    $redis.expire("#{ClassroomManagerController::SERIALIZED_GOOGLE_CLASSROOMS_FOR_}#{user_id}", SERIALIZED_GOOGLE_CLASSROOMS_CACHE_LIFE)
+    $redis.set("#{Teachers::ClassroomManagerController::SERIALIZED_GOOGLE_CLASSROOMS_FOR_}#{user_id}", serialized_data)
+    $redis.expire("#{Teachers::ClassroomManagerController::SERIALIZED_GOOGLE_CLASSROOMS_FOR_}#{user_id}", SERIALIZED_GOOGLE_CLASSROOMS_CACHE_LIFE)
     PusherTrigger.run(user_id, 'google-classrooms-retrieved', "Google classrooms found for #{user_id}.")
   end
 end
