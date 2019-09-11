7bf0c9751465346fdef0e1b6154d951372f71894
Emilia Friedberg <emiliafriedberg@gmail.com>
2019-09-11 09:22:25 -0400

    featured activity packs update (#5422)
    * wip
    
    * working on getting the types in
    
    * get filtering by activity pack type working
    
    * get front end working correctly
    
    * tests
    
    * replace standards count with type
    
    * cleanup
    
    * one more piece of cleanup

--- services/QuillLMS/client/app/bundles/Teacher/components/lesson_planner/unit_templates_manager/unit_template_minis/unit_template_mini/unit_template_first_row.jsx
+++ services/QuillLMS/client/app/bundles/Teacher/components/lesson_planner/unit_templates_manager/unit_template_minis/unit_template_mini/unit_template_first_row.jsx
@@ -1,54 +1,33 @@
-'use strict'
+import React from 'react'
+import moment from 'moment'
 
- import React from 'react'
- import CategoryLabel from '../../../category_labels/category_label'
- import moment from 'moment'
+import CategoryLabel from '../../../category_labels/category_label'
 
- const cutOffTimeForNew = moment().subtract('months', 1).unix()
+const cutOffTimeForNew = moment().subtract('months', 1).unix()
 
- export default React.createClass({
-  propTypes: {
-    data: React.PropTypes.object.isRequired,
-    actions: React.PropTypes.object,
-    modules: React.PropTypes.shape({
-      string: React.PropTypes.object.isRequired
-    })
-  },
+export default class UnitTemplateFirstRow extends React.Component {
+  getBackgroundColor() {
+    return this.props.data.type.primary_color;
+  }
 
-  sayNumberOfStandards: function () {
-    return this.props.modules.string.sayNumberOfThings(this.props.data.number_of_standards, 'Standard', 'Standards');
-  },
-
-  getBackgroundColor: function () {
-    return this.props.data.unit_template_category.primary_color;
-  },
-
-  getCategoryBackgroundColor: function () {
-    return this.props.data.unit_template_category.secondary_color;
-  },
-
-  newFlag: function(){
+  newFlag() {
     if (cutOffTimeForNew < this.props.data.created_at) {
       return <span className='new-flag category-label'>NEW</span>
     }
-  },
+  }
 
-  render: function () {
+  render() {
     return (
       <div style={{backgroundColor: this.getBackgroundColor()}} className='first-row'>
         <div className='col-xs-12'>
-          <div className='row' style={{padding: '0px 15px', display: 'flex', alignItems: 'center', justifyContent: 'space-between'}}>
-            <div style={{flex: 1}}>
-              <div className='standards-count'>
-                {this.sayNumberOfStandards()}
-              </div>
+          <div style={{display: 'flex', alignItems: 'center', justifyContent: 'space-between'}}>
+            <div className='unit-template-type'>
+              {this.props.data.type.name}
             </div>
-            <div className='text-right' style={{flex: 1}}>
-              <CategoryLabel
-                  data={this.props.data.unit_template_category}
-                  extraClassName='float-right'
-                  backgroundColor={this.getCategoryBackgroundColor()}/>
-            </div>
+            <CategoryLabel
+              data={this.props.data.unit_template_category}
+              extraClassName='float-right'
+            />
           </div>
           <div className='row'>
             <div className='col-xs-12'>
@@ -63,4 +42,4 @@
     );
   }
 
-});
+}
