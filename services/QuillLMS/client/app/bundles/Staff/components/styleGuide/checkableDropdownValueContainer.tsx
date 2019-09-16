33a7c3666b08df666e785d45758351c34d6bb3de
Emilia Friedberg <emiliafriedberg@gmail.com>
2019-09-16 16:08:11 -0400

    dropdown revisions

--- services/QuillLMS/client/app/bundles/Staff/components/styleGuide/checkableDropdownValueContainer.tsx
+++ services/QuillLMS/client/app/bundles/Staff/components/styleGuide/checkableDropdownValueContainer.tsx
@@ -4,7 +4,7 @@ import { components } from 'react-select';
 export const CheckableDropdownValueContainer = props => {
   const { optionType, value, options } = props.selectProps
   let text = `${value.length} ${optionType}${value.length === 1 ? '' : 's'}`
-  if (value.length >= options.length) {
+  if (value.length === options.length - 1) {
     text = `All ${optionType}s`
   }
   return (
