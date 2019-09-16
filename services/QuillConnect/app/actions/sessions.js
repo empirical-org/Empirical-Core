4ba229362ffdc5a134e69ea9fcd0a633bfa0b261
Thomas Robertson <thomas.robertson@quill.org>
2019-09-16 14:31:53 -0400

    Increase the percentage of sessions getting new style sessions

--- services/QuillConnect/app/actions/sessions.js
+++ services/QuillConnect/app/actions/sessions.js
@@ -42,7 +42,7 @@ export default {
 
     // This is the whole number percentage of users who will be assigned
     // to the new session type.
-    const percentAssigned = 10;
+    const percentAssigned = 50;
     if (sessionID && simpleHash(sessionID) % 100 < percentAssigned) {
       const normalizedSession = normalizeSession(cleanedSession)
       // Let's start including an updated time on our sessions
