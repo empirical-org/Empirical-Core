8e3ac5edc83e353c456120f2ba362aa3e2630546
Emilia Friedberg <emiliafriedberg@gmail.com>
2019-09-16 16:19:08 -0400

    finish revisions

--- services/QuillLMS/client/app/bundles/Staff/components/styleGuide/checkableDropdownOption.tsx
+++ services/QuillLMS/client/app/bundles/Staff/components/styleGuide/checkableDropdownOption.tsx
@@ -5,17 +5,18 @@ const indeterminateSrc = 'https://assets.quill.org/images/icons/indeterminate.sv
 const smallWhiteCheckSrc = 'https://assets.quill.org/images/shared/check-small-white.svg'
 
 export const CheckableDropdownOption = props => {
-  const { value } = props.selectProps
+  const { value, options } = props.selectProps
   const anyOptionsAreSelected = !!value.length
+  const allOptionsAreSelected = value.length === (options.length - 1)
   let checkbox = <span className="quill-checkbox unselected"/>
-  if (props.value === 'All' && anyOptionsAreSelected) {
-    checkbox = <span className="quill-checkbox selected">
-      <img src={indeterminateSrc} alt="check"/>
-    </span>
-  } else if (props.isSelected && props.value !== 'All') {
-    checkbox = <span className="quill-checkbox selected">
+  if (props.isSelected || allOptionsAreSelected) {
+    checkbox = (<span className="quill-checkbox selected">
       <img src={smallWhiteCheckSrc} alt="check" />
-    </span>
+    </span>)
+  } else if (props.value === 'All' && anyOptionsAreSelected) {
+    checkbox = (<span className="quill-checkbox selected">
+      <img src={indeterminateSrc} alt="check"/>
+    </span>)
   }
 
   return (
