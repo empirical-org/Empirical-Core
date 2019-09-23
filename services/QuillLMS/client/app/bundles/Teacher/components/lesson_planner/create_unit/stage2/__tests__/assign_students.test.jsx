9d7ee971c0733c02f6f553ff8881aedf86afb402
Emilia Friedberg <emiliafriedberg@gmail.com>
2019-09-23 16:41:15 -0400

    wip - tests

---
+++ services/QuillLMS/client/app/bundles/Teacher/components/lesson_planner/create_unit/stage2/__tests__/assign_students.test.jsx
@@ -0,0 +1,94 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+
+import AssignStudents from '../assign_students'
+import ClassroomCard from '../classroom_card.jsx'
+
+import { classroomProps } from './test_data/test_data'
+
+describe('Assign students component', () => {
+
+  it('should render', () => {
+    const wrapper = shallow(
+      <ClassroomCard
+        classroom={classroom}
+        students={students}
+        toggleStudentSelection={() => {}}
+        toggleClassroomSelection={() => {}}
+      />
+    )
+    expect(wrapper).toMatchSnapshot()
+  })
+
+  describe('if any students are selected', () => {
+    const selectedStudents = students.map(s => {
+      const student = s
+      student.isSelected = true
+      return student
+    })
+    const wrapper = shallow(
+      <ClassroomCard
+        classroom={classroom}
+        students={selectedStudents}
+        toggleStudentSelection={() => {}}
+        toggleClassroomSelection={() => {}}
+      />
+    )
+
+    it('should render a dropdown', () => {
+      expect(wrapper.find(DropdownInput).exists()).toBe(true)
+    })
+
+    it('should render a checked checkbox', () => {
+      expect(wrapper.find('.quill-checkbox.selected').exists()).toBe(true)
+    })
+
+  })
+
+  describe('if there are no students but emptyClassroomSelected is true', () => {
+    classroom.emptyClassroomSelected = true
+    const wrapper = shallow(
+      <ClassroomCard
+        classroom={classroom}
+        students={[]}
+        toggleStudentSelection={() => {}}
+        toggleClassroomSelection={() => {}}
+      />
+    )
+
+    it('should render a checked checkbox', () => {
+      expect(wrapper.find('.quill-checkbox.selected').exists()).toBe(true)
+    })
+
+    it('should render a placeholder', () => {
+      expect(wrapper.find('.empty-class-students').exists()).toBe(true)
+    })
+  })
+
+  describe('if there are no selected students', () => {
+    const selectedStudents = students.map(s => {
+      const student = s
+      delete student.isSelected
+      return student
+    })
+    classroom.emptyClassroomSelected = false
+    const wrapper = shallow(
+      <ClassroomCard
+        classroom={classroom}
+        students={selectedStudents}
+        toggleStudentSelection={() => {}}
+        toggleClassroomSelection={() => {}}
+      />
+    )
+
+    it('should not render a dropdown', () => {
+      expect(wrapper.find(DropdownInput).exists()).toBe(false)
+    })
+
+    it('should not render a checked checkbox', () => {
+      expect(wrapper.find('.quill-checkbox.unselected').exists()).toBe(true)
+    })
+
+  })
+
+})
