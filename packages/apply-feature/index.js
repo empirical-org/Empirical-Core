51d4583deaea0bd8690d481c58f3e52b08e3b24e
Thomas Robertson <thomas.robertson@quill.org>
2019-09-25 15:58:44 -0400

    Return the bool primitive of the comparison instead of a new bool object
    Apparently naive conditionals around Boolean objects treat them as Objects (that is, truthy), even if their value is false.

--- packages/apply-feature/index.js
+++ packages/apply-feature/index.js
@@ -2,5 +2,5 @@ import stringHash from 'string-hash';
 
 export function applyFeatureToPercentage(identifier, percentage) {
   if (typeof identifier !== 'string') return false;
-  return new Boolean(stringHash(identifier) % 100 < percentage);
+  return (stringHash(identifier) % 100 < percentage);
 }
