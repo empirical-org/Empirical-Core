78738f7c1f19191f64dcbe35fd89569276d8391a
Emilia Friedberg <emiliafriedberg@gmail.com>
2019-09-27 09:49:07 -0400

    assignment review page (#5458)
    * turn notification bar into env variable
    
    * create unit name section is done
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * get date stuff rendering the way it should
    
    * finish empty state for classes
    
    * create a class inline form works
    
    * wip
    
    * assignment works!!!
    
    * add all classes and students checkbox
    
    * hook things up so we use that page for unit templates as well
    
    * several final fixes
    
    * pull classroom card out into its own thing
    
    * wip - tests
    
    * add jest tests
    
    * test updates
    
    * a couple of revisions
    
    * update snapshots
    
    * remove dumb failing test
    
    * add back snapshot

---
+++ services/QuillLMS/client/app/bundles/Teacher/components/lesson_planner/create_unit/stage2/__tests__/assign_students.test.jsx
@@ -0,0 +1,135 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+
+import AssignStudents, {
+  createAClassForm,
+  importGoogleClassroomsModal,
+  googleClassroomEmailModal,
+  googleClassroomsEmptyModal
+} from '../assign_students'
+import ClassroomCard from '../classroom_card.jsx'
+import CreateAClassInlineForm from '../create_a_class_inline_form.tsx'
+import ImportGoogleClassroomsModal from '../../../../classrooms/import_google_classrooms_modal.tsx'
+import GoogleClassroomEmailModal from '../../../../classrooms/google_classroom_email_modal.tsx'
+import GoogleClassroomsEmptyModal from '../../../../classrooms/google_classrooms_empty_modal.tsx'
+
+
+import { classroomProps, user } from './test_data/test_data'
+
+describe('Assign students component', () => {
+
+  it('should render', () => {
+    const wrapper = shallow(
+      <AssignStudents
+        user={user}
+        classrooms={classroomProps}
+        toggleStudentSelection={() => {}}
+        toggleClassroomSelection={() => {}}
+        fetchClassrooms={() => {}}
+      />
+    )
+    expect(wrapper).toMatchSnapshot()
+  })
+
+  describe('if this.state.showFormOrModal = createAClassForm', () => {
+    const wrapper = shallow(
+      <AssignStudents
+        user={user}
+        classrooms={classroomProps}
+        toggleStudentSelection={() => {}}
+        toggleClassroomSelection={() => {}}
+        fetchClassrooms={() => {}}
+      />
+    )
+    wrapper.setState({ showFormOrModal: createAClassForm })
+
+    it('should render a createAClassInlineForm component', () => {
+      expect(wrapper.find(CreateAClassInlineForm).exists()).toBe(true)
+    })
+  })
+
+  describe('if this.state.showFormOrModal = importGoogleClassroomsModal', () => {
+    const wrapper = shallow(
+      <AssignStudents
+        user={user}
+        classrooms={classroomProps}
+        toggleStudentSelection={() => {}}
+        toggleClassroomSelection={() => {}}
+        fetchClassrooms={() => {}}
+      />
+    )
+    wrapper.setState({ showFormOrModal: importGoogleClassroomsModal })
+
+    it('should render a importGoogleClassroomsModal component', () => {
+      expect(wrapper.find(ImportGoogleClassroomsModal).exists()).toBe(true)
+    })
+  })
+
+  describe('if this.state.showFormOrModal = googleClassroomEmailModal', () => {
+    const wrapper = shallow(
+      <AssignStudents
+        user={user}
+        classrooms={classroomProps}
+        toggleStudentSelection={() => {}}
+        toggleClassroomSelection={() => {}}
+        fetchClassrooms={() => {}}
+      />
+    )
+    wrapper.setState({ showFormOrModal: googleClassroomEmailModal })
+
+    it('should render a googleClassroomEmailModal component', () => {
+      expect(wrapper.find(GoogleClassroomEmailModal).exists()).toBe(true)
+    })
+  })
+
+  describe('if this.state.showFormOrModal = googleClassroomsEmptyModal', () => {
+    const wrapper = shallow(
+      <AssignStudents
+        user={user}
+        classrooms={classroomProps}
+        toggleStudentSelection={() => {}}
+        toggleClassroomSelection={() => {}}
+        fetchClassrooms={() => {}}
+      />
+    )
+    wrapper.setState({ showFormOrModal: googleClassroomsEmptyModal })
+
+    it('should render a googleClassroomsEmptyModal component', () => {
+      expect(wrapper.find(GoogleClassroomsEmptyModal).exists()).toBe(true)
+    })
+  })
+
+  describe('if there are classrooms', () => {
+    const wrapper = shallow(
+      <AssignStudents
+        user={user}
+        classrooms={classroomProps}
+        toggleStudentSelection={() => {}}
+        toggleClassroomSelection={() => {}}
+        fetchClassrooms={() => {}}
+      />
+    )
+
+    it('should render a classroom card for each classroom', () => {
+      expect(wrapper.find(ClassroomCard).length).toBe(classroomProps.length)
+    })
+
+  })
+
+  describe('if there are no classrooms', () => {
+    const wrapper = shallow(
+      <AssignStudents
+        user={user}
+        classrooms={[]}
+        toggleStudentSelection={() => {}}
+        toggleClassroomSelection={() => {}}
+        fetchClassrooms={() => {}}
+      />
+    )
+
+    it('should render an empty state', () => {
+      expect(wrapper.find('.no-active-classes').exists()).toBe(true)
+    })
+  })
+
+})
