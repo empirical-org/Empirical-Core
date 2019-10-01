ccfd5c325db9ceafeb8f6d7818df21cf133738c3
Emilia Friedberg <emiliafriedberg@gmail.com>
2019-10-01 10:28:29 -0400

    update lessons copy (#5470)
    * make importing google classroom students synchronous (#5369)
    
    * make importing google classroom students synchronous
    
    * fix test
    
    * Add careers images to CDN
    
    * Put temp phone banner up for teachers. (#5405) (#5406)
    
    * Fix a naming bug and add a test. (#5420) (#5421)
    
    * Fix a naming bug and add a test.
    
    * remove unneeded stub.
    
    * Other test fix.
    
    * merge develop
    
    * lessons copy/minor css updates

--- services/QuillLessons/app/components/classroomLessons/play/projectorModal.tsx
+++ services/QuillLessons/app/components/classroomLessons/play/projectorModal.tsx
@@ -8,8 +8,8 @@ const ProjectorModal: React.SFC<{closeModal: any}> = (props) => {
           <img onClick={props.closeModal} className="exit" src="https://assets.quill.org/images/icons/CloseIcon.svg"/>
           <img className="illustration" src="https://assets.quill.org/images/illustrations/projector_modal.svg" />
           <h1><span>Next:</span> Project this window</h1>
-          <p>Project this screen to your students by adjusting your display settings to unmirrored mode so that you can project this window while looking at a different window on your computer.</p>
-          <p className="follow-instructions">Drag this window to the left or right of your screen to project.</p>
+          <p>Set your computer's display settings so that you can project the slides to students while keeping the lesson plan and student responses on your computer.</p>
+          <p className="follow-instructions">Once you've adjusted your display settings, drag the window you want to project all the way to the left or right until it appears on the projector.</p>
           <button onClick={props.closeModal}>Got it!</button>
         </div>
         <div className="bottom-section">
@@ -18,17 +18,18 @@ const ProjectorModal: React.SFC<{closeModal: any}> = (props) => {
           <div className="list-container">
             <div className="list windows-list">
               <p className="list-header">For Windows:</p>
-              <p className="list-item"><span>1.</span> Go to Control Panel or right-click on your desktop.</p>
-              <p className="list-item"><span>2.</span> Choose Display Settings.</p>
-              <p className="list-item"><span>3.</span> Select Extend Desktop from Multiple Display Dropdown.</p>
+              <p className="list-item"><span>1.</span> Go to Control Panel or right-click on your desktop</p>
+              <p className="list-item"><span>2.</span> Select Display settings</p>
+              <p className="list-item"><span>3.</span> Click the Multiple displays dropdown</p>
+              <p className="list-item"><span>4.</span> Select Extend these displays</p>
             </div>
 
             <div className="list mac-list">
               <p className="list-header">For Macs:</p>
-              <p className="list-item"><span>1.</span> Go to System Preferences.</p>
-              <p className="list-item"><span>2.</span> Go to Displays.</p>
-              <p className="list-item"><span>3.</span> Select the Arrangement tab.</p>
-              <p className="list-item"><span>4.</span> Uncheck Mirror Displays.</p>
+              <p className="list-item"><span>1.</span> Go to System Preferences</p>
+              <p className="list-item"><span>2.</span> Go to Displays</p>
+              <p className="list-item"><span>3.</span> Select the Arrangement tab</p>
+              <p className="list-item"><span>4.</span> Uncheck Mirror Displays</p>
             </div>
           </div>
 
