0e3bd6ceb10a1236e8befc73cfe320c4749eb118
Emilia Friedberg <emiliafriedberg@gmail.com>
2019-10-02 16:06:22 -0400

    move classrooms with students stuff out because we aren't using it here

--- services/QuillLMS/client/app/bundles/Teacher/components/classrooms_with_students/ClassroomsWithStudents.jsx
+++ services/QuillLMS/client/app/bundles/Teacher/components/classrooms_with_students/ClassroomsWithStudents.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Classroom from './classroom.jsx';
 import EditStudentsButton from './EditStudentsButton.jsx';
-import getParameterByName from '../../../modules/get_parameter_by_name.js';
+import getParameterByName from '../modules/get_parameter_by_name.js';
 
 export default class ClassroomsWithStudents extends React.Component {
 
