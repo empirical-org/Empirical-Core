3f0a82ef4e2c6b20f6d3729166ba70ab770e3eae
Emilia Friedberg <emiliafriedberg@gmail.com>
2019-10-08 14:45:09 -0400

    create new snapshots

--- services/QuillLMS/client/app/bundles/Teacher/components/assignment_flow/__tests__/leaving_modal.test.jsx
+++ services/QuillLMS/client/app/bundles/Teacher/components/assignment_flow/__tests__/leaving_modal.test.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 
-import LeavingModal from '../move_students_modal'
+import LeavingModal from '../leaving_modal'
 
 describe('LeavingModal component', () => {
   const wrapper = shallow(
