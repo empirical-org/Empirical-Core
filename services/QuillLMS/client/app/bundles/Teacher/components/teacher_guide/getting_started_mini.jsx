a47ce558425c594897cf0864dea58582682f8905
Emilia Friedberg <emiliafriedberg@gmail.com>
2019-10-09 10:24:19 -0400

    wip

--- services/QuillLMS/client/app/bundles/Teacher/components/teacher_guide/getting_started_mini.jsx
+++ services/QuillLMS/client/app/bundles/Teacher/components/teacher_guide/getting_started_mini.jsx
@@ -1,61 +1,51 @@
-'use strict'
-
 import React from 'react'
 import PercentageGraph from './percentage_graph'
 import CheckboxSection from './checkbox_sections'
 import LoadingIndicator from '../shared/loading_indicator'
 
-export default React.createClass({
-  propTypes: {
-    checkboxData: React.PropTypes.any.isRequired
-  },
+export default class GettingStartedMini extends React.Component {
+  percentageCompleted = () => {
+    const boxes = this.props.checkboxData
+    const ratio = boxes.filter(obj => obj.completed === true).length / boxes.length
+    return Math.floor(ratio * 100)
+  }
 
-  percentageCompleted: function(){
-    var boxes = this.props.checkboxData;
-    var ratio = boxes.filter(obj => obj.completed === true).length / boxes.length;
-    var percentage = Math.floor(ratio * 100);
-    return percentage;
-  },
-
-  graphSection: function(){
-    var content;
+  graphSection() {
+    let content;
     if (this.props.checkboxData.loading) {
       content = <LoadingIndicator/>;
     } else {
       content =
-        [<h3 key='h3-tag'>Getting Started</h3>,
+      [
+        <h3 key='h3-tag'>Getting Started</h3>,
         <PercentageGraph key='percentage-graph' percentage={this.percentageCompleted()}/>,
-        <a className='green-link' key='all-tasks' href='/teachers/teacher_guide'>View All Tasks ></a>]
+        <a className='green-link' key='all-tasks' href='/teachers/teacher_guide'>View All Tasks ></a>
+      ]
     }
     return <div id='graph-section'>{content}</div>
-  },
-
-  checklistSection: function() {
-    var content;
-    if (this.props.checkboxData.loading) {
-      content = <LoadingIndicator/>;
-    } else {
-       content = <CheckboxSection checkboxes={this.props.checkboxData} dashboard={true}/>;}
-   return content;
-  },
-
-
-  render: function() {
-    return (
-      <div className='mini-content'>
-      <div className='row'>
-        <div className='col-sm-4'>
-          {this.graphSection()}
-      </div>
-      <div className='col-sm-8'>
-          {this.checklistSection()}
-      </div>
-    </div>
-    </div>
-  );
-
   }
 
+  checklistSection() {
+    if (this.props.checkboxData.loading) {
+      return <LoadingIndicator />;
+    }
+    return (<CheckboxSection
+      checkboxes={this.props.checkboxData}
+      dashboard
+    />)
+  }
 
-
-});
+  render() {
+    return (
+      <div className='mini-content'>
+        <div className='row'>
+          <div className='col-sm-4'>
+            {this.graphSection()}
+          </div>
+          <div className='col-sm-8'>
+            {this.checklistSection()}
+          </div>
+        </div>
+      </div>)
+  }
+}
