3195dadba4ae76eb3e77ac4ab460ccdbe8dcf9cb
Emilia Friedberg <emiliafriedberg@gmail.com>
2019-10-17 14:54:08 -0400

    final fix

--- services/QuillLMS/client/app/bundles/Staff/components/styleGuide/tooltips.tsx
+++ services/QuillLMS/client/app/bundles/Staff/components/styleGuide/tooltips.tsx
@@ -11,7 +11,8 @@ class Tooltips extends React.Component<any, any> {
       <h2 className="style-guide-h2">Tooltips</h2>
       <div className="element-container">
         <pre>
-{`<div className="tooltips-container">
+          {
+`<div className="tooltips-container">
   <Tooltip
     tooltipText="I am a tooltip!"
     tooltipTriggerText="Hover here"
@@ -20,7 +21,8 @@ class Tooltips extends React.Component<any, any> {
     tooltipText="I am a different tooltip!"
     tooltipTriggerText="Or here"
   />
-</div>`}
+</div>`
+          }
         </pre>
         <div className="tooltips-container">
           <Tooltip
