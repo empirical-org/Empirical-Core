c7c168e646d39c12f3e0897a0c6d5217e33f5c54
Thomas Robertson <thomas.robertson@quill.org>
2019-10-16 21:40:32 -0400

    Remove testing console.log statements

--- lambdas/rematching/index.js
+++ lambdas/rematching/index.js
@@ -7,17 +7,12 @@ exports.handler = async (event) => {
   try {
     return await rematchIndividualQuestion(response, type, question, referenceResponses).
       then((rematch) => {
-        console.log("rematch successful");
-        console.log(rematch);
-        console.log(typeof rematch);
         return {
           statusCode: 200,
           body: JSON.stringify(rematch),
         };
       });
   } catch(err) {
-    console.log("rematch error");
-    console.log(JSON.stringify(err));
     return {
       statusCode: 500,
       body: JSON.stringify(err),
