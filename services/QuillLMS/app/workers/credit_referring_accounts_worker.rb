09d1e3df086261f17985b5fff0aeca99d774318b
Anathomical <thomas.robertson@quill.org>
2019-10-22 16:59:44 -0400

    Feature/quilllms/unify cron jobs (#5546)
    * Move the referral activation cron job to new cron framework
    
    * Move some more cron jobs into Cron and add tests
    
    * Migrate staff account changes job to new cron
    
    * Migrate the recurring subscription renewal job to new cron
    
    * Migrate sync Salesmachine task to new cron
    
    * Migrate the number of activities and cities task to cron
    
    * Remove recommendation assignments report job
    
    * Make changes based on PR feedback

---
+++ services/QuillLMS/app/workers/credit_referring_accounts_worker.rb
@@ -0,0 +1,23 @@
+class CreditReferringAccountsWorker
+  include Sidekiq::Worker
+
+  def perform
+    find_qualifying_uncredited_referring_users.each do |id|
+      provide_referral_credit_to_user(id)
+    end
+  end
+
+  def find_qualifying_uncredited_referring_users
+    ReferralsUser.ids_due_for_activation
+  end
+
+  def provide_referral_credit_to_user(user_id)
+    referred_user = ReferralsUser.find(user_id)
+    referred_user.update!(activated: true)
+    referring_user = referred_user.user
+    referring_user.credit_transactions << CreditTransaction.new(
+      amount: 28,
+      source: referred_user
+    )
+  end
+end
