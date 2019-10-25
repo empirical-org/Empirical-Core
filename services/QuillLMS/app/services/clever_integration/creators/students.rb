fb2acc5860578f0176a11b8e30b620088a8699e9
Emilia Friedberg <emiliafriedberg@gmail.com>
2019-10-25 13:08:44 -0400

    fix issue with overwriting existing students with clever data (#5556)
    * fix issue with overwriting existing students with clever data
    
    * add test
    
    * fix test
    
    * check for empty strings; make sure student is set to something

--- services/QuillLMS/app/services/clever_integration/creators/students.rb
+++ services/QuillLMS/app/services/clever_integration/creators/students.rb
@@ -11,12 +11,18 @@ module CleverIntegration::Creators::Students
   private
 
   def self.create_student(parsed_student_response)
-    student = User.find_by(email: parsed_student_response[:email])
+    if parsed_student_response[:email].present?
+      student = User.find_by(email: parsed_student_response[:email])
+    else
+      student = nil
+    end
+
     if student
       student.clever_id = parsed_student_response[:clever_id]
     else
       student = User.find_or_initialize_by(clever_id: parsed_student_response[:clever_id])
     end
+
     student.update(parsed_student_response.merge({
       role: 'student',
       account_type: 'Clever'
