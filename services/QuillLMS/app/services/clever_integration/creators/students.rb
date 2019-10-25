fcff69cb02f4c149bc21c61e4b121d5a73d56421
Emilia Friedberg <emiliafriedberg@gmail.com>
2019-10-25 12:09:03 -0400

    fix issue with overwriting existing students with clever data

--- services/QuillLMS/app/services/clever_integration/creators/students.rb
+++ services/QuillLMS/app/services/clever_integration/creators/students.rb
@@ -11,12 +11,16 @@ module CleverIntegration::Creators::Students
   private
 
   def self.create_student(parsed_student_response)
-    student = User.find_by(email: parsed_student_response[:email])
+    if parsed_student_response[:email]
+      student = User.find_by(email: parsed_student_response[:email])
+    end
+
     if student
       student.clever_id = parsed_student_response[:clever_id]
     else
       student = User.find_or_initialize_by(clever_id: parsed_student_response[:clever_id])
     end
+    
     student.update(parsed_student_response.merge({
       role: 'student',
       account_type: 'Clever'
