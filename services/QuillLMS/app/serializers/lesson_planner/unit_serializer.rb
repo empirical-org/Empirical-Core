d62fb4eadfa79be6417b36ac23295a9331eb01dd
Haronil Estevez <haronil@quill.org>
2019-10-28 17:02:09 -0400

    use correct order of arguments for each_with_object

--- services/QuillLMS/app/serializers/lesson_planner/unit_serializer.rb
+++ services/QuillLMS/app/serializers/lesson_planner/unit_serializer.rb
@@ -73,7 +73,7 @@ class LessonPlanner::UnitSerializer < ActiveModel::Serializer
   end
 
   def dueDates
-    object.reload.unit_activities.uniq(&:activity).each_with_object({}) do |acc, unit_activity|
+    object.reload.unit_activities.uniq(&:activity).each_with_object({}) do |unit_activity, acc|
       acc[unit_activity.activity.id] = unit_activity.formatted_due_date
       acc
     end
