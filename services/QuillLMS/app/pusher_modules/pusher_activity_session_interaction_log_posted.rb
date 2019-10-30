e0dca3d0b4d8e52d952d659135bc37a5526d9f5d
Emilia Friedberg <emiliafriedberg@gmail.com>
2019-10-30 14:13:37 -0400

    add test

--- services/QuillLMS/app/pusher_modules/pusher_activity_session_interaction_log_posted.rb
+++ services/QuillLMS/app/pusher_modules/pusher_activity_session_interaction_log_posted.rb
@@ -1,14 +1,14 @@
 module PusherActivitySessionInteractionLogPosted
 
-  def self.run(teachers)
+  def self.run(teacher_ids)
     pusher_client = Pusher::Client.new(
         app_id: ENV["PUSHER_APP_ID"],
         key: ENV["PUSHER_KEY"],
         secret: ENV["PUSHER_SECRET"],
         encrypted: true
     )
-    teachers.each do |tid|
-      pusher_client.trigger(tid.to_s, 'as-interaction-log-pushed', message: "Progress is happening")
+    teacher_ids.each do |tid|
+      pusher_client.trigger(tid.to_s, 'as-interaction-log-pushed', message: "Progress is happening.")
     end
   end
 
