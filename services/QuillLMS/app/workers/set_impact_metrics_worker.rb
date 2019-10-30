ae7985ae6977308d641c3cdce42ed7b898409f5a
Emilia Friedberg <emiliafriedberg@gmail.com>
2019-10-30 11:09:50 -0400

    refactor activity sessions query to just be rails

--- services/QuillLMS/app/workers/set_impact_metrics_worker.rb
+++ services/QuillLMS/app/workers/set_impact_metrics_worker.rb
@@ -6,27 +6,21 @@ class SetImpactMetricsWorker
   end
 
   def set_impact_metrics
-    activity_sessions_query = ActiveRecord::Base.connection.execute("
-      SELECT
-        COUNT(DISTINCT user_id) AS number_of_students,
-        COUNT(DISTINCT activity_sessions.id) AS number_of_activities
-      FROM activity_sessions
-      WHERE activity_sessions.state = 'finished'
-    ")
+    finished_activity_sessions_query = ActivitySession.where(state: 'finished')
     teachers_query = User.select(:id).joins(:units).joins(:classroom_units).joins("JOIN activity_sessions ON classroom_units.id = activity_sessions.classroom_unit_id").where("activity_sessions.state = 'finished'").group('users.id').having('count(activity_sessions) > 9')
     teacher_ids = teachers_query.to_a.map(&:id)
     schools_query = School.joins(:schools_users).where("schools_users.user_id IN (?)", teacher_ids).distinct
     low_income_schools_query = schools_query.where("free_lunches > 39")
 
-    number_of_sentences = activity_sessions_query.to_a[0]['number_of_activities'].to_i.floor(-5) * 10
+    number_of_sentences = finished_activity_sessions_query.size.floor(-5) * 10
     $redis.set(PagesController::NUMBER_OF_SENTENCES, number_of_sentences)
-    number_of_students = activity_sessions_query.to_a[0]['number_of_students'].to_i.floor(-5)
+    number_of_students = finished_activity_sessions_query.pluck(:user_id).uniq.size.floor(-5)
     $redis.set(PagesController::NUMBER_OF_STUDENTS, number_of_students)
-    number_of_teachers = teachers_query.to_a.length.floor(-2)
+    number_of_teachers = teachers_query.to_a.size.floor(-2)
     $redis.set(PagesController::NUMBER_OF_TEACHERS, number_of_teachers)
-    number_of_schools = schools_query.to_a.length
+    number_of_schools = schools_query.to_a.size
     $redis.set(PagesController::NUMBER_OF_SCHOOLS, number_of_schools)
-    number_of_low_income_schools = low_income_schools_query.to_a.length
+    number_of_low_income_schools = low_income_schools_query.to_a.size
     $redis.set(PagesController::NUMBER_OF_LOW_INCOME_SCHOOLS, number_of_low_income_schools)
     $redis.set(PagesController::IMPACT_METRICS_LAST_SET, Time.now)
   end
