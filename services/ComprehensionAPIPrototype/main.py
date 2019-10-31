c38793fb8926a6206ef72ff4b53bac16d6279155
Daniel Drabik <dan@quill.org>
2019-10-30 22:49:55 -0400

    Move all single label settings to YAML.

--- services/ComprehensionAPIPrototype/main.py
+++ services/ComprehensionAPIPrototype/main.py
@@ -3,19 +3,6 @@ from flask import jsonify
 from flask import make_response
 import yaml
 
-LABELS = {
-  "greenhouse_general" : 'Greenhouse gases are a concern. Include specific stats to make a stronger argument.',
-  "greenhouse_specifc" : 'You are correct! Well done!',
-  "harms_animals" : "It does harm animals",
-  "outside_scope" : "Your answer is outside the scope of this article. Use evidence from the passage to make your argument",
-  "because_as_prep" : "Maybe because isn't the right preposition to use here.",
-  "harms_env_no_mention" : "It does harm the environment, but include why that is bad",
-  "plagiarism" : "It seems you've copied directly from the passage, try to write in your own words",
-  "irrelevant" : "It seems that your answer is irrelevant",
-  "insufficient" : "Your answer is short and insufficent, please try again"
-}
-
-
 def response_endpoint(request):
     request_json = request.get_json()
 
@@ -25,29 +12,35 @@ def response_endpoint(request):
     if entry == None or prompt_id == None:
         return make_response(jsonify(message="error"), 400)
 
-    with open("models.yml", 'r') as ymlfile:
-      configs = yaml.load(ymlfile)['models']
+    model_settings = model_settings_for(prompt_id)
 
-    if prompt_id in configs:
-      model_settings = configs[prompt_id]
-    else:
+    if model_settings == None:
       return make_response(jsonify(message="error: model not found"), 400)
 
     automl_response = automl_prediction(entry, model_settings['automl'])
-    prediction_label = label(automl_response.payload)
-    feedback = LABELS[prediction_label]
+    prediction_labels = labels_for(automl_response.payload, model_settings['label_type'])
+    feedback = feedback_for(prediction_labels, model_settings['feedback'], model_settings['label_type'])
 
-    log(request=request_json, label=prediction_label, feedback=feedback)
+    log(request=request_json, labels=prediction_labels, feedback=feedback)
 
     return make_response(jsonify(message=feedback,correct=True), 200)
 
 def param_for(key, request, request_json):
-  if request.args and key in request.args:
-      return request.args.get(key)
-  elif request_json and key in request_json:
-      return request_json[key]
-  else:
-      return None
+    if request.args and key in request.args:
+        return request.args.get(key)
+    elif request_json and key in request_json:
+        return request_json[key]
+    else:
+        return None
+
+def model_settings_for(prompt_id):
+    with open("models.yml", 'r') as ymlfile:
+      configs = yaml.load(ymlfile)['models']
+
+    if prompt_id in configs:
+        return configs[prompt_id]
+    else:
+        return None
 
 def automl_prediction(entry, settings):
     prediction_client = automl.PredictionServiceClient()
@@ -57,8 +50,17 @@ def automl_prediction(entry, settings):
 
     return prediction_client.predict(model_url, payload, {})
 
-def label(payload):
+def feedback_for(labels, feedback_settings, type):
+    return feedback_settings[labels] if type == 'single' else 'TODO'
+
+def labels_for(payload, type):
+    return single_label(payload) if type == 'single' else multi_labels(payload)
+
+def single_label(payload):
     return sorted(payload, key=scoreSort, reverse=True)[0].display_name
+
+def multi_labels(payload):
+    return 'TODO'
 
 def scoreSort(e):
     return e.classification.score
