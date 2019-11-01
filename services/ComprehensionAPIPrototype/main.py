309bc85f8c316408db64d9ea9dbbfa838ce49813
Daniel Drabik <dan@quill.org>
2019-10-31 21:53:12 -0400

    Revert for now.

--- services/ComprehensionAPIPrototype/main.py
+++ services/ComprehensionAPIPrototype/main.py
@@ -39,7 +39,7 @@ def model_settings_for(prompt_id):
     with open("models.yml", 'r') as ymlfile:
       configs = yaml.load(ymlfile)['models']
 
-    if int(prompt_id) in configs:
+    if prompt_id in configs:
         return configs[prompt_id]
     else:
         return None
