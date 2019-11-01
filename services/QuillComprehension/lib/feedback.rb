3e02f42c7db8a05d67dfa1c8ff6b74fbbb84b1f9
dandrabik <dandrabik@users.noreply.github.com>
2019-11-01 11:30:05 -0400

    Comprehension Prototype for Multilabel  (#5568)
    * Add an initial single_label test.
    
    * Factor out param extraction.
    
    * Extract prompt id.
    
    * Start using yaml configs.
    
    * Move all single label settings to YAML.
    
    * Add multi-label code and settings for voting-so
    
    * Pass along prompt id.
    
    * Add default_feedback for each model
    
    * Return whether an answer is correct.
    
    * Return label in the json response for debugging.
    
    * Add some more logging.
    
    * Show request.
    
    * cast prompt_id to int.
    
    * Revert for now.
    
    * cast prompt_id in both places.
    
    * Update front end to show label and correct.
    
    * Update requirements.txt.
    
    * Better dictionary lookup syntax.
    
    * Use yams safe loader
    
    * Use shorter Dict.get method.
    
    * Clean up tests a bit.
    
    * Refactor get param for edgewise.

--- services/QuillComprehension/lib/feedback.rb
+++ services/QuillComprehension/lib/feedback.rb
@@ -2,14 +2,14 @@ class Feedback
   include HTTParty
   base_uri 'us-east1-comprehension-247816.cloudfunctions.net/response-api-alpha'
   format :json
-  attr_reader :prompt_key, :entry
+  attr_reader :prompt_id, :entry
 
-  def initialize(prompt_key:, entry:)
-    @prompt_key = prompt_key
+  def initialize(prompt_id:, entry:)
+    @prompt_id = prompt_id
     @entry = entry
   end
 
   def response
-    self.class.post('', query: {entry: entry})
+    self.class.post('', query: {entry: entry, prompt_id: prompt_id})
   end
 end
