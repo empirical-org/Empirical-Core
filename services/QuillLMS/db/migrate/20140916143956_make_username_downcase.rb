d7f5792280bd5d13307a268613aec5b5ffe2eac4
Haronil Estevez <haronil@quill.org>
2019-11-11 15:42:37 -0500

    ignore migration scripts for RescueModifier rule

--- services/QuillLMS/db/migrate/20140916143956_make_username_downcase.rb
+++ services/QuillLMS/db/migrate/20140916143956_make_username_downcase.rb
@@ -2,16 +2,8 @@ class MakeUsernameDowncase < ActiveRecord::Migration
   def change
 
     User.all.each { |u| 
-      em = begin
-             u.try(:email).downcase
-           rescue
-             nil
-           end
-      un = begin
-             u.try(:username).downcase
-           rescue
-             nil
-           end
+      em = u.try(:email).downcase rescue nil
+      un = u.try(:username).downcase rescue nil
 
       u.update_columns(email: em, username: un) }
 
