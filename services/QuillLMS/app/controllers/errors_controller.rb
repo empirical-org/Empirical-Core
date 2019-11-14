f7a4d980f169cd0bb7c5a98fae2adc00be237c96
Anathomical <thomas.robertson@quill.org>
2019-11-14 14:50:09 -0500

    Add the Style/RedundantReturn rule to Rubocop (#5618)
    Then auto-correct the code and manually fix places where the corrections aren't right

--- services/QuillLMS/app/controllers/errors_controller.rb
+++ services/QuillLMS/app/controllers/errors_controller.rb
@@ -1,12 +1,10 @@
 class ErrorsController < ApplicationController
   def error_404
     render 'error_404', :status => 404
-    return
   end
 
   def error_500
     ErrorWorker.perform_async
     render 'error_500', :status => 500
-    return
   end
 end
