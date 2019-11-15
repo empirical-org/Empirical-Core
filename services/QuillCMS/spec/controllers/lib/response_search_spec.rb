9d12f3ebbb6ff19708fe76b6e9fd3cf77e204d70
cissyxyu <cissy@quill.org>
2019-11-15 15:22:17 -0500

    updated w/ changes as suggested by thomas

--- services/QuillCMS/spec/controllers/lib/response_search_spec.rb
+++ services/QuillCMS/spec/controllers/lib/response_search_spec.rb
@@ -6,34 +6,51 @@ require "modules/response_search"
 RSpec.describe ResponseSearch do
   let(:test_query_filters) { { 'text' => '', 'filters' => "", 'excludeMisspellings' => 'true'} }
 
-  it 'build_regex_query_string builds a correct regex search query' do
-    test_1 = '/normal testt? regex/'
-    expect(ResponseSearch.build_regex_query_string(test_1)).to eq('/.*normal testt? regex.*/')
+  describe '#build_regex_query_string' do
+    it 'should add wildcards to start and end of regex if there is no start or end symbol in the original' do
+      test_1 = '/normal testt? regex/'
+      expect(ResponseSearch.build_regex_query_string(test_1)).to eq('/.*normal testt? regex.*/')  
+    end
+
+    it 'should remove ^ symbol but NOT add wildcard at beginning of regex if original regex starts with ^' do
+      test_2 = '/^starts with regex/'
+      expect(ResponseSearch.build_regex_query_string(test_2)).to eq('/starts with regex.*/')
+    end
     
-    test_2 = '/^starts with regex/'
-    expect(ResponseSearch.build_regex_query_string(test_2)).to eq('/starts with regex.*/')
-    
-    test_3 = '/ends with regex$/'
-    expect(ResponseSearch.build_regex_query_string(test_3)).to eq('/.*ends with regex/')
-  
-    test_4 = '/^starts and ends with regex$/'
-    expect(ResponseSearch.build_regex_query_string(test_4)).to eq('/starts and ends with regex/')
+    it 'should remove $ symbol but NOT add wildcard at end of regex if original regex ends with $' do
+      test_3 = '/ends with regex$/'
+      expect(ResponseSearch.build_regex_query_string(test_3)).to eq('/.*ends with regex/')
+    end
+
+    it 'should remove both ^ and $ symbols and add NO wildcards if original regex starts and ends with ^/$' do
+      test_4 = '/^starts and ends with regex$/'
+      expect(ResponseSearch.build_regex_query_string(test_4)).to eq('/starts and ends with regex/')
+    end
+  end  
+
+  describe '#get_query_values' do
+    it 'should search in field \'text.keyword\' if original query contains regex' do
+      test_query_filters['text'] = '/test_regex/'
+      expect(ResponseSearch.get_query_values('000', test_query_filters)[:query_string][:default_field]).to eq('text.keyword')
+    end
+
+    it 'should search in field \'text\' if original query does not contain regex' do
+      test_query_filters['text'] = 'test_no_regex'
+      expect(ResponseSearch.get_query_values('000', test_query_filters)[:query_string][:default_field]).to eq('text')
+    end  
   end
 
-  it 'get_query_values searches \'text\' without regex and \'text.keyword\' with regex' do
-    test_query_filters['text'] = '/test_regex/'
-    expect(ResponseSearch.get_query_values('000', test_query_filters)[:query_string][:default_field]).to eq('text.keyword')
-    
-    test_query_filters['text'] = 'test_no_regex'
-    expect(ResponseSearch.get_query_values('000', test_query_filters)[:query_string][:default_field]).to eq('text')
-  end
+  describe '#build_query_string' do
+    it 'should build query string with appropriate regex if original query contains regex' do
+      test_query_filters['text'] = '/test_regex/'
+      expected_result = "/.*test_regex.*/ AND question_uid:(\"000\")"
+      expect(ResponseSearch.build_query_string('000', test_query_filters, true)).to eq(expected_result)
+    end
 
-  it 'build_query_string builds an appropriate query string' do
-    test_query_filters['text'] = '/test_regex/'
-    expected_result = "/.*test_regex.*/ AND question_uid:(\"000\")"
-    expect(ResponseSearch.build_query_string('000', test_query_filters, true)).to eq(expected_result)
-    
-    test_query_filters['text'] = 'test_no_regex'
-    expected_result = "\"test_no_regex\" AND question_uid:(\"000\")"
+    it 'should build query string with appropriate string query if original query contains no regex' do
+      test_query_filters['text'] = 'test_no_regex'
+      expected_result = "\"test_no_regex\" AND question_uid:(\"000\")"
+      expect(ResponseSearch.build_query_string('000', test_query_filters, false)).to eq(expected_result)
+    end
   end
 end
