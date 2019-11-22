8e95cf9e91a5402bc37d6c2ec579c3775f134631
Emilia Friedberg <emiliafriedberg@gmail.com>
2019-11-22 09:53:54 -0500

    make sure generated usernames are always downcased

---
+++ services/QuillLMS/spec/services/generate_username_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+describe GenerateUsername do
+  it 'generates a downcased username made up of the first name, last name, and class code of the student' do
+    expect(GenerateUsername.new('John', 'Smith', 'student').call).to eq 'john.smith@student'
+  end
+
+  it 'inserts a number after the last name if there is already a student in the database with that username' do
+    create(:student, { username: 'john.smith@student' })
+    expect(GenerateUsername.new('John', 'Smith', 'student').call).to eq ('john.smith2@student')
+  end
+end
