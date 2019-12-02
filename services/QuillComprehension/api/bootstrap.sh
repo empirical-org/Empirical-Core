196b0037518c54fb3768f1ba0a35151fc7a6572a
Thomas Robertson <thomas.robertson@quill.org>
2019-12-02 13:45:37 -0500

    Tweak some bootstrap.sh lines

--- services/QuillComprehension/api/bootstrap.sh
+++ services/QuillComprehension/api/bootstrap.sh
@@ -6,10 +6,8 @@ source env/bin/activate
 pip install --upgrade pip
 wget https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-272.0.0-darwin-x86_64.tar.gz
 tar xf google-cloud-sdk-272.0.0-darwin-x86_64.tar.gz --directory ~/
-./google-cloud-sdk/install.sh
+~/google-cloud-sdk/install.sh
 rm google-cloud-sdk-272.0.0-darwin-x86_64.tar.gz
-source ~/.bash_profile
-source env/bin/activate
 ~/google-cloud-sdk/bin/gcloud init
 brew install openssl
-env LDFLAGS="-I/usr/local/opt/openssl/include -L/usr/local/opt/openssl/lib" pip install psycopg2 pip install -r requirements.txt
+env LDFLAGS="-I/usr/local/opt/openssl/include -L/usr/local/opt/openssl/lib" pip install -r requirements.txt.dev
