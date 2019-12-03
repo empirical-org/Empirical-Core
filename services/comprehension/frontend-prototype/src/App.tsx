d1a67c5d8c725487809eaa7236d51cc87efc584a
Emilia Friedberg <emiliafriedberg@gmail.com>
2019-12-03 13:05:36 -0500

    remove other antd stuff

--- services/comprehension/frontend-prototype/src/App.tsx
+++ services/comprehension/frontend-prototype/src/App.tsx
@@ -1,8 +1,6 @@
 import * as React from "react";
 import {HashRouter} from "react-router-dom";
 import {Provider} from "react-redux";
-import { LocaleProvider } from "antd";
-import enUS from "antd/lib/locale-provider/en_US";
 import {route} from "./routes";
 import { configureStore, initStore } from "./store/configStore";
 
@@ -10,15 +8,13 @@ const store = configureStore();
 store.dispatch(initStore());
 
 class App extends React.Component<{}, {}> {
-    public render(): JSX.Element {
-        return (
-          <LocaleProvider locale={enUS}>
-            <Provider store={store}>
-              <HashRouter basename="/">{route}</HashRouter>
-            </Provider>
-          </LocaleProvider>
-        );
-    }
+  public render(): JSX.Element {
+    return (
+      <Provider store={store}>
+        <HashRouter basename="/">{route}</HashRouter>
+      </Provider>
+    );
+  }
 }
 
 export default App;
