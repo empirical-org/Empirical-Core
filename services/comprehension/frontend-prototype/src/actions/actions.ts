253d8509013f476e8afba5695ef40e1bb188e88a
Emilia Friedberg <emiliafriedberg@gmail.com>
2019-12-04 14:26:51 -0500

    rename

--- services/comprehension/frontend-prototype/src/actions/actions.ts
+++
@@ -1,5 +0,0 @@
-import { ActionTypes, IInitStoreAction } from './actionTypes'
-
-export const initStoreAction = (): IInitStoreAction => {
-  return {type: ActionTypes.INIT_STORE}
-}
--- services/comprehension/frontend-prototype/src/helpers/getParameterByName.ts
+++
@@ -1,9 +0,0 @@
-export default function getParameterByName(name: string, url: string) {
-  if (!url) { url = window.location.href; }
-  name = name.replace(/[\[\]]/g, '\\$&');
-  const regex = new RegExp(`[?&]${name}(=([^&#]*)|&|#|$)`),
-    results = regex.exec(url);
-  if (!results) { return null; }
-  if (!results[2]) { return ''; }
-  return decodeURIComponent(results[2].replace(/\+/g, ' '));
-}
