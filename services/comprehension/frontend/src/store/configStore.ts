b11ca04d9d3862bc1a605760b22782f72fe23aa6
Emilia Friedberg <emiliafriedberg@gmail.com>
2019-12-04 14:23:41 -0500

    rename

--- services/comprehension/frontend/src/store/configStore.ts
+++ services/comprehension/frontend/src/store/configStore.ts
@@ -1,7 +1,6 @@
 import { createStore, applyMiddleware, Dispatch, Action } from "redux";
 import {composeWithDevTools} from "redux-devtools-extension/developmentOnly";
 import thunk from "redux-thunk";
-// import { TodoItem } from "../model/TodoItem";
 import { initStoreAction } from "../actions/actions";
 import { rootReducer } from "../reducers/rootReducer";
 
