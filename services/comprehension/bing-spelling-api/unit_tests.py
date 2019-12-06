1ba170aa977bcfc55f981acc65af88f475c34223
Cissy Yu <cissy@quill.org>
2019-12-06 16:07:51 -0500

    changes from thomas's suggestions

--- services/comprehension/bing-spelling-api/unit_tests.py
+++ services/comprehension/bing-spelling-api/unit_tests.py
@@ -8,73 +8,89 @@ import time
 
 
 # Create a fake "app" for generating test request contexts.
-@pytest.fixture(scope="module")
+@pytest.fixture(scope='class')
 def app():
     return flask.Flask(__name__)
 
-# Set API calls 1 second apart because the free version of Azure
-# blocks quick calls in succession
-def teardown_function(function):   
-    time.sleep(1)
+class TestParameterChecks:
 
-def test_missing_prompt_id(app):
-    with app.test_request_context(json={'entry': 'This is spelled correctly.', 'prompt_id': None}):
-      response = main.response_endpoint(flask.request)
-      data = json.loads(response.data)
+      def test_missing_prompt_id(self, app):
+          with app.test_request_context(json={'entry': 'This is spelled correctly.', 'prompt_id': None}):
+              response = main.response_endpoint(flask.request)
+              data = json.loads(response.data)
 
-      assert response.status_code == 400
+              assert response.status_code == 400
 
-def test_missing_entry(app):
-    with app.test_request_context(json={'entry': None, 'prompt_id': 000}):
-      response = main.response_endpoint(flask.request)
-      data = json.loads(response.data)
+      def test_missing_entry(self, app):
+          with app.test_request_context(json={'entry': None, 'prompt_id': 000}):
+              response = main.response_endpoint(flask.request)
+              data = json.loads(response.data)
 
-      assert response.status_code == 400
+              assert response.status_code == 400
 
-def test_spelled_correctly_branch(app):
-    with app.test_request_context(json={'entry': 'This is spelled correctly.', 'prompt_id': 000}):
-      response = main.response_endpoint(flask.request)
-      data = json.loads(response.data)
+class TestRequestProcessing:
+    # Set API calls 1 second apart because the free version of Azure
+    # blocks quick calls in succession
+    def teardown_method(self, method):   
+        time.sleep(1)    
 
-      assert response.status_code == 200
-      assert data.get('feedback') == 'Correct spelling!'
-      assert data.get('feedback_type') == 'spelling'
-      assert data.get('optimal') == True
-      assert len(data.get('highlight')) == 0
+    def test_spelled_correctly_branch(self, app):
+        with app.test_request_context(json={'entry': 'This is spelled correctly.', 'prompt_id': 000}):
+            response = main.response_endpoint(flask.request)
+            data = json.loads(response.data)
 
-def test_spelled_incorrectly_branch(app):
-    with app.test_request_context(json={'entry': 'This is spelllled incorrectly.', 'prompt_id': 000}):
-      response = main.response_endpoint(flask.request)
-      data = json.loads(response.data)
+            assert response.status_code == 200
+            assert data.get('feedback') == 'Correct spelling!'
+            assert data.get('feedback_type') == 'spelling'
+            assert data.get('optimal') == True
+            assert len(data.get('highlight')) == 0  
 
-      assert response.status_code == 200
-      assert data.get('feedback') == 'Try again. There may be a spelling mistake.'
-      assert data.get('feedback_type') == 'spelling'
-      assert data.get('optimal') == False
-      assert data.get('highlight')[0].get('text') == 'spelllled'
+    def test_spelled_incorrectly_branch(self, app):
+        with app.test_request_context(json={'entry': 'This is spelllled incorrectly.', 'prompt_id': 000}):
+            response = main.response_endpoint(flask.request)
+            data = json.loads(response.data)
 
-def test_correct_spelling():
-    response = main.get_bing_api_response('This is spelled correctly.')
-    misspelled = response.get('flaggedTokens')
-    assert len(misspelled) == 0
+            assert response.status_code == 200
+            assert data.get('feedback') == 'Try again. There may be a spelling mistake.'
+            assert data.get('feedback_type') == 'spelling'
+            assert data.get('optimal') == False
+            assert data.get('highlight')[0].get('text') == 'spelllled'
 
-def test_incorrect_spelling_single_error_middle_of_sentence():
-    response = main.get_bing_api_response('This is spellllled incorrectly.')
-    misspelled = response.get('flaggedTokens')
-    assert len(misspelled) == 1
-    assert 'spellllled' in misspelled[0].get('token')
+class TestBingSpellingApi(object):
+    # Set API calls 1 second apart because the free version of Azure
+    # blocks quick calls in succession
+    def teardown_method(self, method):   
+        time.sleep(1) 
+      
+    def test_correct_spelling(self):
+        response = main.get_bing_api_response('This is spelled correctly.')
+        misspelled = response.get('flaggedTokens')
+        assert len(misspelled) == 0
 
-def test_incorrect_spelling_single_error_end_of_sentence():
-    response = main.get_bing_api_response('This is spelled incorrectlee.')
-    misspelled = response.get('flaggedTokens')
-    assert len(misspelled) == 1
-    assert 'incorrectlee' in misspelled[0].get('token')
+    def test_incorrect_spelling_single_error_middle_of_sentence(self):
+        response = main.get_bing_api_response('This is spellllled incorrectly.')
+        misspelled = response.get('flaggedTokens')
+        assert len(misspelled) == 1
+        assert 'spellllled' in misspelled[0].get('token')
 
-def test_incorrect_spelling_multiple_errors():
-    response = main.get_bing_api_response('Thissss is spellllled incorrectlee.')
-    misspelled = response.get('flaggedTokens')
-    assert len(misspelled) == 3
-    assert 'Thissss' in misspelled[0].get('token')
-    assert 'spellllled' in misspelled[1].get('token')
-    assert 'incorrectlee' in misspelled[2].get('token')
+    def test_incorrect_spelling_single_error_end_of_sentence(self):
+        response = main.get_bing_api_response('This is spelled incorrectlee.')
+        misspelled = response.get('flaggedTokens')
+        assert len(misspelled) == 1
+        assert 'incorrectlee' in misspelled[0].get('token')
+
+    def test_incorrect_spelling_multiple_errors(self):
+        response = main.get_bing_api_response('Thissss is spellllled incorrectlee.')
+        misspelled = response.get('flaggedTokens')
+        assert len(misspelled) == 3
+        assert 'Thissss' in misspelled[0].get('token')
+        assert 'spellllled' in misspelled[1].get('token')
+        assert 'incorrectlee' in misspelled[2].get('token')
+
+
+
+
+
+
+
     
