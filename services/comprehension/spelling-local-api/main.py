d9ecfe44c0882da7a3cb39b4831744dc1b26c36b
Cissy Yu <cissy@quill.org>
2019-12-10 09:11:03 -0500

    changed spell check library to symspellpy

--- services/comprehension/spelling-local-api/main.py
+++ services/comprehension/spelling-local-api/main.py
@@ -3,37 +3,46 @@ from symspellpy import SymSpell, Verbosity
 from flask import jsonify
 from flask import make_response
 import string
-import re
+
 
 def response_endpoint(request):
     request_json = request.get_json()
 
-    entry = param_for('entry', request, request_json)
-    prompt_id = param_for('prompt_id', request, request_json)
+    entry = request_json.get('entry')
+    prompt_id = request_json.get('prompt_id')
 
     if entry == None or prompt_id == None:
         return make_response(jsonify(message="error"), 400)
 
-    misspelled_flagged = get_misspelled_words_no_casing(entry)
+    try:
+        misspelled_flagged = get_misspelled_words(entry)
+    except AssertionError as error:
+        return make_response(jsonify(message=error), 500)
 
-    if not misspelled_flagged:
-        return make_response(jsonify(feedback="Correct spelling!", feedback_type="spelling", optimal=True, response_uid="q23123@3sdfASDF", highlight=[]), 200)
-    else:
-        return make_response(jsonify(feedback="Try again. There may be a spelling mistake.", feedback_type="spelling", optimal=False, response_uid="q23123@3sdfASDF", highlight=get_misspelled_highlight_list_with_casing(misspelled_flagged, entry)), 200)
+    response_data = {
+        'feedback_type': 'spelling',
+        'response_uid': 'q23123@3sdfASDF',
+        'feedback': 'Correct spelling!',
+        'optimal': True,
+        'highlight': [],
+    }
 
-def get_misspelled_words_no_casing(entry):
+    if misspelled_flagged:
+        response_data.update({
+            'feedback': 'Try again. There may be a spelling mistake.',
+            'optimal': False,
+            'highlight': get_misspelled_highlight_list(misspelled_flagged),
+        })
+
+    return make_response(jsonify(**response_data), 200)
+
+def get_misspelled_words(entry):
     sym_spell = SymSpell(max_dictionary_edit_distance=2, prefix_length=7)
     dictionary_path = pkg_resources.resource_filename("symspellpy", "frequency_dictionary_en_82_765.txt")
     bigram_path = pkg_resources.resource_filename("symspellpy", "frequency_bigramdictionary_en_243_342.txt")
 
-    if not sym_spell.load_dictionary(dictionary_path, term_index=0,
-                                     count_index=1):
-        print("Dictionary file not found")
-        return
-    if not sym_spell.load_bigram_dictionary(bigram_path, term_index=0,
-                                            count_index=2):
-        print("Bigram dictionary file not found")
-        return
+    assert (sym_spell.load_dictionary(dictionary_path, term_index=0, count_index=1)), "Could not load dictionary resource."
+    assert (sym_spell.load_bigram_dictionary(bigram_path, term_index=0, count_index=2)), "Could not load bigram resource."
 
     entry = entry.strip(string.punctuation)
     corrected_entry = sym_spell.lookup_compound(entry, max_edit_distance=2, transfer_casing=True)[0].term
@@ -41,16 +50,9 @@ def get_misspelled_words_no_casing(entry):
 
     return wrong_words
 
-def get_misspelled_highlight_list_with_casing(flagged, entry):
-    highlight = []
-    # misspelled_original = re.findall(r"(?=("+'|'.join(flagged)+r"))", entry, re.IGNORECASE)
-    for word in flagged:
-        wordObj = {"type": "response","id": None,"text": word}
-        highlight.append(wordObj)
-    return highlight
-
-def param_for(key, request, request_json):
-    if request.args and key in request.args:
-        return request.args.get(key)
-    else:
-        return (request_json or {}).get(key)
+def get_misspelled_highlight_list(misspelled_flagged):
+    return list(map(lambda entry: {
+        'type': 'response',
+        'id': None,
+        'text': entry,
+    }, misspelled_flagged))
--- services/comprehension/spelling-local-api/unit_tests.py
+++ services/comprehension/spelling-local-api/unit_tests.py
@@ -11,74 +11,70 @@ from flask import json
 def app():
     return flask.Flask(__name__)
 
-def test_missing_prompt_id(app):
-    with app.test_request_context(json={'entry': 'This is spelled correctly.', 'prompt_id': None}):
-      response = main.response_endpoint(flask.request)
-      data = json.loads(response.data)
+class TestParameterChecks:
 
-      assert response.status_code == 400
+    def test_missing_prompt_id(app):
+        with app.test_request_context(json={'entry': 'This is spelled correctly.', 'prompt_id': None}):
+          response = main.response_endpoint(flask.request)
+          data = json.loads(response.data)
 
-def test_missing_entry(app):
-    with app.test_request_context(json={'entry': None, 'prompt_id': 000}):
-      response = main.response_endpoint(flask.request)
-      data = json.loads(response.data)
+          assert response.status_code == 400
 
-      assert response.status_code == 400
+    def test_missing_entry(app):
+        with app.test_request_context(json={'entry': None, 'prompt_id': 000}):
+          response = main.response_endpoint(flask.request)
+          data = json.loads(response.data)
 
-def test_spelled_correctly_branch(app):
-    with app.test_request_context(json={'entry': 'This is spelled correctly.', 'prompt_id': 000}):
-      response = main.response_endpoint(flask.request)
-      data = json.loads(response.data)
+          assert response.status_code == 400
 
-      assert response.status_code == 200
-      assert data.get('feedback') == 'Correct spelling!'
-      assert data.get('feedback_type') == 'spelling'
-      assert data.get('optimal') == True
-      assert len(data.get('highlight')) == 0
+class TestBranches:
 
-def test_spelled_incorrectly_branch(app):
-    with app.test_request_context(json={'entry': 'This is spelllled incorrectly.', 'prompt_id': 000}):
-      response = main.response_endpoint(flask.request)
-      data = json.loads(response.data)
+    def test_spelled_correctly_branch(app):
+        with app.test_request_context(json={'entry': 'This is spelled correctly.', 'prompt_id': 000}):
+          response = main.response_endpoint(flask.request)
+          data = json.loads(response.data)
 
-      assert response.status_code == 200
-      assert data.get('feedback') == 'Try again. There may be a spelling mistake.'
-      assert data.get('feedback_type') == 'spelling'
-      assert data.get('optimal') == False
-      assert data.get('highlight')[0].get('text') == 'spelllled'
+          assert response.status_code == 200
+          assert data.get('feedback') == 'Correct spelling!'
+          assert data.get('feedback_type') == 'spelling'
+          assert data.get('optimal') == True
+          assert len(data.get('highlight')) == 0
 
+    def test_spelled_incorrectly_branch(app):
+        with app.test_request_context(json={'entry': 'This is spelllled incorrectly.', 'prompt_id': 000}):
+          response = main.response_endpoint(flask.request)
+          data = json.loads(response.data)
 
-def test_correct_spelling():
-    misspelled = main.get_misspelled_words_no_casing('This is spelled correctly.')
-    assert len(misspelled) == 0
+          assert response.status_code == 200
+          assert data.get('feedback') == 'Try again. There may be a spelling mistake.'
+          assert data.get('feedback_type') == 'spelling'
+          assert data.get('optimal') == False
+          assert data.get('highlight')[0].get('text') == 'spelllled'
 
-def test_incorrect_spelling_single_error_middle_of_sentence():
-    misspelled = main.get_misspelled_words_no_casing('This is spellllled correctly.')
-    assert len(misspelled) == 1
-    assert 'spellllled' in misspelled
+class TestApiSpellCheck:
 
-def test_incorrect_spelling_single_error_end_of_sentence():
-    misspelled = main.get_misspelled_words_no_casing('This is spelled incorrectlee.')
-    assert len(misspelled) == 1
-    assert 'incorrectlee' in misspelled
+    def test_correct_spelling():
+        misspelled = main.get_misspelled_words('This is spelled correctly.')
+        assert len(misspelled) == 0
 
-def test_incorrect_spelling_single_error_beginning_of_sentence():
-    misspelled = main.get_misspelled_words_no_casing('Thissss is spelled incorrectly.')
-    assert len(misspelled) == 1
-    assert 'thissss' in misspelled
+    def test_incorrect_spelling_single_error_middle_of_sentence():
+        misspelled = main.get_misspelled_words('This is spellllled correctly.')
+        assert len(misspelled) == 1
+        assert 'spellllled' in misspelled
 
-def test_incorrect_spelling_multiple_errors():
-    misspelled = main.get_misspelled_words_no_casing('Thissss is spellllled incorrectlee.')
-    assert len(misspelled) == 3
-    assert 'thissss' in misspelled
-    assert 'spellllled' in misspelled
-    assert 'incorrectlee' in misspelled
+    def test_incorrect_spelling_single_error_end_of_sentence():
+        misspelled = main.get_misspelled_words('This is spelled incorrectlee.')
+        assert len(misspelled) == 1
+        assert 'incorrectlee' in misspelled
 
-def test_casing():
-    flagged = ['thissss', 'spEled']
-    entry = 'Thissss is spEled incorrectly.'
-    highlight = main.get_misspelled_highlight_list_with_casing(flagged, entry)
-    assert len(highlight) == 2
-    assert highlight[0]['text'] == 'Thissss'
-    assert highlight[1]['text'] == 'spEled'
+    def test_incorrect_spelling_single_error_beginning_of_sentence():
+        misspelled = main.get_misspelled_words('Thissss is spelled incorrectly.')
+        assert len(misspelled) == 1
+        assert 'Thissss' in misspelled
 
+    def test_incorrect_spelling_multiple_errors():
+        misspelled = main.get_misspelled_words('Thissss is spellllled incorrectlee.')
+        assert len(misspelled) == 3
+        assert 'Thissss' in misspelled
+        assert 'spellllled' in misspelled
+        assert 'incorrectlee' in misspelled
