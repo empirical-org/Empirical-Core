b073797a7ec17432df95a4d4c664f0e867bce8d7
Emilia Friedberg <emiliafriedberg@gmail.com>
2019-12-12 14:15:51 -0500

    more tests; fix rubberbanding

--- services/comprehension/frontend/src/tests/studentView/promptStep.test.tsx
+++ services/comprehension/frontend/src/tests/studentView/promptStep.test.tsx
@@ -4,7 +4,9 @@ import { shallow } from 'enzyme';
 import PromptStep from '../../components/studentView/promptStep'
 import EditorContainer from '../../components/studentView/editorContainer'
 
-import { activityOne } from './data'
+import { activityOne, optimalSubmittedResponse, suboptimalSubmittedResponse, } from './data'
+
+const prompt = activityOne.prompts[0]
 
 const defaultProps = {
   active: false,
@@ -14,7 +16,7 @@ const defaultProps = {
   completeStep: () => {},
   stepNumberComponent: <span />,
   onClick: () => {},
-  prompt: activityOne.prompts[0],
+  prompt,
   passedRef: () => {},
   submittedResponses: []
 }
@@ -34,7 +36,7 @@ describe('PromptStep component', () => {
     describe('before any responses have been submitted', () => {
       const wrapper = shallow(<PromptStep
         { ...defaultProps}
-        active={true}
+        active
         className="step active"
       />)
 
@@ -46,7 +48,129 @@ describe('PromptStep component', () => {
         expect(wrapper.find(EditorContainer)).toHaveLength(1)
       })
 
+      it('has a button with a disabled class and the text "Get feedback"', () => {
+        expect(wrapper.find('button.disabled')).toHaveLength(1)
+        expect(wrapper.find('button.disabled').text()).toEqual("Get feedback")
+      })
+
     })
+
+    describe('before any responses have been submitted', () => {
+      const wrapper = shallow(<PromptStep
+        { ...defaultProps}
+        active
+        className="step active"
+      />)
+
+      it('matches snapshot', () => {
+        expect(wrapper).toMatchSnapshot()
+      })
+
+      it('renders an EditorContainer', () => {
+        expect(wrapper.find(EditorContainer)).toHaveLength(1)
+      })
+
+      it('has a button with a disabled class and the text "Get feedback"', () => {
+        expect(wrapper.find('button.disabled')).toHaveLength(1)
+        expect(wrapper.find('button.disabled').text()).toEqual("Get feedback")
+      })
+
+    })
+
+    describe('when an optimal response has been submitted', () => {
+      const submittedResponses = [optimalSubmittedResponse]
+      const wrapper = shallow(<PromptStep
+        { ...defaultProps}
+        active
+        className="step active"
+        submittedResponses={submittedResponses}
+      />)
+
+      it('matches snapshot', () => {
+        expect(wrapper).toMatchSnapshot()
+      })
+
+      it('renders an EditorContainer', () => {
+        expect(wrapper.find(EditorContainer)).toHaveLength(1)
+      })
+
+      it('has a non-disabled button with the text "Start next sentence"', () => {
+        expect(wrapper.find('button.disabled')).toHaveLength(0)
+        expect(wrapper.find('button').text()).toEqual("Start next sentence")
+      })
+
+    })
+
+    describe('when a suboptimal response has been submitted', () => {
+      const submittedResponses = [suboptimalSubmittedResponse]
+      const wrapper = shallow(<PromptStep
+        { ...defaultProps}
+        active
+        className="step active"
+        submittedResponses={submittedResponses}
+      />)
+
+      it('matches snapshot', () => {
+        expect(wrapper).toMatchSnapshot()
+      })
+
+      it('renders an EditorContainer', () => {
+        expect(wrapper.find(EditorContainer)).toHaveLength(1)
+      })
+
+      it('has a disabled button with the text "Get new feedback"', () => {
+        expect(wrapper.find('button.disabled')).toHaveLength(1)
+        expect(wrapper.find('button.disabled').text()).toEqual("Get new feedback")
+      })
+    })
+
+    describe('when the max attempts have been reached', () => {
+      const submittedResponses = [suboptimalSubmittedResponse, suboptimalSubmittedResponse, suboptimalSubmittedResponse, suboptimalSubmittedResponse, suboptimalSubmittedResponse]
+      const wrapper = shallow(<PromptStep
+        { ...defaultProps}
+        active
+        className="step active"
+        submittedResponses={submittedResponses}
+      />)
+
+      it('matches snapshot', () => {
+        expect(wrapper).toMatchSnapshot()
+      })
+
+      it('renders an EditorContainer', () => {
+        expect(wrapper.find(EditorContainer)).toHaveLength(1)
+      })
+
+      it('has a non-disabled button with the text "Start next sentence"', () => {
+        expect(wrapper.find('button.disabled')).toHaveLength(0)
+        expect(wrapper.find('button.disabled').text()).toEqual("Start next sentence")
+      })
+    })
+
+    describe('when the max attempts have been reached or the last answer is optimal and every other question has been completed', () => {
+      const submittedResponses = [optimalSubmittedResponse]
+      const wrapper = shallow(<PromptStep
+        { ...defaultProps}
+        active
+        everyOtherStepCompleted
+        className="step active"
+        submittedResponses={submittedResponses}
+      />)
+
+      it('matches snapshot', () => {
+        expect(wrapper).toMatchSnapshot()
+      })
+
+      it('renders an EditorContainer', () => {
+        expect(wrapper.find(EditorContainer)).toHaveLength(1)
+      })
+
+      it('has a non-disabled button with the text "Done"', () => {
+        expect(wrapper.find('button.disabled')).toHaveLength(0)
+        expect(wrapper.find('button.disabled').text()).toEqual("Done")
+      })
+    })
+
   })
 
 })
