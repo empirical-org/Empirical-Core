013c9b6acbf38650389488aa05259dd3f7b43b78
Thomas Robertson <thomas.robertson@quill.org>
2019-12-19 16:23:57 -0500

    Fix lint error

--- services/QuillCMS/spec/factories/response.rb
+++ services/QuillCMS/spec/factories/response.rb
@@ -2,6 +2,6 @@ FactoryBot.define do
   factory :response, class: 'Response' do
     feedback 'Sample feedback'
     question_uid SecureRandom.uuid
-    sequence(:text) { |n| 'I am text #{n}' }
+    sequence(:text) { |n| "I am text #{n}" }
   end
 end
