762049cb5bd6ee134ebffb6b0adbd31093735be4
Thomas Robertson <thomas.robertson@quill.org>
2019-12-19 17:19:44 -0500

    Update script paths to prepend bin/ when appropriate

--- services/comprehension/main-api/bin/establish_proxy.sh
+++ services/comprehension/main-api/bin/establish_proxy.sh
@@ -1,1 +1,1 @@
-./cloud_sql_proxy -instances="comprehension-247816:us-east1:comprehension-app-engine"=tcp:5434
+./bin/cloud_sql_proxy -instances="comprehension-247816:us-east1:comprehension-app-engine"=tcp:5434
--- services/comprehension/main-api/bin/start_server_on_prod_db.sh
+++ services/comprehension/main-api/bin/start_server_on_prod_db.sh
@@ -1,4 +1,4 @@
-./establish_proxy.sh &
+./bin/establish_proxy.sh &
 # Wait for the proxy to finish setting up
 sleep 3
 python manage.py runserver
