0ef57f96084d68f8ce5ad54f8f06b8eebd70a32d
Emilia Friedberg <emiliafriedberg@gmail.com>
2019-12-30 10:39:51 -0500

    fix showAdminEdition

---
+++ services/QuillLessons/app/components/classroomLessons/admin/deleteSlideButton.tsx
@@ -0,0 +1,18 @@
+import * as React from 'react'
+
+interface DeleteSlideButtonProps {
+  slideKey: string;
+  deleteSlide: Function;
+}
+
+export default class DeleteSlideButton extends React.Component<DeleteSlideButtonProps, any> {
+
+  handleDeleteSlideClick() {
+    const { deleteSlide, slideKey, } = this.props
+    deleteSlide(slideKey)
+  }
+
+  render() {
+    return (<span className="slide-delete" onClick={this.handleDeleteSlideClick}>Delete Slide</span>)
+  }
+}
