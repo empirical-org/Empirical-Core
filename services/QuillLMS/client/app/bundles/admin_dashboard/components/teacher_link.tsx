3d2a797f56ff911311b4abfa1950d212bce2c92e
Emilia Friedberg <emiliafriedberg@gmail.com>
2020-01-02 13:14:41 -0500

    add react/jsx no target blank linter error and then do a lot of other linting (#5755)
    * wip
    
    * fully linted files
    
    * fix showAdminEdition
    
    * fix congratulationsModal
    
    * fix customizeEditionHeader
    
    * fix teacherNavbar
    
    * fix create_new_accounts
    
    * fix quote_request_modal
    
    * fix class_repor
    
    * fix lesson_recommendation_row
    
    * fix recommendations
    
    * fix student_scores
    
    * fix markdown_parser
    
    * fix school_selector
    
    * fix diagnostic_activity_packs
    
    * fix tests
    
    * wip - small errors
    
    * remove deprecation warning since UNSAFE blah doesn't work yet
    
    * fix premium minis
    
    * make sure deleting a slide works
    
    * more small fixes

--- services/QuillLMS/client/app/bundles/admin_dashboard/components/teacher_link.tsx
+++ services/QuillLMS/client/app/bundles/admin_dashboard/components/teacher_link.tsx
@@ -1,20 +1,21 @@
 import * as React from 'react';
 
+const clickInvalidLink = () => alert('Your Premium Subscription has expired. Please visit Quill.org/premium to access this feature.')
+
 const TeacherLink = ({ name, path, isValid, }) => {
   let linkClass = 'green-link teacher-link';
-  let alertText = 'Your Premium Subscription has expired. Please visit ' +
-    'Quill.org/premium to access this feature.'
-
   if (isValid) {
+    /* eslint-disable react/jsx-no-target-blank */
     return(
       <div>
         <a className={linkClass} href={path} target="_blank">{name}</a>
       </div>
     );
+    /* eslint-enable react/jsx-no-target-blank */
   }
   return(
     <div>
-      <a className={linkClass} onClick={() => alert(alertText)}>{name}</a>
+      <a className={linkClass} onClick={clickInvalidLink}>{name}</a>
     </div>
   );
 };
