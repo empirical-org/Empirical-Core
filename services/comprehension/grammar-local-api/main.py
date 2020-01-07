6082769a6e769176495994936d5d2e956d04fd24
yvespeirsman <yves@nlp.town>
2020-01-07 09:36:53 +0100

    Defined feedback and feedback type at top of main

--- services/comprehension/grammar-local-api/main.py
+++ services/comprehension/grammar-local-api/main.py
@@ -5,6 +5,10 @@ from grammarcheck import GrammarChecker
 GRAMMAR_MODEL_PATH = "model/grammar/"
 checker = GrammarChecker(GRAMMAR_MODEL_PATH)
 
+FEEDBACK_TYPE = 'grammar'
+POS_FEEDBACK = 'Correct grammar!'
+NEG_FEEDBACK = 'Try again. There may be a grammar error.'
+
 
 def check_grammar(request):
     request_json = request.get_json()
@@ -12,30 +16,20 @@ def check_grammar(request):
     entry = param_for('entry', request, request_json)
     prompt_id = param_for('prompt_id', request, request_json)
 
-    print("E", entry)
-    print("P", prompt_id)
-
     if entry is None or prompt_id is None:
         return make_response(jsonify(message="error"), 400)
 
     # errors is a list of tuples (token, token character offset, error type)
     errors = checker.check(entry)
 
-    if not errors:
-        return make_response(jsonify(feedback="Correct grammar!",
-                                     feedback_type="grammar",
-                                     optimal=True,
-                                     response_uid="q23123@3sdfASDF",
-                                     highlight=[]),
-                             200)
-    else:
-        return make_response(jsonify(feedback="Try again. There may be a grammar error.",
-                                     feedback_type="grammar",
-                                     optimal=False,
-                                     response_uid="q23123@3sdfASDF",
-                                     highlight=errors),
-                             200)
-    
+    response_data = {"feedback_type": FEEDBACK_TYPE,
+                     "response_uid": "q23123@3sdfASDF",
+                     "feedback": NEG_FEEDBACK if errors else POS_FEEDBACK
+                     "optimal": not errors,
+                     "highlight": errors}
+
+    return make_response(jsonify(**response_data), 200)
+
 
 def param_for(key, request, request_json):
     if request.args and key in request.args:
