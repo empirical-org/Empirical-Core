aae9e64358c45f59c669347e4fe8c21ed0c5c7cd
Emilia Friedberg <emiliafriedberg@gmail.com>
2020-01-08 10:01:25 -0500

    lint qcl conceptExplanation

--- packages/quill-component-library/src/components/feedback/conceptExplanation.tsx
+++ packages/quill-component-library/src/components/feedback/conceptExplanation.tsx
@@ -1,18 +1,12 @@
 import React from 'react'
 
-// interface ConceptExplanationProps {
-//   description: string;
-//   leftBox: string;
-//   rightBox: string;
-// }
-
-const ConceptExplanation = (props: any) => (
+const ConceptExplanation = ({ description, leftBox, rightBox, }) => (
   <div className="concept-explanation">
-    <div className="concept-explanation-title"><img src={`${process.env.QUILL_CDN_URL}/images/icons/books.jpg`} /> Here's a Hint</div>
-    <div className="concept-explanation-description" dangerouslySetInnerHTML={{__html: props.description}} />
+    <div className="concept-explanation-title"><img src={`${process.env.QUILL_CDN_URL}/images/icons/books.jpg`} /> Here&#39;s a Hint</div>
+    <div className="concept-explanation-description" dangerouslySetInnerHTML={{__html: description}} />
     <div className="concept-explanation-see-write">
-      <div className="concept-explanation-see" dangerouslySetInnerHTML={{__html: props.leftBox}} />
-      <div className="concept-explanation-write" dangerouslySetInnerHTML={{__html: props.rightBox}} />
+      <div className="concept-explanation-see" dangerouslySetInnerHTML={{__html: leftBox}} />
+      <div className="concept-explanation-write" dangerouslySetInnerHTML={{__html: rightBox}} />
     </div>
   </div>
 )
