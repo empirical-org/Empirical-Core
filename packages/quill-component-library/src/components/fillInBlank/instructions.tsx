8bcd99bda9684e69115481dfcae86436c264a480
Emilia Friedberg <emiliafriedberg@gmail.com>
2020-01-08 10:00:23 -0500

    lint qcl instructions

--- packages/quill-component-library/src/components/fillInBlank/instructions.tsx
+++ packages/quill-component-library/src/components/fillInBlank/instructions.tsx
@@ -2,14 +2,10 @@ import React from 'react';
 
 const questionIconSrc = `${process.env.QUILL_CDN_URL}/images/icons/direction.svg`
 
-// interface InstructionsProps {
-//   html: 'string'
-// }
-
-const Instructions = (props: any) => (
+const Instructions = ({ html, }) => (
   <div className="feedback-row">
     <img alt="icon" className="info" src={questionIconSrc} style={{ marginTop: 3, }} />
-    <div dangerouslySetInnerHTML={{ __html: props.html, }} />
+    <div dangerouslySetInnerHTML={{ __html: html, }} />
   </div>
 );
 
