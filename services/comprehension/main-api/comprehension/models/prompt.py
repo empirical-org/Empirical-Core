4260d6290f0d890d0eba288251ae126ab3c2a1b3
Cissy Yu <cissy@quill.org>
2020-01-14 16:36:51 -0500

    removed unneeded files

--- services/comprehension/main-api/comprehension/models/prompt.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-from . import TimestampedModel
-
-
-class Prompt(TimestampedModel):
-    text = models.TextField(null=False)
-    max_attempts = models.PositiveIntegerField(default=5)
-    max_attempts_feedback = models.TextField(null=False)
