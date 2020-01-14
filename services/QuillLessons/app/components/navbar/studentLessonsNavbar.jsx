e03cdc45b65b8eefd0ed0dcaca7700220ae624b0
Emilia Friedberg <emiliafriedberg@gmail.com>
2020-01-14 13:36:50 -0500

    new navbar for apps (#5778)
    * add backup fonts
    
    * Auto stash before cherrypick of "add backup fonts"
    
    * add alt text to ell sentenceCombining
    
    * add alt text to diagnostic playFillInBlankQuestion and lint
    
    * add alt text to diagnostic studentLessons/fillInBlank and lint
    
    * add alt text to connect studentLessons/fillInBlank and lint
    
    * lint connect turk/sentenceCombining
    
    * set up diagnostic and connect to use cdn
    
    * add new quill logo to student navbars
    
    * add new image files
    
    * lint diagnostic sentenceFragmentTemplateComponentML
    
    * lint diagnostic sentenceFragmentTemplateComponent
    
    * lint diagnostic sentenceFragment
    
    * lint diagnostic renderTextEditor
    
    * lint diagnostic renderFormForAnswer
    
    * lint diagnostic renderEndState
    
    * lint connect renderEndState
    
    * lint feedback
    
    * lint question
    
    * lint fillInBlankQuestion
    
    * lint ell diagnostic sentenceFragment
    
    * wip
    
    * lint diagnostic sentenceCombining
    
    * lint diagnostic ell sentenceCombining
    
    * lint connect sentenceFragmentTemplateComponentML
    
    * lint connect turk sentenceCombining
    
    * remove connect sentenceFragmentTemplateComponent because it is never actually being used
    
    * lint connect renderTextEditor
    
    * lint connect sentenceFragment
    
    * lint connect renderFormForAnswer
    
    * lint qcl instructions
    
    * lint qcl conceptExplanation
    
    * minor version bump for qcl
    
    * update qcl version everywhere it gets used
    
    * run install for diagnostic and connect
    
    * add alt tags
    
    * fix some bugs
    
    * patch bump the qcl version
    
    * update qcl version everywhere it's used
    
    * install latest qcl in connect and diagnostic
    
    * fix final bugs
    
    * update tests
    
    * update student nav for all apps
    
    * refactor student navbars to be functional components
    
    Co-authored-by: Eric Adams <erictayloradams@gmail.com>

--- services/QuillLessons/app/components/navbar/studentLessonsNavbar.jsx
+++ services/QuillLessons/app/components/navbar/studentLessonsNavbar.jsx
@@ -1,44 +1,43 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-const Navbar = React.createClass({
-  handleLogoClick() {
-    if (window.confirm('Are you sure you want to leave your lesson?')) {
-      window.location = process.env.EMPIRICAL_BASE_URL;
-    }
-  },
+const quillLogoSrc = `${process.env.QUILL_CDN_URL}/images/logos/quill-logo-white.svg`
 
-  render() {
-    const data = this.props.classroomSessions.data;
-    const lessonData = this.props.classroomLesson.data;
-    const editionData = this.props.customize.editionQuestions;
-    let slideName
-    if (editionData && editionData.questions && data) {
-      if (data.current_slide > 0) {
-        slideName = [<span>Slide {parseInt(data.current_slide)}</span>, `: ${editionData.questions[data.current_slide].data.teach.title}`]
-      } else {
-        slideName = "Lobby"
-      }
+const handleLogoClick = () => {
+  if (window.confirm('Are you sure you want to leave your lesson?')) {
+    window.location = process.env.EMPIRICAL_BASE_URL;
+  }
+}
+
+const Navbar = ({ classroomSessions, classroomLesson, customize, }) => {
+  const { data } = classroomSessions;
+  const lessonData = classroomLesson.data;
+  const editionData = customize.editionQuestions;
+  let slideName
+  if (editionData && editionData.questions && data) {
+    if (data.current_slide > 0) {
+      slideName = [<span key="slide-number">Slide {parseInt(data.current_slide)}</span>, `: ${editionData.questions[data.current_slide].data.teach.title}`]
     } else {
-      slideName = ''
+      slideName = "Lobby"
     }
-    return (
-      <header className={'nav student-nav'} style={{ height: '66px', }}>
-        <nav className="student-lessons">
-          <a onClick={() => this.handleLogoClick()}>
-            <img
-              alt="quill-logo"
-              className="quill-logo"
-              src="https://d2t498vi8pate3.cloudfront.net/assets/home-header-logo-8d37f4195730352f0055d39f7e88df602e2d67bdab1000ac5886c5a492400c9d.png"
-            />
-          </a>
-          <div className="slide-name" key="slide-name">{slideName}</div>
-          <div className="lesson-name">{lessonData.title}</div>
-        </nav>
-      </header>
-    );
-  },
-});
+  } else {
+    slideName = ''
+  }
+  return (
+    <header className='nav student-nav'>
+      <nav className="student-lessons">
+        <img
+          alt="Quill.org logo"
+          className="quill-logo"
+          onClick={handleLogoClick}
+          src={quillLogoSrc}
+        />
+        <div className="slide-name" key="slide-name">{slideName}</div>
+        <div className="lesson-name">{lessonData.title}</div>
+      </nav>
+    </header>
+  );
+}
 
 function select(props) {
   return {
