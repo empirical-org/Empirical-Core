affdbbbb7b4fcc6b1784b1bf2476a96d8a52e931
Cissy Yu <cissy@quill.org>
2020-01-14 10:58:14 -0500

    adding more rule based model stuff

--- services/comprehension/main-api/comprehension/models/regex_rule_set.py
+++ services/comprehension/main-api/comprehension/models/regex_rule_set.py
@@ -1,24 +1,71 @@
 from django.db import models
 
 from . import DjangoChoices
+import re
 
 
-class RegexRuleSet(Model):
-    class FLAGS(DjangoChoices):
+class RegexRuleSet(models.Model):
+    class PASS_ORDER(DjangoChoices):
         FIRST = 'first'
         SECOND = 'second'
 
     prompt_id = models.IntegerField(null=False)
     name = models.TextField(null=False)
     feedback = models.TextField(null=False)
-    pass_order = models.TextField(null=False, choices=FLAGS.get_for_choices())
+    priority = models.IntegerField(null=False, default=1)
+    pass_order = models.TextField(null=False, choices=PASS_ORDER.get_for_choices())
+
+    class Meta:
+        unique_together = ('prompt_id', 'priority','pass_order',)
 
     def __str__(self):
         return self.name
 
-class Rule(Model):
+
+
+class Rule(models.Model):
     regex_text = models.TextField(null=False)
     regex_rule_set = models.ForeignKey(RegexRuleSet, on_delete=models.CASCADE)
 
     def __str__(self):
         return "%s: %s" % (self.regex_rule_set, self.regex_text)
+
+    def match(self, entry):
+        return not re.search(self.regex_text, entry)
+
+
+def get_rules_based_feedback_first_pass(request):
+    request_json = request.get_json()
+
+    entry = request_json.get('entry')
+    prompt_id = request_json.get('prompt_id')
+
+    if entry == None or prompt_id == None:
+        return make_response(jsonify(message="error"), 400)
+
+    regex_rule_set_list = Prompt.regex_rule_set_set.get(
+                    pass_order=RegexRuleSet.PASS_ORDER.FIRST)
+                    .order_by('priority')
+
+    response_data = {
+        'feedback_type': 'rules-based',
+        'response_uid': 'q23123@3sdfASDF',
+        'feedback': 'All rules-based checks passed!',
+        'optimal': True,
+        'highlight': []
+    }
+
+    for regex_rule_set in regex_rule_set_list:
+        for rule in regex_rule_set.rule_set:
+            if not rule.match(entry):
+                response_data.update({
+                    'feedback': regex_rule_set.feedback
+                    'optimal': False
+                })
+                return response_data
+
+    return response_data
+
+
+
+
--- services/comprehension/main-api/comprehension/tests/test_views.py
+++ services/comprehension/main-api/comprehension/tests/test_views.py
@@ -32,3 +32,35 @@ class ActivityViewTest(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.content,
                          b'There are 1 Activities in the DB')
+
+class RegexRuleTest(TestCase):
+    def setUp(self):
+        self.regex_rule_set = RegexRuleSet.objects.create(prompt_id=0,
+                                name='Test Rule Set',
+                                feedback='Test feedback',
+                                priority=1,
+                                pass_order=Activity.FLAGS.FIRST)
+
+    def test_name_not_nullable(self):
+        self.regex_rule_set.title = None
+        with self.assertRaises(ValidationError):
+            self.regex_rule_set.save()
+
+    def test_feedback_not_nullable(self):
+        self.regex_rule_set.feedback = None
+        with self.assertRaises(ValidationError):
+            self.regex_rule_set.save()
+
+    def test_priority_unique_on_prompt_id(self):
+        self.regex_rule_set_2 = RegexRuleSet.objects.create(prompt_id=0,
+                                    name='Test Rule Set Duplicate',
+                                    feedback='Test feedback',
+                                    priority=1,
+                                    pass_order=Activity.PASS_ORDER.FIRST)
+        with self.assertRaises(ValidationError):
+            self.regex_rule_set_2.save()
+
+    def test_pass_order_validation(self):
+        self.regex_rule_set.pass_order = 'DEFINITELY NOT A VALID FLAG'
+        with self.assertRaises(ValidationError):
+            self.regex_rule_set.save()
