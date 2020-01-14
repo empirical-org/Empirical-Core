33e3734eeedb348895276abb5bf7447f47a9788b
Emilia Friedberg <emiliafriedberg@gmail.com>
2020-01-14 11:34:04 -0500

    update diagnostic eslDiagnostic titleCard

--- services/QuillDiagnostic/app/components/eslDiagnostic/titleCard.tsx
+++ services/QuillDiagnostic/app/components/eslDiagnostic/titleCard.tsx
@@ -11,29 +11,31 @@ export interface ComponentProps {
 class TitleCard extends Component<ComponentProps, any> {
 
   getContentHTML() {
-    let html = this.props.data.content ? this.props.data.content : translations.english[this.props.data.key];
-    if (this.props.language !== 'english') {
-      const textClass = this.props.language === 'arabic' ? 'right-to-left arabic-title-div' : '';
-      html += `<br/><div class="${textClass}">${translations[this.props.language][this.props.data.key]}</div>`;
+    const { data, language, } = this.props
+    let html = data.content ? data.content : translations.english[data.key];
+    if (language !== 'english') {
+      const textClass = language === 'arabic' ? 'right-to-left arabic-title-div' : '';
+      html += `<br/><div class="${textClass}">${translations[language][data.key]}</div>`;
     }
     return html;
   }
 
   getButtonText() {
+    const { language, } = this.props
     let text = translations.english['continue button text']
-    if (this.props.language !== 'english') {
-      text += ` / ${translations[this.props.language]['continue button text']}`
+    if (language !== 'english') {
+      text += ` / ${translations[language]['continue button text']}`
     }
     return text
   }
 
   render() {
+    const { nextQuestion, } = this.props
     return (
       <div className="landing-page">
         <div className="landing-page-html" dangerouslySetInnerHTML={{ __html: this.getContentHTML(), }} />
-        <button className="button student-begin" onClick={this.props.nextQuestion}>
+        <button className="quill-button large contained primary" onClick={nextQuestion} type="button">
           {this.getButtonText()}
-          <img className="begin-arrow" src={beginArrow} />
         </button>
       </div>
     );
