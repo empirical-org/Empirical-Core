9de35d44c896c06d63255b81c03ed5b55e9981e1
Thomas Robertson <thomas.robertson@quill.org>
2020-01-15 20:46:54 -0500

    Refactory Activity view to be class-based

--- services/comprehension/main-api/comprehension/views/__init__.py
+++ services/comprehension/main-api/comprehension/views/__init__.py
@@ -24,24 +24,3 @@ def index(request):
 def list_activities(request):
     activities = Activity.objects.all()
     return HttpResponse(f"There are {len(activities)} Activities in the DB")
-
-
-def show_activity(request, id):
-    try:
-        activity = Activity.objects.get(pk=id)
-    except Activity.DoesNotExist:
-        raise Http404
-    passages = activity.get_passages()
-    prompts = activity.get_prompts()
-    data = {
-        'activity_id': activity.id,
-        'title': activity.title,
-        'passages': [passage.text for passage in passages],
-        'prompts': [{
-            'prompt_id': prompt.id,
-            'text': prompt.text,
-            'max_attempts': prompt.max_attempts,
-            'max_attempts_feedback': prompt.max_attempts_feedback,
-        } for prompt in prompts]
-    }
-    return JsonResponse(data)
