aeab00471dd72328d50454f502828c7c4b9e26a4
Cissy Yu <cissy@quill.org>
2020-01-16 10:34:06 -0500

    modified formatting based on first meeting w thomas

--- services/comprehension/main-api/comprehension/urls.py
+++ services/comprehension/main-api/comprehension/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^activities$', views.list_activities, name='list_activities'),
     url(r'^activities/(?P<id>.*)$', views.show_activity, name='show_activity'),
+    url(r'^first_pass$', views._first_pass, name='first_pass'),
+    url(r'^second_pass$', views.second_pass, name='second_pass')
 ]
--- services/comprehension/main-api/comprehension/views.py
+++ services/comprehension/main-api/comprehension/views.py
@@ -31,3 +31,18 @@ def show_activity(request, id):
         } for prompt in prompts]
     }
     return JsonResponse(data)
+
+def first_pass(request):
+    request_json = request.get_json()
+
+    entry = request_json.get('entry')
+    prompt_id = request_json.get('prompt_id')
+
+    if entry == None or prompt_id == None:
+        return make_response(jsonify(message="error"), 400)
+
+    prompt = Prompt.objects.get(id=prompt_id)
+    response = prompt.process_regex_rules(entry, RuleSet.PASS_ORDER.FIRST)
+
+    return JsonResponse(response)
+
