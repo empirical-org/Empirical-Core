3e5b1f7433c23f7e62793f6ca22e653312842d14
Emilia Friedberg <emiliafriedberg@gmail.com>
2020-01-16 12:17:24 -0500

    add new progress bar to proofreader

---
+++ services/QuillProofreader/src/components/proofreaderActivities/progressBar.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react'
+
+interface ProgressBarProps {
+  percent: number;
+  answeredQuestionCount: number;
+  questionCount: number;
+}
+
+const ProgressBar = ({ percent, answeredQuestionCount, questionCount, }: ProgressBarProps) => (
+  <div className="progress-bar-container">
+    <progress className="progress activity-progress" max="100" value={percent} />
+    <p>{answeredQuestionCount} of {questionCount} sentences completed</p>
+  </div>)
+
+export default ProgressBar
