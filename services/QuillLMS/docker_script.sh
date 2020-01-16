96da8624739f6df8d8936e6a7fa65d6f004ea7e4
Haronil Estevez <haronil@quill.org>
2020-01-16 18:34:16 -0500

    make sure to use correct app bundles

--- services/QuillLMS/docker_script.sh
+++ services/QuillLMS/docker_script.sh
@@ -1,3 +1,7 @@
+export APP_BUNDLE_JS=$(ls -ltrah public/webpack/app-bundle-*.js | tail -1 | awk -F "/" '{print $3}')
+sed -e "s@app-bundle.*.js\"@$APP_BUNDLE_JS\"@g" -i public/webpack/manifest.json
+export APP_BUNDLE_CSS=$(ls -ltrah public/webpack/app-bundle-*.css | tail -1 | awk -F "/" '{print $3}')
+sed -e "s@app-bundle.*.css\"@$APP_BUNDLE_CSS\"@g" -i public/webpack/manifest.json
 nohup bundle exec puma -C config/puma.rb > rake.out 2>&1 &
 #nohup rails s > rake.out 2>&1 &
 sed -e "s@/webpack@$DEFAULT_URL/webpack@g" -i public/webpack/manifest.json
