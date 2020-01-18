c3dd3a9d672811c4a9f6311dd69a0f4a8e743eaa
eadams17 <erictayloradams@gmail.com>
2020-01-17 19:19:06 -0500

    add tests for prop dependent rendering

--- services/QuillDiagnostic/app/components/diagnostics/__tests__/studentDiagnostic.test.jsx
+++ services/QuillDiagnostic/app/components/diagnostics/__tests__/studentDiagnostic.test.jsx
@@ -24,23 +24,87 @@ jest.mock('request');
 const mockProps = {
     dispatch: jest.fn(),
     lessons: {
-        hasreceivedata: false
+        hasreceiveddata: false
     },
     playDiagnostic: {
-        currentQuestion: null
+        currentQuestion: null,
+        questionSet: null,
+        answeredQuestions: []
     },
     sentenceFragments: {
-        hasreceivedata: false
+        hasreceiveddata: false
     },
     questions: {
-        hasreceivedata: false
+        hasreceiveddata: false
+    },
+    params: {
+        diagnosticID: 'testID'
     }
 };
 
 describe('StudentDiagnostic Container prop-dependent component rendering', () => {
     const container = shallow(<StudentDiagnostic {...mockProps} />);
-    it("renders a DiagnosticProgressBar and SmartSpinner if no props have been received", () => {
+    let mockPlayDiagnosticProp = {
+        questionSet: [],
+        answeredQuestions: [],
+        currentQuestion: {
+            type: 'SC',
+            data: {
+                key: 'test-key'
+            }
+        }
+    };
+    it("renders a DiagnosticProgressBar with 25% load message and SmartSpinner if no props have been received", () => {
         expect(container.find(DiagnosticProgressBar).length).toEqual(1);
         expect(container.find(SmartSpinner).length).toEqual(1);
+        expect(container.find(SmartSpinner).props().message).toEqual('Loading Your Lesson 25%');
+    });
+    it("renders a DiagnosticProgressBar with 50% load message and SmartSpinner if playDiagnostic.questionSet props has not been received", () => {
+        container.setProps({
+            lessons: {
+                hasreceiveddata: true,
+                data: {
+                    testID: {
+                        landingPageHtml: 'test-html'
+                    }
+                }
+            },
+            questions: { hasreceiveddata: true },
+            sentenceFragments: { hasreceiveddata: true }
+        });
+        expect(container.find(DiagnosticProgressBar).length).toEqual(1);
+        expect(container.find(SmartSpinner).length).toEqual(1);
+        expect(container.find(SmartSpinner).props().message).toEqual('Loading Your Lesson 50%');
+    });
+    it("renders a PlayDiagnosticQuestion component if currentQuestion.type is equal to SC", () => {
+        container.setProps({ playDiagnostic: mockPlayDiagnosticProp });
+        expect(container.find(PlayDiagnosticQuestion).length).toEqual(1);
+    });
+    it("renders a PlaySentenceFragment component if currentQuestion.type is equal to SF", () => {
+        mockPlayDiagnosticProp.currentQuestion.type = 'SF';
+        container.setProps({ playDiagnostic: mockPlayDiagnosticProp });
+        expect(container.find(PlaySentenceFragment).length).toEqual(1);
+    });
+    it("renders a PlayFillInTheBlankQuestion component if currentQuestion.type is equal to FB", () => {
+        mockPlayDiagnosticProp.currentQuestion.type = 'FB';
+        container.setProps({ playDiagnostic: mockPlayDiagnosticProp });
+        expect(container.find(PlayFillInTheBlankQuestion).length).toEqual(1);
+    });
+    it("renders a PlayTitleCard component if currentQuestion.type is equal to TL", () => {
+        mockPlayDiagnosticProp.currentQuestion.type = 'TL';
+        container.setProps({ playDiagnostic: mockPlayDiagnosticProp });
+        expect(container.find(PlayTitleCard).length).toEqual(1);
+    });
+    it("renders a FinishedDiagnostic component if answeredQuestions is greater than 0 and unansweredQuestions is 0", () => {
+        mockPlayDiagnosticProp.currentQuestion = null;
+        mockPlayDiagnosticProp.answeredQuestions = [{}, {}, {}];
+        mockPlayDiagnosticProp.unansweredQuestions = [];
+        container.setProps({ playDiagnostic: mockPlayDiagnosticProp });
+        expect(container.find(FinishedDiagnostic).length).toEqual(1);
+    });
+    it("renders a LandingPage component in all other cases", () => {
+        mockPlayDiagnosticProp.answeredQuestions = [];
+        container.setProps({ playDiagnostic: mockPlayDiagnosticProp });
+        expect(container.find(LandingPage).length).toEqual(1);
     });
 });
