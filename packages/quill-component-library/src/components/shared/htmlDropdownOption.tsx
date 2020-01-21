5ba2cc021d00bc4301c96d63d864d0c591324a1f
Emilia Friedberg <emiliafriedberg@gmail.com>
2020-01-21 10:43:34 -0500

    wip

--- packages/quill-component-library/src/components/shared/htmlDropdownOption.tsx
+++ packages/quill-component-library/src/components/shared/htmlDropdownOption.tsx
@@ -1,13 +1,23 @@
 import React from 'react'
 import { components } from 'react-select';
 
-export const HTMLDropdownOption = props => {
-  const { label, } = props
-  return (
-    <div className="html-dropdown-option">
-      <components.Option {...props}>
-        <div dangerouslySetInnerHTML={{ __html: label }} />
-      </components.Option>
-    </div>
-  );
+export class HTMLDropdownOption extends React.Component<any> {
+  handleMouseEnter = () => {
+    const { options, selectProps, data, } = this.props
+    const index = options.findIndex(opt => opt.value === data.value)
+    selectProps.updateCursor(index)
+  }
+
+  render() {
+    const { label, selectProps, options, data, } = this.props
+    const passedProps = {...this.props}
+    passedProps.isFocused = options[selectProps.cursor] === data
+    return (
+      <div className="html-dropdown-option" onMouseEnter={this.handleMouseEnter}>
+        <components.Option {...this.props}>
+          <div dangerouslySetInnerHTML={{ __html: label }} />
+        </components.Option>
+      </div>
+    );
+  }
 };
