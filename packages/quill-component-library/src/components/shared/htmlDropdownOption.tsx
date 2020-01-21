e3cd23e4533bf5bc29af7a6c1fc22bae08e3ec33
Emilia Friedberg <emiliafriedberg@gmail.com>
2020-01-21 10:49:41 -0500

    pass inner props instead of all props

--- packages/quill-component-library/src/components/shared/htmlDropdownOption.tsx
+++ packages/quill-component-library/src/components/shared/htmlDropdownOption.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { components } from 'react-select';
 
-export class HTMLDropdownOption extends React.StatelessComponent {
+export class HTMLDropdownOption extends React.Component<any> {
   handleMouseEnter = () => {
     const { options, selectProps, data, } = this.props
     const index = options.findIndex(opt => opt.value === data.value)
@@ -9,12 +9,12 @@ export class HTMLDropdownOption extends React.StatelessComponent {
   }
 
   render() {
-    const { label, selectProps, options, data, } = this.props
+    const { label, selectProps, options, data, innerProps, } = this.props
     const passedProps = {...this.props}
     passedProps.isFocused = options[selectProps.cursor] === data
     return (
       <div className="html-dropdown-option" onMouseEnter={this.handleMouseEnter}>
-        <components.Option {...this.props}>
+        <components.Option {...innerProps}>
           <div dangerouslySetInnerHTML={{ __html: label }} />
         </components.Option>
       </div>
