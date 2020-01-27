86ffd0b3d2ae75f54fa34281fa49a656da3f29e7
Emilia Friedberg <emiliafriedberg@gmail.com>
2020-01-27 12:52:59 -0500

    wip

--- packages/quill-component-library/src/components/shared/carouselAnimation.tsx
+++ packages/quill-component-library/src/components/shared/carouselAnimation.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
-import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
-const CarouselAnimation = (props) => (
-  <ReactCSSTransitionGroup
+import ReactCSSTransitionReplace from 'react-css-transition-replace'
+
+const CarouselAnimation = ({ children, }) => (
+  <ReactCSSTransitionReplace
+    transitionAppear={true}
+    transitionAppearTimeout={400}
     transitionEnterTimeout={300}
     transitionLeaveTimeout={300}
     transitionName="carousel"
   >
-    {props.children}
-  </ReactCSSTransitionGroup>
+    {children}
+  </ReactCSSTransitionReplace>
 )
 
 export { CarouselAnimation }
