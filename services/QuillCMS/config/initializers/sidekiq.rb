3926abbb4dc2a5138d50bc1270631c69e32c88d6
Thomas Robertson <thomas.robertson@quill.org>
2020-01-28 11:28:10 -0500

    Add middleware to stick workers to the master db

--- services/QuillCMS/config/initializers/sidekiq.rb
+++ services/QuillCMS/config/initializers/sidekiq.rb
@@ -1,8 +1,18 @@
 sidekiq_base_url = ENV['REDISCLOUD_URL'] || 'redis://localhost:6379'
 sidekiq_url = "#{sidekiq_base_url}/2"
 
+class StickToLeaderDbSidekiqMiddleware
+  def call(worker, msg, queue)
+    ActiveRecord::Base.connection.stick_to_master!
+    yield
+  end
+end
+
 Sidekiq.configure_server do |config|
   config.redis = { url: sidekiq_url }
+  config.server_middleware do |chain|
+    chain.add(StickToLeaderDbSidekiqMiddleware)
+  end
 end
 
 Sidekiq.configure_client do |config|
