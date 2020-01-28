f4faf84fff649d4c298c54c40cac7ff3338fed15
Anathomical <thomas.robertson@quill.org>
2020-01-28 14:49:12 -0500

    Feature/comprehension/variable feedback (#5835)
    * Use Comprehension API for feedback
    
    This includes a modification to submit previously returned feedback to the API so that it can read a sense of history.
    
    * Fix bad merge conflict resolution
    
    * Update ml feedback endpoint to handle variable feedback
    
    * Refactor the way variable feedback works
    
    We were cycling through all feedback, then going to the default, but it seems likely that what we would rather do is just cycle through the feedback repeatedly in order.
    
    * Remove references to the old "single Ml feedback" view
    
    We've refactored this code to allow us to use a single view definition with a param instead to DRY the code out
    
    * Rename ML Feedback view
    
    Now that we've refactored to use a single View class for both Single and Multi label feedback
    
    * Add tests for multiple feedback
    
    * Add migration for variable feedback
    
    * Remove migration file in prep for re-creating it
    
    We want them to be linear, and I'm about to merge another migration in from a different branch
    
    * Add new migration for variable feedback
    
    * Fix flake8 stuff

--- services/comprehension/main-api/comprehension/models/prompt.py
+++ services/comprehension/main-api/comprehension/models/prompt.py
@@ -35,12 +35,13 @@ class Prompt(TimestampedModel):
         feedback.update(feedback=CORRECT_FEEDBACK, optimal=True)
         return feedback
 
-    def fetch_auto_ml_feedback(self, entry, multi_label=True):
+    def fetch_auto_ml_feedback(self, entry, previous_feedback=[],
+                               multi_label=True):
         if multi_label:
             labels = self._request_ml_labels(entry)
         else:
             labels = self._request_single_ml_label(entry)
-        return self._get_feedback_for_labels(labels)
+        return self._get_feedback_for_labels(labels, previous_feedback)
 
     def _request_single_ml_label(self, entry):
         return self.ml_model.request_single_label(entry)
@@ -48,13 +49,27 @@ class Prompt(TimestampedModel):
     def _request_ml_labels(self, entry):
         return self.ml_model.request_labels(entry)
 
-    def _get_feedback_for_labels(self, labels):
+    def _get_feedback_for_labels(self, labels, previous_feedback=[]):
         combined_labels = combine_labels(labels)
-        try:
-            return self.ml_feedback.get(combined_labels=combined_labels)
-        except MLFeedback.DoesNotExist:
+        filtered_feedback = list(filter(self._filter_feedback(combined_labels),
+                                        previous_feedback))
+        label_match_count = len(filtered_feedback)
+        feedback_options = (self.ml_feedback
+                                .filter(combined_labels=combined_labels)
+                                .order_by('order')
+                                .all())
+        available_feedback_count = len(feedback_options)
+
+        if not available_feedback_count:
             return self._get_default_feedback()
+
+        next_feedback_index = label_match_count % available_feedback_count
+        return feedback_options[next_feedback_index]
 
     def _get_default_feedback(self):
         default_label = MLFeedback.DEFAULT_FEEDBACK_LABEL
         return self.ml_feedback.get(combined_labels=default_label)
+
+    @staticmethod
+    def _filter_feedback(labels):
+        return lambda x: x.get('labels') == labels
