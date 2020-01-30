4c517e0d3f0c90ae3d7423a3bcc27415c2d2a40f
Cissy Yu <cissy@quill.org>
2020-01-30 17:06:06 -0500

    incorporating suggestions

--- packages/quill-marking-logic/src/libs/matchers/exact_match.spec.ts
+++ packages/quill-marking-logic/src/libs/matchers/exact_match.spec.ts
@@ -19,21 +19,4 @@ describe('The exactMatch function', () => {
         assert.equal(matchedResponse.id, savedResponses[0].id);
     });
 
-    it('Should take a response string and find the corresponding saved response if the string is a different casing and caseSensitive is false', () => {
-      const responseString = "MY DOG TOOK A NAP.";
-      const savedResponses = [
-        {
-          id: 1,
-          text: "My dog took a nap.",
-          feedback: "Good job, that's a sentence!",
-          optimal: true,
-          count: 1,
-          question_uid: 'questionOne'
-        }
-      ]
-      const caseSensitive = false
-      const matchedResponse = exactMatch(responseString, savedResponses, caseSensitive);
-      assert.equal(matchedResponse.id, savedResponses[0].id);
-  });
-
 });
--- packages/quill-marking-logic/src/libs/matchers/exact_match.ts
+++ packages/quill-marking-logic/src/libs/matchers/exact_match.ts
@@ -2,14 +2,8 @@ import * as _ from 'underscore'
 import {stringNormalize} from 'quill-string-normalizer'
 import {Response} from '../../interfaces'
 
-export function exactMatch(responseString:string, responses:Array<Response>, caseSensitive:Boolean=true):Response|undefined {
-  if (caseSensitive) {
-    return _.find(responses,
-      resp => stringNormalize(resp.text) === stringNormalize(responseString)
-    );
-  } else {
-      return _.find(responses,
-      resp => stringNormalize(resp.text).toLowerCase() === stringNormalize(responseString).toLowerCase()
-    );
-  }
+export function exactMatch(responseString:string, responses:Array<Response>):Response|undefined {
+  return _.find(responses,
+    resp => stringNormalize(resp.text) === stringNormalize(responseString)
+  );
 }
