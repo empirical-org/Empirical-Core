6582cb8ce48c5b7c133bc2f1dd98ecf4c0c2154d
Emilia Friedberg <emiliafriedberg@gmail.com>
2020-01-31 15:07:35 -0500

    dependabot; removing nbsp spaces from connect responses; add isEll flag (#5863)
    * Bump rack from 1.6.11 to 1.6.12 in /services/QuillLMS
    
    Bumps [rack](https://github.com/rack/rack) from 1.6.11 to 1.6.12.
    - [Release notes](https://github.com/rack/rack/releases)
    - [Changelog](https://github.com/rack/rack/blob/master/CHANGELOG.md)
    - [Commits](https://github.com/rack/rack/compare/1.6.11...1.6.12)
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * Jest testing for Diagnostic Container (#5819)
    
    * setup test framework for diagnostic container
    
    * add test files
    
    * add tests for prop dependent rendering
    
    * add component function tests
    
    * update tests for standard diagnostic
    
    * reconcile lock file
    
    * remove empty file
    
    * small variable updates
    
    * Add isELL flag for Diagnostic activities; convert associated files into Typescript (#5846)
    
    * various refactoring; add testing for updated components; fix eslint errors; remove deprecated eslint rule
    
    * typescript updates
    
    * add more interfaces and typescript reducers; update tests
    
    * add lesson interface; fix eslint errors; update function names; update tests
    
    * add &nbsp; to the list of replaced characters, and prevent paste (#5862)
    
    Co-authored-by: hequill <55706216+hequill@users.noreply.github.com>
    Co-authored-by: Eric Adams <erictayloradams@gmail.com>

---
+++ services/QuillDiagnostic/app/components/lessons/__tests__/lessonFormComponents.test.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { DeleteButton, NameInput } from '../lessonFormComponents.tsx';
+
+describe('DeleteButton component', () => {
+    const mockProps = {
+        onChange: jest.fn(),
+        questionId: 'birds-arent-real'
+    };
+    const component = shallow(<DeleteButton {...mockProps} />);
+    it('should call onChange() prop function with questionId prop as argument when button is clicked', () => {
+        component.find('button').simulate('click');
+        expect(mockProps.onChange).toHaveBeenCalledWith(mockProps.questionId);
+    });
+});
+
+describe('NameInput component', () => {
+    const mockProps = {
+        name: 'Donna Summer',
+        onChange: jest.fn()
+    };
+    const component = shallow(<NameInput {...mockProps} />);
+    it('should pass name prop to input element a value', () => {
+        expect(component.find('input').props().value).toEqual(mockProps.name);
+    });
+    it('should call onChange() prop function with "name" and e arguments when input text changes', () => {
+        const e = {
+            target: {
+                value: 'Queen of Disco'
+            }
+        };
+        component.find('input').simulate('change', e);
+        expect(mockProps.onChange).toHaveBeenCalledWith('name', e);
+    });
+});
