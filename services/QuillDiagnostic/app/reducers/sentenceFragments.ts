9c10b26c747a421b37d1dbb8b7b05967d6bdcb70
Emilia Friedberg <emiliafriedberg@gmail.com>
2020-01-31 15:07:35 -0500

    dependabot; removing nbsp spaces from connect responses; add isEll flag (#5863)
    * Bump rack from 1.6.11 to 1.6.12 in /services/QuillLMS
    
    Bumps [rack](https://github.com/rack/rack) from 1.6.11 to 1.6.12.
    - [Release notes](https://github.com/rack/rack/releases)
    - [Changelog](https://github.com/rack/rack/blob/master/CHANGELOG.md)
    - [Commits](https://github.com/rack/rack/compare/1.6.11...1.6.12)
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * Jest testing for Diagnostic Container (#5819)
    
    * setup test framework for diagnostic container
    
    * add test files
    
    * add tests for prop dependent rendering
    
    * add component function tests
    
    * update tests for standard diagnostic
    
    * reconcile lock file
    
    * remove empty file
    
    * small variable updates
    
    * Add isELL flag for Diagnostic activities; convert associated files into Typescript (#5846)
    
    * various refactoring; add testing for updated components; fix eslint errors; remove deprecated eslint rule
    
    * typescript updates
    
    * add more interfaces and typescript reducers; update tests
    
    * add lesson interface; fix eslint errors; update function names; update tests
    
    * add &nbsp; to the list of replaced characters, and prevent paste (#5862)
    
    Co-authored-by: hequill <55706216+hequill@users.noreply.github.com>
    Co-authored-by: Eric Adams <erictayloradams@gmail.com>

---
+++ services/QuillDiagnostic/app/reducers/sentenceFragments.ts
@@ -0,0 +1,97 @@
+import { Action } from "redux";
+import ActionTypes from '../constants';
+import _ from 'lodash';
+import { SentenceFragment } from '../interfaces/SentenceFragment';
+
+export interface SentenceFragmentsReducerState {
+    data: { [key: string]: SentenceFragment },
+    hasreceiveddata: boolean,
+    states: { [key: string]: string },
+    submittingnew: boolean,
+}
+
+type SentenceFragmentsReducerAction = Action & { data: SentenceFragment, sfid: string, rid: string }
+
+const initialState = {
+    data: {}, 
+    hasreceiveddata: false,
+    newSentenceFragmentModalOpen: false,
+    states: {}, 
+    submittingnew: false,
+};
+
+export default (
+    currentState = initialState,
+    action: SentenceFragmentsReducerAction,
+) => {
+    let newState: SentenceFragmentsReducerState;
+    switch(action.type){
+        case ActionTypes.RECEIVE_SENTENCE_FRAGMENTS_DATA:
+            return Object.assign({},currentState,{
+                hasreceiveddata: true,
+                data: action.data
+            });
+        case ActionTypes.AWAIT_NEW_SENTENCE_FRAGMENT_RESPONSE:
+            return Object.assign({},currentState,{
+                submittingnew: true
+            });
+        case ActionTypes.RECEIVE_NEW_SENTENCE_FRAGMENT_RESPONSE:
+            return Object.assign({},currentState,{
+                submittingnew: false,
+                newSentenceFragmentModalOpen: false
+            });
+        case ActionTypes.START_SENTENCE_FRAGMENT_EDIT:
+            newState = _.cloneDeep(currentState);
+            newState.states[action.sfid] = ActionTypes.EDITING_SENTENCE_FRAGMENT;
+            return newState;
+        case ActionTypes.FINISH_SENTENCE_FRAGMENT_EDIT:
+            newState = _.cloneDeep(currentState);
+            delete newState.states[action.sfid];
+            return newState;
+        case ActionTypes.SUBMIT_SENTENCE_FRAGMENT_EDIT:
+            newState = _.cloneDeep(currentState);
+            newState.states[action.sfid] = ActionTypes.SUBMITTING_SENTENCE_FRAGMENT;
+            return newState;
+        case ActionTypes.TOGGLE_NEW_SENTENCE_FRAGMENT_MODAL:
+            return Object.assign({},currentState,{
+                newSentenceFragmentModalOpen: !currentState.newSentenceFragmentModalOpen
+            });
+        case ActionTypes.START_RESPONSE_EDIT:
+            newState = _.cloneDeep(currentState);
+            newState.states[action.sfid] = ActionTypes.START_RESPONSE_EDIT + "_" + action.rid;
+            return newState;
+        case ActionTypes.FINISH_RESPONSE_EDIT:
+            newState = _.cloneDeep(currentState);
+            delete newState.states[action.sfid];
+            return newState;
+        case ActionTypes.START_CHILD_RESPONSE_VIEW:
+            newState = _.cloneDeep(currentState);
+            newState.states[action.sfid] = ActionTypes.START_CHILD_RESPONSE_VIEW + "_" + action.rid;
+            return newState;
+        case ActionTypes.CANCEL_CHILD_RESPONSE_VIEW:
+            newState = _.cloneDeep(currentState);
+            delete newState.states[action.sfid];
+            return newState;
+        case ActionTypes.START_FROM_RESPONSE_VIEW:
+            newState = _.cloneDeep(currentState);
+            newState.states[action.sfid] = ActionTypes.START_FROM_RESPONSE_VIEW + "_" + action.rid;
+            return newState;
+        case ActionTypes.CANCEL_FROM_RESPONSE_VIEW:
+            newState = _.cloneDeep(currentState);
+            delete newState.states[action.sfid];
+            return newState;
+        case ActionTypes.START_TO_RESPONSE_VIEW:
+            newState = _.cloneDeep(currentState);
+            newState.states[action.sfid] = ActionTypes.START_TO_RESPONSE_VIEW + "_" + action.rid;
+            return newState;
+        case ActionTypes.CANCEL_TO_RESPONSE_VIEW:
+            newState = _.cloneDeep(currentState);
+            delete newState.states[action.sfid];
+            return newState;
+        case ActionTypes.SUBMIT_RESPONSE_EDIT:
+            newState = _.cloneDeep(currentState);
+            newState.states[action.sfid] = ActionTypes.SUBMITTING_RESPONSE;
+            return newState;
+        default: return currentState || initialState;
+    }
+};
