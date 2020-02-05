35bb9de82ce6ebf12f51c5fc2518309c48218528
Emilia Friedberg <emiliafriedberg@gmail.com>
2020-02-05 12:03:03 -0500

    finish student navbar for desktop

--- services/QuillLMS/client/app/bundles/Student/components/studentNavbarDropdown.tsx
+++ services/QuillLMS/client/app/bundles/Student/components/studentNavbarDropdown.tsx
@@ -8,13 +8,21 @@ interface StudentNavbarDropdownProps {
 
 interface StudentNavbarDropdownState {
   isOpen: boolean;
+  cursor: null|number;
 }
 
 const MAXIMUM_NAME_LENGTH = 27
 const ENTER = 'Enter'
 const TAB = 'Tab'
+const ARROWDOWN = 'ArrowDown'
+const ARROWUP = 'ArrowUp'
 const MOUSEDOWN = 'mousedown'
 const KEYDOWN = 'keydown'
+
+const links = [
+  <a href="/account_settings" id="settings-link" key="settings-link">Settings</a>,
+  <a href="/session" id="logout-link" key="logout-link">Logout</a>
+]
 
 export default class StudentNavbarDropdown extends React.Component<StudentNavbarDropdownProps, StudentNavbarDropdownState> {
   private dropdownContainer: any // eslint-disable-line react/sort-comp
@@ -23,13 +31,27 @@ export default class StudentNavbarDropdown extends React.Component<StudentNavbar
     super(props)
 
     this.state = {
-      isOpen: false
+      isOpen: false,
+      cursor: null
     }
   }
 
   componentDidMount() {
     document.addEventListener(MOUSEDOWN, this.handleClickOutsideDropdown, false)
-    document.addEventListener(KEYDOWN, this.handleTab, false)
+    document.addEventListener(KEYDOWN, this.handleKeyDown, false)
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener(MOUSEDOWN, this.handleClickOutsideDropdown, false)
+    document.removeEventListener(KEYDOWN, this.handleKeyDown, false)
+  }
+
+  updateFocusedLink = () => {
+    const { cursor, } = this.state
+    const focusedLink = links[cursor]
+
+    const element = document.getElementById(focusedLink.key)
+    element.focus()
   }
 
   dropdownContainerRef = (node) => this.dropdownContainer = node
@@ -40,19 +62,44 @@ export default class StudentNavbarDropdown extends React.Component<StudentNavbar
     }
   }
 
-  handleTab = (e) => {
-    if (e.key !== TAB) { return }
-    this.setState({ isOpen: false, })
+  handleKeyDown = (e) => {
+    const { cursor, } = this.state
+
+    const inactiveNode = !(this.dropdownContainer && this.dropdownContainer.contains(e.target))
+    const keyWasNotTab = e.key !== TAB
+
+    if (inactiveNode && keyWasNotTab) { return }
+
+    switch (e.key) {
+      case ARROWDOWN:
+        e.preventDefault()
+        if (cursor < links.length - 1) {
+          this.setState(prevState => {
+            if (prevState.cursor !== null) {
+              return { cursor: prevState.cursor + 1 }
+            }
+            return { cursor: 0 }
+          }, this.updateFocusedLink)
+        } else if (cursor === null && links.length === 1) {
+          this.setState({ cursor: 0 }, this.updateFocusedLink)
+        } else {
+          this.updateFocusedLink()
+        }
+        break
+      case ARROWUP:
+        e.preventDefault()
+        this.setState(prevState => ({ cursor: Math.max(prevState.cursor - 1, 0) }), this.updateFocusedLink)
+        break
+      case TAB:
+        this.setState({ isOpen: false, })
+        break
+      default:
+        break
+    }
   }
 
   handleNameClick = () => {
     this.setState(prevState => ({ isOpen: !prevState.isOpen }))
-  }
-
-  handleEnterOnName = (e) => {
-    if (e.key !== ENTER) { return }
-
-    this.handleNameClick()
   }
 
   renderDropdown() {
@@ -61,8 +108,7 @@ export default class StudentNavbarDropdown extends React.Component<StudentNavbar
     if (!isOpen) { return }
 
     return (<div className="dropdown-menu">
-      <a href="/account_settings">Settings</a>
-      <a href="/session">Logout</a>
+      {links}
     </div>)
   }
 
@@ -70,7 +116,7 @@ export default class StudentNavbarDropdown extends React.Component<StudentNavbar
     const { name, } = this.props
     const displayedName = name.length > MAXIMUM_NAME_LENGTH ? `${name.substring(0, MAXIMUM_NAME_LENGTH)}...`: name
     return (<div className="student-navbar-dropdown student-navbar-item" ref={this.dropdownContainerRef}>
-      <button className="focus-on-dark" id="name-container" onClick={this.handleNameClick} onKeyDown={this.handleEnterOnName} type="button">
+      <button className="focus-on-dark" id="name-container" onClick={this.handleNameClick} type="button">
         <img alt="Avatar icon" src={avatarSrc} />
         <span>{displayedName}</span>
         <div aria-label="Dropdown icon" className="dropdown-indicator" />
