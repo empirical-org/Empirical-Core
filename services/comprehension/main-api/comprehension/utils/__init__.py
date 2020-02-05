dd306bff9f739fcf35ccd3e480cf350ef71dca53
Thomas Robertson <thomas.robertson@quill.org>
2020-02-05 12:00:17 -0500

    Add util for building highlight payloads for the API

--- services/comprehension/main-api/comprehension/utils/__init__.py
+++ services/comprehension/main-api/comprehension/utils/__init__.py
@@ -2,16 +2,28 @@ def combine_labels(labels):
     return '_'.join(sorted(labels))
 
 
-def construct_feedback_payload(feedback, feedback_type, optimal, labels=None):
+def construct_feedback_payload(feedback, feedback_type, optimal,
+                               highlight=[], labels=None):
     payload = {
         'feedback': feedback,
         'feedback_type': feedback_type,
         'optimal': optimal,
         'response_id': 'PLACEHOLDER',
-        'highlight': [],
+        'highlight': highlight,
     }
 
     if labels:
         payload['labels'] = labels
 
     return payload
+
+
+def construct_highlight_payload(highlight_type, highlight_text,
+                                starting_index=0, highlight_id=None):
+    return {
+        'type': highlight_type,
+        'id': highlight_id,
+        'text': highlight_text,
+        'category': None,
+        'character': starting_index,
+    }
