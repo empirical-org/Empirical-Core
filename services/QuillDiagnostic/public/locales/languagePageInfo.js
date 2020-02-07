3edc02953fa0ba78872426d93834733fc96fd713
Eric Adams <erictayloradams@gmail.com>
2020-02-07 14:12:38 -0500

    update languagePageInfo

--- services/QuillDiagnostic/public/locales/languagePageInfo.js
+++ services/QuillDiagnostic/public/locales/languagePageInfo.js
@@ -1,4 +1,5 @@
 export const languages = ['english', 'arabic', 'chinese', 'french', 'hindi', 'spanish', 'vietnamese'];
+export const languagesV2 = ['english', 'chinese', 'hindi', 'spanish', 'french', 'arabic', 'russian', 'portuguese', 'urdu', 'german', 'japanese', 'korean', 'vietnamese', 'thai', 'ukrainian', 'tagalog', 'dari'];
 
 export const languageData = {
     'english': {
@@ -29,4 +30,75 @@ export const languageData = {
         flag: 'https://s3.amazonaws.com/empirical-core-prod/assets/flags/Vietnam.png',
         label: 'Tiếng Việt'
     }
+}
+
+export const languageDataV2 = {
+    'english': {
+        flag: 'https://s3.amazonaws.com/empirical-core-prod/assets/flags/U.S._Outlying_Islands.png',
+        label: 'English'
+    },
+    'chinese': {
+        flag: 'https://s3.amazonaws.com/empirical-core-prod/assets/flags/China.png',
+        label: '中文'
+    },
+    'hindi': {
+        flag: 'https://s3.amazonaws.com/empirical-core-prod/assets/flags/India.png',
+        label: 'हिंदी'
+    },
+    'spanish': {
+        flag: 'https://s3.amazonaws.com/empirical-core-prod/assets/flags/Spain.png',
+        label: 'Español'
+    },
+    'french': {
+        flag: 'https://s3.amazonaws.com/empirical-core-prod/assets/flags/France.png',
+        label: 'Français'
+    },
+    'arabic': {
+        flag: 'https://s3.amazonaws.com/empirical-core-prod/assets/flags/Egypt.png',
+        label: 'العربية'
+    },
+    'russian': {
+        flag: '',
+        label: 'Русский'
+    },
+    'portuguese': {
+        flag: '',
+        label: 'Português'
+    },
+    'urdu': {
+        flag: '',
+        label: 'اردو'
+    },
+    'german': {
+        flag: '',
+        label: 'Deutsche'
+    },
+    'japanese': {
+        flag: '',
+        label: '日本語'
+    },
+    'korean': {
+        flag: '',
+        label: '한국어'
+    },
+    'vietnamese': {
+        flag: 'https://s3.amazonaws.com/empirical-core-prod/assets/flags/Vietnam.png',
+        label: 'Tiếng Việt'
+    },
+    'thai': {
+        flag: '',
+        label: 'ไทย'
+    },
+    'ukrainian': {
+        flag: '',
+        label: 'Українська'
+    },
+    'tagalog': {
+        flag: '',
+        label: 'Tagalog'
+    },
+    'dari': {
+        flag: '',
+        label: 'درى'
+    },
 }
