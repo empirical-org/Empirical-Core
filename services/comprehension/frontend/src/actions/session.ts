a5aac902f73d354fa67092c9977cda41d119beab
Emilia Friedberg <emiliafriedberg@gmail.com>
2020-02-07 14:51:37 -0500

    remove spoofed passage highlights

--- services/comprehension/frontend/src/actions/session.ts
+++ services/comprehension/frontend/src/actions/session.ts
@@ -21,19 +21,13 @@ export const getFeedback = (activityUID: string, entry: string, promptID: string
       json: true,
     }
 
-    const potentialHighlights = [
-      [{type: 'passage', id: 'what', text: "When softball player Ariana Chretien heard the news, she grew worried. The cuts meant that Chretien would have to choose between two of her passions--softball and aviation. Although Chretien had received multiple softball scholarship offers, Eastern Michigan was the only school that also offered an aviation major. She explained, 'My major is aviation, and not a lot of schools have that. So, either I find a school with aviation but no scholarships or scholarships but no major.'"}],
-      [{ type: "passage", id: "whatever", text: "The judge emphasized that the cuts to the women’s teams made the situation even worse. He explained that the cuts showed that the school was not making enough effort to give women equal access to sports."}],
-      [{ type: "passage", id: "whatever", text: "Title IX requires schools to give men and women equal chances to play sports."}]
-    ]
-
     request.post(requestObject, (e, r, body) => {
       const feedbackObj: FeedbackObject = {
         feedback: body.feedback,
         feedback_type: body.feedback_type,
         optimal: body.optimal,
         response_id: body.response_id,
-        highlight: body.highlight.concat(potentialHighlights[Math.floor(Math.random() * potentialHighlights.length)]),
+        highlight: body.highlight,
       }
       dispatch({ type: ActionTypes.RECORD_FEEDBACK, promptID, feedbackObj });
       if (callback) {
