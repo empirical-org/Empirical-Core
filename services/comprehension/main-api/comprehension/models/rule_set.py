26b38d93b81ec63a390fd86787bf6637cf2a89c6
Cissy Yu <cissy@quill.org>
2020-02-11 11:36:48 -0500

    added all changes to structure for rule pass functions

--- services/comprehension/main-api/comprehension/models/rule_set.py
+++ services/comprehension/main-api/comprehension/models/rule_set.py
@@ -27,3 +27,36 @@ class RuleSet(BaseModel):
 
     class Meta:
         unique_together = ('prompt', 'priority', 'pass_order', )
+
+    def process_rule_set(self, entry):
+        rules_list = self.rules.all()
+        if self.match == self.REGEX_MATCH_TYPES.ALL:
+            return self._process_match_all(rules_list, entry)
+        elif self.match == self.REGEX_MATCH_TYPES.ANY:
+            return self._process_match_any(rules_list,
+                                                    entry)
+        else:
+            return True
+
+    def _process_match_any(self, rules_list, entry):
+        # match 'any' returns CORRECT at the first match it finds
+        if not rules_list:
+            return True
+
+        for rule in rules_list:
+            if rule.match(entry):
+                break
+        else:
+            return False
+
+        return True
+
+    def _process_match_all(self, rules_list, entry):
+        # match 'all' returns CORRECT after checking the whole list
+        # and verifying all items are correct
+        for rule in rules_list:
+            if not rule.match(entry):
+                return False
+
+        return True
+
