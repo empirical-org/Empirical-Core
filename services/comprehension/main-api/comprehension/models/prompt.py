5633b022962353c469a6df75cc4beb32c8658991
Cissy Yu <cissy@quill.org>
2020-02-11 11:36:48 -0500

    added all changes to structure for rule pass functions

--- services/comprehension/main-api/comprehension/models/prompt.py
+++ services/comprehension/main-api/comprehension/models/prompt.py
@@ -6,7 +6,6 @@ from .ml_model import MLModel
 from ..exceptions import ComprehensionException
 from ..utils import combine_labels
 
-
 CORRECT_FEEDBACK_OBJ = {
                           'feedback': 'All rules-based checks passed!',
                           'optimal': True
@@ -42,43 +41,13 @@ class Prompt(TimestampedModel):
                      order_by('priority').all())
 
         for rule_set in rule_sets:
-            rules_list = rule_set.rules.all()
-            if rule_set.match == 'any':
-                is_passing = self._process_match_all(rules_list, entry)
-            elif rule_set.match == 'all':
-                is_passing = self._process_match_any(rules_list,
-                                                     entry)
-            else:
-                is_passing = True
-
+            is_passing = rule_set.process_rule_set(entry)
             if not is_passing:
                 feedback = INCORRECT_FEEDBACK_OBJ
                 feedback.update(feedback=rule_set.feedback)
                 return feedback
 
         return CORRECT_FEEDBACK_OBJ
-
-    def _process_match_any(self, rules_list, entry):
-        # match 'any' returns CORRECT at the first match it finds
-        if not rules_list:
-            return True
-
-        for rule in rules_list:
-            if rule.match(entry):
-                break
-        else:
-            return False
-
-        return True
-
-    def _process_match_all(self, rules_list, entry):
-        # match 'all' returns CORRECT after checking the whole list
-        # and verifying all items are correct
-        for rule in rules_list:
-            if not rule.match(entry):
-                return False
-
-        return True
 
     def fetch_auto_ml_feedback(self, entry, previous_feedback=[],
                                multi_label=True):
---
+++ services/comprehension/main-api/comprehension/tests/models/test_rule_set.py
@@ -0,0 +1,112 @@
+from unittest.mock import patch
+
+from django.core.exceptions import ValidationError
+from django.test import TestCase
+
+from ..factories.ml_feedback import MLFeedbackFactory
+from ..factories.ml_model import MLModelFactory
+from ..factories.prompt import PromptFactory
+from ..factories.rule_set import RuleSetFactory
+from ..factories.rule import RuleFactory
+from ...models.ml_model import MLModel
+from ...models.prompt import Prompt
+from ...models.rule_set import RuleSet
+from ...utils import combine_labels
+
+
+class RuleSetModelTest(TestCase):
+    def setUp(self):
+        self.rule_set = RuleSetFactory()
+        self.prompt = PromptFactory()
+
+
+class RuleSetValidationTest(RuleSetModelTest):
+    def test_name_not_nullable(self):
+        self.rule_set.name = None
+        with self.assertRaises(ValidationError):
+            self.rule_set.save()
+
+    def test_feedback_not_nullable(self):
+        self.rule_set.feedback = None
+        with self.assertRaises(ValidationError):
+            self.rule_set.save()
+
+    def test_priority_unique_on_prompt_id(self):
+        self.rule_set_duplicate = RuleSet(prompt=self.rule_set.prompt,
+                                          name="duplicate",
+                                          pass_order=self.rule_set.pass_order,
+                                          feedback="duplicate",
+                                          priority=self.rule_set.priority)
+        with self.assertRaises(ValidationError):
+            self.rule_set_duplicate.save()
+
+    def test_pass_order_validation(self):
+        self.rule_set.pass_order = 'DEFINITELY NOT A VALID FLAG'
+        with self.assertRaises(ValidationError):
+            self.rule_set.save()
+
+
+class RuleSetFunctionTest(RuleSetModelTest):
+    def test_first_pass(self):
+        rule_set = (RuleSetFactory(
+                    feedback='Test feedback',
+                    pass_order=RuleSet.PASS_ORDER.FIRST,
+                    prompt=self.prompt))
+        RuleFactory(regex_text='^test', rule_set=rule_set)
+        result = rule_set.process_rule_set('incorrect test correct')
+        self.assertFalse(result)
+        result = rule_set.process_rule_set('test correct')
+        self.assertTrue(result)
+
+    def test_second_pass(self):
+        rule_set = (RuleSetFactory(feedback='Test feedback',
+                                   pass_order=RuleSet.PASS_ORDER.SECOND,
+                                   prompt=self.prompt))
+        RuleFactory(regex_text='^test', rule_set=rule_set)
+        result = rule_set.process_rule_set('incorrect test correct')
+        self.assertFalse(result)
+        result = rule_set.process_rule_set('test correct')
+        self.assertTrue(result)
+
+    def test_match_all_regex(self):
+        prompt = PromptFactory()
+        rule_set = (RuleSetFactory(
+                    feedback='Test feedback',
+                    pass_order=RuleSet.PASS_ORDER.FIRST,
+                    prompt=prompt,
+                    match='all'))
+        RuleFactory(regex_text='incorrect sequence', rule_set=rule_set)
+        result = rule_set.process_rule_set('test incorrect sequence')
+        self.assertFalse(result)
+        result = rule_set.process_rule_set('this is a perfect test')
+        self.assertTrue(result)
+
+    def test_match_any_regex(self):
+        rule_set = (RuleSetFactory(
+                    feedback='Test feedback',
+                    pass_order=RuleSet.PASS_ORDER.FIRST,
+                    prompt=self.prompt,
+                    match='any'))
+        RuleFactory(regex_text='^test', rule_set=rule_set)
+        result = rule_set.process_rule_set('wrong sequence test')
+        self.assertFalse(result)
+        result = rule_set.process_rule_set('test correct sequence test')
+        self.assertTrue(result)
+
+    def test_two_rules_in_rule_set(self):
+        rule_set = (RuleSetFactory(
+                    feedback='Test feedback',
+                    pass_order=RuleSet.PASS_ORDER.FIRST,
+                    prompt=self.prompt,
+                    match='any'))
+
+        RuleFactory(regex_text='^test', rule_set=rule_set)
+        RuleFactory(regex_text='teeest$', rule_set=rule_set)
+
+        result_one = rule_set.process_rule_set('test correct teest')
+        result_two = rule_set.process_rule_set('test teeest')
+        result_three = rule_set.process_rule_set('teest test incorrect')
+
+        self.assertTrue(result_one)
+        self.assertTrue(result_two)
+        self.assertFalse(result_three)
