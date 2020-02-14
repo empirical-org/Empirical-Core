8c1fb3c39c70405856356f7158d9395cd00a8d83
Haronil Estevez <haronil@quill.org>
2020-02-14 06:49:03 -0500

    check that user is a staff user before redirecting to any admin child endpoint

--- services/QuillDiagnostic/app/routers/Admin/index.js
+++ services/QuillDiagnostic/app/routers/Admin/index.js
@@ -5,45 +5,44 @@ const newSessionEndpoint = `${process.env.EMPIRICAL_BASE_URL}/session/new`;
 
 export default {
   path: 'admin',
-  indexRoute: { onEnter: (nextState, replace) =>
-    fetch(usersEndpoint, {
-      method: 'GET',
-      mode: 'cors',
-      credentials: 'include',
-    }).then((response) => {
-      if (!response.ok) {
-        throw Error(response.statusText);
-      }
-      return response.json();
-    }).then((response) => {
-        if (response.user === null || (response.user.hasOwnProperty('role') && response.user.role !== 'staff')){
-         window.location = newSessionEndpoint;
-        }
-        else {
-         if (response.user !== null && (response.user.hasOwnProperty('role') && response.user.role === 'staff')){
-          window.location = '/#/admin/question-health';
-          }
-        }
-    }).catch((error) => {
-      // to do, use Sentry to capture error
-    })},
+  indexRoute: { onEnter: (nextState, replace) => replace('/admin/question-health'), },
   getChildRoutes: (partialNextState, cb) => {
-    Promise.all([
-      import(/* webpackChunkName: "admin-concept_feedback" */ './routes/ConceptFeedback/index.js'),
-      import(/* webpackChunkName: "admin-concepts" */ './routes/Concepts/index.js'),
-      import(/* webpackChunkName: "admin-dashboard" */ './routes/DataDash/index.js'),
-      import(/* webpackChunkName: "admin-question-health" */ './routes/QuestionHealth/index.js'),
-      import(/* webpackChunkName: "admin-title-cards" */ './routes/TitleCards/index.js'),
-      import(/* webpackChunkName: "admin-lessons" */ './routes/Lessons/index.js'),
-      import(/* webpackChunkName: "admin-questions" */ './routes/Questions/index.js'),
-      import(/* webpackChunkName: "admin-fill-in-the-blanks" */ './routes/FillInTheBlanks/index.js'),
-      import(/* webpackChunkName: "admin-sentence-fragments" */ './routes/SentenceFragments/index.js'),
-      import(/* webpackChunkName: "admin-item-levels" */ './routes/ItemLevels/index.js'),
-      import(/* webpackChunkName: "admin-item-levels" */ './routes/CloneConnectQuestions/index.js')
-    ])
-    .then(modules => cb(null, modules.map(module => module.default)))
-    // to do, use Sentry to capture error
-    // .catch(err => console.error('Dynamic page loading failed', err));
+    fetch(usersEndpoint, {
+        method: 'GET',
+        mode: 'cors',
+        credentials: 'include',
+      }).then((response) => {
+        if (!response.ok) {
+          throw Error(response.statusText);
+        }
+        return response.json();
+      }).then((response) => {
+          if (response.user === null || (response.user.hasOwnProperty('role') && response.user.role !== 'staff')){
+            window.location = newSessionEndpoint;
+          }
+          else {
+           if (response.user !== null && (response.user.hasOwnProperty('role') && response.user.role === 'staff')){
+            Promise.all([
+              import(/* webpackChunkName: "admin-concept_feedback" */ './routes/ConceptFeedback/index.js'),
+              import(/* webpackChunkName: "admin-concepts" */ './routes/Concepts/index.js'),
+              import(/* webpackChunkName: "admin-dashboard" */ './routes/DataDash/index.js'),
+              import(/* webpackChunkName: "admin-question-health" */ './routes/QuestionHealth/index.js'),
+              import(/* webpackChunkName: "admin-title-cards" */ './routes/TitleCards/index.js'),
+              import(/* webpackChunkName: "admin-lessons" */ './routes/Lessons/index.js'),
+              import(/* webpackChunkName: "admin-questions" */ './routes/Questions/index.js'),
+              import(/* webpackChunkName: "admin-fill-in-the-blanks" */ './routes/FillInTheBlanks/index.js'),
+              import(/* webpackChunkName: "admin-sentence-fragments" */ './routes/SentenceFragments/index.js'),
+              import(/* webpackChunkName: "admin-item-levels" */ './routes/ItemLevels/index.js'),
+              import(/* webpackChunkName: "admin-item-levels" */ './routes/CloneConnectQuestions/index.js')
+            ])
+            .then(modules => cb(null, modules.map(module => module.default)))
+            // to do, use Sentry to capture error
+            // .catch(err => console.error('Dynamic page loading failed', err));
+            }
+          }
+      }).catch((error) => {
+        // to do, use Sentry to capture error
+      })
   },
   getComponent(nextState, cb) {
     require.ensure([], (require) => {
