36d6e80048da9848b2e145262f63e66683f98aab
Emilia Friedberg <emiliafriedberg@gmail.com>
2020-02-20 15:24:26 -0500

    add more tests

--- services/QuillLMS/client/app/bundles/Teacher/components/student_profile/__tests__/student_profile_unit.test.jsx
+++ services/QuillLMS/client/app/bundles/Teacher/components/student_profile/__tests__/student_profile_unit.test.jsx
@@ -1,52 +1,30 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { mount } from 'enzyme';
 
 import StudentProfileUnit from '../student_profile_unit';
+import { DataTable } from 'quill-component-library/dist/componentLibrary'
+
+import { categorizedActivities } from './test_data'
 
 describe('StudentProfileUnit component', () => {
 
-  it('should render unit name', () => {
-    const wrapper = shallow(
+  it('should render', () => {
+    const wrapper = mount(
       <StudentProfileUnit
-        data={{complete: [{unit_name: 'Unit' }]}}
+        data={categorizedActivities}
         unitName="Unit"
       />
     );
     expect(wrapper.find('.unit-name').text()).toBe('Unit');
   });
 
-  it('should render "Completed Activities" and "Assigned Activities" <StudentProfileActivities /> components if incomplete and complete are defined', () => {
-    const wrapper = shallow(
+  it('should render a data table for both complete activities and incomplete activities', () => {
+    const wrapper = mount(
       <StudentProfileUnit
-        data={{
-          incomplete: [{unit_name: 'Incomplete' }, {unit_name: 'Incomplete' }, {unit_name: 'Incomplete' }],
-          complete: [{unit_name: 'Complete' }, {unit_name: 'Complete' }]
-        }}
+        data={categorizedActivities}
         unitName="Unit"
       />
     );
-  });
-
-  it('should render only "Completed Activities" if incomplete is not defined', () => {
-    const wrapper = shallow(
-      <StudentProfileUnit
-        data={{
-          complete: [{unit_name: 'Complete' }, {unit_name: 'Complete' }]
-        }}
-        unitName="Complete"
-      />
-    );
-  });
-
-  it('should render only "Assigned Activities" if complete is not defined', () => {
-    const wrapper = shallow(
-      <StudentProfileUnit
-        data={{
-          incomplete: [{unit_name: 'Incomplete' }, {unit_name: 'Incomplete' }, {unit_name: 'Incomplete' }],
-        }}
-        unitName="Incomplete"
-      />
-    );
-  });
-
+    expect(wrapper.find(DataTable).length).toBe(Object.keys(categorizedActivities).length);
+  })
 });
