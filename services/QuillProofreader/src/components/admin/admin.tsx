c75dfc07e519d8a02b4aaccc95e71024966a23ff
Haronil Estevez <haronil@quill.org>
2020-02-20 22:23:46 -0500

    add userData state and async fetchUser function for testing authenticated staff user

--- services/QuillProofreader/src/components/admin/admin.tsx
+++ services/QuillProofreader/src/components/admin/admin.tsx
@@ -25,57 +25,59 @@ class AdminContainer extends React.Component<AdminContainerProps> {
   }
 
   componentWillMount() {
+    this.fetchUser().then(userData => this.setState({ userData }));
     this.props.dispatch(proofreaderActivitiesActions.startListeningToActivities());
     this.props.dispatch(conceptsActions.startListeningToConcepts());
+    this.state = { userData: '' }
+  }
+
+  async fetchUser() {
+    return fetch(usersEndpoint, {
+      method: 'GET',
+      mode: 'cors',
+      credentials: 'include',
+    }).then((response) => {
+    if (!response.ok) {
+      throw Error(response.statusText);
+    }
+    return response.json();
+    });
   }
 
   render() {
-    fetch(usersEndpoint, {
-        method: 'GET',
-        mode: 'cors',
-        credentials: 'include',
-      }).then((response) => {
-        if (!response.ok) {
-          throw Error(response.statusText);
-        }
-        return response.json();
-      }).then((response) => {
-          if (response.user === null || (response.user.hasOwnProperty('role') && response.user.role !== 'staff')){
-            window.location = newSessionEndpoint;
-          }
-          else {
-            return (
-              <div style={{ display: 'flex', backgroundColor: "white", height: '100vw' }}>
-                <section className="section is-fullheight" style={{ display: 'flex', flexDirection: 'row', paddingTop: 0, paddingBottom: 0, }}>
-                  <aside className="menu" style={{ minWidth: 220, borderRight: '1px solid #e3e3e3', padding: 15, paddingLeft: 0, }}>
-                    <p className="menu-label">
-                      General
-                    </p>
-                    <ul className="menu-list">
-                      <TabLink activeClassName="is-active" to={'/admin/lessons'}>Proofreader Activities</TabLink>
-                    </ul>
-                    <p className="menu-label">
-                      Supporting
-                    </p>
-                    <ul className="menu-list">
-                      <TabLink activeClassName="is-active" to={'/admin/concepts'}>Concepts</TabLink>
-                    </ul>
-                  </aside>
-                  <div className="admin-container">
-                    {this.props.children}
-                  </div>
-                </section>
-                <Switch>
-                  <Route component={Lesson} path={`/admin/lessons/:lessonID`} />
-                  <Route component={Lessons} path={`/admin/lessons`} />
-                  <Route component={Concepts} path={`/admin/concepts`} />
-                </Switch>
-              </div>
-            );
-          }
-      }).catch((error) => {
-        // to do, use Sentry to capture error
-      })
+    if (this.state.userData.user === null || (this.state.userData.hasOwnProperty('role') && this.state.userData.user.role !== 'staff')){
+      window.location = newSessionEndpoint;
+    }
+    else {
+      return (
+        <div style={{ display: 'flex', backgroundColor: "white", height: '100vw' }}>
+          <section className="section is-fullheight" style={{ display: 'flex', flexDirection: 'row', paddingTop: 0, paddingBottom: 0, }}>
+            <aside className="menu" style={{ minWidth: 220, borderRight: '1px solid #e3e3e3', padding: 15, paddingLeft: 0, }}>
+              <p className="menu-label">
+                General
+              </p>
+              <ul className="menu-list">
+                <TabLink activeClassName="is-active" to={'/admin/lessons'}>Proofreader Activities</TabLink>
+              </ul>
+              <p className="menu-label">
+                Supporting
+              </p>
+              <ul className="menu-list">
+                <TabLink activeClassName="is-active" to={'/admin/concepts'}>Concepts</TabLink>
+              </ul>
+            </aside>
+            <div className="admin-container">
+              {this.props.children}
+            </div>
+          </section>
+          <Switch>
+            <Route component={Lesson} path={`/admin/lessons/:lessonID`} />
+            <Route component={Lessons} path={`/admin/lessons`} />
+            <Route component={Concepts} path={`/admin/concepts`} />
+          </Switch>
+        </div>
+      );
+    }
   }
 }
 
