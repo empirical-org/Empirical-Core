31e954c0342b096122775084e39e77f121fa2825
Eric Adams <erictayloradams@gmail.com>
2020-02-25 18:38:42 -0500

    remove locally changed files

--- packages/quill-component-library/src/components/shared/progressBar.tsx
+++ packages/quill-component-library/src/components/shared/progressBar.tsx
@@ -4,13 +4,13 @@ interface ProgressBarProps {
   percent: number;
   answeredQuestionCount: number;
   questionCount: number;
-  label: string;
+  thingsCompleted: string;
 }
 
-const ProgressBar = ({ percent, answeredQuestionCount, questionCount, label, }: ProgressBarProps) => (
+const ProgressBar = ({ percent, answeredQuestionCount, questionCount, thingsCompleted, }: ProgressBarProps) => (
   <div className="progress-bar-container">
     <progress className="progress activity-progress" max="100" value={percent} />
-    <p>{answeredQuestionCount} of {questionCount} {label}</p>
+    <p>{answeredQuestionCount} of {questionCount} {thingsCompleted} completed</p>
   </div>)
 
 export { ProgressBar }
--- services/QuillDiagnostic/app/libs/calculateProgress.ts
+++ services/QuillDiagnostic/app/libs/calculateProgress.ts
@@ -1,19 +1,16 @@
-import playDiagnostic from '../interfaces/playDiagnostic.ts';
-import Question from '../interfaces/question.ts';
+const removeTitleCardQuestions = (questions) => questions.filter(q => q.type !== 'TL')
 
-const removeTitleCardQuestions = (questions: Array<Question>) => questions.filter(q => q.type !== 'TL')
-
-export const questionCount = (playDiagnostic: playDiagnostic) => {
+export const questionCount = (playDiagnostic) => {
   return removeTitleCardQuestions(playDiagnostic.questionSet).length
 }
 
-const unansweredQuestionCount = (playDiagnostic: playDiagnostic) => {
+const unansweredQuestionCount = (playDiagnostic) => {
   return removeTitleCardQuestions(playDiagnostic.unansweredQuestions).length
 }
 
-export const answeredQuestionCount = (playDiagnostic: playDiagnostic) => questionCount(playDiagnostic) - unansweredQuestionCount(playDiagnostic);
+export const answeredQuestionCount = (playDiagnostic) => questionCount(playDiagnostic) - unansweredQuestionCount(playDiagnostic) - 1
 
-export const getProgressPercent = (playDiagnostic: playDiagnostic) => {
+export const getProgressPercent = (playDiagnostic) => {
   let percent;
   if (playDiagnostic && playDiagnostic.unansweredQuestions && playDiagnostic.questionSet) {
     percent = ((answeredQuestionCount(playDiagnostic)) / questionCount(playDiagnostic)) * 100
