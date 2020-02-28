209d161bfba8270b340b43e006d9f5ad8f220940
Emilia Friedberg <emiliafriedberg@gmail.com>
2020-02-28 10:32:03 -0500

    new student dashboard (#5936)
    * tabs - wip
    
    * empty state
    
    * get to-do activity tables rendering
    
    * patch qcl
    
    * patch qcl
    
    * install new qcl
    
    * styling changes
    
    * patch qcl
    
    * install new qcl
    
    * wip
    
    * patch qcl
    
    * whyyy
    
    * wip
    
    * get activities rendering correctly on mobile
    
    * add launched lesson view
    
    * fix tests
    
    * add tests for student profile units
    
    * add more tests
    
    * final css change
    
    * pinned activity updates
    
    * odds and ends
    
    * update alt text
    
    * update tests
    
    * revisions
    
    * patch qcl
    
    * update qcl version
    
    * final revisions
    
    * handle case where there is no next activity session
    
    * put back a bunch of scss i inadvertently deleted
    
    * whitespace commit
    
    * update jest tests
    
    * try updating circleci to run jest tests from the main directory
    
    * revisions
    
    * student results page (#5963)
    
    * new student results page
    
    * add new rspec tests
    
    * update tests
    
    * add skip to main content button (#5971)
    
    * edited copy
    
    * get skip-main button working
    
    * move skip to main styles to quill-component-library
    
    * patch qcl
    
    * wip
    
    * set up skip to main content button in grammar
    
    * add skip to main content button to proofreader
    
    * change skip to main content button casing
    
    Co-authored-by: cissyxyu <57366100+cissyxyu@users.noreply.github.com>
    
    Co-authored-by: cissyxyu <57366100+cissyxyu@users.noreply.github.com>
    
    * revisions
    
    Co-authored-by: cissyxyu <57366100+cissyxyu@users.noreply.github.com>

---
+++ services/QuillLMS/client/app/bundles/Teacher/components/student_profile/student_profile_classwork_tabs.tsx
@@ -0,0 +1,52 @@
+import * as React from 'react'
+import { ALL_ACTIVITIES, TO_DO_ACTIVITIES, COMPLETED_ACTIVITIES, } from '../../../../constants/student_profile'
+
+const BREAKPOINT = 682
+
+interface StudentProfileClassworkTabsProps {
+  onClickTab: (string: string) => void;
+  activeClassworkTab: string;
+}
+
+export default class StudentProfileClassworkTabs extends React.Component<StudentProfileClassworkTabsProps, {}> {
+  text = (string) => {
+    if (window.innerWidth > BREAKPOINT) { return string }
+
+    return string.split(' ')[0]
+  }
+
+  handleAllActivitiesClick = () => {
+    const { onClickTab, } = this.props
+    onClickTab(ALL_ACTIVITIES)
+  }
+
+  handleToDoActivitiesClick = () => {
+    const { onClickTab, } = this.props
+    onClickTab(TO_DO_ACTIVITIES)
+  }
+
+  handleCompletedActivitiesClick = () => {
+    const { onClickTab, } = this.props
+    onClickTab(COMPLETED_ACTIVITIES)
+  }
+
+  render() {
+    const { activeClassworkTab, } = this.props
+
+    const allActivitiesTabClassName = activeClassworkTab === ALL_ACTIVITIES ? "quill-tab active" : "quill-tab"
+    const toDoActivitiesTabClassName = activeClassworkTab === TO_DO_ACTIVITIES ? "quill-tab active" : "quill-tab"
+    const completedActivitiesTabClassName = activeClassworkTab === COMPLETED_ACTIVITIES ? "quill-tab active" : "quill-tab"
+
+    return (
+      <div className="student-profile-tab-container">
+        <div className="container">
+          <div className="student-profile-tabs">
+            <button className={allActivitiesTabClassName} onClick={this.handleAllActivitiesClick} type="button">{this.text(ALL_ACTIVITIES)}</button>
+            <button className={toDoActivitiesTabClassName} onClick={this.handleToDoActivitiesClick} type="button">{this.text(TO_DO_ACTIVITIES)}</button>
+            <button className={completedActivitiesTabClassName} onClick={this.handleCompletedActivitiesClick} type="button">{this.text(COMPLETED_ACTIVITIES)}</button>
+          </div>
+        </div>
+      </div>
+    );
+  };
+}
