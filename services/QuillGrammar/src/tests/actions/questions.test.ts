04bdafcd61d31ccccb94f34469dfb7e68ef00006
Anathomical <thomas.robertson@quill.org>
2020-03-11 16:55:15 -0400

    Grammar/migrate from firebase (#6061)
    * Add some new reducers for more flexible data handling
    
    This lets us add and remove individual questions to the question store
    
    * Add new libs for LMS-based Question API
    
    * Update actions to use LMS API instead of Firebase
    
    * Update components to dispatch all actions
    
    Also make some updates to please the React linter
    
    * Update LMS to properly import Grammar questions from Firebase
    
    Just a tweak to the Firebase import rake task since we need it to take an additional argument now
    
    * Add basic tests for Question actions that go to the API
    
    * Add tests for the QuestionApis
    
    * Rename request mock since it was overriding the request lib
    
    Apparently jest will allow you to create mocks that globally override un-pathed libs (stuff installed via npm).  So if the mock file has the same name as the lib, it'll automatically override, and we don't want that here.

---
+++ services/QuillGrammar/src/tests/actions/questions.test.ts
@@ -0,0 +1,147 @@
+import { mockQuestionApi, } from '../__mocks__/question_api'
+import { mockFocusPointApi, } from '../__mocks__/focus_point_api'
+import { mockIncorrectSequenceApi, } from '../__mocks__/incorrect_sequence_api'
+jest.mock('../../libs/questions_api', () => ({
+  FocusPointApi: mockFocusPointApi,
+  IncorrectSequenceApi: mockIncorrectSequenceApi,
+  QuestionApi: mockQuestionApi,
+}))
+
+import { mockDispatch as dispatch, } from '../__mocks__/dispatch'
+
+import {
+  GRAMMAR_QUESTION_TYPE
+} from '../../libs/questions_api'
+
+import {
+  startListeningToQuestions,
+  getQuestion,
+  updateFlag,
+  submitNewQuestion,
+  submitQuestionEdit,
+  submitNewIncorrectSequence,
+  submitEditedIncorrectSequence,
+  deleteIncorrectSequence,
+  updateIncorrectSequences,
+  submitNewFocusPoint,
+  submitEditedFocusPoint,
+  submitBatchEditedFocusPoint,
+  deleteFocusPoint,
+} from '../../actions/questions'
+
+describe('Questions actions', () => {
+  describe('startListeningToQuestions', () => {
+    it('should call QuestionApi.getAll()', () => {
+      dispatch(startListeningToQuestions())
+      expect(mockQuestionApi.getAll).toHaveBeenLastCalledWith(GRAMMAR_QUESTION_TYPE)
+    })
+  })
+
+  describe('getQuestion', () => {
+    it('should call QuestionApi.get()', () => {
+      const MOCK_ID = 1
+      dispatch(getQuestion(MOCK_ID))
+      expect(mockQuestionApi.get).toHaveBeenLastCalledWith(MOCK_ID)
+    })
+  })
+
+  describe('updateFlag', () => {
+    it('should call QuestionApi.updateFlag()', () => {
+      const MOCK_ID = 1
+      const MOCK_FLAG = 'FLAG'
+      dispatch(updateFlag(MOCK_ID, MOCK_FLAG))
+      expect(mockQuestionApi.updateFlag).toHaveBeenLastCalledWith(MOCK_ID, MOCK_FLAG)
+    })
+  })
+
+  describe('submitNewQuestion', () => {
+    it('should call QuestionApi.getAll()', () => {
+      const MOCK_CONTENT = { mock: 'content', answers: [] }
+      dispatch(submitNewQuestion(MOCK_CONTENT))
+      expect(mockQuestionApi.create).toHaveBeenLastCalledWith(GRAMMAR_QUESTION_TYPE, MOCK_CONTENT)
+    })
+  })
+
+  describe('submitQuestionEdit', () => {
+    it('should call QuestionApi.update()', () => {
+      const MOCK_ID = 1
+      const MOCK_CONTENT = { mock: 'content', answers: [] }
+      dispatch(submitQuestionEdit(MOCK_ID, MOCK_CONTENT))
+      expect(mockQuestionApi.update).toHaveBeenLastCalledWith(MOCK_ID, MOCK_CONTENT)
+    })
+  })
+
+  describe('submitNewIncorrectSequence', () => {
+    it('should call IncorrectSequenceApi.create()', () => {
+      const MOCK_ID = 1
+      const MOCK_CONTENT = { mock: 'content' }
+      dispatch(submitNewIncorrectSequence(MOCK_ID, MOCK_CONTENT))
+      expect(mockIncorrectSequenceApi.create).toHaveBeenLastCalledWith(MOCK_ID, MOCK_CONTENT)
+    })
+  })
+
+  describe('submitEditedIncorrectSequence', () => {
+    it('should call IncorrectSequenceApi.update()', () => {
+      const MOCK_ID = 1
+      const MOCK_CONTENT = { mock: 'content' }
+      const MOCK_IS_ID = 2
+      dispatch(submitEditedIncorrectSequence(MOCK_ID, MOCK_CONTENT, MOCK_IS_ID))
+      expect(mockIncorrectSequenceApi.update).toHaveBeenLastCalledWith(MOCK_ID, MOCK_IS_ID, MOCK_CONTENT)
+    })
+  })
+
+  describe('deleteIncorrectSequence', () => {
+    it('should call IncorrectSequenceApi.remove()', () => {
+      const MOCK_ID = 1
+      const MOCK_IS_ID = 2
+      dispatch(deleteIncorrectSequence(MOCK_ID, MOCK_IS_ID))
+      expect(mockIncorrectSequenceApi.remove).toHaveBeenLastCalledWith(MOCK_ID, MOCK_IS_ID)
+    })
+  })
+
+  describe('updateIncorrectSequences', () => {
+    it('should call IncorrectSequenceApi.updateAllForQuestion()', () => {
+      const MOCK_ID = 1
+      const MOCK_CONTENT = { mock: 'content' }
+      dispatch(updateIncorrectSequences(MOCK_ID, MOCK_CONTENT))
+      expect(mockIncorrectSequenceApi.updateAllForQuestion).toHaveBeenLastCalledWith(MOCK_ID, MOCK_CONTENT)
+    })
+  })
+
+  describe('submitNewFocusPoint', () => {
+    it('should call FocusPointApi.create()', () => {
+      const MOCK_ID = 1
+      const MOCK_CONTENT = { mock: 'content' }
+      dispatch(submitNewFocusPoint(MOCK_ID, MOCK_CONTENT))
+      expect(mockFocusPointApi.create).toHaveBeenLastCalledWith(MOCK_ID, MOCK_CONTENT)
+    })
+  })
+
+  describe('submitEditedFocusPoint', () => {
+    it('should call FocusPointApi.update()', () => {
+      const MOCK_ID = 1
+      const MOCK_CONTENT = { mock: 'content' }
+      const MOCK_IS_ID = 2
+      dispatch(submitEditedFocusPoint(MOCK_ID, MOCK_CONTENT, MOCK_IS_ID))
+      expect(mockFocusPointApi.update).toHaveBeenLastCalledWith(MOCK_ID, MOCK_IS_ID, MOCK_CONTENT)
+    })
+  })
+
+  describe('deleteFocusPoint', () => {
+    it('should call FocusPointApi.remove()', () => {
+      const MOCK_ID = 1
+      const MOCK_IS_ID = 2
+      dispatch(deleteFocusPoint(MOCK_ID, MOCK_IS_ID))
+      expect(mockFocusPointApi.remove).toHaveBeenLastCalledWith(MOCK_ID, MOCK_IS_ID)
+    })
+  })
+
+  describe('submitBatchEditedFocusPoint', () => {
+    it('should call FocusPointApi.updateAllForQuestion()', () => {
+      const MOCK_ID = 1
+      const MOCK_CONTENT = { mock: 'content' }
+      dispatch(submitBatchEditedFocusPoint(MOCK_ID, MOCK_CONTENT))
+      expect(mockFocusPointApi.updateAllForQuestion).toHaveBeenLastCalledWith(MOCK_ID, MOCK_CONTENT)
+    })
+  })
+})
