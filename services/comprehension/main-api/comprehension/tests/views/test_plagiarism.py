4658cb6ca167554a4b8eb5d7ffd97f97949727fb
Cissy Yu <cissy@quill.org>
2020-03-11 15:59:14 -0400

    incorporated thomas's suggestions

--- services/comprehension/main-api/comprehension/tests/views/test_plagiarism.py
+++ services/comprehension/main-api/comprehension/tests/views/test_plagiarism.py
@@ -9,16 +9,17 @@ from ..factories.activity import ActivityFactory
 from ..factories.activity_prompt import ActivityPromptFactory
 from ..factories.activity_passage import ActivityPassageFactory
 from ...views.plagiarism import PlagiarismFeedbackView
-from ...views.plagiarism import (FEEDBACK_TYPE, FEEDBACK_ID, CORRECT_FEEDBACK,
-                                 INCORRECT_FEEDBACK)
+from ...views.plagiarism import (FEEDBACK_TYPE, FEEDBACK_ID, CORRECT_FEEDBACK)
 from ...utils import construct_feedback_payload
-from ...lib import test_constants
+
+
+PASSAGE_TEXT = "This is a test passage. \n\nPlease do not plagiarize this, students!"
 
 
 class TestPlagiarismFeedbackView(TestCase):
     def setUp(self):
         self.factory = RequestFactory()
-        self.passage = PassageFactory(text=test_constants.PASSAGE_TEXT)
+        self.passage = PassageFactory(text=PASSAGE_TEXT)
         self.prompt = PromptFactory()
         self.activity = ActivityFactory()
         self.activity_prompt = ActivityPromptFactory(activity=self.activity,
@@ -27,6 +28,7 @@ class TestPlagiarismFeedbackView(TestCase):
         self.activity_passage = ActivityPassageFactory(activity=self.activity,
                                                        passage=self.passage,
                                                        order=1)
+        self.plagView = PlagiarismFeedbackView()
 
     def test_get_plagiarism_feedback_404(self):
         request_body = {
@@ -40,7 +42,7 @@ class TestPlagiarismFeedbackView(TestCase):
         with self.assertRaises(Http404):
             PlagiarismFeedbackView.as_view()(request)
 
-    def test_get_not_plagiarized_feedback(self):
+    def test_plagiarism_integration(self):
         request_body = {
             'prompt_id': self.prompt.id,
             'entry': 'This is a totally original sentence, not plagiarized.'
@@ -62,24 +64,22 @@ class TestPlagiarismFeedbackView(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertEqual(json_body, expected)
 
-    def test_get_plagiarized_feedback(self):
-        request_body = {
-            'prompt_id': self.prompt.id,
-            'entry': 'poor Bolivian factory worker who'
-        }
-        request = self.factory.post('/feedback/plagiarism',
-                                    data=json.dumps(request_body),
-                                    content_type='application/json')
+    def test_is_not_plagiarized(self):
+        entry = 'This is a totally original sentence, not plagiarized.'
+        self.assertFalse(self.plagView._check_is_plagiarism(entry, self.passage))
 
-        response = PlagiarismFeedbackView().post(request)
-        json_body = json.loads(response.content)
+    def test_is_plagiarized(self):
+        entry = 'Please do not plagiarize this'
+        self.assertTrue(self.plagView._check_is_plagiarism(entry, self.passage))
 
-        expected = construct_feedback_payload(
-                      INCORRECT_FEEDBACK,
-                      FEEDBACK_TYPE,
-                      False,
-                      FEEDBACK_ID
-                    )
+    def test_is_plagiarized_with_punctuation(self):
+        entry = 'Please, do not!! plagiarize this? students'
+        self.assertTrue(self.plagView._check_is_plagiarism(entry, self.passage))
 
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(json_body, expected)
+    def test_is_plagiarized_with_capitalization(self):
+        entry = 'please DO NOT plagiariZE THis'
+        self.assertTrue(self.plagView._check_is_plagiarism(entry, self.passage))
+
+    def test_is_plagiarized_across_paragraphs(self):
+        entry = 'test passage please DO NOT yada yada'
+        self.assertTrue(self.plagView._check_is_plagiarism(entry, self.passage))
