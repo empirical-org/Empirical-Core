9109f1d1384325ea0e78e038156228d9931c82f9
Eric Adams <erictayloradams@gmail.com>
2020-03-12 17:34:57 -0500

    fix spacing

--- services/QuillLMS/client/app/bundles/Teacher/components/progress_reports/diagnostic_reports/student_report.tsx
+++ services/QuillLMS/client/app/bundles/Teacher/components/progress_reports/diagnostic_reports/student_report.tsx
@@ -9,72 +9,72 @@ import QuestionData from '../../../../interfaces/questionData';
 import { requestGet } from '../../../../../modules/request/index.js';
 
 export interface StudentReportState {
-	loading: boolean,
-	students: Student[],
+  loading: boolean,
+  students: Student[],
 }
 
 export class StudentReport extends React.Component<RouteComponentProps, StudentReportState> {
 
-	state = {
-		loading: true,
-		students: null
-	};
+  state = {
+    loading: true,
+    students: null
+  };
 
-	componentDidMount() {
-		const { params } = this.props;
-		this.getStudentData(params);
-	}
+  componentDidMount() {
+    const { params } = this.props;
+    this.getStudentData(params);
+  }
 
-	componentWillReceiveProps(nextProps: RouteComponentProps) {
-		const { params } = nextProps;
-		this.setState({ loading: true });
-		this.getStudentData(params);
-	}
+  componentWillReceiveProps(nextProps: RouteComponentProps) {
+    const { params } = nextProps;
+    this.setState({ loading: true });
+    this.getStudentData(params);
+  }
 
-	selectedStudent = (students: Student[]) => {
-		const { params } = this.props;
-		const { studentId } = params;
-		return studentId ? students.find((student: Student) => student.id === parseInt(studentId)) : students[0];
-	}
+  selectedStudent = (students: Student[]) => {
+    const { params } = this.props;
+    const { studentId } = params;
+      return studentId ? students.find((student: Student) => student.id === parseInt(studentId)) : students[0];
+  }
 
-	getStudentData = (params: RouteComponentProps['params']) => {
-		requestGet(`/teachers/progress_reports/students_by_classroom/u/${params.unitId}/a/${params.activityId}/c/${params.classroomId}`, (data: { students: Student[] }) => {
-			const { students } = data;
-			this.setState({ students, loading: false });
-		});
-	}
+  getStudentData = (params: RouteComponentProps['params']) => {
+    requestGet(`/teachers/progress_reports/students_by_classroom/u/${params.unitId}/a/${params.activityId}/c/${params.classroomId}`, (data: { students: Student[] }) => {
+      const { students } = data;
+      this.setState({ students, loading: false });
+    });
+  }
 
-	studentBoxes = (students: Student[]) => {
-		const studentData = this.selectedStudent(students);
-		const concept_results = _.sortBy(studentData.concept_results, 'question_number')
-		return concept_results.map((question: QuestionData, index: number) => {
-			if (studentData.activity_classification === 'connect' || studentData.activity_classification === 'sentence') {
-				return <ConnectStudentReportBox boxNumber={index + 1} key={index} questionData={question} />
-			}
-			return <StudentReportBox boxNumber={index + 1} key={index} questionData={question} />
-		})
-	}
+  studentBoxes = (students: Student[]) => {
+    const studentData = this.selectedStudent(students);
+    const concept_results = _.sortBy(studentData.concept_results, 'question_number')
+    return concept_results.map((question: QuestionData, index: number) => {
+      if (studentData.activity_classification === 'connect' || studentData.activity_classification === 'sentence') {
+        return <ConnectStudentReportBox boxNumber={index + 1} key={index} questionData={question} />
+      }
+      return <StudentReportBox boxNumber={index + 1} key={index} questionData={question} />
+    })
+  }
 
-	render() {
-		const { loading, students } = this.state;
-		if (loading) {
-			return <LoadingSpinner />
-		} else {
-			const student = this.selectedStudent(students);
-			const { name, score } = student;
-			return (
-				<div className='individual-student-activity-view'>
-					<h3 className='activity-view-header'>{name}  <strong className='activity-view-score'>{score}%</strong></h3>
-					{this.studentBoxes(students)}
-					<div className='how-we-grade'>
-						<p className="title title-not-started pull-right">
-							<a href="https://support.quill.org/activities-implementation/how-does-grading-work" rel='noreferrer noopener' target="_blank" >How We Grade <i className="fas fa-long-arrow-alt-right" /></a>
-						</p>
-					</div>
-				</div>
-			);
-		}
-	}
+  render() {
+    const { loading, students } = this.state;
+    if (loading) {
+      return <LoadingSpinner />
+    } else {
+      const student = this.selectedStudent(students);
+      const { name, score } = student;
+      return (
+        <div className='individual-student-activity-view'>
+          <h3 className='activity-view-header'>{name}  <strong className='activity-view-score'>{score}%</strong></h3>
+          {this.studentBoxes(students)}
+          <div className='how-we-grade'>
+            <p className="title title-not-started pull-right">
+              <a href="https://support.quill.org/activities-implementation/how-does-grading-work" rel='noreferrer noopener' target="_blank" >How We Grade <i className="fas fa-long-arrow-alt-right" /></a>
+            </p>
+          </div>
+        </div>
+      );
+    }
+  }
 }
 
 export default StudentReport;
