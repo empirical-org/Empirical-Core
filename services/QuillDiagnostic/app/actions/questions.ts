3bd8561283405be2ab6c1342f9923230af8753bb
Eric Adams <erictayloradams@gmail.com>
2020-03-17 15:38:28 -0400

    update search to only trigger on submit

--- services/QuillDiagnostic/app/actions/questions.ts
+++ services/QuillDiagnostic/app/actions/questions.ts
@@ -421,10 +421,9 @@ function updatePageNumber(pageNumber, qid) {
   };
 }
 
-function updateStringFilter(stringFilter, qid) {
+function updateStringFilter(stringFilter) {
   return (dispatch) => {
     dispatch(setStringFilter(stringFilter));
-    dispatch(searchResponses(qid));
   };
 }
 
