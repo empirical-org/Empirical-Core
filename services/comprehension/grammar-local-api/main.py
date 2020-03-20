c4b0472115dc6ce3459169c988dd92cbd1f46db3
Anathomical <thomas.robertson@quill.org>
2020-03-19 21:02:01 -0400

    Process prompt_text + entry, but only return errors from entry (#6109)
    * Process prompt_text + entry, but only return errors from entry
    
    In order to validate things like capitalization and non-fragmentary sentences, we need to analyze complete sentences which can be obtained by combining the original prompt text with the student's entry.  However, we don't want to return errors that may be found in the prompt text because the student can't edit that part and could get stuck.
    
    * Fix flake8

--- services/comprehension/grammar-local-api/main.py
+++ services/comprehension/grammar-local-api/main.py
@@ -1,5 +1,5 @@
 from flask import jsonify, make_response
-from grammarcheck import GrammarChecker
+from grammarcheck import Error, GrammarChecker
 
 
 GRAMMAR_MODEL_PATH = "model/grammar/"
@@ -14,13 +14,14 @@ def check_grammar(request):
     request_json = request.get_json()
 
     entry = request_json.get('entry')
+    prompt_text = request_json.get('prompt_text', '')
     prompt_id = request_json.get('prompt_id')
 
     if entry is None or prompt_id is None:
         return make_response(jsonify(message="error"), 400)
 
-    # errors is a list of tuples (token, token character offset, error type)
-    errors = checker.check(entry)
+    errors = check_for_errors(entry, prompt_text)
+
     # convert errors to hashes fitting the API protocol for highlights
     highlights = list(map(lambda e: highlight_hash(e), errors))
 
@@ -31,6 +32,25 @@ def check_grammar(request):
                      "highlight": highlights}
 
     return make_response(jsonify(**response_data), 200)
+
+
+def check_for_errors(entry, prompt_text=''):
+    full_sentence = f'{prompt_text} {entry}'.strip()
+    # errors is a list of tuples (token, token character offset, error type)
+    errors = checker.check(full_sentence)
+
+    # If the error is in the stem, we don't care about it
+    entry_errors = list(filter(lambda e: (e.index > len(prompt_text) or
+                                          len(prompt_text) == 0),
+                               errors))
+
+    # Now we need to modify the highlight index by the length of the prompt
+    # text + 1 because we add a space
+    offset_adjusted_errors = [Error(text=e.text,
+                                    index=e.index - len(prompt_text) - 1,
+                                    type=e.type) for e in entry_errors]
+
+    return offset_adjusted_errors
 
 
 def highlight_hash(error):
