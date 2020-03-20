07d2054b09f5be24f1c234898578272b888513ca
Emilia Friedberg <emiliafriedberg@gmail.com>
2020-03-20 10:55:36 -0400

    run react-to-react-dom codemod on javascript files

--- services/QuillLMS/client/app/bundles/Teacher/components/modules/lesson_planner_state.jsx
+++ services/QuillLMS/client/app/bundles/Teacher/components/modules/lesson_planner_state.jsx
@@ -1,18 +1,16 @@
 'use strict'
 
- import React from 'react'
+export default  function () {
 
- export default  function () {
+ this.updateCreateUnit = function (state, hash) {
+   var newCU = _.extend({}, state.createUnit, hash)
+   state.createUnit = newCU;
+   return state
+ }
 
-  this.updateCreateUnit = function (state, hash) {
-    var newCU = _.extend({}, state.createUnit, hash)
-    state.createUnit = newCU;
-    return state
-  }
-
-  this.updateCreateUnitModel = function (state, hash) {
-    var newCUM = _.extend({}, state.createUnit.model, hash);
-    state.createUnit.model = newCUM;
-    return state;
-  }
+ this.updateCreateUnitModel = function (state, hash) {
+   var newCUM = _.extend({}, state.createUnit.model, hash);
+   state.createUnit.model = newCUM;
+   return state;
+ }
 }
