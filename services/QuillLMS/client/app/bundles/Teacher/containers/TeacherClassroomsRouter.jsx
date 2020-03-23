ff0841a8a37fd06cc0a347dfbb50a64233ba566a
Emilia Friedberg <emiliafriedberg@gmail.com>
2020-03-23 10:57:34 -0400

    get classes pages working

--- services/QuillLMS/client/app/bundles/Teacher/containers/TeacherClassroomsRouter.jsx
+++ services/QuillLMS/client/app/bundles/Teacher/containers/TeacherClassroomsRouter.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
-import { Router, Route, IndexRoute, browserHistory } from 'react-router'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import ActiveClassrooms from '../components/classrooms/active_classrooms.tsx'
 import ArchivedClassrooms from '../components/classrooms/archived_classrooms.tsx'
 
 const TeacherClassroomsRouter = props => (
-  <Router history={browserHistory} Router>
-    <Route path="/teachers/classrooms">
-      <IndexRoute component={() => <ActiveClassrooms {...props} />} />
-      <Route component={() => <ArchivedClassrooms {...props} />} path="archived" />
-    </Route>
+  <Router>
+    <Switch>
+      <Route component={() => <ArchivedClassrooms {...props} />} path="/teachers/classrooms/archived" />
+      <Route exact path="/teachers/classrooms" component={() => <ActiveClassrooms {...props} />} />
+    </Switch>
   </Router>
 )
 
