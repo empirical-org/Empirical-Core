2c36a05875954f4050e20bfbe4119fcf8253506c
Thomas Robertson <thomas.robertson@quill.org>
2020-03-24 18:19:37 -0400

    Add test cases for LessonApi controller
    Also modify a couple of additional tests to make a factory name make sense

---
+++ services/QuillLMS/spec/factories/lesson.rb
@@ -0,0 +1,31 @@
+FactoryBot.define do
+  data = {
+    flag: "archived",
+    introURL: "",
+    name: "Mr. Scibienski 1",
+    questions: [ {
+      key: "-KDvcYlMzXMGe2pZFkoe",
+      questionType: "questions"
+    }, {
+      key: "-KDxvlu4HqU0XbKdA-TO",
+      questionType: "questions"
+    }, {
+      key: "-KDyfJTLaU8PzZwoVuNl",
+      questionType: "questions"
+    }, {
+      key: "-KE7jdsVS1l5_6I7XyEc",
+      questionType: "questions"
+    }, {
+      key: "-KFBdHkF01cjnGn0MC4X",
+      questionType: "questions"
+    }, {
+      key: "-KDxw6oGJ9eYDVt-onRb",
+      questionType: "questions"
+    } ]
+  }
+
+  factory :lesson do
+    uid                   SecureRandom.uuid
+    data                  data
+  end
+end
