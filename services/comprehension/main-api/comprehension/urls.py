3a117a18f826d9628380d28f0160c7641df8b1e6
Anathomical <thomas.robertson@quill.org>
2020-03-25 14:22:54 -0400

    Add unstyled session feedback views (#6106)
    * Add unstyled session feedback views
    
    * Add staff_only to session views
    
    Also add test cases for that behavior
    
    * Add base admin styles to new session views
    
    Just to have some sort of default table styling instead of no styling
    
    * Tweak session views to include attempt count information
    
    * Tweak table data to match specifications
    
    And to add undefined feedback details such as label

--- services/comprehension/main-api/comprehension/urls.py
+++ services/comprehension/main-api/comprehension/urls.py
@@ -7,6 +7,10 @@ from .views.feedback_rules import RulesBasedFeedbackView
 from .views.ml_feedback import MLFeedbackView
 from .views.rule import RuleView
 from .views.plagiarism import PlagiarismFeedbackView
+from .views.session_feedback_history import SessionFeedbackHistoryView
+from .views.session_feedback_history_detail import (
+    SessionFeedbackHistoryDetailView
+)
 from .models.rule_set import RuleSet
 
 urlpatterns = [
@@ -26,5 +30,11 @@ urlpatterns = [
         RulesBasedFeedbackView.as_view(pass_order=RuleSet.PASS_ORDER.SECOND),
         name='get_rules_based_feedback_second_pass'),
     url(r'^feedback/plagiarism$', PlagiarismFeedbackView.as_view(),
-        name='get_plagiarism_feedback')
+        name='get_plagiarism_feedback'),
+    url(r'^session_history$',
+        SessionFeedbackHistoryView.as_view(),
+        name='get_session_feedback_history'),
+    url(r'^session_history/(?P<session_id>.*)$',
+        SessionFeedbackHistoryDetailView.as_view(),
+        name='get_session_feedback_history_detail'),
 ]
