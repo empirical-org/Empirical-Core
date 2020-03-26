1344b83d69e0224a8fb90b51ae312a3ae134fd45
Thomas Robertson <thomas.robertson@quill.org>
2020-03-26 12:01:11 -0400

    Refactor TitleCards to have a separate interfaces file

--- services/QuillConnect/app/libs/title_cards_api.ts
+++ services/QuillConnect/app/libs/title_cards_api.ts
@@ -1,32 +1,23 @@
 import { requestGet, requestPost, requestPut } from './request';
+import { TitleCard, TitleCards } from '../interfaces/title_cards';
 
 const titleCardApiBaseUrl = `${process.env.EMPIRICAL_BASE_URL}/api/v1/title_cards`;
 const titleCardType = 'connect_title_card';
 
-interface TitleCardProps {
-  uid: string;
-  content: string;
-  title: string;
-}
-
-interface TitleCardBatchProps {
-  title_cards: TitleCardProps[];
-}
-
 class TitleCardApi {
-  static getAll(): Promise<TitleCardBatchProps> {
+  static getAll(): Promise<TitleCards> {
     return requestGet(`${titleCardApiBaseUrl}.json?title_card_type=${titleCardType}`);
   }
 
-  static get(uid: string): Promise<TitleCardProps> {
+  static get(uid: string): Promise<TitleCard> {
     return requestGet(`${titleCardApiBaseUrl}/${uid}.json`);
   }
 
-  static create(data: TitleCardProps): Promise<TitleCardProps> {
+  static create(data: TitleCard): Promise<TitleCard> {
     return requestPost(`${titleCardApiBaseUrl}.json?title_card_type=${titleCardType}`, data);
   }
 
-  static update(uid: string, data: TitleCardProps): Promise<TitleCardProps> {
+  static update(uid: string, data: TitleCard): Promise<TitleCard> {
     return requestPut(`${titleCardApiBaseUrl}/${uid}.json`, data);
   }
 }
