28a906b22f19055e31b684bd11c97243f8a8189c
Thomas Robertson <thomas.robertson@quill.org>
2020-03-26 18:12:31 -0400

    Update Lesson model with a lesson_type
    This allows us to have both Connect and Diagnostic lessons

--- services/QuillLMS/app/models/lesson.rb
+++ services/QuillLMS/app/models/lesson.rb
@@ -1,14 +1,15 @@
 class Lesson < ActiveRecord::Base
+  TYPES = [
+    TYPE_CONNECT_LESSON = 'connect_lesson',
+    TYPE_DIAGNOSTIC_LESSON = 'diagnostic_lesson',
+  ]
   validates :data, presence: true
   validates :uid, presence: true, uniqueness: true
+  validates :lesson_type, presence: true, inclusion: {in: TYPES}
   validate :data_must_be_hash
 
   def as_json(options=nil)
     data
-  end
-
-  private def new_uuid
-    SecureRandom.uuid
   end
 
   private def data_must_be_hash
