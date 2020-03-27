c11b7843535656c2b48e3dd3ed22935411f9178d
Cissy Yu <cissy@quill.org>
2020-03-27 11:58:00 -0400

    all front end changes

--- packages/quill-component-library/src/components/feedback/conceptExplanation.tsx
+++ packages/quill-component-library/src/components/feedback/conceptExplanation.tsx
@@ -1,7 +1,16 @@
 import React from 'react'
 
+function getClassName(description, leftBox, rightBox) {
+  if (description && leftBox && rightBox) {
+    return "concept-explanation"
+  }
+  else {
+    return "concept-explanation empty"
+  }
+}
+
 const ConceptExplanation = ({ description, leftBox, rightBox, }) => (
-  <div className="concept-explanation">
+  <div className={getClassName(description, leftBox, rightBox)}>
     <div className="concept-explanation-title"><img alt="Light Bulb Icon" src={`${process.env.QUILL_CDN_URL}/images/icons/hint.svg`} /><span>Hint</span></div>
     <div className="concept-explanation-description" dangerouslySetInnerHTML={{__html: description}} />
     <div className="concept-explanation-see-write">
