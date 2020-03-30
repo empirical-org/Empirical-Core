bffdce468ab09799aaaa391399001bd302364968
Anathomical <thomas.robertson@quill.org>
2020-03-30 17:35:10 -0400

    Diagnostic/migrate firebase diagnostics collections (#6136)
    * Remove commented out obsolete code
    
    * Update Lesson model with a lesson_type
    
    This allows us to have both Connect and Diagnostic lessons
    
    * Update Lesson API to handle different types
    
    In prep for copying code to Diagnostic
    
    * Change lesson action file extension in prep for updates
    
    * Copy Lesson actions from Connect, tweak for Diagnostic
    
    * Tweak methodology for determining Question merging logic
    
    * Fix rubocop linting errors

--- services/QuillLMS/spec/controllers/api/v1/lessons_controller_spec.rb
+++ services/QuillLMS/spec/controllers/api/v1/lessons_controller_spec.rb
@@ -6,24 +6,24 @@ describe Api::V1::LessonsController, type: :controller do
 
   describe "#index" do
     it "should return a list of Lessons" do
-      get :index 
+      get :index, lesson_type: lesson.lesson_type 
       expect(JSON.parse(response.body).keys.length).to eq(1)
     end
 
     it "should include the response from the db" do
-      get :index
+      get :index, lesson_type: lesson.lesson_type 
       expect(JSON.parse(response.body).keys.first).to eq(lesson.uid)
     end
   end
 
   describe "#show" do
     it "should return the specified lesson" do
-      get :show, id: lesson.uid
+      get :show, lesson_type: lesson.lesson_type, id: lesson.uid
       expect(JSON.parse(response.body)).to eq(lesson.data)
     end
 
     it "should return a 404 if the requested Lesson is not found" do
-      get :show, id: 'doesnotexist'
+      get :show, lesson_type: lesson.lesson_type, id: 'doesnotexist'
       expect(response.status).to eq(404)
       expect(response.body).to include("The resource you were looking for does not exist")
     end
@@ -35,7 +35,7 @@ describe Api::V1::LessonsController, type: :controller do
       data = {foo: "bar"}
       expect(SecureRandom).to receive(:uuid).and_return(uuid)
       pre_create_count = Lesson.count
-      post :create, lesson: data
+      post :create, lesson_type: lesson.lesson_type, lesson: data
       expect(Lesson.count).to eq(pre_create_count + 1)
     end
   end
@@ -43,13 +43,13 @@ describe Api::V1::LessonsController, type: :controller do
   describe "#update" do
     it "should update the existing record" do
       data = {"foo" => "bar"}
-      put :update, id: lesson.uid, lesson: data
+      put :update, lesson_type: lesson.lesson_type, id: lesson.uid, lesson: data
       lesson.reload
       expect(lesson.data).to eq(data)
     end
 
     it "should return a 404 if the requested Lesson is not found" do
-      get :update, id: 'doesnotexist'
+      get :update, lesson_type: lesson.lesson_type, id: 'doesnotexist'
       expect(response.status).to eq(404)
       expect(response.body).to include("The resource you were looking for does not exist")
     end
