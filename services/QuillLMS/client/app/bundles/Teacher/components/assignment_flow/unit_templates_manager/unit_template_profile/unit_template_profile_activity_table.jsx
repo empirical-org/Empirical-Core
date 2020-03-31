801d06763cffb5b350f7d41825fbbde0f230deb7
Emilia Friedberg <emiliafriedberg@gmail.com>
2020-03-31 12:01:31 -0400

    fix dumb mistake

--- services/QuillLMS/client/app/bundles/Teacher/components/assignment_flow/unit_templates_manager/unit_template_profile/unit_template_profile_activity_table.jsx
+++ services/QuillLMS/client/app/bundles/Teacher/components/assignment_flow/unit_templates_manager/unit_template_profile/unit_template_profile_activity_table.jsx
@@ -6,7 +6,7 @@ import React from 'react'
 import ReactTooltip from 'react-tooltip'
 import ReactTable from 'react-table'
 
-export default class extends UnitTemplateProfileActivityTable React.Component {
+export default class UnitTemplateProfileActivityTable extends React.Component {
   static propTypes = {
     data: PropTypes.object,
     actions: PropTypes.object
