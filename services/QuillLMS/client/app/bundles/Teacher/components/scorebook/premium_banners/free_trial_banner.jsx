6b8dd7a728a0e43f6dd6ee2fa31c62f36fc13391
Cissy Yu <cissy@quill.org>
2020-03-31 12:32:40 -0400

    all copy changes

--- services/QuillLMS/client/app/bundles/Teacher/components/scorebook/premium_banners/free_trial_banner.jsx
+++ services/QuillLMS/client/app/bundles/Teacher/components/scorebook/premium_banners/free_trial_banner.jsx
@@ -9,10 +9,14 @@ export default React.createClass({
     return { trialStarted: false, };
   },
 
-  beginTrial() {
-    requestPost('/subscriptions', { subscription: { account_type: 'Teacher Trial', }, }, () => {
-      this.setState({ trialStarted: true, })
-    })
+  // beginTrial() {
+  //   requestPost('/subscriptions', { subscription: { account_type: 'Teacher Trial', }, }, () => {
+  //     this.setState({ trialStarted: true, })
+  //   })
+  // },
+
+  activateSubscription() {
+    window.location.href = '/subscriptions/activate_covid_subscription'
   },
 
   render() {
@@ -22,14 +26,14 @@ export default React.createClass({
     return (
       <div className="row free-trial-promo">
         <div className="col-md-9 col-xs-12 pull-left">
-          <h4>Try Premium for Free</h4>
-          <span>Unlock your Premium trial to save time grading and gain actionable insights.</span>
+          <h4>Activate free Quill Premium</h4>
+          <span>You can activate Quill Premium through the end of the 2019/2020 school year.</span>
           <br />
           <a href="/premium">Learn more about Premium</a>
         </div>
         <div className="col-md-3 col-xs-12 pull-right">
           <div className="premium-button-box text-center">
-            <button className="btn-orange" onClick={this.beginTrial} type="button">Try it Free for 30 Days</button>
+            <button className="btn-orange" onClick={this.activateSubscription} type="button">Get Premium Free</button>
             <br />
             <span>No credit card required</span>
           </div>
