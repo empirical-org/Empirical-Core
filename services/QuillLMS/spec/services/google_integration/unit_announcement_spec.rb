dddb4020ebaac7c1e0f9b5665d281c0d304c2e26
Thomas Robertson <thomas.robertson@quill.org>
2020-04-08 10:22:51 -0400

    Fix and update tests

--- services/QuillLMS/spec/services/google_integration/unit_announcement_spec.rb
+++ services/QuillLMS/spec/services/google_integration/unit_announcement_spec.rb
@@ -1,14 +1,14 @@
 require 'rails_helper'
 
-describe GoogleIntegration::LessonAnnouncement do
+describe GoogleIntegration::UnitAnnouncement do
   let(:classroom_unit) { create(:classroom_unit) }
   let(:unit_activity) { create(:unit_activity) }
-  let(:subject) { GoogleIntegration::LessonAnnouncement.new(classroom_unit, unit_activity) }
+  let(:subject) { GoogleIntegration::UnitAnnouncement.new(classroom_unit, unit_activity) }
 
   describe "#post" do
     it "sends GoogleApiErrors to NewRelic" do
       expect(subject).to receive(:can_post_to_google_classroom?).and_return(true)
-      expect(subject).to receive(:handle_response).and_raise(GoogleIntegration::LessonAnnouncement::GoogleApiError)
+      expect(subject).to receive(:handle_response).and_raise(GoogleIntegration::UnitAnnouncement::GoogleApiError)
       expect(NewRelic::Agent).to receive(:add_custom_attributes)
       expect(NewRelic::Agent).to receive(:notice_error)
       subject.post
@@ -21,7 +21,7 @@ describe GoogleIntegration::LessonAnnouncement do
         status: 403,
         body: '{}'
       })
-      expect{subject.send(:handle_response) { mock_response }}.to raise_error(GoogleIntegration::LessonAnnouncement::GoogleApiError)
+      expect{subject.send(:handle_response) { mock_response }}.to raise_error(GoogleIntegration::UnitAnnouncement::GoogleApiError)
     end
   end
 end
