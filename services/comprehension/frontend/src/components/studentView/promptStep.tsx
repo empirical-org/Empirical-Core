b5b78adc4f99ba7330b8e6ed064f96779f3c608a
Emilia Friedberg <emiliafriedberg@gmail.com>
2020-04-14 15:11:53 -0400

    fix tests

--- services/comprehension/frontend/src/components/studentView/promptStep.tsx
+++ services/comprehension/frontend/src/components/studentView/promptStep.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import Filter from 'bad-words'
+import * as Filter from 'bad-words'
 
 import EditorContainer from './editorContainer'
 import Feedback from './feedback'
