60347d2fce7fb20b2d36b295559edb5c78e39a84
yvespeirsman <yves@nlp.town>
2020-04-14 10:55:48 +0200

    Added code for BERT-based grammar correction

---
+++ services/comprehension/grammar-local-api/quillnlp/models/bert/preprocessing.py
@@ -0,0 +1,187 @@
+import torch
+import numpy as np
+from torch.utils.data import TensorDataset, DataLoader
+from typing import List, Dict, Union
+from enum import Enum
+
+from transformers.tokenization_bert import BertTokenizer
+from transformers.tokenization_distilbert import DistilBertTokenizer
+
+
+class NLPTask(Enum):
+    SINGLE_LABEL_CLASSIFICATION = "single-label classification"
+    MULTI_LABEL_CLASSIFICATION = "multi-label classification"
+    SEQUENCE_LABELING = "sequence labeling"
+
+
+class BertInputItem(object):
+    """ A BertInputItem contains all the information that is needed to finetune
+    a Bert model.
+
+    Attributes:
+        input_ids: the ids of the input tokens
+        input_mask: a list of booleans that indicates what tokens should be masked
+        segment_ids: a list of segment ids for the tokens
+        label_ids: a label id or a list of label ids for the input
+    """
+
+    def __init__(self, text: str,
+                 input_ids: List[int],
+                 input_mask: List[int],
+                 segment_ids: List[int],
+                 label_ids: Union[int or List[int]]):
+        self.text = text
+        self.input_ids = input_ids
+        self.input_mask = input_mask
+        self.segment_ids = segment_ids
+        self.label_ids = label_ids
+
+
+def create_label_vocabulary(data: List[Dict]) -> Dict:
+    """
+    Creates a dictionary that maps the labels in the data to their id. This
+    works for both single-label data (where the "label" property is a string),
+    and multi-label data (where the "label" property is a list.
+
+    Args:
+        data: a list of data items as dicts of the form {"text": ..., "label": ...}
+
+    Returns: a dictionary that maps label strings to label ids
+
+    """
+
+    label2idx = {}
+    for item in data:
+        if type(item["label"]) == str and item["label"] not in label2idx:
+            label2idx[item["label"]] = len(label2idx)
+        elif type(item["label"]) == list:
+            for l in item["label"]:
+                if l not in label2idx:
+                    label2idx[l] = len(label2idx)
+
+    return label2idx
+
+
+def convert_data_to_input_items(examples: List[Dict], label2idx: Dict,
+                                max_seq_length: int, tokenizer: BertTokenizer,
+                                task: NLPTask) -> List[BertInputItem]:
+    """
+    Converts a list of input examples to BertInputItems
+
+    Args:
+        examples: a list of examples as dicts of the form {"text": ..., "label": ...}
+        label2idx: a dict that maps label strings to label ids
+        max_seq_length: the maximum sequence length for the input items
+        tokenizer: the tokenizer that will be used to tokenize the text
+        task: the type of NLP task
+
+    Returns: a list of BertInputItems
+
+    """
+
+    input_items = []
+    for (ex_index, ex) in enumerate(examples):
+
+        # Create a list of token ids
+        toks = tokenizer.encode_plus(ex["text"], max_length=max_seq_length, pad_to_max_length=True)
+        input_ids = toks["input_ids"]
+        segment_ids = toks["token_type_ids"]
+        input_mask = toks["attention_mask"]
+
+        if task == NLPTask.SINGLE_LABEL_CLASSIFICATION:
+            label_ids = label2idx[ex["label"]]
+        elif task == NLPTask.MULTI_LABEL_CLASSIFICATION:
+            label_ids = np.zeros(len(label2idx))
+            for label in ex["label"]:
+                label_ids[label2idx[label]] = 1
+        elif task == NLPTask.SEQUENCE_LABELING:
+            if "entities" not in ex:
+                label_ids = [label2idx["O"]] * len(input_ids)
+            else:
+                tokens = tokenizer.convert_ids_to_tokens(input_ids)
+                label_ids = [label2idx["O"]]
+                cur_index = 0
+                for num, tok in enumerate(tokens[1:]):
+
+                    if num > 0 and not tok.startswith("##"):
+                        cur_index += 1
+
+                    found_entity = False
+                    for entity in ex["entities"]:
+                        if cur_index >= entity[0] and cur_index <= entity[1]:
+                            label_ids.append(label2idx[entity[2]])
+                            found_entity = True
+                    if not found_entity:
+                        label_ids.append(label2idx["O"])
+
+                    if tok.startswith("##"):
+                        cur_index += len(tok) - 2
+                    else:
+                        cur_index += len(tok)
+
+            assert len(label_ids) == len(input_ids)
+        else:
+            raise ValueError(f"Unknown NLP Task {task}")
+
+        input_items.append(
+            BertInputItem(text=ex["text"],
+                          input_ids=input_ids,
+                          input_mask=input_mask,
+                          segment_ids=segment_ids,
+                          label_ids=label_ids))
+    return input_items
+
+
+def get_data_loader(input_items: List[BertInputItem], batch_size: int,
+                    task: NLPTask, shuffle: bool=True) -> DataLoader:
+    """
+    Constructs a DataLoader for a list of BERT input items.
+
+    Args:
+        input_items: a list of BERT input items
+        batch_size: the batch size
+        shuffle: indicates whether the data should be shuffled or not.
+
+    Returns: a DataLoader for the input items
+
+    """
+    all_input_ids = torch.tensor([f.input_ids for f in input_items], dtype=torch.long)
+    all_input_mask = torch.tensor([f.input_mask for f in input_items], dtype=torch.long)
+    all_segment_ids = torch.tensor([f.segment_ids for f in input_items], dtype=torch.long)
+    if task == NLPTask.SINGLE_LABEL_CLASSIFICATION or task == NLPTask.SEQUENCE_LABELING:
+        all_label_ids = torch.tensor([f.label_ids for f in input_items], dtype=torch.long)
+    elif task == NLPTask.MULTI_LABEL_CLASSIFICATION:
+        all_label_ids = torch.tensor([f.label_ids for f in input_items], dtype=torch.float)
+    else:
+        raise ValueError("No data loader for NLPTask {task}")
+
+    data = TensorDataset(all_input_ids, all_input_mask, all_segment_ids, all_label_ids)
+
+    dataloader = DataLoader(data, shuffle=shuffle, batch_size=batch_size)
+
+    return dataloader
+
+
+
+def preprocess(data: List[Dict],
+               model: str,
+               label2idx: Dict,
+               max_seq_length: int) -> List[BertInputItem]:
+    """
+    Runs the full preprocessing pipeline on a list of data items.
+
+    Args:
+        data: a list of examples as dicts of the form {"text": ..., "label": ...}
+        model: the name of the BERT model
+        label2idx: a dict that maps label strings to label ids
+        max_seq_length: the maximum sequence length for the input items
+
+    Returns: a list of BertInputItems
+    """
+    if "distilbert" in model:
+        tokenizer = DistilBertTokenizer.from_pretrained(model)
+    else:
+        tokenizer = BertTokenizer.from_pretrained(model)
+    bert_items = convert_data_to_input_items(data, label2idx, max_seq_length, tokenizer)
+
+    return bert_items
---
+++ services/comprehension/grammar-local-api/quillnlp/models/bert/train.py
@@ -0,0 +1,185 @@
+import os
+from typing import List, Dict
+
+import torch
+import numpy as np
+from tqdm import tqdm_notebook as tqdm, trange
+from torch.nn import Sigmoid, Softmax
+from torch.utils.data import DataLoader
+from quillnlp.models.bert.models import BertForMultiLabelSequenceClassification
+
+from transformers.optimization import AdamW
+from transformers.modeling_bert import BertForSequenceClassification, BertForTokenClassification
+from transformers.modeling_distilbert import DistilBertForSequenceClassification
+from transformers.modeling_utils import PreTrainedModel
+
+
+def train(model: PreTrainedModel, train_dataloader: DataLoader, dev_dataloader: DataLoader,
+          batch_size: int, gradient_accumulation_steps: int, device, num_train_epochs: int=20,
+          warmup_proportion: float=0.1, learning_rate: float=1e-5, patience: int=5,
+          eval_frequency: int=None,
+          output_dir: str="/tmp/", model_file_name:str="model.bin") -> str:
+    """
+    Trains a BERT Model on a set of training data, tuning it on a set of development data
+
+    Args:
+        model: the model that will be trained
+        train_dataloader: a DataLoader with training data
+        dev_dataloader: a DataLoader with development data (for early stoopping)
+        batch_size: the batch size for training
+        gradient_accumulation_steps: the number of steps that gradients will be accumulated
+        device: the device where training will take place ("cpu" or "cuda")
+        num_train_epochs: the maximum number of training epochs
+        warmup_proportion: the proportion of training steps for which the learning rate will be warmed up
+        learning_rate: the initial learning rate
+        patience: the number of epochs after which training will stop if no improvement on the dev
+                  set is observed
+        output_dir: the directory where the model will be saved
+        model_file_name: the filename of the model file
+
+    Returns: the path to the trained model
+
+    """
+
+    def warmup_linear(x, warmup=0.002):
+        if x < warmup:
+            return x / warmup
+        return 1.0 - x
+
+    output_model_file = os.path.join(output_dir, model_file_name)
+
+    num_train_steps_per_epoch = int(len(train_dataloader.dataset) / batch_size / gradient_accumulation_steps)
+    eval_frequency = num_train_steps_per_epoch if eval_frequency is None else eval_frequency
+
+    num_train_steps = num_train_steps_per_epoch * num_train_epochs
+    max_grad_norm = 5
+
+    param_optimizer = list(model.named_parameters())
+    no_decay = ['bias', 'LayerNorm.bias', 'LayerNorm.weight']
+    optimizer_grouped_parameters = [
+        {'params': [p for n, p in param_optimizer if not any(nd in n for nd in no_decay)], 'weight_decay': 0.01},
+        {'params': [p for n, p in param_optimizer if any(nd in n for nd in no_decay)], 'weight_decay': 0.0}
+        ]
+
+    optimizer = AdamW(optimizer_grouped_parameters, lr=learning_rate, correct_bias=False)
+
+    global_step = 0
+    loss_history = []
+    stop_training = False
+    for epoch in trange(int(num_train_epochs), desc="Epoch"):
+
+        model.train()
+        tr_loss = 0
+        for step, batch in enumerate(tqdm(train_dataloader, desc="Training iteration")):
+            batch = tuple(t.to(device) for t in batch)
+            input_ids, input_mask, segment_ids, label_ids = batch
+
+            if type(model) == BertForSequenceClassification or type(model) == BertForMultiLabelSequenceClassification or type(model) == BertForTokenClassification:
+                outputs = model(input_ids, attention_mask=input_mask, token_type_ids=segment_ids, labels=label_ids)
+            elif type(model) == DistilBertForSequenceClassification:
+                outputs = model(input_ids, attention_mask=input_mask, labels=label_ids)
+            loss = outputs[0]
+
+            if gradient_accumulation_steps > 1:
+                loss = loss / gradient_accumulation_steps
+
+            loss.backward()
+
+            tr_loss += loss.item()
+
+            if (step + 1) % gradient_accumulation_steps == 0:
+                lr_this_step = learning_rate * warmup_linear(global_step / num_train_steps, warmup_proportion)
+                for param_group in optimizer.param_groups:
+                    param_group['lr'] = lr_this_step
+                optimizer.step()
+                optimizer.zero_grad()
+                global_step += 1
+
+                if global_step % eval_frequency == 0:
+
+                    dev_loss, _, _, _ = evaluate(model, dev_dataloader, device)
+                    print(f"Epoch {epoch} step {global_step}: dev loss = {dev_loss}")
+
+                    if len(loss_history) == 0 or dev_loss < min(loss_history):
+                        model_to_save = model.module if hasattr(model, 'module') else model
+                        torch.save(model_to_save.state_dict(), output_model_file)
+                        print(f"Lower loss => saving model to {output_model_file}.")
+
+                    if len(loss_history) >= patience and dev_loss > max(loss_history[-patience:]):
+                        print("No improvement on development set. Finish training.")
+                        stop_training = True
+                        break
+
+                    loss_history.append(dev_loss)
+
+        if stop_training:
+            break
+
+    return output_model_file
+
+
+def evaluate(model: PreTrainedModel, dataloader: DataLoader, device: str) -> (int, List[int], List[int]):
+    """
+    Evaluates a Bert Model on a labelled data set.
+
+    Args:
+        model: the BertModel to be evaluated
+        dataloader: the DataLoader with the test data
+        device: the device where evaluation will take place ("cpu" or "cuda")
+
+    Returns: a tuple with (the evaluation loss, a list with the correct labels,
+            and a list with the predicted labels)
+
+    """
+
+    model.eval()
+
+    eval_loss = 0
+    nb_eval_steps = 0
+    predicted_labels, correct_labels, probabilities = [], [], []
+
+    for step, batch in enumerate(tqdm(dataloader, desc="Evaluation iteration")):
+        batch = tuple(t.to(device) for t in batch)
+        input_ids, input_mask, segment_ids, label_ids = batch
+
+        with torch.no_grad():
+            if type(model) == BertForSequenceClassification or type(model) == BertForMultiLabelSequenceClassification or type(model) == BertForTokenClassification:
+                tmp_eval_loss, logits = model(input_ids, attention_mask=input_mask,
+                                              token_type_ids=segment_ids, labels=label_ids)
+            elif type(model) == DistilBertForSequenceClassification:
+                tmp_eval_loss, logits = model(input_ids, attention_mask=input_mask, labels=label_ids)
+
+        if type(model) == BertForSequenceClassification or type(model) == DistilBertForSequenceClassification:
+            softmax = Softmax()
+            outputs = softmax(logits.to('cpu'))
+            label_ids = label_ids.to('cpu').numpy()
+            predicted_labels += list(np.argmax(outputs, axis=1))
+
+        elif type(model) == BertForTokenClassification:
+            softmax = Softmax(dim=2)
+            outputs = softmax(logits.to('cpu'))
+            label_ids = label_ids.to('cpu').numpy()
+            predicted_labels += list(np.argmax(outputs, axis=2))
+
+        elif type(model) == BertForMultiLabelSequenceClassification:
+            sig = Sigmoid()
+            outputs = sig(logits).to('cpu').numpy()
+            label_ids = label_ids.to('cpu').numpy()
+            predicted_labels += list(outputs >= 0.5)
+
+        correct_labels.extend(list(label_ids))
+        probabilities.extend(list(outputs))
+
+        eval_loss += tmp_eval_loss.mean().item()
+        nb_eval_steps += 1
+
+    eval_loss = eval_loss / nb_eval_steps
+
+    correct_labels = np.array(correct_labels)
+    if type(model) == BertForTokenClassification:
+        predicted_labels = np.array([t.numpy() for t in predicted_labels])
+    else:
+        predicted_labels = np.array(predicted_labels)
+
+    return eval_loss, probabilities, correct_labels, predicted_labels
+
