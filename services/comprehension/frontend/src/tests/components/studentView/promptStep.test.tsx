c071c41cd9c998280d14001f79004b2a225ea2ce
Emilia Friedberg <emiliafriedberg@gmail.com>
2020-04-16 10:36:38 -0400

    make sure jest tests pass

--- services/comprehension/frontend/src/tests/components/studentView/promptStep.test.tsx
+++ services/comprehension/frontend/src/tests/components/studentView/promptStep.test.tsx
@@ -2,8 +2,10 @@ import * as React from 'react'
 import { mount } from 'enzyme';
 import toJson from 'enzyme-to-json';
 
-import PromptStep, { TOO_LONG_FEEDBACK, TOO_SHORT_FEEDBACK, MULTIPLE_SENTENCES_FEEDBACK, PROFANITY_FEEDBACK } from '../../../components/studentView/promptStep'
+import PromptStep from '../../../components/studentView/promptStep'
 import EditorContainer from '../../../components/studentView/editorContainer'
+import { TOO_LONG_FEEDBACK, TOO_SHORT_FEEDBACK, MULTIPLE_SENTENCES_FEEDBACK, PROFANITY_FEEDBACK } from '../../../modules/prefilters'
+
 
 import { activityOne, optimalSubmittedResponse, suboptimalSubmittedResponse, } from './data'
 
