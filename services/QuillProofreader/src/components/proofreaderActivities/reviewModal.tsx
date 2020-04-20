e4e94116d749e88992cb3beae2fd7db6ae562630
Emilia Friedberg <emiliafriedberg@gmail.com>
2020-04-20 11:46:50 -0400

    finish everything but the review parts

--- services/QuillProofreader/src/components/proofreaderActivities/reviewModal.tsx
+++ services/QuillProofreader/src/components/proofreaderActivities/reviewModal.tsx
@@ -1,16 +1,16 @@
 import * as React from 'react'
 
-const ReviewModal: React.SFC<{closeModal: any, numberOfErrors: number, numberOfCorrectChanges: number}> = (props) => {
- return (<div className="review-modal-container">
+const ReviewModal: React.SFC<{closeModal: any, numberOfErrors: number, numberOfCorrectChanges: number}> = ({ closeModal, numberOfErrors, numberOfCorrectChanges, }) => {
+  const highScoreMessage = numberOfCorrectChanges > numberOfErrors / 2 ? "Good work! " : null
+  return (<div className="review-modal-container">
    <div className="review-modal-background" />
    <div className="review-modal">
      <div className="top-section">
-       {props.numberOfCorrectChanges > props.numberOfErrors / 2 ? <h1>Good work!</h1> : null}
-       <p>You found {props.numberOfCorrectChanges} of {props.numberOfErrors} errors.</p>
-       <button onClick={props.closeModal}>Review Your Work</button>
+       <h1>{highScoreMessage} You found {numberOfCorrectChanges} of {numberOfErrors} errors.</h1>
+       <button className="quill-button medium primary contained focus-on-light" onClick={closeModal} tabIndex={1}>Review</button>
      </div>
    </div>
- </div>)
+  </div>)
 }
 
 export default ReviewModal
