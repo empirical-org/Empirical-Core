4c02cfa2d0aa46b8120459d8640a847f011b8262
Haronil Estevez <haronil@quill.org>
2020-04-21 10:39:03 -0400

    raise error if admin is nil

--- services/QuillLMS/app/workers/premium_school_subscription_email_worker.rb
+++ services/QuillLMS/app/workers/premium_school_subscription_email_worker.rb
@@ -5,8 +5,10 @@ class PremiumSchoolSubscriptionEmailWorker
     @user = User.find(user_id)
     school = @user.school
     admin = school&.schools_admins&.first&.try(:user)
-    if admin
+    unless admin.nil?
       @user.send_premium_school_subscription_email(school, admin)
+    else
+      raise "admin is nil"
     end
   end
 
