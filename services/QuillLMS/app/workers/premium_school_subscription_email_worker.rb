4b597fe57ca8930f92ae34bee65dca65fd7958c3
Haronil Estevez <haronil@quill.org>
2020-04-21 12:24:49 -0400

    update PremiumSchoolSubscriptionEmailWorker

--- services/QuillLMS/app/workers/premium_school_subscription_email_worker.rb
+++ services/QuillLMS/app/workers/premium_school_subscription_email_worker.rb
@@ -4,11 +4,8 @@ class PremiumSchoolSubscriptionEmailWorker
   def perform(user_id)
     @user = User.find(user_id)
     school = @user.school
-    #admin = school&.schools_admins&.first&.try(:user)
-    admin = school.schools_admins.first.try(:user)
-    if admin.nil?
-      raise "admin is nil"
-    else
+    admin = school&.schools_admins&.first&.try(:user)
+    unless admin.nil?
       @user.send_premium_school_subscription_email(school, admin)
     end
   end
