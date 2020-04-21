b9e150abae14c833dbb3d6172a908b90e7169a17
Haronil Estevez <haronil@quill.org>
2020-04-21 10:54:41 -0400

    test previous syntax

--- services/QuillLMS/app/workers/premium_school_subscription_email_worker.rb
+++ services/QuillLMS/app/workers/premium_school_subscription_email_worker.rb
@@ -4,7 +4,8 @@ class PremiumSchoolSubscriptionEmailWorker
   def perform(user_id)
     @user = User.find(user_id)
     school = @user.school
-    admin = school&.schools_admins&.first&.try(:user)
+    #admin = school&.schools_admins&.first&.try(:user)
+    admin = school.schools_admins.first.try(:user)
     if admin.nil?
       raise "admin is nil"
     else
