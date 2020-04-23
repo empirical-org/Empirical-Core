6029b9f4537f486e02be5d57d2960814bccd6086
dandrabik <dandrabik@users.noreply.github.com>
2020-04-23 14:45:31 -0400

    Render AP routes on page, Support both caps and non-caps. (#6242)
    * Add caps route for AP.
    
    * Don’t redirect, just render for AP links.

--- services/QuillLMS/config/routes.rb
+++ services/QuillLMS/config/routes.rb
@@ -8,7 +8,8 @@ EmpiricalGrammar::Application.routes.draw do
   end
 
   # temporary setup for AP landing page
-  get '/ap' => redirect('activities/packs/193')
+  get '/AP' => 'teachers/unit_templates#index', defaults: {id: 193}
+  get '/ap' => 'teachers/unit_templates#index', defaults: {id: 193}
 
   post "/graphql", to: "graphql#execute"
 
