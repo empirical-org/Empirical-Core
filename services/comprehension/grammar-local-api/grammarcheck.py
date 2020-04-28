11dd4e66bb3bcdad7eb74115d8240a9ec782f181
Cissy Yu <cissy@quill.org>
2020-04-28 15:19:44 -0400

    linting

--- services/comprehension/grammar-local-api/grammarcheck.py
+++ services/comprehension/grammar-local-api/grammarcheck.py
@@ -8,7 +8,6 @@ from collections import namedtuple
 from spacy.tokens.doc import Doc
 from spacy.tokens.token import Token
 
-
 BASE_SPACY_MODEL = "en_core_web_sm"
 
 # Tag, part-of-speech and dependency constants
