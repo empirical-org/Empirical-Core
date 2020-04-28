4a03e7fc68a7f81ecbf716a2293a4a9248a481bf
Eric Adams <erictayloradams@gmail.com>
2020-04-28 12:10:05 -0400

    update classroom and remove unused modal

--- services/QuillLMS/client/app/bundles/Teacher/components/classrooms_with_students/classroom.jsx
+++ services/QuillLMS/client/app/bundles/Teacher/components/classrooms_with_students/classroom.jsx
@@ -1,42 +1,40 @@
 import React from 'react'
 import Student from './student'
-import Button from 'react-bootstrap/lib/Button';
-import Panel from 'react-bootstrap/lib/Panel';
 import _ from 'underscore'
 
 export default class Classroom extends React.Component {
- constructor(props) {
-   super(props)
-
-   this.state = {open: false};
- }
+  state = { open: false }
 
  componentDidMount() {
    window.scrollTo(0, 0);
  }
 
  handleClassroomSelection = (e) => {
-   var checked = e.target.checked;
-   this.props.toggleClassroomSelection(this.props.classroom, checked);
+   const { classroom, toggleClassroomSelection } = this.props
+   const checked = e.target.checked;
+   toggleClassroomSelection(classroom, checked);
  };
 
- toggleClassroomCollapse = () => {
-   this.setState({ open: !this.state.open });
+ handleToggleClassroomCollapse = () => {
+   this.setState(prevState => ({ open: !prevState.open }));
  };
 
  determineCheckbox = () => {
-   var allSelected;
-   if (this.props.students.length > 0) {
-     var selected = _.where(this.props.students, {isSelected: true});
-     allSelected = (selected.length == this.props.students.length);
+   const { allSelected, classroom, students } = this.props
+   const { id } = classroom
+   let allStudentsSelected;
+   if (students.length > 0) {
+     const selected = _.where(students, {isSelected: true});
+     allStudentsSelected = (selected.length == students.length);
    } else {
-     allSelected = this.props.allSelected;
+     allStudentsSelected = allSelected;
    }
    return (
      <input
-       checked={allSelected ? 'checked' : null}
+       aria-labelledby="checkbox-container"
+       checked={allStudentsSelected ? 'checked' : null}
        className='css-checkbox classroom_checkbox'
-       id={'classroom_checkbox_' + this.props.classroom.id}
+       id={'classroom_checkbox_' + id}
        onChange={this.handleClassroomSelection}
        type='checkbox'
      />
@@ -44,12 +42,14 @@ export default class Classroom extends React.Component {
  };
 
  angleIcon = () => {
-   return this.state.open === true ? 'up' : 'down';
+   const { open } = this.state
+   return open ? 'up' : 'down';
  };
 
  selectedStudentCount = () => {
+   const { students } = this.props
    let selectedStudentCount = 0
-   this.props.students.forEach((s) => {
+   students.forEach((s) => {
      if (s.isSelected) {
        selectedStudentCount += 1;
      }
@@ -58,9 +58,10 @@ export default class Classroom extends React.Component {
  };
 
  renderStudentCountText = () => {
-   const numberOfStudents = this.props.students.length
+  const { allSelected, students } = this.props
+   const numberOfStudents = students.length
    const selectedStudentCount = this.selectedStudentCount()
-   if (numberOfStudents === 0 && this.props.allSelected) {
+   if (numberOfStudents === 0 && allSelected) {
      return '(Empty class - all added students will be assigned)'
    } else if (selectedStudentCount === 0) {
      return '(0 students will be assigned)'
@@ -75,33 +76,40 @@ export default class Classroom extends React.Component {
  // columns style. This is because with the way multicolumn css works, we will
  // otherwise experience some strange formatting we don't want here.
  shouldUnsetColumns = () => {
-   if(this.props.students.length < 4) {
+  const { open, } = this.state
+  const { students, } = this.props
+   if(!open) {
+     return { display: 'none' }
+   } else if(students.length < 4) {
      return { columns: 'unset' }
    }
  };
 
  renderPanel = () => {
-   const { open, } = this.state
+  const { classroom, handleStudentCheckboxClick, students, toggleStudentSelection } = this.props
+  const { id } = classroom
+  const { open, } = this.state
 
-   if (!open) { return }
+  if (!open) { return }
 
-   const studentList = this.props.students.map((student) => (<Student
-     classroom={this.props.classroom}
-     handleStudentCheckboxClick={this.props.handleStudentCheckboxClick}
-     key={`c${this.props.classroom.id}s${student.id}`}
-     student={student}
-     toggleStudentSelection={this.props.toggleStudentSelection}
-   />)
-   )
-
-   return (<Panel collapsible expanded={this.state.open} ref='studentList'>
-     <div className='panel-body student-panel-body' style={this.shouldUnsetColumns()}>
-       {studentList}
-     </div>
-   </Panel>)
+  const studentList = students.map((student) => (<Student
+    classroom={classroom}
+    handleStudentCheckboxClick={handleStudentCheckboxClick}
+    key={`c${id}s${student.id}`}
+    student={student}
+    toggleStudentSelection={toggleStudentSelection}
+  />)
+  )
+  return (
+    <div className='panel-body student-panel-body' style={this.shouldUnsetColumns()}>
+      {studentList}
+    </div>
+  )
  }
 
  render() {
+  const { classroom } = this.props
+  const { id, name } = classroom
    return (
      <div className='panel-group'>
        <div className='panel panel-default'>
@@ -110,15 +118,15 @@ export default class Classroom extends React.Component {
              <span>
                Select Entire Class
              </span>
-             <Button className='toggle-button pull-right select-by-student-button' onClick={this.toggleClassroomCollapse}>
+             <button className='toggle-button pull-right select-by-student-button' onClick={this.handleToggleClassroomCollapse} type="button">
                <span className='pull-right panel-select-by-student' >
                  Select by Student <i className={'fas fa-angle-' + this.angleIcon()} />
                </span>
-             </Button>
-             <div>
+             </button>
+             <div id="checkbox-container">
                {this.determineCheckbox()}
-               <label className='css-label' htmlFor={'classroom_checkbox_' + this.props.classroom.id}>
-                 {this.props.classroom.name}
+               <label className='css-label' htmlFor={'classroom_checkbox_' + id}>
+                 {name}
                  <span style={{marginLeft: '5px', fontWeight: '600'}}>
                    {this.renderStudentCountText()}
                  </span>
