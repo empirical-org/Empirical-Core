7979ba4c235a356610a0dec262c25ee8a0c04c43
Cissy Yu <cissy@quill.org>
2020-04-28 11:28:54 -0400

    modify script to catch old questions without question types

--- services/QuillLMS/script/lessons/add_lesson_type_to_lessons.rb
+++ services/QuillLMS/script/lessons/add_lesson_type_to_lessons.rb
@@ -7,8 +7,10 @@ Lesson.all.each do |lesson|
   end
   first_question = data["questions"][0]
   question_type = first_question["questionType"]
-  if !PERMITTED_TYPES.include? question_type
-    raise "Error - the question type #{question_type} is not permitted."
+  if question_type.empty?
+    puts "Found a blank question type on lesson #{lesson.uid}"
+  elsif !PERMITTED_TYPES.include? question_type
+    raise "Error - the question type #{question_type} on lesson #{lesson.uid} is not permitted."
   end
   data["questionType"] = question_type
   lesson.data = data
