a86645cc60b1702ab3b578ca671e1f6d03191716
Haronil Estevez <haronil@quill.org>
2020-04-28 15:53:27 -0400

    remove changes to structure.sql

--- services/QuillLMS/db/structure.sql
+++ services/QuillLMS/db/structure.sql
@@ -4938,12 +4938,6 @@ CREATE INDEX index_users_on_classcode ON public.users USING btree (classcode);
 
 CREATE INDEX index_users_on_clever_id ON public.users USING btree (clever_id);
 
---
--- Name: index_users_on_google_id; Type: INDEX; Schema: public; Owner: -
---
-
-CREATE INDEX index_users_on_google_id ON public.users USING btree (google_id);
-
 
 --
 -- Name: index_users_on_email; Type: INDEX; Schema: public; Owner: -
@@ -5999,7 +5993,4 @@ INSERT INTO schema_migrations (version) VALUES ('20200326220320');
 INSERT INTO schema_migrations (version) VALUES ('20200409151835');
 
 INSERT INTO schema_migrations (version) VALUES ('20200415170227');
-
-INSERT INTO schema_migrations (version) VALUES ('20200417172506');
-                                                               
 
