503956ab9260e95ff1af43d2aecc1721da5bfa2b
Cissy Yu <cissy@quill.org>
2020-04-28 10:42:37 -0400

    typo

--- services/QuillLMS/script/lessons/add_lesson_type_to_lessons.rb
+++ services/QuillLMS/script/lessons/add_lesson_type_to_lessons.rb
@@ -7,7 +7,7 @@ Lesson.all.each do |lesson|
   end
   first_question = data["questions"][0]
   question_type = first_question["questionType"]
-  if !PERMITTED_TYPES.include? questionType
+  if !PERMITTED_TYPES.include? question_type
     raise "Error - the question type #{question_type} is not permitted."
   end
   data["questionType"] = question_type
