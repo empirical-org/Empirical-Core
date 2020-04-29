4f88010dce4da14a41c3b71310551e6fe9a94ba2
Haronil Estevez <haronil@quill.org>
2020-04-29 09:50:16 -0400

    use worker instead of task for refreshing google access tokens

--- services/QuillLMS/app/models/cron.rb
+++ services/QuillLMS/app/models/cron.rb
@@ -20,6 +20,7 @@ class Cron
     RenewExpiringRecurringSubscriptionsWorker.perform_async
     SyncSalesmachineWorker.perform_async
     DailyStatsEmailJob.perform_async(date)
+    RefreshGoogleAccessTokensWorker.perform_async
   end
 
   def self.run_saturday
