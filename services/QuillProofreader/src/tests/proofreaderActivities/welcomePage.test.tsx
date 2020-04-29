8b896a6042cf4330b3f49772e70acc7f4c06789b
Emilia Friedberg <emiliafriedberg@gmail.com>
2020-04-29 11:38:29 -0400

    add welcome page to proofreader (#6272)
    * add welcome page to proofreader
    
    * minor changes
    
    * minor css change and add test

---
+++ services/QuillProofreader/src/tests/proofreaderActivities/welcomePage.test.tsx
@@ -0,0 +1,14 @@
+import * as React from "react";
+import { shallow } from "enzyme";
+import WelcomePage from "../../components/proofreaderActivities/welcomePage";
+
+describe("<WelcomePage />", () => {
+  const wrapper = shallow(<WelcomePage
+    onNextClick={() => {}}
+  />)
+
+    it("should render", () => {
+      expect(wrapper).toMatchSnapshot();
+    });
+
+});
