9c61e9c7916a2f5444740cb90777e04809690e78
Haronil Estevez <haronil@quill.org>
2020-04-29 11:14:36 -0400

    add entry to cron spec

--- services/QuillLMS/spec/models/cron_spec.rb
+++ services/QuillLMS/spec/models/cron_spec.rb
@@ -36,6 +36,11 @@ describe "Cron", type: :model do
       expect(DailyStatsEmailJob).to receive(:perform_async)
       Cron.interval_1_day
     end
+
+    it "enqueuesRefreshGoogleAccessTokensWorker" do
+      expectRefreshGoogleAccessTokensWorker).to receive(:perform_async)
+      Cron.interval_1_day
+    end
   end
 
   describe "#run_saturday" do
