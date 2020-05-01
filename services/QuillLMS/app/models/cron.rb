620136d09d308014058e95d424bc720d298c2caf
Haronil Estevez <haronil@quill.org>
2020-05-01 13:08:31 -0400

    refresh google refresh tokens

--- services/QuillLMS/app/models/cron.rb
+++ services/QuillLMS/app/models/cron.rb
@@ -20,7 +20,7 @@ class Cron
     RenewExpiringRecurringSubscriptionsWorker.perform_async
     SyncSalesmachineWorker.perform_async
     DailyStatsEmailJob.perform_async(date)
-    RefreshGoogleAccessTokensWorker.perform_async
+    RefreshGoogleRefreshTokensWorker.perform_async
   end
 
   def self.run_saturday
