5c6a5f7ff582415f6d49dae5d1cabba0b34aa783
Thomas Robertson <thomas.robertson@quill.org>
2020-05-01 15:05:33 -0400

    Update Proofreader Passage API to look at Lessons API

--- services/QuillProofreader/src/lib/proofreader_activities_api.ts
+++ services/QuillProofreader/src/lib/proofreader_activities_api.ts
@@ -3,27 +3,27 @@ import { ProofreaderActivity, ProofreaderActivities } from '../interfaces/proofr
 
 const PROOFREADER_PASSAGE_TYPE = 'proofreader_passage'
 
-const questionApiBaseUrl = `${process.env.EMPIRICAL_BASE_URL}/api/v1/questions`;
+const questionApiBaseUrl = `${process.env.EMPIRICAL_BASE_URL}/api/v1/lessons`;
 
 class ProofreaderPassageApi {
   static getAll(): Promise<Array<ProofreaderActivity>> {
-    return requestGet(`${questionApiBaseUrl}.json?question_type=${PROOFREADER_PASSAGE_TYPE}`);
+    return requestGet(`${questionApiBaseUrl}.json?lesson_type=${PROOFREADER_PASSAGE_TYPE}`);
   }
 
   static get(uid: string): Promise<ProofreaderActivity> {
-    return requestGet(`${questionApiBaseUrl}/${uid}.json`);
+    return requestGet(`${questionApiBaseUrl}/${uid}.json?lesson_type=${PROOFREADER_PASSAGE_TYPE}`);
   }
 
   static create(data: ProofreaderActivity): Promise<ProofreaderActivities> {
-    return requestPost(`${questionApiBaseUrl}.json?question_type=${PROOFREADER_PASSAGE_TYPE}`, {question: data});
+    return requestPost(`${questionApiBaseUrl}.json?lesson_type=${PROOFREADER_PASSAGE_TYPE}`, {lesson: data});
   }
 
   static update(uid: string, data: ProofreaderActivity): Promise<ProofreaderActivity> {
-    return requestPut(`${questionApiBaseUrl}/${uid}.json`, {question: data});
+    return requestPut(`${questionApiBaseUrl}/${uid}.json?lesson_type=${PROOFREADER_PASSAGE_TYPE}`, {lesson: data});
   }
 
   static remove(uid: string): Promise<string> {
-    return requestDelete(`${questionApiBaseUrl}/${uid}.json`);
+    return requestDelete(`${questionApiBaseUrl}/${uid}.json?lesson_type=${PROOFREADER_PASSAGE_TYPE}`);
   }
 }
 
