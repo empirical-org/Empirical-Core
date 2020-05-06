dd7fcdedfcd382887d09a5164006dcd89bb55bb9
Haronil Estevez <haronil@quill.org>
2020-05-06 15:51:29 -0400

    Revert "comprehension activities api"
    This reverts commit 3f4446201e7c8411b7d86e1cf74dfced6258d293.

--- services/comprehension/main-api/comprehension/urls.py
+++ services/comprehension/main-api/comprehension/urls.py
@@ -1,5 +1,4 @@
 from django.conf.urls import url
-from django.urls import include, path
 
 from . import views
 from .views.activity import ActivityView
@@ -13,17 +12,6 @@ from .views.session_feedback_history_detail import (
     SessionFeedbackHistoryDetailView
 )
 from .models.rule_set import RuleSet
-from rest_framework import routers
-
-router = routers.DefaultRouter()
-router.register(r'activities_api', views.ActivityViewSet)
-
-# Wire up our API using automatic URL routing.
-# Additionally, we include login URLs for the browsable API.
-urlpatterns = [
-    path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
-]
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
@@ -49,6 +37,4 @@ urlpatterns = [
     url(r'^session_history/(?P<session_id>.*)$',
         SessionFeedbackHistoryDetailView.as_view(),
         name='get_session_feedback_history_detail'),
-    path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
