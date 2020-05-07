0f115f45e2d7319ee40ed4066c54417708f8c1f1
Haronil Estevez <haronil@quill.org>
2020-05-07 17:05:26 -0400

    update serializers.py

--- services/comprehension/main-api/comprehension/serializers.py
+++ services/comprehension/main-api/comprehension/serializers.py
@@ -1,8 +1,27 @@
 from rest_framework import serializers
-from .models.activity import Activity
+from .models.activity import Activity, ActivityPassage, ActivityPrompt
+from .models.passage import Passage
+from .models.prompt import Prompt
 
-
-class ActivitySerializer(serializers.HyperlinkedModelSerializer):
+#class ActivitySerializer(serializers.HyperlinkedModelSerializer):
+class ActivitySerializer(serializers.ModelSerializer):
     class Meta:
         model = Activity
-        fields = ['title', 'flags', 'passages', 'prompts']
+        #fields = ['title', 'flag', 'passages', 'prompts']
+        fields = ['id', 'title', 'flag', 'passages', 'prompts']
+
+class PassageSerializer(serializers.ModelSerializer):
+    activity_passage = ActivitySerializer(read_only=True)
+
+    class Meta:
+        #model = Passage
+        model = ActivityPassage
+        fields = ['text']
+
+class PromptSerializer(serializers.ModelSerializer):
+    activity_passage = ActivitySerializer(read_only=True, many=True)
+
+    class Meta:
+        #model = Prompt
+        model = ActivityPrompt
+        fields = ['id', 'text', 'max_attempts', 'max_attempts_feedback']
