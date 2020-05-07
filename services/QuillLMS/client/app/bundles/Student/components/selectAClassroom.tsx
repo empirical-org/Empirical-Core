cef43f452cddc359d59f14bf03c11810fb5d9415
Emilia Friedberg <emiliafriedberg@gmail.com>
2020-05-07 09:16:13 -0400

    teacher previewing version of student dashboard (#6310)
    * add routes for a teacher to start and stop previewing a student dash
    
    * add banner to student dashboard if being previewed
    
    * add account settings version of banner and mobile optimize both
    
    * make account settings page uneditable from preview mode
    
    * add modals for previewing activities and attempting to join class
    
    * update some snapshots
    
    * add tests
    
    * minor changes
    
    * linter fixes
    
    * test update
    
    Co-authored-by: hequill <55706216+hequill@users.noreply.github.com>

--- services/QuillLMS/client/app/bundles/Student/components/selectAClassroom.tsx
+++ services/QuillLMS/client/app/bundles/Student/components/selectAClassroom.tsx
@@ -2,12 +2,28 @@ import * as React from 'react'
 
 const addCircleSrc = `${process.env.CDN_URL}/images/icons/add-circle.svg`
 
-export default class SelectAClassroom extends React.Component<any, any> {
+export default class SelectAClassroom extends React.Component<any, { showJoinClassModal: boolean}> {
+  constructor(props) {
+    super(props)
+
+    this.state = { showJoinClassModal: false }
+  }
+
   componentDidMount() {
     document.title = 'Quill.org | Classes'
   }
-  
-  handleJoinClassClick = () => window.location.href = '/add_classroom'
+
+  handleJoinClassClick = () => {
+    const { isBeingPreviewed, } = this.props
+
+    if (isBeingPreviewed) {
+      this.setState({ showJoinClassModal: true })
+    } else {
+      window.location.href = '/add_classroom'
+    }
+  }
+
+  handleCloseJoinClassModalClick = () => this.setState({ showJoinClassModal: false, })
 
   renderClassroomCards = () => {
     const { classrooms, onClickCard, } = this.props
@@ -37,8 +53,31 @@ export default class SelectAClassroom extends React.Component<any, any> {
     </div>)
   }
 
+  renderJoinClassModal = () => {
+    const { showJoinClassModal, } = this.state
+
+    if (!showJoinClassModal) { return }
+
+    return (<div className="modal-container student-profile-modal-container">
+      <div className="modal-background" />
+      <div className="student-profile-modal quill-modal modal-body">
+        <div>
+          <h3 className="title">Sorry, only a student can join a class.</h3>
+        </div>
+        <div className="student-profile-modal-text">
+          <p>If you would like to add this student to another class, you can go back to your dashboard and invite them from the Classes page.</p>
+        </div>
+        <div className="form-buttons">
+          <a className="quill-button outlined secondary medium focus-on-light" href="/teachers/unset_preview_as_student?redirect=/teachers/classrooms">Classes page</a>
+          <button className="quill-button contained primary medium focus-on-light" onClick={this.handleCloseJoinClassModalClick} type="button">Continue viewing</button>
+        </div>
+      </div>
+    </div>)
+  }
+
   render() {
     return (<div className="container select-a-classroom-container">
+      {this.renderJoinClassModal()}
       <div className="select-a-classroom">
         <h1>Classes</h1>
         {this.renderClassroomCards()}
---
+++ services/QuillLMS/client/app/bundles/Teacher/components/student_profile/pinned_activity_modal.tsx
@@ -0,0 +1,28 @@
+import * as React from 'react'
+import activityLaunchLink from '../modules/generate_activity_launch_link.js';
+
+interface PinnedActivityModalProps {
+  activityId: string,
+  classroomUnitId: string,
+  name: string,
+  teacherName: string,
+  onClosePinnedActivityModalClick: () => void
+}
+
+const PinnedActivityModal = ({ name, teacherName, classroomUnitId, activityId, onClosePinnedActivityModalClick, }: PinnedActivityModalProps) => (<div className="modal-container pinned-activity-modal-container">
+  <div className="modal-background" />
+  <div className="pinned-activity-modal quill-modal modal-body">
+    <div>
+      <h3 className="title">Join: {name}</h3>
+    </div>
+    <div className="pinned-activity-modal-text">
+      <p>Your teacher, {teacherName}, has launched a live Quill Lesson.</p>
+    </div>
+    <div className="form-buttons">
+      <button className="quill-button outlined secondary medium focus-on-light" onClick={onClosePinnedActivityModalClick} type="button">Not now</button>
+      <a className="quill-button contained primary medium focus-on-light" href={activityLaunchLink(classroomUnitId, activityId)}>Join the lesson</a>
+    </div>
+  </div>
+</div>)
+
+export default PinnedActivityModal
