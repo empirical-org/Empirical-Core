b1796d72e8316e1cc7a10ad1c2691162e1974615
Daniel Drabik <dan@quill.org>
2020-05-13 17:31:01 -0400

    Make frontend deploys on post ‘FINISHED’ message.

--- scripts/post_slack_deploy.sh
+++ scripts/post_slack_deploy.sh
@@ -7,7 +7,7 @@
 username=$(git config user.name)
 if $4; then status='FINISHED'; else status='STARTED'; fi
 
-message=":ship: *$1 $2 Deploy* \`$status\`. *_$3_* branch, deployed by: $username"
+message=":ship: *$1 $2 Deploy* \`$status\`. branch: *_$3_*, deployed by: $username"
 webhook=$(heroku config:get SLACK_API_WEBHOOK --app empirical-grammar)
 
 curl -X POST -H 'Content-type: application/json' --data "{'text':'$message'}" $webhook
