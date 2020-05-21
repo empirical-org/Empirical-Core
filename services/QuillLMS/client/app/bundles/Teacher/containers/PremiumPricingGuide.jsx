e3fca277788b5f2ddf0a2d7944492977dd96278f
Emilia Friedberg <emiliafriedberg@gmail.com>
2020-05-21 09:34:04 -0400

    premium page updates (#6393)
    * premium page visual updates
    
    * css updates for the rest of the page for mobile
    
    * update test
    
    * handle extant subscriptions
    
    * small css changes
    
    * fix test
    
    * stop showing covid banner on July 31st; update test
    
    * one more adjustment
    
    * real final change
    
    * uncomment config

--- services/QuillLMS/client/app/bundles/Teacher/containers/PremiumPricingGuide.jsx
+++ services/QuillLMS/client/app/bundles/Teacher/containers/PremiumPricingGuide.jsx
@@ -7,7 +7,7 @@ import TeacherPremium from '../components/premium/teacher_premium.jsx';
 import SchoolPremium from '../components/premium/school_premium.jsx';
 import QuestionsAndAnswers from './QuestionsAndAnswers.jsx'
 
-export default class extends React.Component {
+export default class PremiumPricingGuide extends React.Component {
   subscribers = () => {
     return (
     [{ name: 'Achievement first school logo', source: '/images/subscribers/1_achievement.png', id: 'achievement-first'},
@@ -29,30 +29,44 @@ export default class extends React.Component {
     );
   };
 
+  renderCovidBanner() {
+    if (Date.now() >= Date.parse('7/31/2020')) { return }
+
+    return (<div className="covid-premium-banner">
+      <p>We&#39;re supporting schools impacted by closures by providing free Quill Premium access through the end of the 2019-20 school year (July 31, 2020)</p>
+      <p>
+        <a href="/subscriptions/activate_covid_subscription">Activate teacher premium</a> - <a href="https://forms.gle/ePA3C866hfKkN6BRA">Request free access to School and District Premium</a>
+      </p>
+    </div>)
+  }
+
   render() {
     return (
-      <div className="container" id="premium-pricing-guide">
-        <div className="overview text-center">
-          <div className="header">
-            <h1>Pricing Guide</h1>
-            <p>We’re here to support anyone impacted by school closures by providing every single teacher with free Premium.</p>
+      <div>
+        {this.renderCovidBanner()}
+        <div className="container" id="premium-pricing-guide">
+          <div className="overview text-center">
+            <div className="header">
+              <h1>Pricing Guide</h1>
+              <p>Save time grading and gain actionable insights with Quill Premium.</p>
+            </div>
+            <PremiumPricingMinisRow {...this.props} />
+            <DistrictPricingBox />
           </div>
-          <PremiumPricingMinisRow {...this.props} />
-          <DistrictPricingBox />
+
+          <div className="features text-center">
+            <TeacherPremium />
+            <SchoolPremium />
+          </div>
+
+          <SubscriberLogos subscribers={this.subscribers()} />
+          <TeacherTestimonial />
+
+          <QuestionsAndAnswers
+            questionsAndAnswersFile="premium"
+            supportLink="https://support.quill.org/quill-premium"
+          />
         </div>
-
-        <div className="features text-center">
-          <TeacherPremium />
-          <SchoolPremium />
-        </div>
-
-        <SubscriberLogos subscribers={this.subscribers()} />
-        <TeacherTestimonial />
-
-        <QuestionsAndAnswers
-          questionsAndAnswersFile="premium"
-          supportLink="https://support.quill.org/quill-premium"
-        />
       </div>
 
     );
