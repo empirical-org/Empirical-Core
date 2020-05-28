77cfb16d8ef3e74d32fce5dc9f08f561c4c292ef
Cissy Yu <cissy@quill.org>
2020-05-28 11:00:46 -0400

    added tests

--- packages/quill-marking-logic/src/libs/matchers/required_words_match.spec.ts
+++ packages/quill-marking-logic/src/libs/matchers/required_words_match.spec.ts
@@ -11,6 +11,14 @@ const savedResponses: Array<Response> = [
     optimal: true,
     count: 1,
     question_uid: 'question 1'
+  },
+  {
+    id: 2,
+    text: "My dog took a Nap.",
+    feedback: "Good job, that's a sentence!",
+    optimal: true,
+    count: 1,
+    question_uid: 'question 1'
   }
 ]
 
@@ -30,7 +38,7 @@ describe('The requiredWordsChecker', () => {
     const partialResponse: PartialResponse =  {
         feedback: requiredWordsMatch(responseString, savedResponses).feedback,
         author: 'Required Words Hint',
-        parent_id: 1,
+        parent_id: 2,
         concept_results: [
           conceptResultTemplate('mdFUuuNR7N352bbMw4Mj9Q')
         ]
@@ -44,6 +52,11 @@ describe('The requiredWordsChecker', () => {
   it('Should return undefined if the response string is not missing a required word', () => {
     const responseString = "My dog took a nap.";
     assert.equal(requiredWordsChecker(responseString, savedResponses), undefined);
+  });
+
+  it('Should return the lowercased missing word if optimal responses contain both uppercase and lowercase', () => {
+    const responseString = "My dog took a.";
+    assert.notEqual(requiredWordsChecker(responseString, savedResponses).feedback.indexOf('nap'), -1);
   });
 
 })
