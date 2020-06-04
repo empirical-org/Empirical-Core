0e9bd87949c31fd04c78ea50c38ff6942e71109f
Cissy Yu <cissy@quill.org>
2020-06-04 13:21:07 -0400

    typo

--- services/QuillLMS/db/migrate/20200603171807_make_data_json_blob.rb
+++ services/QuillLMS/db/migrate/20200603171807_make_data_json_blob.rb
@@ -1,5 +1,5 @@
 class MakeDataJsonBlob < ActiveRecord::Migration
   def change
-    change_column :activity, :data, 'jsonb USING CAST(field_name AS jsonb)'
+    change_column :activities, :data, 'jsonb USING CAST(field_name AS jsonb)'
   end
 end
