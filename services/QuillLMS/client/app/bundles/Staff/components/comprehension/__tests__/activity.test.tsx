ca95d37fe37c271baffcec3ba5c9d280a1966286
Eric Adams <erictayloradams@gmail.com>
2020-06-05 13:56:11 -0500

    fix test

--- services/QuillLMS/client/app/bundles/Staff/components/comprehension/__tests__/activity.test.tsx
+++ services/QuillLMS/client/app/bundles/Staff/components/comprehension/__tests__/activity.test.tsx
@@ -10,11 +10,7 @@ describe('Activity component', () => {
     </MemoryRouter>
   );
 
-<<<<<<< HEAD
-  it('should render 3 NavLinks', () => {
-=======
   it('should render 6 NavLinks', () => {
->>>>>>> 2cefd93296c048018da60e10425af34e1f0fe49a
     expect(container.find(NavLink).length).toEqual(6);
   });
 });
