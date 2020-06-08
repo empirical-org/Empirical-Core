47fc0475323d341ef156b34a43310e30aea439ca
Cissy Yu <cissy@quill.org>
2020-06-08 13:26:36 -0400

    fix anomlous migration files

--- services/QuillLMS/db/migrate/20200604165331_migrate_lessons_to_activity.rb
+++ services/QuillLMS/db/migrate/20200604165331_migrate_lessons_to_activity.rb
@@ -3,11 +3,34 @@ class MigrateLessonsToActivity < ActiveRecord::Migration
     # Before running this part, make sure that the inconsistent names in this
     # spreadsheet are resolved:
     # https://docs.google.com/spreadsheets/d/1mSXhH0K_cI3aKY7pbO15qnDciBqomwMAueJmBIni8Zo/edit#gid=1194313690
+    lessons_to_archive = [
+                          '-KSIwNNwPar51hq9bNIw',
+                          '-LJ_F7B64Mn4YtIRE-Sk',
+                          '-LKldebIl3TApTzAWgBM',
+                          '-KKxoMfLSawnlMtZUXC6',
+                          '-LKldaleWFYhQN2eVJ7T',
+                          '-KW_2fEvZBJB3GKYpzK2'
+                        ]
+    lessons_to_alpha = [
+                         '-LKldZKSHx3hP9_2Vv5v',
+                         '-LKldYuOrBMBpfujBbjg',
+                         '-LKld_Wf0lzgNnEvY5aA'
+                       ]
+
+    Lesson.where(uid: lessons_to_archive).each do |lesson|
+      lesson[:data]["flag"] = "archived"
+      lesson.save!
+    end
+
+    Lesson.where(uid: lessons_to_alpha).each do |lesson|
+      lesson[:data]["flag"] = "alpha"
+      lesson.save!
+    end
 
     Lesson.all.each do |lesson|
       activity = Activity.find_by(uid: lesson.uid)
       if activity.blank?
-        activity = Activity.new(:name=> lesson[:data]["name"], :uid=>lesson.uid, :flags=>[lesson[:data]["flag"]])
+        activity = Activity.new(:name=> lesson[:data]["name"], :uid=>lesson.uid)
 
         if lesson.lesson_type == Lesson::TYPE_CONNECT_LESSON
           activity.activity_classification_id = 5
