7f46557beb5fc7da77becf75b95da650a4457059
Emilia Friedberg <emiliafriedberg@gmail.com>
2020-06-22 09:10:18 -0400

    pull consts from projectedanswer outside of component

--- services/QuillLessons/app/components/classroomLessons/play/projectedAnswers.tsx
+++ services/QuillLessons/app/components/classroomLessons/play/projectedAnswers.tsx
@@ -1,44 +1,44 @@
 import * as React from 'react'
 
+const renderYourAnswer = (projector, response) => {
+  if (projector || !response) { return }
+
+  return (<div className="your-answer-container">
+    <p className="answer-header">Your response</p>
+    <p className="your-answer" dangerouslySetInnerHTML={{ __html: response}} />
+  </div>)
+}
+
+const renderClassAnswersList = (selectedSubmissionOrder, submissions, sampleCorrectAnswer) => {
+  const selected = selectedSubmissionOrder ? selectedSubmissionOrder.map((key, index) => {
+    let text
+    if (submissions && submissions[key] && submissions[key].data) {
+      text = submissions[key].data
+    } else if (key === 'correct' && sampleCorrectAnswer){
+      text = sampleCorrectAnswer
+    } else {
+      text = ''
+    }
+    return <li dangerouslySetInnerHTML={{ __html: text}} key={index} />
+  }) : null;
+  return (
+    <ol className="class-answer-list">
+      {selected}
+    </ol>
+  )
+}
+
 const ProjectedAnswers = ({ selectedSubmissions, selectedSubmissionOrder, projector, response, sampleCorrectAnswer, submissions, }) => {
-  const renderYourAnswer = () => {
-    if (projector || !response) { return }
-
-    return (<div className="your-answer-container">
-      <p className="answer-header">Your response</p>
-      <p className="your-answer" dangerouslySetInnerHTML={{ __html: response}} />
-    </div>)
-  }
-
-  const renderClassAnswersList = () => {
-    const selected = selectedSubmissionOrder ? selectedSubmissionOrder.map((key, index) => {
-      let text
-      if (submissions && submissions[key] && submissions[key].data) {
-        text = submissions[key].data
-      } else if (key === 'correct' && sampleCorrectAnswer){
-        text = sampleCorrectAnswer
-      } else {
-        text = ''
-      }
-      return <li dangerouslySetInnerHTML={{ __html: text}} key={index} />
-    }) : null;
-    return (
-      <ol className="class-answer-list">
-        {selected}
-      </ol>
-    )
-  }
-
   let classAnswers
   if (selectedSubmissions && selectedSubmissionOrder) {
     classAnswers = (<div>
       <p className="answer-header">Class responses</p>
-      {renderClassAnswersList()}
+      {renderClassAnswersList(selectedSubmissionOrder, submissions, sampleCorrectAnswer)}
     </div>)
   }
   return (
     <div className="display-mode">
-      {renderYourAnswer()}
+      {renderYourAnswer(projector, response)}
       {classAnswers}
     </div>
   );
