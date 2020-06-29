663016d1a7816b3f5182e27fc7809e38e6e84a15
Emilia Friedberg <emiliafriedberg@gmail.com>
2020-06-29 09:04:24 -0400

    lessons fill in the list and multistep (#6477)
    * package lock updates
    
    * semicolon/whitespace changes
    
    * delete files that are not in use
    
    * rename unsafe lifecycles; delete unused files
    
    * remove unnecessary React import
    
    * run the class codemod; delete updated files that aren't in use
    
    * update .babelrc to get the tests to pass
    
    * fix linter error
    
    * make sure to update unsafe lifecycle methods in javascript files too
    
    * package upgrades
    
    * get new routing structure beginning to work
    
    * get all admin pages working
    
    * wip
    
    * customization seems to be working
    
    * student view stuff is working
    
    * update how tests get run
    
    * lint and circle ci fix
    
    * fix issue where circle ci fails with no tests
    
    * delete that line
    
    * update React classes in Quill Lessons (#6416)
    
    * remove unnecessary React import
    
    * run the class codemod; delete updated files that aren't in use
    
    * update .babelrc to get the tests to pass
    
    * fix linter error
    
    * final step of React Lessons upgrade (#6421)
    
    * package upgrades
    
    * get new routing structure beginning to work
    
    * get all admin pages working
    
    * wip
    
    * customization seems to be working
    
    * student view stuff is working
    
    * update how tests get run
    
    * lint and circle ci fix
    
    * fix issue where circle ci fails with no tests
    
    * delete that line
    
    * update lessons navbar; standardize widths
    
    * add test for student lessons navbar
    
    * add image files and update watch teacher modal
    
    * add image files and update watch teacher modal
    
    * lobby slides for student and projector view
    
    * add test for lobby and new snapshots
    
    * fix bug with rendering projector before any students have joined
    
    * also don't try to render if students is null
    
    * make sure everything on the projector lobby view is #000
    
    * update scss for student static page
    
    * inactive single answer
    
    * active and submitted views are done
    
    * get left and right buttons for projector styled
    
    * get displaying answers looking and working correctly
    
    * focus state for show/hide buttons
    
    * fix sidebar clicking
    
    * rotate left arrow
    
    * wip
    
    * minor revisions
    
    * tests for days
    
    * fix prop sorting
    
    * handle spoofing window location a little differently
    
    * get tests passing consistently (I think)
    
    * all non-mobile view
    
    * one more styling change
    
    * some minor changes for mobile
    
    * update existing tests
    
    * revisions
    
    * add a bunch of tests for smaller components
    
    * add tests for fill in blank component itself
    
    * wip
    
    * don't show cues if there are none where the string has length
    
    * wip
    
    * clean up
    
    * wip
    
    * wip
    
    * wip
    
    * retrying answers works
    
    * displaying answers selectively
    
    * some final changes
    
    * make sure the responses can be saved to the LMS correctly
    
    * some revisions
    
    * feat/lessons-fill-in-the-list
    
    * more changes I caught in review
    
    * update existing tests
    
    * new tests
    
    * just do checked instead of checked === true
    
    * minor changes
    
    * revisions
    
    * lessons multistep (#6484)
    
    * get multistep inactive - review ready to go
    
    * get review student row to handle multistep
    
    * finish displayed answers for multistep
    
    * tests

---
+++ services/QuillLessons/app/components/classroomLessons/play/__tests__/listBlanks.test.jsx
@@ -0,0 +1,76 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import toJson from 'enzyme-to-json';
+
+import ListBlanks, { PROJECT } from '../listBlanks';
+import { listBlanksProps } from './data'
+
+const submissions = {
+  student: {
+    data: {
+      0: "Skip",
+      1: "Run",
+      2: "Jump"
+    },
+    timestamp: "2020-06-23T14:56:41.181Z"
+  }
+}
+
+const selectedSubmissionOrder = ["student#0", "student#2"]
+
+const selectedSubmissions = {"student#0": true, "student#2": true}
+
+describe('ListBlanks component', () => {
+  describe('student view', () => {
+    const mockGetParameterByName = jest.fn().mockReturnValue('student')
+    jest.mock('../../../../libs/getParameterByName', () => ({
+      default: mockGetParameterByName
+    }))
+
+    describe('inactive', () => {
+      it('renders', () => {
+        const wrapper = mount(<ListBlanks {...listBlanksProps} />)
+        expect(toJson(wrapper)).toMatchSnapshot()
+      })
+    })
+
+    describe('when the student has submitted an answer', () => {
+      it('renders', () => {
+        const wrapper = mount(<ListBlanks {...listBlanksProps} submissions={submissions} />)
+        expect(toJson(wrapper)).toMatchSnapshot()
+      })
+    })
+
+    describe('when the student has submitted an answer and the teacher is having them retry part of it', () => {
+      it('renders', () => {
+        const retrySubmissions = {...submissions}
+        retrySubmissions.student.data[1] = ''
+        const wrapper = mount(<ListBlanks {...listBlanksProps} submissions={retrySubmissions} />)
+        expect(toJson(wrapper)).toMatchSnapshot()
+      })
+    })
+
+    describe('when an answer is being displayed', () => {
+      it('renders', () => {
+        const wrapper = mount(<ListBlanks {...listBlanksProps} mode={PROJECT} selected_submission_order={selectedSubmissionOrder} selected_submissions={selectedSubmissions} submissions={submissions} />)
+        expect(toJson(wrapper)).toMatchSnapshot()
+      })
+    })
+  })
+
+  describe('projector view', () => {
+    describe('inactive', () => {
+      it('renders', () => {
+        const wrapper = mount(<ListBlanks {...listBlanksProps} projector={true} />)
+        expect(toJson(wrapper)).toMatchSnapshot()
+      })
+    })
+
+    describe('when an answer is being displayed', () => {
+      it('renders', () => {
+        const wrapper = mount(<ListBlanks {...listBlanksProps} mode={PROJECT} projector={true} selected_submission_order={selectedSubmissionOrder} selected_submissions={selectedSubmissions} submissions={submissions} />)
+        expect(toJson(wrapper)).toMatchSnapshot()
+      })
+    })
+  })
+})
--- services/QuillLessons/app/components/classroomLessons/play/__tests__/projectedAnswers.test.jsx
+++ services/QuillLessons/app/components/classroomLessons/play/__tests__/projectedAnswers.test.jsx
@@ -4,7 +4,7 @@ import toJson from 'enzyme-to-json';
 
 import ProjectedAnswers from '../projectedAnswers';
 
-const props = {
+const singleSubmissionProps = {
   response: "We waited in line at the store. <strong>The</strong> line was very long!",
   selectedSubmissionOrder: ["student"],
   selectedSubmissions: {
@@ -18,16 +18,54 @@ const props = {
   }
 }
 
+const multipleSubmissionProps = {
+  response: {
+    0: "Skip",
+    1: "Run",
+    2: "Jump"
+  },
+  selectedSubmissionOrder: ["student#0", "student#2"],
+  selectedSubmissions: {
+    "student#0": true,
+    "student#2": true
+  },
+  submissions: {
+    student: {
+      data: {
+        0: "Skip",
+        1: "Run",
+        2: "Jump"
+      },
+      timestamp: "2020-06-23T14:56:41.181Z"
+    }
+  }
+}
+
 describe('ProjectedAnswers component', () => {
 
-  it('renders on the student screen', () => {
-    const wrapper = mount(<ProjectedAnswers {...props} />)
-    expect(toJson(wrapper)).toMatchSnapshot()
+  describe("when each student submits a single response", () => {
+    it('renders on the student screen', () => {
+      const wrapper = mount(<ProjectedAnswers {...singleSubmissionProps} />)
+      expect(toJson(wrapper)).toMatchSnapshot()
+    })
+
+    it('renders on the projector screen', () => {
+      const wrapper = mount(<ProjectedAnswers {...singleSubmissionProps} projector={true} response={null} />)
+      expect(toJson(wrapper)).toMatchSnapshot()
+    })
   })
 
-  it('renders on the projector screen', () => {
-    const wrapper = mount(<ProjectedAnswers {...props} projector={true} response={null} />)
-    expect(toJson(wrapper)).toMatchSnapshot()
+  describe("when each student submits multiple responses", () => {
+    it('renders on the student screen', () => {
+      const wrapper = mount(<ProjectedAnswers {...multipleSubmissionProps} />)
+      expect(toJson(wrapper)).toMatchSnapshot()
+    })
+
+    it('renders on the projector screen', () => {
+      const wrapper = mount(<ProjectedAnswers {...multipleSubmissionProps} projector={true} response={null} />)
+      expect(toJson(wrapper)).toMatchSnapshot()
+    })
   })
+
 
 })
