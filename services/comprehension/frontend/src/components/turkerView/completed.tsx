64218b6149dddd07d32c4b33f4064fc8529f6bc4
Eric Adams <erictayloradams@gmail.com>
2020-06-29 17:23:18 -0500

    add completion page

--- services/comprehension/frontend/src/components/turkerView/completed.tsx
+++ services/comprehension/frontend/src/components/turkerView/completed.tsx
@@ -3,7 +3,12 @@ import * as React from "react";
 export const TurkCompleted = () => {
   return(
     <div className="turk-completed-container">
-      completed!
+      <h1>Activity Completed!</h1>
+      <p>Thank you for completing this activity.</p>
+      <p>Your responses will be used to develop our system so that millions of students around the world can improve their reading and writing skills.</p>
+      <h1>Payment Directions</h1>
+      <p>Use the following HIT code for payment:</p>
+      <div className="code">{Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15)}</div>
     </div>
   );
 }
