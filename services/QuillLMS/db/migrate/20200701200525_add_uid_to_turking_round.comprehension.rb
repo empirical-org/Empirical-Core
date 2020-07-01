911a78e2a2acb135c4639e40e80e5778624a28e6
Thomas Robertson <thomas.robertson@quill.org>
2020-07-01 16:11:59 -0400

    Fix linting error in migrations

--- services/QuillLMS/db/migrate/20200701200525_add_uid_to_turking_round.comprehension.rb
+++ services/QuillLMS/db/migrate/20200701200525_add_uid_to_turking_round.comprehension.rb
@@ -3,5 +3,5 @@ class AddUidToTurkingRound < ActiveRecord::Migration
   def change
     add_column :comprehension_turking_rounds, :uuid, :uuid
     add_index :comprehension_turking_rounds, :uuid, unique: true
- end
+  end
 end
