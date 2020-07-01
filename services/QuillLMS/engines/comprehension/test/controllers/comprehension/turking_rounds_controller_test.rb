66487f6539bd7236b89a69bc35f41818680c1f84
Thomas Robertson <thomas.robertson@quill.org>
2020-07-01 10:16:12 -0400

    Fix a linting error around indentation

--- services/QuillLMS/engines/comprehension/test/controllers/comprehension/turking_rounds_controller_test.rb
+++ services/QuillLMS/engines/comprehension/test/controllers/comprehension/turking_rounds_controller_test.rb
@@ -7,7 +7,7 @@ module Comprehension
     end
 
     context "index" do
-     should "return successfully - no turking_round" do
+      should "return successfully - no turking_round" do
         get :index
 
         parsed_response = JSON.parse(response.body)
