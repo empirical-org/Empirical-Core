01d52bf3c116cfbaa008e174d1729d3eba8dde19
Anathomical <thomas.robertson@quill.org>
2020-07-06 09:44:46 -0400

    Lms/migrate turking round (#6500)
    * Add Turking Round model to Comprehension engine
    
    * Install new engine migrations to the main LMS app
    
    * Mark private functions as private individually
    
    * Add module namespacing to Turking controller
    
    * Fix a linting error around indentation
    
    * Updates in response to PR feedback
    
    * Fix linting error
    
    * Add uuid value to TurkingRound model
    
    So we can utilize a non-sequential ID for public URLs, and so we have a pseudo-random string to use for turkers to send as a completion confirmation
    
    * Fix linting error in migrations
    
    * Some tweaks based on additional PR feedback
    
    * Refactor some of our assert_equals...false to refutes

---
+++ services/QuillLMS/engines/comprehension/app/models/comprehension/turking_round.rb
@@ -0,0 +1,36 @@
+module Comprehension
+  class TurkingRound < ActiveRecord::Base
+    attr_readonly :uuid
+
+    before_validation :set_default_uuid, on: :create
+
+    belongs_to :activity, inverse_of: :turking_rounds
+
+    validates_presence_of :activity_id
+    validates_presence_of :uuid, on: :update
+    validates :uuid, uniqueness: true
+    validates :expires_at, presence: true
+    validate :expires_at_in_future, on: :create
+
+    def serializable_hash(options = nil)
+      options ||= {}
+
+      super(options.reverse_merge(
+        only: ['id', 'uuid', 'activity_id', 'expires_at']
+      ))
+    end
+
+    def expire!
+      update_attributes(expires_at: Time.zone.now)
+    end
+
+    private def expires_at_in_future
+      return if expires_at.blank?
+      errors.add(:expires_at, 'must be in the future') unless expires_at > Time.zone.now
+    end
+
+    private def set_default_uuid
+      self.uuid ||= SecureRandom.uuid
+    end
+  end
+end
