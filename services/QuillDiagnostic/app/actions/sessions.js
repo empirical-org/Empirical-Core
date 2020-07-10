a9858f88ee42e0271c56c26aa23edad5f3a45e43
Thomas Robertson <thomas.robertson@quill.org>
2020-07-10 13:07:46 -0400

    Refactor Diagnostic to read and write active session data to LMS

--- services/QuillDiagnostic/app/actions/sessions.js
+++ services/QuillDiagnostic/app/actions/sessions.js
@@ -1,33 +1,33 @@
 import rootRef from '../libs/firebase';
 import _ from 'lodash';
 
+import { SessionApi } from '../libs/sessions_api';
+
 const C = require('../constants').default;
 
 const sessionsRef = rootRef.child('savedSessions');
 
 export default {
   get(sessionID, cb) {
-    sessionsRef.child(sessionID).once('value', (snapshot) => {
-      if (snapshot.exists()) {
-        const session = snapshot.val();
-        if (session.currentQuestion) {
-          if (session.currentQuestion.question) {
-            session.currentQuestion.question.attempts = [];
-          } else {
-            session.currentQuestion.data.attempts = [];
-          }
+    SessionApi.get(sessionID).then((session) => {
+      const processed_session = process_session(session)
+      cb(processed_session)
+    }).catch((error) => {
+      sessionsRef.child(sessionID).once('value', (snapshot) => {
+        if (snapshot.exists()) {
+          const session = snapshot.val();
+          const processed_session = process_session(session)
+          cb(processed_session);
         }
-        session.unansweredQuestions ? true : session.unansweredQuestions = [];
-        cb(session);
-      }
-    });
+      })
+    })
   },
 
   update(sessionID, session) {
     const cleanSession = _.pickBy(session);
     const cleanedSession = JSON.parse(JSON.stringify(cleanSession));
     delete_null_properties(cleanedSession, true);
-    sessionsRef.child(sessionID).set(cleanedSession);
+    SessionApi.update(sessionID, cleanedSession);
   },
 
   delete(sessionID) {
@@ -35,6 +35,18 @@ export default {
   },
 
 };
+
+function process_session(session) {
+  if (session.currentQuestion) {
+    if (session.currentQuestion.question) {
+      session.currentQuestion.question.attempts = [];
+    } else {
+      session.currentQuestion.data.attempts = [];
+    }
+  }
+  session.unansweredQuestions ? true : session.unansweredQuestions = [];
+  return session
+}
 
 function delete_null_properties(test, recurse) {
   for (const i in test) {
