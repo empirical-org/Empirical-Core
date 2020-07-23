803623d1effc351d979cd7518e548297dca6271b
Anathomical <thomas.robertson@quill.org>
2020-07-23 13:06:56 -0400

    Proofreader/use lms for sessions (#6550)
    * Tweak ActiveActivityRecord behavior to support shared sessions
    
    While Connect and Diagnostic use unique UIDs for their sessions, Proofreader and Grammar share UIDs as a way of bridging Proofreader activities directly into Grammar activities.  Now that all sessions are stored in the same place, we need to support that behavior here.
    
    * Refactor Grammar to pull sessions from LMS instead of Firebase
    
    * Refactor Proofreader to use LMS for sessions instead of Firebase
    
    * Fix a couple of typos
    
    * Update base URL for Grammar sessions libs
    
    * Tweak actions to use dispatch wrappers
    
    * Remove an old commented-out bit of code

---
+++ services/QuillLMS/client/app/bundles/Proofreader/lib/sessions_api.ts
@@ -0,0 +1,22 @@
+import { requestDelete, requestGet, requestPost, requestPut } from './request';
+
+const sessionApiBaseUrl = `${process.env.DEFAULT_URL}/api/v1/active_activity_sessions`;
+
+class SessionApi {
+  static get(uid: string): Promise<object> {
+    return requestGet(`${sessionApiBaseUrl}/${uid}.json`);
+  }
+
+  static update(uid: string, data: object): Promise<object> {
+    return requestPut(`${sessionApiBaseUrl}/${uid}.json`, {active_activity_session: data});
+  }
+
+  static remove(uid: string): Promise<string> {
+    return requestDelete(`${sessionApiBaseUrl}/${uid}.json`);
+  }
+}
+
+export {
+  SessionApi,
+  sessionApiBaseUrl
+}
