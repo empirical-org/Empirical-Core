4d2e757f783a07906eff6c6b627bf27dc0f6d864
Emilia Friedberg <emiliafriedberg@gmail.com>
2020-07-27 14:43:06 -0400

    lint ap file

--- services/QuillLMS/client/app/bundles/Teacher/components/college_board/ap.tsx
+++ services/QuillLMS/client/app/bundles/Teacher/components/college_board/ap.tsx
@@ -20,9 +20,9 @@ const Ap = ({ isPartOfAssignmentFlow, }: ApContainerProps) => {
       <div className="container pre-ap-header-container">
         <div className="header-left">
           <div className="logo-container">
-            <img alt="College Board logo" src="https://assets.quill.org/images/college_board/college-board-logo.svg"/>
-            <div className="divider"></div>
-            <img alt="Quill logo" src="https://assets.quill.org/images/logos/quill-logo-green.svg"/>
+            <img alt="College Board logo" src="https://assets.quill.org/images/college_board/college-board-logo.svg" />
+            <div className="divider" />
+            <img alt="Quill logo" src="https://assets.quill.org/images/logos/quill-logo-green.svg" />
           </div>
           <div className="header-text-container">
             <h1>Official AP Writing Practice</h1>
@@ -30,13 +30,13 @@ const Ap = ({ isPartOfAssignmentFlow, }: ApContainerProps) => {
             {getStartedButton}
           </div>
         </div>
-        <img alt="Photograph of a teacher talking to a classroom of high school students" src="https://assets.quill.org/images/college_board/teaching-english-practice.png"/>
+        <img alt="Photograph of a teacher talking to a classroom of high school students" src="https://assets.quill.org/images/college_board/teaching-english-practice.png" />
       </div>
     </div>
     <div className="white-section-wrapper">
       <div className="container pre-ap-activities-section">
         <div className="header">
-          <img alt="Illustration of a pencil drawing a line" src="https://assets.quill.org/images/college_board/sentence-writing-pencil.svg"/>
+          <img alt="Illustration of a pencil drawing a line" src="https://assets.quill.org/images/college_board/sentence-writing-pencil.svg" />
           <div className="text-container">
             <h2>Sentence-Level Writing Practice</h2>
             <p>Identify which sentence-level skills your students need to practice with the AP Writing Skills Surveys. Then, assign activities recommended for each student based on their survey responses so they can practice and improve their proficiency with those skills.</p>
@@ -62,7 +62,7 @@ const Ap = ({ isPartOfAssignmentFlow, }: ApContainerProps) => {
     <div className="white-section-wrapper" id="info-blurbs-1-wrapper">
       <div className="container info-blurbs-section">
         <div className="info-blurb-container">
-          <img alt="A recommended activity pack report showing four students being recommended a mixture of activities for relative clauses and participial phrases." src="https://assets.quill.org/images/college_board/ap-recommendations.svg"/>
+          <img alt="A recommended activity pack report showing four students being recommended a mixture of activities for relative clauses and participial phrases." src="https://assets.quill.org/images/college_board/ap-recommendations.svg" />
           <div className="text-container">
             <p className="info-blurb-header">Personalized Recommendations</p>
             <p className="info-blurb-text">After students complete the AP Writing Skills Survey, you'll receive recommendations for each student based on their responses and tailored to their individual needs. Each student will be recommended up to 45 sentence combining activities, grouped by concept into seven packs, that provide meaningful, targeted practice. You can assign all the activities with one click, or you can pick and choose.</p>
@@ -73,10 +73,10 @@ const Ap = ({ isPartOfAssignmentFlow, }: ApContainerProps) => {
             <p className="info-blurb-header">Writing with Targeted Feedback</p>
             <p className="info-blurb-text">Students receive immediate feedback that guides them through the revision process, pushing them to think critically about their writing and make revisions that ultimately lead to strong, sophisticated, and grammatically accurate sentences. The feedback inspires students to strengthen their sentence structure, but also supports them in editing for mechanics such as punctuation and spelling.</p>
           </div>
-          <img alt="An illustration showing a student combining two sentences, getting feedback from Quill, and then getting a correct response." src="https://assets.quill.org/images/college_board/ap-additional-practice.svg"/>
+          <img alt="An illustration showing a student combining two sentences, getting feedback from Quill, and then getting a correct response." src="https://assets.quill.org/images/college_board/ap-additional-practice.svg" />
         </div>
         <div className="info-blurb-container">
-          <img alt="A report showing that a student scored 79% on parallel structure, 98% on parallel structure with joining words, and 58% on advanced parallel structure." src="https://assets.quill.org/images/college_board/ap-reports.svg"/>
+          <img alt="A report showing that a student scored 79% on parallel structure, 98% on parallel structure with joining words, and 58% on advanced parallel structure." src="https://assets.quill.org/images/college_board/ap-reports.svg" />
           <div className="text-container">
             <p className="info-blurb-header">Data Reports</p>
             <p className="info-blurb-text">You can monitor the progress of your students and continue to identify areas of need and areas of strength through multiple data reports. Use the analysis report to review your students' work sentence-by-sentence, or use the summary report to get a high-level sense of where your students could use some extra support.</p>
@@ -91,12 +91,12 @@ const Ap = ({ isPartOfAssignmentFlow, }: ApContainerProps) => {
           <p className="cb-message-sub-header">Message from College Board</p>
         </div>
         <div className="quote-container">
-          <img src="https://assets.quill.org/images/college_board/exaggerated-quote-left.svg" id="first-quote"/>
+          <img id="first-quote" src="https://assets.quill.org/images/college_board/exaggerated-quote-left.svg" />
           <div className="inner-text-container">
             <p>Teaching writing is hard. Even harder can be attempting to pinpoint exactly when we learned how and why to write clearly. That’s why the College Board has worked with Quill.org to ensure that this new, free, comprehensive offering helps students practice targeted sentence construction skills in their writing. Sustained practice and exposure to targeted skills is important to improvement and comfort.</p>
             <p>Teaching writing may always be hard, but student practice in meaningful skills can make the learning that much clearer. Through this practice, students can begin to own the how and why of their writing.</p>
           </div>
-          <img src="https://assets.quill.org/images/college_board/exaggerated-quote-right.svg" id="second-quote"/>
+          <img id="second-quote" src="https://assets.quill.org/images/college_board/exaggerated-quote-right.svg" />
         </div>
       </div>
     </div>
