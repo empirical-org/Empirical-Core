14e27b56d2beba011f4302a4fe79ee760ef0b564
Emilia Friedberg <emiliafriedberg@gmail.com>
2020-07-27 15:19:00 -0400

    add new college board mini to homepge

---
+++ services/QuillLMS/client/app/bundles/Teacher/components/dashboard/college_board_mini.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react'
+
+const CollegeBoardMini = () => (
+  <div className="mini_container results-overview-mini-container col-md-4 col-sm-5 text-center college-board-mini">
+    <div className="mini_content">
+      <h3>AP and Pre-AP Activities <span className="new-tag">NEW</span></h3>
+      <p>The College Board has partnered with Quill to provide free and open source activities for high school students.</p>
+      <p>These activities cover key writing skills needed for AP exams, as well as activities based on Pre-AP texts.</p>
+      <a className="bg-quillgreen text-white" href="/assign/college-board">Explore the Activities</a>
+    </div>
+  </div>
+)
+
+export default CollegeBoardMini
