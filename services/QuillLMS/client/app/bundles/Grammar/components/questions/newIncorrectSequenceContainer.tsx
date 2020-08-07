3b8989e7af892ca7df4b06d891a011793c17c98b
Emilia Friedberg <emiliafriedberg@gmail.com>
2020-08-07 13:21:09 -0400

    add comment

--- services/QuillLMS/client/app/bundles/Grammar/components/questions/newIncorrectSequenceContainer.tsx
+++ services/QuillLMS/client/app/bundles/Grammar/components/questions/newIncorrectSequenceContainer.tsx
@@ -14,6 +14,7 @@ class NewIncorrectSequencesContainer extends React.Component {
   submitSequenceForm = (data) => {
     const { dispatch, match, history, } = this.props
     delete data.conceptResults.null;
+    // the only difference in the route between this page and the one where you can see all the incorrect sequences is the `/new` at the end of the path, so removing that will send us back to the main list
     const url = match.url.replace('/new', '')
     const callback = () => history.push(url)
     dispatch(questionActions.submitNewIncorrectSequence(match.params.questionID, data, callback))
