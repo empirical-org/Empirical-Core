d22146237364bd262ff4faa9d5fe78ee801af9a2
Emilia Friedberg <emiliafriedberg@gmail.com>
2020-08-10 15:09:38 -0400

    delete duplicate demo destroyer file and fix one in use

--- services/QuillLMS/lib/report_demo_destroyer.rb
+++
@@ -1,9 +0,0 @@
-module ReportDemoDestroyer
-  def self.destroy_demo(name)
-    email = name ? "hello+#{name}@quill.org" : "hello+demoteacher@quill.org"
-    teacher  = User.find_by_email(email)
-    SalesContact.where(user_id: teacher.id).destroy_all
-    Notification.where(user_id: teacher.id).destroy_all
-    teacher.destroy
-  end
-end
