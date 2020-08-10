6a0aae2e2d1b77893bbb2d38ac4ea7db75ffd4a7
Emilia Friedberg <emiliafriedberg@gmail.com>
2020-08-10 15:00:30 -0400

    delete associated notifications and sales contact records w/ demo user

--- services/QuillLMS/lib/report_demo_destroyer.rb
+++ services/QuillLMS/lib/report_demo_destroyer.rb
@@ -2,6 +2,8 @@ module ReportDemoDestroyer
   def self.destroy_demo(name)
     email = name ? "hello+#{name}@quill.org" : "hello+demoteacher@quill.org"
     teacher  = User.find_by_email(email)
+    SalesContact.where(user_id: teacher.id).destroy_all
+    Notification.where(user_id: teacher.id).destroy_all
     teacher.destroy
   end
 end
