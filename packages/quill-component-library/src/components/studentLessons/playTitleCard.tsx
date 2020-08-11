5ad8bb5f54e70413f5f46bd76c31173e271a5a40
Emilia Friedberg <emiliafriedberg@gmail.com>
2020-08-11 13:42:40 -0400

    button copy updates

--- packages/quill-component-library/src/components/studentLessons/playTitleCard.tsx
+++ packages/quill-component-library/src/components/studentLessons/playTitleCard.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 
-const PlayTitleCard = ({ data, handleContinueClick, }) => (
+const PlayTitleCard = ({ data, handleContinueClick, isLastQuestion, }) => (
   <div className="landing-page">
     <div className="landing-page-html" dangerouslySetInnerHTML={{__html: data.content}} />
     <button className="quill-button focus-on-light large primary contained" onClick={handleContinueClick} type="button">
-      Continue
+      {isLastQuestion ? 'Next' : 'Continue'}
     </button>
   </div>
 )
