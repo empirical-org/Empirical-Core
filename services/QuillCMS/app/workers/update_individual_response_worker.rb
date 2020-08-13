868ab7b6101e94626774e8e3d7d4f65664b57fe6
Cissy Yu <cissy@quill.org>
2020-08-13 16:41:52 -0400

    Revert "new elasticsearch"
    This reverts commit d9f0fb8a6de86316d9ee8a1caea616a2e3dcdb33.

--- services/QuillCMS/app/workers/update_individual_response_worker.rb
+++
@@ -1,8 +0,0 @@
-class UpdateIndividualResponseWorker
-  include Sidekiq::Worker
-
-  def perform(response_id)
-    response = Response.find(response_id)
-    response.update_index_in_elastic_search
-  end
-end
