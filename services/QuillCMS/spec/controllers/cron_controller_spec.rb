c8998c77d35ce9980b04b3186d66160d94d8bdb0
Cissy Yu <cissy@quill.org>
2020-08-17 11:57:28 -0400

    newline

--- services/QuillCMS/spec/controllers/cron_controller_spec.rb
+++ services/QuillCMS/spec/controllers/cron_controller_spec.rb
@@ -1,7 +1,7 @@
 require "rails_helper"
 
 RSpec.describe CronController, type: :controller do
-  it 'should return 200 for found' do
+  it 'should call Cron.run and return 200' do
     expect(Cron).to receive(:run)
     expect(response.status).to eq(200)
     get :run
