e1bd075d6942f8c5651b39a2a8b4801b18280be7
Thomas Robertson <thomas.robertson@quill.org>
2020-08-18 14:46:49 -0400

    Fix some linting issues

--- services/QuillLMS/spec/support/thread_safe_for_ruby2_6.rb
+++ services/QuillLMS/spec/support/thread_safe_for_ruby2_6.rb
@@ -1,8 +1,9 @@
+# This snipped taken from https://github.com/rails/rails/issues/34790#issuecomment-450502805
 if RUBY_VERSION>='2.6.0'
   if Rails.version < '5'
     class ActionController::TestResponse < ActionDispatch::TestResponse
       def recycle!
-        # hack to avoid MonitorMixin double-initialize error:
+        # This is a quick hack to avoid MonitorMixin double-initialize error:
         @mon_mutex_owner_object_id = nil
         @mon_mutex = nil
         initialize
