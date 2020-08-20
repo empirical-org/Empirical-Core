556fc47c5c26fca056244f5d2761ffe98c3293e8
Cissy Yu <cissy@quill.org>
2020-08-20 14:34:37 -0400

    dan suggestions

--- services/QuillCMS/app/models/cron.rb
+++ services/QuillCMS/app/models/cron.rb
@@ -1,8 +1,8 @@
 class Cron
 
   def self.run
-    current_time = Time.now
-    if current_time.hour == 23
+    current_time = Time.zone.now
+    if current_time.hour == 18
       UpdateElasticsearchWorker.perform_async(current_time)
     end
   end
--- services/QuillCMS/app/workers/update_elasticsearch_worker.rb
+++ services/QuillCMS/app/workers/update_elasticsearch_worker.rb
@@ -1,8 +1,9 @@
 class UpdateElasticsearchWorker
   include Sidekiq::Worker
 
-  def perform(time)
-    responses = Response.select(:id).where("updated_at >= ?", time.beginning_of_day)
+  def perform(time_string)
+    time_object = Time.parse(time_string)
+    responses = Response.select(:id).where("updated_at >= ?", time_object.beginning_of_day)
     responses.each do |response|
       UpdateIndividualResponseWorker.perform_async(response.id)
     end
