d322eb22c17940c82aa4585638dab9b2992876cb
Cissy Yu <cissy@quill.org>
2020-08-20 16:14:11 -0400

    update to 3am

--- services/QuillCMS/app/models/cron.rb
+++ services/QuillCMS/app/models/cron.rb
@@ -2,7 +2,7 @@ class Cron
 
   def self.run
     current_time = Time.zone.now
-    if current_time.hour == 18
+    if current_time.hour == 3
       UpdateElasticsearchWorker.perform_async(current_time)
     end
   end
--- services/QuillCMS/spec/models/cron_spec.rb
+++ services/QuillCMS/spec/models/cron_spec.rb
@@ -3,15 +3,15 @@ require "rails_helper"
 RSpec.describe Cron do
   describe "kicks off appropriate jobs at the appropriate time" do
 
-    it "should not kick off jobs if the hour is not 11PM" do
-      allow(Time).to receive(:now).and_return(Time.now.beginning_of_day)
+    it "should not kick off jobs if the hour is not 3AM" do
+      allow(Time).to receive(:now).and_return(Time.zone.now.beginning_of_day)
       expect(UpdateIndividualResponseWorker).to_not receive(:perform_async)
       Cron.run
     end
 
-    it "should kick off job if the hour is 11PM" do
-      allow(Time).to receive(:now).and_return(Time.zone.now.end_of_day - 1.minute)
-      expect(UpdateElasticsearchWorker).to receive(:perform_async).with(Time.zone.now.end_of_day - 1.minute)
+    it "should kick off job if the hour is 3AM" do
+      allow(Time).to receive(:now).and_return(Time.zone.now.beginning_of_day + 3.hour)
+      expect(UpdateElasticsearchWorker).to receive(:perform_async).with(Time.zone.now.beginning_of_day + 3.hour)
       Cron.run
     end
   end
