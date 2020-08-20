36be109e7ce255a2a4b499196528aa576b11a5fd
Emilia Friedberg <emiliafriedberg@gmail.com>
2020-08-20 09:35:33 -0400

    revisions

--- services/QuillLMS/client/app/bundles/Teacher/components/progress_reports/diagnostic_reports/empty_diagnostic_progress_report.jsx
+++ services/QuillLMS/client/app/bundles/Teacher/components/progress_reports/diagnostic_reports/empty_diagnostic_progress_report.jsx
@@ -7,7 +7,7 @@ const EmptyDiagnosticProgressReport = () => (
     <img alt="Illustration showing a monochromatic document with a magnifying glass inspecting the document" src={diagnosticSrc} />
     <h2>Start by assigning a diagnostic</h2>
     <p>Nothing to see here yet! Once you assign a diagnostic your students’ reports will show up here.</p>
-    <a className="quill-button small primary contained" href="/assign/diagnostic">Assign a diagnostic</a>
+    <a className="quill-button medium primary contained" href="/assign/diagnostic">Assign a diagnostic</a>
     <a href="/tools/diagnostic">Learn more about Quill Diagnostic</a>
   </section>
 )
