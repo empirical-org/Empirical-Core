a84cc30b9ba65bf81ecf6148e1db0f12003832f2
Eric Adams <erictayloradams@gmail.com>
2020-08-20 14:21:04 -0500

    add focus indicator for show menu button

--- services/QuillLMS/client/app/bundles/Grammar/components/Header.tsx
+++ services/QuillLMS/client/app/bundles/Grammar/components/Header.tsx
@@ -15,7 +15,7 @@ export const Header: React.SFC<HeaderProps> = ({ previewShowing, onTogglePreview
     return (
       <Layout.Header className="header">
         <Row align="middle" justify="space-between" style={{height: '100%', maxWidth: '800px', margin: 'auto'}} type="flex">
-          {!previewShowing && <button className="quill-button medium secondary outlined" onClick={handleTogglePreview} type="button">Show menu</button>}
+          {!previewShowing && <button className="quill-button medium secondary outlined focus-on-dark" onClick={handleTogglePreview} type="button">Show menu</button>}
           <a className="focus-on-dark" href={process.env.DEFAULT_URL}><img alt="Quill logo" src={quillLogoSrc} /></a>
           <a className="focus-on-dark" href={process.env.DEFAULT_URL}>Save and exit</a>
         </Row>
