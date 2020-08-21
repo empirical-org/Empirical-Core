0477a4729bb87c1396e52b7895bfa868c168c45a
Cissy Yu <cissy@quill.org>
2020-08-21 11:18:39 -0400

    for testing take out time

--- services/QuillCMS/app/models/cron.rb
+++ services/QuillCMS/app/models/cron.rb
@@ -2,8 +2,6 @@ class Cron
 
   def self.run
     current_time = Time.zone.now
-    if current_time.hour == 3
-      UpdateElasticsearchWorker.perform_async(current_time)
-    end
+    UpdateElasticsearchWorker.perform_async(current_time)
   end
 end
