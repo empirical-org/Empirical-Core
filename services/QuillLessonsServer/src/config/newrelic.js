11e3fd65470334fd52425ca5ef4267328b7c8a92
Daniel Drabik <dan@quill.org>
2020-08-25 15:16:17 -0400

    Fix typo.

--- services/QuillLessonsServer/src/config/newrelic.js
+++ services/QuillLessonsServer/src/config/newrelic.js
@@ -3,6 +3,6 @@ import newrelic from 'newrelic';
 // // shorter wrapper around the startWebTransaction function
 export function nrTrack(key, data, callback) {
   newrelic.startWebTransaction('socket/' + key, function transactionHandler() {
-    data ? callback(data) : callback;
+    data ? callback(data) : callback();
   });
 }
