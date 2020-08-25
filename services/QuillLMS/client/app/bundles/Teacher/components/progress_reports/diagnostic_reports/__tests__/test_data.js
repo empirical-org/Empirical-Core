aaaff7b0f25366dad98d734fd2090e90a2dc1851
Emilia Friedberg <emiliafriedberg@gmail.com>
2020-08-25 10:33:42 -0400

    diagnostic top level page (#6649)
    * wip
    
    * more changes
    
    * hopefully got css done now
    
    * patch qcl
    
    * try to pass keys to tooltips
    
    * patch qcl
    
    * for now
    
    * ch ch ch changes
    
    * finish tests
    
    * update diagnostic activity packs test
    
    * wip
    
    * get backend working
    
    * small adjustments
    
    * fix tests
    
    * update test
    
    * add empty state
    
    * wip
    
    * wip
    
    * revisions
    
    * add tests
    
    * update snapshots

---
+++ services/QuillLMS/client/app/bundles/Teacher/components/progress_reports/diagnostic_reports/__tests__/test_data.js
@@ -0,0 +1,269 @@
+export const diagnostics = [
+  {
+    name: "Starter Diagnostic",
+    id: 849,
+    individual_assignments: [
+      {
+        assigned_count: 5,
+        completed_count: 5,
+        classroom_name: "Quill Classroom Extremely Long So Long SUper Duper",
+        activity_name: "Starter Diagnostic",
+        activity_id: 849,
+        unit_id: 826268,
+        unit_name: "Quill Activity Pack",
+        classroom_id: 484134,
+        assigned_date: "2020-08-17T14:26:07.185Z"
+      },
+      {
+        assigned_count: 0,
+        completed_count: 0,
+        classroom_name: "A New Class",
+        activity_name: "Starter Diagnostic",
+        activity_id: 849,
+        unit_id: 826270,
+        unit_name: "Starter Diagnostic",
+        classroom_id: 484135,
+        assigned_date: "2020-08-19T16:48:48.094Z"
+      },
+      {
+        assigned_count: 5,
+        completed_count: 0,
+        classroom_name: "Quill Classroom Extremely Long So Long SUper Duper",
+        activity_name: "Starter Diagnostic",
+        activity_id: 849,
+        unit_id: 826270,
+        unit_name: "Starter Diagnostic",
+        classroom_id: 484134,
+        assigned_date: "2020-08-19T16:48:48.160Z"
+      }
+    ],
+    last_assigned: "2020-08-19T16:48:48.160Z",
+    classes_count: 2,
+    total_assigned: 10,
+    total_completed: 5
+  },
+  {
+    name: "Intermediate Diagnostic",
+    id: 850,
+    individual_assignments: [
+      {
+        assigned_count: 0,
+        completed_count: 0,
+        classroom_name: "A New Class",
+        activity_name: "Intermediate Diagnostic",
+        activity_id: 850,
+        unit_id: 826269,
+        unit_name: "Every Diagnostic",
+        classroom_id: 484135,
+        assigned_date: "2020-08-17T17:23:21.451Z"
+      },
+      {
+        assigned_count: 5,
+        completed_count: 0,
+        classroom_name: "Quill Classroom Extremely Long So Long SUper Duper",
+        activity_name: "Intermediate Diagnostic",
+        activity_id: 850,
+        unit_id: 826269,
+        unit_name: "Every Diagnostic",
+        classroom_id: 484134,
+        assigned_date: "2020-08-17T17:23:21.471Z"
+      }
+    ],
+    last_assigned: "2020-08-17T17:23:21.471Z",
+    classes_count: 2,
+    total_assigned: 5,
+    total_completed: 0
+  },
+  {
+    name: "AP® Writing Skills Survey",
+    id: 992,
+    individual_assignments: [
+      {
+        assigned_count: 0,
+        completed_count: 0,
+        classroom_name: "A New Class",
+        activity_name: "AP® Writing Skills Survey",
+        activity_id: 992,
+        unit_id: 826269,
+        unit_name: "Every Diagnostic",
+        classroom_id: 484135,
+        assigned_date: "2020-08-17T17:23:21.451Z"
+      },
+      {
+        assigned_count: 5,
+        completed_count: 0,
+        classroom_name: "Quill Classroom Extremely Long So Long SUper Duper",
+        activity_name: "AP® Writing Skills Survey",
+        activity_id: 992,
+        unit_id: 826269,
+        unit_name: "Every Diagnostic",
+        classroom_id: 484134,
+        assigned_date: "2020-08-17T17:23:21.471Z"
+      }
+    ],
+    last_assigned: "2020-08-17T17:23:21.471Z",
+    classes_count: 2,
+    total_assigned: 5,
+    total_completed: 0
+  },
+  {
+    name: "ELL Diagnostic",
+    id: 447,
+    individual_assignments: [
+      {
+        assigned_count: 0,
+        completed_count: 0,
+        classroom_name: "A New Class",
+        activity_name: "ELL Diagnostic",
+        activity_id: 447,
+        unit_id: 826269,
+        unit_name: "Every Diagnostic",
+        classroom_id: 484135,
+        assigned_date: "2020-08-17T17:23:21.451Z"
+      },
+      {
+        assigned_count: 5,
+        completed_count: 0,
+        classroom_name: "Quill Classroom Extremely Long So Long SUper Duper",
+        activity_name: "ELL Diagnostic",
+        activity_id: 447,
+        unit_id: 826269,
+        unit_name: "Every Diagnostic",
+        classroom_id: 484134,
+        assigned_date: "2020-08-17T17:23:21.471Z"
+      }
+    ],
+    last_assigned: "2020-08-17T17:23:21.471Z",
+    classes_count: 2,
+    total_assigned: 5,
+    total_completed: 0
+  },
+  {
+    name: "Pre-AP® Writing Skills Survey 2: Tools for Sentence Expansion",
+    id: 1230,
+    individual_assignments: [
+      {
+        assigned_count: 0,
+        completed_count: 0,
+        classroom_name: "A New Class",
+        activity_name: "Pre-AP® Writing Skills Survey 2: Tools for Sentence Expansion",
+        activity_id: 1230,
+        unit_id: 826269,
+        unit_name: "Every Diagnostic",
+        classroom_id: 484135,
+        assigned_date: "2020-08-17T17:23:21.451Z"
+      },
+      {
+        assigned_count: 5,
+        completed_count: 0,
+        classroom_name: "Quill Classroom Extremely Long So Long SUper Duper",
+        activity_name: "Pre-AP® Writing Skills Survey 2: Tools for Sentence Expansion",
+        activity_id: 1230,
+        unit_id: 826269,
+        unit_name: "Every Diagnostic",
+        classroom_id: 484134,
+        assigned_date: "2020-08-17T17:23:21.471Z"
+      }
+    ],
+    last_assigned: "2020-08-17T17:23:21.471Z",
+    classes_count: 2,
+    total_assigned: 5,
+    total_completed: 0
+  },
+  {
+    name: "Advanced Diagnostic",
+    id: 888,
+    individual_assignments: [
+      {
+        assigned_count: 0,
+        completed_count: 0,
+        classroom_name: "A New Class",
+        activity_name: "Advanced Diagnostic",
+        activity_id: 888,
+        unit_id: 826269,
+        unit_name: "Every Diagnostic",
+        classroom_id: 484135,
+        assigned_date: "2020-08-17T17:23:21.451Z"
+      },
+      {
+        assigned_count: 5,
+        completed_count: 0,
+        classroom_name: "Quill Classroom Extremely Long So Long SUper Duper",
+        activity_name: "Advanced Diagnostic",
+        activity_id: 888,
+        unit_id: 826269,
+        unit_name: "Every Diagnostic",
+        classroom_id: 484134,
+        assigned_date: "2020-08-17T17:23:21.471Z"
+      }
+    ],
+    last_assigned: "2020-08-17T17:23:21.471Z",
+    classes_count: 2,
+    total_assigned: 5,
+    total_completed: 0
+  },
+  {
+    name: "Pre-AP® Writing Skills Survey 1: Basics of Sentence Patterns",
+    id: 1229,
+    individual_assignments: [
+      {
+        assigned_count: 0,
+        completed_count: 0,
+        classroom_name: "A New Class",
+        activity_name: "Pre-AP® Writing Skills Survey 1: Basics of Sentence Patterns",
+        activity_id: 1229,
+        unit_id: 826269,
+        unit_name: "Every Diagnostic",
+        classroom_id: 484135,
+        assigned_date: "2020-08-17T17:23:21.451Z"
+      },
+      {
+        assigned_count: 5,
+        completed_count: 0,
+        classroom_name: "Quill Classroom Extremely Long So Long SUper Duper",
+        activity_name: "Pre-AP® Writing Skills Survey 1: Basics of Sentence Patterns",
+        activity_id: 1229,
+        unit_id: 826269,
+        unit_name: "Every Diagnostic",
+        classroom_id: 484134,
+        assigned_date: "2020-08-17T17:23:21.471Z"
+      }
+    ],
+    last_assigned: "2020-08-17T17:23:21.471Z",
+    classes_count: 2,
+    total_assigned: 5,
+    total_completed: 0
+  },
+  {
+    name: "ELL Starter Diagnostic",
+    id: 1161,
+    individual_assignments: [
+      {
+        assigned_count: 0,
+        completed_count: 0,
+        classroom_name: "A New Class",
+        activity_name: "ELL Starter Diagnostic",
+        activity_id: 1161,
+        unit_id: 826269,
+        unit_name: "Every Diagnostic",
+        classroom_id: 484135,
+        assigned_date: "2020-08-17T17:23:21.451Z"
+      },
+      {
+        assigned_count: 5,
+        completed_count: 0,
+        classroom_name: "Quill Classroom Extremely Long So Long SUper Duper",
+        activity_name: "ELL Starter Diagnostic",
+        activity_id: 1161,
+        unit_id: 826269,
+        unit_name: "Every Diagnostic",
+        classroom_id: 484134,
+        assigned_date: "2020-08-17T17:23:21.471Z"
+      }
+    ],
+    last_assigned: "2020-08-17T17:23:21.471Z",
+    classes_count: 2,
+    total_assigned: 5,
+    total_completed: 0
+  }
+]
