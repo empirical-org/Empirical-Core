60e07b9b1dd068e7e1bb4740bdcd2295d21ee54b
Cissy Yu <cissy@quill.org>
2020-08-27 13:10:22 -0400

    change 24 hour thing

--- services/QuillCMS/app/workers/update_elasticsearch_worker.rb
+++ services/QuillCMS/app/workers/update_elasticsearch_worker.rb
@@ -3,7 +3,7 @@ class UpdateElasticsearchWorker
 
   def perform(time_string)
     time_object = Time.parse(time_string)
-    responses = Response.select(:id).where("updated_at >= ?", time_object.beginning_of_day)
+    responses = Response.select(:id).where("updated_at >= ?", time_object - 24.hour)
     responses.each do |response|
       UpdateIndividualResponseWorker.perform_async(response.id)
     end
