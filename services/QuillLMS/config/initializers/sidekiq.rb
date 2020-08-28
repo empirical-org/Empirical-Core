2847dc7be88cb91ae5f1f0ed269b7942700a508f
Daniel Drabik <dan@quill.org>
2020-08-28 16:39:08 -0400

    Remove ascii code.

--- services/QuillLMS/config/initializers/sidekiq.rb
+++ services/QuillLMS/config/initializers/sidekiq.rb
@@ -17,7 +17,7 @@ module SidekiqQueue
   # CRITICAL: Jobs that impact the user experience,
   # e.g. that the user may be waiting on, like student imports
   CRITICAL = 'critical'
-  # DEFAULT: Jobs should run soon, but won’t have an effect on the user experience
+  # DEFAULT: Jobs should run soon, but won't have an effect on the user experience
   # if they are delayed. These should not be long-running jobs, put those in LOW.
   DEFAULT = 'default'
   # LOW: Jobs that might be long-running that we don't want to clog up the main workers
