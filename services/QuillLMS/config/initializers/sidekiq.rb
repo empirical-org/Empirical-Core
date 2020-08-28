1837955bf82d5a7b2374f4fe879f636a7f6f9464
Daniel Drabik <dan@quill.org>
2020-08-28 15:52:49 -0400

    Update notes.

--- services/QuillLMS/config/initializers/sidekiq.rb
+++ services/QuillLMS/config/initializers/sidekiq.rb
@@ -12,10 +12,15 @@ end
 #end
 
 module SidekiqQueue
-  # CRITICAL: Jobs that impact the user experience, e.g. that user may be waiting on, like Student imports
+  # QUEUE DEFINITIONS
+
+  # CRITICAL: Jobs that impact the user experience,
+  # e.g. that the user may be waiting on, like student imports
   CRITICAL = 'critical'
-  # DEFAULT: Jobs should run soon, but won’t have an effect on the user experience if they are delayed. These should not be long-running jobs, put those in LOW.
+  # DEFAULT: Jobs should run soon, but won’t have an effect on the user experience
+  # if they are delayed. These should not be long-running jobs, put those in LOW.
   DEFAULT = 'default'
-  # LOW: Jobs that might be long-running that we don't want to clog up the main workers and that don’t need to run now.
+  # LOW: Jobs that might be long-running that we don't want to clog up the main workers
+  # and that can be delayed.
   LOW = 'low'
 end
