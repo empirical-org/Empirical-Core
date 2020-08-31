c29d0d29fd993e0e6d5c6ac5d7c25a2cf8f12b88
Emilia Friedberg <emiliafriedberg@gmail.com>
2020-08-31 10:27:22 -0400

    bulk archive classes (#6661)
    * add bulk archive classes banner and modal
    
    * add code to ensure teachers can't archive classes they don't own
    
    * jest tests
    
    * update broken rspec test
    
    * fix spacing

--- services/QuillLMS/spec/controllers/teachers/classrooms_controller_spec.rb
+++ services/QuillLMS/spec/controllers/teachers/classrooms_controller_spec.rb
@@ -226,6 +226,26 @@ describe Teachers::ClassroomsController, type: :controller do
     end
   end
 
+  describe '#bulk_archive' do
+    let!(:teacher) { create(:teacher) }
+    let!(:owned_classroom_1) { create(:classroom, :with_no_teacher) }
+    let!(:owned_classroom_2) { create(:classroom, :with_no_teacher) }
+    let!(:unowned_classroom) { create(:classroom) }
+    let!(:classrooms_teacher_1) { create(:classrooms_teacher, classroom: owned_classroom_1, user: teacher, role: 'owner')}
+    let!(:classrooms_teacher_2) { create(:classrooms_teacher, classroom: owned_classroom_2, user: teacher, role: 'owner')}
+
+    before do
+      allow(controller).to receive(:current_user) { teacher }
+    end
+
+    it 'should hide the classrooms that are owned by the teacher and not hide the one that is not' do
+      put :bulk_archive, ids: [owned_classroom_1.id, owned_classroom_2.id, unowned_classroom.id]
+      expect(owned_classroom_1.reload.visible).to eq false
+      expect(owned_classroom_2.reload.visible).to eq false
+      expect(unowned_classroom.reload.visible).to eq true
+    end
+  end
+
   describe '#unhide' do
     let!(:classroom) { create(:classroom) }
     let(:teacher) { classroom.owner }
