716b096f3dcc5fb9a80a74995cc86c3e25eee955
Cissy Yu <cissy@quill.org>
2020-09-01 13:48:54 -0400

    omit time check for testing

--- services/QuillCMS/app/models/cron.rb
+++ services/QuillCMS/app/models/cron.rb
@@ -2,11 +2,11 @@ class Cron
 
   def self.run
     current_time = Time.zone.now
-    if current_time.hour == 3
-      beginning_of_yesterday = current_time.yesterday.beginning_of_day
-      end_of_yesterday = current_time.yesterday.end_of_day
-      UpdateElasticsearchWorker.perform_async(beginning_of_yesterday, end_of_yesterday)
-      UpdateElasticsearchWorker.perform_async(current_time.beginning_of_day, current_time)
-    end
+    # if current_time.hour == 3
+    beginning_of_yesterday = current_time.yesterday.beginning_of_day
+    end_of_yesterday = current_time.yesterday.end_of_day
+    UpdateElasticsearchWorker.perform_async(beginning_of_yesterday, end_of_yesterday)
+    UpdateElasticsearchWorker.perform_async(current_time.beginning_of_day, current_time)
+    # end
   end
 end
