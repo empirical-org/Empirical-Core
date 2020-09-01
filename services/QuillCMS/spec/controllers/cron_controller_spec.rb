a12b856a89e86de8e61687856967fe303d8d68c6
Cissy Yu <cissy@quill.org>
2020-09-01 14:35:14 -0400

    change spec

--- services/QuillCMS/spec/controllers/cron_controller_spec.rb
+++ services/QuillCMS/spec/controllers/cron_controller_spec.rb
@@ -4,6 +4,6 @@ RSpec.describe CronController, type: :controller do
   it 'should call Cron.run and return 200' do
     expect(Cron).to receive(:run)
     expect(response.status).to eq(200)
-    get :run
+    post :new
   end
 end
