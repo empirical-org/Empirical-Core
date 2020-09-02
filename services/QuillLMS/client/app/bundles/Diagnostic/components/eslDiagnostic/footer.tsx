0e1cc3bc2dd69fbe49b16dc7883aaa05e180afd3
Emilia Friedberg <emiliafriedberg@gmail.com>
2020-09-02 09:34:48 -0400

    add crossorigin: anonymous to script tags; ell diagnostic mobile menu (#6689)
    * ell diagnostic mobile menu (#6683)
    
    * new diagnostic mobile language menu
    
    * update font size
    
    * add tests
    
    * update student navbar code
    
    * add crossorigin anonymous to the script tags (#6684)

--- services/QuillLMS/client/app/bundles/Diagnostic/components/eslDiagnostic/footer.tsx
+++ services/QuillLMS/client/app/bundles/Diagnostic/components/eslDiagnostic/footer.tsx
@@ -1,12 +1,18 @@
 import * as React from 'react';
 import { DropdownInput } from 'quill-component-library/dist/componentLibrary';
+import useWindowSize from '../../hooks/useWindowSize'
 import { languages, languagesV2, languageData, languageDataV2 } from '../../modules/translation/languagePageInfo';
 
 interface FooterProps {
   diagnosticID: string,
   language: string,
-  updateLanguage(language: string): any
+  updateLanguage(language: string): any,
+  handleClickOpenMobileLanguageMenu(): any
 }
+
+const languageIconSrc = `${process.env.CDN_URL}/images/icons/language.svg`
+
+const MAX_VIEW_WIDTH_FOR_MOBILE = 895
 
 const options = (diagnosticID: string): Array<{value: string, label: string}> => {
   const langs = diagnosticID === 'ell' ? languages : languagesV2;
@@ -17,30 +23,41 @@ const options = (diagnosticID: string): Array<{value: string, label: string}> =>
   }))
 }
 
-class Footer extends React.Component<FooterProps> {
+const Footer = ({ diagnosticID, language, handleClickOpenMobileLanguageMenu, updateLanguage, }: FooterProps) => {
+  const size = useWindowSize();
+  const onMobile = () => size.width <= MAX_VIEW_WIDTH_FOR_MOBILE
 
-  onChange = (option: { value: string}) => {
+  const onChange = (option: { value: string}) => {
     const language = option.value;
-    const { updateLanguage, } = this.props;
     updateLanguage(language)
   }
 
-  render() {
-    const { diagnosticID, language } = this.props;
-    const value = options(diagnosticID).find(opt => language === opt.value)
+  const value = options(diagnosticID).find(opt => language === opt.value)
+  if (onMobile()) {
+    return (
+      <div className="ell-footer mobile">
+        <div className="student-container">
+          <button className="passthrough-button focus-on-light" onClick={handleClickOpenMobileLanguageMenu} type="button">
+            <img alt="" src={languageIconSrc} />
+            <span>Change directions language</span>
+          </button>
+        </div>
+      </div>)
+    }
+
     return (<div className="ell-footer">
       <div className="student-container">
         <DropdownInput
           className="ell-language-selector"
-          handleChange={this.onChange}
+          handleChange={onChange}
           label="Directions language"
           options={options(diagnosticID)}
           usesCustomOption
           value={value}
         />
       </div>
-    </div>)
-  }
+    </div>
+  )
 }
 
 export default Footer;
--- services/QuillLMS/client/app/bundles/Diagnostic/components/navbar/studentNavbar.jsx
+++ services/QuillLMS/client/app/bundles/Diagnostic/components/navbar/studentNavbar.jsx
@@ -2,19 +2,64 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 
+import { closeLanguageMenu, updateLanguage, } from '../../actions/diagnostics'
+import { languages, languagesV2, languageData, languageDataV2 } from '../../modules/translation/languagePageInfo';
+import i18n from '../../i18n';
+
 const quillLogoSrc = `${process.env.CDN_URL}/images/logos/quill-logo-white.svg`
+const closeSrc = `${process.env.CDN_URL}/images/icons/close-white.svg`
 
 const handleSaveAndExitClick = () => {
   window.location.assign(`${process.env.DEFAULT_URL}/profile`);
 }
 
-const renderLinks = () => (
-  <div className="student-nav-section">
-    <button activeClassName="is-active" className="student-nav-item focus-on-dark" key="a-tag-student-navabar" onClick={handleSaveAndExitClick} tabIndex="0" type="button">Save and exit</button>
-  </div>
-);
+const LanguageOptions = ({ diagnosticID, dispatch, }) => {
+  // once we remove the original ELL Diagnostic, we can move to have only have the second versions
+  let langs = diagnosticID === 'ell' ? languages : languagesV2;
+  let langData = diagnosticID === 'ell' ? languageData : languageDataV2;
 
-const StudentNavbar = () => (
+  const handleClickLanguage = (e) => {
+    const language = e.currentTarget.value;
+    i18n.changeLanguage(language);
+    dispatch(updateLanguage(language));
+    dispatch(closeLanguageMenu())
+  }
+
+  return langs.map(language => {
+    return(
+      <button className="language-button" key={`${language}-button`} onClick={handleClickLanguage} type="button" value={language}>
+        <img alt={`${language} flag`} src={langData[language].flag} />
+        <span>{langData[language].label}</span>
+      </button>
+    );
+  })
+}
+
+const Links = ({ playDiagnostic, dispatch, }) => {
+  const { languageMenuOpen, diagnosticID, } = playDiagnostic
+  const handleClickClose = () => dispatch(closeLanguageMenu())
+
+  if (!languageMenuOpen) {
+    return (
+      <div className="student-nav-section">
+        <button activeClassName="is-active" className="student-nav-item focus-on-dark" key="a-tag-student-navabar" onClick={handleSaveAndExitClick} tabIndex="0" type="button">Save and exit</button>
+      </div>
+    )
+  }
+
+  return (<div className="student-nav-section">
+    <button className="student-nav-item focus-on-dark close-language-menu" onClick={handleClickClose} type="button">
+      <img alt="Close icon" src={closeSrc} />
+      <span>Close</span>
+    </button>
+    <div className="mobile-student-language-menu">
+      <h2>Choose a directions language</h2>
+      <LanguageOptions diagnosticID={diagnosticID} dispatch={dispatch} />
+    </div>
+  </div>)
+};
+
+export const StudentNavbar = ({ playDiagnostic, dispatch, }) => (
   <header className='nav student-nav'>
     <div className="container">
       <div className="student-nav-section">
@@ -25,14 +70,14 @@ const StudentNavbar = () => (
           />
         </a>
       </div>
-      {renderLinks()}
+      <Links dispatch={dispatch} playDiagnostic={playDiagnostic} />
     </div>
   </header>
 );
 
 function select(state) {
   return {
-    routing: state.routing
+    playDiagnostic: state.playDiagnostic
   };
 }
 
