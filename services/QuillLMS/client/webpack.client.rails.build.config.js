88d715e9af3ece76ffede45ad84a15862a007d13
Emilia Friedberg <emiliafriedberg@gmail.com>
2020-09-09 10:48:41 -0400

    add the hard source webpack plugin

--- services/QuillLMS/client/webpack.client.rails.build.config.js
+++ services/QuillLMS/client/webpack.client.rails.build.config.js
@@ -4,17 +4,22 @@
 // Run like this:
 // cd client && yarn run build:client
 // Note that Foreman (Procfile.dev) has also been configured to take care of this.
+const HardSourceWebpackPlugin = require('hard-source-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 const { merge } = require('webpack-merge');
 const config = require('./webpack.client.base.config');
 const { resolve } = require('path');
 const webpackConfigLoader = require('react-on-rails/webpackConfigLoader');
-const TerserPlugin = require('terser-webpack-plugin');
 
 const configPath = resolve('..', 'config');
 const { output } = webpackConfigLoader(configPath);
 
 const devBuild = process.env.RAILS_ENV === 'development';
+
+const SpeedMeasurePlugin = require("speed-measure-webpack-plugin");
+
+const smp = new SpeedMeasurePlugin();
 
 if (devBuild) {
   console.log('Webpack dev build for Rails'); // eslint-disable-line no-console
@@ -73,6 +78,7 @@ module.exports = merge(config, {
       // both options are optional
       filename: '[name]-bundle-[hash].css',
       chunkFilename: '[id].css',
-    })
+    }),
+    new HardSourceWebpackPlugin()
   ],
-});
+})
