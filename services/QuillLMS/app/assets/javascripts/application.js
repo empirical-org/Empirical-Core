d1ebfa665af19dd89db68d57d1f93a07deed4f2d
Emilia Friedberg <emiliafriedberg@gmail.com>
2020-09-10 16:27:04 -0400

    revert webpack reversion (#6720)
    * Revert "Revert "bring webpack and webpack-related packages up to date""
    
    * update babel config to make login page work (#6719)
    
    * update babel to make async work
    
    * remove unused jquery-ui/sortable call
    
    * only transpile ts code in non-dev mode
    
    * just go back to only ever transpiling ts code
    
    * explicitly import jquery into tools bundle
    
    * do the same thing a little differently

--- services/QuillLMS/app/assets/javascripts/application.js
+++ services/QuillLMS/app/assets/javascripts/application.js
@@ -4,6 +4,5 @@
 // CRITICAL that vendor-bundle must be BEFORE bootstrap-sprockets and turbolinks
 // since it is exposing jQuery and jQuery-ujs
 
-//= require jquery-ui/sortable
 //= require jquery
 //= require bootstrap-sprockets
