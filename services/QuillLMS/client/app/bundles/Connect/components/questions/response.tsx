e95279b7ee5106303a369699dba846015893dde1
Cissy Yu <cissy@quill.org>
2020-09-25 18:53:27 -0400

    change import

--- services/QuillLMS/client/app/bundles/Connect/components/questions/response.tsx
+++ services/QuillLMS/client/app/bundles/Connect/components/questions/response.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import * as React from 'react';
 import _ from 'underscore';
 import questionActions from '../../actions/questions';
 import sentenceFragmentActions from '../../actions/sentenceFragments';
