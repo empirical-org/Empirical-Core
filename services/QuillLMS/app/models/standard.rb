b32ac12ace41ec88a33b51f9248ba8368e9d5f45
Emilia Friedberg <emiliafriedberg@gmail.com>
2020-10-01 13:30:56 -0400

    migrations for custom activity pack table renaming (#6815)
    * add necessary migrations
    
    * duplicate existing model files; run migrations; add rake task
    
    * call standard grade standard level instead

---
+++ services/QuillLMS/app/models/standard.rb
@@ -0,0 +1,17 @@
+class Standard < ActiveRecord::Base
+  include Uid
+
+  belongs_to :standard_level
+  belongs_to :standard_category
+
+  has_many :activities, dependent: :destroy
+
+  default_scope -> { order(:name) }
+
+  validates :standard_level, presence: true
+  validates :name, presence: true, uniqueness: true
+
+  def name_prefix
+    name.split(' ').first
+  end
+end
