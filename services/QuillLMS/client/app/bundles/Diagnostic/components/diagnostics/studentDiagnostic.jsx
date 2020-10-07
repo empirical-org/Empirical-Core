ea9ed34478e9ce620856000ff1cc4e81dcb0078d
Eric Adams <erictayloradams@gmail.com>
2020-10-07 15:36:39 -0500

    fix issue with Starter and Intermediate Diagnostics title cards not loading (#6838)
    * update startActivity function in studentDiagnostic; fix question count for progress percentage
    
    * update test

--- services/QuillLMS/client/app/bundles/Diagnostic/components/diagnostics/studentDiagnostic.jsx
+++ services/QuillLMS/client/app/bundles/Diagnostic/components/diagnostics/studentDiagnostic.jsx
@@ -176,6 +176,9 @@ export class StudentDiagnostic extends React.Component {
 
   startActivity = () => {
     const { dispatch, } = this.props
+    const data = this.questionsForLesson()
+    const action = loadData(data);
+    dispatch(action);
     const next = nextQuestion();
     dispatch(next);
   }
@@ -300,11 +303,14 @@ export class StudentDiagnostic extends React.Component {
 
     const displayedAnsweredQuestionCount = currentQuestionIsTitleCard && currentQuestionIsNotFirstQuestion ? calculatedAnsweredQuestionCount + 1 : calculatedAnsweredQuestionCount
 
+    const progressPercent = getProgressPercent(playDiagnostic);
+    const totalQuestionCount = questionCount(playDiagnostic);
+
     return (<ProgressBar
-      answeredQuestionCount={displayedAnsweredQuestionCount}
+      answeredQuestionCount={displayedAnsweredQuestionCount > totalQuestionCount ? totalQuestionCount : displayedAnsweredQuestionCount}
       label='questions'
-      percent={getProgressPercent(playDiagnostic)}
-      questionCount={questionCount(playDiagnostic)}
+      percent={progressPercent}
+      questionCount={totalQuestionCount}
     />)
   }
 
