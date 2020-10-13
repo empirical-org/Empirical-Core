9763ce18034f39155d5feda25e0e247077f7997b
cissyxyu <57366100+cissyxyu@users.noreply.github.com>
2020-10-13 10:15:38 -0400

    Fix proofreader activities to save changes  (#6844)
    * change wording
    
    * take out redis key
    
    * Refactor admin responses component in Grammar (#6765) (#6794)
    
    * destructure props
    
    * refactor
    
    * import react
    
    * incorporrate changes
    
    * add parernt
    
    * add bug reporting
    
    * Manually remove code from production branch.
    
    * pass params
    
    * commit test
    
    * remove files
    
    Co-authored-by: Emilia Friedberg <emiliafriedberg@gmail.com>
    Co-authored-by: dandrabik <dandrabik@users.noreply.github.com>
    Co-authored-by: Anathomical <thomas.robertson@quill.org>
    Co-authored-by: Eric Adams <erictayloradams@gmail.com>
    Co-authored-by: Daniel Drabik <dan@quill.org>

---
+++ services/QuillLMS/client/app/bundles/Proofreader/tests/actions/sessions.test.ts
@@ -0,0 +1,35 @@
+import { SessionApi } from '../../lib/sessions_api'
+import { mockDispatch as dispatch } from '../__mocks__/dispatch'
+import * as sessionActions from '../../actions/session'
+
+describe('Session actions', () => {
+
+  describe('setSessionReducerToSavedSession', () => {
+    it('should call Session.get and setSessionReducer', async () => {
+      const MOCK_PASSAGE = "test passage"
+      const MOCK_SESSION_ID = "id"
+      const sessionApiGet= Promise.resolve({"passage": MOCK_PASSAGE})
+      const apiSpy = jest.spyOn(SessionApi, 'get').mockImplementation(() => sessionApiGet);
+      const actionsSpy = jest.spyOn(sessionActions, "setSessionReducer").mockImplementation(() => Promise.resolve());
+      dispatch(sessionActions.setSessionReducerToSavedSession(MOCK_SESSION_ID, false))
+      expect(apiSpy).toHaveBeenCalledWith(MOCK_SESSION_ID)
+      await sessionApiGet
+      expect(actionsSpy).toHaveBeenCalledWith(MOCK_PASSAGE)
+    })
+  })
+
+  describe('updateSessionOnFirebase', () => {
+    it('should call Session.update and setSessionReducerToSavedSession', async () => {
+      const MOCK_PASSAGE = "test passage"
+      const MOCK_SESSION_ID = "id"
+      const sessionApiUpdate= Promise.resolve({"passage": MOCK_PASSAGE})
+      const apiSpy = jest.spyOn(SessionApi, 'update').mockImplementation(() => sessionApiUpdate);
+      const actionsSpy = jest.spyOn(sessionActions, "setSessionReducerToSavedSession").mockImplementation(() => Promise.resolve());
+      dispatch(sessionActions.updateSessionOnFirebase(MOCK_SESSION_ID, null, null))
+      expect(apiSpy).toHaveBeenCalledWith(MOCK_SESSION_ID, {"passage":null})
+      await sessionApiUpdate
+      expect(actionsSpy).toHaveBeenCalledWith(MOCK_SESSION_ID)
+    })
+  })
+
+})
