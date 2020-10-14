617bb7c3dfea1867f9fea69776e341a226f6e98f
Cissy Yu <cissy@quill.org>
2020-10-14 13:56:48 -0400

    change migration

--- services/QuillLMS/db/migrate/20201013204354_create_content_partner_activities.rb
+++ services/QuillLMS/db/migrate/20201013204354_create_content_partner_activities.rb
@@ -1,8 +1,8 @@
 class CreateContentPartnerActivities < ActiveRecord::Migration
   def change
     create_table :content_partner_activities do |t|
-      t.references :content_partner_id, foreign_key: true
-      t.references :activity_id, foreign_key: true
+      t.references :content_partner, foreign_key: true
+      t.references :activity, foreign_key: true
 
       t.timestamps null: false
     end
