6afa48dfe09c51030060456f6a797c7fde0a1e6d
Cissy Yu <cissy@quill.org>
2020-10-14 13:07:03 -0400

    emilias suggestions

--- services/QuillLMS/db/migrate/20201013203506_create_content_partners.rb
+++ services/QuillLMS/db/migrate/20201013203506_create_content_partners.rb
@@ -1,7 +1,7 @@
 class CreateContentPartners < ActiveRecord::Migration
   def change
     create_table :content_partners do |t|
-      t.string :name
+      t.string :name, :null => false
       t.string :description
 
       t.timestamps null: false
--- services/QuillLMS/db/migrate/20201013204354_create_content_partner_activities.rb
+++ services/QuillLMS/db/migrate/20201013204354_create_content_partner_activities.rb
@@ -1,8 +1,8 @@
 class CreateContentPartnerActivities < ActiveRecord::Migration
   def change
     create_table :content_partner_activities do |t|
-      t.integer :content_partner_id
-      t.integer :activity_id
+      t.references :content_partner_id, foreign_key: true
+      t.references :activity_id, foreign_key: true
 
       t.timestamps null: false
     end
