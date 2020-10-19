c230be13af4983a3c1ce7c75bc8d190f703c7e3f
Cissy Yu <cissy@quill.org>
2020-10-19 10:56:47 -0400

    add fk

--- services/QuillLMS/db/migrate/20201019142759_add_raw_score_to_activity.rb
+++ services/QuillLMS/db/migrate/20201019142759_add_raw_score_to_activity.rb
@@ -1,5 +1,5 @@
 class AddRawScoreToActivity < ActiveRecord::Migration
   def change
-    add_column :activities, :raw_score, :string
+    add_reference :activities, :raw_score, foreign_key: true
   end
 end
