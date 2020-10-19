b258bb225d444ca6ebeb89dff7fa64c3c6ffcdb8
Eric Adams <erictayloradams@gmail.com>
2020-10-19 17:44:51 -0500

    Diagnostic/ Teacher Activity Preview (#6861)
    * add sidebar menu, navbar; update ant styling
    
    * use shared menu component; update props
    
    * add logic for updating playDiagnostic properties in reducer
    
    * fix helper function; format feedback for SC question
    
    * add logic for handling fillInBlank
    
    * add logic for handling sentence fragments, update helper functions, refactors
    
    * add ability to skip from landing page; fix issue with attempted question getting overwritten from next action
    
    * fix issue for initial activity load without questions; add optimal feedback
    
    * add logic for handling ELL Diagnostics
    
    * handle edge case for ELL SC questions
    
    * fix issue with language selection; update styling for title cards
    
    * minor refactors; update tests

--- services/QuillLMS/client/app/bundles/Diagnostic/routes.tsx
+++ services/QuillLMS/client/app/bundles/Diagnostic/routes.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import { RouteConfig } from "react-router-config";
+
 import Admin from "./components/admin/admin"
 import DiagnosticRouter from "./components/shared/diagnosticRouter"
 import Turk from './components/turk/turkDiagnostic.jsx';
@@ -11,7 +12,7 @@ export const routes: RouteConfig[] = [
   },
   {
     path: "/play/diagnostic/:diagnosticID",
-    component: () => (<DiagnosticRouter />)
+    component: (props: any) => (<DiagnosticRouter {...props} />)
   },
   {
     path: "/turk/:diagnosticID",
