f6e0371555e6b1a6a58bd395cd8eee5e47042306
cissyxyu <57366100+cissyxyu@users.noreply.github.com>
2020-10-20 13:35:21 -0400

    RawScore: add model, add function to convert an activity's raw score to grade level (#6873)
    * take out redis key
    
    * all model changes and tests
    
    * extra files
    
    * linter

---
+++ services/QuillLMS/spec/factories/raw_score.rb
@@ -0,0 +1,12 @@
+FactoryBot.define do
+  factory :raw_score do
+    name 'score'
+    trait :eight_hundred_to_nine_hundred do
+      name { '800-900' }
+    end
+
+    trait :five_hundred_to_six_hundred do
+      name { '500-600' }
+    end
+  end
+end
