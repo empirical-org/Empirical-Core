b3627c34e9ffcf45ff39674a2809aec2b73655c3
Cissy Yu <cissy@quill.org>
2020-10-21 10:44:47 -0400

    fix migration

--- services/QuillLMS/db/migrate/20201020200935_create_teacher_saved_activities.rb
+++ services/QuillLMS/db/migrate/20201020200935_create_teacher_saved_activities.rb
@@ -1,10 +1,11 @@
 class CreateTeacherSavedActivities < ActiveRecord::Migration
   def change
     create_table :teacher_saved_activities do |t|
-      t.references :teacher, foreign_key: {to_table: :users}, null: false
+      t.integer :teacher_id, null: false
       t.references :activity, foreign_key: true, null: false
 
       t.timestamps null: false
     end
+    add_foreign_key :teacher_saved_activities, :users, column: :teacher_id
   end
 end
