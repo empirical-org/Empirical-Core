b1a9edec5817c1546a1cd70db48d707bf0c2a2c3
Cissy Yu <cissy@quill.org>
2020-10-21 09:18:32 -0400

    foreign key added

--- services/QuillLMS/db/migrate/20201020200935_create_teacher_saved_activities.rb
+++ services/QuillLMS/db/migrate/20201020200935_create_teacher_saved_activities.rb
@@ -1,7 +1,7 @@
 class CreateTeacherSavedActivities < ActiveRecord::Migration
   def change
     create_table :teacher_saved_activities do |t|
-      t.references :teacher, references: :users, null: false
+      t.references :teacher, foreign_key: true, references: :users, null: false
       t.references :activity, foreign_key: true, null: false
 
       t.timestamps null: false
