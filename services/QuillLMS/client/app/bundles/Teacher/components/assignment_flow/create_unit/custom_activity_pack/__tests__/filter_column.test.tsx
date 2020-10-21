23b6bb415f0b218dea297aedaf1b5ac293fb93a3
Emilia Friedberg <emiliafriedberg@gmail.com>
2020-10-21 13:45:36 -0400

    finish adding tests

---
+++ services/QuillLMS/client/app/bundles/Teacher/components/assignment_flow/create_unit/custom_activity_pack/__tests__/filter_column.test.tsx
@@ -0,0 +1,28 @@
+import * as React from 'react'
+import { mount } from 'enzyme';
+
+import { activities } from './data'
+
+import FilterColumn from '../filter_column'
+
+describe('FilterColumn component', () => {
+  const props = {
+    activities: activities,
+    filteredActivities: activities,
+    calculateNumberOfFilters: () => 0,
+    resetAllFilters: () => {},
+    activityClassificationFilters: [],
+    handleActivityClassificationFilterChange: () => {},
+    gradeLevelFilters: [],
+    handleGradeLevelFilterChange: () => {},
+    activityCategoryFilters: [],
+    handleActivityCategoryFilterChange: () => {},
+    filterActivities: (ignoredKey?: string) => activities
+  }
+
+  it('should render', () => {
+    const wrapper = mount(<FilterColumn {...props} />)
+    expect(wrapper).toMatchSnapshot();
+  });
+
+})
---
+++ services/QuillLMS/client/app/bundles/Teacher/components/assignment_flow/create_unit/custom_activity_pack/__tests__/header.test.tsx
@@ -0,0 +1,30 @@
+import * as React from 'react'
+import { mount } from 'enzyme';
+
+import { activities } from './data'
+
+import Header from '../header'
+
+describe('Header component', () => {
+  const props = {
+    handleClickContinue: (event: any) => {},
+    selectedActivities: [],
+    setSelectedActivities: (selectedActivities) => {},
+    toggleActivitySelection: (activity, isSelected: boolean) => {}
+  }
+
+  describe('with no selected activities', () => {
+    it('should render', () => {
+      const wrapper = mount(<Header {...props} />)
+      expect(wrapper).toMatchSnapshot();
+    });
+  })
+
+  describe('with some selected activities', () => {
+    it('should render', () => {
+      const wrapper = mount(<Header {...props} selectedActivities={activities.splice(5, 10)} />)
+      expect(wrapper).toMatchSnapshot();
+    });
+  })
+
+})
