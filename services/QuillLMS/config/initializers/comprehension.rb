2cc31a13cec660f372e5aa899a16619eaa0b399f
Thomas Robertson <thomas.robertson@quill.org>
2020-10-22 11:10:05 -0400

    Add initializer config for extra-engine models to LMS proper

--- services/QuillLMS/config/initializers/comprehension.rb
+++ services/QuillLMS/config/initializers/comprehension.rb
@@ -1,2 +1,8 @@
 # Set the Engines connection to the parent activity here.
 Comprehension.parent_activity_class = "Activity"
+
+# Set the Engines connection to the activity session here.
+Comprehension.parent_activity_class = "ActivitySession"
+
+# Set the Engines connection to the concept here.
+Comprehension.parent_activity_class = "Concept"
