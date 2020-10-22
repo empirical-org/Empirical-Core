689e48e803fa187926352db625725d083b2ea720
Cissy Yu <cissy@quill.org>
2020-10-22 10:45:28 -0400

    redundant self

--- services/QuillLMS/app/models/standard_level.rb
+++ services/QuillLMS/app/models/standard_level.rb
@@ -11,9 +11,9 @@ class StandardLevel < ActiveRecord::Base
   before_save :archive_or_unarchive_standards
 
   def archive_or_unarchive_standards
-    if self.visible_changed?
-      self.standards.each do |standard|
-        standard.visible = self.visible
+    if visible_changed?
+      standards.each do |standard|
+        standard.visible = visible
         standard.save
       end
     end
