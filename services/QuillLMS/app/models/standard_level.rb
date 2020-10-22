88768afec3aa9880d49b9d0d1d78ade433228441
Cissy Yu <cissy@quill.org>
2020-10-22 12:24:03 -0400

    shorten

--- services/QuillLMS/app/models/standard_level.rb
+++ services/QuillLMS/app/models/standard_level.rb
@@ -13,8 +13,7 @@ class StandardLevel < ActiveRecord::Base
   def archive_or_unarchive_standards
     if visible_changed?
       standards.each do |standard|
-        standard.visible = visible
-        standard.save
+        standard.update(visible: visible)
       end
     end
   end
