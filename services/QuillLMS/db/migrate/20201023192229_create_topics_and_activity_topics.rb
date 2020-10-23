a20fb76420cd435e00db33e1e62b7e9d11f68759
Cissy Yu <cissy@quill.org>
2020-10-23 16:41:19 -0400

    typo boolean

--- services/QuillLMS/db/migrate/20201023192229_create_topics_and_activity_topics.rb
+++ services/QuillLMS/db/migrate/20201023192229_create_topics_and_activity_topics.rb
@@ -3,7 +3,7 @@ class CreateTopicsAndActivityTopics < ActiveRecord::Migration
     create_table :topics do |t|
       t.string :name, null: false
       t.integer :level, null: false
-      t.boolean :visible, null: false, default: :true
+      t.boolean :visible, null: false, default: true
       t.integer :parent_id
     end
     add_foreign_key :topics, :topics, column: :parent_id, index: true
