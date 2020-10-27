2c27820d8219a3bd9269e18e3dacfca0ae6a3868
Daniel Drabik <dan@quill.org>
2020-10-27 19:16:57 -0400

    Fix join_classroom bug with better redirect.

--- services/QuillLMS/spec/controllers/students_controller_spec.rb
+++ services/QuillLMS/spec/controllers/students_controller_spec.rb
@@ -26,6 +26,27 @@ describe StudentsController do
     end
   end
 
+
+  describe '#join_classroom' do
+    let(:student) { create(:student) }
+
+    before do
+      allow(controller).to receive(:current_user) { student }
+    end
+    it 'should redirect for an invalid class_code' do
+      get :join_classroom, classcode: 'nonsense_doesnt_exist'
+
+      expect(response).to redirect_to '/classes'
+    end
+
+    it 'should redirect for a valid class_code' do
+      classroom = create(:classroom, code: 'existing_code')
+      get :join_classroom, classcode: 'existing_code'
+
+      expect(response).to redirect_to "/classrooms/#{classroom.id}?joined=success"
+    end
+  end
+
   describe '#account_settings' do
     it 'should set the current user and js file' do
       get :account_settings
