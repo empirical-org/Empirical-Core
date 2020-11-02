37d695c8c866cb5723a72644403952473af39a0d
Cissy Yu <cissy@quill.org>
2020-11-02 10:19:45 -0500

    extra files removed

--- services/QuillLMS/app/controllers/api/v1/activity_sessions_controller.rb
+++ services/QuillLMS/app/controllers/api/v1/activity_sessions_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::ActivitySessionsController < Api::ApiController
 
   before_action :doorkeeper_authorize!, only: [:destroy]
-  before_action :find_activity_session, only: [:show, :update, :destroy, :update_comprehension_session]
+  before_action :find_activity_session, only: [:show, :update, :destroy]
   before_action :strip_access_token_from_request
   before_action :transform_incoming_request, only: [:update, :create]
 
@@ -39,10 +39,6 @@ class Api::V1::ActivitySessionsController < Api::ApiController
         errors: @activity_session.errors
       }, status: :unprocessable_entity, serializer: ActivitySessionSerializer
     end
-  end
-
-  def update_comprehension_session
-    update
   end
 
   # POST
