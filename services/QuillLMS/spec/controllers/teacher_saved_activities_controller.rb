fa48c42b1d0d3aa5bebdc84fc4f7c8bae9c73a4a
Emilia Friedberg <emiliafriedberg@gmail.com>
2020-11-04 12:39:44 -0500

    rubocop

--- services/QuillLMS/spec/controllers/teacher_saved_activities_controller.rb
+++ services/QuillLMS/spec/controllers/teacher_saved_activities_controller.rb
@@ -9,21 +9,21 @@ describe TeacherSavedActivitiesController do
 
   describe '#saved_activity_ids_for_current_user' do
     it 'should return a list of activity ids the user has saved' do
-      activity_1 = create(:activity)
-      activity_2 = create(:activity)
-      TeacherSavedActivity.create(activity: activity_1, teacher: user)
-      TeacherSavedActivity.create(activity: activity_2, teacher: user)
+      activity1 = create(:activity)
+      activity2 = create(:activity)
+      TeacherSavedActivity.create(activity: activity1, teacher: user)
+      TeacherSavedActivity.create(activity: activity2, teacher: user)
       get :saved_activity_ids_for_current_user
-      expect(response.body).to eq({ activity_ids: [activity_1.id, activity_2.id]}.to_json)
+      expect(response.body).to eq({ activity_ids: [activity1.id, activity2.id]}.to_json)
       expect(response.status).to be(200)
     end
   end
 
   describe '#create_by_activity_id_for_current_user' do
     it 'should create a new teacher saved activity for that activity and the current user' do
-      activity_1 = create(:activity)
-      post :create_by_activity_id_for_current_user, { activity_id: activity_1.id}
-      expect(TeacherSavedActivity.find_by(teacher: user, activity: activity_1)).to be
+      activity1 = create(:activity)
+      post :create_by_activity_id_for_current_user, { activity_id: activity1.id}
+      expect(TeacherSavedActivity.find_by(teacher: user, activity: activity1)).to be
       expect(response.status).to be(200)
     end
   end
@@ -31,18 +31,18 @@ describe TeacherSavedActivitiesController do
   describe '#destroy_by_activity_id_for_current_user' do
     describe 'when the teacher saved activity exists' do
       it 'should destroy the teacher saved activity for that activity and the current user' do
-        activity_1 = create(:activity)
-        TeacherSavedActivity.create(activity: activity_1, teacher: user)
-        delete :destroy_by_activity_id_for_current_user, { activity_id: activity_1.id}
-        expect(TeacherSavedActivity.find_by(teacher: user, activity: activity_1)).not_to be
+        activity1 = create(:activity)
+        TeacherSavedActivity.create(activity: activity1, teacher: user)
+        delete :destroy_by_activity_id_for_current_user, { activity_id: activity1.id}
+        expect(TeacherSavedActivity.find_by(teacher: user, activity: activity1)).not_to be
         expect(response.status).to be(200)
       end
     end
 
     describe 'when the teacher saved activity does not exist' do
       it 'should not error' do
-        activity_1 = create(:activity)
-        delete :destroy_by_activity_id_for_current_user, { activity_id: activity_1.id}
+        activity1 = create(:activity)
+        delete :destroy_by_activity_id_for_current_user, { activity_id: activity1.id}
         expect(response.status).to be(200)
       end
     end
