662646fa8f3b48fb375901e54fb390e2b57f6b16
dandrabik <dandrabik@users.noreply.github.com>
2020-11-05 10:08:17 -0500

    Isolate Google jobs into separate sidekiq queue. (#6938)

--- services/QuillLMS/app/workers/google_student_classroom_worker.rb
+++ services/QuillLMS/app/workers/google_student_classroom_worker.rb
@@ -1,6 +1,6 @@
 class GoogleStudentClassroomWorker
   include Sidekiq::Worker
-  sidekiq_options queue: SidekiqQueue::CRITICAL
+  sidekiq_options queue: SidekiqQueue::GOOGLE
 
   def perform(student_id)
     begin
