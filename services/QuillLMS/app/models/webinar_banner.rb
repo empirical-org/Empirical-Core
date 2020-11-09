a5da4e6a87e5715165e8e2a6a0a226cad33236b9
cissyxyu <57366100+cissyxyu@users.noreply.github.com>
2020-11-09 12:27:14 -0500

    Remove a bunch of webinar banners (#6955)
    * take out redis key
    
    * deletions
    
    * extra files
    
    * delete past dates

--- services/QuillLMS/app/models/webinar_banner.rb
+++ services/QuillLMS/app/models/webinar_banner.rb
@@ -7,31 +7,15 @@ class WebinarBanner
   # RECURRING have the key format DayOfWeek-Hour
 
   ONE_OFFS = {
-    '10-7-11' => ["Quill's Spotlight on Exploring the Activity Library", "#{ZOOM_URL}/WN_uzoWu3HuQCa2ZpyQ0j4C5g"],
-    '10-7-16' => ["Quill's Spotlight on Exploring the Activity Library", "#{ZOOM_URL}/WN_ptLJPr_ZQTilmzklfDVrjA"],
-    '10-14-11' => ["Quill's Spotlight on Ways to Use Quill with Google Classroom", "#{ZOOM_URL}/WN_DhZRpHV7Q4K413v4SzcK1A"],
-    '10-14-16' => ["Quill's Spotlight on Ways to Use Quill with Google Classroom", "#{ZOOM_URL}/WN_DHVXyOXzS3mytUCqK_UbIA"],
-    '10-21-11' => ["Quill's Spotlight on Keeping Your Remote Classroom on Track", "#{ZOOM_URL}/WN_UiN_HtS7TwmYQObgOVCOGw"],
-    '10-21-16' => ["Quill's Spotlight on Keeping Your Remote Classroom on Track", "#{ZOOM_URL}/WN_DHVXyOXzS3mytUCqK_UbIA"],
-    '10-28-11' => ["Quill's Spotlight on Using Quill to Support Students with IEPs", "#{ZOOM_URL}/WN_4RGmnNfiSSSqO-EB13ecbw"],
-    '10-28-16' => ["Quill's Spotlight on Using Quill to Support Students with IEPs", "#{ZOOM_URL}/WN_YgimZ7j2TTCGW7HiniJAwg"],
-    '11-4-11' => ["Quill's Spotlight on Data and Instruction", "#{ZOOM_URL}/WN_8Dq0_YxNRa23-qWZT4qDaQ"],
-    '11-4-16' => ["Quill's Spotlight on Data and Instruction", "#{ZOOM_URL}/WN_d_oq8DgBQjGpeHzXXzM1AA"],
-    '11-18-11' => ["Quill's Spotlight on Equitable Grading using Quill", "#{ZOOM_URL}/WN_fggbcB53QcGSjVgqCvCkDQ"],
     '11-18-16' => ["Quill's Spotlight on Equitable Grading using Quill", "#{ZOOM_URL}/WN_BNF3nVnyRVqrKQ9udZis-w"],
-    '12-2-11' => ["Quill's Spotlight on Using Quill to Support English Language Learners", "#{ZOOM_URL}/WN_xewDiLaYSEGM3jljnyYKAg"],
     '12-2-16' => ["Quill's Spotlight on Using Quill to Support English Language Learners", "#{ZOOM_URL}/WN_R3h0pKEHTVqiLlNwdFHF4g"],
-    '12-9-11' => ["Quill's Spotlight on Sentence Fluency at the Lesson Level", "#{ZOOM_URL}/WN_i4r1iasTS2WqH38mVmabaA"],
     '12-9-16' => ["Quill's Spotlight on Sentence Fluency at the Lesson Level", "#{ZOOM_URL}/WN_M0eiXLFaSEq-GQOAzj0kSA"],
-    '12-16-11' => ["Quill's Spotlight on Encouraging & Empowering Your Writers", "#{ZOOM_URL}/WN_1WBmjTBeSTmq_f3Kikkaaw"],
     '12-16-16' => ["Quill's Spotlight on Encouraging & Empowering Your Writers", "#{ZOOM_URL}/WN_yB6Lltm3QH2h4AnobnCdLw"]
   }
 
   RECURRING = {
-    '1-11' => ["Quill 101",  "#{ZOOM_URL}/WN_OmB4MXOgTkWh_jeTFXUTnw"],
     '1-16' => ["Quill 101", "#{ZOOM_URL}/WN_a4Z1_Zs6RSGUWwr_t0V18Q"],
-    '2-11' => ["Quill in Pre-AP® and AP®: Spotlight on Data and Instruction", "#{ZOOM_URL}/WN_UuoWsG_8Q5Cpbh9Bqqi24g"],
-    '2-16' => ["Quill in Pre-AP® and AP®: Spotlight on Data and Instruction", "#{ZOOM_URL}/WN_UuoWsG_8Q5Cpbh9Bqqi24g"],
+    '2-16' => ["Quill in Pre-AP® and AP®: Spotlight on Data and Instruction", "#{ZOOM_URL}/WN_UuoWsG_8Q5Cpbh9Bqqi24g"]
   }
 
   def initialize(time)
