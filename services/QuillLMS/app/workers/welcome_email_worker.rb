f18184219fe0d805fdda5a42fcc06a3f0934ceec
Emilia Friedberg <emiliafriedberg@gmail.com>
2020-11-09 12:54:44 -0500

    remove welcome email and related code

--- services/QuillLMS/app/workers/welcome_email_worker.rb
+++
@@ -1,9 +0,0 @@
-class WelcomeEmailWorker
-  include Sidekiq::Worker
-  sidekiq_options queue: SidekiqQueue::CRITICAL
-
-  def perform(id)
-    @user = User.find(id)
-    @user.send_welcome_email
-  end
-end
