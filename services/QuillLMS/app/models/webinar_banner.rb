3a123ed57851b1e3ae68d61684b01735967c1c4f
Cissy Yu <cissy@quill.org>
2020-11-09 11:57:12 -0500

    delete past dates

--- services/QuillLMS/app/models/webinar_banner.rb
+++ services/QuillLMS/app/models/webinar_banner.rb
@@ -7,16 +7,6 @@ class WebinarBanner
   # RECURRING have the key format DayOfWeek-Hour
 
   ONE_OFFS = {
-    '10-7-11' => ["Quill's Spotlight on Exploring the Activity Library", "#{ZOOM_URL}/WN_uzoWu3HuQCa2ZpyQ0j4C5g"],
-    '10-7-16' => ["Quill's Spotlight on Exploring the Activity Library", "#{ZOOM_URL}/WN_ptLJPr_ZQTilmzklfDVrjA"],
-    '10-14-11' => ["Quill's Spotlight on Ways to Use Quill with Google Classroom", "#{ZOOM_URL}/WN_DhZRpHV7Q4K413v4SzcK1A"],
-    '10-14-16' => ["Quill's Spotlight on Ways to Use Quill with Google Classroom", "#{ZOOM_URL}/WN_DHVXyOXzS3mytUCqK_UbIA"],
-    '10-21-11' => ["Quill's Spotlight on Keeping Your Remote Classroom on Track", "#{ZOOM_URL}/WN_UiN_HtS7TwmYQObgOVCOGw"],
-    '10-21-16' => ["Quill's Spotlight on Keeping Your Remote Classroom on Track", "#{ZOOM_URL}/WN_DHVXyOXzS3mytUCqK_UbIA"],
-    '10-28-11' => ["Quill's Spotlight on Using Quill to Support Students with IEPs", "#{ZOOM_URL}/WN_4RGmnNfiSSSqO-EB13ecbw"],
-    '10-28-16' => ["Quill's Spotlight on Using Quill to Support Students with IEPs", "#{ZOOM_URL}/WN_YgimZ7j2TTCGW7HiniJAwg"],
-    '11-4-11' => ["Quill's Spotlight on Data and Instruction", "#{ZOOM_URL}/WN_8Dq0_YxNRa23-qWZT4qDaQ"],
-    '11-4-16' => ["Quill's Spotlight on Data and Instruction", "#{ZOOM_URL}/WN_d_oq8DgBQjGpeHzXXzM1AA"],
     '11-18-16' => ["Quill's Spotlight on Equitable Grading using Quill", "#{ZOOM_URL}/WN_BNF3nVnyRVqrKQ9udZis-w"],
     '12-2-16' => ["Quill's Spotlight on Using Quill to Support English Language Learners", "#{ZOOM_URL}/WN_R3h0pKEHTVqiLlNwdFHF4g"],
     '12-9-16' => ["Quill's Spotlight on Sentence Fluency at the Lesson Level", "#{ZOOM_URL}/WN_M0eiXLFaSEq-GQOAzj0kSA"],
