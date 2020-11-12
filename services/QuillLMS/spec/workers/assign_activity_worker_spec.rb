8c2f2ac4a916ec34f4f0209fe69ed34d3a6d4804
Emilia Friedberg <emiliafriedberg@gmail.com>
2020-11-12 11:43:33 -0500

    fix sometimes failing test

--- services/QuillLMS/spec/workers/assign_activity_worker_spec.rb
+++ services/QuillLMS/spec/workers/assign_activity_worker_spec.rb
@@ -2,9 +2,13 @@ require 'rails_helper'
 
 describe AssignActivityWorker, type: :worker do
   let(:worker) { AssignActivityWorker.new }
-  let(:analytics) { SegmentAnalytics.new }
+  let(:analytics) { double(:analytics, identify: true, track: true) }
   let(:teacher) { create(:teacher) }
   let(:unit) { create(:unit) }
+
+  before do
+    allow(SegmentAnalytics).to receive(:new) { analytics }
+  end
 
   it 'sends a segment.io event' do
     expect(analytics).to receive(:track_activity_pack_assignment).with(teacher.id, unit.id)
