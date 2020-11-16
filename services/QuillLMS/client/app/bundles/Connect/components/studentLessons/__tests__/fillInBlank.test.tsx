ef1087257338dcbc5f293b235989bf2f074d48fc
Eric Adams <erictayloradams@gmail.com>
2020-11-16 10:43:28 -0600

    fix spacing issue and correct answer submission for Connect FIB (#6973)

--- services/QuillLMS/client/app/bundles/Connect/components/studentLessons/__tests__/fillInBlank.test.tsx
+++ services/QuillLMS/client/app/bundles/Connect/components/studentLessons/__tests__/fillInBlank.test.tsx
@@ -1,25 +1,37 @@
 import 'whatwg-fetch'
 import * as React from 'react';
 import { shallow } from 'enzyme';
-import { fillInBlankQuestionWithOneAttempt, conceptsFeedback } from '../../../test/studentLessonData'
+import stripHtml from "string-strip-html";
 
+import { fillInBlankQuestionWithOneAttempt, conceptsFeedback } from '../../../test/studentLessonData'
 import PlayFillInBlankQuestion from '../fillInBlank';
+
+jest.mock('string-strip-html', () => ({
+  default: jest.fn()
+}));
+const mockAnon = () => jest.fn();
 
 describe('PlayFillInBlankQuestion component', () => {
   const wrapper = shallow(
     <PlayFillInBlankQuestion
       conceptsFeedback={conceptsFeedback}
-      dispatch={() => {}}
-      nextQuestion={() => {}}
+      dispatch={mockAnon}
+      nextQuestion={mockAnon}
       question={fillInBlankQuestionWithOneAttempt}
-      submitResponse={() => {}}
+      submitResponse={mockAnon}
     />
-  )
-
+  );
   it('#generateInputs will return array of inputs from last attempt if it exists', () => {
     const promptArray = fillInBlankQuestionWithOneAttempt.prompt.split('___')
     const generatedInputs = wrapper.instance().generateInputs(promptArray)
     expect(generatedInputs[0]).toBe(['the'][0]);
-  })
-
-})
+  });
+  it('#zipInputsAndText combines text and inputs, and strips html from string output', () => {
+    wrapper.setState({
+      inputVals: ["want", "my", "brother's"],
+      fillInBlank: ["I ", " ", " ", " <em>big, red</em> bicycle."]
+    });
+    wrapper.instance().zipInputsAndText();
+    expect(stripHtml).toHaveBeenCalled();
+  });
+});
