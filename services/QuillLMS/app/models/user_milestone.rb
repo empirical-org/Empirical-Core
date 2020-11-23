c2e2a72af78cb6fcabccc26f104ad7db43e2eacd
Emilia Friedberg <emiliafriedberg@gmail.com>
2020-11-23 09:55:27 -0500

    remove 'user completed milestone' event

--- services/QuillLMS/app/models/user_milestone.rb
+++ services/QuillLMS/app/models/user_milestone.rb
@@ -1,13 +1,4 @@
 class UserMilestone < ActiveRecord::Base
   belongs_to :user
   belongs_to :milestone
-  after_commit :report_milestone_to_segment
-
-  private
-
-  def report_milestone_to_segment
-    ::ReportMilestoneToSegmentWorker.perform_async(user_id, milestone.name)
-  end
-
-
 end
