aece66e1de5a001ba1729f9f1bd079f2f7c44bd0
Eric Adams <erictayloradams@gmail.com>
2020-11-30 16:59:14 -0600

    update tests

--- services/QuillLMS/client/app/bundles/Comprehension/components/turkerView/__tests__/completed.test.tsx
+++ services/QuillLMS/client/app/bundles/Comprehension/components/turkerView/__tests__/completed.test.tsx
@@ -4,6 +4,7 @@ import { shallow } from 'enzyme';
 import { TurkCompleted } from '../completed';
 
 describe('TurkCompleted Component', () => {
+  Object.defineProperty(global.document, 'queryCommandSupported', { value: jest.fn() });
   const component = shallow(<TurkCompleted code='saideira-maracuja' />);
 
   it('should match snapshot', () => {
