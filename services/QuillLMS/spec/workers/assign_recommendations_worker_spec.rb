817e87e163796a0df4c43e1dcbfa603167d0c1b1
Emilia Friedberg <emiliafriedberg@gmail.com>
2020-12-02 09:34:28 -0500

    segment updates (#7025)
    * segment updates
    
    * remove commented out line
    
    * fix bug with pusher
    
    * fix issue with sidekiq not accepting keyword params

--- services/QuillLMS/spec/workers/assign_recommendations_worker_spec.rb
+++ services/QuillLMS/spec/workers/assign_recommendations_worker_spec.rb
@@ -2,31 +2,32 @@ require 'rails_helper'
 
 describe AssignRecommendationsWorker do
   let(:subject) { described_class.new }
+  let(:unit_template) { create(:unit_template) }
+  let(:classroom) { create(:classroom) }
+  let(:teacher) { classroom.owner }
+  let(:student) { create(:student) }
+  let(:analyzer) { double(:analyzer, track: true) }
+
+  before(:each) do
+    allow(Analyzer).to receive(:new) { analyzer }
+    allow(PusherRecommendationCompleted).to receive(:run) { true }
+    allow(Units::Creator).to receive(:assign_unit_template_to_one_class) { true }
+    allow(Units::Updater).to receive(:assign_unit_template_to_one_class) { true }
+  end
 
   describe "#perform" do
-    before do
-      allow(Units::Creator).to receive(:assign_unit_template_to_one_class) { true }
-      allow(Units::Updater).to receive(:assign_unit_template_to_one_class) { true }
-    end
-
     context 'when no units is found' do
       context 'when unit was found with the unit template name' do
-        let(:unit_template) { create(:unit_template) }
-        let(:classroom) { create(:classroom) }
-        let(:teacher) { classroom.owner }
-        let(:student) { create(:student) }
         let!(:unit) { create(:unit, name: unit_template.name, user: teacher, visible: false) }
 
-        let(:analyzer) { double(:analyzer, track: true) }
-
         def call_method
-          subject.perform(
-              unit_template.id,
-              classroom.id,
-              [student.id],
-              "last",
-              "lesson"
-          )
+          subject.perform({
+            unit_template_id: unit_template.id,
+            classroom_id: classroom.id,
+            student_ids: [student.id],
+            last: "last",
+            lesson: "lesson"
+          })
         end
 
         before do
@@ -65,20 +66,14 @@ describe AssignRecommendationsWorker do
       end
 
       context 'when unit was not found with the unit template name' do
-        let(:unit_template) { create(:unit_template) }
-        let(:classroom) { create(:classroom) }
-        let(:teacher) { classroom.owner }
-        let(:student) { create(:student) }
-        let(:analyzer) { double(:analyzer, track: true) }
-
         def call_method
-          subject.perform(
-              unit_template.id,
-              classroom.id,
-              [student.id],
-              "last",
-              "lesson"
-          )
+          subject.perform({
+            unit_template_id: unit_template.id,
+            classroom_id: classroom.id,
+            student_ids: [student.id],
+            last: "last",
+            lesson: "lesson"
+          })
         end
 
         before do
@@ -112,28 +107,17 @@ describe AssignRecommendationsWorker do
     end
 
     context 'when more than one unit is found' do
-      let(:unit_template) { create(:unit_template) }
-      let(:classroom) { create(:classroom) }
-      let(:teacher) { classroom.owner }
-      let(:student) { create(:student) }
       let!(:unit) { create(:unit, unit_template: unit_template, user: teacher, visible: false, updated_at: Date.today) }
       let!(:unit1) { create(:unit, unit_template: unit_template, user: teacher, visible: false, updated_at: Date.today-1.day) }
 
-      let(:analyzer) { double(:analyzer, track: true) }
-
       def call_method
-        subject.perform(
-            unit_template.id,
-            classroom.id,
-            [student.id],
-            "last",
-            "lesson"
-        )
-      end
-
-      before do
-        allow(Analyzer).to receive(:new) { analyzer }
-        allow(PusherRecommendationCompleted).to receive(:run) { true }
+        subject.perform({
+          unit_template_id: unit_template.id,
+          classroom_id: classroom.id,
+          student_ids: [student.id],
+          last: "last",
+          lesson: "lesson"
+        })
       end
 
       it 'should make the unit visible' do
@@ -167,27 +151,16 @@ describe AssignRecommendationsWorker do
     end
 
     context 'when only one unit is found' do
-      let(:unit_template) { create(:unit_template) }
-      let(:classroom) { create(:classroom) }
-      let(:teacher) { classroom.owner }
-      let(:student) { create(:student) }
       let!(:unit) { create(:unit, unit_template: unit_template, user: teacher, visible: false) }
 
-      let(:analyzer) { double(:analyzer, track: true) }
-
       def call_method
-        subject.perform(
-            unit_template.id,
-            classroom.id,
-            [student.id],
-            "last",
-            "lesson"
-        )
-      end
-
-      before do
-        allow(Analyzer).to receive(:new) { analyzer }
-        allow(PusherRecommendationCompleted).to receive(:run) { true }
+        subject.perform({
+          unit_template_id: unit_template.id,
+          classroom_id: classroom.id,
+          student_ids: [student.id],
+          last: "last",
+          lesson: "lesson"
+        })
       end
 
       it 'should make the unit visible' do
@@ -219,5 +192,63 @@ describe AssignRecommendationsWorker do
         call_method
       end
     end
+
+    context 'when it is the last worker and assigning_all_recommended_packs is not true' do
+      def call_method
+        subject.perform({
+          unit_template_id: unit_template.id,
+          classroom_id: classroom.id,
+          student_ids: [student.id],
+          last: true,
+          lesson: false,
+          assigning_all_recommended_packs: false
+        })
+      end
+
+      it 'should not track that all recommendations are being assigned' do
+        expect(analyzer).not_to receive(:track).with(teacher, SegmentIo::BackgroundEvents::ASSIGN_ALL_RECOMMENDATIONS)
+        call_method
+      end
+
+    end
+
+    context 'when it is not the last worker and assigning_all_recommended_packs is true' do
+      def call_method
+        subject.perform({
+          unit_template_id: unit_template.id,
+          classroom_id: classroom.id,
+          student_ids: [student.id],
+          last: false,
+          lesson: false,
+          assigning_all_recommended_packs: true
+        })
+      end
+
+      it 'should not track that all recommendations are being assigned' do
+        expect(analyzer).not_to receive(:track).with(teacher, SegmentIo::BackgroundEvents::ASSIGN_ALL_RECOMMENDATIONS)
+        call_method
+      end
+
+    end
+
+    context 'when it is the last worker and assigning_all_recommended_packs is true' do
+      def call_method
+        subject.perform({
+          unit_template_id: unit_template.id,
+          classroom_id: classroom.id,
+          student_ids: [student.id],
+          last: true,
+          lesson: false,
+          assigning_all_recommended_packs: true
+        })
+      end
+
+      it 'should track that all recommendations are being assigned' do
+        expect(analyzer).to receive(:track).with(teacher, SegmentIo::BackgroundEvents::ASSIGN_ALL_RECOMMENDATIONS)
+        call_method
+      end
+
+    end
+
   end
 end
