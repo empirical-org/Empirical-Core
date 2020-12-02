4e31e37851dac26dfd609244fb60f530535bc913
Emilia Friedberg <emiliafriedberg@gmail.com>
2020-12-02 09:34:01 -0500

    standards internal tools (#7018)
    * update backend
    
    * archive associated standards when you archive a standard category or level
    
    * get live page rendering
    
    * small changes
    
    * fix tests; small change for tooltips
    
    * update jest tests
    
    * show pointer mouse icon
    
    * revisions
    
    * update how export works

--- services/QuillLMS/client/app/bundles/Staff/components/attributesManager/index.tsx
+++ services/QuillLMS/client/app/bundles/Staff/components/attributesManager/index.tsx
@@ -1,30 +1,35 @@
 import * as React from 'react'
 import { Route, Switch, Link, } from 'react-router-dom';
 
+import Standards from './standards/index'
 import Topics from './topics/index'
 import Readability from './readability/index'
 
+const STANDARDS = 'standards'
 const TOPICS = 'topics'
 const READABILITY = 'readability'
 
 const AttributesManager = ({ match, location, }) => {
 
   let activeLink
-  if (location.pathname.includes(TOPICS)) {
-    activeLink = TOPICS
-  } else if (location.pathname.includes(READABILITY)) {
-    activeLink = READABILITY
-  }
+
+  [STANDARDS, TOPICS, READABILITY].forEach(path => {
+    if (location.pathname.includes(path)) {
+      activeLink = path
+    }
+  })
 
   return (
     <div className="attributes-manager">
       <div className="cms-manager-nav">
         <div className="cms-manager-links">
+          <Link className={activeLink === STANDARDS ? 'active': ''} to={`${match.path}/${STANDARDS}`}>Standards</Link>
           <Link className={activeLink === READABILITY ? 'active': ''} to={`${match.path}/${READABILITY}`}>Readability</Link>
           <Link className={activeLink === TOPICS ? 'active': ''} to={`${match.path}/${TOPICS}`}>Topics</Link>
         </div>
       </div>
       <Switch>
+        <Route component={Standards} path={`${match.path}/${STANDARDS}`} />
         <Route component={Readability} path={`${match.path}/${READABILITY}`} />
         <Route component={Topics} path={`${match.path}/${TOPICS}`} />
       </Switch>
