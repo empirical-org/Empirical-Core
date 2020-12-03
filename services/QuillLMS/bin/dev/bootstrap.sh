1404ee656527ba15f6658221d2d2e3076d9810e9
Peter Kong <pkong@quill.org>
2020-12-02 21:37:33 -0700

    [PK] updates to bootstrap.sh and README setup docs

--- services/QuillLMS/bin/dev/bootstrap.sh
+++ services/QuillLMS/bin/dev/bootstrap.sh
@@ -22,7 +22,6 @@ brew doctor
 
 echo 'Install requirements'
 # TODO, edit script to use version 10.5, this is version 10.6 of Postgres
-brew install postgresql@10
 brew install redis
 brew install rbenv
 brew install ruby-build
@@ -49,8 +48,8 @@ fi
 curl -fsSL https://github.com/rbenv/rbenv-installer/raw/master/bin/rbenv-doctor | bash
 
 eval "$(rbenv init -)"
-echo 'eval "$(rbenv init -)"' >> ~/.bash_profile
-source ~/.bash_profile
+echo 'eval "$(rbenv init -)"' >> ~/.zprofile
+source ~/.zprofile
 cd .
 rbenv rehash
 
@@ -73,6 +72,7 @@ git remote add quill-lms-prod  https://git.heroku.com/empirical-grammar.git
 echo 'Install Bundler'
 # there are breaking changes in bundler 2.0, so pin to this version for now.
 gem install bundler -v 1.17.3
+gem install foreman -v 0.87.2
 
 # set bundle config, needed for sidekiq-pro
 export BUNDLE_GEMS__CONTRIBSYS__COM=$(heroku config:get BUNDLE_GEMS__CONTRIBSYS__COM -a empirical-grammar)
@@ -105,16 +105,18 @@ echo 'Copy config files'
 cp -n config/database.yml.example config/database.yml
 cp -n .env-sample .env
 
-echo 'start postgres'
-brew services restart postgresql@10
+### homebrew postgres installation. ###################
+# Uncomment if you don't want to use Postgres.app
+# brew install postgresql@10
+# brew services restart postgresql@10
+# echo 'export PATH="/usr/local/opt/postgresql@10/bin:$PATH"' >> ~/.zprofile
+# export PKG_CONFIG_PATH="/usr/local/opt/postgresql@10/lib/pkgconfig"
 
-echo 'export PATH="/usr/local/opt/postgresql@10/bin:$PATH"' >> ~/.bash_profile
-export PKG_CONFIG_PATH="/usr/local/opt/postgresql@10/lib/pkgconfig"
-
-echo 'create the app DB user quill_dev and quill_test'
-# Ideally, this would be more restrictive (shouldn't need a superuser), but would need some DB edits
-createuser quill_dev --superuser
-createuser quill_test --superuser
+# echo 'create the app DB user quill_dev and quill_test'
+# # Ideally, this would be more restrictive (shouldn't need a superuser), but would need some DB edits
+# createuser quill_dev --superuser
+# createuser quill_test --superuser
+### homebrew postgres installation #####################
 
 echo 'set up app DB, migrate database'
 bundle exec rake db:setup
@@ -128,4 +130,4 @@ brew install redis
 brew services restart redis
 
 echo 'start server'
-foreman start -f Procfile.dev
+foreman start -f Procfile.static
