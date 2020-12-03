4d808e5226de075b73d89fb7e5ebfdcfe3d78eae
Emilia Friedberg <emiliafriedberg@gmail.com>
2020-12-03 14:34:39 -0500

    fix additional broken test

--- services/QuillLMS/spec/workers/classroom_creation_worker_spec.rb
+++ services/QuillLMS/spec/workers/classroom_creation_worker_spec.rb
@@ -9,6 +9,7 @@ describe ClassroomCreationWorker, type: :worker do
     worker.perform(classroom.id)
 
     expect(analytics.backend.track_calls.size).to eq(2)
+    expect(analytics.backend.track_calls[0][:event]).to eq("#{SegmentIo::BackgroundEvents::CLASSROOM_CREATION} | Manual")
     expect(analytics.backend.track_calls[0][:event]).to eq(SegmentIo::BackgroundEvents::CLASSROOM_CREATION)
   end
 end
