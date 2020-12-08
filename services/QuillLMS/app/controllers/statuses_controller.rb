b81469c637a39843f86b094d8a3fe9a354762c30
happythenewsad <happythenewsad@users.noreply.github.com>
2020-12-08 09:55:33 -0700

    Nr deploy notifier (#7041)
    * added route, specs, cassette
    
    * bug fix
    
    * fixing lint error
    
    * adding env var so Circle CI passes
    
    * added blank new relic api key to .env-sample
    
    * feedback
    
    Co-authored-by: Peter Kong <pkong@quill.org>

--- services/QuillLMS/app/controllers/statuses_controller.rb
+++ services/QuillLMS/app/controllers/statuses_controller.rb
@@ -1,5 +1,6 @@
 class StatusesController < ApplicationController
   skip_before_action :stick_to_leader_db, only: [:database_follower]
+  protect_from_forgery with: :null_session
 
   def index
     render plain: 'OK'
@@ -34,4 +35,20 @@ class StatusesController < ApplicationController
     render json: Sidekiq::Stats.new.queues
   end
 
+  def deployment_notification
+    resp = Faraday.post("https://api.newrelic.com/v2/applications/#{ENV['NEW_RELIC_APP_ID']}/deployments.json") do |req|
+      req.headers['X-Api-Key'] = ENV['NEW_RELIC_REST_API_KEY']
+      req.headers['Content-Type'] = 'application/json'
+      req.body = {
+        "deployment": {
+          "revision": "Commit: #{params['head_long']} | Heroku release: #{params['release']}",
+          "changelog": params['git_log'],
+          "description": params['head_long'],
+          "user": params['user'],
+          "timestamp": Time.now.utc
+        }
+      }.to_json
+    end
+    render plain: "OK", status: resp.status
+  end
 end
