1e263b98ff8269da158a24c63e9ba9c02f69ccad
Emilia Friedberg <emiliafriedberg@gmail.com>
2020-12-09 08:18:41 -0500

    content partner internal tools (#7030)
    * add visible column to content partner table
    
    * create internal tools for content partners
    
    * add tests
    
    * small css change
    
    * revisions

---
+++ services/QuillLMS/client/app/bundles/Staff/components/attributesManager/contentPartners/index.tsx
@@ -0,0 +1,101 @@
+import * as React from 'react'
+import { Route, Switch, Link, } from 'react-router-dom';
+
+import ContentPartnersTable from './contentPartnersTable'
+
+import { Snackbar, defaultSnackbarTimeout } from '../../../../Shared/index'
+import { requestGet, requestPut, requestPost, } from '../../../../../modules/request/index'
+
+const ARCHIVED = 'archived'
+
+const ContentPartners = ({ match, location, }) => {
+  const [contentPartners, setContentPartners] = React.useState([])
+  const [showSnackbar, setShowSnackbar] = React.useState(false)
+
+  React.useEffect(() => {
+    getInitialData();
+  }, []);
+
+  React.useEffect(() => {
+    if (showSnackbar) {
+      setTimeout(() => setShowSnackbar(false), defaultSnackbarTimeout)
+    }
+  }, [showSnackbar])
+
+  function getInitialData() {
+    getContentPartners()
+  }
+
+  function getContentPartners() {
+    requestGet('/cms/content_partners',
+      (data) => {
+        setContentPartners(data.content_partners);
+      }
+    )
+  }
+
+  function saveContentPartnerChanges(contentPartner) {
+    requestPut(`/cms/content_partners/${contentPartner.id}`, { content_partner: contentPartner, },
+      (data) => {
+        getInitialData()
+        setShowSnackbar(true)
+      }
+    )
+  }
+
+  function createNewContentPartner(contentPartner) {
+    requestPost(`/cms/content_partners`, { content_partner: contentPartner, },
+      (data) => {
+        getInitialData()
+        setShowSnackbar(true)
+      }
+    )
+  }
+
+  let activeLink
+
+  [ARCHIVED].forEach(path => {
+    if (location.pathname.includes(path)) {
+      activeLink = path
+    }
+  })
+
+  const sharedProps = {
+    path: `${match.path}`,
+    saveContentPartnerChanges,
+    contentPartners
+  }
+
+  return (
+    <div className="content-partners-manager">
+      <Snackbar text="Changes saved" visible={showSnackbar} />
+      <div className="cms-tabs">
+        <Link className={activeLink ? '': 'active'} to={`${match.path}`}>Live</Link>
+        <Link className={activeLink === ARCHIVED ? 'active': ''} to={`${match.path}/${ARCHIVED}`}>Archived</Link>
+      </div>
+      <Switch>
+        <Route
+          component={() => (
+            <ContentPartnersTable
+              {...sharedProps}
+              visible={false}
+            />
+          )}
+          path={`${match.path}/${ARCHIVED}`}
+        />
+        <Route
+          component={() => (
+            <ContentPartnersTable
+              {...sharedProps}
+              createNewContentPartner={createNewContentPartner}
+              visible={true}
+            />
+          )}
+          path={match.path}
+        />
+      </Switch>
+    </div>
+  )
+}
+
+export default ContentPartners
