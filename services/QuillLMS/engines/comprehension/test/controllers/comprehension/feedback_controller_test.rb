8c62cbe6d33bf8f6b9216f9c04f53af1cd04e954
Cissy Yu <cissy@quill.org>
2020-12-15 15:56:57 -0500

    comment changes

--- services/QuillLMS/engines/comprehension/test/controllers/comprehension/feedback_controller_test.rb
+++ services/QuillLMS/engines/comprehension/test/controllers/comprehension/feedback_controller_test.rb
@@ -14,11 +14,19 @@ module Comprehension
         assert_equal parsed_response["optimal"], true
       end
 
+      should "return 404 if prompt id does not exist" do
+        prompt = create(:comprehension_prompt)
+        post 'plagiarism', entry: "No plagiarism here.", prompt_id: 100, session_id: 1, previous_feedback: []
+        assert_equal response.status, 404
+      end
+
       should "return successfully when there is plagiarism" do
         prompt = create(:comprehension_prompt)
         post 'plagiarism', entry: "bla bla bla do not plagiarize this text please", prompt_id: prompt.id, session_id: 1, previous_feedback: []
         parsed_response = JSON.parse(response.body)
         assert_equal parsed_response["optimal"], false
+        assert_equal parsed_response["highlight"][0]["text"], "do not plagiarize this text please"
+        assert_equal parsed_response["highlight"][1]["text"], "do not plagiarize this text please"
       end
 
       should "return successfully when there is plagiarism and ignore punctuation" do
@@ -26,6 +34,17 @@ module Comprehension
         post 'plagiarism', entry: "bla bla bla do not plagiarize,,,,.. this text please", prompt_id: prompt.id, session_id: 1, previous_feedback: []
         parsed_response = JSON.parse(response.body)
         assert_equal parsed_response["optimal"], false
+        assert_equal parsed_response["highlight"][0]["text"], "do not plagiarize this text please"
+        assert_equal parsed_response["highlight"][1]["text"], "do not plagiarize,,,,.. this text please"
+      end
+
+      should "return successfully when there is plagiarism and ignore punctuation in both passage and entry" do
+        prompt = create(:comprehension_prompt)
+        post 'plagiarism', entry: "bla bla bla do not plagiarize,,,,.. this text please! thank,, you.", prompt_id: prompt.id, session_id: 1, previous_feedback: []
+        parsed_response = JSON.parse(response.body)
+        assert_equal parsed_response["optimal"], false
+        assert_equal parsed_response["highlight"][0]["text"], "do not plagiarize this text please, thank you"
+        assert_equal parsed_response["highlight"][1]["text"], "do not plagiarize,,,,.. this text please! thank,, you"
       end
 
       should "return second feedback when session has already received plagiarism feedback" do
