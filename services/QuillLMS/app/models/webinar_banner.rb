bdbcb658383793b348e92ca5ba0e0cc421e3372a
Emilia Friedberg <emiliafriedberg@gmail.com>
2020-12-16 14:22:10 -0500

    update webinar banners to extend dates of 101 webinar

--- services/QuillLMS/app/models/webinar_banner.rb
+++ services/QuillLMS/app/models/webinar_banner.rb
@@ -6,16 +6,10 @@ class WebinarBanner
   # ONE-OFFS have the key format Month-Day-Hour
   # RECURRING have the key format DayOfWeek-Hour
 
-  ONE_OFFS = {
-    '11-18-16' => ["Quill's Spotlight on Equitable Grading using Quill", "#{ZOOM_URL}/WN_BNF3nVnyRVqrKQ9udZis-w"],
-    '12-2-16' => ["Quill's Spotlight on Using Quill to Support English Language Learners", "#{ZOOM_URL}/WN_R3h0pKEHTVqiLlNwdFHF4g"],
-    '12-9-16' => ["Quill's Spotlight on Sentence Fluency at the Lesson Level", "#{ZOOM_URL}/WN_M0eiXLFaSEq-GQOAzj0kSA"],
-    '12-16-16' => ["Quill's Spotlight on Encouraging & Empowering Your Writers", "#{ZOOM_URL}/WN_yB6Lltm3QH2h4AnobnCdLw"]
-  }
+  ONE_OFFS = {}
 
   RECURRING = {
-    '1-16' => ["Quill 101", "#{ZOOM_URL}/WN_a4Z1_Zs6RSGUWwr_t0V18Q"],
-    '2-16' => ["Quill in Pre-AP® and AP®: Spotlight on Data and Instruction", "#{ZOOM_URL}/WN_UuoWsG_8Q5Cpbh9Bqqi24g"]
+    '1-16' => ["Quill Webinar 101: Getting Started", "#{ZOOM_URL}/WN_a4Z1_Zs6RSGUWwr_t0V18Q"]
   }
 
   def initialize(time)
--- services/QuillLMS/spec/models/webinar_banner_spec.rb
+++ services/QuillLMS/spec/models/webinar_banner_spec.rb
@@ -16,12 +16,13 @@ describe WebinarBanner, type: :model do
   end
 
   it "does return true for show? when the key does have an associated webinar" do
-    time =  DateTime.new(2020,12,16,16,1,0)
+    time =  DateTime.new(2021,01,18,16,1,0)
     banner = WebinarBanner.new(time)
     expect(banner.show?).to eq(true)
   end
 
-  it "does return correct link and title when the key does have an associated one-off webinar" do
+  # commenting out this test for now because we don't have any one-off webinars scheduled
+  xit "does return correct link and title when the key does have an associated one-off webinar" do
     time =  DateTime.new(2020,12,16,16,1,0)
     banner = WebinarBanner.new(time)
     expect(banner.title).to eq("Quill's Spotlight on Encouraging & Empowering Your Writers")
@@ -29,9 +30,9 @@ describe WebinarBanner, type: :model do
   end
 
   it "does return correct link and title when the key does have an associated recurring webinar" do
-    time =  DateTime.new(2020,10,26,16,1,0)
+    time =  DateTime.new(2021,01,18,16,1,0)
     banner = WebinarBanner.new(time)
-    expect(banner.title).to eq("Quill 101")
+    expect(banner.title).to eq("Quill Webinar 101: Getting Started")
     expect(banner.link).to eq("https://quill-org.zoom.us/webinar/register/WN_a4Z1_Zs6RSGUWwr_t0V18Q")
   end
 
