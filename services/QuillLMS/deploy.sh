f3633006191e91ca5e34a862626d3cb98fe50b8a
dandrabik <dandrabik@users.noreply.github.com>
2021-01-08 10:21:27 -0500

    Deploy Script - Ensure updated local history of origin/production (#7101)
    * Ensure that local git has current pointer to origin/production
    
    * Update comments. Add extra fetches.

--- services/QuillLMS/deploy.sh
+++ services/QuillLMS/deploy.sh
@@ -41,9 +41,15 @@ then
     if [ $1 == 'prod' ]
     then
         # For production, push directly from the remote production branch without going local
+        # This 'remote merge' requires your local git history/pointers of the remote branches to be up-to-date, so we run a 'git fetch' to do that.
+        # Documented here: https://github.com/empirical-org/test_repo/blob/destination_branch/test_file.txt
+        git fetch origin production
+        git fetch origin $DEPLOY_GIT_BRANCH
         git push --no-verify --force origin origin/production:refs/heads/$DEPLOY_GIT_BRANCH
+
         sh ../../scripts/post_slack_deploy_description.sh $app_name
     else
+        git fetch origin $DEPLOY_GIT_BRANCH
         git push --no-verify --force origin ${current_branch}:$DEPLOY_GIT_BRANCH
     fi
     open "https://dashboard.heroku.com/apps/$HEROKU_APP/activity"
