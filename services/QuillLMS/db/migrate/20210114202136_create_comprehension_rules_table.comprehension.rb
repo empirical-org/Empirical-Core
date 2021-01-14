bb51f8bdf6f1cfb1b4fdba9a128cc4245584afed
Cissy Yu <cissy@quill.org>
2021-01-14 15:22:56 -0500

    add migration file to LMS

---
+++ services/QuillLMS/db/migrate/20210114202136_create_comprehension_rules_table.comprehension.rb
@@ -0,0 +1,18 @@
+# This migration comes from comprehension (originally 20210114182832)
+class CreateComprehensionRulesTable < ActiveRecord::Migration
+  def change
+    create_table :comprehension_rules do |t|
+      t.string :uid
+      t.string :name
+      t.string :description
+      t.boolean :universal
+      t.string :rule_type
+      t.boolean :optimal
+      t.integer :suborder
+      t.string :concept_uid
+
+      t.timestamps null: false
+    end
+    add_index :comprehension_rules, :uid
+  end
+end
