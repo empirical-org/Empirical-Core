86ad7761f8f7f558f3372d1440ef195d15caffc4
Emilia Friedberg <emiliafriedberg@gmail.com>
2021-01-21 14:00:47 -0500

    update everywhere we use sortablelist to use the same component (#7138)

--- services/QuillLMS/client/app/bundles/Teacher/components/assignment_flow/create_unit/custom_activity_pack/staff_activity_category_filters.tsx
+++ services/QuillLMS/client/app/bundles/Teacher/components/assignment_flow/create_unit/custom_activity_pack/staff_activity_category_filters.tsx
@@ -4,9 +4,8 @@ import { SortableHandle, } from 'react-sortable-hoc';
 import { Activity, ActivityCategory, ActivityCategoryEditor } from './interfaces'
 import { ACTIVITY_CATEGORY_FILTERS, AVERAGE_FONT_WIDTH, } from './shared'
 
-import SortableList from '../../../shared/sortableList'
 import { requestPost, } from '../../../../../../modules/request/index'
-import { Tooltip, Input, Snackbar, defaultSnackbarTimeout } from '../../../../../Shared/index'
+import { Tooltip, Input, Snackbar, defaultSnackbarTimeout, SortableList, } from '../../../../../Shared/index'
 
 const smallWhiteCheckSrc = `${process.env.CDN_URL}/images/shared/check-small-white.svg`
 const reorderSrc = `${process.env.CDN_URL}/images/icons/reorder.svg`
