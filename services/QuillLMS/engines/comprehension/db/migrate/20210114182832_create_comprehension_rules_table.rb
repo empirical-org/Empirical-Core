ee46b9d7036753753a2f42c03e8a1afcd8bc6f02
Thomas Robertson <thomas.robertson@quill.org>
2021-01-21 14:03:17 -0500

    Update migrations to use null: false requirements and unique indexes

--- services/QuillLMS/engines/comprehension/db/migrate/20210114182832_create_comprehension_rules_table.rb
+++ services/QuillLMS/engines/comprehension/db/migrate/20210114182832_create_comprehension_rules_table.rb
@@ -1,17 +1,17 @@
 class CreateComprehensionRulesTable < ActiveRecord::Migration
   def change
     create_table :comprehension_rules do |t|
-      t.string :uid
-      t.string :name
+      t.string :uid, null: false
+      t.string :name, null: false
       t.string :description
-      t.boolean :universal
-      t.string :rule_type
-      t.boolean :optimal
+      t.boolean :universal, null: false
+      t.string :rule_type, null: false
+      t.boolean :optimal, null: false
       t.integer :suborder
-      t.string :concept_uid
+      t.string :concept_uid, null: false
 
       t.timestamps null: false
     end
-    add_index :comprehension_rules, :uid
+    add_index :comprehension_rules, :uid, unique: true
   end
 end
