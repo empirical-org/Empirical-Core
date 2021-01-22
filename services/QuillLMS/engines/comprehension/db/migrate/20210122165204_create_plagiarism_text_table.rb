7c1ef7a5c9f4e4352c6ee4597b9b07d8080037d1
Cissy Yu <cissy@quill.org>
2021-01-22 15:07:03 -0500

    migration files

--- services/QuillLMS/engines/comprehension/db/migrate/20210122165204_create_plagiarism_text_table.rb
+++ services/QuillLMS/engines/comprehension/db/migrate/20210122165204_create_plagiarism_text_table.rb
@@ -1,12 +1,12 @@
 class CreatePlagiarismTextTable < ActiveRecord::Migration
   def change
-    create_table :comprehension_plagiarism_text do |t|
-      t.references :prompt, null: false
-      t.string :plagiarism_text, null: false
-    
+    create_table :comprehension_plagiarism_texts do |t|
+      t.references :rule, null: false
+      t.string :text, null: false
+
       t.timestamps null: false
     end
 
-    add_index :comprehension_plagiarism_text, :prompt_id    
+    add_index :comprehension_plagiarism_texts, :rule_id, unique: true
   end
 end
