2bd3e1174fea66d8a082208753dcaedf86534889
Cissy Yu <cissy@quill.org>
2021-01-22 15:45:40 -0500

    inverse of relationship

--- services/QuillLMS/engines/comprehension/app/models/comprehension/feedback.rb
+++ services/QuillLMS/engines/comprehension/app/models/comprehension/feedback.rb
@@ -1,12 +1,13 @@
 module Comprehension
   class Feedback < ActiveRecord::Base
-    MIN_FEEDBACK_LENGTH = 10 
+    MIN_FEEDBACK_LENGTH = 10
     MAX_FEEDBACK_LENGTH = 500
-    belongs_to :rule    
+    belongs_to :rule, inverse_of: :feedbacks
 
+    validates_presence_of :rule
     validates :text, presence: true, length: {minimum: MIN_FEEDBACK_LENGTH, maximum: MAX_FEEDBACK_LENGTH}
     validates :order, numericality: {only_integer: true, greater_than_or_equal_to: 0}, uniqueness: {scope: :rule_id}
-    
+
     def serializable_hash(options = nil)
       options ||= {}
 
