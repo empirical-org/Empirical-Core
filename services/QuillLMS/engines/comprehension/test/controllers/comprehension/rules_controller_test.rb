9a6397da1bf55ac0ad6ebb40d3ee5a164a78c61e
Anathomical <thomas.robertson@quill.org>
2021-01-26 15:25:51 -0500

    lms/comprehension-add-feedback-highlights-2 (#7167)
    * Update models and controllers with new model name
    
    This should point at the newly-created copy of the data in the db
    
    * Update tests and model references to the new RegexRule model
    
    * Update routes for regex_rules and add migration to drop old table
    
    * Install migration to remove old "comprehension_rule" table from LMS
    
    * added migration, model, and test for rule
    
    * added model and controller tests, tweaked the type column to be rule_type
    
    * take out ability to modify uid from controller
    
    * rename the migration file for importing
    
    * add migration file to LMS
    
    * added migration, model, and test for rule
    
    * added model and controller tests, tweaked the type column to be rule_type
    
    * take out ability to modify uid from controller
    
    * rename the migration file for importing
    
    * add migration file to LMS
    
    * Fix some linting
    
    * merge conflicts
    
    * Update migrations to use null: false requirements and unique indexes
    
    * Provide sample object from factory so uniqueness shoulda doesn't crash
    
    * Commit updated db structure after migrations are all run
    
    * Make sure there are no records before we try creating one
    
    * add 2 migration files
    
    * add models controllers and tests
    
    * Add migrations for highlights
    
    * Add models and tests for highlights
    
    * Add a nested update test
    
    * Fix some linting errors
    
    Co-authored-by: Cissy Yu <cissy@quill.org>
    Co-authored-by: cissyxyu <57366100+cissyxyu@users.noreply.github.com>

--- services/QuillLMS/engines/comprehension/test/controllers/comprehension/rules_controller_test.rb
+++ services/QuillLMS/engines/comprehension/test/controllers/comprehension/rules_controller_test.rb
@@ -141,6 +141,23 @@ module Comprehension
         assert_equal feedback.order, parsed_response['feedbacks'][0]['order']
         
         assert_equal 1, Feedback.count
+      end
+
+      should "create nested highlight record when nested in feedback_attributes" do
+        assert_equal 0, Highlight.count    
+    
+        feedback = create(:comprehension_feedback, rule: @rule)
+        highlight = build(:comprehension_highlight, starting_index: 2)
+        post :create, rule: { concept_uid: @rule.concept_uid, description: @rule.description, name: @rule.name, optimal: @rule.optimal, suborder: @rule.suborder, rule_type: @rule.rule_type, universal: @rule.universal, feedbacks_attributes: [{text: feedback.text, description: feedback.description, order: feedback.order, highlights_attributes: [{text: highlight.text, highlight_type: highlight.highlight_type, starting_index: highlight.starting_index }]}]}
+
+        parsed_response = JSON.parse(response.body)
+        assert_equal 201, response.code.to_i
+
+        assert_equal highlight.text, parsed_response['feedbacks'][0]['highlights'][0]['text']
+        assert_equal highlight.highlight_type, parsed_response['feedbacks'][0]['highlights'][0]['highlight_type']
+        assert_equal highlight.starting_index, parsed_response['feedbacks'][0]['highlights'][0]['starting_index']
+        
+        assert_equal 1, Highlight.count
       end 
     end
 
@@ -216,9 +233,23 @@ module Comprehension
 
         assert_equal 204, response.code.to_i
         assert_equal "", response.body
-        
+
         feedback.reload
         assert_equal feedback.text, new_text
+      end
+
+      should "update nested highlight attributes in feedback if present" do
+        feedback = create(:comprehension_feedback, rule: @rule)
+        highlight = create(:comprehension_highlight, feedback: feedback)
+        new_text = "New text to highlight"
+    
+        post :update, id: @rule.id, rule: { feedbacks_attributes: [{id: feedback.id, highlights_attributes: [{id: highlight.id, text: new_text}]}]}
+
+        assert_equal 204, response.code.to_i
+        assert_equal "", response.body
+
+        highlight.reload
+        assert_equal new_text, highlight.text
       end 
 
     end
