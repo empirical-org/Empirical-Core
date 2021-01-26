58e431accea2f7e732c223fb8d967a1a3fcfe391
cissyxyu <57366100+cissyxyu@users.noreply.github.com>
2021-01-26 14:09:25 -0500

    Add PlagiarismFeedback migrations (#7170)
    * add migration files
    
    * add indices to migration tables
    
    * migration files
    
    * add foreign key
    
    * linting

---
+++ services/QuillLMS/engines/comprehension/db/migrate/20210122165204_create_plagiarism_text_table.rb
@@ -0,0 +1,13 @@
+class CreatePlagiarismTextTable < ActiveRecord::Migration
+  def change
+    create_table :comprehension_plagiarism_texts do |t|
+      t.references :rule, null: false
+      t.string :text, null: false
+
+      t.timestamps null: false
+    end
+
+    add_index :comprehension_plagiarism_texts, :rule_id, unique: true
+    add_foreign_key :comprehension_plagiarism_texts, :comprehension_rules, column: :rule_id, on_delete: :cascade
+  end
+end
