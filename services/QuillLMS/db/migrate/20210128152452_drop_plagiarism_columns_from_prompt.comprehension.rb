f17dd20529abffb65814f4619fbef00d3f27e6d3
Cissy Yu <cissy@quill.org>
2021-01-28 12:39:37 -0500

    typo on migrations

--- services/QuillLMS/db/migrate/20210128152452_drop_plagiarism_columns_from_prompt.comprehension.rb
+++ services/QuillLMS/db/migrate/20210128152452_drop_plagiarism_columns_from_prompt.comprehension.rb
@@ -1,8 +1,8 @@
 # This migration comes from comprehension (originally 20210128152309)
 class DropPlagiarismColumnsFromPrompt < ActiveRecord::Migration
   def change
-    remove_column :comprehension_prompt, :plagiarism_text
-    remove_column :comprehension_prompt, :plagiarism_first_feedback
-    remove_column :comprehension_prompt, :plagiarism_second_feedback
+    remove_column :comprehension_prompts, :plagiarism_text
+    remove_column :comprehension_prompts, :plagiarism_first_feedback
+    remove_column :comprehension_prompts, :plagiarism_second_feedback
   end
 end
--- services/QuillLMS/engines/comprehension/db/migrate/20210128152309_drop_plagiarism_columns_from_prompt.rb
+++ services/QuillLMS/engines/comprehension/db/migrate/20210128152309_drop_plagiarism_columns_from_prompt.rb
@@ -1,7 +1,7 @@
 class DropPlagiarismColumnsFromPrompt < ActiveRecord::Migration
   def change
-    remove_column :comprehension_prompt, :plagiarism_text
-    remove_column :comprehension_prompt, :plagiarism_first_feedback
-    remove_column :comprehension_prompt, :plagiarism_second_feedback
+    remove_column :comprehension_prompts, :plagiarism_text
+    remove_column :comprehension_prompts, :plagiarism_first_feedback
+    remove_column :comprehension_prompts, :plagiarism_second_feedback
   end
 end
