eed1286cba1de40958f7686ebad127275b01ef3e
dandrabik <dandrabik@users.noreply.github.com>
2021-01-29 10:41:05 -0500

    Only delay by 6 hours for testing purposes. (#7190)

--- services/QuillCMS/app/controllers/responses_controller.rb
+++ services/QuillCMS/app/controllers/responses_controller.rb
@@ -43,7 +43,7 @@ class ResponsesController < ApplicationController
   def create_or_increment
     transformed_response = transformed_new_vals(params_for_create).to_h
     # delaying this to off-hours to eliminate read/write traffic in peak hours
-    CreateOrIncrementResponseWorker.perform_in(12.hours, transformed_response)
+    CreateOrIncrementResponseWorker.perform_in(6.hours, transformed_response)
     render json: {}
   end
 
