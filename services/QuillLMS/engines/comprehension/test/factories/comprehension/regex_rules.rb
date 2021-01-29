74f3acdeb267090f4c5d02c84c61754af418361a
Cissy Yu <cissy@quill.org>
2021-01-29 13:59:01 -0500

    delete references to rule_set

--- services/QuillLMS/engines/comprehension/test/factories/comprehension/regex_rules.rb
+++ services/QuillLMS/engines/comprehension/test/factories/comprehension/regex_rules.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :comprehension_regex_rule, class: 'Comprehension::RegexRule' do
-    association :rule_set, factory: :comprehension_rule_set
+    association :rule, factory: :comprehension_rule
     regex_text { "MyString" }
     case_sensitive { false }
   end
--- services/QuillLMS/engines/comprehension/test/factories/comprehension/rule_sets.rb
+++
@@ -1,8 +0,0 @@
-FactoryBot.define do
-  factory :comprehension_rule_set, class: 'Comprehension::RuleSet' do
-    association :activity, factory: :comprehension_activity
-    name { "MyString" }
-    feedback { "MyString" }
-    priority { 1 }
-  end
-end
