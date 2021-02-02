68bfa73b8627ec04a35adb9cd016286e905e8286
Eric Adams <erictayloradams@gmail.com>
2021-02-02 10:53:01 -0600

    add missing question prop for Cue component (#7200)
    * add missing question prop
    
    * add tests

---
+++ services/QuillLMS/client/app/bundles/Connect/components/questions/__tests__/question.test.jsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import { Question } from '../question';
+import Cues from '../../renderForQuestions/cues.jsx';
+
+// TODO: add more tests
+
+describe('Question component', () => {
+
+  const mockProps = {
+    match: {
+      params: {
+        questionID: 'abc'
+      }
+    },
+    questions: {
+      hasreceiveddata: true,
+      data: {
+        abc: {
+          value: 'test1'
+        },
+        def: {
+          value: 'test2'
+        },
+        ghi: {
+          value: 'test3'
+        }
+      },
+       states: {
+        abc: {
+          value: 'test1'
+        },
+        def: {
+          value: 'test2'
+        },
+        ghi: {
+          value: 'test3'
+        }
+       }
+    },
+    massEdit: {
+      numSelectedResponses: 3
+    }
+  }
+
+  const component = shallow(<Question {...mockProps} />);
+
+  it('passes the correct props to Cues component', () => {
+    expect(component.find(Cues).props().displayArrowAndText).toEqual(true);
+    expect(component.find(Cues).props().question).toEqual(mockProps.questions.data['abc']);
+  });
+});
