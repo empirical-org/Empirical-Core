5b52588bde12a8262bb429aec606d053d059b560
Cissy Yu <cissy@quill.org>
2021-02-02 14:03:05 -0600

    chagne to transaction

--- services/QuillLMS/engines/comprehension/lib/comprehension/comprehension/regex_migration_runner.rb
+++ services/QuillLMS/engines/comprehension/lib/comprehension/comprehension/regex_migration_runner.rb
@@ -1,19 +1,21 @@
 module Comprehension
   class RegexMigrationRunner
     def self.run
-      RuleSet.all.each do |rule_set|
-        rule = Rule.find_or_create_by!(
-          name: rule_set.name,
-          suborder: rule_set.priority,
-          universal: false,
-          optimal: false,
-          rule_type: Rule::TYPE_REGEX,
-          concept_uid: 'whtever'
-        )
-        rule.prompts << rule_set.prompts if rule.prompts != rule_set.prompts
-        Feedback.find_or_create_by!(rule: rule, text: rule_set.feedback, order: rule_set.priority)
-        rule_set.regex_rules.each do |regex_rule|
-          regex_rule.update!(rule_id: rule.id)
+      ActiveRecord::Base.transaction do
+        RuleSet.all.each do |rule_set|
+          rule = Rule.create!(
+            name: rule_set.name,
+            suborder: rule_set.priority,
+            universal: false,
+            optimal: false,
+            rule_type: Rule::TYPE_REGEX,
+            concept_uid: 'temp-uid'
+          )
+          rule.prompts << rule_set.prompts if rule.prompts != rule_set.prompts
+          Feedback.create!(rule: rule, text: rule_set.feedback, order: rule_set.priority)
+          rule_set.regex_rules.each do |regex_rule|
+            regex_rule.update!(rule_id: rule.id)
+          end
         end
       end
     end
