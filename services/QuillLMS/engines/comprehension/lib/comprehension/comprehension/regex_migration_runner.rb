97ccd9558e3f6706c547ea605ca1176bcbd90527
Cissy Yu <cissy@quill.org>
2021-02-03 10:53:43 -0600

    take out if statement

--- services/QuillLMS/engines/comprehension/lib/comprehension/comprehension/regex_migration_runner.rb
+++ services/QuillLMS/engines/comprehension/lib/comprehension/comprehension/regex_migration_runner.rb
@@ -12,7 +12,7 @@ module Comprehension
             concept_uid: 'temp-uid',
             sequence_type: Rule::TYPE_INCORRECT_SEQ
           )
-          rule.prompts << rule_set.prompts if rule.prompts != rule_set.prompts
+          rule.prompts << rule_set.prompts
           Feedback.create!(rule: rule, text: rule_set.feedback, order: rule_set.priority)
           rule_set.regex_rules.each do |regex_rule|
             regex_rule.update!(rule_id: rule.id)
