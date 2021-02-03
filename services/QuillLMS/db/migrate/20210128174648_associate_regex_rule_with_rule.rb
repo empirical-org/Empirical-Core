0ce3cd08e90e7c6a61ba93aee692b742cbbc098b
cissyxyu <57366100+cissyxyu@users.noreply.github.com>
2021-02-03 14:52:08 -0600

    Migrate Comprehension Regex over to new Model format (#7207)
    * add migration runner class and db migrations
    
    * add new relationships and tests
    
    * add rake task
    
    * add test
    
    * chagne to transaction
    
    * extend max length to 100
    
    * up char limit to 250
    
    * add sequence type checks
    
    * add rule
    
    * take out if statement
    
    * take out sequence type
    
    * change last migration
    
    * make task idempotent
    
    * put in regex rule api endpoint
    
    * take out not null requirement on concept_uid
    
    * added prompts to initialization
    
    * take out db structure file

---
+++ services/QuillLMS/db/migrate/20210128174648_associate_regex_rule_with_rule.rb
@@ -0,0 +1,8 @@
+# This migration comes from comprehension (originally 20210128155938)
+class AssociateRegexRuleWithRule < ActiveRecord::Migration
+  def change
+    add_reference :comprehension_regex_rules, :rule, index: true
+    add_foreign_key :comprehension_regex_rules, :comprehension_rules, column: :rule_id, on_delete: :cascade
+    change_column :comprehension_rules, :concept_uid, :string, null: true
+  end
+end
