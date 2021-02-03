5daea2d12304cbb0cf67fd6b2eb15cc489998e26
Cissy Yu <cissy@quill.org>
2021-02-03 13:21:07 -0600

    added prompts to initialization

--- services/QuillLMS/engines/comprehension/lib/comprehension/comprehension/regex_migration_runner.rb
+++ services/QuillLMS/engines/comprehension/lib/comprehension/comprehension/regex_migration_runner.rb
@@ -9,9 +9,9 @@ module Comprehension
             suborder: rule_set.priority,
             universal: false,
             optimal: false,
-            rule_type: Rule::TYPE_REGEX
+            rule_type: Rule::TYPE_REGEX,
+            prompts: rule_set.prompts
           )
-          rule.prompts << rule_set.prompts
           Feedback.create!(rule: rule, text: rule_set.feedback, order: rule_set.priority)
           rule_set.regex_rules.each do |regex_rule|
             regex_rule.update!(rule_id: rule.id)
