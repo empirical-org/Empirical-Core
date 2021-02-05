ba0792fdcf361588603b0640a68803364a58acff
Cissy Yu <cissy@quill.org>
2021-02-05 13:12:15 -0600

    extra file deleted

--- services/QuillLMS/engines/comprehension/test/dummy/db/schema.rb
+++ services/QuillLMS/engines/comprehension/test/dummy/db/schema.rb
@@ -11,8 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20210122165204) do
+ActiveRecord::Schema.define(version: 20210128155938) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -68,26 +67,15 @@ ActiveRecord::Schema.define(version: 20210122165204) do
 
   create_table "comprehension_prompts", force: :cascade do |t|
     t.integer  "activity_id"
-    t.integer  "max_attempts",               limit: 2
-    t.string   "conjunction",                limit: 20
+    t.integer  "max_attempts",          limit: 2
+    t.string   "conjunction",           limit: 20
     t.string   "text"
     t.text     "max_attempts_feedback"
-    t.datetime "created_at",                            null: false
-    t.datetime "updated_at",                            null: false
-    t.text     "plagiarism_text"
-    t.text     "plagiarism_first_feedback"
-    t.text     "plagiarism_second_feedback"
+    t.datetime "created_at",                       null: false
+    t.datetime "updated_at",                       null: false
   end
 
   add_index "comprehension_prompts", ["activity_id"], name: "index_comprehension_prompts_on_activity_id", using: :btree
-
-  create_table "comprehension_prompts_rule_sets", force: :cascade do |t|
-    t.integer "prompt_id"
-    t.integer "rule_set_id"
-  end
-
-  add_index "comprehension_prompts_rule_sets", ["prompt_id"], name: "index_comprehension_prompts_rule_sets_on_prompt_id", using: :btree
-  add_index "comprehension_prompts_rule_sets", ["rule_set_id"], name: "index_comprehension_prompts_rule_sets_on_rule_set_id", using: :btree
 
   create_table "comprehension_prompts_rules", force: :cascade do |t|
     t.integer  "prompt_id",  null: false
@@ -100,27 +88,14 @@ ActiveRecord::Schema.define(version: 20210122165204) do
   add_index "comprehension_prompts_rules", ["rule_id"], name: "index_comprehension_prompts_rules_on_rule_id", using: :btree
 
   create_table "comprehension_regex_rules", force: :cascade do |t|
-    t.integer  "rule_set_id",                null: false
     t.string   "regex_text",     limit: 200, null: false
     t.boolean  "case_sensitive",             null: false
     t.datetime "created_at",                 null: false
     t.datetime "updated_at",                 null: false
+    t.integer  "rule_id"
   end
 
-  add_index "comprehension_regex_rules", ["rule_set_id"], name: "index_comprehension_regex_rules_on_rule_set_id", using: :btree
-
-  create_table "comprehension_rule_sets", force: :cascade do |t|
-    t.integer  "activity_id"
-    t.integer  "prompt_id"
-    t.string   "name",        limit: 100
-    t.text     "feedback"
-    t.integer  "priority",    limit: 2
-    t.datetime "created_at",              null: false
-    t.datetime "updated_at",              null: false
-  end
-
-  add_index "comprehension_rule_sets", ["activity_id"], name: "index_comprehension_rule_sets_on_activity_id", using: :btree
-  add_index "comprehension_rule_sets", ["prompt_id"], name: "index_comprehension_rule_sets_on_prompt_id", using: :btree
+  add_index "comprehension_regex_rules", ["rule_id"], name: "index_comprehension_regex_rules_on_rule_id", using: :btree
 
   create_table "comprehension_rules", force: :cascade do |t|
     t.string   "uid",         null: false
@@ -160,4 +135,5 @@ ActiveRecord::Schema.define(version: 20210122165204) do
 
   add_foreign_key "comprehension_highlights", "comprehension_feedbacks", column: "feedback_id", on_delete: :cascade
   add_foreign_key "comprehension_plagiarism_texts", "comprehension_rules", column: "rule_id", on_delete: :cascade
+  add_foreign_key "comprehension_regex_rules", "comprehension_rules", column: "rule_id", on_delete: :cascade
 end
