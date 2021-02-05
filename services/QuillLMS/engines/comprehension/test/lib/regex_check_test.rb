df5da1e5334a40699464a7803b83413509f9fd8e
Cissy Yu <cissy@quill.org>
2021-02-05 13:04:06 -0600

    Add a priority test

--- services/QuillLMS/engines/comprehension/test/lib/regex_check_test.rb
+++ services/QuillLMS/engines/comprehension/test/lib/regex_check_test.rb
@@ -5,8 +5,8 @@ module Comprehension
   class RegexCheckTest < ActiveSupport::TestCase
     setup do
       @prompt = create(:comprehension_prompt)
-      @rule = create(:comprehension_rule, rule_type: 'Regex', prompts: [@prompt])
-      @regex_rule = create(:comprehension_regex_rule, rule: @rule, regex_text: '^Test') 
+      @rule = create(:comprehension_rule, rule_type: 'Regex', prompts: [@prompt], suborder: 0)
+      @regex_rule = create(:comprehension_regex_rule, rule: @rule, regex_text: '^Test')
       @feedback = create(:comprehension_feedback, rule: @rule, text: 'This string begins with the word Test!')
     end
 
@@ -40,6 +40,15 @@ module Comprehension
         refute feedback[:optimal]
         assert_equal feedback[:entry], entry
         assert_equal feedback[:concept_uid], @rule.concept_uid
+        assert_equal feedback[:rule_uid], @rule.uid
+      end
+
+      should 'return the highest priority feedback when two feedbacks exist' do
+        new_rule = create(:comprehension_rule, rule_type: 'Regex', prompts: [@prompt], suborder: 1)
+        new_regex_rule = create(:comprehension_regex_rule, rule: new_rule, regex_text: '^Testing')
+        entry = "Test this is a good regex match"
+        regex_check = Comprehension::RegexCheck.new(entry, @prompt)
+        feedback = regex_check.feedback_object
         assert_equal feedback[:rule_uid], @rule.uid
       end
     end
