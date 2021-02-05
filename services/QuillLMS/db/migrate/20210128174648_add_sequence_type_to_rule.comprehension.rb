327b30d3f6cf246f0e9c07fc8df53abcf4473b1a
Cissy Yu <cissy@quill.org>
2021-02-05 13:08:09 -0600

    Take out extra db files

--- services/QuillLMS/db/migrate/20210128174648_add_sequence_type_to_rule.comprehension.rb
+++
@@ -1,8 +0,0 @@
-# This migration comes from comprehension (originally 20210128155938)
-class AddSequenceTypeToRule < ActiveRecord::Migration
-  def change
-    add_column :comprehension_rules, :sequence_type, :string
-    add_reference :comprehension_regex_rules, :rule, index: true
-    add_foreign_key :comprehension_regex_rules, :comprehension_rules, column: :rule_id, on_delete: :cascade
-  end
-end
