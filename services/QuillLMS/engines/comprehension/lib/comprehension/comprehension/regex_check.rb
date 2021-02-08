84121eab3d1fb9690dbdb7615ce7382d6989f79d
cissyxyu <57366100+cissyxyu@users.noreply.github.com>
2021-02-08 12:34:09 -0600

    Regex student-facing processing API (#7229)
    * add migration runner class and db migrations
    
    * add new relationships and tests
    
    * add rake task
    
    * add test
    
    * chagne to transaction
    
    * create endpoint and add before_action in controller
    
    * started work on regex method in controller
    
    * add functionality in
    
    * add test files
    
    * add tests
    
    * Add a priority test
    
    * Take out extra db files
    
    * extra file deleted
    
    * linting
    
    * change endpoint
    
    * better naming
    
    * change param

---
+++ services/QuillLMS/engines/comprehension/lib/comprehension/comprehension/regex_check.rb
@@ -0,0 +1,42 @@
+module Comprehension
+  class RegexCheck
+
+    ALL_CORRECT_FEEDBACK = 'All regex checks passed.'
+    attr_reader :entry, :prompt
+
+    def initialize(entry, prompt)
+      @entry = entry
+      @prompt = prompt
+    end
+
+    def feedback_object
+      {
+        feedback: feedback,
+        feedback_type: Rule::TYPE_REGEX,
+        optimal: matched_rule.blank?,
+        response_id: '',
+        entry: @entry,
+        concept_uid: matched_rule&.concept_uid || '',
+        rule_uid: matched_rule&.uid || '',
+        highlight: []
+      }
+    end
+
+    private def feedback
+      return ALL_CORRECT_FEEDBACK unless matched_rule
+      matched_rule&.feedbacks&.first&.text
+    end
+
+    private def matched_rule
+      @matched_rule ||= first_failing_regex_rule
+    end
+
+    private def first_failing_regex_rule
+      rules = @prompt.rules.where(rule_type: Rule::TYPE_REGEX).order(:suborder)
+      rules.each do |rule|
+        return rule unless rule.regex_is_passing?(@entry)
+      end
+      nil
+    end
+  end
+end
