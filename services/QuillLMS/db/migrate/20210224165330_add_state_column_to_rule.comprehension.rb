11ba056c2a425b0fba9366374d5ea4f988cc419f
Thomas Robertson <thomas.robertson@quill.org>
2021-02-24 14:54:35 -0500

    Fix migration to be runnable when not in the present of model updates

--- services/QuillLMS/db/migrate/20210224165330_add_state_column_to_rule.comprehension.rb
+++ services/QuillLMS/db/migrate/20210224165330_add_state_column_to_rule.comprehension.rb
@@ -2,7 +2,7 @@
 class AddStateColumnToRule < ActiveRecord::Migration
   def change
     add_column :comprehension_rules, :state, :string
-    Comprehension::Rule.update_all(state: Comprehension::Rule::STATE_ACTIVE)
+    Comprehension::Rule.update_all(state: 'active')
     change_column :comprehension_rules, :state, :string, null: false
   end
 end
