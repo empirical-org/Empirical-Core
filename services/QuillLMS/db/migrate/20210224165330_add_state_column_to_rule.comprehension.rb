85699af241702ae0dc4d5a5a2aa0cfe926c438a9
Thomas Robertson <thomas.robertson@quill.org>
2021-02-24 11:53:52 -0500

    Install migrations

---
+++ services/QuillLMS/db/migrate/20210224165330_add_state_column_to_rule.comprehension.rb
@@ -0,0 +1,8 @@
+# This migration comes from comprehension (originally 20210218195536)
+class AddStateColumnToRule < ActiveRecord::Migration
+  def change
+    add_column :comprehension_rules, :state, :string
+    Comprehension::Rule.update_all(state: Comprehension::Rule::STATE_ACTIVE)
+    change_column :comprehension_rules, :state, :string, null: false
+  end
+end
