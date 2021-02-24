6a263a3995b453b687e608e21f99707dcb3985fe
Thomas Robertson <thomas.robertson@quill.org>
2021-02-24 11:51:53 -0500

    Tweak Rule state migration to account for existing rules

--- services/QuillLMS/engines/comprehension/db/migrate/20210218195536_add_state_column_to_rule.rb
+++ services/QuillLMS/engines/comprehension/db/migrate/20210218195536_add_state_column_to_rule.rb
@@ -1,5 +1,7 @@
 class AddStateColumnToRule < ActiveRecord::Migration
   def change
-    add_column :comprehension_rules, :state, :string, null: false
+    add_column :comprehension_rules, :state, :string
+    Comprehension::Rule.update_all(state: Comprehension::Rule::STATE_ACTIVE)
+    change_column :comprehension_rules, :state, :string, null: false
   end
 end
