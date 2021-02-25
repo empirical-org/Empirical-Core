4c21f0e898fa36c70a3744b8901ba7b57bfea607
Eric Adams <erictayloradams@gmail.com>
2021-02-25 13:49:45 -0600

    update test

--- services/QuillLMS/client/app/bundles/Staff/components/comprehension/__tests__/activities.test.tsx
+++ services/QuillLMS/client/app/bundles/Staff/components/comprehension/__tests__/activities.test.tsx
@@ -1,18 +1,37 @@
 import * as React from 'react';
-import { mount } from 'enzyme';
-import { MemoryRouter } from 'react-router-dom';
+import { shallow } from 'enzyme';
+import { createMemoryHistory, createLocation } from 'history';
 import 'whatwg-fetch';
 
 import Activities from '../activities';
+import { DataTable } from '../../../../Shared/index';
+
+const mockActivities = [{ id: 1, title: 'First' }, { id: 2, title: 'Second' }]
+jest.mock("react-query", () => ({
+  useQuery: jest.fn(() => ({
+    data: { activities: mockActivities},
+    error: null,
+    status: "success",
+    isFetching: true,
+  })),
+}));
 
 describe('Activities component', () => {
-  const container = mount(
-    <MemoryRouter>
-      <Activities location={{}} match={{ params: { activityId: '17'} }} />
-    </MemoryRouter>
+  const mockProps = {
+    match: {
+      params: {},
+      isExact: true,
+      path: '',
+      url:''
+    },
+    history: createMemoryHistory(),
+    location: createLocation('')
+  }
+  const container = shallow(
+      <Activities {...mockProps} />
   );
 
-  it('should render Activities', () => {
-    expect(container).toMatchSnapshot();
+  it('should render a DataTable passing activites', () => {
+    expect(container.find(DataTable).length).toEqual(1)
   });
 });
