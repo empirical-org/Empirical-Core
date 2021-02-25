bd99cc24074710cee471cc07f2287ef523273a50
cissyxyu <57366100+cissyxyu@users.noreply.github.com>
2021-02-25 13:05:42 -0600

    Catch invalid regex text before it is saved and return error message (#7271)
    * error edit and tests
    
    * add a test for create
    
    * linting

--- services/QuillLMS/engines/comprehension/app/models/comprehension/regex_rule.rb
+++ services/QuillLMS/engines/comprehension/app/models/comprehension/regex_rule.rb
@@ -7,6 +7,7 @@ module Comprehension
     belongs_to :rule, inverse_of: :regex_rules
 
     before_validation :set_default_case_sensitivity, on: :create
+    before_save :validate_regex
 
     validates_presence_of :rule
     validates :regex_text, presence: true, length: {maximum: MAX_REGEX_TEXT_LENGTH}
@@ -23,6 +24,15 @@ module Comprehension
     private def set_default_case_sensitivity
       return if case_sensitive.in? CASE_SENSITIVE_ALLOWED_VALUES
       self.case_sensitive = DEFAULT_CASE_SENSITIVITY
+    end
+
+    private def validate_regex
+      begin
+        Regexp.new(regex_text)
+      rescue RegexpError => e
+        rule.errors.add(:errors, "Invalid regex: #{e}")
+        false
+      end
     end
   end
 end
