ee1fce93380ad1506c518915809babe52b4699ec
Thomas Robertson <thomas.robertson@quill.org>
2021-03-04 16:32:08 -0500

    Add a validation to Label to prevent name collisions within a prompt

--- services/QuillLMS/engines/comprehension/app/models/comprehension/label.rb
+++ services/QuillLMS/engines/comprehension/app/models/comprehension/label.rb
@@ -10,12 +10,21 @@ module Comprehension
     validates :rule, presence: true, uniqueness: true
     validates :name, presence: true, length: {in: NAME_MIN_LENGTH..NAME_MAX_LENGTH}
 
+    validate :name_unique_for_prompt, on: :create
+
     def serializable_hash(options = nil)
       options ||= {}
 
       super(options.reverse_merge(
         only: [:id, :name, :rule_id]
       ))
+    end
+
+    private def name_unique_for_prompt
+      prompt_labels = rule&.prompts&.first&.rules&.map { |r| r.label }
+      if prompt_labels&.map { |l| l&.name }&.include?(name)
+        errors.add(:name, "can't be the same as any other labels related to the same prompt")
+      end
     end
   end
 end
