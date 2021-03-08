24916fb65cbddd4d515c76619be3ca7c94539abd
Thomas Robertson <thomas.robertson@quill.org>
2021-03-08 16:52:54 -0500

    Revert changes to FeedbackHistoryController
    Since we're using a new controller for this

--- services/QuillLMS/app/controllers/api/v1/feedback_histories_controller.rb
+++ services/QuillLMS/app/controllers/api/v1/feedback_histories_controller.rb
@@ -1,36 +1,16 @@
 class Api::V1::FeedbackHistoriesController < Api::ApiController
-  before_action :set_feedback_history, only: [:update, :destroy]
+  before_action :set_feedback_history, only: [:show, :update, :destroy]
 
-  DEFAULT_PAGE_SIZE = 25
-
-  # GET /feedback_histories.json?page=1&activity_id=33
+  # GET /feedback_histories.json
   def index
-    activity_id = params[:activity_id]
-    page = [params[:page].to_i, 1].max
-    records = FeedbackHistory.list_by_activity_session
+    @feedback_histories = FeedbackHistory.all
 
-    count = records.length
-
-    render json: {
-      total_pages: count / DEFAULT_PAGE_SIZE,
-      total_activity_sessions: count,
-      current_page: page,
-      activity_sessions: records.map(&:serialize_list_by_activity_session)
-    }
+    render json: @feedback_histories
   end
 
   # GET /feedback_histories/1.json
   def show
-    activity_session_uid = params[:id]
-
-    puts 'id'
-    puts activity_session_uid
-    puts FeedbackHistory.where(activity_session_uid: activity_session_uid).count
-    render json: FeedbackHistory.serialize_detail_by_activity_session(activity_session_uid)
-  end
-
-  def show_by_activity_session
-
+    render json: @feedback_history
   end
 
   # POST /feedback_histories.json
@@ -93,19 +73,25 @@ class Api::V1::FeedbackHistoriesController < Api::ApiController
   end
 
   private def batch_feedback_history_params
-    params.permit(feedback_histories: [
-      :activity_session_uid,
-      :prompt_id,
-      :concept_uid,
-      :attempt,
-      :entry,
-      :feedback_text,
-      :feedback_type,
-      :optimal,
-      :used,
-      :time,
-      :metadata,
-      :rule_uid
-    ])[:feedback_histories]
+    # Note: nested params MUST be permitted last in any list
+    params.permit(
+      feedback_histories: [
+        :activity_session_uid,
+        :prompt_id,
+        :concept_uid,
+        :attempt,
+        :entry,
+        :feedback_text,
+        :feedback_type,
+        :optimal,
+        :used,
+        :time,
+        :rule_uid,
+        metadata: [
+          :response_id,
+          highlight: []
+        ]
+      ]
+    )[:feedback_histories]
   end
 end
