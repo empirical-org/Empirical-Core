48f3003f3c912eaf5a18c1aa407831d5a6149363
Cissy Yu <cissy@quill.org>
2021-03-09 15:51:55 -0600

    pusher logging

--- services/QuillLMS/client/app/bundles/admin_dashboard/containers/AdminDashboard.jsx
+++ services/QuillLMS/client/app/bundles/admin_dashboard/containers/AdminDashboard.jsx
@@ -49,9 +49,9 @@ export default class AdminDashboard extends React.Component {
   };
 
   initializePusher = () => {
-    if (process.env.RAILS_ENV === 'development') {
-      Pusher.logToConsole = true;
-    }
+
+    Pusher.logToConsole = true;
+
     const adminId = String(this.state.model.id)
     const pusher = new Pusher(process.env.PUSHER_KEY, { encrypted: true, });
     const channel = pusher.subscribe(adminId);
