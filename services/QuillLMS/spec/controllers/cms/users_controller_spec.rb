d593eadd7b584bae522dbe7091333e764f1f919d
Anathomical <thomas.robertson@quill.org>
2021-03-10 10:59:19 -0500

    comprehension/fix-turking-session-bug (#7349)
    * Try turning off CSRF validation to fix bug
    
    * Tweak for linting

--- services/QuillLMS/spec/controllers/cms/users_controller_spec.rb
+++ services/QuillLMS/spec/controllers/cms/users_controller_spec.rb
@@ -54,16 +54,16 @@ describe Cms::UsersController do
           "school"=> nil,
           "school_id"=> nil,
           "id"=> teacher.id.to_s
-        },
+         },
          {
-           "name" => student.name,
-           "email" => student.email,
-           "role" => student.role,
-           "subscription" => nil,
-           "last_sign_in" => nil,
-           "school" => nil,
-           "school_id" => nil,
-           "id" => student.id.to_s
+          "name" => student.name,
+          "email" => student.email,
+          "role" => student.role,
+          "subscription" => nil,
+          "last_sign_in" => nil,
+          "school" => nil,
+          "school_id" => nil,
+          "id" => student.id.to_s
          }], "userSearchQuery"=> {"class_code"=> class_code}})
       expect(ChangeLog.last.action).to eq(ChangeLog::USER_ACTIONS[:search])
       expect(ChangeLog.last.explanation).to include('class_code')
