26d35a0cefe9317a6aff65357c5ad934d4b5a0ce
Cissy Yu <cissy@quill.org>
2021-03-11 14:20:56 -0600

    take out sleep and change pusher

--- services/QuillLMS/app/pusher_modules/pusher_admin_users_completed.rb
+++ services/QuillLMS/app/pusher_modules/pusher_admin_users_completed.rb
@@ -7,9 +7,6 @@ module PusherAdminUsersCompleted
         secret: ENV["PUSHER_SECRET"],
         encrypted: true
     )
-    # delay 2 seconds to prevent a race condition where
-    # the front end hasn't set up Pusher receiver yet
-    sleep(2)
     pusher_client.trigger(
       admin_id.to_s,
      "admin-users-found",
--- services/QuillLMS/client/app/bundles/admin_dashboard/containers/AdminDashboardRouter.jsx
+++ services/QuillLMS/client/app/bundles/admin_dashboard/containers/AdminDashboardRouter.jsx
@@ -2,10 +2,14 @@ import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import AdminDashboardContainer from './AdminDashboardContainer';
 
-const AdminDashboardRouter = props => (
-  <BrowserRouter>
-    <Route component={AdminDashboardContainer} path="/teachers/admin_dashboard" />
-  </BrowserRouter>
-  );
-
-export default AdminDashboardRouter;
+export default function AdminDashboardRouter(props) {
+  return (
+    <BrowserRouter>
+    <Route path="/teachers/admin_dashboard"
+          render={passedProps => (
+            <AdminDashboardContainer {...props} {...passedProps}/>
+          )}
+        />
+    </BrowserRouter>
+  )
+}
