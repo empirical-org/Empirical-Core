46c593dcaa0d88f89a68a8531d1ff10c4b42fac0
Eric Adams <erictayloradams@gmail.com>
2021-03-15 14:12:26 -0300

    remove csrf check

--- services/QuillLMS/engines/comprehension/app/controllers/comprehension/automl_models_controller.rb
+++ services/QuillLMS/engines/comprehension/app/controllers/comprehension/automl_models_controller.rb
@@ -1,5 +1,6 @@
 module Comprehension
   class AutomlModelsController < ApplicationController
+    skip_before_action :verify_authenticity_token
     before_action :set_automl_model, only: [:show, :update, :activate, :destroy]
 
     # GET /automl_models.json
