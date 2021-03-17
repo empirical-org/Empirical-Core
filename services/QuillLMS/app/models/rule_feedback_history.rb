fedda919916de9dc6ef5e5fc9e94d7789db91839
Peter Kong <pkong@quill.org>
2021-03-16 21:27:57 -0600

    basic query working

--- services/QuillLMS/app/models/rule_feedback_history.rb
+++ services/QuillLMS/app/models/rule_feedback_history.rb
@@ -1,30 +1,47 @@
 class RuleFeedbackHistory
     def self.generate_report(conj)
 
-    
-        # prompts = Comprehension::Prompt.where(conjunction: 'so')
-        rules = Comprehension::Rule.find_by_sql(
-            "select * from comprehension_rules as rules \
-            INNER JOIN comprehension_prompts_rules as prompts_rules ON rules.id = prompts_rules.rule_id \
-            INNER JOIN comprehension_prompts as prompts ON prompts_rules.prompt_id = prompts.id \ 
-            WHERE conjunction = '#{conj}'
-        ")        
+        sql_result = self.exec_query(conj)
+        prepare_sql_results(sql_result)
+    end
 
-        prepare_sql_results(rules)
+    def self.exec_query(conj)
+        # prompts = Comprehension::Prompt.where(conjunction: 'so')
+        sql = <<~SQL
+            select rules.id,rules.uid,rules.rule_type,rules.suborder, rules.name from comprehension_rules as rules 
+            INNER JOIN comprehension_prompts_rules as prompts_rules ON rules.id = prompts_rules.rule_id 
+            INNER JOIN comprehension_prompts as prompts ON prompts_rules.prompt_id = prompts.id 
+            INNER JOIN feedback_histories as feedbacks ON feedbacks.rule_uid = rules.uid
+            WHERE prompts.conjunction = '#{conj}'
+        SQL
+        rules = Comprehension::Rule.find_by_sql(sql)
     end
 
     def self.prepare_sql_results(relations)
-        # api_order: 1, -> suborder
-        # api_name: element of Enum<API_NAMES>, -> rule.rule_type
-        # rule_order: 1,
+        relations.map do |r|
+            {
+                api_order: r.suborder,
+                api_name: r.rule_type,
+                rule_order: 1, # TODO: update later,
+                feedback_first_layer: 'To Be Implemented', # TODO: to be implemented
+                rule_description: r.name,
+                pct_strong: 0, # TODO: to be implemented
+                scored_responses: 0,
+                pct_scored: 0
+        # api_order: 1, -> suborder !!
+        # api_name: element of Enum<API_NAMES>, -> rule.rule_type !!
+        # rule_order: 1, !!
         # feedback_first_layer, -> rule.feedbacks.first
-        # rule_description: "A lovely rule" -> rule.description
-        # pct_strong: 0.85
+        # rule_description: "A lovely rule" -> rule.name !!
+        # pct_strong: 0.85 !!
         # total_responses: 100, -> calculable
         # scored_responses: 100, -> hardcode to 0
         # pct_scored: 1.00 -> hardcode to 0
+            }
 
-        relations
+        end
+
+
     end
 
 end
--- services/QuillLMS/spec/models/rule_feedback_history_spec.rb
+++ services/QuillLMS/spec/models/rule_feedback_history_spec.rb
@@ -15,60 +15,62 @@ RSpec.describe RuleFeedbackHistory, type: :model do
     )
   end 
 
-  it 'should foo' do 
-    activity1 = Comprehension::Activity.create!(title: 'Title 1', parent_activity_id: 1, target_level: 1)
+  describe '#generate_report' do 
+    it 'should foo' do 
+      activity1 = Comprehension::Activity.create!(title: 'Title 1', parent_activity_id: 1, target_level: 1)
 
-    # prompts
-    so_prompt_1 = Comprehension::Prompt.create!(activity: activity1, conjunction: 'so', text: 'Some feedback text', max_attempts_feedback: 'Feedback')
-    because_prompt_1 = Comprehension::Prompt.create!(activity: activity1, conjunction: 'because', text: 'Some feedback text', max_attempts_feedback: 'Feedback')
-  
-    # rules
-    so_rule_1 = rule_factory { { name: 'so_rule_1', rule_type: 'autoML'} } 
-    because_rule_1 = rule_factory { {name: 'because_rule_1'} } 
+      # prompts
+      so_prompt_1 = Comprehension::Prompt.create!(activity: activity1, conjunction: 'so', text: 'Some feedback text', max_attempts_feedback: 'Feedback')
+      because_prompt_1 = Comprehension::Prompt.create!(activity: activity1, conjunction: 'because', text: 'Some feedback text', max_attempts_feedback: 'Feedback')
+    
+      # rules
+      so_rule_1 = rule_factory { { name: 'so_rule_1', rule_type: 'autoML'} } 
+      because_rule_1 = rule_factory { {name: 'because_rule_1'} } 
 
-    # prompts rules
-    Comprehension::PromptsRule.create!(prompt: so_prompt_1, rule: so_rule_1)
-    Comprehension::PromptsRule.create!(prompt: because_prompt_1, rule: because_rule_1)
+      # feedbacks
+      create(:feedback_history, rule_uid: so_rule_1.uid)
 
-    result = RuleFeedbackHistory.generate_report('so')
+      # prompts rules
+      Comprehension::PromptsRule.create!(prompt: so_prompt_1, rule: so_rule_1)
+      Comprehension::PromptsRule.create!(prompt: because_prompt_1, rule: because_rule_1)
 
-    binding.pry
+      result = RuleFeedbackHistory.generate_report('so')
 
+      expect(result.count).to eq 1
+      expect(result.first[:rule_description]).to eq 'so_rule_1'
+ 
 
+      # prompt = params[:prompt]
+      # rules = Rule.where(prompt: prompt)
+      # rules.join(feedback_histories)
 
-    # create(:user) do |user|
-    #     user.posts.create(attributes_for(:post))
-    #   end
+      # CREATE TABLE public.comprehension_feedbacks (
+      #     id integer NOT NULL,
+      #     rule_id integer NOT NULL,
+      #     text character varying NOT NULL,
+      #     description character varying,
+      #     "order" integer NOT NULL,
+      #     created_at timestamp without time zone NOT NULL,
+      #     updated_at timestamp without time zone NOT NULL
+      # );
 
-
-    # prompt = params[:prompt]
-    # rules = Rule.where(prompt: prompt)
-    # rules.join(feedback_histories)
-
-    # CREATE TABLE public.comprehension_feedbacks (
-    #     id integer NOT NULL,
-    #     rule_id integer NOT NULL,
-    #     text character varying NOT NULL,
-    #     description character varying,
-    #     "order" integer NOT NULL,
-    #     created_at timestamp without time zone NOT NULL,
-    #     updated_at timestamp without time zone NOT NULL
-    # );
-
-#     CREATE TABLE public.comprehension_rules (
-#     id integer NOT NULL,
-#     uid character varying NOT NULL,
-#     name character varying NOT NULL,
-#     description character varying,
-#     universal boolean NOT NULL,
-#     rule_type character varying NOT NULL,
-#     optimal boolean NOT NULL,
-#     suborder integer,
-#     concept_uid character varying NOT NULL,
-#     created_at timestamp without time zone NOT NULL,
-#     updated_at timestamp without time zone NOT NULL
-# );
+  #     CREATE TABLE public.comprehension_rules (
+  #     id integer NOT NULL,
+  #     uid character varying NOT NULL,
+  #     name character varying NOT NULL,
+  #     description character varying,
+  #     universal boolean NOT NULL,
+  #     rule_type character varying NOT NULL,
+  #     optimal boolean NOT NULL,
+  #     suborder integer,
+  #     concept_uid character varying NOT NULL,
+  #     created_at timestamp without time zone NOT NULL,
+  #     updated_at timestamp without time zone NOT NULL
+  # );
 
 
+    end
   end
+
+
 end
