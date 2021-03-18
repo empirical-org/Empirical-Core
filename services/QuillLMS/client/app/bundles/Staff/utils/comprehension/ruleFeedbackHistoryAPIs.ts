ceaf68416d93f4953a31ffe868df579ad08c2221
Emilia Friedberg <emiliafriedberg@gmail.com>
2021-03-18 12:59:39 -0400

    update the rule feedback history apis to use main api

--- services/QuillLMS/client/app/bundles/Staff/utils/comprehension/ruleFeedbackHistoryAPIs.ts
+++ services/QuillLMS/client/app/bundles/Staff/utils/comprehension/ruleFeedbackHistoryAPIs.ts
@@ -1,9 +1,9 @@
-import { handleApiError, apiFetch } from '../../helpers/comprehension';
+import { handleApiError, mainApiFetch } from '../../helpers/comprehension';
 
 export const fetchRuleFeedbackHistories = async (key: string, activityId: string, selectedPromptId: string) => {
   if (!selectedPromptId) { return }
 
-  const response = await apiFetch(`rule_feedback_histories/${activityId}?conjunction=${selectedPromptId}`);
+  const response = await mainApiFetch(`rule_feedback_histories/${activityId}?conjunction=${selectedPromptId}`);
   const ruleFeedbackHistories = await response.json();
   return {
     error: handleApiError('Failed to fetch rule feedback histories, please refresh the page.', response),
