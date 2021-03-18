263460a3f339b1ff0499bb404f48d5133e5acbde
Emilia Friedberg <emiliafriedberg@gmail.com>
2021-03-18 15:28:25 -0400

    fix request handling

--- services/QuillLMS/client/app/bundles/Staff/utils/comprehension/ruleFeedbackHistoryAPIs.ts
+++ services/QuillLMS/client/app/bundles/Staff/utils/comprehension/ruleFeedbackHistoryAPIs.ts
@@ -7,6 +7,6 @@ export const fetchRuleFeedbackHistories = async (key: string, activityId: string
   const ruleFeedbackHistories = await response.json();
   return {
     error: handleApiError('Failed to fetch rule feedback histories, please refresh the page.', response),
-    ruleFeedbackHistories
+    ruleFeedbackHistories: ruleFeedbackHistories.rule_feedback_histories
   };
 }
