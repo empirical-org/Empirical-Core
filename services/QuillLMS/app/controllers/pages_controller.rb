cd46cb1f22dc4369401b36489beb26f88c28b3df
Emilia Friedberg <emiliafriedberg@gmail.com>
2021-03-24 15:32:52 -0400

    revisions

--- services/QuillLMS/app/controllers/pages_controller.rb
+++ services/QuillLMS/app/controllers/pages_controller.rb
@@ -478,6 +478,10 @@ class PagesController < ApplicationController
     @style_file = ApplicationController::DIAGNOSTIC
   end
 
+  def administrator
+    @title = 'Administrator'
+  end
+
   private
 
   def determine_layout
