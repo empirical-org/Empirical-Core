419919c164a9079ecc582aa4afd8ca4041dd6811
Emilia Friedberg <emiliafriedberg@gmail.com>
2021-04-05 09:04:44 -0400

    implement activity feed controller function (#7399)
    * implement activity feed
    
    * fix linter errors
    
    * test update
    
    * update unit assignment variables file
    
    * log some more stuff
    
    * remove exclamation
    
    * stop using the shared file
    
    * fix dumb bug, I think
    
    * update test
    
    * implement some of Dan's requested changes

--- services/QuillLMS/config/routes.rb
+++ services/QuillLMS/config/routes.rb
@@ -216,6 +216,7 @@ EmpiricalGrammar::Application.routes.draw do
       end
     end
 
+    get 'activity_feed', to: 'classroom_manager#activity_feed'
     get 'unset_preview_as_student', to: 'classroom_manager#unset_preview_as_student'
     get 'preview_as_student/:student_id', to: 'classroom_manager#preview_as_student'
     get 'getting_started' => 'classroom_manager#getting_started'
