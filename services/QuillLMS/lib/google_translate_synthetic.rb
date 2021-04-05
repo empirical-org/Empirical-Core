a6c98977ce91c207205c728b76d8c6640cb04bc0
Daniel Drabik <dan@quill.org>
2021-04-05 18:20:42 -0400

    Working version of training export.

--- services/QuillLMS/lib/google_translate_synthetic.rb
+++ services/QuillLMS/lib/google_translate_synthetic.rb
@@ -6,6 +6,16 @@ require "google/cloud/translate"
 class GoogleTranslateSynthetic
 
   SyntheticResult = Struct.new(:original, :label, :translations, keyword_init: true)
+  TrainRow = Struct.new(:text, :label, :synthetic, :type, keyword_init: true) do
+    def to_a
+      [type, text, label]
+    end
+  end
+  TYPE_TRAIN = 'TRAIN'
+  TYPE_VALIDATION = 'VALIDATION'
+  TYPE_TEST = 'TEST'
+  TEST_PERCENTAGE = 0.10
+
 
   # NB, there is a V3, but that throws errors with our current Google Integration
   TRANSLATOR = Google::Cloud::Translate.new(version: :v2)
@@ -22,6 +32,8 @@ class GoogleTranslateSynthetic
     he:  'hebrew',
     ko:  'korean',
   }
+  TRAIN_LANGUAGES = DEFAULT_LANGUAGES.slice(:ko, :he, :ar, :zh)
+
   ENGLISH = :en
   # the API with throw an error if you send too many text strings at a time.
   # Throttling to 100 sentences at a time.
@@ -76,5 +88,50 @@ class GoogleTranslateSynthetic
         end
       end
     end
+  end
+
+  def self.generate_training_export(input_file_path, output_file_path, languages: TRAIN_LANGUAGES.keys)
+    raise if languages.size > 4
+
+    texts_and_labels = CSV.open(input_file_path).to_a
+
+    synthetics = GoogleTranslateSynthetic.new(texts_and_labels, languages: languages)
+
+    synthetics.fetch_results
+
+    data = []
+    synthetics.results.each do |result|
+      data << TrainRow.new(text: result.original, label: result.label, synthetic: false, type: nil)
+      result.translations.each do |_, new_text|
+        data << TrainRow.new(text: new_text, label: result.label, synthetic: true, type: TYPE_TRAIN)
+      end
+    end
+
+    # remove duplicates
+    uniq_data = data.uniq
+    # determine how many test items
+    test_count = (uniq_data.size * TEST_PERCENTAGE).ceil
+    # determine original counts
+    original_count = uniq_data.count {|data| data.type.nil? }
+    #assign test and validations
+    original_types = type_list(count: original_count, test_count: test_count).shuffle
+
+    uniq_data.each do |train_row|
+      next unless train_row.type.nil?
+
+      train_row.type = original_types.shift
+    end
+
+    CSV.open(output_file_path, "w") do |csv|
+      uniq_data.each {|row| csv << row.to_a }
+    end
+  end
+
+  private_class_method def self.type_list(count: , test_count:)
+    [
+      Array.new(test_count, TYPE_TEST),
+      Array.new(test_count, TYPE_VALIDATION),
+      Array.new(count - (test_count * 2), TYPE_TRAIN)
+    ].flatten
   end
 end
