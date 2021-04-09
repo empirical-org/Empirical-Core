5475bf549f3f3c2dbb265f9dab1857a73090f75c
Daniel Drabik <dan@quill.org>
2021-04-09 17:28:44 -0400

    Small optimization to pull less data.

--- services/QuillCMS/app/workers/rematch_responses_for_question_worker.rb
+++ services/QuillCMS/app/workers/rematch_responses_for_question_worker.rb
@@ -6,23 +6,25 @@ class RematchResponsesForQuestionWorker
   sidekiq_options retry: 3
 
   def perform(question_uid, question_type)
-    responses_to_reprocess = get_ungraded_responses(question_uid) + get_machine_graded_responses(question_uid)
+    response_ids_to_reprocess = get_ungraded_responses(question_uid) + get_machine_graded_responses(question_uid)
     reference_response_ids = get_human_graded_response_ids(question_uid)
-    responses_to_reprocess.each do |response|
-      RematchResponseWorker.perform_async(response.id, question_type, question_uid, reference_response_ids)
+    response_ids_to_reprocess.each do |response_id|
+      RematchResponseWorker.perform_async(response_id, question_type, question_uid, reference_response_ids)
     end
   end
 
-  def get_ungraded_responses(question_uid)
+  def get_ungraded_response_ids(question_uid)
     Response.where(question_uid: question_uid)
             .where(parent_id: nil)
             .where(parent_uid: nil)
             .where(optimal: nil)
+            .pluck(:id)
   end
 
-  def get_machine_graded_responses(question_uid)
+  def get_machine_graded_responses_ids(question_uid)
     Response.where(question_uid: question_uid)
             .where("responses.parent_id IS NOT NULL OR responses.parent_uid IS NOT NULL")
+            .pluck(:id)
   end
 
   def get_human_graded_response_ids(question_uid)
