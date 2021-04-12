0503b6020299f51dbad6d50594f18cd0fed6ca85
happythenewsad <happythenewsad@users.noreply.github.com>
2021-04-12 11:12:54 -0400

    Feature/first feedback (#7454)
    * hydrating first feedbacks
    
    * done
    
    * formatting
    
    * linting
    
    * fixing spec
    
    Co-authored-by: Peter Kong <pkong@quill.org>

--- services/QuillLMS/spec/lib/rule_feedback_history_spec.rb
+++ services/QuillLMS/spec/lib/rule_feedback_history_spec.rb
@@ -42,7 +42,7 @@ RSpec.describe RuleFeedbackHistory, type: :model do
       expected = {
         api_name: 'autoML',
         rule_order: "1", 
-        feedback_first_layer: 'To Be Implemented', 
+        first_feedback: nil, 
         rule_name: 'so_rule1',
         pct_strong: 0, 
         scored_responses: 0,
@@ -51,6 +51,39 @@ RSpec.describe RuleFeedbackHistory, type: :model do
 
       expect(expected <= formatted.first).to be true
 
+    end
+  end
+
+  describe '#postprocessing' do 
+    it 'should include feedback_histories' do 
+      # activities
+      activity1 = Comprehension::Activity.create!(title: 'Title 1', parent_activity_id: 1, target_level: 1, name: 'an_activity_1')
+
+      # prompts
+      so_prompt1 = Comprehension::Prompt.create!(activity: activity1, conjunction: 'so', text: 'Some feedback text', max_attempts_feedback: 'Feedback')
+      because_prompt1 = Comprehension::Prompt.create!(activity: activity1, conjunction: 'because', text: 'Some feedback text', max_attempts_feedback: 'Feedback')
+    
+      # rules
+      so_rule1 = rule_factory { { name: 'so_rule1', rule_type: 'autoML'} } 
+      because_rule1 = rule_factory { {name: 'because_rule1'} } 
+
+      #feedbacks
+      f3 = Comprehension::Feedback.create!(rule_id: so_rule1.id, order: 3, text: 'lorem ipsum dolor 3')
+      f1 = Comprehension::Feedback.create!(rule_id: so_rule1.id, order: 1, text: 'lorem ipsum dolor 1')
+      f2 = Comprehension::Feedback.create!(rule_id: so_rule1.id, order: 2, text: 'lorem ipsum dolor 2')
+
+      # feedback_histories
+      create(:feedback_history, rule_uid: so_rule1.uid)
+      create(:feedback_history, rule_uid: so_rule1.uid)
+
+      # prompts rules
+      Comprehension::PromptsRule.create!(prompt: so_prompt1, rule: so_rule1)
+      Comprehension::PromptsRule.create!(prompt: because_prompt1, rule: because_rule1)
+
+      sql_result = RuleFeedbackHistory.exec_query(conjunction: 'so', activity_id: activity1.id)
+      post_result = RuleFeedbackHistory.postprocessing(sql_result)
+
+      expect(post_result.first.first_feedback).to eq f1.text
     end
   end
 
@@ -84,6 +117,8 @@ RSpec.describe RuleFeedbackHistory, type: :model do
 
     xit 'should only return results related to the specified activity' do 
     end
+
+
   end
 
 
