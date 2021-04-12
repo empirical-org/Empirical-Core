f0b137f77bcbc5edcdfe60e2b9fa4e775aaea677
Peter Kong <pkong@quill.org>
2021-04-12 16:57:50 -0600

    lint

--- services/QuillLMS/lib/rule_feedback_history.rb
+++ services/QuillLMS/lib/rule_feedback_history.rb
@@ -26,12 +26,12 @@ class RuleFeedbackHistory
         rules = Comprehension::Rule.find_by_sql(sql)
     end
 
-    def self.feedback_history_to_json(fh)
+    def self.feedback_history_to_json(f_h)
         {
-            response_id: fh.id,
-            datetime: fh.updated_at,
-            entry: fh.entry,
-            highlight: fh.metadata.class == Hash ? fh.metadata['highlight'] : '',
+            response_id: f_h.id,
+            datetime: f_h.updated_at,
+            entry: f_h.entry,
+            highlight: f_h.metadata.class == Hash ? f_h.metadata['highlight'] : '',
             view_session_url: 'Not yet available'
         }
     end
@@ -39,8 +39,8 @@ class RuleFeedbackHistory
     def self.generate_rulewise_report(rule_uid)
         feedback_histories = FeedbackHistory.where(rule_uid: rule_uid)
         response_jsons = []
-        feedback_histories.each do |fh|
-            response_jsons.append(feedback_history_to_json(fh))
+        feedback_histories.each do |f_h|
+            response_jsons.append(feedback_history_to_json(f_h))
         end
 
         {
