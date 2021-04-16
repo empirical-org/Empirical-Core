5a843997c57f4389ab386d7dbe7d0e29a26456b4
Daniel Drabik <dan@quill.org>
2021-04-16 11:22:06 -0400

    Fix up tests and remove a lot of stubbing.

--- services/QuillCMS/app/workers/rematch_responses_for_question_worker.rb
+++ services/QuillCMS/app/workers/rematch_responses_for_question_worker.rb
@@ -47,6 +47,7 @@ class RematchResponsesForQuestionWorker
             .where.not(optimal: nil)
             .where(parent_id: nil)
             .pluck(:id)
+            .sort() # sorting to make tests idempotent
   end
 
   def retrieve_question(question_uid)
