10af91b18c8a8e033e806740c83dc2bbafeb5cc3
Cissy Yu <cissy@quill.org>
2021-04-19 17:52:29 -0400

    add all changes for prompt dashboard

---
+++ services/QuillLMS/client/app/bundles/Connect/components/shared/loading_indicator.jsx
@@ -0,0 +1,12 @@
+"use strict";
+import React from 'react'
+
+export default class LoadingIndicator extends React.Component {
+    render() {
+		return (
+  <div className="spinner-container">
+    <img alt="loading spinner" className='spinner' src='/images/loader_still.svg' />
+  </div>
+		);
+	}
+}
