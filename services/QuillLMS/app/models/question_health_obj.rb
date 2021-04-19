342a2942db4c0e5d7b9349f741c56d4afdd97fff
Cissy Yu <cissy@quill.org>
2021-04-19 18:58:50 -0400

    add mapping from question type to url

--- services/QuillLMS/app/models/question_health_obj.rb
+++ services/QuillLMS/app/models/question_health_obj.rb
@@ -1,4 +1,10 @@
 class QuestionHealthObj
+  QUESTION_TYPE_TO_URL = {
+    :connect_sentence_combining => 'questions',
+    :connect_sentencec_fragments => 'sentence-fragments',
+    :connect_fill_in_blanks => 'fill-in-the-blanks',
+    :grammar => 'questions'
+  }
 
   def initialize(activity, question, question_number, tool)
     @activity = activity
@@ -11,15 +17,19 @@ class QuestionHealthObj
     health_dashboard = QuestionHealthDashboard.new(@activity.id, @question_number, @question.uid)
     data = @question.data
     {
-      url: ENV['DEFAULT_URL'].to_s + "/" + @tool + "/#/admin/questions/" + @question.uid + "/responses",
+      url: ENV['DEFAULT_URL'].to_s + "/" + @tool + "/#/admin/" + question_url + "/" + @question.uid + "/responses",
       text: data["prompt"],
       flag: data["flag"],
-      number_of_incorrect_sequences: data["incorrectSequences"].length,
-      number_of_focus_points: data["focusPoints"].length,
+      number_of_incorrect_sequences: data["incorrectSequences"]&.length,
+      number_of_focus_points: data["focusPoints"]&.length,
       percent_common_unmatched: health_dashboard.cms_dashboard_stats["percent_common_unmatched"],
       percent_specified_algorithms: health_dashboard.cms_dashboard_stats["percent_specified_algos"],
       difficulty: health_dashboard.average_attempts_for_question,
       percent_reached_optimal: health_dashboard.percent_reached_optimal_for_question
     }
+  end
+
+  private def question_url
+    QUESTION_TYPE_TO_URL[@question.question_type.to_sym]
   end
 end
