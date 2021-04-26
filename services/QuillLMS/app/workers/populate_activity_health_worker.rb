34204a0252e827ec0259ddaee393f27c3f8b584a
Cissy Yu <cissy@quill.org>
2021-04-26 17:52:38 -0400

    some tweaks after testing the workers

--- services/QuillLMS/app/workers/populate_activity_health_worker.rb
+++ services/QuillLMS/app/workers/populate_activity_health_worker.rb
@@ -7,7 +7,7 @@ class PopulateActivityHealthWorker
     serializer = SerializeActivityHealth.new(@activity)
     activity_health = ActivityHealth.create!(serializer.data)
 
-    serializer.prompt_data.each do |prompt_data|
+    serializer.prompt_data&.each do |prompt_data|
       PromptHealth.create!(prompt_data.merge({activity_health: activity_health}))
     end
   end
--- services/QuillLMS/app/workers/populate_all_activity_healths_worker.rb
+++ services/QuillLMS/app/workers/populate_all_activity_healths_worker.rb
@@ -1,9 +1,12 @@
 class PopulateAllActivityHealthsWorker
   include Sidekiq::Worker
+  RELEVENT_ACTIVITY_CLASSIFICATION_IDS = [2, 5]
+  RELEVENT_FLAGS = ["production"]
 
   def perform
-    Activity.where(flags: ["production"]).each do |act|
-      PopulateActivityHealthWorker.perform(act.id)
+    ActivityHealth.destroy_all!
+    Activity.where(flags: RELEVENT_FLAGS, activity_classification_id: RELEVENT_ACTIVITY_CLASSIFICATION_IDS).each do |act|
+      PopulateActivityHealthWorker.perform_async(act.id)
     end
   end
 end
