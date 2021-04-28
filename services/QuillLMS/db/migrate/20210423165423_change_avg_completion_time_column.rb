0ed380a89b3970ffd5b0237f97063e9061ce0f97
Cissy Yu <cissy@quill.org>
2021-04-28 15:48:55 -0400

    change migration file to use rename column

--- services/QuillLMS/db/migrate/20210423165423_change_avg_completion_time_column.rb
+++ services/QuillLMS/db/migrate/20210423165423_change_avg_completion_time_column.rb
@@ -1,9 +1,8 @@
 class ChangeAvgCompletionTimeColumn < ActiveRecord::Migration
   def change
     remove_column :activity_healths, :avg_completion_time, :time
-    remove_column :activity_healths, :recent_assignments, :integer
     add_column :activity_healths, :avg_mins_to_complete, :float
     add_column :activity_healths, :flag, :string
-    add_column :activity_healths, :recent_plays, :integer
+    rename_column :activity_healths, :recent_assignments, :recent_plays
   end
 end
