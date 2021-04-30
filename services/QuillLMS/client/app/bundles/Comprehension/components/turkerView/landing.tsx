48cd44763b88fc5c927866f36385e9d7483f4e1a
cissyxyu <57366100+cissyxyu@users.noreply.github.com>
2021-04-30 15:48:33 -0400

    Create a weekly job that populates all Activity and Prompt Health data (#7545)
    * modify all tests
    
    * change correct to score
    
    * linting
    
    * change model name
    
    * resolve pk comments
    
    * all commits squashed from other branch
    
    * change migration file to use rename column
    
    * merge from change query branch
    
    * remove extra files
    
    * pk suggestions
    
    * add alphabetical sorting and turn activity pack to json data type
    
    * brendan suggestions

--- services/QuillLMS/client/app/bundles/Comprehension/components/turkerView/landing.tsx
+++ services/QuillLMS/client/app/bundles/Comprehension/components/turkerView/landing.tsx
@@ -15,12 +15,12 @@ export const TurkLanding = ({ handleStartActivity}) => {
   return(
     <section className="turk-landing-container">
       <section className="turk-landing-header-container">
-        <h1>Quill Activity - Beta Testing of New Content</h1>
+        <h1>Amazon Mechanical Turk Task</h1>
         <p>You are completing a reading and writing activity. Your responses will be used to develop our system so that millions of students around the world can improve their reading and writing skills.</p>
       </section>
       <section className="turk-landing-directions-container">
         <h1>Directions:</h1>
-        <div className="bolded">You should:</div>
+        <div className="bolded">In order to accurately complete the HIT for payment, you <p>must</p> do the following:</div>
         <ul>
           {ListElement('Read the passage')}
           {ListElement('Complete all three sentences')}
@@ -33,6 +33,7 @@ export const TurkLanding = ({ handleStartActivity}) => {
           {ListElement('Use proper capitalization (i.e. do not write in capital letters)')}
           {ListElement('Be based on the text, but written in your own words')}
         </ul>
+        <p className="bolded">By following these directions, you will be provided with a code to input in order to be paid for the HIT.</p>
       </section>
       <section className="button-container">
         <button
