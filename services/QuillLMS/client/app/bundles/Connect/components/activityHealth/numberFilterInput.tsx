abf7848a19a2911880639e21c5bb3b5ac4e2b8e8
Cissy Yu <cissy@quill.org>
2021-05-10 13:44:55 -0400

    linting

--- services/QuillLMS/client/app/bundles/Connect/components/activityHealth/numberFilterInput.tsx
+++ services/QuillLMS/client/app/bundles/Connect/components/activityHealth/numberFilterInput.tsx
@@ -10,15 +10,15 @@ interface NumberFilterInputProps {
 
 export const NumberFilterInput: React<NumberFilterInputProps> = ({ handleChange, label, filter }) => {
   return (
-  <div style={{ display: 'flex' }}>
-    <input
-      aria-label={label}
-      defaultValue={filter ? filter.value : ''}
-      onChange={e => handleChange(e.target.value)}
-      placeholder={`0-5, >1, <1`}
-      style={{width: '100px', marginRight: '0.5rem'}}
-      type="text"
-    />
-  </div>
+    <div style={{ display: 'flex' }}>
+      <input
+        aria-label={label}
+        defaultValue={filter ? filter.value : ''}
+        onChange={e => handleChange(e.target.value)}
+        placeholder={`0-5, >1, <1`}
+        style={{width: '100px', marginRight: '0.5rem'}}
+        type="text"
+      />
+    </div>
   );
 }
