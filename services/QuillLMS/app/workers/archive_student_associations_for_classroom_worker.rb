0306c25d1b65bfb896bccf457f16adf957c7fe1b
brendanshean <brendan@quill.org>
2021-05-11 09:29:59 -0400

    Fix contains query with ArchiveStudentAssociationsForClassroomWorker.perform (#7615)

--- services/QuillLMS/app/workers/archive_student_associations_for_classroom_worker.rb
+++ services/QuillLMS/app/workers/archive_student_associations_for_classroom_worker.rb
@@ -1,10 +1,10 @@
 class ArchiveStudentAssociationsForClassroomWorker
   include Sidekiq::Worker
-  
+
   def perform(student_id, classroom_id)
     ClassroomUnit
       .where(classroom_id: classroom_id)
-      .where("classroom_units.assigned_student_ids @> '{?}'", ActiveRecord::Base.connection.quote(student_id))
+      .where("classroom_units.assigned_student_ids @> '{?}'", student_id)
       .each { |classroom_unit| classroom_unit.remove_assigned_student(student_id) }
   end
 end
