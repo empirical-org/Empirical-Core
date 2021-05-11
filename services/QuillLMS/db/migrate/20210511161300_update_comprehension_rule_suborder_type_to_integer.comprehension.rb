343e2bf09c66f54af0d4e23cb5e8f1de40736924
Eric Adams <erictayloradams@gmail.com>
2021-05-11 12:31:08 -0400

    add migration to revert rule suborder to integer

---
+++ services/QuillLMS/db/migrate/20210511161300_update_comprehension_rule_suborder_type_to_integer.comprehension.rb
@@ -0,0 +1,13 @@
+# This migration comes from comprehension (originally 20210511160025)
+class UpdateComprehensionRuleSuborderTypeToInteger < ActiveRecord::Migration
+  def change
+    change_column :comprehension_rules, :suborder, 'integer USING CAST(suborder AS integer)', null: true
+  end
+
+  Comprehension::Rule.all.each do |rule|
+    if rule.suborder != nil
+      rule.suborder = rule.suborder.to_i
+      rule.save!
+    end
+  end
+end
