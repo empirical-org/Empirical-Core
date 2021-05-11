cfc0645cef8228e1923c807d9f53b969b7afa6ed
Eric Adams <erictayloradams@gmail.com>
2021-05-11 14:39:21 -0400

    remove unneeded integer conversion; fix test

--- services/QuillLMS/engines/comprehension/db/migrate/20210511160025_update_comprehension_rule_suborder_type_to_integer.rb
+++ services/QuillLMS/engines/comprehension/db/migrate/20210511160025_update_comprehension_rule_suborder_type_to_integer.rb
@@ -2,11 +2,4 @@ class UpdateComprehensionRuleSuborderTypeToInteger < ActiveRecord::Migration
   def change
     change_column :comprehension_rules, :suborder, 'integer USING CAST(suborder AS integer)', null: true
   end
-
-  Comprehension::Rule.all.each do |rule|
-    if !rule.suborder.nil?
-      rule.suborder = rule.suborder.to_i
-      rule.save!
-    end
-  end
 end
