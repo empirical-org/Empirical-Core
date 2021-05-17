51d64452eda02a87bc4e40c59483a01c6ef2265c
Brendan Shean <brendanshean@gmail.com>
2021-05-17 13:18:54 -0400

    Fix spec failures in feedback controller test

--- services/QuillLMS/engines/comprehension/test/controllers/comprehension/feedback_controller_test.rb
+++ services/QuillLMS/engines/comprehension/test/controllers/comprehension/feedback_controller_test.rb
@@ -18,25 +18,60 @@ module Comprehension
 
     context "plagiarism" do
       should "return successfully" do
-        post 'plagiarism', entry: "No plagiarism here.", prompt_id: @prompt.id, session_id: 1, previous_feedback: []
+        post 'plagiarism',
+          params: {
+            entry: "No plagiarism here.",
+            prompt_id: @prompt.id,
+            session_id: 1,
+            previous_feedback: []
+          },
+          as: :json
+
         parsed_response = JSON.parse(response.body)
         assert_equal parsed_response["optimal"], true
       end
 
       should "return 404 if prompt id does not exist" do
-        post 'plagiarism', entry: "No plagiarism here.", prompt_id: 100, session_id: 1, previous_feedback: []
+        post 'plagiarism',
+          params: {
+            entry: "No plagiarism here.",
+            prompt_id: 100,
+            session_id: 1,
+            previous_feedback: []
+          },
+          as: :json
+
         assert_equal response.status, 404
       end
 
       should "return successfully when there is plagiarism" do
-        post 'plagiarism', entry: "bla bla bla do not plagiarize this text please", prompt_id: @prompt.id, session_id: 1, previous_feedback: []
+        post 'plagiarism',
+          params: {
+            entry: "bla bla bla do not plagiarize this text please",
+            prompt_id: @prompt.id,
+            session_id: 1,
+            previous_feedback: []
+          },
+          as: :json
+
         parsed_response = JSON.parse(response.body)
         assert_equal parsed_response["optimal"], false
         assert_equal parsed_response["highlight"][0]["text"], "do not plagiarize this text please"
         assert_equal parsed_response["highlight"][1]["text"], "do not plagiarize this text please"
         assert_equal parsed_response["feedback"], @first_feedback.text
 
-        post 'plagiarism', entry: "bla bla bla do not plagiarize this text please", prompt_id: @prompt.id, session_id: 1, previous_feedback: [parsed_response]
+        # Remove after upgrade to 5.2 https://github.com/rails/rails/issues/31643#issuecomment-611067165
+        @request.env.delete("RAW_POST_DATA")
+
+        post 'plagiarism',
+          params: {
+            entry: "bla bla bla do not plagiarize this text please",
+            prompt_id: @prompt.id,
+            session_id: 5,
+            previous_feedback: [parsed_response]
+          },
+          as: :json
+
         parsed_response = JSON.parse(response.body)
         assert_equal parsed_response["optimal"], false
         assert_equal parsed_response["feedback"], @second_feedback.text
@@ -45,23 +80,59 @@ module Comprehension
 
     context "regex" do
       should "return successfully" do
-        post 'regex', :rule_type => @rule_regex.rule_type, entry: "no regex problems here.", prompt_id: @prompt.id, session_id: 1, previous_feedback: []
+        post 'regex',
+          params: {
+            rule_type: @rule_regex.rule_type,
+            entry: "no regex problems here.",
+            prompt_id: @prompt.id,
+            session_id: 1,
+            previous_feedback: []
+          },
+          as: :json
+
         parsed_response = JSON.parse(response.body)
         assert_equal parsed_response["optimal"], true
       end
 
       should "return 404 if prompt id does not exist" do
-        post 'regex', :rule_type => @rule_regex.rule_type, entry: "no regex problems here.", prompt_id: 100, session_id: 1, previous_feedback: []
+        post 'regex',
+          params: {
+            rule_type: @rule_regex.rule_type,
+            entry: "no regex problems here.",
+            prompt_id: 100,
+            session_id: 1,
+            previous_feedback: []
+          },
+          as: :json
+
         assert_equal response.status, 404
       end
 
       should "return 404 if rule_type does not exist" do
-        post 'regex', :rule_type => "not-rule-type", entry: "no regex problems here.", prompt_id: 100, session_id: 1, previous_feedback: []
+        post 'regex',
+          params: {
+            rule_type: "not-rule-type",
+            entry: "no regex problems here.",
+            prompt_id: 100,
+            session_id: 1,
+            previous_feedback: []
+          },
+          as: :json
+
         assert_equal response.status, 404
       end
 
       should "return successfully when there is regex feedback" do
-        post 'regex', :rule_type => @rule_regex.rule_type, entry: "test regex response", prompt_id: @prompt.id, session_id: 1, previous_feedback: []
+        post 'regex',
+          params: {
+            rule_type: @rule_regex.rule_type,
+            entry: "test regex response",
+            prompt_id: @prompt.id,
+            session_id: 1,
+            previous_feedback: []
+          },
+          as: :json
+
         parsed_response = JSON.parse(response.body)
         assert_equal parsed_response["optimal"], false
       end
@@ -69,12 +140,28 @@ module Comprehension
 
     context "#automl" do
       should "return 404 if prompt id does not exist" do
-        post 'automl', entry: 'some text', prompt_id: @prompt.id + 1000, session_id: 1, previous_feedback: []
+        post 'automl',
+          params: {
+            entry: 'some text',
+            prompt_id: @prompt.id + 1000,
+            session_id: 1,
+            previous_feedback: []
+          },
+          as: :json
+
         assert_equal response.status, 404
       end
 
       should "return 404 if prompt has no associated automl_model" do
-        post 'automl', entry: 'some text', prompt_id: @prompt.id, session_id: 1, previous_feedback: []
+        post 'automl',
+          params: {
+            entry: 'some text',
+            prompt_id: @prompt.id,
+            session_id: 1,
+            previous_feedback: []
+          },
+          as: :json
+
         assert_equal response.status, 404
       end
 
@@ -83,7 +170,14 @@ module Comprehension
 
         AutomlCheck.stub_any_instance(:matched_rule, @rule) do
           Rule.stub_any_instance(:determine_feedback_from_history, @first_feedback) do
-            post 'automl', entry: entry, prompt_id: @prompt.id, session_id: 1, previous_feedback: []
+            post 'automl',
+              params: {
+                entry: entry,
+                prompt_id: @prompt.id,
+                session_id: 1,
+                previous_feedback: []
+              },
+              as: :json
 
             parsed_response = JSON.parse(response.body)
             assert_equal parsed_response, {
@@ -106,7 +200,15 @@ module Comprehension
         stub_request(:get, "https://api.cognitive.microsoft.com/bing/v7.0/SpellCheck?mode=proof&text=test%20spelin%20error")
         .to_return(status: 200, body: {flaggedTokens: [{token: 'spelin'}]}.to_json, headers: {})
 
-        post 'spelling', entry: "test spelin error", prompt_id: @prompt.id, session_id: 1, previous_feedback: []
+        post 'spelling',
+          params: {
+            entry: "test spelin error",
+            prompt_id: @prompt.id,
+            session_id: 1,
+            previous_feedback: []
+          },
+          as: :json
+
         parsed_response = JSON.parse(response.body)
         assert_equal response.status, 200
         assert_equal parsed_response["optimal"], false
@@ -116,7 +218,15 @@ module Comprehension
         stub_request(:get, "https://api.cognitive.microsoft.com/bing/v7.0/SpellCheck?mode=proof&text=there%20is%20no%20spelling%20error%20here")
         .to_return(status: 200, body: {error: {message: "There's a problem here"}}.to_json, headers: {})
 
-        post 'spelling', entry: "there is no spelling error here", prompt_id: @prompt.id, session_id: 1, previous_feedback: []
+        post 'spelling',
+          params: {
+            entry: "there is no spelling error here",
+            prompt_id: @prompt.id,
+            session_id: 1,
+            previous_feedback: []
+          },
+          as: :json
+
         parsed_response = JSON.parse(response.body)
         assert_equal response.status, 500
         assert_equal parsed_response["error"], "There's a problem here"
