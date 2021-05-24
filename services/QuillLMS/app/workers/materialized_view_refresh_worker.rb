b113b5a17ad0aa0af154ce8f1b6bccded3268ca9
happythenewsad <happythenewsad@users.noreply.github.com>
2021-05-24 16:41:38 -0400

    check in previous migration artifacts; add scheduled mat view refreshes (#7680)
    * migration artifacts from unrelated commit
    
    * periodically refresh mat views
    
    Co-authored-by: Peter Kong <pkong@quill.org>

---
+++ services/QuillLMS/app/workers/materialized_view_refresh_worker.rb
@@ -0,0 +1,12 @@
+class MaterializedViewRefreshWorker
+    include Sidekiq::Worker
+    sidekiq_options queue: SidekiqQueue::LOW
+    MATVIEWS = [
+      'feedback_histories_grouped_by_rule_uid'
+    ]
+    def perform
+        MATVIEWS.each do |matview| 
+            ActiveRecord::Base.refresh_materialized_view(matview)
+        end
+    end
+end
---
+++ services/QuillLMS/spec/workers/materialized_view_refresh_worker_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+describe MaterializedViewRefreshWorker, type: :worker do
+  let(:worker) { MaterializedViewRefreshWorker.new }
+
+  it 'sends a segment.io event' do
+    MaterializedViewRefreshWorker::MATVIEWS.each do |matview|
+      expect(ActiveRecord::Base).to receive(:refresh_materialized_view).with(matview)
+    end 
+    worker.perform
+  end
+end
