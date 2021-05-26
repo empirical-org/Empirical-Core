30af5d9749a4ac443941f8d27cf8444955471577
Thomas Robertson <thomas.robertson@quill.org>
2021-05-26 16:18:20 -0400

    Do some performance tweaking
    Mostly this tries to roll up all the counting into a single query with aggregation done in SQL rather than in iteration over rows.

--- services/QuillLMS/app/lib/rule_feedback_history.rb
+++ services/QuillLMS/app/lib/rule_feedback_history.rb
@@ -1,26 +1,33 @@
 class RuleFeedbackHistory
     def self.generate_report(conjunction:, activity_id:)
         sql_result = exec_query(conjunction: conjunction, activity_id: activity_id)
-        postprocessed = postprocessing(sql_result)
-        format_sql_results(postprocessed)
+        #postprocessed = postprocessing(sql_result)
+        format_sql_results(sql_result)
     end
 
     def self.exec_query(conjunction:, activity_id:)
-        sql = <<~SQL
-          select rules.uid as rules_uid,
-          feedback_histories.feedback_history_ids as feedback_histories_id_array,
-            prompts.activity_id as activity_id,
-            rules.rule_type as rule_type,
-            rules.suborder as rule_suborder,
-            rules.name as rule_name,
-            rules.note as rule_note
-            FROM comprehension_rules as rules
-            INNER JOIN comprehension_prompts_rules as prompts_rules ON rules.id = prompts_rules.rule_id
-            INNER JOIN comprehension_prompts as prompts ON prompts_rules.prompt_id = prompts.id
-            LEFT JOIN feedback_histories_grouped_by_rule_uid as feedback_histories ON feedback_histories.rule_uid = rules.uid
-            WHERE prompts.conjunction = '#{conjunction}' AND activity_id = '#{activity_id}'
-        SQL
-        rules = Comprehension::Rule.find_by_sql(sql)
+        Comprehension::Rule.select(<<~SELECT
+          comprehension_rules.uid AS rules_uid,
+          prompts.activity_id AS activity_id,
+          comprehension_rules.rule_type AS rule_type,
+          comprehension_rules.suborder AS rule_suborder,
+          comprehension_rules.name AS rule_name,
+          comprehension_rules.note AS rule_note,
+          count(feedback_history_ratings.rating) AS total_responses,
+          count(CASE WHEN feedback_history_ratings.rating = true THEN 1 END) AS total_strong,
+          count(CASE WHEN feedback_history_ratings.rating = false THEN 1 END) AS total_weak,
+          count(CASE WHEN feedback_history_flags.flag = '#{FeedbackHistoryFlag::FLAG_REPEATED_RULE_CONSECUTIVE}' THEN 1 END) AS repeated_consecutive,
+          count(CASE WHEN feedback_history_flags.flag = '#{FeedbackHistoryFlag::FLAG_REPEATED_RULE_NON_CONSECUTIVE}' THEN 1 END) AS repeated_non_consecutive
+          SELECT
+        )
+        .joins('INNER JOIN comprehension_prompts_rules as prompts_rules ON comprehension_rules.id = prompts_rules.rule_id')
+        .joins('INNER JOIN comprehension_prompts as prompts ON prompts_rules.prompt_id = prompts.id')
+        .joins('LEFT JOIN feedback_histories ON feedback_histories.rule_uid = comprehension_rules.uid')
+        .joins('LEFT JOIN feedback_history_ratings ON feedback_histories.id = feedback_history_ratings.feedback_history_id')
+        .joins('LEFT JOIN feedback_history_flags ON feedback_histories.id = feedback_history_flags.feedback_history_id')
+        .where("prompts.conjunction = ? AND activity_id = ?", conjunction, activity_id)
+        .group('rules_uid, activity_id, rule_type, rule_suborder, rule_name, rule_note')
+        .includes(:feedbacks)
     end
 
     def self.feedback_history_to_json(f_h)
@@ -48,56 +55,13 @@ class RuleFeedbackHistory
         }
     end
 
-    def self.postprocessing(rules_sql_result)
-        rules_sql_result.each do |r|
-
-            feedback_histories = FeedbackHistory
-                .where(id: r.feedback_histories_id_array).includes(:feedback_history_ratings).includes(:feedback_history_flags)
-
-            total_scored = feedback_histories.reduce(0) do |memo, feedback_history|
-                memo + feedback_history.feedback_history_ratings.count
-            end
-
-            total_strong = 0
-            total_weak = 0
-            repeated_consecutive = 0
-            repeated_non_consecutive = 0
-            if total_scored > 0
-                feedback_histories.each do |history|
-                  total_strong += 1 if history.feedback_history_ratings.any?(&:rating)
-                  total_weak += 1 if history.feedback_history_ratings.any? { |hr| hr.rating == false }
-                  repeated_consecutive += 1 if history.feedback_history_flags.any? { |f| f.flag == FeedbackHistoryFlag::FLAG_REPEATED_RULE_CONSECUTIVE }
-                  repeated_non_consecutive += 1 if history.feedback_history_flags.any? { |f| f.flag == FeedbackHistoryFlag::FLAG_REPEATED_RULE_NON_CONSECUTIVE }
-                end
-
-            end
-            r.define_singleton_method(:total_strong) { total_strong }
-            r.define_singleton_method(:total_weak) { total_weak }
-            r.define_singleton_method(:repeated_consecutive) { repeated_consecutive }
-            r.define_singleton_method(:repeated_non_consecutive) { repeated_non_consecutive }
-
-            r.define_singleton_method(:total_responses) { feedback_histories.count }
-        end
-
-        rule_feedbacks = Comprehension::Rule
-            .includes(:feedbacks)
-            .where(uid: rules_sql_result.map(&:rules_uid))
-
-        rules_sql_result.each do |r|
-            feedbacks = rule_feedbacks.find_by(uid: r.rules_uid).feedbacks
-            r.first_feedback = feedbacks.empty? ? '' : feedbacks.min_by {|f| f.order}.text
-        end
-
-        rules_sql_result
-    end
-
     def self.format_sql_results(relations)
         relations.map do |r|
             {
                 rule_uid: r.rules_uid,
                 api_name: r.rule_type,
                 rule_order: r.rule_suborder,
-                first_feedback: r.first_feedback,
+                first_feedback: r.feedbacks.first&.text || '',
                 rule_note: r.rule_note,
                 rule_name: r.rule_name,
                 total_responses: r.total_responses,
