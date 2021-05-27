f3f419305585a46384afa47cdecbbbdc40dcb801
Anathomical <thomas.robertson@quill.org>
2021-05-27 17:15:14 -0400

    Tweak LMS reporting to give "null" scores (#7709)
    Since we don't expect more traditional performance in Comprehension, we don't want to provide confusing numbers

--- services/QuillLMS/client/app/bundles/Comprehension/tests/libs/conceptResults.test.ts
+++ services/QuillLMS/client/app/bundles/Comprehension/tests/libs/conceptResults.test.ts
@@ -12,11 +12,6 @@ import {
 } from './conceptResults.data'
 
 describe("Getting concept results from a completed Comprehension activity", () => {
-  it("should determine the correct percentage", () => {
-    const result = calculatePercentage(submittedResponses)
-    expect(result).toEqual(0.8333333333333334)
-  })
-
   it("should generate concept results with concept UIDs and optimal flags", () => {
     const result = generateConceptResults(currentActivity, submittedResponses)
     expect(result).toEqual(expectedPayload.concept_results)    
