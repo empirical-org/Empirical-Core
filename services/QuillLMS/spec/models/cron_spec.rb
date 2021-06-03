f89b092d6574f4da82cb480681f56033313fac39
dandrabik <dandrabik@users.noreply.github.com>
2021-06-03 10:32:31 -0400

    Run Rematch All daily for questions updated the previous day. (#7746)
    * Run Rematch for the questions update the previous day.
    
    * Less retries for Rematch Response.

--- services/QuillLMS/spec/models/cron_spec.rb
+++ services/QuillLMS/spec/models/cron_spec.rb
@@ -36,6 +36,11 @@ describe "Cron", type: :model do
       expect(ResetDemoAccountWorker).to receive(:perform_async)
       Cron.interval_1_day
     end
+
+    it "enqueues RematchUpdatedQuestionsWorker" do
+      expect(RematchUpdatedQuestionsWorker).to receive(:perform_async)
+      Cron.interval_1_day
+    end
   end
 
   describe "#run_saturday" do
