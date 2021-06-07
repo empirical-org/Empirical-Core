f2c58087bb31d1357e2b3ec12ef6b4a12ade86ef
Thomas Robertson <thomas.robertson@quill.org>
2021-06-07 13:29:01 -0400

    Add New Relic to the Go production endpoint

--- services/comprehension/feedback-api-main/src/endpoint/go.mod
+++ services/comprehension/feedback-api-main/src/endpoint/go.mod
@@ -2,4 +2,7 @@ module quill.org/comprehension-go-endpoint
 
 go 1.13
 
-require github.com/gin-gonic/gin v1.7.2
+require (
+	github.com/gin-gonic/gin v1.7.2
+	github.com/newrelic/go-agent v3.13.0+incompatible
+)
--- services/comprehension/feedback-api-main/src/endpoint/main.go
+++ services/comprehension/feedback-api-main/src/endpoint/main.go
@@ -3,10 +3,23 @@ package main
 import (
 	"net/http"
 	"github.com/gin-gonic/gin"
+	"github.com/newrelic/go-agent/_integrations/nrgin/v1"
 )
 
 func main() {
 	router := gin.Default()
+
+	if newrelic_key := os.Getenv("NEW_RELIC_LICENSE_KEY"); "" != newrelic_key {
+		cfg := newrelic.NewConfig("Comprehension Go (Production)", newrelic_key)
+		cfg.Logger = newrelic.NewDebugLogger(os.Stdout)
+		app, err := newrelic.NewApplication(cfg)
+		if nil != err {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		router.Use(nrgin.Middleware(app))
+	}
+
 	router.OPTIONS("/", func(c *gin.Context) {
 		c.Header("Access-Control-Allow-Origin", "*")
 		c.Header("Access-Control-Allow-Methods", "POST")
