d71536ae5202077070ec900950d981eb4d15ea94
Thomas Robertson <thomas.robertson@quill.org>
2021-06-07 13:37:38 -0400

    Add a deploy script for deploying to Heroku

---
+++ services/comprehension/feedback-api-main/deploy.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+current_branch=`git rev-parse --abbrev-ref HEAD`
+app_name="Comprehension Go Endpoint"
+
+case $1 in
+  prod)
+    DEPLOY_GIT_BRANCH=deploy-comprehension-go-prod
+    HEROKU_APP=comprehension-go-endpoint
+    NR_URL="https://rpm.newrelic.com/accounts/2639113/applications/548856875"
+    current_branch="origin/production"
+    ;;
+  *)
+    echo "You must provide an environment argument of 'prod'."
+    exit 1
+esac
+
+read -r -p "Deploy branch '$current_branch' to '$1' environment? [y/N]" response
+if [[ "$response" =~ ^([y])$ ]]
+then
+    sh ../../../scripts/post_slack_deploy.sh $app_name $1 $current_branch false
+    if [ $1 == 'prod' ]
+    then
+        # For production, push directly from the remote production branch without going local
+        # This 'remote merge' requires your local git history/pointers of the remote branches to be up-to-date, so we run a 'git fetch' to do that.
+        # Documented here: https://github.com/empirical-org/test_repo/blob/destination_branch/test_file.txt
+        git fetch origin production
+        git fetch origin $DEPLOY_GIT_BRANCH
+        git push --no-verify --force origin origin/production:refs/heads/$DEPLOY_GIT_BRANCH
+
+        sh ../../../scripts/post_slack_deploy_description.sh $app_name
+    open "https://dashboard.heroku.com/apps/$HEROKU_APP/activity"
+    open $NR_URL
+    echo "Deploy screen opened in your browser, you can monitor from there."
+else
+    echo "Ok, we won't deploy. Have a good day!"
+fi
