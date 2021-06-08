789de5365ddbe2a632b0ac308880074efc7b202c
Thomas Robertson <thomas.robertson@quill.org>
2021-06-08 14:32:19 -0400

    Add staging environment to deploy script

--- services/comprehension/feedback-api-main/deploy.sh
+++ services/comprehension/feedback-api-main/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 current_branch=`git rev-parse --abbrev-ref HEAD`
-app_name="Comprehension Go Endpoint"
+app_name="ComprehensionGoEndpoint"
 
 case $1 in
   prod)
@@ -9,6 +9,11 @@ case $1 in
     HEROKU_APP=comprehension-go-endpoint
     NR_URL="https://one.newrelic.com/launcher/nr1-core.explorer?platform[filters]=Iihkb21haW4gPSAnQVBNJyBBTkQgdHlwZSA9ICdBUFBMSUNBVElPTicpIg==&platform[timeRange][duration]=1800000&platform[$isFallbackTimeRange]=true&pane=eyJuZXJkbGV0SWQiOiJhcG0tbmVyZGxldHMub3ZlcnZpZXciLCJlbnRpdHlHdWlkIjoiTWpZek9URXhNM3hCVUUxOFFWQlFURWxEUVZSSlQwNThNVEUxTVRrM09UZzJOZyIsImlzT3ZlcnZpZXciOnRydWUsInJlZmVycmVycyI6eyJsYXVuY2hlcklkIjoibnIxLWNvcmUuZXhwbG9yZXIiLCJuZXJkbGV0SWQiOiJucjEtY29yZS5saXN0aW5nIn19&sidebars[0]=eyJuZXJkbGV0SWQiOiJucjEtY29yZS5hY3Rpb25zIiwic2VsZWN0ZWROZXJkbGV0Ijp7Im5lcmRsZXRJZCI6ImFwbS1uZXJkbGV0cy5vdmVydmlldyIsImlzT3ZlcnZpZXciOnRydWV9LCJlbnRpdHlHdWlkIjoiTWpZek9URXhNM3hCVUUxOFFWQlFURWxEUVZSSlQwNThNVEUxTVRrM09UZzJOZyJ9"
     current_branch="origin/production"
+    ;;
+  staging)
+    DEPLOY_GIT_BRANCH=deploy-comprehension-go-staging
+    HEROKU_APP=comprehension-go-staging
+    NR_URL="https://one.newrelic.com/launcher/nr1-core.explorer?platform[timeRange][duration]=1800000&platform[$isFallbackTimeRange]=true&platform[filters]=Iihkb21haW4gPSAnQVBNJyBBTkQgdHlwZSA9ICdBUFBMSUNBVElPTicpIg==&pane=eyJuZXJkbGV0SWQiOiJhcG0tbmVyZGxldHMub3ZlcnZpZXciLCJlbnRpdHlHdWlkIjoiTWpZek9URXhNM3hCVUUxOFFWQlFURWxEUVZSSlQwNThNVEUxTXprMU1EWTFPUSIsImlzT3ZlcnZpZXciOnRydWUsInJlZmVycmVycyI6eyJsYXVuY2hlcklkIjoibnIxLWNvcmUuZXhwbG9yZXIiLCJuZXJkbGV0SWQiOiJucjEtY29yZS5saXN0aW5nIn19"
     ;;
   *)
     echo "You must provide an environment argument of 'prod'."
@@ -29,6 +34,9 @@ then
         git push --no-verify --force origin origin/production:refs/heads/$DEPLOY_GIT_BRANCH
 
         sh ../../../scripts/post_slack_deploy_description.sh $app_name
+    else
+        git fetch origin $DEPLOY_GIT_BRANCH
+        git push --no-verify --force origin ${current_branch}:$DEPLOY_GIT_BRANCH
     fi
     open "https://dashboard.heroku.com/apps/$HEROKU_APP/activity"
     open $NR_URL
