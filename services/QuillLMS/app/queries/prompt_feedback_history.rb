92879201fc956a2c9d98994f63e9fad0b0ae170d
Thomas Robertson <thomas.robertson@quill.org>
2021-06-11 16:53:37 -0400

    Add tests and tweak serialization to match them

--- services/QuillLMS/app/queries/prompt_feedback_history.rb
+++ services/QuillLMS/app/queries/prompt_feedback_history.rb
@@ -8,14 +8,14 @@ class PromptFeedbackHistory
     def self.prompt_health_query(activity_id)
         FeedbackHistory.select(<<~SELECT
           prompt_id,
-          COUNT(feedback_histories.id) AS total_responses,
+          COUNT(DISTINCT feedback_histories.id) AS total_responses,
           COUNT(DISTINCT feedback_histories.feedback_session_uid) AS session_count,
           comprehension_prompts.text AS display_name,
           COUNT(DISTINCT CASE WHEN optimal = true THEN feedback_histories.feedback_session_uid END) AS num_final_attempt_optimal,
           COUNT(DISTINCT CASE WHEN attempt = 5 AND optimal = false THEN feedback_histories.feedback_session_uid END) AS num_final_attempt_not_optimal,
           1.0 * SUM(CASE WHEN optimal = true THEN feedback_histories.attempt END) / COUNT(DISTINCT CASE WHEN optimal = true THEN feedback_histories.feedback_session_uid END) AS avg_attempts_to_optimal,
-          COUNT(DISTINCT CASE WHEN flag = 'repeated-consecutive' THEN feedback_histories.feedback_session_uid END) AS num_sessions_with_consecutive_repeated_rules,
-          COUNT(DISTINCT CASE WHEN flag = 'repeated-non-consecutive' THEN feedback_histories.feedback_session_uid END) AS num_sessions_with_non_consecutive_repeated_rules,
+          COUNT(DISTINCT CASE WHEN flag = 'repeated-consecutive' THEN feedback_histories.feedback_session_uid END) AS num_sessions_consecutive_repeated,
+          COUNT(DISTINCT CASE WHEN flag = 'repeated-non-consecutive' THEN feedback_histories.feedback_session_uid END) AS num_sessions_non_consecutive_repeated,
           COUNT(DISTINCT CASE WHEN attempt = 1 AND optimal = true THEN feedback_histories.feedback_session_uid END) AS num_first_attempt_optimal,
           COUNT(DISTINCT CASE WHEN attempt = 1 AND optimal = false THEN feedback_histories.feedback_session_uid END) AS num_first_attempt_not_optimal
         SELECT
@@ -29,11 +29,15 @@ class PromptFeedbackHistory
 
     def self.serialize_results(results)
       serialized_rows = results.map do |result|
-        result.serializable_hash(
-          only: [:prompt_id, :total_responses, :session_count, :display_name, :num_final_attempt_optimal, :num_final_attempt_not_optimal, :avg_attempts_to_optimal, :num_sessions_with_consecutive_repeated_rule, :num_sessions_with_non_consecutive_repeated_rule, :num_first_attempt_optimal, :num_first_attempt_not_optimal],
+        payload = result.serializable_hash(
+          only: [:prompt_id, :total_responses, :session_count, :display_name, :num_final_attempt_optimal, :num_final_attempt_not_optimal, :avg_attempts_to_optimal, :num_first_attempt_optimal, :num_first_attempt_not_optimal],
           include: []
         )
+        payload['num_sessions_with_consecutive_repeated_rule'] = result.num_sessions_consecutive_repeated
+        payload['num_sessions_with_non_consecutive_repeated_rule'] = result.num_sessions_non_consecutive_repeated
+        payload['avg_attempts_to_optimal'] = payload['avg_attempts_to_optimal']&.round(2) || 0
+        payload
       end
-      serialized_rows.group_by { |r| r[:prompt_id]}
+      serialized_rows.map{ |row| [row['prompt_id'], row] }.to_h
     end
 end
--- services/QuillLMS/spec/queries/prompt_feedback_history_spec.rb
+++ services/QuillLMS/spec/queries/prompt_feedback_history_spec.rb
@@ -1,190 +1,201 @@
 require 'rails_helper'
 
 RSpec.describe PromptFeedbackHistory, type: :model do
-  describe '#promptwise_sessions' do 
-    it 'should aggregate rows correctly' do 
-      main_activity = Comprehension::Activity.create!(name: 'Title_1', title: 'Title 1', parent_activity_id: 1, target_level: 1)
-      unused_activity = Comprehension::Activity.create!(name: 'Title_2', title: 'Title 2', parent_activity_id: 2, target_level: 1)
-
-      prompt1 = Comprehension::Prompt.create!(activity: main_activity, conjunction: 'because', text: 'Some feedback text', max_attempts_feedback: 'Feedback')
-      prompt2 = Comprehension::Prompt.create!(activity: main_activity, conjunction: 'because', text: 'Some feedback text', max_attempts_feedback: 'Feedback')
-      prompt3 = Comprehension::Prompt.create!(activity: unused_activity, conjunction: 'because', text: 'Some feedback text', max_attempts_feedback: 'Feedback')
-
-      session_uid1 = SecureRandom.uuid
-      session_uid2 = SecureRandom.uuid
-      session_uid3 = SecureRandom.uuid
-
-      f_h1 = create(:feedback_history, attempt: 1, optimal: false, prompt_id: prompt1.id, feedback_session_uid: session_uid1)
-      f_h2 = create(:feedback_history, attempt: 2, optimal: true, prompt_id: prompt1.id, feedback_session_uid: session_uid1)
-      f_h3 = create(:feedback_history, attempt: 1, optimal: false, prompt_id: prompt2.id, feedback_session_uid: session_uid2)
-      f_h4 = create(:feedback_history, attempt: 2, optimal: false, prompt_id: prompt2.id, feedback_session_uid: session_uid2)
-      f_h5 = create(:feedback_history, prompt_id: prompt3.id, feedback_session_uid: session_uid3)
-
-      result = PromptFeedbackHistory.promptwise_sessions(main_activity.id)
-
-      expect(result.count).to eq 2
-      expect(result.first.at_least_one_optimal).to be true 
-      expect(result.first.attempt_cardinal).to eq 2
-      expect(result.last.at_least_one_optimal).to be false
-      expect(result.last.attempt_cardinal).to eq 2
+  def generate_feedback_history(prompt_id, session_uid: nil, attempts: 1, ends_optimally: true)
+    histories = []
+    session_uid = SecureRandom.uuid unless session_uid
+    (attempts - 1).times do |idx|
+      histories.append(create(:feedback_history, attempt: idx + 1, optimal: false, prompt_id: prompt_id, feedback_session_uid: session_uid))
     end
-
+    histories.append(create(:feedback_history, attempt: attempts, optimal: ends_optimally, prompt_id: prompt_id, feedback_session_uid: session_uid))
+    histories
   end
 
-  describe '#consecutive_repeated_rule?' do 
-    it 'should calculate correctly' do 
-      expect( 
-        PromptFeedbackHistory.consecutive_repeated_rule?([1,2,3,4], %w(a b c d))
-      ).to eq false
+  before do
+    @main_activity = Comprehension::Activity.create!(name: 'Title_1', title: 'Title 1', parent_activity_id: 1, target_level: 1)
+    @unused_activity = Comprehension::Activity.create!(name: 'Title_2', title: 'Title 2', parent_activity_id: 2, target_level: 1)
 
-      expect( 
-        PromptFeedbackHistory.consecutive_repeated_rule?([], [])
-      ).to eq false
+    @prompt1 = Comprehension::Prompt.create!(activity: @main_activity, conjunction: 'because', text: 'Some feedback text', max_attempts_feedback: 'Feedback')
+    @prompt2 = Comprehension::Prompt.create!(activity: @main_activity, conjunction: 'because', text: 'Some feedback text', max_attempts_feedback: 'Feedback')
+    @prompt3 = Comprehension::Prompt.create!(activity: @unused_activity, conjunction: 'because', text: 'Some feedback text', max_attempts_feedback: 'Feedback')
 
-      expect( 
-        PromptFeedbackHistory.consecutive_repeated_rule?([1,2,3,4], %w(a b c c))
-      ).to eq true
+    generate_feedback_history(@prompt3.id)
+  end
 
-      expect( 
-        PromptFeedbackHistory.consecutive_repeated_rule?([1,2,3,4,5], %w(a a b c c))
-      ).to eq true
+  describe '#prompt_health_query' do 
+    it 'should count total relevant FeedbackHistory records as total_responses' do
+      generate_feedback_history(@prompt1.id, attempts: 2)
+      generate_feedback_history(@prompt1.id, attempts: 3)
+      generate_feedback_history(@prompt2.id, attempts: 3)
+
+      result = PromptFeedbackHistory.prompt_health_query(@main_activity.id)
+
+      expect(result.all.map(&:total_responses)).to eq([5, 3])
+    end
+
+    it 'should not count feedback_history records from a different activity_id' do
+      generate_feedback_history(@prompt1.id, attempts: 3)
+      generate_feedback_history(@prompt3.id, attempts: 3)
+
+      result = PromptFeedbackHistory.prompt_health_query(@main_activity.id)
+
+      expect(result.all.length).to eq(1)
+    end
+
+    it 'should count total unique feedback_session_uids as session_count' do
+      generate_feedback_history(@prompt1.id, attempts: 1)
+      generate_feedback_history(@prompt1.id, attempts: 1)
+      generate_feedback_history(@prompt1.id, attempts: 1)
+
+      result = PromptFeedbackHistory.prompt_health_query(@main_activity.id)
+
+      expect(result.all[0].session_count).to eq(3)
+    end
+
+    it 'should show the text of the prompt as display_name' do
+      generate_feedback_history(@prompt1.id, attempts: 1)
+
+      result = PromptFeedbackHistory.prompt_health_query(@main_activity.id)
+
+      expect(result.all[0].display_name).to eq(@prompt1.text)
+    end
+
+    it 'should count the number of sessions that end optimally as num_final_attempt_optimal' do
+      generate_feedback_history(@prompt1.id, attempts: 1)
+      generate_feedback_history(@prompt1.id, attempts: 1)
+      generate_feedback_history(@prompt1.id, attempts: 1, ends_optimally: false)
+
+      result = PromptFeedbackHistory.prompt_health_query(@main_activity.id)
+
+      expect(result.all[0].num_final_attempt_optimal).to eq(2)
+    end
+
+    it 'should count the number of sessions that do not end optimally as num_final_attempt_not_optimal' do
+      generate_feedback_history(@prompt1.id, attempts: 1)
+      generate_feedback_history(@prompt1.id, attempts: 1)
+      generate_feedback_history(@prompt1.id, attempts: 1, attempts: 5, ends_optimally: false)
+
+      result = PromptFeedbackHistory.prompt_health_query(@main_activity.id)
+
+      expect(result.all[0].num_final_attempt_not_optimal).to eq(1)
+    end
+
+    it 'should calculate the average number of attempts required to get optimal feedback as avg_attempts_to_optimal' do
+      generate_feedback_history(@prompt1.id, attempts: 1)
+      generate_feedback_history(@prompt1.id, attempts: 2)
+
+      result = PromptFeedbackHistory.prompt_health_query(@main_activity.id)
+
+      expect(result.all[0].avg_attempts_to_optimal).to eq(1.5)
+    end
+
+    it 'should not use sessions that did not end optimally to calculate avg_attempts_to_optimal' do
+      generate_feedback_history(@prompt1.id, attempts: 1)
+      generate_feedback_history(@prompt1.id, attempts: 2)
+      generate_feedback_history(@prompt1.id, attempts: 3, ends_optimally: false)
+
+      result = PromptFeedbackHistory.prompt_health_query(@main_activity.id)
+
+      expect(result.all[0].avg_attempts_to_optimal).to eq(1.5)
+    end
+
+    it 'should count the number of sessions with at least one consecutive-repeated flag as num_sessions_consecutive_repeated' do
+      feedback_histories = generate_feedback_history(@prompt1.id, attempts: 3)
+      generate_feedback_history(@prompt1.id)
+      create(:feedback_history_flag, feedback_history: feedback_histories[1], flag: FeedbackHistoryFlag::FLAG_REPEATED_RULE_CONSECUTIVE)
+
+      result = PromptFeedbackHistory.prompt_health_query(@main_activity.id)
+
+      expect(result.all[0].num_sessions_consecutive_repeated).to eq(1)
+    end
+
+    it 'should count the number of sessions with at least one non-consecutive-repeated flag as num_sessions_non_consecutive_repeated' do
+      feedback_histories = generate_feedback_history(@prompt1.id, attempts: 3)
+      generate_feedback_history(@prompt1.id)
+      create(:feedback_history_flag, feedback_history: feedback_histories[1], flag: FeedbackHistoryFlag::FLAG_REPEATED_RULE_NON_CONSECUTIVE)
+
+      result = PromptFeedbackHistory.prompt_health_query(@main_activity.id)
+
+      expect(result.all[0].num_sessions_non_consecutive_repeated).to eq(1)
+    end
+
+    it 'should count each session only once no matter how many flags it has' do
+      feedback_histories = generate_feedback_history(@prompt1.id, attempts: 4)
+      create(:feedback_history_flag, feedback_history: feedback_histories[1], flag: FeedbackHistoryFlag::FLAG_REPEATED_RULE_NON_CONSECUTIVE)
+      create(:feedback_history_flag, feedback_history: feedback_histories[2], flag: FeedbackHistoryFlag::FLAG_REPEATED_RULE_NON_CONSECUTIVE)
+
+      result = PromptFeedbackHistory.prompt_health_query(@main_activity.id)
+
+      expect(result.all[0].num_sessions_non_consecutive_repeated).to eq(1)
+    end
+
+    it 'should count the number of sessions that had an optimal first feedback as num_first_attempt_optimal' do
+      generate_feedback_history(@prompt1.id, attempts: 1)
+      generate_feedback_history(@prompt1.id, attempts: 1)
+      generate_feedback_history(@prompt1.id, attempts: 3, ends_optimally: false)
+
+      result = PromptFeedbackHistory.prompt_health_query(@main_activity.id)
+
+      expect(result.all[0].num_first_attempt_optimal).to eq(2)
+    end
+
+    it 'should count the number of sessions that had non-optimal first feedback as num_first_attempt_not_optimal' do
+      generate_feedback_history(@prompt1.id, attempts: 1)
+      generate_feedback_history(@prompt1.id, attempts: 1)
+      generate_feedback_history(@prompt1.id, attempts: 3, ends_optimally: false)
+
+      result = PromptFeedbackHistory.prompt_health_query(@main_activity.id)
+
+      expect(result.all[0].num_first_attempt_not_optimal).to eq(1)
     end
   end
 
-  describe '#non_consecutive_repeated_rule?' do 
-    it 'should calculate correctly' do 
-      expect( 
-        PromptFeedbackHistory.non_consecutive_repeated_rule?([1,2,3,4], %w(a b c d))
-      ).to eq false
+  describe '#serialize_results' do
+    it 'should group results by prompt id' do
+      generate_feedback_history(@prompt1.id, attempts: 1)
+      generate_feedback_history(@prompt2.id, attempts: 1)
 
-      expect( 
-        PromptFeedbackHistory.non_consecutive_repeated_rule?([1,2,3,4], %w(a b c a))
-      ).to eq true
-    end 
+      result = PromptFeedbackHistory.prompt_health_query(@main_activity.id)
+      parsed_result = PromptFeedbackHistory.serialize_results(result)
 
-    it 'should ignore consecutive repeats' do 
-      expect( 
-        PromptFeedbackHistory.non_consecutive_repeated_rule?([1,2,3,4], %w(a a c d))
-      ).to eq false
-
-      expect( 
-        PromptFeedbackHistory.non_consecutive_repeated_rule?([1,2,3,4], %w(a a a d))
-      ).to eq false
-
-      expect( 
-        PromptFeedbackHistory.non_consecutive_repeated_rule?([1,2,3,4,5], %w(a a c d c))
-      ).to eq true
-    end
-  end
-  
-  describe '#first_attempt_optimal?' do 
-    it 'should return true' do 
-      expect(
-        PromptFeedbackHistory.first_attempt_optimal?([1], [true])
-      ).to be true
-      
-      expect(
-        PromptFeedbackHistory.first_attempt_optimal?([2, 1], [true, false])
-      ).to be false
-    end
-  end
-
-  describe '#promptwise_postprocessing' do 
-
-    let(:main_activity) { create(:activity) }
-    let(:unused_activity) { create(:activity) }
-
-    let!(:p1) do
-      Comprehension::Prompt.create!(
-        id: 1,
-        activity: main_activity,
-        text: 'lorem ipsum1',
-        conjunction: Comprehension::Prompt::CONJUNCTIONS.first,
-        max_attempts: 5
-      )
+      expect(parsed_result.keys).to eq([@prompt1.id, @prompt2.id])
     end
 
-    let!(:p2) do 
-        Comprehension::Prompt.create!(
-        id: 2,
-        activity: main_activity,
-        text: 'lorem ipsum2',
-        conjunction: Comprehension::Prompt::CONJUNCTIONS.first,
-        max_attempts: 5
-        )
-    end
+    it 'should generate hashes with expected keys' do
+      generate_feedback_history(@prompt1.id, attempts: 2)
+      generate_feedback_history(@prompt2.id, attempts: 1)
 
-    let!(:as1) { create(:activity_session, activity_id: main_activity.id) }
-    let!(:as2) { create(:activity_session, activity_id: main_activity.id) }
-    let!(:as3) { create(:activity_session, activity_id: unused_activity.id) }
+      result = PromptFeedbackHistory.prompt_health_query(@main_activity.id)
+      parsed_result = PromptFeedbackHistory.serialize_results(result)
 
-    it 'should format' do 
-      f_h1 = create(:feedback_history, feedback_session_uid: as1.uid, attempt: 1, optimal: false, prompt_id: 1)
-      f_h2 = create(:feedback_history, feedback_session_uid: as1.uid, attempt: 2, optimal: true, prompt_id: 1)
-      f_h3 = create(:feedback_history, feedback_session_uid: as2.uid, attempt: 1, optimal: false, prompt_id: 2)
-      f_h4 = create(:feedback_history, feedback_session_uid: as2.uid, attempt: 2, optimal: false, prompt_id: 2)
-      f_h5 = create(:feedback_history, feedback_session_uid: as3.uid, prompt_id: 3)
-
-      result = PromptFeedbackHistory.promptwise_sessions(main_activity.id)
-      processed = PromptFeedbackHistory.promptwise_postprocessing(result)
-
-      first_prompt = processed[:"1"]
-      second_prompt = processed[:"2"]
-      
-      expect(first_prompt).to include(
-        {
-          total_responses: 2.0,
-          session_count: 1.0,
-          display_name: "lorem ipsum1",
-
-          num_final_attempt_optimal: 1.0,
-          num_final_attempt_not_optimal: 0.0,
-
+      expect(parsed_result).to eq({
+	@prompt1.id => {
+          prompt_id: @prompt1.id,
+          total_responses: 2,
+          session_count: 1,
+          display_name: @prompt1.text,
+          num_final_attempt_optimal: 1,
+          num_final_attempt_not_optimal: 0,
           avg_attempts_to_optimal: 2.0,
-
           num_sessions_with_consecutive_repeated_rule: 0.0,
           num_sessions_with_non_consecutive_repeated_rule: 0.0,
-
-          num_first_attempt_optimal: 0.0,
-          num_first_attempt_not_optimal: 1.0
-        })
-      expect(second_prompt).to include(
-        {
-          total_responses: 2.0,
-          session_count: 1.0,
-          display_name: "lorem ipsum2",
-
-          num_final_attempt_optimal: 0.0,
-          num_final_attempt_not_optimal: 1.0,
-
-          avg_attempts_to_optimal: 0.0,
-
+          num_first_attempt_optimal: 0,
+          num_first_attempt_not_optimal: 1
+	}.stringify_keys,
+	@prompt2.id => {
+          prompt_id: @prompt2.id,
+          total_responses: 1,
+          session_count: 1,
+          display_name: @prompt2.text,
+          num_final_attempt_optimal: 1,
+          num_final_attempt_not_optimal: 0,
+          avg_attempts_to_optimal: 1.0,
           num_sessions_with_consecutive_repeated_rule: 0.0,
           num_sessions_with_non_consecutive_repeated_rule: 0.0,
-
-          num_first_attempt_optimal: 0.0,
-          num_first_attempt_not_optimal: 1.0
-        })
+          num_first_attempt_optimal: 1,
+          num_first_attempt_not_optimal: 0
+	}.stringify_keys
+      })
     end
-
-    it 'should format consecutive repeated attempts' do 
-      f_h1 = create(:feedback_history, feedback_session_uid: as1.uid, attempt: 1, optimal: false, prompt_id: 1, rule_uid: 1)
-      f_h2 = create(:feedback_history, feedback_session_uid: as1.uid, attempt: 2, optimal: true, prompt_id: 1, rule_uid: 2)
-      f_h3 = create(:feedback_history, feedback_session_uid: as2.uid, attempt: 1, optimal: false, prompt_id: 2, rule_uid: 1)
-      f_h4 = create(:feedback_history, feedback_session_uid: as2.uid, attempt: 2, optimal: false, prompt_id: 2, rule_uid: 1)
-
-      result = PromptFeedbackHistory.promptwise_sessions(main_activity.id)
-      processed = PromptFeedbackHistory.promptwise_postprocessing(result)
-
-      second_prompt = processed[:"2"]
-
-      expect(second_prompt).to include(
-        {
-          num_sessions_with_consecutive_repeated_rule: 1.0,
-          num_sessions_with_non_consecutive_repeated_rule: 0.0,
-
-        })
-    end
-
   end
 end
