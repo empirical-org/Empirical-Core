8a7ca79347dcfb3f8eda0e80f7dec31a506a2440
Brendan Shean <brendanshean@gmail.com>
2021-07-06 10:59:32 -0400

    Fix division by zero

--- services/QuillLMS/lib/tasks/progress_bar.rb
+++ services/QuillLMS/lib/tasks/progress_bar.rb
@@ -5,6 +5,8 @@ class ProgessBar
   end
 
   def increment
+    return if @total.zero?
+
     complete = format("%<percent_completed>.2f%%", percent_completed: ((@counter / @total.to_f) * 100))
     print "\r\e[0K#{@counter}/#{@total} (#{complete})"
     @counter += 1
