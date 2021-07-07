f1990adf4bf99a0b63f227d4b7485245ec4661f2
Emilia Friedberg <emiliafriedberg@gmail.com>
2021-07-07 13:14:42 -0400

    regex rule ordering

--- services/QuillLMS/engines/comprehension/app/controllers/comprehension/rules_controller.rb
+++ services/QuillLMS/engines/comprehension/app/controllers/comprehension/rules_controller.rb
@@ -45,6 +45,22 @@ module Comprehension
       head :no_content
     end
 
+    def update_rule_order
+      ordered_rules = ordered_rules_params[:ordered_rule_ids].map.with_index do |id, index|
+        rule = Comprehension::Rule.find(id)
+        rule.suborder = index
+        rule
+      end
+
+      if ordered_rules.all? { |r| r.valid? }
+        ordered_rules.each { |r| r.save! }
+      else
+        render json: {error_messages: ordered_rules.map { |r| r.errors }.join('; ')}, status: :unprocessable_entity
+      end
+
+      render(json: {status: 200})
+    end
+
     private def set_rule
       # warning - the id param is getting used as both an id and a uid, which is an antipattern
       @rule = Comprehension::Rule.find_by_uid(params[:id]) || Comprehension::Rule.find(params[:id])
@@ -58,6 +74,10 @@ module Comprehension
          label_attributes: [:id, :name, :state],
          feedbacks_attributes: [:id, :text, :description, :order, highlights_attributes: [:id, :text, :highlight_type, :starting_index]]
       )
+    end
+
+    private def ordered_rules_params
+      params.permit(ordered_rule_ids: [])
     end
   end
 end
