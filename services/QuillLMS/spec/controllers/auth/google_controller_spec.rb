aedd1c6d0f183cd2eadda4b9120d0f318d7e34e1
Brendan Shean <brendanshean@gmail.com>
2021-07-13 16:19:49 -0400

    Fix failing spec

--- services/QuillLMS/spec/controllers/auth/google_controller_spec.rb
+++ services/QuillLMS/spec/controllers/auth/google_controller_spec.rb
@@ -3,7 +3,11 @@ require 'rails_helper'
 describe Auth::GoogleController, type: :controller do
 
   it 'shows error message for nonexistent accounts with no role' do
+<<<<<<< HEAD
     get 'google', params: { :role => nil }
+=======
+    get 'authorization_and_authentication', {:role => nil}
+>>>>>>> fa7fe6822 (Fix failing spec)
     expect(flash[:error]).to include("We could not find your account. Is this your first time logging in?")
     expect(response).to redirect_to "/session/new"
   end
