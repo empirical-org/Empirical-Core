6135c9342e0c6d224e578e6d61664324e84d3191
happythenewsad <happythenewsad@users.noreply.github.com>
2021-07-15 09:20:52 -0600

    fixing failing spec (#7905)
    * fixing failing spec
    
    * feedback
    
    * more feedback
    
    Co-authored-by: Peter Kong <pkong@quill.org>

--- services/QuillLMS/spec/models/concerns/teacher_spec.rb
+++ services/QuillLMS/spec/models/concerns/teacher_spec.rb
@@ -542,24 +542,29 @@ describe User, type: :model do
     end
   end
 
-  describe '#number_of_assigned_students_per_activity_assigned' do
+  describe '#assigned_students_per_activity_assigned' do
     context 'assigned students' do
       let!(:teacher) { create(:teacher, :with_classrooms_students_and_activities) }
       let!(:assigned_units) { teacher.assigned_students_per_activity_assigned }
 
-      it 'should return the correct number of students assigned' do
+      it 'should return only the correct students assigned' do
+        student_ids = assigned_units.pluck(:assigned_student_ids).flatten
 
-        assigned_units_arr = assigned_units.to_a
-        teacher.classroom_units.each { |cu|
-          cu.unit.unit_activities.each { |ua|
+        other_teacher = create(:teacher, :with_classrooms_students_and_activities) 
+        other_assigned_activities_ids = other_teacher.assigned_students_per_activity_assigned.pluck(:id)
+        other_student_ids = other_teacher.assigned_students_per_activity_assigned
+          .pluck(:assigned_student_ids)
+          .flatten
+
+        expect(other_assigned_activities_ids & assigned_units.pluck(:id)).to be_empty
+        expect(other_student_ids & student_ids).to be_empty
+
+        teacher.classroom_units.each do |cu|
+          cu.unit.unit_activities.each do |ua|
             matching_element = assigned_units.detect {|u| u.id == ua.activity_id && u.assigned_student_ids == cu.assigned_student_ids}
-            expect(matching_element).to_not be_nil
             expect(matching_element.assigned_student_ids).to eq(cu.assigned_student_ids)
-            expect(matching_element.created_at).to eq(ua.created_at)
-            assigned_units_arr.delete_at(assigned_units_arr.index(matching_element))
-          }
-        }
-        expect(assigned_units_arr).to be_empty
+          end
+        end
       end
     end
 
