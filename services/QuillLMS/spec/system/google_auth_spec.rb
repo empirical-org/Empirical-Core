9c0779123221dd8a73cd3e14bd74ceb36e502e14
Brendan Shean <brendanshean@gmail.com>
2021-07-27 09:09:08 -0400

    Add in auth_credential

--- services/QuillLMS/spec/system/google_auth_spec.rb
+++ services/QuillLMS/spec/system/google_auth_spec.rb
@@ -3,16 +3,25 @@ require 'rails_helper'
 RSpec.describe 'Google Auth' do
   let(:email) { ENV.fetch('GOOGLE_TEST_USER_EMAIL') }
   let(:password) { ENV.fetch('GOOGLE_TEST_USER_PASSWORD') }
-  let!(:user) { create(:user, role: 'teacher', email: email, password: password) }
+  let(:google_id) { ENV.fetch('GOOGLE_TEST_USER_GOOGLE_ID') }
+  let(:user) { create(:user, role: 'teacher', email: email, password: password, google_id: google_id) }
+
+  before do
+    create(:auth_credential,
+      refresh_token: ENV.fetch('GOOGLE_TEST_USER_REFRESH_TOKEN'),
+      provider: 'google',
+      user: user
+    )
+  end
 
   it 'user can login via google auth', :js do
     visit root_path
     click_link 'Log In'
     click_button 'Log in with Google'
-    fill_in 'identifierId', with:  user.email
+    fill_in 'identifierId', with: user.email
     click_button 'Next'
     fill_in 'password', with: user.password
     click_button 'Next'
-    click_button 'Allow'
+    sleep 5
   end
 end
