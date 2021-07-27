828f2542f1dcd67ac264215376d666889575ea74
Peter Kong <pkong@quill.org>
2021-07-26 19:47:52 -0600

    wip

---
+++ services/QuillLMS/config/initializers/secure_headers.rb
@@ -0,0 +1,9 @@
+SecureHeaders::Configuration.default do |config|
+  config.cookies = {
+    secure: true, # mark all cookies as "Secure"
+    httponly: true, # mark all cookies as "HttpOnly"
+    samesite: {
+      lax: true # mark all cookies as SameSite=lax
+    }
+  }
+end
