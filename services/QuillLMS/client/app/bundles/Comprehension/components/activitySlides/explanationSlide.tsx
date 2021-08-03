ac639d798dd3cc5adb8700d2cacbaa719ef49181
Eric Adams <erictayloradams@gmail.com>
2021-08-03 10:51:30 -0500

    add some styling

--- services/QuillLMS/client/app/bundles/Comprehension/components/activitySlides/explanationSlide.tsx
+++ services/QuillLMS/client/app/bundles/Comprehension/components/activitySlides/explanationSlide.tsx
@@ -11,13 +11,13 @@ export const ExplanationSlide = ({ slideData, onHandleClick }) => {
   return(
     <div className="explanation-slide-container">
       <section id="information-section">
-        <p>Good to know</p>
-        <section>
+        <p className="subtext">Good to know</p>
+        <section id="header-container">
           <p id="header">{header}</p>
           {isBeta && <div id="beta-tag">BETA</div>}
         </section>
         <img alt={imageAlt} id="image" src={`${process.env.CDN_URL}/${imageUrl}`} />
-        <p id="subtext">{subtext}</p>
+        <p className="subtext">{subtext}</p>
       </section>
       <section id="button-container">
         <button className="quill-button large secondary outlined" onClick={handleClick} type="submit">{buttonText}</button>
