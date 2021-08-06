64c4cc5e6d07030ba628278dd8e811c90726bcd0
Cissy Yu <cissy@quill.org>
2021-08-06 11:49:51 -0400

    revert broken syntax for assigned student ids

--- services/QuillLMS/app/services/demo/report_demo_creator.rb
+++ services/QuillLMS/app/services/demo/report_demo_creator.rb
@@ -134,7 +134,7 @@ module Demo::ReportDemoCreator
   def self.create_replayed_activity_session(student)
     replayed_session = ActivitySession.unscoped.where({activity_id: REPLAYED_ACTIVITY_ID, user_id: REPLAYED_SAMPLE_USER_ID, is_final_score: true}).first
     student_id = student.id
-    cu = ClassroomUnit.where(student_id: assigned_student_ids).to_a.first
+    cu = ClassroomUnit.where("#{student.id} = ANY (assigned_student_ids)").to_a.first
     act_session = ActivitySession.create({activity_id: REPLAYED_ACTIVITY_ID, classroom_unit_id: cu.id, user_id: student.id, state: "finished", percentage: replayed_session&.percentage})
     replayed_session&.concept_results&.each do |cr|
       values = {
