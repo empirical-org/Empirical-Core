425d421f086bb778c8808a65a88f176f284f9754
Daniel Drabik <dan@quill.org>
2021-08-18 15:37:11 -0400

    Refresh the response views overnight.

--- services/QuillCMS/app/models/cron.rb
+++ services/QuillCMS/app/models/cron.rb
@@ -2,11 +2,32 @@ class Cron
 
   def self.run
     current_time = Time.zone.now
-    if current_time.hour == 3
-      beginning_of_yesterday = current_time.yesterday.beginning_of_day
-      end_of_yesterday = current_time.yesterday.end_of_day
-      UpdateElasticsearchWorker.perform_async(beginning_of_yesterday, end_of_yesterday)
-      UpdateElasticsearchWorker.perform_async(current_time.beginning_of_day, current_time)
+
+    return unless current_time.hour == 3
+
+    update_elasticsearch(current_time)
+    refresh_response_views
+  end
+
+  private_class_method def self.update_elasticsearch(time)
+    beginning_of_yesterday = time.yesterday.beginning_of_day
+    end_of_yesterday = time.yesterday.end_of_day
+
+    UpdateElasticsearchWorker.perform_async(beginning_of_yesterday, end_of_yesterday)
+    UpdateElasticsearchWorker.perform_async(time.beginning_of_day, time)
+  end
+
+  private_class_method def self.refresh_response_views
+    # spread out run by the length of timeout to run them separately
+    # default to 10 minutes
+    minute_string = 'min'
+    timeout = RefreshResponsesViewWorker::REFRESH_TIMEOUT
+    default_interval = 600 # 10 minutes
+
+    interval = minute_string.in?(timeout) ? (timeout.gsub(minute_string,'').to_i * 60) : default_inteveral
+
+    RefreshResponsesViewWorker::VIEWS.each.with_index do |view, index|
+      RefreshResponsesViewWorker.perform_in(index * interval, view)
     end
   end
 end
--- services/QuillCMS/spec/models/cron_spec.rb
+++ services/QuillCMS/spec/models/cron_spec.rb
@@ -15,5 +15,14 @@ RSpec.describe Cron do
       expect(UpdateElasticsearchWorker).to receive(:perform_async).with(Time.zone.now.beginning_of_day, Time.zone.now.beginning_of_day + 3.hour)
       Cron.run
     end
+
+    it "should run a refresh responsee view job if the hour is 3AM" do
+      allow(Time).to receive(:now).and_return(Time.zone.now.beginning_of_day + 3.hour)
+
+      expect(RefreshResponsesViewWorker).to receive(:perform_in).with(0, 'GradedResponse')
+      expect(RefreshResponsesViewWorker).to receive(:perform_in).with(600, 'MultipleChoiceResponse')
+      Cron.run
+    end
+
   end
 end
