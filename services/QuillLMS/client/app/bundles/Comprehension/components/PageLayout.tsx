5f5b61d26ba6b0aa7d5997cd2097f28204427356
Emilia Friedberg <emiliafriedberg@gmail.com>
2021-08-24 09:05:44 -0400

    evidence accessibility (#8112)

--- services/QuillLMS/client/app/bundles/Comprehension/components/PageLayout.tsx
+++ services/QuillLMS/client/app/bundles/Comprehension/components/PageLayout.tsx
@@ -7,12 +7,21 @@ import { routes } from "../routes";
 
 const PageLayout: React.StatelessComponent<{}> = (props: any) => {
   const { user } = props;
-    return (
-      <div aria-live="polite" className="app-container">
-        <Header />
-        {renderRoutes(routes, { user })}
-      </div>
-    );
+
+  function handleSkipToMainContentClick () {
+    const element = document.getElementById("main-content")
+    if (!element) { return }
+    element.focus()
+    element.scrollIntoView()
+  }
+
+  return (
+    <div aria-live="polite" className="app-container">
+      <button className="skip-main" onClick={handleSkipToMainContentClick} type="button">Skip to main content</button>
+      <Header />
+      <div id="main-content" tabIndex={-1}>{renderRoutes(routes, { user })}</div>
+    </div>
+  );
 };
 
 export default PageLayout;
