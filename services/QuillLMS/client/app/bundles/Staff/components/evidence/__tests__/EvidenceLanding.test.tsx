fa2a20475f60f77ec4da22e608880a4fd85feb67
Emilia Friedberg <emiliafriedberg@gmail.com>
2021-08-25 11:09:01 -0400

    rename frontend bundles to evidence

---
+++ services/QuillLMS/client/app/bundles/Staff/components/evidence/__tests__/EvidenceLanding.test.tsx
@@ -0,0 +1,18 @@
+import * as React from 'react';
+import { shallow } from 'enzyme';
+import { MemoryRouter } from 'react-router-dom';
+
+import EvidenceLanding from '../EvidenceLanding';
+
+const { firstBy } = jest.requireActual('thenby');
+
+describe('EvidenceLanding component', () => {
+  const container = shallow(
+    <MemoryRouter>
+      <EvidenceLanding />
+    </MemoryRouter>
+  );
+  it('should render EvidenceLanding', () => {
+    expect(container.find('withRouter(EvidenceLanding)').length).toEqual(1);
+  });
+});
---
+++ services/QuillLMS/client/app/bundles/Staff/components/evidence/configureRules/ruleViewFormWrapper.tsx
@@ -0,0 +1,63 @@
+import * as React from "react";
+import { useQuery } from 'react-query';
+import { withRouter } from 'react-router-dom';
+
+import RuleViewForm from './ruleViewForm';
+
+import { RuleInterface } from '../../../interfaces/evidenceInterfaces';
+import { blankRule } from '../../../../../constants/evidence';
+import { fetchRule } from '../../../utils/evidence/ruleAPIs';
+import { Spinner } from '../../../../Shared/index';
+
+const RuleViewFormWrapper = ({ activityData, isSemantic, isUniversal, location, requestErrors, rulePromptsDisabled, ruleTypeDisabled, submitRule, match }) => {
+  const { state } = location;
+  const { params } = match;
+  const { activityId, ruleId } = params;
+
+  // cache rule data
+  const { data: ruleData } = useQuery({
+    queryKey: [`rule-${ruleId}`, ruleId],
+    queryFn: fetchRule
+  });
+
+  let rule: RuleInterface;
+
+  if(!ruleId) {
+    const ruleType = state && state.ruleType;
+    const promptIds = state && state.promptIds;
+    const ruleToBeCreated = {...blankRule};
+    if(ruleType) {
+      ruleToBeCreated.rule_type = ruleType;
+    }
+    if(promptIds) {
+      ruleToBeCreated.prompt_ids = promptIds;
+    }
+    rule = ruleToBeCreated
+  } else {
+    rule = ruleData && ruleData.rule;
+  }
+
+  if(!rule) {
+    return(
+      <div className="loading-spinner-container">
+        <Spinner />
+      </div>
+    );
+  }
+
+  return(
+    <RuleViewForm
+      activityData={activityData}
+      activityId={activityId}
+      isSemantic={isSemantic}
+      isUniversal={isUniversal}
+      requestErrors={requestErrors}
+      rule={rule}
+      rulePromptsDisabled={rulePromptsDisabled}
+      ruleTypeDisabled={ruleTypeDisabled}
+      submitRule={submitRule}
+    />
+  );
+}
+
+export default withRouter<any, any>(RuleViewFormWrapper)
