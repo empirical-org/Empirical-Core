54ad196ffc93b8c4c3be508a9962e6d7fb5785bc
cissyxyu <57366100+cissyxyu@users.noreply.github.com>
2021-08-31 14:35:00 -0400

    Add Routes/Views for admin to edit Activity Classifications/Activity Packs/Activity Pack Categories (#8166)
    * remove joining word algorithm and file
    
    * router changes and back end endpoint additions
    
    * update snapshot
    
    * emilia changes
    
    * back to regular react import

---
+++ services/QuillLMS/client/app/bundles/Teacher/containers/CmsActivitiesRouter.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import UnitTemplates from './UnitTemplates';
+import ActivityClassifications from './ActivityClassifications';
+import UnitTemplateCategories from './UnitTemplateCategories';
+
+import UnitTemplateCategory from '../components/unit_template_categories/unit_template_category';
+import UnitTemplate from '../components/unit_templates/unit_template.jsx'
+import ActivityClassification from '../components/activity_classifications/activity_classification.jsx'
+
+function CmsActivitiesRouter(props) {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route component={routerProps => <UnitTemplate {...props} {...routerProps} />} path="/cms/unit_templates/:id/edit" />
+        <Route component={routerProps => <ActivityClassification {...props} {...routerProps} />} path="/cms/activity_classifications/:id/edit" />
+        <Route component={routerProps => <UnitTemplateCategory {...props} {...routerProps} />} path="/cms/unit_template_categories/:id/edit" />
+        <Route component={UnitTemplates} path="/cms/unit_templates" />
+        <Route component={ActivityClassifications} path="/cms/activity_classifications" />
+        <Route component={UnitTemplateCategories} path="/cms/unit_template_categories" />
+      </Switch>
+    </BrowserRouter>
+  );
+};
+
+export default CmsActivitiesRouter;
