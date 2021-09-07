5f58a37ffdecc11ef9e8c92fecaae27208e64b48
Emilia Friedberg <emiliafriedberg@gmail.com>
2021-09-07 09:14:37 -0400

    copy edits for evidence (#8181)

--- services/QuillLMS/client/app/bundles/Evidence/components/activitySlides/postActivitySlide.tsx
+++ services/QuillLMS/client/app/bundles/Evidence/components/activitySlides/postActivitySlide.tsx
@@ -6,19 +6,19 @@ export const PostActivitySlide = ({ responses, user }) => {
     window.location.href = "/"
   }
 
-  function getRevisionCount() {
+  function getRevisionCopy() {
     let count = 0;
-    if(!responses) { return '0 revisions' }
+    if(!responses) { return 'You used evidence to write 3 strong sentences!' }
     Object.keys(responses).map(key => {
       count += responses[key].length;
     });
-    if(count <= 3) { return '0 revisions' }
+    if(count <= 3) { return 'You used evidence to write 3 strong sentences!' }
     // we subtract 3 to account for a student having 3 optimal first attempts
     const calculatedCount = count - 3;
     if(calculatedCount === 1) {
-      return '1 revision';
+      return 'You made 1 revision!';
     }
-    return `${calculatedCount} revisions`;
+    return `You made ${calculatedCount} revisions!`;
   }
 
   const completionText = user ? `${user}, you completed the activity!` : 'You completed the activity!'
@@ -27,7 +27,7 @@ export const PostActivitySlide = ({ responses, user }) => {
     <div className="post-activity-slide-container">
       <section id="information-section">
         <p className="slide-sub-text" id="first-sub-text">{completionText}</p>
-        <p id="revision-text">{`You made ${getRevisionCount()}!`}</p>
+        <p id="revision-text">{getRevisionCopy()}</p>
         <img alt="An illustration of a party popper" id="celebration-vector" src={`${process.env.CDN_URL}/images/comprehension/celebrating-activity-completion.svg`} />
         <section id="reminder-badge-section">
           <img alt="An illustration of an A+ that is crossed out" id="grade-badge" src={`${process.env.CDN_URL}/images/comprehension/no-grade-badge.svg`} />
