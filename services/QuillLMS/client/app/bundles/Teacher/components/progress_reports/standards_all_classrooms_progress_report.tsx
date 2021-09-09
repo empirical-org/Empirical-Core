338a02299b5792dcddb05fb0462e1ad1da75b7e8
Cissy Yu <cissy@quill.org>
2021-09-09 13:24:24 -0400

    fixed linting

--- services/QuillLMS/client/app/bundles/Teacher/components/progress_reports/standards_all_classrooms_progress_report.tsx
+++ services/QuillLMS/client/app/bundles/Teacher/components/progress_reports/standards_all_classrooms_progress_report.tsx
@@ -1,6 +1,5 @@
-import React from 'react'
+import * as React from 'react'
 import request from 'request'
-import {CSVDownload, CSVLink} from 'react-csv'
 import queryString from 'query-string';
 
 import CSVDownloadForProgressReport from './csv_download_for_progress_report.jsx'
@@ -8,7 +7,6 @@ import ReactTable from 'react-table'
 import 'react-table/react-table.css'
 import ItemDropdown from '../general_components/dropdown_selectors/item_dropdown'
 import LoadingSpinner from '../shared/loading_indicator.jsx'
-import moment from 'moment'
 import userIsPremium from '../modules/user_is_premium'
 import {sortByStandardLevel} from '../../../../modules/sortingMethods.js'
 import EmptyStateForReport from './empty_state_for_report'
@@ -16,11 +14,26 @@ import { Tooltip } from '../../../Shared/components/shared'
 
 import _ from 'underscore'
 
+interface StandardsAllClassroomsProgressReportProps {
+
+}
+
+interface StandardsAllClassroomsProgressReportState {
+  loading: boolean,
+  errors: boolean,
+  selectedClassroomId: string | string[],
+  standardsData: Array<Object>,
+  students: Array<any>,
+  updatingData: boolean,
+  classrooms: Array<any>,
+  userIsPremium: boolean
+}
+
 const showAllClassroomKey = 'All Classrooms'
 const showAllStudentsKey = 'All Students'
 
 
-export default class StandardsAllClassroomsProgressReport extends React.Component {
+export default class StandardsAllClassroomsProgressReport extends React.Component<StandardsAllClassroomsProgressReportProps, StandardsAllClassroomsProgressReportState> {
   constructor(props) {
     super(props)
     this.state = {
@@ -29,7 +42,9 @@ export default class StandardsAllClassroomsProgressReport extends React.Componen
       selectedClassroomId: queryString.parse(window.location.search).classroom_id,
       updatingData: true,
       classrooms: [],
-      userIsPremium: userIsPremium()
+      userIsPremium: userIsPremium(),
+      standardsData: null,
+      students: []
     }
   }
 
@@ -125,7 +140,7 @@ export default class StandardsAllClassroomsProgressReport extends React.Componen
     const averageFontWidth = 7
     const headerWidthNumber = 300
     const rowDisplayText = row.original['name']
-    let style: React.CSSProperties = { width: `300px`, minWidth: `300px`, textAlign: `left` as CSS.TextAlignProperty }
+    let style: React.CSSProperties = { width: `300px`, minWidth: `300px` }
     const key = `${row.id}`
     const sectionClass = 'something-class'
     const sectionText = (<a className="row-link-disguise" href={row.original['link']}>
