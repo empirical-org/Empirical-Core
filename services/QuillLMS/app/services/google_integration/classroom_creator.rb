ada7f00928ead74ff3a88dc781cb410fb1e4dd71
brendanshean <brendan@quill.org>
2021-09-10 09:31:59 -0400

    Refactor google integration namespace pt1, attempt #2 (#8237)
    * Refactor google_integration namespaces (#8218)
    
    * Fix worker name bug

---
+++ services/QuillLMS/app/services/google_integration/classroom_creator.rb
@@ -0,0 +1,59 @@
+module GoogleIntegration
+  class ClassroomCreator
+    attr_reader :data, :google_classroom_id, :teacher_id
+
+    def initialize(data)
+      @data = data
+      @google_classroom_id = data[:google_classroom_id]
+      @teacher_id = data[:teacher_id]
+    end
+
+    def run
+      classroom
+    end
+
+    private def classroom
+      ::Classroom.create!(
+        google_classroom_id: google_classroom_id,
+        name: name,
+        synced_name: synced_name,
+        teacher_id: teacher_id,
+        classrooms_teachers_attributes: [
+          {
+            user_id: teacher_id,
+            role: role
+          }
+        ]
+      )
+    end
+
+    private def name
+      data[:name].present? ? valid_name : "Classroom #{google_classroom_id}"
+    end
+
+    private def other_owned_classroom_names
+      @other_owned_classroom_names ||= teacher.classrooms_i_own.pluck(:name)
+    end
+
+    private def role
+      'owner'
+    end
+
+    private def teacher
+      ::User.find(teacher_id)
+    end
+
+    private def synced_name
+      data[:name]
+    end
+
+    private def valid_name
+      temp_name = data[:name]
+
+      loop do
+        return temp_name unless other_owned_classroom_names.include?(temp_name)
+        temp_name += '_1'
+      end
+    end
+  end
+end
---
+++ services/QuillLMS/app/services/google_integration/teacher_classroom_data_adapter.rb
@@ -0,0 +1,26 @@
+module GoogleIntegration
+  class TeacherClassroomDataAdapter
+    attr_reader :user, :data
+
+    def initialize(user, data)
+      @user = user
+      @data = data
+    end
+
+    def run
+      clean_data
+    end
+
+    private def clean_data
+      data.tap do |cleaned_data|
+        cleaned_data[:google_classroom_id] = data.fetch(:id)
+        cleaned_data[user_role_id_key] = user.id
+      end
+    end
+
+    private def user_role_id_key
+      "#{user.role}_id".to_sym
+    end
+  end
+end
+
