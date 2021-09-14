65ae963cd604930ce174fe96811e5fd703a87668
Cissy Yu <cissy@quill.org>
2021-09-14 11:13:29 -0400

    find in batches

--- services/QuillCMS/app/workers/bulk_reindex_responses_worker.rb
+++ services/QuillCMS/app/workers/bulk_reindex_responses_worker.rb
@@ -5,12 +5,13 @@ class BulkReindexResponsesWorker
   sidekiq_options retry: 5, queue: 'low'
 
   def perform(starting_index, end_index, batch_size)
-    responses = Response.find_each(start: starting_index, finish: end_index, batch_size: batch_size)
-    Response.__elasticsearch__.client.bulk(
-      index: "responses",
-      type: "response",
-      body: responses.map { |r| {index: {_id: r.id, data: r.as_indexed_json}}}
-    )
+    Response.find_in_batches(start: starting_index, finish: end_index, batch_size: batch_size) do |batch|
+      Response.__elasticsearch__.client.bulk(
+        index: "responses",
+        type: "response",
+        body: batch.map { |r| {index: {_id: r.id, data: r.as_indexed_json}}}
+      )
+    end
   end
 
   def self.cancel!(jid)
