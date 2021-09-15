a40a02bb30bcc5945f115635059632a8c0d01507
Daniel Drabik <dan@quill.org>
2021-09-15 16:25:50 -0400

    Clean up code with in_batches.

--- services/QuillLMS/app/workers/delete_obsolete_active_activity_sessions_worker.rb
+++ services/QuillLMS/app/workers/delete_obsolete_active_activity_sessions_worker.rb
@@ -1,22 +1,14 @@
 class DeleteObsoleteActiveActivitySessionsWorker
   include Sidekiq::Worker
   sidekiq_options queue: SidekiqQueue::LOW
-  BATCH_SIZE = 1_000
 
   def perform
-    loop do
-      obsolete = obsolete_query
-
-      break if obsolete.empty?
-
-      obsolete.destroy_all
-    end
+    obsolete_query.in_batches.delete_all
   end
 
   private def obsolete_query
     ActiveActivitySession
       .select(:id)
       .obsolete
-      .limit(BATCH_SIZE)
   end
 end
