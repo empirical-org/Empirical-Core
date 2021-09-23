04d4b8fa97970712a047b0e6f1a8833078a4e28d
Emilia Friedberg <emiliafriedberg@gmail.com>
2021-09-23 10:23:21 -0400

    remove extra nonsense

--- services/QuillLMS/app/controllers/api/v1/feedback_histories_controller.rb
+++ services/QuillLMS/app/controllers/api/v1/feedback_histories_controller.rb
@@ -34,14 +34,6 @@ class Api::V1::FeedbackHistoriesController < Api::ApiController
     end
   end
 
-  def store_student_report
-    feedback_session = FeedbackSession.find_by_activity_session_uid(student_report_params[:activity_session_uid])
-    feedback_history = FeedbackHistory.find_by(entry: student_report_params[:entry], feedback_session_uid: feedback_session.uid)
-    new_feedback_history_metadata = feedback_history.metadata
-    new_feedback_history_metadata['student_report'] = student_report_params[:student_report]
-    feedback_history.update(metadata: new_feedback_history_metadata)
-  end
-
   private def set_feedback_history
     @feedback_history = FeedbackHistory.find_by!(id: params[:id])
   end
