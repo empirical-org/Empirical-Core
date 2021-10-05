082f68655093855140dc583d6b100c029dbe5371
cissyxyu <57366100+cissyxyu@users.noreply.github.com>
2021-10-05 14:29:54 -0400

    Admin upload teacher and student rosters (#8313)
    * add endpoint and first test
    
    * add some front end paths (still broken)
    
    * front end added
    
    * linting and test fixes
    
    * add new line at EOF
    
    * changed everything to live in Staff, still have bug with cannot resolve module
    
    * fix so the routing works

---
+++ services/QuillLMS/client/app/bundles/Staff/startup/UploadRostersAppClient.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import UploadRosters from '../components/UploadRosters'
+
+const UploadRostersAppClient = (props) => (
+  <UploadRosters {...props} />
+);
+
+export default UploadRostersAppClient
