752b25580572f4d1e72542859ec0c7dffe74bd45
Anathomical <thomas.robertson@quill.org>
2021-10-15 11:51:33 -0400

    lms/nil-check-user-ids-in-jobs (#8397)
    * Make sure that the user_id passed in goes to an actual user record
    
    So that we don't crash and retry a job over and over when it's in a state that can't possibly complete
    
    * Update tests

--- services/QuillLMS/app/workers/premium_school_subscription_email_worker.rb
+++ services/QuillLMS/app/workers/premium_school_subscription_email_worker.rb
@@ -2,7 +2,8 @@ class PremiumSchoolSubscriptionEmailWorker
   include Sidekiq::Worker
 
   def perform(user_id)
-    @user = User.find(user_id)
+    @user = User.find_by_id(user_id)
+    return unless @user
     school = @user.school
     admin = school&.schools_admins&.first&.try(:user)
     @user.send_premium_school_subscription_email(school, admin)
