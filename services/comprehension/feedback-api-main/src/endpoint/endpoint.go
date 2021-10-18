4d07f55c08161f8cfa019faf04902302f996dfb4
Anathomical <thomas.robertson@quill.org>
2021-10-18 14:49:01 -0400

    go/reduce-api-timeout (#8406)
    * Set a 5 second client-side timeout for API calls and test behavior
    
    Now that we do serial feedback we don't want to wait forever to send the user feedback if one of the APIs is taking forever to return.
    
    * Rename global `client` to be more distinct and avoid confusion

--- services/comprehension/feedback-api-main/src/endpoint/endpoint.go
+++ services/comprehension/feedback-api-main/src/endpoint/endpoint.go
@@ -24,10 +24,11 @@ var default_api_response = APIResponse{
 }
 
 // TODO: This is a temporary replacement `http` that allows us to bypass SSL security checks
-var client = &http.Client {
+var base_client = &http.Client {
 	Transport: &http.Transport {
 		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
 	},
+	Timeout: time.Millisecond * 5000,
 }
 
 func GetFeedbackHistoryUrl() (string) {
@@ -108,7 +109,7 @@ func Endpoint(context *gin.Context) {
 	var returnable_result APIResponse
 
 	for index, url := range urls {
-		results[index] = getAPIResponse(url, index, request_body)
+		results[index] = getAPIResponse(url, index, request_body, base_client)
 		return_index, finished := processResults(results, len(urls))
 
 		if finished {
@@ -134,7 +135,7 @@ func Endpoint(context *gin.Context) {
 		return
 	}
 
-	recordFeedback(request_object, returnable_result, GetFeedbackHistoryUrl())
+	recordFeedback(request_object, returnable_result, GetFeedbackHistoryUrl(), base_client)
 
 	context.Header("Access-Control-Allow-Origin", "*")
 	context.Header("Content-Type", "application/json")
@@ -159,7 +160,7 @@ func processResults(results map[int]InternalAPIResponse, length int) (int, bool)
 	return automl_index, all_correct
 }
 
-func getAPIResponse(url string, priority int, json_params [] byte) InternalAPIResponse {
+func getAPIResponse(url string, priority int, json_params [] byte, client *http.Client) InternalAPIResponse {
 
 	// response_json, err := http.Post(url, "application/json", bytes.NewReader(json_params))
 
@@ -222,7 +223,7 @@ func buildFeedbackHistory(request_object APIRequest, feedback APIResponse, used 
 	}
 }
 
-func recordFeedback(incoming_params APIRequest, feedback APIResponse, feedback_history_url string) {
+func recordFeedback(incoming_params APIRequest, feedback APIResponse, feedback_history_url string, client *http.Client) {
 
 
 	history := buildFeedbackHistory(incoming_params, feedback, true, time.Now())
