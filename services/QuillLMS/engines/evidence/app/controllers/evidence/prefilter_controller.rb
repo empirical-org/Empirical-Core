350550f87e048bfbbeb2072dfe45978cd22453b5
Peter Kong <pkong@quill.org>
2021-10-29 08:26:39 -0600

    removing sketch code

--- services/QuillLMS/engines/evidence/app/controllers/evidence/prefilter_controller.rb
+++
@@ -1,9 +0,0 @@
-module Evidence
-  class OpinionController < ApiController
-
-    def fetch
-      oapi_client = Opinion::Client.new(entry: params['entry'], prompt_text: params['prompt_text'])
-      render json: Opinion::FeedbackAssembler.run(oapi_client.post)
-    end
-  end
-end
