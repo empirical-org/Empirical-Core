de76f36b60e2139a3888057f007f35905c8ade47
Peter Kong <pkong@quill.org>
2021-11-10 12:21:35 -0700

    removing comments

--- services/QuillLMS/client/app/bundles/Evidence/components/studentView/activityFollowUp.tsx
+++ services/QuillLMS/client/app/bundles/Evidence/components/studentView/activityFollowUp.tsx
@@ -5,7 +5,6 @@ import ThankYouSlide from '../activitySlides/thankYouSlide'
 import ActivitySurvey from '../activitySlides/activitySurvey'
 
 const ActivityFollowUp = ({ responses, user, sessionID, saveActivitySurveyResponse, }) => {
-  console.log("in ActivityFollowUp")
   const [showActivitySurvey, setShowActivitySurvey] = React.useState(false)
   const [submittedActivitySurvey, setSubmittedActivitySurvey] = React.useState(false)
 
@@ -14,7 +13,6 @@ const ActivityFollowUp = ({ responses, user, sessionID, saveActivitySurveyRespon
   if (submittedActivitySurvey) {
     return <ThankYouSlide />
   }
-  console.log("HERE 1")
   if (showActivitySurvey) {
     return (<ActivitySurvey
       saveActivitySurveyResponse={saveActivitySurveyResponse}
@@ -22,7 +20,6 @@ const ActivityFollowUp = ({ responses, user, sessionID, saveActivitySurveyRespon
       setSubmittedActivitySurvey={setSubmittedActivitySurvey}
     />)
   }
-  console.log("HERE 2")
   return <PostActivitySlide handleClick={onClickNext} responses={responses} user={user} />
 }
 
