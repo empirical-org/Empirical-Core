b51f85eef732245fe8552601d67e88014a78a09c
Brendan Shean <brendanshean@gmail.com>
2021-11-11 15:10:55 -0500

    Revert "Add check in clever importing for non-unique & invalid classroom_teachers (#8487)"
    This reverts commit 83a5235e4bfc3c1fe80607704b9da9c5a1d4c236.

--- services/QuillLMS/app/services/clever_integration/associators/classrooms_to_teacher.rb
+++ services/QuillLMS/app/services/clever_integration/associators/classrooms_to_teacher.rb
@@ -1,22 +1,11 @@
 module CleverIntegration::Associators::ClassroomsToTeacher
 
   def self.run(classrooms, teacher)
-    classrooms.map do |classroom|
-      role = get_role(classroom, teacher)
-      ClassroomsTeacher.find_or_create_by!(classroom: classroom, user: teacher, role: role)
-    rescue ActiveRecord::RecordNotUnique, ActiveRecord::RecordInvalid => e
-      NewRelic::Agent.add_custom_attributes(classroom_id: classroom.id, teacher_id: teacher.id, role: role)
-      NewRelic::Agent.notice_error(e)
-    ensure
+    updated_classrooms = classrooms.map do |classroom|
+      role = classroom&.owner && classroom&.owner != teacher ? 'coteacher' : 'owner'
+      ClassroomsTeacher.find_or_create_by(classroom: classroom, user: teacher, role: role)
       classroom.reload
     end
-  end
-
-  def self.get_role(classroom, teacher)
-    owner_ids = ClassroomsTeacher.where(classroom: classroom, role: 'owner').pluck(:user_id)
-
-    return 'owner' if owner_ids.empty?
-
-    owner_ids.include?(teacher.id) ? 'owner' : 'coteacher'
+    updated_classrooms
   end
 end
