9b5d700dae207dd5370eba36abdb59f9f9e275ab
brendanshean <brendan@quill.org>
2021-11-16 08:21:37 -0500

    Fix issue with clever classrooms missing teachers (#8505)

--- services/QuillLMS/app/services/google_integration/classroom_creator.rb
+++ services/QuillLMS/app/services/google_integration/classroom_creator.rb
@@ -1,5 +1,7 @@
 module GoogleIntegration
   class ClassroomCreator
+    OWNER = ClassroomsTeacher::ROLE_TYPES[:owner].freeze
+
     attr_reader :data, :google_classroom_id, :teacher_id
 
     def initialize(data)
@@ -36,7 +38,7 @@ module GoogleIntegration
     end
 
     private def role
-      'owner'
+      OWNER
     end
 
     private def teacher
--- services/QuillLMS/spec/services/clever_integration/importers/classrooms_spec.rb
+++ services/QuillLMS/spec/services/clever_integration/importers/classrooms_spec.rb
@@ -1,39 +1,20 @@
 require 'rails_helper'
 
-describe 'CleverIntegration::Importers::Classrooms' do
-
-  let!(:teacher) {
-    create(:teacher, :signed_up_with_clever)
-  }
-
+describe CleverIntegration::Importers::Classrooms do
+  let!(:teacher) { create(:teacher, :signed_up_with_clever) }
   let!(:district_token) { '1' }
 
-  let!(:sections_response) {
-    section = Clever::Section.new({
-      id: teacher.clever_id,
-      name: 'section1',
-      grade: '2'
-    })
-    section_response = Clever::SectionResponse.new({ data: section })
-    Clever::SectionsResponse.new({
-      data: [section_response]
-     })
-  }
+  let!(:section) { Clever::Section.new(id: teacher.clever_id, name: 'section1', grade: '2') }
+  let!(:section_response) { Clever::SectionResponse.new(data: section) }
+  let!(:sections_response) { Clever::SectionsResponse.new(data: [section_response]) }
 
-  before do
-    allow_any_instance_of(Clever::DataApi).to receive(:get_sections_for_teacher).and_return(sections_response)
-  end
+  before { allow_any_instance_of(Clever::DataApi).to receive(:get_sections_for_teacher).and_return(sections_response) }
 
   def subject
     CleverIntegration::Importers::Classrooms.run(teacher, district_token)
     Classroom.find_by(clever_id: teacher.clever_id, name: 'section1', grade: '2')
   end
 
-  it 'creates a classroom' do
-    expect(subject).to_not be_nil
-  end
-
-  it 'associates classroom to teacher' do
-    expect(subject.owner).to eq(teacher)
-  end
+  it { expect(subject).to_not be_nil }
+  it { expect(subject.owner).to eq(teacher) }
 end
