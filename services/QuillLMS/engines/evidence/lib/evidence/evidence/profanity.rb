16e35a964724719d5beff6918296fe0735a57b57
dandrabik <dandrabik@users.noreply.github.com>
2021-11-16 10:58:12 -0500

    Profanity Prefilter Optimization (#8511)
    * Optimizations for the profanity check.
    
    * Add some comments and more tests.

--- services/QuillLMS/engines/evidence/lib/evidence/evidence/profanity.rb
+++ services/QuillLMS/engines/evidence/lib/evidence/evidence/profanity.rb
@@ -1,13 +1,30 @@
-module Evidence 
-  class Profanity 
-    def self.profane?(word)
+module Evidence
+  class Profanity
+
+    def self.profane?(entry)
+      # find the badword substrings that exist in the entry
+      found_bad_words = BadWords::ALL.select do |word|
+        entry.downcase.include?(word.gsub('*',''))
+      end
+
+      return false if found_bad_words.empty?
+
+      # do a more rigorous word-by-word check for found bad words
+      entry.split(' ').any? { |word| profane_word_check?(word, found_bad_words)}
+    end
+
+    # keeping this for now for comparison and benchmarking
+    def self.profane_legacy?(entry)
+      entry.split(' ').any? { |word| profane_word_check?(word)}
+    end
+
+    def self.profane_word_check?(word, bad_words = BadWords::ALL)
       return false unless word.is_a?(String) && word.length > 1
       word = word.downcase.gsub(/[.!?]/, '')
 
-      a_match = BadWords::ALL.find do |badword|
+      a_match = bad_words.any? do |badword|
         match?(badword: badword, word: word)
       end
-      a_match ? true : false
     end
 
     def self.match?(badword:, word:)
@@ -17,12 +34,12 @@ module Evidence
          word.match?(regex)
       elsif badword.start_with?('*')
         regex = ::Regexp.new("#{stripped_badword}$")
-         word.match?(regex) 
+         word.match?(regex)
       elsif badword.end_with?('*')
         regex = ::Regexp.new("^#{stripped_badword}")
-         word.match?(regex) 
-      else 
-         stripped_badword == word 
+         word.match?(regex)
+      else
+         stripped_badword == word
       end
     end
 
