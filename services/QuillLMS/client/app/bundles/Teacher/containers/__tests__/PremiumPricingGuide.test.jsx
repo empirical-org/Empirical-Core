8f8467403967ef8b7e24d5999a9082a3c16b3777
cissyxyu <57366100+cissyxyu@users.noreply.github.com>
2021-11-17 15:02:37 -0500

    Add premium banner to premium page (#8506)
    * front end changes
    
    * dont show it for non signed in users and fix the premium redirect after login bug
    
    * premium
    
    * chagne upgrade now to conditionally bring up payments
    
    * linting
    
    * update snapshot
    
    * changes from comments
    
    * sort props alphabetically
    
    * try fixing premium banner
    
    * try to fix premium confirmation
    
    * linting

--- services/QuillLMS/client/app/bundles/Teacher/containers/__tests__/PremiumPricingGuide.test.jsx
+++ services/QuillLMS/client/app/bundles/Teacher/containers/__tests__/PremiumPricingGuide.test.jsx
@@ -4,6 +4,12 @@ import { shallow } from 'enzyme';
 import PremiumPricingGuide from '../PremiumPricingGuide.jsx';
 
 describe('PremiumPricingGuide container', () => {
+  beforeEach(() => {
+    const div = document.createElement('div');
+    div.setAttribute('id', 'current-user-id');
+    div.setAttribute('content', false);
+    document.body.appendChild(div);
+  });
 
   it('should render', () => {
     expect(shallow(<PremiumPricingGuide diagnosticActivityCount={9} independentPracticeActivityCount={400} lessonsActivityCount={50} />)).toMatchSnapshot();
