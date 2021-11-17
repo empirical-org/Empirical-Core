be6999677ad51683721a1ca4ac4cfa8ca121cfd2
Cissy Yu <cissy@quill.org>
2021-11-17 14:21:47 -0500

    all styling changes in place

--- services/QuillLMS/client/app/bundles/Teacher/components/dashboard/demo_modal.tsx
+++ services/QuillLMS/client/app/bundles/Teacher/components/dashboard/demo_modal.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react'
 
-const classroomActivitiesSrc = `${process.env.CDN_URL}/images/pages/dashboard/illustrations-classroom-activities.svg`
 const exploreDemoLink = `${process.env.DEFAULT_URL}/teachers/view_demo`
 
 function goToDemo() {
@@ -12,16 +11,15 @@ const DemoModal = ({ close, size }) => {
 
   return (<div className="modal-container welcome-modal-container">
     <div className="modal-background" />
-    <div className="quill-modal">
-      <img alt="exit-popup" className="exit" onClick={close} src="https://assets.quill.org/images/icons/CloseIcon.svg" />
+    <div className="quill-modal explore-demo-modal">
+      <img alt="close-modal" className="pull-right modal-button-close close-demo-modal" onClick={close} src={`${process.env.CDN_URL}/images/shared/close_x.svg`} />
       <div className="modal-body">
-        <h1>Explore Demo!</h1>
-        <div className="modal-text">
+        <h1>Explore Demo</h1>
+        <div className="explore-demo-modal-text">
           <p>You are about to enter a Demo Account. All data is for demo purposes only - you can exit the Demo and re-enter your own account at any time.</p>
         </div>
         <button className="quill-button contained primary medium" onClick={goToDemo} type="button">Let&apos;s go!</button>
       </div>
-      <img alt="Teacher at projector in classroom" src={classroomActivitiesSrc} />
     </div>
   </div>)
 }
