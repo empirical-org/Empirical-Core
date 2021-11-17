d13872f935cd292a39e175c98225f5cde09806df
dandrabik <dandrabik@users.noreply.github.com>
2021-11-17 16:36:33 -0500

    Track user_id in NR reports. (#8523)
    * Track user_id in NR reports.
    
    * Fix lint.

---
+++ services/QuillLMS/app/controllers/concerns/new_relic_attributable.rb
@@ -0,0 +1,13 @@
+module NewRelicAttributable
+  extend ActiveSupport::Concern
+
+  included do
+    before_action :set_new_relic_attributes
+  end
+
+  private def set_new_relic_attributes
+    ::NewRelic::Agent.add_custom_attributes({
+      user_id: current_user.try(:id)
+    })
+  end
+end
