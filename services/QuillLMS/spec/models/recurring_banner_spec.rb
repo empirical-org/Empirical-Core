c3214775eb031d421fb650c15c69be18efc47634
Cissy Yu <cissy@quill.org>
2021-11-18 11:34:16 -0500

    remove recurring banners

--- services/QuillLMS/spec/models/recurring_banner_spec.rb
+++ services/QuillLMS/spec/models/recurring_banner_spec.rb
@@ -33,11 +33,11 @@ describe RecurringBanner, type: :model do
     expect(banner.title).to eq(nil)
   end
 
-  it "does return true for show? when the key does have an associated webinar" do
-    time =  DateTime.new(2021,9,2,11,1,0)
-    banner = RecurringBanner.new(time)
-    expect(banner.show?(true)).to eq(true)
-  end
+  # it "does return true for show? when the key does have an associated webinar" do
+  #   time =  DateTime.new(2021,9,2,11,1,0)
+  #   banner = RecurringBanner.new(time)
+  #   expect(banner.show?(true)).to eq(true)
+  # end
 
   it "does not return true for show? when the key falls on a skipped day" do
     time =  DateTime.new(2021,1,18,16,1,0)
@@ -45,11 +45,11 @@ describe RecurringBanner, type: :model do
     expect(banner.show?(true)).to eq(false)
   end
 
-  it "does return correct link and title when the key does have an associated recurring webinar" do
-    time =  DateTime.new(2021,9,2,11,1,0)
-    banner = RecurringBanner.new(time)
-    expect(banner.title).to eq("<strong>Quill Webinar 101: Getting Started</strong> is live now!")
-    expect(banner.link).to eq("https://quill-org.zoom.us/webinar/register/WN_vA0O4ltWSJKMLqghSm4otw")
-  end
+  # it "does return correct link and title when the key does have an associated recurring webinar" do
+  #   time =  DateTime.new(2021,9,2,11,1,0)
+  #   banner = RecurringBanner.new(time)
+  #   expect(banner.title).to eq("<strong>Quill Webinar 101: Getting Started</strong> is live now!")
+  #   expect(banner.link).to eq("https://quill-org.zoom.us/webinar/register/WN_vA0O4ltWSJKMLqghSm4otw")
+  # end
 
 end
