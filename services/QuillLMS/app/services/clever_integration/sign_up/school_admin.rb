aa3647c3b3d6fb52c6716780d2f2bea8ccfd12e6
brendanshean <brendan@quill.org>
2021-11-22 08:16:53 -0500

    Revert log clever import libary (#8536)
    * Revert "Temporarily log clever importing for school admin (#8531)"
    
    This reverts commit 66b496d31a9e590e36063ca66de2199c1225c44e.
    
    * Revert "Temporarily log clever importing (#8522)"
    
    This reverts commit 2bf4487c3543a5937e80b1a95ed30d50859b3be8.

--- services/QuillLMS/app/services/clever_integration/sign_up/school_admin.rb
+++ services/QuillLMS/app/services/clever_integration/sign_up/school_admin.rb
@@ -12,12 +12,10 @@ module CleverIntegration::SignUp::SchoolAdmin
   end
 
   def self.library_integration(auth_hash)
-    log_import(:district_integration, auth_hash) # TODO: remove this temporary call
     CleverIntegration::Importers::Library.run(auth_hash)
   end
 
   def self.district_integration(auth_hash, district)
-    log_import(:library_integration, auth_hash) # TODO: remove this temporary call
     user = create_user(auth_hash)
     if user.present?
       associate_user_to_district(user, district)
@@ -26,18 +24,6 @@ module CleverIntegration::SignUp::SchoolAdmin
     else
       {type: 'user_failure', data: "No User Present"}
     end
-  end
-
-  #TODO: remove this method
-  def self.log_import(action, auth_hash)
-    return if Rails.env.test?
-
-    user = User.find(ENV['CLEVER_IMPORT_LOG_USER_ID'])
-    changed_attr = auth_hash.dig(:info, :user_type)
-
-    return if ChangeLog.exists?(changed_record: user, changed_attribute: changed_attr, action: action, user: user)
-
-    ChangeLog.create(explanation: auth_hash.to_json, changed_record: user, changed_attribute: changed_attr, action: action, user: user)
   end
 
   def self.parse_data(auth_hash)
