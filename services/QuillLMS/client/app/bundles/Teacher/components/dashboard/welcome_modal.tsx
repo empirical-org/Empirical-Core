85d146f2b14dce7a53c26e19b79c939a11c8a38c
Cissy Yu <cissy@quill.org>
2021-11-30 16:42:20 -0500

    last stylechanges before draft shown to tom and peter g

--- services/QuillLMS/client/app/bundles/Teacher/components/dashboard/welcome_modal.tsx
+++ services/QuillLMS/client/app/bundles/Teacher/components/dashboard/welcome_modal.tsx
@@ -3,6 +3,12 @@ import Confetti from 'react-confetti'
 
 const classroomActivitiesSrc = `${process.env.CDN_URL}/images/pages/dashboard/illustrations-classroom-activities.svg`
 
+const exploreDemoLink = `${process.env.DEFAULT_URL}/teachers/view_demo`
+
+function goToDemo() {
+  window.location.href = exploreDemoLink
+}
+
 const WelcomeModal = ({ close, size }) => {
   const { width, height } = size;
 
@@ -20,9 +26,21 @@ const WelcomeModal = ({ close, size }) => {
         <div className="welcome-modal-text">
           <p>Our mission as a non-profit is to help students become strong writers, so all our content is completely free to use with an unlimited number of students.</p>
         </div>
-        <button className="quill-button contained primary medium" onClick={close} type="button">Let&apos;s go!</button>
+        <div>
+        <div className="welcome-modal-demo welcome-modal-card">
+          <img alt="Teacher at projector in classroom" src="https://assets.quill.org/images/pages/dashboard/illustrations-classroom-activities.svg" />
+          <h4>Try a Demo</h4>
+          <p>Play around with a fully loaded demo to see sample student data and reports.</p>
+          <button className="quill-button contained primary medium" onClick={goToDemo}>Start exploring</button>
+        </div>
+        <div className="welcome-modal-card welcome-modal-go">
+          <img alt="Teacher at projector in classroom" src="https://assets.quill.org/images/pages/dashboard/illustrations-classroom-activities.svg" />
+          <h4>Start setting up</h4>
+          <p>Ready to set up your own classes? Jump right into your account.</p>
+          <button className="quill-button contained primary medium" onClick={close} type="button">Let&apos;s go!</button>
+        </div>
+        </div>
       </div>
-      <img alt="Teacher at projector in classroom" src={classroomActivitiesSrc} />
     </div>
   </div>)
 }
