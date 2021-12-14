7308b8ee450ff46931a4a1bac4fee242cf7940cb
Emilia Friedberg <emiliafriedberg@gmail.com>
2021-12-14 09:45:59 -0500

    try again with the tests

--- services/QuillLMS/engines/evidence/spec/models/evidence/activity_spec.rb
+++ services/QuillLMS/engines/evidence/spec/models/evidence/activity_spec.rb
@@ -95,7 +95,7 @@ module Evidence
 
     context '#flag' do
       it 'should return the parent activity flag' do
-        parent_activity = ::Activity.create(:name => "test name", :flags => ['alpha'])
+        parent_activity = ::Activity.create(:name => "test name", :flag => 'alpha')
         activity = create(:evidence_activity, :parent_activity_id => parent_activity.id)
         expect(activity.flag).to be(parent_activity.flag)
       end
@@ -103,7 +103,7 @@ module Evidence
 
     context '#flag=' do
       it 'should update the parent activity flag' do
-        parent_activity = ::Activity.create(:name => "test name", :flags => ['alpha'])
+        parent_activity = ::Activity.create(:name => "test name", :flag => 'alpha')
         activity = create(:evidence_activity, :parent_activity_id => parent_activity.id)
         activity.update(flag: 'beta')
         expect(parent_activity.flag).to be('beta')
