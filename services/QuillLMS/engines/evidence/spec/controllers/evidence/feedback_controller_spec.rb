86f8ef4b63a8851d5d1d4289972c83b2d6899dc7
Emilia Friedberg <emiliafriedberg@gmail.com>
2022-01-05 14:26:47 -0500

    add spec to make sure it will also match the second feedback

--- services/QuillLMS/engines/evidence/spec/controllers/evidence/feedback_controller_spec.rb
+++ services/QuillLMS/engines/evidence/spec/controllers/evidence/feedback_controller_spec.rb
@@ -10,28 +10,30 @@ module Evidence
       create(:evidence_regex_rule, :regex_text => "^test", :rule => (rule_regex))
       create(:evidence_prompts_rule, :rule => (rule), :prompt => (prompt))
       create(:evidence_prompts_rule, :rule => (rule_regex), :prompt => (prompt))
-      create(:evidence_plagiarism_text, :text => (plagiarized_text), :rule => (rule))
+      create(:evidence_plagiarism_text, :text => (plagiarized_text1), :rule => (rule))
+      create(:evidence_plagiarism_text, :text => (plagiarized_text2), :rule => (rule))
     end
-    
+
     let!(:prompt) { create(:evidence_prompt) }
     let!(:rule) { create(:evidence_rule, :rule_type => "plagiarism") }
     let!(:rule_regex) { create(:evidence_rule, :rule_type => "rules-based-1") }
-    let(:plagiarized_text) { "do not plagiarize this text please there will be consequences" }
+    let(:plagiarized_text1) { "do not plagiarize this text please there will be consequences" }
+    let(:plagiarized_text2) { "this is completely different text that you also should not plagiarize or else" }
     let!(:first_feedback) { create(:evidence_feedback, :text => "here is our first feedback", :rule => (rule), :order => 0) }
     let!(:second_feedback) { create(:evidence_feedback, :text => "here is our second feedback", :rule => (rule), :order => 1) }
-    
-    describe '#prefilter' do 
-      let!(:profanity_rule) do 
-        create(:evidence_rule, rule_type: 'prefilter', uid: 'fdee458a-f017-4f9a-a7d4-a72d1143abeb')
-      end 
 
-      context 'profanity violation' do 
-        it 'should return correct rule uid' do 
-          post "prefilter", :params => ({ 
-            :entry => "nero was an ahole", 
-            :prompt_id => prompt.id, 
-            :session_id => 1, 
-            :previous_feedback => ([]) 
+    describe '#prefilter' do
+      let!(:profanity_rule) do
+        create(:evidence_rule, rule_type: 'prefilter', uid: 'fdee458a-f017-4f9a-a7d4-a72d1143abeb')
+      end
+
+      context 'profanity violation' do
+        it 'should return correct rule uid' do
+          post "prefilter", :params => ({
+            :entry => "nero was an ahole",
+            :prompt_id => prompt.id,
+            :session_id => 1,
+            :previous_feedback => ([])
           }), :as => :json
           expect(response.status).to eq 200
           parsed_response = JSON.parse(response.body)
@@ -40,13 +42,13 @@ module Evidence
         end
       end
 
-      context 'no violation' do 
-        it 'should return successfully' do 
-          post "prefilter", :params => ({ 
-            :entry => "the quick brown fox", 
-            :prompt_id => prompt.id, 
-            :session_id => 1, 
-            :previous_feedback => ([]) 
+      context 'no violation' do
+        it 'should return successfully' do
+          post "prefilter", :params => ({
+            :entry => "the quick brown fox",
+            :prompt_id => prompt.id,
+            :session_id => 1,
+            :previous_feedback => ([])
           }), :as => :json
           expect(response.status).to eq 200
           parsed_response = JSON.parse(response.body)
@@ -68,15 +70,29 @@ module Evidence
         expect(404).to(eq(response.status))
       end
 
-      it 'should return successfully when there is plagiarism' do
-        post("plagiarism", :params => ({ :entry => ("bla bla bla #{plagiarized_text}"), :prompt_id => prompt.id, :session_id => 1, :previous_feedback => ([]) }), :as => :json)
+      it 'should return successfully when there is plagiarism that matches the first plagiarism text string' do
+        post("plagiarism", :params => ({ :entry => ("bla bla bla #{plagiarized_text1}"), :prompt_id => prompt.id, :session_id => 1, :previous_feedback => ([]) }), :as => :json)
         parsed_response = JSON.parse(response.body)
         expect(false).to(eq(parsed_response["optimal"]))
-        expect(plagiarized_text).to(eq(parsed_response["highlight"][0]["text"]))
-        expect(plagiarized_text).to(eq(parsed_response["highlight"][1]["text"]))
+        expect(plagiarized_text1).to(eq(parsed_response["highlight"][0]["text"]))
+        expect(plagiarized_text1).to(eq(parsed_response["highlight"][1]["text"]))
         expect(first_feedback.text).to(eq(parsed_response["feedback"]))
         request.env.delete("RAW_POST_DATA")
-        post("plagiarism", :params => ({ :entry => ("bla bla bla #{plagiarized_text}"), :prompt_id => prompt.id, :session_id => 5, :previous_feedback => ([parsed_response]) }), :as => :json)
+        post("plagiarism", :params => ({ :entry => ("bla bla bla #{plagiarized_text1}"), :prompt_id => prompt.id, :session_id => 5, :previous_feedback => ([parsed_response]) }), :as => :json)
+        parsed_response = JSON.parse(response.body)
+        expect(false).to(eq(parsed_response["optimal"]))
+        expect(second_feedback.text).to(eq(parsed_response["feedback"]))
+      end
+
+      it 'should return successfully when there is plagiarism that matches the second plagiarism text string' do
+        post("plagiarism", :params => ({ :entry => ("bla bla bla #{plagiarized_text2}"), :prompt_id => prompt.id, :session_id => 1, :previous_feedback => ([]) }), :as => :json)
+        parsed_response = JSON.parse(response.body)
+        expect(false).to(eq(parsed_response["optimal"]))
+        expect(plagiarized_text2).to(eq(parsed_response["highlight"][0]["text"]))
+        expect(plagiarized_text2).to(eq(parsed_response["highlight"][1]["text"]))
+        expect(first_feedback.text).to(eq(parsed_response["feedback"]))
+        request.env.delete("RAW_POST_DATA")
+        post("plagiarism", :params => ({ :entry => ("bla bla bla #{plagiarized_text2}"), :prompt_id => prompt.id, :session_id => 5, :previous_feedback => ([parsed_response]) }), :as => :json)
         parsed_response = JSON.parse(response.body)
         expect(false).to(eq(parsed_response["optimal"]))
         expect(second_feedback.text).to(eq(parsed_response["feedback"]))
