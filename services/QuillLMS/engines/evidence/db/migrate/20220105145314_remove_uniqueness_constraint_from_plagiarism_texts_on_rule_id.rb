4d7db5e989d9893967b3e6cecad657f58485690c
Emilia Friedberg <emiliafriedberg@gmail.com>
2022-01-05 09:57:56 -0500

    remove uniqueness constraint so it is possible to add multiple strings

---
+++ services/QuillLMS/engines/evidence/db/migrate/20220105145314_remove_uniqueness_constraint_from_plagiarism_texts_on_rule_id.rb
@@ -0,0 +1,6 @@
+class RemoveUniquenessConstraintFromPlagiarismTextsOnRuleId < ActiveRecord::Migration[5.1]
+  def change
+    remove_index :comprehension_plagiarism_texts, :rule_id
+    add_index :comprehension_plagiarism_texts, :rule_id
+  end
+end
