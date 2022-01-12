01d8d6669d75cc8ba1cb91ade5aff28a7de98d1e
Daniel Drabik <dan.drabik@gmail.com>
2022-01-12 11:53:26 -0500

    Update comments

--- services/QuillLMS/app/models/concerns/user_cacheable.rb
+++ services/QuillLMS/app/models/concerns/user_cacheable.rb
@@ -1,12 +1,16 @@
 # frozen_string_literal: true
 
 # This is a Mixin for the User class to cache objects for a user
-# Used to define common cache_keys in the app and force a common pattern
-# To add caching to a controller, use one of the public methods here. They have been tested
-# for proper cache expiration (using `touch` and updated_at changes)
+# Also defines common cache_keys in the app and forces a common pattern
+# To add caching to a controller, use one of the public methods here.
+# They have been tested for proper cache expiration
+# (using `touch` and `updated_at` changes)
+
+# Note, this is in the User since for nil objects we'd want to avoid sharing caches between users
+# So, defaulting to the user object as a cache key to avoid that.
 
 # Sample Controller Usage
-# response = user.classroom_unit_cache(classroom_unit: classroom_unit, key: 'controller.method') do
+# response = user.classroom_unit_cache(classroom_unit, key: 'controller.method') do
 #   # some expensive action to cache that only needs to update
 #   # when a classroom_unit or its children change
 # end
@@ -32,13 +36,14 @@ module UserCacheable
     model_cache(classroom_unit, key: key, groups: groups) { yield }
   end
 
+  # note: object for caching defaults to the 'user'
+  # This avoids nil cache keys shared by different users
   private def model_cache_key(object, key:, groups:)
     group_array = groups.to_a.sort_by(&:first).flatten
 
     [key, *group_array, object || self]
   end
 
-  # note: object for caching defaults to the user to avoid nil cache keys shared by different people
   private def model_cache(object, key:, groups:)
     Rails.cache.fetch(model_cache_key(object, key: key, groups: groups)) do
       yield
@@ -59,7 +64,7 @@ module UserCacheable
       return ClassroomUnit.find_by(unit_id: unit_id, classroom_id: classroom_id)
     end
 
-    # use maximium updated_at of these classroom_units
+    # use ClassroomUnit with the maximium `updated_at`
     ClassroomUnit
       .select("classroom_units.*, MAX(classroom_units.updated_at)")
       .unscoped
