43979e96ece4fb4bbbb5f6d13668a9c99b555d61
Emilia Friedberg <emiliafriedberg@gmail.com>
2022-01-20 09:16:39 -0500

    have apis just use quill authentication (#8695)
    * have apis just use quill authentication
    
    * remove doorkeeper_token

--- services/QuillLMS/app/controllers/api/v1/progress_reports_controller.rb
+++ services/QuillLMS/app/controllers/api/v1/progress_reports_controller.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 class Api::V1::ProgressReportsController < Api::ApiController
-  include QuillAuthentication
   before_action :authorize!, only: :student_overview_data
 
   def activities_scores_by_classroom_data
