0ba6a453d23e4a7534a8017b75d3d66ca8953945
Brendan Shean <brendanshean@gmail.com>
2022-01-24 10:16:14 -0500

    Fix Style/StringLiteralsInInterpolation violations

--- services/QuillLMS/script/map_concepts_to_activities_across_apps.rb
+++ services/QuillLMS/script/map_concepts_to_activities_across_apps.rb
@@ -129,8 +129,8 @@ concepts.each do |c|
     index = organized_concepts.find_index(existing_oc)
     organized_concepts[index] = new_oc
   else
-    grandparent_name = c["grandparent_name"] ?  "#{c["grandparent_name"]} | " : ''
-    parent_name = c["parent_name"] ?  "#{c["parent_name"]} | " : ''
+    grandparent_name = c["grandparent_name"] ?  "#{c['grandparent_name']} | " : ''
+    parent_name = c["parent_name"] ?  "#{c['parent_name']} | " : ''
     new_oc = {}
     new_oc["grades_connect_activities"] = [],
     new_oc["grades_grammar_activities"] = [],
