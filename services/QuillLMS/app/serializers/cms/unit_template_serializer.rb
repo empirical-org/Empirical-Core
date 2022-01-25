70ab1f5bd51d5d8a624f1e9968a3c0d787673e05
Cissy Yu <cissy@quill.org>
2022-01-25 12:16:26 -0500

    try to get serializer working

--- services/QuillLMS/app/serializers/cms/unit_template_serializer.rb
+++ services/QuillLMS/app/serializers/cms/unit_template_serializer.rb
@@ -2,5 +2,5 @@
 
 class Cms::UnitTemplateSerializer < ActiveModel::Serializer
   attributes :id, :name, :author_id, :time, :unit_template_category_id, :grades, :flag, :order_number, :activity_info, :image_link, :readability, :diagnostic_names
-  has_many :activities
+  has_many :activities, serializer: ActivitySerializer
 end
