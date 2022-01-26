3a3f27bc3bf59f21deb0d6dabe5e72b32f69329d
Brendan Shean <brendanshean@gmail.com>
2022-01-26 09:04:26 -0500

    Rename error_404 and error_500 partials

--- services/QuillLMS/app/controllers/errors_controller.rb
+++ services/QuillLMS/app/controllers/errors_controller.rb
@@ -2,11 +2,11 @@
 
 class ErrorsController < ApplicationController
   def error404
-    render 'error_404', :status => 404
+    render 'error404', status: 404
   end
 
   def error500
     ErrorWorker.perform_async
-    render 'error_500', :status => 500
+    render 'error500', status: 500
   end
 end
