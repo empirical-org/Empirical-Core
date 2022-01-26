07075ca7d176cab0be196e3d99f91b2b5be5706d
Brendan Shean <brendanshean@gmail.com>
2022-01-26 14:00:57 -0500

    Fix bug where repurposed old clever classrooms are not showing up on new teacher page

--- services/QuillLMS/app/services/clever_integration/teacher_imported_classrooms_updater.rb
+++ services/QuillLMS/app/services/clever_integration/teacher_imported_classrooms_updater.rb
@@ -4,11 +4,15 @@ module CleverIntegration
   class TeacherImportedClassroomsUpdater < ApplicationService
     attr_reader :teacher_id
 
+    COTEACHER = ClassroomsTeacher::ROLE_TYPES[:coteacher]
+    OWNER = ClassroomsTeacher::ROLE_TYPES[:owner]
+
     def initialize(teacher_id)
       @teacher_id = teacher_id
     end
 
     def run
+      update_classrooms_teachers
       update_classrooms
       update_classrooms_students
     end
@@ -25,6 +29,10 @@ module CleverIntegration
       @classrooms_data ||= TeacherClassroomsData.new(teacher, serialized_classrooms_data)
     end
 
+    private def existing_classrooms_where_teacher_was_added_in_clever
+      ::Classroom.where(clever_id: classroom_clever_ids - imported_classrooms.pluck(:clever_id))
+    end
+
     private def imported_classrooms
       teacher.clever_classrooms.where(clever_id: classroom_clever_ids)
     end
@@ -38,11 +46,20 @@ module CleverIntegration
     end
 
     private def update_classrooms
-     imported_classrooms.each { |classroom| ClassroomUpdater.run(classroom, classroom_data(classroom)) }
+     imported_classrooms.each { |classroom| ClassroomUpdater.run(classroom, classroom_data(classroom) )}
     end
 
     private def update_classrooms_students
       ImportClassroomStudentsWorker.perform_async(teacher_id, imported_classrooms.map(&:id))
+    end
+
+    private def update_classrooms_teachers
+      existing_classrooms_where_teacher_was_added_in_clever.each do |classroom|
+        teacher.classrooms_teachers.create!(
+          classroom: classroom,
+          role: classroom.classrooms_teachers.exists?(role: OWNER) ? COTEACHER : OWNER
+        )
+      end
     end
   end
 end
