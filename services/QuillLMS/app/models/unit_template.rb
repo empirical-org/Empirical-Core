97c5da16376c1a51f755d45b1596d5a95b45ae52
Brendan Shean <brendanshean@gmail.com>
2022-01-26 10:54:39 -0500

    Fix spec failure

--- services/QuillLMS/app/models/unit_template.rb
+++ services/QuillLMS/app/models/unit_template.rb
@@ -44,6 +44,12 @@ class UnitTemplate < ApplicationRecord
   ALPHA = 'alpha'
   PRIVATE = 'private'
 
+  scope :production, -> {where("unit_templates.flag IN('#{PRODUCTION}') OR unit_templates.flag IS null")}
+  scope :gamma_user, -> { where("unit_templates.flag IN('#{PRODUCTION}','#{GAMMA}') OR unit_templates.flag IS null")}
+  scope :beta_user, -> { where("unit_templates.flag IN('#{PRODUCTION}','#{GAMMA}','#{BETA}') OR unit_templates.flag IS null")}
+  scope :alpha_user, -> { where("unit_templates.flag IN('#{PRODUCTION}','#{GAMMA}','#{BETA}','#{ALPHA}') OR unit_templates.flag IS null")}
+  scope :private_user, -> { where("unit_templates.flag IN('private', '#{PRODUCTION}','#{GAMMA}','#{BETA}','#{ALPHA}') OR unit_templates.flag IS null")}
+
   USER_SCOPES = {
     PRIVATE => UnitTemplate.private_user,
     ALPHA => UnitTemplate.alpha_user,
@@ -51,11 +57,6 @@ class UnitTemplate < ApplicationRecord
     GAMMA => UnitTemplate.gamma_user
   }
 
-  scope :production, -> {where("unit_templates.flag IN('#{PRODUCTION}') OR unit_templates.flag IS null")}
-  scope :gamma_user, -> { where("unit_templates.flag IN('#{PRODUCTION}','#{GAMMA}') OR unit_templates.flag IS null")}
-  scope :beta_user, -> { where("unit_templates.flag IN('#{PRODUCTION}','#{GAMMA}','#{BETA}') OR unit_templates.flag IS null")}
-  scope :alpha_user, -> { where("unit_templates.flag IN('#{PRODUCTION}','#{GAMMA}','#{BETA}','#{ALPHA}') OR unit_templates.flag IS null")}
-  scope :private_user, -> { where("unit_templates.flag IN('private', '#{PRODUCTION}','#{GAMMA}','#{BETA}','#{ALPHA}') OR unit_templates.flag IS null")}
   around_save :delete_relevant_caches
 
   WHOLE_CLASS_LESSONS = 'Whole class lessons'
