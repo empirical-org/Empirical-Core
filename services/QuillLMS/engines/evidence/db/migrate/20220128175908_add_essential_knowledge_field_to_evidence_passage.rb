7592a9b102644d714b05f6b259e67ff3c69f8360
Eric Adams <erictayloradams@gmail.com>
2022-01-28 15:09:10 -0300

    fix rubocop errors

--- services/QuillLMS/engines/evidence/db/migrate/20220128175908_add_essential_knowledge_field_to_evidence_passage.rb
+++ services/QuillLMS/engines/evidence/db/migrate/20220128175908_add_essential_knowledge_field_to_evidence_passage.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class AddEssentialKnowledgeFieldToEvidencePassage < ActiveRecord::Migration[5.1]
   def change
     add_column :comprehension_passages, :essential_knowledge_text, :string, default: ''
