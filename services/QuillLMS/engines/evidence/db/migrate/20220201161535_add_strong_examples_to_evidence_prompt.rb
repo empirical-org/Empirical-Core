9468826a780494d9c1ec30ab17017dc1c374a4e4
Eric Adams <erictayloradams@gmail.com>
2022-02-01 13:41:52 -0300

    add migrations for first and second examples for Evidence prompts

---
+++ services/QuillLMS/engines/evidence/db/migrate/20220201161535_add_strong_examples_to_evidence_prompt.rb
@@ -0,0 +1,6 @@
+class AddStrongExamplesToEvidencePrompt < ActiveRecord::Migration[5.1]
+  def change
+    add_column :comprehension_prompts, :first_strong_example, :string, default: ''
+    add_column :comprehension_prompts, :second_strong_example, :string, default: ''
+  end
+end
