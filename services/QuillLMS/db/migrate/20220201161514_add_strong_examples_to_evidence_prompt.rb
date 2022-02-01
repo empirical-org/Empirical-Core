e5ff72980982252962ad47889e7862aa53ea5665
Eric Adams <erictayloradams@gmail.com>
2022-02-01 13:44:36 -0300

    add string literal comment

--- services/QuillLMS/db/migrate/20220201161514_add_strong_examples_to_evidence_prompt.rb
+++ services/QuillLMS/db/migrate/20220201161514_add_strong_examples_to_evidence_prompt.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class AddStrongExamplesToEvidencePrompt < ActiveRecord::Migration[5.1]
   def change
     add_column :comprehension_prompts, :first_strong_example, :string, default: ''
