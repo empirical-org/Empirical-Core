b55a8ecb227956c8df1566bf1b945f3abe9b3c09
Emilia Friedberg <emiliafriedberg@gmail.com>
2022-02-10 08:57:39 -0500

    fix test

--- services/QuillLMS/client/app/bundles/Staff/components/evidence/configureSettings/activityForm.tsx
+++ services/QuillLMS/client/app/bundles/Staff/components/evidence/configureSettings/activityForm.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import { Link } from 'react-router-dom';
 import { EditorState, ContentState } from 'draft-js';
-import _ from 'lodash'
+import * as _ from 'lodash'
 
 import PromptsForm from './promptsForm';
 
