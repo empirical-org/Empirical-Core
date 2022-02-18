afe32d6c5bc0f1cc6bde855a32a5f735609a24de
Emilia Friedberg <emiliafriedberg@gmail.com>
2022-02-18 10:17:28 -0500

    make required changes

--- services/QuillLMS/client/app/bundles/Teacher/components/progress_reports/diagnostic_reports/diagnostics/studentResponsesIndex.tsx
+++ services/QuillLMS/client/app/bundles/Teacher/components/progress_reports/diagnostic_reports/diagnostics/studentResponsesIndex.tsx
@@ -11,13 +11,16 @@ import LoadingSpinner from '../../../shared/loading_indicator.jsx'
 import { requestGet } from '../../../../../../modules/request/index';
 import {
   DataTable,
+  expandIcon,
 } from '../../../../../Shared/index'
 
 interface Student {
   name: string;
-  score: number;
-  proficiency: string;
   id: number;
+  total_acquired_skills_count: number;
+  total_possible_skills_count: number;
+  total_correct_skills_count: number;
+  total_pre_correct_skills_count: number;
 }
 
 const preTestDesktopHeaders = (isSortable) => ([
@@ -121,9 +124,16 @@ function calculateSkillsPercentage(correct, total) {
   return Math.round((correct/total) * 100)
 }
 
+function alphabeticalName(name) {
+  const nameArray = name.split(' ')
+  const lastName = nameArray[nameArray.length - 1]
+  return `${lastName} ${nameArray.join(' ')}`
+}
+
 export const StudentResponsesIndex = ({ passedStudents, match, mobileNavigation, location, isPostDiagnostic, }) => {
   const [loading, setLoading] = React.useState<boolean>(!passedStudents);
   const [students, setStudents] = React.useState<Student[]>(passedStudents || []);
+  const [scoringExplanationIsOpen, setScoringExplanationIsOpen] = React.useState<boolean>(false);
 
   const { activityId, classroomId, } = match.params
   const unitId = qs.parse(location.search.replace('?', '')).unit
@@ -149,15 +159,11 @@ export const StudentResponsesIndex = ({ passedStudents, match, mobileNavigation,
     )
   }
 
+  function handleToggleExplanation() { setScoringExplanationIsOpen(!scoringExplanationIsOpen) }
+
   const responsesLink = (studentId: number) => unitId ? `/diagnostics/${activityId}/classroom/${classroomId}/responses/${studentId}?unit=${unitId}` : `/diagnostics/${activityId}/classroom/${classroomId}/responses/${studentId}`
 
   if (loading) { return <LoadingSpinner /> }
-
-  function alphabeticalName(name) {
-    const nameArray = name.split(' ')
-    const lastName = nameArray[nameArray.length - 1]
-    return `${lastName} ${nameArray.join(' ')}`
-  }
 
   const worthSorting = students.filter(s => s.total_correct_skills_count).length
 
@@ -192,6 +198,30 @@ export const StudentResponsesIndex = ({ passedStudents, match, mobileNavigation,
     }
   })
 
+  let scoringExplanation = (
+    <button className="toggle-explanation is-closed" onClick={handleToggleExplanation} type="button">
+      <div>
+        <h3>In this report, you’ll see the number of skills each student answered correctly on their assessment rather than the number of questions answered correctly.</h3>
+        <p>(Expand to show more information)</p>
+      </div>
+      <img alt={expandIcon.alt} src={expandIcon.src} />
+    </button>
+  )
+
+  if (scoringExplanationIsOpen) {
+    scoringExplanation = (
+      <button className="toggle-explanation is-open" onClick={handleToggleExplanation} type="button">
+        <div>
+          <h3>In this report, you’ll see the number of skills each student answered correctly on their assessment rather than the number of questions answered correctly.</h3>
+          <p>In an effort to provide teachers with the most accurate and valuable data, Quill has made an update to how students’ skill understanding and growth are displayed in this report. Our focus is on measuring the development of key writing skills by making this the primary metric in the reporting, but we also understand that this growth metric needs to be contextualized as a percentage.</p>
+          <p>Each diagnostic assess a distinct number of specific skills. That skill number is different than the total number of questions, as some skills are aligned to more than one question. In this report, you’ll see the number of skills each student answered correctly on their assessment rather than the number of questions answered correctly. This change in calculation means that the percentages you now see are slightly different than the percentages you may have previously seen when Quill reported the number of questions correct on any diagnostic assessment.</p>
+        </div>
+        <img alt={expandIcon.alt} src={expandIcon.src} />
+      </button>
+    )
+  }
+
+
   return (
     <main className="student-responses-index-container">
       <header>
@@ -215,6 +245,7 @@ export const StudentResponsesIndex = ({ passedStudents, match, mobileNavigation,
           rows={mobileRows}
         />
       </div>
+      <div className="scoring-explanation-container">{scoringExplanation}</div>
     </main>
   )
 }
--- services/QuillLMS/client/app/bundles/Teacher/components/progress_reports/diagnostic_reports/student_report.tsx
+++ services/QuillLMS/client/app/bundles/Teacher/components/progress_reports/diagnostic_reports/student_report.tsx
@@ -42,8 +42,8 @@ export class StudentReport extends React.Component<StudentReportProps, StudentRe
     this.getStudentData(params);
   }
 
-  componentWillReceiveProps(nextProps: StudentReportProps) {
-    const { params } = nextProps;
+  componentDidUpdate() {
+    const { params } = this.props;
     this.setState({ loading: true });
     this.getStudentData(params);
   }
@@ -82,7 +82,7 @@ export class StudentReport extends React.Component<StudentReportProps, StudentRe
 
     if (boldingExplanationIsOpen) {
       return (
-        <button className="bolding-explanation is-open" onClick={this.handleToggleBoldingExplanation} type="button">
+        <button className="toggle-explanation is-open" onClick={this.handleToggleBoldingExplanation} type="button">
           <div>
             <h3>The bolded text helps you see the edits. It is not what the student sees.</h3>
             <p>In each student response, we have bolded all of the text that was added or edited by the student during the previous revision so that it is easier for you to skim the student&#39;s results and quickly see what changed in their writing.</p>
@@ -94,7 +94,7 @@ export class StudentReport extends React.Component<StudentReportProps, StudentRe
     }
 
     return (
-      <button className="bolding-explanation is-closed" onClick={this.handleToggleBoldingExplanation} type="button">
+      <button className="toggle-explanation is-closed" onClick={this.handleToggleBoldingExplanation} type="button">
         <div>
           <h3>The bolded text helps you see the edits. It is not what the student sees.</h3>
           <p>(Expand to show more information)</p>
