4d2769e14a0420cd3414d21956f90f63c5a63acb
Peter Kong <pkong@quill.org>
2022-02-24 22:17:52 -0700

    wip

--- services/QuillLMS/app/controllers/api/v1/active_activity_sessions_controller.rb
+++ services/QuillLMS/app/controllers/api/v1/active_activity_sessions_controller.rb
@@ -2,31 +2,25 @@
 
 class Api::V1::ActiveActivitySessionsController < Api::ApiController
   before_action :activity_session_by_uid, only: [:show]
-
+  REDIS_PREFIX = 'active_activity_session_data_'
   def show
     render json: @activity_session.as_json
   end
 
   def update
-    retried = false
-    begin
-      @activity_session = ActiveActivitySession.find_or_initialize_by(uid: params[:id])
-      @activity_session.data ||= {}
-      @activity_session.data = @activity_session.data.merge(working_params)
-      @activity_session.save!
-    rescue ActiveRecord::RecordNotUnique => e
-      # Due to the way that ActiveRecord handles unique validations such as the one on UID,
-      # it is possible that parallel calls to "update" will result in two threads trying
-      # to create the same UID record and running into the RecordNotUnique error.  If this
-      # happens, it should be safe to just retry the function because now the record is
-      # in the database so find_or_initialize_by should locate the existing record.
-      # If, for some reason, running this a second time generates the same error, we don't
-      # want to keep retrying because that indicates some other, more worrying, problem.
-      raise e if retried
+    cache_key = "#{REDIS_PREFIX}#{params[:id]}"
 
-      retried = true
-      retry
-    end
+    @aas_data = $redis.get(cache_key).
+      yield_self { |x| x.nil? ? '{}' : x }.
+      yield_self { |x| JSON.parse x }
+
+    merged = @aas_data.merge(working_params)
+    $redis.set(cache_key, merged.to_json)
+    # @activity_session = ActiveActivitySession.find_or_initialize_by(uid: params[:id])
+    # @activity_session.data ||= {}
+    # @activity_session.data = @activity_session.data.merge(working_params)
+    # @activity_session.save!
+
     head :no_content
   end
 
