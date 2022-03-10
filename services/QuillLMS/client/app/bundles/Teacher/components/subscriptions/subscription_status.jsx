b791fee554d9bc992c3f952543a7da79feada6b1
Cissy Yu <cissy@quill.org>
2022-03-10 14:23:45 -0600

    one more copy change

--- services/QuillLMS/client/app/bundles/Teacher/components/subscriptions/subscription_status.jsx
+++ services/QuillLMS/client/app/bundles/Teacher/components/subscriptions/subscription_status.jsx
@@ -24,7 +24,7 @@ function teacherPremiumCopy(subscriptionType) {
     <span>
       With {subscriptionType}, you will have access to all of Quill’s free reports as well as additional advanced reporting.
       You will also be able to view and print reports of your students’ progress. Our advanced reports support concept, Common Core, and overall progress analysis.
-      <a className="green-link" href="https://support.quill.org/quill-premium">Here’s more information</a> about your {subscriptionType} features.
+      <a className="green-link" href="https://support.quill.org/quill-premium"> Here’s more information</a> about your {subscriptionType} features.
     </span>
   )
 };
