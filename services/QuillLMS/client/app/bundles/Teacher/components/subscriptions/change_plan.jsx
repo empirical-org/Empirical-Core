9c675b88a95558743a059d904de050a200f672de
cissyxyu <57366100+cissyxyu@users.noreply.github.com>
2022-03-15 15:12:52 -0500

    Update user-facing subscription plan names (#8924)
    * all changes and tests
    
    * linting fix
    
    * accurate snapshots and last linting
    
    * add tooltip code
    
    * fix snapshot
    
    * change copy slightly on history tooltip
    
    * one more copy change
    
    * style change
    
    * add changes to premium confirmation
    
    * update snapshot
    
    * update snapshot
    
    * emilia changes

--- services/QuillLMS/client/app/bundles/Teacher/components/subscriptions/change_plan.jsx
+++ services/QuillLMS/client/app/bundles/Teacher/components/subscriptions/change_plan.jsx
@@ -12,7 +12,7 @@ export default class extends React.Component {
       <div className="change-plan">
         <div className="radio-group">
           <label className="radio-option">
-            <input checked={recurring} name="recurring" onChange={this.handleChange} type="radio" value={true} /> {this.props.subscriptionType} Premium - ${this.props.price} Annual Subscription
+            <input checked={recurring} name="recurring" onChange={this.handleChange} type="radio" value={true} /> {this.props.subscriptionType} - ${this.props.price} Annual Subscription
           </label>
           <label className="radio-option">
             <input checked={!recurring} name="recurring" onChange={this.handleChange} type="radio" value={false} /> Quill Basic - Free
