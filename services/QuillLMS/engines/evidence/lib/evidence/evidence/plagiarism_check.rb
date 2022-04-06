3c1ddcad4ddee23ef33e98b0eec5cb39b3cedf9d
Thomas Robertson <thomas.robertson@quill.org>
2022-04-06 15:32:35 -0500

    Refactor based on a suggestion from Dan

--- services/QuillLMS/engines/evidence/lib/evidence/evidence/plagiarism_check.rb
+++ services/QuillLMS/engines/evidence/lib/evidence/evidence/plagiarism_check.rb
@@ -98,10 +98,6 @@ module Evidence
       !clean_passage.empty? && [clean_entry.split.size, clean_passage.split.size].min >= MATCH_MINIMUM
     end
 
-    private def intersect_with_duplicates(arr1, arr2)
-      (arr1 & arr2).flat_map { |n| [n]*[arr1.count(n), arr2.count(n)].min }
-    end
-
     private def clean(str)
       str.gsub(/[[:punct:]]/, '').downcase
     end
@@ -158,13 +154,9 @@ module Evidence
       # we know that there's a minimum number of words that have to be identical.  This function
       # confirms that minimum amount of overlap to justify doing a set of Levenshtein calculations.
       source_arrays.any? do |source_array|
-        intersection = source_array & target_array
-        # '&' collapses duplicates so that ['the', 'the'] & ['the', 'the'] == ['the']
-        # This throws off the expected value of length, so we use this bit here to add the duplicates back into the array.
-        # Note, the order will be scrambled [1,2,1] & [1,2,1] run through this flat_map will be [1,1,2], but since we are using
-        # length in this calculation, order doesn't matter
-        with_duplicates = intersection.flat_map { |word| [word]*[source_array.count(word), target_array.count(word)].min }
-        with_duplicates.length >= (MATCH_MINIMUM - FUZZY_CHARACTER_THRESHOLD)
+        [(target_array - source_array).length,
+         (source_array - target_array).length
+        ].max <= (MATCH_MINIMUM - FUZZY_CHARACTER_THRESHOLD)
       end
     end
 
