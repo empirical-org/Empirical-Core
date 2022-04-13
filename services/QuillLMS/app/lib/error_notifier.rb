120afe9c9bdef49b2d19f222234fa002a59b0fdf
Daniel Drabik <dan.drabik@gmail.com>
2022-04-13 14:40:20 -0400

    Add extra context to Raven (sentry) errors.

--- services/QuillLMS/app/lib/error_notifier.rb
+++ services/QuillLMS/app/lib/error_notifier.rb
@@ -3,7 +3,7 @@
 module ErrorNotifier
   # pass an exception
   def self.report(error, options = {})
-    Raven.capture_exception(error)
+    Raven.capture_exception(error, extra: options)
     NewRelic::Agent.notice_error(error, options)
   end
 end
