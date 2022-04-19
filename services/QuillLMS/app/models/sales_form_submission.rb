e67d72fa600e88cde3ea01716a376e75a30626a6
Eric Adams <erictayloradams@gmail.com>
2022-04-19 12:57:29 -0700

    add more validations

--- services/QuillLMS/app/models/sales_form_submission.rb
+++ services/QuillLMS/app/models/sales_form_submission.rb
@@ -1,16 +1,26 @@
 # frozen_string_literal: true
 
 class SalesFormSubmission < ApplicationRecord
+  COLLECTION_TYPES = [
+    SCHOOL_COLLECTION_TYPE = 'school',
+    DISTRICT_COLLECTION_TYPE = 'district'
+  ]
+  SUBMISSION_TYPES = [
+    QUOTE_REQUEST_TYPE = 'quote request',
+    RENEWAL_REQUEST_TYPE = 'renewal request'
+  ]
+
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :email, presence: true
+  validates_email_format_of :email, message: :invalid
   validates :phone_number, presence: true
   validates :zipcode, presence: true
-  validates :collection_type, presence: true
   validates :school_name, presence: true
   validates :district_name, presence: true
-  validates :school_premium_count_estimate, presence: true
-  validates :teacher_premium_count_estimate, presence: true
-  validates :student_premium_count_estimate, presence: true
-  validates :submission_type, presence: true
+  validates :school_premium_count_estimate, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :teacher_premium_count_estimate, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :student_premium_count_estimate, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :collection_type, presence: true, inclusion: { in: COLLECTION_TYPES }
+  validates :submission_type, presence: true, inclusion: { in: SUBMISSION_TYPES }
 end
