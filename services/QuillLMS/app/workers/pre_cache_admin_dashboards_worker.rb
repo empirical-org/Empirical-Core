dd717212763a161815870c5db2291f0563d093ec
dandrabik <dandrabik@users.noreply.github.com>
2022-04-19 17:10:44 -0400

    Have overnight pre_cache use the DEFAULT Sidekiq queue. (#9073)
    * Have overnight pre_cache use the default queue.
    
    * Make mock more specific.
    
    * Lint.

--- services/QuillLMS/app/workers/pre_cache_admin_dashboards_worker.rb
+++ services/QuillLMS/app/workers/pre_cache_admin_dashboards_worker.rb
@@ -8,7 +8,7 @@ class PreCacheAdminDashboardsWorker
     active_admin_ids = User.where('last_sign_in >= ?', School.school_year_start(Time.now)).joins(:schools_admins).pluck(:id)
 
     active_admin_ids.each do |id|
-      FindAdminUsersWorker.perform_async(id)
+      FindAdminUsersWorker.set(queue: SidekiqQueue::DEFAULT).perform_async(id)
     end
   end
 end
