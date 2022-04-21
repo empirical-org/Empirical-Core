3cdb4c3e36a3fa74aac22ba14ede88335655b840
Thomas Robertson <thomas.robertson@quill.org>
2022-04-21 15:53:44 -0500

    Update deploy scripts to work with non-Github Heroku deploys

--- services/QuillLMS/deploy.sh
+++ services/QuillLMS/deploy.sh
@@ -82,7 +82,7 @@ if [[ "$response" =~ ^([y])$ ]]
 then
     sh ../../scripts/post_slack_deploy.sh $app_name $1 $current_branch false
 
-    heroku git:remote -a $app_name
+    heroku git:remote -a $HEROKU_APP
 
     if [ $1 == 'prod' ]
     then
@@ -91,13 +91,17 @@ then
         # Documented here: https://github.com/empirical-org/test_repo/blob/destination_branch/test_file.txt
         git fetch origin production
         git fetch origin $DEPLOY_GIT_BRANCH
-        git push --no-verify --force heroku origin/production:refs/heads/main
+        # Heroku's git server keeps `push` calls open during the full deploy process
+        # This means that under normal circumstances, this `push` call wouldn't terminate for, like, 20 minutes and the scriptwouldn't terminate
+        # In order to avoid that, we apply `nohup` to disconnect stdin, pipe outputs to /dev/null, and end the command with `&` to run the command in the background
+        nohup git push --quiet --no-verify --force heroku origin/production:refs/heads/main &> /dev/null &
 
         sh ../../scripts/post_slack_deploy_description.sh $app_name
         open $AUTOSCALE_URL
     else
         git fetch origin $DEPLOY_GIT_BRANCH
-        git push --no-verify --force heroku ${current_branch}:main
+        # See note in the $1=="prod" condition for an explanation of this command's construction
+        nohup git push --quiet --no-verify --force heroku ${current_branch}:main &> /dev/null &
     fi
     open "https://dashboard.heroku.com/apps/$HEROKU_APP/activity"
     open $URL
