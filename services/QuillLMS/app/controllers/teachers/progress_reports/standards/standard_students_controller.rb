edc95fb8838c5e8b3489e4350b9a226d72768362
Thomas Robertson <thomas.robertson@quill.org>
2022-04-25 16:19:25 -0500

    Add caching to standards reports

--- services/QuillLMS/app/controllers/teachers/progress_reports/standards/standard_students_controller.rb
+++ services/QuillLMS/app/controllers/teachers/progress_reports/standards/standard_students_controller.rb
@@ -6,30 +6,37 @@ class Teachers::ProgressReports::Standards::StandardStudentsController < Teacher
     respond_to do |format|
       format.html
       format.json do
-        students = ::ProgressReports::Standards::NewStudent.new(current_user).results(params)
-        students_json = students.map do |student|
-          serializer = ::ProgressReports::Standards::StudentSerializer.new(student)
-          # Doing this because can't figure out how to get custom params into serializers
-          serializer.classroom_id = classroom_id
-          serializer.as_json(root: false)
-        end
-        standards = ::ProgressReports::Standards::NewStandard.new(current_user).results(params)
-        standards_json = standards.map do |standard|
-          serializer = ::ProgressReports::Standards::StandardSerializer.new(standard)
-          # Doing this because can't figure out how to get custom params into serializers
-          serializer.classroom_id = classroom_id
-          serializer.as_json(root: false)
-        end
-        classrooms_i_teach = current_user.classrooms_i_teach
-        selected_classroom = classroom_id == 0 ? 'All Classrooms' : classrooms_i_teach.find{|c| c.id == classroom_id}
-        render json: {
-          selected_classroom: selected_classroom,
-          classrooms: classrooms_i_teach,
-          students: students_json,
-          standards: standards_json,
-          units: ProgressReports::Standards::Unit.new(current_user).results({}),
-          teacher: UserWithEmailSerializer.new(current_user).as_json(root: false)
+        cache_groups = {
+          classroom_id: params["classroom_id"],
+          standard_id: params["standard_id"]
         }
+        response = current_user.all_classrooms_cache(key: 'teachers.progress_reports.standards_standard_students.index', groups: cache_groups) do
+          students = ::ProgressReports::Standards::NewStudent.new(current_user).results(params)
+          students_json = students.map do |student|
+            serializer = ::ProgressReports::Standards::StudentSerializer.new(student)
+            # Doing this because can't figure out how to get custom params into serializers
+            serializer.classroom_id = classroom_id
+            serializer.as_json(root: false)
+          end
+          standards = ::ProgressReports::Standards::NewStandard.new(current_user).results(params)
+          standards_json = standards.map do |standard|
+            serializer = ::ProgressReports::Standards::StandardSerializer.new(standard)
+            # Doing this because can't figure out how to get custom params into serializers
+            serializer.classroom_id = classroom_id
+            serializer.as_json(root: false)
+          end
+          classrooms_i_teach = current_user.classrooms_i_teach
+          selected_classroom = classroom_id == 0 ? 'All Classrooms' : classrooms_i_teach.find{|c| c.id == classroom_id}
+          {
+            selected_classroom: selected_classroom,
+            classrooms: classrooms_i_teach,
+            students: students_json,
+            standards: standards_json,
+            units: ProgressReports::Standards::Unit.new(current_user).results({}),
+            teacher: UserWithEmailSerializer.new(current_user).as_json(root: false)
+          }
+        end
+        render json: response
       end
     end
   end
