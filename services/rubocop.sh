014d96ac85b31e2e96924949aa6a850bf2f27ef6
Brendan Shean <brendanshean@gmail.com>
2022-05-04 13:53:07 -0400

    Add rubocop-rails

--- services/rubocop.sh
+++ services/rubocop.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 rbenv local 2.6.6
-gem install rubocop -v 1.25.0
-gem install rubocop-rspec -v 2.8.0
+gem install rubocop -v 1.28.2
+gem install rubocop-rails -v 2.14.2
+gem install rubocop-rspec -v 2.10.0
 rubocop
