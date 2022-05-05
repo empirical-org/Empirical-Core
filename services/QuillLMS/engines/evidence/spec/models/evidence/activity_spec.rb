cede305018f6849ccabf88b34fdbe33cdf9e46f5
Emilia Friedberg <emiliafriedberg@gmail.com>
2022-05-05 07:29:51 -0400

    allow for nil target levels

--- services/QuillLMS/engines/evidence/spec/models/evidence/activity_spec.rb
+++ services/QuillLMS/engines/evidence/spec/models/evidence/activity_spec.rb
@@ -22,8 +22,6 @@ module Evidence
 
     context 'should validations' do
 
-      it { should validate_presence_of(:target_level) }
-
       it { should validate_numericality_of(:target_level).only_integer.is_greater_than_or_equal_to(1).is_less_than_or_equal_to(12) }
 
       it { should validate_presence_of(:title) }
