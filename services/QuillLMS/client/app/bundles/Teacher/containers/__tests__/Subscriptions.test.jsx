42d261a7b839fbade8152ced8e21880e48421f11
Emilia Friedberg <emiliafriedberg@gmail.com>
2022-05-12 07:14:58 -0400

    fix more tests

--- services/QuillLMS/client/app/bundles/Teacher/containers/__tests__/Subscriptions.test.jsx
+++ services/QuillLMS/client/app/bundles/Teacher/containers/__tests__/Subscriptions.test.jsx
@@ -5,7 +5,27 @@ import Subscriptions from '../Subscriptions';
 
 const sharedProps = {
   premiumCredits: [],
-  userAuthorityLevel: null
+  userAuthorityLevel: null,
+  subscriptionStatus: {
+    "id": 7,
+    "expiration": '2070-01-01',
+    "created_at": "2022-02-16T14: 07: 25.976Z",
+    "updated_at": "2022-02-16T14: 07: 25.976Z",
+    "account_type": "School Paid",
+    "lastFour": null,
+    "purchaser_email": "emilia+3@quill.org",
+    "start_date": "2022-02-16",
+    "subscription_type_id": null,
+    "purchaser_id": null,
+    "recurring": false,
+    "de_activated_date": null,
+    "payment_method": null,
+    "payment_amount": null,
+    "expired": false,
+    "purchaser_name": null,
+    "mail_to": "emilia+3@quill.org",
+    "renewal_price": null
+  }
 }
 
 describe('Subscriptions UI state iterations', () => {
