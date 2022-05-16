2fa8ef3c0fd217d4214d81adef7a767af86f8094
Cissy Yu <cissy@quill.org>
2022-05-16 12:56:57 -0400

    initial styling and components added

--- services/QuillLMS/client/app/bundles/Teacher/components/dashboard/welcome_modal.tsx
+++ services/QuillLMS/client/app/bundles/Teacher/components/dashboard/welcome_modal.tsx
@@ -1,7 +1,9 @@
 import * as React from 'react'
 import Confetti from 'react-confetti'
 
-const classroomActivitiesSrc = `${process.env.CDN_URL}/images/pages/dashboard/illustrations-classroom-activities.svg`
+const classroomActivitiesSrc = `${process.env.CDN_URL}/images/pages/dashboard/illustrations-classroom-activities-2.svg`
+
+const exploreDemoLink = `${process.env.DEFAULT_URL}/teachers/view_demo`
 
 const WelcomeModal = ({ close, size }) => {
   const { width, height } = size;
@@ -17,13 +19,22 @@ const WelcomeModal = ({ close, size }) => {
       />
       <div className="welcome-modal quill-modal">
         <div className="modal-body">
-          <h1>Welcome to Quill!</h1>
-          <div className="welcome-modal-text">
-            <p>Our mission as a non-profit is to help students become strong writers, so all our content is completely free to use with an unlimited number of students.</p>
+          <div className="welcome-modal-image-box">
+            <h1>Welcome to Quill!</h1>
+            <img alt="Teacher at projector in classroom" src={classroomActivitiesSrc} />
           </div>
-          <button className="quill-button contained primary medium" onClick={close} type="button">Let&apos;s go!</button>
+          <div>
+            <div className="welcome-modal-text">
+              <p className="welcome-modal-header">Want to see Quill&apos;s full potential?</p>
+              <p>Play around with a demo account to see sample student data and reports.</p>
+            </div>
+            <br />
+            <div className="welcome-modal-buttons">
+              <a className="quill-button contained secondary medium outlined welcome-modal-demo" href={exploreDemoLink}>Explore demo</a>
+              <button className="quill-button contained primary medium welcome-modal-skip" onClick={close} type="button">Maybe later, skip</button>
+            </div>
+          </div>
         </div>
-        <img alt="Teacher at projector in classroom" src={classroomActivitiesSrc} />
       </div>
     </div>
   )
