a68831be55e8a68a3e47dc0c42012370c5f9ba94
Peter Kong <pkong@quill.org>
2022-05-17 15:06:27 -0600

    add retry + 1s timeout

--- services/QuillLMS/engines/evidence/spec/lib/opinion/client_spec.rb
+++ services/QuillLMS/engines/evidence/spec/lib/opinion/client_spec.rb
@@ -30,13 +30,27 @@ module Evidence
       it 'should raise error on timeout' do
         stub_request(:post, mock_endpoint).to_timeout
 
-        expect { client.post }.to raise_error(Evidence::Opinion::Client::APITimeoutError, "request took longer than 5 seconds")
+        expect { client.post }.to raise_error(
+          Evidence::Opinion::Client::APITimeoutError, "request took longer than #{Evidence::Opinion::Client::API_TIMEOUT} seconds"
+        )
+      end
+
+      it 'should process successfully on retry' do
+        mock_response = double
+        allow(mock_response).to receive(:success?).and_return true
+        allow(mock_response).to receive(:filter).and_return []
+        allow(client).to receive(:api_request).and_raise(Net::ReadTimeout)
+        allow(client).to receive(:api_request).and_return(mock_response)
+
+        expect { client.post }.to_not raise_error
       end
 
       it 'should raise error on Net::ReadTimeout timeout' do
         stub_request(:post, mock_endpoint).to_raise(Net::ReadTimeout)
 
-        expect { client.post }.to raise_error(Evidence::Opinion::Client::APITimeoutError, "request took longer than 5 seconds")
+        expect { client.post }.to raise_error(
+          Evidence::Opinion::Client::APITimeoutError, "request took longer than #{Evidence::Opinion::Client::API_TIMEOUT} seconds"
+        )
       end
     end
   end
