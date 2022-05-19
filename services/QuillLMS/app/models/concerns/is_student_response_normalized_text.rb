71cce8c90e963df68c42b910afade8ce5a2a536a
Thomas Robertson <thomas.robertson@quill.org>
2022-05-19 12:52:17 -0400

    Add a small comment

--- services/QuillLMS/app/models/concerns/is_student_response_normalized_text.rb
+++ services/QuillLMS/app/models/concerns/is_student_response_normalized_text.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 
+# This concern is intended to describe the many different tables that we use
+# to normalize different types of text.  Basically these tables all do the
+# same thing, but we wanted to keep the various concerns separated.
 module IsStudentResponseNormalizedText
   extend ActiveSupport::Concern
 
