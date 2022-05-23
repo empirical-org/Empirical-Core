e4fca2bdc505a233b49e45c9916e1ca08f3bc314
Peter Kong <pkong@quill.org>
2022-05-23 09:58:11 -0600

    wip

--- services/QuillLMS/app/lib/rule_feedback_history.rb
+++ services/QuillLMS/app/lib/rule_feedback_history.rb
@@ -56,9 +56,11 @@ class RuleFeedbackHistory
     start_filter = start_date ? ["feedback_histories.created_at >= ?", start_date] : []
     end_filter = end_date ? ["feedback_histories.created_at <= ?", end_date] : []
 
-    feedback_histories = FeedbackHistory.where(rule_uid: rule_uid, prompt_id: prompt_id, used: true).where(*start_filter).where(*end_filter)
-    # feedback_histories = feedback_histories.where("feedback_histories.created_at >= ?", start_date) if start_date
-    # feedback_histories = feedback_histories.where("feedback_histories.created_at <= ?", end_date) if end_date
+    feedback_histories = FeedbackHistory.where(rule_uid: rule_uid, prompt_id: prompt_id, used: true).tap do |query|
+      query.where("feedback_histories.created_at >= ?", start_date) if start_date
+      query.where("feedback_histories.created_at <= ?", end_date) if start_date
+    end
+
     if turk_session_id
       feedback_histories = feedback_histories.joins('LEFT JOIN feedback_sessions ON feedback_histories.feedback_session_uid = feedback_sessions.uid')
       feedback_histories = feedback_histories.joins('LEFT JOIN comprehension_turking_round_activity_sessions ON feedback_sessions.activity_session_uid = comprehension_turking_round_activity_sessions.activity_session_uid')
