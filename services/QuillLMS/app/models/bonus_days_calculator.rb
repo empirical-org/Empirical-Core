2d65c2238dcf6347e079b4c9d29f0596d991a717
Brendan Shean <brendanshean@gmail.com>
2022-05-25 07:15:55 -0400

    Make bonus days calculator a pure function

--- services/QuillLMS/app/models/bonus_days_calculator.rb
+++ services/QuillLMS/app/models/bonus_days_calculator.rb
@@ -5,30 +5,27 @@ class BonusDaysCalculator < ApplicationService
   JULY = 7
   DECEMBER = 12
 
-  attr_reader :user
+  attr_reader :user, :start
 
   delegate :school, to: :user
 
-  def initialize(user)
+  def initialize(user, start: Date.current)
     @user = user
+    @start = start
   end
 
   def run
     return 0 if user.nil? || school.nil? || school_has_paid_before?
 
-    (plan_ends - today).to_i
+    (plan_ends - start).to_i
   end
 
   private def plan_ends
-    today.month < JULY ? today.change(month: JUNE, day: 30) : today.change(month: DECEMBER, day: 31)
+    start.month < JULY ? start.change(month: JUNE, day: 30) : start.change(month: DECEMBER, day: 31)
   end
 
   private def school_has_paid_before?
     ::Subscription.school_or_user_has_ever_paid?(school)
-  end
-
-  private def today
-    @today ||= Date.current
   end
 end
 
--- services/QuillLMS/spec/services/bonus_days_calculator_spec.rb
+++ services/QuillLMS/spec/services/bonus_days_calculator_spec.rb
@@ -8,35 +8,43 @@ RSpec.describe BonusDaysCalculator do
 
   before { allow(user).to receive(:school).and_return(school) }
 
-  subject { described_class.run(user) }
+  subject { described_class.run(*args) }
 
   context 'nil user' do
+    let(:args) { [user] }
+
     before { allow(user).to receive(:nil?).and_return(true) }
 
     it { expect(subject).to eq 0 }
   end
 
   context 'nil school' do
+    let(:args) { [user] }
+
     before { allow(school).to receive(:nil?).and_return(true) }
 
     it { expect(subject).to eq 0 }
   end
 
   context 'school has paid before' do
+    let(:args) { [user] }
+
     before { allow(Subscription).to receive(:school_or_user_has_ever_paid?).with(school).and_return(true) }
 
     it { expect(subject).to eq 0 }
   end
 
-  context 'school has not paid before', :travel_back do
+  context 'school has not paid before' do
+    let(:args) { [user, start: start] }
+
     context 'day is before JULY' do
-      before { travel_to Date.current.change(month: described_class::JUNE, day: 1) }
+      let(:start) { Date.current.change(month: described_class::JUNE, day: 1) }
 
       it { expect(subject).to eq 29 }
     end
 
     context 'day is after JULY' do
-      before { travel_to Date.current.change(month: described_class::DECEMBER, day: 1) }
+      let(:start) { Date.current.change(month: described_class::DECEMBER, day: 1) }
 
       it { expect(subject).to eq 30 }
     end
