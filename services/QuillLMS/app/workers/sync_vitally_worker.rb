157679e55bdab0fc3ae5c6d1f9e7c611702b368f
Cissy Yu <cissy@quill.org>
2022-05-27 12:57:04 -0400

    add some limits bc sidekiq is too slow

--- services/QuillLMS/app/workers/sync_vitally_worker.rb
+++ services/QuillLMS/app/workers/sync_vitally_worker.rb
@@ -15,7 +15,13 @@ class SyncVitallyWorker
     # Don't synchronize non-production data
     return unless ENV['SYNC_TO_VITALLY'] == 'true'
 
-    schools_to_sync.each_slice(100) do |school_batch|
+    schools = schools_to_sync
+
+    districts_to_sync(schools).each_slice(100) do |district_batch|
+      district_ids = district_batch.map { |district| district.id }
+      district_ids.each { |id| SyncVitallyOrganizationWorker.perform_async(id) }
+    end
+    schools.each_slice(100) do |school_batch|
       school_ids = school_batch.map { |school| school.id }
       SyncVitallyAccountsWorker.perform_async(school_ids)
     end
@@ -23,19 +29,15 @@ class SyncVitallyWorker
       user_ids = user_batch.map { |user| user.id }
       SyncVitallyUsersWorker.perform_async(user_ids)
     end
-    districts_to_sync(schools_to_sync).each_slice(100) do |district_batch|
-      district_ids = district_batch.map { |district| district.id }
-      district_ids.each { |id| SyncVitallyOrganizationWorker.perform_async(id) }
-    end
   end
   # rubocop:enable Metrics/CyclomaticComplexity
 
   def schools_to_sync
-    School.select(:id, :district_id).distinct.joins(:users).where('users.role = ?', 'teacher')
+    School.select(:id, :district_id).distinct.joins(:users).where('users.role = ?', 'teacher').limit(100)
   end
 
   def users_to_sync
-    User.select(:id).joins(:school).where(:role => USER_ROLES_TO_SYNC)
+    User.select(:id).joins(:school).where(:role => USER_ROLES_TO_SYNC).limit(100)
   end
 
   def districts_to_sync(schools)
