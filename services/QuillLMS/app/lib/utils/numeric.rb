2e4aa9d80c3fb7e1da959dd29744aa7ce5ec6dcf
Peter Kong <pkong@quill.org>
2022-06-22 11:46:30 -0400

    rename files, linting

--- services/QuillLMS/app/lib/utils/numeric.rb
+++ services/QuillLMS/app/lib/utils/numeric.rb
@@ -1,6 +1,8 @@
-module Utils::Integer
+# frozen_string_literal: true
+
+module Utils::Numeric
   def self.to_human_string(int)
-    raise ArgumentError unless int.class == Integer
+    raise ArgumentError, "Not a numeric" unless int.is_a?(Numeric)
 
     millions = lambda {|x| x >= 1_000_000 && x < 1_000_000_000 }
     billions = lambda {|x| x >= 1_000_000_000 }
