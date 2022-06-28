d4d4217e3646883a5a0bf978d3a9773ea2584f5f
Daniel Drabik <dan.drabik@gmail.com>
2022-06-28 17:28:23 -0400

    Add spelling test.

--- services/QuillLMS/spec/lib/synthetic/data_spec.rb
+++ services/QuillLMS/spec/lib/synthetic/data_spec.rb
@@ -45,4 +45,29 @@ describe Synthetic::Data do
       expect(first_result.translations[:es]).to eq 'goodbye'
     end
   end
+
+  describe '#fetch_synthetic_spelling_errors' do
+    let(:labeled_with_spelling) {[['their text', 'label_1'], ['no spelling', 'label_2']]}
+    let(:mock_translator) { double() }
+    let(:synthetics) { Synthetic::Data.new(labeled_with_spelling)}
+
+    it 'fetch and store translations' do
+
+      # synthetics.stub(:translator).and_return(mock_translator)
+      # # translate to spanish mock
+      # expect(mock_translator).to receive(:translate).with(labeled_data.map(&:first), from: :en, to: :es).and_return([double(text: 'adios'), double(text: 'hola')])
+      # # translate to english mock
+      # expect(mock_translator).to receive(:translate).with(['adios', 'hola'], from: :es, to: :en).and_return([double(text: 'goodbye'), double(text: 'hello')])
+
+      synthetics.fetch_synthetic_spelling_errors
+
+      expect(synthetics.results.count).to eq 2
+
+      first_result = synthetics.results.first
+
+      expect(first_result.text).to eq 'their text'
+      expect(first_result.label).to eq 'label_1'
+      expect(first_result.misspellings['their']).to eq 'ther text'
+    end
+  end
 end
