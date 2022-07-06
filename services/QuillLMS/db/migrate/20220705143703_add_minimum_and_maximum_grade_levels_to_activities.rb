45bc89056b08249338674bd18631f3d9d2230d8e
Emilia Friedberg <emiliafriedberg@gmail.com>
2022-07-06 08:52:27 -0400

    run migration again

--- services/QuillLMS/db/migrate/20220705143703_add_minimum_and_maximum_grade_levels_to_activities.rb
+++ services/QuillLMS/db/migrate/20220705143703_add_minimum_and_maximum_grade_levels_to_activities.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class AddMinimumAndMaximumGradeLevelsToActivities < ActiveRecord::Migration[5.2]
   def change
     add_column :activities, :minimum_grade_level, :integer
