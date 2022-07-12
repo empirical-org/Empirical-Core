df6e4250272b97963598f1d58d0e37521de2e87f
Emilia Friedberg <emiliafriedberg@gmail.com>
2022-07-12 12:11:40 -0400

    do a weird import path

--- services/QuillLMS/client/app/bundles/Teacher/components/assignment_flow/create_unit/custom_activity_pack/__tests__/grade_level_filters.test.tsx
+++ services/QuillLMS/client/app/bundles/Teacher/components/assignment_flow/create_unit/custom_activity_pack/__tests__/grade_level_filters.test.tsx
@@ -4,12 +4,11 @@ import { mount } from 'enzyme';
 import { activities } from './data'
 
 import GradeLevelFilters from '../grade_level_filters'
-import NumberSuffixBuilder from '../../../../modules/NumberSuffixBuilder.js'
 
 describe('gradeLevelFilters component', () => {
   const props = {
     gradeLevelFilters: [],
-    handleGradeLevelFilterChange: (gradeLevelFilters) => {}
+    handleGradeLevelFilterChange: (gradeLevelFilters: number[]) => {}
   }
 
   it('should render', () => {
