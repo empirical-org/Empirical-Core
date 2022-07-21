c3628314a134b2f5e7923dcad24d8b746eebe639
Daniel Drabik <dan.drabik@gmail.com>
2022-07-21 15:03:24 -0400

    Use shorter method from PR feedback.

--- services/QuillLMS/app/lib/synthetic/generators/base.rb
+++ services/QuillLMS/app/lib/synthetic/generators/base.rb
@@ -10,13 +10,11 @@ module Synthetic
 
       # takes an array of unique strings, e.g. ['original string', 'sample 2']
       def initialize(string_array, options = {})
-        # create and hash with each string  to an empty hash, e.g.
-        # {'string' => {}, 'string 2' => {}
-        @results_hash = string_array.map {|string| [string,{}] }.to_h
+        @results_hash = string_array.to_h {|string| [string,{}] }
       end
 
       # returns a hash of the form:
-      # {'original string' => {'transform_key' => 'generated string', 'transform_key2' => 'generated string2'}, 'sample 2' =>...}
+      # {'original string' => {'transform_key' => 'generated string', 'transform_key2' => 'generated string2'}, 'string 2' =>...}
       # examples:
       # {'hello' => {'es' => 'greetings', 'ko' => 'hi'}, 'sample 2' =>...}
       # {'their happy' => {'their' => 'ther happy'}, 'sample 2' =>...}
