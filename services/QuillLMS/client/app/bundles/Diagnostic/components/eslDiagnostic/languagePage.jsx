5954e36d413563fe0031333cc0a6e5f7dbb94d1f
Eric Adams <erictayloradams@gmail.com>
2022-07-21 11:22:27 -0400

    remove if check

--- services/QuillLMS/client/app/bundles/Diagnostic/components/eslDiagnostic/languagePage.jsx
+++ services/QuillLMS/client/app/bundles/Diagnostic/components/eslDiagnostic/languagePage.jsx
@@ -9,10 +9,10 @@ export class LanguagePage extends React.Component {
   handleClickLanguage = (e) => {
     const language = e.currentTarget.value;
     const { dispatch, setLanguage, previewMode, begin, idData } = this.props;
-    if(process.env.NODE_ENV === 'staging' && language !== ENGLISH && idData.studentId && idData.teacherId) {
+    // if(process.env.NODE_ENV === 'staging' && language !== ENGLISH && idData.studentId && idData.teacherId) {
       const { teacherId, studentId } = idData;
       dispatch(TrackAnalyticsEvent(Events.DIAGNOSTIC_LANGUAGE_SELECTED, { language, user_id: teacherId, properties: { student_id: studentId } }));
-    }
+    // }
     setLanguage(language);
     if(previewMode) {
       begin();
