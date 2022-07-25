568298770be1f45d354bddf13b7e2bab8b2c2a68
brendanshean <brendanshean@gmail.com>
2022-07-25 07:07:59 -0400

    Unify google-oauth2 access (#9395)
    * Update google-oauth2 access
    
    * Dan suggestion
    
    * Fix spurious failure

--- services/QuillLMS/client/app/bundles/Teacher/components/modules/get_auth_token.js
+++
@@ -1,1 +0,0 @@
-export default () => document.getElementsByName('csrf-token')[0] ? document.getElementsByName('csrf-token')[0].content : 0;
--- services/QuillLMS/spec/controllers/auth/google_controller_spec.rb
+++ services/QuillLMS/spec/controllers/auth/google_controller_spec.rb
@@ -5,7 +5,7 @@ require 'rails_helper'
 describe Auth::GoogleController, type: :controller do
 
   it 'shows error message for nonexistent accounts with no role' do
-    get 'authorization_and_authentication', params: { role: nil }
+    get 'online_access_callback', params: { role: nil }
     expect(flash[:error]).to include("We could not find your account. Is this your first time logging in?")
     expect(response).to redirect_to "/session/new"
   end
