932ddfb2d516a3a2236ab736f9d1ca13349f6c35
Brendan Shean <brendanshean@gmail.com>
2022-07-25 10:51:58 -0400

    Remove rails_12_factor gem

--- services/QuillLMS/config/environments/production.rb
+++ services/QuillLMS/config/environments/production.rb
@@ -115,4 +115,14 @@ EmpiricalGrammar::Application.configure do
     'www.quill.org.' => 'www.quill.org',
     'quill.org' => 'www.quill.org'
   }
+
+  # Disable serving static files from the `/public` folder by default since
+  # Apache or NGINX already handles this.
+  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+
+  if ENV["RAILS_LOG_TO_STDOUT"].present?
+    logger = ActiveSupport::Logger.new($stdout)
+    logger.formatter = config.log_formatter
+    config.logger = ActiveSupport::TaggedLogging.new(logger)
+  end
 end
