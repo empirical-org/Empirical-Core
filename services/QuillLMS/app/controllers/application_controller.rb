0e10797d0debcaa9436834259d3b7d7d83ca3374
Brendan Shean <brendanshean@gmail.com>
2022-07-26 11:42:39 -0400

    Handle invalid authenticity token errors

--- services/QuillLMS/app/controllers/application_controller.rb
+++ services/QuillLMS/app/controllers/application_controller.rb
@@ -2,6 +2,7 @@
 
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  include InvalidAuthenticityTokenHandler
   include QuillAuthentication
 
   CLEVER_REDIRECT = :clever_redirect
