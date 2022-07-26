09d343d3cd30345d3c3016d3f1df635fdc1c8d0a
Brendan Shean <brendanshean@gmail.com>
2022-07-26 12:49:16 -0400

    Rename PUMA_WORKERS to WEB_CONCURRENCY

--- services/QuillLMS/config/puma.rb
+++ services/QuillLMS/config/puma.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-workers Integer(ENV['PUMA_WORKERS'] || 3)
+workers Integer(ENV['WEB_CONCURRENCY'] || 3)
 threads Integer(ENV['PUMA_MIN_THREADS']  || 1), Integer(ENV['PUMA_MAX_THREADS'] || 10)
 
 preload_app!
