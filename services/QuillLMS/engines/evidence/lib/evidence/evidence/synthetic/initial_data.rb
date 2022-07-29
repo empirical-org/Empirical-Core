e055acabcc9ee7097e9d0bc20d651ef0f109e9fd
Daniel Drabik <dan.drabik@gmail.com>
2022-07-29 11:09:50 -0400

    Move test constants out of generator file.

--- services/QuillLMS/engines/evidence/lib/evidence/evidence/synthetic/initial_data.rb
+++ services/QuillLMS/engines/evidence/lib/evidence/evidence/synthetic/initial_data.rb
@@ -3,26 +3,6 @@
 module Evidence
   module Synthetic
     class InitialData
-      NFL_PASSAGE = "It would have been Julio Jones’ first touchdown as the Tennessee Titans’ wide receiver, and it couldn’t have come at a better time: with just over a minute to go in the first half, the trailing Titans needed a touchdown to start catching up to the Seattle Seahawks. Fans held their breath as the ball spiraled toward Jones—who caught it as he leapt into the end zone. Touchdown! Jones’ fellow Titans ran toward him to celebrate.
-  But their celebration would soon turn sour. As a scoring play, Jones’ touchdown was subject to an automatic review. Had his whole foot landed within the end zone? Officials scrutinized the instant replay footage and determined that Jones’ heel was out of bounds. The touchdown was overturned.
-  Instant Replay Takes Off
-  Whenever a point is scored or a coach challenges a call made on the field, replay officials are called to action. Using a video recording called instant replay, replay officials view the same play from different angles, often in slow motion. Replay officials can either confirm that the call made on the field was accurate or correct the call if necessary.
-  The National Football League (NFL) began experimenting with instant replay in the 1970s. Right away, they recognized that instant replay had the potential to make football games more accurate. Slow-motion replay footage can help officials pinpoint small but crucial errors that they couldn’t see otherwise, like Jones’ out-of-bounds heel.
-  Instant replay can also remove individual referee bias. Because the officials reviewing replays are different from the officials on the field, players and fans can be confident that one biased official alone will never throw a game. As the former NFL director of officiating said when instant replay was first introduced, “Replay gives us a better chance to walk off the field error-free.”
-  An Instant Controversy
-  But not everyone agrees. Some critics say the rules of the game weren’t written to account for being able to see barely perceptible ball movements. For example, an imprecise definition of what counts as a “catch” caused problems once instant replay was introduced. One player’s catch was ruled incomplete after an instant replay review because his hands had left the ball for a split second—even though the play looked perfect to the naked eye.
-  Critics also argue that the pursuit of perfect calls has removed the valuable human connection between players and referees. An official off the field making a call based on a slow-motion video can make the game feel less personal.
-  “I miss the human element of trusting the officials to make the calls in the moment and then the rest of us having to live with what they called,” Seahawks head coach Pete Carroll told NBC. “I really liked the game better when the officials were just as much a part of the game as the players.”
-  On Further Review
-  Despite the controversies surrounding instant replay, it’s likely here to stay. The NFL’s confidence in the power of instant replay to make the sport more accurate has led to an increased reliance on replay technology. In 2007, the NFL voted to make instant replay a permanent feature of the game. Four years later, the league expanded its use of instant replay even further by beginning to automatically review all plays where a point is scored.
-  For players like Jones, instant replays can lead to heartbreak—but at the end of the day, they’re just part of the modern game. “I felt like I was in. I felt like I scored,” he told reporters after the game. \“But it’s not my place to do [the replay officials’] job. They’re professionals at what they do.\""
-
-      NFL_BECAUSE = "Instant replay can make NFL games more accurate because"
-      NFL_BUT = "Instant replay can make NFL games more accurate, but"
-      NFL_SO = "Instant replay can make NFL games more accurate, so"
-
-      NFL_NOUNS = ['the NFL', 'officials', 'players', 'coaches', 'instant replay', 'it', 'they']
-
       Result = Struct.new(:text, :seed, keyword_init: true)
 
       WORD_SPLIT_COUNT = 70
@@ -39,14 +19,14 @@ module Evidence
 
       attr_reader :passage, :stem, :nouns, :results
 
-      # passage = Evidence::Synthetic::InitialDataGenerator::NFL_PASSAGE
-      # stem = Evidence::DataGeneration::NFL_BECAUSE
-      # nouns = Evidence::DataGeneration::NFL_NOUNS
-      # generation = Evidence::DataGeneration.new(passage: passage, stem: stem, nouns: nouns)
-      # generation.run
+      # passage = Evidence::Synthetic::TestConstants::NFL_PASSAGE
+      # stem = Evidence::Synthetic::TestConstants::NFL_BECAUSE
+      # nouns = Evidence::Synthetic::TestConstants::NFL_NOUNS
+      # generator = Evidence::Synthetic::InitialData.new(passage: passage, stem: stem, nouns: nouns)
+      # generator.run
 
       # file_path = "/Users/danieldrabik/Dropbox/quill/synthetic/test_generation_#{Time.current.strftime('%Y-%m-%d-%T')}.csv"
-      # generaton.to_csv(file_path)
+      # generator.to_csv(file_path)
 
       def initialize(passage:, stem:, nouns: [])
         @passage = passage
