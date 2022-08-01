cfae257c0f96819f1e88c3cd6c4fb67e9c82807b
Eric Adams <erictayloradams@gmail.com>
2022-08-01 13:24:03 -0400

    readd formatCustomProperties function

--- services/QuillLMS/client/app/bundles/Diagnostic/components/eslDiagnostic/languagePage.jsx
+++ services/QuillLMS/client/app/bundles/Diagnostic/components/eslDiagnostic/languagePage.jsx
@@ -9,9 +9,9 @@ export class LanguagePage extends React.Component {
   handleClickLanguage = (e) => {
     const language = e.currentTarget.value;
     const { dispatch, setLanguage, previewMode, begin } = this.props;
-    // if(process.env.NODE_ENV === 'production' && language !== ENGLISH) {
+    if(process.env.NODE_ENV === 'staging' && language !== ENGLISH) {
       dispatch(TrackAnalyticsEvent(Events.DIAGNOSTIC_LANGUAGE_SELECTED, { language }));
-    // }
+    }
     setLanguage(language);
     if(previewMode) {
       begin();
