a3def5f8291add04fe67d61ef63ca75bf68781ca
Thomas Robertson <thomas.robertson@quill.org>
2022-08-01 14:59:52 -0400

    Make sure that the record returned has been persisted

--- services/QuillLMS/app/workers/save_activity_session_concept_results_worker.rb
+++ services/QuillLMS/app/workers/save_activity_session_concept_results_worker.rb
@@ -8,7 +8,7 @@ class SaveActivitySessionConceptResultsWorker
   def perform(old_concept_result_ids)
     OldConceptResult.where(id: old_concept_result_ids).each do |old_concept_result|
       concept_result = ConceptResult.find_or_create_from_old_concept_result(old_concept_result)
-      unless concept_result
+      unless concept_result&.id
         SaveActivitySessionConceptResultsWorker.perform_async([old_concept_result.id])
         ErrorNotifier.report(ConceptResultCopyFailedException.new("Failed to copy OldConceptResult #{old_concept_result.id}.  Retrying."))
       end
