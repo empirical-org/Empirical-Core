a0b46078da6895324a91b35297399f2cf2a8b1a2
Eric Adams <erictayloradams@gmail.com>
2022-08-10 14:23:45 -0400

    Segment/ add teacher signed in properties (#9499)
    * create SegmentProperties delegator
    
    * remove binding.pry
    
    * add params for teacher signed in event
    
    * update namespacing; add tests
    
    * fix tests
    
    * Segment/ remove activity pack assignment event (#9501)
    
    * remove first activity pack assignment event
    
    * remove redundant rubocop disable comments
    
    * uncomment tests
    
    * update naming

---
+++ services/QuillLMS/app/models/segment_integration/user.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+module SegmentIntegration
+  class User < SimpleDelegator
+    def identify_params
+      {
+        user_id: id,
+        traits: {
+          **common_params,
+          auditor: auditor?,
+          first_name: first_name,
+          last_name: last_name,
+          email: email
+        }.reject {|_,v| v.nil? },
+        integrations: integration_rules
+      }
+    end
+
+    def common_params
+      {
+        district: school&.district&.name,
+        school_id: school&.id,
+        school_name: school&.name,
+        premium_state: premium_state,
+        premium_type: subscription&.account_type,
+        is_admin: admin?
+      }.reject {|_,v| v.nil? }
+    end
+
+    def integration_rules
+      { all: true, Intercom: (teacher?) }
+    end
+  end
+end
