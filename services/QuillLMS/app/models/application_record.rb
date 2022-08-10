a938b5a89bc0deda657bdb2bed86f2bd9e08c53d
Brendan Shean <brendanshean@gmail.com>
2022-08-10 10:08:57 -0400

    Fix togglable configuration

--- services/QuillLMS/app/models/application_record.rb
+++ services/QuillLMS/app/models/application_record.rb
@@ -3,5 +3,9 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
 
-  connects_to database: { writing: :primary, reading: :replica }
+  if ENV.fetch('USE_MULTI_DB_CONFIGURATION', false) == 'true'
+    connects_to database: { writing: :primary, reading: :replica }
+  else
+    connects_to database: { writing: :primary, reading: :primary }
+  end
 end
