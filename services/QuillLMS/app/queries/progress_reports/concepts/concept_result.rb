32868a7fedc958191552242bccac753b0f18f364
Thomas Robertson <thomas.robertson@quill.org>
2022-08-11 12:44:57 -0400

    Refactor concept_result raw SQL to use concept_results
    Instead of old_concept_results

--- services/QuillLMS/app/queries/progress_reports/concepts/concept_result.rb
+++ services/QuillLMS/app/queries/progress_reports/concepts/concept_result.rb
@@ -3,10 +3,10 @@
 class ProgressReports::Concepts::ConceptResult
 
   def self.results(teacher, filters)
-    query = ::OldConceptResult.select(<<-SELECT
-      cast(old_concept_results.metadata->>'correct' as int) as is_correct,
+    query = ::ConceptResult.select(<<-SELECT
+      cast(concept_results.correct as int) as is_correct,
       activity_sessions.user_id,
-      old_concept_results.concept_id
+      concept_results.concept_id
     SELECT
   ).joins({activity_session: {classroom_unit: :classroom}})
      .joins("INNER JOIN classrooms_teachers ON classrooms.id = classrooms_teachers.classroom_id")
@@ -30,30 +30,4 @@ class ProgressReports::Concepts::ConceptResult
 
     query
   end
-  # # Used as a CTE (common table expression) by other models to get progress report data.
-  # def self.correct_results_for_progress_report(teacher, filters)
-
-  # end
-
-  # def self.grammar_counts
-  #   select("concept_tags.name, #{correct_result_count_sql} as correct_result_count, #{incorrect_result_count_sql}  as incorrect_result_count")
-  #   .joins(concept_tag: :concept_class)
-  #   .where(concept_classes: {name: "Grammar Concepts"})
-  #   .group("concept_tags.name")
-  #   .order("concept_tags.name asc")
-  #   .having("#{correct_result_count_sql} > 0 or #{incorrect_result_count_sql} > 0")
-  # end
-
-  # def self.correct_result_count_sql
-  #   "SUM(CASE WHEN cast(concept_tag_results.metadata->>'correct' as int) = 1 THEN 1 ELSE 0 END)"
-  # end
-
-  # def self.incorrect_result_count_sql
-  #   "SUM(CASE WHEN cast(concept_tag_results.metadata->>'correct' as int) = 0 THEN 1 ELSE 0 END)"
-  # end
-
-  # def self.total_result_count_sql
-  #   "DISTINCT(COUNT(concept_tag_results.id)) as total_result_count"
-  # end
-
 end
