fe86775954e2917e6705325fb2b8181f97e01dfd
Thomas Robertson <thomas.robertson@quill.org>
2022-08-12 16:55:20 -0400

    Fix another minor spec

--- services/QuillLMS/spec/models/activity_session_spec.rb
+++ services/QuillLMS/spec/models/activity_session_spec.rb
@@ -50,7 +50,7 @@ describe ActivitySession, type: :model, redis: true do
   it { should have_one(:classroom).through(:classroom_unit) }
   it { should have_one(:unit).through(:classroom_unit) }
   it { should have_many(:concept_results) }
-  it { should have_many(:concepts).through(:old_concept_results) }
+  it { should have_many(:concepts).through(:concept_results) }
   it { should have_many(:teachers).through(:classroom) }
   it { should belong_to(:user) }
 
