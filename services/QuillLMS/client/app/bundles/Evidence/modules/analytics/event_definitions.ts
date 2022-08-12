2ed827a7bfd123c2ed98388d04b30cfa9c8432f7
Eric Adams <erictayloradams@gmail.com>
2022-08-12 16:45:43 -0400

    Segment/ add explicit event param (#9524)
    * add explicit event param
    
    * add event arg for getCurrentStepDataForEventTracking
    
    * add skip for event key
    
    * update event param config
    
    * update tests
    
    * fix other test
    
    * remove args
    
    * remove arg

--- services/QuillLMS/client/app/bundles/Evidence/modules/analytics/event_definitions.ts
+++ services/QuillLMS/client/app/bundles/Evidence/modules/analytics/event_definitions.ts
@@ -16,18 +16,21 @@ class Event {
 // name collision (see validation in the 'events.ts' file).
 const EventDefinitions = [
   {EVIDENCE_ACTIVITY_STARTED: new Event('evidenceActivityStarted', [
+    'event',
     'activityID',
     'sessionID',
     'user_id',
     'properties'
   ])},
   {EVIDENCE_PASSAGE_READ: new Event('evidencePassageRead', [
+    'event',
     'activityID',
     'sessionID',
     'user_id',
     'properties'
   ])},
   {EVIDENCE_PROMPT_STARTED: new Event('evidencePromptStarted', [
+    'event',
     'activityID',
     'promptID',
     'sessionID',
@@ -35,6 +38,7 @@ const EventDefinitions = [
     'properties'
   ])},
   {EVIDENCE_ENTRY_SUBMITTED: new Event('evidenceEntrySubmitted', [
+    'event',
     'activityID',
     'attemptNumber',
     'promptID',
@@ -47,6 +51,7 @@ const EventDefinitions = [
     'properties'
   ])},
   {EVIDENCE_FEEDBACK_RECEIVED: new Event('evidenceFeedbackReceived', [
+    'event',
     'activityID',
     'attemptNumber',
     'promptID',
@@ -60,6 +65,7 @@ const EventDefinitions = [
     'properties'
   ])},
   {EVIDENCE_PROMPT_COMPLETED: new Event('evidencePromptCompleted', [
+    'event',
     'activityID',
     'promptID',
     'sessionID',
@@ -67,18 +73,21 @@ const EventDefinitions = [
     'properties'
   ])},
   {EVIDENCE_ACTIVITY_COMPLETED: new Event('evidenceActivityCompleted', [
+    'event',
     'activityID',
     'sessionID',
     'user_id',
     'properties'
   ])},
   {EVIDENCE_ACTIVITY_SAVED: new Event('evidenceActivitySaved', [
+    'event',
     'activityID',
     'sessionID',
     'user_id',
     'properties'
   ])},
   {STUDENT_RATED_AN_ACTIVITY: new Event('Student rated an activity', [
+    'event',
     'user_id',
     'properties'
   ])}
