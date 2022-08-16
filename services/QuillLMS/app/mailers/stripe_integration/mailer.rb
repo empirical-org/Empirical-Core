4f2ed6835bef8ce63478f936ec67895bbbbf1b67
brendanshean <brendanshean@gmail.com>
2022-08-16 08:16:39 -0400

    A few stripe integration updates (#9530)
    * Remove guard clause for nil purchaser email
    
    * Add some webhook event types to ignore list
    
    * Add ENV configuration for stripe notifications mailer
    
    * Thomas suggestion

--- services/QuillLMS/app/mailers/stripe_integration/mailer.rb
+++ services/QuillLMS/app/mailers/stripe_integration/mailer.rb
@@ -5,10 +5,11 @@ module StripeIntegration
     default from: QUILL_TEAM_EMAIL_ADDRESS
 
     STRIPE_DASHBOARD_URL = 'https://dashboard.stripe.com'
+    STRIPE_NOTIFICATIONS_EMAIL = ENV.fetch('STRIPE_NOTIFICATIONS_EMAIL', '')
     CHARGE_DISPUTE_CREATED_URL = "#{STRIPE_DASHBOARD_URL}/disputes?statuses[0]=needs_response"
 
     def charge_dispute_created
-      mail(to: QUILL_TEAM_EMAIL_ADDRESS, subject: 'Charge Dispute Created') do |format|
+      mail(to: STRIPE_NOTIFICATIONS_EMAIL, subject: 'Charge Dispute Created') do |format|
         format.text do
           render plain: "A response is needed on a disupted stripe subscription charge: #{CHARGE_DISPUTE_CREATED_URL}"
         end
