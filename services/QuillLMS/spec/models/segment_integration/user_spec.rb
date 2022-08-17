2c8a170343fe7891b2481372383ee4f90aa375cb
Eric Adams <erictayloradams@gmail.com>
2022-08-17 12:26:32 +0100

    add flags property

--- services/QuillLMS/spec/models/segment_integration/user_spec.rb
+++ services/QuillLMS/spec/models/segment_integration/user_spec.rb
@@ -3,23 +3,29 @@
 require 'rails_helper'
 
 RSpec.describe SegmentIntegration::User do
-  let(:teacher) { create(:teacher) }
+  let(:teacher1) { create(:teacher) }
+  let(:teacher2) { create(:teacher, flags: ["private", "beta"]) }
 
   context '#identify_params' do
 
     it 'returns the expected params hash' do
       params = {
-        user_id: teacher.id,
+        user_id: teacher1.id,
         traits: {
-          **teacher.segment_user.common_params,
-          auditor: teacher.auditor?,
-          first_name: teacher.first_name,
-          last_name: teacher.last_name,
-          email: teacher.email
+          **teacher1.segment_user.common_params,
+          auditor: teacher1.auditor?,
+          first_name: teacher1.first_name,
+          last_name: teacher1.last_name,
+          email: teacher1.email,
+          flags: teacher1.flags&.join(", ")
         }.reject {|_,v| v.nil? },
-        integrations: teacher.segment_user.integration_rules
+        integrations: teacher1.segment_user.integration_rules
       }
-      expect(teacher.segment_user.identify_params).to eq params
+      expect(teacher1.segment_user.identify_params).to eq params
+    end
+
+    it 'returns a comma separated string value for the flags array' do
+      expect(teacher2.segment_user.identify_params[:traits][:flags]).to eq "private, beta"
     end
   end
 
@@ -27,21 +33,21 @@ RSpec.describe SegmentIntegration::User do
 
     it 'returns the expected params hash' do
       params = {
-        district: teacher.school&.district&.name,
-        school_id: teacher.school&.id,
-        school_name: teacher.school&.name,
-        premium_state: teacher.premium_state,
-        premium_type: teacher.subscription&.account_type,
-        is_admin: teacher.admin?
+        district: teacher1.school&.district&.name,
+        school_id: teacher1.school&.id,
+        school_name: teacher1.school&.name,
+        premium_state: teacher1.premium_state,
+        premium_type: teacher1.subscription&.account_type,
+        is_admin: teacher1.admin?
       }.reject {|_,v| v.nil? }
-      expect(teacher.segment_user.common_params).to eq params
+      expect(teacher1.segment_user.common_params).to eq params
     end
   end
 
   context '#integration_rules' do
 
     it 'returns the expected params hash for no user' do
-      expect(teacher.segment_user.integration_rules).to eq({ all: true, Intercom: true })
+      expect(teacher1.segment_user.integration_rules).to eq({ all: true, Intercom: true })
     end
   end
 end
