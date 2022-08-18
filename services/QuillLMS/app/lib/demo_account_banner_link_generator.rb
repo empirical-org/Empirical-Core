483e6ff1809cf8472ad7a0fedc7571404259112f
Emilia Friedberg <emiliafriedberg@gmail.com>
2022-08-18 13:46:07 -0400

    add tests

--- services/QuillLMS/app/lib/demo_account_banner_link_generator.rb
+++ services/QuillLMS/app/lib/demo_account_banner_link_generator.rb
@@ -12,10 +12,14 @@ module DemoAccountBannerLinkGenerator
   end
 
   def demo_account_recommendations_link
+    return "#{base_link}#diagnostics" unless recommendations_link_is_valid
+
     "#{base_link}#diagnostics/#{pre_diagnostic_id}/classroom/#{classroom_id}/recommendations"
   end
 
   def demo_account_growth_summary_link
+    return "#{base_link}#diagnostics" unless growth_summary_link_is_valid
+
     "#{base_link}#diagnostics/#{post_diagnostic_id}/classroom/#{classroom_id}/growth_summary"
   end
 
@@ -28,14 +32,39 @@ module DemoAccountBannerLinkGenerator
   end
 
   private def pre_diagnostic_id
-    starter_diagnostic_pre_test.id
+    starter_diagnostic_pre_test&.id
   end
 
   private def post_diagnostic_id
-    starter_diagnostic_pre_test.follow_up_activity_id
+    starter_diagnostic_pre_test&.follow_up_activity_id
   end
 
   private def classroom_id
     @classroom_id ||= current_user&.classrooms_i_own&.find { |c| c.name == DEMO_ACCOUNT_CLASSROOM_NAME }&.id
   end
+
+  private def recommendations_link_is_valid
+    return if classroom_id.nil?
+    return if starter_diagnostic_pre_test.nil?
+
+    unit_activities = current_user&.unit_activities&.where(activity_id: pre_diagnostic_id)
+
+    return if unit_activities.none?
+    return if ClassroomUnit.find_by(unit_id: unit_activities.map(&:unit_id), classroom_id: classroom_id).nil?
+
+    true
+  end
+
+  private def growth_summary_link_is_valid
+    return if classroom_id.nil?
+    return if Activity.find_by(id: post_diagnostic_id).nil?
+
+    unit_activities = current_user&.unit_activities&.where(activity_id: post_diagnostic_id)
+
+    return if unit_activities.none?
+    return if ClassroomUnit.find_by(unit_id: unit_activities.map(&:unit_id), classroom_id: classroom_id).nil?
+
+    true
+  end
+
 end
--- services/QuillLMS/spec/controllers/lib/demo_account_banner_link_generator_spec.rb
+++ services/QuillLMS/spec/controllers/lib/demo_account_banner_link_generator_spec.rb
@@ -3,80 +3,78 @@
 require 'rails_helper'
 
 class FakeController < ApplicationController
-  include QuillAuthentication
+  include DemoAccountBannerLinkGenerator
 end
 
 describe FakeController, type: :controller do
 
-  describe 'authentication methods' do
-    let(:classroom) { create(:classroom, :with_coteacher) }
-    let(:coteacher) { classroom.coteachers.first }
-    let(:owner) { classroom.owner }
-    let(:random_teacher) {create(:teacher)}
+  describe 'link generation methods' do
+    let!(:classroom) { create(:classroom, name: DemoAccountBannerLinkGenerator::DEMO_ACCOUNT_CLASSROOM_NAME ) }
+    let!(:owner) { classroom.owner }
+    let!(:post_test_activity) { create(:diagnostic_activity) }
+    let!(:pre_test_activity) { create(:diagnostic_activity, id: Activity::STARTER_DIAGNOSTIC_ACTIVITY_ID, follow_up_activity_id: post_test_activity.id )}
+    let!(:pre_test_unit) { create(:unit, user_id: owner.id) }
+    let!(:pre_test_unit_activity) { create(:unit_activity, unit: pre_test_unit, activity: pre_test_activity) }
+    let!(:pre_test_classroom_unit) { create(:classroom_unit, classroom: classroom, unit: pre_test_unit)}
+    let!(:post_test_unit) { create(:unit, user_id: owner.id) }
+    let!(:post_test_unit_activity) { create(:unit_activity, unit: post_test_unit, activity: post_test_activity) }
+    let!(:post_test_classroom_unit) { create(:classroom_unit, classroom: classroom, unit: post_test_unit)}
 
-    describe '#classroom_owner' do
-      it 'should return nil if current_user is owner of the classroom' do
-        session[:user_id] = owner.id
-        expect(controller.classroom_owner!(classroom)).to eq(nil)
-      end
 
-      it 'should redirect if current_user is not owner of the classroom' do
-        session[:user_id] = coteacher.id
-        expect(controller).to receive(:auth_failed)
-        controller.classroom_owner!(classroom)
-      end
+    before do
+      session[:user_id] = owner.id
     end
 
-    describe '#classroom_coteacher' do
-      it 'should return nil if current_user is coteacher of the classroom' do
-        session[:user_id] = coteacher.id
-        expect(controller.classroom_coteacher!(classroom)).to eq(nil)
+    describe '#demo_account_recommendations_link' do
+      it 'should return the correct link if the classroom exists' do
+        expect(controller.demo_account_recommendations_link).to eq("#{teachers_progress_reports_diagnostic_reports_path}#diagnostics/#{pre_test_activity.id}/classroom/#{classroom.id}/recommendations")
       end
 
-      it 'should redirect if current_user is not coteacher of the classroom' do
-        session[:user_id] = owner.id
-        expect(controller).to receive(:auth_failed)
-        controller.classroom_coteacher!(classroom)
+      it 'should return the diagnostic reports index link if the classroom does not exist' do
+        classroom.destroy
+        expect(controller.demo_account_recommendations_link).to eq("#{teachers_progress_reports_diagnostic_reports_path}#diagnostics")
       end
+
+      it 'should return the diagnostic reports index link if the pre test activity does not exist' do
+        pre_test_activity.destroy
+        expect(controller.demo_account_recommendations_link).to eq("#{teachers_progress_reports_diagnostic_reports_path}#diagnostics")
+      end
+
+      it 'should return the diagnostic reports index link if the pre test unit activity does not exist' do
+        pre_test_unit_activity.destroy
+        expect(controller.demo_account_recommendations_link).to eq("#{teachers_progress_reports_diagnostic_reports_path}#diagnostics")
+      end
+
+      it 'should return the diagnostic reports index link if the pre test classroom unit does not exist' do
+        pre_test_classroom_unit.destroy
+        expect(controller.demo_account_recommendations_link).to eq("#{teachers_progress_reports_diagnostic_reports_path}#diagnostics")
+      end
+
     end
 
-    describe '#classroom_teacher' do
-      it 'should return nil if current_user is coteacher of the classroom' do
-        session[:user_id] = coteacher.id
-        expect(controller.classroom_teacher!(classroom)).to eq(nil)
+    describe '#demo_account_growth_summary_link' do
+      it 'should return the correct link if the classroom exists' do
+        expect(controller.demo_account_growth_summary_link).to eq("#{teachers_progress_reports_diagnostic_reports_path}#diagnostics/#{post_test_activity.id}/classroom/#{classroom.id}/growth_summary")
       end
 
-      it 'should return nil if current_user is owner of the classroom' do
-        session[:user_id] = owner.id
-        expect(controller.classroom_teacher!(classroom)).to eq(nil)
+      it 'should return the diagnostic reports index link if the classroom does not exist' do
+        classroom.destroy
+        expect(controller.demo_account_growth_summary_link).to eq("#{teachers_progress_reports_diagnostic_reports_path}#diagnostics")
       end
 
-      it 'should redirect if current_user is not associated with the classroom via classrooms_teachers' do
-        session[:user_id] = random_teacher.id
-        expect(controller).to receive(:auth_failed)
-        controller.classroom_teacher!(classroom)
-      end
-    end
-
-    describe '#preview_student_id=' do
-      let!(:student) { create(:student) }
-      let!(:teacher) { create(:teacher) }
-
-      describe 'if passed a student id' do
-        it 'should set the session preview_student_id and the current user to the student' do
-          controller.preview_student_id=(student.id)
-          expect(session[:preview_student_id]).to eq(student.id)
-          expect(controller.current_user).to eq(student)
-        end
+      it 'should return the diagnostic reports index link if the post test activity does not exist' do
+        post_test_activity.destroy
+        expect(controller.demo_account_growth_summary_link).to eq("#{teachers_progress_reports_diagnostic_reports_path}#diagnostics")
       end
 
-      describe 'if passed a nil student id' do
-        it 'should set the session preview_student_id to nil and the current user to the one saved in the session' do
-          session[:user_id] = teacher.id
-          controller.preview_student_id=nil
-          expect(session[:preview_student_id]).to eq(nil)
-          expect(controller.current_user).to eq(teacher)
-        end
+      it 'should return the diagnostic reports index link if the post test unit activity does not exist' do
+        post_test_unit_activity.destroy
+        expect(controller.demo_account_growth_summary_link).to eq("#{teachers_progress_reports_diagnostic_reports_path}#diagnostics")
+      end
+
+      it 'should return the diagnostic reports index link if the post test classroom unit does not exist' do
+        post_test_classroom_unit.destroy
+        expect(controller.demo_account_growth_summary_link).to eq("#{teachers_progress_reports_diagnostic_reports_path}#diagnostics")
       end
     end
 
