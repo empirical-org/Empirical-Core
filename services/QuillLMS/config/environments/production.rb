a1165eaa832669602d6a56b1afbd6159f286eb8c
Brendan Shean <brendanshean@gmail.com>
2022-08-18 11:23:33 -0400

    Fix development connection pooling bug

--- services/QuillLMS/config/environments/production.rb
+++ services/QuillLMS/config/environments/production.rb
@@ -126,9 +126,7 @@ EmpiricalGrammar::Application.configure do
     config.logger = ActiveSupport::TaggedLogging.new(logger)
   end
 
-  if ENV.fetch('USE_MULTI_DB_CONFIGURATION', false) == 'true'
-    config.active_record.database_selector = { delay: 2.seconds }
-    config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
-    config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
-  end
+  config.active_record.database_selector = { delay: 2.seconds }
+  config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
+  config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 end
