d4ac467c0dfb272f2f07954fce7a48eb0bdff2ff
Daniel Drabik <dan.drabik@gmail.com>
2022-08-19 17:07:43 -0400

    Grammar hot fix - client response uses string keys.

--- services/QuillLMS/engines/evidence/lib/evidence/evidence/check/opinion.rb
+++ services/QuillLMS/engines/evidence/lib/evidence/evidence/check/opinion.rb
@@ -4,8 +4,8 @@ module Evidence
   class Check::Opinion < Check::Base
 
     def run
-      opinion_client = Opinion::Client.new(entry: entry, prompt_text: prompt.text)
-      @response = Opinion::FeedbackAssembler.run(opinion_client.post)
+      opinion_client = ::Evidence::Opinion::Client.new(entry: entry, prompt_text: prompt.text)
+      @response = ::Evidence::Opinion::FeedbackAssembler.run(opinion_client.post)
     end
 
   end
