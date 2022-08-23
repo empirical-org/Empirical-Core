dae2b5a32b5a24caad3102a2774773824fd120c5
Thomas Robertson <thomas.robertson@quill.org>
2022-08-23 11:32:30 -0400

    Allow SyncOrganization worker to handle rate-limiting from the API
    When we get a rate-limited response, we'll randomly schedule the item to get re-processed in 2 to 20 minutes.  Our limited rate is 1000 items per minute, and resets 60 seconds after the first request, so this should give us room for about 20k items while we currently sync around 12k

--- services/QuillLMS/app/workers/sync_vitally_organization_worker.rb
+++ services/QuillLMS/app/workers/sync_vitally_organization_worker.rb
@@ -3,9 +3,24 @@
 class SyncVitallyOrganizationWorker
   include Sidekiq::Worker
 
+  MINIMUM_REQUEUE_WAIT_MINUTES = 2
+  MAXIMUM_REQUEUE_WAIT_MINUTES = 20
+
   def perform(district_id)
     district = District.find(district_id)
     api = VitallyRestApi.new
-    api.create("organizations", district.vitally_data)
+    response = api.create("organizations", district.vitally_data)
+    if request_rate_limited?(response)
+      requeue_after_rate_limit(district_id)
+    end
+  end
+
+  private def request_rate_limited?(response)
+    response.code == 429
+  end
+
+  private def requeue_after_rate_limit(district_id)
+    delay = rand(MINIMUM_REQUEUE_WAIT_MINUTES..MAXIMUM_REQUEUE_WAIT_MINUTES)
+    SyncVitallyOrganizationWorker.perform_in(delay.minutes, district_id)
   end
 end
--- services/QuillLMS/spec/workers/sync_vitally_organization_worker_spec.rb
+++ services/QuillLMS/spec/workers/sync_vitally_organization_worker_spec.rb
@@ -6,13 +6,24 @@ describe SyncVitallyOrganizationWorker do
   subject { described_class.new }
 
   let(:district) { create(:district) }
+  let(:response_double) { double }
+  let(:vitally_api_double) { double }
 
   describe '#perform' do
     it 'build payload from district object and send them to Vitally' do
-      vitally_api_double = double
-
+      expect(response_double).to receive(:code).and_return(200)
+      expect(vitally_api_double).to receive(:create).with('organizations', district.vitally_data).and_return(response_double)
       expect(VitallyRestApi).to receive(:new).and_return(vitally_api_double)
-      expect(vitally_api_double).to receive(:create).with('organizations', district.vitally_data)
+      subject.perform(district.id)
+    end
+
+    it 'should re-queue via perform_in when the Vitally API call rate limits us' do
+      expect(response_double).to receive(:code).and_return(429)
+      expect(vitally_api_double).to receive(:create).and_return(response_double)
+      expect(VitallyRestApi).to receive(:new).and_return(vitally_api_double)
+
+      expect(SyncVitallyOrganizationWorker).to receive(:perform_in).with(anything, district.id)
+
       subject.perform(district.id)
     end
   end
