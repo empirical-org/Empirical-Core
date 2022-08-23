2399368982cdcd53f071e7156ee883c1e6eab97e
Peter Kong <pkong@quill.org>
2022-08-23 14:46:34 -0600

    wip

--- services/QuillLMS/app/models/concerns/flagset.rb
+++ services/QuillLMS/app/models/concerns/flagset.rb
@@ -7,22 +7,31 @@ module Flagset
   #     @name, @display_name = name, display_name
   #   end
   # end
+  #FLAGS = %w(production archived alpha beta gamma private)
   extend ActiveSupport::Concern
 
   self.flagsets = {
     alpha: {
       display_name: 'Alpha',
       flags: {
-        alpha:            {  display_name: 'Alpha' },
-        evidence_beta_1:  {  display_name: 'Evidence Beta 1' },
-        evidence_beta_2:  {  display_name: 'Evidence Beta 2' },
-        beta:             {  display_name: 'Beta' },
-        college_board:    {  display_name: 'College Board' },
-        production:       {  display_name: 'Production' }
+        'alpha' =>             {  display_name: 'Alpha' },
+        'evidence_beta_1' =>   {  display_name: 'Evidence Beta 1' },
+        'evidence_beta_2' =>   {  display_name: 'Evidence Beta 2' },
+        'beta' =>              {  display_name: 'Beta' },
+        'college_board' =>     {  display_name: 'College Board' },
+        'production' =>        {  display_name: 'Production' }
       }
     }
   }
 
+  def is_activity_viewable?(activity)
+    return false unless activity.is_a?(::Activity)
+
+    user_flagset = Flagset.flagsets.fetch(user.flagset){ return false } #user.flagset cannot be nil
+
+    # if any of left exist in right
+    user_flagset[:flags].keys.intersection(activity.flags) > 0
+  end
 end
 
 
