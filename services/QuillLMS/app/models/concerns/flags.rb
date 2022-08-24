5c8e23a12f947d63620a84c1a6d270568ea2e717
happythenewsad <happythenewsad@users.noreply.github.com>
2022-08-24 12:27:35 -0600

    backfill college_board flag (#9590)
    Co-authored-by: Peter Kong <pkong@quill.org>

--- services/QuillLMS/app/models/concerns/flags.rb
+++ services/QuillLMS/app/models/concerns/flags.rb
@@ -2,7 +2,7 @@
 
 module Flags
   extend ActiveSupport::Concern
-  FLAGS = %w(production archived alpha beta gamma private)
+  FLAGS = %w(production archived alpha beta gamma private college_board)
 
   module ClassMethods
     def flag_all(flag)
