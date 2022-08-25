d557bc83e13b4b1cb8c89235eed9e3cf7c2be7aa
Thomas Robertson <thomas.robertson@quill.org>
2022-08-25 10:47:51 -0400

    Add a migration to make districts.nces_id a bigint instead of int
    We have a known school that is paying for premium that has a 12-digit NCES ID, and that ID can't actually be set on the district without this migration.  Having an NCES ID mismatch is disrupting our Clever integration

---
+++ services/QuillLMS/db/migrate/20220825144048_incrase_district_nces_id_size.rb
@@ -0,0 +1,12 @@
+class IncraseDistrictNcesIdSize < ActiveRecord::Migration[6.1]
+  def up
+    change_column :districts, :nces_id, :bigint
+  end
+
+  def down
+    # NOTE: Once we actually have some columns with bigints in them
+    # this rollback step will start failing until they're removed.
+    # Which is to say, this probably won't actually be easily roll-back-able
+    change_column :districts, :nces_id, :int
+  end
+end
