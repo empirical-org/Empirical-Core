a1762fe4dc2bfcf2ce89d91431d46e7e81b7ace8
Thomas Robertson <thomas.robertson@quill.org>
2022-08-25 15:29:05 -0400

    Update migration to no-op when rolled back
    Because once data of the new size gets into the database, the rollback won't be possible until/unless it's removed.

--- services/QuillLMS/db/migrate/20220825144048_incrase_district_nces_id_size.rb
+++ services/QuillLMS/db/migrate/20220825144048_incrase_district_nces_id_size.rb
@@ -8,7 +8,7 @@ class IncraseDistrictNcesIdSize < ActiveRecord::Migration[6.1]
   def down
     # NOTE: Once we actually have some columns with bigints in them
     # this rollback step will start failing until they're removed.
-    # Which is to say, this probably won't actually be easily roll-back-able
-    change_column :districts, :nces_id, :int
+    # Which is to say, this probably won't actually be easily roll-back-able,
+    # so this function no-ops
   end
 end
