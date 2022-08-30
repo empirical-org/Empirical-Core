3f2e31b1a8ca849244fe2e99fab9aee49541a026
Anathomical <thomas.robertson@quill.org>
2022-08-30 10:53:28 -0400

    Do not prohibit names from units assigned to archived classrooms (#9605)
    Some teachers intend to archive their classrooms from last year and then reuse the activity pack names from then this year

--- services/QuillLMS/spec/controllers/teachers/units_controller_spec.rb
+++ services/QuillLMS/spec/controllers/teachers/units_controller_spec.rb
@@ -18,6 +18,14 @@ describe Teachers::UnitsController, type: :controller do
    )
   end
 
+  let!(:classroom_unit2) do
+    create(:classroom_unit,
+     unit: unit2,
+     classroom: classroom,
+     assigned_student_ids: [student.id]
+   )
+  end
+
   let!(:diagnostic) { create(:diagnostic) }
   let!(:diagnostic_activity) { create(:diagnostic_activity)}
   let!(:unit_activity) { create(:unit_activity, unit: unit, activity: diagnostic_activity, due_date: Time.current )}
@@ -61,9 +69,21 @@ describe Teachers::UnitsController, type: :controller do
 
     it 'should render the correct json' do
       get :prohibited_unit_names, as: :json
-      expect(response.body).to eq({
-          prohibitedUnitNames: unit_names.concat(unit_template_names)
-      }.to_json)
+      expect(JSON.parse(response.body)['prohibitedUnitNames']).to match_array(unit_names.concat(unit_template_names))
+    end
+
+    it 'should not include unit names from archived classrooms' do
+      classroom2 = create(:classroom, visible: false)
+      create(:classrooms_teacher, classroom: classroom2, user: teacher, role: 'owner')
+      unit3 = create(:unit, user: teacher)
+      create(:classroom_unit,
+        unit: unit3,
+        classroom: classroom2,
+        assigned_student_ids: [student.id]
+      )
+
+      get :prohibited_unit_names, as: :json
+      expect(JSON.parse(response.body)['prohibitedUnitNames']).not_to include(unit3.name.downcase)
     end
   end
 
