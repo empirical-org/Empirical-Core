fd01060a87c2b284795440f42f7c80682871f451
Peter Kong <pkong@quill.org>
2022-08-30 12:29:54 -0600

    lint

--- services/QuillLMS/spec/models/concerns/flags_spec.rb
+++ services/QuillLMS/spec/models/concerns/flags_spec.rb
@@ -5,6 +5,7 @@ require 'rails_helper'
 RSpec.describe Flags, type: :model do
   context '#convert_flag_display_names_to_internal_names' do
     let(:activity) {create(:activity) }
+
     it 'should coerce flag display names to proper internal names' do
       activity.flags = ['Evidence Beta 2', 'production']
       activity.save!
