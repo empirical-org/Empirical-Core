9d0c6b8a94dfeab75dcee6f68720953a4450c057
Thomas Robertson <thomas.robertson@quill.org>
2022-08-31 14:20:04 -0400

    Update backend spec to actually test payload data and format
    The old version just kind of skipped all data-related stuff

--- services/QuillLMS/spec/controllers/grades_controller_spec.rb
+++ services/QuillLMS/spec/controllers/grades_controller_spec.rb
@@ -17,13 +17,29 @@ describe GradesController do
   end
 
   describe '#tooltip' do
-    let(:result) { ["query_result"] }
-
-    before { allow(RawSqlRunner).to receive(:execute) { result } }
+    let(:student_user) { create(:user) }
+    let(:activity_session) { create(:activity_session, user: student_user) }
+    let(:concept) { create(:concept) }
+    let!(:concept_result) { create(:concept_result, activity_session: activity_session, concept: concept) }
+    let(:due_date) { Time.current }
+    let!(:classrooms_teacher) { create(:classrooms_teacher, user: teacher, classroom: activity_session.classroom_unit.classroom) }
+    let(:timestamp_format) { '%Y-%m-%d %H:%M:%S.%6N' }
 
     it 'should render the correct json' do
-      get :tooltip, params: { user_id: teacher.id, completed: true, classroom_unit_id: "", activity_id: "" }
-      expect(response.body).to eq({concept_results: result, scores: result}.to_json)
+      get :tooltip, params: { user_id: student_user.id, completed: true, classroom_unit_id: activity_session.classroom_unit_id, activity_id: activity_session.activity_id }
+      expect(JSON.parse(response.body).deep_symbolize_keys).to eq({
+        concept_results: [{
+          correct: concept_result.correct,
+          description: activity_session.activity.description,
+          name: concept.name,
+          completed_at: activity_session.completed_at.strftime(timestamp_format),
+          due_date: activity_session.classroom_unit.unit_activities.first.due_date
+        }],
+        scores: [{
+          percentage: activity_session.percentage,
+          completed_at: activity_session.completed_at.strftime(timestamp_format)
+        }]
+      })
     end
   end
 end
