cff2af1c911e4fd9735b6755146820583b153736
Daniel Drabik <dan.drabik@gmail.com>
2022-09-01 17:03:40 -0400

    Add a basic test for uploads.

--- services/QuillLMS/app/controllers/cms/csv_uploads_controller.rb
+++ services/QuillLMS/app/controllers/cms/csv_uploads_controller.rb
@@ -4,9 +4,9 @@ class Cms::CsvUploadsController < Cms::CmsController
 
   def create
     uploader = StaffCSVUploader.new
+
     uploader.store!(params[:file])
 
-    puts (uploader.filename)
     render json: {filename: uploader.filename}
   end
 end
