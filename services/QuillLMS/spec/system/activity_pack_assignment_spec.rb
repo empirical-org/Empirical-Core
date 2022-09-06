a9e66b28f92c93397bd8e3ccbdcd1ad5f64e2b76
Emilia Friedberg <emiliafriedberg@gmail.com>
2022-09-06 08:26:41 -0400

    what if we call wait not on the driver

--- services/QuillLMS/spec/system/activity_pack_assignment_spec.rb
+++ services/QuillLMS/spec/system/activity_pack_assignment_spec.rb
@@ -64,7 +64,7 @@ RSpec.describe 'Activity Pack Assignment' do
     click_on 'Assign a diagnostic'
     first(:button, 'Select').click
     find('span.all-classes-text', text: 'All classes and students').sibling('span').click
-    driver.wait.until.find_element(:class, "review-activities-data-table-section")
+    wait.until.find_element(:class, "review-activities-data-table-section")
     click_on 'Assign pack to classes'
     click_on 'Next'
     click_on 'Take me to my dashboard'
