5b87d492c8ffb259a2e2069f6f6cacb843f986c6
Daniel Drabik <dan.drabik@gmail.com>
2022-09-06 15:59:44 -0400

    Reorder method.

--- services/QuillLMS/engines/evidence/app/workers/evidence/synthetic_labeled_data_worker.rb
+++ services/QuillLMS/engines/evidence/app/workers/evidence/synthetic_labeled_data_worker.rb
@@ -10,10 +10,9 @@ module Evidence
       uploader.retrieve_from_store!(filename)
 
       csv_array = CSV.parse(uploader.file.read)
-
-      activity = Evidence::Activity.find(activity_id)
       csv_hash = Evidence::Synthetic::LabeledDataGenerator.csvs_from_run(csv_array, filename)
 
+      activity = Evidence::Activity.find(activity_id)
       subject = "Evidence Labeled Synthetic Data: #{activity_id} - #{activity.title}"
 
       Evidence.file_mailer.send_multiple_files(Evidence::Synthetic::EMAIL, subject, csv_hash).deliver_now!
