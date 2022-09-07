0f3d07811641fcd81da1a244f7d641e4dcdb13fc
Cissy Yu <cissy@quill.org>
2022-09-07 14:09:11 +0800

    typo fix

--- services/QuillLMS/app/workers/sync_sales_form_submission_to_vitally_worker.rb
+++ services/QuillLMS/app/workers/sync_sales_form_submission_to_vitally_worker.rb
@@ -43,7 +43,7 @@ class SyncSalesFormSubmissionToVitallyWorker
     if @sales_form_submission.district_collection? && @sales_form_submission.district.present?
       api.update(SalesFormSubmission::VITALLY_DISTRICTS_TYPE, @sales_form_submission.district.id, vitally_has_opportunity_trait)
     elsif @sales_form_submission.school_collection? && @sales_form_submission.school.present?
-      api.update(SalesFormSubmission::VITALLY_SCHOOLS_TYPE, @sales_form_submission.school.id, vitallyHasOpportunityTrait)
+      api.update(SalesFormSubmission::VITALLY_SCHOOLS_TYPE, @sales_form_submission.school.id, vitally_has_opportunity_trait)
     end
   end
 
