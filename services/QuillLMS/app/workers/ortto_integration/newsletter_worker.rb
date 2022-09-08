f1296c0da1711ecf3726a773a0eed0b2949cf49b
Peter Kong <pkong@quill.org>
2022-09-08 11:12:24 -0600

    wip

--- services/QuillLMS/app/workers/ortto_integration/newsletter_worker.rb
+++ services/QuillLMS/app/workers/ortto_integration/newsletter_worker.rb
@@ -4,23 +4,33 @@ module OrttoIntegration
   class NewsletterWorker
     include Sidekiq::Worker
     sidekiq_options queue: SidekiqQueue::LOW
+    ORTTO_URL = 'https://api.ap3api.com/v1/person/merge'
 
+    def perform(user_email:, subscribe:)
+      user = User.find_by_email(user_email)
+      return unless user
 
-    def perform(user_email:, subscribe: )
-      ## START HERE
-      # user = User.find_by(id: user_id)
-      # return unless user
-
-      # resp = Faraday.post("https://api.newrelic.com/v2/applications/#{ENV['NEW_RELIC_APP_ID']}/deployments.json") do |req|
-      # req.headers['X-Api-Key'] = ENV['NEW_RELIC_REST_API_KEY']
-      # req.headers['Content-Type'] = 'application/json'
-      # req.body = {
-
-      # }.to_json
-
+      resp = Faraday.post(ORTTO_URL) do |req|
+        req.headers['X-Api-Key']    = ENV['ORTTO_API_KEY']
+        req.headers['Content-Type'] = 'application/json'
+        req.body = {
+          'people': [
+            {
+              'fields': {
+                'str::email': user_email,
+                'bol::p': subscribe
+              }
+            }
+          ],
+          'async': false,
+          'merge_by': ['str::email'],
+          'merge_strategy': 2,
+          'find_strategy': 0
+        }.to_json
+      end
 
     rescue StandardError => e
-      # ErrorNotifier #NewRelic::Agent.notice_error(e, user_id: teacher_id)
+      ErrorNotifier.report(e)
     end
   end
 end
