d051fb269e1c1e8e94169651ea350c9027776707
Daniel Drabik <dan.drabik@gmail.com>
2022-09-08 16:51:25 -0400

    Move HTMLTagRemover to its own class for reuse.

---
+++ services/QuillLMS/engines/evidence/lib/evidence/evidence/html_tag_remover.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+module Evidence
+  class HTMLTagRemover < ApplicationService
+    HTML_TAG_REGEX = /<("[^"]*"|'[^']*'|[^'">])*>/
+
+    attr_reader :html
+
+    def initialize(html)
+      @html = html
+    end
+
+    def run
+      html
+        .gsub(HTML_TAG_REGEX, " ") # remove html tags
+        .gsub("&#x27;", "'") # replace html single quotes
+        .gsub("&quot;","\"") # replace html double quotes
+        .gsub(/\s+/," ") # replace multiple spaces with single space
+        .strip
+    end
+  end
+end
