68876fedf45ae331a1f073f703799e2214856667
Thomas Robertson <thomas.robertson@quill.org>
2022-09-08 17:24:00 -0400

    Ensure that we can create ConceptResult from json when metadata is string
    Instead of a full object, sometimes `metadata` is stringified json

--- services/QuillLMS/app/models/concept_result.rb
+++ services/QuillLMS/app/models/concept_result.rb
@@ -65,6 +65,10 @@ class ConceptResult < ApplicationRecord
     data_hash = data_hash.deep_symbolize_keys
 
     metadata = data_hash[:metadata]
+    if metadata.kind_of?(String)
+      metadata = JSON.parse(metadata).deep_symbolize_keys if metadata.kind_of?(String)
+      data_hash[:metadata] = metadata
+    end
 
     response = new(
       activity_session_id: data_hash[:activity_session_id],
