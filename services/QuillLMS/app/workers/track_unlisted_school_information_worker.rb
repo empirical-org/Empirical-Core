45be1e15cee5f033a5b6ddc4b8f8daa0f73bb241
Emilia Friedberg <emiliafriedberg@gmail.com>
2022-09-08 10:53:44 -0400

    fix lint issue

--- services/QuillLMS/app/workers/track_unlisted_school_information_worker.rb
+++ services/QuillLMS/app/workers/track_unlisted_school_information_worker.rb
@@ -7,10 +7,10 @@ class TrackUnlistedSchoolInformationWorker
   def perform(user_id, school_name, school_zipcode)
     analytics = SegmentAnalytics.new
     user = User.find_by_id(user_id)
-    
-    if user && school_name
-      analytics.track_teacher_school_not_listed(user, school_name, school_zipcode)
-    end
+
+    return if user.nil? || school.name.nil?
+
+    analytics.track_teacher_school_not_listed(user, school_name, school_zipcode)
   end
 
 end
