97f44e72547915cc8268613c4779af53c8bd5f4b
Daniel Drabik <dan.drabik@gmail.com>
2022-09-09 12:30:14 -0400

    Remove duplicate line.

--- services/QuillLMS/engines/evidence/app/workers/evidence/synthetic_labeled_data_worker.rb
+++ services/QuillLMS/engines/evidence/app/workers/evidence/synthetic_labeled_data_worker.rb
@@ -15,7 +15,6 @@ module Evidence
       csv_array = CSV.parse(uploader.file.read)
       csv_hash = Evidence::Synthetic::LabeledDataGenerator.csvs_from_run(csv_array, filename, passage)
 
-      activity = Evidence::Activity.find(activity_id)
       subject = "Evidence Labeled Synthetic Data: #{activity_id} - #{activity.title}"
 
       Evidence.file_mailer.send_multiple_files(Evidence::Synthetic::EMAIL, subject, csv_hash).deliver_now!
