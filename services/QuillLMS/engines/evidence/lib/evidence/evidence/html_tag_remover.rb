5918bc1d2119e61071f7e7339535db363aecbd21
Daniel Drabik <dan.drabik@gmail.com>
2022-09-09 12:31:46 -0400

    Update comments.

--- services/QuillLMS/engines/evidence/lib/evidence/evidence/html_tag_remover.rb
+++ services/QuillLMS/engines/evidence/lib/evidence/evidence/html_tag_remover.rb
@@ -14,9 +14,9 @@ module Evidence
       html
         .gsub(HTML_TAG_REGEX, " ") # remove html tags
         .gsub("&#x27;", "'") # replace html single quotes
-        .gsub(/(‘|’)/,"'") # replace html single quotes
+        .gsub(/(‘|’)/,"'") # replace html single curly quotes
         .gsub("&quot;","\"") # replace html double quotes
-        .gsub(/(“|”)/,"\"") # replace html double quotes
+        .gsub(/(“|”)/,"\"") # replace html double curly quotes
         .gsub(/\s+/," ") # replace multiple spaces with single space
         .strip
     end
