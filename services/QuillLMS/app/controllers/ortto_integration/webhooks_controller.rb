cbaccadda848464567fb8da8239aa83ce88281d2
Peter Kong <pkong@quill.org>
2022-09-13 17:27:14 -0600

    wip

--- services/QuillLMS/app/controllers/ortto_integration/webhooks_controller.rb
+++ services/QuillLMS/app/controllers/ortto_integration/webhooks_controller.rb
@@ -3,6 +3,8 @@
 module OrttoIntegration
   class WebhooksController < ApplicationController
     protect_from_forgery except: :create
+    http_basic_authenticate_with name: 'ortto_webhook_user', password: ENV['ORTTO_WEBHOOK_PASSWORD']
+
     before_action :valid_params?, only: [:create]
 
     class OrttoWebhookBadRequestException < StandardError; end
@@ -28,6 +30,7 @@ module OrttoIntegration
     end
 
     private def valid_params?
+      puts "\n\n --- PARAMS: #{params.inspect}"
       return false unless params[:email]
       return false unless params[:action_name] && VALID_ACTIONS.include?(params[:action_name])
 
