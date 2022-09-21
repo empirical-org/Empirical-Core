6b577d3775e300e1dc37db6941d6cc5ba0d57151
Thomas Robertson <thomas.robertson@quill.org>
2022-09-21 12:49:10 -0400

    Update migration to remove unused column, update annotation

--- services/QuillLMS/db/migrate/20220920190724_drop_old_concept_results.rb
+++ services/QuillLMS/db/migrate/20220920190724_drop_old_concept_results.rb
@@ -1,5 +1,8 @@
+# frozen_string_literal: true
+
 class DropOldConceptResults < ActiveRecord::Migration[6.1]
   def up
+    remove_column :concept_results, :old_concept_result_id
     drop_table :old_concept_results
   end
 
