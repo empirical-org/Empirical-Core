051539efaa47557c5c413685766b068efb41be91
Thomas Robertson <thomas.robertson@quill.org>
2022-09-23 14:42:32 -0400

    Fix linting

--- services/QuillLMS/app/workers/clean_concept_result_instructions_worker.rb
+++ services/QuillLMS/app/workers/clean_concept_result_instructions_worker.rb
@@ -5,11 +5,11 @@ class CleanConceptResultInstructionsWorker
   sidekiq_options queue: SidekiqQueue::MIGRATION
 
   def perform(start, stop)
-    stop = ConceptResult.maximum(:id) unless stop
+    stop ||= ConceptResult.maximum(:id)
 
     ConceptResult.includes(:concept_result_instructions).where(id: start..stop).find_each do |concept_result|
-      return unless concept_result.concept_result_instructions&.text
-      return unless concept_result.concept_result_instructions.text == concept_result.extra_metadata['instructions']
+      next unless concept_result.concept_result_instructions&.text
+      next unless concept_result.concept_result_instructions.text == concept_result.extra_metadata['instructions']
 
       concept_result.update(extra_metadata: concept_result.extra_metadata.except('instructions'))
     end
