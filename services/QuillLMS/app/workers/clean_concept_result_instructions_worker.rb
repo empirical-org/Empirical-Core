f83e02f51b0d996da059da5a9de333d47d60cf78
Anathomical <thomas.robertson@quill.org>
2022-09-27 14:25:17 -0400

    Lms/fix concept result instructions (#9756)
    * Add a worker to remove duplicate "instructions" values if present
    
    An earlier version of the ConceptResult code (fixed a while ago) duplicated "instructions" into the extra_metadata value even when it was normalized into `concept_result_instructions`.  This job takes batches of IDs and cleans up that value in any ConceptResults with that pattern.
    
    * Add a rake task to enqueue all ConceptResult records for clean-up
    
    * Fix linting
    
    * Clean up testing default set of stop value that we don't need

---
+++ services/QuillLMS/app/workers/clean_concept_result_instructions_worker.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+class CleanConceptResultInstructionsWorker
+  include Sidekiq::Worker
+  sidekiq_options queue: SidekiqQueue::MIGRATION
+
+  def perform(start, stop)
+    ConceptResult.includes(:concept_result_instructions).where(id: start..stop).find_each do |concept_result|
+      next unless concept_result.concept_result_instructions&.text
+      next unless concept_result.concept_result_instructions.text == concept_result.extra_metadata['instructions']
+
+      concept_result.update(extra_metadata: concept_result.extra_metadata.except('instructions'))
+    end
+  end
+end
