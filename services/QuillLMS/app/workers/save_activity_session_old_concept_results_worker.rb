25d90b64d6ae367bc610e79e03722b2bc6b9451e
Anathomical <thomas.robertson@quill.org>
2022-09-28 14:43:47 -0400

    Lms/remove old concept results (#9745)
    * Remove files related to OldConceptResult and migrations
    
    * Remove code linking ConceptResult and OldConceptResult
    
    * Clean up a spec with left-over OldConceptResult factory usage
    
    * Add a migration to drop the old_concept_results table
    
    * Update migration to remove unused column, update annotation
    
    * Update annotations
    
    * Revert annotations to pre-migration version
    
    * Remove migration from this PR so it can be placed in a separate one
    
    * Tweak specs based on PR feedback
    
    * Revert changes to db/structure since we aren't running migration

--- services/QuillLMS/app/workers/save_activity_session_old_concept_results_worker.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-class SaveActivitySessionOldConceptResultsWorker
-  include Sidekiq::Worker
-
-  def perform(json_payload)
-    SaveActivitySessionConceptResultsWorker.perform_async(json_payload)
-  end
-end
