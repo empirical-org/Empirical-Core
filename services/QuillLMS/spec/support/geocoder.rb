6ead94c1841897f87bc1de0c7c68915700c9da13
Emilia Friedberg <emiliafriedberg@gmail.com>
2022-09-29 08:49:03 -0400

    automatically set timezones for users (#9714)
    * first stab at it
    
    * adjustment
    
    * need to save gemfile and gemfile lock too
    
    * small adjustments based on user testing
    
    * small refactor and add tests
    
    * step down in cyclomatic complexity
    
    * updates
    
    * add string literal comment
    
    * fix test

---
+++ services/QuillLMS/spec/support/geocoder.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+Geocoder.configure(lookup: :test, ip_lookup: :test)
+
+Geocoder::Lookup::Test.set_default_stub(
+  [
+    {
+      timezone: 'America/New_York'
+    }
+  ]
+)
