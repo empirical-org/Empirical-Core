deaaaf46ab029e5e782f9c81b79f62568ff8767f
Emilia Friedberg <emiliafriedberg@gmail.com>
2022-09-29 08:49:46 -0400

    add screenreader-only fill in the blank instructions (#9771)
    * add screeenreader-only fill in the blank instructions
    
    * update snapshot

--- services/QuillLMS/client/app/bundles/Shared/libs/fillInBlankInputLabel.tsx
+++ services/QuillLMS/client/app/bundles/Shared/libs/fillInBlankInputLabel.tsx
@@ -1,8 +1,8 @@
 export const fillInBlankInputLabel = (cues, blankAllowed=false) => {
-  const blankAllowedText = blankAllowed ? ' or leave it blank' : ''
+  const blankAllowedText = blankAllowed ? ' or leave it empty' : ''
   if (!cues || !cues.length) {
-    return `Add words to complete the sentence${blankAllowedText}`
+    return `Add words to fill the blank${blankAllowedText}.`
   }
 
-  return `Complete the sentence with one of the following options${blankAllowedText}: ${cues.join(', ')}`
+  return `Choose which one of the following options fits best in the blank ${blankAllowedText}: ${cues.join(', ')}.`
 }
