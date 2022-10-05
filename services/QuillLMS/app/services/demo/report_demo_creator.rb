fe6e18ecb3742170dd7d1d7d30deb89905dccca2
Daniel Drabik <dan.drabik@gmail.com>
2022-10-05 12:18:48 -0400

    Reset the classroom code, since the demo class reuses it.

--- services/QuillLMS/app/services/demo/report_demo_creator.rb
+++ services/QuillLMS/app/services/demo/report_demo_creator.rb
@@ -315,7 +315,7 @@ module Demo::ReportDemoCreator
 
       # Note, you can't early return within a transaction in Rails 6.1+
       if teacher && demo_classroom_modified?(teacher)
-        demo_classroom(teacher)&.update(visible: false)
+        demo_classroom(teacher)&.update(visible: false, code: nil)
         create_demo_classroom_data(teacher, teacher_demo: true)
       end
     end
