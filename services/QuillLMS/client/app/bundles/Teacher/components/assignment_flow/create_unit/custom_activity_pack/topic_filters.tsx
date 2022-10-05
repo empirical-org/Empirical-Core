7a12f2a8e2e96493eeb351e679ea17cbc5dd3d5c
Cissy Yu <cissy@quill.org>
2022-10-05 18:07:09 +0800

    revert changes to topic filters

--- services/QuillLMS/client/app/bundles/Teacher/components/assignment_flow/create_unit/custom_activity_pack/topic_filters.tsx
+++ services/QuillLMS/client/app/bundles/Teacher/components/assignment_flow/create_unit/custom_activity_pack/topic_filters.tsx
@@ -1,20 +1,16 @@
 import * as React from 'react';
 
 import { Activity, Topic } from './interfaces'
-import { TOPIC_FILTERS, AVERAGE_FONT_WIDTH, activityClassificationGroupings, } from './shared'
+import { TOPIC_FILTERS, AVERAGE_FONT_WIDTH, } from './shared'
 
 import { Tooltip } from '../../../../../Shared/index'
-import { requestGet } from '../../../../../../modules/request/index'
 
 const dropdownIconSrc = `${process.env.CDN_URL}/images/icons/dropdown.svg`
 const indeterminateSrc = `${process.env.CDN_URL}/images/icons/indeterminate.svg`
 const smallWhiteCheckSrc = `${process.env.CDN_URL}/images/shared/check-small-white.svg`
-const LEVEL_THREE = 3
-const LEVEL_TWO = 2
 
 interface Grouping {
   levelTwoIds: string[],
-  levelOneIds: string[]
   group: string
 }
 
@@ -23,7 +19,6 @@ interface IndividualTopicFilterRowProps {
   topicKey: number,
   handleTopicFilterChange: (topicFilters: number[]) => void,
   uniqueLevelTwoTopics: Topic[],
-  uniqueLevelOneTopics: Topic[],
   filteredActivities: Activity[],
 }
 
@@ -31,11 +26,8 @@ interface TopicToggleProps {
   filteredActivities: Activity[],
   grouping: Grouping,
   uniqueLevelTwoTopics: Topic[],
-  uniqueLevelOneTopics: Topic[],
   topicFilters: number[],
   handleTopicFilterChange: (topicFilters: number[]) => void,
-  level: number,
-  topics: Topic[]
 }
 
 interface TopicFiltersProps {
@@ -45,7 +37,7 @@ interface TopicFiltersProps {
   handleTopicFilterChange: (topicFilters: number[]) => void,
 }
 
-const IndividualTopicFilterRow = ({ topicFilters, topicKey, handleTopicFilterChange, uniqueLevelOneTopics, filteredActivities, }: IndividualTopicFilterRowProps) => {
+const IndividualTopicFilterRow = ({ topicFilters, topicKey, handleTopicFilterChange, uniqueLevelTwoTopics, filteredActivities, }: IndividualTopicFilterRowProps) => {
   function checkIndividualFilter() {
     const newTopicFilters = Array.from(new Set(topicFilters.concat([topicKey])))
     handleTopicFilterChange(newTopicFilters)
@@ -56,7 +48,7 @@ const IndividualTopicFilterRow = ({ topicFilters, topicKey, handleTopicFilterCha
     handleTopicFilterChange(newTopicFilters)
   }
 
-  const topic = uniqueLevelOneTopics.find(t => t.id === topicKey)
+  const topic = uniqueLevelTwoTopics.find(t => t.id === topicKey)
   const activityCount = filteredActivities.filter(act => act.topics && act.topics.find(t => t.id === topicKey)).length
   let checkbox = <button aria-label={`Check ${topic.name}`} className="focus-on-light quill-checkbox unselected" onClick={checkIndividualFilter} type="button" />
 
@@ -71,7 +63,7 @@ const IndividualTopicFilterRow = ({ topicFilters, topicKey, handleTopicFilterCha
   const topicNameElement = topic.name.length * AVERAGE_FONT_WIDTH >= 182 ? <Tooltip tooltipText={topic.name} tooltipTriggerText={topic.name} tooltipTriggerTextClass="tooltip-trigger-text" /> : <span>{topic.name}</span>
 
   return (
-    <div className="level-one-row filter-row topic-row" key={topicKey}>
+    <div className="individual-row filter-row topic-row" key={topicKey}>
       <div>
         {checkbox}
         {topicNameElement}
@@ -81,30 +73,31 @@ const IndividualTopicFilterRow = ({ topicFilters, topicKey, handleTopicFilterCha
   )
 }
 
-const TopicToggle = ({filteredActivities, grouping, uniqueLevelTwoTopics, uniqueLevelOneTopics, topicFilters, handleTopicFilterChange, level, topics, }: TopicToggleProps) => {
+const TopicToggle = ({filteredActivities, grouping, uniqueLevelTwoTopics, topicFilters, handleTopicFilterChange, }: TopicToggleProps) => {
   const [isOpen, setIsOpen] = React.useState(false)
 
   function toggleIsOpen() { setIsOpen(!isOpen) }
 
   function uncheckAllFilters() {
-    const newTopicFilters = topicFilters.filter(levelOneId => !grouping.levelOneIds.includes(levelOneId))
+    const newTopicFilters = topicFilters.filter(levelTwoId => !grouping.levelTwoIds.includes(levelTwoId))
     handleTopicFilterChange(newTopicFilters)
   }
 
   function checkAllFilters() {
-    const newTopicFilters = Array.from(new Set(topicFilters.concat(grouping.levelOneIds)))
+    const newTopicFilters = Array.from(new Set(topicFilters.concat(grouping.levelTwoIds)))
     handleTopicFilterChange(newTopicFilters)
   }
 
   const toggleArrow = <button aria-label="Toggle menu" className="interactive-wrapper focus-on-light filter-toggle-button" onClick={toggleIsOpen} type="button"><img alt="" className={isOpen ? 'is-open' : 'is-closed'} src={dropdownIconSrc} /></button>
   let topLevelCheckbox = <button aria-label="Check all nested filters" className="focus-on-light quill-checkbox unselected" onClick={checkAllFilters} type="button" />
-  const topLevelActivityCount = filteredActivities.filter(act => act.topics && act.topics.some(t => grouping.levelOneIds.includes(t.id))).length
 
-  if (grouping.levelOneIds.every(levelOneId => topicFilters.includes(levelOneId))) {
+  const topLevelActivityCount = filteredActivities.filter(act => act.topics && act.topics.some(t => grouping.levelTwoIds.includes(t.id))).length
+
+  if (grouping.levelTwoIds.every(levelTwoId => topicFilters.includes(levelTwoId))) {
     topLevelCheckbox = (<button aria-label="Uncheck all nested filters" className="focus-on-light quill-checkbox selected" onClick={uncheckAllFilters} type="button">
       <img alt="Checked checkbox" src={smallWhiteCheckSrc} />
     </button>)
-  } else if (grouping.levelOneIds.some(levelTwoId => topicFilters.includes(levelTwoId))) {
+  } else if (grouping.levelTwoIds.some(levelTwoId => topicFilters.includes(levelTwoId))) {
     topLevelCheckbox = (<button aria-label="Uncheck all nested filters" className="focus-on-light quill-checkbox selected" onClick={uncheckAllFilters} type="button">
       <img alt="Indeterminate checkbox" src={indeterminateSrc} />
     </button>)
@@ -113,48 +106,22 @@ const TopicToggle = ({filteredActivities, grouping, uniqueLevelTwoTopics, unique
   }
 
   let individualFilters = <span />
-
   if (isOpen) {
-    if (level === LEVEL_THREE) {
-      individualFilters = grouping.levelTwoIds.map((levelTwoId: string) =>
-      {
-        const levelTwoTopic = topics.find(t => t.id === levelTwoId);
-        const filteredLevelOneTopics = uniqueLevelOneTopics.filter(t => t.parent_id === levelTwoTopic.id)
-        const filteredGrouping = {group: levelTwoTopic.name, levelTwoIds: grouping.levelTwoIds, levelOneIds: filteredLevelOneTopics.map(t => t.id)}
-        return (
-          <TopicToggle
-            filteredActivities={filteredActivities}
-            grouping={filteredGrouping}
-            handleTopicFilterChange={handleTopicFilterChange}
-            key={levelTwoTopic.name}
-            level={LEVEL_TWO}
-            topicFilters={topicFilters}
-            uniqueLevelOneTopics={uniqueLevelOneTopics}
-            uniqueLevelTwoTopics={uniqueLevelTwoTopics}
-          />
-        )
-      }
-      )
-    } else {
-      individualFilters = grouping.levelOneIds.map((levelOneId: string) =>
-        (<IndividualTopicFilterRow
-          filteredActivities={filteredActivities}
-          handleTopicFilterChange={handleTopicFilterChange}
-          key={levelOneId}
-          topicFilters={topicFilters}
-          topicKey={levelOneId}
-          uniqueLevelOneTopics={uniqueLevelOneTopics}
-          uniqueLevelTwoTopics={uniqueLevelTwoTopics}
-        />)
-      )
-    }
+    individualFilters = grouping.levelTwoIds.map((levelTwoId: string) =>
+      (<IndividualTopicFilterRow
+        filteredActivities={filteredActivities}
+        handleTopicFilterChange={handleTopicFilterChange}
+        key={levelTwoId}
+        topicFilters={topicFilters}
+        topicKey={levelTwoId}
+        uniqueLevelTwoTopics={uniqueLevelTwoTopics}
+      />)
+    )
   }
-
-  const className = level === LEVEL_THREE ? "top-level" : "individual-row"
 
   return (
     <section className="toggle-section activity-classification-toggle">
-      <div className={className+" filter-row"}>
+      <div className="top-level filter-row">
         <div>
           {toggleArrow}
           {topLevelCheckbox}
@@ -168,50 +135,30 @@ const TopicToggle = ({filteredActivities, grouping, uniqueLevelTwoTopics, unique
 }
 
 const TopicFilters = ({ activities, filterActivities, topicFilters, handleTopicFilterChange, }: TopicFiltersProps) => {
-  const [topics, setTopics] = React.useState([])
-  let levelOneTopicsInUse = []
-  let uniqueLevelOneTopics = []
-  let uniqueLevelTwoTopics = []
-  let filteredActivities = []
-  let topicGroupings = []
+  let levelTwoTopics = []
+  let levelThreeTopics = []
+  activities.forEach(a => {
+    const levelTwoTopic = a.topics && a.topics.filter(t => Number(t.level) === 2)
+    const levelThreeTopic = a.topics && a.topics.filter(t => Number(t.level) === 3)
+    levelTwoTopics = levelTwoTopics.concat(levelTwoTopic)
+    levelThreeTopics = levelThreeTopics.concat(levelThreeTopic)
+  })
 
-  React.useEffect(() => {
-    getTopics();
-  }, []);
-
-  function getTopics() {
-    requestGet('/topics',
-      (data) => {
-        setTopics(data.topics)
-      }
-    )
-  }
+  const uniqueLevelTwoTopicIds = Array.from(new Set(levelTwoTopics.map(a => a.id)))
+  const uniqueLevelTwoTopics = uniqueLevelTwoTopicIds.map(id => levelTwoTopics.find(t => t.id === id))
+  const uniqueLevelThreeTopicIds = Array.from(new Set(levelThreeTopics.map(a => a.id)))
+  const uniqueLevelThreeTopics = uniqueLevelThreeTopicIds.map(id => levelThreeTopics.find(t => t.id === id))
 
   function clearAllTopicFilters() { handleTopicFilterChange([]) }
 
-  function getUniqueTopics(topics) { return Array.from(new Set(topics.map(a => a.id))).map(id => topics.find(t => t.id === id)) }
+  const filteredActivities = filterActivities(TOPIC_FILTERS)
 
-  if (topics.length) {
-    activities.forEach(a => { levelOneTopicsInUse = levelOneTopicsInUse.concat(a.topics) })
-    uniqueLevelOneTopics = getUniqueTopics(levelOneTopicsInUse)
-
-    const levelTwoTopicsInUse = uniqueLevelOneTopics.map(levelOneTopic => topics.find(topic => topic.id === levelOneTopic.parent_id))
-    uniqueLevelTwoTopics = getUniqueTopics(levelTwoTopicsInUse)
-
-    const levelThreeTopicsInUse = uniqueLevelTwoTopics.map(levelTwoTopic => topics.find(topic => topic.id === levelTwoTopic.parent_id))
-    const uniqueLevelThreeTopics = getUniqueTopics(levelThreeTopicsInUse)
-
-    filteredActivities = filterActivities(TOPIC_FILTERS)
-
-    topicGroupings = uniqueLevelThreeTopics.sort((a, b) => a.name.localeCompare(b.name)).map(levelThree => {
-      const levelTwoIds =  uniqueLevelTwoTopics.filter(levelTwo => levelTwo.parent_id === levelThree.id).sort((a, b) => a.name.localeCompare(b.name)).map(levelTwo => levelTwo.id)
-      return {
-        group: levelThree.name,
-        levelTwoIds: levelTwoIds,
-        levelOneIds: uniqueLevelOneTopics.filter(levelOne => levelTwoIds.includes(levelOne.parent_id)).sort((a, b) => a.name.localeCompare(b.name)).map(levelOne => levelOne.id)
-      }
-    })
-  }
+  const topicGroupings = uniqueLevelThreeTopics.sort((a, b) => a.name.localeCompare(b.name)).map(levelThree => {
+    return {
+      group: levelThree.name,
+      levelTwoIds: uniqueLevelTwoTopics.filter(levelTwo => levelTwo.parent_id === levelThree.id).sort((a, b) => a.name.localeCompare(b.name)).map(levelTwo => levelTwo.id)
+    }
+  })
 
   const topicToggles = topicGroupings.map(grouping =>
     (<TopicToggle
@@ -219,14 +166,10 @@ const TopicFilters = ({ activities, filterActivities, topicFilters, handleTopicF
       grouping={grouping}
       handleTopicFilterChange={handleTopicFilterChange}
       key={grouping.group}
-      level={LEVEL_THREE}
       topicFilters={topicFilters}
-      topics={topics}
-      uniqueLevelOneTopics={uniqueLevelOneTopics}
       uniqueLevelTwoTopics={uniqueLevelTwoTopics}
     />)
   )
-
   const clearButton = topicFilters.length ? <button className="interactive-wrapper clear-filter focus-on-light" onClick={clearAllTopicFilters} type="button">Clear</button> : <span />
   return (
     <section className="filter-section">
