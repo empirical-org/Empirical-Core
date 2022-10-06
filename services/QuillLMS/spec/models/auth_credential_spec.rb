d27861f08d272564c67b794e914d11695880feff
Brendan Shean <brendanshean@gmail.com>
2022-10-06 09:11:32 -0400

    Fix bug with nil access_token check on reset_session

--- services/QuillLMS/spec/models/auth_credential_spec.rb
+++ services/QuillLMS/spec/models/auth_credential_spec.rb
@@ -34,7 +34,7 @@ describe AuthCredential, type: :model do
   context described_class::GOOGLE_PROVIDER do
     let(:factory) { :google_auth_credential }
 
-    context '#google_authorized?' do
+    describe '#google_authorized?' do
       context 'nil expires_at' do
         before { auth_credential.update(expires_at: nil) }
 
@@ -56,6 +56,16 @@ describe AuthCredential, type: :model do
 
         it { should_not_be_clever_authorized}
         it { should_not_be_google_authorized }
+      end
+    end
+
+    describe '#refresh_token_expires_at' do
+      it { expect(auth_credential.refresh_token_expires_at).not_to be_nil }
+
+      context 'nil expires_at' do
+        before { auth_credential.update(expires_at: nil) }
+
+        it { expect(auth_credential.refresh_token_expires_at).to be_nil }
       end
     end
   end
