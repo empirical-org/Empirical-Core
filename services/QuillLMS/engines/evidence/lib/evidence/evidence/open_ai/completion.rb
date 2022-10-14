c212da8e7decfdb7b556197111a2b5cfc273e05f
Daniel Drabik <dan.drabik@gmail.com>
2022-10-14 10:29:09 -0400

    Comment sic

--- services/QuillLMS/engines/evidence/lib/evidence/evidence/open_ai/completion.rb
+++ services/QuillLMS/engines/evidence/lib/evidence/evidence/open_ai/completion.rb
@@ -18,7 +18,7 @@ module Evidence
       }
       BLANK = ''
       STOP_TOKENS = [". ", ", "]
-      MAX_COUNT = 128 # API has an undocument max of 128 for 'n'
+      MAX_COUNT = 128 # API has an undocumented max of 128 for 'n'
       MIN_RESPONSE_LENGTH = 10
 
       attr_accessor :response, :prompt, :temperature, :count, :model_key, :options_hash
