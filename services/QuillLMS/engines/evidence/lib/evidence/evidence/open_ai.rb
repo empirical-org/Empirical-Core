13eba48cd91da49d33a2f09ee87eff91460400f7
Daniel Drabik <dan.drabik@gmail.com>
2022-10-14 14:58:25 -0400

    Sic

--- services/QuillLMS/engines/evidence/lib/evidence/evidence/open_ai.rb
+++ services/QuillLMS/engines/evidence/lib/evidence/evidence/open_ai.rb
@@ -3,6 +3,6 @@
 module Evidence
   module OpenAI
     API_KEY = ENV['OPENAI_API_KEY']
-    MAX_COUNT = 128 # API has an undocument max of 128 for 'n'
+    MAX_COUNT = 128 # API has an undocumented max of 128 for 'n'
   end
 end
