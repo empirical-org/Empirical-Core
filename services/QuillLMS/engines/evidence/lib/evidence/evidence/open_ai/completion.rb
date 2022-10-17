41ba9ee757f75d97b925516ebd5c28cd985df7f1
dandrabik <dandrabik@users.noreply.github.com>
2022-10-17 13:36:25 -0400

    OpenAI Edit API (#9838)
    * Extract out concern for completion endpoint.
    
    * Refactor out to a concern.
    
    * Add Edit endpoint api and spec.
    
    * Move concerns to folder.
    
    * Lint - single line methods.
    
    * Add link to docs.
    
    * Add spec for API concern
    
    * Sic
    
    * Clean up spec.

--- services/QuillLMS/engines/evidence/lib/evidence/evidence/open_ai/completion.rb
+++ services/QuillLMS/engines/evidence/lib/evidence/evidence/open_ai/completion.rb
@@ -3,12 +3,12 @@
 module Evidence
   module OpenAI
     class Completion < Evidence::ApplicationService
-      include HTTParty
-      base_uri 'https://api.openai.com/v1'
+      include Evidence::OpenAI::Concerns::API
+      include Evidence::OpenAI::Concerns::SentenceResults
 
-      API_KEY = ENV['OPENAI_API_KEY']
-      MAX_TOKENS = 24
       ENDPOINT = '/completions'
+
+      MAX_TOKENS = 24
       # A-D, (A)Least -> (D)Most Complex/Expensive
       MODELS = {
         ada: 'text-ada-001',
@@ -16,11 +16,11 @@ module Evidence
         curie: 'text-curie-001',
         davinci: 'text-davinci-002'
       }
-      BLANK = ''
+
       STOP_TOKENS = [". ", "; ", "? ", "! "] # max of 4 stop tokens
       MAX_COUNT = 128 # API has an undocument max of 128 for 'n'
 
-      attr_accessor :response, :prompt, :temperature, :count, :model_key, :options_hash
+      attr_accessor :prompt, :temperature, :count, :model_key, :options_hash
 
       def initialize(prompt:, temperature: 0.5, count: 1, model_key: :curie, options_hash: {})
         @prompt = prompt
@@ -30,56 +30,21 @@ module Evidence
         @options_hash = options_hash
       end
 
-      def headers
-        {
-          "Content-Type" => "application/json",
-          "Authorization" => "Bearer #{API_KEY}"
-        }
+      def endpoint
+        ENDPOINT
       end
 
+      # https://beta.openai.com/docs/api-reference/completions/create
       def request_body
         # NB: 'suffix' key in documentation is not a valid key, will raise error
         {
           model: MODELS[model_key],
           temperature: temperature,
           prompt: prompt,
-          n: [count.to_i, MAX_COUNT].min,
+          n: [count.to_i, Evidence::OpenAI::MAX_COUNT].min,
           max_tokens: MAX_TOKENS,
           stop: STOP_TOKENS
         }.merge(options_hash)
-      end
-
-      def run
-        return response if response.present?
-
-        @response = self.class.post(ENDPOINT, body: request_body.to_json, headers: headers)
-
-        cleaned_results
-      end
-
-      def cleaned_results
-        result_texts_removed_characters
-          .map{|r| r&.split(/\n/)&.first } # drop anything after a \n
-          .map{|r| r&.strip } # remove leading/ending spaces
-          .compact
-          .select {|r| r.length >= 10}
-          .uniq
-      end
-
-      def result_texts_removed_characters
-        result_texts
-          .map{|r| r&.gsub(/^(\n|-|\s)+/, BLANK)} # strip all leading \n, -, or whitespace
-          .map{|r| r&.gsub(/(\]|\[|=|\d\))/, BLANK)} # strip brackets, equal signs, and 1), 2)
-      end
-
-      private def result_texts
-        response
-          .parsed_response['choices']
-          .map{|r| r['text']}
-      end
-
-      def reset_response
-        @response = nil
       end
     end
   end
---
+++ services/QuillLMS/engines/evidence/lib/evidence/evidence/open_ai/concerns/api.rb
@@ -0,0 +1,44 @@
+# frozen_string_literal: true
+
+module Evidence
+  module OpenAI
+    module Concerns::API
+      extend ActiveSupport::Concern
+
+      included do
+        include HTTParty
+        base_uri 'https://api.openai.com/v1'
+
+        attr_accessor :response
+      end
+
+      # classes using this concern require these methods
+      def endpoint
+        raise NotImplementedError
+      end
+
+      def request_body
+        raise NotImplementedError
+      end
+
+      def cleaned_results
+        raise NotImplementedError
+      end
+
+      def run
+        return cleaned_results if response.present?
+
+        @response = self.class.post(endpoint, body: request_body.to_json, headers: headers)
+
+        cleaned_results
+      end
+
+      def headers
+        {
+          "Content-Type" => "application/json",
+          "Authorization" => "Bearer #{Evidence::OpenAI::API_KEY}"
+        }
+      end
+    end
+  end
+end
