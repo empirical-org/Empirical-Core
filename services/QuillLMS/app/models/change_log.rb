da502dc50cb8b7fa106c709dc7e296411cba7557
Eric Adams <erictayloradams@gmail.com>
2022-10-19 15:08:21 -0400

    add migrations and rake task

--- services/QuillLMS/app/models/change_log.rb
+++ services/QuillLMS/app/models/change_log.rb
@@ -4,7 +4,7 @@
 #
 # Table name: change_logs
 #
-#  id                  :integer          not null, primary key
+#  id                  :integer          not null
 #  action              :string           not null
 #  changed_attribute   :string
 #  changed_record_type :string           not null
@@ -15,15 +15,6 @@
 #  updated_at          :datetime
 #  changed_record_id   :integer
 #  user_id             :integer
-#
-# Indexes
-#
-#  index_change_logs_on_changed_record_type_and_changed_record_id  (changed_record_type,changed_record_id)
-#  index_change_logs_on_user_id                                    (user_id)
-#
-# Foreign Keys
-#
-#  fk_rails_...  (user_id => users.id)
 #
 class ChangeLog < ApplicationRecord
   RENAMED = 'Renamed'
---
+++ services/QuillLMS/lib/tasks/update_feedback_history_activity_version.rake
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+namespace :update_feedback_history_activity_version do
+  desc 'update feedback histories with type "semantic" to "autoML" to match rule_type'
+  task :run => :environment do
+    FeedbackHistory.where(activity_version: 0).each do |feedback_history|
+      feedback_history.update(activity_version: 1)
+    end
+  end
+end
