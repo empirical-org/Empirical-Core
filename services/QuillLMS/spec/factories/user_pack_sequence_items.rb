ff7d518f04085919d824d63fed7f28821109e467
brendanshean <brendanshean@gmail.com>
2022-10-21 11:29:18 -0400

    Staggered Release Endpoints part 3 (#9866)
    * Create UserPackSequenceItem model
    
    * Prevent nullable user and pack_sequence_item

---
+++ services/QuillLMS/spec/factories/user_pack_sequence_items.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+# == Schema Information
+#
+# Table name: user_pack_sequence_items
+#
+#  id                    :bigint           not null, primary key
+#  status                :string           default("locked")
+#  created_at            :datetime         not null
+#  updated_at            :datetime         not null
+#  pack_sequence_item_id :bigint           not null
+#  user_id               :bigint           not null
+#
+# Indexes
+#
+#  index_user_pack_sequence_items_on_pack_sequence_item_id  (pack_sequence_item_id)
+#  index_user_pack_sequence_items_on_user_id                (user_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (pack_sequence_item_id => pack_sequence_items.id)
+#  fk_rails_...  (user_id => users.id)
+#
+FactoryBot.define do
+  factory :user_pack_sequence_item do
+    pack_sequence_item
+    user
+    status { UserPackSequenceItem::LOCKED }
+
+    trait(:locked) { status UserPackSequenceItem::LOCKED }
+    trait(:unlocked) { status UserPackSequenceItem::UNLOCKED }
+  end
+end
