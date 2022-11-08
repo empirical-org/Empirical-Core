8474218ad7226bdbb6f54ead524bd26d9a6f8631
Eric Adams <erictayloradams@gmail.com>
2022-11-08 11:59:57 -0500

    fix lint error

--- services/QuillLMS/app/controllers/cms/rosters_controller.rb
+++ services/QuillLMS/app/controllers/cms/rosters_controller.rb
@@ -18,6 +18,7 @@ class Cms::RostersController < Cms::CmsController
         next unless email
         raise "Teacher with email #{email} already exists." if User.find_by(email: email).present?
         raise "Please provide a last name or password for teacher #{t[:name]}, otherwise this account will have no password." if t[:password].blank? && t[:name].split[1].blank?
+
         password = t[:password].present? ? t[:password] : t[:name].split[1]
         teacher = User.create!(name: t[:name], email: email, password: password, password_confirmation: password, role: 'teacher')
         SchoolsUsers.create!(school: school, user: teacher)
