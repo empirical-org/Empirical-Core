bb02ebe06840253cfbe303145f210329234f07a1
Cissy Yu <cissy@quill.org>
2022-11-17 17:49:42 +0800

    perform nightly

--- services/QuillLMS/app/workers/populate_all_activity_healths_worker.rb
+++ services/QuillLMS/app/workers/populate_all_activity_healths_worker.rb
@@ -6,11 +6,6 @@ class PopulateAllActivityHealthsWorker
   INTERVAL = 180 # 3 minutes
 
   def perform
-    perform_on_connect_and_grammar_activities
-    perform_on_evidence_activities
-  end
-
-  private def perform_on_connect_and_grammar_activities
     ActivityHealth.destroy_all
     # execute this command to start primary key indices back from 1
     ActiveRecord::Base.connection.execute("TRUNCATE activity_healths RESTART IDENTITY CASCADE")
@@ -24,19 +19,6 @@ class PopulateAllActivityHealthsWorker
     # spread these, to cut down on DB resource contention.
     activities.each.with_index do |activity, index|
       PopulateActivityHealthWorker.perform_in(index * INTERVAL, activity.id)
-    end
-  end
-
-  private def perform_on_evidence_activities
-    Evidence::ActivityHealth.destroy_all
-    # execute this command to start primary key indices back from 1
-    ActiveRecord::Base.connection.execute("TRUNCATE evidence_activity_healths RESTART IDENTITY CASCADE")
-
-    activities = Evidence::Activity.all
-
-    # spread these, to cut down on DB resource contention.
-    activities.each.with_index do |activity, index|
-      PopulateEvidenceActivityHealthWorker.perform_in(index * INTERVAL, activity.id)
     end
   end
 end
