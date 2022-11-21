5729837d7515d9571e6e60326d955e584985966e
Emilia Friedberg <emiliafriedberg@gmail.com>
2022-11-21 10:34:33 -0500

    refactor

--- services/QuillLMS/client/app/bundles/Connect/libs/conceptResults/sharedConceptResultsFunctions.js
+++ services/QuillLMS/client/app/bundles/Connect/libs/conceptResults/sharedConceptResultsFunctions.js
@@ -16,25 +16,22 @@ export function getConceptResultsForAttempt(question, attemptIndex, question_typ
   const prompt = question.prompt.replace(/(<([^>]+)>)/ig, '').replace(/&nbsp;/ig, '');
   const answer = question.attempts[attemptIndex].response.text;
   const attemptNumber = attemptIndex + 1;
+  const { response, } = question.attempts[attemptIndex]
+
   let conceptResults = [];
-  if (question.attempts[attemptIndex].response?.conceptResults) {
-    conceptResults = hashToCollection(question.attempts[attemptIndex].response.conceptResults) || hashToCollection(question.attempts[attemptIndex].response.concept_results) || [];
-  } else if (question.attempts[attemptIndex].response?.concept_results) {
-    conceptResults = hashToCollection(question.attempts[attemptIndex].response.concept_results) || [];
+
+  if (response) {
+    const conceptResultObject = response.conceptResults || response.concept_results
+    conceptResults = hashToCollection(conceptResultObject) || [];
   }
-  if (conceptResults.length === 0 && question_type === 'sentence-fragment-expansion') {
-    return;
-  }
+
   if (conceptResults.length === 0) {
-    let score;
-    if (question.attempts[attemptIndex].response) {
-      score = question.attempts[attemptIndex].response.optimal;
-    }
     conceptResults = [{
       conceptUID: question.conceptID,
-      correct: score || false,
+      correct: response?.optimal || false,
     }];
   }
+
   return conceptResults.map(conceptResult => ({
     concept_uid: conceptResult.conceptUID,
     question_type,
