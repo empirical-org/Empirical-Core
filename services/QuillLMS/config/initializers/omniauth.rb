466a63c0aea9aec4ff531e45519c62c9d8ef6e64
brendanshean <brendanshean@gmail.com>
2022-12-01 11:34:46 -0500

    Update clever-omniauth gem (#10001)
    * Update clever api
    
    * Try different fork
    
    * Add config option for user_info

--- services/QuillLMS/config/initializers/omniauth.rb
+++ services/QuillLMS/config/initializers/omniauth.rb
@@ -25,6 +25,7 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :clever,
     Clever::CLIENT_ID,
     Clever::CLIENT_SECRET,
+    get_user_info: true,
     provider_ignores_state: true
 
   provider :google_oauth2,
