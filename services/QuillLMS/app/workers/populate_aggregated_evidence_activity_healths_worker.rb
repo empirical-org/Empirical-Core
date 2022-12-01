ce4e0dc9a924e2c16bfa290326131b838417b63f
Cissy Yu <cissy@quill.org>
2022-12-01 11:38:24 +0800

    do not run for archived activities

--- services/QuillLMS/app/workers/populate_aggregated_evidence_activity_healths_worker.rb
+++ services/QuillLMS/app/workers/populate_aggregated_evidence_activity_healths_worker.rb
@@ -10,8 +10,9 @@ class PopulateAggregatedEvidenceActivityHealthsWorker
     # execute this command to start primary key indices back from 1
     ActiveRecord::Base.connection.execute("TRUNCATE evidence_activity_healths RESTART IDENTITY CASCADE")
 
+    evidence_activities = Evidence::Activity.select { |a| a.flag != Flags::ARCHIVED.to_sym }
     # spread these, to cut down on DB resource contention.
-    Evidence::Activity.pluck(:id).each.with_index do |id, index|
+    evidence_activities.pluck(:id).each.with_index do |id, index|
       PopulateEvidenceActivityHealthWorker.perform_in(index * INTERVAL, id)
     end
   end
