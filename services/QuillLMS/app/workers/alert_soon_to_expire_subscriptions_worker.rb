c19a36d689cffda64f2e9a5e19f5f657c80b3768
Daniel Drabik <dan.drabik@gmail.com>
2022-12-02 18:02:42 -0500

    Small renaming and adding some comments.

--- services/QuillLMS/app/workers/alert_soon_to_expire_subscriptions_worker.rb
+++ services/QuillLMS/app/workers/alert_soon_to_expire_subscriptions_worker.rb
@@ -14,30 +14,33 @@ class AlertSoonToExpireSubscriptionsWorker
   SCHOOL_EXPIRE_IN_30 = SegmentIo::BackgroundEvents::SCHOOL_SUB_WILL_EXPIRE_IN_30
   SCHOOL_EXPIRE_IN_14 = SegmentIo::BackgroundEvents::SCHOOL_SUB_WILL_EXPIRE_IN_14
 
+  # Send a segment event for each credit card subscription that is expiring soon
+  # to trigger specific reminder emails from Intercom
+  # Events/emails differ on time, school/teacher, and whether auto-renew is on (renewing vs. expiring)
   def perform
     current_time = Time.current
     in_30_days = current_time + 30.days
     in_14_days = current_time + 14.days
     in_7_days = current_time + 7.days
 
-    # renewing
-    track_teachers(paid_renewing_subs.for_teachers.expiring(in_30_days), TEACHER_RENEW_IN_30)
-    track_teachers(paid_renewing_subs.for_teachers.expiring(in_7_days), TEACHER_RENEW_IN_7)
-    track_schools(paid_renewing_subs.for_schools.expiring(in_30_days), SCHOOL_RENEW_IN_30)
-    track_schools(paid_renewing_subs.for_schools.expiring(in_7_days), SCHOOL_RENEW_IN_7)
+    # renewing subscriptions (credit card only)
+    track_teachers(renewing_subs.for_teachers.expiring(in_30_days), TEACHER_RENEW_IN_30)
+    track_teachers(renewing_subs.for_teachers.expiring(in_7_days), TEACHER_RENEW_IN_7)
+    track_schools(renewing_subs.for_schools.expiring(in_30_days), SCHOOL_RENEW_IN_30)
+    track_schools(renewing_subs.for_schools.expiring(in_7_days), SCHOOL_RENEW_IN_7)
 
-    # expiring
-    track_teachers(paid_expiring_subs.for_teachers.expiring(in_30_days), TEACHER_EXPIRE_IN_30)
-    track_teachers(paid_expiring_subs.for_teachers.expiring(in_14_days), TEACHER_EXPIRE_IN_14)
-    track_schools(paid_expiring_subs.for_schools.expiring(in_30_days), SCHOOL_EXPIRE_IN_30)
-    track_schools(paid_expiring_subs.for_schools.expiring(in_14_days), SCHOOL_EXPIRE_IN_14)
+    # expiring subscriptions (credit card only)
+    track_teachers(expiring_subs.for_teachers.expiring(in_30_days), TEACHER_EXPIRE_IN_30)
+    track_teachers(expiring_subs.for_teachers.expiring(in_14_days), TEACHER_EXPIRE_IN_14)
+    track_schools(expiring_subs.for_schools.expiring(in_30_days), SCHOOL_EXPIRE_IN_30)
+    track_schools(expiring_subs.for_schools.expiring(in_14_days), SCHOOL_EXPIRE_IN_14)
   end
 
-  private def paid_renewing_subs
+  private def renewing_subs
     Subscription.paid_with_card.recurring
   end
 
-  private def paid_expiring_subs
+  private def expiring_subs
     Subscription.paid_with_card.not_recurring
   end
 
