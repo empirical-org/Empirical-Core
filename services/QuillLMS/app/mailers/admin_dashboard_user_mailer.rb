fdf2b9e1709d2f77164ebaf39c5741c96f1d7484
Eric Adams <erictayloradams@gmail.com>
2022-12-05 15:28:31 -0500

    Admin user management flow/ email updates (#10014)
    * add new email templates and methods
    
    * add email workers
    
    * add tests
    
    * disable rubocop
    
    * add department name
    
    * namespace refactoring
    
    * update spec
    
    * fix rubocop error
    
    * remove commented out code

---
+++ services/QuillLMS/app/mailers/admin_dashboard_user_mailer.rb
@@ -0,0 +1,60 @@
+# frozen_string_literal: true
+
+class AdminDashboardUserMailer < UserMailer
+  include EmailApiHelper
+  include ActionView::Helpers::NumberHelper
+
+  default from: 'hello@quill.org', foo: 'bar'
+
+  def teacher_account_created_email(user, admin_name, school_name, is_reminder)
+    @user = user
+    @admin_name = admin_name
+    @school_name = school_name
+    # placeholder url-- will get updated in subsequent PR for new account setup landing page
+    @set_password_link = ENV['DEFAULT_URL']
+    subject = is_reminder ? "🔔 Reminder: #{user.first_name}, a Quill account was created for you" : "[Action Required] #{user.first_name}, a Quill account was created for you"
+    mail to: user.email, subject: subject
+  end
+
+  def admin_account_created_email(user, admin_name, school_name, is_reminder)
+    @user = user
+    @admin_name = admin_name
+    @school_name = school_name
+    @set_password_link = ENV['DEFAULT_URL']
+    subject = is_reminder ? "🔔 Reminder: #{user.first_name}, a Quill school admin account was created for you" : "[Action Required] #{user.first_name}, a Quill school admin account was created for you"
+    mail to: user.email, subject: subject
+  end
+
+  def teacher_link_school_email(user, admin_name, school)
+    @user = user
+    @admin_name = admin_name
+    @school = school
+    @link_school_link = "#{ENV['DEFAULT_URL']}/teachers/#{@user.id}/schools/#{@school.id}"
+    mail to: user.email, subject: "[Action Required] #{user.first_name}, please link your Quill account to #{school.name}"
+  end
+
+  def made_school_admin_email(user, admin_name, school_name)
+    @user = user
+    @admin_name = admin_name
+    @school_name = school_name
+    mail to: user.email, subject: "#{user.first_name}, you are now a Quill admin for #{school_name}"
+  end
+
+  def made_school_admin_link_school_email(user, admin_name, school)
+    @user = user
+    @admin_name = admin_name
+    @school = school
+    @link_school_link = "#{ENV['DEFAULT_URL']}/teachers/#{@user.id}/schools/#{@school.id}"
+    mail to: user.email, subject: "#{user.first_name}, you are now a Quill admin for #{school.name}"
+  end
+
+  def made_school_admin_change_school_email(user, admin_name, new_school, existing_school)
+    @user = user
+    @admin_name = admin_name
+    @new_school = new_school
+    @existing_school = existing_school
+    @link_school_link = "#{ENV['DEFAULT_URL']}/teachers/#{@user.id}/schools/#{@new_school.id}"
+    mail to: user.email, subject: "#{user.first_name}, you are now a Quill admin for #{new_school.name}"
+  end
+
+end
---
+++ services/QuillLMS/app/models/mailer/user.rb
@@ -0,0 +1,53 @@
+# frozen_string_literal: true
+
+module Mailer
+  class User < SimpleDelegator
+    def send_admin_dashboard_teacher_account_created_email(admin_name, school_name, is_reminder)
+      AdminDashboardUserMailer.teacher_account_created_email(self, admin_name, school_name, is_reminder).deliver_now! if email.present?
+    end
+
+    def send_admin_dashboard_admin_account_created_email(admin_name, school_name, is_reminder)
+      AdminDashboardUserMailer.admin_account_created_email(self, admin_name, school_name, is_reminder).deliver_now! if email.present?
+    end
+
+    def send_admin_dashboard_teacher_link_school_email(admin_name, school)
+      AdminDashboardUserMailer.teacher_link_school_email(self, admin_name, school).deliver_now! if email.present?
+    end
+
+    def send_admin_dashboard_made_school_admin_email(admin_name, school_name)
+      AdminDashboardUserMailer.made_school_admin_email(self, admin_name, school_name).deliver_now! if email.present?
+    end
+
+    def send_admin_dashboard_made_school_admin_link_school_email(admin_name, school)
+      AdminDashboardUserMailer.made_school_admin_link_school_email(self, admin_name, school).deliver_now! if email.present?
+    end
+
+    def send_admin_dashboard_made_school_admin_change_school_email(admin_name, new_school, existing_school)
+      AdminDashboardUserMailer.made_school_admin_change_school_email(self, admin_name, new_school, existing_school).deliver_now! if email.present?
+    end
+
+    def send_internal_tool_admin_account_created_email(school_name)
+      InternalToolUserMailer.admin_account_created_email(self, school_name).deliver_now! if email.present?
+    end
+
+    def send_internal_tool_made_school_admin_email(school_name)
+      InternalToolUserMailer.made_school_admin_email(self, school_name).deliver_now! if email.present?
+    end
+
+    def send_internal_tool_made_school_admin_link_school_email(school)
+      InternalToolUserMailer.made_school_admin_link_school_email(self, school).deliver_now! if email.present?
+    end
+
+    def send_internal_tool_made_school_admin_change_school_email(new_school, existing_school)
+      InternalToolUserMailer.made_school_admin_change_school_email(self, new_school, existing_school).deliver_now! if email.present?
+    end
+
+    def send_internal_tool_district_admin_account_created_email(district_name)
+      InternalToolUserMailer.district_admin_account_created_email(self, district_name).deliver_now! if email.present?
+    end
+
+    def send_internal_tool_made_district_admin_email(district_name)
+      InternalToolUserMailer.made_district_admin_email(self, district_name).deliver_now! if email.present?
+    end
+  end
+end
