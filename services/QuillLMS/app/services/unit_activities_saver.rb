4ed858be4b0a2439c65a78d8d9fedff5c3df94b6
Brendan Shean <brendanshean@gmail.com>
2022-12-12 07:52:53 -0500

    Fix race condition with unit activities

--- services/QuillLMS/app/services/unit_activities_saver.rb
+++ services/QuillLMS/app/services/unit_activities_saver.rb
@@ -22,28 +22,29 @@ class UnitActivitiesSaver < ApplicationService
       activity_id = activity_data[:id].to_i
       due_date = activity_data[:due_date]
       publish_date = activity_data[:publish_date]
-      unit_activity = unit_activities.find { |ua| ua.activity_id == activity_id }
 
-      if unit_activity
-        unit_activity.save_new_attributes_and_adjust_dates!(
-          due_date: due_date || unit_activity.due_date,
-          publish_date: publish_date || unit_activity.publish_date,
-          order_number: order_number,
-          visible: true
-        )
-      else
-        UnitActivity.create!(
-          activity_id: activity_id,
-          due_date: due_date,
-          publish_date: publish_date,
-          order_number: order_number,
-          unit_id: unit_id
-        )
+      begin
+        unit_activity = UnitActivity.find_by(unit_id: unit_id, activity_id: activity_id)
+
+        if unit_activity
+          unit_activity.save_new_attributes_and_adjust_dates!(
+            due_date: due_date || unit_activity.due_date,
+            publish_date: publish_date || unit_activity.publish_date,
+            order_number: order_number,
+            visible: true
+          )
+        else
+          UnitActivity.create!(
+            activity_id: activity_id,
+            due_date: due_date,
+            publish_date: publish_date,
+            order_number: order_number,
+            unit_id: unit_id
+          )
+        end
+      rescue ActiveRecord::RecordNotUnique
+        retry
       end
     end
-  end
-
-  private def unit_activities
-    @unit_activities ||= UnitActivity.where(unit_id: unit_id)
   end
 end
