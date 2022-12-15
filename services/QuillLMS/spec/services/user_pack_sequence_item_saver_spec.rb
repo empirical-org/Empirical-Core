5594eb2d2094ab92dd7b874d2a0bee9939920119
Brendan Shean <brendanshean@gmail.com>
2022-12-15 08:04:55 -0500

    Fix calculation of locks for multiple pack_sequences

--- services/QuillLMS/spec/services/user_pack_sequence_item_saver_spec.rb
+++ services/QuillLMS/spec/services/user_pack_sequence_item_saver_spec.rb
@@ -8,8 +8,9 @@ RSpec.describe UserPackSequenceItemSaver do
   let(:classroom) { create(:classroom) }
   let(:user) { create(:student) }
 
-  let(:pack_sequence_item1) { create(:pack_sequence_item) }
-  let(:pack_sequence_item2) { create(:pack_sequence_item) }
+  let(:pack_sequence1) { create(:pack_sequence) }
+  let(:pack_sequence_item1) { create(:pack_sequence_item, pack_sequence: pack_sequence1) }
+  let(:pack_sequence_item2) { create(:pack_sequence_item, pack_sequence: pack_sequence1) }
 
   let(:locked) { described_class::LOCKED }
   let(:unlocked) { described_class::UNLOCKED }
@@ -23,7 +24,7 @@ RSpec.describe UserPackSequenceItemSaver do
   end
 
   context 'one pack_sequence_item with one result' do
-    let(:result1) { result(completed1, pack_sequence_item1.id) }
+    let(:result1) { result(completed1, pack_sequence1, pack_sequence_item1) }
     let(:results) { [result1] }
 
     context 'result is completed' do
@@ -42,8 +43,8 @@ RSpec.describe UserPackSequenceItemSaver do
   end
 
   context 'one pack_sequence_item with two results' do
-    let(:result1) { result(completed1, pack_sequence_item1.id) }
-    let(:result2) { result(completed2, pack_sequence_item1.id) }
+    let(:result1) { result(completed1, pack_sequence1, pack_sequence_item1) }
+    let(:result2) { result(completed2, pack_sequence1, pack_sequence_item1) }
     let(:results) { [result1, result2] }
 
     context 'results are completed, uncompleted' do
@@ -64,8 +65,8 @@ RSpec.describe UserPackSequenceItemSaver do
   end
 
   context 'two pack_sequences_items with one result each' do
-    let(:result1) { result(completed1, pack_sequence_item1.id) }
-    let(:result2) { result(completed2, pack_sequence_item2.id) }
+    let(:result1) { result(completed1, pack_sequence1, pack_sequence_item1) }
+    let(:result2) { result(completed2, pack_sequence1, pack_sequence_item2) }
     let(:results) { [result1, result2] }
 
     context 'results are uncompleted, completed' do
@@ -88,9 +89,9 @@ RSpec.describe UserPackSequenceItemSaver do
   end
 
   context 'two pack_sequence_items, one with two results and the other with one result' do
-    let(:result1) { result(completed1, pack_sequence_item1.id) }
-    let(:result2) { result(completed2, pack_sequence_item1.id) }
-    let(:result3) { result(completed3, pack_sequence_item2.id) }
+    let(:result1) { result(completed1, pack_sequence1, pack_sequence_item1) }
+    let(:result2) { result(completed2, pack_sequence1, pack_sequence_item1) }
+    let(:result3) { result(completed3, pack_sequence1, pack_sequence_item2) }
     let(:results) { [result1, result2, result3] }
 
     context 'results are uncompleted, completed, completed' do
@@ -104,10 +105,50 @@ RSpec.describe UserPackSequenceItemSaver do
     end
   end
 
-  def result(completed, pack_sequence_item_id)
+  context 'two pack_sequences' do
+    let(:pack_sequence2) { create(:pack_sequence) }
+    let(:pack_sequence_item3) { create(:pack_sequence_item, pack_sequence: pack_sequence2) }
+    let(:pack_sequence_item4) { create(:pack_sequence_item, pack_sequence: pack_sequence2) }
+
+    let(:result1) { result(completed1, pack_sequence1, pack_sequence_item1) }
+    let(:result2) { result(completed2, pack_sequence1, pack_sequence_item2) }
+    let(:result3) { result(completed3, pack_sequence2, pack_sequence_item3) }
+    let(:result4) { result(completed3, pack_sequence2, pack_sequence_item4) }
+    let(:results) { [result1, result2, result3, result4] }
+
+    context 'results are uncompleted, completed, completed, uncompleted' do
+      let(:completed1) { false }
+      let(:completed2) { true }
+      let(:completed3) { true }
+      let(:completed4) { false }
+
+      it { expect { subject }.to change(UserPackSequenceItem, :count).from(0).to(4) }
+      it { should_save_user_pack_sequence_item(pack_sequence_item1, unlocked) }
+      it { should_save_user_pack_sequence_item(pack_sequence_item2, locked) }
+      it { should_save_user_pack_sequence_item(pack_sequence_item3, unlocked) }
+      it { should_save_user_pack_sequence_item(pack_sequence_item4, unlocked) }
+    end
+
+    context 'results are uncompleted, completed, uncompleted, completed' do
+      let(:completed1) { false }
+      let(:completed2) { true }
+      let(:completed3) { false }
+      let(:completed4) { true }
+
+      it { expect { subject }.to change(UserPackSequenceItem, :count).from(0).to(4) }
+      it { should_save_user_pack_sequence_item(pack_sequence_item1, unlocked) }
+      it { should_save_user_pack_sequence_item(pack_sequence_item2, locked) }
+      it { should_save_user_pack_sequence_item(pack_sequence_item3, unlocked) }
+      it { should_save_user_pack_sequence_item(pack_sequence_item4, locked) }
+    end
+  end
+
+
+  def result(completed, pack_sequence, pack_sequence_item)
     {
       described_class::COMPLETED_KEY => completed,
-      described_class::PACK_SEQUENCE_ITEM_ID_KEY => pack_sequence_item_id
+      described_class::PACK_SEQUENCE_ID_KEY => pack_sequence.id,
+      described_class::PACK_SEQUENCE_ITEM_ID_KEY => pack_sequence_item.id
     }
   end
 
