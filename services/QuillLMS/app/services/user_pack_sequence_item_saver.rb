bb02694d607c7a175de75de4f11149ad92ec9a83
Brendan Shean <brendanshean@gmail.com>
2022-12-16 14:03:41 -0500

    rescue from race condition

--- services/QuillLMS/app/services/user_pack_sequence_item_saver.rb
+++ services/QuillLMS/app/services/user_pack_sequence_item_saver.rb
@@ -3,6 +3,8 @@
 class UserPackSequenceItemSaver < ApplicationService
   attr_reader :classroom_id, :user_id, :user_pack_sequence_item_statuses
 
+  class UserPackSequenceItemSaverError < StandardError; end
+
   LOCKED = UserPackSequenceItem::LOCKED
   UNLOCKED = UserPackSequenceItem::UNLOCKED
   COMPLETED_KEY = UserPackSequenceItemQuery::COMPLETED_KEY
@@ -28,22 +30,22 @@ class UserPackSequenceItemSaver < ApplicationService
     previous_packs_unfinished = false
     status = UNLOCKED
 
-    user_pack_sequence_item_results.each do |user_pack_sequence_item_result|
-      current_pack_sequence_id = user_pack_sequence_item_result[PACK_SEQUENCE_ID_KEY]
+    UserPackSequenceItemQuery.call(classroom_id, user_id).each do |result|
+      current_pack_sequence_id = result[PACK_SEQUENCE_ID_KEY]
 
       unless previous_pack_sequence_id == current_pack_sequence_id
         status = UNLOCKED
         previous_packs_unfinished = false
       end
 
-      current_pack_sequence_item_id = user_pack_sequence_item_result[PACK_SEQUENCE_ITEM_ID_KEY]
+      current_pack_sequence_item_id = result[PACK_SEQUENCE_ITEM_ID_KEY]
       pack_sequence_item_changed = previous_pack_sequence_item_id != current_pack_sequence_item_id
 
       status = LOCKED if previous_packs_unfinished && pack_sequence_item_changed
 
       user_pack_sequence_item_statuses[current_pack_sequence_item_id] = status
 
-      previous_packs_unfinished ||= !user_pack_sequence_item_result[COMPLETED_KEY]
+      previous_packs_unfinished ||= !result[COMPLETED_KEY]
 
       previous_pack_sequence_id = current_pack_sequence_id
       previous_pack_sequence_item_id = current_pack_sequence_item_id
@@ -52,13 +54,15 @@ class UserPackSequenceItemSaver < ApplicationService
 
   private def save_statuses
     user_pack_sequence_item_statuses.each_pair do |pack_sequence_item_id, status|
-      UserPackSequenceItem
-        .find_or_create_by!(pack_sequence_item_id: pack_sequence_item_id, user_id: user_id)
-        .tap { |upsi| upsi.update!(status: status) unless status == upsi.status }
-    end
-  end
+      begin
+        UserPackSequenceItem
+          .find_or_create_by!(pack_sequence_item_id: pack_sequence_item_id, user_id: user_id)
+          .tap { |upsi| upsi.update!(status: status) unless status == upsi.status }
+      rescue ActiveRecord::RecordInvalid => e
+        next if e.message == "Validation failed: Pack sequence item can't be blank"
 
-  private def user_pack_sequence_item_results
-    @user_pack_sequence_item_results ||= UserPackSequenceItemQuery.call(classroom_id, user_id)
+        raise UserPackSequenceItemSaverError, e.message
+      end
+    end
   end
 end
