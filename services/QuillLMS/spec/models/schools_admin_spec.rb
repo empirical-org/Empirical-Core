a90b3d9b97c07f64585d0332737dda56d85d3ea8
Emilia Friedberg <emiliafriedberg@gmail.com>
2022-12-20 11:24:40 -0500

    remove tests for old school admin email

--- services/QuillLMS/spec/models/schools_admin_spec.rb
+++ services/QuillLMS/spec/models/schools_admin_spec.rb
@@ -6,8 +6,6 @@ describe SchoolsAdmins, type: :model, redis: true do
   it { should belong_to(:school) }
   it { should belong_to(:user) }
 
-  it { is_expected.to callback(:send_admin_email).after(:create) }
-
   let(:user) { create(:user, email: 'test@quill.org') }
   let(:admins) { create(:schools_admins, user: user) }
 
@@ -17,9 +15,4 @@ describe SchoolsAdmins, type: :model, redis: true do
     end
   end
 
-  describe '#send_admin_email' do
-    it 'should kick off background job to send email' do
-      expect{ admins.send_admin_email }.to change(NewAdminEmailWorker.jobs, :size)
-    end
-  end
 end
