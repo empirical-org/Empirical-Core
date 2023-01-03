0a4c4ecfce2ed1ba1609d6ce2549bfee4d76b6a2
Emilia Friedberg <emiliafriedberg@gmail.com>
2023-01-03 10:10:02 -0500

    fix bad conditional

--- services/QuillLMS/app/models/unit_activity.rb
+++ services/QuillLMS/app/models/unit_activity.rb
@@ -159,7 +159,7 @@ class UnitActivity < ApplicationRecord
           MAX(acts.percentage) AS max_percentage,
           CASE WHEN
             false IN (unit.visible, ua.visible, cu.visible, acts.visible)
-            OR #{user_id.to_i} = ALL(cu.assigned_student_ids::int[])
+            OR NOT #{user_id.to_i} = ANY(cu.assigned_student_ids::int[])
           THEN true ELSE false END as archived,
           SUM(CASE WHEN pre_activity_sessions_classroom_units.id > 0 AND pre_activity_sessions.state = '#{ActivitySession::STATE_FINISHED}' THEN 1 ELSE 0 END) > 0 AS completed_pre_activity_session,
           SUM(CASE WHEN acts.state = '#{ActivitySession::STATE_FINISHED}' THEN 1 ELSE 0 END) > 0 AS #{ActivitySession::STATE_FINISHED_KEY},
