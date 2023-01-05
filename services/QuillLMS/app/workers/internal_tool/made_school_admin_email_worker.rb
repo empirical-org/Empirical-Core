aa4553401032328f574dc7d7977cfff59c3f7f60
Eric Adams <erictayloradams@gmail.com>
2023-01-05 17:02:46 -0300

    remove return

--- services/QuillLMS/app/workers/internal_tool/made_school_admin_email_worker.rb
+++ services/QuillLMS/app/workers/internal_tool/made_school_admin_email_worker.rb
@@ -6,8 +6,9 @@ class InternalTool::MadeSchoolAdminEmailWorker
   def perform(user_id, school_id)
     user = User.find_by(id: user_id)
     school_name = School.find_by(id: school_id)&.name
-
-    return unless user && school_name
+    puts "user: #{user}"
+    puts "school_name: #{school_name}"
+    # return unless user && school_name
 
     user.mailer_user.send_internal_tool_made_school_admin_email(school_name)
 
