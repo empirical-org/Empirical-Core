d8e7c442366ce7fe4f45feded6a2a733e05abb69
dandrabik <dandrabik@users.noreply.github.com>
2023-01-06 15:23:57 -0500

    Revert "Fire multiple events for each activity topic on activity completion (#10106)"
    This reverts commit 2168a04808c94f12fd3d90aa7606b38d631ebe96.

--- services/QuillLMS/app/models/segment_integration/activity.rb
+++ services/QuillLMS/app/models/segment_integration/activity.rb
@@ -13,7 +13,10 @@ module SegmentIntegration
       {
         **common_params,
         concepts: activity_categories.pluck(:name).join(", "),
-        content_partners: content_partners.pluck(:name).join(", ")
+        content_partners: content_partners.pluck(:name).join(", "),
+        first_topic: topics.first&.name,
+        second_topic: topics.second&.name,
+        third_topic: topics.third&.name
       }.reject {|_,v| v.nil? }
     end
 
