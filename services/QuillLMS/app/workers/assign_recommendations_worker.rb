3518303aa7d71f490afeec1512aa7d4bc9496af9
Brendan Shean <brendanshean@gmail.com>
2023-01-09 07:26:29 -0800

    Fix method call

--- services/QuillLMS/app/workers/assign_recommendations_worker.rb
+++ services/QuillLMS/app/workers/assign_recommendations_worker.rb
@@ -82,7 +82,7 @@ class AssignRecommendationsWorker
   end
 
   def track_recommendation_assignment(teacher, release_method)
-    Analyzer.new.track(
+    Analyzer.new.track_with_attributes(
       teacher,
       SegmentIo::BackgroundEvents::ASSIGN_RECOMMENDATIONS,
       properties: { release_method: release_method }
