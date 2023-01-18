b7b78e5e75a3fb6067db3e909c663302ec09fbd4
Cissy Yu <cissy@quill.org>
2023-01-18 10:44:42 +0800

    alex comments

--- services/QuillLMS/client/app/bundles/Teacher/components/premium/premium_features_data.ts
+++ services/QuillLMS/client/app/bundles/Teacher/components/premium/premium_features_data.ts
@@ -144,25 +144,29 @@ export const premiumFeatures = ({ independentPracticeActivityCount, diagnosticAc
         tooltipText: "Access 160+ best practices and implementation articles as needed to improve your understanding and implementation of Quill.",
       },
       {
-        label: "Guided onboarding",
-        school: true,
-        tooltipText: "Work with your assigned Senior Instructional Coach to get all users on your license set up quickly.",
-      },
-      {
-        label: "Two professional development sessions",
-        school: true,
-        tooltipText: "Assigned Senior Instructional Coach delivers PDs tailored to the needs of teachers.",
-      },
-      {
         label: "Priority technical support",
         school: true,
         tooltipText: "Support tickets identified with priority user status to resolve tech issues swiftly.",
       },
       {
+        label: "Quill Academy",
+        school: true,
+      },
+      {
         label: "Getting started training",
         schoolText: "Included with District Premium",
         tooltipText: "Work with your assigned Senior Instructional Coach for a live, hour-long virtual training to get teachers set up for success on Quill.",
-      }
+      },
+      {
+        label: "Guided onboarding",
+        schoolText: "Included with District Premium",
+        tooltipText: "Work with your assigned Senior Instructional Coach to get all users on your license set up quickly.",
+      },
+      {
+        label: "Two professional development sessions",
+        schoolText: "Included with District Premium",
+        tooltipText: "Assigned Senior Instructional Coach delivers PDs tailored to the needs of teachers.",
+      },
     ]
   }
 ])
