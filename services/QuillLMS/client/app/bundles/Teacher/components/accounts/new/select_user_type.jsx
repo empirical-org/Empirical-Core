92ef8e3e8f82b8ed393f08c084e2e5e34a4119b8
Eric Adams <erictayloradams@gmail.com>
2023-01-18 08:19:22 -0600

    Teacher Onboarding/ selecting and storing signup role (#10152)
    * update UI
    
    * add migration for role_selected_at_signup
    
    * add tests
    
    * skip school selection for individual contributor

--- services/QuillLMS/client/app/bundles/Teacher/components/accounts/new/select_user_type.jsx
+++ services/QuillLMS/client/app/bundles/Teacher/components/accounts/new/select_user_type.jsx
@@ -1,13 +1,14 @@
 import * as React from 'react'
-import { Card } from '../../../../Shared/index'
+import { Card, networkIcon, STUDENT, TEACHER, INDIVIDUAL_CONTRIBUTOR } from '../../../../Shared/index'
 
 import AssignActivityPackBanner from '../assignActivityPackBanner'
 import { requestPost, } from '../../../../../modules/request/index'
 
-const studentPencilImg = `${process.env.CDN_URL}/images/onboarding/student-pencil.svg`
-const teacherChalkboardImg = `${process.env.CDN_URL}/images/onboarding/teacher-chalkboard.svg`
+const studentPencilImg = `${process.env.CDN_URL}/images/onboarding/student-pencil-colored.svg`
+const teacherChalkboardImg = `${process.env.CDN_URL}/images/onboarding/teacher-chalkboard-colored.svg`
+const homeSchoolImg = `${process.env.CDN_URL}/images/onboarding/home-building-colored.svg`
 
-class SelectUserType extends React.Component {
+export class SelectUserType extends React.Component {
   setRoleOnSession = (role) => {
     requestPost(`${process.env.DEFAULT_URL}/account/role`, { role, },
       (body) => {
@@ -24,11 +25,15 @@ class SelectUserType extends React.Component {
   }
 
   handleClickStudent = () => {
-    this.setRoleOnSession('student');
+    this.setRoleOnSession(STUDENT);
   }
 
   handleClickTeacher = () => {
-    this.setRoleOnSession('teacher');
+    this.setRoleOnSession(TEACHER);
+  }
+
+  handleClickIndividualContributor = () => {
+    this.setRoleOnSession(INDIVIDUAL_CONTRIBUTOR);
   }
 
   handleKeyDownOnLogIn = (e) => {
@@ -46,6 +51,7 @@ class SelectUserType extends React.Component {
       <div>
         <AssignActivityPackBanner />
         <div className="container account-form" id='user-type'>
+          <img alt={networkIcon.alt} className="network-icon" src={networkIcon.src} />
           <h1>Welcome! Let&#39;s create your account. Choose one.</h1>
           <div className="quill-cards">
             <Card
@@ -56,11 +62,18 @@ class SelectUserType extends React.Component {
               text="Select this option to join your teacher’s class and complete assigned activities."
             />
             <Card
-              header="Teacher or Guardian"
+              header="K-12 Teacher"
               imgAlt="Chalkboard"
               imgSrc={teacherChalkboardImg}
               onClick={this.handleClickTeacher}
               text="Select this option to create classes, assign activities, and view reports."
+            />
+            <Card
+              header="Parent, Tutor or Caregiver"
+              imgAlt="home"
+              imgSrc={homeSchoolImg}
+              onClick={this.handleClickIndividualContributor}
+              text="Select this option to assign activities and view reports for your individual student(s)."
             />
           </div>
           <div className="agreements-and-link-to-login">
