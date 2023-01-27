74f456f8976b9b972e396a9565fdc01c04c5c0f1
Emilia Friedberg <emiliafriedberg@gmail.com>
2023-01-27 12:33:27 -0500

    tests

---
+++ services/QuillLMS/client/app/bundles/Teacher/components/blog_posts/__tests__/blog_post.test.jsx
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import { mount } from 'enzyme';
+
+import { blogPosts, blogPostWithBody, author, } from './data'
+
+import BlogPost from '../blog_post';
+
+const props = {
+  mostRecentPosts: blogPosts,
+  blogPost: blogPostWithBody,
+  author
+}
+
+describe('BlogPost component', () => {
+  it('renders', () => {
+    const wrapper = mount(<BlogPost {...props} />)
+    expect(wrapper).toMatchSnapshot()
+  })
+
+})
