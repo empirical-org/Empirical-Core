b45f3919e2cacc090b5c8fd0cc00b1b25a7ac52b
Eric Adams <erictayloradams@gmail.com>
2023-01-31 17:34:05 -0300

    update tests'

--- services/QuillLMS/spec/serializers/admin/teacher_serializer_spec.rb
+++ services/QuillLMS/spec/serializers/admin/teacher_serializer_spec.rb
@@ -26,21 +26,59 @@ describe Admin::TeacherSerializer do
   describe 'serializer properties' do
     let!(:school1) { create(:school) }
     let!(:school2) { create(:school) }
-    let!(:teacher) { create(:teacher) }
+    let!(:classroom) { create(:classroom_with_a_couple_students) }
+    let!(:teacher) { classroom.owner }
+    let!(:student1) { classroom.students.first }
+    let!(:student2) { classroom.students.last }
+    let!(:school_users1) { create(:schools_users, user: teacher, school: school2) }
+    let!(:schools_users2) { create(:schools_users, user: classroom.students.first, school: school2) }
+    let!(:schools_users3) { create(:schools_users, user: classroom.students.last, school: school2) }
+    let!(:schools_admins1) { create(:schools_admins, user: teacher, school: school1) }
+    let!(:schools_admins2) { create(:schools_admins, user: teacher, school: school2) }
+    let!(:unit) { create(:unit, user_id: teacher.id)}
+    let!(:student1) { classroom.students.first }
+    let!(:student2) { classroom.students.second }
+    let!(:time2) { Time.current }
+    let!(:time1) { time2 - (10.minutes) }
+    let!(:classroom_unit) { create(:classroom_unit, classroom_id: classroom.id, unit: unit, assigned_student_ids: classroom.students.ids) }
+    let!(:activity_session1) { create(:activity_session_without_concept_results,
+                                                  user: student1,
+                                                  state: 'finished',
+                                                  started_at: time1,
+                                                  completed_at: time2,
+                                                  classroom_unit: classroom_unit
+                                                  ) }
 
-    subject { described_class.new(teacher) }
+    let!(:activity_session2) { create(:activity_session_without_concept_results,
+                                                  user: student1,
+                                                  state: 'finished',
+                                                  started_at: time1,
+                                                  completed_at: time2,
+                                                  classroom_unit: classroom_unit
+                                                  ) }
+    let!(:activity_session3) { create(:activity_session_without_concept_results,
+                                                  user: student2,
+                                                  state: 'finished',
+                                                  started_at: time1,
+                                                  completed_at: time2,
+                                                  classroom_unit: classroom_unit
+                                                  ) }
+    let!(:activity_session3) { create(:activity_session_without_concept_results,
+                                                  user: student2,
+                                                  state: 'started',
+                                                  started_at: time1,
+                                                  completed_at: time2,
+                                                  classroom_unit: classroom_unit
+                                                  ) }
+    let!(:record_instance) { TeachersData.run([teacher.id])[0] }
 
-    it 'returns the expected "schools" payload' do
-       create(:schools_users, user: teacher, school: school2)
-       create(:schools_admins, user: teacher, school: school2)
-       create(:schools_admins, user: teacher, school: school1)
-       teacher.reload
+    subject { described_class.new(record_instance) }
 
-       expect(subject.schools).to eq([
-        { name: school2.name, id: school2.id, role: 'Admin' },
-        { name: school1.name, id: school1.id, role: 'Admin' }
-       ])
+    it 'returns the expected payload' do
+      expect(subject.schools.count).to eq(2)
+      expect(subject.number_of_students).to eq(classroom.students.count)
+      expect(subject.number_of_activities_completed).to eq(3)
+      expect(subject.time_spent).to eq("0 hours")
     end
-    # TODO: add tests for remaining properties
   end
 end
