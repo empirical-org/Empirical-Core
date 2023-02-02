bb3e11da608ddab7901bd8ff6f8a744f388c2c53
Thomas Robertson <thomas.robertson@quill.org>
2023-02-02 16:07:15 -0500

    Add code to ensure that User.role gets set SchoolsAdmins are created
    We want to make sure that however a user gets assigned as an admin, they are guaranteed to get set to the proper role

--- services/QuillLMS/spec/models/schools_admin_spec.rb
+++ services/QuillLMS/spec/models/schools_admin_spec.rb
@@ -7,11 +7,29 @@ describe SchoolsAdmins, type: :model, redis: true do
   it { should belong_to(:user) }
 
   let(:user) { create(:user, email: 'test@quill.org') }
-  let(:admins) { create(:schools_admins, user: user) }
 
   describe '#admin' do
+    let(:admins) { create(:schools_admins, user: user) }
+
     it 'should return the user associated' do
       expect(admins.admin).to eq(admins.user)
+    end
+  end
+
+  describe '#set_user_role' do
+    let(:school) { create(:school) }
+
+    it 'should set the User.role value to "admin" if it is not already set' do
+      expect(user).to receive(:update).with(role: User::ADMIN)
+
+      SchoolsAdmins.create(school: school, user: user)
+    end
+
+    it 'should not make any changes if the User.role is already "admin"' do
+      user.update(role: User::ADMIN)
+      expect(user).not_to receive(:update).with(role: User::ADMIN)
+
+      SchoolsAdmins.create(school: school, user: user)
     end
   end
 end
