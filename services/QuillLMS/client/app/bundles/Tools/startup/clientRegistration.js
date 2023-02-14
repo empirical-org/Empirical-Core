8e35cd3d1617dab8e591186874337442c379bd16
Eric Adams <erictayloradams@gmail.com>
2023-02-14 19:13:34 -0300

    update client registration

--- services/QuillLMS/client/app/bundles/Tools/startup/clientRegistration.js
+++ services/QuillLMS/client/app/bundles/Tools/startup/clientRegistration.js
@@ -2,7 +2,6 @@ import ReactOnRails from 'react-on-rails';
 import 'lazysizes';
 import 'lazysizes/plugins/parent-fit/ls.parent-fit';
 
-import QuestionsAndAnswersApp from './questionsAndAnswersAppClient.jsx'
 import EvidenceToolApp from './evidenceToolAppClient'
 import EvidenceHomeSectionApp from './evidenceHomeSectionAppClient'
 
