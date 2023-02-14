13b7d2d98340c9a9832436ff25a85137959cb206
Eric Adams <erictayloradams@gmail.com>
2023-02-14 11:40:25 -0300

    Speed optimizations/ convert shared SubNavbar to partial (#10267)
    * convert generic subheader to partial
    
    * remove import

--- services/QuillLMS/client/app/bundles/Public/startup/clientRegistration.js
+++ services/QuillLMS/client/app/bundles/Public/startup/clientRegistration.js
@@ -2,6 +2,5 @@ import 'lazysizes';
 import 'lazysizes/plugins/parent-fit/ls.parent-fit';
 import ReactOnRails from 'react-on-rails';
 import PremiumPricingGuideApp from './PremiumPricingGuideAppClient.jsx';
-import SubNavbarApp from './SubNavbarAppClient';
 
-ReactOnRails.register({ PremiumPricingGuideApp, SubNavbarApp });
+ReactOnRails.register({ PremiumPricingGuideApp });
--- services/QuillLMS/client/app/bundles/Tools/startup/clientRegistration.js
+++ services/QuillLMS/client/app/bundles/Tools/startup/clientRegistration.js
@@ -5,9 +5,8 @@ import 'lazysizes/plugins/parent-fit/ls.parent-fit';
 import QuestionsAndAnswersApp from './questionsAndAnswersAppClient.jsx'
 import EvidenceToolApp from './evidenceToolAppClient'
 import EvidenceHomeSectionApp from './evidenceHomeSectionAppClient'
-import SubNavbarApp from './subNavbarAppClient'
 
 require('../../../assets/styles/home.scss');
 require('../../Home/bootstrap_carousel.js');
 
-ReactOnRails.register({ QuestionsAndAnswersApp, EvidenceToolApp, EvidenceHomeSectionApp, SubNavbarApp });
+ReactOnRails.register({ QuestionsAndAnswersApp, EvidenceToolApp, EvidenceHomeSectionApp });
