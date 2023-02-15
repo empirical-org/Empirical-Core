629e20b591357f309b107715e20bd1739dfb79ab
Peter Kong <pkong@quill.org>
2023-02-15 08:07:57 -0700

    fixing syntax errors in existing code

--- services/QuillLMS/client/app/bundles/Login/startup/clientRegistration.js
+++ services/QuillLMS/client/app/bundles/Login/startup/clientRegistration.js
@@ -2,5 +2,5 @@ import 'lazysizes';
 import 'lazysizes/plugins/parent-fit/ls.parent-fit';
 import ReactOnRails from 'react-on-rails';
 import LoginFormApp from './LoginFormAppClient.jsx';
-
+console.log(".............login clientRegistration")
 ReactOnRails.register({ LoginFormApp, });
