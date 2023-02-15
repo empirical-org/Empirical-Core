a6e9b004a4971133ddb83a8f97b439d81ed01f6e
Emilia Friedberg <emiliafriedberg@gmail.com>
2023-02-15 08:47:47 -0500

    purely frontend changes

--- services/QuillLMS/client/app/bundles/Teacher/components/accounts/new/sign_up_teacher.jsx
+++ services/QuillLMS/client/app/bundles/Teacher/components/accounts/new/sign_up_teacher.jsx
@@ -9,7 +9,7 @@ import AgreementsAndLinkToLogin from './agreements_and_link_to_login'
 import AssignActivityPackBanner from '../assignActivityPackBanner'
 import { Input, } from '../../../../Shared/index'
 import { requestPost, } from '../../../../../modules/request/index'
-import { INDIVIDUAL_CONTRIBUTOR, ADMIN, } from '../../../../Shared/index';
+import { INDIVIDUAL_CONTRIBUTOR, ADMIN, TEACHER, } from '../../../../Shared/index';
 
 class SignUpTeacher extends React.Component {
   constructor(props) {
@@ -49,6 +49,7 @@ class SignUpTeacher extends React.Component {
   handleSubmit = (e) => {
     const { firstName, lastName, email, password, sendNewsletter, timesSubmitted, } = this.state
     e.preventDefault();
+    const isAdmin = window.location.href.includes(ADMIN)
 
     requestPost(
       `${process.env.DEFAULT_URL}/account`,
@@ -57,13 +58,12 @@ class SignUpTeacher extends React.Component {
           name: `${firstName} ${lastName}`,
           password,
           email,
-          role: 'teacher',
+          role: isAdmin ? ADMIN : TEACHER,
           send_newsletter: sendNewsletter,
         }
       },
       (body) => {
         const isIndividualContributor = window.location.href.includes(INDIVIDUAL_CONTRIBUTOR);
-        const isAdmin = window.location.href.includes(ADMIN)
 
         if (isAdmin) {
           window.location = '/sign-up/verify-email'
--- services/QuillLMS/client/app/bundles/Teacher/components/accounts/new/verify_email.tsx
+++ services/QuillLMS/client/app/bundles/Teacher/components/accounts/new/verify_email.tsx
@@ -1,26 +1,69 @@
 import * as React from 'react';
+import qs from 'qs'
 
 import AssignActivityPackBanner from '../assignActivityPackBanner'
-import { Snackbar, defaultSnackbarTimeout, } from '../../../../Shared/index'
+import { Snackbar, defaultSnackbarTimeout, Spinner, } from '../../../../Shared/index'
 import useSnackbarMonitor from '../../../../Shared/hooks/useSnackbarMonitor'
-import { requestPut, requestGet, } from '../../../../../modules/request/index'
+import { requestPut, requestPost, } from '../../../../../modules/request/index'
 
 const emailVerificationSrc = `${process.env.CDN_URL}/images/onboarding/email_verification.svg`
 
-const VerifySchool = ({ user, }) => {
+const VerifyEmail = ({ user, location, }) => {
+  const token = qs.parse(location.search.replace('?', '')).token
+
   const [showSnackbar, setShowSnackbar] = React.useState(false)
+  const [snackbarText, setSnackbarText] = React.useState('')
+  const [loading, setLoading] = React.useState(!!token)
 
   useSnackbarMonitor(showSnackbar, setShowSnackbar, defaultSnackbarTimeout)
 
+  React.useEffect(() => {
+    if (!user) {
+      redirectToLogin()
+    }
+    else if (token) {
+      verifyToken()
+    }
+  }, [])
+
+  function redirectToLogin() {
+    requestPost(
+      '/session/set_post_auth_redirect',
+      { post_auth_redirect: window.location.href },
+      () => window.location.href = '/session/new'
+    );
+  }
+
   function resendVerification() {
-    // TODO: actually resend verification
-    setShowSnackbar(true)
+    requestPut(`${process.env.DEFAULT_URL}/verify_emails/resend_verification_email`, {}, () => {
+      setSnackbarText(`We sent another verification email to ${user.email}`)
+      setShowSnackbar(true)
+    })
+  }
+
+  function verifyToken() {
+    requestPut(`${process.env.DEFAULT_URL}/verify_emails/verify_by_token`, { token, },
+      () => {
+        window.location.href = '/sign-up/select-sub-role'
+      },
+      (body) => {
+        setLoading(false)
+        setSnackbarText(body.error)
+        setShowSnackbar(true)
+      }
+    )
+  }
+
+  if (loading) {
+    return (
+      <Spinner />
+    )
   }
 
   return (
     <div>
       <AssignActivityPackBanner />
-      <Snackbar text={`We sent another verification email to ${user.email}`} visible={showSnackbar} />
+      <Snackbar text={snackbarText} visible={showSnackbar} />
       <div className="container account-form add-admin-info email-verification">
         <img alt="" src={emailVerificationSrc} />
         <h1>Thanks {user.name.split(' ')[0]}, you&#39;re almost done!</h1>
@@ -32,4 +75,4 @@ const VerifySchool = ({ user, }) => {
   )
 }
 
-export default VerifySchool
+export default VerifyEmail
