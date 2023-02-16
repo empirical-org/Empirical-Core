909df07107c317893a19008ac03988cae84c0985
Brendan Shean <brendanshean@gmail.com>
2023-02-16 08:58:49 -0500

    Add check for nil pack_sequence_item

--- services/QuillLMS/app/workers/save_user_pack_sequence_item_worker.rb
+++ services/QuillLMS/app/workers/save_user_pack_sequence_item_worker.rb
@@ -7,6 +7,7 @@ class SaveUserPackSequenceItemWorker
 
   def perform(pack_sequence_item_id, status, user_id)
     return if pack_sequence_item_id.nil? || user_id.nil?
+    return unless PackSequenceItem.exists?(id: pack_sequence_item_id)
 
     upsi = UserPackSequenceItem.create_or_find_by!(pack_sequence_item_id: pack_sequence_item_id, user_id: user_id)
     return if upsi.status == status
