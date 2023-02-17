a48d0a07bcb48196636e87f263ccbc85f29138a1
Peter Kong <pkong@quill.org>
2023-02-16 21:11:46 -0700

    updated to config, added rollup import plugin

--- services/QuillLMS/vite.config.ts
+++ services/QuillLMS/vite.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite'
 import RubyPlugin from 'vite-plugin-ruby'
 import react from '@vitejs/plugin-react'
+import friendlyTypeImports from 'rollup-plugin-friendly-type-imports';
 
 export default defineConfig({
   plugins: [
-    RubyPlugin(),
-    react()
+    friendlyTypeImports(),
+    RubyPlugin()
   ],
 })
 
