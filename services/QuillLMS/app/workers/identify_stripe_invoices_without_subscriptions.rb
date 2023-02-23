9a5a02610fa0dfb3dca19e1eaa0fbdcab18e1c4d
Thomas Robertson <thomas.robertson@quill.org>
2023-02-23 16:01:42 -0500

    Introduce a job and mailer to report on Stripe invoices w/o subscriptions

---
+++ services/QuillLMS/app/workers/identify_stripe_invoices_without_subscriptions.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+class IdentifyStripeInvoicesWithoutSubscriptions
+  include Sidekiq::Worker
+
+  # We don't care about invoices created before we began using this workflow 
+  INVOICE_START_EPOCH = DateTime.new(2023,1,1).to_i
+  RELEVANT_INVOICE_STATUSES = ['open', 'paid']
+
+  def perform
+    invoices = []
+    Stripe::Invoice.list({limit: 100, created: {gte: INVOICE_START_EPOCH}}).auto_paging_each do |invoice|
+      invoices.append(invoice) if RELEVANT_INVOICE_STATUSES.include?(invoice.status) && !Subscription.find_by(stripe_invoice_id: invoice.id)
+    end
+
+    invoice_payloads = invoices.map do |invoice|
+      {
+        id: invoice.id,
+        created: Time.at(invoice.created).to_datetime,
+        total: invoice.total / 100.0,
+        customer_name: invoice.customer_name,
+        customer_email: invoice.customer_email,
+        description: invoice.description
+      }
+    end
+
+    StripeIntegration::Mailer.invoices_without_subscriptions(invoice_payloads).deliver_now!
+  end
+end
