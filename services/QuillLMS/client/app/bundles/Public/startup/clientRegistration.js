ec3faa6c3e121f2494744821a97925287f1cedb6
Eric Adams <erictayloradams@gmail.com>
2023-02-24 13:18:33 -0300

    Speed optimizations/ part 4 (#10278)
    * remove react component to rails view
    
    * reconfigure bundles
    
    * update client registration
    
    * add lazyload script
    
    * update bundle imports
    
    * remove tool bundle
    
    * add Evidence widget animation
    
    * fix lazy load
    
    * additional lazy loading updates
    
    * fix carousels
    
    * add more webp images
    
    * move blogposts app to public bundle
    
    * final update
    
    * move BlogPostApp back to Teacher bundle
    
    * small css change
    
    * update snapshots
    
    * small css

--- services/QuillLMS/client/app/bundles/Public/startup/clientRegistration.js
+++ services/QuillLMS/client/app/bundles/Public/startup/clientRegistration.js
@@ -1,6 +1,14 @@
 import 'lazysizes';
 import 'lazysizes/plugins/parent-fit/ls.parent-fit';
 import ReactOnRails from 'react-on-rails';
-import PremiumPricingGuideApp from './PremiumPricingGuideAppClient.jsx';
 
-ReactOnRails.register({ PremiumPricingGuideApp });
+import PremiumPricingGuideApp from './premiumPricingGuideApp';
+import QuestionsAndAnswersApp from './questionsAndAnswersApp'
+import ApApp from './apApp'
+import PreApApp from './preApApp'
+import SpringBoardApp from './springBoardApp'
+
+require('../../../assets/styles/home.scss');
+require('../../Home/bootstrap_carousel.js');
+
+ReactOnRails.register({ PremiumPricingGuideApp, QuestionsAndAnswersApp, ApApp, PreApApp, SpringBoardApp });
--- services/QuillLMS/config/initializers/secure_headers.rb
+++ services/QuillLMS/config/initializers/secure_headers.rb
@@ -59,6 +59,7 @@ SecureHeaders::Configuration.default do |config|
       "https://*.coview.com",
       "https://*.sentry.io",
       "https://*.heapanalytics.com",
+      "https://cdn.jsdelivr.net/npm/vanilla-lazyload@17.8.3/dist/lazyload.min.js",
       "https://*.salesmate.io"
     ],
 
