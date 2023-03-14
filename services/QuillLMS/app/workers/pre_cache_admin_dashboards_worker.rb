467482ec2f08ce59c324c312c58b51207583f409
Brendan Shean <brendanshean@gmail.com>
2023-03-14 15:30:53 -0400

    Revert "Revert "Lms/cache admin reports (#10359)""
    This reverts commit 7e9cde9ee23464cb7138d1b51451e1049933381f.

--- services/QuillLMS/app/workers/pre_cache_admin_dashboards_worker.rb
+++ services/QuillLMS/app/workers/pre_cache_admin_dashboards_worker.rb
@@ -13,6 +13,9 @@ class PreCacheAdminDashboardsWorker
 
     active_admin_ids.each do |id|
       FindAdminUsersWorker.set(queue: SidekiqQueue::DEFAULT).perform_async(id)
+      FindDistrictActivityScoresWorker.set(queue: SidekiqQueue::DEFAULT).perform_async(id)
+      FindDistrictStandardsReportsWorker.set(queue: SidekiqQueue::DEFAULT).perform_async(id)
+      FindDistrictConceptReportsWorker.set(queue: SidekiqQueue::DEFAULT).perform_async(id)
     end
   end
 end
