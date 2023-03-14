4fd8ca860a73d79fb5399c8c23161bafb679b4db
Eric Adams <erictayloradams@gmail.com>
2023-03-14 17:38:28 -0300

    add public view for AP page

--- services/QuillLMS/client/app/bundles/Public/startup/clientRegistration.js
+++ services/QuillLMS/client/app/bundles/Public/startup/clientRegistration.js
@@ -4,7 +4,6 @@ import ReactOnRails from 'react-on-rails';
 
 import PremiumPricingGuideApp from './premiumPricingGuideApp';
 import QuestionsAndAnswersApp from './questionsAndAnswersApp'
-import ApApp from './apApp'
 import PreApApp from './preApApp'
 import SpringBoardApp from './springBoardApp'
 import DemoAccountBanner from './demoAccountBannerApp'
@@ -12,4 +11,4 @@ import DemoAccountBanner from './demoAccountBannerApp'
 require('../../../assets/styles/home.scss');
 require('../../Home/bootstrap_carousel.js');
 
-ReactOnRails.register({ PremiumPricingGuideApp, QuestionsAndAnswersApp, ApApp, PreApApp, SpringBoardApp, DemoAccountBanner });
+ReactOnRails.register({ PremiumPricingGuideApp, QuestionsAndAnswersApp, PreApApp, SpringBoardApp, DemoAccountBanner });
