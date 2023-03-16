644e7a3010afbc34642f0a162945efa8dbb3d053
Cissy Yu <cissy@quill.org>
2023-03-16 14:13:05 +0800

    linting

--- services/QuillLMS/app/models/bonus_days_calculator.rb
+++ services/QuillLMS/app/models/bonus_days_calculator.rb
@@ -18,9 +18,11 @@ class BonusDaysCalculator < ApplicationService
   end
 
   private def plan_ends
-    start.month < Subscription::SUMMER_CUTOFF_MONTH ?
-      start.change(month: Subscription::SUMMER_EXPIRATION_MONTH, day: Subscription::SUMMER_EXPIRATION_DAY) :
+    if start.month < Subscription::SUMMER_CUTOFF_MONTH
+      start.change(month: Subscription::SUMMER_EXPIRATION_MONTH, day: Subscription::SUMMER_EXPIRATION_DAY)
+    else
       start.change(month: Subscription::WINTER_EXPIRATION_MONTH, day: Subscription::WINTER_EXPIRATION_DAY)
+    end
   end
 end
 
