ceed591ed3f7cf80522ad13c82317f7947286bf7
Daniel Drabik <dan.drabik@gmail.com>
2023-03-17 11:54:37 -0400

    Wip data store.

--- services/QuillLMS/engines/evidence/app/models/evidence/text_generation.rb
+++ services/QuillLMS/engines/evidence/app/models/evidence/text_generation.rb
@@ -1,5 +1,8 @@
 module Evidence
   class TextGeneration < ApplicationRecord
+    store :data,
+      accessors: [:source_text, :language, :word, :word_list, :ml_prompt, :count, :index, :temperature, :label, :stem, :conjunction],
+      coder: JSON
     has_many :prompt_texts
 
     validates :name, presence: true
