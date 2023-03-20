1926ccac08f35f706686d4d03ad56228f27a6e7d
Thomas Robertson <thomas.robertson@quill.org>
2023-03-20 13:20:38 -0400

    Turn of admin pre-caching to avoid temp space issues on the database
    We'll revisit caching or perhaps BigQuery as a solution at a later date

--- services/QuillLMS/spec/workers/pre_cache_admin_dashboard_worker_spec.rb
+++ services/QuillLMS/spec/workers/pre_cache_admin_dashboard_worker_spec.rb
@@ -9,9 +9,6 @@ describe PreCacheAdminDashboardsWorker, type: :worker do
   let!(:current_admin2) { create(:user, last_sign_in: Time.current) }
   let!(:not_admin) { create(:user, last_sign_in: Time.current) }
   let(:mock_users_worker) {double(:mock_users_worker, perform_async: nil)}
-  let(:mock_activity_worker) {double(:mock_activity_worker, perform_async: nil)}
-  let(:mock_standards_worker) {double(:mock_standards_worker, perform_async: nil)}
-  let(:mock_concept_worker) {double(:mock_concept_worker, perform_async: nil)}
 
   before do
     create(:schools_admins, user: old_admin)
@@ -19,32 +16,11 @@ describe PreCacheAdminDashboardsWorker, type: :worker do
     create(:schools_admins, user: current_admin2)
 
     allow(FindAdminUsersWorker).to receive(:set).with(queue: SidekiqQueue::DEFAULT).and_return(mock_users_worker)
-    allow(FindDistrictActivityScoresWorker).to receive(:set).with(queue: SidekiqQueue::LOW, retry: 0).and_return(mock_activity_worker)
-    allow(FindDistrictStandardsReportsWorker).to receive(:set).with(queue: SidekiqQueue::LOW, retry: 0).and_return(mock_standards_worker)
-    allow(FindDistrictConceptReportsWorker).to receive(:set).with(queue: SidekiqQueue::LOW, retry: 0).and_return(mock_concept_worker)
   end
 
   it 'enqueues FindAdminUsersWorker for all active admins' do
     expect(mock_users_worker).to receive(:perform_async).with(current_admin1.id).once
     expect(mock_users_worker).to receive(:perform_async).with(current_admin2.id).once
-    worker.perform
-  end
-
-  it 'enqueues FindDistrictActivityScoresWorker for admins' do
-    expect(mock_activity_worker).to receive(:perform_async).with(current_admin1.id).once
-    expect(mock_activity_worker).to receive(:perform_async).with(current_admin2.id).once
-    worker.perform
-  end
-
-  it 'enqueues FindDistrictStandardsReportWorker for admins' do
-    expect(mock_standards_worker).to receive(:perform_async).with(current_admin1.id).once
-    expect(mock_standards_worker).to receive(:perform_async).with(current_admin2.id).once
-    worker.perform
-  end
-
-  it 'enqueues FindDistrictConceptReportsWorker for admins' do
-    expect(mock_concept_worker).to receive(:perform_async).with(current_admin1.id).once
-    expect(mock_concept_worker).to receive(:perform_async).with(current_admin2.id).once
     worker.perform
   end
 
