f280f70838509811b919256d06f4e73773ad826a
Thomas Robertson <thomas.robertson@quill.org>
2023-03-20 12:16:43 -0400

    Add a bunch of logging to chase down an intermittent error
    We haven't been able to consistently reproduce this, so hopefully getting access to console logging will make it easier for us to get some info when someone is in a place where they can consistently reproduce it.

--- services/QuillLMS/client/app/bundles/Teacher/components/assignment_flow/create_unit/stage2/classroom_card.tsx
+++ services/QuillLMS/client/app/bundles/Teacher/components/assignment_flow/create_unit/stage2/classroom_card.tsx
@@ -38,6 +38,13 @@ export default class ClassroomCard extends React.Component<ClassroomCardProps, C
   }
 
   handleClick = (e) => {
+    // This was added March 20, 2023 in an attempt to chase down an
+    // intermittent bug.  If it's been here for 6+ months, feel free to kill it
+    console.log('handleClick()')
+    console.log('e.target:')
+    console.log(e.target)
+    console.log(`this.studentSection.contains(e.target): ${this.studentSection.contains(e.target)}`)
+
     if (this.studentSection && this.studentSection.contains(e.target)) {
       this.setState({ isActive: true})
     } else {
@@ -82,6 +89,14 @@ export default class ClassroomCard extends React.Component<ClassroomCardProps, C
     }) : []
 
     const selectedStudents = options.filter(s => s.isSelected)
+
+    // This was added March 20, 2023 in an attempt to chase down an
+    // intermittent bug.  If it's been here for 6+ months, feel free to kill it
+    console.log('renderStudentSection()')
+    console.log(`selectedStudents.length: ${selectedStudents.length}`)
+    console.log(`emptyClassroomSelected: ${emptyClassroomSelected}`)
+    console.log(`isActive: ${isActive}`)
+    console.log(`options.length: ${options.length}`)
 
     if (!selectedStudents.length && !emptyClassroomSelected && !isActive) { return null }
 
