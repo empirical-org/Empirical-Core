3f785a90c37b467ee35837e5c53f296a0a556454
Anathomical <thomas.robertson@quill.org>
2023-03-23 13:59:37 -0400

    Update the Clever TeacherUpdater so that it doesn't make admins teachers (#10425)
    This code was overly-aggressive about assigning the "teacher" role to users when they logged in.  This change makes it so that they only get made a teacher if they're not already some other kind of "school staff" type of user (as proxied by having a role in TEACHER_INFO_ROLES).

--- services/QuillLMS/app/services/clever_integration/teacher_updater.rb
+++ services/QuillLMS/app/services/clever_integration/teacher_updater.rb
@@ -5,7 +5,7 @@ module CleverIntegration
     attr_reader :data, :teacher
 
     ACCOUNT_TYPE = ::User::CLEVER_ACCOUNT
-    ROLE = ::User::TEACHER
+    DEFAULT_ROLE = ::User::TEACHER
 
     def initialize(teacher, data)
       @teacher = teacher
@@ -17,8 +17,14 @@ module CleverIntegration
       teacher
     end
 
+    private def role
+      return @teacher.role if User::TEACHER_INFO_ROLES.include?(@teacher.role)
+
+      DEFAULT_ROLE
+    end
+
     private def teacher_attrs
-      data.merge(account_type: ACCOUNT_TYPE, google_id: nil, role: ROLE, signed_up_with_google: false)
+      data.merge(account_type: ACCOUNT_TYPE, google_id: nil, role: role, signed_up_with_google: false)
     end
 
     private def update
