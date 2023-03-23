6b05fd491203e55c3aa4ecd42c37b3af7f9e146c
Emilia Friedberg <emiliafriedberg@gmail.com>
2023-03-23 14:30:14 -0400

    fix test

--- services/QuillLMS/spec/workers/admin_received_admin_upgrade_request_from_teacher_analytics_worker_spec.rb
+++ services/QuillLMS/spec/workers/admin_received_admin_upgrade_request_from_teacher_analytics_worker_spec.rb
@@ -15,7 +15,7 @@ describe AdminReceivedAdminUpgradeRequestFromTeacherAnalyticsWorker do
     before { allow(SegmentAnalytics).to receive(:new) { analyzer } }
 
     it 'should track the event' do
-      expect(analyzer).to receive(:track_admin_received_admin_upgrade_request_form_teacher).with(admin, teacher, reason, new_user)
+      expect(analyzer).to receive(:track_admin_received_admin_upgrade_request_from_teacher).with(admin, teacher, reason, new_user)
       subject.perform(admin.id, teacher.id, reason, new_user)
     end
   end
