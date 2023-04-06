5fd4bf37ff9a17935e52a817dfcfe3daf2a88e75
Cissy Yu <cissy@quill.org>
2023-04-06 14:46:52 +0800

    styling fix right side

--- services/QuillLMS/client/app/bundles/Teacher/components/scorebook/premium_banners/premium_banner_builder.jsx
+++ services/QuillLMS/client/app/bundles/Teacher/components/scorebook/premium_banners/premium_banner_builder.jsx
@@ -87,15 +87,19 @@ export default class PremiumBannerBuilder extends React.Component {
           </div>
           <div className='row school-premium-banner'>
             <div className='container'>
-              <div className='col-md-9 col-xs-12 pull-left'>
-                <h4>Representing a School or District?</h4>
-                <span>Starting April 1st, when you purchase School or District Premium, you&apos;ll receive Quill Premium for free for the remainder of the school year!</span>
-              </div>
-              <div className='col-md-3 col-xs-12 pull-right'>
-                <div className='premium-button-box text-center'>
-                  <a href='/premium/request-school-quote'><button className='btn-orange' type='button'>Get in touch!</button></a>
+              <span>
+                <div className='row'>
+                  <div className='col-md-9 col-xs-12 pull-left'>
+                    <h4>Representing a School or District?</h4>
+                    <span>Starting April 1st, when you purchase School or District Premium, you&apos;ll receive Quill Premium for free for the remainder of the school year!</span>
+                  </div>
+                  <div className='col-md-3 col-xs-12 pull-right'>
+                    <div className='premium-button-box text-center'>
+                      <a href='/premium/request-school-quote'><button className='btn-orange' type='button'>Get in touch!</button></a>
+                    </div>
+                  </div>
                 </div>
-              </div>
+              </span>
             </div>
           </div>
         </div>
