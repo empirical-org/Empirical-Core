c035d77306229c0090dc9aa5f180a4aab326bd33
Brendan Shean <brendanshean@gmail.com>
2023-04-06 13:12:54 -0400

    Minor refactor

--- services/QuillLMS/app/services/auth/learn_worlds/sso_request.rb
+++ services/QuillLMS/app/services/auth/learn_worlds/sso_request.rb
@@ -8,7 +8,7 @@ module Auth
 
       attr_reader :user
 
-      delegate :email, :username, to: :user
+      delegate :email, :learn_worlds_account, :username, to: :user
 
       def initialize(user)
         @user = user
@@ -26,13 +26,13 @@ module Auth
       end
 
       private def data
-        user.learn_worlds_account ? existing_user_data : new_user_data
+        learn_worlds_account ? existing_user_data : new_user_data
       end
 
       private def existing_user_data
         {
           redirectURL: COURSES_ENDPOINT,
-          user_id: user.learn_worlds_account.external_id
+          user_id: learn_worlds_account.external_id
         }
       end
 
