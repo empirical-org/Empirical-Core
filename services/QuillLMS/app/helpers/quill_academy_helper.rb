f1ca96e209805b111c2af9b7260f67b32c7bb464
Eric Adams <erictayloradams@gmail.com>
2023-04-10 15:18:13 -0400

    add category and content cards

---
+++ services/QuillLMS/app/helpers/quill_academy_helper.rb
@@ -0,0 +1,69 @@
+# frozen_string_literal: true
+
+module QuillAcademyHelper
+  AVAILABLE = "Available now"
+  COMING_SOON = "Coming SY '22-'23"
+  JUMPSTART_WITH_QUILL = "Jump Start with Quill"
+  PLANNING_AND_IMPLEMENTING_A_DIAGNOSTICS_STRATEGY = "Planning & Implementing a Diagnostics Strategy"
+  QUILLS_INDEPENDENT_TOOLS = "Quill's Independent Tools: Grammar, Connect, & Proofreader"
+  USING_QUILL_LESSONS = "Using Quill Lessons"
+  USING_QUILL_READING_FOR_EVIDENCE = "Using Quill Reading for Evidence"
+  UNDERSTANDING_QUILL_DATA_REPORTS = "Understanding Quill Data Reports"
+  DATA_AND_INSTRUCTION_BEST_PRACTICES = "Data & Instruction Best Practices"
+  EXPLORING_THE_QUILL_ACTIVITY_LIBRARY = "Exploring the Quill Activity Library"
+  IMPROVING_SENTENCE_FLUENCY = "Improving Sentence Fluency"
+  ENCOURAGING_AND_EMPOWERING_YOUR_WRITERS = "Encouraging and Empowering Your Writers"
+  USING_QUILL_TO_SUPPORT_STUDENTS_EXCEPTIONAL_LEARNERS = "Using Quill to Support Students Exceptional Learners"
+  USING_QUILL_TO_SUPPORT_ENGLISH_LANGUAGE_LEARNERS = "Using Quill to Support English Language Learners"
+
+  COURSES = {
+    JUMPSTART_WITH_QUILL => {
+      header: JUMPSTART_WITH_QUILL,
+      text: "This course leads you through the initial set-up of your account and classes, understanding your dashboard, and the possibilities for future assignments."
+    },
+    PLANNING_AND_IMPLEMENTING_A_DIAGNOSTICS_STRATEGY => {
+      header: PLANNING_AND_IMPLEMENTING_A_DIAGNOSTICS_STRATEGY,
+      text: "Learn how to create and launch a strong diagnostic strategy that helps you identify and target student grammar needs. This course covers the selection and administering of a diagnostic, plus the interpreting and action on the diagnostic results."
+    },
+    QUILLS_INDEPENDENT_TOOLS => {
+      header: QUILLS_INDEPENDENT_TOOLS,
+      text: "Learn more about our independent activity grammar tools that unlock the power of feedback."
+    },
+    USING_QUILL_LESSONS => {
+      header: USING_QUILL_LESSONS,
+      text: "Learn more about Quill Lessons which is our interactive writing tool that helps teachers lead students through live grammar lessons. In this course, you will learn how to use and implement the tool to maximize group learning."
+    },
+    USING_QUILL_READING_FOR_EVIDENCE => {
+      header: USING_QUILL_READING_FOR_EVIDENCE,
+      text: "Learn more about Quill Reading for Evidence which provides students with nonfiction texts paired with AI-powered writing prompts, instead of multiple-choice questions, to enable deeper thinking."
+    },
+    UNDERSTANDING_QUILL_DATA_REPORTS => {
+      header: UNDERSTANDING_QUILL_DATA_REPORTS,
+      text: "Quill offers a range of student data reports to help you make sense of student performance. In this course, you will learn how to navigate these reports."
+    },
+    DATA_AND_INSTRUCTION_BEST_PRACTICES => {
+      header: DATA_AND_INSTRUCTION_BEST_PRACTICES,
+      text: "Explore strategies to intentionally embed Quill into your instructional plans and the data reports that can provide immediate insight into student learning."
+    },
+    EXPLORING_THE_QUILL_ACTIVITY_LIBRARY => {
+      header: EXPLORING_THE_QUILL_ACTIVITY_LIBRARY,
+      text: "Explore the different pathways to assigning the just-right content to your students, including pre-made activity packs and curating your own packs using the Quill activity library."
+    },
+    IMPROVING_SENTENCE_FLUENCY => {
+      header: IMPROVING_SENTENCE_FLUENCY,
+      text: "Explore ways to expand students' work on the Quill platform to improve their sentence fluency in their writing beyond Quill."
+    },
+    ENCOURAGING_AND_EMPOWERING_YOUR_WRITERS => {
+      header: ENCOURAGING_AND_EMPOWERING_YOUR_WRITERS,
+      text: "Explore ways to encourage and increase students' writing self-efficacy using Quill's tools beyond extrinsic carrot-and-stick approaches."
+    },
+    USING_QUILL_TO_SUPPORT_STUDENTS_EXCEPTIONAL_LEARNERS => {
+      header: USING_QUILL_TO_SUPPORT_STUDENTS_EXCEPTIONAL_LEARNERS,
+      text: "Learn how to modify Quill's learning plans for your students with IEPs, provide targeted support, and track students' growth and progress."
+    },
+    USING_QUILL_TO_SUPPORT_ENGLISH_LANGUAGE_LEARNERS => {
+      header: USING_QUILL_TO_SUPPORT_ENGLISH_LANGUAGE_LEARNERS,
+      text: "Explore Quill's content created specifically for English Language Learners and learn strategies to utilize these resources to support students' written language acquisition."
+    }
+  }
+end
