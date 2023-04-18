cff9724df43d05211d69ef7849178274996a8045
Anathomical <thomas.robertson@quill.org>
2023-04-18 09:33:56 -0400

    evidence/add-positive-automl-concept-results (#10443)
    * Modify Evidence conceptResult generation to include topic-optimals
    
    Basically, we want to include an optimal concept result for the AutoML concept when specific feedback types are received.  Specifically, if a feedback type is lower priority than AutoML, we want to flag that AutoML passed by generating an optimal ConceptResult.
    
    * Refactor front-end to use API call to get topic optimal data
    
    * Add a backend API endpoint to produce topic-optimal data
    
    * Add a spec for the new topic_optimal_info endpoint
    
    * Remove extraneous file that's not part of this PR
    
    * Mock new action in container test so that it doesn't crash

--- services/QuillLMS/client/app/bundles/Evidence/libs/conceptResults.ts
+++ services/QuillLMS/client/app/bundles/Evidence/libs/conceptResults.ts
@@ -9,7 +9,7 @@ export const ATTEMPTS_TO_SCORE = {
   5: 0.0
 }
 
-export const generateConceptResults = (currentActivity, submittedResponses) => {
+export const generateConceptResults = (currentActivity, submittedResponses, topicOptimalData) => {
   const conjunctionToQuestionNumber = {
     because: 1,
     but: 2,
@@ -22,18 +22,31 @@ export const generateConceptResults = (currentActivity, submittedResponses) => {
     const prompt = Object.values(currentActivity.prompts).filter((prompt) => prompt.id == promptID)[0]
     responses.forEach((response, index) => {
       const attempt = index + 1
+      const conceptResultMetadata = {
+        answer: response.entry,
+        attemptNumber: attempt,
+        correct: response.optimal ? 1 : 0,
+        directions: (attempt == 1) ? DIRECTIONS : responses[index - 1].feedback,
+        prompt: prompt.text,
+        questionNumber: conjunctionToQuestionNumber[prompt.conjunction],
+        questionScore: ATTEMPTS_TO_SCORE[responses.length],
+      }
+
+      if (topicOptimalData && topicOptimalData.rule_types.includes(response.feedback_type)) {
+        conceptResults.push({
+          concept_uid: topicOptimalData.concept_uids[prompt.id],
+          question_type: 'comprehension',
+          metadata: {
+            ...conceptResultMetadata,
+            ...{correct: 1}
+          }
+        })
+      }
+
       conceptResults.push({
         concept_uid: response.concept_uid,
         question_type: 'comprehension',
-        metadata: {
-          answer: response.entry,
-          attemptNumber: attempt,
-          correct: response.optimal ? 1 : 0,
-          directions: (attempt == 1) ? DIRECTIONS : responses[index - 1].feedback,
-          prompt: prompt.text,
-          questionNumber: conjunctionToQuestionNumber[prompt.conjunction],
-          questionScore: ATTEMPTS_TO_SCORE[responses.length],
-        }
+        metadata: conceptResultMetadata
       })
     })
   }
