8e25658af29383566c180b36657736c56a047080
Daniel Drabik <dan.drabik@gmail.com>
2023-04-19 17:28:04 -0400

    Rename migration.

--- services/QuillLMS/db/migrate/20230413215936_create_evidence_text_generations.evidence.rb
+++ services/QuillLMS/db/migrate/20230413215936_create_evidence_text_generations.evidence.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 # This migration comes from evidence (originally 20230306215123)
-class CreateEvidencePromptTextGenerations < ActiveRecord::Migration[6.1]
+class CreateEvidenceTextGenerations < ActiveRecord::Migration[6.1]
   def change
     create_table :evidence_text_generations do |t|
       t.string :type, null: false
--- services/QuillLMS/engines/evidence/db/migrate/20230306215123_create_evidence_text_generations.rb
+++ services/QuillLMS/engines/evidence/db/migrate/20230306215123_create_evidence_text_generations.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class CreateEvidencePromptTextGenerations < ActiveRecord::Migration[6.1]
+class CreateEvidenceTextGenerations < ActiveRecord::Migration[6.1]
   def change
     create_table :evidence_text_generations do |t|
       t.string :type, null: false
