b372c6a43d62f5a90c421823b9e0193b124ccf55
Brendan Shean <brendanshean@gmail.com>
2023-04-26 11:33:28 -0400

    Fix bug with teacher_admin_subscriptions_url

--- services/QuillLMS/spec/requests/stripe_integration/subscription_checkout_sessions_controller_spec.rb
+++ services/QuillLMS/spec/requests/stripe_integration/subscription_checkout_sessions_controller_spec.rb
@@ -9,26 +9,30 @@ RSpec.describe StripeIntegration::SubscriptionCheckoutSessionsController, type: 
   describe '#create' do
     let(:stripe_checkout_session) { create(:stripe_checkout_session) }
     let(:redirect_url) { stripe_checkout_session.url }
-    let(:params) { { customer_email: customer_email, stripe_price_id: stripe_price_id } }
+    let(:params) { { customer_email: customer_email, stripe_price_id: stripe_price_id, school_ids: school_ids } }
     let(:url) { '/stripe_integration/subscription_checkout_sessions' }
+    let(:school_ids) { nil }
 
     subject { post url, params: params, as: :json }
 
     context 'happy path' do
-      before { allow(StripeCheckoutSession).to receive(:custom_find_or_create_by!).and_return(stripe_checkout_session) }
+      before { allow(Stripe::Checkout::Session).to receive(:create).and_return(stripe_checkout_session) }
 
-      it 'creates a stripe checkout session and provides a redirect' do
-        subject
-        expect(response.body).to eq({ redirect_url: redirect_url }.to_json)
-      end
+      it { should_create_stripe_checkout_session_and_provide_redirect }
 
       context 'customer already has stripe_customer_id attached' do
         before { customer.update(stripe_customer_id: stripe_customer_id) }
 
-        it 'creates a stripe checkout session and provides a redirect' do
-          subject
-          expect(response.body).to eq({ redirect_url: redirect_url }.to_json)
-        end
+        it { should_create_stripe_checkout_session_and_provide_redirect }
+      end
+
+      context 'school ids are provided and customer is an admin' do
+        let(:school) { create(:school) }
+        let(:school_ids) { [school.id].to_json }
+
+        before { customer.update(role: 'admin') }
+
+        it { should_create_stripe_checkout_session_and_provide_redirect }
       end
     end
 
@@ -37,5 +41,10 @@ RSpec.describe StripeIntegration::SubscriptionCheckoutSessionsController, type: 
 
       it { expect { subject }.to raise_error described_class::CustomerNotFoundError }
     end
+  end
+
+  def should_create_stripe_checkout_session_and_provide_redirect
+    subject
+    expect(response.body).to eq({ redirect_url: redirect_url }.to_json)
   end
 end
