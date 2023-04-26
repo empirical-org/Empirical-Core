725bbcf2a0de5dd4f005a67a271fff6f96ea1ae7
Thomas Robertson <thomas.robertson@quill.org>
2023-04-26 11:28:17 -0400

    Add some missing annotations to factories

--- services/QuillLMS/spec/factories/teacher_notification/student_completed_all_assigned_activities.rb
+++ services/QuillLMS/spec/factories/teacher_notification/student_completed_all_assigned_activities.rb
@@ -1,5 +1,25 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: teacher_notifications
+#
+#  id            :bigint           not null, primary key
+#  email_sent    :datetime
+#  message_attrs :jsonb
+#  type          :text
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#  user_id       :bigint           not null
+#
+# Indexes
+#
+#  index_teacher_notifications_on_user_id  (user_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (user_id => users.id)
+#
 FactoryBot.define do
   factory :teacher_notification_student_completed_all_assigned_activities, class: 'TeacherNotification::StudentCompletedAllAssignedActivities' do
     user
