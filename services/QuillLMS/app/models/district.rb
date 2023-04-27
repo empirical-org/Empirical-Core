4b8e3470847c4ffe66cb1c5bbee7fbc8c06b162f
Cissy Yu <cissy@quill.org>
2023-04-27 11:15:43 +0800

    first draft changes

--- services/QuillLMS/app/models/district.rb
+++ services/QuillLMS/app/models/district.rb
@@ -67,8 +67,34 @@ class District < ApplicationRecord
         total_students: total_students,
         total_schools: total_schools,
         **vitally_diagnostic_rollups,
-        **vitally_subscription_rollups
+        **vitally_subscription_rollups,
+        **vitally_activities_and_students_rollups
       }
+    }
+  end
+
+  def vitally_activities_and_students_rollups
+    current_time = Time.current
+    school_year_start = School.school_year_start(current_time)
+    last_school_year_start = School.school_year_start(current_time - 1.year)
+    active_students_this_year = active_students(school_year_start, current_time)
+    active_students_last_year = active_students(last_school_year_start, school_year_start)
+    active_students_all_time = active_students
+    activities_completed_this_year = activities_completed(school_year_start, current_time)
+    activities_completed_last_year = activities_completed(last_school_year_start, school_year_start)
+    activities_completed_all_time = activities_completed
+
+    {
+      active_students_this_year: active_students_this_year,
+      active_students_last_year: active_students_last_year,
+      active_students_all_time: active_students_all_time,
+      activities_completed_this_year: activities_completed_this_year,
+      activities_completed_last_year: activities_completed_last_year,
+      activites_completed_all_time: activities_completed_all_time,
+      activities_completed_per_student_this_year: active_students_this_year > 0 ? ((activities_completed_this_year.to_f / active_students_this_year).round(2)) : 0,
+      activites_completed_per_student_last_year: active_students_last_year > 0 ? ((activities_completed_last_year.to_f / active_students_last_year).round(2)) : 0,
+      activities_completed_per_student_all_time: active_students_all_time > 0 ? ((activities_completed_all_time.to_f / active_students_all_time).round(2)) : 0,
+      last_active_time: last_active_time,
     }
   end
 
@@ -135,6 +161,62 @@ class District < ApplicationRecord
 
   def premium?
     subscription&.present?
+  end
+
+  def active_students(start_date: nil, end_date: nil)
+
+    # use raw SQL to bypass scope limits (visible: true) on classrooms
+    active_students = ActivitySession.select(:user_id)
+      .distinct
+      .joins("JOIN classroom_units on classroom_units.id=activity_sessions.classroom_unit_id")
+      .joins("JOIN classrooms ON classrooms.id=classroom_units.classroom_id")
+      .joins("JOIN classrooms_teachers ON classrooms_teachers.classroom_id=classrooms.id")
+      .joins("JOIN schools_users ON schools_users.user_id = classrooms_teachers.user_id")
+      .joins("JOIN schools ON schools_users.school_id=schools.id")
+      .joins("JOIN districts ON schools.district_id = districts.id")
+      .where(state: 'finished').where('districts.id = ?', id)
+
+    if start_date.present?
+      active_students = active_students.where("activity_sessions.updated_at >= ?", start_date)
+    end
+
+    if end_date.present?
+      active_students = active_students.where("activity_sessions.updated_at <= ?", end_date)
+    end
+
+    active_students
+  end
+
+  def activities_completed(start_date: nil, end_date: nil)
+
+    # use raw SQL to bypass scope limits (visible: true) on classrooms
+    activities_completed = ClassroomsTeacher.joins("JOIN users ON users.id=classrooms_teachers.user_id")
+      .joins("JOIN schools_users ON schools_users.user_id=users.id")
+      .joins("JOIN schools ON schools.id=schools_users.school_id")
+      .joins("JOIN classrooms ON classrooms.id=classrooms_teachers.classroom_id")
+      .joins("JOIN classroom_units ON classroom_units.classroom_id=classrooms.id")
+      .joins("JOIN activity_sessions ON activity_sessions.classroom_unit_id=classroom_units.id")
+      .joins("JOIN districts ON schools.district_id = districts.id")
+      .where('districts.id = ?', id)
+      .where('activity_sessions.state = ?', 'finished')
+
+    if start_date.present?
+      activities_completed = activities_completed.where("activity_sessions.updated_at >= ?", start_date)
+    end
+
+    if end_date.present?
+      activities_completed = activities_completed.where("activity_sessions.updated_at <= ?", end_date)
+    end
+
+    activities_completed
+  end
+
+  def last_active_time
+    User.joins("JOIN schools_users ON schools_users.user_id = users.id")
+      .joins("JOIN districts ON schools.district_id = districts.id")
+      .where('districts.id = ?', id)
+      .order(:last_sign_in, :desc)
+      .first.last_sign_in
   end
 
   private def latest_subscription
