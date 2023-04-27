984b45d0aed36f0cc97622324fdf4105d238992f
Brendan Shean <brendanshean@gmail.com>
2023-04-27 11:10:18 -0400

    Add backend tests

--- services/QuillLMS/app/controllers/canvas_instances_controller.rb
+++ services/QuillLMS/app/controllers/canvas_instances_controller.rb
@@ -4,30 +4,25 @@ class CanvasInstancesController < ApplicationController
   before_action :admin!
 
   def create
-    if canvas_instance.errors.any?
-      render json: canvas_instance.errors, status: :unprocessable_entity
-    elsif canvas_config.errors.any?
-      render json: canvas_config.errors, status: :unprocessable_entity
-    elsif canvas_instance_schools_errors
-      render json: canvas_instance_schools.errors, status: :unprocessable_entity
-    else
-      render json: canvas_instance, status: :created
-    end
+    render json: canvas_objects, status: :created
+  rescue ActiveRecord::NotNullViolation, ActiveRecord::RecordInvalid => e
+    render json: { errors: e }, status: :unprocessable_entity
   end
 
   private def canvas_instance
-    @canvas_instance ||= CanvasInstance.find_or_create_by(canvas_instance_params)
+    @canvas_instance ||= CanvasInstance.find_or_create_by!(canvas_instance_params)
   end
 
+  # No find_or_create_by since find requires searching of encrypted values
   private def canvas_config
-    @canvas_config ||= canvas_instance.canvas_configs.create(canvas_config_params)
-    # change log for canvas config creation with admin_id
+    @canvas_config ||= canvas_instance.canvas_configs.create!(canvas_config_params)
   end
 
   private def canvas_instance_schools
-    @canvas_instance_schools ||= school_ids.each do |school_id|
-      canvas_instance.canvas_school_instances.find_or_create_by(school_id: school_id)
-    end
+    @canvas_instance_schools ||=
+      School
+        .where(id: school_ids)
+        .map { |school| canvas_instance.canvas_school_instances.find_or_create_by!(school: school) }
   end
 
   private def canvas_config_params
@@ -46,6 +41,16 @@ class CanvasInstancesController < ApplicationController
     params
       .require(:canvas_instance_schools)
       .permit(:school_ids)
+  end
+
+  private def canvas_objects
+    ActiveRecord::Base.transaction do
+      {
+        canvas_instance: canvas_instance,
+        canvas_config: canvas_config
+        # canvas_instance_schools: canvas_instance_schools
+      }
+    end
   end
 
   private def school_ids
--- services/QuillLMS/spec/requests/canvas_instances_controller_spec.rb
+++ services/QuillLMS/spec/requests/canvas_instances_controller_spec.rb
@@ -26,13 +26,77 @@ RSpec.describe CanvasInstancesController do
 
   context 'user is an admin' do
     let(:user) { create(:admin) }
+    let(:school_ids) { [school.id] }
+
+    let(:valid_params) do
+      {
+        canvas_instance: attributes_for(:canvas_instance),
+        canvas_config: attributes_for(:canvas_config),
+        canvas_instance_schools: school_ids
+      }
+    end
 
     context 'valid params' do
+      let(:params) { valid_params }
+
+      it { should_return_created }
+      it { expect { subject}.to change(CanvasInstance, :count).from(0).to(1) }
+      it { expect { subject}.to change(CanvasConfig, :count).from(0).to(1) }
+      it { expect { subject}.to change(CanvasInstanceSchool, :count).from(0).to(1) }
+
+      context 'school_ids is empty' do
+        let(:school_ids) { [] }
+
+        it { expect { subject}.to change(CanvasInstance, :count).from(0).to(1) }
+        it { expect { subject}.to change(CanvasConfig, :count).from(0).to(1) }
+        it { expect { subject}.not_to change(CanvasInstanceSchool, :count) }
+      end
     end
 
     context 'invalid params' do
-      context 'nil url' do
-        it expect(subject).to render_template(:new)
+      context 'empty url' do
+        let(:params) { valid_params.merge(canvas_instance: { url: '' }) }
+
+        it { should_return_unprocessable_entity }
+        it { should_not_create_canvas_objects }
       end
+
+      context 'invalid url' do
+        let(:params) { valid_params.merge(canvas_instance: { url: 'not-a-url' }) }
+
+        it { should_return_unprocessable_entity }
+        it { should_not_create_canvas_objects }
+      end
+
+      context 'empty client_id' do
+        let(:params) { valid_params.merge(canvas_config: { client_id: '' }) }
+
+        it { should_return_unprocessable_entity }
+        it { should_not_create_canvas_objects }
+      end
+
+      context 'empty client_secret' do
+        let(:params) { valid_params.merge(canvas_config: { client_secret: '' }) }
+
+        it { should_return_unprocessable_entity }
+        it { should_not_create_canvas_objects }
+      end
+    end
+  end
+
+  def should_return_unprocessable_entity
+    subject
+    expect(response).to have_http_status(:unprocessable_entity)
+  end
+
+  def should_return_created
+    subject
+    expect(response).to have_http_status(:created)
+  end
+
+  def should_not_create_canvas_objects
+    expect(CanvasInstance.count).to eq(0)
+    expect(CanvasConfig.count).to eq(0)
+    expect(CanvasInstanceSchool.count).to eq(0)
   end
 end
