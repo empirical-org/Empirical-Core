f375793d8587a6ec367c6ccb6fa9c3b1a96e470b
Thomas Robertson <thomas.robertson@quill.org>
2023-05-02 14:10:48 -0400

    Add a worker and spec to actually send email notifications

---
+++ services/QuillLMS/app/workers/teacher_notifications/send_rollup_email_worker.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+module TeacherNotifications
+  class SendRollupEmailWorker
+    include Sidekiq::Worker
+  
+    def perform(user_id)
+      user = User.find_by(id: user_id)
+      notifications = TeacherNotification.where(user: user, email_sent: nil)
+      return if notifications.empty?
+
+      TeacherNotifications::RollupMailer.rollup(user, notifications).deliver_now!
+      notifications.update_all(email_sent: DateTime.current)
+    end
+  end
+end
