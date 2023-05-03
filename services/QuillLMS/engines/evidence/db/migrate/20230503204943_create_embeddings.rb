3bf8f87118f7c1e2d62033c912d5fcd3e071585f
Daniel Drabik <dan.drabik@gmail.com>
2023-05-03 17:04:53 -0400

    Add test migration for embeddings.

---
+++ services/QuillLMS/engines/evidence/db/migrate/20230503204943_create_embeddings.rb
@@ -0,0 +1,20 @@
+class CreateEmbeddings < ActiveRecord::Migration[6.1]
+  def up
+    execute <<-SQL
+      CREATE TABLE embeddings (
+        id SERIAL PRIMARY KEY,
+        prompt_id INTEGER NOT NULL,
+        text TEXT NOT NULL,
+        label TEXT NOT NULL,
+        embeddings vector(1536) NOT NULL
+      );
+      CREATE INDEX idx_embeddings_embeddings ON embeddings USING ivfflat(embeddings);
+    SQL
+  end
+
+  def down
+    execute <<-SQL
+      DROP TABLE embeddings;
+    SQL
+  end
+end
