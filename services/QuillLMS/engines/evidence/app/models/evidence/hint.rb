7628b27c45ed8750dcfdf0b8a74fd81523e079c2
Thomas Robertson <thomas.robertson@quill.org>
2023-05-10 15:32:18 -0400

    Add Annotate gem to Evidence because it's nice to have

--- services/QuillLMS/engines/evidence/app/models/evidence/hint.rb
+++ services/QuillLMS/engines/evidence/app/models/evidence/hint.rb
@@ -1,5 +1,18 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: evidence_hints
+#
+#  id             :bigint           not null, primary key
+#  explanation    :string           not null
+#  image_link     :string           not null
+#  image_alt_text :string           not null
+#  rule_id        :bigint
+#  created_at     :datetime         not null
+#  updated_at     :datetime         not null
+#  name           :text
+#
 module Evidence
   class Hint < ApplicationRecord
     has_many :rules, inverse_of: :hint
--- services/QuillLMS/engines/evidence/spec/factories/evidence/hints.rb
+++ services/QuillLMS/engines/evidence/spec/factories/evidence/hints.rb
@@ -1,5 +1,18 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: evidence_hints
+#
+#  id             :bigint           not null, primary key
+#  explanation    :string           not null
+#  image_link     :string           not null
+#  image_alt_text :string           not null
+#  rule_id        :bigint
+#  created_at     :datetime         not null
+#  updated_at     :datetime         not null
+#  name           :text
+#
 FactoryBot.define do
   factory :evidence_hint, class: 'Evidence::Hint' do
     name { "Hint Name" }
