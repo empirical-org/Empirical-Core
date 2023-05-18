e92369bdf64ae0a435bd2790cb3857d8d4fcc561
Thomas Robertson <thomas.robertson@quill.org>
2023-05-18 14:03:50 -0400

    Pass a "timestamp" value through to the Unlink API
    Because that's required in this endpoint even though it's not required for other endpoints

--- services/QuillLMS/spec/workers/sync_vitally_unlinks_worker_spec.rb
+++ services/QuillLMS/spec/workers/sync_vitally_unlinks_worker_spec.rb
@@ -8,16 +8,19 @@ describe SyncVitallyUnlinksWorker do
   let(:school) { create(:school) }
   let(:user) { create(:teacher, school: school) }
   let(:vitally_api_double) { double }
+  let(:now) { DateTime.current }
 
   before do
     allow(VitallyApi).to receive(:new).and_return(vitally_api_double)
+    allow(DateTime).to receive(:current).and_return(now)
   end
 
   describe '#perform' do
     it 'constructs an Unlink API payload and passes it to the vitally API' do
       expect(vitally_api_double).to receive(:unlink).with({
         userId: user.id,
-        accountId: school.id
+        accountId: school.id,
+        timestamp: now
       })
 
       subject.perform(user.id, school.id)
