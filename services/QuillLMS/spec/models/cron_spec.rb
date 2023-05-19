067b7eac11f806fff99b7a4602203d5a594e5806
Cissy Yu <cissy@quill.org>
2023-05-19 17:54:56 +0800

    fix spec

--- services/QuillLMS/spec/models/cron_spec.rb
+++ services/QuillLMS/spec/models/cron_spec.rb
@@ -121,6 +121,7 @@ describe "Cron", type: :model do
     end
 
     it "enqueues TeacherNotifications::EnqueueUsersForRollupEmailWorker" do
+      expect(TeacherNotifications::EnqueueUsersForRollupEmailWorker).to receive(:perform_async).with(TeacherInfo::WEEKLY_EMAIL) if Date.today.wday == 5
       expect(TeacherNotifications::EnqueueUsersForRollupEmailWorker).to receive(:perform_async).with(TeacherInfo::DAILY_EMAIL)
       Cron.interval_1_day
     end
