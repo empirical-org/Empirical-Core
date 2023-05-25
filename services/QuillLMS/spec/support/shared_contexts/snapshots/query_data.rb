f3b01c3c4ecfe3eee0621b7d5bdd1674eaff1120
Thomas Robertson <thomas.robertson@quill.org>
2023-05-25 16:58:41 -0400

    Add an comment explaining the source of the values in shared context

--- services/QuillLMS/spec/support/shared_contexts/snapshots/query_data.rb
+++ services/QuillLMS/spec/support/shared_contexts/snapshots/query_data.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 RSpec.shared_context 'Snapshot Query Params' do
+  # Note that these values were selected arbitrarily, but are re-used for consistency
   let(:timeframe_start) { '2023-01-01' }
   let(:timeframe_end) { '2023-05-01' }
   let(:school_ids) { [32628] }
