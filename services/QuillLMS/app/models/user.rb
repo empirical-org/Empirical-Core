e30708745f5136cba8f9e3f48b0ddd400c3a0ef9
Thomas Robertson <thomas.robertson@quill.org>
2023-06-01 17:17:55 -0400

    Add a model to track teacher logins when we send Segment events

--- services/QuillLMS/app/models/user.rb
+++ services/QuillLMS/app/models/user.rb
@@ -124,6 +124,7 @@ class User < ApplicationRecord
   has_many :invitations, foreign_key: 'inviter_id'
   has_many :objectives, through: :checkboxes
   has_many :user_activity_classifications, dependent: :destroy
+  has_many :user_logins, dependent: :destroy
   has_many :user_subscriptions
   has_many :subscriptions, through: :user_subscriptions
   has_many :activity_sessions
