b5e0c6cf4038c65ed6cadc6d58c27cec39609e5c
Emilia Friedberg <emiliafriedberg@gmail.com>
2023-06-02 09:49:28 -0400

    compress csv before emailing

--- services/QuillLMS/app/mailers/user_mailer.rb
+++ services/QuillLMS/app/mailers/user_mailer.rb
@@ -165,7 +165,7 @@ class UserMailer < ActionMailer::Base
   end
 
   def feedback_history_session_csv_download(email, csv_file_path)
-    attachments[FEEDBACK_SESSIONS_CSV_FILENAME] = File.read(csv_file_path)
+    attachments[FEEDBACK_SESSIONS_CSV_FILENAME] = ActiveSupport::Gzip.compress(File.read(csv_file_path))
     mail from: "The Quill Team <hello@quill.org>", to: email, subject: FEEDBACK_SESSIONS_CSV_DOWNLOAD
 
     File.delete(csv_file_path) if File.exist?(csv_file_path)
