34df2c12e525b8c8a96acfd0ce9bceff15818085
Emilia Friedberg <emiliafriedberg@gmail.com>
2023-06-06 14:27:39 -0400

    commit rankings

--- services/QuillLMS/client/app/bundles/PremiumHub/components/usage_snapshots/__tests__/snapshotRanking.test.tsx
+++ services/QuillLMS/client/app/bundles/PremiumHub/components/usage_snapshots/__tests__/snapshotRanking.test.tsx
@@ -6,6 +6,20 @@ import { timeframes, grades, schools, } from './data'
 import { SMALL, NEGATIVE, POSITIVE, MEDIUM, } from '../shared'
 import SnapshotRanking from '../snapshotRanking';
 
+const data = [
+  {value: "Comma Before Coordinating Conjunctions", count: 3777 },
+  {value: "Compound Objects", count: 3339 },
+  {value: "Subordinating Conjunction at the Beginning of a Sentence", count: 2232 },
+  {value: "Gerunds as Subjects", count: 456 },
+  {value: "Split Infinitives", count: 789 },
+  {value: "Parallel Structure", count: 1234 },
+  {value: "Subject-Verb Agreement", count: 5678 },
+  {value: "Pronoun-Antecedent Agreement", count: 9012 },
+  {value: "Appositive Phrases", count: 3456 },
+  {value: "Dangling Modifiers", count: 7890 }
+]
+
+
 describe('SnapshotRanking component', () => {
   const mockProps = {
     label: 'Sentences written',
@@ -32,6 +46,14 @@ describe('SnapshotRanking component', () => {
   describe ('when there is no data ', () => {
     it('should match snapshot', () => {
       const component = mount(<SnapshotRanking {...mockProps} />);
+
+      expect(component).toMatchSnapshot();
+    });
+  })
+
+  describe ('when there is data ', () => {
+    it('should match snapshot', () => {
+      const component = mount(<SnapshotRanking {...mockProps} passedData={data} />);
 
       expect(component).toMatchSnapshot();
     });
