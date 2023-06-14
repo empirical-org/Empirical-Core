da24c31eecf1a121294a5c4baa64062c109678c1
Eric Adams <erictayloradams@gmail.com>
2023-06-14 16:51:50 -0400

    Evidence teacher preview menu (#10644)
    * add preview menu to Evidence play
    
    * extract helper functions; various CSS changes
    
    * add remaining preview menu elements
    
    * add dispatch action for previewSessionStep
    
    * handle preview data changes during activity play
    
    * update snapshot; small refactors
    
    * additional tweaks
    
    * skip tracking for preview
    
    * update spec
    
    * prevent menu button for showing for students
    
    * remove mobile check
    
    * skip tracking in header for preview mode
    
    * tweak showPreview logic
    
    * update conditional
    
    * post spec suggested revisions

--- services/QuillLMS/client/app/bundles/Evidence/components/PageLayout.tsx
+++ services/QuillLMS/client/app/bundles/Evidence/components/PageLayout.tsx
@@ -1,13 +1,29 @@
 import * as React from "react";
 import { renderRoutes } from "react-router-config";
+import { useQuery } from 'react-query';
 
 import Header from "./Header";
 
-import { ScreenreaderInstructions, } from '../../Shared/index';
+import { fetchUserRole } from '../../Shared/utils/userAPIs';
+import { ScreenreaderInstructions, TeacherPreviewMenu, INTRODUCTION } from '../../Shared/index';
 import { routes } from "../routes";
+import getParameterByName from '../helpers/getParameterByName';
 
-const PageLayout: React.StatelessComponent<{}> = (props: any) => {
+const PageLayout = (props: any) => {
   const { user } = props;
+  const turkSession = getParameterByName('turk', window.location.href);
+  const isPlaying = window.location.href.includes('play');
+  const { data } = useQuery("user-role", fetchUserRole);
+  const isTeacherOrAdmin = data && data.role && data.role !== 'student';
+
+  const [previewShowing, setPreviewShowing] = React.useState<boolean>(false);
+  const [questionToPreview, setQuestionToPreview] = React.useState<any>(INTRODUCTION);
+
+  React.useEffect(() => {
+    if(isTeacherOrAdmin && !turkSession) {
+      setPreviewShowing(true)
+    }
+  }, [isTeacherOrAdmin])
 
   function handleSkipToMainContentClick () {
     const element = document.getElementById("main-content")
@@ -16,12 +32,41 @@ const PageLayout: React.StatelessComponent<{}> = (props: any) => {
     element.scrollIntoView()
   }
 
+  function handleTogglePreviewMenu() {
+    setPreviewShowing(!previewShowing);
+  }
+
+  function handleToggleQuestion(step: string) {
+    setQuestionToPreview(step);
+  }
+
+  const showPreview = previewShowing && isTeacherOrAdmin && isPlaying;
+  const contentClass = showPreview ? 'evidence-preview-mode' : '';
+  const isOnMobile = window.innerWidth < 1100;
+
   return (
     <div className="app-container">
       <ScreenreaderInstructions />
       <button className="skip-main" onClick={handleSkipToMainContentClick} type="button">Skip to main content</button>
-      <Header />
-      <div id="main-content" tabIndex={-1}>{renderRoutes(routes, { user })}</div>
+      <Header isOnMobile={isOnMobile} isTeacher={isTeacherOrAdmin} onTogglePreview={handleTogglePreviewMenu} previewShowing={showPreview} />
+      <div className="activity-container">
+        {showPreview && <aside
+          className="sider-container"
+          style={{ overflowY: 'auto', width: '360px' }}
+        >
+          <TeacherPreviewMenu
+            isOnMobile={isOnMobile}
+            onTogglePreview={handleTogglePreviewMenu}
+            onToggleQuestion={handleToggleQuestion}
+            questionToPreview={questionToPreview}
+            showPreview={previewShowing}
+          />
+        </aside>}
+        <div className={contentClass} id="main-content" tabIndex={-1}>{renderRoutes(routes, {
+          user,
+          previewMode: showPreview
+        })}</div>
+      </div>
     </div>
   );
 };
