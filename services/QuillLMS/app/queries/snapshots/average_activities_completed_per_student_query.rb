a410b0f450893c32a0942a68adf1285bb30dc409
Thomas Robertson <thomas.robertson@quill.org>
2023-06-20 12:53:28 -0400

    Make small tweaks based on PR feedback:
    - Add comments explaining naive params pass-through
    - Use attr_reader

--- services/QuillLMS/app/queries/snapshots/average_activities_completed_per_student_query.rb
+++ services/QuillLMS/app/queries/snapshots/average_activities_completed_per_student_query.rb
@@ -2,15 +2,20 @@
 
 module Snapshots
   class AverageActivitiesCompletedPerStudentQuery < PeriodQuery
+    attr_reader :params
+
     def initialize(*params)
+      # We pull params here because we're going to do a naive pass-through to sibling queries below
       @params = params
 
       super
     end
 
     def run
-      activities_completed = ActivitiesCompletedQuery.run(*@params)[:count]
-      active_students = ActiveStudentsQuery.run(*@params)[:count]
+      # This query, and both of the queries below, inherit their `initialize` method from
+      # PeriodQuery.  This means that they should all have identical method signatures.
+      activities_completed = ActivitiesCompletedQuery.run(*params)[:count]
+      active_students = ActiveStudentsQuery.run(*params)[:count]
       # max prevents us from dividing by 0 if there are no active students
       {
         'count': activities_completed / [active_students, 1].max.to_f
