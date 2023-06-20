d1e1ee0cf84b31888818434e29051ef14ef1b9e2
Thomas Robertson <thomas.robertson@quill.org>
2023-06-20 11:16:28 -0400

    Use existing queries inside AverageActivitiesCompletedPerStudent query
    This is simpler than trying to construct a single query that can do both things, we just do the two queries that go into the average calculation.

--- services/QuillLMS/app/queries/snapshots/average_activities_completed_per_student_query.rb
+++ services/QuillLMS/app/queries/snapshots/average_activities_completed_per_student_query.rb
@@ -1,10 +1,20 @@
 # frozen_string_literal: true
 
 module Snapshots
-  class AverageActivitiesCompletedPerStudentQuery < ActivitySessionCountQuery
-    def select_clause
-      # "greatest" avoids division by 0 error
-      "SELECT COUNT(DISTINCT activity_sessions.id) / greatest(COUNT(DISTINCT activity_sessions.user_id), 1) AS count"
+  class AverageActivitiesCompletedPerStudentQuery < PeriodQuery
+    def initialize(*params)
+      @params = params
+
+      super
+    end
+
+    def run
+      activities_completed = ActivitiesCompletedQuery.run(*@params)[:count]
+      active_students = ActiveStudentsQuery.run(*@params)[:count]
+      # max prevents us from dividing by 0 if there are no active students
+      {
+        'count': activities_completed / [active_students, 1].max.to_f
+      }
     end
   end
 end
