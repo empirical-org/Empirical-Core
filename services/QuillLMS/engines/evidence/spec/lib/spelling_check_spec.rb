53c1aa737ccc9332d5423992942e5d82683c4f7d
Cissy Yu <cissy@quill.org>
2023-06-21 14:30:22 +0800

    change written string to constatnt in URL

--- services/QuillLMS/engines/evidence/spec/lib/spelling_check_spec.rb
+++ services/QuillLMS/engines/evidence/spec/lib/spelling_check_spec.rb
@@ -9,7 +9,7 @@ module Evidence
     context 'should #feedback_object' do
 
       it 'should return appropriate feedback attributes if there is a spelling error' do
-        stub_request(:get, "https://api.bing.microsoft.com/v7.0/spellcheck?mode=proof&text=there%20is%20a%20spelin%20error%20here").to_return(:status => 200, :body => { :flaggedTokens => ([{ :token => "spelin" }]) }.to_json, :headers => ({}))
+        stub_request(:get, "#{::Evidence::SpellingCheck::BING_API_URL}?mode=proof&text=there%20is%20a%20spelin%20error%20here").to_return(:status => 200, :body => { :flaggedTokens => ([{ :token => "spelin" }]) }.to_json, :headers => ({}))
         entry = "there is a spelin error here"
         spelling_check = Evidence::SpellingCheck.new(entry)
         feedback = spelling_check.feedback_object
@@ -24,7 +24,7 @@ module Evidence
 
       it 'should not flag error if the spelling error downcased is in exception list' do
         spelling_error = "SpeliN"
-        stub_request(:get, "https://api.bing.microsoft.com/v7.0/spellcheck?mode=proof&text=there%20is%20a%20spelin%20error%20here").to_return(:status => 200, :body => { :flaggedTokens => ([{ :token => spelling_error }]) }.to_json, :headers => ({}))
+        stub_request(:get, "#{::Evidence::SpellingCheck::BING_API_URL}?mode=proof&text=there%20is%20a%20spelin%20error%20here").to_return(:status => 200, :body => { :flaggedTokens => ([{ :token => spelling_error }]) }.to_json, :headers => ({}))
         stub_const("Evidence::SpellingCheck::EXCEPTIONS", [spelling_error.downcase])
         entry = "there is a spelin error here"
         spelling_check = Evidence::SpellingCheck.new(entry)
@@ -40,7 +40,7 @@ module Evidence
       end
 
       it 'should return appropriate feedback attributes if there is no spelling error' do
-        stub_request(:get, "https://api.bing.microsoft.com/v7.0/spellcheck?mode=proof&text=there%20is%20no%20spelling%20error%20here").to_return(:status => 200, :body => { :flaggedTokens => ({}) }.to_json, :headers => ({}))
+        stub_request(:get, "#{::Evidence::SpellingCheck::BING_API_URL}?mode=proof&text=there%20is%20no%20spelling%20error%20here").to_return(:status => 200, :body => { :flaggedTokens => ({}) }.to_json, :headers => ({}))
         entry = "there is no spelling error here"
         spelling_check = Evidence::SpellingCheck.new(entry)
         feedback = spelling_check.feedback_object
@@ -53,7 +53,7 @@ module Evidence
       end
 
       it 'should return appropriate feedback attributes if there is no spelling error even if Bing does not return a "flaggedTokens" value' do
-        stub_request(:get, "https://api.bing.microsoft.com/v7.0/spellcheck?mode=proof&text=there%20is%20no%20spelling%20error%20here").to_return(:status => 200, :body => {}.to_json, :headers => ({}))
+        stub_request(:get, "#{::Evidence::SpellingCheck::BING_API_URL}?mode=proof&text=there%20is%20no%20spelling%20error%20here").to_return(:status => 200, :body => {}.to_json, :headers => ({}))
         entry = "there is no spelling error here"
         spelling_check = Evidence::SpellingCheck.new(entry)
         feedback = spelling_check.feedback_object
@@ -66,7 +66,7 @@ module Evidence
       end
 
       it 'should raise error if the Bing API request times out' do
-        stub_request(:get, "https://api.bing.microsoft.com/v7.0/spellcheck?mode=proof&text=there%20is%20no%20spelling%20error%20here").to_timeout
+        stub_request(:get, "#{::Evidence::SpellingCheck::BING_API_URL}?mode=proof&text=there%20is%20no%20spelling%20error%20here").to_timeout
         entry = "there is no spelling error here"
         spelling_check = Evidence::SpellingCheck.new(entry)
 
@@ -74,7 +74,7 @@ module Evidence
       end
 
       it 'should raise error if the Bing API request times out with a Net::ReadTimeout' do
-        stub_request(:get, "https://api.bing.microsoft.com/v7.0/spellcheck?mode=proof&text=there%20is%20no%20spelling%20error%20here").to_raise(Net::ReadTimeout)
+        stub_request(:get, "#{::Evidence::SpellingCheck::BING_API_URL}?mode=proof&text=there%20is%20no%20spelling%20error%20here").to_raise(Net::ReadTimeout)
         entry = "there is no spelling error here"
         spelling_check = Evidence::SpellingCheck.new(entry)
 
@@ -83,7 +83,7 @@ module Evidence
 
 
       it 'should return appropriate error if the endpoint returns an error' do
-        stub_request(:get, "https://api.bing.microsoft.com/v7.0/spellcheck?mode=proof&text=there%20is%20no%20spelling%20error%20here").to_return(:status => 200, :body => { :error => ({ :message => "There's a problem here" }) }.to_json, :headers => ({}))
+        stub_request(:get, "#{::Evidence::SpellingCheck::BING_API_URL}?mode=proof&text=there%20is%20no%20spelling%20error%20here").to_return(:status => 200, :body => { :error => ({ :message => "There's a problem here" }) }.to_json, :headers => ({}))
         entry = "there is no spelling error here"
         spelling_check = Evidence::SpellingCheck.new(entry)
         feedback = spelling_check.feedback_object
@@ -135,7 +135,7 @@ module Evidence
 
     context 'should handle appropriate Bing API errors' do
       it 'should raise exception if the endpoint returns a rate-limit error' do
-        stub_request(:get, "https://api.bing.microsoft.com/v7.0/spellcheck?mode=proof&text=there%20is%20no%20spelling%20error%20here").to_return(:status => 429, :body => { :error => ({ :message => "There's a problem here" }) }.to_json, :headers => ({}))
+        stub_request(:get, "#{::Evidence::SpellingCheck::BING_API_URL}?mode=proof&text=there%20is%20no%20spelling%20error%20here").to_return(:status => 429, :body => { :error => ({ :message => "There's a problem here" }) }.to_json, :headers => ({}))
         entry = "there is no spelling error here"
         spelling_check = Evidence::SpellingCheck.new(entry)
         expect { spelling_check.feedback_object }.to raise_error(Evidence::SpellingCheck::BingRateLimitException)
