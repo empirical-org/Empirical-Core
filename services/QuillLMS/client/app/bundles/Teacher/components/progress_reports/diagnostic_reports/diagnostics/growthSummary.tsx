85d6f6a976894da154e37d8761ff01f55db191f6
Eric Adams <erictayloradams@gmail.com>
2023-06-23 11:11:41 -0400

    Teacher Diagnostic Report Changes: add classwide growth percentages (#10680)
    * calculate classwide average growth change
    
    * add classwide growth percentages
    
    * update tests
    
    * account for no growth instances
    
    * add comments and remove Post label
    
    * fix lint error

--- services/QuillLMS/client/app/bundles/Teacher/components/progress_reports/diagnostic_reports/diagnostics/growthSummary.tsx
+++ services/QuillLMS/client/app/bundles/Teacher/components/progress_reports/diagnostic_reports/diagnostics/growthSummary.tsx
@@ -10,6 +10,10 @@ import {
   fileDocumentIcon,
   noDataYet,
   triangleUpIcon,
+  PRE,
+  POST,
+  sumProficiencyScores,
+  calculateClassGrowthPercentage
 } from './shared';
 import SkillGroupTooltip from './skillGroupTooltip';
 
@@ -20,20 +24,24 @@ import {
 import DemoOnboardingTour, { DEMO_ONBOARDING_DIAGNOSTIC_GROWTH_SUMMARY, } from '../../../shared/demo_onboarding_tour';
 import LoadingSpinner from '../../../shared/loading_indicator.jsx';
 
+function renderGrowthElement(delta: number, text: string) {
+  return delta > 0 ? <span className="growth-element">{triangleUpIcon}<span>{Math.round(delta)}{`% ${text}`}</span></span> : <span className="growth-element no-growth">No growth</span>
+}
+
 const SkillGroupSummaryCard = ({ skillGroupSummary, completedStudentCount }: { skillGroupSummary: SkillGroupSummary, completedStudentCount: number }) => {
   const { name, description, not_yet_proficient_in_post_test_student_names, proficiency_scores_by_student } = skillGroupSummary
   let cardContent = noDataYet
 
   if (completedStudentCount) {
     const numberOfStudentsNeedingPracticeInPost = not_yet_proficient_in_post_test_student_names.length
-    const preProficiencyScoreTotalSum = Object.values(proficiency_scores_by_student).reduce((total, student_score) => { return total += student_score.pre }, 0)
-    const postProficiencyScoreTotalSum = Object.values(proficiency_scores_by_student).reduce((total, student_score) => { return total += student_score.post }, 0)
+    const preProficiencyScoreTotalSum = sumProficiencyScores(proficiency_scores_by_student, PRE)
+    const postProficiencyScoreTotalSum = sumProficiencyScores(proficiency_scores_by_student, POST)
     const preProficiencyClassPercentage = Math.round((preProficiencyScoreTotalSum / completedStudentCount) * 100)
     const postProficiencyClassPercentage = Math.round((postProficiencyScoreTotalSum / completedStudentCount) * 100)
     const delta = postProficiencyClassPercentage - preProficiencyClassPercentage
 
     let needPracticeElement = <span className="need-practice-element no-practice-needed">No practice needed</span>
-    let growthElement = delta > 0 ? <span className="growth-element">{triangleUpIcon}<span>{Math.round(delta)}% growth</span></span> : <span className="growth-element no-growth">No growth</span>
+    const growthElement = renderGrowthElement(delta, 'growth')
 
     if (numberOfStudentsNeedingPracticeInPost) {
       const tooltipText = `<p>${not_yet_proficient_in_post_test_student_names.join('<br>')}</p>`
@@ -88,12 +96,14 @@ const SkillGroupSummaryCard = ({ skillGroupSummary, completedStudentCount }: { s
   )
 }
 
-export const GrowthResults = ({ passedStudentResults, passedSkillGroupSummaries, match, mobileNavigation, }) => {
+export const GrowthResults = ({ activityName, passedStudentResults, passedSkillGroupSummaries, match, mobileNavigation, }) => {
   const [loading, setLoading] = React.useState<boolean>(!passedStudentResults);
   const [studentResults, setStudentResults] = React.useState<StudentResult[]>(passedStudentResults || []);
   const [skillGroupSummaries, setSkillGroupSummaries] = React.useState<SkillGroupSummary[]>(passedSkillGroupSummaries || []);
+  const [classwideGrowthAverage, setClasswideGrowthAverage] = React.useState<number>(null);
 
   const { activityId, classroomId, } = match.params
+  const completedStudentCount = studentResults.filter(sr => sr.skill_groups).length
 
   React.useEffect(() => {
     getResults()
@@ -103,6 +113,13 @@ export const GrowthResults = ({ passedStudentResults, passedSkillGroupSummaries,
     setLoading(true)
     getResults()
   }, [activityId, classroomId])
+
+  React.useEffect(() => {
+    // classwideGrowthAverage result may be 0 in some instances so we check for initial null value
+    if (skillGroupSummaries.length && completedStudentCount && classwideGrowthAverage === null) {
+      calculateClassGrowthPercentage({ skillGroupSummaries, completedStudentCount, setClasswideGrowthAverage })
+    }
+  }, [skillGroupSummaries])
 
   function getResults() {
     requestGet(`/teachers/progress_reports/diagnostic_growth_results_summary?activity_id=${activityId}&classroom_id=${classroomId}`,
@@ -114,9 +131,21 @@ export const GrowthResults = ({ passedStudentResults, passedSkillGroupSummaries,
     )
   }
 
+  function renderClasswideGrowthAverage() {
+    if(classwideGrowthAverage === null) { return }
+
+    const classwideGrowthDisplayedAverage = classwideGrowthAverage > 0 ? classwideGrowthAverage: 0;
+    const activityDisplayedName = activityName.replace('(Post)', '').trim();
+    return(
+      <section className="lower-header-section">
+        <span className="activity-name">{`${activityDisplayedName}:`}</span>
+        {renderGrowthElement(classwideGrowthDisplayedAverage, 'Class-wide skill growth')}
+      </section>
+    )
+  }
+
   if (loading) { return <LoadingSpinner /> }
 
-  const completedStudentCount = studentResults.filter(sr => sr.skill_groups).length
 
   const skillGroupSummaryCards = skillGroupSummaries.map(skillGroupSummary => <SkillGroupSummaryCard completedStudentCount={completedStudentCount} key={skillGroupSummary.name} skillGroupSummary={skillGroupSummary} />)
 
@@ -124,8 +153,11 @@ export const GrowthResults = ({ passedStudentResults, passedSkillGroupSummaries,
     <main className="results-summary-container growth-results-summary-container">
       <DemoOnboardingTour pageKey={DEMO_ONBOARDING_DIAGNOSTIC_GROWTH_SUMMARY} />
       <header>
-        <h1>Class summary</h1>
-        <a className="focus-on-light" href="https://support.quill.org/en/articles/5698227-how-do-i-read-the-growth-results-summary-report" rel="noopener noreferrer" target="_blank">{fileDocumentIcon}<span>Guide</span></a>
+        <section className="upper-header-section">
+          <h1>Class summary</h1>
+          <a className="focus-on-light" href="https://support.quill.org/en/articles/5698227-how-do-i-read-the-growth-results-summary-report" rel="noopener noreferrer" target="_blank">{fileDocumentIcon}<span>Guide</span></a>
+        </section>
+        {renderClasswideGrowthAverage()}
       </header>
       {mobileNavigation}
       <section className="skill-group-summary-cards">{skillGroupSummaryCards}</section>
