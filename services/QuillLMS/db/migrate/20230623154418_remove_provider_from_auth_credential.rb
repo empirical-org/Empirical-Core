7ccc2d20587861a64100ae2956179af229fbe318
Brendan Shean <brendanshean@gmail.com>
2023-06-23 11:08:51 -0500

    Remove provider from auth_credential

---
+++ services/QuillLMS/db/migrate/20230623154418_remove_provider_from_auth_credential.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+class RemoveProviderFromAuthCredential < ActiveRecord::Migration[6.1]
+  def up
+    remove_index :auth_credentials, :provider
+    remove_column :auth_credentials, :provider
+  end
+
+  def down
+    add_column :auth_credentials, :provider, :string, null: false, index: true
+
+    AuthCredential.reset_column_information
+
+    AuthCredential::TYPES.map(&:constantize).each do |klass|
+      klass.update_all(provider: klass::PROVIDER)
+    end
+  end
+end
