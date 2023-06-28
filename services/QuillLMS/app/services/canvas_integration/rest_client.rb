0d0520da223a0009c454a4390f2abe4146ea8f75
Brendan Shean <brendanshean@gmail.com>
2023-06-28 11:05:37 -0400

    Add RestClient

---
+++ services/QuillLMS/app/services/canvas_integration/rest_client.rb
@@ -0,0 +1,52 @@
+# frozen_string_literal: true
+
+module CanvasIntegration
+  class RestClient
+    COURSES_PATH = 'courses'
+
+    attr_reader :canvas_auth_credential, :canvas_instance
+
+    def initialize(canvas_auth_credential, canvas_instance)
+      @canvas_auth_credential = canvas_auth_credential
+      @canvas_instance = canvas_instance
+    end
+
+    def teacher_classrooms
+      { canvas_instance_id: canvas_instance.id, classrooms: classrooms }
+    end
+
+    private def api
+      @api ||=
+        ::LMS::Canvas.new(
+          canvas_instance.url,
+          canvas_auth_credential,
+          client_id: canvas_instance.client_id,
+          client_secret: canvas_instance.client_secret,
+          refresh_token: canvas_auth_credential.refresh_token,
+          redirect_uri: Auth::Canvas::OMNIAUTH_CALLBACK_PATH
+        )
+    end
+
+    private def classrooms
+      courses_data.map do |course_data|
+        sections_data(course_data[:id]).map do |section_data|
+          ClassroomDataAdapater.run(course_data, section_data)
+        end
+      end.flatten
+    end
+
+    private def courses_data
+      get_data(COURSES_PATH)
+    end
+
+    private def get_data(path)
+      JSON
+        .parse(api.api_get_request(path).body)
+        .map(&:deep_symbolize_keys)
+    end
+
+    private def sections_data(course_id)
+      get_data("#{COURSES_PATH}/#{course_id}/sections?include[]=students")
+    end
+  end
+end
