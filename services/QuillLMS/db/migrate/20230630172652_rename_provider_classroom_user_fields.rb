31969d28955d27d54bcd5ff656b3b581456a8f50
Brendan Shean <brendanshean@gmail.com>
2023-06-30 16:04:43 -0400

    Fix field name

--- services/QuillLMS/db/migrate/20230630172652_rename_provider_classroom_user_fields.rb
+++ services/QuillLMS/db/migrate/20230630172652_rename_provider_classroom_user_fields.rb
@@ -3,6 +3,6 @@
 class RenameProviderClassroomUserFields < ActiveRecord::Migration[6.1]
   def change
     rename_column :provider_classroom_users, :provider_classroom_id, :classroom_external_id
-    rename_column :provider_classroom_users, :user_external_id, :user_external_id
+    rename_column :provider_classroom_users, :provider_user_id, :user_external_id
   end
 end
