27ca2c3c4a07df2e379edbe176b4be3c46498fcf
Brendan Shean <brendanshean@gmail.com>
2023-07-02 10:07:16 -0400

    Rename RetreiveTeacherClassroomsWorker -> HydrateTeacherClassroomsCacheWorker

---
+++ services/QuillLMS/app/workers/google_integration/hydrate_teacher_classrooms_cache_worker.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+module GoogleIntegration
+  class HydrateTeacherClassroomsCacheWorker
+    include Sidekiq::Worker
+    sidekiq_options queue: SidekiqQueue::CRITICAL_EXTERNAL
+
+    def perform(user_id)
+      user = ::User.find_by(id: user_id)
+
+      return if user.nil?
+      return unless user.teacher? && user.google_authorized?
+
+      TeacherClassroomsCacheHydrator.run(user)
+    end
+  end
+end
