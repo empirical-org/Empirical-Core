b3db65140f30bba1fa38d881451373622f2d9839
Brendan Shean <brendanshean@gmail.com>
2023-07-02 10:19:23 -0400

    Add condition to UpdateTeacherImportedClassroomsWorker

--- services/QuillLMS/app/workers/google_integration/update_teacher_imported_classrooms_worker.rb
+++ services/QuillLMS/app/workers/google_integration/update_teacher_imported_classrooms_worker.rb
@@ -6,14 +6,13 @@ module GoogleIntegration
     sidekiq_options queue: SidekiqQueue::CRITICAL_EXTERNAL
 
     def perform(user_id)
-      return unless google_authorized?(user_id)
+      user = ::User.find_by(id: user_id)
 
-      TeacherClassroomsCacheHydrator.run(user_id)
-      TeacherImportedClassroomsUpdater.run(user_id)
-    end
+      return if user.nil?
+      return unless user.teacher? && user.google_authorized?
 
-    private def google_authorized?(user_id)
-      user_id && ::User.find_by(id: user_id)&.google_authorized?
+      TeacherClassroomsCacheHydrator.run(user)
+      TeacherImportedClassroomsUpdater.run(user)
     end
   end
 end
