f35283861c6432b5eb19e5614669a33d20b20672
Brendan Shean <brendanshean@gmail.com>
2023-07-03 09:36:00 -0400

    Add reauthorize_google_modal component

---
+++ services/QuillLMS/client/app/bundles/Teacher/components/classrooms/reauthorize_google_modal.tsx
@@ -0,0 +1,49 @@
+import * as React from 'react';
+
+interface ReauthorizeGoogleModalProps {
+  googleLink: string;
+  close: () => void;
+}
+
+export default class ReauthorizeGoogleModal extends React.Component<ReauthorizeGoogleModalProps, {}> {
+  handleReauthorizeClick = () => {
+    const { googleLink } = this.props
+    window.location.href = googleLink
+  }
+
+  renderReauthorizeButton() {
+    return (
+      <button className='quill-button contained primary medium' onClick={this.handleReauthorizeClick} type="button">
+        <span>Reauthorize</span>
+      </button>
+    )
+  }
+
+  render() {
+    const { close } = this.props
+
+    return (
+      <div className="modal-container reauthorize-google-modal-container">
+        <div className="modal-background" />
+        <div className="reauthorize-google-modal quill-modal modal-body">
+          <div>
+            <h3 className="title">
+              Reauthorize Google
+            </h3>
+          </div>
+          <p>
+            To import a new Google classroom, you need to reauthorize Google access.
+            <br />
+            Clicking reauthorize will re-direct you to Google.
+          </p>
+          <div className="form-buttons">
+            <button className="quill-button outlined secondary medium" onClick={close} type="button">
+              Cancel
+            </button>
+            {this.renderReauthorizeButton()}
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
