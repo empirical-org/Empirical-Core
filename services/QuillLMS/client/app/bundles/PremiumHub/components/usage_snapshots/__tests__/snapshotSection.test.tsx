edabe1860f847cf05a7fb8addccf1ac7ff9c26e2
Emilia Friedberg <emiliafriedberg@gmail.com>
2023-07-05 09:20:46 -0400

    add teacher and classroom filters to frontend for usage snapshot report (#10714)
    * add filters to frontend
    
    * mostly get dynamic filters working but clear behavior is weird
    
    * fix weirdness with clearing filters
    
    * handle grades
    
    * adjustment to behavior with removing last pill
    
    * remove deduplication logic from frontend
    
    * put it back in place bc backend deduplication doesn't seem to be working
    
    * Revert "put it back in place bc backend deduplication doesn't seem to be working"
    
    This reverts commit 3f8629c54d155d48c77628b759798a72f53012f3.
    
    * fix bug that was causing way too many requests to be made
    
    * remove unnecessary logic around empty arrays

--- services/QuillLMS/client/app/bundles/PremiumHub/components/usage_snapshots/__tests__/snapshotSection.test.tsx
+++ services/QuillLMS/client/app/bundles/PremiumHub/components/usage_snapshots/__tests__/snapshotSection.test.tsx
@@ -1,7 +1,7 @@
 import { mount } from 'enzyme';
 import * as React from 'react';
 
-import { timeframes, grades, schools, } from './data'
+import { timeframes, grades, schools, teachers, classrooms, } from './data'
 
 import { snapshotSections, } from '../shared'
 import SnapshotSection from '../snapshotSection';
@@ -11,6 +11,8 @@ describe('SnapshotSection component', () => {
     searchCount: 1,
     selectedGrades: grades.map(g => g.value),
     selectedSchoolIds: schools.map(s => s.id),
+    selectedTeacherIds: teachers.map(s => s.id),
+    selectedClassroomIds: classrooms.map(s => s.id),
     selectedTimeframe: timeframes[0].value,
     adminId: 1,
     customTimeframeStart: null,
