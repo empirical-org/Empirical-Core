1983b37b1a60297b4667f19b6d0b046fc2c1ce93
Cissy Yu <cissy@quill.org>
2023-07-06 18:34:27 +0800

    fix last spec

--- services/QuillLMS/spec/workers/set_impact_metrics_worker_spec.rb
+++ services/QuillLMS/spec/workers/set_impact_metrics_worker_spec.rb
@@ -5,77 +5,56 @@ require 'rails_helper'
 describe SetImpactMetricsWorker do
   subject { described_class.new }
 
-  context '#perform', :big_query_snapshot do
-    include_context 'QuillBigQuery TestRunner Setup'
+  context '#perform' do
+    let(:activity_sessions) { create_list(:activity_session, 100)}
 
-    let!(:school) { create(:school) }
-    let!(:low_income_school) { create(:school, free_lunches: 40) }
-    let!(:first_classroom) { create(:classroom_with_students_and_activities) }
-    let!(:second_classroom) { create(:classroom_with_students_and_activities) }
-    let!(:first_schools_user) { create(:schools_users, school: school, user: first_classroom.owner)}
-    let!(:second_schools_user) { create(:schools_users, school: low_income_school, user: second_classroom.owner)}
-    let!(:archived_activity_session) { create(:activity_session, user: User.where(role: 'student').first, visible: false )}
+    let(:example_json1) {
+      activity_sessions.map {|as| {"id" => as.id}}
+    }
 
-    let(:runner_context) do
+    let(:teachers) { create_list(:teacher, 150)}
+
+    let(:example_json2) {
+      teachers.map {|t| {"id" => t.id}}
+    }
+
+    let(:example_json3) {
       [
-        school,
-        low_income_school,
-        first_classroom,
-        second_classroom,
-        first_schools_user,
-        second_schools_user,
-        archived_activity_session
+        {"id" => 333, "free_lunches" => 30},
+        {"id" => 334, "free_lunches" => 0},
+        {"id" => 545, "free_lunches" => 54}
       ]
-    end
-
-    before(:all) do
-      @number_of_sentences = ActivitySession.unscoped.where(state: 'finished').count.floor(-5) * 10
-      @number_of_students = ActivitySession.unscoped.where(state: 'finished').count('DISTINCT(user_id)')
-      teachers_query = User.select(:id)
-        .joins(:units, classroom_units: :activity_sessions)
-        .where("activity_sessions.state = 'finished'")
-        .group('users.id')
-        .having('count(activity_sessions) > ?', SetImpactMetricsWorker::ACTIVITY_SESSION_MINIMUM)
-      teacher_ids = teachers_query.to_a.map(&:id)
-      @number_of_teachers = teachers_query.to_a.count.floor(-2)
-      schools_query = School.joins(:schools_users).where(schools_users: {user_id: teacher_ids}).distinct
-      @number_of_schools = schools_query.to_a.count
-      low_income_schools_query = schools_query.where("free_lunches > ?", SetImpactMetricsWorker::FREE_LUNCH_MINIMUM)
-      @number_of_low_income_schools = low_income_schools_query.to_a.count
-    end
-
-    let(:query_args) { }
-    let(:cte_records) { [runner_context] }
-
-    let(:runner) { QuillBigQuery::TestRunner.new(cte_records) }
+    }
 
     before do
-      allow(QuillBigQuery::Runner).to receive(:new).and_return(runner)
+      allow(QuillBigQuery::ActivitiesAllTimeQuery).to receive(:run).and_return(example_json1)
+      allow(QuillBigQuery::ActiveTeachersAllTimeQuery).to receive(:run).and_return(example_json2)
+      allow(QuillBigQuery::SchoolsContainingXTeachersQuery).to receive(:run).with({teacher_ids: teachers.map(&:id)}).and_return(example_json3)
     end
 
     it 'should set the NUMBER_OF_SENTENCES redis value' do
       subject.perform
-      expect($redis.get(PagesController::NUMBER_OF_SENTENCES)).to eq(@number_of_sentences.to_s)
+      expect($redis.get(PagesController::NUMBER_OF_SENTENCES)).to eq("#{example_json1.size.floor(-5) * 10}")
     end
 
-    # it 'should set the NUMBER_OF_STUDENTS redis value' do
-    #   subject.perform
-    #   expect($redis.get(PagesController::NUMBER_OF_STUDENTS)).to eq(@number_of_students.to_s)
-    # end
+    it 'should set the NUMBER_OF_STUDENTS redis value' do
+      subject.perform
+      expect($redis.get(PagesController::NUMBER_OF_STUDENTS)).to eq("#{example_json1.count("DISTINCT(user_id)").floor(-5)}")
+    end
 
-    # it 'should set the NUMBER_OF_TEACHERS redis value' do
-    #   subject.perform
-    #   expect($redis.get(PagesController::NUMBER_OF_TEACHERS)).to eq(@number_of_teachers.to_s)
-    # end
+    it 'should set the NUMBER_OF_TEACHERS redis value' do
+      subject.perform
+      expect($redis.get(PagesController::NUMBER_OF_TEACHERS)).to eq("100")
+    end
 
-    # it 'should set the NUMBER_OF_SCHOOLS redis value' do
-    #   subject.perform
-    #   expect($redis.get(PagesController::NUMBER_OF_SCHOOLS)).to eq(@number_of_schools.to_s)
-    # end
+    it 'should set the NUMBER_OF_SCHOOLS redis value' do
+      subject.perform
+      expect($redis.get(PagesController::NUMBER_OF_SCHOOLS)).to eq("3")
+    end
 
-    # it 'should set the NUMBER_OF_LOW_INCOME_SCHOOLS redis value' do
-    #   subject.perform
-    #   expect($redis.get(PagesController::NUMBER_OF_LOW_INCOME_SCHOOLS)).to eq(@number_of_low_income_schools.to_s)
-    # end
+    it 'should set the NUMBER_OF_LOW_INCOME_SCHOOLS redis value' do
+      subject.perform
+      expect($redis.get(PagesController::NUMBER_OF_LOW_INCOME_SCHOOLS)).to eq("1")
+    end
   end
 end
