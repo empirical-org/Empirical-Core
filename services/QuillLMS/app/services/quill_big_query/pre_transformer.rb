59f3b5e98ab72f98e1fe3f3e19a301d13f4d9a23
Cissy Yu <cissy@quill.org>
2023-07-10 13:40:05 +0800

    spec for pretransformer

--- services/QuillLMS/app/services/quill_big_query/pre_transformer.rb
+++ services/QuillLMS/app/services/quill_big_query/pre_transformer.rb
@@ -2,47 +2,54 @@
 
 module QuillBigQuery
   class PreTransformer
+    ARRAY_TYPE = 'ARRAY'
+    STRING_TYPE = 'STRING'
+    INT_TYPE = 'INTEGER'
+
+    class UnsupportedArrayParameterTypeError < StandardError; end
+    class EmptyArrayParameterError < StandardError; end
 
     def initialize(query, *array_params)
       @query = query
       @transformed_array_params = self.class.build_array_query_params(*array_params)
     end
 
+    # according to Google Cloud BigQuery API as documented here:
+    # https://cloud.google.com/bigquery/docs/reference/rest/v2/QueryParameter
     def self.build_array_query_params(*array_params)
       array_params.map do |array_param|
+        raise EmptyArrayParameterError if array_param.values.first.first.blank?
+
         name = array_param.keys.first
         array = array_param.values.first
         type = type_lookup(array_param.values.first.first)
         {
-          'name' => name,
+          'name' => name.to_s,
           'parameterType' => {
-            'type' => 'ARRAY',
+            'type' => ARRAY_TYPE,
             'arrayType' => {
               'type' => type
             }
           },
           'parameterValue' => {
-            'arrayValues' => [
-              array.map { |v| { 'value' => v.to_s }}
-            ]
+            'arrayValues' => array.map { |v| { 'value' => v.to_s }}
           }
         }
       end
     end
 
     def self.type_lookup(object)
-      return 'STRING' if object.is_a? String
+      raise UnsupportedArrayParameterTypeError unless (object.is_a?(String) || object.is_a?(Integer))
 
-      return 'INTEGER'
+      return STRING_TYPE if object.is_a? String
+      return INT_TYPE
     end
 
     def transformed_query
       body_object = {
         'query' => @query,
         'useLegacySql' => false,
-        'queryParameters'=> [
-          @transformed_array_params
-        ]
+        'queryParameters'=> @transformed_array_params
       }
     end
   end
