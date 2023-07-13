b73db03ad651cc66ce474e45197f3f93f21da234
Cissy Yu <cissy@quill.org>
2023-07-13 22:55:17 +0800

    deleted mailer preview

--- services/QuillLMS/lib/mailer_previews/rollup_mailer_preview.rb
+++
@@ -1,9 +0,0 @@
-class RollupMailerPreview < ActionMailer::Preview
-
-  def welcome_email
-    @user = User.where(role: 'teacher').first
-    @notifications = TeacherNotification.all
-    TeacherNotifications::RollupMailer.rollup(@user, @notifications)
-  end
-
-end
