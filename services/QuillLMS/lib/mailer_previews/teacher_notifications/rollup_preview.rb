4751b76fd144eebef523efa9c91182a935b75126
Cissy Yu <cissy@quill.org>
2023-07-13 22:51:28 +0800

    delete unneeded changes

--- services/QuillLMS/lib/mailer_previews/teacher_notifications/rollup_preview.rb
+++
@@ -1,7 +0,0 @@
-class RollupPreview < ActionMailer::Preview
-  def welcome
-    @user = User.where(role: 'teacher').first
-    @notifications = [TeacherNotification.last]
-    TeacherNotifications::RollupMailer.rollup(@user, @notifications)
-  end
-end
