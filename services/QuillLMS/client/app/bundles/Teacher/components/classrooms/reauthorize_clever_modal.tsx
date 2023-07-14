1d92ae06388f4789f01fd4a11a0587ea19a94814
Brendan Shean <brendanshean@gmail.com>
2023-07-14 13:28:25 -0400

    Emilia suggestions

--- services/QuillLMS/client/app/bundles/Teacher/components/classrooms/reauthorize_clever_modal.tsx
+++ services/QuillLMS/client/app/bundles/Teacher/components/classrooms/reauthorize_clever_modal.tsx
@@ -5,45 +5,36 @@ interface ReauthorizeCleverModalProps {
   close: () => void;
 }
 
-export default class ReauthorizeCleverModal extends React.Component<ReauthorizeCleverModalProps, {}> {
-  handleReauthorizeClick = () => {
-    const { cleverLink } = this.props
+const ReauthorizeCleverModal = ({ cleverLink, close }: ReauthorizeCleverModalProps) => {
+  function handleReauthorizeClick() {
     window.location.href = cleverLink
   }
 
-  renderReauthorizeButton() {
-    return (
-      <button className='quill-button contained primary medium' onClick={this.handleReauthorizeClick} type="button">
-        <span>Reauthorize</span>
-      </button>
-    )
-  }
-
-  render() {
-    const { close } = this.props
-
-    return (
-      <div className="modal-container reauthorize-clever-modal-container">
-        <div className="modal-background" />
-        <div className="reauthorize-clever-modal quill-modal modal-body">
-          <div>
-            <h3 className="title">
-              Reauthorize Clever
-            </h3>
-          </div>
-          <p>
-            To import a new Clever classroom, you need to reauthorize Clever access.
-            <br />
-            Clicking reauthorize will re-direct you to Clever.
-          </p>
-          <div className="form-buttons">
-            <button className="quill-button outlined secondary medium" onClick={close} type="button">
-              Cancel
-            </button>
-            {this.renderReauthorizeButton()}
-          </div>
+  return (
+    <div className="modal-container reauthorize-clever-modal-container">
+      <div className="modal-background" />
+      <div className="reauthorize-clever-modal quill-modal modal-body">
+        <div>
+          <h3 className="title">
+            Reauthorize Clever
+          </h3>
+        </div>
+        <p>
+          To import a new Clever classroom, you need to reauthorize Clever access.
+          <br />
+          Clicking reauthorize will re-direct you to Clever.
+        </p>
+        <div className="form-buttons">
+          <button className="quill-button focus-on-light outlined secondary medium" onClick={close} type="button">
+            Cancel
+          </button>
+          <button className='quill-button focus-on-light contained primary medium' onClick={handleReauthorizeClick} type="button">
+            Reauthorize
+          </button>
         </div>
       </div>
-    )
-  }
+    </div>
+  )
 }
+
+export default ReauthorizeCleverModal
--- services/QuillLMS/client/app/bundles/Teacher/components/classrooms/reauthorize_google_modal.tsx
+++ services/QuillLMS/client/app/bundles/Teacher/components/classrooms/reauthorize_google_modal.tsx
@@ -5,39 +5,36 @@ interface ReauthorizeGoogleModalProps {
   close: () => void;
 }
 
-export default class ReauthorizeGoogleModal extends React.Component<ReauthorizeGoogleModalProps, {}> {
-  handleReauthorizeClick = () => {
-    const { googleLink } = this.props
+const ReauthorizeGoogleModal = ({ googleLink, close }: ReauthorizeGoogleModalProps) => {
+  function handleReauthorizeClick() {
     window.location.href = googleLink
   }
 
-  render() {
-    const { close } = this.props
-
-    return (
-      <div className="modal-container reauthorize-google-modal-container">
-        <div className="modal-background" />
-        <div className="reauthorize-google-modal quill-modal modal-body">
-          <div>
-            <h3 className="title">
-              Reauthorize Google
-            </h3>
-          </div>
-          <p>
-            To import a new Google classroom, you need to reauthorize Google access.
-            <br />
-            Clicking reauthorize will re-direct you to Google.
-          </p>
-          <div className="form-buttons">
-            <button className="quill-button outlined secondary medium" onClick={close} type="button">
-              Cancel
-            </button>
-            <button className='quill-button contained primary medium' onClick={this.handleReauthorizeClick} type="button">
-              <span>Reauthorize</span>
-            </button>
-          </div>
+  return (
+    <div className="modal-container reauthorize-google-modal-container">
+      <div className="modal-background" />
+      <div className="reauthorize-google-modal quill-modal modal-body">
+        <div>
+          <h3 className="title">
+            Reauthorize Google
+          </h3>
+        </div>
+        <p>
+          To import a new Google classroom, you need to reauthorize Google access.
+          <br />
+          Clicking reauthorize will re-direct you to Google.
+        </p>
+        <div className="form-buttons">
+          <button className="quill-button focus-on-light outlined secondary medium" onClick={close} type="button">
+            Cancel
+          </button>
+          <button className='quill-button focus-on-light contained primary medium' onClick={handleReauthorizeClick} type="button">
+            Reauthorize
+          </button>
         </div>
       </div>
-    )
-  }
+    </div>
+  )
 }
+
+export default ReauthorizeGoogleModal
