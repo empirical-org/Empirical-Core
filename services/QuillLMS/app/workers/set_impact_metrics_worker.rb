76fc69234854d62018d98145fa70d8a9bb3e9642
Cissy Yu <cissy@quill.org>
2023-07-19 12:31:51 +0800

    one last comment

--- services/QuillLMS/app/workers/set_impact_metrics_worker.rb
+++ services/QuillLMS/app/workers/set_impact_metrics_worker.rb
@@ -18,7 +18,7 @@ class SetImpactMetricsWorker
     teacher_ids = teachers.to_a.map {|teacher| teacher["id"]}
 
     schools = ImpactMetrics::SchoolsContainingCertainTeachersQuery.run(teacher_ids)
-    low_income_schools = schools.select { |school| school["free_lunches"].present? && school["free_lunches"] > FREE_LUNCH_MINIMUM}
+    low_income_schools = schools.select { |school| school.fetch('free_lunches', 0) > FREE_LUNCH_MINIMUM}
 
     number_of_sentences = self.class.round_to_ten_thousands(finished_activity_sessions_count) * SENTENCES_PER_ACTIVITY_SESSION
     $redis.set(PagesController::NUMBER_OF_SENTENCES, number_of_sentences)
