bbd202b5b5faaf6a987dd50748c529377fb36725
Emilia Friedberg <emiliafriedberg@gmail.com>
2023-07-21 10:39:48 -0400

    fix lint and test

--- services/QuillLMS/app/controllers/grades_controller.rb
+++ services/QuillLMS/app/controllers/grades_controller.rb
@@ -10,14 +10,14 @@ class GradesController < ApplicationController
   end
 
   def tooltip
-    render json: { sessions: key_target_skills_query }.to_json
+    render json: { sessions: tooltip_query }.to_json
   end
 
   private def tooltip_params
     params.permit(:classroom_unit_id, :user_id, :completed, :activity_id)
   end
 
-  private def key_target_skills_query
+  private def tooltip_query
     activity_sessions = ActivitySession
       .includes(:concept_results, :activity, :unit)
       .where(
@@ -29,90 +29,38 @@ class GradesController < ApplicationController
       )
       .order(:completed_at)
 
-      activity_sessions.map do |activity_session|
+    activity_sessions.map { |activity_session| format_activity_sessions_for_tooltip(activity_session) }
+  end
 
-        unit_activity = UnitActivity.find_by(unit: activity_session.unit, activity: activity_session.activity)
+  private def format_activity_sessions_for_tooltip(activity_session)
+    unit_activity = UnitActivity.find_by(unit: activity_session.unit, activity: activity_session.activity)
 
-        questions = activity_session.concept_results.group_by { |cr| cr.question_number }
+    questions = activity_session.concept_results.group_by { |cr| cr.question_number }
 
-        key_target_skill_concepts = questions.map { |key, question| get_key_target_skill_concept_for_question(question) }
+    key_target_skill_concepts = questions.map { |key, question| get_key_target_skill_concept_for_question(question) }
 
-        correct_key_target_skill_concepts = key_target_skill_concepts.filter { |ktsc| ktsc[:correct] }
+    correct_key_target_skill_concepts = key_target_skill_concepts.filter { |ktsc| ktsc[:correct] }
 
-        grouped_key_target_skill_concepts = key_target_skill_concepts
-          .group_by { |ktsc| ktsc[:name] }
-          .map do |key, key_target_skill_group|
-            {
-              name: key_target_skill_group.first[:name],
-              correct: key_target_skill_group.filter { |ktsc| ktsc[:correct] }.length,
-              incorrect: key_target_skill_group.filter { |ktsc| ktsc[:correct] == false }.length,
-            }
-          end
-
+    grouped_key_target_skill_concepts = key_target_skill_concepts
+      .group_by { |ktsc| ktsc[:name] }
+      .map do |key, key_target_skill_group|
         {
-          id: activity_session.id,
-          percentage: activity_session.percentage,
-          description: activity_session.activity.description,
-          due_date: unit_activity.due_date,
-          completed_at: activity_session.completed_at + current_user.utc_offset,
-          grouped_key_target_skill_concepts: grouped_key_target_skill_concepts,
-          number_of_questions: questions.length,
-          number_of_correct_questions: correct_key_target_skill_concepts.length
+          name: key_target_skill_group.first[:name],
+          correct: key_target_skill_group.filter { |ktsc| ktsc[:correct] }.length,
+          incorrect: key_target_skill_group.filter { |ktsc| ktsc[:correct] == false }.length,
         }
       end
-  end
 
-  private def tooltip_query
-    # TODO(upgrade) Use ActiveRecord::Sanitization.sanitize_sql_for_conditions
-    return unless tooltip_params['completed']
-
-    RawSqlRunner.execute(
-      <<-SQL
-        SELECT
-          concept_results.correct,
-          activities.description,
-          concepts.name,
-          activity_sessions.completed_at + INTERVAL '#{current_user.utc_offset} seconds' AS completed_at,
-          unit_activities.due_date
-        FROM activity_sessions
-        LEFT JOIN concept_results
-          ON concept_results.activity_session_id = activity_sessions.id
-        LEFT JOIN concepts
-          ON concept_results.concept_id = concepts.id
-        JOIN classroom_units
-          ON classroom_units.id = activity_sessions.classroom_unit_id
-        JOIN activities
-          ON activities.id = activity_sessions.activity_id
-        JOIN unit_activities
-          ON activities.id = unit_activities.activity_id
-          AND unit_activities.unit_id = classroom_units.unit_id
-        WHERE activity_sessions.classroom_unit_id = #{ActiveRecord::Base.connection.quote(tooltip_params[:classroom_unit_id].to_i)}
-          AND activity_sessions.user_id = #{ActiveRecord::Base.connection.quote(tooltip_params[:user_id].to_i)}
-          AND activity_sessions.activity_id = #{ActiveRecord::Base.connection.quote(tooltip_params[:activity_id].to_i)}
-          AND activity_sessions.is_final_score IS true
-          AND activity_sessions.visible
-      SQL
-    ).to_a
-  end
-
-  private def tooltip_scores_query
-    # activity_sessions.completed_at + INTERVAL '#{current_user.utc_offset} seconds' AS completed_at
-    RawSqlRunner.execute(
-      <<-SQL
-        SELECT
-          activity_sessions.percentage,
-          activity_sessions.completed_at + INTERVAL '#{current_user.utc_offset} seconds' AS completed_at
-        FROM activity_sessions
-        WHERE activity_sessions.classroom_unit_id = #{ActiveRecord::Base.connection.quote(tooltip_params[:classroom_unit_id].to_i)}
-          AND activity_sessions.user_id = #{ActiveRecord::Base.connection.quote(tooltip_params[:user_id].to_i)}
-          AND activity_sessions.activity_id = #{ActiveRecord::Base.connection.quote(tooltip_params[:activity_id].to_i)}
-          AND (activity_sessions.percentage IS NOT NULL
-            OR activity_sessions.state = 'finished'
-          )
-          AND activity_sessions.visible
-        ORDER BY activity_sessions.completed_at
-      SQL
-    ).to_a
+    {
+      id: activity_session.id,
+      percentage: activity_session.percentage,
+      description: activity_session.activity.description,
+      due_date: unit_activity.due_date,
+      completed_at: activity_session.completed_at.to_i + current_user.utc_offset.seconds,
+      grouped_key_target_skill_concepts: grouped_key_target_skill_concepts,
+      number_of_questions: questions.length,
+      number_of_correct_questions: correct_key_target_skill_concepts.length
+    }
   end
 
   private def authorize!
