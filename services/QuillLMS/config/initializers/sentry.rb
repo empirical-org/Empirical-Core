6dbc19e7ea967377040b2905692f614dd4c2730e
Brendan Shean <brendanshean@gmail.com>
2023-07-23 08:31:57 -0400

    Upgrade Raven to Sentry

--- services/QuillLMS/config/initializers/sentry.rb
+++ services/QuillLMS/config/initializers/sentry.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
-Raven.configure do |config|
-  config.environments = %W(staging production)
+Sentry.init do |config|
+  config.enabled_environments = %W(staging production)
+  config.breadcrumbs_logger = [:active_support_logger, :http_logger]
 end
