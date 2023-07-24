9323e90d31739528ff4b9af2209e9d10e5fcb075
Emilia Friedberg <emiliafriedberg@gmail.com>
2023-07-24 10:43:01 -0400

    adjust activity analysis

--- services/QuillLMS/client/app/bundles/Teacher/components/modules/score_color.js
+++ services/QuillLMS/client/app/bundles/Teacher/components/modules/score_color.js
@@ -2,6 +2,6 @@ import GradeColor from './grade_color'
 
 // generally used for setting color class names, hence the score color concat at end
 export default function (grade) {
-  const color = GradeColor(grade)
+  const color = GradeColor(grade/100)
   return `${color}-score-color`;
 }
