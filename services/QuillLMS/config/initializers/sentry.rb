eb6d07e6eae4b64f78fce24937fb17bb0f071c0d
Brendan Shean <brendanshean@gmail.com>
2023-07-26 13:19:49 -0400

    Add sentry dsn in for staging/testing

--- services/QuillLMS/config/initializers/sentry.rb
+++ services/QuillLMS/config/initializers/sentry.rb
@@ -3,4 +3,6 @@
 Sentry.init do |config|
   config.enabled_environments = %W(staging production)
   config.breadcrumbs_logger = [:active_support_logger, :http_logger]
+
+  config.dsn = ENV['SENTRY_DSN'] if ENV['SENTRY_DSN'].present?
 end
