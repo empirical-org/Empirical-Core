517b18d99e4d061bec51f9813ef7fcaab9fac0d1
Brendan Shean <brendanshean@gmail.com>
2023-07-30 16:10:14 -0400

    Fix tests

--- services/QuillLMS/client/app/bundles/Teacher/components/assignment_flow/create_unit/stage2/__tests__/assign_students.test.jsx
+++ services/QuillLMS/client/app/bundles/Teacher/components/assignment_flow/create_unit/stage2/__tests__/assign_students.test.jsx
@@ -1,22 +1,15 @@
 import { shallow } from 'enzyme'
 import React from 'react'
 
-import { classroomProps, user } from './test_data/test_data'
+import { classroomProps, googleUser, user } from './test_data/test_data'
 
-import NoClassroomsToImportModal from '../../../../classrooms/no_classrooms_to_import_modal'
-import ImportProviderClassroomsModal from '../../../../classrooms/import_provider_classrooms_modal'
 import LinkProviderAccountModal from '../../../../classrooms/link_provider_account_modal'
-import AssignStudents, {
-  noClassroomsToImportModal,
-  importProviderClassroomsModal,
-  linkProviderAccountModal
-} from '../assign_students'
+import AssignStudents from '../assign_students'
 import ClassroomCard from '../classroom_card.tsx'
 import CreateAClassInlineForm from '../create_a_class_inline_form.tsx'
 
 
 describe('Assign students component', () => {
-
   it('should render', () => {
     const wrapper = shallow(
       <AssignStudents
@@ -30,56 +23,56 @@ describe('Assign students component', () => {
     expect(wrapper).toMatchSnapshot()
   })
 
-  // describe('if this.state.showFormOrModal = createAClassForm', () => {
+  describe('user clicks create a class button', () => {
+    const wrapper = shallow(
+      <AssignStudents
+        classrooms={classroomProps}
+        fetchClassrooms={() => {}}
+        toggleClassroomSelection={() => {}}
+        toggleStudentSelection={() => {}}
+        user={user}
+      />
+    )
+
+    it('should render a createAClassInlineForm component', () => {
+      wrapper.find('.create-a-class-button').simulate('click')
+      expect(wrapper.find(CreateAClassInlineForm).exists()).toBe(true)
+    })
+  })
+
+  // describe('user clicks import from google', () => {
   //   const wrapper = shallow(
   //     <AssignStudents
   //       classrooms={classroomProps}
   //       fetchClassrooms={() => {}}
   //       toggleClassroomSelection={() => {}}
   //       toggleStudentSelection={() => {}}
-  //       user={user}
+  //       user={googleUser}
   //     />
   //   )
-
-  //   it('should render a createAClassInlineForm component', () => {
-  //     wrapper.find('.create-a-class').simulate('click')
-  //     expect(wrapper.find(CreateAClassInlineForm).exists()).toBe(true)
-  //   })
-  // })
-
-  // describe('if this.state.showFormOrModal = importProviderClassroomsModal', () => {
-  //   const wrapper = shallow(
-  //     <AssignStudents
-  //       classrooms={classroomProps}
-  //       fetchClassrooms={() => {}}
-  //       toggleClassroomSelection={() => {}}
-  //       toggleStudentSelection={() => {}}
-  //       user={user}
-  //     />
-  //   )
-  //   wrapper.setState({ showFormOrModal: importProviderClassroomsModal })
 
   //   it('should render a importProviderClassroomsModal component', () => {
   //     expect(wrapper.find(ImportProviderClassroomsModal).exists()).toBe(true)
   //   })
   // })
 
-  // describe('if this.state.showFormOrModal = linkProviderAccountModal', () => {
-  //   const wrapper = shallow(
-  //     <AssignStudents
-  //       classrooms={classroomProps}
-  //       fetchClassrooms={() => {}}
-  //       toggleClassroomSelection={() => {}}
-  //       toggleStudentSelection={() => {}}
-  //       user={user}
-  //     />
-  //   )
-  //   wrapper.setState({ showFormOrModal: linkProviderAccountModal })
+  describe('if this.state.showFormOrModal = linkProviderAccountModal', () => {
+    const wrapper = shallow(
+      <AssignStudents
+        classrooms={classroomProps}
+        fetchClassrooms={() => {}}
+        toggleClassroomSelection={() => {}}
+        toggleStudentSelection={() => {}}
+        user={user}
+      />
+    )
 
-  //   it('should render a linkProviderAccountModal component', () => {
-  //     expect(wrapper.find(LinkProviderAccountModal).exists()).toBe(true)
-  //   })
-  // })
+    wrapper.find('button').filterWhere(node => node.text() === 'Import from Canvas').simulate('click');
+
+    it('should render a linkProviderAccountModal component', () => {
+      expect(wrapper.find(LinkProviderAccountModal).exists()).toBe(true)
+    })
+  })
 
   // describe('if this.state.showFormOrModal = noClassroomsToImportModal', () => {
   //   const wrapper = shallow(
@@ -91,6 +84,8 @@ describe('Assign students component', () => {
   //       user={user}
   //     />
   //   )
+
+
   //   wrapper.setState({ showFormOrModal: noClassroomsToImportModal })
 
   //   it('should render a noClassroomsToImportModal component', () => {
@@ -98,37 +93,37 @@ describe('Assign students component', () => {
   //   })
   // })
 
-  // describe('if there are classrooms', () => {
-  //   const wrapper = shallow(
-  //     <AssignStudents
-  //       classrooms={classroomProps}
-  //       fetchClassrooms={() => {}}
-  //       toggleClassroomSelection={() => {}}
-  //       toggleStudentSelection={() => {}}
-  //       user={user}
-  //     />
-  //   )
+  describe('if there are classrooms', () => {
+    const wrapper = shallow(
+      <AssignStudents
+        classrooms={classroomProps}
+        fetchClassrooms={() => {}}
+        toggleClassroomSelection={() => {}}
+        toggleStudentSelection={() => {}}
+        user={user}
+      />
+    )
 
-  //   it('should render a classroom card for each classroom', () => {
-  //     expect(wrapper.find(ClassroomCard).length).toBe(classroomProps.length)
-  //   })
+    it('should render a classroom card for each classroom', () => {
+      expect(wrapper.find(ClassroomCard).length).toBe(classroomProps.length)
+    })
 
-  // })
+  })
 
-  // describe('if there are no classrooms', () => {
-  //   const wrapper = shallow(
-  //     <AssignStudents
-  //       classrooms={[]}
-  //       fetchClassrooms={() => {}}
-  //       toggleClassroomSelection={() => {}}
-  //       toggleStudentSelection={() => {}}
-  //       user={user}
-  //     />
-  //   )
+  describe('if there are no classrooms', () => {
+    const wrapper = shallow(
+      <AssignStudents
+        classrooms={[]}
+        fetchClassrooms={() => {}}
+        toggleClassroomSelection={() => {}}
+        toggleStudentSelection={() => {}}
+        user={user}
+      />
+    )
 
-  //   it('should render an empty state', () => {
-  //     expect(wrapper.find('.no-active-classes').exists()).toBe(true)
-  //   })
-  // })
+    it('should render an empty state', () => {
+      expect(wrapper.find('.no-active-classes').exists()).toBe(true)
+    })
+  })
 
 })
