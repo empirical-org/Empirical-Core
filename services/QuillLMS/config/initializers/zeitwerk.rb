1757626c19ec6b7abec2ef47653074b7ded2845b
Brendan Shean <brendanshean@gmail.com>
2023-07-31 15:10:36 -0400

    Update zeitwerk initializer

--- services/QuillLMS/config/initializers/zeitwerk.rb
+++ services/QuillLMS/config/initializers/zeitwerk.rb
@@ -3,10 +3,13 @@
 Rails.autoloaders.each do |autoloader|
   autoloader.inflector = Zeitwerk::Inflector.new
   autoloader.inflector.inflect(
+    'auto_ml' => 'AutoML',
     'concept_replacement_cms_worker' => 'ConceptReplacementCMSWorker',
     'concept_replacement_lms_worker' => 'ConceptReplacementLMSWorker',
     'ell_starter_diagnostic_email_job' => 'ELLStarterDiagnosticEmailJob',
     'graphiql' => 'GraphiQL',
+    'html_tag_remover' => 'HTMLTagRemover',
+    'open_ai' => 'OpenAI',
     'pusher_csv_export_completed' => 'PusherCSVExportCompleted',
     'report_demo_ap_creator' => 'ReportDemoAPCreator',
     'sso_request' => 'SSORequest',
