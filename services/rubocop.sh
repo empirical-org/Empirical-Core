5e724d972ef485d2233601e6930686b6f79d9c8e
Brendan Shean <brendanshean@gmail.com>
2023-08-01 14:55:34 -0400

    Update rubocop.sh

--- services/rubocop.sh
+++ services/rubocop.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
-rbenv local 2.7.8
-gem install rubocop -v 1.28.2
-gem install rubocop-rspec -v 2.10
-gem install activesupport -v 6.1.5.1
-gem install rubocop-rails -v 2.14.2
+RUBY_VERSION='3.0.6'
+RUBOCOP_VERSION='1.28.2'
+RUBOCOP_RSPEC_VERSION='2.10'
+RUBOCOP_RAILS_VERSION='2.14.2'
+
+if command -v asdf &> /dev/null; then
+    asdf local ruby $RUBY_VERSION
+elif command -v rbenv &> /dev/null; then
+    rbenv local $RUBY_VERSION
+fi
+
+gem install rubocop -v $RUBOCOP_VERSION
+gem install rubocop-rspec -v $RUBOCOP_RSPEC_VERSION
+gem install rubocop-rails -v $RUBOCOP_RAILS_VERSION
 rubocop
