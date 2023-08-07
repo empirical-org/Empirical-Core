0e601f8877b64c6f9ca1426861a42f2708d4fa1f
Brendan Shean <brendanshean@gmail.com>
2023-08-07 08:37:30 -0400

    Move namespace out of constant declaration in spec

--- services/QuillLMS/config/initializers/rails_admin.rb
+++ services/QuillLMS/config/initializers/rails_admin.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-Rails.application.config.after_initialize do
+Rails.application.config.eager_load do
   RailsAdmin.config do |config|
     config.asset_source = :sprockets
 
