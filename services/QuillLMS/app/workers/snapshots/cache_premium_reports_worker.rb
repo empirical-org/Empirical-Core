110ed086094aedc17c0bf495d1d2f88bc4965e96
Eric Adams <erictayloradams@gmail.com>
2023-08-08 09:46:46 -0400

    Premium Hub Data Export (#10843)
    * add components for data export
    
    * add backend logic for data export query; tweak frontend to handle data fetch
    
    * tweak query
    
    * add table styling
    
    * tweak query and add some data formatting boilerplate:
    
    * finishing touches; add tests
    
    * small refactors; fix lint errors
    
    * various refactors
    
    * add test
    
    * update tests
    
    * add worker spec

---
+++ services/QuillLMS/app/workers/snapshots/cache_premium_reports_worker.rb
@@ -0,0 +1,44 @@
+# frozen_string_literal: true
+
+module Snapshots
+  class CachePremiumReportsWorker
+    include Sidekiq::Worker
+
+    PUSHER_EVENT = 'data-export-cached'
+
+    QUERIES = {
+      'data-export' => Snapshots::DataExportQuery
+    }
+
+    def perform(cache_key, query, user_id, timeframe, school_ids, filters)
+      payload = generate_payload(query, timeframe, school_ids, filters)
+      Rails.cache.write(cache_key, payload, expires_in: cache_expiry)
+
+      PusherTrigger.run(user_id, PUSHER_EVENT,
+        {
+          query: query,
+          timeframe: timeframe['name'],
+          school_ids: school_ids
+        }.merge(filters)
+      )
+    end
+
+    private def cache_expiry
+      # We need to pass a number of seconds to Redis as a TTL value, and we
+      # want to expire our caches overnight so that each day reports can be
+      # updated, so we want the number of seconds until end of day
+      now = DateTime.current
+      now.end_of_day.to_i - now.to_i
+    end
+
+    private def generate_payload(query, timeframe, school_ids, filters)
+      filters_symbolized = filters.symbolize_keys
+
+      QUERIES[query].run(**{
+        timeframe_start: DateTime.parse(timeframe['current_start']),
+        timeframe_end: DateTime.parse(timeframe['current_end']),
+        school_ids: school_ids
+      }.merge(filters_symbolized))
+    end
+  end
+end
