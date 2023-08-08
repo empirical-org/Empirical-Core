171c918f154751624e748993bd6dd0035e7f3951
Brendan Shean <brendanshean@gmail.com>
2023-08-08 16:26:50 -0400

    Update rubocop.sh

--- services/rubocop.sh
+++ services/rubocop.sh
@@ -10,7 +10,10 @@ elif command -v rbenv &> /dev/null; then
     rbenv local $RUBY_VERSION
 fi
 
-gem install rubocop -v $RUBOCOP_VERSION
-gem install rubocop-rspec -v $RUBOCOP_RSPEC_VERSION
-gem install rubocop-rails -v $RUBOCOP_RAILS_VERSION
+gem install unicode-display_width
+gem install rubocop -v 1.28.2
+gem install rubocop-rspec -v 2.10
+gem install rubocop-rails -v 2.14.2
+gem install rubocop-ast -v 1.29
+gem install ruby-progressbar
 rubocop
