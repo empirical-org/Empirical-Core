16dac2fec14654555a44826b06493ddb141765b4
Peter Kong <pkong@quill.org>
2023-08-09 20:38:24 -0400

    this config has a working build

--- services/QuillLMS/vite.config.ts
+++ services/QuillLMS/vite.config.ts
@@ -1,6 +1,6 @@
 import { viteCommonjs } from '@originjs/vite-plugin-commonjs';
-//import react from '@vitejs/plugin-react';
 import replace from '@rollup/plugin-replace';
+//import react from '@vitejs/plugin-react';
 import fs from 'fs/promises';
 import path, { resolve } from 'path';
 import friendlyTypeImports from 'rollup-plugin-friendly-type-imports';
@@ -31,11 +31,20 @@ export default defineConfig(({command, mode}) => {
       }
     },
 
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `@import "./app/assets/stylesheets/variables.scss";`
+        }
+      }
+    },
+
     define: {
+      //process: ({ env: {}}) // Doing this triggers a rollup parse error
     },
     plugins: [
       replace({
-        'process.env.NODE_ENV': JSON.stringify('production'),
+        'process.env.NODE_ENV': JSON.stringify('production'), // TODO: parameterize
         'process.env.BAR': JSON.stringify('foo')
       }),
       requireTransform(),
@@ -45,15 +54,39 @@ export default defineConfig(({command, mode}) => {
       RubyPlugin(),
       //react() // required, to address the 'global' not found error
       // react({
-      //   jsxRuntime: 'classic',
+      //   //jsxRuntime: 'classic',
       // })
     ],
     esbuild: {
+      //loader: "tsx",//may not want this directive at all; still experimenting
+      //include: /src\/.*\.jsx?$/
+      // // Business as usual for .jsx and .tsx files
+      // "client/**/*.jsx",
+      // "client/**/*.tsx",
+      // "node_modules/**/*.jsx",
+      // "node_modules/**/*.tsx",
+      // "client/node_modules/**/*.jsx",
+      // "client/node_modules/**/*.tsx",
+
+      // // js file rules
+      // "client/**/*.js",
+      // "node_modules/**/*.js",
+      // "client/node_modules/**/*.js",
+
+      // // Add these lines to allow all .ts files to contain JSX
+      // "client/**/*.ts",
+      // "node_modules/**/*.ts",
+      // "client/node_modules/**/*.ts",
+      include: /\.(tsx?|jsx?)$/,
+      exclude: [],
+      loader: 'tsx'
+
     },
     optimizeDeps: {
       esbuildOptions: {
 
         // so jsx syntax can be detected
+        // Removing the plugin does not fix "SyntaxError: Unexpected token" in build mode
         plugins: [
           {
             name: "load-js-files-as-jsx",
