31eea53d6bbc8d81e18cdb92c67e3c8d789752d7
Brendan Shean <brendanshean@gmail.com>
2023-08-10 20:07:18 -0400

    Update AccessTokenRefresher

--- services/QuillLMS/spec/services/google_integration/access_token_refresher_spec.rb
+++ services/QuillLMS/spec/services/google_integration/access_token_refresher_spec.rb
@@ -4,26 +4,43 @@ require 'rails_helper'
 
 module GoogleIntegration
   RSpec.describe AccessTokenRefresher do
-    subject { described_class.run(auth_credential, authorization_client) }
+    subject { described_class.run(auth_credential) }
 
     let(:auth_credential) { create(:google_auth_credential) }
-
-    let(:authorization_client) do
-      instance_double(
-        Signet::OAuth2::Client,
-        refresh!: nil,
-        access_token: access_token,
-        refresh_token: refresh_token,
-        expires_at: expires_at
-      )
-    end
+    let(:signet_authorization_error) { ::Signet::AuthorizationError.new('error') }
 
     let(:access_token) { 'access_token' }
     let(:refresh_token) { 'refresh_token' }
     let(:expires_at) { 1.hour.from_now }
 
-    it { expect { subject }.to change(auth_credential, :access_token).to(access_token) }
-    it { expect { subject }.to change(auth_credential, :refresh_token).to(refresh_token) }
-    it { expect { subject }.to change(auth_credential, :expires_at).to(be_within(1.second).of(1.hour.from_now)) }
+    let(:client) do
+      double(
+        Signet::OAuth2::Client,
+        access_token: access_token,
+        expires_at: expires_at,
+        refresh!: nil,
+        refresh_token: refresh_token
+      )
+    end
+
+    before { allow(SignetClientFetcher).to receive(:run).with(auth_credential.refresh_token).and_return(client) }
+
+    context 'when the refresh token is invalid' do
+      before { allow(client).to receive(:refresh!).and_raise(signet_authorization_error) }
+
+      it { expect { subject }.to raise_error(described_class::ClientRefreshError) }
+
+      it 'destroys auth credential if token cannot be refreshed' do
+        subject
+      rescue described_class::ClientRefreshError
+        expect(auth_credential).not_to be_persisted
+      end
+    end
+
+    context 'when the refresh token is valid' do
+      it { expect { subject }.to change { auth_credential.reload.access_token }.to(access_token) }
+      it { expect { subject }.to change { auth_credential.reload.expires_at }.to(expires_at) }
+      it { expect { subject }.to change { auth_credential.reload.refresh_token }.to(refresh_token) }
+    end
   end
 end
