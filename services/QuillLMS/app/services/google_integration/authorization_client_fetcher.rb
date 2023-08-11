e9977a45dba5259da9f0023126daae75ab88cf4b
brendanshean <brendanshean@gmail.com>
2023-08-10 22:00:38 -0400

    Fix signet:authorization error (#10899)
    * Add SignetClientFetcher
    
    * Update AccessTokenRefresher
    
    * Add RefreshTokenValidator
    
    * Update AuthorizationClientFetcher
    
    * Fix spec
    
    * Handle nil TeacherClassroomsCache

--- services/QuillLMS/app/services/google_integration/authorization_client_fetcher.rb
+++ services/QuillLMS/app/services/google_integration/authorization_client_fetcher.rb
@@ -2,8 +2,6 @@
 
 module GoogleIntegration
   class AuthorizationClientFetcher < ::ApplicationService
-    TOKEN_CREDENTIAL_URI = 'https://oauth2.googleapis.com/token'
-
     attr_reader :auth_credential
 
     def initialize(auth_credential)
@@ -11,24 +9,28 @@ module GoogleIntegration
     end
 
     def run
+      validate_refresh_token
       refresh_access_token
-      client
+      fetch_signet_client
     end
 
-    private def client
-      @client ||=
-        Signet::OAuth2::Client.new(
-          client_id: Auth::Google::CLIENT_ID,
-          client_secret: Auth::Google::CLIENT_SECRET,
-          refresh_token: auth_credential.refresh_token,
-          token_credential_uri: TOKEN_CREDENTIAL_URI
-        )
+    private def fetch_signet_client
+      SignetClientFetcher.run(refresh_token)
     end
 
     private def refresh_access_token
       return unless auth_credential.access_token_expired?
 
-      AccessTokenRefresher.run(auth_credential, client)
+      AccessTokenRefresher.run(auth_credential)
+    end
+
+    private def refresh_token
+      # reload in case the AccessTokenRefresher updated the refresh_token
+      auth_credential.reload.refresh_token
+    end
+
+    private def validate_refresh_token
+      RefreshTokenValidator.run(auth_credential)
     end
   end
 end
