0d928790f58c4885723141ad5cb3f13a52cd59bd
Peter Kong <pkong@quill.org>
2023-08-11 16:52:14 -0400

    comment out initializers/react_on_rails.rb

--- services/QuillLMS/config/initializers/react_on_rails.rb
+++ services/QuillLMS/config/initializers/react_on_rails.rb
@@ -1,15 +1,15 @@
 # frozen_string_literal: true
 
 # Shown below are the defaults for configuration
-ReactOnRails.configure do |config|
-  config.generated_assets_dir = Rails.env.test? ? File.join(%w[public webpack-test]) : File.join(%w[public webpack])
-  config.build_test_command = "npm run build:test"
-  config.server_bundle_js_file = ""
-  config.server_renderer_pool_size = 1
-  config.server_renderer_timeout = 20
-  config.development_mode = Rails.env.development?
-  config.replay_console = true
-  config.logging_on_server = true
-  config.prerender = false
-  config.trace = Rails.env.development?
-end
+# ReactOnRails.configure do |config|
+#   config.generated_assets_dir = Rails.env.test? ? File.join(%w[public webpack-test]) : File.join(%w[public webpack])
+#   config.build_test_command = "npm run build:test"
+#   config.server_bundle_js_file = ""
+#   config.server_renderer_pool_size = 1
+#   config.server_renderer_timeout = 20
+#   config.development_mode = Rails.env.development?
+#   config.replay_console = true
+#   config.logging_on_server = true
+#   config.prerender = false
+#   config.trace = Rails.env.development?
+# end
