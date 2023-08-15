fceca2c2d58e13fcee2b37694eb4485d64c393e7
Brendan Shean <brendanshean@gmail.com>
2023-08-14 20:31:40 -0400

    Fix import error

--- services/QuillLMS/client/app/bundles/Teacher/components/progress_reports/constants.js
+++ services/QuillLMS/client/app/bundles/Teacher/components/progress_reports/constants.js
@@ -4,5 +4,6 @@ export const DIAGNOSTIC_KEY = 'diagnostic'
 export const CONNECT_KEY = 'connect'
 export const LESSONS_KEY = 'lessons'
 export const EVIDENCE_KEY = 'evidence'
+export const PROOFREADER_KEY = 'passage'
 
 export const NOT_SCORED_DISPLAY_TEXT = 'Not Scored'
