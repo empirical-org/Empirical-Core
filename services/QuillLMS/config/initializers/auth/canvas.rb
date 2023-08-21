c41cc26ef090d42209829b0f913f09854c37d304
Brendan Shean <brendanshean@gmail.com>
2023-08-21 08:08:59 -0400

    Add scopes to canvas oauth config

--- services/QuillLMS/config/initializers/auth/canvas.rb
+++ services/QuillLMS/config/initializers/auth/canvas.rb
@@ -8,9 +8,25 @@ module Auth
     OMNIAUTH_CALLBACK_PATH = '/auth/canvas/callback'
 
     REAUTHORIZATION_PATH = OMNIAUTH_REQUEST_PATH
+
+    SCOPE_OPTIONS = %w[
+      url:GET|/api/v1/courses
+      url:GET|/api/v1/courses/:course_id/sections
+      url:GET|/api/v1/courses/:course_id/sections/:id
+      url:GET|/api/v1/sections/:id
+      url:GET|/api/v1/courses
+      url:GET|/api/v1/courses/:id
+      url:GET|/api/v1/courses/:course_id/students
+      url:GET|/api/v1/courses/:course_id/users
+      url:GET|/api/v1/courses/:course_id/users/:id
+      url:GET|/api/v1/users/:user_id/courses
+      /auth/userinfo
+    ].freeze
   end
 end
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :canvas, setup: Auth::Canvas::Setup
+  provider :canvas,
+    setup: Auth::Canvas::Setup,
+    scope: Auth::Canvas::SCOPE_OPTIONS.join(' ')
 end
