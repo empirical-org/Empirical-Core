48e41bcdc9248fe7639e37bfbac9dc5ca490b559
Emilia Friedberg <emiliafriedberg@gmail.com>
2023-08-28 10:28:40 -0400

    fix other tests

--- services/QuillLMS/spec/services/diagnostics_organized_by_classroom_fetcher_spec.rb
+++ services/QuillLMS/spec/services/diagnostics_organized_by_classroom_fetcher_spec.rb
@@ -16,61 +16,49 @@ RSpec.describe DiagnosticsOrganizedByClassroomFetcher do
   let!(:date_before_question_switchover) { DateTime.new(2023, 7, 18, 0, 0, 0) }
   let!(:date_after_question_switchover) { DateTime.new(2023, 7, 20, 0, 0, 0) }
 
-  subject { described_class.new(user, is_demo) }
+  subject { described_class.new(user) }
 
-  context "user is demo" do
-    let(:is_demo) { true }
+  describe 'record_with_aggregated_activity_sessions' do
 
-    describe 'record_with_aggregated_activity_sessions' do
+    context 'there are no completed activity sessions' do
+      it { expect(subject.record_with_aggregated_activity_sessions(activity.id, classroom.id)['eligible_for_question_scoring']).to eq(true) }
+    end
+
+    context 'all activity sessions were completed after the question scoring switchover date' do
+      let!(:activity_session) { create(:activity_session, user: student1, activity: activity, classroom_unit: classroom_unit1, completed_at: date_after_question_switchover)}
+
+      it { expect(subject.record_with_aggregated_activity_sessions(activity.id, classroom.id)['eligible_for_question_scoring']).to eq(true) }
+    end
+
+    context 'all activity sessions were completed before the question scoring switchover date' do
+      let!(:activity_session) { create(:activity_session, user: student1, activity: activity, classroom_unit: classroom_unit1, completed_at: date_before_question_switchover)}
+
       it { expect(subject.record_with_aggregated_activity_sessions(activity.id, classroom.id)['eligible_for_question_scoring']).to eq(false) }
     end
-  end
 
-  context "user is not demo" do
-    let(:is_demo) { false }
+    context 'every student who completed an activity session for the given activity and classroom before the switchover also completed one after' do
+      let!(:unit2) { create(:unit, user: user) }
+      let!(:unit_activity2) { create(:unit_activity, unit: unit2, activity: activity )}
+      let!(:classroom_unit2) { create(:classroom_unit, unit: unit2, classroom: classroom, assigned_student_ids: [student1.id]) }
+      let!(:activity_session1) { create(:activity_session, user: student1, activity: activity, classroom_unit: classroom_unit1, completed_at: date_after_question_switchover)}
+      let!(:activity_session2) { create(:activity_session, user: student1, activity: activity, classroom_unit: classroom_unit2, completed_at: date_before_question_switchover)}
 
-    describe 'record_with_aggregated_activity_sessions' do
-
-      context 'there are no completed activity sessions' do
-        it { expect(subject.record_with_aggregated_activity_sessions(activity.id, classroom.id)['eligible_for_question_scoring']).to eq(true) }
-      end
-
-      context 'all activity sessions were completed after the question scoring switchover date' do
-        let!(:activity_session) { create(:activity_session, user: student1, activity: activity, classroom_unit: classroom_unit1, completed_at: date_after_question_switchover)}
-
-        it { expect(subject.record_with_aggregated_activity_sessions(activity.id, classroom.id)['eligible_for_question_scoring']).to eq(true) }
-      end
-
-      context 'all activity sessions were completed before the question scoring switchover date' do
-        let!(:activity_session) { create(:activity_session, user: student1, activity: activity, classroom_unit: classroom_unit1, completed_at: date_before_question_switchover)}
-
-        it { expect(subject.record_with_aggregated_activity_sessions(activity.id, classroom.id)['eligible_for_question_scoring']).to eq(false) }
-      end
-
-      context 'every student who completed an activity session for the given activity and classroom before the switchover also completed one after' do
-        let!(:unit2) { create(:unit, user: user) }
-        let!(:unit_activity2) { create(:unit_activity, unit: unit2, activity: activity )}
-        let!(:classroom_unit2) { create(:classroom_unit, unit: unit2, classroom: classroom, assigned_student_ids: [student1.id]) }
-        let!(:activity_session1) { create(:activity_session, user: student1, activity: activity, classroom_unit: classroom_unit1, completed_at: date_after_question_switchover)}
-        let!(:activity_session2) { create(:activity_session, user: student1, activity: activity, classroom_unit: classroom_unit2, completed_at: date_before_question_switchover)}
-
-        it { expect(subject.record_with_aggregated_activity_sessions(activity.id, classroom.id)['eligible_for_question_scoring']).to eq(true) }
-      end
-
-      context 'not every student who completed an activity session for the given activity and classroom before the switchover also completed one after' do
-        let!(:student2) { create(:student) }
-        let!(:classroom_student2) { create(:students_classrooms, classroom: classroom, student: student2) }
-        let!(:unit2) { create(:unit, user: user) }
-        let!(:unit_activity2) { create(:unit_activity, unit: unit2, activity: activity )}
-        let!(:classroom_unit2) { create(:classroom_unit, unit: unit2, classroom: classroom, assigned_student_ids: [student1.id, student2.id]) }
-        let!(:activity_session1) { create(:activity_session, user: student1, activity: activity, classroom_unit: classroom_unit1, completed_at: date_after_question_switchover)}
-        let!(:activity_session2) { create(:activity_session, user: student1, activity: activity, classroom_unit: classroom_unit2, completed_at: date_before_question_switchover)}
-        let!(:activity_session3) { create(:activity_session, user: student2, activity: activity, classroom_unit: classroom_unit2, completed_at: date_before_question_switchover)}
-
-        it { expect(subject.record_with_aggregated_activity_sessions(activity.id, classroom.id)['eligible_for_question_scoring']).to eq(false) }
-      end
-
+      it { expect(subject.record_with_aggregated_activity_sessions(activity.id, classroom.id)['eligible_for_question_scoring']).to eq(true) }
     end
+
+    context 'not every student who completed an activity session for the given activity and classroom before the switchover also completed one after' do
+      let!(:student2) { create(:student) }
+      let!(:classroom_student2) { create(:students_classrooms, classroom: classroom, student: student2) }
+      let!(:unit2) { create(:unit, user: user) }
+      let!(:unit_activity2) { create(:unit_activity, unit: unit2, activity: activity )}
+      let!(:classroom_unit2) { create(:classroom_unit, unit: unit2, classroom: classroom, assigned_student_ids: [student1.id, student2.id]) }
+      let!(:activity_session1) { create(:activity_session, user: student1, activity: activity, classroom_unit: classroom_unit1, completed_at: date_after_question_switchover)}
+      let!(:activity_session2) { create(:activity_session, user: student1, activity: activity, classroom_unit: classroom_unit2, completed_at: date_before_question_switchover)}
+      let!(:activity_session3) { create(:activity_session, user: student2, activity: activity, classroom_unit: classroom_unit2, completed_at: date_before_question_switchover)}
+
+      it { expect(subject.record_with_aggregated_activity_sessions(activity.id, classroom.id)['eligible_for_question_scoring']).to eq(false) }
+    end
+
   end
 
 end
