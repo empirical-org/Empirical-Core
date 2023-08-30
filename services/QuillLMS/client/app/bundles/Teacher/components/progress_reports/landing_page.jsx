ed814cc9ab8e820737b5c2c95196afe46741621c
Emilia Friedberg <emiliafriedberg@gmail.com>
2023-08-30 12:25:43 -0400

    update my reports page

--- services/QuillLMS/client/app/bundles/Teacher/components/progress_reports/landing_page.jsx
+++ services/QuillLMS/client/app/bundles/Teacher/components/progress_reports/landing_page.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 
 import { PROGRESS_REPORTS_SELECTED_CLASSROOM_ID, } from './progress_report_constants';
 
-import { DropdownInput, } from '../../../Shared/index';
 import DemoOnboardingTour, { DEMO_ONBOARDING_STUDENT_REPORTS_LANDING_PAGE, } from '../shared/demo_onboarding_tour';
 
 const ALL = 'ALL'
@@ -14,7 +13,7 @@ const miniList = () => {
       title: 'Activity Summary',
       href: '/teachers/classrooms/scorebook',
       img: `${process.env.CDN_URL}/images/shared/visual_overview.svg`,
-      bodyText: 'Quickly see which activities your students have completed with color coded icons that show level of proficiency.',
+      bodyText: 'Quickly see which activities your students have completed and the skills that were demonstrated.',
       pStyle: { padding: '0px 2px', },
       flag: null,
     }, {
@@ -28,14 +27,14 @@ const miniList = () => {
       href: '/teachers/progress_reports/diagnostic_reports/#/diagnostics',
       id: 'diagnostic-reports-card',
       img: `${process.env.CDN_URL}/images/shared/diagnostic.svg`,
-      bodyText: 'View the results of the diagnostics, and get a personalized learning plan with recommended activities.',
+      bodyText: 'View diagnostic results and get a personalized learning plan with recommended activities.',
       flag: null,
     }, {
       title: 'Activity Scores',
       premium: true,
       href: '/teachers/progress_reports/activities_scores_by_classroom',
       img: `${process.env.CDN_URL}/images/illustrations/activity-scores-illustration.svg`,
-      bodyText: 'View and download the overall score and the individual scores on each activity in an activity pack as a CSV.',
+      bodyText: 'View and download each student’s overall score and their individual scores per activity as a PDF or CSV.',
       flag: null,
       pStyle: { padding: '0px 7px', },
     }, {
@@ -43,21 +42,21 @@ const miniList = () => {
       premium: true,
       href: '/teachers/progress_reports/concepts/students',
       img: `${process.env.CDN_URL}/images/shared/concepts.svg`,
-      bodyText: 'View an overall summary of how each of your students is performing on all of the  writing and grammar concepts.',
+      bodyText: 'View an overall summary of how each of your students is performing across all writing and grammar concepts.',
       flag: null,
     }, {
       title: 'Standards',
       premium: true,
       href: '/teachers/progress_reports/standards/classrooms',
       img: `${process.env.CDN_URL}/images/shared/common_core.svg`,
-      bodyText: 'Following the Common Core? Check this view to see how your students are performing on specific standards.',
+      bodyText: 'View this report to see how your students are performing on specific National standards.',
       flag: null,
     }, {
       title: 'Data Export',
       premium: true,
       href: '/teachers/progress_reports/activity_sessions',
       img: `${process.env.CDN_URL}/images/shared/list_overview.svg`,
-      bodyText: 'You can export the data as a CSV file by filtering for the classrooms, activity packs, or students you would like to export and then pressing "Download Report."',
+      bodyText: 'You can export the data as a CSV file by filtering for the classrooms, activity packs, or students you would like to export and then pressing “Download Report.”',
       flag: null,
       pStyle: { marginTop: '-17px', },
     }
@@ -65,7 +64,7 @@ const miniList = () => {
 }
 
 const miniBuilder = (mini) => {
-  const premium = mini.premium ? <h4 className="premium">Premium<i aria-hidden="true" className="fas fa-star" /></h4> : null;
+  const premium = mini.premium ? <h4 className="premium"><span>Premium</span><img alt="" src={`${process.env.CDN_URL}/images/icons/yellow-diamond.svg`} /></h4> : null;
   return (
     <div className="generic-mini" id={mini.id} key={mini.title}>
       <a href={mini.href}>
@@ -82,16 +81,6 @@ const miniBuilder = (mini) => {
 
 
 const LandingPage = ({ classrooms, flag, }) => {
-  const selectedClassroom = classrooms && classrooms.find(c => Number(c.id) === Number(window.localStorage.getItem(PROGRESS_REPORTS_SELECTED_CLASSROOM_ID)))
-  const [selectedClassroomId, setSelectedClassroomId] = React.useState(selectedClassroom && selectedClassroom.id || ALL)
-
-  function onClassesDropdownChange(e) {
-    window.localStorage.setItem(PROGRESS_REPORTS_SELECTED_CLASSROOM_ID, e.value)
-    setSelectedClassroomId(e.value)
-  }
-
-  const dropdownOptions = [ALL_OPTION].concat(classrooms ? classrooms.map(c => ({ value: c.id, label: c.name, })) : [])
-
   const minis = () => {
     const minisArr = [];
     miniList().forEach((mini) => {
@@ -115,12 +104,6 @@ const LandingPage = ({ classrooms, flag, }) => {
       <div className="generic-mini-container">
         <div className="header">
           <h1>Choose which type of report you’d like to see:</h1>
-          <DropdownInput
-            handleChange={onClassesDropdownChange}
-            isSearchable={false}
-            options={dropdownOptions}
-            value={dropdownOptions.find(opt => opt.value === selectedClassroomId)}
-          />
         </div>
         <div className="generic-minis">{minis()}</div>
       </div>
