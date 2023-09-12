2c2e3a25523a086c89bd4a09f2d591f305762272
Emilia Friedberg <emiliafriedberg@gmail.com>
2023-09-12 15:35:27 -0400

    small refactor

--- services/QuillLMS/client/app/bundles/Teacher/components/accounts/login/__tests__/login_form.test.tsx
+++ services/QuillLMS/client/app/bundles/Teacher/components/accounts/login/__tests__/login_form.test.tsx
@@ -10,6 +10,13 @@ const props = {
   googleOfflineAccessExpired: null
 }
 
+const setup = () => {
+  render(<LoginForm {...props} />)
+  const user = userEvent.setup()
+
+  return { user, }
+}
+
 describe('LoginForm', () => {
   test('it should render', () => {
     const { asFragment } = render(<LoginForm {...props} />);
@@ -17,8 +24,7 @@ describe('LoginForm', () => {
   })
 
   test('typing in the email or username field should update the text of the field', async () => {
-    render(<LoginForm {...props} />)
-    const user = userEvent.setup()
+    const { user, } = setup()
 
     const typedText = 'anything'
     await user.type(screen.getByRole('textbox', { name: /email or username/i }), typedText)
@@ -27,8 +33,7 @@ describe('LoginForm', () => {
   })
 
   test('typing in the password field should update the text of the field', async () => {
-    render(<LoginForm {...props} />)
-    const user = userEvent.setup()
+    const { user, } = setup()
 
     const typedText = 'anything'
 
@@ -39,8 +44,7 @@ describe('LoginForm', () => {
   })
 
   test('clicking the eye icon should toggle the password visibility and change the name of the button', async () => {
-    render(<LoginForm {...props} />)
-    const user = userEvent.setup()
+    const { user, } = setup()
 
     expect(screen.getByLabelText(/^password/i).type).toBe('password')
     expect(screen.getByRole('button', { name: /show password/i })).toBeTruthy()
@@ -50,8 +54,7 @@ describe('LoginForm', () => {
   })
 
   test('clicking the checkbox should toggle the keep me signed in value', async () => {
-    render(<LoginForm {...props} />)
-    const user = userEvent.setup()
+    const { user, } = setup()
 
     expect(screen.getByRole('checkbox', { name: /keep me signed in/i })).toBeChecked()
     await user.click(screen.getByRole('checkbox', { name: /keep me signed in/i }))
@@ -59,8 +62,7 @@ describe('LoginForm', () => {
   })
 
   test('the log in button should activate when both the email/username field and the password field have content', async () => {
-    render(<LoginForm {...props} />)
-    const user = userEvent.setup()
+    const { user, } = setup()
 
     expect(screen.getByRole('button', { name: /log in$/i })).toBeDisabled()
 
