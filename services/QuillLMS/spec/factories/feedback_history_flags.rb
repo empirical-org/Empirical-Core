1257df8ecf78c87f82acaebad0aea435670c0ddd
Brendan Shean <brendanshean@gmail.com>
2023-09-12 11:10:04 -0400

    Update factory annotations

--- services/QuillLMS/spec/factories/feedback_history_flags.rb
+++ services/QuillLMS/spec/factories/feedback_history_flags.rb
@@ -1,5 +1,15 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: feedback_history_flags
+#
+#  id                  :integer          not null, primary key
+#  flag                :string           not null
+#  created_at          :datetime         not null
+#  updated_at          :datetime         not null
+#  feedback_history_id :integer          not null
+#
 FactoryBot.define do
   factory :feedback_history_flag do
     association :feedback_history, factory: :feedback_history
