6b8b97b43c8022d6ee57731875463cf2d0721b87
Brendan Shean <brendanshean@gmail.com>
2023-09-13 09:03:46 -0400

    Add configuration

---
+++ services/QuillLMS/engines/evidence/config/initializers/google/cloud/ai_platform.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require "google/cloud/ai_platform"
+
+Google::Cloud::AIPlatform.configure do |config|
+  config.credentials = JSON.parse(ENV.fetch('AI_PLATFORM_CREDENTIALS', '{}'))
+  config.endpoint = ENV.fetch('AI_PLATFORM_ENDPOINT', '')
+end
