c2778c003621585b357819361363a6983fbfc76b
Thomas Robertson <thomas.robertson@quill.org>
2023-09-15 16:58:37 -0400

    WIP Begin stubbing scoring calculations into Pre Diagnostic query

--- services/QuillLMS/app/queries/admin_diagnostic_reports/pre_diagnostic_aggregate_completed_query.rb
+++ services/QuillLMS/app/queries/admin_diagnostic_reports/pre_diagnostic_aggregate_completed_query.rb
@@ -4,7 +4,16 @@ module AdminDiagnosticReports
   class PreDiagnosticAggregateCompletedQuery < DiagnosticAggregateQuery
     def specific_select_clause
       <<-SQL
-          COUNT(DISTINCT activity_sessions.id) AS pre_students_assigned
+          COUNT(DISTINCT activity_sessions.id) AS pre_students_completed
+          AVG(activity_sessions.percentage) AS pre_average_percentage
+      SQL
+    end
+
+    def scores_subquery
+      <<-SQL
+        SELECT
+            activity_sessions.id,
+            SAFE_DIVIDE(MAX(concept_results.question_number), COUNT(concept_results.correct = true)) * 100 AS score
       SQL
     end
 
@@ -12,6 +21,8 @@ module AdminDiagnosticReports
       super + <<-SQL
         JOIN lms.activity_sessions
           ON classroom_units.id = activity_sessions.classroom_unit_id
+        JOIN special.concept_results
+          ON activity_sessions.id = concept_results.activity_session_id
         JOIN lms.activities
           ON activity_sessions.activity_id = activities.id
       SQL
