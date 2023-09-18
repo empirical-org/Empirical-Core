56a3ab5b4cde95bac9040ef7c22368c50cc3d393
Brendan Shean <brendanshean@gmail.com>
2023-09-18 11:19:12 -0400

    Fix linter error

--- services/QuillLMS/engines/evidence/spec/factories/evidence/automl_models.rb
+++ services/QuillLMS/engines/evidence/spec/factories/evidence/automl_models.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 FactoryBot.define do
-  factory :evidence_automl_model, class: Evidence::AutomlModel do
+  factory :evidence_automl_model, class: 'Evidence::AutomlModel' do
     sequence(:model_external_id) { |n| "MODEL-ID-#{n}" }
     sequence(:endpoint_external_id) { |n| "ENDPOINT-ID-#{n}" }
     name { "AutoML-Model-Name" }
