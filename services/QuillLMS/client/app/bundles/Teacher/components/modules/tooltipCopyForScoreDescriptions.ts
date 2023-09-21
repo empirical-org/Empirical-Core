e6ba9f4aa3aee9768ff767a1d04560a54575ecd7
Emilia Friedberg <emiliafriedberg@gmail.com>
2023-09-21 13:04:57 -0400

    make updates

---
+++ services/QuillLMS/client/app/bundles/Teacher/components/modules/tooltipCopyForScoreDescriptions.ts
@@ -0,0 +1,8 @@
+export default {
+  'frequently demonstrated skill': "The student reaches an optimal response by the final attempt in at least 5 of every 6 questions. This student can produce a strong response either on the first attempt or with feedback.",
+  'sometimes demonstrated skill': 'The student reaches an optimal response by the final attempt in 4 of 6 questions to 2 of 6 questions. This student could benefit from additional Quill practice to strengthen this skill.',
+  'rarely demonstrated skill': 'The student produces an optimal response in fewer than 2 out of 6 questions. This student may need direct teacher support.',
+  'completed': 'Quill Reading for Evidence, Quill Lessons, and Quill Diagnostic activities do not provide a score for students. Instead, a blue square is provided once the activity is completed.',
+  'in progress': 'The student started this activity but has not yet completed it.',
+  'not started': 'This assigned activity has not yet been started. If the activity has a lock icon, it is not yet available for the student.'
+}
