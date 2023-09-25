1eae67604321c0c930cd35c6f8509cd0a7a50ac3
Brendan Shean <brendanshean@gmail.com>
2023-09-25 13:46:10 -0400

    Remove temp file

--- services/QuillLMS/engines/evidence/app/services/evidence/blah_fetcher.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-module Evidence
-  class BlahFetcher < ApplicationService
-    def run
-      ['Name', 'model_external_id', 'model_endpoint_id']
-    end
-  end
-end
