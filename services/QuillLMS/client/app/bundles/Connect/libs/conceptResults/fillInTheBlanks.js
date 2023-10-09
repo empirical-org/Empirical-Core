57a05bf132e76e57a6e385d43b7e6c7d859a3099
Emilia Friedberg <emiliafriedberg@gmail.com>
2023-10-09 14:18:45 -0400

    update fill in the blanks concept results file to use shared function (#11109)

--- services/QuillLMS/client/app/bundles/Connect/libs/conceptResults/fillInTheBlanks.js
+++ services/QuillLMS/client/app/bundles/Connect/libs/conceptResults/fillInTheBlanks.js
@@ -1,39 +1,18 @@
+import { getConceptResultsForAttempt } from './sharedConceptResultsFunctions';
+
 import { hashToCollection, } from '../../../Shared/index';
 import { formattedCues } from '../formattedCues';
 
 export function getConceptResultsForFillInTheBlanks(question) {
-  const prompt = question.prompt.replace(/(<([^>]+)>)/ig, '').replace(/&nbsp;/ig, '');
-  let answer = question.attempts[0].submitted;
-  let conceptResults = [];
-  const responseObject = question.attempts[0].response;
-  if (responseObject) {
-    const conceptResultObject = responseObject.conceptResults || responseObject.concept_results
-    conceptResults = hashToCollection(conceptResultObject) || [];
-    answer = responseObject.text;
-  } else {
-    conceptResults = [];
-  }
-  if (conceptResults.length === 0) {
-    conceptResults = [{
-      conceptUID: question.conceptID,
-      correct: false,
-    }];
-  }
-  let directions = question.instructions || 'Fill in the blanks.';
+  const nestedConceptResults = question.attempts.map((attempt, index) => getConceptResultsForFillInTheBlanksAttempt(question, index));
+  return [].concat.apply([], nestedConceptResults); // Flatten nested Array
+}
+
+export function getConceptResultsForFillInTheBlanksAttempt(question, attemptIndex) {
+  let directions = question.instructions || 'Fill in the blanks.'
+
   if (question.cues && question.cues[0] !== '') {
     directions += ` ${formattedCues(question.cues)}`;
   }
-  return conceptResults.map((conceptResult, i) => ({
-    concept_uid: conceptResult.conceptUID,
-    question_type: 'fill-in-the-blanks',
-    metadata: {
-      correct: conceptResult.correct ? 1 : 0,
-      directions,
-      prompt,
-      answer,
-      attemptNumber: i + 1,
-      question_uid: question.key,
-      question_concept_uid: question.conceptID
-    },
-  }));
+  return getConceptResultsForAttempt(question, attemptIndex, 'fill-in-the-blanks', directions);
 }
