fc70777100b83552a3a9ba914c0b1e78f364a255
Cissy Yu <cissy@quill.org>
2023-10-13 18:31:07 +0800

    add comment on manifest typo

--- services/QuillLMS/app/assets/config/manifest.js
+++ services/QuillLMS/app/assets/config/manifest.js
@@ -1,2 +1,2 @@
-// this file is required as of sprockets 4.0. left blank because wejust use default bundle settings.
+// this file is required as of sprockets 4.0. left blank because we just use default bundle settings.
 {}
