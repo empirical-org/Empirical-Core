1d8b23045ac48b939714891efcd3dae4a566039f
Thomas Robertson <thomas.robertson@quill.org>
2023-10-13 11:47:00 -0400

    Add some type-checking to the new front-end utility function

--- services/QuillLMS/client/app/bundles/PremiumHub/shared.tsx
+++ services/QuillLMS/client/app/bundles/PremiumHub/shared.tsx
@@ -48,7 +48,7 @@ export function mapItemsIfNotAll(selectedItems, allItems, mapKey = 'id') {
 }
 
 /* Should produce the same output as backend `app/services/payload_hasher.rb:run` */
-export function hashPayload(payloadArray) {
+export function hashPayload(payloadArray: Array<any>) {
   const joinedPayload = payloadArray.join('-')
   return md5(joinedPayload)
 }
