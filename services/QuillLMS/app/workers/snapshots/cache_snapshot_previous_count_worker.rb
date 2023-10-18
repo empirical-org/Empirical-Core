8a789fdf900b756d81ac415931c58cf52e898df5
Thomas Robertson <thomas.robertson@quill.org>
2023-10-18 19:56:10 -0400

    Remove file that was removed from parent branch

--- services/QuillLMS/app/workers/snapshots/cache_snapshot_previous_count_worker.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-module Snapshots
-  class CacheSnapshotPreviousCountWorker < CacheSnapshotCountWorker
-    PUSHER_EVENT = 'admin-snapshot-previous-count-cached'
-  end
-end
