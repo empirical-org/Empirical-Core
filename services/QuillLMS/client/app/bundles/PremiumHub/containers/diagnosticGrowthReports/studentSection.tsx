602946164098c481050552b7100c7845af33f71a
Eric Adams <erictayloradams@gmail.com>
2023-10-20 15:39:45 -0400

    add student section

---
+++ services/QuillLMS/client/app/bundles/PremiumHub/containers/diagnosticGrowthReports/studentSection.tsx
@@ -0,0 +1,131 @@
+import * as React from 'react'
+import { Spinner, DataTable, noResultsMessage, DropdownInput } from '../../../Shared/index'
+import { DropdownObjectInterface } from '../../../Staff/interfaces/evidenceInterfaces'
+import { DIAGNOSTIC_REPORT_DEFAULT_CELL_WIDTH, diagnosticTypeDropdownOptions } from '../../shared'
+
+const headers = [
+  {
+    name: 'Student Name',
+    attribute: 'name',
+    width: DIAGNOSTIC_REPORT_DEFAULT_CELL_WIDTH,
+    noTooltip: true,
+    isSortable: true
+  },
+  {
+    name: '',
+    attribute: 'preToPostImprovedSkills',
+    width: DIAGNOSTIC_REPORT_DEFAULT_CELL_WIDTH,
+    primaryTitle: 'Pre to Post:',
+    secondaryTitle: 'Improved Skills',
+    tooltipName: '',
+    tooltipDescription: '',
+    noTooltip: true,
+    isSortable: false
+  },
+  {
+    name: '',
+    attribute: 'preQuestionsCorrect',
+    width: DIAGNOSTIC_REPORT_DEFAULT_CELL_WIDTH,
+    primaryTitle: 'Pre:',
+    secondaryTitle: 'Questions Correct',
+    tooltipName: '',
+    tooltipDescription: '',
+    noTooltip: true,
+    isSortable: false
+  },
+  {
+    name: '',
+    attribute: 'preSkillsProficient',
+    width: DIAGNOSTIC_REPORT_DEFAULT_CELL_WIDTH,
+    primaryTitle: 'Pre:',
+    secondaryTitle: 'Skills Proficient',
+    tooltipName: '',
+    tooltipDescription: '',
+    noTooltip: true,
+    isSortable: false
+  },
+  {
+    name: '',
+    attribute: 'totalActivitiesAndTimespent',
+    width: DIAGNOSTIC_REPORT_DEFAULT_CELL_WIDTH,
+    primaryTitle: 'Total Activities &',
+    secondaryTitle: 'Time Spent',
+    tooltipName: '',
+    tooltipDescription: '',
+    noTooltip: true,
+    isSortable: false
+  },
+  {
+    name: '',
+    attribute: 'postQuestionsCorrect',
+    width: DIAGNOSTIC_REPORT_DEFAULT_CELL_WIDTH,
+    primaryTitle: 'Post:',
+    secondaryTitle: 'Questions Correct',
+    tooltipName: '',
+    tooltipDescription: '',
+    noTooltip: true,
+    isSortable: false
+  },
+  {
+    name: '',
+    attribute: 'postSkillsImprovedOrMaintained',
+    width: DIAGNOSTIC_REPORT_DEFAULT_CELL_WIDTH,
+    primaryTitle: 'Post: Skills',
+    secondaryTitle: 'Improved Or Maintained',
+    tooltipName: '',
+    tooltipDescription: '',
+    noTooltip: true,
+    isSortable: false
+  },
+]
+
+
+export const StudentSection = ({
+  loadingFilters,
+  customStartDate,
+  customEndDate,
+  pusherChannel,
+  searchCount,
+  selectedClassrooms,
+  allClassrooms,
+  selectedGrades,
+  allGrades,
+  selectedSchools,
+  selectedTeachers,
+  allTeachers,
+  selectedTimeframe,
+  handleClickDownloadReport,
+  openMobileFilterMenu
+}) => {
+
+  const [diagnosticTypeValue, setDiagnosticTypeValue] = React.useState<DropdownObjectInterface>(diagnosticTypeDropdownOptions[0])
+
+  function handleDiagnosticTypeOptionChange(option) {
+    setDiagnosticTypeValue(option)
+  }
+
+  if (loadingFilters) {
+    return <Spinner />
+  }
+
+  return (
+    <React.Fragment>
+      <DropdownInput
+        className="diagnostic-type-dropdown"
+        handleChange={handleDiagnosticTypeOptionChange}
+        isSearchable={true}
+        label="Diagnostic:"
+        options={diagnosticTypeDropdownOptions}
+        value={diagnosticTypeValue}
+      />
+      <DataTable
+        className="growth-diagnostic-reports-by-skill-table reporting-format"
+        emptyStateMessage={noResultsMessage('diagnostic')}
+        headers={headers}
+        rows={[]}
+      />
+    </React.Fragment>
+  )
+}
+
+export default StudentSection;
