d86b6bb66825a5bc79bf7813f811f605f15fd5a6
Emilia Friedberg <emiliafriedberg@gmail.com>
2023-10-24 12:45:36 -0400

    add some more tests

--- services/QuillLMS/spec/controllers/admin_report_filter_selections_controller_spec.rb
+++ services/QuillLMS/spec/controllers/admin_report_filter_selections_controller_spec.rb
@@ -1,67 +1,71 @@
-# frozen_string_literal: true
-
 require 'rails_helper'
 
 describe AdminReportFilterSelectionsController, type: :controller do
   let(:user) { create(:user) }
+  let(:valid_attributes) { { report: AdminReportFilterSelection::REPORTS.sample, user_id: user.id } }
+  let(:invalid_attributes) { { report: 'invalid_report', user_id: user.id } }
 
   before do
     allow(controller).to receive(:current_user).and_return(user)
   end
 
   describe "POST #show" do
-    let(:admin_report_filter_selection) { create(:admin_report_filter_selection, user: user) }
+    context "with valid params" do
 
-    it "returns http success" do
-      post :show, params: { report: admin_report_filter_selection.report }
-      expect(response).to have_http_status(:success)
+      it "returns the requested admin report filter selection" do
+        admin_report_filter_selection = create(:admin_report_filter_selection, valid_attributes)
+
+        post :show, params: { admin_report_filter_selection: { report: admin_report_filter_selection.report } }
+        expect(response).to have_http_status(:ok)
+        expect(JSON.parse(response.body)['report']).to eq(admin_report_filter_selection.report)
+      end
     end
 
-    it "returns the correct admin report filter selection" do
-      post :show, params: { report: admin_report_filter_selection.report }
-      parsed_body = JSON.parse(response.body)
-      expect(parsed_body['id']).to eq(admin_report_filter_selection.id)
-    end
-
-    context "when no record is found" do
-      it "returns a null record" do
-        post :show, params: { report: 'non_existent_report' }
-        parsed_body = JSON.parse(response.body)
-        expect(parsed_body).to eq(nil)
+    context "with invalid params" do
+      it "returns null" do
+        post :show, params: { admin_report_filter_selection: invalid_attributes }
+        expect(JSON.parse(response.body)).to eq(nil)
       end
     end
   end
 
   describe "POST #create_or_update" do
     context "with valid params" do
-      let(:report) { AdminReportFilterSelection::REPORTS.sample }
-      let(:filter_selections) { 'some_selections' }
-      let(:valid_attributes) { { report: report, filter_selections: filter_selections } }
-
-      it "updates the admin report filter selection if it exists" do
-        existing_record = create(:admin_report_filter_selection, user: user, report: report)
-        post :create_or_update, params: valid_attributes
-        existing_record.reload
-        expect(existing_record.filter_selections).to eq(filter_selections)
-      end
-
-      it "creates a new admin report filter selection if it does not exist" do
+      it "creates a new AdminReportFilterSelection" do
         expect {
-          post :create_or_update, params: valid_attributes
+          post :create_or_update, params: { admin_report_filter_selection: valid_attributes }
         }.to change(AdminReportFilterSelection, :count).by(1)
       end
 
-      it "returns http success" do
-        post :create_or_update, params: valid_attributes
-        expect(response).to have_http_status(:success)
+      it "updates the filter selections" do
+        report = AdminReportFilterSelection::REPORTS.sample
+        admin_report_filter_selection = create(:admin_report_filter_selection, user: user, report: report, filter_selections: { 'timeframe' => { 'value' => 'last_week' } })
+
+        timeframe = { 'value' => 'last_month', 'name' => 'Last Month' }
+        school ={ 'id' => 1, 'name' => 'School 1' }
+
+        updated_filter_selections = {
+          'timeframe' => timeframe,
+          'schools' => [school]
+        }
+        updated_attributes = { report: report, filter_selections: updated_filter_selections }
+
+        post :create_or_update, params: { admin_report_filter_selection: updated_attributes }
+        admin_report_filter_selection.reload
+
+        expect(admin_report_filter_selection.filter_selections['timeframe']['value']).to eq(timeframe['value'])
+        expect(admin_report_filter_selection.filter_selections['schools'][0]['name']).to eq(school['name'])
+      end
+
+      it "renders a JSON response with the admin report filter selection" do
+        post :create_or_update, params: { admin_report_filter_selection: valid_attributes }
+        expect(response).to have_http_status(:ok)
       end
     end
 
     context "with invalid params" do
-      let(:invalid_attributes) { { report: nil } }
-
-      it "returns http unprocessable entity" do
-        post :create_or_update, params: invalid_attributes
+      it "renders a JSON response with errors for the new admin report filter selection" do
+        post :create_or_update, params: { admin_report_filter_selection: invalid_attributes }
         expect(response).to have_http_status(:unprocessable_entity)
       end
     end
