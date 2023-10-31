9165bc623700811febff39c1efcc8dab54c17f48
Emilia Friedberg <emiliafriedberg@gmail.com>
2023-10-31 08:26:49 -0400

    copy changes and test updates (#11185)

--- services/QuillLMS/client/app/bundles/Teacher/components/progress_reports/diagnostic_reports/diagnostics/skillGroupTooltip.tsx
+++ services/QuillLMS/client/app/bundles/Teacher/components/progress_reports/diagnostic_reports/diagnostics/skillGroupTooltip.tsx
@@ -7,7 +7,7 @@ import {
 
 const SkillGroupTooltip = ({ name, description, }) => (
   <Tooltip
-    tooltipText={`<p>${name}<br/><br/>${description}</p>`}
+    tooltipText={`<p>${name}<br/><br/>${description}<br/><br/>The average score is based on the overall questions answered correctly across the class.</p>`}
     tooltipTriggerText={<img alt={helpIcon.alt} src={helpIcon.src} />}
   />
 )
