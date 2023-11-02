be6d37a099064eb2f855a70ac2a4420fac3f68bc
brendanshean <brendanshean@gmail.com>
2023-11-02 14:38:29 -0400

    Truncate long classroom names (#11204)
    * Truncate long classroom names
    
    * Rename DuplicateNameResolver to ValidNameBuilder and rename parameter

--- services/QuillLMS/app/services/canvas_integration/classroom_creator.rb
+++ services/QuillLMS/app/services/canvas_integration/classroom_creator.rb
@@ -57,7 +57,7 @@ module CanvasIntegration
     end
 
     private def valid_name
-      ::DuplicateNameResolver.run(data[:name], other_owned_classroom_names)
+      ::ValidNameBuilder.run(data[:name], other_owned_classroom_names)
     end
   end
 end
--- services/QuillLMS/app/services/google_integration/classroom_creator.rb
+++ services/QuillLMS/app/services/google_integration/classroom_creator.rb
@@ -49,7 +49,7 @@ module GoogleIntegration
     end
 
     private def valid_name
-      ::DuplicateNameResolver.run(data[:name], other_owned_classroom_names)
+      ::ValidNameBuilder.run(data[:name], other_owned_classroom_names)
     end
   end
 end
