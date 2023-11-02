f1878059520db21c1077ca131214341065925faf
Brendan Shean <brendanshean@gmail.com>
2023-11-02 11:34:48 -0400

    Truncate long classroom names

--- services/QuillLMS/app/services/duplicate_name_resolver.rb
+++ services/QuillLMS/app/services/duplicate_name_resolver.rb
@@ -4,9 +4,12 @@ class DuplicateNameResolver < ApplicationService
   attr_reader :name, :existing_names, :max_before_randomized
 
   MAX_BEFORE_RANDOMIZED = 10
+  MAX_STRING_LENGTH = 255
+  RANDOM_STRING_LENGTH = SecureRandom.urlsafe_base64.length
+  TRUNCATE_LENGTH = MAX_STRING_LENGTH - RANDOM_STRING_LENGTH
 
-  def initialize(name, existing_names, max_before_randomized=MAX_BEFORE_RANDOMIZED)
-    @name = name
+  def initialize(name, existing_names, max_before_randomized=MAX_BEFORE_RANDOMIZED, truncate_length=TRUNCATE_LENGTH)
+    @name = name.truncate(truncate_length)
     @existing_names = existing_names
     @max_before_randomized = max_before_randomized
   end
--- services/QuillLMS/spec/services/duplicate_name_resolver_spec.rb
+++ services/QuillLMS/spec/services/duplicate_name_resolver_spec.rb
@@ -3,33 +3,47 @@
 require 'rails_helper'
 
 RSpec.describe DuplicateNameResolver do
-  let(:name) { 'name' }
-
   subject { described_class.run(name, existing_names) }
+
+  let(:name) { 'name' }
 
   context "#{name} is not in existing names" do
     let(:existing_names) { [] }
 
-    it { expect(subject).to eq name }
+    it { is_expected.to eq name }
   end
 
   context "#{name} is in existing names" do
     let(:max) { described_class::MAX_BEFORE_RANDOMIZED }
     let(:existing_names) { [name] }
 
-    it { expect(subject).to eq "#{name}_1"}
+    it { is_expected.to eq "#{name}_1" }
 
     context "duplicates up to penulimate are also in existing names" do
       let(:existing_names) { [name] + 1.upto(max - 1).map { |n| "#{name}_#{n}" } }
 
-      it { expect(subject).to eq "#{name}_#{max}" }
+      it { is_expected.to eq "#{name}_#{max}" }
     end
 
     context "duplicates up to max are also in existing names" do
       let(:existing_names) { [name] + 1.upto(max).map { |n| "#{name}_#{n}" } }
 
-      it { expect(subject).not_to eq nil }
-      it { expect(subject).not_to eq "#{name}_#{max}" }
+      it { is_expected.not_to eq nil }
+      it { is_expected.not_to eq "#{name}_#{max}" }
+    end
+  end
+
+  context "name is longer than TRUNCATE_LENGTH" do
+    let(:name) { 'a' * (described_class::TRUNCATE_LENGTH * 500)  }
+    let(:existing_names) { [] }
+    let(:truncated_name) { name.truncate(described_class::TRUNCATE_LENGTH) }
+
+    it { is_expected.to eq truncated_name }
+
+    context "truncated name is in existing names" do
+      let(:existing_names) { [truncated_name] }
+
+      it { is_expected.to eq "#{truncated_name}_1" }
     end
   end
 end
