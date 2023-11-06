fc76ebae8fb55615721eb36be2caac465309e3c6
happythenewsad <happythenewsad@users.noreply.github.com>
2023-11-06 14:23:51 -0700

    QA change requests for Admin CSV download feature (#11210)
    * add csv extension to s3 file names
    
    * increase file expiration to 1 year
    
    * enable file autodownload; pin file expiry to maximum: 7 days
    
    * add clicktracking=off param to email template <a> tag
    
    ---------
    
    Co-authored-by: Peter Kong <pkong@quill.org>

--- services/QuillLMS/app/uploaders/admin_report_csv_uploader.rb
+++ services/QuillLMS/app/uploaders/admin_report_csv_uploader.rb
@@ -3,7 +3,12 @@
 class AdminReportCsvUploader < CarrierWave::Uploader::Base
   attr_reader :admin_id
 
+  # Max value is 604800, 7 days
+  # See: https://docs.aws.amazon.com/AmazonS3/latest/API/sigv4-query-string-auth.html
+  FILE_EXPIRATION = 604800
+
   fog_directory ENV.fetch('ADMIN_REPORT_FOG_DIRECTORY', 'admin-report-fog-directory-staging')
+  fog_authenticated_url_expiration FILE_EXPIRATION
 
   FILENAME_PREFIX = 'ADMIN_REPORT_'
 
@@ -23,6 +28,6 @@ class AdminReportCsvUploader < CarrierWave::Uploader::Base
     # See: http://stackoverflow.com/questions/6920926/carrierwave-create-the-same-unique-filename-for-all-versioned-files
     random_token = Digest::SHA2.hexdigest("#{Time.current.utc}--#{admin_id}").first(6)
     date = Date.current.strftime("%m-%d-%y")
-    "#{FILENAME_PREFIX}#{admin_id}_#{date}_#{random_token}"
+    "#{FILENAME_PREFIX}#{admin_id}_#{date}_#{random_token}.csv"
   end
 end
