e64622869d98f42c61e3a339e73f76d0d8fc08fc
Brendan Shean <brendanshean@gmail.com>
2023-11-15 08:57:07 -0500

    Add uploader and refactor existing

--- services/QuillLMS/app/uploaders/admin_report_pdf_uploader.rb
+++ services/QuillLMS/app/uploaders/admin_report_pdf_uploader.rb
@@ -1,9 +1,8 @@
 # frozen_string_literal: true
 
-class AdminReportCsvUploader < ApplicationUploader
+class AdminReportPdfUploader < ApplicationUploader
   FILENAME_PREFIX = 'ADMIN_REPORT_'
 
-  # See: https://docs.aws.amazon.com/AmazonS3/latest/API/sigv4-query-string-auth.html
   fog_authenticated_url_expiration 7.days.to_i
   fog_directory ADMIN_REPORT_FOG_DIRECTORY
 
@@ -15,11 +14,11 @@ class AdminReportCsvUploader < ApplicationUploader
   end
 
   def filename
-    "ADMIN_REPORT_#{@admin_id}_#{date}_#{generate_token}.csv"
+    "ADMIN_REPORT_#{@admin_id}_#{date}_#{generate_token}.pdf"
   end
 
   def fog_attributes
-    { 'Content-Type' => 'text/csv' }
+    { 'Content-Type' => 'application/pdf' }
   end
 
   def fog_public
