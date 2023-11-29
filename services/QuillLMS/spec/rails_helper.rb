4468eb8f896007d6a79f7922e24e7c9cf9918052
Brendan Shean <brendanshean@gmail.com>
2023-11-29 08:19:00 -0500

    Thomas suggestion

--- services/QuillLMS/spec/rails_helper.rb
+++ services/QuillLMS/spec/rails_helper.rb
@@ -85,7 +85,7 @@ RSpec.configure do |config|
 
   config.filter_run_excluding benchmarking: true
   config.filter_run_excluding big_query_snapshot: true
-  config.filter_run_excluding broken: true
+  config.filter_run_excluding broken: true # TODO: remove after fixing
   config.filter_run_excluding external_api: true
 
   config.silence_filter_announcements = true
