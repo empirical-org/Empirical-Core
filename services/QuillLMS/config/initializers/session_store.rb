623d99dd227829557285ffdb9781f39de8b89dd7
Daniel Drabik <dan.drabik@gmail.com>
2023-11-30 16:37:15 -0500

    Test expire configs.

--- services/QuillLMS/config/initializers/session_store.rb
+++ services/QuillLMS/config/initializers/session_store.rb
@@ -2,13 +2,21 @@
 
 # Be sure to restart your server when you modify this file.
 
+module SessionStore
+  DEFAULT_OPTIONS = {
+    httponly: true,
+    same_site: :lax, # Rails default since 6.1, making it explicit
+    expire_after: 60.days,
+    secure: Rails.env.production? || Rails.env.staging?
+  }
+end
 
 if Rails.env.production?
-  EmpiricalGrammar::Application.config.session_store :cookie_store, key: '_quill_session', domain: '.quill.org'
+  EmpiricalGrammar::Application.config.session_store :cookie_store, key: '_quill_session', domain: '.quill.org', **SessionStore::DEFAULT_OPTIONS
 elsif Rails.env.staging?
-  EmpiricalGrammar::Application.config.session_store :cookie_store, key: '_quill_staging_session', domain: ['staging.quill.org', 'sprint.quill.org']
+  EmpiricalGrammar::Application.config.session_store :cookie_store, key: '_quill_staging_session', domain: ['staging.quill.org'], **SessionStore::DEFAULT_OPTIONS
 elsif Rails.env.cypress?
-  EmpiricalGrammar::Application.config.session_store :cookie_store, key: '_quill_session', domain: :all
+  EmpiricalGrammar::Application.config.session_store :cookie_store, key: '_quill_session', domain: :all, **SessionStore::DEFAULT_OPTIONS
 else
-  EmpiricalGrammar::Application.config.session_store :cookie_store, key: '_quill_development_session', domain: :all
+  EmpiricalGrammar::Application.config.session_store :cookie_store, key: '_quill_development_session', domain: :all, **SessionStore::DEFAULT_OPTIONS
 end
