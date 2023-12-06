505c8d318ea6715d04786e7bad78f51c15d1abc7
Brendan Shean <brendanshean@gmail.com>
2023-12-06 12:48:19 -0500

    wip

---
+++ services/QuillLMS/app/workers/blah_worker.rb
@@ -0,0 +1,44 @@
+# frozen_string_literal: true
+
+class BlahWorker
+  include Sidekiq::Worker
+
+  class CloudUploadError < StandardError; end
+
+  # PDF_TEMPLATE = 'pdfs/admin_usage_snapshot_report'
+  PDF_TEMPLATE = 'pages/pdfz'
+
+  def perform
+    raise CloudUploadError, "Unable to upload PDF for user #{user_id}" unless uploader.store!(pdf_file)
+
+    # The response-content-disposition param triggers browser file download instead of screen rendering
+    uploader.url(query: {"response-content-disposition" => "attachment;"})
+  end
+
+  private def data
+    [
+      {
+        label: 'Highlights',
+        snapshot_sections: [
+          {
+            label: 'Sentences Written',
+            count: '1,000,000',
+            change: '-100'
+          }
+        ]
+      }
+    ]
+  end
+
+  private def pdf_file
+    PdfFileBuilder.run(data, PDF_TEMPLATE)
+  end
+
+  private def uploader
+    @uploader ||= AdminUsageSnapshotReportPdfUploader.new(admin_id: user_id)
+  end
+
+  private def user_id
+    1
+  end
+end
