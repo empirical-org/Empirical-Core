47f279cfd3eebefd79550144cf0b4710ff9b437f
Emilia Friedberg <emiliafriedberg@gmail.com>
2023-12-06 11:12:36 -0500

    everything good with the file as .scss but not actually converted

--- services/QuillLMS/config/initializers/assets.rb
+++ services/QuillLMS/config/initializers/assets.rb
@@ -7,6 +7,7 @@ Rails.application.config.assets.precompile += [
   'cookie_warning.js',
   'jquery-1.8.2',
   'sign_up_email.css',
+  'admin_usage_snapshot_report_pdf.scss',
   'stats_email.scss',
   'rollup_email.scss'
 ]
