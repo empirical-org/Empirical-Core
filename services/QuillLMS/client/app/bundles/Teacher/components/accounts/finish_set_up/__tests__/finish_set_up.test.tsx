22029a02580ad2b409e529fd8f13f8d2eee89974
Peter Kong <pkong@quill.org>
2023-12-06 10:37:16 -0500

    fix mock- and snapshot- related jest failures

--- services/QuillLMS/client/app/bundles/Teacher/components/accounts/finish_set_up/__tests__/finish_set_up.test.tsx
+++ services/QuillLMS/client/app/bundles/Teacher/components/accounts/finish_set_up/__tests__/finish_set_up.test.tsx
@@ -3,12 +3,6 @@ import * as React from 'react';
 
 import FinishSetUp from '../finish_set_up';
 
-jest.mock('qs', () => ({
-  default: {
-    parse: jest.fn(() => ({}))
-  }
-}))
-
 describe('FinishSetUp component', () => {
   const props = {
     email: 'emilia@quill.org',
