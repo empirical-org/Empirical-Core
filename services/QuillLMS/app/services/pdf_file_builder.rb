58e3f6072a2377d252ce68b2c0c730dfc832288b
Brendan Shean <brendanshean@gmail.com>
2023-12-06 10:28:03 -0500

    Enable grover

--- services/QuillLMS/app/services/pdf_file_builder.rb
+++ services/QuillLMS/app/services/pdf_file_builder.rb
@@ -18,11 +18,15 @@ class PdfFileBuilder < ApplicationService
   end
 
   private def html
-    ApplicationController.renderer.render(locals: { data: data }, template: template, layout: 'pdf')
+    ApplicationController
+      .renderer
+      .render(locals: { data: data }, template: template, layout: 'pdf')
   end
 
   private def pdf
-    ''
+    Grover
+      .new(html)
+      .to_pdf
   end
 
   private def tempfile
