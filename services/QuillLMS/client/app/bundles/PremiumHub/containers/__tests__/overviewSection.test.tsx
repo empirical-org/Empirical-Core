ea492450c88d2ef22109b9c7588081ca411c0257
Eric Adams <erictayloradams@gmail.com>
2023-12-07 17:05:37 -0500

    AGDR/ Configure Overview section (#11308)
    * add logic for fetching data from all endpoints
    
    * add logic for aggregating top level data
    
    * add logic to consolidate aggregate row data
    
    * add formatting and small spec details
    
    * add tooltips; tweak styling
    
    * add page for no diagnostic data
    
    * add/update tests
    
    * fix linter errors:
    
    * small tweaks
    
    * refactors
    
    * more revisions;
    
    * remove debugger
    
    * fix buttons

--- services/QuillLMS/client/app/bundles/PremiumHub/containers/__tests__/overviewSection.test.tsx
+++ services/QuillLMS/client/app/bundles/PremiumHub/containers/__tests__/overviewSection.test.tsx
@@ -4,26 +4,29 @@ import { render, screen } from "@testing-library/react";
 import OverviewSection from "../diagnosticGrowthReports/overviewSection";
 
 const props = {
-  loadingFilters: true,
-  customStartDate: null,
-  customEndDate: null,
-  pusherChannel: null,
   searchCount: 0,
-  selectedClassrooms: [],
-  availableClassrooms: [],
   selectedGrades: [],
-  availableGrades: [],
-  selectedSchools: [],
-  selectedTeachers: [],
-  availableTeachers: [],
-  selectedTimeframe: {
-    label: "This school year",
-    name: "This school year",
-    value: "this-school-year"
-  },
-  handleClickDownloadReport: jest.fn(),
-  openMobileFilterMenu: jest.fn()
+  selectedSchoolIds: [],
+  selectedTeacherIds: [],
+  selectedClassroomIds: [],
+  selectedTimeframe: "This school year",
+  pusherChannel: null,
+  hasAdjustedFiltersFromDefault: false,
+  handleSetNoDiagnosticDataAvailable: jest.fn()
 }
+
+const mockData = [
+  {
+    id: 1663,
+    name: "Starter Baseline Diagnostic (Pre)",
+    preDiagnosticCompleted: '57 of 65 Students',
+    studentsCompletedPractice: "258 Students",
+    averageActivitiesAndTimeSpent: "17 Activities (26:35)",
+    postDiagnosticCompleted: "57 of 65 Students",
+    overallSkillGrowth: 'No Growth',
+    aggregate_rows: []
+  }
+]
 
 describe('OverviewSection', () => {
   describe('loading state', () => {
@@ -34,22 +37,23 @@ describe('OverviewSection', () => {
       expect(loadingSpinner.getAttribute('class')).toEqual('spinner')
     })
   })
-  describe('loaded state', () => {
-    test('it should render the expected header components', () => {
-      props.loadingFilters = false
-      const { asFragment } = render(<OverviewSection {...props} />);
-      expect(asFragment()).toMatchSnapshot();
-      expect(screen.getByRole('columnheader', { name: /diagnostic name/i })).toBeInTheDocument()
-      expect(screen.getByRole('columnheader', { name: /pre diagnostic completed/i })).toBeInTheDocument()
-      expect(screen.getByRole('columnheader', { name: /completed activities/i })).toBeInTheDocument()
-      expect(screen.getByRole('columnheader', { name: /average activities & time spent/i })).toBeInTheDocument()
-      expect(screen.getByRole('columnheader', { name: /post diagnostic completed/i })).toBeInTheDocument()
-      expect(screen.getByRole('columnheader', { name: /overall skill growth/i })).toBeInTheDocument()
-    })
-    test('it should render the expected empty state message', () => {
-      const { asFragment } = render(<OverviewSection {...props} />);
-      expect(asFragment()).toMatchSnapshot();
-      expect(screen.getByText(/there is no diagnostic data available for the filters selected\. try modifying or removing a filter to see results\./i)).toBeInTheDocument()
-    })
-  })
+  // TODO: add more thorough testing in a subsequent PR
+
+  // describe('loaded state', () => {
+  //   test('it should render the expected header components', () => {
+  //     const { asFragment } = render(<OverviewSection {...props} />);
+  //     expect(asFragment()).toMatchSnapshot();
+  //     expect(screen.getByRole('columnheader', { name: /diagnostic name/i })).toBeInTheDocument()
+  //     expect(screen.getByRole('columnheader', { name: /pre diagnostic completed/i })).toBeInTheDocument()
+  //     expect(screen.getByRole('columnheader', { name: /completed activities/i })).toBeInTheDocument()
+  //     expect(screen.getByRole('columnheader', { name: /average activities & time spent/i })).toBeInTheDocument()
+  //     expect(screen.getByRole('columnheader', { name: /post diagnostic completed/i })).toBeInTheDocument()
+  //     expect(screen.getByRole('columnheader', { name: /overall skill growth/i })).toBeInTheDocument()
+  //   })
+  //   test('it should render the expected empty state message', () => {
+  //     const { asFragment } = render(<OverviewSection {...props} />);
+  //     expect(asFragment()).toMatchSnapshot();
+  //     expect(screen.getByText(/there is no diagnostic data available for the filters selected\. try modifying or removing a filter to see results\./i)).toBeInTheDocument()
+  //   })
+  // })
 })
--- services/QuillLMS/client/app/bundles/PremiumHub/containers/diagnosticGrowthReports/overviewSection.tsx
+++ services/QuillLMS/client/app/bundles/PremiumHub/containers/diagnosticGrowthReports/overviewSection.tsx
@@ -1,64 +1,80 @@
 import * as React from 'react'
 import { Spinner, DataTable, noResultsMessage, DropdownInput } from '../../../Shared/index'
 import { DropdownObjectInterface } from '../../../Staff/interfaces/evidenceInterfaces'
-import { DIAGNOSTIC_REPORT_DEFAULT_CELL_WIDTH, groupByDropdownOptions } from '../../shared'
+import { groupByDropdownOptions, hashPayload } from '../../shared'
+import { requestPost, } from '../../../../modules/request';
+import { aggregateOverviewData, averageActivitiesAndTimeSpentTooltipText, completedActivitiesTooltipText, diagnosticNameTooltipText, overallSkillGrowthTooltipText, postDiagnosticCompletedTooltipText, preDiagnosticCompletedTooltipText } from './helpers';
+
+const QUERY_KEY = 'admin-diagnostic-overview'
+const PUSHER_EVENT_KEY = 'admin-diagnostic-overview-cached';
+const PRE_DIAGNOSTIC_ASSIGNED_QUERYSTRING = 'pre-diagnostic-assigned'
+const POST_DIAGNOSTIC_ASSIGNED_QUERYSTRING = 'post-diagnostic-assigned'
+const PRE_DIAGNOSTIC_COMPLETED_QUERYSTRING = 'pre-diagnostic-completed'
+const POST_DIAGNOSTIC_COMPLETED_QUERYSTRING = 'post-diagnostic-completed'
+const RECOMMENDATIONS_QUERYSTRING = 'recommendations'
+const DEFAULT_CELL_WIDTH = '176px'
+
+const DIAGNOSTIC_NAME = 'Diagnostic Name'
+const PRE_DIAGNOSTIC_COMPLETED = 'Pre Diagnostic Completed'
+const COMPLETED_ACTIVITIES = 'Completed Activities'
+const OVERALL_SKILL_GROWTH = 'Overall Skill Growth'
 
 const headers = [
   {
-    name: 'Diagnostic Name',
+    name: DIAGNOSTIC_NAME,
     attribute: 'name',
-    width: DIAGNOSTIC_REPORT_DEFAULT_CELL_WIDTH,
-    tooltipName: '',
-    tooltipDescription: '',
+    width: DEFAULT_CELL_WIDTH,
+    tooltipName: DIAGNOSTIC_NAME,
+    tooltipDescription: diagnosticNameTooltipText,
     noTooltip: true,
     isSortable: false
   },
   {
-    name: 'Pre Diagnostic Completed',
+    name: PRE_DIAGNOSTIC_COMPLETED,
     attribute: 'preDiagnosticCompleted',
-    width: DIAGNOSTIC_REPORT_DEFAULT_CELL_WIDTH,
-    tooltipName: '',
-    tooltipDescription: '',
+    width: DEFAULT_CELL_WIDTH,
+    tooltipName: PRE_DIAGNOSTIC_COMPLETED,
+    tooltipDescription: preDiagnosticCompletedTooltipText,
     noTooltip: true,
     isSortable: false
   },
   {
-    name: 'Completed Activities',
+    name: COMPLETED_ACTIVITIES,
     attribute: 'studentsCompletedPractice',
-    width: DIAGNOSTIC_REPORT_DEFAULT_CELL_WIDTH,
-    tooltipName: '',
-    tooltipDescription: '',
+    width: DEFAULT_CELL_WIDTH,
+    tooltipName: COMPLETED_ACTIVITIES,
+    tooltipDescription: completedActivitiesTooltipText,
     noTooltip: true,
     isSortable: false
   },
   {
     name: '',
     attribute: 'averageActivitiesAndTimeSpent',
-    width: DIAGNOSTIC_REPORT_DEFAULT_CELL_WIDTH,
+    width: DEFAULT_CELL_WIDTH,
     primaryTitle: 'Average Activities &',
     secondaryTitle: 'Time Spent',
-    tooltipName: '',
-    tooltipDescription: '',
+    tooltipName: 'Average Activities & Time Spent',
+    tooltipDescription: averageActivitiesAndTimeSpentTooltipText,
     noTooltip: true,
     isSortable: false
   },
   {
     name: '',
     attribute: 'postDiagnosticCompleted',
-    width: DIAGNOSTIC_REPORT_DEFAULT_CELL_WIDTH,
+    width: DEFAULT_CELL_WIDTH,
     primaryTitle: 'Post Diagnostic',
     secondaryTitle: 'Completed',
-    tooltipName: '',
-    tooltipDescription: '',
+    tooltipName: 'Post Diagnostic Completed',
+    tooltipDescription: postDiagnosticCompletedTooltipText,
     noTooltip: true,
     isSortable: false
   },
   {
-    name: 'Overall Skill Growth',
+    name: OVERALL_SKILL_GROWTH,
     attribute: 'overallSkillGrowth',
-    width: DIAGNOSTIC_REPORT_DEFAULT_CELL_WIDTH,
-    tooltipName: '',
-    tooltipDescription: '',
+    width: '106px',
+    tooltipName: OVERALL_SKILL_GROWTH,
+    tooltipDescription: overallSkillGrowthTooltipText,
     noTooltip: true,
     isSortable: false
   }
@@ -66,31 +82,182 @@ const headers = [
 
 
 export const OverviewSection = ({
-  loadingFilters,
-  customStartDate,
-  customEndDate,
-  pusherChannel,
   searchCount,
-  selectedClassrooms,
-  availableClassrooms,
   selectedGrades,
-  availableGrades,
-  selectedSchools,
-  selectedTeachers,
-  availableTeachers,
+  selectedSchoolIds,
+  selectedTeacherIds,
+  selectedClassroomIds,
   selectedTimeframe,
-  handleClickDownloadReport,
-  openMobileFilterMenu
+  pusherChannel,
+  hasAdjustedFiltersFromDefault,
+  handleSetNoDiagnosticDataAvailable
 }) => {
 
   const [groupByValue, setGroupByValue] = React.useState<DropdownObjectInterface>(groupByDropdownOptions[0])
+  const [loading, setLoading] = React.useState<boolean>(true);
+  const [preDiagnosticAssignedData, setPreDiagnosticAssignedData] = React.useState<any>(null);
+  const [postDiagnosticAssignedData, setPostDiagnosticAssignedData] = React.useState<any>(null);
+  const [preDiagnosticCompletedData, setPreDiagnosticCompletedData] = React.useState<any>(null);
+  const [postDiagnosticCompletedData, setPostDiagnosticCompletedData] = React.useState<any>(null);
+  const [recommendationsData, setRecommendationsData] = React.useState<any>(null);
+  const [aggregatedData, setAggregatedData] = React.useState<any>([]);
+  const [pusherMessage, setPusherMessage] = React.useState<string>(null)
+
+  React.useEffect(() => {
+    initializePusher()
+  }, [pusherChannel])
+
+  React.useEffect(() => {
+    getData()
+  }, [searchCount, groupByValue])
+
+  React.useEffect(() => {
+    if (!pusherMessage) return
+
+    if (filtersMatchHash(pusherMessage)) getData()
+  }, [pusherMessage])
+
+  React.useEffect(() => {
+    if (preDiagnosticAssignedData && postDiagnosticAssignedData && preDiagnosticCompletedData && postDiagnosticCompletedData && recommendationsData) {
+      aggregateOverviewData({
+        preDiagnosticAssignedData,
+        postDiagnosticAssignedData,
+        preDiagnosticCompletedData,
+        postDiagnosticCompletedData,
+        recommendationsData,
+        setAggregatedData,
+        hasAdjustedFiltersFromDefault,
+        handleSetNoDiagnosticDataAvailable,
+        setLoading
+      })
+    }
+  }, [preDiagnosticAssignedData, postDiagnosticAssignedData, preDiagnosticCompletedData, postDiagnosticCompletedData, recommendationsData])
+
+  function initializePusher() {
+    pusherChannel?.bind(PUSHER_EVENT_KEY, (body) => {
+      const { message, } = body
+
+      setPusherMessage(message)
+    });
+  };
+
+  function getSearchParams(queryString) {
+    return {
+      query: queryString,
+      timeframe: selectedTimeframe,
+      school_ids: selectedSchoolIds,
+      teacher_ids: selectedTeacherIds,
+      classroom_ids: selectedClassroomIds,
+      grades: selectedGrades,
+      group_by: groupByValue.value
+    }
+  }
+
+  function getData() {
+    setLoading(true)
+    getPreDiagnosticAssignedData()
+    getPreDiagnosticCompletedData()
+    getPostDiagnosticAssignedData()
+    getPostDiagnosticCompletedData()
+    getRecommendationsData()
+  }
+
+  function getPreDiagnosticAssignedData () {
+    const searchParams = getSearchParams(PRE_DIAGNOSTIC_ASSIGNED_QUERYSTRING)
+
+    requestPost('/admin_diagnostic_reports/report', searchParams, (body) => {
+      if (!body.hasOwnProperty('results')) {
+        return
+      } else {
+        const { results, } = body
+        setPreDiagnosticAssignedData(results)
+      }
+    })
+  }
+
+  function getPostDiagnosticAssignedData() {
+    const searchParams = getSearchParams(POST_DIAGNOSTIC_ASSIGNED_QUERYSTRING)
+
+    requestPost('/admin_diagnostic_reports/report', searchParams, (body) => {
+      if (!body.hasOwnProperty('results')) {
+        return
+      } else {
+        const { results, } = body
+        setPostDiagnosticAssignedData(results)
+      }
+    })
+  }
+
+  function getPreDiagnosticCompletedData() {
+    const searchParams = getSearchParams(PRE_DIAGNOSTIC_COMPLETED_QUERYSTRING)
+
+    requestPost('/admin_diagnostic_reports/report', searchParams, (body) => {
+      if (!body.hasOwnProperty('results')) {
+        return
+      } else {
+        const { results, } = body
+        setPreDiagnosticCompletedData(results)
+      }
+    })
+  }
+
+  function getPostDiagnosticCompletedData() {
+    const searchParams = getSearchParams(POST_DIAGNOSTIC_COMPLETED_QUERYSTRING)
+
+    requestPost('/admin_diagnostic_reports/report', searchParams, (body) => {
+      if (!body.hasOwnProperty('results')) {
+        return
+      } else {
+        const { results, } = body
+        setPostDiagnosticCompletedData(results)
+      }
+    })
+  }
+
+  function getRecommendationsData() {
+    const searchParams = getSearchParams(RECOMMENDATIONS_QUERYSTRING)
+
+    requestPost('/admin_diagnostic_reports/report', searchParams, (body) => {
+      if (!body.hasOwnProperty('results')) {
+        return
+      } else {
+        const { results, } = body
+        setRecommendationsData(results)
+      }
+    })
+  }
+
+  function filtersMatchHash(hashMessage) {
+    const filterTarget = [].concat(
+      QUERY_KEY,
+      selectedTimeframe,
+      selectedSchoolIds,
+      selectedGrades,
+      selectedTeacherIds,
+      selectedClassroomIds
+    )
+
+    const filterHash = hashPayload(filterTarget)
+
+    return hashMessage == filterHash
+  }
 
   function handleFilterOptionChange(option) {
     setGroupByValue(option)
   }
 
-  if (loadingFilters) {
-    return <Spinner />
+  function renderContent() {
+    if(loading) {
+      return <Spinner />
+    }
+    return(
+      <DataTable
+        className="growth-diagnostic-reports-overview-table reporting-format"
+        emptyStateMessage={noResultsMessage('diagnostic')}
+        headers={headers}
+        rows={aggregatedData}
+      />
+    )
   }
 
   return (
@@ -103,12 +270,7 @@ export const OverviewSection = ({
         options={groupByDropdownOptions}
         value={groupByValue}
       />
-      <DataTable
-        className="growth-diagnostic-reports-overview-table reporting-format"
-        emptyStateMessage={noResultsMessage('diagnostic')}
-        headers={headers}
-        rows={[]}
-      />
+      {renderContent()}
     </React.Fragment>
   )
 }
