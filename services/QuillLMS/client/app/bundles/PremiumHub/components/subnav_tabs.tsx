e955ae4b1ff172e148fd562fbfb09ab01b6d3b29
Emilia Friedberg <emiliafriedberg@gmail.com>
2023-12-08 14:30:57 -0500

    finish getting everything in place and testing

--- services/QuillLMS/client/app/bundles/PremiumHub/components/subnav_tabs.tsx
+++ services/QuillLMS/client/app/bundles/PremiumHub/components/subnav_tabs.tsx
@@ -1,5 +1,9 @@
 import * as React from 'react';
+import { Link } from 'react-router-dom';
+
 import { renderNavList } from '../../Shared';
+import { MOUSEDOWN, KEYDOWN, } from '../../Shared/utils/eventNames'
+import { ESCAPE, } from '../../Shared/utils/keyNames'
 
 const ACTIVITY_SCORES = 'Activity Scores';
 const CONCEPT_REPORTS = 'Concept Reports';
@@ -10,8 +14,9 @@ const STANDARDS_REPORTS = 'Standards Reports';
 const USAGE_SNAPSHOT_REPORT = 'Usage Snapshot Report'
 const DATA_EXPORT = 'Data Export'
 const DIAGNOSTIC_GROWTH_REPORT = 'Diagnostic Growth Report'
+const PREMIUM_REPORTS = 'Premium Reports'
 
-const tabsWithoutHiddenTabs = {
+const baseTabs = {
   [OVERVIEW]: {
     label: OVERVIEW,
     url: '/teachers/premium_hub'
@@ -20,6 +25,14 @@ const tabsWithoutHiddenTabs = {
     label: SCHOOL_SUBSCRIPTIONS,
     url: '/teachers/premium_hub/school_subscriptions'
   },
+  [INTEGRATIONS]: {
+    label: INTEGRATIONS,
+    url: '/teachers/premium_hub/integrations'
+  },
+}
+
+const mobileTabs = {
+  ...baseTabs,
   [ACTIVITY_SCORES]: {
     label: ACTIVITY_SCORES,
     url: '/teachers/premium_hub/district_activity_scores'
@@ -32,18 +45,45 @@ const tabsWithoutHiddenTabs = {
     label: STANDARDS_REPORTS,
     url: '/teachers/premium_hub/district_standards_reports'
   },
-  [INTEGRATIONS]: {
-    label: INTEGRATIONS,
-    url: '/teachers/premium_hub/integrations'
-  },
   [USAGE_SNAPSHOT_REPORT]: {
     label: USAGE_SNAPSHOT_REPORT,
     url: '/teachers/premium_hub/usage_snapshot_report'
   }
 }
 
+const premiumReportDropdownItems = [
+  {
+    label: USAGE_SNAPSHOT_REPORT,
+    url: '/teachers/premium_hub/usage_snapshot_report',
+    new: true
+  },
+  // TODO: uncomment the following two when the reports are ready for public access
+  // {
+  //   label: DIAGNOSTIC_GROWTH_REPORT,
+  //   url: '/teachers/premium_hub/diagnostic_growth_report',
+  //   new: true
+  // },
+  // {
+  //   label: DATA_EXPORT,
+  //   url: '/teachers/premium_hub/data_export',
+  //   new: true
+  // },
+  {
+    label: ACTIVITY_SCORES,
+    url: '/teachers/premium_hub/district_activity_scores'
+  },
+  {
+    label: CONCEPT_REPORTS,
+    url: '/teachers/premium_hub/district_concept_reports'
+  },
+  {
+    label: STANDARDS_REPORTS,
+    url: '/teachers/premium_hub/district_standards_reports'
+  },
+]
+
 const tabs = {
-  ...tabsWithoutHiddenTabs,
+  ...baseTabs,
   [DIAGNOSTIC_GROWTH_REPORT]: {
     label: DIAGNOSTIC_GROWTH_REPORT,
     url: '/teachers/premium_hub/diagnostic_growth_report'
@@ -53,6 +93,85 @@ const tabs = {
     url: '/teachers/premium_hub/data_export'
   }
 }
+
+const PremiumReportsDropdown = ({ activeTab }) => {
+  const dropdownId = "premium-reports-nav-dropdown"
+  const [isOpen, setIsOpen] = React.useState(false);
+  const dropdownRef = React.useRef(null);
+  const buttonRef = React.useRef(null);
+
+  React.useEffect(() => {
+    document.addEventListener(MOUSEDOWN, handleClickOutside);
+    document.addEventListener(KEYDOWN, handleKeyDown);
+    return () => {
+      document.removeEventListener(MOUSEDOWN, handleClickOutside);
+      document.removeEventListener(KEYDOWN, handleKeyDown);
+    };
+  }, [dropdownRef]);
+
+  const activeClass = activeTab === PREMIUM_REPORTS ? 'active' : '';
+  const openClass = isOpen ? 'open' : '';
+
+  function toggleDropdown() {
+    setIsOpen(!isOpen);
+    if (!isOpen) {
+
+      // setTimeout here is a workaround to make sure that the dropdownRef is actually available in the DOM by the time this executes (it pushes execution to the end of the event queue, after the rerender)
+      setTimeout(() => {
+        const firstItem = dropdownRef.current.querySelector('a');
+        if (firstItem) firstItem.focus();
+      }, 0);
+    }
+  }
+
+  function closeDropdown() {
+    setIsOpen(false);
+    // Return focus to the button when dropdown closes
+    buttonRef.current.focus();
+  }
+
+  function handleClickOutside(event) {
+    if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+      closeDropdown();
+    }
+  }
+
+  function handleKeyDown(event) {
+    if (event.key === ESCAPE) {
+      closeDropdown();
+    }
+  }
+
+  function renderDropdown() {
+    return (
+      <div className={dropdownId} id={dropdownId} ref={dropdownRef} role="menu">
+        {premiumReportDropdownItems.map((item) => (
+          <Link className="focus-on-light" key={item.label} onClick={closeDropdown} role="menuitem" to={item.url}>
+            {item.label}
+            {item.new ? <span className="new-tag">NEW</span> : null}
+          </Link>
+        ))}
+      </div>
+    );
+  }
+
+  return (
+    <li className="premium-reports-tab">
+      <button
+        aria-controls={dropdownId}
+        aria-expanded={isOpen}
+        aria-haspopup="true"
+        className={`premium-reports-button focus-on-dark ${activeClass} ${openClass}`}
+        onClick={toggleDropdown}
+        ref={buttonRef}
+        type="button"
+      >
+        <span>Premium Reports</span>
+      </button>
+      {isOpen ? renderDropdown() : null}
+    </li>
+  );
+};
 
 export const AdminSubnav = ({ path }) => {
 
@@ -67,16 +186,13 @@ export const AdminSubnav = ({ path }) => {
 
   function determineActiveTab() {
     const { pathname } = path;
-    if (pathname.includes('/district_activity_scores')) {
-      setActiveTab(ACTIVITY_SCORES)
-    } else if (pathname.includes('/district_concept_reports')) {
-      setActiveTab(CONCEPT_REPORTS)
-    } else if (pathname.includes('/district_standards_reports')) {
-      setActiveTab(STANDARDS_REPORTS)
+
+    const reportPaths = ['/district_activity_scores', '/district_concept_reports', '/district_standards_reports', '/usage_snapshot_report']
+
+    if (reportPaths.find(path => pathname.includes(path))) {
+      setActiveTab(PREMIUM_REPORTS)
     } else if (pathname.includes('/school_subscriptions')) {
       setActiveTab(SCHOOL_SUBSCRIPTIONS)
-    } else if (pathname.includes('/usage_snapshot_report')) {
-      setActiveTab(USAGE_SNAPSHOT_REPORT)
     } else if (pathname.includes('/diagnostic_growth_report')) {
       setActiveTab(DIAGNOSTIC_GROWTH_REPORT)
     } else if (pathname.includes('/data_export')) {
@@ -98,7 +214,7 @@ export const AdminSubnav = ({ path }) => {
 
   const dropdownClass = dropdownOpen ? 'open' : '';
 
-  const tabsToShow = window.location.href.includes('data_export') || window.location.href.includes('diagnostic_growth_report') ? tabs : tabsWithoutHiddenTabs
+  const tabsToShow = window.location.href.includes('data_export') || window.location.href.includes('diagnostic_growth_report') ? tabs : baseTabs
 
   return(
     <React.Fragment>
@@ -109,13 +225,13 @@ export const AdminSubnav = ({ path }) => {
               <p>{activeTab}</p>
               <i className="fa fa-thin fa-angle-down" />
             </button>
-            {renderNavList({ tabs: tabsToShow, handleLinkClick: handleLinkClick, activeTab, listClass: 'dropdown-menu' })}
+            {renderNavList({ tabs: mobileTabs, handleLinkClick: handleLinkClick, activeTab, listClass: 'dropdown-menu' })}
           </div>
         </div>
       </div >
       <div className="tab-subnavigation-wrapper desktop class-subnav premium-hub-subnav">
         <div className="container">
-          {renderNavList({ tabs: tabsToShow, handleLinkClick: handleLinkClick, activeTab })}
+          {renderNavList({ tabs: tabsToShow, handleLinkClick: handleLinkClick, activeTab, childElement: <PremiumReportsDropdown activeTab={activeTab} /> })}
         </div>
       </div>
     </React.Fragment>
