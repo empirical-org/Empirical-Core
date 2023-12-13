7423b42050bd40cf222a82f9bebd9380f0026d44
Brendan Shean <brendanshean@gmail.com>
2023-12-13 09:32:56 -0500

    wip

--- services/QuillLMS/client/app/bundles/PremiumHub/components/usage_snapshots/reportSubscriptionModal.tsx
+++ services/QuillLMS/client/app/bundles/PremiumHub/components/usage_snapshots/reportSubscriptionModal.tsx
@@ -5,50 +5,55 @@ const WEEKLY = 'Weekly';
 const ON = 'On';
 const OFF = 'Off';
 
-
-const ReportSubscriptionModal = ({ cancel, hasSubscription, existingFrequency, isOpen, save }) => {
+const ReportSubscriptionModal = ({ cancel, existingFrequency, isOpen, save }) => {
   if (!isOpen) return null;
 
-  const [status, setStatus] = React.useState(hasSubscription ? ON : OFF);
-  const [showFrequency, setShowFrequency] = React.useState(hasSubscription);
+  const [isSubscribed, setIsSubscribed] = React.useState(existingFrequency ? true : false);
+  const [showFrequency, setShowFrequency] = React.useState(existingFrequency ? true : false);
   const [frequency, setFrequency] = React.useState(existingFrequency || MONTHLY);
 
-  function handleStatusOptionChange(e) {
-    setStatus(e.target.value)
+  function handleIsSubscribedOptionChange(e) {
+    setIsSubscribed(e.target.value === ON)
+    setShowFrequency(e.target.value === ON)
   }
 
-  function statusRadioOptions() {
+  function isSubscribedAndFrequency() {
     return (
-      <div className="radio-options">
-        <div className="radio">
-          <label id='on-radio-button'>
-            <input
-              aria-labelledby='blah-on'
-              checked={status === ON}
-              onChange={handleStatusOptionChange}
-              type="radio"
-              value={ON}
-            />
-            On
-          </label>
-        </div>
-        <div className="radio">
-          <label id='off-radio-button' />
-          <input
-            aria-labelledby='blah-of'
-            checked={status === OFF}
-            onChange={handleStatusOptionChange}
-            type="radio"
-            value={OFF}
-          />
-          Off
-        </div>
+      <div className="is-subscribed-and-frequency">
+        <h3 className="is-subscribed-label">Email me this report</h3>
+        <div className="is-subscribed-options">
+          <div className="radio">
+            <label id='is-subscribed-on-button'>
+              <input
+                aria-labelledby='is-subscribed-on-button'
+                checked={isSubscribed}
+                onChange={handleIsSubscribedOptionChange}
+                type="radio"
+                value={ON}
+              />
+              On
+            </label>
+          </div>
+          <div className="radio">
+            <label id='is-subscribed-off-button'>
+              <input
+                aria-labelledby='is-subscribed-off-button'
+                checked={!isSubscribed}
+                onChange={handleIsSubscribedOptionChange}
+                type="radio"
+                value={OFF}
+              />
+              Off
+            </label>
+          </div>
+        </div >
       </div>
     )
   }
 
   return (
-    <div className="manage-report-subscription-modal">
+    // <div className="manage-report-subscription-modal">
+    <div>
       <div className="title-and-description">
         <h2>Subscribe to this report</h2>
         <p className="description">
@@ -57,10 +62,7 @@ const ReportSubscriptionModal = ({ cancel, hasSubscription, existingFrequency, i
           filters later by turning off the report and re-subscribing.
         </p>
       </div>
-      <div className="status-and-frequency">
-        <h3 className="label">Email me this report</h3>
-        {statusRadioOptions()}
-      </div>
+      {isSubscribedAndFrequency()}
     </div>
   );
 }
