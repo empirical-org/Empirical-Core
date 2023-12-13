c2eb520c5fb394a3209b93419c7ec7a81b9263c1
Brendan Shean <brendanshean@gmail.com>
2023-12-13 11:10:12 -0500

    Add dropdown and radio buttons side by side

--- services/QuillLMS/client/app/bundles/PremiumHub/components/usage_snapshots/reportSubscriptionModal.tsx
+++ services/QuillLMS/client/app/bundles/PremiumHub/components/usage_snapshots/reportSubscriptionModal.tsx
@@ -1,5 +1,7 @@
 import * as React from 'react'
 
+import { DropdownInput } from '../../../Shared/index';
+
 const MONTHLY = 'Monthly';
 const WEEKLY = 'Weekly';
 const ON = 'On';
@@ -9,51 +11,77 @@ const ReportSubscriptionModal = ({ cancel, existingFrequency, isOpen, save }) =>
   if (!isOpen) return null;
 
   const [isSubscribed, setIsSubscribed] = React.useState(existingFrequency ? true : false);
-  const [showFrequency, setShowFrequency] = React.useState(existingFrequency ? true : false);
   const [frequency, setFrequency] = React.useState(existingFrequency || MONTHLY);
 
-  function handleIsSubscribedOptionChange(e) {
-    setIsSubscribed(e.target.value === ON)
-    setShowFrequency(e.target.value === ON)
+  const frequencyOptions = [{ "value": MONTHLY, "label": MONTHLY }, { "value": WEEKLY, "label": WEEKLY }]
+
+  const handleIsSubscribedOptionChange = (option: { target: { value: string } }) => {
+    setIsSubscribed(option.target.value === ON)
   }
 
-  function isSubscribedAndFrequency() {
+  const handleFrequencyOptionChange = (e: { value: string }) => {
+    setFrequency(e.value)
+  }
+
+  function showFrequencyOptions() {
+    const selectedFrequency = frequencyOptions.find(option => frequency === option.value)
+
     return (
-      <div className="is-subscribed-and-frequency">
-        <h3 className="is-subscribed-label">Email me this report</h3>
-        <div className="is-subscribed-options">
-          <div className="radio">
-            <label id='is-subscribed-on-button'>
-              <input
-                aria-labelledby='is-subscribed-on-button'
-                checked={isSubscribed}
-                onChange={handleIsSubscribedOptionChange}
-                type="radio"
-                value={ON}
-              />
-              On
-            </label>
-          </div>
-          <div className="radio">
-            <label id='is-subscribed-off-button'>
-              <input
-                aria-labelledby='is-subscribed-off-button'
-                checked={!isSubscribed}
-                onChange={handleIsSubscribedOptionChange}
-                type="radio"
-                value={OFF}
-              />
-              Off
-            </label>
-          </div>
-        </div >
+      <div className="frequency-options">
+        <DropdownInput
+          handleChange={handleFrequencyOptionChange}
+          isSearchable={false}
+          label="Frequency"
+          options={frequencyOptions}
+          value={selectedFrequency}
+        />
       </div>
     )
   }
 
-  return (
-    // <div className="manage-report-subscription-modal">
-    <div>
+  function showIsSubscribedOptions() {
+    return (
+      <div className="is-subscribed-options">
+        <h3 className="is-subscribed-label">Email me this report</h3>
+        <div className="radio">
+          <label id='is-subscribed-on-button'>
+            <input
+              aria-labelledby='is-subscribed-on-button'
+              checked={isSubscribed}
+              onChange={handleIsSubscribedOptionChange}
+              type="radio"
+              value={ON}
+            />
+            On
+          </label>
+        </div>
+        <div className="radio">
+          <label id='is-subscribed-off-button'>
+            <input
+              aria-labelledby='is-subscribed-off-button'
+              checked={!isSubscribed}
+              onChange={handleIsSubscribedOptionChange}
+              type="radio"
+              value={OFF}
+            />
+            Off
+          </label>
+        </div>
+      </div >
+    )
+  }
+
+  function showIsSubscribedAndFrequency() {
+    return (
+      <div className="is-subscribed-and-frequency">
+        {showIsSubscribedOptions()}
+        {isSubscribed && showFrequencyOptions()}
+      </div>
+    )
+  }
+
+  function showTitleAndDescription() {
+    return (
       <div className="title-and-description">
         <h2>Subscribe to this report</h2>
         <p className="description">
@@ -62,7 +90,37 @@ const ReportSubscriptionModal = ({ cancel, existingFrequency, isOpen, save }) =>
           filters later by turning off the report and re-subscribing.
         </p>
       </div>
-      {isSubscribedAndFrequency()}
+    )
+  }
+
+  function showSaveAndCancelButtons() {
+    return (
+      <div className="save-and-cancel-buttons">
+        <button
+          className="quill-button medium secondary outlined focus-on-light"
+          onClick={cancel}
+          type="button"
+        >
+          Cancel
+        </button>
+        <button
+          className="quill-button medium primary contained focus-on-light"
+          onClick={save}
+          type="button"
+        >
+          Save
+        </button>
+      </div>
+    )
+  }
+
+  return (
+    <div className="manage-report-subscription-modal">
+      <div>
+        {showTitleAndDescription()}
+        {showIsSubscribedAndFrequency()}
+        {showSaveAndCancelButtons()}
+      </div>
     </div>
   );
 }
--- services/QuillLMS/client/app/bundles/PremiumHub/containers/UsageSnapshotsContainer.tsx
+++ services/QuillLMS/client/app/bundles/PremiumHub/containers/UsageSnapshotsContainer.tsx
@@ -9,16 +9,16 @@ import ReportSubscriptionModal from '../components/usage_snapshots/reportSubscri
 
 const MAX_VIEW_WIDTH_FOR_MOBILE = 950
 
-// const Tab = ({ section, setSelectedTab, selectedTab }) => {
-//   function handleSetSelectedTab() { setSelectedTab(section) }
+const Tab = ({ section, setSelectedTab, selectedTab }) => {
+  function handleSetSelectedTab() { setSelectedTab(section) }
 
-//   let className = 'tab'
-//   if (section === selectedTab) {
-//     className += ' selected-tab'
-//   }
+  let className = 'tab'
+  if (section === selectedTab) {
+    className += ' selected-tab'
+  }
 
-//   return <button className={className} onClick={handleSetSelectedTab} type="button"><img alt="" src={SECTION_NAME_TO_ICON_URL[section]} /><span>{section}</span></button>
-// }
+  return <button className={className} onClick={handleSetSelectedTab} type="button"><img alt="" src={SECTION_NAME_TO_ICON_URL[section]} /><span>{section}</span></button>
+}
 
 export const UsageSnapshotsContainer = ({
   accessType,
@@ -58,102 +58,102 @@ export const UsageSnapshotsContainer = ({
     setIsManageSubscriptionReportModalOpen(false);
   }
 
-  // if (loadingFilters) {
-  //   return <Spinner />
-  // }
+  if (loadingFilters) {
+    return <Spinner />
+  }
 
-  // const tabs = TAB_NAMES.map(s => (
-  //   <Tab
-  //     key={s}
-  //     section={s}
-  //     selectedTab={selectedTab}
-  //     setSelectedTab={setSelectedTab}
-  //   />
-  // ))
+  const tabs = TAB_NAMES.map(s => (
+    <Tab
+      key={s}
+      section={s}
+      selectedTab={selectedTab}
+      setSelectedTab={setSelectedTab}
+    />
+  ))
 
-  // const tabDropdownOptions = TAB_NAMES.map(tab => ({ value: tab, label: tab, }))
+  const tabDropdownOptions = TAB_NAMES.map(tab => ({ value: tab, label: tab, }))
 
-  // const tabDropdown = (
-  //   <DropdownInput
-  //     handleChange={handleSetSelectedTabFromDropdown}
-  //     id="tab-dropdown"
-  //     isSearchable={false}
-  //     label=""
-  //     options={tabDropdownOptions}
-  //     value={tabDropdownOptions.find(opt => opt.value === selectedTab)}
-  //   />
-  // )
-
-  // const sectionsToShow = selectedTab === ALL ? snapshotSections : snapshotSections.filter(s => s.name === selectedTab)
-  // const snapshotSectionComponents = sectionsToShow.map(section => (
-  //   <SnapshotSection
-  //     active={section.name === selectedTab}
-  //     className={section.className}
-  //     customTimeframeEnd={customEndDate?.toDate()}
-  //     customTimeframeStart={customStartDate?.toDate()}
-  //     itemGroupings={section.itemGroupings}
-  //     key={section.name}
-  //     name={section.name}
-  //     pusherChannel={pusherChannel}
-  //     searchCount={searchCount}
-  //     selectedClassroomIds={mapItemsIfNotAll(selectedClassrooms, availableClassrooms)}
-  //     selectedGrades={mapItemsIfNotAll(selectedGrades, availableGrades, 'value')}
-  //     selectedSchoolIds={selectedSchools.map(s => s.id)}
-  //     selectedTeacherIds={mapItemsIfNotAll(selectedTeachers, availableTeachers)}
-  //     selectedTimeframe={selectedTimeframe.value}
-  //   />
-  // ))
-
-  // if (accessType !== FULL) {
-  //   return restrictedPage
-  // }
-
-  return (
-    <ReportSubscriptionModal
-      cancel={handleManageReportSubscriptionCancel}
-      existingFrequency={null}
-      isOpen={isManageReportSubscriptionModalOpen}
-      save={handleManageReportSubscriptionSave}
+  const tabDropdown = (
+    <DropdownInput
+      handleChange={handleSetSelectedTabFromDropdown}
+      id="tab-dropdown"
+      isSearchable={false}
+      label=""
+      options={tabDropdownOptions}
+      value={tabDropdownOptions.find(opt => opt.value === selectedTab)}
     />
   )
 
-  // <main>
-  //   <div className="header">
-  //     <h1>
-  //       <span>Usage Snapshot Report</span>
-  //       <a href="https://support.quill.org/en/articles/8358350-how-do-i-use-the-usage-snapshot-report" rel="noopener noreferrer" target="_blank">
-  //         <img alt={documentFileIcon.alt} src={documentFileIcon.src} />
-  //         <span>Guide</span>
-  //       </a>
-  //     </h1>
-  //     <button
-  //       className="quill-button manage-subscription-button contained primary medium focus-on-light"
-  //       onClick={handleClickManageReportSubscription}
-  //       type="button"
-  //     >
-  //       <img alt={whiteEmailIcon.alt} src={whiteEmailIcon.src} />
-  //       <span>Subscribe</span>
-  //     </button>
-  //     <button className="quill-button download-report-button contained primary medium focus-on-light" onClick={handleClickDownloadReport} type="button">
-  //       <img alt={whiteArrowPointingDownIcon.alt} src={whiteArrowPointingDownIcon.src} />
-  //       <span>Download</span>
-  //     </button>
-  //   </div>
-  //   <div aria-hidden={true} className="tabs">
-  //     {size.width >= MAX_VIEW_WIDTH_FOR_MOBILE ? tabs : tabDropdown}
-  //   </div>
-  //   <div className="filter-button-container">
-  //     <button className="interactive-wrapper focus-on-light" onClick={openMobileFilterMenu} type="button">
-  //       <img alt={filterIcon.alt} src={filterIcon.src} />
-  //       Filters
-  //     </button>
-  //   </div>
-  //   <div className="sections">
-  //     {snapshotSectionComponents}
-  //   </div>
-  //   <div id="bottom-element" />
+  const sectionsToShow = selectedTab === ALL ? snapshotSections : snapshotSections.filter(s => s.name === selectedTab)
+  const snapshotSectionComponents = sectionsToShow.map(section => (
+    <SnapshotSection
+      active={section.name === selectedTab}
+      className={section.className}
+      customTimeframeEnd={customEndDate?.toDate()}
+      customTimeframeStart={customStartDate?.toDate()}
+      itemGroupings={section.itemGroupings}
+      key={section.name}
+      name={section.name}
+      pusherChannel={pusherChannel}
+      searchCount={searchCount}
+      selectedClassroomIds={mapItemsIfNotAll(selectedClassrooms, availableClassrooms)}
+      selectedGrades={mapItemsIfNotAll(selectedGrades, availableGrades, 'value')}
+      selectedSchoolIds={selectedSchools.map(s => s.id)}
+      selectedTeacherIds={mapItemsIfNotAll(selectedTeachers, availableTeachers)}
+      selectedTimeframe={selectedTimeframe.value}
+    />
+  ))
 
-  //  </main>
+  if (accessType !== FULL) {
+    return restrictedPage
+  }
+
+  return (
+
+    <main>
+      <div className="header">
+        <h1>
+          <span>Usage Snapshot Report</span>
+          <a href="https://support.quill.org/en/articles/8358350-how-do-i-use-the-usage-snapshot-report" rel="noopener noreferrer" target="_blank">
+            <img alt={documentFileIcon.alt} src={documentFileIcon.src} />
+            <span>Guide</span>
+          </a>
+        </h1>
+        <button
+          className="quill-button manage-subscription-button contained primary medium focus-on-light"
+          onClick={handleClickManageReportSubscription}
+          type="button"
+        >
+          <img alt={whiteEmailIcon.alt} src={whiteEmailIcon.src} />
+          <span>Subscribe</span>
+        </button>
+        <button className="quill-button download-report-button contained primary medium focus-on-light" onClick={handleClickDownloadReport} type="button">
+          <img alt={whiteArrowPointingDownIcon.alt} src={whiteArrowPointingDownIcon.src} />
+          <span>Download</span>
+        </button>
+      </div>
+      <div aria-hidden={true} className="tabs">
+        {size.width >= MAX_VIEW_WIDTH_FOR_MOBILE ? tabs : tabDropdown}
+      </div>
+      <div className="filter-button-container">
+        <button className="interactive-wrapper focus-on-light" onClick={openMobileFilterMenu} type="button">
+          <img alt={filterIcon.alt} src={filterIcon.src} />
+          Filters
+        </button>
+      </div>
+      <div className="sections">
+        {snapshotSectionComponents}
+      </div>
+      <div id="bottom-element" />
+
+      <ReportSubscriptionModal
+        cancel={handleManageReportSubscriptionCancel}
+        existingFrequency={null}
+        isOpen={isManageReportSubscriptionModalOpen}
+        save={handleManageReportSubscriptionSave}
+      />
+    </main>
+  )
 }
 
 export default UsageSnapshotsContainer;
