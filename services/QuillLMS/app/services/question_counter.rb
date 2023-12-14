e14af358282b3dad31bd721b4f5d487780e09b15
Daniel Drabik <dan.drabik@gmail.com>
2023-12-14 14:15:06 -0500

    Tweaks based on testing.

--- services/QuillLMS/app/services/question_counter.rb
+++ services/QuillLMS/app/services/question_counter.rb
@@ -3,24 +3,24 @@
 class QuestionCounter < ApplicationService
   attr_reader :activity, :tool_key
 
-  PROOFREADER_REGEX = ::Regexp.new(/\{\+/)
+  PROOFREADER_REGEX = ::Regexp.new(/\{(\+|\-)/)
   LESSONS_COUNTS = Configs[:lessons_question_counts]
 
   TOOL_CALCULATORS = {
     passage:    -> (a) { a.data.dig('passage')&.scan(PROOFREADER_REGEX)&.size},
-    sentence:   -> (a) { a.data.dig('questions')&.size },
+    sentence:   -> (a) { grammar_calculator(a) },
     connect:    -> (a) { a.data.dig('questions')&.size },
     diagnostic: -> (a) { a.data.dig('questions')&.size },
-    evidence:   -> (_) { 3 },
-    lessons:    -> (a) { LESSONS_COUNTS.dig(a.uid) }
+    lessons:    -> (a) { LESSONS_COUNTS.dig(a.uid) },
+    evidence:   -> (_) { 3 }
   }
   TOOL_DEFAULTS = {
     passage: 10,
     sentence: 10,
     connect:  10,
     diagnostic: 25,
-    evidence: 3,
-    lessons:  5
+    lessons:  5,
+    evidence: 3
   }
   DEFAULT = 10
 
@@ -33,5 +33,14 @@ class QuestionCounter < ApplicationService
     TOOL_CALCULATORS[tool_key]&.call(activity) ||
       TOOL_DEFAULTS[tool_key] ||
       DEFAULT
+  end
+
+  def self.grammar_calculator(activity)
+    array_count = activity.data.dig('questions')&.size
+    if array_count.present? && array_count != 0
+      return array_count
+    end
+
+    activity.data.dig('concepts')&.values&.map(&:values)&.flatten&.sum
   end
 end
--- services/QuillLMS/spec/services/question_counter_spec.rb
+++ services/QuillLMS/spec/services/question_counter_spec.rb
@@ -6,6 +6,7 @@ describe QuestionCounter do
   subject { described_class.run(activity) }
 
   let(:question_list) { [{ key: 'fake_key' }, { key: 'fake_key' }] }
+  let(:concept_list) { {'1232' => {"quantity" => 2}, '1235' => {"quantity" => 6}} }
   let(:passage) { "Yuri Gagarin is {+famous-famous,|nb0JW1r5pRB5ouwAzTgMbQ} because he was the first person to travel into outer space and orbit the Earth. He was born on {+March-march|E635Hrr0tuMsBDm7lLfrPg} 9, 1934, in the Soviet Union.<br/><br/><br/>Gagarin first learned to fly a plane when he was young man studying in Saratov. After finishing {+school,-school|m8sKnkzLg1mIAkkXeqHOWw}"}
   let(:default_size) {99}
 
@@ -19,7 +20,7 @@ describe QuestionCounter do
   end
 
   context 'tool default' do
-    let(:activity) { create(:connect_activity, data: {}) }
+    let(:activity) { build(:connect_activity, data: {}) }
 
     it { expect(subject).to eq described_class::TOOL_DEFAULTS[:connect] }
   end
@@ -29,51 +30,57 @@ describe QuestionCounter do
       stub_const("QuestionCounter::DEFAULT", default_size)
     end
 
-    let(:classification) {create(:classification, key: 'some-unknown')}
-    let(:activity) { create(:activity, classification: classification) }
+    let(:classification) {build(:classification, key: 'some-unknown')}
+    let(:activity) { build(:activity, classification: classification) }
 
     it { expect(subject).to eq default_size }
   end
 
   context 'evidence' do
-    let(:activity) { create(:evidence_lms_activity) }
+    let(:activity) { build(:evidence_lms_activity) }
 
     it { expect(subject).to eq 3 }
   end
 
   context 'connect' do
-    let(:activity) { create(:connect_activity, data: {questions: question_list}) }
+    let(:activity) { build(:connect_activity, data: {questions: question_list}) }
 
     it { expect(subject).to eq question_list.size }
   end
 
   context 'grammar' do
-    let(:activity) { create(:grammar_activity, data: {questions: question_list}) }
+    let(:activity) { build(:grammar_activity, data: {questions: question_list}) }
 
     it { expect(subject).to eq question_list.size }
+
+    context 'blank question list, use concept list' do
+      let(:activity) { build(:grammar_activity, data: {questions: [], concepts: concept_list}) }
+
+      it { expect(subject).to eq 8 }
+    end
   end
 
   context 'diagnostic' do
-    let(:activity) { create(:diagnostic_activity, data: {questions: question_list}) }
+    let(:activity) { build(:diagnostic_activity, data: {questions: question_list}) }
 
     it { expect(subject).to eq question_list.size }
   end
 
   context 'proofreader' do
-    let(:activity) { create(:proofreader_activity, data: {passage: passage }) }
+    let(:activity) { build(:proofreader_activity, data: {passage: passage }) }
 
     it { expect(subject).to eq 3 }
   end
 
   context 'lessons' do
     context 'value in list' do
-      let(:activity) { create(:lesson_activity, uid: '-KsJlSHlJ9G0rlVlRIfk') }
+      let(:activity) { build(:lesson_activity, uid: '-KsJlSHlJ9G0rlVlRIfk') }
 
       it { expect(subject).to eq 4 }
     end
 
     context 'value NOT in list' do
-      let(:activity) { create(:lesson_activity, uid: 'asdfsadfsdf') }
+      let(:activity) { build(:lesson_activity, uid: 'asdfsadfsdf') }
 
       it { expect(subject).to eq described_class::TOOL_DEFAULTS[:lessons] }
     end
