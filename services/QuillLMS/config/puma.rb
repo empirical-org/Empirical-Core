6365e2a23e312701afc32123024be413fd4cdf62
Emilia Friedberg <emiliafriedberg@gmail.com>
2023-12-15 10:48:23 -0500

    undo changes to database.yml and puma.rb file

--- services/QuillLMS/config/puma.rb
+++ services/QuillLMS/config/puma.rb
@@ -12,7 +12,8 @@ environment ENV['RACK_ENV'] || 'development'
 on_worker_boot do
   # worker specific setup
   ActiveSupport.on_load(:active_record) do
-    config = Rails.application.config.database_configuration[Rails.env]["primary"]
+    config = ActiveRecord::Base.configurations[Rails.env] ||
+                Rails.application.config.database_configuration[Rails.env]
     config['pool'] = ENV['MAX_THREADS'] || 16
     ActiveRecord::Base.establish_connection(config)
   end
