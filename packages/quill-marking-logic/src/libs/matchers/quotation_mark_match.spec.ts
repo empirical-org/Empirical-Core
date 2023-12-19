a4edad5d50941dc785c98edba1438d0c3dc8fa01
Cissy Yu <cissy@quill.org>
2023-12-19 21:20:17 +0800

    simplify tests

--- packages/quill-marking-logic/src/libs/matchers/quotation_mark_match.spec.ts
+++ packages/quill-marking-logic/src/libs/matchers/quotation_mark_match.spec.ts
@@ -8,72 +8,37 @@ import {conceptResultTemplate} from '../helpers/concept_result_template'
 
 describe('The quotationMarkMatch function', () => {
 
+  const savedResponses: Array<Response> = [
+    {
+      id: 1,
+      text: "My dog took a nap.",
+      feedback: "Good job, that's a sentence!",
+      optimal: true,
+      count: 2,
+      question_uid: "questionOne"
+    },
+    {
+      id: 2,
+      text: "My dog took another nap.",
+      feedback: "Good job, that's a sentence!",
+      optimal: true,
+      count: 1,
+      question_uid: "questionTwo"
+    }
+  ]
+
   it('Should take a response string and return true if response does not contain two adjacent single quotes', () => {
     const responseString = "There are adjacent single quotes in \'\' this string.";
-    const savedResponses: Array<Response> = [
-      {
-        id: 1,
-        text: "My dog took a nap.",
-        feedback: "Good job, that's a sentence!",
-        optimal: true,
-        count: 2,
-        question_uid: "questionOne"
-      },
-      {
-        id: 2,
-        text: "My dog took another nap.",
-        feedback: "Good job, that's a sentence!",
-        optimal: true,
-        count: 1,
-        question_uid: "questionTwo"
-      }
-    ]
     assert.ok(quotationMarkMatch(responseString, savedResponses));
   });
 
   it('Should take a response string and return false if response does not contain two adjacent single quotes', () => {
     const responseString = "There are only double \" \" quotes in this string.";
-    const savedResponses: Array<Response> = [
-      {
-        id: 1,
-        text: "My dog took a nap.",
-        feedback: "Good job, that's a sentence!",
-        optimal: true,
-        count: 2,
-        question_uid: "questionOne"
-      },
-      {
-        id: 2,
-        text: "My dog took another nap.",
-        feedback: "Good job, that's a sentence!",
-        optimal: true,
-        count: 1,
-        question_uid: "questionTwo"
-      }
-    ]
     assert.notOk(quotationMarkMatch(responseString, savedResponses));
   });
 
   it('Should take a response string and find top optimal response if the response string contains two adjacent single quotes', () => {
     const responseString = "There are two adjacent single quotes \'\' in this string.";
-    const savedResponses: Array<Response> = [
-      {
-        id: 1,
-        text: "My dog took a nap.",
-        feedback: "Good job, that's a sentence!",
-        optimal: true,
-        count: 2,
-        question_uid: "questionOne"
-      },
-      {
-        id: 2,
-        text: "My dog took another nap.",
-        feedback: "Good job, that's a sentence!",
-        optimal: true,
-        count: 1,
-        question_uid: "questionTwo"
-      }
-    ]
     const partialResponse: PartialResponse =  {
       feedback: feedbackStrings.quotationMarkError,
       author: 'Quotation Mark Hint',
