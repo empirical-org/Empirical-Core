1d1f5ee475ec0ae9f9d486c7aab164a0a0952dd3
Emilia Friedberg <emiliafriedberg@gmail.com>
2023-12-19 15:01:37 -0500

    add snapshot tests for overview section

--- services/QuillLMS/client/app/bundles/PremiumHub/components/overview/__tests__/integrationsSection.test.tsx
+++ services/QuillLMS/client/app/bundles/PremiumHub/components/overview/__tests__/integrationsSection.test.tsx
@@ -2,7 +2,6 @@ import * as React from "react";
 import { render, } from "@testing-library/react";
 import { MemoryRouter } from 'react-router-dom';
 
-
 import IntegrationsSection from '../integrationsSection'
 
 describe('IntegrationsSection', () => {
