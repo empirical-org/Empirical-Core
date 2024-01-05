a8ca9bd3b41d7fa94e18b6a6952773ee42d87572
Anathomical <thomas.robertson@quill.org>
2024-01-05 14:28:22 -0500

    lms/fix-intermittent-spec-failure (#11391)
    * Explicitly call the callback function in the test case
    
    This should avoid any weird timing issues with the after_save hook
    
    * Try mocking DateTime.current to push the between creation and update

--- services/QuillLMS/spec/models/classroom_unit_spec.rb
+++ services/QuillLMS/spec/models/classroom_unit_spec.rb
@@ -114,8 +114,13 @@ describe ClassroomUnit, type: :model, redis: true do
   end
 
   describe '#hide_all_activity_sessions' do
-    it { expect { classroom_unit.update(visible: false) }.to change { activity_session.reload.visible }.from(true).to(false) }
-    it { expect { classroom_unit.update(visible: false) }.to change { activity_session.reload.updated_at } }
+    before do
+      # try making sure that the gap between initialization and update is treated wide enough to register a difference in updated_at
+      allow(DateTime).to receive(:current).and_return(Time.now + 1.minute)
+    end
+
+    it { expect { classroom_unit.send(:hide_all_activity_sessions) }.to change { activity_session.reload.visible }.from(true).to(false) }
+    it { expect { classroom_unit.send(:hide_all_activity_sessions) }.to change { activity_session.reload.updated_at } }
   end
 
   describe '#check_for_assign_on_join_and_update_students_array_if_true callback' do
