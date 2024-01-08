bc30f35eb3016356e3e3437d2818961c6a68fc92
Emilia Friedberg <emiliafriedberg@gmail.com>
2024-01-08 09:46:23 -0500

    remove extra columns; make overview page actual main page; clean up

--- services/QuillLMS/app/queries/teachers_data.rb
+++ services/QuillLMS/app/queries/teachers_data.rb
@@ -1,13 +1,7 @@
 # frozen_string_literal: true
 
 module TeachersData
-  # AVERAGE_TIME_SPENT was gathered from below query on 1/4/2016
-  # https://dataclips.heroku.com/tympuxntqzshpmngbnbivaectbqm-average-time-per-activity_session?autosave=true
-  AVERAGE_TIME_SPENT = 441 # (seconds)  ie interval '7 minutes 21 seconds'
-
   # num_students
-  # num_questions_completd
-  # num_time_spent
 
   # use Outer Joins to account for the fact that some of the teachers shown in the Premium Hub dashboard
   # will have no classrooms, etc.
@@ -32,32 +26,6 @@ module TeachersData
       GROUP BY users.id"
     )
 
-    activities_count_query = User.find_by_sql(
-      "SELECT
-        users.id,
-        COUNT(DISTINCT activity_sessions.id) AS number_of_activities_completed
-      FROM users
-      INNER JOIN units ON users.id = units.user_id
-      INNER JOIN classroom_units ON units.id = classroom_units.unit_id
-      INNER JOIN activity_sessions ON classroom_units.id = activity_sessions.classroom_unit_id
-      WHERE users.id IN (#{teacher_ids_str})
-      AND activity_sessions.completed_at >= '#{last_july_first}'::date
-      GROUP BY users.id"
-    )
-
-    time_spent_query = User.find_by_sql(
-      "SELECT
-        users.id,
-        #{time_spent} AS time_spent
-      FROM users
-      INNER JOIN units ON users.id = units.user_id
-      INNER JOIN classroom_units ON units.id = classroom_units.unit_id
-      INNER JOIN activity_sessions ON classroom_units.id = activity_sessions.classroom_unit_id
-      WHERE users.id IN (#{teacher_ids_str})
-      AND activity_sessions.completed_at >= '#{last_july_first}'::date
-      GROUP BY users.id"
-    )
-
     combiner = {}
     number_of_students_query.each do |row|
       combiner[row.id] = {
@@ -67,16 +35,6 @@ module TeachersData
         last_sign_in: row.last_sign_in,
         number_of_students: row.number_of_students
       }
-    end
-
-    activities_count_query.each do |row|
-      if combiner[row.id]
-        combiner[row.id][:number_of_activities_completed] = row.number_of_activities_completed
-      end
-    end
-
-    time_spent_query.each do |row|
-      combiner[row.id][:time_spent] = row.time_spent
     end
 
     result = combiner.keys.map do |key|
@@ -93,38 +51,10 @@ module TeachersData
         hash_value[:number_of_students]
       end
 
-      user.define_singleton_method(:number_of_activities_completed) do
-        hash_value[:number_of_activities_completed]
-      end
-
-      user.define_singleton_method(:time_spent) { hash_value[:time_spent] }
-
       user.define_singleton_method(:has_valid_subscription) { User.find_by_id(key).subscription_is_valid? }
 
       user
     end
 
-  end
-
-  def self.time_spent
-    "SUM (
-      CASE
-      WHEN (activity_sessions.timespent IS NOT NULL) THEN activity_sessions.timespent
-      WHEN (activity_sessions.started_at IS NULL)
-        OR (activity_sessions.completed_at IS NULL)
-        OR (activity_sessions.completed_at - activity_sessions.started_at < interval '1 minute')
-        OR (activity_sessions.completed_at - activity_sessions.started_at > interval '30 minutes')
-      THEN #{AVERAGE_TIME_SPENT}
-      ELSE
-        EXTRACT (
-          'epoch' FROM (activity_sessions.completed_at - activity_sessions.started_at)
-        )
-      END)"
-  end
-
-  def self.last_july_first
-    today = Date.current
-    july_first_of_this_year = Date.parse("01-07-#{today.year}")
-    today.month > 7 ? july_first_of_this_year : july_first_of_this_year - 1.year
   end
 end
--- services/QuillLMS/spec/queries/teachers_data_spec.rb
+++ services/QuillLMS/spec/queries/teachers_data_spec.rb
@@ -52,31 +52,4 @@ describe 'TeachersData' do
   it 'number_of_students works' do
     expect(@results.first.number_of_students).to eq(2)
   end
-
-  it 'number_of_activities_completed works' do
-    expect(@results.first.number_of_activities_completed).to eq(2)
-  end
-
-  it 'time_spent works' do
-    expect(@results.first.time_spent).to eq(2*(time2 - time1))
-  end
-
-  context 'time spent on activities is less than 1 minute' do
-
-    let!(:time1) { time2 - 2.seconds }
-
-    it 'uses default_time_spent instead' do
-      expect(@results.first.time_spent).to eq(2*default_time_spent)
-    end
-  end
-
-  context 'time spent on activities is greater than 30 minutes' do
-
-    let!(:time1) { time2 - 2.days}
-
-    it 'uses default_time_spent instead' do
-      expect(@results.first.time_spent).to eq(2*default_time_spent)
-    end
-  end
-
 end
