422425a864f0b8d15e004415ad1614326ef9e89a
Daniel Drabik <dan.drabik@gmail.com>
2024-01-10 12:42:01 -0500

    Alter learning hours query to use force BI Engine.

--- services/QuillLMS/app/queries/snapshots/student_learning_hours_query.rb
+++ services/QuillLMS/app/queries/snapshots/student_learning_hours_query.rb
@@ -2,8 +2,11 @@
 
 module Snapshots
   class StudentLearningHoursQuery < ReportingSessionCountQuery
+
+    # BI Engine seems to have an issue using RAM for SUM()
+    # Adding * 1 causes BI Engine to use RAM for some reason
     def select_clause
-      "SELECT IFNULL(SUM(recent_reporting_sessions.time_spent), 0) / 3600.0 AS count"
+      "SELECT IFNULL(SUM(time_spent * 1), 0) / 3600.0 AS count"
     end
   end
 end
