b7dc67c518290ede0aeda13b762543dda45c3265
Brendan Shean <brendanshean@gmail.com>
2024-01-22 13:05:50 -0500

    Add specs for snapshot section builder

--- services/QuillLMS/spec/services/pdfs/admin_usage_snapshot_reports/snapshot_sections_builder_spec.rb
+++ services/QuillLMS/spec/services/pdfs/admin_usage_snapshot_reports/snapshot_sections_builder_spec.rb
@@ -3,35 +3,29 @@
 require 'rails_helper'
 
 RSpec.describe Pdfs::AdminUsageSnapshotReports::SnapshotSectionsBuilder do
+  subject { described_class.run(admin_report_filter_selection) }
+
   let(:admin_report_filter_selection) { create(:admin_report_filter_selection) }
 
-  describe '#run' do
-    let(:service) { described_class.new(admin_report_filter_selection) }
+  let(:count_data) { { count: nil } }
+  let(:ranking_data) { [{ value: nil, count: nil }] }
+  let(:feedback_data) { { type: 'feedback' } }
+  let(:processed_data) { [count_data, ranking_data, feedback_data] }
 
-    before do
-      allow(Pdfs::AdminUsageSnapshotReports::CountDataInjector).to receive(:run).and_return({ injected: 'count_data' })
-      allow(Pdfs::AdminUsageSnapshotReports::RankingDataInjector).to receive(:run).and_return({ injected: 'ranking_data' })
-      # You may need to setup additional mocks or stubs depending on the implementation details of your injectors
-    end
+  before do
+    allow(Pdfs::AdminUsageSnapshotReports::CountDataInjector).to receive(:run).and_return(count_data)
+    allow(Pdfs::AdminUsageSnapshotReports::RankingDataInjector).to receive(:run).and_return(ranking_data)
+  end
 
-    it 'processes each section and their respective items' do
-      result = service.run
+  it { is_expected.to be_an(Array) }
+  it { is_expected.not_to be_empty }
+  it { expect(subject.first).to include(:itemGroupings) }
 
-      expect(result).not_to be_empty
-      expect(result.first[:itemGroupings]).not_to be_empty
-
-      # More detailed tests here, e.g., checking if COUNT items are processed by CountDataInjector
-      result.each do |section|
-        section[:itemGroupings].each do |grouping|
-          grouping[:items].each do |item|
-            if item[:type] == 'count'
-              expect(item).to include(:injected => 'count_data')
-            elsif item[:type] == 'ranking'
-              expect(item).to include(:injected => 'ranking_data')
-            else
-              # Assert for other types or default case
-            end
-          end
+  it 'processes each section and their respective items' do
+    subject.each do |section|
+      section[:itemGroupings].each do |grouping|
+        grouping[:items].each do |item|
+          expect(processed_data).to include(item)
         end
       end
     end
