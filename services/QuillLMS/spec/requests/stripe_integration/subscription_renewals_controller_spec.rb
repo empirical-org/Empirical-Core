5281c18fae5b18b9a6118ff9449a6cb8bc6eb5b9
Brendan Shean <brendanshean@gmail.com>
2024-01-25 14:12:31 -0500

    Some refactoring

--- services/QuillLMS/spec/requests/stripe_integration/subscription_renewals_controller_spec.rb
+++ services/QuillLMS/spec/requests/stripe_integration/subscription_renewals_controller_spec.rb
@@ -5,20 +5,29 @@ require 'rails_helper'
 RSpec.describe StripeIntegration::SubscriptionRenewalsController, type: :request do
   include_context 'Stripe Subscription'
 
-  let(:stripe_subscription_id) { "sub_#{SecureRandom.hex}" }
-  let(:cancel_at_period_end) { true }
-  let(:options) { { cancel_at_period_end: cancel_at_period_end } }
-  let(:update_stripe_subscription) { allow(Stripe::Subscription).to receive(:update).with(stripe_subscription_id, **options) }
-  let(:url) { '/stripe_integration/subscription_renewals' }
-  let(:params) { { stripe_subscription_id: stripe_subscription_id, cancel_at_period_end: cancel_at_period_end } }
+  subject { post url, params: { stripe_subscription_id:, cancel_at_period_end: }, as: :json }
 
-  before { allow(Stripe::Subscription).to receive(:retrieve).with(stripe_subscription_id).and_return(stripe_subscription) }
+  let(:cancel_at_period_end) { true }
+  let(:stripe_subscription_id) { "sub_#{SecureRandom.hex}" }
+  let(:url) { '/stripe_integration/subscription_renewals' }
+
+  before do
+    allow(Stripe::Subscription)
+      .to receive(:retrieve)
+      .with(stripe_subscription_id)
+      .and_return(stripe_subscription)
+  end
 
   context 'Stripe Subscription exists' do
-    before { update_stripe_subscription.and_return({}) }
+    before do
+      allow(Stripe::Subscription)
+        .to receive(:update)
+        .with(stripe_subscription_id, { cancel_at_period_end: })
+        .and_return({})
+    end
 
     it 'returns 200 if the request to Stripe is successful' do
-      post url, params: params, as: :json
+      subject
 
       expect(response.media_type).to eq 'application/json'
       expect(response).to have_http_status :ok
@@ -27,13 +36,12 @@ RSpec.describe StripeIntegration::SubscriptionRenewalsController, type: :request
 
   context 'Stripe Subscription exists but was canceled' do
     let(:stripe_subscription_status) { StripeIntegration::Subscription::CANCELED }
+    let(:error_class) { described_class::UpdatingCanceledSubscriptionError }
 
     it 'returns 500 since updates are not allowed on canceled subscriptions' do
-      expect(ErrorNotifier)
-        .to receive(:report)
-        .with(described_class::UpdatingCanceledSubscriptionError, stripe_subscription_id: stripe_subscription_id)
+      expect(ErrorNotifier).to receive(:report).with(error_class, stripe_subscription_id:)
 
-      post url, params: params, as: :json
+      subject
 
       expect(response.media_type).to eq 'application/json'
       expect(response).to have_http_status :internal_server_error
@@ -42,15 +50,19 @@ RSpec.describe StripeIntegration::SubscriptionRenewalsController, type: :request
 
   context 'Stripe Subscription does not exist' do
     let(:error_msg) { "No such subscription: '#{stripe_subscription_id}'" }
+    let(:error_class) { Stripe::InvalidRequestError }
 
-    before { update_stripe_subscription.and_raise(Stripe::InvalidRequestError.new(error_msg, :id)) }
+    before do
+      allow(Stripe::Subscription)
+        .to receive(:update)
+        .with(stripe_subscription_id, { cancel_at_period_end: })
+        .and_raise(error_class.new(error_msg, :id))
+    end
 
     it 'returns 500 if there are errors with the request to Stripe' do
-      expect(ErrorNotifier)
-        .to receive(:report)
-        .with(Stripe::InvalidRequestError, stripe_subscription_id: stripe_subscription_id)
+      expect(ErrorNotifier).to receive(:report).with(error_class, stripe_subscription_id:)
 
-      post url, params: params, as: :json
+      subject
 
       expect(response.media_type).to eq 'application/json'
       expect(response).to have_http_status :internal_server_error
