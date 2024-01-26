bceef340373853bfc52d08ea544b2e229359d6b8
Cissy Yu <cissy@quill.org>
2024-01-26 17:36:37 +0800

    config directory defined

--- services/QuillLMS/config/initializers/global.rb
+++ services/QuillLMS/config/initializers/global.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 Global.configure do |config|
-  config.backend :filesystem, environment: Rails.env.to_s, path: Rails.root.join('config/global').to_s
+  config.backend :filesystem, environment: Rails.env.to_s, config_directory: Rails.root.join('config/global').to_s, path: Rails.root.join('config/global').to_s
 end
