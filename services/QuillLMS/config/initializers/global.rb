45b2f459d8d8e6625022555544c35a6b03cd9495
Cissy Yu <cissy@quill.org>
2024-01-29 14:02:01 +0800

    backend try 2

--- services/QuillLMS/config/initializers/global.rb
+++ services/QuillLMS/config/initializers/global.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Global.configure do |config|
-  config.backend          = :filesystem
+  config.backend :filesystem, environment: Rails.env.to_s, path: Rails.root.join('config/global').to_s
   config.environment      = Rails.env.to_s
   config.config_directory = Rails.root.join('config/global').to_s
 end
