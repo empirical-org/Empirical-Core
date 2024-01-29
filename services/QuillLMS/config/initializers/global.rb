58a58f00ff373fb4e8e76921f049fb18f2ede1a6
Cissy Yu <cissy@quill.org>
2024-01-29 13:34:24 +0800

    back to old global definition

--- services/QuillLMS/config/initializers/global.rb
+++ services/QuillLMS/config/initializers/global.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
 Global.configure do |config|
-  config.backend :filesystem, environment: Rails.env.to_s, config_directory: Rails.root.join('config/global').to_s, path: Rails.root.join('config/global').to_s
+  config.environment = Rails.env.to_s
+  config.config_directory = Rails.root.join('config/global').to_s
 end
