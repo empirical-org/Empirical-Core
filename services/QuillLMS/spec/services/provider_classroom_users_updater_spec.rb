a62807d7953b33e4318ad8519a040f759fa5b834
brendanshean <brendanshean@gmail.com>
2024-01-29 14:09:15 -0500

    Fix intermittent specs involving update_all and updated_at checks (#11452)
    * Add wait and change matcher for update_all updated_at specs
    
    * Rename to change_after_waiting
    
    * blah
    
    * Use stubbing of DateTime.current
    
    * Update descriptions
    
    * Fix provider classroom user
    
    * Change to deleted_at
    
    * Another attempt
    
    * Print precision
    
    * Add sleep
    
    * add logger to test.rb
    
    * logging stuff
    
    * Add back in all specs
    
    * more attempts
    
    * try again
    
    * Add stdout flush
    
    * change log level to warn
    
    * remove logging configuration
    
    * temporarily suppress puts
    
    * remove sleep
    
    * Add sleep back in
    
    * Make sleep 3
    
    * Get rid of suppress statements in circle
    
    * Get rid of puts statements for rake testing while testing
    
    * Change stubbing
    
    * Update sleep
    
    * Add spacing to instance methods
    
    * remove logging
    
    * Add unscoped to test
    
    * Use same stubbing duration
    
    * add back in spacing

--- services/QuillLMS/spec/services/provider_classroom_users_updater_spec.rb
+++ services/QuillLMS/spec/services/provider_classroom_users_updater_spec.rb
@@ -19,17 +19,18 @@ RSpec.describe ProviderClassroomUsersUpdater do
     { user1: { before: DELETED, now: DELETED }, user2: { before: DELETED, now: DELETED } }
   ]
 
-  context 'setting update_at during update_all calls' do
-    let(:classroom_external_id) { Faker::Number.number}
-    let(:deleted_classroom_user) { create(:google_classroom_user, :deleted, classroom_external_id: classroom_external_id) }
-    let(:active_classroom_user) { create(:google_classroom_user, :active, classroom_external_id: classroom_external_id) }
+  context 'setting updated_at during update_all calls' do
+    subject { described_class.run(classroom_external_id, [deleted_classroom_user.user_external_id], GoogleClassroomUser) }
 
-    it do
-      expect do
-        described_class.run(classroom_external_id, [deleted_classroom_user.user_external_id], GoogleClassroomUser)
-      end.to change { deleted_classroom_user.reload.updated_at }
-        .and change { active_classroom_user.reload.updated_at }
-    end
+    let(:classroom_external_id) { Faker::Number.number}
+
+    let!(:deleted_classroom_user) { create(:google_classroom_user, :deleted, classroom_external_id:) }
+    let!(:active_classroom_user) { create(:google_classroom_user, :active, classroom_external_id:) }
+
+    before { allow(DateTime).to receive(:current).and_return(1.day.from_now) }
+
+    it { expect { subject }.to change { deleted_classroom_user.reload.updated_at } }
+    it { expect { subject }.to change { active_classroom_user.reload.updated_at } }
   end
 
   context 'canvas' do
@@ -55,4 +56,3 @@ RSpec.describe ProviderClassroomUsersUpdater do
     it_behaves_like 'a provider classroom users updater'
   end
 end
-
