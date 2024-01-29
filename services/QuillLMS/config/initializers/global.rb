400d4a196d55fa827c5633521e5b1bd8a2ccf602
Cissy Yu <cissy@quill.org>
2024-01-29 14:20:28 +0800

    try different definition

--- services/QuillLMS/config/initializers/global.rb
+++ services/QuillLMS/config/initializers/global.rb
@@ -2,6 +2,5 @@
 
 Global.configure do |config|
   config.backend :filesystem, environment: Rails.env.to_s, path: Rails.root.join('config/global').to_s
-  config.environment      = Rails.env.to_s
   config.config_directory = Rails.root.join('config/global').to_s
 end
