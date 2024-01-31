4f0aad4bbc8b7c2c1256a5610be896ce14267d95
brendanshean <brendanshean@gmail.com>
2024-01-31 09:03:29 -0500

    Fix identify stripe invoices with subscriptions bug (#11462)
    * Fix blank email error with IdentifyStripeInvoicesWithoutSubscriptions
    
    * move filtering inside of paging
    
    * Fix gte
    
    * reword comment

--- services/QuillLMS/spec/workers/identify_stripe_invoices_without_subscriptions_worker_spec.rb
+++ services/QuillLMS/spec/workers/identify_stripe_invoices_without_subscriptions_worker_spec.rb
@@ -2,7 +2,7 @@
 
 require 'rails_helper'
 
-describe IdentifyStripeInvoicesWithoutSubscriptionsWorker do
+RSpec.describe IdentifyStripeInvoicesWithoutSubscriptionsWorker do
   include_context 'Stripe Invoice'
 
   subject { described_class.new }
@@ -20,14 +20,14 @@ describe IdentifyStripeInvoicesWithoutSubscriptionsWorker do
     end
 
     it 'should send an email that includes invoices with no associated Quill Subscriptions' do
-      expect(StripeIntegration::Mailer).to receive(:invoices_without_subscriptions).with([{
+      expect(StripeIntegration::Mailer).to receive(:invoices_without_subscriptions).with([
         id: stripe_invoice.id,
         created: Time.at(stripe_invoice.created).getlocal.to_datetime,
         total: stripe_invoice.total / 100.0,
         customer_name: stripe_invoice.customer_name,
         customer_email: stripe_invoice.customer_email,
         number: stripe_invoice.number
-      }]).and_return(mailer_double)
+      ]).and_return(mailer_double)
 
       subject.perform
     end
