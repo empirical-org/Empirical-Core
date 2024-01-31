dcf3140e0d3e5b0731a8cc82f0c34ec13a46b6a7
Daniel Drabik <dan.drabik@gmail.com>
2024-01-31 13:05:15 -0500

    Put shared example login in contexts, might be corrupting global state.

--- services/QuillLMS/spec/support/shared_examples/snapshots/snapshots_period_length_rspec.rb
+++ services/QuillLMS/spec/support/shared_examples/snapshots/snapshots_period_length_rspec.rb
@@ -3,19 +3,16 @@
 require 'rails_helper'
 
 RSpec.shared_examples 'snapshots period length' do |timeframe_key, length, custom_start, custom_end|
-  let(:now) { DateTime.current.change(usec: 0) }
-  let(:timeframe) {described_class.calculate_timeframes(timeframe_key, custom_start:, custom_end:)}
-
-  # Periods are from beginning of day to end of day,
-  # so they are 1 second under the full amount, e.g. 29.999999999
-  # rounding at 6 decimals to make comparison deterministic
-  subject {(timeframe.last - timeframe.first).to_f.round(6)}
-
-  before do
-    allow(DateTime).to receive(:current).and_return(now)
-  end
-
   context "'#{timeframe_key}'" do
+    let(:now) { DateTime.current.change(usec: 0) }
+    let(:timeframe) {described_class.calculate_timeframes(timeframe_key, custom_start:, custom_end:)}
+
+    # Periods are from beginning of day to end of day,
+    # so they are 1 second under the full amount, e.g. 29.999999999
+    # rounding at 6 decimals to make comparison deterministic
+    subject {(timeframe.last - timeframe.first).to_f.round(6)}
+
+
     it { expect(subject).to eq length}
   end
 end
--- services/QuillLMS/spec/support/shared_examples/snapshots/snapshots_timeframe_times_of_day_rspec.rb
+++ services/QuillLMS/spec/support/shared_examples/snapshots/snapshots_timeframe_times_of_day_rspec.rb
@@ -3,29 +3,31 @@
 require 'rails_helper'
 
 RSpec.shared_examples 'snapshots timeframe times of day' do |timeframe_key, custom_start, custom_end|
-  let(:now) { DateTime.current.change(usec: 0) }
+  context 'period' do
+    let(:now) { DateTime.current.change(usec: 0) }
 
-  before do
-    allow(DateTime).to receive(:current).and_return(now)
-  end
+    before do
+      allow(DateTime).to receive(:current).and_return(now)
+    end
 
-  let(:current_timeframe) do
-    described_class.calculate_timeframes(timeframe_key, custom_start:, custom_end:)
-  end
-  let(:previous_timeframe) do
-    described_class.calculate_timeframes(timeframe_key, custom_start:, custom_end:, previous_timeframe: true)
-  end
+    let(:current_timeframe) do
+      described_class.calculate_timeframes(timeframe_key, custom_start:, custom_end:)
+    end
+    let(:previous_timeframe) do
+      described_class.calculate_timeframes(timeframe_key, custom_start:, custom_end:, previous_timeframe: true)
+    end
 
-  context "'#{timeframe_key}' current start" do
-    it { expect(current_timeframe.first).to be_start_of_day }
-  end
-  context "'#{timeframe_key}' current end" do
-    it { expect(current_timeframe.last).to be_end_of_day }
-  end
-  context "'#{timeframe_key}' previous start" do
-    it { expect(previous_timeframe.first).to be_start_of_day }
-  end
-  context "'#{timeframe_key}' previous end" do
-    it { expect(previous_timeframe.last).to be_end_of_day }
+    context "'#{timeframe_key}' current start" do
+      it { expect(current_timeframe.first).to be_start_of_day }
+    end
+    context "'#{timeframe_key}' current end" do
+      it { expect(current_timeframe.last).to be_end_of_day }
+    end
+    context "'#{timeframe_key}' previous start" do
+      it { expect(previous_timeframe.first).to be_start_of_day }
+    end
+    context "'#{timeframe_key}' previous end" do
+      it { expect(previous_timeframe.last).to be_end_of_day }
+    end
   end
 end
