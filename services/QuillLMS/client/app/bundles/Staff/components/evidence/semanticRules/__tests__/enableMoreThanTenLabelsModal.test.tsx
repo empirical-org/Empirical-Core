4412eddab0acf0008087e7c526f122e1d24f622d
Brendan Shean <brendanshean@gmail.com>
2024-02-01 12:45:41 -0500

    Emilia suggestion

--- services/QuillLMS/client/app/bundles/Staff/components/evidence/semanticRules/__tests__/enableMoreThanTenLabelsModal.test.tsx
+++ services/QuillLMS/client/app/bundles/Staff/components/evidence/semanticRules/__tests__/enableMoreThanTenLabelsModal.test.tsx
@@ -1,14 +1,20 @@
 import React from 'react';
-import { render, fireEvent, screen } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event'
 import EnableMoreThanLabelsModal from '../enableMoreThanTenLabelsModal';
 
 describe('EnableMoreThanLabelsModal', () => {
   const mockSave = jest.fn();
   const mockCancel = jest.fn();
 
-  const setup = (isOpen = true) => render(
-    <EnableMoreThanLabelsModal cancel={mockCancel} isOpen={isOpen} save={mockSave} />
-  );
+  function setup(isOpen = true) {
+    return {
+      user: userEvent.setup(),
+      ...render(
+        <EnableMoreThanLabelsModal cancel={mockCancel} isOpen={isOpen} save={mockSave} />
+      )
+    }
+  }
 
   beforeEach(() => {
     mockSave.mockClear();
@@ -32,24 +38,24 @@ describe('EnableMoreThanLabelsModal', () => {
     expect(screen.getByRole('button', { name: /save/i })).toBeInTheDocument();
   });
 
-  it('updates input value on change', () => {
-    setup();
+  it('updates input value on change', async () => {
+    const { user } = setup();
     const input = screen.getByLabelText(/additional labels/i) as HTMLInputElement;
-    fireEvent.change(input, { target: { value: 'label1, label2' } });
+    await user.type(input, 'label1, label2');
     expect(input.value).toBe('label1, label2');
   });
 
-  it('calls save function with input value on save button click', () => {
-    setup();
+  it('calls save function with input value on save button click', async () => {
+    const { user } = setup();
     const input = screen.getByLabelText(/additional labels/i);
-    fireEvent.change(input, { target: { value: 'label1, label2' } });
-    fireEvent.click(screen.getByRole('button', { name: /save/i }));
+    await user.type(input, 'label1, label2');
+    await user.click(screen.getByRole('button', { name: /save/i }));
     expect(mockSave).toHaveBeenCalledWith('label1, label2');
   });
 
-  it('calls cancel function on cancel button click', () => {
-    setup();
-    fireEvent.click(screen.getByRole('button', { name: /cancel/i }));
+  it('calls cancel function on cancel button click', async () => {
+    const { user } = setup();
+    await user.click(screen.getByRole('button', { name: /cancel/i }));
     expect(mockCancel).toHaveBeenCalled();
   });
 });
