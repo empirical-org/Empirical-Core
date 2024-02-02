ffd6f09f6e34bc64bd7846b6a245cb7c5b5e06f2
Emilia Friedberg <emiliafriedberg@gmail.com>
2024-02-02 13:02:10 -0500

    fix

--- services/QuillCMS/app/workers/rematch_responses_for_question_worker.rb
+++ services/QuillCMS/app/workers/rematch_responses_for_question_worker.rb
@@ -16,7 +16,7 @@ class RematchResponsesForQuestionWorker
 
       question_hash = retrieve_question(question_uid)
 
-      schedule_jobs(ungraded, question_type, question_hash, human_graded_ids, machine_graded.length.empty?)
+      schedule_jobs(ungraded, question_type, question_hash, human_graded_ids, machine_graded.empty?)
       schedule_jobs(machine_graded, question_type, question_hash, human_graded_ids, true)
     end
   end
