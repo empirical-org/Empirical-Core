2eacd4ffeecd59ac58da735354ebc13d318c2169
Emilia Friedberg <emiliafriedberg@gmail.com>
2024-02-06 09:18:37 -0500

    try againwith the lint

--- services/QuillCMS/spec/workers/rematch_finished_worker_spec.rb
+++ services/QuillCMS/spec/workers/rematch_finished_worker_spec.rb
@@ -3,15 +3,13 @@ require 'rails_helper'
 require 'webmock/rspec'
 
 describe RematchFinishedWorker do
-  subject { described_class.new }
-
   describe '#perform' do
     let(:question_key) { 'some_question_key' }
 
     it 'should trigger the RematchingFinished worker' do
       expect(RematchingFinished).to receive(:run).with(question_key)
 
-      subject.perform(question_key)
+      described_class.new.perform(question_key)
     end
   end
 end
