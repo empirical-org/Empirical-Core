1cc8680ecc189a999927ac4edb1dfced7872b737
Emilia Friedberg <emiliafriedberg@gmail.com>
2024-02-15 09:18:12 -0500

    try it like this instead

--- services/QuillLMS/client/app/bundles/Shared/components/shared/sortableList.tsx
+++ services/QuillLMS/client/app/bundles/Shared/components/shared/sortableList.tsx
@@ -6,41 +6,6 @@ import { CSS } from '@dnd-kit/utilities';
 
 import { DragHandleProvider, } from '../../hooks/useDragHandle'
 
-class IgnoreInteractiveElementsPointerSensor extends PointerSensor {
-  static activators = [
-    {
-      eventName: 'onPointerDown',
-      handler: ({nativeEvent: event}) => {
-        if (
-          !event.isPrimary ||
-          event.button !== 0 ||
-          isInteractiveElement(event.target)
-        ) {
-          return false;
-        }
-
-        return true;
-      },
-    },
-  ];
-}
-
-function isInteractiveElement(element) {
-  const interactiveElements = [
-    'button',
-    'input',
-    'textarea',
-    'select',
-    'option',
-  ];
-
-  if (interactiveElements.includes(element.tagName.toLowerCase())) {
-    return true;
-  }
-
-  return false;
-}
-
 const dropAnimationConfig: DropAnimation = {
   sideEffects: defaultDropAnimationSideEffects({
     styles: {
@@ -99,7 +64,11 @@ export const SortableList = ({ data, sortCallback, helperClass, useDragHandle, }
   const [activeId, setActiveId] = React.useState(null)
 
   const sensors = useSensors(
-    useSensor(IgnoreInteractiveElementsPointerSensor),
+    useSensor(PointerSensor, {
+      activationConstraint: {
+        distance: 8,
+      },
+    }),
     useSensor(KeyboardSensor, {
       coordinateGetter: sortableKeyboardCoordinates,
     })
