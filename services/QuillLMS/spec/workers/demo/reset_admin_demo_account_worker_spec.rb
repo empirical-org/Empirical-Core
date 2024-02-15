215f029a601a33690f82aafda32a15876f0a34a6
Emilia Friedberg <emiliafriedberg@gmail.com>
2024-02-15 10:03:10 -0500

    fix test

--- services/QuillLMS/spec/workers/demo/reset_admin_demo_account_worker_spec.rb
+++ services/QuillLMS/spec/workers/demo/reset_admin_demo_account_worker_spec.rb
@@ -7,8 +7,8 @@ describe Demo::ResetAdminDemoAccountWorker, type: :worker do
 
   describe "#perform" do
     it "should call reset for the admin demo" do
-      expect(DemoDemo::CreateAdminReport).to receive(:new).with(described_class::STAFF_DEMO_EMAIL)
-      expect_any_instance_of(DemoDemo::CreateAdminReport).to receive(:reset)
+      expect(Demo::CreateAdminReport).to receive(:new).with(described_class::STAFF_DEMO_EMAIL)
+      expect_any_instance_of(Demo::CreateAdminReport).to receive(:reset)
 
       worker.perform
     end
