2e9864a9a8a4c791d74af4c869ea812859b6e3e7
Emilia Friedberg <emiliafriedberg@gmail.com>
2024-02-16 08:39:44 -0500

    take out of low priority

--- services/QuillLMS/app/workers/demo/reset_admin_demo_account_worker.rb
+++ services/QuillLMS/app/workers/demo/reset_admin_demo_account_worker.rb
@@ -3,7 +3,7 @@
 # destroy admin demo account and recreates it from scratch
 class Demo::ResetAdminDemoAccountWorker
   include Sidekiq::Worker
-  sidekiq_options queue: SidekiqQueue::LOW, retry: false
+  sidekiq_options retry: false
 
   ADMIN_DEMO_EMAIL = "hello+demoadmin-admindemoschool@quill.org"
 
