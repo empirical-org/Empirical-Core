4f570ba634889a8e9b5d1079f423d7a2ab3acaab
Emilia Friedberg <emiliafriedberg@gmail.com>
2024-02-21 07:48:41 -0500

    fix test

--- services/QuillLMS/spec/workers/demo/reset_admin_demo_account_worker_spec.rb
+++ services/QuillLMS/spec/workers/demo/reset_admin_demo_account_worker_spec.rb
@@ -9,7 +9,6 @@ describe Demo::ResetAdminDemoAccountWorker, type: :worker do
 
   before do
     allow(Demo::CreateAdminReport).to receive(:new).with(described_class::ADMIN_DEMO_EMAIL).and_return(admin_report_instance)
-    allow(admin_report_instance).to receive(:reset)
   end
 
   it "calls reset on the Demo::CreateAdminReport instance" do
