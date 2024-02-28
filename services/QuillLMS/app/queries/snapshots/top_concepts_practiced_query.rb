3fdd32ebd6001724b869e6900f8b74e05fb64f60
Daniel Drabik <dan.drabik@gmail.com>
2024-02-28 13:46:54 -0500

    Fix issue with mat_view name.

--- services/QuillLMS/app/queries/snapshots/top_concepts_practiced_query.rb
+++ services/QuillLMS/app/queries/snapshots/top_concepts_practiced_query.rb
@@ -5,7 +5,7 @@ module Snapshots
     def from_and_join_clauses
       super + <<-SQL
         INNER JOIN lms.activity_category_activities
-          ON recent_reporting_sessions.activity_id = activity_category_activities.activity_id
+          ON #{materialized_view.name}.activity_id = activity_category_activities.activity_id
         INNER JOIN lms.activity_categories
           ON activity_category_activities.activity_category_id = activity_categories.id
       SQL
@@ -14,7 +14,7 @@ module Snapshots
     def select_clause
       <<-SQL
         SELECT activity_categories.name as value,
-        SUM(recent_reporting_sessions.activity_count) AS count
+        SUM(#{materialized_view.name}.activity_count) AS count
       SQL
     end
 
