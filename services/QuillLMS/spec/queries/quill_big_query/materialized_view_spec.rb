e81d6f95b46b16a3584687a2f6106b514b2de41d
Daniel Drabik <dan.drabik@gmail.com>
2024-02-28 16:13:44 -0500

    Rename and add more tests.

--- services/QuillLMS/spec/queries/quill_big_query/materialized_view_spec.rb
+++ services/QuillLMS/spec/queries/quill_big_query/materialized_view_spec.rb
@@ -3,42 +3,62 @@
 require 'rails_helper'
 
 describe QuillBigQuery::MaterializedView do
-  let(:query_key) { 'reporting_sessions_view' }
+  let(:view_key) { 'reporting_sessions_view' }
+
+  subject {described_class.new(view_key)}
 
   describe 'production config without stubs' do
-    subject {described_class.new(query_key)}
+    it {expect { subject.sql }.not_to raise_error }
 
-    it 'should find SQL file' do
-      expect { subject.sql }.not_to raise_error
-    end
+    it {expect(subject.name).to eq('recent_reporting_sessions_view')}
+    it {expect(subject.name_with_dataset).to eq('lms.recent_reporting_sessions_view')}
+    it {expect(subject.name_fallback).to eq('recent_reporting_sessions_local')}
   end
 
-  describe 'refresh!' do
+  describe 'text sql' do
     let(:report_sessions_sql_from_fixture) {'SELECT 2'}
     let(:drop_sql) {'DROP MATERIALIZED VIEW IF EXISTS lms.recent_reporting_sessions_view'}
     let(:create_sql) do
       "CREATE MATERIALIZED VIEW lms.recent_reporting_sessions_view AS (#{report_sessions_sql_from_fixture}); SELECT COUNT(*) FROM lms.recent_reporting_sessions_view;"
     end
+    let(:fallback_with) {"recent_reporting_sessions_local AS (#{report_sessions_sql_from_fixture})"}
 
     before do
       stub_const('QuillBigQuery::MaterializedView::QUERY_FOLDER', Rails.root.join('spec/fixtures/sql/'))
     end
 
-    subject { described_class.new(query_key).refresh! }
+    it {expect(subject.fallback_with_clause).to eq fallback_with}
 
-    it 'should call refresh! properly' do
-      expect(QuillBigQuery::WritePermissionsRunner).to receive(:run).with(drop_sql)
-      expect(QuillBigQuery::WritePermissionsRunner).to receive(:run).with(create_sql)
+    describe 'refresh!' do
+      it 'should call refresh! properly' do
+        expect(QuillBigQuery::WritePermissionsRunner).to receive(:run).with(drop_sql)
+        expect(QuillBigQuery::WritePermissionsRunner).to receive(:run).with(create_sql)
 
-      subject
+        subject.refresh!
+      end
+
+      context 'unknown key' do
+        let(:view_key) {'some-unknown-key'}
+
+        it { expect {subject}.to raise_error(QuillBigQuery::MaterializedView::InvalidQueryKeyError)}
+      end
     end
 
-    context 'unknown key' do
-      let(:query_key) {'some-unknown-key'}
+    describe 'drop!' do
+      it 'should call drop! properly' do
+        expect(QuillBigQuery::WritePermissionsRunner).to receive(:run).with(drop_sql)
+        expect(QuillBigQuery::WritePermissionsRunner).to_not receive(:run).with(create_sql)
 
-      it 'should raise with unknown key' do
+        subject.drop!
+      end
+    end
 
-        expect {subject}.to raise_error(QuillBigQuery::MaterializedView::InvalidQueryKeyError)
+    describe 'create!' do
+      it 'should call drop! properly' do
+        expect(QuillBigQuery::WritePermissionsRunner).to_not receive(:run).with(drop_sql)
+        expect(QuillBigQuery::WritePermissionsRunner).to receive(:run).with(create_sql)
+
+        subject.create!
       end
     end
   end
