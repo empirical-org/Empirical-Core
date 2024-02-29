956e29d30939974570c9351a9208ce4bed19df6e
Daniel Drabik <dan.drabik@gmail.com>
2024-02-29 12:13:01 -0500

    Checkout from bq_view branch.

---
+++ services/QuillLMS/app/queries/quill_big_query/materialized_view_query.rb
@@ -0,0 +1,47 @@
+# frozen_string_literal: true
+
+module QuillBigQuery
+  class MaterializedViewQuery < Query
+
+    BROKEN_MATERIALIZED_VIEW_ERRORS = [
+      ::Google::Cloud::NotFoundError,
+      ::Google::Cloud::InvalidArgumentError
+    ]
+
+    # Array of uillBigQuery::MaterializedView defined by child
+    def materialized_views = raise NotImplementedError
+
+    def materialized_view(key)
+      @materialized_views ||= {}
+      @materialized_views[key] ||= QuillBigQuery::MaterializedView.new(key)
+    end
+
+    def run_query
+      query_runner(query)
+    rescue *BROKEN_MATERIALIZED_VIEW_ERRORS
+      query_runner(query_fallback)
+    end
+
+    private def query_runner(query)
+      post_query_transform(runner.execute(query))
+    end
+
+    def query_fallback
+      <<-SQL.squish
+        WITH
+        #{query_fallback_with_clauses}
+        #{query_fallback_body}
+      SQL
+    end
+
+    private def query_fallback_with_clauses = materialized_views.map(&:fallback_with_clause).join(', ')
+
+    private def query_fallback_body
+      materialized_views.inject(query) do |query, view|
+        query
+          .gsub(view.name_with_dataset, view.name_fallback)
+          .gsub(view.name, view.name_fallback)
+      end
+    end
+  end
+end
