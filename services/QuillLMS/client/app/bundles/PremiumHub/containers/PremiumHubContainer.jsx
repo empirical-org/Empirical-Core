152dbf316f03e237d0b9225264a42692f64fe28d
Emilia Friedberg <emiliafriedberg@gmail.com>
2024-02-29 08:12:18 -0500

    update locked pages for admin reports (#11540)
    * wip
    
    * have all report pages load the nonpremiumreport
    
    * update tests
    
    * update images and new icon
    
    * no unused imports
    
    * update images and quotes

--- services/QuillLMS/client/app/bundles/PremiumHub/containers/PremiumHubContainer.jsx
+++ services/QuillLMS/client/app/bundles/PremiumHub/containers/PremiumHubContainer.jsx
@@ -77,10 +77,10 @@ const PremiumHubContainer = ({ id, history, location, children, user, }) => {
     if (accessType() === FULL || accessType() === LOADING) { return <span /> }
 
     // this checks that the last part of the path (after the final '/') matches the overview page (which is just /teachers/premium_hub) or the integrations page
-    const onIntegrationsOverviewOrUsageSnapshotPage = ['integrations', 'premium_hub', 'usage_snapshot_report'].some((path) => location.pathname.split('/').pop() === path)
+    const onPageThatShouldNotShowBanner = ['integrations', 'premium_hub', 'usage_snapshot_report', 'data_export', 'diagnostic_growth_report', 'district_concept_reports', 'district_activity_scores', 'district_standards_reports'].some((path) => location.pathname.split('/').pop() === path)
     const onSubscriptionPageWithExpiredPremium = location.pathname.includes('subscriptions') && administers_school_with_current_or_expired_premium
 
-    if (accessType() === LIMITED && !(onIntegrationsOverviewOrUsageSnapshotPage || onSubscriptionPageWithExpiredPremium)) {
+    if (accessType() === LIMITED && !(onPageThatShouldNotShowBanner || onSubscriptionPageWithExpiredPremium)) {
       return (
         <Banner
           bodyText="Subscribe to School or District Premium to unlock all Premium Hub features. Manage teacher accounts, access teacher reports, and view school-wide student data."
