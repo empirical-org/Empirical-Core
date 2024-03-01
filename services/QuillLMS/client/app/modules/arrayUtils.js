065abae5d88cf4926226fcfc521a880a99745d11
Peter Kong <pkong@quill.org>
2024-02-29 21:41:12 -0700

    add skill group udf with spec

--- services/QuillLMS/client/app/modules/arrayUtils.js
+++ services/QuillLMS/client/app/modules/arrayUtils.js
@@ -67,14 +67,134 @@ export function studentwiseAggregateUDF(scores, conceptNames, activityIds, compl
 
   const numAssignedRecommendedCompleted = zipped.slice(canonicalPreTestIdx, canonicalPostTestIdx).length - 1
 
-  const skillScoreAssoc = conceptAllowList.reduce(
-    (accum, currentValue) => ({[currentValue]: zipped.find(elem => elem.conceptName == currentValue)?.score}),
-    {}
-  )
+  // need to account for accum
+  // const skillScoreAssoc = conceptAllowList.reduce(
+  //   (accum, currentValue) => ({[currentValue]: zipped.find(elem => elem.conceptName == currentValue)?.score}),
+  //   {}
+  // )
 
   return {
     errorMessage,
     spannedActivityCount: numAssignedRecommendedCompleted,
     ...skillScoreAssoc
+  }
+}
+
+export function studentwiseSkillGroupUDF(scores, activityIds, completedAts, skillGroupNames) {
+  function zipAndSort(scores, activityIds, completedAts, skillGroupNames) {
+    const zipped = completedAts.map(
+      (elem, i) => ({
+        completedAt: elem,
+        score: scores[i],
+        activityId: parseInt(activityIds[i]),
+        skillGroupName: skillGroupNames[i]
+
+      })
+    )
+    return zipped.sort((a,b) => (new Date(a.completedAt) - new Date(b.completedAt)))
+  }
+
+  function findLastIndex(array, fn) {
+    const reversedArray = [...array].reverse()
+    const reversedIdx = reversedArray.findIndex(fn)
+    if (reversedIdx === -1) return -1
+    return array.length - 1 - reversedIdx
+  }
+
+  function allArraysEqualLength(...arrayLengths) {
+    const firstArrayLength = arrayLengths.pop()
+
+    return arrayLengths.every(x => x == firstArrayLength)
+  }
+
+  function getSkillScore(zipped, errorMessageArray, skillGroupName, preOrPost) {
+    if (preOrPost == 'pre') {
+      const row = zipped.filter(x => x.activityId == PRE_DIAGNOSTIC_ACTIVITY_ID).find(
+        x => x.skillGroupName == skillGroupName
+      )
+      if (!row) {
+        errorMessageArray.push(`Could not find row with skillGroupName ${skillGroupName}`)
+        return 0
+      }
+      return row.score
+    }
+    else {
+      const row = zipped.filter(x => x.activityId == POST_DIAGNOSTIC_ACTIVITY_ID).find(
+        x => x.skillGroupName == skillGroupName
+      )
+      if (!row) {
+        errorMessageArray.push(`Could not find row with skillGroupName ${skillGroupName}`)
+        return 0
+      }
+      return row.score
+    }
+  }
+
+  const skillGroupAllowList = [
+    { id: 123, name: 'Capitalization' },
+    { id: 124, name: 'Plural and Possessive Nouns' },
+    { id: 125, name: 'Adjectives and Adverbs' },
+    { id: 126, name: 'Prepositional Phrases' },
+    { id: 127, name: 'Capitalization' },
+    { id: 128, name: 'Compound Subjects, Objects, and Predicates' },
+    { id: 216, name: 'Subject-Verb Agreement' }
+  ]
+
+  let errorMessageArray = []
+
+  const defaultReturnValue = {
+    errorMessage: "Default error message",
+    ...skillGroupAllowList.reduce((accum, elem) => ({[`${elem.name}_pre`]: 0, [`${elem.name}_post`]: 0}), {})
+  }
+
+  if (!allArraysEqualLength(scores.length, activityIds.length, completedAts.length, skillGroupNames.length)) {
+    return {
+      ...defaultReturnValue,
+      ...{ errorMessage: `Unequal input lengths: ${scores.length} ${activityIds.length} ${completedAts.length} ${skillGroupNames.length}` }
+    }
+  }
+
+  const zipped = zipAndSort(scores, activityIds, completedAts, skillGroupNames)
+  const PRE_DIAGNOSTIC_ACTIVITY_ID = 1663
+  const POST_DIAGNOSTIC_ACTIVITY_ID = 1664
+  const canonicalPreTestIdx = zipped.findIndex( elem => elem.activityId == PRE_DIAGNOSTIC_ACTIVITY_ID )
+  const canonicalPostTestIdx = findLastIndex( zipped, elem => elem.activityId == POST_DIAGNOSTIC_ACTIVITY_ID )
+
+  if ((canonicalPreTestIdx  == -1) ||
+      (canonicalPostTestIdx == -1)) {
+    return {
+      ...defaultReturnValue,
+      ...{ errorMessage: `Bad index(es): canonicalPreTestIdx: ${canonicalPreTestIdx} canonicalPostTestIdx: ${canonicalPostTestIdx}` }
+    }
+  }
+
+  const canonicalPreTest = zipped[canonicalPreTestIdx]
+  const canonicalPostTest = zipped[canonicalPostTestIdx]
+
+  if ((!canonicalPreTest?.completedAt) ||
+      (!canonicalPostTest?.completedAt) ||
+      (canonicalPreTest.completedAt >= canonicalPostTest.completedAt)) {
+    return {
+      ...defaultReturnValue,
+      ...{ errorMessage: `Nonexistent or incorrect sequencing: canonicalPreTest?.completedAt: ${canonicalPreTest?.completedAt} canonicalPostTest?.completedAt: ${canonicalPostTest?.completedAt}`}
+    }
+  }
+
+  const skillScores = skillGroupAllowList.reduce(
+    (accum, currentValue) => {
+      const preColumnName = `${currentValue.name}_pre`
+      const postColumnName = `${currentValue.name}_post`
+      return {
+        [preColumnName]: getSkillScore(zipped, errorMessageArray, currentValue.name, 'pre'),
+        [postColumnName]: getSkillScore(zipped, errorMessageArray, currentValue.name, 'post'),
+        ...accum
+      }
+    },
+    {}
+  )
+
+  return {
+    errorMessage: errorMessageArray.join(' '),
+    ...skillScores
   }
 }
--- services/QuillLMS/client/app/modules/arrayUtils.test.js
+++ services/QuillLMS/client/app/modules/arrayUtils.test.js
@@ -1,18 +1,18 @@
-import { studentwiseAggregateUDF } from "./arrayUtils"
+import { studentwiseAggregateUDF, studentwiseSkillGroupUDF } from "./arrayUtils"
 
 describe('studentwiseAggregateUDF', () => {
 
-  test('should extract correct spanned-activity counts', () => {
-    const activity_ids = ["1678", "3", "4", "1664"];
-    const completed_ats = ['2022-01-01T00:00:00Z', '2022-01-01T00:01:00Z', '2024-01-01T00:01:00Z',  '2022-01-02T00:00:00Z'];
-    const scores = [80, 42, 0, 90];
-    const concept_names = ['a', 'Pronouns', 'c', 'd']
-    const result = studentwiseAggregateUDF(scores, concept_names, activity_ids, completed_ats);
-    expect(result.spannedActivityCount).toEqual(1)
-    expect(result.errorMessage).toEqual(undefined)
-    expect(result.Pronouns).toEqual(42)
+  // test('should extract correct spanned-activity counts', () => {
+  //   // const activity_ids = ["1678", "3", "4", "1664"];
+  //   // const completed_ats = ['2022-01-01T00:00:00Z', '2022-01-01T00:01:00Z', '2024-01-01T00:01:00Z',  '2022-01-02T00:00:00Z'];
+  //   // const scores = [80, 42, 0, 90];
+  //   // const concept_names = ['a', 'Pronouns', 'c', 'd']
+  //   // const result = studentwiseAggregateUDF(scores, concept_names, activity_ids, completed_ats);
+  //   // expect(result.spannedActivityCount).toEqual(1)
+  //   // expect(result.errorMessage).toEqual(undefined)
+  //   // expect(result.Pronouns).toEqual(42)
 
-  })
+  // })
   // test('should return default value on insufficient input', () => {
   //   const result = studentwiseAggregateUDF(
   //     [], [], []
@@ -45,5 +45,23 @@ describe('studentwiseAggregateUDF', () => {
   //     postTestScore: 90
   //   });
   // });
+
+})
+
+
+describe('studentwiseSkillGroupUDF', () => {
+
+  test('should return skillGroup-score pairs', () => {
+    const activityIds = ["1663", "1663", "1664", "1664"];
+    const completedAts = ['2022-01-01T00:00:00Z', '2022-01-01T00:01:00Z', '2022-01-02T00:00:00Z', '2022-01-02T00:01:00Z'];
+    const scores = [0.5, 0.6, 0.7, 0.8];
+    const skillGroupNames = ['Plural and Possessive Nouns', 'Capitalization', 'Plural and Possessive Nouns', 'Capitalization' ]
+    const result = studentwiseSkillGroupUDF(scores, activityIds, completedAts, skillGroupNames);
+
+    expect(result['Plural and Possessive Nouns_pre']).toEqual(0.5)
+    expect(result['Capitalization_pre']).toEqual(0.6)
+    expect(result['Plural and Possessive Nouns_post']).toEqual(0.7)
+    expect(result['Capitalization_post']).toEqual(0.8)
+  })
 
 })
