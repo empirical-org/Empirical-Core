cd87ca1af8f03f383301aca1ec41bf48d798bb8b
Peter Kong <pkong@quill.org>
2024-03-02 22:18:44 -0700

    combine skill group and activity span UDFS

--- services/QuillLMS/client/app/modules/arrayUtils.js
+++ services/QuillLMS/client/app/modules/arrayUtils.js
@@ -1,85 +1,5 @@
-
 // BigQuery does not currently accept DATETIMEs as arguments for JS UDFs
 // So we cast DATETIMES to STRINGS before calling this function
-
-// (scores Array<FLOAT64>, conceptNames Array<STRING>, activityIds Array<INTEGER>, completedAts Array<STRING>)
-export function studentwiseCompletedActivitiesUDF(scores, conceptNames, activityIds, completedAts) {
-  function zipAndSort(scores, conceptNames, activityIds, completedAts) {
-    const zipped = completedAts.map(
-      (elem, i) => ({
-        completedAt: elem,
-        score: scores[i],
-        activityId: activityIds[i],
-        conceptName: conceptNames[i]
-
-      })
-    )
-    return zipped.sort((a,b) => (new Date(a.completedAt) - new Date(b.completedAt)))
-  }
-
-  function findLastIndex(array, fn) {
-    const reversedArray = [...array].reverse()
-    const reversedIdx = reversedArray.findIndex(fn)
-    if (reversedIdx === -1) return -1
-    return array.length - 1 - reversedIdx
-  }
-
-  function allArraysEqualLength(scoresLength, conceptNamesLength, activityIdsLength, completedAtsLength) {
-    return [scoresLength, conceptNamesLength, activityIdsLength].every(x => x == completedAtsLength)
-  }
-
-  if (!allArraysEqualLength(scores.length, conceptNames.length, activityIds.length, completedAts.length)) {
-    errorMessage = `Unequal input lengths: ${scores.length} ${conceptNames.length} ${activityIds.length} ${completedAts.length}`
-  }
-
-  const zipped = zipAndSort(scores, conceptNames, activityIds, completedAts)
-  const diagnosticPreActivityIds = "1678 1568 1161 1668 1590 1663".split(' ').map(x => parseInt(x))
-  const diagnosticPostActivityIds = "1664 1669 1680 1774 1814 1818".split(' ').map(x => parseInt(x))
-  const conceptAllowList = ['Pronouns'] // TODO
-  let errorMessage = undefined
-
-  // We currently define 'canonical' tests as those which maximally span a student's
-  // activities, chronologically
-  const canonicalPreTestIdx = zipped.findIndex( elem => diagnosticPreActivityIds.includes(parseInt(elem.activityId)) )
-  const canonicalPostTestIdx = findLastIndex(zipped, elem => diagnosticPostActivityIds.includes(parseInt(elem.activityId)) )
-
-  const defaultReturnValue = { errorMessage: "Something happened", Pronouns: 0, spannedActivityCount: 0 }
-
-  if ((canonicalPreTestIdx  == -1) ||
-      (canonicalPostTestIdx == -1)) {
-    return {
-      ...defaultReturnValue,
-      ...{ errorMessage: `Bad index(es): canonicalPreTestIdx: ${canonicalPreTestIdx} canonicalPostTestIdx: ${canonicalPostTestIdx}` }
-    }
-  }
-
-  const canonicalPreTest = zipped[canonicalPreTestIdx]
-  const canonicalPostTest = zipped[canonicalPostTestIdx]
-
-  if ((!canonicalPreTest?.completedAt) ||
-      (!canonicalPostTest?.completedAt) ||
-      (canonicalPreTest.completedAt >= canonicalPostTest.completedAt)) {
-    return {
-      ...defaultReturnValue,
-      ...{ errorMessage: `Nonexistent or incorrect sequencing: canonicalPreTest?.completedAt: ${canonicalPreTest?.completedAt} canonicalPostTest?.completedAt: ${canonicalPostTest?.completedAt}`}
-    }
-  }
-
-  const numAssignedRecommendedCompleted = zipped.slice(canonicalPreTestIdx, canonicalPostTestIdx).length - 1
-
-  // need to account for accum
-  // const skillScoreAssoc = conceptAllowList.reduce(
-  //   (accum, currentValue) => ({[currentValue]: zipped.find(elem => elem.conceptName == currentValue)?.score}),
-  //   {}
-  // )
-
-  return {
-    errorMessage,
-    spannedActivityCount: numAssignedRecommendedCompleted,
-    ...skillScoreAssoc
-  }
-}
-
 export function studentwiseSkillGroupUDF(scores, activityIds, completedAts, skillGroupNames) {
   function boolToInt(bool) { return bool ? 1 : 0}
 
@@ -150,10 +70,10 @@ export function studentwiseSkillGroupUDF(scores, activityIds, completedAts, skil
   }
 
   if (!allArraysEqualLength(scores.length, activityIds.length, completedAts.length, skillGroupNames.length)) {
-    return {
+    return JSON.stringify({
       ...defaultReturnValue,
       ...{ errorMessage: `Unequal input lengths: ${scores.length} ${activityIds.length} ${completedAts.length} ${skillGroupNames.length}` }
-    }
+    })
   }
 
   const zipped = zipAndSort(scores, activityIds, completedAts, skillGroupNames)
@@ -164,10 +84,10 @@ export function studentwiseSkillGroupUDF(scores, activityIds, completedAts, skil
 
   if ((canonicalPreTestIdx  == -1) ||
       (canonicalPostTestIdx == -1)) {
-    return {
+    return JSON.stringify({
       ...defaultReturnValue,
       ...{ errorMessage: `Bad index(es): canonicalPreTestIdx: ${canonicalPreTestIdx} canonicalPostTestIdx: ${canonicalPostTestIdx}` }
-    }
+    })
   }
 
   const canonicalPreTest = zipped[canonicalPreTestIdx]
@@ -176,10 +96,10 @@ export function studentwiseSkillGroupUDF(scores, activityIds, completedAts, skil
   if ((!canonicalPreTest?.completedAt) ||
       (!canonicalPostTest?.completedAt) ||
       (canonicalPreTest.completedAt >= canonicalPostTest.completedAt)) {
-    return {
+    return JSON.stringify({
       ...defaultReturnValue,
       ...{ errorMessage: `Nonexistent or incorrect sequencing: canonicalPreTest?.completedAt: ${canonicalPreTest?.completedAt} canonicalPostTest?.completedAt: ${canonicalPostTest?.completedAt}`}
-    }
+    })
   }
 
   const skillScores = skillGroupAllowList.reduce(
@@ -195,9 +115,12 @@ export function studentwiseSkillGroupUDF(scores, activityIds, completedAts, skil
     {}
   )
 
+  const numAssignedRecommendedCompleted = zipped.slice(canonicalPreTestIdx, canonicalPostTestIdx).length - 1
+
   return JSON.stringify(
     {
       errorMessage: errorMessageArray.join(' '),
+      numAssignedRecommendedCompleted,
       ...skillScores
     }
   )
--- services/QuillLMS/client/app/modules/arrayUtils.test.js
+++ services/QuillLMS/client/app/modules/arrayUtils.test.js
@@ -50,8 +50,7 @@ describe('studentwiseCompletedActivitiesUDF', () => {
 
 
 describe('studentwiseSkillGroupUDF', () => {
-
-  test('should return skillGroup-score pairs', () => {
+  it('should return skillGroup-score pairs', () => {
     const activityIds = ["1663", "1663", "1664", "1664"];
     const completedAts = ['2022-01-01T00:00:00Z', '2022-01-01T00:01:00Z', '2022-01-02T00:00:00Z', '2022-01-02T00:01:00Z'];
     const scores = [false, false, true, true];
@@ -63,6 +62,17 @@ describe('studentwiseSkillGroupUDF', () => {
     expect(parsedResult['Capitalization_pre']).toEqual(0)
     expect(parsedResult['Plural and Possessive Nouns_post']).toEqual(1)
     expect(parsedResult['Capitalization_post']).toEqual(1)
+  })
+
+  it('should extract correct spanned-activity counts', () => {
+    const activityIds = ["1663", "3", "4", "1664"];
+    const completedAts = ['2022-01-01T00:00:00Z', '2022-01-01T00:01:00Z', '2024-01-01T00:01:00Z',  '2022-01-02T00:00:00Z'];
+    const scores = [false, false, true, true];
+    const skillGroupNames = "a b c d".split(' ')
+
+    const result = studentwiseSkillGroupUDF(scores, activityIds, completedAts, skillGroupNames);
+    const parsedResult = JSON.parse(result)
+    expect(parsedResult.numAssignedRecommendedCompleted).toEqual(1)
   })
 
 })
