b52f3eabc7ec4a2a1966c806183e4ac1b4406aac
Brendan Shean <brendanshean@gmail.com>
2024-03-05 09:56:12 -0500

    Move app over to engine db/migrate

--- services/QuillLMS/engines/evidence/lib/evidence/engine.rb
+++ services/QuillLMS/engines/evidence/lib/evidence/engine.rb
@@ -32,5 +32,13 @@ module Evidence
         FactoryBot.definition_file_paths << File.expand_path('../../../spec/factories/evidence', __FILE__)
       end
     end
+
+    initializer :append_migrations do |app|
+      unless app.root.to_s.match root.to_s
+        config.paths["db/migrate"].expanded.each do |expanded_path|
+          app.config.paths["db/migrate"] << expanded_path
+        end
+      end
+    end
   end
 end
