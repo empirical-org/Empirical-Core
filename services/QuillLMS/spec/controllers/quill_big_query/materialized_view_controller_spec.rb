e6b171c4137e5b5ca967a346e4da1eb1cac1f280
Anathomical <thomas.robertson@quill.org>
2024-03-11 10:55:33 -0400

    Update materialized view refresh controller to take multiple views (#11583)

--- services/QuillLMS/spec/controllers/quill_big_query/materialized_view_controller_spec.rb
+++ services/QuillLMS/spec/controllers/quill_big_query/materialized_view_controller_spec.rb
@@ -21,6 +21,19 @@ RSpec.describe QuillBigQuery::MaterializedViewsController do
       subject
     end
 
+    context 'multiple keys' do
+      let(:view_key2) { 'some_other_key' }
+      let(:view_key_param) { "#{view_key},#{view_key2}" }
+      let(:params) { { api_key:, view_key: view_key_param } }
+
+      it 'should enqueue every comma-separated job passed in' do
+        expect(QuillBigQuery::MaterializedViewRefreshWorker).to receive(:perform_async).with(view_key)
+        expect(QuillBigQuery::MaterializedViewRefreshWorker).to receive(:perform_async).with(view_key2)
+
+        subject
+      end
+    end
+
     context 'no API_KEY set' do
       before do
         stub_const('QuillBigQuery::MaterializedViewsController::API_KEY', '')
