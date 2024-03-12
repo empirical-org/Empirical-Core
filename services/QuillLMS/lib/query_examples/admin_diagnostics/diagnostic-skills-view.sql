656f469c9a5536f81c3349944665ca9d7bd807b7
Daniel Drabik <dan.drabik@gmail.com>
2024-03-12 15:42:45 -0400

    Query samples of admin diagnostics changes.

---
+++ services/QuillLMS/lib/query_examples/admin_diagnostics/diagnostic-skills-view.sql
@@ -0,0 +1,68 @@
+        /*
+           Data Processed By Query: 170.17 GB
+           Bytes Billed For Query:  0.0 GB
+           Total Query Time:        0 ms
+           Total Slot Time:          ms
+           BI Engine Mode Used:     
+             BI Engine Code:          
+             BI Engine Message:       
+        */
+        WITH aggregate_rows AS (                SELECT
+          skill_group_name,
+          classrooms.id AS aggregate_id,
+          classrooms.name AS name,
+          'classroom' AS group_by,
+                  COUNT(DISTINCT performance.pre_activity_session_id) AS pre_students_completed,
+        COUNT(DISTINCT performance.post_activity_session_id) AS post_students_completed,
+        ROUND(SAFE_DIVIDE(SUM(performance.pre_questions_correct), CAST(SUM(performance.pre_questions_total) AS float64)), 2) AS pre_score,
+        ROUND(SAFE_DIVIDE(SUM(performance.post_questions_correct), CAST(SUM(performance.post_questions_total) AS float64)), 2) AS post_score,
+        /*
+        The value below is used to duplicate growth percentage calculations from teacher reports:
+          It is intended to work only when aggregated at the Skill Group level
+          The ROUND(..., 2) statements ensure that we aggregate at the same level of rounding as the teacher report uses (that report rounds all scores to integer percentages) which lets us avoid being off by 1% from the teacher reports because of higher precision
+          The CASE statements ensure that we don't calculate the overall pre-Diagnostic performance, but rather calculate it only the performance for students who have also completed the post-Diagnostic
+          NULL values are ignored when executing aggregate queries such as SUM, so rows from `performance` that have NULL "post" data will be excluded from the aggregation
+          We subtract the pre-Diagnostic average from the post-Diagnostic average, and then use GREATEST to treat cases where post-Diagnostic scores are worse as if they were equal instead
+        */
+        GREATEST(
+          ROUND(SAFE_DIVIDE(SUM(performance.post_questions_correct), CAST(SUM(performance.post_questions_total) AS float64)), 2)
+            - ROUND(SAFE_DIVIDE(SUM(CASE WHEN performance.post_activity_session_id IS NOT NULL THEN performance.pre_questions_correct ELSE NULL END),
+                CAST(SUM(CASE WHEN performance.post_activity_session_id IS NOT NULL THEN performance.pre_questions_total ELSE NULL END) AS float64)), 2),
+        0) AS growth_percentage,
+        COUNT(DISTINCT CASE WHEN (pre_questions_correct = pre_questions_total AND post_questions_correct = post_questions_total) THEN performance.student_id ELSE NULL END) AS maintained_proficiency,
+        COUNT(DISTINCT CASE WHEN (pre_questions_correct < pre_questions_total AND post_questions_correct > pre_questions_correct) THEN performance.student_id ELSE NULL END) AS improved_proficiency,
+        COUNT(DISTINCT CASE WHEN (post_questions_correct < pre_questions_correct OR (pre_questions_correct < pre_questions_total AND post_questions_correct = pre_questions_correct)) THEN performance.student_id ELSE NULL END) AS recommended_practice
+
+
+                FROM lms.pre_post_diagnostic_skill_group_performance_view AS performance
+        JOIN lms.active_classroom_stubs_view AS classrooms ON performance.classroom_id = classrooms.id
+        JOIN lms.classrooms_teachers ON classrooms.id = classrooms_teachers.classroom_id AND classrooms_teachers.role = 'owner'
+        JOIN lms.schools_users ON classrooms_teachers.user_id = schools_users.user_id
+        JOIN lms.activities ON performance.activity_id = activities.id
+        JOIN lms.active_user_names_view AS users ON classrooms_teachers.user_id = users.id
+
+                WHERE
+          performance.pre_activity_session_completed_at BETWEEN '2023-08-01 00:00:00' AND '2023-12-01 00:00:00'
+          
+          
+          AND classrooms_teachers.role = 'owner'
+          AND activities.id = 1663
+          AND schools_users.school_id IN (38811,38804,38801,38800,38779,38784,38780,38773,38765,38764)
+          
+
+        GROUP BY aggregate_id, classrooms.name, skill_group_name
+        
+        
+)
+                SELECT
+          skill_group_name,
+          NULL as aggregate_id,
+          'ROLLUP' AS name,
+          group_by,
+          SUM(pre_students_completed) AS pre_students_completed, SUM(post_students_completed) AS post_students_completed, SUM(pre_students_completed * pre_score) / SUM(pre_students_completed) AS pre_score, SUM(post_students_completed * post_score) / SUM(post_students_completed) AS post_score, SUM(post_students_completed * growth_percentage) / SUM(post_students_completed) AS growth_percentage, SUM(maintained_proficiency) AS maintained_proficiency, SUM(improved_proficiency) AS improved_proficiency, SUM(recommended_practice) AS recommended_practice
+        FROM aggregate_rows
+        GROUP BY skill_group_name, group_by
+        UNION ALL
+        SELECT *
+          FROM aggregate_rows
+
---
+++ services/QuillLMS/lib/query_examples/admin_diagnostics/diagnostic-students-view.sql
@@ -0,0 +1,76 @@
+        /*
+           Data Processed By Query: 170.12 GB
+           Bytes Billed For Query:  0.0 GB
+           Total Query Time:        0 ms
+           Total Slot Time:          ms
+           BI Engine Mode Used:     
+             BI Engine Code:          
+             BI Engine Message:       
+        */
+        WITH aggregate_rows AS (                SELECT
+          students.id AS student_id,
+          students.name AS student_name,
+          students.id AS aggregate_id,
+          students.name AS name,
+          'student' AS group_by,
+          performance.skill_group_name AS skill_group_name,
+                  CASE WHEN performance.pre_questions_correct < performance.post_questions_correct THEN 1 ELSE 0 END AS pre_to_post_improved_skill_count,
+        performance.pre_questions_correct AS pre_questions_correct,
+        performance.pre_questions_total AS pre_questions_total,
+        SAFE_DIVIDE(CAST(performance.pre_questions_correct AS float64), performance.pre_questions_total) AS pre_questions_percentage,
+        performance.post_questions_correct AS post_questions_correct,
+        performance.post_questions_total AS post_questions_total,
+        SAFE_DIVIDE(CAST(performance.post_questions_correct AS float64), performance.post_questions_total) AS post_questions_percentage,
+        1 AS total_skills,
+        CASE WHEN performance.pre_questions_correct = performance.pre_questions_total THEN 1 ELSE 0 END AS pre_skills_proficient,
+        CASE WHEN performance.pre_questions_correct = performance.pre_questions_total THEN 0 ELSE 1 END AS pre_skills_to_practice,
+        CASE WHEN performance.post_questions_correct < performance.post_questions_total AND performance.post_questions_correct <= performance.pre_questions_correct THEN 1 ELSE 0 END post_skills_to_practice,
+        CASE WHEN performance.post_questions_correct > performance.pre_questions_correct THEN 1 ELSE 0 END AS post_skills_improved,
+        CASE WHEN performance.post_questions_correct = performance.pre_questions_correct AND performance.post_questions_correct = performance.post_questions_total THEN 1 ELSE 0 END post_skills_maintained,
+        CASE WHEN performance.post_questions_correct > performance.pre_questions_correct OR (performance.post_questions_correct = performance.pre_questions_correct AND performance.post_questions_correct = performance.post_questions_total) THEN 1 ELSE 0 END post_skills_improved_or_maintained
+
+
+                        FROM lms.pre_post_diagnostic_skill_group_performance_view AS performance
+        JOIN lms.active_classroom_stubs_view AS classrooms ON performance.classroom_id = classrooms.id
+        JOIN lms.classrooms_teachers ON classrooms.id = classrooms_teachers.classroom_id AND classrooms_teachers.role = 'owner'
+        JOIN lms.schools_users ON classrooms_teachers.user_id = schools_users.user_id
+        JOIN lms.activities ON performance.activity_id = activities.id
+        JOIN lms.active_user_names_view AS users ON classrooms_teachers.user_id = users.id
+
+        JOIN lms.active_user_names_view AS students ON performance.student_id = students.id
+
+                WHERE
+          performance.pre_assigned_at BETWEEN '2023-08-01 00:00:00' AND '2023-12-01 00:00:00'
+          
+          
+          AND classrooms_teachers.role = 'owner'
+          AND performance.activity_id = 1663
+          AND schools_users.school_id IN (38811,38804,38801,38800,38779,38784,38780,38773,38765,38764)
+          
+
+              GROUP BY skill_group_name,
+        aggregate_id,
+        students.name,
+        student_id,
+        performance.pre_questions_correct,
+        performance.pre_questions_total,
+        performance.post_questions_correct,
+        performance.post_questions_total
+
+                ORDER BY TRIM(SUBSTR(TRIM(student_name), STRPOS(student_name, ' ') + 1)), skill_group_name
+
+                LIMIT 5000
+
+)
+                SELECT
+          student_id, student_name,
+          NULL as aggregate_id,
+          'ROLLUP' AS name,
+          group_by,
+          'ROLLUP' AS skill_group_name, SUM(pre_to_post_improved_skill_count) AS pre_to_post_improved_skill_count, SUM(pre_questions_correct) AS pre_questions_correct, SUM(pre_questions_total) AS pre_questions_total, AVG(pre_questions_percentage) AS pre_questions_percentage, SUM(post_questions_correct) AS post_questions_correct, SUM(post_questions_total) AS post_questions_total, AVG(post_questions_percentage) AS post_questions_percentage, SUM(total_skills) AS total_skills, SUM(pre_skills_proficient) AS pre_skills_proficient, SUM(pre_skills_to_practice) AS pre_skills_to_practice, SUM(post_skills_to_practice) AS post_skills_to_practice, SUM(post_skills_improved) AS post_skills_improved, SUM(post_skills_maintained) AS post_skills_maintained, SUM(post_skills_improved_or_maintained) AS post_skills_improved_or_maintained
+        FROM aggregate_rows
+        GROUP BY student_id, student_name, group_by
+        UNION ALL
+        SELECT *
+          FROM aggregate_rows
+
