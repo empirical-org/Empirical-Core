1a9215b4071d62d68f898b27182556d2f3ca4a86
Thomas Robertson <thomas.robertson@quill.org>
2024-03-13 11:49:31 -0400

    Move comment out of SQL per PR feedback, add performance benchmark

--- services/QuillLMS/app/queries/admin_diagnostic_reports/post_diagnostic_completed_view_query.rb
+++ services/QuillLMS/app/queries/admin_diagnostic_reports/post_diagnostic_completed_view_query.rb
@@ -18,16 +18,14 @@ module AdminDiagnosticReports
     end
 
     def specific_select_clause
+      # The GREATEST value below is used to duplicate growth percentage calculations from teacher reports:
+      #   It is intended to work only when aggregated at the Skill Group level
+      #   The ROUND(..., 2) statements ensure that we aggregate at the same level of rounding as the teacher report uses (that report rounds all scores to integer percentages) which lets us avoid being off by 1% from the teacher reports because of higher precision
+      #   The CASE statements ensure that we don't calculate the overall pre-Diagnostic performance, but rather calculate it only the performance for students who have also completed the post-Diagnostic
+      #   NULL values are ignored when executing aggregate queries such as SUM, so rows from `performance` that have NULL "post" data will be excluded from the aggregation
+      #   We subtract the pre-Diagnostic average from the post-Diagnostic average, and then use GREATEST to treat cases where post-Diagnostic scores are worse as if they were equal instead
       <<-SQL
         COUNT(DISTINCT performance.post_activity_session_id) AS post_students_completed,
-        /*
-        The value below is used to duplicate growth percentage calculations from teacher reports:
-          It is intended to work only when aggregated at the Skill Group level
-          The ROUND(..., 2) statements ensure that we aggregate at the same level of rounding as the teacher report uses (that report rounds all scores to integer percentages) which lets us avoid being off by 1% from the teacher reports because of higher precision
-          The CASE statements ensure that we don't calculate the overall pre-Diagnostic performance, but rather calculate it only the performance for students who have also completed the post-Diagnostic
-          NULL values are ignored when executing aggregate queries such as SUM, so rows from `performance` that have NULL "post" data will be excluded from the aggregation
-          We subtract the pre-Diagnostic average from the post-Diagnostic average, and then use GREATEST to treat cases where post-Diagnostic scores are worse as if they were equal instead
-        */
         GREATEST(
          ROUND(SAFE_DIVIDE(SUM(performance.post_questions_correct), CAST(SUM(performance.post_questions_total) AS float64)), 2)
             - ROUND(SAFE_DIVIDE(SUM(CASE WHEN performance.post_activity_session_id IS NOT NULL THEN performance.pre_questions_correct ELSE NULL END),
