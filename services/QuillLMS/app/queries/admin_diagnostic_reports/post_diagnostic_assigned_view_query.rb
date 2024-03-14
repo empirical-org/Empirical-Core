8ebd6e59e65b8370dc7fa046a0c92d789edf4877
Thomas Robertson <thomas.robertson@quill.org>
2024-03-14 18:19:08 -0400

    Replace Post Assigned query with one built on a materialized view
    Note: benchmarks included suggest no speed improvements, but a cost savings

---
+++ services/QuillLMS/app/queries/admin_diagnostic_reports/post_diagnostic_assigned_view_query.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+module AdminDiagnosticReports
+  class PostDiagnosticAssignedViewQuery < DiagnosticAggregateViewQuery
+    def specific_select_clause
+      <<-SQL
+          COUNT(post_assigned_at) AS post_students_assigned
+      SQL
+    end
+
+    def relevant_date_column
+      "performance.post_assigned_at"
+    end
+
+    private def rollup_aggregation_hash
+      {
+        post_students_assigned: sum_aggregate
+      }
+    end
+  end
+end
