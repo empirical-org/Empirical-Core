c72fd7b16b11849231d625a62ed996576b8600fc
Thomas Robertson <thomas.robertson@quill.org>
2024-03-18 14:55:39 -0400

    Comment out more of the spec since no tests run

--- services/QuillLMS/spec/queries/admin_diagnostic_reports/diagnostic_performance_by_student_view_query_spec.rb
+++ services/QuillLMS/spec/queries/admin_diagnostic_reports/diagnostic_performance_by_student_view_query_spec.rb
@@ -7,125 +7,125 @@ module AdminDiagnosticReports
     include_context 'Admin Diagnostic Aggregate CTE'
     include_context 'Pre Post Diagnostic Skill Group Performance View'
 
-    context 'big_query_snapshot', :big_query_snapshot do
-      # Some of our tests include activity_sessions having NULL in its timestamps so we need a version that has timestamps with datetime data in them so that WITH in the CTE understands the data type expected
-      let(:reference_activity_session) { create(:activity_session, :finished) }
-      # Some of our tests expect no concept_results to exist, but BigQuery needs to know what one is shaped like in order to build temporary queries
-      let(:reference_concept_results) { create(:concept_result, extra_metadata: {question_uid: SecureRandom.uuid}) }
+    # TODO: Some of these specs fail intermittently.  In the interest of getting the code out the door, we're commenting them out, but we should come back and fix that soon
+    #context 'big_query_snapshot', :big_query_snapshot do
+    #  # Some of our tests include activity_sessions having NULL in its timestamps so we need a version that has timestamps with datetime data in them so that WITH in the CTE understands the data type expected
+    #  let(:reference_activity_session) { create(:activity_session, :finished) }
+    #  # Some of our tests expect no concept_results to exist, but BigQuery needs to know what one is shaped like in order to build temporary queries
+    #  let(:reference_concept_results) { create(:concept_result, extra_metadata: {question_uid: SecureRandom.uuid}) }
 
-      let(:query_args) do
-        {
-          timeframe_start: timeframe_start,
-          timeframe_end: timeframe_end,
-          school_ids: school_ids,
-          grades: grades,
-          teacher_ids: teacher_ids,
-          classroom_ids: classroom_ids,
-          diagnostic_id: pre_diagnostic.id
-        }
-      end
+    #  let(:query_args) do
+    #    {
+    #      timeframe_start: timeframe_start,
+    #      timeframe_end: timeframe_end,
+    #      school_ids: school_ids,
+    #      grades: grades,
+    #      teacher_ids: teacher_ids,
+    #      classroom_ids: classroom_ids,
+    #      diagnostic_id: pre_diagnostic.id
+    #    }
+    #  end
 
-      let(:cte_records) do
-        [
-          classrooms,
-          teachers,
-          classrooms_teachers,
-          schools,
-          schools_users,
-          view_records,
-          reference_activity_session,
-          reference_concept_results
-        ]
-      end
+    #  let(:cte_records) do
+    #    [
+    #      classrooms,
+    #      teachers,
+    #      classrooms_teachers,
+    #      schools,
+    #      schools_users,
+    #      view_records,
+    #      reference_activity_session,
+    #      reference_concept_results
+    #    ]
+    #  end
 
-      # TODO: Some of these specs fail intermittently.  In the interest of getting the code out the door, we're commenting them out, but we should come back and fix that soon
-      #context 'base case: students finished both pre and post' do
-      #  it { expect(results.map{|r| r[:student_id]}).to include(*students.map(&:id)) }
-      #  it { expect(results.map{|r| r[:pre_questions_total]}).to eq([1] * students.length) }
-      #  it { expect(results.map{|r| r[:post_questions_total]}).to eq([1] * students.length) }
-      #end
+    #  context 'base case: students finished both pre and post' do
+    #    it { expect(results.map{|r| r[:student_id]}).to include(*students.map(&:id)) }
+    #    it { expect(results.map{|r| r[:pre_questions_total]}).to eq([1] * students.length) }
+    #    it { expect(results.map{|r| r[:post_questions_total]}).to eq([1] * students.length) }
+    #  end
 
-      #context 'no students assigned pre-diagnostics' do
-      #  let(:pre_diagnostic_assigned_students) { [] }
+    #  context 'no students assigned pre-diagnostics' do
+    #    let(:pre_diagnostic_assigned_students) { [] }
 
-      #  it { expect(results).to eq([]) }
-      #end
+    #    it { expect(results).to eq([]) }
+    #  end
 
-      #context 'students who are assigned to a classroom unit, but have no completions, show up in the data set' do
-      #  let(:pre_diagnostic_activity_sessions) { [] }
+    #  context 'students who are assigned to a classroom unit, but have no completions, show up in the data set' do
+    #    let(:pre_diagnostic_activity_sessions) { [] }
 
-      #  it { expect(results.map{|r| r[:student_id]}).to include(*students.map(&:id)) }
-      #  it { expect(results.map{|r| r[:pre_questions_total]}).to eq([0] * students.length) }
-      #end
+    #    it { expect(results.map{|r| r[:student_id]}).to include(*students.map(&:id)) }
+    #    it { expect(results.map{|r| r[:pre_questions_total]}).to eq([0] * students.length) }
+    #  end
 
-      #context 'students who have completed a pre but not a post activity show up in the data' do
-      #  let(:post_diagnostic_activity_sessions) { [] }
+    #  context 'students who have completed a pre but not a post activity show up in the data' do
+    #    let(:post_diagnostic_activity_sessions) { [] }
 
-      #  it { expect(results.map{|r| r[:student_id]}).to include(*students.map(&:id)) }
-      #  it { expect(results.map{|r| r[:pre_questions_total]}).to eq([1] * students.length) }
-      #  it { expect(results.map{|r| r[:post_questions_total]}).to eq([nil] * students.length) }
-      #end
+    #    it { expect(results.map{|r| r[:student_id]}).to include(*students.map(&:id)) }
+    #    it { expect(results.map{|r| r[:pre_questions_total]}).to eq([1] * students.length) }
+    #    it { expect(results.map{|r| r[:post_questions_total]}).to eq([nil] * students.length) }
+    #  end
 
-      #context 'student completed two sets of diagnostics in the same classroom' do
-      #  let(:classrooms) { [create(:classroom)] }
-      #  let(:pre_diagnostic_classroom_units) { pre_diagnostic_units.map.with_index { |unit, i| create(:classroom_unit, classroom: classrooms.first, unit: unit, assigned_student_ids: pre_diagnostic_assigned_students.map(&:id)) } }
+    #  context 'student completed two sets of diagnostics in the same classroom' do
+    #    let(:classrooms) { [create(:classroom)] }
+    #    let(:pre_diagnostic_classroom_units) { pre_diagnostic_units.map.with_index { |unit, i| create(:classroom_unit, classroom: classrooms.first, unit: unit, assigned_student_ids: pre_diagnostic_assigned_students.map(&:id)) } }
 
-      #  it { expect(results.map{|r| r[:student_id]}).to include(*students.map(&:id)) }
+    #    it { expect(results.map{|r| r[:student_id]}).to include(*students.map(&:id)) }
 
-      #  it { expect(results.map{|r| r[:pre_questions_total]}).to eq([1] * students.length) }
+    #    it { expect(results.map{|r| r[:pre_questions_total]}).to eq([1] * students.length) }
 
-      #  it { expect(results.map{|r| r[:post_questions_total]}).to eq([1] * students.length) }
-      #end
+    #    it { expect(results.map{|r| r[:post_questions_total]}).to eq([1] * students.length) }
+    #  end
 
-      #context 'student completed two sets of diagnostics in different classrooms' do
-      #  let(:classroom_count) { 2 }
+    #  context 'student completed two sets of diagnostics in different classrooms' do
+    #    let(:classroom_count) { 2 }
 
-      #  it { expect(results.length).to eq(students.length * classroom_count) }
-      #  it { expect(results.map{|r| r[:pre_questions_total]}).to eq([1] * students.length * 2) }
-      #  it { expect(results.map{|r| r[:post_questions_total]}).to eq([1] * students.length * 2) }
+    #    it { expect(results.length).to eq(students.length * classroom_count) }
+    #    it { expect(results.map{|r| r[:pre_questions_total]}).to eq([1] * students.length * 2) }
+    #    it { expect(results.map{|r| r[:post_questions_total]}).to eq([1] * students.length * 2) }
 
-      #end
+    #  end
 
-      #context 'sorting' do
-      #  let(:student_names) do
-      #    [
-      #      "John Smith",
-      #      "John Baker",
-      #      "John Thompson"
-      #    ]
-      #  end
-      #  let(:sorted_student_names) { student_names.sort_by { |name| name.split(" ", 2).last } }
-      #  let(:students) { student_names.map { |name| create(:student, name: name) } }
+    #  context 'sorting' do
+    #    let(:student_names) do
+    #      [
+    #        "John Smith",
+    #        "John Baker",
+    #        "John Thompson"
+    #      ]
+    #    end
+    #    let(:sorted_student_names) { student_names.sort_by { |name| name.split(" ", 2).last } }
+    #    let(:students) { student_names.map { |name| create(:student, name: name) } }
 
-      #  it { expect(results.map{|r| r[:student_name]}).to eq(sorted_student_names) }
+    #    it { expect(results.map{|r| r[:student_name]}).to eq(sorted_student_names) }
 
-      #  context 'with same last name' do
-      #    let(:student_names) do
-      #      [
-      #        "Bethany Smith",
-      #        "Alex Smith",
-      #        "Zachary Smith"
-      #      ]
-      #    end
-      #    let(:sorted_student_names) { student_names.sort }
+    #    context 'with same last name' do
+    #      let(:student_names) do
+    #        [
+    #          "Bethany Smith",
+    #          "Alex Smith",
+    #          "Zachary Smith"
+    #        ]
+    #      end
+    #      let(:sorted_student_names) { student_names.sort }
 
-      #    it { expect(results.map{|r| r[:student_name]}).to eq(sorted_student_names) }
+    #      it { expect(results.map{|r| r[:student_name]}).to eq(sorted_student_names) }
 
-      #    context 'with identical names where at least one is repeated' do
-      #      let(:classroom_count) { 2 }
-      #      let(:students) do
-      #        # Have to specify these because username and email are constructed based on name if one is not explicitly provided
-      #        [
-      #          create(:student, name: "John Smith", username: "johnsmith1", email: "johnsmith1@example.com"),
-      #          create(:student, name: "John Smith", username: "johnsmith2", email: "johnsmith2@example.com")
-      #        ]
-      #      end
-      #      let(:expected_student_ids) { (students.map(&:id) * 2).sort }
+    #      context 'with identical names where at least one is repeated' do
+    #        let(:classroom_count) { 2 }
+    #        let(:students) do
+    #          # Have to specify these because username and email are constructed based on name if one is not explicitly provided
+    #          [
+    #            create(:student, name: "John Smith", username: "johnsmith1", email: "johnsmith1@example.com"),
+    #            create(:student, name: "John Smith", username: "johnsmith2", email: "johnsmith2@example.com")
+    #          ]
+    #        end
+    #        let(:expected_student_ids) { (students.map(&:id) * 2).sort }
 
-      #      it { expect(results.map{|r| r[:student_id]}).to eq(expected_student_ids) }
-      #    end
-      #  end
-      #end
-    end
+    #        it { expect(results.map{|r| r[:student_id]}).to eq(expected_student_ids) }
+    #      end
+    #    end
+    #  end
+    #end
   end
 end
