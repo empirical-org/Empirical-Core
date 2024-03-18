fcfaef6882369dea82d653e880c6730fbc08c25c
Brendan Shean <brendanshean@gmail.com>
2024-03-18 18:25:22 -0400

    Fix linter error

--- services/QuillLMS/db/migrate/20240318155131_add_name_to_evidence_research_gen_ai_passage.evidence.rb
+++ services/QuillLMS/db/migrate/20240318155131_add_name_to_evidence_research_gen_ai_passage.evidence.rb
@@ -3,6 +3,8 @@
 # This migration comes from evidence (originally 20240318155008)
 class AddNameToEvidenceResearchGenAiPassage < ActiveRecord::Migration[7.0]
   def change
+    # rubocop:disable Rails/NotNullColumn
     add_column :evidence_research_gen_ai_passages, :name, :string, null: false
+    # rubocop:enable Rails/NotNullColumn
   end
 end
--- services/QuillLMS/engines/evidence/db/migrate/20240318155008_add_name_to_evidence_research_gen_ai_passage.rb
+++ services/QuillLMS/engines/evidence/db/migrate/20240318155008_add_name_to_evidence_research_gen_ai_passage.rb
@@ -2,6 +2,8 @@
 
 class AddNameToEvidenceResearchGenAiPassage < ActiveRecord::Migration[7.0]
   def change
+    # rubocop:disable Rails/NotNullColumn
     add_column :evidence_research_gen_ai_passages, :name, :string, null: false
+    # rubocop:enable Rails/NotNullColumn
   end
 end
