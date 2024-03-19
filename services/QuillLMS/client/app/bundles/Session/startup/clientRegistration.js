5bd7b10b2fd17a0990caac78bf1ce1f26f8aa57b
Cissy Yu <cissy@quill.org>
2024-03-19 13:20:08 +0800

    add premium footer to sign up/new teacher flow

--- services/QuillLMS/client/app/bundles/Session/startup/clientRegistration.js
+++ services/QuillLMS/client/app/bundles/Session/startup/clientRegistration.js
@@ -1,7 +1,11 @@
 import 'lazysizes';
 import 'lazysizes/plugins/parent-fit/ls.parent-fit';
 import ReactOnRails from 'react-on-rails';
+
 import FinishSetUpApp from './FinishSetUpAppClient';
 import NewAccountApp from './NewAccountAppClient.jsx';
 
-ReactOnRails.register({ NewAccountApp, FinishSetUpApp, });
+import PremiumFooterApp from '../../Footer/startup/PremiumFooterAppClient.jsx';
+
+
+ReactOnRails.register({ NewAccountApp, FinishSetUpApp, PremiumFooterApp});
