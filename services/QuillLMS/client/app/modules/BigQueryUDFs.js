e043d59e383ed30fb13d5371342bd60b8f7da962
Peter Kong <pkong@quill.org>
2024-03-25 10:18:52 -0600

    add correct recommendedActivities lookup; dedupe interDiagnosticActivities

--- services/QuillLMS/client/app/modules/BigQueryUDFs.js
+++ services/QuillLMS/client/app/modules/BigQueryUDFs.js
@@ -56,8 +56,6 @@ export function studentwiseSkillGroupUDF(elements) {
     }
   }
 
-
-
   function findLastIndex(array, startIndex, fn) {
     const reversedArray = [...array.slice(startIndex)].reverse()
     const reversedIdx = reversedArray.findIndex(fn)
@@ -143,12 +141,12 @@ export function studentwiseSkillGroupUDF(elements) {
   }
 
   const recommendedActivities = {
-    1161: [147, 148, 149, 150, 151],  // ELL Starter
-    1568: [250, 251, 252, 253, 254, 255],  // ELL Intermediate
-    1590: [258, 259, 260, 261, 262],  // ELL Advanced
-    1663: [306, 263, 307, 264, 308, 265, 266, 267, 268, 363], // Starter
-    1668: [309, 287, 310, 288, 311, 289, 290, 291, 292, 293, 294], // Intermediate
-    1678: [312, 275, 276, 313, 314, 277, 278, 279, 362]   // Advanced
+    1161: [1096, 1097, 1098, 1099, 1100, 1101, 1137, 1102, 1103, 1104, 1105, 1144, 1106, 1107, 1108, 1109, 1110, 1111, 1112, 1113, 1153, 1114, 1152, 1136, 1145, 1154, 1151, 1138, 1115, 1117, 1116, 1118, 1119, 1120, 1121, 1122, 1123, 1124, 1125, 1126, 1127, 1128, 1129, 1130, 1131, 1132, 1133, 1134, 1135, 1156, 1157, 1158, 1159, 1160, 1139, 1155, 1142, 1140, 1141, 1143, 1148, 1146, 1149, 1147, 1150],  // ELL Starter
+    1568: [1541, 1543, 1546, 1544, 1545, 1550, 1547, 1551, 1552, 1553, 1554, 1555, 1557, 1558, 1548, 1559, 1560, 1561, 1563, 1567, 1569, 1562, 1564, 1571, 1570, 1573, 1549],  // ELL Intermediate
+    1590: [1575, 1576, 1577, 1578, 1579, 1580, 1581, 1582, 1583, 1584, 1585, 1586, 1587, 1591, 1588, 1625, 1589, 1626, 1627, 1628, 1629, 1654, 1657, 1655, 1658, 1660, 1662, 1661],  // ELL Advanced
+    1663: [802, 181, 804, 885, 801, 887, 886, 803, 283, 1440, 1308, 808, 431, 301, 438, 775, 844, 843, 124, 713, 1407, 717, 1418, 1409, 599, 712, 600, 846, 435, 436, 434, 437, 837, 433, 113, 111, 107, 112, 1054, 742, 2506], // Starter
+    1668: [435, 436, 434, 837, 1005, 424, 426, 428, 429, 430, 776, 417, 418, 1221, 2502, 2498, 2500, 2496, 2497, 2501, 2499, 755, 759, 851, 863, 757, 985, 986, 861, 993, 752, 754, 841, 2495, 887, 886, 840, 770, 769, 774, 772, 896, 1486, 1452, 1487, 1488, 848, 1308, 737, 1425, 1345, 2245], // Intermediate
+    1678: [653, 862, 868, 869, 1211, 1220, 1213, 1212, 1214, 594, 595, 596, 1049, 598, 1002, 443, 450, 1237, 876, 878, 752, 754, 1235, 1414, 1283, 1281, 1223, 1441]   // Advanced
   }
 
   const zipped = elements.map(parseElement).sort((a,b) => (new Date(a.completedAt) - new Date(b.completedAt)))
@@ -208,9 +206,11 @@ export function studentwiseSkillGroupUDF(elements) {
     {}
   )
   const interDiagnosticActivities = zipped.slice(canonicalPreTestIdx + 1, canonicalPostTestIdx)
-  const numAssignedRecommendedCompleted = interDiagnosticActivities.filter(
+  const nonUniques = interDiagnosticActivities.filter(
     elem => recommendedActivities[PRE_DIAGNOSTIC_ACTIVITY_ID].includes(elem.activityId)
-  ).length
+  )
+  // project the object array to a primitive array, so we can use Set to compute uniques-by-activity
+  const numAssignedRecommendedCompleted = [... new Set(nonUniques.map(x => x.activityId))].length
 
   return JSON.stringify(
     {
@@ -258,7 +258,7 @@ export function tierUDF(numAssignedRecommendedCompleted, recommendedActivityCoun
     }
   }
 
-  return "-1"
+  return `${percentage}`
 }
 
 
