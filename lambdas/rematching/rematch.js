63837a470108c3d04e08215209ea29c36565d9f3
Emilia Friedberg <emiliafriedberg@gmail.com>
2024-04-02 12:25:19 -0400

    maybe now

--- lambdas/rematching/rematch.js
+++ lambdas/rematching/rematch.js
@@ -1,7 +1,8 @@
+import { checkSentenceCombining, checkSentenceFragment, checkDiagnosticQuestion, checkFillInTheBlankQuestion, checkDiagnosticSentenceFragment, checkGrammarQuestion, ConceptResult, } from '../../services/QuillLMS/client/app/bundles/Shared/quill-marking-logic/src/main'
+
 const _ = require('lodash');
 const u = require('underscore');
 const Sequelize = require('sequelize');
-const { checkSentenceCombining, checkSentenceFragment, checkDiagnosticQuestion, checkFillInTheBlankQuestion, checkDiagnosticSentenceFragment, checkGrammarQuestion, ConceptResult, } = require('../../services/QuillLMS/client/app/bundles/Shared/quill-marking-logic/src/main.ts')
 const CMS_URL = 'https://cms.quill.org'
 const FIREBASE_NAME = 'quillconnect'
 
