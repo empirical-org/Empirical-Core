3bfebcc2a25301153be13162af3752c74b2b7ef1
Cissy Yu <cissy@quill.org>
2024-04-02 15:37:06 +0800

    index log

--- services/QuillLMS/client/app/bundles/Teacher/components/assignment_flow/create_unit/custom_activity_pack/index.tsx
+++ services/QuillLMS/client/app/bundles/Teacher/components/assignment_flow/create_unit/custom_activity_pack/index.tsx
@@ -346,7 +346,9 @@ const CustomActivityPack = ({
     topics,
   }
 
+  console.log(selectedActivities)
   const selectedActivitiesFilteredByFlag =  isStaff && !flagFilters.length ? [] : selectedActivities.filter(a => filterByFlag(flagFilters, a))
+
 
   return (
     <div className="custom-activity-pack-page">
