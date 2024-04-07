1ed76d502055bf6089bcabb19788dc6ce42a7147
Brendan Shean <brendanshean@gmail.com>
2024-04-07 13:18:17 -0400

    Update experiment_controller spec

--- services/QuillLMS/engines/evidence/spec/controllers/evidence/research/gen_ai/experiments_controller_spec.rb
+++ services/QuillLMS/engines/evidence/spec/controllers/evidence/research/gen_ai/experiments_controller_spec.rb
@@ -14,20 +14,31 @@ module Evidence
         end
 
         describe 'POST #create' do
-          let(:llm_prompt_template_id) { create(:evidence_research_gen_ai_llm_prompt_template).id }
-          let(:passage_prompt_id) { create(:evidence_research_gen_ai_passage_prompt).id }
-          let(:llm_config_id) { create(:evidence_research_gen_ai_llm_config).id }
+          let(:llm_configs) { create_list(:evidence_research_gen_ai_llm_config, num_llm_configs) }
+          let(:llm_config_ids) { llm_configs.map(&:id) }
+          let(:num_llm_configs) { rand(1..3) }
+
+          let(:llm_prompt_templates) { create_list(:evidence_research_gen_ai_llm_prompt_template, num_llm_prompt_templates) }
+          let(:llm_prompt_template_ids) { llm_prompt_templates.map(&:id) }
+          let(:num_llm_prompt_templates) { rand(1..3) }
+
+          let(:passage_prompts) { create_list(:evidence_research_gen_ai_passage_prompt, num_passage_prompts) }
+          let(:passage_prompt_ids) { passage_prompts.map(&:id) }
+          let(:num_passage_prompts) { rand(1..3) }
+
+          let(:num_examples) { rand(1..3) }
+
           let(:llm_prompt) { create(:evidence_research_gen_ai_llm_prompt) }
 
           subject do
-            post :create,
-              params: {
-                research_gen_ai_experiment: {
-                  llm_config_id:,
-                  llm_prompt_template_id:,
-                  passage_prompt_id:
-                }
+            post :create, params: {
+              research_gen_ai_experiment: {
+                llm_config_ids:,
+                llm_prompt_template_ids:,
+                passage_prompt_ids:,
+                num_examples:
               }
+            }
           end
 
           context 'as not staff' do
@@ -40,31 +51,28 @@ module Evidence
           context 'as staff' do
             let(:is_staff) { true }
 
-            before do
-              allow(LLMPromptBuilder)
-                .to receive(:run)
-                .with(llm_prompt_template_id:, passage_prompt_id:)
-                .and_return(llm_prompt)
-            end
+            before { allow(LLMPromptBuilder).to receive(:run).and_return(llm_prompt) }
 
             context 'with valid parameters' do
+              let(:total_combinations) { num_llm_configs * num_llm_prompt_templates * num_passage_prompts }
+
               before { allow(RunExperimentWorker).to receive(:perform_async) }
 
-              it { expect { subject }.to change(Experiment, :count).by(1)}
+              it { expect { subject }.to change(Experiment, :count).by(total_combinations) }
 
-              it 'enqueues a worker and redirects to the newly created experiment' do
-                expect(RunExperimentWorker).to receive(:perform_async)
+              it 'enqueues a worker for each combination and redirects' do
+                expect(RunExperimentWorker).to receive(:perform_async).exactly(total_combinations).times
                 subject
-                expect(response).to redirect_to(Experiment.last)
+                expect(response).to redirect_to(research_gen_ai_experiments_path)
               end
             end
 
             context 'with invalid parameters' do
-              let(:llm_config_id) { nil }
+              let(:llm_config_ids) { [nil] }
 
               it { expect { subject }.not_to change(Experiment, :count) }
 
-              it 'does not enqueue a worker and re-renders the new template' do
+              it 'does not enqueue a worker' do
                 expect(RunExperimentWorker).not_to receive(:perform_async)
                 subject
               end
