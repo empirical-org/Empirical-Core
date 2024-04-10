156c3f269206271d30ad5c9c4454edd9d03b32cc
cissyxyu <57366100+cissyxyu@users.noreply.github.com>
2024-04-10 13:42:41 +0800

    Show custom Max Attempts Feedback for Connect fill in blank questions  (#11704)
    * add fill in blank max attempt feedback
    
    * change to shared function
    
    * change to component rather than function
    
    * add final attempt snapshot
    
    * linting

---
+++ services/QuillLMS/client/app/bundles/Shared/components/studentLessons/finalAttemptFeedback.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import { Feedback } from '../renderForQuestions/feedback';
+
+const FinalAttemptFeedback = ({latestAttempt, correctResponse}) => {
+  const finalAttemptFeedback = `<b>Good try!</b> Compare your response to the strong response, and then go on to the next question.<br><br><b>Your response</b><br>${latestAttempt}<br><br><b>A strong response</b><br>${correctResponse}`
+  return (
+    <Feedback
+      feedback={<p dangerouslySetInnerHTML={{ __html: finalAttemptFeedback }} />}
+      feedbackType="incorrect-continue"
+    />
+  )
+};
+
+export { FinalAttemptFeedback };
