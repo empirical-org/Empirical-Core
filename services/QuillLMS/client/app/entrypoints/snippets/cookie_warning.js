95c5219d80df0b2afeeb36f4da047e8a6d488cdf
Peter Kong <pkong@quill.org>
2024-04-10 14:54:30 -0600

    Revert "add entrypoint subdir"
    This reverts commit 28040081a3a6f0c2cda6ea97f3f53f0abaa75c9e.

--- services/QuillLMS/client/app/entrypoints/snippets/cookie_warning.js
+++
@@ -1,5 +0,0 @@
-if(!navigator.cookieEnabled) {
-  alert('You seem to have disabled your browser\'s cookies. To use Quill, you must enable them.')
-}
-
-console.log("~~~~~~~~~~~~~~~~~~this is the cookie warning script")
