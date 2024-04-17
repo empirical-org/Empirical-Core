dbb1bbb78127ee307f65f99d40d717e98bbe3632
Daniel Drabik <dan.drabik@gmail.com>
2024-04-17 14:10:37 -0400

    Revert change to fix test.

--- services/QuillLMS/spec/factories/standards.rb
+++ services/QuillLMS/spec/factories/standards.rb
@@ -22,7 +22,7 @@ FactoryBot.define do
   factory :standard do
     sequence(:name) { |n| "Standard #{n}" }
     standard_level
-    standard_category
+    standard_category { StandardCategory.first || create(:standard_category) }
 
     trait :with_change_log do
       after(:create) do |t|
