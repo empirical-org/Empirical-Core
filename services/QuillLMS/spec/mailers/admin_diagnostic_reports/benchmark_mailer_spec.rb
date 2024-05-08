c3498241fc07c7e75c543c59286035493bda5a03
Thomas Robertson <thomas.robertson@quill.org>
2024-05-08 13:07:02 -0400

    Update spec to stub ENV const

--- services/QuillLMS/spec/mailers/admin_diagnostic_reports/benchmark_mailer_spec.rb
+++ services/QuillLMS/spec/mailers/admin_diagnostic_reports/benchmark_mailer_spec.rb
@@ -4,11 +4,8 @@ require 'rails_helper'
 
 module AdminDiagnosticReports
   describe BenchmarkMailer, type: :mailer do
-    before do
-      allow_any_instance_of(ActionView::Base).to receive(:vite_stylesheet_tag)
-    end
-
     let(:email) { 'address@example.com' }
+    let(:recipient_email) { 'recipient@example.com' }
     let(:query_name) { 'sample_query' }
     let(:elapsed_time) { 1.1 }
     let(:report_text) { "#{query_name} - #{elapsed_time} seconds" }
@@ -21,8 +18,13 @@ module AdminDiagnosticReports
     end
     let(:mail) { described_class.benchmark_report(data) }
 
+    before do
+      allow_any_instance_of(ActionView::Base).to receive(:vite_stylesheet_tag)
+      stub_const("AdminDiagnosticReports::BenchmarkMailer::RECIPIENT_EMAIL", recipient_email)
+    end
+
     it { expect(mail.subject).to eq('Admin Diagnostic Report Query Runtimes') }
-    it { expect(mail.to).to eq([described_class::RECIPIENT_EMAIL]) }
+    it { expect(mail.to).to eq([recipient_email]) }
     it { expect(mail.from).to eq(['hello@quill.org']) }
     it { expect(mail.body).to include(report_text) }
   end
