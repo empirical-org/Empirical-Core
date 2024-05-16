c9a0f273695e814bca0e57e4814151c36f98d232
Brendan Shean <brendanshean@gmail.com>
2024-05-16 08:01:52 -0400

    Remove old spec

--- services/QuillLMS/engines/evidence/spec/services/evidence/research/gen_ai/malformed_json_fixer_spec.rb
+++ services/QuillLMS/engines/evidence/spec/services/evidence/research/gen_ai/malformed_json_fixer_spec.rb
@@ -91,37 +91,3 @@ module Evidence
     end
   end
 end
-
-
-
-
-
-
-
-
-
-# # frozen_string_literal: true
-
-# require 'rails_helper'
-
-# module Evidence
-#   module Research
-#     module GenAI
-#       RSpec.describe MissingJSONQuoteFixer do
-#         subject { described_class.run(raw_text:) }
-
-#         let(:fixed_text) { '{"key1":"value1","key2":"value2"}' }
-
-#         context 'missing opening quote' do
-#           context 'on first key' do
-#             let(:raw_text) { '{key1":"value1","key2":"value2"}' }
-
-#             it { is_expected.to eq fixed_text }
-#           end
-
-
-
-
-#     end
-#   end
-# end
