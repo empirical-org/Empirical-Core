0612b82df67bc1168ce116a17f302400a773b1b4
Eric Adams <erictayloradams@gmail.com>
2024-05-20 18:52:19 -0400

    add styling for containers

--- services/QuillLMS/client/app/bundles/Staff/components/styleGuide/checkBoxes.tsx
+++ services/QuillLMS/client/app/bundles/Staff/components/styleGuide/checkBoxes.tsx
@@ -1,25 +1,34 @@
 import * as React from 'react';
+import _ from 'lodash';
 
 const STATES = ['inactive', 'active', 'indeterminate', 'disabled']
+const MODES = ['On Light', 'On Dark']
 
 const Checkboxes = () => {
   return(
     <div id="checkboxes">
       <h2 className="style-guide-h2">Checkboxes</h2>
-      <div className="States-container">
+      <div className="states-and-modes-container">
         <h4 className="style-guide-h4">States</h4>
-        <div className="States-options-container">
+        <div className="options-container">
           {STATES.map(state => {
             return(
-              <div className="state">
-                <p className="state-label">{state}</p>
+              <div className="option-container">
+                <p className="option-label">{_.capitalize(state)}</p>
               </div>
             )
           })}
         </div>
-      </div>
-      <div className="modes-container">
         <h4 className="style-guide-h4">Modes</h4>
+        <div className="options-container">
+          {MODES.map(mode => {
+            return (
+              <div className="option-container">
+                <p className="option-label">{mode}</p>
+              </div>
+            )
+          })}
+        </div>
       </div>
     </div>
   )
