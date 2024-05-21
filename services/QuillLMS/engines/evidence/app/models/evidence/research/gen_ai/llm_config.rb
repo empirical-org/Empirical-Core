525cdc03c7dfffc7112ff3e694b20a09e602c78d
Brendan Shean <brendanshean@gmail.com>
2024-05-21 08:58:57 -0400

    Add in JSON mode for open ai as well as gpt-4o config

--- services/QuillLMS/engines/evidence/app/models/evidence/research/gen_ai/llm_config.rb
+++ services/QuillLMS/engines/evidence/app/models/evidence/research/gen_ai/llm_config.rb
@@ -24,17 +24,23 @@ module Evidence
           GEMINI_1_5_FLASH_LATEST = 'gemini-1-5-flash-latest'
         ].freeze
 
-        JSON_FORMAT_RESPONSES = { "generationConfig": { "response_mime_type": "application/json" } }.freeze
+        GOOGLE_JSON_FORMAT_RESPONSES = { "generationConfig": { "response_mime_type": "application/json" } }.freeze
 
         OPEN_AI = 'open_ai'
+
+        OPEN_AI_VERSIONS = [
+          GPT_3_5_TURBO_0125 = 'gpt-3.5-turbo-0125',
+          GPT_4_TURBO_2024_04_09 = 'gpt-4-turbo-2024-04-09',
+          GPT_4_O = 'gpt-4o'
+        ]
+
+        OPEN_AI_JSON_FORMAT_RESPONSES = { "response_format": {"type": "json_object"} }.freeze
 
         VENDOR_MAP = {
           GOOGLE => Evidence::Gemini::Completion,
           OPEN_AI => Evidence::OpenAI::Completion
         }.freeze
 
-        VERSIONS_MAP = {
-        }
 
         validates :vendor, presence: true
         validates :version, presence: true
@@ -43,11 +49,14 @@ module Evidence
 
         def llm_client = VENDOR_MAP.fetch(vendor) { raise UnsupportedVendorError }
 
-        def request_body_customizations
-          return {} unless vendor == GOOGLE
-          return {} unless version.in? [GEMINI_1_5_PRO_LATEST, GEMINI_1_5_FLASH_LATEST]
+        def google? = vendor == GOOGLE
+        def open_ai? = vendor == OPEN_AI
 
-          JSON_FORMAT_RESPONSES
+        def request_body_customizations
+          return OPEN_AI_JSON_FORMAT_RESPONSES if open_ai? && version.in?([GPT_3_5_TURBO_0125, GPT_4_TURBO_2024_04_09, GPT_4_O])
+          return GOOGLE_JSON_FORMAT_RESPONSES if google? && version.in?([GEMINI_1_5_PRO_LATEST, GEMINI_1_5_FLASH_LATEST])
+
+          {}
         end
 
         def to_s = "#{vendor}: #{version}"
