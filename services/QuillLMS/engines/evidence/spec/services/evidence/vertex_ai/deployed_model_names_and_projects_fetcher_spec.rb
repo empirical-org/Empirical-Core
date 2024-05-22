9d1b09e7b40732e36bfc3d1b1fbdd7ff5dacdae1
Brendan Shean <brendanshean@gmail.com>
2024-05-22 13:01:17 -0400

    Change deployed_model_names to deployed_model_names_and_projects_fetcher

--- services/QuillLMS/engines/evidence/spec/services/evidence/vertex_ai/deployed_model_names_and_projects_fetcher_spec.rb
+++ services/QuillLMS/engines/evidence/spec/services/evidence/vertex_ai/deployed_model_names_and_projects_fetcher_spec.rb
@@ -4,13 +4,13 @@ require 'rails_helper'
 
 module Evidence
   module VertexAI
-    RSpec.describe DeployedModelNamesFetcher do
+    RSpec.describe DeployedModelNamesAndProjectsFetcher do
       subject { described_class.run }
 
       let(:vertex_ai_project_id) { 'your_project_id' }
       let(:vertex_ai_location) { 'your_location' }
-      let(:client_class) { ::Google::Cloud::AIPlatform::V1::EndpointService::Client }
-      let(:client_instance) { instance_double(client_class, list_endpoints: list_endpoints) }
+      let(:client_class) { described_class::ENDPOINT_CLIENT_CLASS }
+      let(:client_instance) { instance_double(client_class, list_endpoints:) }
 
       let(:name1) { 'Good Model' }
       let(:name2) { 'Better Model' }
@@ -21,15 +21,15 @@ module Evidence
       let(:endpoint3) { double(display_name: name3, deployed_models: [double(display_name: name3)]) }
 
       before do
-        stub_const('Evidence::VertexAI::DeployedModelNamesFetcher::VERTEX_AI_PROJECT_ID', vertex_ai_project_id)
-        stub_const('Evidence::VertexAI::DeployedModelNamesFetcher::VERTEX_AI_LOCATION', vertex_ai_location)
-        allow(::Google::Cloud::AIPlatform::V1::EndpointService::Client).to receive(:new).and_return(client_instance)
+        create(:evidence_automl_model, project: vertex_ai_project_id)
+        stub_const('VERTEX_AI_LOCATION', vertex_ai_location)
+        allow(client_class).to receive(:new).and_return(client_instance)
       end
 
       context 'matching deployed models found' do
         let(:list_endpoints) { [endpoint1, endpoint2, endpoint3] }
 
-        it { is_expected.to eq [name3, name1] }
+        it { is_expected.to eq [{name3 => vertex_ai_project_id}, {name1 => vertex_ai_project_id}] }
       end
 
       context 'no matching deployed models found' do
