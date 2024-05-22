8979be78bfa19caf6086c7c727c97082554a1b7a
Brendan Shean <brendanshean@gmail.com>
2024-05-22 15:28:38 -0400

    Remove project_id from vertex_ai initializer

--- services/QuillLMS/engines/evidence/config/initializers/vertex_ai.rb
+++ services/QuillLMS/engines/evidence/config/initializers/vertex_ai.rb
@@ -3,11 +3,9 @@
 require 'google/cloud/ai_platform'
 require 'google/cloud/ai_platform/v1'
 
-VERTEX_AI_PROJECT_ID = ENV.fetch('VERTEX_AI_PROJECT_ID', '')
 VERTEX_AI_LOCATION = ENV.fetch('VERTEX_AI_LOCATION', '')
 VERTEX_AI_ENDPOINT = ENV.fetch('VERTEX_AI_ENDPOINT', '')
 
 ::Google::Cloud::AIPlatform.configure do |config|
-  config.credentials = JSON.parse(ENV.fetch('VERTEX_AI_CREDENTIALS', '{}'))
   config.endpoint = VERTEX_AI_ENDPOINT
 end
