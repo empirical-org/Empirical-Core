0f0523d26a79908e7355a1224224bf2e0a013923
Brendan Shean <brendanshean@gmail.com>
2024-05-23 10:14:29 -0400

    Clean up spec

--- services/QuillLMS/engines/evidence/spec/services/evidence/vertex_ai/deployed_model_names_and_projects_fetcher_spec.rb
+++ services/QuillLMS/engines/evidence/spec/services/evidence/vertex_ai/deployed_model_names_and_projects_fetcher_spec.rb
@@ -7,7 +7,7 @@ module Evidence
     RSpec.describe DeployedModelNamesAndProjectsFetcher do
       subject { described_class.run }
 
-      let(:vertex_ai_project_id) { 'your_project_id' }
+      let(:project) { 'your_project_id' }
       let(:vertex_ai_location) { 'your_location' }
       let(:client_class) { described_class::ENDPOINT_CLIENT_CLASS }
       let(:client_instance) { instance_double(client_class, list_endpoints:) }
@@ -21,7 +21,7 @@ module Evidence
       let(:endpoint3) { double(display_name: name3, deployed_models: [double(display_name: name3)]) }
 
       before do
-        create(:evidence_automl_model, project: vertex_ai_project_id)
+        create(:evidence_automl_model, project:)
         stub_const('VERTEX_AI_LOCATION', vertex_ai_location)
         allow(client_class).to receive(:new).and_return(client_instance)
       end
@@ -29,7 +29,7 @@ module Evidence
       context 'matching deployed models found' do
         let(:list_endpoints) { [endpoint1, endpoint2, endpoint3] }
 
-        it { is_expected.to eq ["#{name3},#{vertex_ai_project_id}", "#{name1},#{vertex_ai_project_id}"] }
+        it { is_expected.to eq ["#{name3},#{project}", "#{name1},#{project}"] }
       end
 
       context 'no matching deployed models found' do
