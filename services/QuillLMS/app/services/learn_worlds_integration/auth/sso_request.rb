eaf5111f5dddc18cca911f7d8b8baf8c165604ce
Peter Kong <pkong@quill.org>
2024-05-31 09:27:08 -0600

    moving things around

--- services/QuillLMS/app/services/learn_worlds_integration/auth/sso_request.rb
+++ services/QuillLMS/app/services/learn_worlds_integration/auth/sso_request.rb
@@ -1,28 +1,26 @@
 # frozen_string_literal: true
 
-module Auth
-  module LearnWorlds
-    class SSORequest < UserRequest
-      class NilEmailError < StandardError; end
-      class NilUserError < StandardError; end
+module LearnWorlds
+  class SSORequest < UserRequest
+    class NilEmailError < StandardError; end
+    class NilUserError < StandardError; end
 
-      attr_reader :user
+    attr_reader :user
 
-      delegate :email, :learn_worlds_account, :username, to: :user
+    delegate :email, :learn_worlds_account, :username, to: :user
 
-      def initialize(user)
-        @user = user
-      end
-
-      def run
-        raise NilUserError if user.nil?
-        raise NilEmailError if email.nil?
-
-        HTTParty.post(SSO_ENDPOINT, body: body, headers: headers)
-      end
-
-
-
+    def initialize(user)
+      @user = user
     end
+
+    def run
+      raise NilUserError if user.nil?
+      raise NilEmailError if email.nil?
+
+      HTTParty.post(SSO_ENDPOINT, body: body, headers: headers)
+    end
+
+
+
   end
 end
