5ba729de279f21b4d4d849a05b2cdf2cb1f905eb
Peter Kong <pkong@quill.org>
2024-06-04 14:34:03 -0400

    minor

--- services/QuillLMS/config/initializers/learn_worlds.rb
+++ services/QuillLMS/config/initializers/learn_worlds.rb
@@ -1,13 +1,13 @@
 # frozen_string_literal: true
 
 module LearnWorldsIntegration
-  BASE_URI = ENV.fetch('LEARN_WORLDS_BASE_URI', '')
-  CLIENT_ID = ENV.fetch('LEARN_WORLDS_CLIENT_ID', '')
-  ACCESS_TOKEN = ENV.fetch('LEARN_WORLDS_ACCESS_TOKEN', '')
+  BASE_URI            = ENV.fetch('LEARN_WORLDS_BASE_URI', '')
+  CLIENT_ID           = ENV.fetch('LEARN_WORLDS_CLIENT_ID', '')
+  ACCESS_TOKEN        = ENV.fetch('LEARN_WORLDS_ACCESS_TOKEN', '')
 
-  SSO_ENDPOINT = "#{BASE_URI}/admin/api/sso"
-  USER_TAGS_ENDPOINT = "#{BASE_URI}/admin/api/v2/users"
-  COURSES_ENDPOINT = "#{BASE_URI}/courses"
+  SSO_ENDPOINT        = "#{BASE_URI}/admin/api/sso"
+  USER_TAGS_ENDPOINT  = "#{BASE_URI}/admin/api/v2/users"
+  COURSES_ENDPOINT    = "#{BASE_URI}/courses"
 
-  WEBHOOK_SIGNATURE = ENV.fetch('LEARN_WORLDS_WEBHOOK_SIGNATURE', '')
+  WEBHOOK_SIGNATURE   = ENV.fetch('LEARN_WORLDS_WEBHOOK_SIGNATURE', '')
 end
