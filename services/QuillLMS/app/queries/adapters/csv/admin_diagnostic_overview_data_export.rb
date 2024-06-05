544b121a940f962cc39a0876c45b6053e89291b3
Thomas Robertson <thomas.robertson@quill.org>
2024-06-05 17:17:30 -0400

    Move column definitions into constants
    Mostly stylistic, but hopefully makes it more obvious that these are effective configurations

--- services/QuillLMS/app/queries/adapters/csv/admin_diagnostic_overview_data_export.rb
+++ services/QuillLMS/app/queries/adapters/csv/admin_diagnostic_overview_data_export.rb
@@ -3,55 +3,55 @@
 module Adapters
   module Csv
     class AdminDiagnosticOverviewDataExport < CsvDataExport
-      def self.ordered_columns
-        {
-          diagnostic_name: {
-            csv_header: 'Diagnostic Name',
-            csv_tooltip:'', # Intentionally blank
-            formatter: Formatter::ALL_CAPS
-          },
-          pre_students_assigned: {
-            csv_header: 'Students Assigned Pre Diagnostic',
-            csv_tooltip: "The total number of students assigned the Pre diagnostic.",
-            formatter: Formatter::BLANK_AS_ZERO
-          },
-          pre_students_completed: {
-            csv_header: 'Students Who Completed Pre Diagnostic',
-            csv_tooltip: "The total number of students who completed the Pre diagnostic.",
-            formatter: Formatter::BLANK_AS_ZERO
-          },
-          students_completed_practice: {
-            csv_header: 'Completed Activities',
-            csv_tooltip: "The total number of students who have completed the practice activities linked to this diagnostic.\n\nA student is counted once the student has completed at least one practice activity linked to this diagnostic.",
-            formatter: Formatter::BLANK_AS_ZERO
-          },
-          average_practice_activities_count: {
-            csv_header: 'Average Activities',
-            csv_tooltip: "Each diagnostic is linked to recommended practice activities. For the students who have completed activities, this shows the average number of completed activities  per student.\n\nThis counts the practice activities connected to this particular diagnostic - not the total number of activities that the student has practiced on Quill.",
-            formatter: Formatter::AS_ROUNDED_INTEGER
-          },
-          average_time_spent_seconds: {
-            csv_header: 'Average Time Spent',
-            csv_tooltip: "Each diagnostic is linked to recommended practice activities. For the students who have completed activities, this shows the average time spent per student in minutes.\n\nThis counts the practice activities connected to this particular diagnostic - not the total number of activities that the student has practiced on Quill.",
-            formatter: Formatter::AS_MINUTES_STRING
-          },
-          post_students_assigned: {
-            csv_header: 'Students Assigned Post Diagnostic',
-            csv_tooltip: "The total number of students assigned the Post diagnostic.",
-            formatter: Formatter::BLANK_AS_ZERO
-          },
-          post_students_completed: {
-            csv_header: 'Students Who Completed Post Diagnostic',
-            csv_tooltip: "The total number of students who completed the Post diagnostic.",
-            formatter: Formatter::BLANK_AS_ZERO
-          },
-          overall_skill_growth: {
-            csv_header: 'Overall Skill Growth',
-            csv_tooltip: "The average increase in growth scores across all of the skills.\n\nThe Performance by Skill report shows the average increase in questions answered correctly for each skill, and the overall growth is the average increase across all skills.\n\nThe growth score is based on only the students who have completed both the Pre and Post diagnostic. The growth score does not include students who only did the Pre.",
-            formatter: Formatter::PERCENT_AS_INTEGER
-          }
+      ORDERED_COLUMNS = {
+        diagnostic_name: {
+          csv_header: 'Diagnostic Name',
+          csv_tooltip:'', # Intentionally blank
+          formatter: Formatter::ALL_CAPS
+        },
+        pre_students_assigned: {
+          csv_header: 'Students Assigned Pre Diagnostic',
+          csv_tooltip: "The total number of students assigned the Pre diagnostic.",
+          formatter: Formatter::BLANK_AS_ZERO
+        },
+        pre_students_completed: {
+          csv_header: 'Students Who Completed Pre Diagnostic',
+          csv_tooltip: "The total number of students who completed the Pre diagnostic.",
+          formatter: Formatter::BLANK_AS_ZERO
+        },
+        students_completed_practice: {
+          csv_header: 'Completed Activities',
+          csv_tooltip: "The total number of students who have completed the practice activities linked to this diagnostic.\n\nA student is counted once the student has completed at least one practice activity linked to this diagnostic.",
+          formatter: Formatter::BLANK_AS_ZERO
+        },
+        average_practice_activities_count: {
+          csv_header: 'Average Activities',
+          csv_tooltip: "Each diagnostic is linked to recommended practice activities. For the students who have completed activities, this shows the average number of completed activities  per student.\n\nThis counts the practice activities connected to this particular diagnostic - not the total number of activities that the student has practiced on Quill.",
+          formatter: Formatter::AS_ROUNDED_INTEGER
+        },
+        average_time_spent_seconds: {
+          csv_header: 'Average Time Spent',
+          csv_tooltip: "Each diagnostic is linked to recommended practice activities. For the students who have completed activities, this shows the average time spent per student in minutes.\n\nThis counts the practice activities connected to this particular diagnostic - not the total number of activities that the student has practiced on Quill.",
+          formatter: Formatter::AS_MINUTES_STRING
+        },
+        post_students_assigned: {
+          csv_header: 'Students Assigned Post Diagnostic',
+          csv_tooltip: "The total number of students assigned the Post diagnostic.",
+          formatter: Formatter::BLANK_AS_ZERO
+        },
+        post_students_completed: {
+          csv_header: 'Students Who Completed Post Diagnostic',
+          csv_tooltip: "The total number of students who completed the Post diagnostic.",
+          formatter: Formatter::BLANK_AS_ZERO
+        },
+        overall_skill_growth: {
+          csv_header: 'Overall Skill Growth',
+          csv_tooltip: "The average increase in growth scores across all of the skills.\n\nThe Performance by Skill report shows the average increase in questions answered correctly for each skill, and the overall growth is the average increase across all skills.\n\nThe growth score is based on only the students who have completed both the Pre and Post diagnostic. The growth score does not include students who only did the Pre.",
+          formatter: Formatter::PERCENT_AS_INTEGER
         }
-      end
+      }
+
+      def self.ordered_columns = ORDERED_COLUMNS
     end
   end
 end
--- services/QuillLMS/app/queries/adapters/csv/admin_diagnostic_students_summary_data_export.rb
+++ services/QuillLMS/app/queries/adapters/csv/admin_diagnostic_students_summary_data_export.rb
@@ -3,6 +3,89 @@
 module Adapters
   module Csv
     class AdminDiagnosticStudentsSummaryDataExport < CsvDataExport
+      ORDERED_COLUMNS =  {
+        student_name: {
+          csv_header: 'Student Name',
+          csv_tooltip: "", # Intentionally empty
+          formatter: Formatter::DEFAULT
+        },
+        pre_questions_ratio: {
+          csv_header: 'Pre: Questions Correct (Ratio)',
+          csv_tooltip: "The ratio of questions the student answered correctly on the Pre diagnostic.",
+          formatter: Formatter::AS_RATIO
+        },
+        pre_questions_percentage: {
+          csv_header: 'Pre: Questions Correct (Percentage)',
+          csv_tooltip: "The percentage of questions the student answered correctly on the Pre diagnostic.",
+          formatter: Formatter::PERCENT_AS_INTEGER
+        },
+        pre_skills_proficient_ratio: {
+          csv_header: 'Pre: Skills Proficient (Ratio)',
+          csv_tooltip: "The ratio of skills the student demonstrated proficiency in on the Pre diagnostic.",
+          formatter: Formatter::AS_RATIO
+        },
+        pre_skills_proficient_list: {
+          csv_header: 'Pre: Skills Proficient (List)',
+          csv_tooltip: "A list of skills the student demonstrated proficiency in on the Pre diagnostic.",
+          formatter: Formatter::AS_LIST
+        },
+        pre_skills_to_practice_list: {
+          csv_header: 'Pre: Skills to Practice (List)',
+          csv_tooltip: "A list of skills the student did not demonstrate proficiency in on the Pre diagnostic.",
+          formatter: Formatter::AS_LIST
+        },
+        completed_activities: {
+          csv_header: 'Total Activities',
+          csv_tooltip: "The total number of activities the student completed that are linked to this particular diagnostic.",
+          formatter: Formatter::DEFAULT
+        },
+        time_spent_seconds: {
+          csv_header: 'Total Time Spent',
+          csv_tooltip: "The total time spent by the student on activities that are linked to this particular diagnostic.",
+          formatter: Formatter::AS_MINUTES_STRING
+        },
+        post_questions_ratio: {
+          csv_header: 'Post: Questions Correct (Ratio)',
+          csv_tooltip: "The ratio of questions the student answered correctly on the Post diagnostic.",
+          formatter: Formatter::AS_RATIO
+        },
+        post_questions_percentage: {
+          csv_header: 'Post: Questions Correct (Percentage)',
+          csv_tooltip: "The percentage of questions the student answered correctly on the Post diagnostic.",
+          formatter: Formatter::PERCENT_AS_INTEGER
+        },
+        post_skills_improved_or_maintained_ratio: {
+          csv_header: 'Post: Skills Improved or Maintained (Ratio)',
+          csv_tooltip: "The ratio of skills the student improved or maintained proficiency in on the Post diagnostic.",
+          formatter: Formatter::AS_RATIO
+        },
+        post_skills_improved: {
+          csv_header: 'Post: Skills Improved (Count)',
+          csv_tooltip: "The number of skills the student improved proficiency in on the Post diagnostic.",
+          formatter: Formatter::DEFAULT
+        },
+        post_skills_maintained: {
+          csv_header: 'Post: Skills Maintained (Count)',
+          csv_tooltip: "The number of skills the student maintained proficiency in on the Post diagnostic.",
+          formatter: Formatter::DEFAULT
+        },
+        post_skills_improved_list: {
+          csv_header: 'Post: Skills Improved (List)',
+          csv_tooltip: "A list of skills the student improved on the Post diagnostic.",
+          formatter: Formatter::AS_LIST
+        },
+        post_skills_maintained_list: {
+          csv_header: 'Post: Skills Maintained (List)',
+          csv_tooltip: "A list of skills the student maintained proficiency in on the Post diagnostic.",
+          formatter: Formatter::AS_LIST
+        },
+        post_skills_to_practice_list: {
+          csv_header: 'Post: Skills with No Growth (List)',
+          csv_tooltip: "A list of skills the student demonstrated no growth in on the Post diagnostic.",
+          formatter: Formatter::AS_LIST
+        }
+      }
+
       def self.add_aggregate_record_to_csv(csv, row, sym_columns)
         processed_row = pre_process_row(row)
         csv << sym_columns.map { |key| format_cell(key, processed_row[key]) }
@@ -30,90 +113,7 @@ module Adapters
         rows.filter{|row| row[filter_key] > 0}.map{|row| row[:skill_group_name]}
       end
 
-      def self.ordered_columns
-        {
-          student_name: {
-            csv_header: 'Student Name',
-            csv_tooltip: "", # Intentionally empty
-            formatter: Formatter::DEFAULT
-          },
-          pre_questions_ratio: {
-            csv_header: 'Pre: Questions Correct (Ratio)',
-            csv_tooltip: "The ratio of questions the student answered correctly on the Pre diagnostic.",
-            formatter: Formatter::AS_RATIO
-          },
-          pre_questions_percentage: {
-            csv_header: 'Pre: Questions Correct (Percentage)',
-            csv_tooltip: "The percentage of questions the student answered correctly on the Pre diagnostic.",
-            formatter: Formatter::PERCENT_AS_INTEGER
-          },
-          pre_skills_proficient_ratio: {
-            csv_header: 'Pre: Skills Proficient (Ratio)',
-            csv_tooltip: "The ratio of skills the student demonstrated proficiency in on the Pre diagnostic.",
-            formatter: Formatter::AS_RATIO
-          },
-          pre_skills_proficient_list: {
-            csv_header: 'Pre: Skills Proficient (List)',
-            csv_tooltip: "A list of skills the student demonstrated proficiency in on the Pre diagnostic.",
-            formatter: Formatter::AS_LIST
-          },
-          pre_skills_to_practice_list: {
-            csv_header: 'Pre: Skills to Practice (List)',
-            csv_tooltip: "A list of skills the student did not demonstrate proficiency in on the Pre diagnostic.",
-            formatter: Formatter::AS_LIST
-          },
-          completed_activities: {
-            csv_header: 'Total Activities',
-            csv_tooltip: "The total number of activities the student completed that are linked to this particular diagnostic.",
-            formatter: Formatter::DEFAULT
-          },
-          time_spent_seconds: {
-            csv_header: 'Total Time Spent',
-            csv_tooltip: "The total time spent by the student on activities that are linked to this particular diagnostic.",
-            formatter: Formatter::AS_MINUTES_STRING
-          },
-          post_questions_ratio: {
-            csv_header: 'Post: Questions Correct (Ratio)',
-            csv_tooltip: "The ratio of questions the student answered correctly on the Post diagnostic.",
-            formatter: Formatter::AS_RATIO
-          },
-          post_questions_percentage: {
-            csv_header: 'Post: Questions Correct (Percentage)',
-            csv_tooltip: "The percentage of questions the student answered correctly on the Post diagnostic.",
-            formatter: Formatter::PERCENT_AS_INTEGER
-          },
-          post_skills_improved_or_maintained_ratio: {
-            csv_header: 'Post: Skills Improved or Maintained (Ratio)',
-            csv_tooltip: "The ratio of skills the student improved or maintained proficiency in on the Post diagnostic.",
-            formatter: Formatter::AS_RATIO
-          },
-          post_skills_improved: {
-            csv_header: 'Post: Skills Improved (Count)',
-            csv_tooltip: "The number of skills the student improved proficiency in on the Post diagnostic.",
-            formatter: Formatter::DEFAULT
-          },
-          post_skills_maintained: {
-            csv_header: 'Post: Skills Maintained (Count)',
-            csv_tooltip: "The number of skills the student maintained proficiency in on the Post diagnostic.",
-            formatter: Formatter::DEFAULT
-          },
-          post_skills_improved_list: {
-            csv_header: 'Post: Skills Improved (List)',
-            csv_tooltip: "A list of skills the student improved on the Post diagnostic.",
-            formatter: Formatter::AS_LIST
-          },
-          post_skills_maintained_list: {
-            csv_header: 'Post: Skills Maintained (List)',
-            csv_tooltip: "A list of skills the student maintained proficiency in on the Post diagnostic.",
-            formatter: Formatter::AS_LIST
-          },
-          post_skills_to_practice_list: {
-            csv_header: 'Post: Skills with No Growth (List)',
-            csv_tooltip: "A list of skills the student demonstrated no growth in on the Post diagnostic.",
-            formatter: Formatter::AS_LIST
-          }
-        }
-      end
+      def self.ordered_columns = ORDERED_COLUMNS
     end
   end
 end
