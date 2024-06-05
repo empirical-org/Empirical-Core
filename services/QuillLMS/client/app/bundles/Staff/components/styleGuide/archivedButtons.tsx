d4f2515c65dec2af56575987786e94403bfe86da
Eric Adams <erictayloradams@gmail.com>
2024-06-05 16:04:45 -0400

    add sizes and states

---
+++ services/QuillLMS/client/app/bundles/Staff/components/styleGuide/archivedButtons.tsx
@@ -0,0 +1,36 @@
+import * as React from "react";
+
+const renderElement =(classNames, buttonText) => (
+  <div className="element">
+    <code>{classNames}</code>
+    <button className={classNames}>{buttonText}</button>
+  </div>
+)
+
+const renderElementRow = (classNames) => (
+  <div className="element-row">
+    {renderElement(`quill-button-archived fun ${classNames}`, 'Button')}
+    {renderElement(`quill-button-archived small ${classNames}`, 'Button')}
+    {renderElement(`quill-button-archived medium ${classNames}`, 'Button')}
+    {renderElement(`quill-button-archived medium icon ${classNames}`, '♡ Button')}
+    {renderElement(`quill-button-archived large ${classNames}`, 'Button')}
+    {renderElement(`quill-button-archived large icon ${classNames}`, '♡ Button')}
+  </div>
+)
+
+const ArchivedButtons = () => {
+  return (
+    <div id="buttons-archived">
+      <h2 className="style-guide-h2">Buttons (Archived)</h2>
+      <div className="element-container">
+        {renderElementRow('primary contained')}
+        {renderElementRow('primary outlined')}
+        {renderElementRow('secondary outlined')}
+        {renderElementRow('disabled contained')}
+        {renderElementRow('disabled outlined')}
+      </div>
+    </div>
+  )
+}
+
+export default ArchivedButtons
--- services/QuillLMS/client/app/bundles/Staff/components/styleGuide/buttons.tsx
+++ services/QuillLMS/client/app/bundles/Staff/components/styleGuide/buttons.tsx
@@ -1,33 +1,49 @@
 import * as React from "react";
+import _ from 'lodash';
+import { DISABLED, HOVER } from "../../../Shared";
 
-const renderElement =(classNames, buttonText) => (
-  <div className="element">
-    <code>{classNames}</code>
-    <button className={classNames}>{buttonText}</button>
-  </div>
-)
-
-const renderElementRow = (classNames) => (
-  <div className="element-row">
-    {renderElement(`quill-button-archived fun ${classNames}`, 'Button')}
-    {renderElement(`quill-button-archived small ${classNames}`, 'Button')}
-    {renderElement(`quill-button-archived medium ${classNames}`, 'Button')}
-    {renderElement(`quill-button-archived medium icon ${classNames}`, '♡ Button')}
-    {renderElement(`quill-button-archived large ${classNames}`, 'Button')}
-    {renderElement(`quill-button-archived large icon ${classNames}`, '♡ Button')}
-  </div>
-)
+const DEFAULT = 'default'
+const BUTTON = 'Button'
+const SIZES = [{ label: 'Extra Small (XS)', value: 'extra-small' }, { label: 'Small', value: 'small' }, { label: 'Medium', value: 'medium' }, { label: 'Large', value: 'large' }]
+const STATES = [DEFAULT, HOVER, DISABLED]
+const COLORS = ['green', 'gold', 'maroon', 'blue', 'teal', 'viridian', 'purple', 'violet', 'red', 'grey']
 
 const Buttons = () => {
   return (
     <div id="buttons">
-      <h2 className="style-guide-h2">Buttons (Archived)</h2>
-      <div className="element-container">
-        {renderElementRow('primary contained')}
-        {renderElementRow('primary outlined')}
-        {renderElementRow('secondary outlined')}
-        {renderElementRow('disabled contained')}
-        {renderElementRow('disabled outlined')}
+      <h2 className="style-guide-h2">Buttons (New)</h2>
+      <div className="variations-container">
+        <h4 className="style-guide-h4">Sizes</h4>
+        <div className="options-container">
+          {SIZES.map(({ label, value }) => {
+            return (
+              <div className="option-container">
+                <p className="option-label">{label}</p>
+                <div className="quill-button-container">
+                  <button className={`quill-button focus-on-light contained ${value}`}>{BUTTON}</button>
+                </div>
+              </div>
+            )
+          })}
+        </div>
+        <h4 className="style-guide-h4">States</h4>
+        <div className="options-container">
+          {STATES.map(state => {
+            let style = ''
+            if (state === HOVER) { style = 'hover' }
+            if (state === DISABLED) { style = 'disabled' }
+            return (
+              <div className="option-container">
+                <p className="option-label">{_.capitalize(state)}</p>
+                <div className="quill-button-container states">
+                  <button className={`quill-button focus-on-light small contained ${style}`} disabled={state === DISABLED}>{BUTTON}</button>
+                  <button className={`quill-button focus-on-light small outlined ${style}`} disabled={state === DISABLED}>{BUTTON}</button>
+                  <button className={`quill-button focus-on-light small outlined grey ${style}`} disabled={state === DISABLED}>{BUTTON}</button>
+                </div>
+              </div>
+            )
+          })}
+        </div>
       </div>
     </div>
   )
