59edbfe097bdd908646ad5bd1757f5c9379f172a
Peter Kong <pkong@quill.org>
2024-06-05 14:10:12 -0400

    move worker to experiment queue

--- services/QuillLMS/app/workers/learn_worlds_integration/sync_user_tags_worker.rb
+++ services/QuillLMS/app/workers/learn_worlds_integration/sync_user_tags_worker.rb
@@ -4,6 +4,8 @@ module LearnWorldsIntegration
   class SyncUserTagsWorker
     include Sidekiq::Worker
 
+    sidekiq_options queue: SidekiqQueue::EXPERIMENT
+
     def perform(user_with_subject_areas)
       LearnWorldsIntegration::UserTagsRequest.run(user_with_subject_areas)
     end
