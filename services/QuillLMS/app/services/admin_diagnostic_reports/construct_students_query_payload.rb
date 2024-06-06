c99755e9493e26ccf0e2b2698bdb11b580b168c1
Thomas Robertson <thomas.robertson@quill.org>
2024-06-06 19:12:25 -0400

    Make sure not to LIMIT 500 when building CSV Student reports

--- services/QuillLMS/app/services/admin_diagnostic_reports/construct_students_query_payload.rb
+++ services/QuillLMS/app/services/admin_diagnostic_reports/construct_students_query_payload.rb
@@ -4,7 +4,8 @@ module AdminDiagnosticReports
   class ConstructStudentsQueryPayload < ConstructQueryPayload
     private def report_specific_filters
       {
-        diagnostic_id: specific_filters&.diagnostic_id || DEFAULT_DIAGNOSTIC_ID
+        diagnostic_id: specific_filters&.diagnostic_id || DEFAULT_DIAGNOSTIC_ID,
+        limited: false
       }
     end
   end
