a03389008605dacdf0fc1cee3d5c7da77245ef26
Thomas Robertson <thomas.robertson@quill.org>
2024-06-11 17:12:58 -0400

    More linting fixes

--- services/QuillLMS/spec/services/formatter_spec.rb
+++ services/QuillLMS/spec/services/formatter_spec.rb
@@ -3,31 +3,31 @@
 require 'rails_helper'
 
 describe Formatter do
-  subject { described_class.run(format, value) }
+  subject { described_class.run(selected_format, value) }
 
   context 'default' do
-    let(:format) { described_class::DEFAULT }
+    let(:selected_format) { described_class::DEFAULT }
     let(:value) { 'Should not be modified' }
 
     it { expect(subject).to eq(value) }
   end
 
   context 'all_caps' do
-    let(:format) { described_class::ALL_CAPS }
+    let(:selected_format) { described_class::ALL_CAPS }
     let(:value) { 'lowercase' }
 
     it { expect(subject).to eq(value.upcase) }
   end
 
   context 'as_list' do
-    let(:format) { described_class::AS_LIST }
+    let(:selected_format) { described_class::AS_LIST }
     let(:value) { [1,2,3] }
 
     it { expect(subject).to eq(value.join(", ")) }
   end
 
   context 'as_minutes_string' do
-    let(:format) { described_class::AS_MINUTES_STRING }
+    let(:selected_format) { described_class::AS_MINUTES_STRING }
 
     context "less than one minute" do
       let(:value) { 34 }
@@ -43,21 +43,21 @@ describe Formatter do
   end
 
   context 'as_ratio' do
-    let(:format) { described_class::AS_RATIO }
+    let(:selected_format) { described_class::AS_RATIO }
     let(:value) { [1,4] }
 
     it { expect(subject).to eq("#{value[0]} of #{value[1]}") }
   end
 
   context 'as_rounded_integer' do
-    let(:format) { described_class::AS_ROUNDED_INTEGER }
+    let(:selected_format) { described_class::AS_ROUNDED_INTEGER }
     let(:value) { 45.67 }
 
     it { expect(subject).to eq(value.round) }
   end
 
   context 'blank_as_zero' do
-    let(:format) { described_class::BLANK_AS_ZERO }
+    let(:selected_format) { described_class::BLANK_AS_ZERO }
 
     context 'nil value' do
       let(:value) { nil }
@@ -79,21 +79,21 @@ describe Formatter do
   end
 
   context 'date' do
-    let(:format) { described_class::DATE }
+    let(:selected_format) { described_class::DATE }
     let(:value) { DateTime.current }
 
-    it { is_expected.to eq("#{value.year}-#{sprintf('%02d', value.month)}-#{sprintf('%02d', value.day)}") }
+    it { is_expected.to eq("#{value.year}-#{format('%02d', value.month)}-#{format('%02d', value.day)}") }
   end
 
   context 'percent_as_integer' do
-    let(:format) { described_class::PERCENT_AS_INTEGER }
+    let(:selected_format) { described_class::PERCENT_AS_INTEGER }
     let(:value) { 0.5672 }
 
     it { expect(subject).to eq((value * 100).round) }
   end
 
   context 'score_or_completed' do
-    let(:format) { described_class::SCORE_OR_COMPLETED }
+    let(:selected_format) { described_class::SCORE_OR_COMPLETED }
 
     context 'nil score' do
       let(:value) { nil }
@@ -115,7 +115,7 @@ describe Formatter do
   end
 
   context 'seconds_to_minutes' do
-    let(:format) { described_class::SECONDS_TO_MINUTES }
+    let(:selected_format) { described_class::SECONDS_TO_MINUTES }
 
     context 'nil' do
       let(:value) { nil }
