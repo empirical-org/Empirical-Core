24811db3cde773942820427057289801d86b976d
Eric Adams <erictayloradams@gmail.com>
2024-06-11 15:56:54 -0400

    add clear input updates

--- services/QuillLMS/client/app/bundles/Staff/components/styleGuide/textFields.tsx
+++ services/QuillLMS/client/app/bundles/Staff/components/styleGuide/textFields.tsx
@@ -1,106 +1,122 @@
 import * as React from "react";
 import { Input, } from '../../../Shared/index';
 
-class TextFields extends React.Component {
+const INPUT_ONE = 'inputOne'
+const INPUT_TWO = 'inputTwo'
+const INPUT_THREE = 'inputThree'
+const INPUT_FOUR = 'inputFour'
 
-  constructor(props) {
-    super(props)
+export const TextFields = () => {
 
-    this.state = {
-      inputOne: null,
-      inputTwo: null,
-      inputThree: 'Bad input',
-      inputFour: 'Even worse input'
-    }
+  const [inputOne, setInputOne] = React.useState<string>(null);
+  const [inputTwo, setInputTwo] = React.useState<string>(null);
+  const [inputThree, setInputThree] = React.useState<string>('Bad input');
+  const [inputFour, setInputFour] = React.useState<string>('Even worse input');
 
-    this.changeSavedValues = this.changeSavedValues.bind(this)
+  const setterFunctions = {
+    [INPUT_ONE]: setInputOne,
+    [INPUT_TWO]: setInputTwo,
+    [INPUT_THREE]: setInputThree,
+    [INPUT_FOUR]: setInputFour,
   }
 
-  changeSavedValues = (key, e) => {
-    this.setState({[key]: e.target.value})
+  function changeSavedValues(key, e) {
+    const setterFunction = setterFunctions[key]
+    setterFunction(e.target.value)
   }
 
-  render() {
-    return (
-      <div id="textFields">
-        <h2 className="style-guide-h2">Text Fields</h2>
-        <div className="element-container">
-          <div className="element-row">
-            <div className="big-element">
-              <h4 className="style-guide-h4">Input</h4>
-              <pre>
-                {`<Input
-  label="Label"
-  handleChange={(e) => {this.changeSavedValues('inputOne', e)}}
-  value={this.state.inputOne}
+  function handleClearInput(key) {
+    const setterFunction = setterFunctions[key]
+    setterFunction('')
+  }
+
+  return (
+    <div id="textFields">
+      <h2 className="style-guide-h2">Text Fields</h2>
+      <div className="element-container">
+        <div className="element-row">
+          <div className="big-element">
+            <h4 className="style-guide-h4">Input</h4>
+            <pre>
+              {`<Input
+label="Label"
+handleCancel={() => handleClearInput(INPUT_ONE)}
+handleChange={(e) => {changeSavedValues('inputOne', e)}}
+value={inputOne}
 />`}
-              </pre>
-              <Input
-                handleChange={(e) => {this.changeSavedValues('inputOne', e)}}
-                label="Label"
-                value={this.state.inputOne}
-              />
-            </div>
-            <div className="big-element">
-              <h4 className="style-guide-h4">Input with helper text and placeholder</h4>
-              <pre>
-                {`<Input
-  label="Label"
-  value={this.state.inputTwo}
-  handleChange={(e) => {this.changeSavedValues('inputTwo', e)}}
-  helperText="Helper text"
-  placeholder="Placeholder"
-/>`}
-              </pre>
-              <Input
-                handleChange={(e) => {this.changeSavedValues('inputTwo', e)}}
-                helperText="Helper text"
-                label="Label"
-                placeholder="Placeholder"
-                value={this.state.inputTwo}
-              />
-            </div>
+            </pre>
+            <Input
+              handleCancel={() => handleClearInput(INPUT_ONE)}
+              handleChange={(e) => {changeSavedValues(INPUT_ONE, e)}}
+              label="Label"
+              value={inputOne}
+            />
           </div>
-          <div className="element-row">
-            <div className="big-element">
-              <h4 className="style-guide-h4">Single-line error</h4>
-              <pre>
-                {`<Input
-  label="Label"
-  handleChange={(e) => {this.changeSavedValues('inputThree', e)}}
-  value={this.state.inputThree}
-  error="Short error message"
+          <div className="big-element">
+            <h4 className="style-guide-h4">Input with helper text and placeholder</h4>
+            <pre>
+              {`<Input
+label="Label"
+value={inputTwo}
+handleCancel={() => handleClearInput(INPUT_TWO)}
+handleChange={(e) => {changeSavedValues('inputTwo', e)}}
+helperText="Helper text"
+placeholder="Placeholder"
 />`}
-              </pre>
-              <Input
-                error="Short error message"
-                handleChange={(e) => {this.changeSavedValues('inputThree', e)}}
-                label="Label"
-                value={this.state.inputThree}
-              />
-            </div>
-            <div className="big-element">
-              <h4 className="style-guide-h4">Multi-line error</h4>
-              <pre>
-                {`<Input
-  label="Label"
-  value={this.state.inputFour}
-  handleChange={(e) => {this.changeSavedValues('inputFour', e)}}
-  error="Error message
+            </pre>
+            <Input
+              handleCancel={() => handleClearInput(INPUT_TWO)}
+              handleChange={(e) => {changeSavedValues(INPUT_TWO, e)}}
+              helperText="Helper text"
+              label="Label"
+              placeholder="Placeholder"
+              value={inputTwo}
+            />
+          </div>
+        </div>
+        <div className="element-row">
+          <div className="big-element">
+            <h4 className="style-guide-h4">Single-line error</h4>
+            <pre>
+              {`<Input
+label="Label"
+handleCancel={() => handleClearInput(INPUT_THREE)}
+handleChange={(e) => {changeSavedValues('inputThree', e)}}
+value={inputThree}
+error="Short error message"
 />`}
-              </pre>
-              <Input
-                error="Error mesage duis mollis, est non commodo luctus, nisi erat porttitor"
-                handleChange={(e) => {this.changeSavedValues('inputFour', e)}}
-                label="Label"
-                value={this.state.inputFour}
-              />
-            </div>
+            </pre>
+            <Input
+              error="Short error message"
+              handleCancel={() => handleClearInput(INPUT_THREE)}
+              handleChange={(e) => {changeSavedValues(INPUT_THREE, e)}}
+              label="Label"
+              value={inputThree}
+            />
+          </div>
+          <div className="big-element">
+            <h4 className="style-guide-h4">Multi-line error</h4>
+            <pre>
+              {`<Input
+label="Label"
+value={inputFour}
+handleCancel={(e) => handleClearInput(INPUT_FOUR, e)}
+handleChange={(e) => {changeSavedValues('inputFour', e)}}
+error="Error message
+/>`}
+            </pre>
+            <Input
+              error="Error mesage duis mollis, est non commodo luctus, nisi erat porttitor"
+              handleCancel={() => handleClearInput(INPUT_FOUR)}
+              handleChange={(e) => {changeSavedValues(INPUT_FOUR, e)}}
+              label="Label"
+              value={inputFour}
+            />
           </div>
         </div>
       </div>
-    )
-  }
+    </div>
+  )
 }
 
 export default TextFields
