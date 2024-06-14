1e83b9364ade700b894c95c3d852691df4281f89
Samantha John <samantha@mechanical-orchard.com>
2024-06-13 21:35:52 -0400

    Add specs for workers.

---
+++ services/QuillLMS/spec/workers/gengo/save_translated_text_worker_spec.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe Gengo::SaveTranslatedTextWorker, type: :worker do
+  let(:worker) { described_class.new }
+  let(:job_id) { "123" }
+
+  it 'calls through to SaveJobsFromOrder' do
+    expect(Gengo::SaveTranslatedText).to receive(:run).with(job_id)
+    worker.perform(job_id)
+  end
+end
