538c4bd1e5fd3c6d39762a37cf9dbf4409a0cb63
Samantha <126436+samanthamjohn@users.noreply.github.com>
2024-06-18 16:19:41 -0400

    Don’t re-translate the same english_text (#11944)
    * Use a constant for SPANISH_LOCALE (es-la) everywhere
    
    * Don’t re-translate the same english_text
    
    * Rubocop fixes
    
    * Code cleanup
    
    * Update indent
    
    ---------
    
    Co-authored-by: Samantha John <samantha@mechanical-orchard.com>

--- services/QuillLMS/app/services/gengo/request_translations.rb
+++ services/QuillLMS/app/services/gengo/request_translations.rb
@@ -3,7 +3,7 @@
 module Gengo
   class RequestTranslations < ApplicationService
     class RequestTranslationError < StandardError; end
-    attr_accessor :english_texts
+    attr_reader :english_texts
 
     STANDARD_COMMENT = <<~STRING
       We are translating the instructions for an English-language grammar activity. The content of the activity itself is not translated. Therefore, please leave words that sound like they are part of the activity in the original english. Often they will between an HTML tag such as in <em>english word</em> or <ul>english  word</ul>.
@@ -16,6 +16,8 @@ module Gengo
     end
 
     def run
+      return if gengo_payload.empty?
+
       response = GengoAPI.postTranslationJobs(jobs: gengo_payload)
       raise RequestTranslationError unless response.present?
 
@@ -24,18 +26,22 @@ module Gengo
     end
 
     def gengo_payload
-      english_texts.reduce({}) do |hash, english_text|
-        hash.merge( { english_text.id.to_s => {
-          type: "text",
-          body_src: english_text.text,
-          lc_src: "en",
-          lc_tgt: Gengo::SPANISH_LOCALE,
-          tier: "standard",
-          slug: english_text.id,
-          group: true,
-          auto_approve: true,
-          comment: STANDARD_COMMENT
-        }})
+      @gengo_payload ||= begin
+        english_texts
+          .select(&:needs_translation?)
+          .each_with_object({}) do |english_text, hash|
+          hash[english_text.id.to_s] = {
+            type: "text",
+            body_src: english_text.text,
+            lc_src: "en",
+            lc_tgt: Gengo::SPANISH_LOCALE,
+            tier: "standard",
+            slug: english_text.id,
+            group: true,
+            auto_approve: true,
+            comment: STANDARD_COMMENT
+          }
+        end
       end
     end
   end
