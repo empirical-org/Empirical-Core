76776371f623c563e9a7d1e10cb2ece0a0297f40
Brendan Shean <brendanshean@gmail.com>
2024-06-19 18:18:35 -0400

    Create Evidence::Research::GenAI::TestExample

--- services/QuillLMS/spec/factories/user_activity_classifications.rb
+++ services/QuillLMS/spec/factories/user_activity_classifications.rb
@@ -6,6 +6,8 @@
 #
 #  id                         :bigint           not null, primary key
 #  count                      :integer          default(0)
+#  created_at                 :datetime         not null
+#  updated_at                 :datetime         not null
 #  activity_classification_id :bigint
 #  user_id                    :bigint
 #
