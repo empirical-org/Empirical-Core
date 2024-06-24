8294ef79457756cdb3cb63e0e47593b862dad534
Samantha John <samantha@mechanical-orchard.com>
2024-06-24 15:54:23 -0400

    Move translate module to main app and copy OpenAI::API  to main app + add tests

---
+++ services/QuillLMS/app/services/concerns/open_ai/api.rb
@@ -0,0 +1,51 @@
+# frozen_string_literal: true
+
+module OpenAI
+  module Api
+    extend ActiveSupport::Concern
+
+    BASE_URI = 'https://api.openai.com/v1'
+    TIMEOUT = 5.minutes.to_i
+
+    included do
+      include HTTParty
+      base_uri BASE_URI
+
+      attr_accessor :response
+    end
+
+    # classes using this concern require these methods
+    def endpoint
+      raise NotImplementedError
+    end
+
+    def request_body
+      raise NotImplementedError
+    end
+
+    def cleaned_results
+      raise NotImplementedError
+    end
+
+    def run
+      return cleaned_results if response.present?
+
+      @response = post_request
+
+      cleaned_results
+    rescue *Evidence::HTTP_TIMEOUT_ERRORS
+      []
+    end
+
+    private def headers
+      {
+        "Content-Type" => "application/json",
+        "Authorization" => "Bearer #{Evidence::OpenAI::API_KEY}"
+      }
+    end
+
+    private def post_request
+      self.class.post(endpoint, body: request_body.to_json, headers: headers, timeout: TIMEOUT)
+    end
+  end
+end
