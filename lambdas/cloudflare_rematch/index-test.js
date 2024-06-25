cc21b37b1fb0434ef995855edf1e5a531fc5217b
Peter Kong <pkong@quill.org>
2024-06-25 18:12:23 -0400

    remove old file

--- lambdas/cloudflare_rematch/index-test.js
+++
@@ -1,9 +0,0 @@
-export default {
-  async fetch(request, env, ctx) {
-    //const body = JSON.parse(request.body);
-    const requestJson = await request.json()
-    console.log("requestJson: ", requestJson)
-    return new Response(JSON.stringify(requestJson));
-  },
-};
-
