68829ab9a89a7888af8892d2f631384a18ae708f
Peter Kong <pkong@quill.org>
2024-06-25 15:10:05 -0400

    check in old index.js again

---
+++ lambdas/rematching/index.js
@@ -0,0 +1,21 @@
+import { rematchIndividualQuestion, } from './rematch'
+
+exports.handler = async (event) => {
+  const body = JSON.parse(event.body);
+  const { response, type, question, referenceResponses } = body
+
+  try {
+    return await rematchIndividualQuestion(response, type, question, referenceResponses).
+      then((rematch) => {
+        return {
+          statusCode: 200,
+          body: JSON.stringify(rematch),
+        };
+      });
+  } catch(err) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify(err),
+    };
+  }
+};
