20f80d44d5a9f5e6af1a5dbc576e0b30124d5981
Peter Kong <pkong@quill.org>
2024-06-27 19:07:50 -0400

    tweak cloudflare response shape

--- lambdas/rematching/index-cloudflare.js
+++ lambdas/rematching/index-cloudflare.js
@@ -9,10 +9,7 @@ var src_default = {
     try {
       return await rematchIndividualQuestion(response, type, question, referenceResponses).
         then((rematch) => {
-          return new Response(JSON.stringify({
-            statusCode: 200,
-            body: JSON.stringify(rematch),
-          }))
+          return new Response(JSON.stringify(rematch))
         });
     } catch(err) {
       return new Response(JSON.stringify({
