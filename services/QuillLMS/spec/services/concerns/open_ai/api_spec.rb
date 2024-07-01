b773fe359e78530dac4c79b19291dd07814d23cd
Samantha <126436+samanthamjohn@users.noreply.github.com>
2024-07-01 16:41:25 -0400

    Add OpenAI translation option (#11981)
    * Add openAI as a translation option and move Gengo job sync into the gengo model so that translatedText can support either gengo or OpenAI
    
    * Ensure that we can handle a nil options in the as_json method.
    
    * Rubocop fixes.
    
    * Add locale to initializer
    
    * Make fetch_translations! Run an asynchronous job
    
    * Delete unused method
    
    * Don’t reference evidence module in open_ai concern.
    
    * Change needs_gengo_translation? To has_gengo_translation?
    
    * Change source to source_api
    
    * Use better variable names
    
    * Default hash name
    
    * Remove unused methods
    
    * Require source_api for translatedText
    
    * Change Gengo::SPANISH_LOCALE to TranslatedText::DEFAULT_LOCALE
    
    * Rename class
    
    * Add newline
    
    * Change factory to use association
    
    * Use a one-liner for system_prompt
    
    * Factory use association
    
    * Update indentation
    
    * Remove extraneous class
    
    * Protect against SQL injection in custom order_by
    
    * Use locale as a default argument
    
    * Fix job_id -> Translation_job_id
    
    * has_gengo_translation? => gengo_translation?
    
    * Whitespace
    
    * Fix name error
    
    * Fix spec
    
    * Use longer names
    
    * Remove extra line to structure.sql
    
    * Rollback accidental change
    
    ---------
    
    Co-authored-by: Samantha John <samantha@mechanical-orchard.com>

---
+++ services/QuillLMS/spec/services/concerns/open_ai/api_spec.rb
@@ -0,0 +1,55 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+module OpenAI
+  RSpec.describe(OpenAI::Api, type: :model) do
+
+    let(:endpoint) {'https://api.openai.com/v1/some_endpoint'}
+    let(:sample_response_body) {{"key"=>"value"}}
+
+    # include headers in response for proper parsing by HTTParty
+    let(:sample_response) { {body: sample_response_body.to_json, headers: {content_type: 'application/json'}} }
+
+    let(:class_without_defined_methods) { Class.new { include ::OpenAI::Api } }
+
+    let(:class_with_methods) do
+      Class.new do
+        include OpenAI::Api
+
+        def request_body = {}
+        def endpoint = '/some_endpoint'
+        def cleaned_results = response.body
+      end
+    end
+
+    context "#class_without_defined_methods" do
+      it "should raise an error on run" do
+        expect{class_without_defined_methods.new.run}.to raise_error(NotImplementedError)
+      end
+    end
+
+    context "#class_with_defined_methods" do
+      it "should run" do
+        stub_request(:post, endpoint).to_return(sample_response)
+        expect(JSON.parse(class_with_methods.new.run)).to eq(sample_response_body)
+      end
+    end
+
+    context "#run hitting a timeout" do
+      it "should rescue and return an empty array" do
+        stub_request(:post, endpoint).to_timeout
+        expect(class_with_methods.new.run).to eq([])
+      end
+    end
+
+    context "#post_request" do
+      let(:api_response) { class_with_methods.new.send(:post_request) }
+
+      it "should pass a timeout" do
+        stub_request(:post, endpoint).to_return(sample_response)
+        expect(api_response.request.options[:timeout]).to eq 300
+      end
+    end
+  end
+end
