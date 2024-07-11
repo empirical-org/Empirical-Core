6b356b7cf228a8261a38e9502b20037981267fcd
Samantha John <samantha@mechanical-orchard.com>
2024-07-11 14:51:31 -0400

    Script to add uid to existing	 focusPoints and incorrectSequences in arrays.

---
+++ services/QuillLMS/app/workers/add_uuid_to_question_data_worker.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+class AddUuidToQuestionDataWorker
+  include Sidekiq::Worker
+  sidekiq_options queue: SidekiqQueue::LOW
+
+  def perform(type, offset, batch_size)
+    questions = Question.where("jsonb_typeof(data->'#{type}') = 'array'")
+    .offset(offset)
+    .limit(batch_size)
+
+    questions.each do |question|
+      # We don't actually need to refresh the cache for questions as the uid will not be used by the front end yet.
+      question.skip_refresh_caches = true
+      question.save_uids_for(type)
+    end
+  end
+end
