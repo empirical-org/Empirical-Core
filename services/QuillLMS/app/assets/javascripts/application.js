ac3c821d97b980a272dc671cfd68b3dd4b0f7b93
happythenewsad <happythenewsad@users.noreply.github.com>
2024-07-17 10:10:15 -0600

    Remove turbolinks (#12081)
    * remove app/assets/javascripts/*
    
    * remove app/assets/javascripts, turbolinks gems
    
    * remove turbolinks-data-track html directives
    
    ---------
    
    Co-authored-by: Peter Kong <pkong@quill.org>

--- services/QuillLMS/app/assets/javascripts/application.js
+++
@@ -1,8 +0,0 @@
-// DO NOT REQUIRE jQuery or jQuery-ujs in this file!
-// DO NOT REQUIRE TREE!
-
-// CRITICAL that vendor-bundle must be BEFORE bootstrap-sprockets and turbolinks
-// since it is exposing jQuery and jQuery-ujs
-
-//= require jquery
-//= require bootstrap-sprockets
--- services/QuillLMS/app/assets/javascripts/test_file_application_static.js
+++
@@ -1,1 +0,0 @@
-console.log('test_file_application_static.js loaded')
