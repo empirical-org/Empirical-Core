537fce94a1fe947132c7afbe5879e0a23bc8f8d6
Samantha John <samantha@mechanical-orchard.com>
2024-07-29 17:38:18 -0400

    Put mock question api in the right spot

--- services/QuillLMS/client/app/bundles/Connect/test/__mocks__/lesson_api.ts
+++ services/QuillLMS/client/app/bundles/Connect/test/__mocks__/lesson_api.ts
@@ -1,6 +1,5 @@
 export const mockLessonApi = {
   getAll: jest.fn().mockImplementation(() => Promise.resolve({})),
-  getAllForActivity: jest.fn().mockImplementation(() => Promise.resolve({})),
   get: jest.fn().mockImplementation(() => Promise.resolve({})),
   create: jest.fn().mockImplementation(() => Promise.resolve({})),
   update: jest.fn().mockImplementation(() => Promise.resolve({})),
