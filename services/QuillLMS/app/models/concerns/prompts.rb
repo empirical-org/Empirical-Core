7ab4769642ac286d077b1ece64fd79c18ddf415b
Samantha John <samantha@mechanical-orchard.com>
2024-07-31 16:17:47 -0400

    Fix specs.

--- services/QuillLMS/app/models/concerns/prompts.rb
+++ services/QuillLMS/app/models/concerns/prompts.rb
@@ -1,23 +1,22 @@
+# frozen_string_literal: true
 
 module Prompts
-  def self.prompt_start(locale:)
-      <<~STRING
-        You are going to do a translation from english to #{locale} using simple words and language at a 5th grade reading level. Use shorter words over longer if possible. The tone should be somewhat casual. Return just the translated text preserving (but not translating) the HTML.
-
-        We are translating the instructions and feedback for an English-language grammar activity. The content of the activity itself is not translated.
-
-      STRING
-  end
-
+  extend ActiveSupport::Concern
+  module ClassMethods
   def prompt_start(locale:)
-    self.class.prompt_start(locale:)
+    <<~STRING
+      You are going to do a translation from english to #{locale} using simple words and language at a 5th grade reading level. Use shorter words over longer if possible. The tone should be somewhat casual. Return just the translated text preserving (but not translating) the HTML.
+
+      We are translating the instructions and feedback for an English-language grammar activity. The content of the activity itself is not translated.
+
+    STRING
   end
 
-  def self.feedback_strings_prompt(locale:)
+  def feedback_strings_prompt(locale:)
     prompt_start(locale:) + feedback_strings_custom_prompt
   end
 
-  def self.feedback_strings_custom_prompt
+  def feedback_strings_custom_prompt
     <<~STRING
       Following is the text to translate in a JSON format. Please return JSON in the same format, with the values translated but not the keys.
 
@@ -30,16 +29,15 @@ module Prompts
     "#{prompt_start(locale:)}#{custom_prompt}#{examples}\n text to translate: "
   end
 
-  private def custom_prompt
+  def custom_prompt
     config_yaml['custom_prompt']
   end
 
+  def config_filename = "#{name.underscore}.yml"
+  def config_file = Rails.root.join('app/models/translation_config', config_filename)
+  def config_yaml = YAML.load_file(config_file)
 
-  private def config_filename = "#{self.class.name.underscore}.yml"
-  private def config_file = Rails.root.join('app/models/translation_config', config_filename)
-  private def config_yaml = YAML.load_file(config_file)
-
-  private def examples
+  def examples
     examples = config_yaml['examples']
     return '' unless examples.present?
 
@@ -50,4 +48,5 @@ module Prompts
     end
     formatted_examples
   end
+end
 end
