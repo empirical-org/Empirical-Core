c566cf0bba17f522809c94df6b91b0a26f140b61
Thomas Robertson <thomas.robertson@quill.org>
2024-08-06 16:28:15 -0400

    Do some refactoring to make mailers construct more cleanly

--- services/QuillLMS/app/mailers/admin_diagnostic_reports/subscription_report_mailer.rb
+++ services/QuillLMS/app/mailers/admin_diagnostic_reports/subscription_report_mailer.rb
@@ -2,9 +2,13 @@
 
 module AdminDiagnosticReports
   class SubscriptionReportMailer < ReportMailer
-    def csv_download_email(user_id, overview_url, skills_url, students_url)
-      @unsubscribe_url
+    def csv_download_email(user_id, overview_url, skills_url, students_url, subscription)
+      @subscription = subscription
+      @premium_hub_url = "#{ENV['DEFAULT_URL']}/teachers/premium_hub"
+      @unsubscribe_url = "#{ENV['DEFAULT_URL']}/email_subscriptions/unsubscribe/#{subscription.cancel_token}"
       super
     end
+
+    private def subject = "🚀 Your #{@subscription.frequency.capitalize} Quill Admin Diagnostic Growth Report is Ready!"
   end
 end
--- services/QuillLMS/app/workers/admin_diagnostic_reports/send_subscription_csv_email_worker.rb
+++ services/QuillLMS/app/workers/admin_diagnostic_reports/send_subscription_csv_email_worker.rb
@@ -3,5 +3,15 @@
 module AdminDiagnosticReports
   class SendSubscriptionCsvEmailWorker < SendCsvEmailWorker
     private def mailer = SubscriptionReportMailer
+
+    private def email_payload
+      @email_payload ||= [
+        user.id,
+        generate_overview_link(user.id, shared_filter_report_name, overview_filter_report_name),
+        generate_skills_link(user.id, shared_filter_report_name, skills_filter_report_name),
+        generate_students_link(user.id, shared_filter_report_name, students_filter_report_name),
+        EmailSubscription.find_by(user: user, subscription_type: EmailSubscription::ADMIN_DIAGNOSTIC_REPORT)
+      ]
+    end
   end
 end
