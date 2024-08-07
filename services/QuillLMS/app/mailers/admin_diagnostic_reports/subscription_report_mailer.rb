1cf2ec27d2afa0a388bd857cae15c317ee66e82a
Thomas Robertson <thomas.robertson@quill.org>
2024-08-07 16:23:15 -0400

    Set frequency for email template

--- services/QuillLMS/app/mailers/admin_diagnostic_reports/subscription_report_mailer.rb
+++ services/QuillLMS/app/mailers/admin_diagnostic_reports/subscription_report_mailer.rb
@@ -4,6 +4,7 @@ module AdminDiagnosticReports
   class SubscriptionReportMailer < ReportMailer
     def csv_download_email(user_id, overview_url, skills_url, students_url, subscription)
       @subscription = subscription
+      @frequency = @subscription.frequency
       @premium_hub_url = "#{ENV['DEFAULT_URL']}/teachers/premium_hub"
       @unsubscribe_url = "#{ENV['DEFAULT_URL']}/email_subscriptions/unsubscribe/#{subscription.cancel_token}"
       super(user_id, overview_url, skills_url, students_url)
