b321841eeb5c3df2f8056c5bf2c76ee9e180c1b8
Thomas Robertson <thomas.robertson@quill.org>
2024-08-07 14:39:46 -0400

    Fix super call to pass appropriate params

--- services/QuillLMS/app/mailers/admin_diagnostic_reports/subscription_report_mailer.rb
+++ services/QuillLMS/app/mailers/admin_diagnostic_reports/subscription_report_mailer.rb
@@ -6,7 +6,7 @@ module AdminDiagnosticReports
       @subscription = subscription
       @premium_hub_url = "#{ENV['DEFAULT_URL']}/teachers/premium_hub"
       @unsubscribe_url = "#{ENV['DEFAULT_URL']}/email_subscriptions/unsubscribe/#{subscription.cancel_token}"
-      super
+      super(user_id, overview_url, skills_url, students_url)
     end
 
     private def subject = "🚀 Your #{@subscription.frequency.capitalize} Quill Admin Diagnostic Growth Report is Ready!"
