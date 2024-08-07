e16b24e4400e0ed29598b52382e8b6210c82b5f2
Brendan Shean <brendanshean@gmail.com>
2024-08-07 14:12:11 -0400

    Add header and student response validation

--- services/QuillLMS/engines/evidence/app/services/evidence/research/gen_ai/dataset_validator.rb
+++ services/QuillLMS/engines/evidence/app/services/evidence/research/gen_ai/dataset_validator.rb
@@ -6,28 +6,46 @@ module Evidence
       class DatasetValidator < ApplicationService
         attr_reader :file
 
+        HEADERS = DatasetImporter::HEADERS
+
         OPTIMAL = HasAssignedStatus::OPTIMAL
         SUBOPTIMAL = HasAssignedStatus::SUBOPTIMAL
-        MISSING_FEEDBACK_ERROR = 'is missing feedback. Please add Curriculum Proposed Feedback or AutoML Primary Feedback.'
+        MISSING_FEEDBACK_ERROR = 'CSV is missing feedback. Please add Curriculum Proposed Feedback or AutoML Primary Feedback.'
+        MISSING_STUDENT_RESPONSE_ERROR = 'is missing a Student Response.'
+        MISSING_HEADERS_ERROR = 'CSV is missing required headers: %s.'
 
         def initialize(file:)
           @file = file
         end
 
         def run
-          [].tap do |errors|
-            CSV.parse(file.read, headers: true).each.with_index(2) do |row, index|
-              next if row['Curriculum Assigned Optimal Status'] == 'TRUE'
-              next if row['Data Partition'] == 'prompt'
-              next if row['Curriculum Proposed Feedback'].present? || row['Optional - AutoML Primary Feedback'].present?
+          return headers_error if missing_headers.any?
 
-              errors << "Row #{index}: #{MISSING_FEEDBACK_ERROR}"
+          [].tap do |errors|
+            csv.each.with_index(2) do |row, index|
+              errors << "Row #{index}: #{MISSING_STUDENT_RESPONSE_ERROR}" if missing_student_response?(row)
+              errors << "Row #{index}: #{MISSING_FEEDBACK_ERROR}" if missing_feedback?(row)
             end
 
             file.rewind
 
             errors.empty? ? nil : errors.join(",\n")
           end
+        end
+
+        private def csv = @csv ||= CSV.parse(file.read, headers: true)
+
+        private def headers_error = format(MISSING_HEADERS_ERROR, missing_headers.join(', '))
+
+        private def missing_headers = HEADERS - csv.headers
+
+        private def missing_student_response?(row) = row['Student Response'].blank?
+
+        private def missing_feedback?(row)
+          row['Curriculum Assigned Optimal Status'] == 'FALSE' &&
+          row['Data Partition'] == 'test' &&
+          row['Curriculum Proposed Feedback'].blank? &&
+          row['Optional - AutoML Primary Feedback'].blank?
         end
       end
     end
--- services/QuillLMS/engines/evidence/spec/services/evidence/research/gen_ai/dataset_validator_spec.rb
+++ services/QuillLMS/engines/evidence/spec/services/evidence/research/gen_ai/dataset_validator_spec.rb
@@ -9,24 +9,11 @@ module Evidence
         subject { described_class.run(file:) }
 
         let(:file) { StringIO.new(content) }
-
-        let(:headers) do
-          [
-            'Curriculum Assigned Optimal Status',
-            'Data Partition',
-            'Optional - AutoML Label',
-            'Optional - AutoML Primary Feedback',
-            'Optional - AutoML Secondary Feedback',
-            'Optional - Curriculum Label',
-            'Curriculum Proposed Feedback',
-            'Optional - Highlight',
-            'Student Response'
-          ]
-        end
+        let(:headers) { DatasetImporter::HEADERS }
 
         context 'when the CSV content is valid' do
           let(:content) do
-            <<-CSV
+            <<~CSV
               #{headers.join(',')}
               TRUE,test,label1,primary_feedback1,secondary_feedback1,curriculum_label1,proposed_feedback1,highlight1,response1
               FALSE,test,label2,,secondary_feedback2,curriculum_label2,proposed_feedback2,highlight2,response2
@@ -38,9 +25,34 @@ module Evidence
           it { is_expected.to eq [] }
         end
 
+        context 'when the CSV content is missing required headers' do
+          let(:content) do
+            <<~CSV
+              Curriculum Assigned Optimal Status,Data Partition,Optional - AutoML Label,Optional - AutoML Primary Feedback
+              TRUE,test,label1,primary_feedback1
+            CSV
+          end
+
+          it { is_expected.to eq("CSV is missing required headers: Optional - AutoML Secondary Feedback, Optional - Curriculum Label, Curriculum Proposed Feedback, Optional - Highlight, Student Response.") }
+        end
+
+        context 'when the CSV content is missing a Student Response' do
+          let(:content) do
+            <<~CSV
+              #{headers.join(',')}
+              TRUE,test,label1,primary_feedback1,secondary_feedback1,curriculum_label1,proposed_feedback1,highlight1,
+              FALSE,test,label2,,secondary_feedback2,curriculum_label2,,highlight2,response2
+              TRUE,prompt,label3,primary_feedback3,secondary_feedback3,curriculum_label3,proposed_feedback3,highlight3,response3
+              FALSE,prompt,label4,primary_feedback4,secondary_feedback4,curriculum_label4,proposed_feedback4,highlight4,response4
+            CSV
+          end
+
+          it { is_expected.to eq(["Row 2: #{described_class::MISSING_STUDENT_RESPONSE_ERROR}", "Row 3: #{described_class::MISSING_FEEDBACK_ERROR}"]) }
+        end
+
         context 'when the CSV content is invalid' do
           let(:content) do
-            <<-CSV
+            <<~CSV
               #{headers.join(',')}
               TRUE,test,label1,primary_feedback1,secondary_feedback1,curriculum_label1,proposed_feedback1,highlight1,response1
               FALSE,test,label2,,secondary_feedback2,curriculum_label2,,highlight2,response2
