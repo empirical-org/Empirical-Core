83e3f4bf6ec6e31add258f6711af3e7596eba70e
Emilia Friedberg <emiliafriedberg@gmail.com>
2024-08-09 09:20:59 -0400

    update evidence onboarding slides

--- services/QuillLMS/client/app/bundles/Evidence/components/activitySlides/explanationSlide.tsx
+++ services/QuillLMS/client/app/bundles/Evidence/components/activitySlides/explanationSlide.tsx
@@ -15,15 +15,15 @@ export const ExplanationSlide = ({ slideData, onHandleClick }) => {
   }, [slideData])
 
   return(
-    <div className="explanation-slide-container">
+    <div className={`explanation-slide-container step-${step}`}>
       <section className="no-focus-outline" id="information-section" ref={containerRef} tabIndex={-1}>
-        <p aria-hidden={true} className="subtext">Good to know</p>
+        <p aria-hidden={true} className="good-to-know">Good to know</p>
         <section id="header-container">
           <h1>{header}</h1>
           {isBeta && <div id="beta-tag">BETA</div>}
         </section>
         <img alt={imageAlt} id="image" src={`${process.env.CDN_URL}/${imageUrl}`} />
-        <p className="subtext">{subtext}</p>
+        <p className="subtext" dangerouslySetInnerHTML={{ __html: subtext, }} />
       </section>
       <Footer buttonText={buttonText} onHandleClick={onHandleClick} step={step} />
     </div>
