1dc9cf9c74ee2df67f9505f547f1a018625aebe5
Peter Kong <pkong@quill.org>
2024-08-15 14:46:51 -0400

    Revert "add logging for bigquery issue"
    This reverts commit 127eac98448a1fd3944f36b756088460c25b5a9d.

--- services/QuillLMS/config/initializers/google/cloud/bigquery.rb
+++ services/QuillLMS/config/initializers/google/cloud/bigquery.rb
@@ -3,6 +3,5 @@
 require 'google/cloud/bigquery'
 
 Google::Cloud::Bigquery.configure do |config|
-  puts "bigquery credential: #{ENV.fetch('BIGQUERY_CREDENTIALS', '{}')}"
   config.credentials = JSON.parse(ENV.fetch('BIGQUERY_CREDENTIALS', '{}'))
 end
