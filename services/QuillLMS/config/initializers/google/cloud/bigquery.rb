127eac98448a1fd3944f36b756088460c25b5a9d
Peter Kong <pkong@quill.org>
2024-08-15 14:40:51 -0400

    add logging for bigquery issue

--- services/QuillLMS/config/initializers/google/cloud/bigquery.rb
+++ services/QuillLMS/config/initializers/google/cloud/bigquery.rb
@@ -3,5 +3,6 @@
 require 'google/cloud/bigquery'
 
 Google::Cloud::Bigquery.configure do |config|
+  puts "bigquery credential: #{ENV.fetch('BIGQUERY_CREDENTIALS', '{}')}"
   config.credentials = JSON.parse(ENV.fetch('BIGQUERY_CREDENTIALS', '{}'))
 end
