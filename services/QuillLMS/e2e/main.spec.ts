c6fecbe9b802c694eda0a9ac4f6a66f0263d2013
Peter Kong <pkong@quill.org>
2024-08-21 11:08:31 -0400

    update pw configs

--- services/QuillLMS/e2e/main.spec.ts
+++ services/QuillLMS/e2e/main.spec.ts
@@ -11,11 +11,13 @@ test('@login form submission with valid credentials', async ({ page }) => {
   await page.goto('http://localhost:5000/');
   await page.getByRole('link', { name: 'Log In' }).click();
   await page.getByLabel('Email or username').click();
+  await expect(page.getByLabel('Email or username')).toBeVisible();
   await page.getByLabel('Email or username').fill('johnnyci@gmail.com');
-  await page.getByLabel('Email or username').press('Tab');
+  //await page.getByLabel('Email or username').press('Tab');
   await page.getByLabel('Password', { exact: true }).fill('password');
   // Other clickable items on this page include the text 'Log in', so we need a more
   // sensitive element selector
+  await expect(page.locator('#log-in')).toBeVisible()
   await page.locator('#log-in').click();
   await page.waitForURL('http://localhost:5000/teachers/classrooms/dashboard');
 
--- services/QuillLMS/playwright.config.ts
+++ services/QuillLMS/playwright.config.ts
@@ -10,6 +10,8 @@ import { defineConfig, devices } from '@playwright/test';
 /**
  * See https://playwright.dev/docs/test-configuration.
  */
+
+
 export default defineConfig({
   testDir: './e2e',
   /* Run tests in files in parallel */
@@ -25,11 +27,14 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: 'http://127.0.0.1:3000',
+    baseURL: 'http://127.0.0.1:5000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
   },
+
+  // per-test timeout, in ms
+  timeout: 90 * 1000,
 
   /* Configure projects for major browsers */
   projects: [
