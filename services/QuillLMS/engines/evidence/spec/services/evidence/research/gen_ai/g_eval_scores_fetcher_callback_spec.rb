373d016d212781814cfa6560b61ec9f49cec3ed4
Brendan Shean <brendanshean@gmail.com>
2024-08-22 15:17:58 -0700

    Add GEvalScoresFetcherCallback spec

--- services/QuillLMS/engines/evidence/spec/services/evidence/research/gen_ai/g_eval_scores_fetcher_callback_spec.rb
+++ services/QuillLMS/engines/evidence/spec/services/evidence/research/gen_ai/g_eval_scores_fetcher_callback_spec.rb
@@ -1,57 +1,80 @@
 # frozen_string_literal: true
 
 require 'rails_helper'
-require 'sidekiq-pro'
 
 module Evidence
   module Research
     module GenAI
-      RSpec.describe GEvalScoresFetcher do
-        subject { described_class.run(trial) }
+      RSpec.describe GEvalScoresFetcherCallback do
+        subject { described_class.new }
 
         let(:trial) { create(:evidence_research_gen_ai_trial) }
-
-        let!(:llm_examples) { create_list(:evidence_research_gen_ai_llm_example, 3, trial:) }
-        let(:llm_example_ids) { llm_examples.pluck(:id) }
-
+        let(:llm_examples) { create_list(:evidence_research_gen_ai_llm_example, 2) }
         let(:g_evals) { create_list(:evidence_research_gen_ai_g_eval, 2) }
         let(:g_eval_ids) { g_evals.pluck(:id) }
-
-        let(:completion_callback_class) { GEvalScoresFetcherCallback }
-        let(:sidekiq_batch) { instance_double('Sidekiq::Batch') }
-        let(:worker) { RunGEvalWorker }
+        let(:options) { { 'trial_id' => trial.id, 'llm_example_ids' => llm_examples.pluck(:id) } }
 
         before do
-          allow(trial).to receive(:g_eval_ids).and_return(g_eval_ids)
-          allow(::Sidekiq::Batch).to receive(:new).and_return(sidekiq_batch)
-          allow(sidekiq_batch).to receive(:on)
-          allow(sidekiq_batch).to receive(:jobs).and_yield
+          trial.update!(g_eval_ids:)
+          trial.update!(evaluation_start_time: Time.zone.now)
+          allow(Trial).to receive(:find).with(options['trial_id']).and_return(trial)
         end
 
-        describe '#run' do
-          it 'creates a new Sidekiq::Batch' do
-            expect(::Sidekiq::Batch).to receive(:new)
-            subject
-          end
-
-          it 'sets up the completion callback' do
-            expect(sidekiq_batch).to receive(:on).with(
-              :complete,
-              completion_callback_class,
-              trial_id: trial.id,
-              llm_example_ids:
-            )
-            subject
-          end
-
-          it 'enqueues jobs for each combination of g_eval_id and llm_example_id' do
-            g_eval_ids.each do |g_eval_id|
-              llm_example_ids.each do |llm_example_id|
-                expect(worker).to receive(:perform_async).with(trial.id, g_eval_id, llm_example_id)
+        describe '#on_complete' do
+          context 'when all GEvalScores are present' do
+            before do
+              g_evals.each do |g_eval|
+                llm_examples.each do |llm_example|
+                  create(:evidence_research_gen_ai_g_eval_score, trial:, g_eval:, llm_example:)
+                end
               end
             end
 
-            subject
+            it 'updates the trial results and evaluation duration' do
+              expect(trial).to receive(:update_results!).with(g_evals: kind_of(Hash))
+              expect(trial).to receive(:update!).with(evaluation_duration: kind_of(Float))
+
+              subject.on_complete(nil, options)
+            end
+
+            it 'calculates the correct g_evals hash' do
+              allow(trial).to receive(:update_results!) do |args|
+                expect(args[:g_evals].keys).to match_array(g_evals.pluck(:id))
+                expect(args[:g_evals].values).to all(be_an(Array))
+                expect(args[:g_evals].values.flatten).to all(be_a(Integer))
+              end
+
+              subject.on_complete(nil, options)
+            end
+          end
+
+          context 'when some GEvalScores are missing' do
+            before do
+              create(
+                :evidence_research_gen_ai_g_eval_score,
+                trial:,
+                g_eval: g_evals.first,
+                llm_example: llm_examples.first
+              )
+            end
+
+            it 'includes nil for missing scores' do
+              allow(trial).to receive(:update_results!) do |args|
+                expect(args[:g_evals].values.flatten).to include(nil)
+              end
+
+              subject.on_complete(nil, options)
+            end
+          end
+
+          it 'calculates the evaluation duration correctly' do
+            start_time = 1.hour.ago
+            allow(trial).to receive(:evaluation_start_time).and_return(start_time.to_s)
+            allow(Time.zone).to receive(:now).and_return(Time.zone.now)
+
+            expect(trial).to receive(:update!).with(evaluation_duration: be_within(1).of(3600))
+
+            subject.on_complete(nil, options)
           end
         end
       end
