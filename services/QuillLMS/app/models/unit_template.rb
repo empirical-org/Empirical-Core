f25bd0eab6c2e9c5d927f1f9f1fc725410fcbf39
Peter Kong <pkong@quill.org>
2024-08-22 15:51:27 -0400

    update serialization declaration

--- services/QuillLMS/app/models/unit_template.rb
+++ services/QuillLMS/app/models/unit_template.rb
@@ -31,7 +31,7 @@ class UnitTemplate < ApplicationRecord
   has_many :partner_contents, dependent: :destroy, as: :content
   has_many :recommendations, dependent: :destroy
   has_many :diagnostics_recommended_by, :through => :recommendations, :source => :activity
-  serialize :grades, :array
+  serialize :grades, type: Array
 
   validates :flag,
     inclusion: { in: Flags::FLAGS },
