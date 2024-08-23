4638e2639c62113477f7a0fcf7ce763dc8b2995c
Peter Kong <pkong@quill.org>
2024-08-23 14:01:18 -0400

    debug

--- services/QuillLMS/e2e/main.spec.ts
+++ services/QuillLMS/e2e/main.spec.ts
@@ -1,13 +1,13 @@
 import { test, expect } from '@playwright/test';
 
-test('has visible footer', async ({ page }) => {
+test.skip('has visible footer', async ({ page }) => {
   await page.goto('http://localhost:5000');
 
   await expect(page.locator('footer')).toBeVisible();
 });
 
 
-test.skip('@login form submission with valid credentials', async ({ page }) => {
+test('@login form submission with valid credentials', async ({ page }) => {
   await page.goto('http://localhost:5000/session/new');
 
   await page.getByLabel('Email or username').click();
@@ -21,8 +21,10 @@ test.skip('@login form submission with valid credentials', async ({ page }) => {
   await page.waitForSelector('#log-in');
   await expect(page.locator('#log-in')).toBeVisible()
   await page.locator('#log-in').click();
+  await page.waitForTimeout(1000);
+  // await page.mouse.wheel(0,200)
 
-  await page.screenshot({ path: 'test-results/playwright_screenshot.png' });
+  await page.screenshot({ path: 'test-results/playwright_login.png', fullPage: true });
 
   await page.waitForURL('http://localhost:5000/teachers/classrooms/dashboard');
 });
