f090b755b8ef28fbd18afe6f9cb859c85c30277b
Peter Kong <pkong@quill.org>
2024-08-26 14:31:41 -0400

    move timeout before click

--- services/QuillLMS/e2e/main.spec.ts
+++ services/QuillLMS/e2e/main.spec.ts
@@ -37,8 +37,10 @@ test('@login form submission with valid credentials', async ({ page }) => {
   // sensitive element selector
   await page.waitForSelector('#log-in');
   await expect(page.locator('#log-in')).toBeVisible()
-  await page.locator('#log-in').click();
+
   await page.waitForTimeout(1000);
+  await page.locator('#log-in').click();
+
   // await page.mouse.wheel(0,200)
 
   await page.screenshot({ path: 'test-results/playwright_login.png', fullPage: true });
