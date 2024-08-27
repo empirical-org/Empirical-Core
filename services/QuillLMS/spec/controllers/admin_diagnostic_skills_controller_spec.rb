29812511e83b50316f73de8276eb2ef62ca65d27
Peter Kong <pkong@quill.org>
2024-08-27 15:53:42 -0400

    Revert "Revert "rails 7.1 (#12270)" (#12296)"
    This reverts commit 172d2d39ea89f2684bed49002b0ac696c07ca3eb.

--- services/QuillLMS/spec/controllers/admin_diagnostic_skills_controller_spec.rb
+++ services/QuillLMS/spec/controllers/admin_diagnostic_skills_controller_spec.rb
@@ -62,7 +62,7 @@ describe AdminDiagnosticSkillsController, type: :controller do
             teacher_ids: teacher_ids,
             classroom_ids: classroom_ids
           }
-        )
+        ).and_return(:example_response)
 
         post :report, params: { query: query_name, group_by: group_by, diagnostic_id: diagnostic_id, timeframe: timeframe_name, school_ids: school_ids, grades: grades, teacher_ids: teacher_ids, classroom_ids: classroom_ids }
       end
