63d6960879d94a85004d5ebe6b0110f92405b7f7
Brendan Shean <brendanshean@gmail.com>
2024-08-29 09:24:14 -0700

    Update datasets#create to accomodate data subset creation

--- services/QuillLMS/engines/evidence/app/controllers/evidence/research/gen_ai/datasets_controller.rb
+++ services/QuillLMS/engines/evidence/app/controllers/evidence/research/gen_ai/datasets_controller.rb
@@ -9,13 +9,10 @@ module Evidence
         end
 
         def create
-          @dataset = stem_vault.datasets.new(dataset_params)
-
-          if @dataset.save
-            DatasetImporter.run(dataset: @dataset, file:)
-            redirect_to @dataset
-          else
-            render :new
+          if file_upload?
+            create_dataset_from_file
+          elsif data_subset?
+            redirect_to DataSubsetBuilder.run(parent_id:, test_example_ids:)
           end
         end
 
@@ -25,9 +22,32 @@ module Evidence
           @trials = @dataset.trials.order(id: :desc)
         end
 
-        private def dataset_params = params.require(:research_gen_ai_dataset).permit(:file)
-        private def file = dataset_params[:file]
         private def stem_vault = @stem_vault ||= StemVault.find(params[:stem_vault_id])
+
+        private def create_dataset_from_file
+          dataset = stem_vault.datasets.new
+
+          if dataset.save
+            DatasetImporter.run(dataset:, file:)
+            redirect_to dataset
+          else
+            render :new
+          end
+        end
+
+        private def file_upload? = file.present?
+        private def file = dataset_params[:file]
+        private def dataset_params = params.require(:research_gen_ai_dataset).permit(:file)
+
+        private def data_subset? = test_example_ids.present? && parent_id.present?
+        private def test_example_ids = data_subset_params[:test_example_ids]
+        private def parent_id = data_subset_params[:parent_id]
+
+        private def data_subset_params
+          params
+            .require(:research_gen_ai_dataset)
+            .permit(:parent_id, test_example_ids: [])
+        end
       end
     end
   end
