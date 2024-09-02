c3b60f608a3c1122cc4dc0c7526bd13f9a3dbcb4
Quill <quill@Cissys-MacBook-Pro.local>
2024-09-03 00:25:40 +0800

    empty line removed

--- services/QuillLMS/app/controllers/static_controller.rb
+++ services/QuillLMS/app/controllers/static_controller.rb
@@ -3,7 +3,6 @@
 class StaticController < ApplicationController
   def manifest
     manifest_path = Rails.root.join('config/manifest.json')
-
     send_file manifest_path, type: 'application/json', disposition: 'inline'
   end
 end
