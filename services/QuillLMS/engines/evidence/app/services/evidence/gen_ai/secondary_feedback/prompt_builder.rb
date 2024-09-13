12adedb54fd9d4c780b5b57d2cba7de1973d177c
Daniel Drabik <dan.drabik@gmail.com>
2024-09-13 14:54:55 -0400

    Trial runs.

--- services/QuillLMS/engines/evidence/app/services/evidence/gen_ai/secondary_feedback/prompt_builder.rb
+++ services/QuillLMS/engines/evidence/app/services/evidence/gen_ai/secondary_feedback/prompt_builder.rb
@@ -5,15 +5,15 @@ module Evidence
     module SecondaryFeedback
       class PromptBuilder < Evidence::GenAI::PromptBuilder
         TEMPLATE_FOLDER = 'app/services/evidence/gen_ai/secondary_feedback/prompts/'
-        DEFAULT_TEMPLATE = '2024_07_17_with_highlight.md'
-        # DEFAULT_TEMPLATE = '2024_09_12_gemini.md'
-        EXAMPLE_LIMIT = 20
+        DEFAULT_TEMPLATE = '2024_08_13_more_instructions.md'
+        EXAMPLE_LIMIT = 50
 
         private def template_variables
           {
             highlight_texts:,
             primary_secondary_examples:,
-            plagiarism_text:
+            plagiarism_text:,
+            stem:
           }
         end
 
@@ -24,7 +24,7 @@ module Evidence
         # private def feedback_data_json = feedback_data.map { |f| {original_feedback: strip_tags(f.primary), secondary_feedback: strip_tags(f.secondary)}.to_json }
 
         private def primary_secondary_examples = markdown_table_rows(feedback_data_tuples)
-        private def feedback_data_tuples = feedback_data.map { |f| [strip_tags(f.primary), strip_tags(f.secondary)] }
+        private def feedback_data_tuples = feedback_data.map { |f| [f.primary, f.secondary] }
 
         private def conjunctions = [prompt.conjunction]
         private def limit = EXAMPLE_LIMIT
