0b1c84e4098d94e1c57e45d229fc356a19965a74
Peter Kong <pkong@quill.org>
2024-09-17 08:45:46 -0400

    Revert "update jests"
    This reverts commit 3d37c08a5ab8773f0d66a55d00e5ef84e526eb6f.

--- services/QuillLMS/client/app/bundles/PremiumHub/components/__tests__/subnav_tabs.test.tsx
+++ services/QuillLMS/client/app/bundles/PremiumHub/components/__tests__/subnav_tabs.test.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { MemoryRouter } from 'react-router-dom';
+import { MemoryRouter } from 'react-router';
 import { render, screen, waitFor } from "@testing-library/react";
 import userEvent from '@testing-library/user-event';
 
