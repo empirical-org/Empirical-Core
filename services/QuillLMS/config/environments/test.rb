07633d6a4fd59394453beb5a4423f9d61b7e1ab5
Brendan Shean <brendanshean@gmail.com>
2024-09-24 11:47:06 -0700

    Fix dataset validation flow when save fails

--- services/QuillLMS/config/environments/test.rb
+++ services/QuillLMS/config/environments/test.rb
@@ -39,7 +39,6 @@ EmpiricalGrammar::Application.configure do
   config.action_mailer.default_url_options = { :host => 'test.yourhost.com' }
 
   config.log_level = :debug
-  config.logger = Logger.new($stdout)
 
   # Used for zeitwerk
   config.eager_load = ENV['CI'].present?
