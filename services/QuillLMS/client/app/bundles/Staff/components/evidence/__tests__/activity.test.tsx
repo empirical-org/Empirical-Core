30960285926727a28a209c7c03e74d94838df3cc
Emilia Friedberg <emiliafriedberg@gmail.com>
2024-10-18 15:20:26 -0400

    fix test

--- services/QuillLMS/client/app/bundles/Staff/components/evidence/__tests__/activity.test.tsx
+++ services/QuillLMS/client/app/bundles/Staff/components/evidence/__tests__/activity.test.tsx
@@ -2,13 +2,20 @@ import { mount } from 'enzyme';
 import * as React from 'react';
 import { MemoryRouter, NavLink } from 'react-router-dom';
 const { firstBy } = jest.requireActual('thenby');
+import { QueryClientProvider } from 'react-query';
+
+import { DefaultReactQueryClient } from '../../../../Shared/index';
+
+const queryClient = new DefaultReactQueryClient();
 
 import Activity from '../activity';
 
 describe('Activity component', () => {
   const container = mount(
     <MemoryRouter>
-      <Activity />
+      <QueryClientProvider client={queryClient} contextSharing={true}>
+        <Activity />
+      </QueryClientProvider>
     </MemoryRouter>
   );
 
